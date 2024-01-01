3db6220ef6fd15fca2740bb9c8e156839086eb19
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:56 +0000

    either morning accordingto finish earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def reverse_list(lst):
-        return lst[::-1]
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
