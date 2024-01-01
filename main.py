138f814f94594351be030a7576345b64513644f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:45 +0000

    call platform free fit public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
