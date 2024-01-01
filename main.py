eb755464062b2243558b915f5c56d0d1b6b0e8cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:19:16 +0000

    naturally equal fairly similar accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
