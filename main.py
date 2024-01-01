2fbd9feb4f0e1be723daf074aaf482dcf17e324d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:49:43 +0000

    bang tonight apparently minimum hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import sys
+def exit_program():
+        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
