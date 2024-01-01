0f636f94a3c097ddd36decd1d90c4bcfa8392260
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:03 +0000

    much apply couple forgive talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
