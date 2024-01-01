644a3fb3912afd3c4d987d76f5db357ccbe52cf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:07:55 +0000

    please various alone lock steer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-print([x**2 for x in range(10)])
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
