eb4cd17789354941e86f3e63647547eacc90d129
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:47 +0000

    extremely system very dark region

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+print(sum(range(10)))
