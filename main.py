0a92360bef8fe809ec2afd37c8a7d85d94a74db8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:54 +0000

    brave fashion 569enjoy convincing early

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
