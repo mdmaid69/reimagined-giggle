e9d427ae2efa2a311975e8958b972cd71c274bdd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:46:45 +0000

    bottom rise hold narrow regional

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_string(s):
+        return s[::-1]
