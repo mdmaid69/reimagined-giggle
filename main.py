1177db3c8b5c29451b8c168dd6548ef4b746a621
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:34:13 +0000

    mother fear adjective opportunity plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
