c10f3c9514c5fe98de949b7b796f50be229aa207
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:10 +0000

    mood important picture jump advise

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
