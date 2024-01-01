9af5e2a04099ca5ae3d19e21c97abc8d1cb8d20d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:17 +0000

    score take design bread little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
