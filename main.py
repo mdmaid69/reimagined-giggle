776f17629c0e338649aac78cb61fabee649a8e46
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:27 +0000

    Ww law Ww road intelligent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-print(math.pi)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
