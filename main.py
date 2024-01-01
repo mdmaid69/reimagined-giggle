eabf01a9218984d8b5a2672ff1b7ea101ed91605
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:26:18 +0000

    often market talk clue criticize

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
