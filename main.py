97b64a3768b3c01d66c88a88b21ab413559788ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:22:00 +0000

    soak we friend solve callback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
