d52305cd427b22d8f55caf30594f7f2dce497d53
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:54 +0000

    happy rare start sky by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
