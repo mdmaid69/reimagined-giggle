51d09f4b738709b9618f235a0d03d68a16725056
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:59 +0000

    close thought last interview ago

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import sys
+def print_python_version():
+        return sys.version
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
