3ad4ec1a183f4469b810085c96f26633249a36d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:23 +0000

    other scared sale dare among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
