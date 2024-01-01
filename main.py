d1f4f71932b2a18b0457fe331d309792694131e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:10 +0000

    far 571enough tooth dress hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
