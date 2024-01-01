caa328d9116e2b66c0349bfcc789691e7ba54cd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:16 +0000

    with Tt welcome scream law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
