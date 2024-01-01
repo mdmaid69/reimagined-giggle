9baa271065e26b9a0a839e99abb3f2e82b148459
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:41:33 +0000

    fond mind news skill black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
