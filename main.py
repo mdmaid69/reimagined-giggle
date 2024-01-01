fdbb91c4f833013e27694c25e71b5cd1c1c4c338
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:18 +0000

    ashamed more lock exhibition myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def find_unique_words(sentence):
+        return set(sentence.split())
+import platform
+def get_os_info():
+        return platform.uname()
