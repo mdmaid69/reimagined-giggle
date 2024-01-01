ae6b793bdc262429c74c367683661a66b6510df6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:55 +0000

    illness reduce support sting treatment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
