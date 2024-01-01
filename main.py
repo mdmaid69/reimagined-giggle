44f9a715f639affa6ca27c4b854738b689e064f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:39 +0000

    above slow charge confidence pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
