1e111bb7c228c185084af8dcbd15056fd62cd777
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:36 +0000

    instrument turnon name mixture tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(lst):
-        return max(lst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
