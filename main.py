eab9cdbd6a908d00f5ab42b4652547a1d857321b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:35:52 +0000

    record polite keepaway throw there

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
