77382d7aec6f80a48176e7124cd8090dda9c34f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:03:21 +0000

    reply broken bring operate contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def subtract_numbers(x, y):
+        return x - y
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
