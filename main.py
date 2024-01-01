7f9fb761c0557d0d2f8b245e8a716974462fa0bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:51 +0000

    shadow 564encourage Rr house ear

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
