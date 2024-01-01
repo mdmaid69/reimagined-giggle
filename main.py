5bce813f403be1870877d8994cb12dbda453ce9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:41 +0000

    clever marry slight school solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
