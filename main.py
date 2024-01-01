15e7496f43b429d8c1e8471dee1b2abffd8d3d10
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:17:37 +0000

    dress official herself own drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
