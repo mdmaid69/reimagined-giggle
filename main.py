faaa8c81ec00742ba11faae88752cb8477d1442a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:59:29 +0000

    inside dear way collection pay

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
