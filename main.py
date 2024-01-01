65bd08a81e7e1fa6fe8381f44108e277607194af
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:12 +0000

    declare deliver angry concerning contrast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
