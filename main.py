6e22f1b5c068200be81d56c64bb6c345b469f141
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:05 +0000

    report since stretch it excellent

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
