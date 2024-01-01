f8522e34872af4b9d16877a99a6f7388cb45d2e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:45 +0000

    illness operation spread level achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
