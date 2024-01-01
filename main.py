70bdf18a448fd8620a8caee016c1dcf32133f77d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:49:48 +0000

    sight maximum ground habit afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+x = 10
+y = 20
+print("Sum:", x + y)
