a233f9a6e3058f36b6a2521e4d041309e9e0e55d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:17 +0000

    best fire please estimate bet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
