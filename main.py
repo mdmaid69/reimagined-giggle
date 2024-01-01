b13e5ad92bc166e90c022e42f118504a0871dc62
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:12:42 +0000

    improve share room range discuss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
