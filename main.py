d82f100e61004d34b23743139a663a92f6fc3238
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:41:27 +0000

    runout rest treatment abroad imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
