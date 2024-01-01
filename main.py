066d7b9132e9a146d824c251e33a23e4900879ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:51 +0000

    book mean school successful steep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
