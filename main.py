7ea97a4577229256a65b7093b5a2011d49a20b47
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:51 +0000

    favourite control market team mine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import time
-print(time.time())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
