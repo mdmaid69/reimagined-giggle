bffa6bd70f61f83cbf75e605ddf3eacfb60e8518
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:34 +0000

    commit stir begin hate punishment

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
