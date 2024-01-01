25a82b97a7a079cd841ff402a813bcd86fe964e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:05:05 +0000

    material attitude ashamed face punish

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
