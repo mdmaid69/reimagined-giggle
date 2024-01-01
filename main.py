c2a1d2c9829f9dedfc0a2e4e9d30856098b21444
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:55:31 +0000

    continue apparently team work squash

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_time_since_epoch():
+        return time.time()
