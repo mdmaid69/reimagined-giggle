2463121a80526bff465fb87b32831980827f1da3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:59:11 +0000

    guard wide cold conversation switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
