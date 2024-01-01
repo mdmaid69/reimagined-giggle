eecd68ed8f27a431521c0a5accc9e54339a9acec
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:50 +0000

    range that disturbing sure otherwise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_set(array):
+        return set(array)
