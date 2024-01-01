586876093e03ddf92492eea47329d580dfefd8bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:28:06 +0000

    direct want dry almost much

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
