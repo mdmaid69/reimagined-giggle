05fd9960a021a6ef8ba0224b9cc885e2f646c3c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:34 +0000

    control concentration kill empty disturb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
