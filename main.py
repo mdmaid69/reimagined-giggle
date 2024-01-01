ddb8c4030c65067bd83cf58f11ba5d7199d7ac00
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:28:41 +0000

    benefit together make broad goout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
