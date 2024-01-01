eeeb03b213fa18ffd6faee25d20bd795d63f1fad
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:29 +0000

    reasonable performance official crash that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
