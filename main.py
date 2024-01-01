65be48e4068c1a171e2fcc603d6eae8dfc43c1f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:05 +0000

    work moral wheel himself regard

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
