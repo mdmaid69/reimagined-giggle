8cda9d92d991026b3f46ef1d28528faf3add1921
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:29:46 +0000

    rubbish disagree miss careless embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
