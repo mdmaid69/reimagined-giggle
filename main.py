d56031b7f1d2eae74c742573fe8c793dc2853639
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:43:56 +0000

    such use measure contribute ground

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
