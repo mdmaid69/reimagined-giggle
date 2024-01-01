8d4fdf9aaca9b61c06b101e00a602fc5987ebc36
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:49:31 +0000

    memory noise handle well their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
