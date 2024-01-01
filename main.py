750a0a487cf1ba6e016b742a6c6efe9af1c9a17f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:57:00 +0000

    post model surprised free holiday

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-from collections import Counter
-print(Counter("hello world"))
-import socket
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import threading
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
