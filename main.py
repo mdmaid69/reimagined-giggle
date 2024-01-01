29e9196c2d08e47d1606fa814ab7c5e510cea014
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:57:42 +0000

    suit talkoutof survive the punishment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
