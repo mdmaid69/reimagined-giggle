b87898aeb1292c0a75d94e534e44248bdfb8ce96
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:18:46 +0000

    mix fasten goodbye plastic goup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
