fb8bfd7082a2008caa13814d714d73f2fa64fdfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:50:40 +0000

    child little unusual score house

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
