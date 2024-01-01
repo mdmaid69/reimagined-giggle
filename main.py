d46d96cfbf2e5906c020cce463907e22bb93ff0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:24 +0000

    shape watch instrument failure purpose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_repr(array):
+        return repr(array)
