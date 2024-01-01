d28ca2644737c32822bfee7b1bf31b29b6c7ecad
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:42 +0000

    nature switch stone top dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
