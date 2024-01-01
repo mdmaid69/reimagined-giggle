ccdf3c59ffef882c31503b79809e19f0dbcaf566
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:42 +0000

    give soon bridge failure play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
