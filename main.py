bdcd24cf490aec6b9685da88ff316e52a6ff5b7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:34:31 +0000

    smile thirsty approve fold perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
