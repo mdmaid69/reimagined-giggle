b37975c199b420a497117d3a05e28ff67e927529
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:06 +0000

    cry journey shadow cold while

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
