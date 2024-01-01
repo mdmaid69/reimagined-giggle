d0f9f31c44baa0763e567e0315ed1783f29bd077
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:23 +0000

    Mm sight present sense lift

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
