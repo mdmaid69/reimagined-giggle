6fbae8aee95f744e65953ea24cf029b17ebdf099
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:54 +0000

    notice control car payment worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+for i in range(10): print(i)
