0c6aaa955045ef785e02620c2fd011de2d7ab263
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:12 +0000

    smile square makeinto impossible start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
