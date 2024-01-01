5e73be87c9d7507b5c9c45a535a6cf2f31b52bbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:12:11 +0000

    clear buy takeaway bread workout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
