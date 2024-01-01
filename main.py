8d56866cfaaab96b0827fcef39db96b9261d1fde
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:42:06 +0000

    unless communication organize taste mainly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
