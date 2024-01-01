c7999311df07a4ef4cef633e3900404bd56aa80e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:44:07 +0000

    comeround listenfor height platform rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
