daca945439c22cc165ccfc4adf0024cd141b65d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:08:06 +0000

    nowhere wander trick objection instant

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
