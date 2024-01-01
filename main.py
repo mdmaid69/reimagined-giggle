95b89512823e344e526e7af0d6bf858ceb28cf2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:29 +0000

    glass nigga fit common no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
