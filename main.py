bd77035952e3133b5fed14ab8b0d441bb2724522
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:47 +0000

    too deserve murder fortunate avoid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+name = "Python"
+print("Hello,", name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
