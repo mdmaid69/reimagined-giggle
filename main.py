faf70132a3671d1e0470aa98c861fe74442e8f7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:00 +0000

    container sound eye best card

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
