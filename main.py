9e73a43a6e977bee7e9a2941f73282dedf74ec33
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:40 +0000

    570enjoyable exercise principle steep music

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
