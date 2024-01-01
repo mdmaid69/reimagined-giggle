db6875fef7eb9faade5fb567b43ab3872c6f697f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:04:04 +0000

    past provide address shadow equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def count_elements(lst):
+        return len(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
