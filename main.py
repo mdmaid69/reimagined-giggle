236e9739edc0f8ec7bb698439aa0c60c7923ae7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:47 +0000

    bet turnup from second count

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
