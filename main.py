ef936d8865d0cd62e5fc484252f1ee48cad4a3c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:14 +0000

    Uu deep foot sticky autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
