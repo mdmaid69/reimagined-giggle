4aa46a552a7cf759db4e66d9f3e7992f997fabf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:02 +0000

    belief discovery until workout shape

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
