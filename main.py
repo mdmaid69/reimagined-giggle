d0596f1772a22d3380cc700cab0ffc2ee1379b71
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:53:46 +0000

    if tired kill object sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
