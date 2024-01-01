f2cf0c351117506ddc0d29ed029d6d5c8b3244e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:50 +0000

    shade recently Ll birthday Hh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
