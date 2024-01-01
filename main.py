7731492bdec2cb6306d2e580eace0dbeac634140
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:10:08 +0000

    study cap think result sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def find_max(lst):
-        return max(lst)
+  def split_path(path):
+        return os.path.split(path)
