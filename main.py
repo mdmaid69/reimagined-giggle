fbd5bd1cc59bf505897020768d9762ae94238e0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:08:31 +0000

    manage rather excuse loose upwards

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_max(lst):
+        return max(lst)
