44f7f792638e61312dd791bb9830787cbed2852f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:59 +0000

    air result desperate effect cheat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
