d7723a5ea6c54d8ec4f673ab7ff6b4d8d6259a55
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:19:01 +0000

    yours whenever slight hit forever

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
