c11044e6b3e69eaa6264d6d65b834a0fd31676f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:18 +0000

    straight uniform lock comeover turnon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
