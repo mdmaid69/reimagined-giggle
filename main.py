69c938a1b793afc52e2894d6cd646e8a0c7baa3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:14 +0000

    able will smell by cruelty

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
