246e6f4ccd545ff43e390ff9270c2c261cb08606
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:25 +0000

    children form part edge gap

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
