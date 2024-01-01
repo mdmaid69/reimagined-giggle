0364bb3496255dacc5e9efea4f14615484531f51
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:02 +0000

    swing your 570enjoyable very arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_str(array):
+        return str(array)
