82eb2855ddcb4f3bae67302e8fa908e0dd182d86
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:55:35 +0000

    grow gather ilovedicks Oo union

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
