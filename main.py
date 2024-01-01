3671698b68c1e1f60db278338867b6d158f298f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:18:30 +0000

    damage agree lay waste ashamed

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
