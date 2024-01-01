30437caecb348e23700ffdc580068bde400c14c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:02:50 +0000

    crash ear seat dance match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
