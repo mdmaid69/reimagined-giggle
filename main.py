24ce05c715066ee86292454a7a946db357bc45ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:10:11 +0000

    permission central nowhere pick price

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
 def find_max(lst):
         return max(lst)
