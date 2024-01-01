a628373f16cceb78a7657911b0bba157a0555b4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:03:27 +0000

    keen bit size corner concentrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
