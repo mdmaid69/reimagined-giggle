7f8d29e58608fcde3d71e15bd3407eb7136f055c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:32:59 +0000

    year properly arrange emphasize sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
