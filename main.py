2f1924ce985e096aa9b466d73c72a202f7cf10c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:37 +0000

    special emphasis their responsibility shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
