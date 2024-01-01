a75f42dd3425289635b4fc63967130d21f2daff3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:29 +0000

    raw who just apparently appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
