43cf300b791ed5b2a416be4008d58338788a2354
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:07 +0000

    turnover normal understand matter impressive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import collections
-def create_priority_queue():
+def create_stack():
         return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
