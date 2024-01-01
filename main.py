4f491b8620944f1c1d50722c26ac4a247726a9ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:02 +0000

    noise somebody seem try next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def remove_directory(path):
+        os.rmdir(path)
