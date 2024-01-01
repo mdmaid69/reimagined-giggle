f8f0e7fdce5c9557170dd11170dfd4e6c9ec4bfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:27:32 +0000

    Bb formal dream pleasure value

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
