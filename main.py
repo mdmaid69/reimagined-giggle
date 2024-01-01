b02f76de7abd7ecd68756c3408972c7cacbd1321
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:11 +0000

    against road priority replace keepoff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
