6a3104d8cd6a4b646844bbd2d88c75eb6121650b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:34 +0000

    boil conversation blow impressed appear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
