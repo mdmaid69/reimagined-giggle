3580e5a44d43fc61e9c6d96757f328597ef9d9f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:43:24 +0000

    unless speak nowhere special muscle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_typecode(array):
+        return array.typecode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
