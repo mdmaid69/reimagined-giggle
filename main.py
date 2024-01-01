7e80260c55a3c8f2016a960e6e707ad74c830057
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:44:33 +0000

    love since space always unkind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-def find_max(lst):
-        return max(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  def convert_to_octal(n):
+        return oct(n)
