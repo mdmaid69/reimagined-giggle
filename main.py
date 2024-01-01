a1c67625caf033b0a9556a10f4464588c3da7b00
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:23:59 +0000

    exploration effort black fill nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
