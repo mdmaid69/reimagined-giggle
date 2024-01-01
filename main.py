6e4c3c6e235f6fd9319a43d11b87bb2c85111a05
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:27 +0000

    me comeover pronounce disappear odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
