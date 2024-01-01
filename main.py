2b21f2cf3fa610f88e6118989000b2a291c59c55
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:03 +0000

    eye among vision rest name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
