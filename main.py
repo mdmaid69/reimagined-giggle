4d6fe09eb84bbcbce06bc966049b9c4fd5bd5869
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:23 +0000

    imagine runover coffee inform urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
