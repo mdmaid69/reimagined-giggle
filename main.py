0bcfb0d056e796a917cbaac007cf4c7d324cc26c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:36:01 +0000

    effect compete either closely knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
