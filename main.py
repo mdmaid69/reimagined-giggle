058ebb4906066a1f8770d8b3bd489a9a92b9ad5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:24:02 +0000

    pinch area part add follow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_work(force, distance):
+        return force * distance
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
