6fa2ad255c225d2001695e090d2eb409daebc4dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:30:19 +0000

    respect grand admire whole reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
