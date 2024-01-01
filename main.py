2c95b497ee770b18e76d62c7ba94906c3130c9be
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:01 +0000

    personal generous pleased fair stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
