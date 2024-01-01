01c601fbe37bda1f38a5ee13e2bb2143d51c090e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:44 +0000

    reserve duty container attract place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
