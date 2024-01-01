32ff2050e633ca85923f8e2fe0dcfecaf26ca74a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:00:55 +0000

    nervous over date foreign 573entertainment

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
