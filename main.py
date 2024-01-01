944e1712da21df0b042556bd1c856d600aea114c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:12:56 +0000

    use pump announce well appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
