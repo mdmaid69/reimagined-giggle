e29fa9dfba5adab32e8c7bab924726ece4190c45
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:05 +0000

    fast surface cry grand use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
