ff33ad26203b40594ce61470dcfa92e460f4625e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:47 +0000

    catch happen bother gate risk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
