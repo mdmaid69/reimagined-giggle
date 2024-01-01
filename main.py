8247238a4fedb5a9d576e0353bf4ca7084ef20d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:00:26 +0000

    night fold explain along copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def convert_to_binary(n):
-        return bin(n)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
