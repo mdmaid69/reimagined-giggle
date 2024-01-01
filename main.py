844fca41ae1773aae6aa260ba7d60f25f63edef1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:08:10 +0000

    neither official ever allow activity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def reverse_array(array):
+        array.reverse()
