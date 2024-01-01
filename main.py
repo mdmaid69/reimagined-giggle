3887f7e836ae78d9926b8762adca98d379082600
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:28:32 +0000

    match temper trousers attach wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
