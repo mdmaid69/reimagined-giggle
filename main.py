5d0ceb17dc6a9c996893864a93e054f81ecbd2fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:09 +0000

    mark safe commit smooth base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
