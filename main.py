c4db8d493b538321b164bbd5c61d4c410c445bfc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:47 +0000

    likely spend successful formal reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
