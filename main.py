1e59a2682bd953ed6910b763c10abc91ed6ec6f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:50:25 +0000

    recognize take fat fork fact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
