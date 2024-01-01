3f34cdffb552a6a694dd51cbf18f0f9aedc7663b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:08 +0000

    Itself high kind famous Oo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
