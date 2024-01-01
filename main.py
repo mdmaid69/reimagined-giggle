674e2ba1efb55d20da7763d010ef83bad8e7e4ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:02 +0000

    contact by used pocket slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
