d40d7788f418b2bad7a6036edbd20bd6071882a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:44:26 +0000

    influence soil spare after building

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
