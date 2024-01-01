db8ead805bddc9858f08fae2cf5a9c374047c775
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:10:35 +0000

    567enemy road decision breakinto wind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
