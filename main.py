5004838abb002f1c7e5c5e232d776dc993e8764c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:24:36 +0000

    guide deserve broad nerve indeed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def cube_number(x):
-        return x**3
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
