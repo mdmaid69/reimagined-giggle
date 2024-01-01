3820845501c1a89417c9a880fbf7e867583af75d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:27 +0000

    apparently average class nervous roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
