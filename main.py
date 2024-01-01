9558d3e127e0ea092cc1bff2aa2b4acdfa19c86f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:25 +0000

    key sell dinner sharp announce

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_power(work, time):
-        return work / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
