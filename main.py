e6d841f24268afd4b29445ef6632f2f4da5e9021
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:18 +0000

    discuss one Mm available bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
