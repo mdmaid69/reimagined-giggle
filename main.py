e2e77925bc04e287e7239b46fda48c9900b1c5e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:54:34 +0000

    protest thorough cope insist shine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
