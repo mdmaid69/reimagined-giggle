e9e5891dc63f5ec0ee4f33d4876d107b5ffb5354
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:03 +0000

    common pipe properly support arrange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
