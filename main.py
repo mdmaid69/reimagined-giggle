f234755be463270f06f1b1ad5366cb2cb6587e40
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:09 +0000

    near deaf half contribute movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
