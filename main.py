05ff742307e4619c92e1cd2c5acb57a0b7922d66
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:52 +0000

    unkind shape breakinto hurry label

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def remove_duplicates(lst):
+        return list(set(lst))
