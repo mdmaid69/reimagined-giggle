e94d8d060c70d3bed14f4067f3fadbebd87eb84a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:38:24 +0000

    otherwise rest test finish church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+print(re.match("h.*o", "hello world"))
