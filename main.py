347e437513e5a90a193bcfe02c7d925ebb76917d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:15 +0000

    so weak week war process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
