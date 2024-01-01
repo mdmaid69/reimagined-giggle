f31b6d7b9c9edb2e5776ff2a102563f30b82c79c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:30:35 +0000

    centre affect expensive comedown trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
