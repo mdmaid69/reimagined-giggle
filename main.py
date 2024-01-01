0566ae04778ea808a162288488576c0f0ad60df6
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:58:42 +0000

    upset force secret way table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
