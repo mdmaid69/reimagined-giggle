3c5fccc36e629e9e43c1ffa1a2746cb1658c76c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:00 +0000

    anywayanyhow block board platform range

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
