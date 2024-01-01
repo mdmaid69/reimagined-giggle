075c7c4b590eea4a3e45b462e7d64a22cb0ef0ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:53 +0000

    reply still scare trade whatever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_float(array):
+        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
