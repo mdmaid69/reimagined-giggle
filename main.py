b39a8c96e972ae9acb1671e1131b1abcd2986a6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:46:57 +0000

    team today middle characteristic development

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def square_number(x):
-        return x**2
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def split_path(path):
+        return os.path.split(path)
