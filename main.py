51698e1a48d86933e4364cd17da9ce538f10b369
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:49 +0000

    nothing afford date cap whistle

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
