cfa014aec30c0b2aed519d01e33c49e96b847b59
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:29:58 +0000

    pretty pollution wrap attitude pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
