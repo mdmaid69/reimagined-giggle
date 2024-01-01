86f5d85a33f11fbd1527277414b1ee7a9b7dbb60
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:53 +0000

    runinto run normal pick disappear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+print(math.pi)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
