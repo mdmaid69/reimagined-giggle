978ac4cfbe3018bed7774794cef443f10f63e801
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:16 +0000

    yours mixture information but appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def remove_duplicates(lst):
+        return list(set(lst))
