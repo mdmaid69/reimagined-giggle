ddbf9738ac903d939b3e96648b8f208d3e3b545b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:00:16 +0000

    invite lead wide means holdout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
