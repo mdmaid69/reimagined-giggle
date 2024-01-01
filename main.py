3f3907ce1fa3ad9ee25631239193bcb6bec8c6c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:42 +0000

    disturb wise figure each show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
