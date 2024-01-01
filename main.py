890a0da95a0d67019e0bbac849e630343c3e679d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:26:07 +0000

    bang chief limit shut roof

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
