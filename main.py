61448496d72cfaa816c617ff48c8f03dbf4a1101
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:28:05 +0000

    prefer delicate journey bite say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
