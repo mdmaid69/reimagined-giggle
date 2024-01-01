f8f7f2cd5fffe2ca80312327c2c80cc252a5d279
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:59 +0000

    science left now mine takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
