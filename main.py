7af21049ab52a1fc3fa9d89c898d9a4c8ccf9d3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:14 +0000

    wire valuable stone their front

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sys
-def print_python_version():
-        print(sys.version)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
