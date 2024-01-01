350b99e4268c69ae58e5d7dd824f89786e9f9879
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:44:51 +0000

    lay police prepare needle breathe

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_irr(cash_flows):
+        rate = 0.1
+        for _ in range(100):
+        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+        if abs(npv) < 1e-6:
+                return rate
+        rate += 0.01
+        return None
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
