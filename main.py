3d452ae22700213dbcc66604a0ac7a9c143691e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:39 +0000

    succeed correction push they cut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
