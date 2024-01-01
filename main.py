7f33b76dee9e2bb157b4df16ccddcd28968b4881
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:14:41 +0000

    taste might radio cap knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_average(lst):
+        return sum(lst) / len(lst)
