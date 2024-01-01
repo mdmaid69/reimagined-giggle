4fbe469db056921963b766a462bba32eb228b490
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:00:36 +0000

    sorry slowly new no fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
