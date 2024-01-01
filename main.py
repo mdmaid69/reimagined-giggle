4249cb9e646e370fa7a8f4eae77c7dd9e17c4322
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:02 +0000

    Ll produce possibility frame rest

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import random
-print(random.randint(0, 100))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
