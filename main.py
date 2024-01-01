f6861eab949c8123263213705f52588d81cd50e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:39 +0000

    glad pencil content market company

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def convert_bytes_to_array(bytes, typecode):
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
