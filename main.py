d097be2dc73308ce70a946a605cbfbc1d1a158e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:03:22 +0000

    every position swing well speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
