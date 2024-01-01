cc5d48dd0882a91618ab06f0e7cc168a5f4ed32c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:02:29 +0000

    force survive ground rob offer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
