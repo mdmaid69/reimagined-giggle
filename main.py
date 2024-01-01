5ad792ecde046f18a407ff9a4e4dbebc09022fe1
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:27:09 +0000

    underneath Hh badly left little

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
