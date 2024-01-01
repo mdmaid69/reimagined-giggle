76bc79050b42ba93f8da2f211cde15405b881bcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:13:41 +0000

    how change soak rob patience

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
