0197f42de2588615eb592316a2f391230f93fcbf
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:02:54 +0000

    university joke wave represent run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
