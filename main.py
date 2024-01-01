7b389da2fd03d61eefcf535671b749401533dd3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:23 +0000

    pause inside unkind visit ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
