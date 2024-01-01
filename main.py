adf701101db5a06b0fdf1c1448beae1279ce57ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:15:32 +0000

    bury can simple convinced clear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
