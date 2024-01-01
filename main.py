0db65d8c51aa5ad16954c305f6ed5514ab486c21
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:55:55 +0000

    collection division equal Tt Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
