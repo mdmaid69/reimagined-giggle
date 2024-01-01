366494646b84c95d5ef2bbdd393482f24c76fc55
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:15 +0000

    row post stay blame fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
