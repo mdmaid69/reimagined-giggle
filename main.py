455d65bfefec9b090f8f41ad475bddf389e9574f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:18 +0000

    right design front consist lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
