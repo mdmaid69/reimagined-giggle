3f11d72c94a2907a4a1ddec48b89ab1a24d082a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:47 +0000

    description break so bury general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
