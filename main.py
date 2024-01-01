3b8cb4d7db5412c9fbdaa96d4ae43ea474f4db39
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:13:13 +0000

    clear surprised shadow control base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
