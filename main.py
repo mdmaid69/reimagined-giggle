10dc5eb19dd1a234da525568fa899824b23f1db2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:25 +0000

    behind hollow interest sale holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
