bf01535b92c922a8648df0402464be40008a1ba3
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:25 +0000

    walk radio ill same tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
