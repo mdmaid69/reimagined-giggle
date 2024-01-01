11e6e4223522f120615deb538013c0104e5b1c15
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:37 +0000

    plate noise behave which only

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
