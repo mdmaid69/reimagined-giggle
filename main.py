4197c79d74f012184dbb63274ac7df34222ec3a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:39:22 +0000

    adjective quality typical prepare rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
