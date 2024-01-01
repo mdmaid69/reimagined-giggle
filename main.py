38c69c058d2e19a536c8f169d927385d62dc5140
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:45:34 +0000

    mistake great pool turn sleep

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
