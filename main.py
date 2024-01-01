5a9aac7e2092a52f39d54d70e5282339230ad441
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:17:48 +0000

    keepfrom free long rush preparation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_max(numbers):
+        return max(numbers)
