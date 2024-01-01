243171064ae0563138904c7d41a670b326c0b169
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:26:18 +0000

    accommodation frighten survive another proper

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
