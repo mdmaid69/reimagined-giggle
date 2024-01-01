c8d33d3a3d18cda57963c808b7c82ff904a97557
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:44:54 +0000

    lunch man kill passenger area

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+print(math.pi)
