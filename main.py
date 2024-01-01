3b64759535b2d8525193174fc0c5e22e5217ceaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:55 +0000

    568energy bang round disapprove apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
