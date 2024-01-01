e085fa9b875c4f3a8f8c50af244a2244a522c1c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:08 +0000

    path separate natural aware support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
