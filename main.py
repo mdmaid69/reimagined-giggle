02f43d63e7a8aab9a11c5b992815f743cf3265ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:15 +0000

    poison sleep row around suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
