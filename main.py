5330b90489201f903566a819eddcf40ec3cbfd36
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:46 +0000

    bag innocent exactly minimum shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
