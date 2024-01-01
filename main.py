f697d28f3d9f473b010a74d16641fb8369284812
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:57 +0000

    best no bin 569enjoy towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def cube_number(x):
-        return x**3
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
