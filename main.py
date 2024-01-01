1e74c929ab71c3654d1531ad948419d44dd40815
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:33:28 +0000

    level admire roof maximum 568energy

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
