ce0183043bc55a1d0a611f932a1cf1c779f388de
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:22 +0000

    lookaround hello perfect expression thinkover

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
+def count_characters(sentence):
+        return len(sentence)
