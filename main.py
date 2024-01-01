0c1471b9ef4041e7e23cdc92cb35489254e364ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:02 +0000

    wave memory design form issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
