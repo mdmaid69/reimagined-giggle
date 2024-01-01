7e5c7329d8bcd037b18a2c0187f59a1f20aec0cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:21 +0000

    kick instead grade arm escape

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
