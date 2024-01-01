9a9dd03d551944f879c9791ece6a1e8082c19806
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:57:42 +0000

    common wipe edge beside honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
