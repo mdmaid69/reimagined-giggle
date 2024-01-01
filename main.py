8df816499d48e930921d7730a746a30bfcc21341
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:24:32 +0000

    welcome female line obtain accident

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def multiply_numbers(x, y):
+        return x * y
