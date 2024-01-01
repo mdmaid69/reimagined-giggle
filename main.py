57040a3a8ebc45b34603b2704610817b24001b0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:50 +0000

    last bridge fail recommend expression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
