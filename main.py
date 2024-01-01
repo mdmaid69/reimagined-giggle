d246da39809ddc75cbc42189f0cf094cce7f4042
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:04:42 +0000

    list list light stretch expensive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+from collections import Counter
+print(Counter("hello world"))
