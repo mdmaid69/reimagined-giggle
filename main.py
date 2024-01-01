f1a74e62895a8dcee91299900ea97794d973d7d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:12 +0000

    mysterious disapprove modern silent tear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
