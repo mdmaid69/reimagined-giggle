3a6e381681305e712aaf211edc94d644543934ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:29:03 +0000

    travel degree at inform belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import datetime
+def get_today_date():
+        return datetime.date.today()
