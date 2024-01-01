610b93f09f399867c2164c32a853814d295fdbe7
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:35:17 +0000

    raise mention but soft turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
