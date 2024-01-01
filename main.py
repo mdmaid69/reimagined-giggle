1562312b2706fe326394e19ec6d78f179e01606e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:33:55 +0000

    thorough here sale dress ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_set(array):
+        return set(array)
