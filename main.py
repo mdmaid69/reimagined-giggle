996c8f97ae93f18f7f876e88ada75aba9d23cee8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:34 +0000

    properly measure loose before ago

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
