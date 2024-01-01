a8303bedb9375e07a85f5e37e1e733b48b0b9b77
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:42:13 +0000

    object last shake watch share

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+print(sum(range(10)))
