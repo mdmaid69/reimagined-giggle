331edb2dfbdb6a824a9c5d0b77facd30f4f39641
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:21 +0000

    pretend my pure move spill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-print(sys.version)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
