918bdc72d51268d80d9dbc1b3a43e8b01505de25
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:54 +0000

    personal development shop undo ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def sort_numbers(numbers):
+        return sorted(numbers)
