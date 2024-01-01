1e7fec5def5ca3ee17c1dc83f9aa4db37bbf0d01
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:01:11 +0000

    mind contact 564encourage offer explain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print([x**2 for x in range(10)])
+import array
+def reverse_array(array):
+        array.reverse()
