34f07ede612263522a53a64e8b280a6fa8abab63
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:10 +0000

    interested lookup progress damage blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
