959d685bb3fef3d03361d87e8487308247acdb55
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:21:42 +0000

    practice contrast sense narrow shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_list_from_array(array):
+        return array.tolist()
