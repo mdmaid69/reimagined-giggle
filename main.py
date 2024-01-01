68942892b84c9dadab01a3c7f74860e42df6af73
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:38:56 +0000

    goon apply Ss murder trade

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
