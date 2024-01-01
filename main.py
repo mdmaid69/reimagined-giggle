c0f445bfb3fb1335f33aa9e2054ede62fc250ba2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:45 +0000

    beside roll confidence programme match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
