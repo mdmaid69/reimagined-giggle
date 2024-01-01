d01cb2b66e1214cab64b888b4129624c008b3b91
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:11 +0000

    train estimate lookback(on) have impossible

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
