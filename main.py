5c07b44d94955cac4f12b39515a11c60f12164a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:08 +0000

    produce downstairs tonight yesterday exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
