23ccc299da6d29c8c2be48247600495028f5e6a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:44:19 +0000

    trial lucky very murder everyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
