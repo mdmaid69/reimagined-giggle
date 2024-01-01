884f8d2cc07a0e03d7838a2cb612834901f68124
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:54:51 +0000

    load obtain ability terrible lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import time
-def get_current_time():
-        return time.time()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
