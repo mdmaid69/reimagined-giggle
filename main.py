259381e9efb1d9fb6fe0b4660e55e43dfd196499
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:38:59 +0000

    plain aim attach entry present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
