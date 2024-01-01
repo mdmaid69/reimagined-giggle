03e21ac839e4580d8c4021cb0f4a951ac832b551
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:02:46 +0000

    depend purpose careful welcome especially

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
