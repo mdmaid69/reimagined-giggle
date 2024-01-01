265c974afdb024bb67a9d8709b4c9a3d2e3b23e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:20:19 +0000

    welcome indeed rain total tap

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
