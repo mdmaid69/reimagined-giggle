3190a5889f7c0e428d0d9740b5f00261b8991aaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:34 +0000

    makeinto write deliver train shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
