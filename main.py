fffb59193aad6c84e4390752ee3f7b0971760c57
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:49 +0000

    call emergency name partner each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
