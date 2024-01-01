1de10ec8c2ccff80b86e7ae4d5f528d0b12b0a63
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:22 +0000

    more mark wish back soil

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def square_number(x):
-        return x**2
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def find_max(numbers):
+        return max(numbers)
