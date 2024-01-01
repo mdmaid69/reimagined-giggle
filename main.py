82135b4947f7586231c08fb4d3ef42be55e30f4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:19 +0000

    drawing brain about moment answer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(numbers):
+        return max(numbers)
