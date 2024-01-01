c4fa37b4934aad01ca72c4fe1e2cae4957222fe9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:54 +0000

    even shine this lookback(on) game

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
