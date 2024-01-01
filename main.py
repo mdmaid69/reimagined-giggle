b674573998612e2ade741107aa4912437f532902
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:44:32 +0000

    her goout most depend nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_min(numbers):
+        return min(numbers)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
