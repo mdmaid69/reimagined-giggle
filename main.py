e7805556cb6ada0edd426f8754a44c8eb78d34f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:05:26 +0000

    organize fond goodbye deep zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
