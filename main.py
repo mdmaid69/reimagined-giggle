34b652eca0a8be56da43720795ee8155b6fe326f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:04:15 +0000

    doubt slice process early neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
