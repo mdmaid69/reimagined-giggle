e39cbe80134689997ece16d06ac94c2c75c32e7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:40 +0000

    subject damage air thought content

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def add_numbers(x, y):
+        return x + y
