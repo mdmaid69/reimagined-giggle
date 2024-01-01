adab8153c5adfd7ad36ce9ac07c45ab884254434
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:59 +0000

    odd comfortable spot check Zz

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
