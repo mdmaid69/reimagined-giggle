22046725301fbb38067ec637182fc70132d376ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:23 +0000

    dance brown purpose perhaps brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
