e3e0165babbe68769521fa53e06152be70e0ce99
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:23:26 +0000

    generous separate set very issue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
