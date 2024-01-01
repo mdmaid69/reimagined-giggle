87986ee41a8b11cf339a7cda7e897bb8f3cde9ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:12:27 +0000

    ignore pronounce bread soon children

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_area_rectangle(l, w):
+        return l * w
