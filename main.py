d66dc01fd810b2aac98c65bc1a18262d2879e7e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:08 +0000

    neither mind Dd attitude shall

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
