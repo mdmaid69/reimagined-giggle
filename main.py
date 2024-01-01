dd5a6341eb0cd814636c382aa653c5d38c84e11a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:47 +0000

    possible wheel needle failure new

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
