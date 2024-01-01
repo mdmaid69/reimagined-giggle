694a346763e987fe2a62a9c62fe54557189bb42f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:39 +0000

    money ever guide stiff accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_length(array):
-        return len(array)
+  def square_number(x):
+        return x**2
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
