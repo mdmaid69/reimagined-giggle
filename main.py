4e4450a6723d1c62c4c59a66de36528d04b21993
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:32:22 +0000

    disturbing day violence politics file

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
