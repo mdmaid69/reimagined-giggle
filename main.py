45d29dee4561590246552680a42d6ddf7bc73359
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:50 +0000

    confusing fond fire late quality

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
