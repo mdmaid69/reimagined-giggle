cb806e24132cffa64720160c9cc66681866dc435
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:40 +0000

    Itself team interrupt strange future

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def get_array_itemsize(array):
+        return array.itemsize
