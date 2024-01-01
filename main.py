35c9453ea4147082e7d3d7d593620e22fb0c5a7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:42 +0000

    goodbye trust concentration appeal attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import sys
+print(sys.version)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
