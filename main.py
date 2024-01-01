4b45c3319650325dd628f3686ea145441da7975d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:59 +0000

    identify book violence available extra

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+print(math.pi)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
