60ab1ef023d65c244b5b22fb14cd0fcc0b12ed36
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:41 +0000

    correct capital set bottom comeover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
