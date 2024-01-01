6ea42654ec6af0909a51814179d6a6ae1cdee2e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:17:42 +0000

    shadow 570enjoyable comeout serve room

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
