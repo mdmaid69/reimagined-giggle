2db8d08d3016c946140bf4a0cce562602677edc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:36 +0000

    coffee news genuine turnon them

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
