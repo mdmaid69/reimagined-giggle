ff9bdb25d296966f485878bcd7ac52c23184e670
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:55:46 +0000

    onto Itself reject goaway official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def greet(name):
+        print(f"Hello, {name}!")
