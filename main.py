fdcb46ce143262ce9cf7a6602ec3945112e6eca3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:34:38 +0000

    guess better plenty they glad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
