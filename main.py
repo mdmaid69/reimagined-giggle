c28632fc47525b05921b7eca9b54c89879b3bcd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:53 +0000

    lookaround though apart remain control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
