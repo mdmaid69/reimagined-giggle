cf2299723f9b54462f4ed2f999ba224929270678
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:44:32 +0000

    only interruption brush happy corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_odd(n):
+        return n % 2 != 0
