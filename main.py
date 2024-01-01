c64aad30b0ad70f8cc48cb4af8349e8d3d292d35
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:34:50 +0000

    wash post responsible remark underneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
