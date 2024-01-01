d9c24c925fde5d0a2f543c4679b8c162431e704f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:45:36 +0000

    swing street pain welcome hers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
