405c7cd0d72a682968115939ef1352a4cc3e5d32
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:58:59 +0000

    rain getoff warn objection clear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
