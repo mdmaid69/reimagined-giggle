d05c47bcac9c356412683f0b7ffd7de0b7497b3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:26:56 +0000

    proud stare its wheel takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import numpy as np
+print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
