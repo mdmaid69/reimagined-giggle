5fc1fbc1f761b4c09c0e203d92f23f6e6cbd48a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:03 +0000

    past nervous wild dress ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
