e803c0d30254a0177f8ed35197ce5dccbc655071
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:18 +0000

    neat single exciting force confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def reverse_array(array):
-        array.reverse()
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
