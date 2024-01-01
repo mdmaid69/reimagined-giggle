be5904bd7cf962c42777ed00dab0472d08b1c415
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:17 +0000

    change meeting poor pleasant hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
