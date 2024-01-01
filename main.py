dd56ffe71feb443df972001ac1179c9a619a03b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:41 +0000

    getout refuse pure release satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
