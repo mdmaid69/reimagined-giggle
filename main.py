16aac1e08fc8e4bb87e687460a2d5b01eb1c5265
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:13 +0000

    professional great surprising bang geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
