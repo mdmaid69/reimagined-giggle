bff94de6abd576093404d3ef2f80732f80a76054
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:05 +0000

    shine home agree increase runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def convert_to_binary(n):
+        return bin(n)
