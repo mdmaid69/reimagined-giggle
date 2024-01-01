8b97c1e551071d077e1ddc5259859c04156bb20a
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:12 +0000

    all getaway then interview check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
