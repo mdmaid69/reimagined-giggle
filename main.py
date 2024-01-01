2ff0da676d27671f289ed1e1528ea60c21632b97
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:33:38 +0000

    wherever accept convincing interview famous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
