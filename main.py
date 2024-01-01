1e4c75d7357c162c72c9f3fe3e8e510ddb1ff7eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:25:22 +0000

    perform throughout hang sentence nature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(5):
-        print(i)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
