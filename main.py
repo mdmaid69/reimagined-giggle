f76d648951dcfda9d06463acd4e0d06bbb6b17ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:26 +0000

    describe tomorrow mix glass sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
