acf723378310de07a7678b0b330a1afb8d2850e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:05:58 +0000

    same Rr 572enter tell damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
