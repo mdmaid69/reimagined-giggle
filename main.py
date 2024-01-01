d4e158dc1c482a303f00d298cf318fae9564faf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:06:40 +0000

    chase polite breakthrough bringup pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_length(array):
-        return len(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
