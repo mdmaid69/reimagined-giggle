b1c2fc692ea46a6642a02cbbf00c3e0f9f2c4f38
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:19 +0000

    hers division crime reduce issue

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  def is_prime(n):
+def is_prime(n):
+        if n < 2: return False
         for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
+        if n % i == 0:
+                return False
         return True
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
