b65cdf32daeec98367997deee72bfe0a84b98e40
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:24:26 +0000

    own find block join sorry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
