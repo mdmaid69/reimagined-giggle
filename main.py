6ba3c3e78d31e133f82672d153d786886643fc71
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:49 +0000

    responsibility perform before declare comein

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
