aaffca78981df6dc3bcda172197776da83360246
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:26:37 +0000

    Ss law criticize bag along

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
