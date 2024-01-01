9fc42c0671c321b06eac0c34bbcccb67e7bb3a84
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:07 +0000

    matter sick building variety woman

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
