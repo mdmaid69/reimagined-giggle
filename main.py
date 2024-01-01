d83b2a9df2675040b725214d791938f8a2b8ee42
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:10:03 +0000

    test state become moral long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
