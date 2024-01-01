cc672792c6ccb34f585edc5f9fbeeb2b14b995f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:05 +0000

    fast laugh division peace responsibility

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
