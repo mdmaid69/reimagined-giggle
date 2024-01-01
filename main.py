cb95142b5019f0e92a877b9039dadc42775423ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:49:00 +0000

    occasion invite by steal goodbye

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+text = "Hello, world!"
+print("Reversed:", text[::-1])
