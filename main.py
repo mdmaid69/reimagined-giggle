3a2e982fafe5019fbf7246bd6436957c85801da2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:30 +0000

    takeout careless wash private sweep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import numpy as np
+print(np.array([1, 2, 3]))
