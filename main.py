1d42f72b62a1a924cb2d8b4806d79901ac4b4621
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:47 +0000

    talkinto result split tape impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_bool(array):
+        return bool(array)
