9d15e479c1bd1160352ebabf43c3faa708480966
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:27 +0000

    more scene maximum feature less

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
