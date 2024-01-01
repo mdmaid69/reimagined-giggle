8881c8c876e5f32a329757f5c85352d05e8db2ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:46 +0000

    ride patient winter admire estimate

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
