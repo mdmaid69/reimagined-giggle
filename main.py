d14a4b7fc23416bfecca332afcb951ae92d8d2ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:45 +0000

    talkinto care turn now curious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
