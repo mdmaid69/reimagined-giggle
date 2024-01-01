d5b04c3d9da65eed7196437d09496cda67af530b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:52:40 +0000

    dry 564encourage door neither water

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
