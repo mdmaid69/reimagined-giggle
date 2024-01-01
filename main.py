6c4a425c5968950d50d656c664bc789010f77adb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:58:08 +0000

    underneath takeout mysterious play reserve

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def square_number(x):
-        return x**2
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
