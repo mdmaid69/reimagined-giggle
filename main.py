66f925256b725dbf412b7d1b350221379ce68812
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:36:37 +0000

    personal trick estimate 565end over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
