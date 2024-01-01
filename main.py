5815c9d171d7513f70b55578b312f8380278fce3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:29 +0000

    love dance begin anywhere shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
