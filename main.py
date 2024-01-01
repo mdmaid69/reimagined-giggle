64e6595791c66454615848d31dff8ced9a64390c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:41:56 +0000

    sometimes bring politics nice appeal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
