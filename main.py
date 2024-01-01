7ef23817e9eeba2b409ad1eb63441ad7ac7e625a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:58:57 +0000

    information pity fond impression likely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_string():
-        return collections.UserString()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
