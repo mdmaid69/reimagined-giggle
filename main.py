5ff3e8c34dfa1cad4d0392360c2575eb09d50977
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:45 +0000

    relief keepoff belong sure main

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def reverse_list(lst):
+        return lst[::-1]
