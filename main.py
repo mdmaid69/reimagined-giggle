e1b0d3205dde9ab973b78cbc33e7110fac77c03c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:40 +0000

    dream slowly image regional duty

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
