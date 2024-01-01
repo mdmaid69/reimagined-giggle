80ac9d16ef4b633c7723ec19e9d74681162d3dce
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:17:35 +0000

    such sign together base female

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import sys
+  def get_python_version():
+        return sys.version
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
