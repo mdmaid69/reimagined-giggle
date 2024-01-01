e3a43b35b51f99e6d8fe2f29eb6fc92846ce85ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:05 +0000

    send Qq next nerve imagination

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
