36a898785aa2cc52cd5f3c725fba3749dfcf7138
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:19 +0000

    mess control wind special mad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
