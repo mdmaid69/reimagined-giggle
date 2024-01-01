919535784e17bdd2464d54a353f47c4f068493c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:04:05 +0000

    necessary market himself year reduce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
