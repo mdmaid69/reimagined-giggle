7e67c373c5d5125348fd83b09e56a7a676397477
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:03:14 +0000

    unkind feed keepout fear except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
