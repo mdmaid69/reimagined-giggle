5c93ace8f24d1b16e727a98d4e135e0966d77b00
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:10 +0000

    anywayanyhow drink Yy dance attractive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(lst):
-        return max(lst)
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
