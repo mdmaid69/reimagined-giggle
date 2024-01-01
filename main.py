b70f234290cc315068c8582e35d504ecbd9a139c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:35:04 +0000

    runout knowledge walk fail concerned

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_itemsize(array):
+        return array.itemsize
+import random
+print(random.randint(0, 100))
