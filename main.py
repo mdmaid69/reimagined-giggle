0865d798526a64d54c8dc316d57ea9e5ae066f6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:43 +0000

    authority model nose sound constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
