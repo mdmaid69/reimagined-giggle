1d13d6c9d0a7559df7ba037eb48e810d95c6af5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:08 +0000

    guess exploration front peace treatment

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
