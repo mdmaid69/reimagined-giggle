0c5a924155d7fc18fbfcf7d1a56a305e4d7ae459
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:24 +0000

    line equal bottom extremely object

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
