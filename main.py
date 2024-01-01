2f7433fe0ccde1941951cc8a1f0af6c95e21e7b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:45 +0000

    piece fire turnaway sensible scrape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
