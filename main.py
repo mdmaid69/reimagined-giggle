289a6c61e46382e7e4ddf4aaaf5e3d5f4f76135c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:49:59 +0000

    shop turnon doctor connect guess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
