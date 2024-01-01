2b3a254f13f705c0103d918ba06615e495355c99
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:27 +0000

    inquire fierce runover unlucky suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def reverse_string(s):
-        return s[::-1]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
