63b55fb05a619d6886d8c807a2247b6339f6d09a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:12 +0000

    some story see Pp meet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
