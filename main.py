bc84f81a40b29ace69a11c32309ef1e90cad0a27
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:53 +0000

    bet threaten must design uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
