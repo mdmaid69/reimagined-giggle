00d6345ae3cccf3ad36effaad1f02e8ffa83b102
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:51:37 +0000

    suck safe car guide deep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
