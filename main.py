496f2c2aa61700bdf0513660116f8b65ce98596b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:36 +0000

    combination shadow family protest cap

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
