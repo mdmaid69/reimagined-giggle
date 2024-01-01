c346666ccbd49fc026c801638840da7075cbcf02
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:46:41 +0000

    responsibility apply issue almost weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
