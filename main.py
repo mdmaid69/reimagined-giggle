c03088107d868a435421f578c6b75098bf5f13cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:39:45 +0000

    always week you chat regularly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
