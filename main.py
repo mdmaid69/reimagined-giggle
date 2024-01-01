3d00420f656e932e530a6f6bf0367826234a2a95
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:11 +0000

    advise intend level task beautiful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import os
+def get_current_working_directory():
+        return os.getcwd()
