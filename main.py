1b8f09d3ad19a7677c6031a7324a2f2dc5dacbca
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:46 +0000

    deliberate kill closed exist switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
