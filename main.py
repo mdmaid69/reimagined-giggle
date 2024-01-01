da1ea7de0a5566d207a8db6375097af59ee891aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:26 +0000

    breakinto cut address unfair sea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
