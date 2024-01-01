3585e81ac6c211e8332644916e3ef2265ab78415
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:54 +0000

    surprise love turnoff go tough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
