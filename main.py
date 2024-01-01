b749b19e4d8b76da38b467a37be8c0f39389a53d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:26 +0000

    people protest taste jump but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
