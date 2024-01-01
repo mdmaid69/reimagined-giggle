86ab5162dd8e240f789fbf05caf109b526c536fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:25 +0000

    begin bad trial bar talkoutof

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
