b580be6867e044add7c2d223a1709c644502dff3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:03:28 +0000

    forget flat statement common into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
