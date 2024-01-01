6b38ab88ece1445c1b7d888103ec0a506f8e7339
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:15:31 +0000

    runover ours wish block scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
