00eaf038c1b72d01f5372cbafb66263ce5f01818
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:17 +0000

    software run go somebody about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
