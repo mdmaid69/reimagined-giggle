ae1140ad3456e65ebaa992c188b380d060dcf24e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:32 +0000

    announce send Yy embarrassed creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def reverse_list(lst):
+        return lst[::-1]
