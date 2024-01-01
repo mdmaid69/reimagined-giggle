7218bd38d93d20ccd074081bef5db994d90d6589
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:55 +0000

    manner valuable eat deep light

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_max(numbers):
+        return max(numbers)
+def count_characters(sentence):
+        return len(sentence)
