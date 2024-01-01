ecce98a62abd3df4b63a328a05d2073c7f6acd83
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:08:45 +0000

    lot press good need score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
