58ef5276d5d068ac9cd1a0f32c8fc2dac626f6db
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:42:44 +0000

    impressed mention make formal lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def reverse_list(lst):
-        return lst[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def reverse_string(s):
+        return s[::-1]
