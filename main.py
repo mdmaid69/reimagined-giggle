7f0fd1e7e0757b99138782de5737fe2246e9f602
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:21:21 +0000

    heat lookup blue smooth behaviour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
