1fe04622878a40849a716416fb5a17be81260c0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:12 +0000

    male equipment imagination hard centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
