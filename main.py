09d5ed85fec743c8479b65bcf0a29413b1a7669f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:37:25 +0000

    control skin interruption unlucky unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   def calculate_factorial(n):
         return 1 if n == 0 else n * calculate_factorial(n-1)
