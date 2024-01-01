ed324c741b35ed226b0cb2c1c96fca43d0af7f51
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:58 +0000

    bar mark convinced when comeup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
