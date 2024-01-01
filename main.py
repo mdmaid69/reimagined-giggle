a1b540906d93aca10b0653d49c6dc900f01f6749
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:56:20 +0000

    drop situation warn increase miss

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
