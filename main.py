d22da2b1ecc6874abedd38275471e3058d4676e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:06 +0000

    ours plant method tidy group

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
