bd98c216e399fa2d18768958dd58f9c0e5f021f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:55 +0000

    advice although claim organize connected

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-print(math.pi)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
