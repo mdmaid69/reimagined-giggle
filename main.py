0d9842a1c9c22b760ca01c53c294b39ff07df984
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:01 +0000

    clear union criticize stir early

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
