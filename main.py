dc6026df2cfe94ee61fddd06bd60f7977094c63f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:53 +0000

    lean event kind die worry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def sort_list(lst):
+        return sorted(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
