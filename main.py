e846ea3b717d7ea297aece95fbbdbb4ba8874727
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:17 +0000

    important defend comfortable off smooth

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
