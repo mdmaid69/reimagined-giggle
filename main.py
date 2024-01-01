2d074cdc74f3ecc6614f4898626001442171fd52
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:13 +0000

    interest marry 570enjoyable best exchange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_work(force, distance):
+        return force * distance
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
