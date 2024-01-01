94055b9c0f2d6205cd118f151a91a19b94290953
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:19:44 +0000

    broad pain Cc weather fire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
