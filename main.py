49e281903fa3d000cd43e5a0c8462b137a7d56a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:38 +0000

    love pencil computer behave wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
