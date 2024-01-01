ea45a00a413fb87c8b78c589bcc539021364cabc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:40 +0000

    own talkback talk cap soft

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
