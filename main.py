2452b49a25b702436b124a435ad6ce6f2de54163
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:31 +0000

    upwards bath priority disappointed fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
