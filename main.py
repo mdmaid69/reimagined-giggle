35927132707e0a252ff27b6b781baf7e67e9b344
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:36 +0000

    judge material narrow door boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
