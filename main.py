da0f5de7109e885980952548c174f24db3975911
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:32 +0000

    relief general disapprove recover strike

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def calculate_area_circle(r):
+        return 3.14 * r**2
