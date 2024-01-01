c96542713025a72b96b2ae14d881e04d0398a596
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:52:08 +0000

    comefrom rid whom please commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import platform
+def get_os_info():
+        return platform.uname()
