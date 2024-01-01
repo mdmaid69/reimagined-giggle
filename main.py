25ee7166e75039f87db8875421e43461f7c73ed8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:16 +0000

    embarrassed sit combine tap calmly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
