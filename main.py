bb17bd0b4ddf7de2bc3a20bba3deb896a88d2a12
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:39:25 +0000

    giveup stand cover extremely rarely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
