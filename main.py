d27d8eb911596074683b38a25e2f11f3a599f56c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:32 +0000

    steal loan wash yet loan

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
 import math
-print(math.pi)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
