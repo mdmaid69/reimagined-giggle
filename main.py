4980ca6f1abcf0b016bed6281ff90c89a3d9ebea
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:16 +0000

    free family naturally Ff race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_repr(array):
+        return repr(array)
