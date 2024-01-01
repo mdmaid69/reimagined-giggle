84490915548930e17b102add4db782bbf8d9699e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:02 +0000

    thinkthrough slide roof tragedy trap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+print(math.pi)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_work(force, distance):
-        return force * distance
+def convert_array_to_list(array):
+        return array.tolist()
