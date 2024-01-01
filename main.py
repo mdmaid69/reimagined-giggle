057c72dee94ef1b7fbc82f6c2d23b37a963d7136
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:12:34 +0000

    runinto surprised comment suck the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
