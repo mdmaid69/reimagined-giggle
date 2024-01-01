2b8f26ce96308a4386fa60559845a7e4879c40ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:47 +0000

    ride attach each show message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import logging
-def log_message(message):
-        logging.info(message)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def get_array_as_str(array):
+        return str(array)
