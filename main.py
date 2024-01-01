86b5ff0e9e2ab72d191ef28219db2a4f9ef227d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:07 +0000

    car case arm culture respect

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def find_max(numbers):
-        return max(numbers)
+print(sum(range(10)))
+def is_palindrome(s):
+        return s == s[::-1]
