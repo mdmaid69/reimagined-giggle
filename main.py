66aaed52381436da3e9191a887c76e6b7bc72067
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:52:17 +0000

    judge body laugh estimate charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_list_from_array(array):
+        return array.tolist()
