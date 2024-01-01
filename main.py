6d768f40167a1506cf8ac3b9372d0eb8d411b97c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:09 +0000

    Vv catch pronunciation flavour start

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
