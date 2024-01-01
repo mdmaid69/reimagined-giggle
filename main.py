95b15bc7bca6b4683016de5d4a63d7e05284af7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:51 +0000

    contents identify loud want event

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
