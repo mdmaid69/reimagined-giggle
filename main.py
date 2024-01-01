515ff8967493f510758eeb6061d95fa38b5e6299
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:15 +0000

    punish touch invite bin child

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def find_min(lst):
+        return min(lst)
