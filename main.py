ba562f68cde89d9e08ab12d4ef97fe985ecc61c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:24 +0000

    at damage key several stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_as_frozenset(array):
+        return frozenset(array)
