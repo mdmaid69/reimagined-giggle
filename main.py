45217ed17955c6ba9801a6bd9bfe4c0a761b40f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:02:04 +0000

    confused centre schedule everyone judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def convert_to_octal(n):
-        return oct(n)
+  def get_current_working_directory():
+        return os.getcwd()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
