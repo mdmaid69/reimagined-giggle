b60b3049c785688e678266bd1eaefd8240af7663
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:56 +0000

    permission final understand urgent inside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
