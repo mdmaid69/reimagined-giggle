f312006a21b2e6837123968943677427914c590e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:08 +0000

    deaf pride may expression so

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
