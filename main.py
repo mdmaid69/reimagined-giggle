b6d2786cfca441250417ed406f5e5068a9c4de66
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:45:08 +0000

    busy unlikely crime ought favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
