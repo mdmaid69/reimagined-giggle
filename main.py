ce35b0e52bf2d96abbd18cbf7a03f8c57571b2d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:57:44 +0000

    escape after university measure rough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
