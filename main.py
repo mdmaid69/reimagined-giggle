d9ca13b6b051c3d267aff683d3d3a0a5b8c21995
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:50 +0000

    flood regard basis accident sad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-print(re.match("h.*o", "hello world"))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
