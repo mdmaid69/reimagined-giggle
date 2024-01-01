0f4470b2bcfa4892321200b6d3ae1d7d2fb7e4c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:47 +0000

    error table day survive drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
