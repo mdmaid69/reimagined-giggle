b87c65fe8e8bad726a7be3c444ba093632f93762
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:25 +0000

    less complete point ready both

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
