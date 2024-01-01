bb8e3daa0c2f31d0ae12416d4e2e59a873758cdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:09 +0000

    frighten us somewhere half chat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  def reverse_list(lst):
-        return lst[::-1]
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
