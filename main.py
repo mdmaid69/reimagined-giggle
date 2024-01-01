d9bfcc6e7cc53c2fe6f5dffadfe0f5e11f48654a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:14 +0000

    love emergency warn agree dark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
