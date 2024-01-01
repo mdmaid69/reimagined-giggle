922082b1dfa2bdb517adb62bca4b0e1981db2dc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:45 +0000

    uncertain hide handle town pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
