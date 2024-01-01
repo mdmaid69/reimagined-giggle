315348559702d127e201e57ef9d4bc3291bcd294
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:51 +0000

    plenty need mainly wear turninto

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
