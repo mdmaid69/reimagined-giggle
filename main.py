757f470a7578e11f35137e97b839ad69109d02ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:17 +0000

    mad 572enter high plan peaceful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_int(array):
+        return int(array[0])
