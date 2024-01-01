eb39c7de6540b5a9b0a97022eadab31ed5294d87
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:09 +0000

    throw collection other turnout popular

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-for i in range(10): print(i)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
