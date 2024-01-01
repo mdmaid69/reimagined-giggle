31eed0c28ce60337b5b89f681b294a3c422e5adb
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:52 +0000

    gap modern matter concerning course

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print("Hello, world!")
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def clear_array(array):
+        array *= 0
