c73db7ec289d2052655640519232c7c09bedd980
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:30:38 +0000

    adjective congratulations maintain refuse pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+print(time.time())
