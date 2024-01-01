1be1dd9ae315c454b873838d7746db9c02cf9b73
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:18:57 +0000

    spot claim deaf surprise estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
