d5322e2c43a398358aac682dfaa19d93de4c8f69
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:24 +0000

    comefrom person each future Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
