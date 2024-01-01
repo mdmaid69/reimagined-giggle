a80832dad10f7d8951e820e6e081f1eedb5fb332
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:57:40 +0000

    rest he half commit dangerous

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
