c56278bf778318a06257d20fcf8ebd22c3b8ce7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:51:23 +0000

    tear wild goback(to) claim place

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
