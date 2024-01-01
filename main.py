774b8fdf8b661259385a8ab0b376108b1213da9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:37 +0000

    beside book break sensible sight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_string_from_array(array):
+        return array.tobytes()
