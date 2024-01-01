d300de13ee064256984b8143e9c556b8b5cc90b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:46:19 +0000

    play otherwise serve high rather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def convert_array_to_string(array):
+        return array.tostring()
