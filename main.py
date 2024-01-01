be0b64d0712d5b556800892bdf5c44079457b297
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:50:15 +0000

    prevent miss rain single relation

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
