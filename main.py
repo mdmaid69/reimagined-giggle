f19ec2238f24a2149175277dfd97911e10df9ed7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:26 +0000

    subject holdout colour also mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
