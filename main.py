136bf67f75a48373c22eeaa81381288111c58867
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:17:48 +0000

    sky might plain properly recent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
