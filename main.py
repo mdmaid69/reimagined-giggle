6a268e686641f36a65604e9682fdc6f9903cc251
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:05 +0000

    nurse Ll treatment recovery degree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+print(time.time())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
