0a25edd39b563e857f6f899434549cd7ae594f09
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:58:45 +0000

    frightened hollow statement embarrassing protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
