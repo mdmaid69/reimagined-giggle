597b151352d84bcf4a57ee47b5b7e2c5fe183d9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:22 +0000

    sudden worst objection coat afraid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
