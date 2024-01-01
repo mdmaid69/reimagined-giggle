1732e60648fc8dffd11be3d284aa3880bd6dcf1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:13 +0000

    smart worry deep bite temperature

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-print("Hello, world!")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
