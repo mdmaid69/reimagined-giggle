0abfeae44a168cd61d72135263f76294fa74728b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:10 +0000

    should score report original behave

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-def find_max(numbers):
-        return max(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
