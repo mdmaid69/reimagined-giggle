3e8fa89944f2dbd782d9b3affe7b2002d1810afe
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:46:26 +0000

    weigh nice explain sick pause

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
