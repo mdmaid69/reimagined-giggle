1a90b545dca154a7f0af4498da07aed8c03c8228
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:19 +0000

    effort neither attractive can defend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def add_numbers(x, y):
+        return x + y
