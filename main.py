1d1982f7cf32a2e67befa89c61f7db7f8c8ea521
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:05:43 +0000

    die contents destroy genuine fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
