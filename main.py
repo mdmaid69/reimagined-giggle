9d29b53ecfb0195ea455c2c78518572c1cdf5d0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:04:00 +0000

    object forward should weekend apparently

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
