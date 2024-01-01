184c6b4a725590e0c163a8367570dcd292dfd344
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:56 +0000

    strike right winter once getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
