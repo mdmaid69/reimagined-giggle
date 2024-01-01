a043e17c0bb4e62aa3b35a4a4742cf190cacb5ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:21 +0000

    sting undo left chin swimming

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
