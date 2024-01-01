e04d982f417fd2e8233939e6ac38f0b3281d97d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:55 +0000

    achieve basically tonight unit sad

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_float(array):
+        return float(array[0])
