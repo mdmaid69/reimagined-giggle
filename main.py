c6b8699fc405b066bd8315cced55be4b948fe95b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:02 +0000

    character often treatment soak Uu

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_exponential(x):
+        return math.exp(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
