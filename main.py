adfbac4cc8f7ba2b2846169d1bf870c5d14a72d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:54:53 +0000

    needle partner someone workupto turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
