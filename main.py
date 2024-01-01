4e43989a2f596e57bf42b5daa0164fe1c4824b2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:31 +0000

    slide calloff urgent attach late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
