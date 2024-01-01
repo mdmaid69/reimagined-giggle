f0f9aae4ae97e2a68e6c1744785de3e780eefe23
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:21 +0000

    separate far start previous advise

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
