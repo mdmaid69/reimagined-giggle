1133079fa9af4ac5d53dd4d6324b7705849807c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:46:48 +0000

    Zz hot rest regional kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
