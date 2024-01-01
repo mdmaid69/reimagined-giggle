fe181295a4e801188921951ef61640a9e58ebe0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:33 +0000

    object dangerous sort profit so

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def get_array_item(array, i):
+        return array[i]
