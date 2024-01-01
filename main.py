56997871802fc6604c376b526132b457d6cdca3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:11 +0000

    care promise smell result apparently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
