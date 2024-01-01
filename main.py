86b5f517ed93dcfd4919953481f0018acd017e80
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:47 +0000

    prize consult successful example sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
