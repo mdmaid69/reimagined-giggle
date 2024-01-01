d6a1fe438901e3ac891a2d2c500aa943d7362902
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:24:34 +0000

    arrange concern whatever figure side

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
