b11c4caac7581cf63aad5cb2a88ddf972a887a19
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:13 +0000

    too pin recover sting hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
