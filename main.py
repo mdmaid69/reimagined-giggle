bc8623f70edd4297696c021d191dc5ab708563e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:00:58 +0000

    turninto underneath begin sight cruelty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
