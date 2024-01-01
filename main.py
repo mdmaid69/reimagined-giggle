9f3e3d846e1396492d20acd9ff034bfa505251f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:54 +0000

    hurry impression welcome something opposite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import sys
+print(sys.version)
+def reverse_string(s):
+        return s[::-1]
