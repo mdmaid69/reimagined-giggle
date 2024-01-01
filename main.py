6be6504526aca3dca8dacee1fb4cbf8e50d19004
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:02 +0000

    problem connect proud fun waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
