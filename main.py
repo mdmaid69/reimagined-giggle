2469a5c0f757bfca092226e79846f605903d22be
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:52:00 +0000

    finish as wait lead load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
