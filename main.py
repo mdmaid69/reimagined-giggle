065af1cbbca0b2cfe1d8759619fa3e30d7e3c1cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:07 +0000

    getaway sad dry private draw

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
