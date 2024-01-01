96a5e463c810583b8b138e601e6e1d9a3e64626a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:49 +0000

    love amount interesting black mend

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
