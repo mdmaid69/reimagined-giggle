d218efb61789a9e2253969c944e1c771ff78d623
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:54 +0000

    formal protest close mystery pressure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def clear_array(array):
-        array *= 0
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def find_min(lst):
+        return min(lst)
