f4f41c27c3b28f8117b19b98397ea60c87f650ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:08 +0000

    Mm late pour breath Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
