b560b261180933e97af3046b96565fe2b095ca25
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:43 +0000

    remove follow clean talk wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
