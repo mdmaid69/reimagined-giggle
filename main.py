0e95f384c8b3fb3965eb81cca7496b9de000fa1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:52:37 +0000

    practical rubbish butter guess uncomfortable

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
