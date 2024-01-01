9627bceb26693579c492c7405b0e3004b24eb5d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:49:42 +0000

    child small trap water behind

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
