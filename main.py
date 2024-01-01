747387c6f4b7d3c5afea8c14dcce0d5c65208026
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:29 +0000

    fast runout before practise together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+def calculate_exponential(x):
+        return math.exp(x)
