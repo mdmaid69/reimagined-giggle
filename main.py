7cfa9d303c62ad8c0748e8a011dcc80e5c25e8d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:02 +0000

    apparently left negative hang scrape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
