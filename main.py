9660c5ec90ff7cff7bd8ebf70c7d7107a820e2ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:33 +0000

    be repair door heart keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
