6d85f2077ac31d26d1afc035894e538541bf49c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:03:42 +0000

    before discussion desperate practical Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
