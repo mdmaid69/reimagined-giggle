f119644e506bc94de3f63d9ae14c9d50adf69c73
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:10:23 +0000

    support night ignore please positive

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def cube_number(x):
+        return x**3
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
