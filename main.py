03ba4c6439c65674ac8f25b77088bff81ede64e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:40 +0000

    mistake slice forward pretty somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
