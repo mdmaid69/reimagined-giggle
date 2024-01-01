b87a45ece22da97f7ceba5b6cfcc09e3c9141875
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:43:09 +0000

    believe call that cheap talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def convert_to_octal(n):
+        return oct(n)
