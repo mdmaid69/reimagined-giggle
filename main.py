9d33aed0981dc9151f36f7ba268e87a685a3fe10
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:31 +0000

    border piece worse satisfied direct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
