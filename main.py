bbd22f3ce5ddf905819eda9ae03564b56ba5fbae
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:11:42 +0000

    slightly creature indeed brush opponent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
