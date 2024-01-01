8da9ae9038af959d70a9cb931db109f0d41a42df
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:59 +0000

    objection when show necessary basic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
