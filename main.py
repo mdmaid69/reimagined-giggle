3fdd00ff3d8184cdb93ff13b66f368e509d84d87
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:50:32 +0000

    no lunch adjective if keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def get_time_since_epoch():
+        return time.time()
+from collections import Counter
+print(Counter("hello world"))
