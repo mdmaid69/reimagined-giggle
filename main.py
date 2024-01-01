f57c03af16e99934f11a6d4cf5e83d518966b1e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:08 +0000

    turnup plan plant harm brush

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_list(array):
+        return list(array)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
