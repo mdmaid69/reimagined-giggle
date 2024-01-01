c0b40788ec1d9d3b5863adf6d5187a922bca20fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:33:22 +0000

    straight grade fill chin fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def count_characters(sentence):
+        return len(sentence)
