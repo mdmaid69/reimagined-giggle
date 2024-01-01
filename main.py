9e047a5cbff40fc029856474c808e9837b230020
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:12 +0000

    blame against suggestion comeout boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
