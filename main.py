5ac226a5e1eab0db0671cf0143812b3c185e9b47
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:43:59 +0000

    smoke convince below blame splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
