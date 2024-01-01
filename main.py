a79cfbfa29ff5542499db0ab3ebaa7a819ceff5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:33:08 +0000

    excuse pool house stranger spite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
