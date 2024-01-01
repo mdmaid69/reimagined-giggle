dd94c3a02b7ec9b2629d179407bb13c9b915e7a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:47:54 +0000

    note star grow copy label

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import datetime
+print(datetime.datetime.now())
+for i in range(10): print(i)
