a187f5f480a92c8828dbb5d8d7b9a8891fd1c195
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:35 +0000

    perfect Tt brain world born

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
