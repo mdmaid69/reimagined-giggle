b745ca870a4c6c7e5af9d5f06bb7b9a71a778830
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:03 +0000

    reduce float along set persuade

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
