9d37ead437dcff93cd7360b40507e1b680568dbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:34:07 +0000

    runout perfect quick black while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
