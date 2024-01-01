47535ce3e98ed1fdf08367ff7c73ba95fef14679
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:48:29 +0000

    stamp practice dark damage ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
