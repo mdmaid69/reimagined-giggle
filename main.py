d8a9ea41e596548c7122b7525d7ccdaa0d4f8e7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:12:38 +0000

    risk giveaway dull Bb sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
