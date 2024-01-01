0f7e6a3eb91e3068771b64cbde252f9560837196
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:25 +0000

    spread criticism cost nothing uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
