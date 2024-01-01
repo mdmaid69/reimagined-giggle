ed4d5b80d3b1b857360b65f2751a4cee874eb942
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:53 +0000

    example live pretty slice main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
