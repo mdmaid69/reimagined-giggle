cb90bf5e90959341388d1f3fc5e9c2a2097c912a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:18:44 +0000

    progress away container confused afternoon

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
