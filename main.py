8ed5813c1fe5325e10a50a17928fbbfae77c4281
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:55:09 +0000

    comeback objection equipment shout geton

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
