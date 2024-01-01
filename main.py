1804404b9c044d4e7fc8ac3c92ec68bc9b644ba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:31:09 +0000

    star rise kill shine loan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
