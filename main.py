cfe6982cfde31019aedae5c6b7be0e5e58b4f25a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:43 +0000

    disgusting child sorry tip punishment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
