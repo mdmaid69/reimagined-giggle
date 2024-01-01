4c5eb5b56d1cfd217772bef313f7351504ea9d58
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:55:08 +0000

    protest workon hollow shade study

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
