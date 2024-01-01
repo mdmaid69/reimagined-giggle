435753b8eee9fe2184da88b81a34418a04df58ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:53 +0000

    Uu minority turnup command disagree

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
