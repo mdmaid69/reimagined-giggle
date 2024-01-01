88213769bf059809792ce90f2e1f631956713419
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:59 +0000

    bill getback house stick scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
