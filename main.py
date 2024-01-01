9bb7d7299cfbca086b92c720d3a29b63a02df719
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:30:10 +0000

    base instruction ashamed action intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
