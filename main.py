18060173a93180200fb6a7ac60ac54ed9d732816
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:42:25 +0000

    upset unless Kk survive several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_odd(n):
+        return n % 2 != 0
