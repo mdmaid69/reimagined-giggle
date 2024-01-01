f853fc6a50187e63b5c0efe78f3cf00718f23c7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:02:40 +0000

    view several deliver below personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
