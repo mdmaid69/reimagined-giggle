b4559bcb198faf0e762b048ceee722afc920f3d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:22:21 +0000

    Tt choose telephone yes vote

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
