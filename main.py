809677bd15153e7af50e72a3eb3c2bc5efc7f0b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:02:04 +0000

    takeover image include mark mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
