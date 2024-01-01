64152b157625019805720f65f401058900aac8b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:04 +0000

    shelter even explain arrival warn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
