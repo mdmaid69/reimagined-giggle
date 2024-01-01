4d6b2a0e18d9baa843ddbbe41427ca6f03e5b5dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:16:40 +0000

    pollution protest involve letter rub

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
