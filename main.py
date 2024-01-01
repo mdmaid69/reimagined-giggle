e33c15c120cdb1cd347d40e5e66d97c78b5c46bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:37 +0000

    generally wish abroad blame power

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
