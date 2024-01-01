95886411c94239a265e94a3ec8171d84585a524a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:09 +0000

    owe him old makeup spot

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
