57e20542388526ec635111e7eb90e3daea8a0e22
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:20 +0000

    hard just crazy nice onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
