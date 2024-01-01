cdfc630022b168e0c49b44489ec8d7782519b238
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:53 +0000

    song part organize refer careful

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
