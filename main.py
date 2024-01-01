0ab098e304974215c77aff29207c6151e4360876
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:23 +0000

    contact organize about push demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
