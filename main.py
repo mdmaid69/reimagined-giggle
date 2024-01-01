dbf1e932c9b700e2cb732720ea4a2b7c687bd36c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:29:19 +0000

    used near ordinary bridge trick

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import tensorflow as tf
+print(tf.__version__)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
