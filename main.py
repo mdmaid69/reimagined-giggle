ef17a7f10a801a593200e41bdcbb39facabc66ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:19:38 +0000

    move explanation corner introduction lost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
