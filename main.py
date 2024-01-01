8e50fa980908253e5e88065ec9d31a5d23c363d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:02 +0000

    chin play national birth influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def cube_number(x):
+        return x**3
