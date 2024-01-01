77c0355bd4ae846b3dfb451ec47abb86b7946daf
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:04 +0000

    creature power improve organize freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
