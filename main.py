48f3d3f053f1cf7ae48bf6ff7454aaf5c261e017
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:57:06 +0000

    want turnup doubt spray must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
