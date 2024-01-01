9018da3f8dec87adc3364472ed94c8be9113c071
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:41 +0000

    cover shout concentration convenient boil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
