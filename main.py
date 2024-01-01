26d70850a381e0339305608c4315d755441ea667
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:12 +0000

    calm giveout stranger belong ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_str(array):
+        return str(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
