4df1b93c0ca28ab0b16f3f5c3de16641126d2bd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:32:05 +0000

    deliver book divide thank turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
