2acf2347195855eafb2af439cb5ede5d7c97dba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:37 +0000

    wherever easy report breakup comefrom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
   def calculate_perimeter_triangle(a, b, c):
         return a + b + c
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
