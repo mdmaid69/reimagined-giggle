209c35433b4e4922cfc7b7250d0725f79378deb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:36 +0000

    wherever programme explain interview day

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
