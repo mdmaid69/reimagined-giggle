ec502eca806f2fa02b5008325a2a803039881566
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:44 +0000

    among left anywayanyhow attach improvement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
