b2054bf9aa7e90eb9890079698a0febb8abd9ef6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:33 +0000

    pipe accordingto me error else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
