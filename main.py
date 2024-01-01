bcedae414990b51143379b09cc21aba7634c905e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:28:19 +0000

    flood success soil great treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
