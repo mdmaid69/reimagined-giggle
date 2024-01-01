4a33235d4954e59e0b8e7db45372c3efc7d43ff2
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:31 +0000

    minimum breathe measure language communication

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
