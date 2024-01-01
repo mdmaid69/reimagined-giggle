3a45896b01a3204e68d738d0602bc20665a4017d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:57 +0000

    flow name house form programme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_sine(x):
+        return math.sin(x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
