ad8bdde06177ea9fe4cf1d5503eb40e15ae88eb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:00:36 +0000

    thank pity later squash somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
