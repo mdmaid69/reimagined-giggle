82b7f28b9ed5146cd7f940968fb42bb4ba2012c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:35:17 +0000

    bill in keepout sign winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
