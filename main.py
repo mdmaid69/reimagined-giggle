bf1cb07dbb05885ba51f0cd43ce47f5a5f1f05be
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:35 +0000

    interested regret shout again shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_pressure(force, area):
+        return force / area
