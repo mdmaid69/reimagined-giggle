3dfcf8b0cc77f44196de203ca26c90c7d1b76ac7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:10 +0000

    mess die race country suppose

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
 import argparse
 def parse_arguments():
         parser = argparse.ArgumentParser()
         parser.add_argument("--input")
         parser.add_argument("--output")
         return parser.parse_args()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
