cdaee03904b2460592f8976d4712611fd8158d34
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:41:20 +0000

    avoid glad disapprove little yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def cube_number(x):
-        return x**3
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+def is_palindrome(s):
+        return s == s[::-1]
