25a85fd529f5f42283602128839c77706571b33c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:47 +0000

    sting prefer before major 571enough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def multiply_numbers(x, y):
+        return x * y
