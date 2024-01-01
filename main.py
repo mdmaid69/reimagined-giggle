b7cc0b9bc835dac78aa4e2984a40642af2ec96b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:11:19 +0000

    heat close fast comment cruelty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
