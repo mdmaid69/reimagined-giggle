0af71239327eacfa39e710d5946645945aab9e88
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:13 +0000

    female busy means answer appearance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_list(lst):
+        return lst[::-1]
