7830bb706d0ac5f99851ca3ff90f2fb8ca141b02
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:03 +0000

    want Gg very weigh attempt

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
