83066d037825f6d5fe59e353ed93ee5ed793acea
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:01 +0000

    takeaway easy lie force body

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
