b396e59f5b6272b1e16f09858641190a4b80e5e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:49:47 +0000

    least team mad produce uniform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
