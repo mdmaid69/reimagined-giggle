eb632a067eea9d20a3182aedb419641c51ca7a60
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:22:28 +0000

    respect more police generous no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
