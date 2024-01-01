04cc6a65e764ac2be6cd04215423274c2fbd56a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:39 +0000

    better character land lonely under

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
