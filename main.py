900cee5a89caf288254dcf185782473fe6009fa1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:16:27 +0000

    well cut suddenly steep put

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
 numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print("Squared:", [n**2 for n in numbers])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
