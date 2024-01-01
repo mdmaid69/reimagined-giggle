d371f773c7c882e661260e46490932cde81b114e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:08:54 +0000

    sort and plenty shine change

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sys
-def exit_program():
-        sys.exit()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_acceleration(speed, time):
+        return speed / time
