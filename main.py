46ff529f47b71f7dc30abfce5ee9283c7fc981a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:14 +0000

    major cheat stop near attract

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+print([x**2 for x in range(10)])
