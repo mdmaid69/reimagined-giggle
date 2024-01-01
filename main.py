36290c99708cb620efd2a3adbf551c7bf8662d9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:55:20 +0000

    aim contrast unpleasant detail trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
