945d1779365dcf5d6c732f865cfcbace5fbbd6ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:23:55 +0000

    history land commit himself interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def square_number(x):
-        return x**2
+def calculate_work(force, distance):
+        return force * distance
+  def reverse_list(lst):
+        return lst[::-1]
