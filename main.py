33e1c87ffa13816bb57a5a6487869c7d72c19d5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:54 +0000

    mix whoever air godown sweet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def cube_number(x):
+        return x**3
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
