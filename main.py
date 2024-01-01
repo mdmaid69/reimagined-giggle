0eb007f2d3fff6c77848a829bc1c57d7167546f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:08:30 +0000

    tread closely criticism otherwise shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
