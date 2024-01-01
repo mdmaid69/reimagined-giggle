a2520d5b4f035fc24bffae7539dee7fe1a122fee
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:03:27 +0000

    Gg other manner evidence actually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import sys
-def exit_program():
-        sys.exit()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def add_to_python_path(path):
+        sys.path.append(path)
