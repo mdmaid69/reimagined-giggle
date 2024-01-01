e6a202bc74bffb40974ebe78330ee8373ecb9b96
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:17:44 +0000

    treat dull whichever similar share

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+print(sum(range(10)))
