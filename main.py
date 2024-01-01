4de3e42c013b0d085dcb2cae0b0cc5266cae09f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:58:41 +0000

    along escape belong measure morning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_even(n):
+        return n % 2 == 0
