5d906f118e0cfc12232ff0f65c65e3bc6156eb36
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:28 +0000

    block score score match appearance

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import sys
+print(sys.version)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
