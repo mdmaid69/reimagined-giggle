b1e853c7e4200412390cdf94c10a76c3689d20c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:08:21 +0000

    entrance during translate noisy Ss

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
