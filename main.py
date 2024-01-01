bed09807034be70a4bab87386a8b5a91cbe3e0f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:58:16 +0000

    closed birthday anything throughout order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
