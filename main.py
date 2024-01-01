13ed0e8e86534360c7b43d3243a0724fa7c4a727
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:34 +0000

    team love takeover off neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
