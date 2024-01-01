359d1412547de96119c85e1e533fad3394c27c71
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:11 +0000

    study along certain broken disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
