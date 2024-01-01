329db551fe48c19b18e2b1115a9c06c06ee7d009
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:30 +0000

    snow human accordingto wonder giveup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
