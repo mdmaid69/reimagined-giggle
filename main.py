4d700d1d67a6fe83f0d77f41fca7082550894c1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:21:08 +0000

    interview save judge instruction wild

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
