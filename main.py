cd80f5e28f8c585431df1d4d4c641aef7a5ffa7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:01:27 +0000

    grand brave gentle store keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def count_words(sentence):
-        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
