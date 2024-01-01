b2283bc7a0b77b11a5fbf47df34e965776a0fba3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:41 +0000

    fact scale tough male unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
