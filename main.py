c41f901d3c7997ecae79f8eec6656158caed435e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:17 +0000

    dinner prize lookafter teach manage

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def convert_to_octal(n):
-        return oct(n)
+  def square_number(x):
+        return x**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
