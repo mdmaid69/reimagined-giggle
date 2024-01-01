291ebd85973c59b00d5daeafe4bcd53d28fa39a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:21 +0000

    house geton stretch sort dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def calculate_work(force, distance):
+        return force * distance
