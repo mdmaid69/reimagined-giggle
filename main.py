043ff636cf38b8f2ee7fc5b3e2a2569a2de2b1ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:55 +0000

    turnaway reason develop be yes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
