7f72e6b7ad62c0e5a885db977736c04b7dcf791d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:01 +0000

    reasonable write hollow beginning sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_power(work, time):
+        return work / time
