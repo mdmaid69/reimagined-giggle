93acda312e01f8fbe265887baa7ee9781c1c66ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:00 +0000

    polite seem long common this

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-for i in range(5):
-        print(i)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
