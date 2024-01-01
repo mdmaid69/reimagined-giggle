8b0a2206d07d3f35d05000845e0f1d1c8b428674
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:20:25 +0000

    close neck stamp turninto whistle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_area_rectangle(l, w):
-        return l * w
+def count_characters(sentence):
+        return len(sentence)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
