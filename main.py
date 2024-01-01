8ea4ff00b472e337e018a1512c8d41d979116ec4
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:22:11 +0000

    shallow hang dream within slice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def reverse_string(s):
+        return s[::-1]
