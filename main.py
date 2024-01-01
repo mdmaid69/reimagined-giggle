96fb9dbb79854d8f96114541dda86f096b04c0d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:55 +0000

    judge material bringforward reason Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
