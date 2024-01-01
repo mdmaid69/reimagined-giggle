0321e6327a0fd43e8588b294b846ab0d727bf111
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:44 +0000

    excuse object each know obtain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def exit_program():
+        sys.exit()
