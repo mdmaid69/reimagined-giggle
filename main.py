a31e83d8a66671be5774144ee49a88f092bc5781
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:24:09 +0000

    equipment loud keepoff maintain put

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Characters:", len(text))
