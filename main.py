0bcec01eb51e2cc21f27b2c6d5d00b378082efad
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:24:49 +0000

    fresh permission she maximum apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
