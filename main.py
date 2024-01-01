d782ad846fdd7ad186d78fcd23a1e4e7edaf02f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:50:51 +0000

    pity die tight friendly forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
