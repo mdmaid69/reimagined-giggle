e851b1411b171874a2f210e0f8e19295c1392e10
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:58 +0000

    unfair project attach free eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sys
-print(sys.version)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
