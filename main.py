23fd380d34109718b8940ce6f69b7f30fa9ad0a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:49:15 +0000

    workout either return keepaway work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
