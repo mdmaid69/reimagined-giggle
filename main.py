a58b04a432dcea77500e91a888eb027331580f7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:19 +0000

    typical married communicate cruelty no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
