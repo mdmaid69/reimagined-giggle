6d729d8d9da69f942e806f2fae70fd4f2d0b98dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:57 +0000

    win hurt touch count furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
