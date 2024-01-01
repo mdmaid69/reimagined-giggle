200e68effbb5c99d2bfad17b74e53be8e85f230b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:47 +0000

    from measure belong please black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
