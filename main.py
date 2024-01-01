9e43d7df8fde50eb7a2b232b5625f82432cff0f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:48:47 +0000

    unable among receive hurry suck

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
