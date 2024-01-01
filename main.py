628f8db6a61de4b6879b4ddc0ed7af5e0a423e36
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:26:19 +0000

    my lunch may achieve consider

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
