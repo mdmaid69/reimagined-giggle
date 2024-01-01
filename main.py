807099610827c08f4b84b86f1dc4c6e9da589a10
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:56 +0000

    fasten fix swallow action sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
