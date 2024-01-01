27ab1105c6290e3bdd895a4280cba98973ee6099
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:02:53 +0000

    prepare scrap exercise smile seem

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
