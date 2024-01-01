f1fb154d950566819aa4524bca6325c1f0d99792
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:55 +0000

    scrap string present importance shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
