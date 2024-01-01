38a8bf728014f23f74dfa693ce196d6334040883
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:58:33 +0000

    pair firm tap suffer anxious

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
