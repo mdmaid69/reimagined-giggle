bc860b18ca4aef8d278ec645b3ac40f24d53fe7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:20:21 +0000

    moon mad sort definite talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
