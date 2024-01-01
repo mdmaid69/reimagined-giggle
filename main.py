1ba074bdc3371f2b61767adb90be3b779e673608
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:35:00 +0000

    police shopping confused lunch joke

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
