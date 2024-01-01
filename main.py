6971b8671acc5f888413a6f61650874a5726e4c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:50 +0000

    steep today contrast common goup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
