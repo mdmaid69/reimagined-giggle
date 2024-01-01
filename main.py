0e837f7d4470930aabe155ebbca3560be2845eb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:25:27 +0000

    breath trade community fence makeup

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def create_user_dict():
+        return collections.UserDict()
