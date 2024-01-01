143d815efdc126217331293d3c8a065c038f9578
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:34:08 +0000

    table above climb nigga secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
