fbedf55843cf2b647486347d6761d65af6ed49fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:51 +0000

    bound design difficult ever beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
