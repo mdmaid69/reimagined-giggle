7c7c1cdd588b752a8629899511150782924c7c95
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:40 +0000

    office gradually if hate way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
