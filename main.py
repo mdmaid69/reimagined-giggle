fcf0e37065c9cabcef7b73d34843cf8f4bd893d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:14 +0000

    us choose lower her war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
