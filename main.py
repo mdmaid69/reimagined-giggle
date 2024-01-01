276344bb0eb304f5360698d1fe136c801468ac73
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:47:33 +0000

    very manner chin drop defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+print(time.time())
