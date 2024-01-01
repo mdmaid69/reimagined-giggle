961af0f6aa08fc9cdc40b141d608a5b2fd3690ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:24:20 +0000

    urgent scene smell container bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import collections
-def create_queue():
-        return collections.deque()
-import sys
-def exit_program():
-        sys.exit()
+def create_ordered_dict():
+        return collections.OrderedDict()
