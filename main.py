bf9562a65d14c7f788a8dfc2f4f4bc80b5618efd
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:07:57 +0000

    clean gradually doubt thinkthrough room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
