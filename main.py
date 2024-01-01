1b65b186304524f3f298ac009a15900da92fa2b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:33 +0000

    risk cry emphasize close tool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+print(math.pi)
