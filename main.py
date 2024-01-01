f5540d0558a68ef74c5b234e4437163e262cbd07
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:22 +0000

    positive accidental year bottom common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
