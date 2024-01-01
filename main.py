e6e7d9aef13b83ab954dea5336c5b6bf3bf99b40
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:59:17 +0000

    become wrong giveout breakup sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_even(n):
+        return n % 2 == 0
