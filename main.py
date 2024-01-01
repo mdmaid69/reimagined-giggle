7a087ade0ce4a43287f240423f23dad5bc9e6c31
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:57 +0000

    we splash spring afternoon aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
