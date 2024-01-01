4c6796ca6d111e81f7a710624edd16c09d475b65
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:47 +0000

    empty uniform bin know thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
