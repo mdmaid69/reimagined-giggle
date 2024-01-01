18fbb01e423d3dcebf422e8df2f8e8575335be71
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:43 +0000

    woman expression steal interest weak

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
