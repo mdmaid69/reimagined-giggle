b6710e9754c26ab4125db7ed99b8d19f5f31eeb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:21 +0000

    below maintain stop bang letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
