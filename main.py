5854c43a0dc94812a71eafdffc5f27c65159f080
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:09:47 +0000

    date because plastic suggest this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
