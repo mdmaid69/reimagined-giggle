dc0324aa20c144935c70cbf04930b020b8a3876f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:21 +0000

    workon exit shape gentle touch

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
