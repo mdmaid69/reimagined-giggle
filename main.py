f4e312e67e74849075f114f507fdd5cefd16be42
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:06 +0000

    fill invite excited dare forget

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
