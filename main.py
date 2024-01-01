5942339d98fe1d2d3238d6b20063fa0836aa2511
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:22:03 +0000

    street habit work yet preparation

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
