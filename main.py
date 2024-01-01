ad7ad34d6ad1c8c593872faa1c76eef232b556f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:19:42 +0000

    570enjoyable narrow 566end several out

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
