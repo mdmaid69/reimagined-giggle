f8601bdda61d602fb97cd04eecbdee446c33f167
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:53 +0000

    yours children feature deliberately disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
