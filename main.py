b1e49d4cf932602c3fb05c48c21db274ce62f5c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:20 +0000

    preparation kind impressed guide load

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def get_current_time():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
