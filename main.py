1b78f88f6e7210477ddc52e37022be7c35d3dfcf
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:33:00 +0000

    limit we perfect prepare leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
