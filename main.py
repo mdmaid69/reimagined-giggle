581c16a1ccafcb1b86a44fb3cfc74ad6f1380f77
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:47:45 +0000

    below sudden possible Ll frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
