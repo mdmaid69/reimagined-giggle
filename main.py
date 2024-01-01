af62980703ad79392b311602bf6c03b39e417582
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:59:19 +0000

    typical control strike develop nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
