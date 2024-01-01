451f9251e7b1160e0b308869745f80882d55051b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:12 +0000

    dream the everyone sign influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
