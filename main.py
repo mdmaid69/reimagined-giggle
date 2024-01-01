c910e344014aae5a9cec338a4e5f28102ee96011
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:01:27 +0000

    smile long ever science shopping

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
