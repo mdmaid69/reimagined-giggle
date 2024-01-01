6b32f9cf97f1d0559ca0deed82722aec0815fe29
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:08:12 +0000

    easy take poison shop affect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
