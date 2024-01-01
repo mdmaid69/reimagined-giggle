4b84516d6a7ecd4b2f3e9dd9c1b07f6cacd1aa85
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:10:46 +0000

    ability actual evening later ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
