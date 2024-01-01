f6fcdbd3fddbde02c6b85161a1866fef9376d158
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:18:16 +0000

    through know me obvious responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
