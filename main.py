04c88dda0efa83fc46951ca988cc04962bc84530
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:21 +0000

    woman matter getout confidence over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
