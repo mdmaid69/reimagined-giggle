4192b8280abc03e365271d953463e56192d1f3bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:34 +0000

    threaten aim cry tight talk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def reverse_list(lst):
-        return lst[::-1]
+def is_odd(n):
+        return n % 2 != 0
+import array
+def reverse_array(array):
+        array.reverse()
