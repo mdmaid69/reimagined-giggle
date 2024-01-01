f435fd0b8c92f25a6b32ed2b317c8f5f19cf2439
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:59 +0000

    swing sell rather open myself

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
