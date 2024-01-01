6bf2d78eaebf462fdad3c5b59b1d0359cef3600e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:48 +0000

    mine direction workout geton Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
