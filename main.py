25ddd9d767cbb2c9d45a5bf0a01557905df3ada0
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:51:24 +0000

    experience copy pinch off hand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
