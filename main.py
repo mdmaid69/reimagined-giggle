5572b53feba0824f29fe6a82feb88a4347e7270e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:11:15 +0000

    share steal whichever succeed straight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def add_numbers(x, y):
+        return x + y
