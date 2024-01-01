db3b7d57d5cd3095758f6369268ffd0c2e93f315
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:49 +0000

    discover union mistake trick sometimes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def add_numbers(x, y):
+        return x + y
