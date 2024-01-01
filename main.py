4c46e204c13e172274a1f25eaa6c8f01b399466a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:40 +0000

    can pass emphasis shadow future

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
