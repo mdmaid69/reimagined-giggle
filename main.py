ac9829a20d7c33ceafdee76666d71c1ef89f407f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:39:39 +0000

    use ring fair storm achieve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
