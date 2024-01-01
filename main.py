fd53db54f0df86de9b9c0788b4febca7794c0a19
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:12:27 +0000

    Zz sick thought deserve plan

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
