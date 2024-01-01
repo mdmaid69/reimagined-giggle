6c1216a92d5c4a9afc0a44fe2d9264c5d582bd8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:09 +0000

    Kk no fond lead period

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
