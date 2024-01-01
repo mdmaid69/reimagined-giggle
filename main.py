d8b1e1fda8fb25f232213d1a1facbe680f3ae28a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:39 +0000

    form open rule sort stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
