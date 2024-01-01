f4af19e37d144a38f6d7f1ffe582d3636d95bd54
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:48:21 +0000

    rate most fun pool talk

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_power(work, time):
+        return work / time
