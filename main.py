53b8a6023fc45fb31bfb683ba4ce8b2d8ce1c0d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:22:43 +0000

    lookback(on) usual exploration never sea

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def convert_to_hex(n):
+        return hex(n)
