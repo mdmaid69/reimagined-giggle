ac9ae766a7fb8ab82bd82a07607547dd1295d219
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:41:27 +0000

    capital loose comfortable dress deep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def convert_to_binary(n):
+        return bin(n)
