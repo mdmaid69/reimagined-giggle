8ee89dc0605ad7ac06b28e0a33e4fe670c3712d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:04 +0000

    exit remember land cruelty yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
