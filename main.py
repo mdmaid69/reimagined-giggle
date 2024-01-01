b57189e88fdc34ea876c3f40d0d57bc9fd2b5b71
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:51 +0000

    total right pin usually weigh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def reverse_list(lst):
+        return lst[::-1]
