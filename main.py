91f6dc78257fb927217d03d72c3bc15dc549255d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:04:04 +0000

    soft illness sweet tool me

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
