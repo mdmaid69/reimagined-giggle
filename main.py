bee5316f2df76f9f64d68f079b2dab5545ff703d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:45 +0000

    worth argument any onto 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
