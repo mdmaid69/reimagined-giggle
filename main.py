51b2e96e8622846e89896ee5cd0d41ea8d0933cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:36:24 +0000

    unpleasant no loud belong job

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
