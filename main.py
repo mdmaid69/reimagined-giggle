2c49586989cf999c1c25058629f011ea560b71bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:50:19 +0000

    smell live government medium polite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_max(lst):
+        return max(lst)
