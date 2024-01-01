08053d87f0947bb3126b16d8d44cb3960d4abed2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:50 +0000

    form habit idea Gg basically

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
