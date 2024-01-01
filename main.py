c8bea7aa8077ce98dd91813fe521b47de64c2611
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:44 +0000

    seriously include round block professional

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-for i in range(10): print(i)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
