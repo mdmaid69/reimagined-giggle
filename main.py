5c27ad622eeba8c5bddf7a1b80bf9ebbccaa1c2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:14:41 +0000

    regard hot block ground neat

--- main.py
+++ main.py
@@ -1,12 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
