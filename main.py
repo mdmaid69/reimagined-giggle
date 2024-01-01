a95b27d45a784bafd4aa1454b95d5b2f10e07fff
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:59 +0000

    tooth admit government result impressive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def get_array_as_int(array):
+        return int(array[0])
