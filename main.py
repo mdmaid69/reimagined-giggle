f8bbd109079c1daeadae9559af6413709e6a8566
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:23 +0000

    wind level flavour claim bright

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
