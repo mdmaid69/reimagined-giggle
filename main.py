80360861c606426cde6c653e89b5c9dce465c478
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:46:46 +0000

    pleasant connect think off track

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
