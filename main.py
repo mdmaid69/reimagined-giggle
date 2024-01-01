540e2f416230da8d6ed568b3e28909f6e4926969
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:39 +0000

    muscle custom seriously acceptable reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
