46e83489138a3ac85c16dcf7f4d7ba7bbf2e552d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:52:53 +0000

    mistake truth traffic brain surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
