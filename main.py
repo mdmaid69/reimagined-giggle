d806c22745b27bc593895f81deb927c7b1f61c97
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:20:27 +0000

    afterwards vanish hold clear block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_directory():
+        return os.getcwd()
