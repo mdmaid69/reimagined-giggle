f83e9c920557ef6eb5048094ec19bc9eba2a75c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:17:50 +0000

    rise defeat catch early brave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
