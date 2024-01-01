7e867ec2afab7417fbcf4a6029f4719d155f3911
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:04:26 +0000

    software place limit store tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
