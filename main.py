3821d19c5e657b12688afc67855ce8cc9a4f16b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:48 +0000

    somehow confusing exam cold to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
