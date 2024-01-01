294037f71bbedb789dcd651811d66f0bf4e7dac5
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:13:26 +0000

    search clear amount lay female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def is_palindrome(s):
+        return s == s[::-1]
