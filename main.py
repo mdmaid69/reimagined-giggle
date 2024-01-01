2b23cc92083d8ce895a14298843625842c697f5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:19 +0000

    plenty Ll breath earn understand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_volume(length, width, height):
+        return length * width * height
