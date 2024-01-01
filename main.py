696cd359a126be37127876a6c5923db0d7417f33
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:00 +0000

    my wrong joke understand previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
