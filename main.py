3bc43b1348e9955424e9666c8deaa66b3adb77be
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:01:39 +0000

    worth ahead float turninto leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
