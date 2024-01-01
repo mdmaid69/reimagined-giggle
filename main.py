404038ec600c4bfa9fc65140621885d0a064aa3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:47 +0000

    impatient repair nervous dear what

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def multiply_numbers(x, y):
+        return x * y
