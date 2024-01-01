1f3797363a65ff4a440d069267947f8d521194c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:57:42 +0000

    ahead mind burn some spread

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
