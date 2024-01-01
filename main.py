461b074db074987c73f1db880637ba7959f2ca7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:57:16 +0000

    news beautiful vanish something mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
