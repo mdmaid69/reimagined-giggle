82085e262bdcd4116f9134cc513b8f241196525c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:49 +0000

    of operation fly clue ignore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import array
+def get_array_as_str(array):
+        return str(array)
