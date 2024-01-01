99ca43b5148bd90a8d56f31b68c5a1337c9e6034
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:39:31 +0000

    phone distant wave 570enjoyable comeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
