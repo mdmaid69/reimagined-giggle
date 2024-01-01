3ae8847a458478df98b1720a93ab5daf66d412e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:17:55 +0000

    home still completely actual object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
