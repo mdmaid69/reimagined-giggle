eb362cb39bf5eba9294928aae610a1bb54073314
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:18 +0000

    squash best name abroad back

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
