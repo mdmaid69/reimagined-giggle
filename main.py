25e642ad60fa351f67853a38a5d91e9886ab5596
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:50 +0000

    notice raw shop land public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
