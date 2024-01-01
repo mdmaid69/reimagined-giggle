b41fbcd1763b8b42e1f8c430c775dcc53312dafd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:43:10 +0000

    workout could blue walk clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
