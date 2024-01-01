89fec67b3c3fd728ed76ca04692bbfd1e0611936
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:05:55 +0000

    talkback black train their heart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_average(lst):
+        return sum(lst) / len(lst)
