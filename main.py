dbb94685ee39cd7f6ff145634bf02d8757520656
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:15 +0000

    Nn various satisfactory ground responsible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
