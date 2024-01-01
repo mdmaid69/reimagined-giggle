343609671be86c773eccd05964dd6fe0bb973027
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:05 +0000

    tooth speech produce original loss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
