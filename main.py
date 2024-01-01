7a0fa1eb2cb5ff76e49b1f364eedc18328b60616
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:32 +0000

    human suggest bound guess worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def calculate_work(force, distance):
+        return force * distance
