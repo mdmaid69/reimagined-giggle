0d84be5b95593f51e23ac8176d6653ac25dec4c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:10:23 +0000

    zero no bear refer demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import math
 def calculate_gamma_function(x):
         return math.gamma(x)
