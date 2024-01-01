791c600c63147120c31c45206d3423ecac587b8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:31 +0000

    start post conclusion Qq task

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
