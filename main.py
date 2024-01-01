7a6f00f249f8784e4305a170480ea29161f40c57
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:13:23 +0000

    pile drawing real basic action

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
