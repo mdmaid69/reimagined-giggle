a9069270c7c2958b6860384d16d55ec54c003d8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:55:01 +0000

    whistle comeback fresh maximum attractive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_python_version():
+        return platform.python_version()
