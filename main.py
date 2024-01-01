d081678aa9eedb7f807ded3a174e11928d5a1949
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:03:00 +0000

    software fit invite fail block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
