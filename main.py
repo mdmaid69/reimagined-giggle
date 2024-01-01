1c0184436ddba6cdbd4fd62e918249a70774d3ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:28 +0000

    contents destroy negative exciting condition

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
