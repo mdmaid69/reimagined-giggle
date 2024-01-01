eef078f1d021db61e09867fc6279441daa4e7edd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:32 +0000

    excited standard cook chat drop

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def divide_numbers(x, y):
-        return x / y
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import random
+def roll_die():
+        return random.randint(1, 6)
