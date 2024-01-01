432b16482961b8a1a9cdba08012b74dee5ae704b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:01 +0000

    more favourite thorough it thirsty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def is_palindrome(s):
+        return s == s[::-1]
