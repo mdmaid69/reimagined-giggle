74bf1537e90560d0403cad2d051102b18bd6878b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:28 +0000

    complete fact feeling whom quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
