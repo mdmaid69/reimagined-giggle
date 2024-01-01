ee6984f02a731030a4121bb8ed8a0b7a231ae4b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:47 +0000

    wire interfere suckme burst Ss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
