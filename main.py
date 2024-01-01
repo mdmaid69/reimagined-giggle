d4156780cfffa5d9d321abac8cbb07c7d5c51da2
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:11 +0000

    tough quiet pronunciation present wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
