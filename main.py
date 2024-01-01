33dfbaea1afbe83e8e7733d25c65e11dc1b55cfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:06:36 +0000

    choice lost use crowded bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
