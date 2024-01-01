c8bae44b3e95b5f1bb4264a2a203e5bab88beed9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:13:45 +0000

    flat mother star embarrassed clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
