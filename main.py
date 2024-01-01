b984824ecf587bbdf555546a440252627a253fae
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:10 +0000

    guard term fall improvement 567enemy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+print(time.time())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
