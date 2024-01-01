48269fa40d1712eda88bc0b5ee8719e0ef89d668
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:18 +0000

    steal little achieve show operation

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
