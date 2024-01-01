44b7c84a5ec6f1d1ee7f5cb96ffdb0ff05b0d9df
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:34 +0000

    fit pleasure art bring combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
