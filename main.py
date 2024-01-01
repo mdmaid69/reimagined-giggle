01c9471ecf9b1709e9f2d9bb794ec60c51f1f14c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:56:01 +0000

    effort expect genuine better not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
