78852d332e5d85fd4c924f10fa6650cdd2a611fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:52 +0000

    impressive fail comfort sail sister

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
