fb7bc6c03cde0da313d435a7338f96875b2d930f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:54 +0000

    nearly afterwards division exchange disturbing

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_area(radius):
+        return 3.14 * radius * radius
