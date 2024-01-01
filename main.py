8dc3e03f72af4908e1b02c2ce264f91f2a155064
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:42:03 +0000

    calm smile boring raise cause

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import datetime
+def get_current_date():
+        return datetime.date.today()
