799bff940874125db29768394e3c93a0ab2d4087
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:27:37 +0000

    one standard everything sweet noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
