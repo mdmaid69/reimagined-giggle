907cc7b7bf20297446870ad48c0812f5eabff6ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:02:47 +0000

    forgive join breath attractive still

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(10): print(i)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
