2a3f5db4242cd1c50840adc6792fd86667ffa860
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:31:49 +0000

    broad exchange underneath pronunciation news

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+print([x**2 for x in range(10)])
