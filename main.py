3fe532dd822d59377264e4fd40fb926608a45c88
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:48 +0000

    contrast embarrassed high quiet ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
