ee59d4979630e89217f47287a063a8b8d6263ac0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:22 +0000

    sad behave computer myself shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
