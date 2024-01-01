869d75101115cf2b9888b62dcfbf340a8151bd5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:18:15 +0000

    chin remark key report vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
