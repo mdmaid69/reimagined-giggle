c8922ebc0541dee95dd536cab623ef53b6b3851e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:48 +0000

    offence petrol embarrassed simple challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
