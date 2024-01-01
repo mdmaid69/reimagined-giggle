34563f7516415293c2d0c4ee62c59a5f715fca7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:28:02 +0000

    snow maybe escape above calm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
 import logging
-def log_message(message):
-        logging.info(message)
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
