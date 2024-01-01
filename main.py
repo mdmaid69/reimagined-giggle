bc7bdef494775016d3faec5abde075211bc77da1
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:32:03 +0000

    person famous pot compare entrance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
