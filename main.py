567540c347b634ce97f7ee4346a2914b4756121d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:58 +0000

    care tidy foreign turnon still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_itemsize(array):
+        return array.itemsize
