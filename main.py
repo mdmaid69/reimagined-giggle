3a2ce91a30dbbb83c5716da9727bc3e13d16f663
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:04 +0000

    thinkof load his fail zero

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
