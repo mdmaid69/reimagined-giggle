29c29631a90a27101f5906d314c52b720affca52
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:00:14 +0000

    mess chat conclusion spring lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+print(sys.version)
