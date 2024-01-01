a450bc390da718174665c68c830dc46ac0e4a5f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:23:01 +0000

    dangerous soil shop support check

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def convert_to_octal(n):
+        return oct(n)
