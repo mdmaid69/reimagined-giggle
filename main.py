07a68d8a1a36972348b8f4d8a81d915def446afc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:44:10 +0000

    rent compare objection power bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_current_working_directory():
+        return os.getcwd()
+  def is_odd(n):
+        return n % 2 != 0
