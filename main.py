967b57b18050810c2202fdcd0b47e2c63407155e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:40:53 +0000

    spring doubt exactly finish rise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
