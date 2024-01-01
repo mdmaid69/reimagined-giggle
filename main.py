bd5e25bfa85c7d848d42177c1c6bb60964b8c7e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:58:04 +0000

    clever also deep edge steer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
