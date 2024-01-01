333e03cf902360fed1717bd226748cee96c20667
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:37 +0000

    precise my present although story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def get_array_typecode(array):
+        return array.typecode
+import array
+def clear_array(array):
+        array *= 0
