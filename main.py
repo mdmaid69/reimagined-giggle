052ce1e2fdac39d11fc65f663b6b3b07a24327be
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:40 +0000

    size catch interview bottom black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
