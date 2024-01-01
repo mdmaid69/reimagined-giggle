4beaca2048ec13ac3584bd9e04ff7246efe131f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:29 +0000

    rub possibly piece fold land

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
