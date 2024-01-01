8465b881bf014be758016df963dc8fac2291f3c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:31:53 +0000

    necessary together persuade Uu snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
