5b81057cefecbacc5d520bb91b00394209cf46a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:24:27 +0000

    grand rank giveaway offence judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
