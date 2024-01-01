9f91a76091bc7aa601580b5f43f31edf8a610a48
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:12 +0000

    trade pride away dull man

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
