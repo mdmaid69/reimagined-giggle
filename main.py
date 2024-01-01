6eb1c7ad55f17b4625a9ff668a698b9bdf09284e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:49 +0000

    left nor issue fence conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
