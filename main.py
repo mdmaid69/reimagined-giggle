4953d9d8cc77882b0cdf2a8bfe2ccec5efd14379
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:18 +0000

    able accident coffee race everywhere

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def cube_number(x):
+        return x**3
