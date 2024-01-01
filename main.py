7201aaa777870fb08c81031ad595a5aa5082ae0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:22 +0000

    entrance talkoutof load getaway entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
