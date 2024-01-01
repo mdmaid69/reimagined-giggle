ed02eae2228ae05a164aa0fd2daabbcdde6ceeac
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:24 +0000

    summer judge search club job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
