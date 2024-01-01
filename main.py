2c36f7102aee7b01f4e796ba45aab6a72df195e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:02 +0000

    become themselves rarely mind spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import time
-def get_current_time():
-        return time.time()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
