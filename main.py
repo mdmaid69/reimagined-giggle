dfc1f762263cbaef13b919b6c39e013e56c47b9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:09 +0000

    brush separate across inquire us

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
