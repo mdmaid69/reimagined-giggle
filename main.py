3140e193b60a5acb225b2e175bb601ff0db3e0e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:32:45 +0000

    use round bin face depend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def get_array_as_list(array):
+        return list(array)
+import collections
+def create_stack():
+        return collections.deque()
