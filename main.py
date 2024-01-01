d1a7e5ed2ea87a21cf1363b45aaf658c79fd3082
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:16 +0000

    tall machine guilty guide behave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def split_path(path):
+        return os.path.split(path)
 import sys
-def print_python_version():
-        return sys.version
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+print(sys.version)
