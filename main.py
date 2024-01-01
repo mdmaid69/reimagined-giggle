902b8c4ac0756c3efd96ec8a675357e265a20ce0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:52:46 +0000

    present some least badly frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
