4c856c23a98cc639dfa1af375ce55f0af0e6c90b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:31:45 +0000

    unkind Gg judge face label

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
