fc6bab882635c4d59ed950c92242d5c8d936447b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:54:22 +0000

    develop practise turnout size mark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
