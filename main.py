4c4b46d000251434d5314059898619b8b4344891
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:49 +0000

    history speed sure uncomfortable direct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
