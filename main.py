aab7abd2e426906aae9be702a5d09ec053912727
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:16 +0000

    lower target may within for

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def reverse_array(array):
-        array.reverse()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
