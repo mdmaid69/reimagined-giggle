2fb54d2d333ba8f2ad6e359efcc4e7c5668a3515
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:46:53 +0000

    attention example world more second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
