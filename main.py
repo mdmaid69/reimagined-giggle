6218bd2bfac43fba1ab12f86438ecedeca383055
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:45:37 +0000

    tall market talkinto serve old

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_work(force, distance):
+        return force * distance
+def count_elements(lst):
+        return len(lst)
