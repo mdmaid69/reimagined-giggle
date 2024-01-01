bb553230c67d624199d186e887f41bfa3972c098
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:06 +0000

    splash delicate separate brother add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
