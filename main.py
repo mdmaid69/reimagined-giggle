fbc0fb62cb6f1090928be6d9d5862ece17428758
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:09 +0000

    bite nose might wherever workupto

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
