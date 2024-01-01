a7875b398f675090c18be3a10a1e988d3a865043
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:50:27 +0000

    joke own runinto wealth slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def reverse_list(lst):
+        return lst[::-1]
