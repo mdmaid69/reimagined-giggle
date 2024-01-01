3b769bba15e9fe84da6bcb74c44825577761e9e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:59:40 +0000

    likely stretch most complete equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
