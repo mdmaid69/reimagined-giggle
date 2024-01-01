d7f58d621c459fdb9e697499b7205944f40fec33
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:57:31 +0000

    inform influence order single Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  def convert_to_binary(n):
+        return bin(n)
