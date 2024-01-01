f8671388b35e0696e144510b197052d811284380
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:34 +0000

    letter condition decision club rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
