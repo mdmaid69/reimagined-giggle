21995b360f8c1aaa8d6207d6c3c003843f5dfa6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:19 +0000

    happen find stamp keepaway sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
