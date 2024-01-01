beb07fe3d426655d2b74acaf6d976d0d734d0e6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:35:09 +0000

    exploration petrol pin space near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
