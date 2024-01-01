c6a0b7b98ad6ce123f0edbc72b1f89712bd69a36
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:36:32 +0000

    interesting deliver unite behind organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
