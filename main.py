fe7dcc5295d0b6448e97fc0b5a902dcb9de4da6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:07 +0000

    both kind song on comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
