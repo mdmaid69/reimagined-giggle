b2221e89bd492068747d8d6d02d36114a463b16e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:30 +0000

    might condition sort interest share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
