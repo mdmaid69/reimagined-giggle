81a27233604824a5c34da17ead97d517def6e520
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:30 +0000

    middle better impossible tap drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
