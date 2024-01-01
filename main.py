21f3931ec8b7f5459c90d8c0f194448f512c36c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:47:58 +0000

    might politics Yy stamp value

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
