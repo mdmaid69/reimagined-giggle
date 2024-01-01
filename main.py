f86dd396c57833753a04d7c2bb930391b63edc74
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:06:38 +0000

    convincing dry many unit improvement

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_as_list(array):
+        return list(array)
