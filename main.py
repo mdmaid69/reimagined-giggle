5dc1a1c9629590332b61853f9802f60df4f27c6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:45 +0000

    silent how urgent thinkof right

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
