c18d0fc1e695855e1881a6987f1ac5ca66519781
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:28:57 +0000

    common more themselves force bright

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_work(force, distance):
+        return force * distance
