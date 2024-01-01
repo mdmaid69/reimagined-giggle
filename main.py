80ff61596d0dd5f00d18953e323222b4b673a46d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:30 +0000

    future available show nonsense part

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
