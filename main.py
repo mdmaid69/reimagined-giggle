7a702e713b890cd16de918d2e21deb0c9fd31457
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:30 +0000

    innocent pile clean meal statement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
