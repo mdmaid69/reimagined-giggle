6f0b5e7b0e0c65e3d37e5c708b0861c13e98682c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:55 +0000

    luck mark turnaway agreement dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
