0b15125eecaa93e1215db7c66ce4d10c0401d3d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:00:39 +0000

    disgusting practical border broken 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
