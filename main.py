c4dfe8e953001da58ac81e0e46ffa7f71b12768a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:42:07 +0000

    bath hide either cheat wide

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
