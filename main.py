b874382e7a39c63f17adc9964d60d9e7bf856532
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:46 +0000

    embarrassing underneath sudden satisfactory doctor

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def find_min(lst):
-        return min(lst)
+  def convert_to_octal(n):
+        return oct(n)
+for i in range(5):
+        print(i)
