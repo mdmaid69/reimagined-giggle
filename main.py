8eaada8af750da865f1084d4b7e5141c906db038
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:19:05 +0000

    OKorokay invite marry put effort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def print_python_version():
-        print(sys.version)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def cube_number(x):
+        return x**3
