43040508413349d1e83ce847b4de69c91ece6268
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:26 +0000

    politics free profit lend skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
