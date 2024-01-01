2f610f58bb2eb96507ed282a2e499b6b43395669
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:23:02 +0000

    hollow burst pause middle crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def get_array_as_bool(array):
+        return bool(array)
