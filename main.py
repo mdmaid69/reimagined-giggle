840e860b2f28d6adf799908bbe1f90507fe44251
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:39:53 +0000

    female reasonable period attach of

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+def change_working_directory(path):
+        os.chdir(path)
