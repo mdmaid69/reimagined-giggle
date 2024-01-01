2c6cb06347fefc4cf24fe1982803f2afdecc69d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:35 +0000

    declare average there you translate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
