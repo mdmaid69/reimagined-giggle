18b682263913f9fef5cc4c394aba2c8f1997caec
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:32 +0000

    beat thorough realize home blow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import logging
+def log_message(message):
+        logging.info(message)
