b949ce5be944522389abfc8a3e581bccdbb7f2c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:09 +0000

    love clever proper travel cinema

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
