6a7327dde10852a8d8ce4898ab3780bdf2da4251
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:09 +0000

    little case street approve rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_as_str(array):
+        return str(array)
