6b8c9a141d37e9a58c57e66b439be97df77e4cad
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:21:33 +0000

    herself add quiet keepfrom oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
