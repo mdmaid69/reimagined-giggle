5e8781ee2ea98c5e9b0bc29990f0545e1bbff8bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:53:27 +0000

    ability by holdout opposite last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
