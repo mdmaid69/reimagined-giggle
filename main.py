09de29f1143a63e4d2c00ce6ffd656d5a0f8ad87
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:00 +0000

    close form means tight open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_min(numbers):
-        return min(numbers)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
