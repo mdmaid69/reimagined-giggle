6900b647550f3c4d3b8603209584220ab1385bc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:53 +0000

    near add track black they

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
