9d81d4e685ab2ec223ed22272308ebf0bdfcbda4
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:56 +0000

    hard cap high claim shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
