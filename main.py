9eca3d55328e561caee83cf67e3e57b1b8b20183
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:22:11 +0000

    sort impossible address inform worse

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
