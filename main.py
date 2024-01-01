f57bf60eeea9c5cabd7c60a728a49562375ea1a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:39:07 +0000

    along present touch book centimetre

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print(sum(range(10)))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
