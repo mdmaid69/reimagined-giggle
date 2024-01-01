1e757fdce49a9ea271c0efa6a719aabb1f8e19e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:19:27 +0000

    unite turn roll look nose

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_power(work, time):
+        return work / time
