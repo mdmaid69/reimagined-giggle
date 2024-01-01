434f1cf48b748bcc3d4b7872445d95bbbbaba09f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:01 +0000

    favourite recently change grateful so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print([x**2 for x in range(10)])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
