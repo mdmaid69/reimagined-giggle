112cda1e85671b51b31c5b45a017516e7037e9be
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:09 +0000

    long answer breakout money regard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
