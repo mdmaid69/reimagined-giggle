57e68f1a8df51575344bd680cf6dd303e6a93b8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:50:28 +0000

    distance chance introduce black before

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
