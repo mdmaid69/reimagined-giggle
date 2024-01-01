b578280b4531f197c13af65d37cfb77be143bcb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:42:27 +0000

    play plain there coat praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
