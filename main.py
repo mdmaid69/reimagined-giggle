f24ed31414fd9a146510a51497db65749d94e517
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:33 +0000

    cost relative approve top discover

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
