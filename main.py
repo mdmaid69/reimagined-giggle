1f9c2cb6790fa5afa37d2027f32359ce98f4bddc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:42:45 +0000

    minute model bringforward seriously bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
