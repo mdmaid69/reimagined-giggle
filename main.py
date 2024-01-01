94ecf80f526fd51b7cb1a775f7342680b9c4cba4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:59:52 +0000

    climb not hard interview able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
