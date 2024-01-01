0b0c20d3f5e27dd866c29acace0701ec5918beef
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:55 +0000

    movement factory grade entrance born

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
