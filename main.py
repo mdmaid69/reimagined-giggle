f33448d3b02b904c35edc232b7650d742a01f448
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:02:59 +0000

    me hospital answer equal damage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import os
+print(os.getcwd())
