c4431ec3a1ae0eaded7c3cb2946aadf13bd1bcd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:17:54 +0000

    ticket comfortable peaceful refuse confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def convert_to_hex(n):
-        return hex(n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
