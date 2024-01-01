85496be8a782e0a7ec20c4224ad076fb0f46cd6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:59 +0000

    holdup whenever wait pay brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
