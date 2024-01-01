4d298919d083d838df587d71092da268faadc539
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:52 +0000

    entry comeoff beautiful callup only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
