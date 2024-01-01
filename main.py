094c9d8c344d43ccfe775b093da04eb5e03156f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:35 +0000

    tongue organize start gentle progress

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+x = 10
+y = 20
+print("Sum:", x + y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
