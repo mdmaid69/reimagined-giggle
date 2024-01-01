0f8d7611513556dc21ce03f8b9197eb151f28af5
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:38 +0000

    trousers further approve runaway nerve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
