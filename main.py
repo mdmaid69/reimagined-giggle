c63f8c53fffbf91b336a63d5631176be8a80218d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:44:44 +0000

    passenger choose damage Pp necessary

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_force(mass, acceleration):
+        return mass * acceleration
