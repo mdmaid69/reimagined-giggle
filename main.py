d2c21f21894df57b263d9edb012dd20c16118b45
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:49 +0000

    early male gift kind happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import sys
+print(sys.version)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
