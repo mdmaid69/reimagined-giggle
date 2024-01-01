394a8f3b65e5a48a070be51acc70422ddc83d263
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:11:08 +0000

    nonsense badly explain or safety

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
