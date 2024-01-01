9f91c81f12f09d59c9bb484f06e8e6015486065f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:10:09 +0000

    listenfor boil dry wind sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
