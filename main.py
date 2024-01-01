1ae0f51b24b6d8d5b338f5b94d4decde9bb85cb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:00:48 +0000

    about true hate mad sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_current_working_directory():
+        return os.getcwd()
