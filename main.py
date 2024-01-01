e6397be61a574cd86358607c7f9fb31a209f54e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:52:39 +0000

    manner gap whether across thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_python_version():
+        return platform.python_version()
