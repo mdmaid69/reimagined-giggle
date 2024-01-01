87f4905099e92e995a9262f5760fba6c3c0b5a24
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:38 +0000

    figure my their negative finish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
