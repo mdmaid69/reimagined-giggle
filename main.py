12ec70ead9af0266ecec049a580825f989b50fdd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:07 +0000

    567enemy considering explanation beyond various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
