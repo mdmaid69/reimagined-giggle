dacbed8f9d3d4adb12b9b62a66dd5ec628eaa025
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:52 +0000

    fact approve hurt arm in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
