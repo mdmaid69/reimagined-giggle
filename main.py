a3be6fc9afba053e7a96dabaac94b17de1414791
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:03:24 +0000

    one question talkback change here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
