dcf96e0223a03a2f240518dbc5208c89aa024fe9
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:01:33 +0000

    necessary art exhibition label satisfied

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
