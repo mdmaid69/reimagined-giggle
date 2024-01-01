b7a84b03b83d1235894f4b61b33ec95caa923d76
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:56 +0000

    round clean holdon loud force

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
