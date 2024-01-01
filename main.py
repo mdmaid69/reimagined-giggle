2742642b9e738baff00b959e2ee28a273c8f18df
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:07:43 +0000

    change cause possible takeoff keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
