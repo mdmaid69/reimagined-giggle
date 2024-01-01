00be7d97465dac5d182b0f8bd9b8344a6b15a08c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:34:06 +0000

    circle positive fall back welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
