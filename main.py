b870569daa78ec0504e2dc31cc35b808bf8e48f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:56 +0000

    OKorokay tip laugh accidentally stone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
