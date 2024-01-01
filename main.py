5f6291626cf58b1a880c3ce4a1fd9afe8173f090
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:10:53 +0000

    somebody steady expression fight pressure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sys
-print(sys.version)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
