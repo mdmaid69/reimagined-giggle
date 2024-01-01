b489f1658cdff1b11fb210a6826f3babde76584a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:29 +0000

    hold environment point laugh whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
