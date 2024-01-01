152e0c2bf802c1181844032bf4dae84f34680839
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:32:35 +0000

    join much circumstance vote message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
