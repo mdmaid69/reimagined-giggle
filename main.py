dca1d701aea456a8733dc932e4beca3f92088c2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:15:11 +0000

    convince factory fat confident fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
