35896101bc75816e778ac1ac3b33be7ac18a6eb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:19:18 +0000

    he form chat post tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
