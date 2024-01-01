adc421617b751c2f1c1ea81e244e9ea2e0930c34
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:54 +0000

    final stranger process takeaway waste

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_directory_name(path):
+        return os.path.dirname(path)
