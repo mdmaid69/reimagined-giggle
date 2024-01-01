e4eaa5f8ca73abfdd58a04c4b0b273cab4174f0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:09 +0000

    rush none sell say skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
