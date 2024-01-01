9d8f5d034a05b2d02035316e3f11383897ccf60d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:43:28 +0000

    sound thumb while disappointed single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
