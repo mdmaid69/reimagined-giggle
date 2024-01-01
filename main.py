9051fb3260fcfd90e4aeac5c77222779d4784af9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:52 +0000

    must complete mother trap central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
