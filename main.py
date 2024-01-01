9835a5d9cea78301b3216bdb3427540785597446
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:00 +0000

    would really not travel thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
