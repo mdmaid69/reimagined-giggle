647eb96a30e2a2034b26dcf03db5cef47f0cf29b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:00 +0000

    report false insist least official

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_time_since_epoch():
+        return time.time()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
