5c2b8b274a093a9f3c9fbae88c7acd41835e5def
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:47:28 +0000

    reduction combine only schedule slice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
