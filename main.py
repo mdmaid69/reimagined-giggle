3091a52308a0d61cc84a56263a84201c95e36dad
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:32 +0000

    quickly service wing pretend boil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
