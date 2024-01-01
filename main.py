9a6e2d8a4cc2eb0b568d12ebdbf3e215c9b34387
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:41 +0000

    right knowledge recently wake 568energy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
