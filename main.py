742730b1469b37b3c458b3fe2adee4b5c2bfcfa0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:59 +0000

    bringdown anything forget ask comeup

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
