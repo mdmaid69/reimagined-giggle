391869c68120c893f28812d086e2fadd4ceb9a93
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:51 +0000

    father male pen opposite stamp

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-print(sum(range(10)))
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
