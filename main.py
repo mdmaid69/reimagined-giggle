1209596e5160464b39e7e8e768eecc910a39b3a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:13:36 +0000

    unkind maintain sound talkoutof Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
