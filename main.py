a217a899855aa198a69b3cdeefc102db2416f0cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:16 +0000

    accident obtain let 564encourage minute

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+def reverse_string(s):
+        return s[::-1]
