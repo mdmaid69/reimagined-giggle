ca3e9606b101d098169ee7b4dc427e18025d9444
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:37 +0000

    OKorokay pen period usually left

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
