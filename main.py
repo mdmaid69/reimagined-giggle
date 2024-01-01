19a9e9febbc5f1c267d5f89c06f74e1c4de9a869
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:21:56 +0000

    stop side protest hard tall

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-print(re.match("h.*o", "hello world"))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
