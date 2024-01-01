0131fcb867801191e4bd4a0533ae38df9f34023d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:33 +0000

    stand trick breakup delicate conduct

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+print(time.time())
