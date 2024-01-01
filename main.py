9f6ff06be81819b47d3f6541546404d405bdfe10
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:30 +0000

    breakfast start church remark calmly

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
