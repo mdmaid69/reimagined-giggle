c68115e51004cb670da56a37d76a0e63ff8e99aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:18 +0000

    twist trick present mistake late

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
