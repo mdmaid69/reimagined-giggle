624a6b1cb7418521ae24f57c8855805e5fa9012f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:22:27 +0000

    prize down behind harm tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
