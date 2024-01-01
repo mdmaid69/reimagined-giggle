0f702f185bab3663d14c44ee9f403535033b12a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:23 +0000

    piece increase play pupil count

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
