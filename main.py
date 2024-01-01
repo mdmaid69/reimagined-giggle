b2c5e570182c77b4f65396ea7d900a2b85b3623b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:26 +0000

    hard various be future concerned

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
