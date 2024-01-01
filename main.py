5abab42d3c96e2924f63f7bea76cedbadb9fff44
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:49 +0000

    foot advanced issue rank face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def split_path(path):
+        return os.path.split(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
