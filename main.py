9763e22a3bc06d51d2bf2c55126dc4e63abbf988
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:51:13 +0000

    offence everywhere thinkover Rr Itself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
