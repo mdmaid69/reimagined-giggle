6da900731ee88f30cc0fca8b65ca053b87f35d62
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:22 +0000

    least sure which track present

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
