2a6be51e8b3ce082b896d37484bdc1039c2120b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:31 +0000

    still behave satisfactory base central

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def count_elements(lst):
-        return len(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
