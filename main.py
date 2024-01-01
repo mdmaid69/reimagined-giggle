871de5c6fc215d3f0ad8b8afeed92c6e62fb5a4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:57 +0000

    because payment smell sweep demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
