98f9714023b117adaeee7a10fa25ce51e1b61e0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:37 +0000

    form real float object post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
