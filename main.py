6aea0b7363af3310c8ddffd0efba5b694d2f0167
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:01 +0000

    stupid we takeaway ours or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
