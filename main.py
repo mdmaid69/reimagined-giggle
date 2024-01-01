7926c61d721b295e7342526f80afefe5f2491282
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:33 +0000

    nurse life wide old delicate

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import os
-print(os.getcwd())
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def get_environment_variable(var):
+        return os.getenv(var)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
