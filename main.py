d017c70c57f51a2b9d9797b9a240f3c72e0243da
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:09 +0000

    work rescue future guard prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_speed(distance, time):
-        return distance / time
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
