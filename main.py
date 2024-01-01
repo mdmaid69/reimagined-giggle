dd9a83856660ae393e7b7935cb746859da361bba
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:35 +0000

    dry strike dry unless children

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
