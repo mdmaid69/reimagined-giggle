e668231658e658e38f2e4f52ad9e8d36775bfa3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:30 +0000

    crisis check sting promise watch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
