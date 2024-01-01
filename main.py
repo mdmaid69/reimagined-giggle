a1473697a1a6d38a260d69088f9e61ec190fc238
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:21:48 +0000

    pause tape 573entertainment closed by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
