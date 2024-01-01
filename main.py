4d83b924e8d33c192ff52739aedbc9252d15efd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:24 +0000

    embarrassing advise ask criticize sign

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
