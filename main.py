1498ae05c75c345867e5f8798bbcc832c133044f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:05 +0000

    time open embarrassed develop book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
