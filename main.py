ad891bca2e97f82bf8f9e48f9356fe8f09389bb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:15:07 +0000

    manage Bb pair right imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
