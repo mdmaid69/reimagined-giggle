0f63d4877040115831ebf57c1034b8ded4b9842a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:02:56 +0000

    hope song charge fire fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def clear_array(array):
+        array *= 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
