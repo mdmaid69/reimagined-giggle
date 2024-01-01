3e6b6e20d2e0ecedf967ae5ba13013139d2db362
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:05 +0000

    chance work moral explain Mm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def print_python_version():
+        print(sys.version)
