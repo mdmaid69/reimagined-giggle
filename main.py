7f5c32ce3c7510d1a770d16781a1739aba385840
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:59:11 +0000

    search soon service claim let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
