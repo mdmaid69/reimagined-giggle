9b5d13baa96b763979660ef05b85246079d44cda
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:34 +0000

    pile weekend difficult cheat strong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
