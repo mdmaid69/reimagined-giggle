22d0a18cbda6bfa7546b3e322ffb9f42c624ae3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:30:50 +0000

    pause breakup basic now building

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_bool(array):
+        return bool(array)
