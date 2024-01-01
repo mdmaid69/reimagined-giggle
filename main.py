0a2b300d6702da333dde0630a08bf991cb165c73
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:31:35 +0000

    claim division load commit reaction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
