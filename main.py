cc1cc001b0c2a79801a526a944e5f13b6c7c9b87
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:21:06 +0000

    permission several after sensible brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def is_palindrome(s):
+        return s == s[::-1]
