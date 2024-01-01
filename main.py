d0db7e43be88397767f6a15fe50a3182d44ad1d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:59 +0000

    comfortable terrible arrangement make before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
