b7551af563d8518c0c6d60b20078c0292e5f73df
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:13 +0000

    prefer blame step soil since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
