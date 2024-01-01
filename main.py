c459fb02e01cc7aa4ab060cd63edc9a567611462
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:16:11 +0000

    father strength drive listen centimetre

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
