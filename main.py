a101d67d8014d11148da50d6cb858b76cb929a74
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:49:34 +0000

    to cheat yesterday take defend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
