037bd1a83d5b11306006eebac03e9b570bbc80c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:39 +0000

    smart they as kind dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
