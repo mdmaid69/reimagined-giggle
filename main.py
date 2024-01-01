100cc1c14baac021cfcfe03019756a9860a2d8e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:23:03 +0000

    law rubbish measure thorough approval

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
