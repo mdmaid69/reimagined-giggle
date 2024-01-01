d2652128da020ecd26d126a13175db49f4aad593
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:31 +0000

    call father pressure right boring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def is_odd(n):
+        return n % 2 != 0
