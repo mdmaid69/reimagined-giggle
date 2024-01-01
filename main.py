4df2599f3e155b4d144dc4ec8cf99095f080c467
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:47 +0000

    many allow work complain again

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
