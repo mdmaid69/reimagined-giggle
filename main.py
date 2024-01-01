ae0ddca5fe910fb757c6497d4776585afabaa40e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:19 +0000

    do success round need game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
