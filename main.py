d2f71ad0459ef8a9b964fecc4be291f628b4984f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:40:13 +0000

    cheap unless money fairly disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_to_binary(n):
+        return bin(n)
 import math
 def calculate_radians_to_degrees(radians):
         return math.degrees(radians)
