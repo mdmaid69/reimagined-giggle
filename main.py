fc060a00a454c13c54629343895b33eb5d5e10fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:39 +0000

    private dance now apparently narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
