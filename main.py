8e2cf47f02308501358f2ab429eeb9b393ee2468
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:41 +0000

    neck argue play announce Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_string_from_array(array):
+        return array.tobytes()
