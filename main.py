d04ff72def95398316928ee85adea046dadc1998
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:53 +0000

    plant flat turninto well advise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
