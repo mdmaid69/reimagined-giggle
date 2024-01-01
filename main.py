433439dccfa2ec8def52565c067ca8b52a288edb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:29:56 +0000

    right along group high belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
