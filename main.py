547b4fa52d766137a1d3ef936517fbee81abaaf9
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:08 +0000

    guess team weather simple yesterday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
