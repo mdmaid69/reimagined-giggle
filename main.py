a97579b5924aeb5558e229e3d7310eb0ad32b791
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:46 +0000

    freeze adjective born passenger best

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
