351e949f1cd1862ca78477389d0f8646c0aee038
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:40:06 +0000

    traffic whether outside exam judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
