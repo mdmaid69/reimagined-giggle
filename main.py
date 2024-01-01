1bdbf28b6775b0e63bd150f19c0d59fa6d541e5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:04 +0000

    age taste last coffee room

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
