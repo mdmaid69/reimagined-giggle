f1d85b6afe1faba994281c6f690bd1054b70b580
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:54 +0000

    name watch immediate to with

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
