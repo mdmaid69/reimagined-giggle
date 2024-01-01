baa64ee8b5b84055c8f6a05be7436466e4323964
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:14:05 +0000

    organize wet perhaps agree welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_work(force, distance):
-        return force * distance
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
