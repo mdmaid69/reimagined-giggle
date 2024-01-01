b2ff4ced58e4f119f411177412f7f066332f6b7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:33 +0000

    control proud desperate flat interesting

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
