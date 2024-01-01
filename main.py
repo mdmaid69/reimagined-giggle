d96fa831f4d78376dff0b1fb370dd0f8c8e5871a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:57:17 +0000

    only spill thank too dream

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_length(array):
+        return len(array)
