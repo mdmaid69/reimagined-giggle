e1a00d2605e9fd86dc30ae42dc994bddbee58194
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:52 +0000

    brown about among hurry card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
