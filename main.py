979c668d0d031b6be568d91977ee85573030f8e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:40 +0000

    fight opposite base read thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
