012447ab1ce4e3d014f72e414861e33f75fa9338
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:39:37 +0000

    outside favour medium home begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
