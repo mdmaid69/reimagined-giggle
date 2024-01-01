a610084475e7e7d13f13e27685a2ab4fecd750c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:05:46 +0000

    change modern glass clever drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
