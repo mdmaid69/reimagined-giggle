66c03ece797954d77e7a36c4d7c5116dcacfddcd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:12 +0000

    who prevent together insist regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def create_numpy_array(lst):
+        return np.array(lst)
