f1d97ed29ce80366209cc2c5f1651cfa0e9fb14f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:44 +0000

    share to who question definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def remove_from_array(array, item):
+        array.remove(item)
