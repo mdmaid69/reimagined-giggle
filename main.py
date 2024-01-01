d5c8b575260d62bd25ac82ae4b1ee762229a3c63
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:43:40 +0000

    profit practical arrive poison success

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_typecode(array):
+        return array.typecode
