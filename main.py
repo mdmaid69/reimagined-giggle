2ef61f131d8650a682aa681788581ee4129c0bdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:37 +0000

    Ii region reduction about impressed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
