b975c0d68f2204420e06cc004875a483f69d71d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:00 +0000

    sing life advantage age advise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
