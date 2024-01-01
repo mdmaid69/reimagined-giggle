33f38f0fddcd21b0e2d6deb1fe6cf06d7299d1a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:51:12 +0000

    confuse plate comeoff delicate boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
