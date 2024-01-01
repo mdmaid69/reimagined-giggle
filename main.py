0348c76ceb01be511d3b88e8d5187df83ad00a7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:53:29 +0000

    inside culture dinner answer about

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-from collections import Counter
-print(Counter("hello world"))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
