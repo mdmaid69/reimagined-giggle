a4e672e2a9cc93c92a628008a63f75cc6a296aa8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:42:35 +0000

    trick operate late like command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
