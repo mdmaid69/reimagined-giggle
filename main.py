080d69e8908f51806b435af606daf849a328cfd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:51 +0000

    rarely chance ignore develop thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_float(array):
+        return float(array[0])
