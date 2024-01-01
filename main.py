7378e7675fb5d904ee04f545e3ea4de37dc2a7af
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:16:41 +0000

    hear mention bye but snow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(lst):
+        return max(lst)
