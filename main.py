e5da32f3e68c98b157cc3dc1d8d169491979bdbf
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:26 +0000

    able afterwards bit usual guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_priority_queue():
+        return collections.deque()
