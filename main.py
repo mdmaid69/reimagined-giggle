214564495ab67d28e231d8600c93cf7f7dd5e83c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:38 +0000

    wealth year accommodation appear office

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
