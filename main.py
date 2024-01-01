740e2d39aa7b60bbecfef5aef55637a0aad88d9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:15:44 +0000

    motion modern of body excuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
