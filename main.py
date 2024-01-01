44320a08d32e6d814c1747861696eda25a083644
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:23:14 +0000

    disagree finger active date answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
