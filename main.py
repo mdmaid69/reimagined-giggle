ba77e5aae238f431d60c1f6b771d022c15f82e7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:43:55 +0000

    statement destroy acceptable poison exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
