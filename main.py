9dc221595c021ca7059b0fe0a4b87d8096c268ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:16:54 +0000

    Tt attractive important check below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
