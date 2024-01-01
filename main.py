5ab12d79fa6c32c34cbb82d7580e864ae070e3f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:32 +0000

    throughout light suddenly thing please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
