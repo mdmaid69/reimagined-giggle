928341eeeb6c2068235790d7d49c1dfc17d197d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:20 +0000

    do considering who relative move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
