49fb8beeb869baf25a6a03890bc8b95e6ff8f428
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:40:33 +0000

    crazy extraordinary produce later creature

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
