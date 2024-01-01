7efd44707389581e6eb9c0f734ce0049e9b3f04e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:58:55 +0000

    satisfied rough equal period plant

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
