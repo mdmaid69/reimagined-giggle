6675fda538c78a4d912bd44910459b7710dd2a93
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:18:40 +0000

    gentle hour foreign squeeze pride

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
