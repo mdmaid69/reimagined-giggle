4a0f16e7bab92e7039d644fa9ca282aeffad33e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:53 +0000

    health lunch accept move skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import time
+def get_time_since_epoch():
+        return time.time()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
