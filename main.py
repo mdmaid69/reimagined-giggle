e7757694f2cfda72275ff2f1873a21f018845e89
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:09:50 +0000

    tragedy than both left except

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
