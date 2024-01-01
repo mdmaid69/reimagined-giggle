735689cf60dee11cece3e19c987ee30dcd705c75
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:18:37 +0000

    law float giveup hers deal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_current_directory():
+        return os.getcwd()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
