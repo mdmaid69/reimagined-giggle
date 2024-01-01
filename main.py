4a1a15eeb2df68fd4359b260c191ac452ab7a1ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:11:23 +0000

    quantity include while thick odd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
