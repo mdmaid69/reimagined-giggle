5041d153edad34fd3b679ff4e04068a7007d2466
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:52:32 +0000

    history stare keepfrom tongue busy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
