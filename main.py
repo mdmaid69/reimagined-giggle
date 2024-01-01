f667c64d7a65d3646cf526e30b113b28f710e017
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:20:40 +0000

    our count could peace set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_min(numbers):
+        return min(numbers)
