83af7ff4e7a8155181b289a2a1b25d7733892a38
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:04 +0000

    path fault book feel easy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_priority_queue():
+        return collections.deque()
