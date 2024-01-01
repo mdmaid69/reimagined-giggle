7a0ea1e7d9ae55ddfbc1b43da2570343cfd40664
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:50:52 +0000

    success properly keepout underneath wonder

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
