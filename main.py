abdfd4f8b3c217ff6d9dd8fbf875d0cdda061d7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:19:53 +0000

    bit behind circle correction seriously

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
