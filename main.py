28691c393ab9567e828d0d58cf3e59106f6c1a40
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:30:57 +0000

    turndown tune stop education impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
