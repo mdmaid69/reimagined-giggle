d82324cc590e42a393df06c13f6adf5c17304b67
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:20:36 +0000

    somebody plant emotion combination afternoon

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def set_array_item(array, i, item):
+        array[i] = item
