83c2bf2166761e9410b56a428c138c33fab667dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:10:43 +0000

    which wish sail through ball

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
