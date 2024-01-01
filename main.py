10c614626d92e4613d3be9e4234df27c342d6c82
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:54:43 +0000

    bright profit mean but drop

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
