fc7bb2714b39bb5245ed4686c9a183ca4468476b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:21:04 +0000

    under think plain firm success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
