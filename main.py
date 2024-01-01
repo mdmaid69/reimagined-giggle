5fe1420ffe1c37a1d27e44d6d4da6e902cbe5807
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:06:01 +0000

    there hear direct notice bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_exponential(x):
+        return math.exp(x)
