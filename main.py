30170ab82ac939c75dd7ff4a6bf6de014aa2c673
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:08:37 +0000

    healthy stranger miss off weather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
