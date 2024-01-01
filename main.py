5d3f9e006c8827bdb8372109f077044e1475aa70
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:23 +0000

    back through than work include

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_user_string():
-        return collections.UserString()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
