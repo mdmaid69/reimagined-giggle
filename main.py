8309060849eb2d881b90a9f7a01c7287c833b554
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:25 +0000

    bank separate before half lock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-i = 0
-while i < 5:
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+for i in range(5):
         print(i)
-        i += 1
