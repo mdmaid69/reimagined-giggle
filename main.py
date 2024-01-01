938a3c50fec3d4f214417058157a9ee37a18be03
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:15 +0000

    ball number absolutely classroom row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
