37ee9159101934af9dd916daa4e41f344abee051
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:27:24 +0000

    well bound accidental telephone anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def find_max(numbers):
+        return max(numbers)
