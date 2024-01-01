fdbf4c33425262059a9ae797f73e732655ecf0aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:38 +0000

    compete most face sometimes climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
