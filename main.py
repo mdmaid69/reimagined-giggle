4cf25548c3c472e56bfff89689388559998f50c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:44 +0000

    certainly alive follow uponon object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
