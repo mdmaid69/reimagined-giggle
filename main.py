e2a0dd1bd769261c31b88b58bd8cabc54e51a57a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:36:40 +0000

    regret aware lean drive legal

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Characters:", len(text))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
