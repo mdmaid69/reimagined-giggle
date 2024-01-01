f7b53954cf4dead2f337eea6a58d5132abdb655c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:20 +0000

    Gg brain cry tall path

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
