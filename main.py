2b68de035685a35103cef7bffb2efcc93730018a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:27:01 +0000

    instruction personal comeoff twist exercise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
