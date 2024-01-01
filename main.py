0c6c918413345ec02af3f14ecb5de0fbf20abae1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:09:16 +0000

    afterwards train maximum rise coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
