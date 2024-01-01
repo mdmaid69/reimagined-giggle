671472f7dae65e91aae96f73e6be10919a8e69e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:01:37 +0000

    stick warm goon state seem

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
