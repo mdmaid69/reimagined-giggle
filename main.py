8c7ed6a7c8df4f776b6c58352f0cbd7780e2304f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:00:33 +0000

    spare pump importance heat rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+x = 10
+y = 20
+print("Sum:", x + y)
