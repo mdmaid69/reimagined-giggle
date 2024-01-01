e086cfe272a96cd24ea72ab4d0238ee4bb0d4aaf
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:28:49 +0000

    cold means urgent close lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
