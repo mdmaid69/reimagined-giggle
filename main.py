87c57be9a6221284f3323e57d3e68704e7ad8d6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:15 +0000

    target win mainly then present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
