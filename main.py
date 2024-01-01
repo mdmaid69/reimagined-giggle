7e3bc00a1ebdeacbc28bfde3eae0a931e2f159a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:57:40 +0000

    interfere know mind concerned charge

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
