ac01976754d42692b57e22288239fb795256d8c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:36:18 +0000

    previous giveup sort amount exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import tensorflow as tf
-print(tf.__version__)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_string_from_array(array):
+        return array.tobytes()
