1425f22085f01e5909eab6178069271e5504ba68
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:18 +0000

    giveout handle wipe right female

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  def is_odd(n):
+        return n % 2 != 0
