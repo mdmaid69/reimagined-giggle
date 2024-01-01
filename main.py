dd074b0faf5d6359a5916ca7022e743ea5374768
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:12:01 +0000

    also silent hungry not nearly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
