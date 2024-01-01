8aad91a5efe211ae952156d123eddaca56d9f9bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:02:06 +0000

    stare meaning goback(to) 573entertainment split

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(lst):
-        return min(lst)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
