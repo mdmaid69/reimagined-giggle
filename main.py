c11665b7b827a0ddecec54a52f9c427e1fed2137
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:46:03 +0000

    wish top nothing getaway information

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_work(force, distance):
+        return force * distance
