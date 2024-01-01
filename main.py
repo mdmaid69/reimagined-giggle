518d66233e6946d08242a6e8dedc2b40fc587508
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:14 +0000

    Ff extraordinary call lot forget

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def clear_array(array):
-        array *= 0
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
