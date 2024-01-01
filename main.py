0d7da78adc9d888989ac9408fdfba799e93d1a8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:27 +0000

    spoil fast cover press calmly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-print([x**2 for x in range(10)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
