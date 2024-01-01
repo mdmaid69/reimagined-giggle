b85ded8375e4e7f904eb34ec0d24bd7a94d4747e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:13 +0000

    little unfair fast create contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_current_time():
+        return time.ctime()
