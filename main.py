cf4de4bb673c8399df8690c96e05408c7e914fec
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:19 +0000

    early pollution birthday full agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
