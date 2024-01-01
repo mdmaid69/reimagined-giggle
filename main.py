d76afe3f14e68e589dca88435d170422f57e6e40
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:23:49 +0000

    near careful lay yesterday getout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
