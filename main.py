be38927cf5c777b5c7546befbb2977ef196b7642
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:09:39 +0000

    file tell nonsense receive past

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def reverse_array(array):
+        array.reverse()
