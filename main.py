3d2671d38c2b18185bac281cebcf1edbb685a7c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:02 +0000

    education round impressed where healthy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
