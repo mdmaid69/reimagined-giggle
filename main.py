eb8052aa0cc7d91bed398723e09b902bf33839f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:01 +0000

    drive probably load shallow early

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
