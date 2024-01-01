0ae829ed43cfa8c81c345b631645a02379172502
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:36 +0000

    excuse sign succeed cheque serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import collections
+def create_counter():
+        return collections.Counter()
