68696e72730a3fa27f161415bc21a7635c2160ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:14:21 +0000

    contrast cry fact preparation throw

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
