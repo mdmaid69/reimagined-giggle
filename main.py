9b4fe07938594eedfd3bbe76d1c96f7f3db0118c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:54 +0000

    pause wrong mistake proof steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
