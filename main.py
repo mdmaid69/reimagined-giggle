4c002663feff0fd95610d57129dee828f12944fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:02 +0000

    after string taste care curious

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
