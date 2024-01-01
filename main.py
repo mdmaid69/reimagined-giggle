e3a4dc1a511628b0be421a78d5cdb9e8fbec9b5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:09 +0000

    wild must quantity visit mouth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
