c08ac5c84fc5b075ebe06497dd11cc4bc3b21272
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:29 +0000

    smart group available will most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_bool(array):
+        return bool(array)
