a52a51a41b216d91984059638688c2d533c73a85
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:11:13 +0000

    meet quick in mother forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_max(lst):
+        return max(lst)
