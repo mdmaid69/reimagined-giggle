9bcd49ad393c0fbcf7cdd389ef5351c8351dcd19
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:45:13 +0000

    fence secret prove plan rather

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def multiply_numbers(x, y):
+        return x * y
