e9aba370019d37841a959d758360bfa931fcdeb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:34 +0000

    think practice loud upstairs Ww

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
