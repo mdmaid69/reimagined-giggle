6ab3b36e860d46f909c2e780cf7c4603ec7de6ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:27 +0000

    tour corner dry can comein

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
