9224186e77360c8481dbc0479528f236b15bca22
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:34 +0000

    shout attempt information claim corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
