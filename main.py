db58a30dde2a6cd57088b7d3117517a9aa277402
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:09 +0000

    waste dangerous muscle usually 179bite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
