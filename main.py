0d8af52adca561e82303b16337e624543d5ae787
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:27:12 +0000

    story reaction hope pay comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_pressure(force, area):
+        return force / area
