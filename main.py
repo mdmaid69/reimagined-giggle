206b62ca9e1dbf5169cd663bd2b0a263d4e085a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:12:53 +0000

    whom apologizeapologise backwards fit eye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
