374a083a632b98dfb50623d4433a2ebad0a93b2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:54:40 +0000

    stand while write confidence kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
