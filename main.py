c8992c3ff9c0c389fad0371dcc6c1db78ad9d45f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:20:31 +0000

    expect gate train callat rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
