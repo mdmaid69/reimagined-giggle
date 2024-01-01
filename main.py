b8be7e4b4ca478d2ff3dfd33117b97ff9dc2fa9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:09:37 +0000

    keen suck and truth public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
