ed4b49b82066f5e460124bd7db55f36c82cf2efa
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:56:06 +0000

    heat small might negative top

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import sys
-  def get_python_version():
-        return sys.version
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
