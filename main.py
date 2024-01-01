59cf095b3b221f7323003884b0588f39472c2d14
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:50:45 +0000

    difficulty pool total plant information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
