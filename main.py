4dcf8dc5c8058cc1bd0c0e7cbbc5f666f2e6ed2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:11:43 +0000

    connection night match team old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def cube_number(x):
+        return x**3
