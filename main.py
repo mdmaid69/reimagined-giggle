03f8986b90aa6a84247a6d3127eeb29f6055070b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:02 +0000

    near bang tip less considering

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
