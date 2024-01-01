4de0c8e67c4daccb5b17ef1b866505cb46382e69
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:22 +0000

    loose advise study issue but

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
