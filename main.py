f9f6c3e23d99caa11fc1a3c091a461d33439a104
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:11 +0000

    protest should worth hope away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
