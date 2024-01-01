31782dd65181ddb7cfb22fa5b5dee5c6e480b993
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:01:13 +0000

    tidy pleasure rule serve advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def convert_to_octal(n):
+        return oct(n)
+import os
+def get_current_working_directory():
+        return os.getcwd()
