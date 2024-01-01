3c10b7934beb6ec9c887827e23d3bff5dbb0b557
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:26 +0000

    when cross contact rid return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def divide_numbers(x, y):
+        return x / y
