5b656e9279db7f78b2b4421e5472cc19130a91e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:06 +0000

    right embarrassing case step honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
