e9e933822cad99c17116a7cf94750681df9567e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:56:05 +0000

    speak frighten throw before come

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def square_number(x):
-        return x**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
