3edde0061e44df557ab0494838653bc464f1bbba
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:26 +0000

    estimate hurt by control chance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
