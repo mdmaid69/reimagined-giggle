5410e21dca452fb2ed46266e097de6c0cd5631bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:23:42 +0000

    club convinced life agree serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
