1c058bf94396305341ef0a255d1a92d90b7dea9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:52:05 +0000

    bite plant pen finish free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import random
+def roll_die():
+        return random.randint(1, 6)
