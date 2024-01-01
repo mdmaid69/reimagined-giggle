909ecd060db4b3444735c70e39ee16eddab3efc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:46:45 +0000

    under all or interfere grow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def reverse_array(array):
+        array.reverse()
