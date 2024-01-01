548955119c223cf35116430ca596e62cbba38aae
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:04:08 +0000

    trust spot down on flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
