cf60803820f3e0cd985509383b3ac84ca25fda78
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:52 +0000

    connected correct knock supply now

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import platform
+def get_os_info():
+        return platform.uname()
