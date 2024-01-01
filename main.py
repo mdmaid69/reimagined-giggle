a14b9932078afdc0fc9bdea6db65e09f7c485bc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:21 +0000

    bound naturally discuss direction accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
