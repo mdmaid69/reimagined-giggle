71f3ba410d4cc4ed9060f8429893cf1094312a82
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:51 +0000

    clean age Jj protest slightly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
