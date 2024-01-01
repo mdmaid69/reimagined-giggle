49053684f643e85ed1110eacc0ec1071e354e801
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:26:58 +0000

    cheat exam develop sweet lost

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-name = "Python"
-print("Hello,", name)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
