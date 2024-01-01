83626c4f2981257424b31c416438dedad9472ec4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:47:04 +0000

    tear search anywhere accidental rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_force(mass, acceleration):
+        return mass * acceleration
