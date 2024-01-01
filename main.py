778fc5cf830a3ac0bb13acb739c8520f25dc2bc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:26 +0000

    improve taste hers fear unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def is_palindrome(s):
+        return s == s[::-1]
