5cbcd966b47033cd19a9530a125f2b526bb8f630
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:33:02 +0000

    but loss silent 570enjoyable catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
