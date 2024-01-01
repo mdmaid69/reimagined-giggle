c61183ddfc2ebfabdede0c9f32547b22b4ee508c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:10 +0000

    get shopping company available fierce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def square_number(x):
+        return x**2
+  def convert_to_octal(n):
+        return oct(n)
