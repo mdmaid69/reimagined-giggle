c3449b7983a518337e4a9d814df52aaedbd8ad00
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:25 +0000

    all night excellent dig cheque

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def greet(name):
+        print(f"Hello, {name}!")
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
