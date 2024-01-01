f193b9537e022e542fbf6080ee2ac0f64644a610
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:41 +0000

    noone search experiment half exact

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def convert_to_octal(n):
-        return oct(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
