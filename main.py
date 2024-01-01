76fb37dd0feb84c9b809e4a5861e3c4591420988
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:11:09 +0000

    answer study both realize track

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def is_even(n):
+        return n % 2 == 0
