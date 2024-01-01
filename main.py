e21a20f441c308e435dc7662ba5e990e963df147
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:42 +0000

    lead write wise key giveaway

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def add_numbers(x, y):
-        return x + y
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
