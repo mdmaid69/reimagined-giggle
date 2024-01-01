0bd246d81e93e48f1a8f3b0fefee07372472caee
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:26:43 +0000

    scheme question name human eye

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import sys
+def exit_program():
+        sys.exit()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
