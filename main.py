ea0c3b5baefb87e50908b5017ed15c8d855d3fa4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:41:04 +0000

    them everything contrast rough much

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def reverse_array(array):
-        array.reverse()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
