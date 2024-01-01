37cedc4814bd455fe8809d9c1524e92b0c8b5132
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:45 +0000

    wind run public split occur

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
