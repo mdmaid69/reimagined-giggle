8e1000f4f05a76185a0ed939adfb849e8bf32099
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:48 +0000

    able fierce scream wrap some

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+i = 0
+while i < 5:
+        print(i)
+        i += 1
