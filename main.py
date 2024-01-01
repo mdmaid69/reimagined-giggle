21537399908cc1c87882c2839ac5974ee247480c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:52:28 +0000

    disappear shop day recovery contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
