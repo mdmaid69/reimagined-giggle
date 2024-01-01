262450e7bf5e193a49fc7e5ad4e0bb2f5fd8ef76
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:18 +0000

    letter cheap control somewhere hear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
