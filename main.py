a2b6ca639415e8b5aece8df7b8d2887ac6555b58
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:49:36 +0000

    protest hate settle find software

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
