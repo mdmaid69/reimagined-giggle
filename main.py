35c2aad6199b5aab50422985864065c6320a8b55
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:37 +0000

    out likely total risk powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def reverse_array(array):
+        array.reverse()
