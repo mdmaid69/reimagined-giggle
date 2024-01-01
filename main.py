55dd27d79332b1425606c6edcb92630c5906b272
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:04:57 +0000

    form against high reason soft

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
