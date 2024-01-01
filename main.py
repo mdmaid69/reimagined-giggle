4d24181fc01168db9f49a2b4440ca15ebc4e166d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:35 +0000

    now nothing deliberately possibly thick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
