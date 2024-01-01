f061dff7fd323fa5924d53ca3cb1438f87ad3554
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:48 +0000

    press shine standard risk squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
