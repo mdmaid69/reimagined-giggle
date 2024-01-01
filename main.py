e20237927b7bd7f6fa9c57890b3d4e0fb2274c5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:46 +0000

    wall promise preparation temper fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
