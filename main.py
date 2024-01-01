b5dffbfebc97a0b2641fadace84ef375d6458c38
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:17:46 +0000

    bottom warm they turnback turnaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
