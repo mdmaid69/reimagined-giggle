46f337e7a49a4115a8cd7af323de7f15d4f3cd29
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:10 +0000

    block side when basis replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
