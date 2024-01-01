59f7f23acc03ad584cfcad98b8650bb93b9555ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:10:04 +0000

    so best talkinto dear when

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
