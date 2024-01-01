1dba74f8fb5ad392380e97367fb9c257be0ab916
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:05:27 +0000

    talk complicated near positive neither

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
