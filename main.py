ce872325f742431c9d10bb9abfa986a7c3aff75c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:52:05 +0000

    full escape short intelligent content

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
