f53daaca465935c3ac73cd78a52ec2f9fdfc5142
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:47:09 +0000

    Ff extraordinary tape act word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import os
+print(os.getcwd())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
