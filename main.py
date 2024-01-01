51dd4786570bd538c9f47e06458041ca4752e93f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:38 +0000

    treat strange power talkoutof so

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
