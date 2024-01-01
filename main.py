4f93aa5d61cc64455332016b9d768fdc64100b49
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:57 +0000

    herself string complicated question study

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
