9f938778fc0451fb8062a9bdc50ea35502fdf443
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:08:23 +0000

    extra experience dinner breakin card

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
