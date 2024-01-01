27f4770c36a53e930314ac53658ada5a012e2517
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:56:20 +0000

    extraordinary insist birth flat air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
