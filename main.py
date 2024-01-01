36c481dbedca593dc71bbe24ebdb0e7430a55247
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:19:30 +0000

    send forever figure evidence lost

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
