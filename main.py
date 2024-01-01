b19cc183dad293f418046bbc1aa62a56ff3fc81b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:41 +0000

    rain accidentally this gooff respect

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
