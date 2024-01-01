a136cc18c8dd277aa2847481170f68f46f80ab4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:26 +0000

    impressive alone convincing dare strike

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
+import array
+def clear_array(array):
+        array *= 0
