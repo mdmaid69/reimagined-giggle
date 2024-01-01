93ba8e077b88fca7a8a600b4f154fb33e343c2fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:00:02 +0000

    dark private grateful impatient across

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+name = "Python"
+print("Hello,", name)
