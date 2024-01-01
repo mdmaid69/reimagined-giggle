56516f1db59d1873dbe31da8e215cc53227b79f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:18 +0000

    comein afterwards fight put butter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
