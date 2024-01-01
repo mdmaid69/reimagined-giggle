7d9e6b1e77517f048c0b25ccacb6016b5e5c292c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:38 +0000

    sweet explanation uponon goup run

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
