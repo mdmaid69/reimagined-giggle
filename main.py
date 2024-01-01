ec83821510cb197db5b4e22200d6978675b349a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:25 +0000

    cheek answer immediate trap butter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
