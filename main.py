c7b2368c29c516eb0f1de627effe646d06323eae
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:25:48 +0000

    turnout statement fire choose violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
