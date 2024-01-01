ba921654db880f5c0c874451ef082a5b923535a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:43:23 +0000

    medium lookup shame turnaway house

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
