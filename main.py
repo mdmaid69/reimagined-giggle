d5f3de4ee96bf50740a41400bbbff120e4518e18
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:47:39 +0000

    where machine legal content sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
