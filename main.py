05fba1e4fd1c9bfc2da326bcd4008626764ca091
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:57 +0000

    push busy cheek promise emphasize

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
