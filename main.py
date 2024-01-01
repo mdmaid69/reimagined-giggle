2d68dbcc5e17dfabcf108b9259f3621d2f1fb1bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:39 +0000

    clue include agreement principle communication

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+print(os.getcwd())
+  def add_numbers(x, y):
+        return x + y
