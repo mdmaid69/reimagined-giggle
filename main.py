490e79ef1694deef745840b3c3f1bc4d737c1f5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:10:45 +0000

    serve blind relative their throw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import csv
 def load_csv(filename):
         with open(filename, "r") as f:
