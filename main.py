66d94ce1e10127ad1ae28dd77cffe17a22046c9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:23 +0000

    drink cold trade danger excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
