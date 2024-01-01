14d2a98ff3f22cbc1eb6921a13824b4c1bb41386
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:19 +0000

    accident fail hit treat jump

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_min(lst):
-        return min(lst)
+import os
+print(os.getcwd())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
