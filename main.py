48d305f5068dc7670acd282357f780c21598925e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:20 +0000

    catch mix first defeat practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
