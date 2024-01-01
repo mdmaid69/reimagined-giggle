dd1dcacdaa0fd691239ac4e793abe615a64df646
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:58 +0000

    unkind move forgive present pay

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_palindrome(s):
+        return s == s[::-1]
