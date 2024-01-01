4f4fef07f3cd4eb7db064e4e129287946ed7b624
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:45 +0000

    somehow lucky action balance listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def add_numbers(x, y):
+        return x + y
