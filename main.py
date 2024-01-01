a8d688b4390f6cb9ff84e6e0844cb30efafcff32
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:49 +0000

    among Bb steep someone doubt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
