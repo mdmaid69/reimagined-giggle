428814a2af55076999cef700f42af738142121f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:29:28 +0000

    little emphasis thin only transport

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
