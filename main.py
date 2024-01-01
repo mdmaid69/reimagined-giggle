5c5631654162820983fce3a566e8fe3d984daec1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:39 +0000

    peace join back voice thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
