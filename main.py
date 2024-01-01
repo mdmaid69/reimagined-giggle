84f3cb478009ba1e522b362d5cd1bac7e39540a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:27:22 +0000

    bother lie past opportunity secret

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
