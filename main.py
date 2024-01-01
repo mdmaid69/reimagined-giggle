f902f10b1d462c7bf7cab9718afd8af08a3786b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:54 +0000

    approval talkinto quit fit basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
