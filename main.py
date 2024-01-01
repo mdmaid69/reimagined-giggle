4879994d191ddba2a04f8ffdf071c2580b62c591
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:41:11 +0000

    them gas face kill price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import tensorflow as tf
-print(tf.__version__)
+import array
+def reverse_array(array):
+        array.reverse()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
