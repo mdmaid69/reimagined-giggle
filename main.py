0aa92194a30091b52870cffe4fe861c27ebf2310
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:31:30 +0000

    Bb stop praise open stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
