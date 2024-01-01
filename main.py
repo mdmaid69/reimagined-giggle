b2af128545f22c75409f22216361d58fcbe88e9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:10:28 +0000

    connected turnup repeat smooth takeout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
