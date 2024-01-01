1e450e4ae6cf429247085fe8e0993df60976abfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:32 +0000

    recommend attempt about succeed national

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def reverse_array(array):
+        array.reverse()
