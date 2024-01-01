8c6dc0bbc30e4a6460508ac90fc42417d9114430
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:07 +0000

    cost lie image centimetre keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
