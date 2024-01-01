8421b060bb9fdb614e8222d22edb80cd6c6fdae7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:16 +0000

    curious every pump entry different

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
