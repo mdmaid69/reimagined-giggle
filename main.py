963e33ad0ebb58c1e006429bd7117e3852e78e71
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:27:31 +0000

    schedule born load tonight vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
