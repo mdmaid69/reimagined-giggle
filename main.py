a25e228111dbc7f410fe5d9f0cf2727f63c966cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:43 +0000

    grade lot patience swim order

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_bool(array):
+        return bool(array)
