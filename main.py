ba64493f43111297b35a87c6af04223123c046b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:16 +0000

    swing rich loss worry set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import sys
-def exit_program():
-        sys.exit()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
