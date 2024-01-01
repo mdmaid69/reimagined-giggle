ea24c354cd9545887ad2f0daa13c623e678b0744
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:39 +0000

    print basically patience round church

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def append_to_array(array, item):
+        array.append(item)
