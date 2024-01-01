a451fe8afcfb55d5e6710b2bea3c73c0d17515fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:45 +0000

    true bet past behaviour scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
