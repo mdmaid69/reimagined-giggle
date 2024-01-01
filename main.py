bf0b7a220e47a7dc25cf638df1c68031caf0910a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:52 +0000

    event impressive point talkinto interested

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def sort_list(lst):
+        return sorted(lst)
