87cff52cf142407444c56b4208bde1ae92b062f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:33 +0000

    waste modern mark wash exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import tensorflow as tf
+print(tf.__version__)
