473d687f6fb178822bd18047594fa74995e171a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:57 +0000

    start mend tradition fond seat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_typecode(array):
+        return array.typecode
