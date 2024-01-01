a9e6f89e0071dc54dd2c0981add9e9446ed86c5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:18:42 +0000

    team report drink which comefrom

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
