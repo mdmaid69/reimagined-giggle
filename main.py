0848abc505397ac58a52834088f63a296f3dd297
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:03 +0000

    tip arrange lot mistake cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
