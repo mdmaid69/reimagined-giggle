8d33c64f0ac98fb57192355f27c04e7909d49d7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:30 +0000

    unkind crowded study correction contact

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import math
+print(math.pi)
