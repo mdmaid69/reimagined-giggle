32ebe5e42957d052f87826d76f30b2dbb06d3e27
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:10:25 +0000

    rubbish difficult crash crash telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
