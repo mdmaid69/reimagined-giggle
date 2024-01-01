98f93bc315674cfc9ae2c7e090fcee64eb3b1058
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:52 +0000

    responsible school fair shrink murder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
