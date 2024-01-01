b0eda19d199f761bb7065be81839801d85518a65
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:23:36 +0000

    popular quantity actual makeinto occasional

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
