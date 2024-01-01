b9bb05c0b9546ca7f9b97ac9a2148a6401cd6af7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:30:50 +0000

    shop bank callout train scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import time
-def get_current_time():
-        return time.time()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+print("Hello, world!")
