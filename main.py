bbb16d27c20e8df54ab8b72244a578c1ba87ff33
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:33 +0000

    alone factory positive section model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def subtract_numbers(x, y):
+        return x - y
