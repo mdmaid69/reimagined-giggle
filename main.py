f4cb785c2b39c8b2317ea89e4ea2f20d67ddc9b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:17 +0000

    moral would base future split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
