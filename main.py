7c17015d3834190f9aaace1e99f2c17599bba957
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:48 +0000

    by grateful meeting occasional on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
