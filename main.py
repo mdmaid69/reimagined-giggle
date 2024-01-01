fc1d2ac3721a255b5437be1d3623551e1210f7e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:57:37 +0000

    mean carry recent chin suddenly

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def split_path(path):
+        return os.path.split(path)
