2a13fcffa6decc1794945f3f832d743e1ec1b784
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:59 +0000

    stretch joke squash worried exactly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
