1734e7fcb1c7598537cb884231ff0f70b47727a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:54 +0000

    equal persuade fun smooth just

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
