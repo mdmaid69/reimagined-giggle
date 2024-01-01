1ed6506f9fd91bdb8472eee727fdcaf8a9921a67
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:47 +0000

    free dangerous reply properly else

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_typecode(array):
+        return array.typecode
