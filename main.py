d8ceaec3d3bf4cb392a64ad971fe6fea8e812853
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:40:27 +0000

    pupil piece take attractive open

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+for i in range(10): print(i)
+def calculate_density(mass, volume):
+        return mass / volume
