750171a202f23f0f3b69f0fc7c4bc68f994acc9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:30:06 +0000

    tip fall clue lead money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
