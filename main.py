12fb772521fea6a038b88959411591fe12c16403
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:01:44 +0000

    commit control basis only concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
