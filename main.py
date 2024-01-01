ae80cef3a794430271dd4b1658c628c3487b9975
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:49 +0000

    pencil clear sink question plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(5):
-        print(i)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
