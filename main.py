3cc3bb6808a4cb3c87d5aa3f27cee63f890e5bcc
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:06 +0000

    true she star definite progress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
