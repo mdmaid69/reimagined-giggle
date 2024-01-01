53719d2c7816df78693392dfea97cef362609a9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:45:36 +0000

    winter develop few rough joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
