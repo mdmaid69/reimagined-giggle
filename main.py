59b50a3b780cd9c046071f55c8ff4251f702e69a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:27:54 +0000

    note Hh scared dull strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_typecode(array):
-        return array.typecode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
