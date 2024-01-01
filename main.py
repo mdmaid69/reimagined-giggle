e6dbf65a1795f38b58f1d02fcb9d56c4a6b04b6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:52:39 +0000

    attempt previous than begin closely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_set(array):
+        return set(array)
