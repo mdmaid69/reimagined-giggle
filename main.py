b38be3c2591147c3bd0ba13c55874353ce69271a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:05 +0000

    high test everywhere free OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
