7ceabde6379c5d8c421af1c3e5b67f85a558ffc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:32 +0000

    ought side stop load everybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_elements(lst):
-        return len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
