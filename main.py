0693e8fba2f2a2ab71fca4882044b09f7b8559b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:37:35 +0000

    friend tend exploration bang cut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
