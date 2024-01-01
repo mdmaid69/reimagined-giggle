3e1330dc914e6f59e915e22bd2bcc6c5d4316d0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:29:02 +0000

    ear say criticize female music

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_current_time():
-        return time.time()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+i = 0
+while i < 5:
+        print(i)
+        i += 1
