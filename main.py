6dbba0ceb60c85750c4dedc2be326d75b7d194a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:14:21 +0000

    pencil mention breathe wide sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
