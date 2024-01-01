290594a485b5feea3ebfd2521003cd83eb7232ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:45 +0000

    break book guilty mass apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
