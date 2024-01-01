a545eed06e02adbfbc699e76ac39f3eeedeca0bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:22 +0000

    alone evidence guess smell information

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
