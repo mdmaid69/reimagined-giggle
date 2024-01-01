f2f71432174210f2afcb0835ee076bf16baf565d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:47 +0000

    lot sport surprise substance declare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
