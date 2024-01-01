44b5e95f8387b76031bc89c357599369841168d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:37 +0000

    more connect head action effort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
