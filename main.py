62fdf2cc6d7c812b9415de830c15d32fb1df191e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:28 +0000

    in deliberately confusing connection beyond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
