db9284371e1c667340c8ee2598d25cf8886d56f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:16:33 +0000

    married sale add stop yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
