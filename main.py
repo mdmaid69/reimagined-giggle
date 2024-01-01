2c4213ef44f9d8ba00dfa5abdb0c0286e745c322
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:33 +0000

    under take contribute shoot worth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def add_numbers(x, y):
-        return x + y
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
