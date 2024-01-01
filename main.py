5b05175aa557737c3d80b9a091dae798051c5c90
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:21:47 +0000

    tooth damage this temper able

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def square_number(x):
+        return x**2
