42d1ac44d5999d0ffe154533b73f899d2bc331e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:57:25 +0000

    just right scale space reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
