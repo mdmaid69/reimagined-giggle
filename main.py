4a215f4c5990c02a34d36b4a9bb237ff66cbdd93
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:19 +0000

    callup emphasis extremely include half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
