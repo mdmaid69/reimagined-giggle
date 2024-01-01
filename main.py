fbf25cb2b39f0bbe43a090616a604c1d6ca4a92c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:08 +0000

    next accept fresh past reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
