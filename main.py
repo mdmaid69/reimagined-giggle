db142679510a672a365282bf8363f9d8bce27a8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:44:45 +0000

    direct turnaway confidence turn poor

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
