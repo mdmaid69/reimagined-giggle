cd99c53c2356670a5a2018014a6fb57aa5fe6109
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:29 +0000

    dear file sweet later hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def multiply_numbers(x, y):
+        return x * y
