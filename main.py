dfada52fd002d93fec3ea06d8a0d43a718b49295
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:58 +0000

    behind cause callback female post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
