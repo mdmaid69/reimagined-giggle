9ed712ba58c1c681f2b25d2114962e65c2d7bacb
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:40 +0000

    spare urgent stamp plastic return

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
