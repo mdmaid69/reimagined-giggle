bc6574c0b40a6f4c0141555d1e6a97d88ed61986
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:11:23 +0000

    breakfast purpose replace dream high

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
