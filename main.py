dba901ddac7880c6a776880d4729c2c2e87c5f27
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:30 +0000

    music concentrate keen relation into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
