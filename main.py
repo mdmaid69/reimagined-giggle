bd1bb0f8dedd7107687c3167c7e05e7d3dc441e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:29 +0000

    shadow yourself invite top call

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
