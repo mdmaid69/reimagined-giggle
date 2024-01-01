fc1e068a039daf45e0a762aefd6cc3ab367f1e0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:45:38 +0000

    lookaround race grand otherwise doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
