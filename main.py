5fe39aafbb71f5a56f2cd2e5a3981c26c8d3b7c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:04 +0000

    around provide unusual safe never

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
