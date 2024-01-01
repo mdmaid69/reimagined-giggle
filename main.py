d02aa5390aec24fc1df67cb18ac24116df5850e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:59 +0000

    command pen wash pleased spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_exponential(x):
+        return math.exp(x)
