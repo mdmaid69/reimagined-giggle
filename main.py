e7d4af24f2190ababf2821ef3bd9f93f9083aff2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:09 +0000

    blind favour experiment pause arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import getpass
+def get_username():
+        return getpass.getuser()
