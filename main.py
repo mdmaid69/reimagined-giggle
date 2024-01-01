f1c9a45cede0fb1cdbe7f1c6eb183b1e14a35286
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:43 +0000

    convincing afford print respect teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_str(array):
+        return str(array)
