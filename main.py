c74cd3e3bbc2e27208ca44e0d27a7ed19d5e5299
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:36:47 +0000

    both hard split journey please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import collections
-def create_stack():
-        return collections.deque()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
