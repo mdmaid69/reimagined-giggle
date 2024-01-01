dedbf1cb3d237a33bf9c27a8356f1b45f9a2d883
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:38:26 +0000

    Kk card instruction control takeoff

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
