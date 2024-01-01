dfd48a90dd7ddb4af5b323946688e4026d42217e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:56:44 +0000

    ill thirsty make put emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
