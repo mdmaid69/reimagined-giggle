78b9c0c7cbfa4c3a0410bf7ee6985745bc355931
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:12 +0000

    pair seat really obtain save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
