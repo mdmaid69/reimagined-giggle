e0ee9663b318d2cb4f9fd9460636cd56030594e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:51:01 +0000

    today accidentally part sport first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_power(work, time):
+        return work / time
