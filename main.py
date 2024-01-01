b5e47dc927ce9ed263252b63b0a343cada94a50d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:14:19 +0000

    usually exam safety rise both

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
