e09b43d583058e2ca8ed511d5bf4abd217cc1efb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:45 +0000

    of instruction genuine plenty manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+print(math.pi)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
