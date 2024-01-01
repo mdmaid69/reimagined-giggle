6e1817dc07a425536aa94f51ec5a8ee865d6d7f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:43 +0000

    usually list top view circle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
