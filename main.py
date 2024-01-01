9a8aae693063564caa9774c32ddfdd8be3622256
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:56 +0000

    ilovedicks ignore speed roll worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
