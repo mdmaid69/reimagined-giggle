cefb00c7b6ea08fd58468953473714b6e1de8304
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:19:03 +0000

    her official regularly absolutely nor

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
