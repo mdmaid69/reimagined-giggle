f30b82d7e4a21d875225463e0daa41dd995af861
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:37:53 +0000

    crime sort sentence discover tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
