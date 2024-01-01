c310fef16592157d2315ba0c35db2826438f6ea0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:40 +0000

    breakthrough begin excellent argument rate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
