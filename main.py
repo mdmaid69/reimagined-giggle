fc6ce6a6c41b23177fce789ac58c80de3062eac1
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:09:12 +0000

    television rely street suit general

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
