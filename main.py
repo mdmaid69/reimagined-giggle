a8d6ea2c65d9ac3ce1db77f547299aa8f03132f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:20:02 +0000

    city workupto boring forgive smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
