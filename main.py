5ddcad3479621a80e6750db509bebad530bf9152
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:07:28 +0000

    mysterious turninto makeinto last race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
