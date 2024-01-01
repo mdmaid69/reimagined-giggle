1dd962fb4e188b7fe814de7019ba72e2746bf3c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:02:37 +0000

    surface comeup land empty success

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
