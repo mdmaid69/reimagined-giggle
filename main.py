8dc9d208eb71604620e00d635a2ff5d4e9509ce4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:56 +0000

    hold quiet water insist turnaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def count_characters(sentence):
+        return len(sentence)
