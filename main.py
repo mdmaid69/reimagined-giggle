509b24d3ba20c3c06690c1695ca6b637cfed959c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:17:57 +0000

    free around cheek jump after

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_power(work, time):
+        return work / time
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
