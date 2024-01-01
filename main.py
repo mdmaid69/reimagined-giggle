2e7cdbb644992754e7ce3600623653fcab4c9570
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:00:20 +0000

    rank difficult between already course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
