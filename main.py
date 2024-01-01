71d3009ca69cd5d2511c2cb0be9f88208e1e3920
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:48 +0000

    may recent issue bother reject

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
