ed53b052dbf7b0c0a0c136192ec4ab1a6af818ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:05 +0000

    spin hide pour interview before

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def print_python_version():
+        return sys.version
