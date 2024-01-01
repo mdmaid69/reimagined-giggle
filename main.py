1873b2f31befd8ef388181c5caa44c39f15bd4ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:27:02 +0000

    calm undo sweep before buy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def divide_numbers(x, y):
+        return x / y
