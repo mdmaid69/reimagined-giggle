13b75d79e0ad837a7705f34d9efefaf0eca11286
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:21:20 +0000

    target minority judge under dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
