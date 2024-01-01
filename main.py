9b9c505cfaa0e1e632ad79905b40384cb9a39e59
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:18 +0000

    hospital let dress temperature soil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+def find_union(list1, list2):
+        return set(list1) | set(list2)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_complex(array):
+        return complex(array[0])
