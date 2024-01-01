7492b91131141807ac97fd2a857f93b908ea2042
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:55 +0000

    ticket breath concerned out match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def sort_list(lst):
+        return sorted(lst)
