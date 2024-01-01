f60a98b42de397fc574724149b9a09e6d9bfd3f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:26 +0000

    drink she completely bringforward make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
