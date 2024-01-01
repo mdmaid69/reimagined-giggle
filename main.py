362ac5ef5038cd14363bca9e237b6611afd4a7b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:20:57 +0000

    promise juice kind Yy fear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
