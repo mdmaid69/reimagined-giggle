6fd9dcf10b5088f82dfe9b310c5ace6cf1332133
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:22 +0000

    exhibition feel safety Kk simple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
