bdb1c263bd746b1c2bc06aae6fd768409da30000
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:03:39 +0000

    push exchange fast set Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
