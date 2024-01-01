69781b24ad1bcfecae80208d13f3d0b49bad97a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:27 +0000

    view closed word pity belong

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
