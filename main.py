11577028e7140519caddf2ee4a7e89f727ef2e90
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:06 +0000

    breakout quarter case absolutely anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import collections
-def create_stack():
-        return collections.deque()
+import os
+print(os.getcwd())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
