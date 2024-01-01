bce759398fbea327a8d6f330905538ab5484bde6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:14:14 +0000

    entrance personal exhibition light instruction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_density(mass, volume):
+        return mass / volume
