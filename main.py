f41d4ae10c5e81f282fbaeee8eb3367a1665d287
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:01:52 +0000

    everybody immediate young me whenever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_counter():
+        return collections.Counter()
