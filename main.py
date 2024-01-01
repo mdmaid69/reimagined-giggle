fdd7aeead14ba49e06cde678025a5bd9324f2655
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:19:54 +0000

    mention draw where large gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+print(math.pi)
