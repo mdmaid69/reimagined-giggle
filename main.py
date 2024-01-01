bed310230619c01f7528ce863eac5be46a75cc70
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:12 +0000

    promise congratulate problem completely intend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def find_min(lst):
+        return min(lst)
