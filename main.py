2ea7ed622db4c1412a1a16d33414637da89bb352
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:46 +0000

    invitation themselves bath common transparent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
