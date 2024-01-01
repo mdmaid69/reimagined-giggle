17b92d69e81128fdbb78e5fc075283c5f1e10427
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:38 +0000

    tie rubbish month many achievement

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
