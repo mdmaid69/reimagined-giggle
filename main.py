6aa68c9dc10e8cfe80c580d12738de06833597eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:41:14 +0000

    distance murder transparent affect sentence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
