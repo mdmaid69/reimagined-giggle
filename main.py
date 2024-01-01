1103a560f5a8784ab9ff1c72ad8fa0747fd6231e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:27 +0000

    there offer contain terrible earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import time
-def get_current_time():
-        return time.time()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
