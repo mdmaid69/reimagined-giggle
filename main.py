3b57f413b0f057dcfab62915f702083c93a2708d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:46 +0000

    success fortunate throughout principle fun

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
