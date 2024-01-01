fa3c7e81aefa6fcadb875dce4d6f559ce58b8706
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:55:54 +0000

    go steer whichever impatient normal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def reverse_list(lst):
+        return lst[::-1]
