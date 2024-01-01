45ca60a0be780540a07067095eff6a4a68bbb6a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:09:48 +0000

    attention push store splash production

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
