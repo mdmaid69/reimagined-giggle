1dc7c808d7af553429c842961be02060546a0e4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:06 +0000

    big exhibition Ff die listenfor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
