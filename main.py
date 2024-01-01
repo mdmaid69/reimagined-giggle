21c74e814939df1ed688a90b7feb62cffacb779f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:11 +0000

    urgent introduce win deal turnback

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
