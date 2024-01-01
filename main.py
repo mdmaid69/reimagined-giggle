2bc22eb491c73255f86f5252bf508a184d516b55
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:13:50 +0000

    spin behave bush programme reject

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
