8687e2edf2051be2b96856c524c9ea2863442fbf
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:32:01 +0000

    only comeout comeround have busy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
