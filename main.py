bbf8b5f1e257b55c24c603f9f81477ed1df62160
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:36:49 +0000

    connect close compare who who

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import datetime
+def get_today_date():
+        return datetime.date.today()
