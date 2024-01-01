be73d358887d2d27cf0bad94992f2cfce8cf191b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:23 +0000

    careless question slide like disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
