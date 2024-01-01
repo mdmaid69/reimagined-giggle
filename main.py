e8b6d32a9bbad02344c2fbd7d5e34e5ce4347d89
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:58:34 +0000

    past snow size plan equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
