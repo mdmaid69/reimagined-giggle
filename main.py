18d9d91c2039ccb24c0a8edd328f8a27108cb9b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:29 +0000

    central crash share bear no

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
