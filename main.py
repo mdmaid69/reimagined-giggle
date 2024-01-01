e2efd8352fb8a7a0e242ce54a021acb84475d643
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:04 +0000

    see collect attractive lot grand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+print(sum(range(10)))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
