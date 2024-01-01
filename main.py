117af9fc7459f299aac1b8fddfcc4fa1839a7e40
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:37 +0000

    arrival generally actual spoil his

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
