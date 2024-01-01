ea882fb7b5410b68d17597e7117af88b10f1b60c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:44 +0000

    identify everyone lay clue favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
