a545469ed6b93affad6005cb5f93d832d73c5a68
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:59:30 +0000

    bar try pity communicate circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
