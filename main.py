371089d563ff97f83ac90664a77dc4a122a97e9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:10 +0000

    frame turnback pleasure burn 568energy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
