a07a252b2f41bafc0cb6b366530d641593494e35
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:46 +0000

    medium Hh attractive suffer excellent

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
