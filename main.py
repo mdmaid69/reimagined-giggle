dc5bfba7420a582aee1d95cf6fb11e6a71dcd7b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:40:51 +0000

    typical comfortable danger introduction much

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
