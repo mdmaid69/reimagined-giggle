f7b3e40a38a0d3cde86cf5534f4c31aa63fecbbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:28:57 +0000

    constantly zero thankyou Ii will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
