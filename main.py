6b7f49f0377417094ee29aa6733a59c77456b8a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:10 +0000

    play have ever operation extremely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
+import sys
+print(sys.version)
