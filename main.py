8a8cfe54b27e4dc9596e9bc76232b6129d61a972
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:48 +0000

    welcome present whistle hollow traffic

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
