0da2909ea147ec99420bf88d91c7a85ced5d8560
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:37 +0000

    bring need protect seem tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
