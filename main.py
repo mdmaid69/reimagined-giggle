a3bd36c38a1d8c246d2d08575ce9bbd834c3fc1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:50:09 +0000

    closely urgent sort broad if

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+print(sum(range(10)))
+import os
+print(os.getcwd())
