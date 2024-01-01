1da5aa0dcfe41ed3cce02343fc5275e958e2ff73
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:40 +0000

    large rest anywhere delight drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+print(list(itertools.permutations([1, 2, 3])))
