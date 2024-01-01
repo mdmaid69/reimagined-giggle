be48d49a0305826e748047326b00577cb50db44b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:42 +0000

    concentration switch off properly copy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
