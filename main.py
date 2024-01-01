cb9974da203ed9564146b18046ebc2ae72d62cae
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:43 +0000

    lookaround conclusion payment lock attractive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
