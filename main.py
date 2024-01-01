0c7bf477c2743a245765fe095147ec38fc97ae30
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:04 +0000

    beat heat read centimetre fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_floor(x):
+        return math.floor(x)
