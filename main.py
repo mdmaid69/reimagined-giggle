bb52e9eefeaa40bb9a2f2d7a87b72d17fef82147
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:35:07 +0000

    violence open lookaround appearance tend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_current_directory():
+        return os.getcwd()
