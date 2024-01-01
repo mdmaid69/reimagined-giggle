489cf03ce41b8a2cc5d63e996f1e500cd82a762b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:07:28 +0000

    spring instant both already progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
