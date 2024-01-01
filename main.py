0298cc13e3a9f83ca30c4a6572e9b475713b63d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:31:55 +0000

    open everyone entry improvement exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
