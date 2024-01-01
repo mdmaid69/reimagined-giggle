569711a284a5fe055d2d43eeef416f2e30569a4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:15 +0000

    provide finger practical modern centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_exponential(x):
+        return math.exp(x)
