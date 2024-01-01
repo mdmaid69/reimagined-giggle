6af2e03dac71e993dd9a7e3e942174cca924d4ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:37:12 +0000

    fix with connect depend each

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
