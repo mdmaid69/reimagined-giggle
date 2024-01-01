973b1ad967c6388c6459f53858d01720ecf8771e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:19 +0000

    accidentally stretch interesting school talkinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
