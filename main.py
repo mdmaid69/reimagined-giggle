9e83059199eee741cedd9854c51b523a7898f837
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:15:26 +0000

    circumstance happen change only copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_max(lst):
+        return max(lst)
