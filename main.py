ced52902be978cb67eef44933b57a5d239188254
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:07 +0000

    could learn figure press admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
