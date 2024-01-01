331fc0a1911ac04a35131c3a78bc1c6a1cd5363b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:49:03 +0000

    variety 179bite responsible tidy family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
