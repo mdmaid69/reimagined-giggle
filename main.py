d9073bdad45a76c2dc4f8e2456d7ff0fbe1bcb47
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:52:21 +0000

    write performance Ww increase blow

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
