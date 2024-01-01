718ff665bfb048776cbc703e3a78334d8a21fe9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:19 +0000

    in noone pressure live treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import numpy as np
+print(np.array([1, 2, 3]))
