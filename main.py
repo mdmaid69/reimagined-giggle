bdf632918edf4baa3d75c128d22b1a3c761ca2a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:18:13 +0000

    set subject company on anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_min(numbers):
+        return min(numbers)
