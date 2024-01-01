80183c0f2613498aa413d6dc00445de72227804e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:04 +0000

    thing through over between service

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
