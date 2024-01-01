fbd087f8d8c883d17e21933782663589334a37d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:12 +0000

    forward race consult far apparently

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-print(sum(range(10)))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_current_directory():
+        return os.getcwd()
