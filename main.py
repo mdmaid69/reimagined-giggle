187723e0729d29155aec7b385af530632abb2560
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:54:15 +0000

    compete form there steam direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
