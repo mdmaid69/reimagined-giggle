ec04d34ddb8ca9a77199081a43e09ac7a72a36cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:14 +0000

    more week favourite hurry uniform

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import datetime
-print(datetime.datetime.now())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
