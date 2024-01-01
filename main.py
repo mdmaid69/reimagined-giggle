ee19fc4daf633a5f05fe803a126bb069c060b304
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:10:04 +0000

    for callat deliver something contact

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_item(array, i):
-        return array[i]
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
