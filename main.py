a3e1dc86ed528b12834cbb4da604c0f9ca9eafa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:03:43 +0000

    truth shoot rent getoff rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
 def pop_from_array(array, i=-1):
         return array.pop(i)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
