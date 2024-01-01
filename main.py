331b8a46bbd3283756488b9f2be370dba7b3ae81
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:02 +0000

    escape doctor pair powerful range

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_acceleration(speed, time):
+        return speed / time
