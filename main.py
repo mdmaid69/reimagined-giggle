e78b406757aafc0eafd40fe23eb945452e840cf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:49:33 +0000

    club split shelter giveout guess

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_work(force, distance):
         return force * distance
-for i in range(5):
-        print(i)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
