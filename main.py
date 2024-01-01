d4853598d813e5a969847a65ea936ea721de6a2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:35:48 +0000

    personal yesterday club deep star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print("Hello, world!")
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
