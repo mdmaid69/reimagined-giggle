9d3ce2eabdf083504c8e927c0cae5539e25cba09
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:53 +0000

    worried custom further juice open

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
