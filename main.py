2dfc92e878b5ec62ad96f61d51ac6a6592ca4016
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:32:32 +0000

    choose enthusiastic Kk everything original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
