fa910f027d7bf43312eb8c91e064502992806ee1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:05:22 +0000

    say example slightly space complicated

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def count_words(sentence):
-        return len(sentence.split())
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def subtract_numbers(x, y):
+        return x - y
