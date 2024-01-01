342888b550a7b53a8902ac600a3d8b1ee6c62680
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:45:46 +0000

    spill sudden find many strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
