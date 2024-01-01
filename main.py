3b3da7d639c242b08ae1ab37277fca677c9b1d79
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:10 +0000

    imagination ahead but after cap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
