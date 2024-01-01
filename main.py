e88ef37dc372b04cb82cdfd764af393d8b8c299b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:11:00 +0000

    callback let patience early mine

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+print([x**2 for x in range(10)])
