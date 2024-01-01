e87cfbce1ac9753645a0ae537273813fce611fb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:21 +0000

    gap far turndown sore mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
