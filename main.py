7057b7ac5b1f3cd6b3844d65566e0b0fa0b5206e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:32 +0000

    tooth waste question dick accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
