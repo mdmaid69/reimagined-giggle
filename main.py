683eb77f4b55d0f7695c239f70899fdeb6160ec2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:25:41 +0000

    trust whole hard contrast certainly

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+print("Hello, world!")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
