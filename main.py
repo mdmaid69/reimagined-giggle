40b42b73c87bf27953a96b710d2ef080a1e76b0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:36:54 +0000

    division friendly firm male practical

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
