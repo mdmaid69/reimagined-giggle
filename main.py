c9f5438e6a919bfc1a6839f5c073835c7a82e605
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:45:56 +0000

    finish course 572enter information my

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import sys
+print(sys.version)
+import array
+def reverse_array(array):
+        array.reverse()
