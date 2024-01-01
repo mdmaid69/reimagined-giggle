ef3471e394b1d1937e0c375e3ad27252680a499a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:42 +0000

    unhappy in term table post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def divide_numbers(x, y):
+        return x / y
