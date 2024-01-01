b0d6af72ad044ea3e20c6a69ca922ed0a0520743
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:25 +0000

    difference fix never sharp holdup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
