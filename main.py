a44cfefd921a61b966219b03abab1c46e6d49148
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:14:14 +0000

    concerned invitation about Bb always

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import time
+def get_current_time():
+        return time.time()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
