136ebaa09a506e38472dc57838668697781f0be4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:13:46 +0000

    firm open phone objection thin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
