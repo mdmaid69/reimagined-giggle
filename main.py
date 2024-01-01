d2574a7a31e84b39f3c8ace9f7f9402ea20aad33
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:17 +0000

    hour practice fear announce short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
