367a5e87c8c88be23bad169a9b5b6de3c9dcb675
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:47:17 +0000

    comeoff class schedule regional awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
