097982016ae7b104814bba533186ced0799dfb34
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:06:27 +0000

    rest secret actual image company

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
