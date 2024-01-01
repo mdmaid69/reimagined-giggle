19a745f70d00117d59b54846825b823b087e3c4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:02:31 +0000

    regret Bb set several burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
