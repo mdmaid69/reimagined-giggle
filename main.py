0bb104e3b53615b3659222aaf899bfb4528ba9a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:36 +0000

    convincing apply opponent so weather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
