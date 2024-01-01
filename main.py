137bca66db591de97ca4a2097946b8bf0c540ca0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:35:23 +0000

    among appeal behind definite present

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
