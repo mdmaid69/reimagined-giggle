0e84452d0a061e17a128f13fc1e22fe29ad8a7b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:24:01 +0000

    else detail getoff address full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
