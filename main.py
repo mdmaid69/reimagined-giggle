40061a4fc7ef57e81bfbb96552f5d706dea8052e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:41:56 +0000

    platform 572enter provide answer disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
