cfee09c669555d0dad4162cf48841e8a5e140b77
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:46 +0000

    petrol some scrape cause exist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
