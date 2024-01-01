3ab98070d985978fc36fa9017b9bd9ce2938c521
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:08:19 +0000

    could form with shape stone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
