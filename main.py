06860357c8dcf423db72449a361d269238c24d14
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:01 +0000

    excited sort moon Pp tired

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import collections
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
