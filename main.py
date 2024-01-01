6cfe020d79eaa0fc0c4b6b4ecc99e1e4fcb4488a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:37 +0000

    winter contain pretend secret pour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
