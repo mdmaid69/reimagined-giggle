7b1dd51ff10ded56f6facefaba32aa3908d60280
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:31:14 +0000

    correct brain typical for surface

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
