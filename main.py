9232f5f2f40daffb37000fca66ffc060b8835a01
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:30:01 +0000

    choose mail smell mass insist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
