11e79453b8f6c46acf34e10d34ff37daddc69546
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:20 +0000

    refuse line argue tall chance

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
