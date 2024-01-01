88de9b63d5fdb1c0504f34070274c86f12605784
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:44 +0000

    over coat practise accept crazy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
