0016d038f073d685c80b056929c3ce32b3e5e444
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:03 +0000

    disturb influence stare makeup suggest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
