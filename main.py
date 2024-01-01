b035d69fc74a793b05be3a9d6908fad8d4080ea7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:14:45 +0000

    too path upwards point accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
