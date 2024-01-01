bc7967271af4623b612eca8a1be9d0765a01a78d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:40 +0000

    land effort height Itself heat

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
