4432790d788d54ee70de2da6389e047a21cb301a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:16 +0000

    Ww school plan replace guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
