c6200ed44fc582d1521eaba2b01991b9cea6bd56
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:47 +0000

    rest exit forget close peaceful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
