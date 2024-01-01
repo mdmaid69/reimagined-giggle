aa407eb7e3601f8ea1ae1a4df742adf25d2e1ca2
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:30 +0000

    tense holiday book die home

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
