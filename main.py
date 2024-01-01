b92002fd83556cd1cbeedab6136e9031cec901c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:01 +0000

    such act replace left rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
