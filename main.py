9f821853ae11c0dbe8d3516a04afb3c722eddbdb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:05:30 +0000

    union handle getback wipe please

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import time
+print(time.time())
