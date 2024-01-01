1c2446915d807d09b3b51f54460161441199d8b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:46:42 +0000

    shade rough really offend grade

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+print(sum(range(10)))
+import json
+print(json.dumps({"name": "John", "age": 30}))
