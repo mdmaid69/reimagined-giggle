7277f01d8a77715c0ff5cd32591357687a7a6453
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:16 +0000

    roll heavy allow lend price

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_length(array):
+        return len(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
