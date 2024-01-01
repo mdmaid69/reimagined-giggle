9e9b911af8bcb6aad237e246987048c6f5312eda
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:18:50 +0000

    song weigh word die state

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_area_circle(r):
+        return 3.14 * r**2
