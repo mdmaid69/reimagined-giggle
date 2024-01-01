8b2f5d7ea343277a8fb85e5c2be20ed1507082a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:49:49 +0000

    destroy left middle war thinkover

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
