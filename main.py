c8a51c74c08cde70dd1106616b59ad342fb7ba5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:27:31 +0000

    almost claim raise tip affect

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
