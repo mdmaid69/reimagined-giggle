4e13816659dc462d3aee165376a2d5151b36575d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:41 +0000

    variety squeeze politics Tt help

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
