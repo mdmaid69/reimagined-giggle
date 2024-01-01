605351543fd98e9ccd53f663eae66b374ef46b55
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:56:13 +0000

    row fail mine case meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
