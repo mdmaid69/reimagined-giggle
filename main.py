dcd3e0f4724329024558669db2a3b9d73dc8dbeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:05:15 +0000

    floor permission clear miss bright

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import sys
-print(sys.version)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def multiply_numbers(x, y):
+        return x * y
