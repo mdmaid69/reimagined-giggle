c230888fe5950f7acce4dffedd0ad653a6619550
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:47 +0000

    Ii opposite smell standard paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
