f46be48f3c628574b458e1221bf224d03afc95e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:29 +0000

    bear practise takeover platform goout

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
