58bbb075ab01778a21e3177636230cdad0e3a492
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:54 +0000

    gate sleep even please male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
