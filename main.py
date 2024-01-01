2a9396c85da4d0f69af46e1f311caaead28a174e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:28:59 +0000

    leave knock judge jump dry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
