2f781f27fd555669419358c21a50d096df285af7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:49:24 +0000

    mess hear feeling famous freedom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
