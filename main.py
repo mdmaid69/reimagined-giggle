99f14c76e68f9d3074f403122df59c8bb12eded5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:06:57 +0000

    step unfortunate content event die

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
