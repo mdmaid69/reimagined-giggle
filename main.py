ccdb166ff8c372ad8455e72af3785ad80dcf6979
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:50:45 +0000

    handle snow spring hungry our

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
