6c235fa98aa3636d647d843a6a0fc012c6e27805
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:59 +0000

    argue body coat control rank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
