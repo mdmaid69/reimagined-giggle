85ae4093c0acd00e5be57549d1a34d0c381fe1a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:00 +0000

    cut adjective clever before look

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import datetime
+def get_today_date():
+        return datetime.date.today()
