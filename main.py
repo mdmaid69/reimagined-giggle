f3369d10bf451ef00f4df1d77a0aae1ea91f2106
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:16 +0000

    slightly she arrival unusual lift

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
