9c964a8030306b508d2706b101cce19d7e09f82b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:01 +0000

    skin live Qq circle recommend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
