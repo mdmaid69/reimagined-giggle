3e30c2b7f303ed17329889df678bff062f353e37
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:19 +0000

    herself post explanation pay thing

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+print("Hello, world!")
+def calculate_power(work, time):
+        return work / time
