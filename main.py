9974077c95d693c7182b1edc7ce070508a3def15
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:17 +0000

    stupid night with loose callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
