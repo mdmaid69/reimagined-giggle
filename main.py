e692f11c1861a945dcb562daeea431e85833e038
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:46 +0000

    practical ours amount dream upwards

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
