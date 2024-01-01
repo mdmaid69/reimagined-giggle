30bb349294d641d42f215efdaf8e7526fa4ce17f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:40:42 +0000

    pick correct obtain quarter 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
