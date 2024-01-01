b47c048c67ac4f85701bb1f5f95510ed9f1bba24
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:59 +0000

    square child fasten disturbing order

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def count_characters(sentence):
-        return len(sentence)
+def sort_list(lst):
+        return sorted(lst)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
