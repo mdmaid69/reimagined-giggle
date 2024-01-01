764f416e18728678339a9467883eabe7ca021121
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:46 +0000

    mood repeat repeat priority operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
