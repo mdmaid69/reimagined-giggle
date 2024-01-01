dc3079dbc56d1458857410793e706d46fe34d2e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:27 +0000

    decide size travel pipe announce

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-for i in range(10): print(i)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
