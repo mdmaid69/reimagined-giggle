45fe66cd6376394f38e123234b75cd23e8be32cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:09:01 +0000

    so hurt past march cost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import random
-print(random.randint(0, 100))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
