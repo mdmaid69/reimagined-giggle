c6e8efe14771af90d696fd1713fc9e362a24c890
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:33:49 +0000

    since badly culture increase Ff

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
