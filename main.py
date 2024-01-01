dd4f7365d304a5d115d1182597216ee4ea60d93e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:51:27 +0000

    except finally 569enjoy 179bite cry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
