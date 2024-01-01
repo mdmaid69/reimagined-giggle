3d6d9f53b51dfea562c9ba5707ad0708290ee928
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:06:50 +0000

    or offend polite up completely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
