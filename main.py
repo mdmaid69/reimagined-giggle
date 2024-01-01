b81c78dd0a92fa359f65184f2cba240ec20c9972
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:32 +0000

    our past forward boil holdup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
