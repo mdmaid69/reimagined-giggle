ffd104bd9af5398c47dadf94fddc58d9c1fe9ad6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:25 +0000

    injury weak world really size

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_list_from_array(array):
+        return array.tolist()
+def add_numbers(a, b):
+        return a + b
