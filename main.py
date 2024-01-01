b1211630bea3302ead974a9036f9eee59af38e52
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:08 +0000

    whistle hit excuse declare cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
