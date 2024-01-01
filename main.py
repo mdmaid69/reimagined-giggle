b1ea4e581a24ebd9dedf7b434a044fe11bb7f830
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:03:02 +0000

    any broad than once stranger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
