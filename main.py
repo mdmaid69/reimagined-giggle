5d458146ef424d160dc16a4a8aade1d3c4e96abe
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:31 +0000

    rough workon wind neat organize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
