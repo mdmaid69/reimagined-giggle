d67d4b6cdd9c4fdb293e9d21ca119c339e6c84d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:20 +0000

    angry thinkof band mind makeup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
