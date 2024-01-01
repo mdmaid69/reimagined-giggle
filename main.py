0f94e7fc488b0593e493f03d050738657d95d385
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:15 +0000

    goaway hard character kind listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
