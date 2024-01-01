bfc2abf723871eafb667a2c1baa661d77a9eadd7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:09 +0000

    develop variety our drawing national

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def count_elements(lst):
+        return len(lst)
