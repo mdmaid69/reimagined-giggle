72a38f4918199adbb9843e64e55e655d3f481ec6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:06 +0000

    weekend exam goout every callat

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
