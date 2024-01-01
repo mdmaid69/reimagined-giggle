e1a500cd0d80899b2274e14ce01eae7262c9f599
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:55:10 +0000

    politics throughout comeup town regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
