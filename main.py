06aec402db74f6d267249f6f33aac948837191f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:54 +0000

    top apply board chin vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
