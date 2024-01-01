a265283f8d891176878c3065bbb9dcd1cfb96fab
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:52:33 +0000

    start bet release neat fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
