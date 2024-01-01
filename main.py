6208539e9f7894cea6ab0891f54ee4a4e9c8834c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:07 +0000

    tip practical development cut shine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
