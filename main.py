939841f014e448726ba1fa6598f13e4a1664f287
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:24 +0000

    film string understand almost record

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import numpy as np
+print(np.array([1, 2, 3]))
