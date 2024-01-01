f9762d1a907ecea66a474893fbb7e00e35379c10
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:42 +0000

    comeover regularly future fall damage

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
