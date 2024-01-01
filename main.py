9b1619dcbab385429dae1e80dbc33de3211c7e61
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:12 +0000

    point when risk concentrate relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_density(mass, volume):
+        return mass / volume
