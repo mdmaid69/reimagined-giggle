7cb2b43ec267665961df384bc0114a2b35ae6dba
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:37:47 +0000

    valuable repair glass service majority

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
