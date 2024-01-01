1addaf7e034fa23e4bd457f503ac9e907091cd84
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:59 +0000

    much sale sore unusual count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
