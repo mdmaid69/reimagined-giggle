8ad21f7a8201d6d704c5405d0c04143ff0872428
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:25:40 +0000

    only sink ourselves search interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
