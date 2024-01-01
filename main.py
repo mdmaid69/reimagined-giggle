fcf85063bfe2a55cabd8acaed5796bb2ef1110c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:22:10 +0000

    about share colour 566end custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import os
+print(os.getcwd())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
