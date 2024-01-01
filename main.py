7e9c129cec89d34e7d553e20fcc9a20d75c29d9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:38:32 +0000

    separate feeling thing belong calloff

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def reverse_string(s):
-        return s[::-1]
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
