3f28a67eb9706ce7b186e8a672d02689e09dca68
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:02 +0000

    pain maximum traffic home brother

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
