b852ec30f1f41134de14fc45e54dcc5ae0df9159
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:50:49 +0000

    cover mine little never situation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
