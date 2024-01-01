ec2034eb9efc0b66b5d4dea223a7fddeb4716392
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:43 +0000

    keepup willing direct tie fire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
