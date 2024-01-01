a8893cc7d11e5c35ea7d35b73333e70135cd8be2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:17 +0000

    forget excuse perform feature find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
