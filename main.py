4f84a0f19515d93e3040b3fdb72423c2aa5d1340
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:18 +0000

    rank job smile care beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
