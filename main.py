54d7781da59e9cf56f364b07899c892935451374
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:04:32 +0000

    same platform colour unhappy complain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def remove_duplicates(lst):
+        return list(set(lst))
