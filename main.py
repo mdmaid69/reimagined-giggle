bde2ebc433959a4154b36952540f303b7b042e48
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:07 +0000

    row poor tip square great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
