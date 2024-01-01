b51861d007f64d9142791ede79de0f1d55341924
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:02 +0000

    trouble emphasis law hair step

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_power(work, time):
+        return work / time
