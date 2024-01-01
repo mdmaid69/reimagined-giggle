39f1f7d3ffacf2a4d25dca75d9d8e2a9b50639b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:04:03 +0000

    summer wear act would purpose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
