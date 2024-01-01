4a76c67d3dc8444d79455c8d4d140a83e0cac25b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:36 +0000

    office sound recently attention doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
