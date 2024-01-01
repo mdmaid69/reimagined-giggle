f4c1d788c48bf92efb6470ddf38be561b3115014
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:10:49 +0000

    colour argument broad clear cruelty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
