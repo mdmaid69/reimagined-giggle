f7dd99d783dccf59ec950f385470529ff3695231
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:44 +0000

    bunch radio young film plenty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def reverse_string(s):
+        return s[::-1]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
