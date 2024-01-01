59ccae51909c7bb69678b6edca992df8f59d99ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:13:59 +0000

    imagination share computer suggestion straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
