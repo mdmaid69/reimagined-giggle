ab2a3b530edf5b07f70b10469a32c32e44bca778
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:19 +0000

    minimum take preparation Ll bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
