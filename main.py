5ea8fb77159c799d95056d037c369a07dac3ca1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:17 +0000

    stir town worth fit Itself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def is_palindrome(s):
+        return s == s[::-1]
