5f2dfa52a7ff4f2988bd17782e62e574d666e674
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:57:47 +0000

    learn belief hit accidental behaviour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import random
+print(random.randint(0, 100))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
