4ce87b27fb438bbf6eeb0f8f7a479a0bfc950186
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:19 +0000

    convince sleep approval afraid earn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def split_path(path):
+        return os.path.split(path)
