9a547d8716556d0a42ebe658b8da18cc3512336b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:02:33 +0000

    tape answer consist loud brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(5):
-        print(i)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
