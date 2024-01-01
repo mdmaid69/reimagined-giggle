ad6c35c120334befb0a08c153c77d8dac158d8c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:53 +0000

    valuable dull direct freedom object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
