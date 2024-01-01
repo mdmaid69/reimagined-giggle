8398b843b197f3b1faf67027f5c9c6ccf6977724
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:29:51 +0000

    flow giveout uponon plan mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
