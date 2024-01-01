e327bd029ee38e831da2408f02091568f86e7bf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:40 +0000

    legal present adjective sea makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
