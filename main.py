dcdbcf08df80cfd6eee57908f06638f68dd04d74
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:19:38 +0000

    upwards steep colour threaten refuse

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def reverse_string(s):
+        return s[::-1]
+import os
+print(os.getcwd())
