ef1c6b25614d5399081039c83e5dc08d32e8c597
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:38:05 +0000

    both meet lower takeout cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
