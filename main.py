6fbcce6fb865b966ffa5418feeed68149b02100a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:02:50 +0000

    excuse famous around statement pour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+import os
+print(os.getcwd())
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_array_to_bytes(array):
+        return array.tobytes()
