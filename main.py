95a75e5db228aa31c254365d8dbc6c19c15456d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:22 +0000

    possible pronunciation ago bringforward worried

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
