13479f79596ba78a291192a7bc40e92d023a9c6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:13:14 +0000

    positive unable minor better family

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
