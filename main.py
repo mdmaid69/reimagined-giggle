c650fe40f1bd9ac5285077d467534ff313ac7e1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:18 +0000

    benefit protest thumb complete ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
