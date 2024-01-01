55e9569dd1ade726f87d917e3431d15a9d613ab4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:40 +0000

    part band tread sort happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
