2e30d1da12347c34db7e2d1b2eec5b2e3981fa49
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:21:55 +0000

    trust Uu report little slow

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def is_palindrome(s):
+        return s == s[::-1]
