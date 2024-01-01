ceb79badfc08b5992f696a252c0ca43a51fea7e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:56:42 +0000

    pocket wash tonight letter rest

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
