9b500adc36b0266e3ea2734f861656ce90ec3eb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:47:31 +0000

    although maximum gather through art

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
