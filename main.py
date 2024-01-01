e55596c07b8cdeff4b24db4e25b2572c4650d955
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:47:00 +0000

    tape over responsible office mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
