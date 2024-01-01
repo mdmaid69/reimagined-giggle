cd5289e9cb5cd58ca937da4edf3a5a0de9aa9e1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:55 +0000

    rest regard sure confident miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
