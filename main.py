f4e7473c565e7d17b1030d74c6bb8bda7a4ceadd
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:00:37 +0000

    term truth shut snow swing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def calculate_power(work, time):
+        return work / time
