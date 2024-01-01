f24a0941f25958bfb94ea09a812cea7a545f4d2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:35:50 +0000

    people fire thumb now disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
