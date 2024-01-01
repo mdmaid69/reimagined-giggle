ce6196da23629ac8a9497263e4014af0c9cf6109
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:25 +0000

    than odd odd feel 565end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
