ab36016faa2ef110122d4e507e0527d82ce69928
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:19:36 +0000

    ring about lose rest lookafter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+for i in range(5):
+        print(i)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
