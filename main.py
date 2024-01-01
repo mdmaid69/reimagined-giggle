57a5d0e9d7ede5941fd2291e910a94abc47b51ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:06 +0000

    attract possible nor be sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_current_directory():
-        return os.getcwd()
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
