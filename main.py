bef3586c9027188044c1874ac69d75d5d2849b7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:33:19 +0000

    tidy raise fly possibility godown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
