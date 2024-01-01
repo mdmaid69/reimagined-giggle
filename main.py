851660f4dde7f99e913ea939242f42630cb4c827
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:29 +0000

    possibly fire neither rid makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
