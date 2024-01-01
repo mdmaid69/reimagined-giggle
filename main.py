5cf3ed476ff16c2a73d5c23ca59264b9e1d42cb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:29 +0000

    flood great practice introduce remain

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def add_numbers(x, y):
-        return x + y
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
