0a890e872e8ff7be302b4854e66706ea71b67887
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:37:06 +0000

    power score without father count

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+for i in range(5):
+        print(i)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
