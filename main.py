1ca0a18b320d4e291ff3183ecd02444ba36fd9da
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:16 +0000

    exhibition gooff angry simple both

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
