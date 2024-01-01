8da1eb9dfd432f3ae05a4e69e941c59be3f5ca21
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:46 +0000

    laugh respect believe term journey

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_min(numbers):
+        return min(numbers)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
