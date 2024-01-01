48067a77d8729ea11c7c58dd83d68af5bac5fe99
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:03 +0000

    live better dish before deaf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def divide_numbers(x, y):
-        return x / y
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def remove_duplicates(lst):
+        return list(set(lst))
