1089ffb197c46a50484db42c954500d58f13dc1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:05 +0000

    paper sure begin the autumn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import datetime
-print(datetime.datetime.now())
+  import sys
+  def get_python_version():
+        return sys.version
+import time
+def get_time_since_epoch():
+        return time.time()
