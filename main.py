e9805af6e7e63a69727cbfa8403131198d5d70c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:23 +0000

    prove pollution maybe idea cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
