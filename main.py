da8b2d320a482f8008bc082a2991cb0104de4c44
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:35 +0000

    advantage estimate move uncomfortable name

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
