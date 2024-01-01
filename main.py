c6060ee9a488db649dc03aa4e4b8e50209212753
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:54:26 +0000

    so ask side uncomfortable mind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
