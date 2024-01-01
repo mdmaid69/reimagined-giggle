b4ee17b8cd650dd16f9310e708009715f9ffafd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:01:34 +0000

    accordingto close laugh sight everything

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def add_numbers(a, b):
+        return a + b
