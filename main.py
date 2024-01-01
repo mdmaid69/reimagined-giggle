e7c8c9f1cb1caa083fdf5440e039b7330c9e9fe4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:27 +0000

    chin factory freeze Rr conduct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_floor(x):
+        return math.floor(x)
