7a5b319719941bb3133dbc82db55dcbf4bbe4fb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:47:58 +0000

    measure improvement practice bend lonely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
