718ef09a071a8793de29723b351ce1eae17e222e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:29:41 +0000

    hit where slide swallow ourselves

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_speed(distance, time):
+        return distance / time
