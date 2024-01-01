6333d02ee0786ee0578250b50d396f468ba90159
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:52:47 +0000

    any unable use phone people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+print(os.getcwd())
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
