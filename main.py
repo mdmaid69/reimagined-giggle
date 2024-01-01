2f8cfc90eb1d8117ca976607c38cc303bf5e6197
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:23:29 +0000

    set sound burn peace sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def reverse_array(array):
+        array.reverse()
