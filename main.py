894c247ab52d08fdbfc063adb3afb0856b942ab8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:02 +0000

    breakthrough recognize goup would suffer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
