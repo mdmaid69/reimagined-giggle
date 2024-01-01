569625f50a92051a323065eed4b549780380a391
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:54 +0000

    surprised entry spread neat fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
