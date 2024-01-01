be1efc1f81aee516e50d423bde6ef0aff93730d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:54:23 +0000

    today lend relief difficult behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
