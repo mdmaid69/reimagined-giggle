0cdc89eb8651e71176196e7644e22f9b3b2aaa53
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:26 +0000

    colour off mess safe track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def reverse_array(array):
+        array.reverse()
+  def subtract_numbers(x, y):
+        return x - y
