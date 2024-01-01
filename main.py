2cd39d10e3b884fd490aacc03ae40bc220484064
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:05 +0000

    small sure question collection government

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
