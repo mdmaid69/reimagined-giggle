78d09b6116d5ab670647aecaed9b10daa4a8faa6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:22:20 +0000

    lot chance against acceptable can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
