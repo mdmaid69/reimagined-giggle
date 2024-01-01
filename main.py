bbbcc5e6f79f338887761986af684c6f93cde95b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:32 +0000

    yes wet system sort comeround

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_time_since_epoch():
-        return time.time()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
