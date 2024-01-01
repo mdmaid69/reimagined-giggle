eb736594e525c6d14ae75daf13a0e09adba350a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:14:12 +0000

    whisper properly money wind mind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import os
-print(os.getcwd())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
