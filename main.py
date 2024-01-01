8179d066ca1d5dc5685d2f89be1f77ad183954ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:21 +0000

    lock hope wise male discovery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import numpy as np
-print(np.array([1, 2, 3]))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
