ed53ea5f8eb8cc4dcfdfb6fce00dc3115937094c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:37 +0000

    over unhappy head getoff suckme

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_binary(n):
+        return bin(n)
