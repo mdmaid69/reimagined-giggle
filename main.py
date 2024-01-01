a5d1b53c601c8c1bac122016ff2d3f215eac06b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:54 +0000

    accommodation each purpose line comein

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_power(work, time):
+        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
