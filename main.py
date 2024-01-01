5e3adb5af370a7c5d41e15763e6debdcde8a0a6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:26:03 +0000

    confidence skirt polite contents paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
