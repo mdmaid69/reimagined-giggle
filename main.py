5b5398bfc5e5cfcc972ee8ce9c51e7ee8790c30e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:33:18 +0000

    me once obvious stay motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
