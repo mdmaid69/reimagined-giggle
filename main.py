f627702aa18ddfdba59a3bb1d7009bb917fd045a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:57:09 +0000

    pencil his live consider shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
