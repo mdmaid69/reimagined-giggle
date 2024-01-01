0aa06ee0225d717ea5f7c6a0da2b4788ea39e5ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:33:01 +0000

    able even lunch even slide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def get_array_as_list(array):
+        return list(array)
