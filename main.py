7f9b2799b6bbe0c2c0e1bd816af7e0bb8a3ea1f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:34:08 +0000

    present blow bunch forever food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
