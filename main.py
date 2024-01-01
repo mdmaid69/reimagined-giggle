8158f3f6e355167a8197a8ab663607ccd00df680
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:44 +0000

    pleasant include clear whisper follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
