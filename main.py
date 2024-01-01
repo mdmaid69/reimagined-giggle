c02d29588b1ab70f9cd0681c4dd58b3040e3a0a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:04:03 +0000

    shoulder detail swallow blame recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
