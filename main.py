51e8d2c8f180b2a589b5613c8fad4d4bf9e02647
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:43:36 +0000

    name luck support further sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
+import time
+def get_current_time():
+        return time.time()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
