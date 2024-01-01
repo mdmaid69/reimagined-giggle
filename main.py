067d8d7a8ea4e66918625a3768f65c388a287d0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:41 +0000

    address conversation minute write table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
