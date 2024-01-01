c9671b3abf88f870d356804eb27805ca770c4568
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:43:31 +0000

    transparent ball habit question deep

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+i = 0
+while i < 5:
+        print(i)
+        i += 1
