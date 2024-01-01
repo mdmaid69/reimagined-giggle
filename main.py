624d92ac86521b06d019abfd0c146f70df8a77b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:33 +0000

    nice touch flat tense correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
