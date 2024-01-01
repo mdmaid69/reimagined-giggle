2af79d1b08fd85f42c87227afb325986cd43db8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:19:12 +0000

    interesting pencil visit radio appearance

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
