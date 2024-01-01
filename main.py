21c382b3eaa4ef9168f9efe55e8f57b2290bfd80
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:06 +0000

    that claim temperature on path

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-print(math.pi)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
