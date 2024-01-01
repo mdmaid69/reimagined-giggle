fb94ddae8dd76a47ec6a4f78bbd69764a2ece3b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:10:21 +0000

    pause defend store frequent unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
