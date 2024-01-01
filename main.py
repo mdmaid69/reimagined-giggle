5e2886fd620377923885cc3db3a77276afa44e37
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:36 +0000

    season complete now view difficulty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def count_elements(lst):
+        return len(lst)
