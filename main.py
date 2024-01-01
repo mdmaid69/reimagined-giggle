5862b1c4c37dd430f54ebfe8d0ada0c80f0d7d26
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:47 +0000

    collection related reserve behaviour sad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
