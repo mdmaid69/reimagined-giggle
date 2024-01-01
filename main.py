ea9b308b8ed0abc1706433f38fb1eff3aea5b165
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:15 +0000

    hungry bringforward dress opposite clear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
