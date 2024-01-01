6cffbac4fe0999ded414c330f86ad53e35c156c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:28 +0000

    healthy task check press contrast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def print_python_version():
+        return sys.version
