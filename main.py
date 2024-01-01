39e40c80d176cf795bf3a4e1e35ca7885cac9a99
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:03 +0000

    568energy share dull ignore find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
