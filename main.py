59e711c95fcbf087ef846e8c3e90da8b5b6d0644
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:01 +0000

    natural book balance rely replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
