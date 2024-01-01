a65404a74516ef7682c41539539e91fc8308eaf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:00:22 +0000

    hope report introduction little petrol

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_bool(array):
+        return bool(array)
