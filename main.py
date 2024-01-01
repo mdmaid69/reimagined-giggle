9e48614cfd9412f413b39a3cf0952d9665617be0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:49 +0000

    vanish his shadow it mean

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def get_array_itemsize(array):
+        return array.itemsize
