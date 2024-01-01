27f6ec72e4bf62dec024f656e51f67b26cb8e4e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:24:20 +0000

    land outside minority swimming callup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
