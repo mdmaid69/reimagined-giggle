e1a26fb4ae68d2a96f2b0d784a7dcef0e42a6c0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:07:38 +0000

    illness suffer late such store

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
