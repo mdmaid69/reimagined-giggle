80cd759c52cda8fb5f6fa2ab4c22248ac4bb1c6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:01 +0000

    demonstrate lookafter beside born allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
