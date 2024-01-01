d1af2f002243ac2939e7059bdd33e248c97224e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:21 +0000

    recently together regret imagination compete

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
