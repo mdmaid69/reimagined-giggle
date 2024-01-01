6600f1afdfafaea7db6249f4fe2ca7e65145609f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:25 +0000

    dark badly building difficult related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import os
-print(os.getcwd())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
