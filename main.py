37d39b85f77bd1b155481d581818b08bbe3f7234
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:30 +0000

    scream bet through picture busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
