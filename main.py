5d253be1f1d3dfc18bd8914191c54a48a418b3e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:16 +0000

    very brown flavour far welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
