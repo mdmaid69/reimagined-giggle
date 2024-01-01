59906ec9b1d82d18147e38a0817297e09d535ffd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:16 +0000

    interest front further war workupto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
