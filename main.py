19cba25c9684ae8041c6508c071f45d2b6ca9b55
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:13 +0000

    convenient building crowded condition sentence

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
