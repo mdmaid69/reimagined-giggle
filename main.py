32cc07e19c20e42bd99e7531f68e53c8bf1e9bc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:31 +0000

    scare cut simple test meet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
