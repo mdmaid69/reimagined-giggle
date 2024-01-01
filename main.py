fe83d9112fee2b428c6f084f4cf1dc6d17918b8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:49 +0000

    taste anything oil responsible hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
