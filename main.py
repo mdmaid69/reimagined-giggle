5b232547a70d99de9ef9a6ac3e1de5acf55fee07
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:26:52 +0000

    term second study luck beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
