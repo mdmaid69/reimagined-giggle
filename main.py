eb897d163ed8dd7a8a2b3c1a9e9d70f62f54266e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:39 +0000

    neither matter protection bound waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
