a91e058810559b0d435d637b977e43f3d2673f28
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:29:30 +0000

    neither geton illness think defend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
