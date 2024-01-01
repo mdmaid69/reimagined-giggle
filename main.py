7c0b5250b9f99012168cd2bb74eead995e815476
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:38 +0000

    achieve compare break crisis maximum

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
