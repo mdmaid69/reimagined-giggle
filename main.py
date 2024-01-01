857a12a404e6cee4cafcaf94d6892979121315a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:58 +0000

    abroad culture bend lie strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
