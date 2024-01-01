85d17dbd023e31ae4cf805e350b3f85c139feef4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:04 +0000

    easy this adjective getback thumb

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
