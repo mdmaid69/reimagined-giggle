9c8e114cc40b9bab2b183b789048ce72d264dde3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:39 +0000

    sentence apart to row pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def convert_to_hex(n):
-        return hex(n)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
