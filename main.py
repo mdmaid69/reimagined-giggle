d6eacea22fa1096a308d08fbda2a1e3710ac7afd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:57:14 +0000

    Ii ticket natural perform reply

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+print("Hello, world!")
