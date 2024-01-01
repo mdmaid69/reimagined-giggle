872d2ae38040b5278fdf067be1383cac26461bb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:35 +0000

    wash middle release crime fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def create_priority_queue():
+        return collections.deque()
+import collections
+def create_user_list():
+        return collections.UserList()
