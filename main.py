1640228983d00f9a745a26156121a8c1f535ec59
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:16:28 +0000

    please light private health leave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
