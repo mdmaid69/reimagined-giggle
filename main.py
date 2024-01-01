d9a42d5ee912fa55cb2191b8e07009fadbc0f022
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:59 +0000

    often and joke violence wire

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
