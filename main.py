8f43c7e1b2a9430bc4969aeebf31dd129822bbef
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:21 +0000

    fear yours breakdown minimum boat

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+print(sum(range(10)))
