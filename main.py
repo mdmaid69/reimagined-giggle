9d391039098c04b63a96d2f4c2768c092468ef70
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:17 +0000

    adjective outside secret beat yours

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
