ea6392c7bfce759aad7994413bd81cd8f8d7b786
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:08 +0000

    waste bus centimetre hope tip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
