9f53b4904df165430f60d13bb4241de4aedc9c12
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:33 +0000

    total which get talkoutof turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_work(force, distance):
-        return force * distance
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
