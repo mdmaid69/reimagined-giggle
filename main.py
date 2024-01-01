bc29fa7eccc6ac2cd633e93d86a494897413fd4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:07:06 +0000

    any problem rare believe them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
