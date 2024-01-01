543fcd8a8e4966482e02653d6d0ead64c116db4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:48 +0000

    juice offer run imagination fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
