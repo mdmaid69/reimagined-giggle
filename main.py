b7f76a7e0f93548eaa8011edbd6299016165b972
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:38 +0000

    post name slice phone grow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import numpy as np
+print(np.array([1, 2, 3]))
