aa1be6d0cb412dec8c22339c8bcaab9b54e22caf
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:38 +0000

    cruelty terrible today tap punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
