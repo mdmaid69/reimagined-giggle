1e9ccc4360dc20e29a6c5b6d41b19647800ef462
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:32:24 +0000

    accident production they charge laugh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
