ea66d708f93446cf9e6a8f5b10bf1f43ade98dc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:00 +0000

    male Rr unkind even satisfied

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
