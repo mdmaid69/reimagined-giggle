86d8152d828465d95dc801c09177deee79a38fae
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:16:47 +0000

    practice hang patient risk turn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
