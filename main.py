ee70e1aac9f4788999029c9e4bc365f9ee50f6da
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:29:00 +0000

    roof new godown which repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_array_index(array, item):
+        return array.index(item)
