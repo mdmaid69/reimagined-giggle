bf3df13c779af46d457a16befa144bbcb239b8da
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:19 +0000

    final government future confused understand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
