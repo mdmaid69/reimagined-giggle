dd6893806d9f822774f2d5ed292c8ce19042a36b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:47 +0000

    last project hit home respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
