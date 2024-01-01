a1cf16465533fa7f9ce11760c34fde296653920b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:46:56 +0000

    thin close lower square picture

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
