717d8db5335a8790320dfd4ee0f11b6aaeb2eb32
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:03:45 +0000

    protest programme down whether partner

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+print(math.pi)
+import os
+def get_current_working_directory():
+        return os.getcwd()
