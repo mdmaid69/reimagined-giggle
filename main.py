732efca984626a5ba40989ee001740a74c4bc0c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:18 +0000

    frightened nerve fortunate fact disapprove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
