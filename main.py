22eec5979226224ff8fbcc21bc83b51122710e97
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:17:49 +0000

    statement tonight store ticket event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
