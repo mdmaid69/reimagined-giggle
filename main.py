4d015fb4b0e809a11c18671406d71954535535a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:07:41 +0000

    stare lookback(on) push communication carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_area_circle(r):
+        return 3.14 * r**2
