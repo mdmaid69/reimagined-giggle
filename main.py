6c5e34304167e040db3bba1d8345ff33681f3b72
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:48 +0000

    surprise tidy distance big change

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
