e5b08e9a861c32e087c6ea0e77b09cc54ef2427d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:32 +0000

    over favourite which preparation born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
