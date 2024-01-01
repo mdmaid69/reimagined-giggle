d91e53c156fd2ead50410acebbdefbe45ae5a167
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:23 +0000

    middle OKorokay burst accommodation Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import collections
+def create_user_list():
+        return collections.UserList()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
