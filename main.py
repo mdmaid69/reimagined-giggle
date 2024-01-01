fcedee1fbdaafae5df4141f72ec1fb7549888264
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:33:32 +0000

    border bear sweep song into

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import collections
+def create_priority_queue():
+        return collections.deque()
