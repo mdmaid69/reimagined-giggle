7a930d1d1e114c4c94afefcb3a5ad7b13112018c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:40:32 +0000

    cross learn match mind result

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
