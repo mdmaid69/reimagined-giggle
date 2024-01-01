91399bdd166e99329b10b8a599e7072387870b70
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:46 +0000

    beside load prevent fight woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_permissions(file_name):
+  def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+import collections
+def create_user_string():
+        return collections.UserString()
