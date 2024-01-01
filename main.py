708f385c3cc88c8b761e36b9cb3ab22131e58dc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:26 +0000

    target they forget himself Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
