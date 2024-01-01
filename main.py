fb10b06de07dcffbfcafc1514106c1e411327e8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:26:20 +0000

    question blame behind nowhere maybe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
