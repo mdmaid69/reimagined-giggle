892fa247e294a04aa1d2db02326c20b143b81b77
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:02:33 +0000

    pronounce degree clear ability quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
