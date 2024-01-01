f8bccb5c598fa883597fcc837801b5fc51bf76a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:59 +0000

    project speed glad responsible uponon

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
