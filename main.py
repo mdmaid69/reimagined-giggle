a3acec3da651b49b6e12eba961ac7fd29a0a9aac
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:46 +0000

    blame ignore couple call scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
