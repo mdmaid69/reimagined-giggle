cabd73574e1014e71551d2ad868c82d168d5994a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:08 +0000

    discovery nor valuable damage below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
