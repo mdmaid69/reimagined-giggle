1868f9f6601e4e34ddb9d12bb18718a99caa1b1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:23:59 +0000

    broken contrast bang needle gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
