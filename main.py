151c8209ccbfab030357fabec0a7a7fe97dccdcd
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:12:33 +0000

    task next his home suit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
