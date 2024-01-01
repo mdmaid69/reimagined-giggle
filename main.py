933509e249ccc3878a927128226b6065a85fcadd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:28:18 +0000

    use on experience accommodation similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
