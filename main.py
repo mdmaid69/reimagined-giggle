6c6b3eed79d9bf668241a55156967765ae9b9340
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:49:01 +0000

    scared hit whistle Oo shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
