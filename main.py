d069e5549d0ef41e28540bd437b03e29735e6f93
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:49:31 +0000

    water shine especially open card

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
