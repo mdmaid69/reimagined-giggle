dfe001c92bb56dbaaba5eb0b549f5faebbf65e43
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:33:14 +0000

    part risk circle throw replace

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
