4e0b1d792a06465c9d9f2557bd2fcf60bbd7756c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:35 +0000

    separate 571enough height quit over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
