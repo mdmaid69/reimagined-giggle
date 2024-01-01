184bb801ff7bec5017563d71fb9d86514449681b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:02:43 +0000

    dish fortunate exhibition lay agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
