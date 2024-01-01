bc192e1f51e0a254faffd410841575a496827e23
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:50:12 +0000

    Ff beyond circumstance ticket clear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
