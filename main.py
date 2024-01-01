7ecbe71b6ab8a81b469e627168c48d78eada07e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:16:50 +0000

    demand rare conclusion mix sink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
