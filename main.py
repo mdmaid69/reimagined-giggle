d8ce6360dba1c01732e88ba861feadcd95560c27
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:30 +0000

    journey lean calloff approve part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
