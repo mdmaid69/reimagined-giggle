49fa5ca7db53d6129a5398e19393d8838680273f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:32 +0000

    snow borrow lonely terrible fairly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-print(sys.version)
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
