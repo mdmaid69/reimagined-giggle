1910d0eb7bc7ebc762d17bb8484d53771007c631
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:46 +0000

    difficulty shape bottom call fast

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import array
+def get_array_item(array, i):
+        return array[i]
+def find_min(lst):
+        return min(lst)
