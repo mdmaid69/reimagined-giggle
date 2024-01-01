b544ca32a28564afbfa708fa7fbf957386bd6dfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:04 +0000

    rain act ability belong basically

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
