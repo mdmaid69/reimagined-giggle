276d9485aae2c6fdd289c2f09c1623927b8a3546
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:56 +0000

    pour trust truth runinto turndown

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def calculate_speed(distance, time):
+        return distance / time
