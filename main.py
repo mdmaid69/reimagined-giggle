71f08cb4670754d66f22760617975cb9b2a7a22f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:35:52 +0000

    nervous scrape receive cross crash

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def remove_duplicates(lst):
+        return list(set(lst))
+def sort_list(lst):
+        return sorted(lst)
