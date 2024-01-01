860a7253f1d3ebc81039fc78c8b93e5f16282897
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:02 +0000

    through handle whisper square substance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
