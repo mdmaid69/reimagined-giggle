34e09acebcdf4e4c5d375c81591e00d124044d57
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:08 +0000

    danger breakup excellent film achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
