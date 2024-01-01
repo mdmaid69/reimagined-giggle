4d9d41c1c0a7f9734315ad5828788b05df6e31b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:09 +0000

    goback(to) bye person neat level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
