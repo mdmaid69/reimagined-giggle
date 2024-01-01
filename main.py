50c86a2cf287d45023169662b342dccf40d4ffe3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:07:20 +0000

    peace fire hardly black quit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-  def remove_duplicates(lst):
-        return list(set(lst))
