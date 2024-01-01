adb8ce3689d6ca9e1139e8c968b15a1a318c061e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:14 +0000

    order receive imagination now body

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
