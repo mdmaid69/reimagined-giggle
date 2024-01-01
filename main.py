4bb22c8ef113848c4ab0538e40cdfd45419cf3ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:17 +0000

    uncertain breakin oil understand zero

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
