4cd7ebde45325027b38f4aaef0366ed2facf5477
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:34 +0000

    public play deliver wide interrupt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
