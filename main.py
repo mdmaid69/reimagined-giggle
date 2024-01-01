801128f6ac6f1bc5c2314170a6428443dcb4bf5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:01:06 +0000

    trial short tone heavy pleased

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def count_words(sentence):
-        return len(sentence.split())
+  def calculate_median(arr):
+        return np.median(arr)
