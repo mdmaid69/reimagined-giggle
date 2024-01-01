6153285a4e352b9d77099e2e4f1412c84e9fdc8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:55:23 +0000

    range strange chin high no

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
