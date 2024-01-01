07ef87ba229ef6f210e359e8cf39dd2ae83dc3db
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:22:42 +0000

    control total mistake tax whistle

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
