604145e2a61717a89be977517e4a62bda2cb130b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:08 +0000

    trust thinkof activity pretty loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import sys
+def print_python_version():
+        return sys.version
