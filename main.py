5932292a13779e3178b1b49bbaf4f85fa380f470
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:24 +0000

    model wire determined workout funny

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
