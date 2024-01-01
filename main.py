3bfa70019720ff054dc3ce7be1a29330a1d61516
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:40:17 +0000

    cough splash push bad pure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_set(array):
+        return set(array)
