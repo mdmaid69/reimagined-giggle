9e1227b0a145116d5b91bb9145ed8fdbbcc1e0bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:57 +0000

    definite patience control official turndown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
