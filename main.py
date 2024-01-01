c62f94a7d0653dee2af613a0c72dd71d99585ed0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:02:00 +0000

    poison carry help class scheme

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import platform
+def get_os_info():
+        return platform.uname()
