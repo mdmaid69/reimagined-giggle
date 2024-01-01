1f684779c6c6655b36120cfabf1818a24adce297
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:21:17 +0000

    wait concerned pleased activity refuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
