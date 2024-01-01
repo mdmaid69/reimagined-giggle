9af15b4ae1eb486ae1918afaba1dc66a2c2f9582
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:43 +0000

    trick interruption body speech emergency

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
