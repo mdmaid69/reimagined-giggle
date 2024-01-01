28889aaf13f5758aee953273a8ae2ff6c8d97601
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:49 +0000

    reduce trick accidental life apart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
