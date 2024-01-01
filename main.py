52f28fc45b19587af7138500a17f1ecccd8ad724
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:09 +0000

    burn creature warm talkinto manner

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
