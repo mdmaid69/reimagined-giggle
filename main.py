ce9eb54e68a01bf8094adc0c33b1353a4636b3c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:58:28 +0000

    judge date beneath shock matter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def find_max(lst):
-        return max(lst)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
