458767fc62953583a3b5836d70df19ca804258c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:30 +0000

    someone benefit shadow sting yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_min(numbers):
+        return min(numbers)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
