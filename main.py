7e28994a247f8eaa64b7f06354a4edbc6d32aeaf
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:49:45 +0000

    profit Dd tired build marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
