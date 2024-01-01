2be95f8af7c3ae1821f56fb61480eb800ff6cb9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:24:50 +0000

    comein point about wise list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
