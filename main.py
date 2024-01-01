7e5628c8cf8f7d04662c143b1f2fe6dd86faacd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:04:51 +0000

    several substance bridge circumstance permission

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
