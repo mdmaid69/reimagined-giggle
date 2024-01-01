c8697ad8eefc9e9e564eebff70f6b8ff825ae471
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:50 +0000

    straight sport generally bother invitation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def convert_to_binary(n):
+        return bin(n)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
