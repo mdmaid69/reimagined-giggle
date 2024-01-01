0cf995376a0f6f22d1d57c7fafbfeac0e51e76a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:41:08 +0000

    entry turnout offer charge as

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
