83120a984a568d653f06bfd556c1c1dd6e119929
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:42:10 +0000

    forgive among nowhere walk attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
