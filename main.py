52b37f9b9ff70667f5857ce365ce6bb08209ae17
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:55 +0000

    plan similar content considering talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
