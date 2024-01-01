7d83f878a50556023d8f3df3699cc657b9b8f50a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:20 +0000

    shake professional complete suddenly feed

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
