793dd564b8374d58a619a2b5c64bc29bbf7fc28d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:24 +0000

    visit confused meet mouth professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
