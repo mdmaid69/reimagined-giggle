1e356c05c04f5c29bdd04c49f3973612dff5ef4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:24:26 +0000

    side situation Uu total present

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
