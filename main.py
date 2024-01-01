3e8a665756d283bfc192fe7f500d523b3a258c3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:36 +0000

    laugh sign unhappy suggest bite

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
