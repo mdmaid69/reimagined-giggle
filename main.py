eed3cf74967a5c26edc2ed3989794628b729e4b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:50 +0000

    knowledge wind spin afternoon give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
