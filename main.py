b67ec26fa1c4014be18c790e59090a3697ca2c42
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:38 +0000

    manage bringdown board conclusion confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
