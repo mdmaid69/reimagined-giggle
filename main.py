b8398360e23c2d8f768a371070e3a9d1dd2e2236
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:55 +0000

    sweep interested cover build deal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
