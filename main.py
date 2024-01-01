f0d64f8d45413442ec878b78da5d621c3093d81b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:33 +0000

    keen discovery no think drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def convert_to_binary(n):
+        return bin(n)
