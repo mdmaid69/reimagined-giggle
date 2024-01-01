3544f9da514e77e046595d19ea3cfc0da43488b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:30 +0000

    sudden platform check deaf when

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import getpass
+def get_username():
+        return getpass.getuser()
