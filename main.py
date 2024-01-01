30c78622492b6707acae0839a86ac1e6edab57fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:30:56 +0000

    smell fail solve Oo conversation

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def add_numbers(a, b):
+        return a + b
