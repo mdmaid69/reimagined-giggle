aacfe792e4c994cdf1d3a49e60594313802f1a05
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:32 +0000

    then agree church hardly serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
