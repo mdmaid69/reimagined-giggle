1a08f39c2c75a7fa804d20931f472aaaa2b20586
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:39 +0000

    doubt result cinema base unusual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
