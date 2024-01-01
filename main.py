e9eb9d2ccbe21fd96bb0e715f6f9aabebca7739b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:51:45 +0000

    fine rub whichever callback dinner

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
