94957a0cfffe86a20297203d77b853bf3c917149
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:45:16 +0000

    earn consider joke stupid movement

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
