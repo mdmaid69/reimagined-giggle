f2b5703e39d5ec3f3e6711c1ccaa61e006989b15
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:06 +0000

    tidy remove pour direct takeaway

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
