4b649e5876a8c5e2eb2d35760c567a132fb9badd
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:37:33 +0000

    name recent smart grow private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
