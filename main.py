73a4281efd24dffcf19f095863270efd158dcdbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:47:21 +0000

    early move intend except snow

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
