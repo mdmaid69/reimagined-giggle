69724bc148ed20046d1bee1383b63512ec40ab37
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:22 +0000

    hers valuable scream experience funny

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
