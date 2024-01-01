c3570d663639164bad4f465cbd2846a5c0c14fc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:42:30 +0000

    steer impressive chance appeal separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
