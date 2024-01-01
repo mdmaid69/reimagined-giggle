1e0c3f34d9d1bc78b28b2e84cb78601dc1b4dfb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:12 +0000

    battery on please sting reduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
