ba08e83b04d35ff6aead617264907d7212bd9cb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:51 +0000

    genuine several during workout experience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
