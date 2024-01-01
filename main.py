f0bd2643e738d490442c4a657059059065f3d82c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:51 +0000

    promise nowhere bar appearance blind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import datetime
-print(datetime.datetime.now())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+print("Hello, world!")
