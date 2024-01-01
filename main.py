c133914c6e7bc77f66151515fb8eeb1b62995721
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:45 +0000

    back speak oil can their

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import sys
+def print_python_version():
+        print(sys.version)
