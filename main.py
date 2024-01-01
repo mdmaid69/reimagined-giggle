fbb197f9aca910d4500064f74514ca02dc53d5e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:30:49 +0000

    favour her taste whisper turnaway

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
