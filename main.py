9664fa72e8dc04e4c1a40e47e2031df6a2baf3a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:05 +0000

    pollution alone sure rest respect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
