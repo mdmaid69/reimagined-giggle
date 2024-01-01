5eca41c882fe287f6eb11eae961d061c82cc817b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:39 +0000

    unusual perform herself worth soil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import platform
-def get_os_info():
-        return platform.uname()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
