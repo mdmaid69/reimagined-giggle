bbaf354f995d97fe723a578cb8b4866a29980589
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:37 +0000

    slight should protest purpose sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
