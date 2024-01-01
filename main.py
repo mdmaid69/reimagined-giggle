c2d7af7f9ae2b2a2a66bb1e45eb131ec279e8342
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:24 +0000

    inside fasten offend accommodation violent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+def convert_to_binary(n):
+        return bin(n)
