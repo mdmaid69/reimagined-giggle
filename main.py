4f161016a41f823dc85ac7866eb191c3a7505f94
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:43:32 +0000

    thirsty majority else murder division

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+name = "Python"
+print("Hello,", name)
