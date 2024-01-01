82dc840db61101a385c52af6cd120eadb734cd15
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:39 +0000

    equal fit to typical command

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
