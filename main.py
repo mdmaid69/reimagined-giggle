dbb85c46fb126a7bfc761cabe789aec50934720f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:10:07 +0000

    popular break window different murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
