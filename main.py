3eb272ec340985b05400b27d29ce68887255cf1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:01:52 +0000

    once tall steer work owe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
