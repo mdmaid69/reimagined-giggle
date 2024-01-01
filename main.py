0000f9eeaf3d7b031f8d97b75366835a1c731dcf
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:21:41 +0000

    rest remember branch question no

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
