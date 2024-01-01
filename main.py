28fe898f11ff79ab3fd767580fdc7be2635fad5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:44:29 +0000

    hair unit kind basis birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
