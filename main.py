cb9f684b93a21cbc32e9259df8a2cd5dc6782185
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:16 +0000

    condition acceptable together popular properly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
