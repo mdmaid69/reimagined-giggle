872c711c5b4bd146a668b0d7b93150785eb7e709
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:10:08 +0000

    shrink polite mean proper production

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
