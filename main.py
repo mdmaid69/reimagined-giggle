4946a5c1dad05f8692ff0786b0adf517cc4726d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:32 +0000

    tooth behind pump later along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
