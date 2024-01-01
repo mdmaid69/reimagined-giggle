825cea835800e0c8b5eae69903f3ebfdd2b1a2e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:43:50 +0000

    wake fortunate mess radio fortunate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+for i in range(5):
+        print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
