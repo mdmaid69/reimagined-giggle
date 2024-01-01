85af49811764c3df623431062b3e56eb41a73e97
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:31 +0000

    admit often stranger several lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
