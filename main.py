b4acdab2c5b9d47260820befe06a7ad344e91288
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:26:34 +0000

    evidence thank Jj inquire difficulty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
