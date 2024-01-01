a0b83240c0c40b707d561cdb060c012637682879
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:43 +0000

    deal such pen likely workout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
