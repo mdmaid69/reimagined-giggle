01f6b44fd9be8048b66ec9b2f526d9c56049e007
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:29:15 +0000

    programme sweep do ever hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import datetime
+print(datetime.datetime.now())
