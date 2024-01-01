c4ecc936a2bcf92300e57ba10bca56b3abd0d02b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:24:59 +0000

    freeze evening worth intelligent permission

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
