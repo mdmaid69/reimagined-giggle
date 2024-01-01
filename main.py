f0faf621562fbc560c8a3d5a958524719474ad98
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:22 +0000

    recommend skirt address pronounce squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
