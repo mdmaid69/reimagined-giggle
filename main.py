780135fba8ab22a755126bc03a67719830d75275
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:59 +0000

    plan scale sister responsible game

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_work(force, distance):
+        return force * distance
