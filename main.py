b1a2736cf83f9248e1fbd3a99097d76b9075e47c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:22:40 +0000

    best loose enthusiastic sensible error

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
