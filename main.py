97151d5b084f88dcf398a2bf40e17b9ca41c4f7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:28:12 +0000

    half black pity mass hand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
