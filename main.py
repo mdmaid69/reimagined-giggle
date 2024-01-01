7c39d562ef61d43a07eb6f7d8be6a26695c4b90e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:18:09 +0000

    comment soft miss Ww invite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
