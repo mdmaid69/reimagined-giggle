86ccf8488c506122789ce2ba0ed9cdf6b4331bc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:52 +0000

    number noisy owe culture completely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+i = 0
+while i < 5:
+        print(i)
+        i += 1
