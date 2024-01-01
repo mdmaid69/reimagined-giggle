03d367115abd561af7077e96e85777acff7fd1d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:49:28 +0000

    noone funny eager expression stiff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
