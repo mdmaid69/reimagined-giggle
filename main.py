cf555e51196dcc21a143e0b0244e9463048d34dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:59 +0000

    length home sweep list calloff

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
