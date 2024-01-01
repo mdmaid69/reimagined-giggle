f2f6703f2fb6756a63bd0204e887d832681f9948
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:07:31 +0000

    party trade stamp dream rule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
