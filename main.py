3322951e41ef72cd44c711aa86d68d4bf2ecaf3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:50:43 +0000

    effect improve least effort damage

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_pressure(force, area):
+        return force / area
