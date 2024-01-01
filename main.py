f008cbbd95724e7788340b1cec977221096714fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:23 +0000

    woman regular bear thinkthrough try

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
