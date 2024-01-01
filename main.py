c647bda3133b8d9bedfc7cbe29eddfad02a7f985
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:47 +0000

    chance near deep hungry unless

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
