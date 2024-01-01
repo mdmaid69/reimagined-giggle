ab24f1591412735575cd20adb0a691d23f9e4cc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:55 +0000

    poison chase recognize may message

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
