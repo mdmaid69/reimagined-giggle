9d6b74d5f2d2ebf43552bccb31141105bf7ea665
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:36 +0000

    block giveout us search study

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
