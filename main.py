c4e94cbb99b249053f0a4e142d007a5410585294
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:41:14 +0000

    upset ear true block Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
