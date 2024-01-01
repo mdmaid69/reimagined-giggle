1f24ca46cd6be2bd4fa37006af6bd25e56778bd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:06 +0000

    talkinto chin unite pronounce congratulate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
