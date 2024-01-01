5fe351b112c6b1ca31903ed7dad5b24d15827bb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:16 +0000

    speak anyone often sit official

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  def reverse_list(lst):
+        return lst[::-1]
