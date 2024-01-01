c99802c00d82aa943fd6138782c8b2baf6c7baa8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:42:48 +0000

    game winter gooff water us

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def reverse_array(array):
+        array.reverse()
+print(sum(range(10)))
