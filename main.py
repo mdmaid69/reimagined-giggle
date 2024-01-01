81470fbba2348aaf3ed9f382f3a1afd17bca9753
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:14:32 +0000

    nowhere tune question minority load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
