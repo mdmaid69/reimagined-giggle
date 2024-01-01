dae76e653800b08547d937c33782c1e1fd79d1b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:34 +0000

    either defend style level consist

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def cube_number(x):
-        return x**3
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
