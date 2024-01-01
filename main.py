1ffcb6df24638e5ed69204d889755c86f1228699
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:30 +0000

    name wealth apply wild shop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
