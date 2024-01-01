a0819078dd210ba18d9582ff631dd38920869213
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:05:12 +0000

    Yy company goon memory cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
