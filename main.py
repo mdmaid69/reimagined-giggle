39ed4fe84833b8b7fcda71d125ac5853586f1a87
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:55:34 +0000

    then couple tend classroom tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
