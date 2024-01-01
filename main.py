3ac8f896534221a09debc1dadfc296f433f1e7e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:36 +0000

    whenever scream bottom tonight pain

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
