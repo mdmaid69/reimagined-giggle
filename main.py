871bc87fbbd2e85f974c032e1734409c891701a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:28:51 +0000

    science drop defend tough busy

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+print(sum(range(10)))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
