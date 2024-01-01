fe00535c0474aa1317c13ea9ec3fdd92346e1cd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:21 +0000

    extremely gift fall there forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
