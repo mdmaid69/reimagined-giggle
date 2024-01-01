4cca963c29f03da1e626f36ea861312129354ea5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:44:38 +0000

    city alive wrap statement blue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
