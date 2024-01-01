b29078c1aa227fcb3bca769ae53ee71c5614fe61
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:26 +0000

    increase science food morning crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
