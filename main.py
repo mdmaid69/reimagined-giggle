a21e2b6afa93a4be7701fa74a37c1b64c4a0aa4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:45:01 +0000

    brush not mark division entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
