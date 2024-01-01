804c9bb95662ca264c8793b0aaad6bfe4bae2d69
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:34 +0000

    traffic fork bread yourself negative

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
