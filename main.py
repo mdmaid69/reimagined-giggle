5cceeccdf08bcaadf4eeaef20a75bd008235dc21
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:48 +0000

    issue pause later shade worth

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def is_palindrome(s):
+        return s == s[::-1]
+  def square_number(x):
+        return x**2
