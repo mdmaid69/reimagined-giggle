ce3414f2f5dece92ad615bd93a5406dab12dc4fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:46 +0000

    push dry basically there nowhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
