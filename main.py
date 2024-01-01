be9c32415b149888118f414e96956d7929c4cf09
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:48:31 +0000

    proper set treat just bye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
