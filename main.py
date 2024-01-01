8cf3db700273c3a626eafc6e22155d125749d2d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:32:09 +0000

    cross likely burn peace coat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
