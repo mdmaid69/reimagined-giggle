c4fe27b1aaef11dd48080b86ebee596edf2d9442
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:42 +0000

    rest please statement example see

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def delete_file(file_name):
+        os.remove(file_name)
+import time
+def get_current_time():
+        return time.time()
