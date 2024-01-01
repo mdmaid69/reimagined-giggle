a67df8c617ed8cad01bab17e267f157a92bca881
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:55:28 +0000

    list hate repair undo decision

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+print(math.pi)
