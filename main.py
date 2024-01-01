ec26cb7aa05b0083862b77717414fcf938e0fc45
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:12:24 +0000

    OKorokay war whoever but scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
