21c3ea0d025e7d685a60cd06779cd736151c7e27
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:49 +0000

    sight Itself hollow serve interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
