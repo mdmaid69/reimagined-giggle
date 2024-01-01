27e24cb259d65ddf3e49dd1e83bbe8cd296b2dd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:55 +0000

    add joke keen plate like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
