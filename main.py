e89f50b263572a9c591226f01466308761a0b796
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:44 +0000

    team lonely force both rub

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def count_words(sentence):
+        return len(sentence.split())
