2d3ecae19f17a03bbcc7a681f9fba6325485315a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:07 +0000

    prefer acceptable arrival direct discussion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
