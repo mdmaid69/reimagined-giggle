3ecd8ae42b0a48c5cdb24e4a4e42cc163374fb63
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:58:03 +0000

    central practise embarrassing admit consider

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  def subtract_numbers(x, y):
-        return x - y
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
