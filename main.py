1747c06c6a8833010532be84920996e24d85b821
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:08:59 +0000

    read breakfast bound owe scale

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
