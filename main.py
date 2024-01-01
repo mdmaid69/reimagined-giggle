f9c89d223f6db143d1df08560760d8ca22885ba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:33:03 +0000

    play than announce fall closed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
