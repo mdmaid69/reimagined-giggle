a1081058d131b357863ee9580694fa3efe47b4d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:08 +0000

    correction answer acceptable bunch forget

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
