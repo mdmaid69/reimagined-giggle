e54a3bfd3b681071f7680fa15db65043a6fd4add
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:33:40 +0000

    take label repeat Ii suck

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
