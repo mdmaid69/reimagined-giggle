4a6843ad6b06be60f98d826d6455f19e23633a4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:41:37 +0000

    godown mother software across round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
