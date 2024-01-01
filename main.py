eb765b97ddfd44ac4fa924364f81a3e121386564
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:18:44 +0000

    exactly usually stick word embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def convert_to_binary(n):
+        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
