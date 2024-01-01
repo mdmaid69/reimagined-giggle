c2afbebd2f7c00769d04214a43adfe27f40770c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:10 +0000

    pass suggestion regard better consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
