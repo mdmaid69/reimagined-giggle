96fa46896ff9b3d0a17945e81d01e045d8300f23
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:05:10 +0000

    compete no calm card battery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import os
+print(os.getcwd())
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
