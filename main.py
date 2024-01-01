65f1ed495f59c33750569170cea68bec0f0db97b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:29:51 +0000

    introduction makeup give lunch Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def multiply_numbers(x, y):
-        return x * y
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_set(array):
+        return set(array)
