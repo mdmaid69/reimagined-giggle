4325ebe8949f1939fc3fc51d9912b726a3cfec2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:01 +0000

    thin cum sail petrol send

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
