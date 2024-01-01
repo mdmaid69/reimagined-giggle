b64f80814c83bf6184c9de8b45f5116765c6974a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:22:32 +0000

    everybody favourite because temperature sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
