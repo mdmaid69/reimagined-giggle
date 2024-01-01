b3fed1b08e6ab66b1d7368e9c7c0f248d999c3de
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:37 +0000

    right suck pot sting alive

--- main.py
+++ main.py
@@ -1,3 +1,11 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
