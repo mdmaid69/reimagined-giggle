d8cd9e5ceea4203be9d48ec5ff16bb41c3ce3d3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:54 +0000

    job fit step chin point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
