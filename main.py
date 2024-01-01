63ed0f1b56574e2b81edc8b559c96cf5a97d5f35
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:38 +0000

    phone badly strike up careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_base_name(path):
+        return os.path.basename(path)
