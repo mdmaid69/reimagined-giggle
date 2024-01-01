b90c9839350db57236df2bd68229587d482adfcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:51 +0000

    goon live until makeinto stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
