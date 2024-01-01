ea36a91519102d9903b3fe4bbc517623c37eea80
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:41 +0000

    greeting variety unite matter home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_time_since_epoch():
+        return time.time()
