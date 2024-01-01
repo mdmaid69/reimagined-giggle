f38a39f8221fdb22dacf2c2ead000e5470ad7a35
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:12 +0000

    naturally eager imagination survive drive

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-print([x**2 for x in range(10)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
