988a80c38756db8ece64c0d7de06e56a0886394d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:33:17 +0000

    impressive find wind worried interruption

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-print("Hello, world!")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
