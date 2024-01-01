40e038a265725f1484febf731c89ddeef997335f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:21 +0000

    class if issue careful platform

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
