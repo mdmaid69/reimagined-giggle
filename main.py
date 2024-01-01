37ebb4cfcd2c99e37df50b28f98af1fd3304ba88
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:34 +0000

    there smoke give deliberate needle

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_power(work, time):
-        return work / time
-from collections import Counter
-print(Counter("hello world"))
+def calculate_area_rectangle(l, w):
+        return l * w
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
