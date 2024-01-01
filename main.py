ed5adcace2fa3baaf77e712977add9261d354928
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:11 +0000

    seriously form normal unpleasant future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_length(array):
-        return len(array)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def count_words(sentence):
+        return len(sentence.split())
