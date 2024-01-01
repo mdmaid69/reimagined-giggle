b2d7faab5b2234ce0c320ae2ee9a011eff5c582f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:10:14 +0000

    direct proof beneath foot room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_min(lst):
-        return min(lst)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
