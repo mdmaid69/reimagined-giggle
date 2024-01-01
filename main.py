3d3d7ef88177f331dba5a3cae96e0e7753516f75
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:46:40 +0000

    she best keepup pretend comment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
