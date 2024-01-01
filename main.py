2093f2277ebe41019218b2d8f694c135ae31e6d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:36:18 +0000

    work exit card holdout guide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import sys
+def exit_program():
+        sys.exit()
