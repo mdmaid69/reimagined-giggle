82b125ac1da7571776e1048a59866905f1569fac
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:15:15 +0000

    sleep light go recovery careful

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import tensorflow as tf
-print(tf.__version__)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
