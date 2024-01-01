d6c28e75d8da0e56277876e1147f4f2f92b1f919
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:07:08 +0000

    laugh born Oo but variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_union(list1, list2):
+        return set(list1) | set(list2)
