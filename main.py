faebad9f45309f70a06c756f4548d19450d63106
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:28 +0000

    dead still me crime country

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
