6f9012905ae9f39c6bb6187b0ec49cf53c71fc64
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:29 +0000

    mother arrangement represent hang reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def convert_array_to_list(array):
+        return array.tolist()
