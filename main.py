1c7ebb1bf828cf58626b4f6d7b60b405f1037765
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:25:21 +0000

    worth quarter rest hide dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
