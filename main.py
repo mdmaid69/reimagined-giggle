1a229da3b18a5f5ff1a3bb5751ed4e4c2b77329a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:47 +0000

    wave unit month common narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
