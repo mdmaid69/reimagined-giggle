78f9f5911ef677123b8796f6a8c5e6a8b5d9f67c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:49:46 +0000

    Hh right manage sentence unlikely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
