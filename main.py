57629bc628acd7965c5eae9dd88af50ca6efbeea
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:15 +0000

    considering serious sad same house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
