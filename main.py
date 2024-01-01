33e882d4676e4ca7457766358af6748936298f80
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:48 +0000

    joke opportunity slightly lunch tend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_max(lst):
+        return max(lst)
