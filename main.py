d66623a42b28233222bd31d9e08fc3e6818c0fa8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:54 +0000

    raw without church runinto break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_power(work, time):
+        return work / time
