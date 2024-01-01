9c1c7c0a6b22c90548073c33dcd2ee7b796dfccc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:43:35 +0000

    ilovedicks matter wealth funny window

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
