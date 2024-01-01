c15896dfe38b5d0f9836741cf6d37d83be79113a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:35:59 +0000

    Vv spoil giveout straight minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def square_number(x):
+        return x**2
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
