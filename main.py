263220b92eb03e5f8e60ca8087fba662a2503042
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:55:26 +0000

    inside offend upset always slight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-import re
-print(re.match("h.*o", "hello world"))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
