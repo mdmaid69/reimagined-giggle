99e4699c5f713f6e15c2637e2303fc5f305a55d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:14 +0000

    everything later show minimum they

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
