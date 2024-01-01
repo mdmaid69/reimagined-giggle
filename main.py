b77573e72b74158c70702c722af69b7d2ffd5037
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:53 +0000

    deal load female goon example

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
