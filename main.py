5c77a88ffc393575f969cf6d5c6f151350912efd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:11 +0000

    sore wheel bunch everything talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
