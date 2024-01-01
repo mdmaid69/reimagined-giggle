491029a1a8e183e83eeb7a36493744f4f22595f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:46:43 +0000

    punish what lend colour float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
