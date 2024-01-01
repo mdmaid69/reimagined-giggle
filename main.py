26398a171945922ae23cf47f685a73d4c7b4db9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:54 +0000

    next judge comment branch tip

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
