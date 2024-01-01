498c9854676c73dfcc8ccc04a479a8e4843d8663
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:31 +0000

    worth straight tidy frightened owe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
