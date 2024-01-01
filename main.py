633cb0294e22fed2ea0f67820388ca1fbb722017
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:56 +0000

    head unhappy smile noise ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
