abc2bc79c7586a785da68860eff3d652fa8cd3de
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:18:19 +0000

    expensive blame wonder region hello

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
