6cc7c9d0ad5fdcdaa54e55f5607a08ec8fc813e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:24:53 +0000

    better belong tip silent warning

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_unique_words(sentence):
+        return set(sentence.split())
