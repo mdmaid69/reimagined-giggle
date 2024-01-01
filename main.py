2d1b4ad65d887c2ed0e51f772f1bc9cefef01383
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:33 +0000

    crime laugh deep do desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
