919b26e58a17aa64c211eb0b7561df2b34658875
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:24 +0000

    available wing copy accident 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-print(sys.version)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_min(lst):
+        return min(lst)
