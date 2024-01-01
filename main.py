f92db9a93cc15da531a70c5cc4cd706f6528885c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:40 +0000

    mother drop prevent brown threaten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
