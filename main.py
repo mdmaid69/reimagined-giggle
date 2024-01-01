bfbcd90742a89a74614a4610a1752b4088af1eff
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:37 +0000

    expensive low and talkinto watch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
