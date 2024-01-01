17b013e3d7f63e81668b67ab256985ae0f26f615
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:10 +0000

    catch tell enthusiastic operation tone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
