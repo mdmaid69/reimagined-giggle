e889b2796af30928f5e5139d0f58962f4b656461
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:04 +0000

    meet sight my division along

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-print(math.pi)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
