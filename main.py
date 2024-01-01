55656aa83f1872fd39d76a364d4fe96eeb6eb059
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:41:52 +0000

    my dry point through guilty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
