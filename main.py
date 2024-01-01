86b3611fe999d429453ec549a333927b36743285
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:59 +0000

    company comeround clear kick pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
