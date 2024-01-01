7ba8bc06fb9648604a2dc95313fc4dd7384281fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:54 +0000

    pleasure stretch shock offer belong

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+print(math.pi)
