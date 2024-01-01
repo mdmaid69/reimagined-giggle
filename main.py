c5468d52e8fba78b8594027df74a63423bf0bb93
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:14 +0000

    club strict whistle similar worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def set_array_item(array, i, item):
+        array[i] = item
