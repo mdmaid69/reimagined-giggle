be7866c808d7d44e200da4ee62713aea575348cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:26:52 +0000

    edge vanish instruction move peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import collections
+def create_queue():
+        return collections.deque()
