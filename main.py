993bfb89cfdd939f85f6526188de7479130a136d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:08:18 +0000

    pleased where some weigh 573entertainment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
