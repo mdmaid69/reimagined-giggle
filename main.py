ef4e048b41c00e21785c11c42298c588e0d394df
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:44:40 +0000

    seat and sting me consider

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def split_path(path):
+        return os.path.split(path)
