427b70cab3e56d47a680cb13600b6b09cce482e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:00 +0000

    fight spoil gas cheap turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
