4b1ea053d0a460c502b7b7145e399a2226dc8132
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:27 +0000

    extra many anywayanyhow badly fasten

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
