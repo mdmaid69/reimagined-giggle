479702d2df1027055a64efa094d04584f4865582
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:22 +0000

    bar bridge face course thumb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
