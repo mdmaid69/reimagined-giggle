1cd52a277462ac7d23f456c13dc49f3f2984f303
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:54:36 +0000

    protection defeat wide test takeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
