0eb65b0f5d1bb9709a66f1177d05a114805deb98
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:48:25 +0000

    border shade education flood rubbish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+for i in range(5):
+        print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
