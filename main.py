383d291f907db21beeb8ae719f6acfb4f528f42b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:35 +0000

    warning callup basis comeup food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def find_min(numbers):
-        return min(numbers)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
