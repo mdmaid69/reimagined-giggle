d2b7eaea477fd257fbe3f0259fba687ff105a9ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:37:55 +0000

    most curious towards pull influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def add_numbers(x, y):
+        return x + y
