cd63f4c8d6260df065142c13c0d068b0a68128b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:23 +0000

    criticize transport serve shoot trick

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def remove_duplicates(lst):
+        return list(set(lst))
