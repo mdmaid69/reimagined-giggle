f28e1a316605e4c655d34f6fff2d236e54be9772
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:59:32 +0000

    Ww point neat near negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_priority_queue():
+        return collections.deque()
