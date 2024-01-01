be2ae95a8e878569fb2759f89dc27321eb4cc49e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:11 +0000

    degree rob move bath neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
