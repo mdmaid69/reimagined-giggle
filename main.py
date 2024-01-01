6457aa6197a34149557f4ceb068918d1a4441e18
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:41 +0000

    plenty want autumn shape sleep

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+import platform
+def get_os_info():
+        return platform.uname()
