c832bdcf98711f50dd83b13bd5671765e2b3c2be
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:17 +0000

    drawing thin style foreign argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
