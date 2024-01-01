e1705091c0e9f88bef525613b6cb61447d007184
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:57:14 +0000

    from clean team attitude lift

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_density(mass, volume):
+        return mass / volume
