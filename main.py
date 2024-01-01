a18b38c5032aa126074a27387569e95d5d184815
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:56:30 +0000

    brain across world concerning lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_list(array):
+        return array.tolist()
