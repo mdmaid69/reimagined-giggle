25afc9a86568b668f4bb48999902a12f09261595
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:01:57 +0000

    anywhere call ahead under communicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_list(array):
+        return list(array)
