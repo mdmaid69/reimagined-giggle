35dd18ac530bc35491df3e5554c0cde7176d1e1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:48:32 +0000

    lookafter ball expensive model nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
