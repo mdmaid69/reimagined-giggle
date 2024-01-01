82994b966b696d3210116fede2bfb0ae2289c9e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:52:48 +0000

    dear squeeze report limit before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
