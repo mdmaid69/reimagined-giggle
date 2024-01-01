a264770e5f8594890cbde08f8d3ac4101282304e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:07 +0000

    hair habit earth demonstrate trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
