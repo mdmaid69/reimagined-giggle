e19791a309c5068f44c1351ca0076c1ad082023c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:03:31 +0000

    violent yet excuse shade temperature

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
