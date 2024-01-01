d70441db1b7a48e0909d5e5972d44442fa2d43a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:03:06 +0000

    pressure record excuse yours divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
