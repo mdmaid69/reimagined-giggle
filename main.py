9f213ecc3e46dcc2fc965e89be54659f43c0981a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:16:59 +0000

    image on mark underneath disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
