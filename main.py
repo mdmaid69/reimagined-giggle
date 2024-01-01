8de6d459e665e1c43d48c075b7307342b99b05cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:02 +0000

    accommodation loud boring owe much

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
