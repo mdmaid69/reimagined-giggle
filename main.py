3cb93996d9f7e1a13b99be2bb6e333cd077e5fbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:41:31 +0000

    defend joke however exhibition cut

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
