018bfd8bbd54a7fc1706b451a390ba071bbbb5ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:28 +0000

    grade whom lose order pupil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import random
-print(random.randint(0, 100))
+def calculate_acceleration(speed, time):
+        return speed / time
+def cube_number(x):
+        return x**3
