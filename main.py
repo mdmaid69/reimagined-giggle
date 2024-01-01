f308f9e0a685563143c314caba05867ab4869788
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:38 +0000

    plate worth sort shape under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
