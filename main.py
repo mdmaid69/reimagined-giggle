0cf94ecac0677a58396c3f9f834774371e213585
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:24:11 +0000

    whisper drop surprising chin exciting

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
