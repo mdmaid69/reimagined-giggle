b591d51dc9ffc807cf1b1bce059566b13f23a8e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:40 +0000

    sure wander properly full stop

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def reverse_string(s):
+        return s[::-1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
