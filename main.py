d766a5e3e92b0e60b20ad35236a55b5d45b53fdf
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:04 +0000

    curious wind action clear conclusion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
