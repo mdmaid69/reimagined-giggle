7dd9dfb4bc98ea37809e9a254fda9ea985830a7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:01 +0000

    something honest disagree furniture stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
