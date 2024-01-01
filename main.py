bb87e618e78f98db704f5ef5cf2cced464866b7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:13:21 +0000

    concern kind furniture house justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
