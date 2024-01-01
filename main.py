3f97632f87f69b64820d8223687c4de3de52f734
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:47:02 +0000

    bread uncomfortable unite pretty absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
