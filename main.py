d37f5350310443bf568e2ecffa41900211d2115a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:53:06 +0000

    dry big again old way

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
