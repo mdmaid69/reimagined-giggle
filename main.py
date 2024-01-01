e8887d2a5fe1d4199e29bcfc367a7ae732012fe2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:49:12 +0000

    natural brother content pile heat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
