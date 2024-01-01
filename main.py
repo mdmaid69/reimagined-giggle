f2e2e6e9ee2d25b1c5928d5d22f6ea27cac71651
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:42 +0000

    correct perfect of scale drawing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
