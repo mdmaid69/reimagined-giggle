dfce5be95a955103525239be1504a3553ad7b9c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:51 +0000

    vote make spring emphasize print

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
