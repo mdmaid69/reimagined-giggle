c43f86dd14b6d3eee5dd91952d1a77bd1eba1d76
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:50 +0000

    during cost search near behave

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
