246292827fb017697379d108b6140fa803584fad
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:38 +0000

    cheque awake modern getback giveout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
