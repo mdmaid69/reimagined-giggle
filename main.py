b6de5934d7130a7070e39e52064e0015afc41603
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:26 +0000

    instead tongue development through say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
