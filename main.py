65c8cf69855f33f96801d90ba08d8239ab49486b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:58:53 +0000

    equipment understand press fork breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+print(sys.version)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
