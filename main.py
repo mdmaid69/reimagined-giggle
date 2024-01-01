eb0eafa373f9b680e439e3c1c9871236370804fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:28 +0000

    watch open plenty top century

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
