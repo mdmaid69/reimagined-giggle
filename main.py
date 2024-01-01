1b5ab3ad2a26ab6d787beb08a7a7c8c3f4c5d6d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:56 +0000

    comment under sight store touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-name = "Python"
-print("Hello,", name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_itemsize(array):
+        return array.itemsize
