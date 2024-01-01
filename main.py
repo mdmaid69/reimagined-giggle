2877ee993f195666ace9848c3543bacc3ac7db43
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:58:50 +0000

    even top fast food quickly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
