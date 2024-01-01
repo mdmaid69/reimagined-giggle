6c658e42cc6b859f7e7c3796c914ca99be6b1e47
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:08:43 +0000

    thing block indeed which language

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def reverse_list(lst):
+        return lst[::-1]
