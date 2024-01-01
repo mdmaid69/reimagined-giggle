2b277ec272fb965cd8adcfaf1bc66f6ea00ac2e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:40:13 +0000

    travel properly exploration depend my

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
