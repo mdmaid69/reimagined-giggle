0bca184a652f34b4c1ecba56d9dd2d4d892e7101
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:39 +0000

    charge to calmly godown thin

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_set(array):
+        return set(array)
