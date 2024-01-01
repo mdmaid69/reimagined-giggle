e10e67e1f7233d29ba5fa6e7062ff13b7fdcbe11
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:12:36 +0000

    variety safe complain wash brave

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
