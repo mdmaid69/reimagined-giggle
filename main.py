aa6be86080c9ad039e8de27427fdc4d37f611719
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:30:21 +0000

    winter Bb possibly spring child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
