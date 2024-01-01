2aba78dc26e10034aaa7d91dd2daca4db284eb45
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:46:29 +0000

    plate off payment such print

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def exit_program():
+        sys.exit()
