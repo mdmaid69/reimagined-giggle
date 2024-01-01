364418e0a597a71e237909f0a2a65636e2897fe0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:58 +0000

    Pp drop shelter uncertain birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def square_number(x):
+        return x**2
 import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
