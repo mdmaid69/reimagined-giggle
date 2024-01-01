f89f8df251966f29db5730b57afc070f760c12cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:23:59 +0000

    hole afraid smile only like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
