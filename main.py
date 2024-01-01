a0f65a5a7a072873ef525175bed128f2442c9a27
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:09 +0000

    building bet port smile bright

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
