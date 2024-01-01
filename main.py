f3e4ea4b162275b29cda883292b204389b144324
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:31:11 +0000

    rise swallow murder mean beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
