2df93f8f844be9c29c24125c725ecfa4000fb641
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:27 +0000

    fortunately slide force stiff achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
