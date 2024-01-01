013d571da05393c250b293dc62a2b4d05269f7fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:30 +0000

    keen test bush defend gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
