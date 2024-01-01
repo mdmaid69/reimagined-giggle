02e76adc9aacc33198831a20d2a021100cc8db10
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:32:36 +0000

    share area scare limit build

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
