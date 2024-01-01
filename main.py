db73f07b5e0adad689f2ff3620f92a79e145e12c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:08 +0000

    bar treatment reaction offend dead

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  def convert_to_binary(n):
-        return bin(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
