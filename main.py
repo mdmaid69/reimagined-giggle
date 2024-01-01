d7ea9a0b48ce0a4adee5b8dc76d063cba617f1a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:12:16 +0000

    safety operate forward house last

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
