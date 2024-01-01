6364f068f0e2e088051dd840d98adcef35a57cee
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:23 +0000

    shallow dry welcome team travel

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def square_number(x):
-        return x**2
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
