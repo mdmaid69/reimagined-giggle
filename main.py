1e95dfc5cc631d2a82a00122f9fc70a7eab3d811
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:22 +0000

    pipe odd push friendly smart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
