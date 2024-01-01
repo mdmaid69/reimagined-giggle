bf43f25505d89a630d8d3b633292ab5214e25eeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:10:53 +0000

    sign telephone tune rather machine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
