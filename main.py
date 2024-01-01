0db04615f2d94dedc8077bf7b782232ca09a3a6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:17:01 +0000

    whisper breakin themselves estimate comeback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(10): print(i)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
