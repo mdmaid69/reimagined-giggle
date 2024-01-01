63f724645e98e6b5d1186d2f5d68b9953192ef8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:33:57 +0000

    tap principle purpose several purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
