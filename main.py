1743e3d19db55cd1403fe5bab745f391d6cca066
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:56:04 +0000

    substance continue petrol degree evidence

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import sys
+  def get_python_version():
+        return sys.version
