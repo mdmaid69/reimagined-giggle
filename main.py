f6c5214f8c95955b023cc458e1d9c24b1fc54bf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:29 +0000

    appearance contact money row earn

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_min(lst):
+        return min(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
