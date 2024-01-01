86c1a67ca694a1059f54699e33e4d8f631bcf5e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:44 +0000

    paper urgent favourite insist measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
