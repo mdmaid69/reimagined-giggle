375cab1fc44d99e0d82c24a78bd168b9f0315082
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:04 +0000

    trip swallow ground simple often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def convert_to_binary(n):
-        return bin(n)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
