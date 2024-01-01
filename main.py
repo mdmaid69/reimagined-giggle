f65ee0ae3fb16f7143823aa45794827addca2493
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:37:50 +0000

    wash lead land store hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
