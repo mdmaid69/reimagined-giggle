990b3b548dbabb8b4a525ed427d884c37acb0c8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:00:20 +0000

    allow fortunately comein bill wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
