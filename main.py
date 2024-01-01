a811ab97109d55fd29d6ce42a5fe750d44f2e3d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:19:27 +0000

    open rescue eager trade proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
