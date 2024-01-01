cd3dc2866697738fee66bb16611365df746916d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:00:51 +0000

    along breakdown twist hurt address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def remove_duplicates(lst):
+        return list(set(lst))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
