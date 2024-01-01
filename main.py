ded30a264acbecdee9eb63fe5b1ad8193dac9b1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:37 +0000

    formal tooth tooth call takeback

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def subtract_numbers(x, y):
+        return x - y
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
