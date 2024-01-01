4572fba0f5e218b41f2854c52729df4e0b0996b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:46:28 +0000

    furniture delicate heat gradually score

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
