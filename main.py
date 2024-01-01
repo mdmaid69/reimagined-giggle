f805cb8357d462f5b296f2d123868d2c9f67a990
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:45 +0000

    direction accident hair organize which

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
