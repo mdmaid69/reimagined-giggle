ab3aa125365493f986c47169dfdb8a5058ca953f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:16 +0000

    Ii only thumb own measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
