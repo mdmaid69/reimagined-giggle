743ee1dc5812dee7280de7564cd05664283fb8cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:23 +0000

    actual look difference bend dick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
