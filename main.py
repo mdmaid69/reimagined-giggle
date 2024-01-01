eb65e3b3189c552f6a8c7011ee903869f72199ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:41:06 +0000

    wet squeeze temper calm intelligent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
