66a8e6d2faebb8b80859d96ba0ecdc7ae75750c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:40:44 +0000

    interview slow road lead freedom

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import sys
+def exit_program():
+        sys.exit()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
