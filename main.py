76783d2da9ae278c50717788147b5e2cb655c586
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:43 +0000

    as uncomfortable concern influence disappointed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
