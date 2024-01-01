0ad6fb9aeae4fdf1118aac764a294c40a17c3fcc
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:51:32 +0000

    false top damp shake year

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_buffer_info(array):
+        return array.buffer_info()
