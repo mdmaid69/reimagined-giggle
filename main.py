c5204d0810b4a76ac95374782792c5bd69fade9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:40:02 +0000

    track freeze wise trap agree

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
