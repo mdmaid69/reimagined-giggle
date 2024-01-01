11b357b23a43fd302e97cb0f02f61717198c48e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:00:59 +0000

    free achieve holdup wing play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
