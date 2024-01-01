c77bb411c978725e63cb8d7797e278d4e09340ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:44 +0000

    length slip talk Ww compare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
