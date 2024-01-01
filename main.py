23397653a4e194d18a55111bb151758b894785b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:06:20 +0000

    limit television language characteristic sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
