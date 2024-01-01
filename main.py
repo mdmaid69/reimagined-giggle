2c95ccb0f432788f4665542f972224f32f95ed20
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:37 +0000

    unfortunate common perform port difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
