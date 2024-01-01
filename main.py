fdc0d7a670e2879c716d5cd21d7fd766e6148c42
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:41 +0000

    split detail glad man occasion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
