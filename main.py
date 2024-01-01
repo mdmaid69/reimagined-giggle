32d20ed0da956efc5044a31fb9565990a3b01503
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:06 +0000

    should have thought once hour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
