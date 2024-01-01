f5f39ab4dd03e9bebf04fc797cf1c0871314e800
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:27:12 +0000

    listen petrol takeout related worry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
