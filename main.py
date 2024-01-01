2d688aaa32625a1f5a967ffa073fefac53a9ed1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:27:55 +0000

    distance fix add 567enemy programme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
