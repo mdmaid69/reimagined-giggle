14de2c4f46f2269e0c07e120b9eebca2cdf96f5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:23 +0000

    quick successful country private meet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import os
+print(os.getcwd())
