42f916bd433faea09eaca02078f4417f896b67ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:39:37 +0000

    mad hide ordinary limit glad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+text = "Hello, world!"
+print("Words:", len(text.split()))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
