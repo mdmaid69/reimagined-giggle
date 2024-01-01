a126d8103127f63f755aad6b2cc7292e0bc5c6f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:14:49 +0000

    list long dull sign belong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
