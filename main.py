13f83453c443d6de6793dc4ddd459e6249fadc67
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:49:22 +0000

    circumstance present nonsense song abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
