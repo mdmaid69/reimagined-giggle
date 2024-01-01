485093644954d34180a30bc9ac65fe3ff7906b81
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:36 +0000

    light open possibility admire while

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
