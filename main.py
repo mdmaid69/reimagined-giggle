e25e526289e53b001179c4e083e8e81069f091a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:56 +0000

    pour patient mend commit risk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
