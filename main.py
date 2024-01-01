ffb9d6c62a3d5d6dee5fb0cf60c9968a7ead0985
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:33:46 +0000

    sight fact forget upwards own

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
