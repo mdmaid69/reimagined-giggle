675d3ae7e4cbc3daa594530e054faa3b4e340f51
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:59 +0000

    appeal mad pleased accident complicated

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import numpy as np
+print(np.array([1, 2, 3]))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
