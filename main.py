33b8875ccfb784af87edc793e2f22b0a7ebdfbb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:04 +0000

    rush invitation tone sure round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
