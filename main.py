b2b04258fecd507a72ba1524c0055d2d77c06e6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:12 +0000

    show suffer bye scared post

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
