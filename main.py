df707fe253b030c529e264034f382b4f234dfebb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:36 +0000

    impatient agreement situation deep write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_index(array, item):
+        return array.index(item)
