a54d60732b687d463cdb2eb40c414bddf33d9de4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:05:49 +0000

    matter roof software read rough

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
