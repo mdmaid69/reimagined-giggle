357a1ea99a76b0b4b436e86c055d7ad692ac250c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:17:34 +0000

    speed sound suggestion Ll related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
