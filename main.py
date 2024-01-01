7f3207879badf42d3f8409551a83a911c15735ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:47 +0000

    smile 572enter reliable vanish go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
