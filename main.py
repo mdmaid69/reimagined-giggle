61334a052f876953a5d41b636bb5523295d469f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:08:31 +0000

    behind similar they table carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
