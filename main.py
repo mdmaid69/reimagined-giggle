88480485df9e36c989e874f02606a18cecc87442
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:55 +0000

    objection dress suggest getback niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
