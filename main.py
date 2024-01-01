4b04aa5f23928fb45728845786f1d4be228e2706
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:23:20 +0000

    thinkof use concern inquire match

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 def calculate_interest(principal, rate, time):
         return principal * (1 + rate)**time
