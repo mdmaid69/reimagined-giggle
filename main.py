732005b8fecc8ed1ccae472f9378a8132264d182
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:01 +0000

    kind trousers equal experiment basis

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
