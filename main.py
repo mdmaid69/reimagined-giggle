e47d49a979469d4beb4d53111f2de923e85e2505
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:25 +0000

    quick workout dick personal react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_priority_queue():
+        return collections.deque()
