2c3dbe7ccc35a4674568a4bad0ab26c57b60307a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:28:29 +0000

    common pile criticize reject his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
