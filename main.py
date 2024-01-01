a301f78e13acaa80f2666cb5627744c9a661380a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:33 +0000

    cruel declare ask holdup beneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import getpass
+def get_username():
+        return getpass.getuser()
