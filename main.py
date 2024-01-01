aafcb6474b78fd88219b8adc282d28e224d5ef97
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:02 +0000

    dull copy demand charge slow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
