f3330d99744c207803683825e3f967319e406afd
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:34 +0000

    explain build question raw best

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
