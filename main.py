97d9f2039c0b5032264394e07623c8a1210fdefd
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:58:10 +0000

    sweet oil collect permission central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
