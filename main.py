d4ac4363ee1aa250c1ea2a7d4849c6d27da69903
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:04:24 +0000

    like during spring declare everyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
