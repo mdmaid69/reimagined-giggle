9d14b3194a3fbba736e1763eb990e7d2e5eb623e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:12:38 +0000

    shop possibly frighten recognize fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
