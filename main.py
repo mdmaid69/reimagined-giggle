e1e349f495097b42afea783beb420a99c49ae685
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:12 +0000

    university view married could test

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import platform
+def get_python_version():
+        return platform.python_version()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
