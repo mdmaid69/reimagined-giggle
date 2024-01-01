a3cff381974b9cdd033978cc54e825c09a0fa37d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:58 +0000

    it disturb light hair minor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import getpass
+def get_username():
+        return getpass.getuser()
