15f00aa45a99efe0a0b43f77a34c48aeb3d6723b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:05 +0000

    answer past roll separate see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
