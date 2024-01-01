fbe79f48fcd8bf49122fe4f35d3d191f6313525e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:16:19 +0000

    increase scared already needle juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
