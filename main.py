531be8b2900169e9e58e11622ae180b49915bbda
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:05:45 +0000

    best delicate ring foreign cross

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-print(os.getcwd())
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
