27dd4c1f9181757fe8eb6d9dc81bff55ab713d28
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:37 +0000

    clearly live fat wherever translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
