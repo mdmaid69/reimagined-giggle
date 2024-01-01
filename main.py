de1c6df1be992a500428d2882324c38278f5ebdd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:13 +0000

    breakinto duty company shout unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
