449585e1084a96df58aa2a3c2894614776fe2440
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:27 +0000

    past fasten company wire lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
