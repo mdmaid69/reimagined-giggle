5e5327d0828b58b41c05821b383b4288dd74c263
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:20:03 +0000

    rain flavour home bye friend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-def multiply_numbers(x, y):
-        return x * y
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def reverse_array(array):
+        array.reverse()
