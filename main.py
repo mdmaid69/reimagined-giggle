5b7e43fe16baadf1a3c8fd45a38cce15ac29de8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:57 +0000

    slowly along neither content zero

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
