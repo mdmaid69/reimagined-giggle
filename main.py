fbc7df39a40ab04ec559f53003d288c1ababc1e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:44 +0000

    table train opposite admit represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
