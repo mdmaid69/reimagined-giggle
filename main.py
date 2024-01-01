5c6f8a1113fe56efea3a2a739c3c3682cc833c1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:41:10 +0000

    dead breakinto common majority power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def split_path(path):
+        return os.path.split(path)
