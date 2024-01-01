bd604f734d866a6a16bb56c29e8c91d814f9ec46
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:48 +0000

    brief move play want thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
