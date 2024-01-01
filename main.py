f9eb48e3ce7296029a07b4ff3b2c4f97dd417172
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:47 +0000

    flight trust clear attention apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
