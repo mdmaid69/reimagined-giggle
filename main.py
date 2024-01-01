a28f9be5c9b84db3ed7102e0628c96e61b6c9ceb
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:15 +0000

    dark cry chance increase flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
