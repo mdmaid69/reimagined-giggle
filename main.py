4b0a5e1dacecfb2604cee9567bc01cb84b47998f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:23 +0000

    load break trick consider only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def get_array_index(array, item):
+        return array.index(item)
