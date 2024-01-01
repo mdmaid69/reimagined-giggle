da0c7c027f06551a8d3eb7a8f037cfabcafae618
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:00 +0000

    slight represent actually no responsibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
