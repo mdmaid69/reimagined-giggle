bf53d5c1e049594bdda47e7e127d56e747a97ea0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:51 +0000

    fat can wise frame judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
