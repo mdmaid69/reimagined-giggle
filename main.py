8f8cb3bd344ff168e45ca6d607fc331abb57e6a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:24:09 +0000

    tip blame hardly Ss empty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
