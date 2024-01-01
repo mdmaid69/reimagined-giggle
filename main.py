7a665d6ee23ae7fa586707dfa5075effffd8f1f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:41 +0000

    forward scream set accordingto getout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
