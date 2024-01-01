abfab4001b248ede915162ca6407cdaf51884abf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:54:51 +0000

    such wrap deliver world roof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
