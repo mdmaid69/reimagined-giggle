dc3bc9b5e0a93ca0cf01d8eb2f4f9f90d665ff54
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:12 +0000

    error ought belong vote ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
