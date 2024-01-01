c2ec0beaccf862fdc6c583771fb3cdeb737e17cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:33:58 +0000

    board contact birth recognize outside

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
