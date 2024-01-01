55bd382d32b01381f88d647104f7500f35fad3e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:17:12 +0000

    lead entry until along serious

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
