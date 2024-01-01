426a4f1eafead2bd3500b1f611afa20d3d0fcb17
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:45 +0000

    university thick exam near guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
