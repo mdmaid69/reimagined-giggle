05afe21d231c7198dd3625ded48809f9e87ba704
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:54 +0000

    bunch communication whichever nonsense shrink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def delete_file(file_name):
+        os.remove(file_name)
