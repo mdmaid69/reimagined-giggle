72e4aa2d13fdb826ee9877cced880a8f1d7198d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:49 +0000

    put difficulty at makeup relationship

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_volume(length, width, height):
+        return length * width * height
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
