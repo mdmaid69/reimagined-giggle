ae21171a6d9e16b2ec59c8a4dffb20ab5fd28197
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:16 +0000

    kill respect them walk plenty

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
