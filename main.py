54358cc44768e23cd046d9cd5fae72996a2b12ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:04:56 +0000

    tip split dress hit related

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
