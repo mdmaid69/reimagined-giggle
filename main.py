2dea6ea70923a605c5eb37d1fb6b772499cba2a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:16 +0000

    adjective getback dull general nonsense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
