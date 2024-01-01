92479a165ab73fe8ce6233ce79805f8ccec2081c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:56 +0000

    secret top where neither reaction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_as_float(array):
+        return float(array[0])
