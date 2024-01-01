908c698091b9e2d22b71ea069dc6b7f8eac85453
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:42:28 +0000

    moral patience distant on insist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
