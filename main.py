50a97ccdb8437f52f3bb1cc4a86cea49bd6978ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:42 +0000

    wave runinto memory noise first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
