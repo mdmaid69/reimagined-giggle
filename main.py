577b2ee7f8049d49d5169bae5f6d13c5c1ec415d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:38 +0000

    matter sit next hour care

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
