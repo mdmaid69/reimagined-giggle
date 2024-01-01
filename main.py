c73d2d6c2a64a737457732c88b960e33b86eb6a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:58:18 +0000

    feed we vote cheque phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
