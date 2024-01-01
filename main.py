aaaf12eccb8586b5c5d407ca01b079435976c583
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:07 +0000

    winter few crazy war study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
