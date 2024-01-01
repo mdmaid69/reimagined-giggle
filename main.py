c9a55dc0934408ced21245f2d80937d34f805efd
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:11 +0000

    total back present dark with

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
