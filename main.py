1c0ba07cc105d2c6d57487a219a13e97f6173567
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:40:30 +0000

    clean passenger sit demonstrate immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def sort_list(lst):
+        return sorted(lst)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def set_array_item(array, i, item):
+        array[i] = item
