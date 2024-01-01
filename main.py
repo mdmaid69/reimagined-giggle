d1dd886a0a1d5bdeec3a1e9fef9b450dac57a74e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:09:53 +0000

    like guess complete continue take

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
