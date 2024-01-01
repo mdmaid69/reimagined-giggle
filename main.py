9e36b3d674b2e48d19fd9434560927fb788fcf05
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:40 +0000

    hello car better tough really

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
