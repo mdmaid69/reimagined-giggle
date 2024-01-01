0bdc4d6deafe1e2a7c494157965195d8dcc1b4e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:44 +0000

    turnon later breakthrough I until

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
