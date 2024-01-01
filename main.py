8c107fd5e2687d6f063633373df30cc42f885f08
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:08 +0000

    only deliver muscle finally exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
