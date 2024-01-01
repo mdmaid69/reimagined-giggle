66d75e26a89b619708af04872a6a7416ecc70604
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:23 +0000

    safety train computer hair comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
