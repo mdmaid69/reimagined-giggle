c79f7162940e736bac3d71868a130e58575daa98
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:06 +0000

    ill comefrom wherever bread except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_force(mass, acceleration):
+        return mass * acceleration
