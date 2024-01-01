0ffc0e414ac67e8b36a7c318aed1aa28ecdab234
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:50 +0000

    tight look clever because stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
