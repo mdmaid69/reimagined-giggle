e75558a82d0745f7b393959c466b2cdc444d4bee
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:05 +0000

    thirsty matter behind smell eat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+print("Hello, world!")
