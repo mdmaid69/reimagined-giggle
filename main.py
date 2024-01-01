97aacb479ad0942d35078062ff52dac74d436a64
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:12:07 +0000

    rarely better prepare backwards guess

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+print([x**2 for x in range(10)])
