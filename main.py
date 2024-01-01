fe15d20e093efc817f48712a8401e1cfa7f500e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:59 +0000

    loose discuss go negative frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
