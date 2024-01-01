0ad2cd855bc262619177f8214aceaa55e3b2f133
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:55 +0000

    splash base ear please recommend

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_power(work, time):
-        return work / time
+def calculate_work(force, distance):
+        return force * distance
+import re
+print(re.match("h.*o", "hello world"))
