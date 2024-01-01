92d3f80d5cdb4bb98c6dee2cde2d07173fc83a35
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:01:05 +0000

    harm arm afford check up

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_as_memoryview(array):
+        return memoryview(array)
