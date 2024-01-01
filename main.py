d62528cdab017e38429cec8f8ab1453f3117f7d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:22 +0000

    attempt towards stage tense play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
