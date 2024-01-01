22de3a49de6bb053e388a4db7fcc31b2c485a52c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:57:30 +0000

    desperate breakfast mistake weekend separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def check_if_array_contains_item(array, item):
+        return item in array
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
