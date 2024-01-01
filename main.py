d79e77c856a963fe0cf6fc3d7a0c140cb8109aeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:01:14 +0000

    perhaps either passenger sit order

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
