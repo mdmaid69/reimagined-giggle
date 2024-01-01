7f575a6c601c7ec3c8f719d5eafdcf03f0326b40
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:44 +0000

    manage around sit smile emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
