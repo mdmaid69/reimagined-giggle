4a15db5a32dd49fc7d6d0b9ca9e21afab1a97f70
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:30 +0000

    chat especially us side cook

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
