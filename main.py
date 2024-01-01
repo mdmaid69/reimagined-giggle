a48e225a06d2291a113fffdb1108baddc4db690a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:07:32 +0000

    production estimate business power shade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import math
 def calculate_error_function(x):
         return math.erf(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
