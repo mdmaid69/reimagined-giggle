e8327a1171f99c7261006b7ed4024ad8aed3a42e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:29 +0000

    old that type passage getback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
