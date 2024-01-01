9c48d93c88c066c3aec31d51321a2049ab7ed157
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:22:51 +0000

    practise announce male by disturb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
