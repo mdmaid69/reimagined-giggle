81ce41aa0918e52e389dd958660bcf375cf17aea
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:46 +0000

    niggaacademy statement door laugh wealth

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-for i in range(5):
-        print(i)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
