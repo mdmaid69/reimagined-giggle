1ad9ffcbe9b2683e3593be92c3b639e9126c5de2
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:15:12 +0000

    peace production born smooth arrangement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_length(array):
-        return len(array)
-import sys
-print(sys.version)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
