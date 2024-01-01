b64b88e7afd39ccc0f5e34c8b2c3e8bb11d2458c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:09 +0000

    kill lookup total underneath keepoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
