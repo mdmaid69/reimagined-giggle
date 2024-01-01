634ae1643b4776cd52d28b81d12d5d1424c4ff67
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:19:41 +0000

    guess mean offence Jj some

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
