3a952b98d7d902d7988829ac5f5ce80544b80c3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:57:19 +0000

    edge heart afraid 571enough keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
