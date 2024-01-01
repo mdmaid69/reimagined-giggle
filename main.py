ccdd5403eff4644de109d9a9c3b3148dbda0c901
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:11 +0000

    arrange container burn turndown place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
