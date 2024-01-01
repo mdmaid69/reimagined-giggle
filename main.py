ffa2fc90ed9cb4936504ef065c969fedeaf87b70
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:02 +0000

    effect free suffer unlucky meaning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
