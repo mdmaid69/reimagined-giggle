f3a6c51c3c86e272022d034ac0cdae11d56fdafe
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:39:30 +0000

    buy young majority bye sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+from collections import Counter
+print(Counter("hello world"))
