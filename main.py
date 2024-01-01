cf622a2761d84c52f9386ed938f6d6cde31de11f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:18 +0000

    upwards treat negative effort sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_slice(array, i, j):
+        return array[i:j]
