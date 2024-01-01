08492b417bb567b0a8beddc3690def29c91ed4eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:00 +0000

    reliable neither month genuine trousers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_square_root(x):
+        return math.sqrt(x)
