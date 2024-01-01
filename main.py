bf8eea8ed710d7e90af86ef2e3cda236cc6d6709
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:43:33 +0000

    loan land point do example

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
