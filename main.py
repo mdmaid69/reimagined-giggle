44b350360a5e0082b3dcd0bc5ce7689cc3ff9420
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:51 +0000

    let plan crazy pleasure that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
