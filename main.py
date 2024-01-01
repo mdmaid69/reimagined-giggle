aced74027f1fb061ab95baed330a4d9657fed9dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:11:15 +0000

    month more border region private

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
