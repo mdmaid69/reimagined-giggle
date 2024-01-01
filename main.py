9d7fd7015b4f47d53a71ff4c4f2f3197ec3865e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:20 +0000

    holdon accidentally sensitive straight gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
