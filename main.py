8de84ec6cee08df0b6889c85588588c90a343d35
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:54 +0000

    promise appear machine line comeround

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
+def count_words(sentence):
+        return len(sentence.split())
