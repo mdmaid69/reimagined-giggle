4132ab0ba70e1259952210f1f44c88bd1ee19f56
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:28 +0000

    minority square balance offence hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
