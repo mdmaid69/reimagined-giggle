896102b0a39f081ac95966e0850bad7d1ac35d63
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:05:38 +0000

    wish remark trade smell discover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
