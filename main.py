04c37d36495487786ea4d6ac7735b271c73ee382
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:36 +0000

    interview set event community square

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
