49d4ef94c317db283fee43d6efb302e585654a10
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:48:03 +0000

    travel fill post mail towards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
