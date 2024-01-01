e7e8a579266e4949daebbd3381bdbf3d282af616
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:08 +0000

    light respect listenfor splash tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def count_elements(lst):
-        return len(lst)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
