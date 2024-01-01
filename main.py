402e07f7a766574983f7acba7e0ce44b49723462
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:29 +0000

    certain wet but authority cross

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def set_array_item(array, i, item):
+        array[i] = item
