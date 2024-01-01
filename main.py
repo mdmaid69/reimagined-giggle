173b51b8c5cc586a493cf5785b03534d9fe23a04
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:11:42 +0000

    next goback(to) professional right smart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
