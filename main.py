492508b61436937fde5db6a84736e1e43dd5b1b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:09:22 +0000

    strict cause exchange gate everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
