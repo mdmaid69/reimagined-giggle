73b3d71ffaad1e44e12b6d3f214e7daeabad5de2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:52 +0000

    family recognize plan emergency workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
