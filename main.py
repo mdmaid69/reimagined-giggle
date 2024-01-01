e1e86bfe087fa695a4002a93ebb55a2d04ba5dc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:28:39 +0000

    equal weekend steep Tt party

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 import math
-print(math.pi)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
