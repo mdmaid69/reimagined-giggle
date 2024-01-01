87f137db26e0da1b8dae3165fef8dda4740e2a01
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:12:02 +0000

    usually dick bet not wave

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
