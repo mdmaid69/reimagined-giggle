35af2630a9b7589829a231aabe1fc22b96ac9e61
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:21 +0000

    round replace our shut goaway

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_odd(n):
+        return n % 2 != 0
+def square_number(x):
+        return x**2
