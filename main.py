e1f28c2f983a05bccb1e7de08f2ae06ab5a4ddfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:35:51 +0000

    present important refuse like world

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
