dd52fdf8248e7a08e7a6ce9a974d9756e04c1bf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:10:19 +0000

    have model room nor how

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def get_array_length(array):
+        return len(array)
