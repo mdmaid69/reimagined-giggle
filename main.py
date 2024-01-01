853c444e09b3fc00529efc78fe28059d2e93cc83
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:02:25 +0000

    themselves promise properly load sing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
