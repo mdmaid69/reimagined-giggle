1e336f2dbfcbce5cc91a8b3352704cd3c0778eb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:57:08 +0000

    just support surprised personal towards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
