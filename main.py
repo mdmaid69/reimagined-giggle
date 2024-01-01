941bf644678e89719ff7eeab598e8e7ad6183069
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:41 +0000

    566end workout laugh runinto attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
 import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_typecode(array):
+        return array.typecode
