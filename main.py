d0a81ab9d7e0708da3475fe57b38c859fb4decdf
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:24 +0000

    treatment teach object abroad friendly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def sort_list(lst):
-        return sorted(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+import json
+print(json.dumps({"name": "John", "age": 30}))
