071849d1f654736c6163e37c82943f5321978f05
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:15 +0000

    form except conversation cost loud

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
