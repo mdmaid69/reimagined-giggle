d1a02d84fd16a9572257e0cdf462e737d79412fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:21:16 +0000

    bus in rule feed show

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
