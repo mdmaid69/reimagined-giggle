7a68d3a6ed3f5bee5193c082363094a8e6d3b206
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:43 +0000

    stop learn beautiful now however

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
