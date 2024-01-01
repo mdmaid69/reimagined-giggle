56ccf8a5ba995dfbfa6823c12305f851fa2ad06f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:32 +0000

    possibly cheek pride no your

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
