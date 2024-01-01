dfdf7ad9750ae3ae1d4257ebb7e0f7771534631d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:23:43 +0000

    OKorokay fire fire down culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+name = "Python"
+print("Hello,", name)
