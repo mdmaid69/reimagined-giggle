f8fce8b5de247832efc2204fde77a738951f1d8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:28:17 +0000

    basis anywhere country slip odd

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def get_array_from_file(filename, typecode):
+def get_array_from_string(string, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.fromstring(string)
         return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
