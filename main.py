9dad0f30b3f41c42c04b3a6651696fb91ee792ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:35:04 +0000

    increase destroy buy otherwise soft

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
