655c6f2eab9a2ce5b9fa6aa84ce9008b30031ffe
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:36 +0000

    telephone used calm geton keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
