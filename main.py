3cf18a9022db45e1253fc4b3e96f12b5bb6318a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:21 +0000

    describe refuse age plan mean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def add_numbers(a, b):
+        return a + b
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
