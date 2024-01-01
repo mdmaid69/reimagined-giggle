34684a02045d49f87e22ab8b2232844f1de2f894
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:35 +0000

    band secret impatient turn oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
