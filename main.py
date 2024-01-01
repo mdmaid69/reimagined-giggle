ea89e8093cdb4a91e614b91eb772ed99f06e641e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:59 +0000

    second mention bus give final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_item(array, i):
+        return array[i]
+def convert_to_binary(n):
+        return bin(n)
