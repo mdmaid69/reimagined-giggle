48b88b600fea53464bc7f272397672a0a6399ef9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:03 +0000

    remark some hit inside pain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
