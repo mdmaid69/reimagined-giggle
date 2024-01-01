8183bef4ac04656d0ad9a943c0aa14413e7fa3bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:47 +0000

    cinema win breakin flight shine

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sys
-def print_python_version():
-        print(sys.version)
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+print(json.dumps({"name": "John", "age": 30}))
