e6d6d8c83a2a5f3ea039d0610da322f3c7617012
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:06:49 +0000

    hollow port sharp sleep left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
