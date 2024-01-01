8a0d4de2785a43037a58f4a5fd5ac54c447e612e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:42 +0000

    instrument shut cruel unit hurry

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def convert_to_binary(n):
-        return bin(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
