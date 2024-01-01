1e133a16d51528fd1b1cb4a2a31cd00484ea7c0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:08:44 +0000

    thinkthrough question fail store demand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
