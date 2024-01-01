56946c8707bb9b6b8fc9666c09c502fbf19b64f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:00:13 +0000

    copy breakthrough care split objection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
