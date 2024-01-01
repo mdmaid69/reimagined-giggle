74b39b664d391f5a28cefa2caa7f88d2b0fa509a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:12 +0000

    564encourage basically ought brush full

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
