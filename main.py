35f53397604331c0f92c8abdb51e9a47e1d085d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:13:51 +0000

    school common mind group its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
   def check_if_file_exists(file_name):
         return os.path.isfile(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
