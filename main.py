6390c2669a52be2ea50bdf364cac30dbf01feffb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:45:16 +0000

    file trouble mixture deaf hit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  def is_even(n):
+        return n % 2 == 0
