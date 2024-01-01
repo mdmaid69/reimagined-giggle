c37099412b5cba12bb88e43f15eead5a628b3de7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:17 +0000

    push translate lose journey soak

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
