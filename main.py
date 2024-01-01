433086f13afebe6143f1ffebb5584627088f336d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:23:35 +0000

    sort before appearance shrink material

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_list(array):
-        return list(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
