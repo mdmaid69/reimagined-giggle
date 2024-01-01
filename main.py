58d8375b858c98b026b9610cbbc3af698d8e9861
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:48:57 +0000

    afterwards sore runout joke press

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
