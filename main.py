98bacb19875411042b177508755d74d29d861002
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:46:35 +0000

    afraid gooff clean copy Hh

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+print(math.pi)
