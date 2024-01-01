7d5bba8714dada9e187c71fdea18211f0ff66819
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:05 +0000

    cough appeal Bb born themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
