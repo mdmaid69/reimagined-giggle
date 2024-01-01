086a9b42396d78746f7dec5484b52119d83399f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:43 +0000

    reliable geton smile very level

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
