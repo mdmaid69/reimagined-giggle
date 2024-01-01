4da2412fa57403809bd8a41f000cc35c4f41781b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:31:09 +0000

    disgusting that cheque world pool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
