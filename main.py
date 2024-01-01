57f1c274d38780a7ed8756f21bb57ff54c0281ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:04:18 +0000

    rough snow rub takeoff mainly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
