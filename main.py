498ae8622e56a308e9bc73e201ac08cb441a7cd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:28:51 +0000

    which black noone reliable related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
