7929f13ce1fe5b295cd077bb7df6c1db42697fa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:49:24 +0000

    promise year turnback still circumstance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
