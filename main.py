5b1cbf19c5add21677c97e8e8ac96d04f87a2e31
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:03:09 +0000

    climb arrival joke provide comment

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def list_files_in_directory(path):
+        return os.listdir(path)
