3e5d84d6e9972a287c8922007decd81028965d11
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:46:30 +0000

    country minimum scrap black pride

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_set(array):
-        return set(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
