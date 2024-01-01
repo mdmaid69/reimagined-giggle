f556e188068e942a46792fad0eaa7e890a9be30d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:10 +0000

    life breakdown comment birth secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_set(array):
+        return set(array)
