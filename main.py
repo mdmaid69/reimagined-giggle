616077c9e8b594c748633038be48d6e97e578b0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:49:34 +0000

    computer blue beneath quantity judge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
