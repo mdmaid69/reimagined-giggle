c66430821a166794352566cd77bd0d371ac974c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:04 +0000

    generous mistake no lot right

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def convert_to_hex(n):
-        return hex(n)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
