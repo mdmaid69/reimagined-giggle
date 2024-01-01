ffec96617ae4032dc6c191c5e7c05d8e2df428b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:35:55 +0000

    very grand team medium successful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
