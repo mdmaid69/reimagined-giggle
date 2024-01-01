74bf25e2fec933bbd5e9c65f33131abc7cff0c5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:01:39 +0000

    strange upstairs discuss none disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+for i in range(5):
+        print(i)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
