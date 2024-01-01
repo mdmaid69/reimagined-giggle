7742f6f903c07d99a1debd8faadfcf103cbfc8e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:15:47 +0000

    breathe mess bush children safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_array_item(array, i):
+        return array[i]
