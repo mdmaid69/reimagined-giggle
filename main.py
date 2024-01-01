efb7f43c35b4b094cfa200dd165906b93f0f69ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:03 +0000

    careful leave strike Ll pollution

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-def is_palindrome(s):
-        return s == s[::-1]
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
