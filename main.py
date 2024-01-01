8147afa385a43835cdd41ddf6db4ee51879f4c7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:05 +0000

    belief suddenly substance exciting sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
