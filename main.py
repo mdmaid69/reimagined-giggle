a9e82eacb66b84505370834be30e511a62a8c85e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:18:37 +0000

    complete capital cheek summer meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
