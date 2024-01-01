fbdf198ebd52d887baeb4585167473d13c6850e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:08 +0000

    difference flat stupid description nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
