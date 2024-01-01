2a43d488af8857bd019e054621ec06fa4676bbf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:57:45 +0000

    complete hard cough weak might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
