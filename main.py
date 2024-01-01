4335d3d0295bfdfb67d0432cd5622f06c1406e96
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:44:58 +0000

    noisy straight mine meaning wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import sys
+print(sys.version)
