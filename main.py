22e22522e60a2d8ea3b0397aa6682c157f439fc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:46 +0000

    thought block feeling pay realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
