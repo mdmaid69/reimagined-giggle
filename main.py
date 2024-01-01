a08a741622dd6b73d6bf5b66e52176d392da7e8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:54:34 +0000

    comeout shut shall conduct keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
