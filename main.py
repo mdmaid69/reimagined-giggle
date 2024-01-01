37955eecbd81c0f1c09d409cec9dc4f2d9964712
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:05 +0000

    send keep what unhappy clean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import numpy as np
+print(np.array([1, 2, 3]))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
