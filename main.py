336b1109ce1141e6542cbd82292bb7cad5c5ab1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:41:20 +0000

    basically row compare design march

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
