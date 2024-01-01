313c1968f6cdd2ffe734f4cf2333a7faebcd0635
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:12 +0000

    slip needle guess advice limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_force(mass, acceleration):
+        return mass * acceleration
