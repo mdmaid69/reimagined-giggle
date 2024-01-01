ca0d528d6442815fa45d58d547bea12f357e257d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:24 +0000

    fall advanced birth help now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
