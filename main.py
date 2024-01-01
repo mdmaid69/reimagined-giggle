274a33714876321a8c9b40353dc11649ba23bd70
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:59:15 +0000

    upset believe save mind use

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
