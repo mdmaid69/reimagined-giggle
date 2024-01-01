15b6a6af1ad1ccd8b3b25d5cf2971b9c063f3ce3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:38 +0000

    bath now law dead hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
