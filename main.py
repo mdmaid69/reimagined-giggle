8ac34dc8b00f433e9cad9a97d01e768001679832
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:38 +0000

    wish serve adjective name consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
