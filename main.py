a4dfb31d28a7d17760df98d4f321d4afacd3e8e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:09:26 +0000

    light friend rough promise embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
