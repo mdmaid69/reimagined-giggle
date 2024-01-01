1f2d603b6bec2cdd75f8d47aab492d48a389f45e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:02:16 +0000

    urgent small stranger drink sign

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_int(array):
+        return int(array[0])
