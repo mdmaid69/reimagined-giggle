291204bbe6e31ea7ba53d8fe92a48a6a7c0b3a3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:05 +0000

    help call possibility properly honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import numpy as np
+print(np.array([1, 2, 3]))
