d47d39374d0dc79b549c9cf899ab66dfcd018754
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:34:35 +0000

    some learn reserve part could

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
