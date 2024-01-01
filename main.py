9727c26fb3efafdc6255d39296a779e9832105b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:48 +0000

    be receive body part level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def add_numbers(x, y):
+        return x + y
