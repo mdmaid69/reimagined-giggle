8eded0e93e758ad2a21174574ef60eb81f106e37
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:31 +0000

    compete comeout adjective strange so

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
