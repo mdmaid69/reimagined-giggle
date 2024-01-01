3d400221e1c2c7ee60f8ac412a8e80eea77da17c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:23:36 +0000

    demonstrate awake cut police piece

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
