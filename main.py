5eee1c24204db25b98edda50a4ed4bd47b0ad5fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:18 +0000

    open own lock arrive welcome

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
