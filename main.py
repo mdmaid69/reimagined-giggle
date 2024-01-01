afbba03d57be04cedc865bbd8ccea3b04bf1aa9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:56:54 +0000

    identify experiment speech note warn

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
