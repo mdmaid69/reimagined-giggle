5372f5e93df89acde59b8e20a23f8d7c40d982b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:56 +0000

    fire clean temper cause pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
