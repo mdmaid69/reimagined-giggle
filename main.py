da4c7f92227fdd8808243cfe4cbd0eeaaf742233
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:14 +0000

    sad smell bottom possibility bush

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_string(s):
-        return s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def find_max(lst):
+        return max(lst)
