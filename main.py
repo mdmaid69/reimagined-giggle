2790875ef391d3f9d65a57ef47e3ceb281e8b5b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:40:37 +0000

    measure duty passage callout calloff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
