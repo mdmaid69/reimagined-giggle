51ec879def4eab1411182d19d8fd441514e17413
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:28 +0000

    succeed damage never bring miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
