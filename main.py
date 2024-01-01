eeecd3db6aa7353d9f796e70251d7c61c5945227
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:08 +0000

    decision read safety stranger makeup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_min(numbers):
+        return min(numbers)
