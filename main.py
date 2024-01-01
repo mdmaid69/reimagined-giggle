658b3fb1c8a93336649df690890d85f6a0559dc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:15 +0000

    emphasize pity operation foreign lucky

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_hex(n):
+        return hex(n)
