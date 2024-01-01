5eca1648567aaaf736071fc512f51ba7fbdc2cc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:25 +0000

    already defend lonely bad snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
