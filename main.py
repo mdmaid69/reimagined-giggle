6d769008930b8da34d7ca886acf30d5ff1839162
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:42:46 +0000

    strike Qq ring needle ahead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
