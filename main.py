64d17a4a07f3f30adf4acd8d14cf7fc5d7cd6c36
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:39 +0000

    cut stone stone persuade very

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
