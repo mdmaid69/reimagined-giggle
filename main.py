2f9c8ba754a7b525e732cc442777e3dff08f0538
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:15:23 +0000

    even write high ring flat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_tuple(array):
+        return tuple(array)
+def calculate_area_rectangle(l, w):
+        return l * w
