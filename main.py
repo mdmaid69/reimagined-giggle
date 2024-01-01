583a9f5fa59a8f4cba8a9a56702cdf0fa5c7f91c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:57:25 +0000

    relative guess surprise film name

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_hex(n):
+        return hex(n)
