785e399e710a061b12bb4b35bd1facda1adcf4b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:31 +0000

    traffic touch suck available big

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
