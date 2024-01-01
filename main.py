9cc715f2980858f225549bb5d71834dc282be594
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:47 +0000

    substance shallow sister understand sometimes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import time
-def get_current_time():
-        return time.ctime()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
