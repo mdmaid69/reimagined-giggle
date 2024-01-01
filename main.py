fc71fc61a1f5492afaeb2143fbf6de8e2f16eff6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:52:34 +0000

    scream mind dry born runout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
