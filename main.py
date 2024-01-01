81bd109cfc4b7273fa636de645e61433feb69343
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:03 +0000

    far here properly doubt strange

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
