9fd125f47817f8fa6d4f14639d425c0da5bb66fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:27 +0000

    chin we getaway storm back

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
