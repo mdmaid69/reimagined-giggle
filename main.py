b586aefba5838e3ff9f35af2c2a4b9d1f98a1447
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:00:38 +0000

    stop reduce score wander able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
