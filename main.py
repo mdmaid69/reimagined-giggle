dbdbd18534b5da8edf36ad7e4ec10606e42ca979
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:08 +0000

    hers down freedom classroom sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def print_python_version():
-        print(sys.version)
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
