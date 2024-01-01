6207559e2016b848bc4fee39586e7f959ba624d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:50 +0000

    confidence all always plastic pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
