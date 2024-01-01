e68f28a8efed77728657a8b91fc2d824190fb775
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:17:10 +0000

    practice thin copy no demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
