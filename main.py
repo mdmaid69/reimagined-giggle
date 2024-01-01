490764a9a8a0df0c449139c7d0d67bce849492a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:20 +0000

    fasten used dull appeal divide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
