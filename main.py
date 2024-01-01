4417377672d801804b55523e6a7e598660cedc46
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:31 +0000

    fine stone mind probably yours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def append_to_array(array, item):
+        array.append(item)
