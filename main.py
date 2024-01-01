7a42738176dad0b0e5d2a1cda3c6ec487a4f1363
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:42:36 +0000

    upwards keen niggaacademy past ignore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
