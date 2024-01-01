37b741bf1beb37a749d5ca87d566f2911adfa45c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:58:26 +0000

    reject bad explain ready no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_priority_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
