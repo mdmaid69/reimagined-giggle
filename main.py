6b7b9702c55dfa7396b4055c8caf26ccbe6cce73
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:45 +0000

    like either adjective spring ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
