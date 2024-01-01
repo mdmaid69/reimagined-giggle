ba062ab1566ef801719c05047a60308fc2fc5a38
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:48 +0000

    design surprised public plenty pollution

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
