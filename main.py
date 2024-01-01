adaa1ba7c7fe2cb280700b6a5e62d59aa111ae5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:44:16 +0000

    collection pool store your if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
