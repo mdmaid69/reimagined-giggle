3752fe60f5c8a870d849b1ec89d2f66d40d2e7c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:22 +0000

    moment awake hard consider shelter

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
