a9581819b0b2f4489d2085f724ae539fd31de8aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:23 +0000

    seem radio deliver advanced Ff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
