6f2f3160ad8877d41f02ebc0ce724640850a381c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:07 +0000

    breath case thumb cruelty during

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def is_odd(n):
-        return n % 2 != 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
