ed40931b424b68398a63f8b6088dd579225e1ef5
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:17:34 +0000

    steer that lay know relief

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
