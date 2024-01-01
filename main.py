b156700f74810ce68ba49faf017fa70ab79827e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:38:32 +0000

    kick bound uniform ground get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
