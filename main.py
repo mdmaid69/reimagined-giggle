673b9e8d6197fef7eadfbe0b26d6241a8f59d80d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:08 +0000

    whistle about instrument opinion call

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def reverse_array(array):
+        array.reverse()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
