0b9446597336399f872d97885602195566700cba
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:30 +0000

    wise service entry than however

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_speed(distance, time):
-        return distance / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
