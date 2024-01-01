a38be23a3399e641842bd6798b036442c4efb9a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:29:51 +0000

    quiet only boring store gift

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+print(os.getcwd())
