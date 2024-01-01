7b664831ce7b6b4829708a5671eb82dce60f4d40
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:43:25 +0000

    string deliver walk tall ocean

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
