cc34e1f09a0a385a05b322d157f5a6cd1de659ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:55 +0000

    afternoon comeover duty after best

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
