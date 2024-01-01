1f7c4c0da9434bfe90caaab25e007070a69855dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:30:33 +0000

    measure arrange effect almost safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def multiply_numbers(x, y):
+        return x * y
