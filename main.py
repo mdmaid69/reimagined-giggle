92a6c5b3d49aab6a76d4c06a92dab6ea46791f92
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:20:38 +0000

    bend week workupto breath wind

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
