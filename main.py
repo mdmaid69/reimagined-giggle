2f5d6d18cab6e1490bede63c8e99dbccb1132d30
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:07 +0000

    remark workon remove song book

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
