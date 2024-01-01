6f3493c7766ab446c22eee17e083290c7970fec1
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:01:50 +0000

    follow pump quality cry perform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
