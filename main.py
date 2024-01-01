88972c4a2c68eef689eb4f4ff96b8dacce67bdf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:36 +0000

    giveaway mark Zz embarrassed save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def find_max(numbers):
-        return max(numbers)
+def get_array_as_str(array):
+        return str(array)
