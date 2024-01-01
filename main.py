a6932cbb1796ee1bbca3091d0c83108a36f8fe87
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:24:55 +0000

    influence snow pocket club nurse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
