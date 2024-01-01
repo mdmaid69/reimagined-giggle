3d77f1e9b49fa45bc805d0ea2947d94eb8309262
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:30 +0000

    score about brave compete if

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def square_number(x):
+        return x**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
