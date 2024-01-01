6bef8306379d2be2353aeb1c10069c3341484bc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:56 +0000

    sound care excited give full

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-for i in range(10): print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
