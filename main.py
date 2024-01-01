2306d57617d406a516f346dc5c05763137a03711
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:15 +0000

    pinch to busy callout start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
