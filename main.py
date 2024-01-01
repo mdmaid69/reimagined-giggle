2346400abdd0496fb218117d790eeb55932bc4bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:22:59 +0000

    identify level art tool reduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
