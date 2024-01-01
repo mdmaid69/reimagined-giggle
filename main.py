318a3c3a20454cddfdbc910276909d5fce3be76b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:24 +0000

    phone progress fairly apparently environment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
