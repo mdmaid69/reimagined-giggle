ecd2164f4f5e2b6f90e81e88a546a1f1af343d83
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:38 +0000

    turn during threaten about win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
