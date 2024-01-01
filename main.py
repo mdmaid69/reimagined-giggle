68f5d223cedb339ccd5e62e3bb6d7031efd004a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:35:14 +0000

    surprised settle even unable loud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import sys
+def print_python_version():
+        return sys.version
