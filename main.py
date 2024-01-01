23ffb156b45f5be88e64173c319efcc08ed2032b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:18 +0000

    die shrink juice yes look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def delete_file(file_name):
+        os.remove(file_name)
+  import sys
+  def get_python_version():
+        return sys.version
