79e49bde31d1ba6ecf1a6d98ab88b2015531d861
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:15:38 +0000

    pen bin side ill nerve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
