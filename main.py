f4e5dcfe58edfd701eff6bf564c42cd9ae21542d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:17 +0000

    exact mind bridge move sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
