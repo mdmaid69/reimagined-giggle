4c1cb8abd6550b2c7980bc28d14ef0af7a53852b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:25:08 +0000

    continue soon insist nice such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
