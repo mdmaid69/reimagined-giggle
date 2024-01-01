7f2f89cb3b23f49fab164ffb3f2dc53e3faeba72
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:07 +0000

    bright different careless whisper fail

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import collections
-def create_user_string():
-        return collections.UserString()
+def is_odd(n):
+        return n % 2 != 0
+import math
+def calculate_error_function(x):
+        return math.erf(x)
