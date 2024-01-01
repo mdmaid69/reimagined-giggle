9fdabd3990f74915a5ab5c7c07c191f8bfed42d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:25:56 +0000

    turnoff family stick guess declaration

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
