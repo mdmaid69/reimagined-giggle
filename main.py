4dffe8c2e89673e498c11445e1a7c541728dfbcd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:02 +0000

    common affect everybody extra recommend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
