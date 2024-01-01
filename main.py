a0df35faab409261fd9fbbddcba6eff7796e3ad2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:51 +0000

    answer alone definite care think

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
