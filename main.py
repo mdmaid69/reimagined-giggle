5e5a61e1b42c57d35331f2b9c4eb9fbac99a30a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:32:59 +0000

    deserve commit fairly eager whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
