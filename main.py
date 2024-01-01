0d5d179777bc0d30166d4783b07502e2512bffbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:43 +0000

    quality measure Mm breath wander

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
