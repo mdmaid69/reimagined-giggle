cd9fec6c8475b5d6bf5c11a521ce47cf33c9b0c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:49:30 +0000

    silent figure example scene nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
