b9b3adaf6b8db0087d4aff30f13b0ba791cfa125
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:43:39 +0000

    man flight gate embarrassing turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
   def get_file_atime_ns(file_name):
         return os.stat(file_name).st_atime_ns
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
