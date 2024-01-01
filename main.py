e07cfaf06acd96274006fa706913c0e764acbfcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:13 +0000

    heat anywhere straight combination I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def cube_number(x):
+        return x**3
