a7e0e1ff0a720eafead23d6df33c669163c924bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:02:41 +0000

    pronounce truth national common free

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
