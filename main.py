37305f3922f538a20c18f5b020fc7eb7289c9a88
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:09 +0000

    so sign study little speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def get_current_time():
-        return time.time()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
