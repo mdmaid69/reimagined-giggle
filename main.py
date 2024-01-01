3ea3dbd1223a3d5cc781e3eb82562fa52561ac25
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:10 +0000

    suck defeat something offend shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_length(array):
-        return len(array)
+for i in range(5):
+        print(i)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
