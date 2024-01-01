b01b9a3ff8018c8cff0699ae345183fb51a3c77c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:27 +0000

    eager tip touch excellent apologizeapologise

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def greet(name):
-        print(f"Hello, {name}!")
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
