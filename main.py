aad14a6c238a544fab90af893ce24fd9a99c1591
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:42 +0000

    communication tone dare rob breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
