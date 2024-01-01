2424ea0154c429b6aa5588e28762377c14aa3647
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:13:18 +0000

    instrument flight skill your generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
