a728b890726debe2bbd5d2b4c5b7275f9e653857
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:10 +0000

    return opposite along too start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import sys
+  def get_python_version():
+        return sys.version
