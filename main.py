297dfecc39c22a7862ddc61f4f676fd37c8cbf81
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:27:30 +0000

    light pain print squash shrink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_length(array):
-        return len(array)
+def reverse_string(s):
+        return s[::-1]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
