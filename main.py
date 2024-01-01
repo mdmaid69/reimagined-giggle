7430a72257542991f03b00bcf9502e3f5091ca34
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:23 +0000

    patience modern climb mix ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
