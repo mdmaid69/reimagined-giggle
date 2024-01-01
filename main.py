7194f52a8a320bd4453dff4c5e07d667a7819144
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:39 +0000

    traffic steam crowded truth information

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_exponential(x):
+        return math.exp(x)
