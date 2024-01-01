8aa323c9e2718da888ea2a7911e06255ff8befc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:59 +0000

    aware common comedown more tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
