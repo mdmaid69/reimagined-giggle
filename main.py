a27a4e27a178066edb43652d85224b44a108264d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:54 +0000

    switch instant present mystery rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
