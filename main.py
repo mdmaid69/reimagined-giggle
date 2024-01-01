4324adcb3b46eb185bb4cfef62f6aa69895fd0bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:50 +0000

    loud face advise separate wait

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
