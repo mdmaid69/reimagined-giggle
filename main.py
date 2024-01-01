1bf402aa3c0a90ab60d50c63241506894d0a99ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:36 +0000

    wealth thank we finally impatient

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
