2b3519cf87e4d46675d7413dcbf1a671573919d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:24 +0000

    different nigga yours left breakout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import math
-print(math.pi)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def divide_numbers(x, y):
+        return x / y
