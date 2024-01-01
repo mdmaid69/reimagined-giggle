d9ff0a49627ca1366df264ae5d5f3ed01e72534a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:38 +0000

    recent wash bend lucky brain

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
