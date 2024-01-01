66f58ff3ac69f2de5f4757a90ebbd8af2b2614c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:26 +0000

    possibility building of 573entertainment keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
