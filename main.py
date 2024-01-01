0a7ce5880ebeff6244da1bc939596ae445019612
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:46:24 +0000

    accidental Jj excited Cc exciting

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
