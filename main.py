aaf62aae65b510975ca751bfdd31b221c5e90fdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:02 +0000

    basically company affect responsibility mark

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
