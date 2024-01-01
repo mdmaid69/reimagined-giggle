e7d6c54019ba9abfdcb5e091b486eb5d59b63262
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:58:02 +0000

    anywayanyhow strong attempt least already

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-print([x**2 for x in range(10)])
+  def convert_to_octal(n):
+        return oct(n)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_bytes(array):
+        return bytes(array)
