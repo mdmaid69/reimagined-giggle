e9a3371fa396bb50b399afde1ad81a812cb750ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:21 +0000

    advise rub reply success keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-print([x**2 for x in range(10)])
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
