d7dc41753b87add239e979479c92182ded6f3bed
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:09 +0000

    tip warm wave underneath company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
