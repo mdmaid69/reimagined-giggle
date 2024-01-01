be858dfd2e01f61027b6b3c720d73d4d28a3811c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:59 +0000

    label cheap create come convinced

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
