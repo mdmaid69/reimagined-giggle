a5f4b250b7d5356b673995809874a839d3947d0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:29 +0000

    criticize related term solve along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
