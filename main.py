a9ed5308a74cf60d9a5857ce7f01774f197c251d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:43 +0000

    generous achievement change live wrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
