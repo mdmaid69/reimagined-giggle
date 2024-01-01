03403d062d909213b979037719b640966858353c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:32 +0000

    capital zero force thing rush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def is_odd(n):
+        return n % 2 != 0
+import sys
+def exit_program():
+        sys.exit()
