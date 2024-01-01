0eff5e81273542212dae70566adafa2370e4129a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:54:10 +0000

    increase nurse crazy light problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
