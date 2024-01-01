e9b12ee56f2da85330cf192179f8358a9d23e3a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:05:56 +0000

    opinion other cause wish comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
