cf8279a3aa670d54cc6550eba96317e26f6da098
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:15:55 +0000

    object world previous ourselves sort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def split_path(path):
+        return os.path.split(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
