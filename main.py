14899d80fc33b360ffe1d60f52ea96ff35404319
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:20:16 +0000

    feature criticize ready save extremely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
