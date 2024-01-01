ddfd91d482acedf39dac0527932ece097dae64ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:01:20 +0000

    apparently impossible justice with pour

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def is_odd(n):
+        return n % 2 != 0
