746ed67d321858b9d3544a1ba16894a5a729e605
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:32:56 +0000

    crash leave getup brave bus

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
