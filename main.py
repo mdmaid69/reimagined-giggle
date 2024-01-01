8c98323913ba85e0187bbd6bc634ef8d363d8f94
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:18 +0000

    loose fear job interview bottom

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
