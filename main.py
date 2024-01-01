96750e8b1e57bac87275884c12a918683560be2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:30 +0000

    any left limit black acceptable

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def reverse_string(s):
-        return s[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def remove_directory(path):
+        os.rmdir(path)
