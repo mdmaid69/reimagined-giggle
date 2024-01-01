0fbe848a4ce3507f345dd40bb1a162564e770f06
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:59:27 +0000

    finish smell classroom whatever holdout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
