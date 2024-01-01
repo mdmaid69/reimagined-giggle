48118cc52e7023fc68a9ff06eb4538706fc8fc1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:02 +0000

    many valuable sick hungry since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
