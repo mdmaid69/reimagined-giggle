a1cda43bb705912ee135307937b5a6387aa0f267
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:29:35 +0000

    study concentration shadow plenty all

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
