5da149345a44d8b9a57afa33b8135394964af135
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:04 +0000

    original minimum convinced meanwhile week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def square_number(x):
-        return x**2
+import time
+def get_current_time():
+        return time.time()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
