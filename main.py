11e411737c99ab53a82434c6ef76426f133c4b49
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:02 +0000

    advantage strength actually bang school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
