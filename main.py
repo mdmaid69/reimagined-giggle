f80c9bd1d442afa4bc1c938733e638f1351bb932
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:07:01 +0000

    could suffer show nonsense narrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
