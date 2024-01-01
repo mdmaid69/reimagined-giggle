8b480dd6146071c83c75be2f05065b7182f59f8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:08 +0000

    takeover test too challenge reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
