b135614c8f904cd25617fa3859f7d55acb45a913
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:56 +0000

    respect away row anybody free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
