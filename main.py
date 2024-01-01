8a45165a4c1e9f101976852e4c85409805312fac
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:40 +0000

    free deliver disgusting murder now

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
