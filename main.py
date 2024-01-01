fd65141738f79824c6957e36a63066a82045e065
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:23 +0000

    brother come comeoff walk gradually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
