f69677dbff3d7536d08ed1f4a0ce1cc08334476e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:13 +0000

    port thinkof shut fear for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
