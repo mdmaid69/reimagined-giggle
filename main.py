ad3f1469b6626751580ad02426ec3db324961de5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:08:28 +0000

    second steer beginning Ll young

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import collections
+def create_user_list():
+        return collections.UserList()
