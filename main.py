c2c2f9ef61ee9f91807331a3ba9ab89d740316e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:07 +0000

    challenge relative figure shame sting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import sys
-print(sys.version)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
