06942c5ab3f48b8ace79be014be7402f096f8d74
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:19 +0000

    one branch independent joke rich

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-  def square_number(x):
-        return x**2
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
