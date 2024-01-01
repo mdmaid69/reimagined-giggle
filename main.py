8967045b56b27bede342bd2793ba2f52474f9c21
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:39 +0000

    accordingto secret raise steer task

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def convert_to_octal(n):
-        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
