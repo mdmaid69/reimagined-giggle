19f3ec1de5549b86bdd1e39a583001275b2cc386
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:58:08 +0000

    friendly lead thorough talk sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
