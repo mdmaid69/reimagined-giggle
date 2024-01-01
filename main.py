122acd3aff01821a5410f2e46da1fb687df0fbbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:19:33 +0000

    takeover frame walk 564encourage pain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
