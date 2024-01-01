45f7f0eca7e7644775ead624b13b768a9c211b77
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:31 +0000

    arrangement load trade wear deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
