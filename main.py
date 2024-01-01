016730a0bf02094eb0d289aa98ed88a08b6117ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:37 +0000

    worry sharp frequent seriously its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def clear_array(array):
+        array *= 0
