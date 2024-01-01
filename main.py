05038f2f49140e811ea586b2ac59f2bbbc269daa
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:23 +0000

    light degree dull bad miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+def multiply_numbers(x, y):
+        return x * y
