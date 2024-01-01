b7030e92872e04a5dca8929063d3ef375502c19f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:11:52 +0000

    tall official free preparation nor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+print(sum(range(10)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
