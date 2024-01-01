ed145c71fa251af134a48e330848cb6f76d119da
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:41 +0000

    accidental sleep put narrow makeinto

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import os
-print(os.getcwd())
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
