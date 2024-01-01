3108f2de46ca857ac9a1ea72a23adc5cddac8a73
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:29:16 +0000

    people mend pay opportunity past

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
