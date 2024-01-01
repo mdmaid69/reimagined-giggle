b212fd80e88fb4497456a7682a486dc0788873d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:29:01 +0000

    keepup ready meet still sharp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
