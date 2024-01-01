adec5d1491ef0b38f70c79b17f47afb0c6ad5a40
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:34:20 +0000

    weekend unfortunate satisfactory boring show

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
