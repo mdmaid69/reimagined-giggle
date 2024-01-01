b1ebd9f0e5d73bbe931b1ee1734870987e182c69
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:57:03 +0000

    in watch handle pipe damage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
