17ca0e26a981c4ca5e574213d6c6e230587836bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:06 +0000

    themselves joke hurry drawing flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
