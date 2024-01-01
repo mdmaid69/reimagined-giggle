ca2633ba30245a91977a0f43f86b2ad574381e49
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:09 +0000

    standard environment plant raw hair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def add_numbers(x, y):
-        return x + y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
