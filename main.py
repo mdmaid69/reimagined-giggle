9b9e5bb72b3a715605d37f60142b5bbf1453d4f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:31 +0000

    drive thinkof trick opinion birth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
