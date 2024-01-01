27f88ed50324d6fe04e5ab8e75966c6f134a44e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:10:19 +0000

    battery regular suggestion touch science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def convert_to_binary(n):
+        return bin(n)
