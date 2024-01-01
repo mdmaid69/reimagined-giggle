b22d496f8f408ca076a78150451029b98e4b8577
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:16:54 +0000

    platform look unlikely brush far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
