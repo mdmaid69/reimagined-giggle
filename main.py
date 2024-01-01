ff7fc15ceee453e0cb66a2bc0f7cb3aaad296be2
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:14 +0000

    convince fact form exact calm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
