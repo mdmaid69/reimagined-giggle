aae788471c324353e135d29657ffc7c8f9c105ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:02:47 +0000

    material we bored blame Ii

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
