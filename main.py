2c21d904defd27087cf7bc4d4e48e184051cd893
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:59:13 +0000

    holdout account afternoon colour mind

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
