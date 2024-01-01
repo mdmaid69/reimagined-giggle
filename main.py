ea7280405ce306e313968d8d955cd0e8512c32fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:09 +0000

    present term praise suckme shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
