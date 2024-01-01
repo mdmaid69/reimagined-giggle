ea5ef8d9d7ad1fb760c68579b61ce68ebe2e4dfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:04 +0000

    pronounce sweet regularly carry town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
