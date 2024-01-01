801aee466c1476eea21c4a028e4e5cf9bd238f5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:19:04 +0000

    motion entry risk radio spin

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
