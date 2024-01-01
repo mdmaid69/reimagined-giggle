313d0adba58cec927e033af6d4b57582ec30c8bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:36:39 +0000

    afford letter bright state joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
