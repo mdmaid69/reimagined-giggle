b02259b06d24d10d4e396da7b54751acb95c845c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:48 +0000

    second term direct sort emphasis

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_user_list():
+        return collections.UserList()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
