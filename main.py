7b56ded55deb182061e8994869fc5efcd9917696
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:39 +0000

    uncomfortable trap comfortable attractive comeround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
