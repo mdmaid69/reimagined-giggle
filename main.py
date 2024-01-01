1b3a586b874cd41156d58a3d2d18a0c1550ffb60
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:15 +0000

    paper suppose road hot message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
+import tensorflow as tf
+print(tf.__version__)
