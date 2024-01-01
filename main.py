370f2f8cdbcf4ead1bcf4bf1e20b788b67226aa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:46 +0000

    bend trousers protect protection immediate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def reverse_list(lst):
+        return lst[::-1]
