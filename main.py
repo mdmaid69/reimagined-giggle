890c14c0ba68511f8b87379b8f84e9e532d14828
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:10 +0000

    neck off begin front class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
