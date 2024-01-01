27515e89847192f8affa01fc6e40aab1d9e07eb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:36 +0000

    sleep business unit hide cut

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
