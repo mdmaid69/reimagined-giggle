5c042c9f2de9945fce0fc2fcfba2dcbb15c6c859
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:26:10 +0000

    usually if workon instead work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
