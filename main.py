71119154ae3ae0562ed8bc2999599467a3177f17
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:31:16 +0000

    desperate somebody lean concern practical

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
