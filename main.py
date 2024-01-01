861d636fe07dad801475f2e9d31e02a65d6f4713
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:32 +0000

    dig rough 179bite afford surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import sys
+print(sys.version)
