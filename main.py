753a2f43df829379ec2da8350d832d3eb64edb9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:29:17 +0000

    little friend narrow base before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
