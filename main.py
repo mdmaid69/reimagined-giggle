9ca84baea025be36cfd37baa42da6109ab6736f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:04 +0000

    computer plain deliver apart we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
