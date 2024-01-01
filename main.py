5b6a7032fe4f5f07c691fb8e01185bb146ac8c2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:36 +0000

    children clever expression finish get

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def sort_list(lst):
-        return sorted(lst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
