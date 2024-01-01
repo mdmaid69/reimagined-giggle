c1100625e1a5942735de69f9ebd3cdc8d94a2127
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:56:31 +0000

    bound miss prize stop face

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+for i in range(10): print(i)
