0043454db895979a894e9ebb6237e47eb74b326d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:43 +0000

    traffic formal loose less steady

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
