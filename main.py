08e150729f0bb2de8e79e106a2f29e59126c1bce
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:09:49 +0000

    visit perform advanced original whistle

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-print([x**2 for x in range(10)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
