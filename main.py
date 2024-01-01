58ce4e00ed72e75d21c13e85736e382f63071e43
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:25 +0000

    normal telephone equipment development anywayanyhow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
