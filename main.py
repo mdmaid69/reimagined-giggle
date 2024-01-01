3aca6ecca4861f90ad8317c945f0401f6807eeb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:10:30 +0000

    obtain innocent notice trap worth

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import time
+def get_time_since_epoch():
+        return time.time()
