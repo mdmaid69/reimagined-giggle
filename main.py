5fe7b6e1532845d6a72a287b2b3939e1d03e6aff
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:56 +0000

    land defend radio tone permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
