a78ba45c5e43e9fa7f93e51f4a0829d1208bb4e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:47:38 +0000

    hope forever straight although choice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
+def reverse_list(lst):
+        return lst[::-1]
