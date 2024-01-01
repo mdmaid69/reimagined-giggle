312c5515c68096e03d5f3c56458f5030ab132d8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:29 +0000

    mistake reach able no scare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
