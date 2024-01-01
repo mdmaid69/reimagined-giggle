f0154a7b5c0a9d223a8b69408a91c1fee1e3bb5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:36 +0000

    giveaway word to freedom damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
