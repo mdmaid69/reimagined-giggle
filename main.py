8b5cbca989fe37454119afe3caeb29704a9312e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:50:14 +0000

    direction snow train press concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
