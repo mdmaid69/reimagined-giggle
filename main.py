1b995c1df9e645db51a1dfb6b3696b16d134330d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:04 +0000

    subject dull freeze form odd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
