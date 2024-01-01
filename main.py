f0d4a7874c0b778a7d152c5d3ed3d2a977d322bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:53:23 +0000

    space point left piece summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import sys
-def exit_program():
-        sys.exit()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
