e129bd06407abf4d269e66465f980f42744013eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:53 +0000

    protest borrow workon practise build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_acceleration(speed, time):
+        return speed / time
