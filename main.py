82286b7972ffce21b70f4d8c4c51df872a5df8f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:14 +0000

    example crazy yet organize must

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
