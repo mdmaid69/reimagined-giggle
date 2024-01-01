0b447599f68f38cd577e6ce4a8dae92f888f6eb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:30:08 +0000

    exchange save difficult loud shame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def square_number(x):
+        return x**2
+import re
+print(re.match("h.*o", "hello world"))
