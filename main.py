6dd30d65de810204f36d39dfa9ba537c09fe7aba
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:05 +0000

    equipment little please back print

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
+def convert_to_hex(n):
+        return hex(n)
