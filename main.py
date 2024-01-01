e972c96c9dfa2869d48336e5e6d52a6e11e6c6bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:52:45 +0000

    skill confused past care average

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
