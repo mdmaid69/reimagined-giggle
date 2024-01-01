9f3de72d6bf055d93edbf2d6ce9a43eb49e0d4f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:21 +0000

    confused downstairs emotion clean trousers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+print(math.pi)
