ecbb4f1bf18c2820b465674a683b5a16c8238d61
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:05 +0000

    whistle ashamed successful sell car

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
