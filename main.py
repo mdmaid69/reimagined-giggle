340d8c49702e881a5ed00bd47c018ecf1fde7fee
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:07 +0000

    injury confused close everything generally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
