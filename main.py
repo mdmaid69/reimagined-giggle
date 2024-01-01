e202b9c30f99a5d2c37df80bac56027293077bea
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:47:37 +0000

    practice immediate unless autumn black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
