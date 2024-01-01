e1939e0ee3bb48c08df2ca77cdc85ba4a45fc4f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:39:01 +0000

    harm sweet can ours note

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def change_working_directory(path):
-        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
