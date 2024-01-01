5050882d4cbd4d66791fd5ce42d0372778fc705e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:09 +0000

    name voice container concerned feature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
