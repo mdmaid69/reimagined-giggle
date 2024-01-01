bbd7f8121bf05590e41b45f38f84cf9122f68e38
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:38:46 +0000

    sure interfere 569enjoy unit guide

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
