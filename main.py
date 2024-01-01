1c34984e2533db5c06ffda0b919686974452c533
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:16 +0000

    boat sentence giveout combination hard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
