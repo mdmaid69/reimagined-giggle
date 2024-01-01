7266ced9b650ace9f1d65b01687aa3e76d0c54f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:56 +0000

    justice succeed black comein less

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
