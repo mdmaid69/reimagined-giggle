bbff1027ef6d04c0370f5a067b6db3dc16b5d46c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:11 +0000

    dig sound wealth convince giveaway

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
