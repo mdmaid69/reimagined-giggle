23cdee37ea792c35587ef50c031e1ed321b451f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:47 +0000

    who join smart stay light

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
