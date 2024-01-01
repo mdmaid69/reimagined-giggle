d1256ddc40b65ef2ba1686c265eb46a839527cd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:41:58 +0000

    responsibility murder empty complete game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
