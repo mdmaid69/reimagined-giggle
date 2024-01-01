4a261c896261d8d04ff0d0116fd26fcceaee023f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:43:08 +0000

    winter nowhere control content comeup

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
