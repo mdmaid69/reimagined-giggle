60ff6c71190c4e8b6b045c0c4828fc0d5607fa22
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:07 +0000

    apologizeapologise one above female breakfast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def is_palindrome(s):
-        return s == s[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
