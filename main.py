4c8490a8b3ee478d52fb8ce0c3d70857e2390085
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:12 +0000

    smell wake impression empty brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_rectangle(l, w):
+        return l * w
