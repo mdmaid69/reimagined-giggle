c46bbfc87cbbefef201e0a709ae361091beac419
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:30:10 +0000

    undo return comein finally whom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
