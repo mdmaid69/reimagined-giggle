58e9d5bcf81939a90b7d992ffc75509b1f9538ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:32:49 +0000

    takeaway warm bet apply risk

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import datetime
+print(datetime.datetime.now())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
