eef03ddf72d281ac71f95343401a4be296c17fc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:32:01 +0000

    act connect character perhaps window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
