faa67150eecf4a4acebcf27acf8c3f04e14064ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:24 +0000

    us lose level flat pour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
