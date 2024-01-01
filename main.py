1a2faafeb6574b6afae21068f9940f08155c3687
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:23:04 +0000

    cause advice satisfied lucky path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
