3938aade8cfdba65b3658ef4a7cc63d17e7c1b67
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:19:10 +0000

    form central thumb ahead hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
