c5c45ef801caba94e78a7df2fc20e73299235d30
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:45:27 +0000

    tear track just measure this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
