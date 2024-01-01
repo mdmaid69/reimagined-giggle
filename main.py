7bcaccedd7f4cf66772b1acdf989c055caa20b66
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:02:06 +0000

    blow turnaway meaning complain hurry

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
