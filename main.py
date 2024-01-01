941aac84389939553f5a3e12918385011f1b7aee
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:48:48 +0000

    meanwhile neither plan visit translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
