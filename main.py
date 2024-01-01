30c92f3f74ccb33227c8a75f34bf2ba529a331a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:49:10 +0000

    medium great touch attempt comfort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
