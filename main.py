29a370f0508248dd0d28487a3a6e7578ce8627f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:40:58 +0000

    circumstance tall top turnup artificial

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def convert_to_binary(n):
+        return bin(n)
+  def remove_duplicates(lst):
+        return list(set(lst))
