f513bf9b5ffcd85af19065fc59c930b57be887d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:41:38 +0000

    interfere opponent exciting enthusiastic moon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
