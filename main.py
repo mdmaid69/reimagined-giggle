4fed4d41152ee21e530826275213ca12aab60d2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:57:49 +0000

    flat course body failure reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_user_list():
+        return collections.UserList()
