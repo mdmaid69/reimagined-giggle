116dbe53c45b8d738ef9c461a5ac65d6036691fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:19 +0000

    contents science run listenfor recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
