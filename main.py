ab0ca339ba6973160252dd36d5ddca6de34f1e8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:54:37 +0000

    open area only human often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
