0ccb22a1355b9b51b88d0a9a4ddec38ed0a0a6b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:25 +0000

    large spite present shame wherever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
