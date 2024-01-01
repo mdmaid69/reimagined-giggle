c1c5be5ea426432cf34163dcf691aa8eb98615e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:52 +0000

    operation once win willing able

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
