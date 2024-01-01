774ff541718dd6882aa26b291d8afc511a07452d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:07 +0000

    path definite scared scheme depend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
