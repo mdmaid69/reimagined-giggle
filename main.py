81cb842389c8891072d39eb4e74b99dd10b06a32
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:35:58 +0000

    expression aim impatient small lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
