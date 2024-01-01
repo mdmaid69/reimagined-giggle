d2eb3a797ef2b6e365dc604a5ebeb42fb18d537b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:49 +0000

    ocean actual town personal foreign

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
