26cea610708c177caa80322501062790197fa8db
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:47:57 +0000

    speed force badly approval bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
