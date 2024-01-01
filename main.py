31210bed6b860b141bb4ccaa68ccec1b03990abd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:53 +0000

    then load raw price frighten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
