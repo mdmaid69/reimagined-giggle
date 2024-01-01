adce920b8ebdff246ffafe8061a1469594f50ab4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:55 +0000

    declare would next discussion guess

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
