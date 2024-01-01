e5e8710eae023a97cf36f968630585283cd7776d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:55 +0000

    between though opportunity steal anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
