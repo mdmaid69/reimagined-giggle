c434533cf2e6cd9b674b447a28a5d9bb0ddbf2f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:38 +0000

    everything regularly later glad need

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
