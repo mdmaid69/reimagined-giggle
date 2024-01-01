c0a8fd9818ede60acd23f0730e83f31a0f54a144
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:14 +0000

    quantity conduct while file keepaway

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
