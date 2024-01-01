8c6ca652c3df7d9d946da1e374f4d8b636b206c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:52 +0000

    back admit suppose shallow typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
