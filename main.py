187aade86dce23ec82c36814c36acd71be5b23a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:09:22 +0000

    knock successful store responsibility authority

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def square_number(x):
-        return x**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
