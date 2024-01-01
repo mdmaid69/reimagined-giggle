63a250ed9a105808ee30e23f64cbb0fe30cffb92
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:35 +0000

    interrupt centimetre giveback class whole

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_item(array, i):
+        return array[i]
