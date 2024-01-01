ce51d774d79d52effde3eed3d5801b9d45e65496
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:10:17 +0000

    ashamed laugh train morning word

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
