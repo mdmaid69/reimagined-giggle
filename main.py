0cb3ec606cd298fe4082dd11a7315a11cc7602e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:37:18 +0000

    cry mistake skill set means

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
