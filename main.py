82d8ce4a71dc9bead4ce1f2f82456b6f55c0a358
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:40:48 +0000

    choice regret guess too taste

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def convert_to_octal(n):
+        return oct(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
