014016c34acc213c0ef4ceb200abd27481f951ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:49:16 +0000

    straight instant expensive almost stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_priority_queue():
+        return collections.deque()
