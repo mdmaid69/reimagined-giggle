548d3e73e66c7433ad0a27d65c9ffb22426a4c32
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:02 +0000

    into detail test central secret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def square_number(x):
+        return x**2
