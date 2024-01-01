836ad7d10f345aee64ea52a73a35b58d0b71a9a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:26 +0000

    confidence flight foreign side transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
