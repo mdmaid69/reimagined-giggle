05217a50156fc0ca823e71710fcec294a0cdb84e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:02 +0000

    free smile sorry pump accidentally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
