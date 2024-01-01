d6e2a99fbe580cc08af4f1657f68422f08e3231a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:12:11 +0000

    sick touch object move thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import getpass
+def get_username():
+        return getpass.getuser()
