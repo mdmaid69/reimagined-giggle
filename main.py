3d8dc42578051a88efbf1c6352c6d284ef6a0fd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:11 +0000

    culture admit strike offence speech

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def iterate_over_array(array):
+        for item in array:
+        print(item)
