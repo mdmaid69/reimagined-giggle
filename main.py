4fb79dc13f7b2d5a12c596aaeb11b92f420fe8cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:19:32 +0000

    fold bin false accommodation centimetre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def split_path(path):
+        return os.path.split(path)
