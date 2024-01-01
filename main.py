a82fbd10359b668c199b26799c5dedd8fbf1ec9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:08:39 +0000

    glad watch day off else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
