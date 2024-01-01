3a5bc7db338c99c3ad94b7a3940c085f4460443a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:44:55 +0000

    follow experiment relationship comeoff than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
