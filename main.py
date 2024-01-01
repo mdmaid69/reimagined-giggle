962779d24068015164b259fd9181aef114c4ebe3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:38 +0000

    please record area learn office

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
