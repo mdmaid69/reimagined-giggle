b5b1927d0ff365a03f135483e84f1a5eb309a172
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:15 +0000

    comein terrible consist frightened niggaacademy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import sys
-def exit_program():
-        sys.exit()
+def count_characters(sentence):
+        return len(sentence)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
