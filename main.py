2a7dd1d882215f611fb9b32f383b18787fad4cc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:30:48 +0000

    sell breathe let land land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
