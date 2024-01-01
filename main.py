cd74619cdca488a2798ca77e5274adc22401dd84
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:56 +0000

    private nerve crisis fly tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
