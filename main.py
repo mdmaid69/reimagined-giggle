2861acc18e8672a7c73bd97580f4677ecb27f5d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:58:58 +0000

    excellent thirsty climb capital point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
