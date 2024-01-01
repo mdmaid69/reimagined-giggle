3f21e1a660c03d7e120caa5b8d94a20f1f06133f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:28:15 +0000

    compare indeed smell scrap laugh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-for i in range(5):
-        print(i)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
