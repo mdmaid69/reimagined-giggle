e02558fa9d9d04e6d39e7e3cf2efbbb15af2164e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:07:19 +0000

    outside appeal stir container down

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
