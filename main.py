ae9cee5620c53c81fc13ade073d662826669baa2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:42 +0000

    once university needle building sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
