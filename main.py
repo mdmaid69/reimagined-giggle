743ea677cefecdb0589af2a9e819d6b7ebb6b44a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:32 +0000

    interrupt scream never normal profit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
