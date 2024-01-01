756d71e097e4df405e3c8579e105358e36f49f44
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:03 +0000

    matter offend sentence every partner

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def add_numbers(x, y):
-        return x + y
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import os
+print(os.getcwd())
