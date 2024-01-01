f95e06f8aa27a8b7fcb864bf8e45bfc53af4b801
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:33:13 +0000

    scrap game cause tune snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
