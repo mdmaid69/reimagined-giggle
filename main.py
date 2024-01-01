8b6bfc53c4c0b1c5c9472d5ceefe765da5661398
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:57 +0000

    sister large attempt estimate genuine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(numbers):
-        return max(numbers)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
