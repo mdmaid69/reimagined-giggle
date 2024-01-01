e68ced61c83fe0026fbf99a7fda5bda457951cba
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:50:41 +0000

    unlikely naturally old listenfor hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def delete_file(file_name):
+        os.remove(file_name)
