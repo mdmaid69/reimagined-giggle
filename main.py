eaa81038efb946239db38e92128cdf0340f7fb8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:52:28 +0000

    dress game sharp limit detail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
