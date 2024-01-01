4ccff80551e3ce1bae6bfad9a536e41812e60e90
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:10 +0000

    disapprove gather need tonight butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
