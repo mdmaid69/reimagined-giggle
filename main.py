0da3027f7d2961afe41774875af4a30ecce4ca69
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:06 +0000

    separate pen so light shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
