1eddcf02aab57465cbb97975d9af9a23a2bb6660
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:44:55 +0000

    include only concentrate bang near

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
