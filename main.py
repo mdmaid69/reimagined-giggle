208c64344c5ba7219a2bceca107e707cf198c7db
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:13 +0000

    exist like answer frighten wander

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
