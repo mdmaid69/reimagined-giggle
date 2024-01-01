fe9dcec701112845b925f98705731ba3a15239ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:02 +0000

    class realize comedown temper bet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
