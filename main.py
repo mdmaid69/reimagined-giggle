a9aa97cdaf5b710db8be8567e5395d955ed10548
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:44 +0000

    cheat trousers suckme fly remember

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
