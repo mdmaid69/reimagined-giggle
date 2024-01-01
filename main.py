0a2f4521eec370eef72914c24a26deebba5d9ef5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:26 +0000

    number sort draw judge enthusiastic

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_pressure(force, area):
-        return force / area
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import time
+def get_current_time():
+        return time.ctime()
