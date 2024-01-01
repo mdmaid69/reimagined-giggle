7087c4decd708b366fb48b51af5aacc90cf9b412
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:00 +0000

    our few sound run brave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_power(work, time):
+        return work / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
