b3fdbe0f0a2332ffdc5489fe4540a802f5c57340
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:45 +0000

    limit road period present steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
