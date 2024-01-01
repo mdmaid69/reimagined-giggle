ddb4f7daadc7c7e45cc6e2d20149192460396980
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:04:19 +0000

    brain workout giveback flavour else

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
