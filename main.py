9149aa0006bc46cea012b8332ee16abfb348a54b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:47:18 +0000

    straight come check extremely free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
