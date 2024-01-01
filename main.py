c125637c14f5f33a3eec4d73704ea68a237ab075
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:22 +0000

    clue emphasize backwards rub think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
