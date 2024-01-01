2bacf2413c4fe484d428f89d29aa80fcfee0b391
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:34 +0000

    whether nice bit care wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
