ad10600b18662fe5adda2ec0e4fe51427fa9089a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:45:29 +0000

    our them because trap fit

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
