f8951468cc430f4d90ab05e9f1f067cab26f6ddd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:29:08 +0000

    have their path store manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def get_array_item(array, i):
+        return array[i]
