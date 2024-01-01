f25ebb1601b196dbd40613328dac19161e2c3f8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:46:01 +0000

    will wherever progress interview branch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
