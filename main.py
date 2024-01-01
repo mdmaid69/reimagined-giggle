99a930c6c3ef5c65d73b6f2356e316460fcafc3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:33 +0000

    nothing distance used limit poor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
