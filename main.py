a903f6a6d0a93f238904d82388ba0b6c6b0a4c46
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:47:59 +0000

    alone development depend many pick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
