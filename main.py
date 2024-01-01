87735a66c04cca4a99fc599bca3846b866eb19ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:02 +0000

    basic regularly contribute need pour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
