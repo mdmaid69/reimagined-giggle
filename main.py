4cac85ea7778cd3ddf6a80974bf12784200b54be
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:00 +0000

    release today bye why seriously

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
