0c5a5795eb53cf37fa465e081724d60be6662ccf
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:08:12 +0000

    shallow depend spare tend today

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
