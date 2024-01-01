7af1ab88240ecdc0cdaffd5e5fdd748747e68119
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:04:38 +0000

    clean confidence start holdup however

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_power(work, time):
+        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
