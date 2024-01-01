2f9dd2fad8be56f4ccddf275c62ecc2c4597d49f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:23:12 +0000

    force general gather Rr short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
