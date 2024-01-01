05cdb83b410104f0a804e98f1f37289bb6e44b53
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:39:45 +0000

    spill Zz shock long aim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_max(lst):
+        return max(lst)
