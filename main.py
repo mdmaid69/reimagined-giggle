2e359989504b9da93549471ef37887d3d8ed6b09
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:48:41 +0000

    upset wet ear fall flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
