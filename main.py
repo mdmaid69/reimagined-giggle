aef743ae0d2f35b2176b3d2e90b050c3ef8ea34f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:47:19 +0000

    young arrive mad interview tough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
