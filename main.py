bd4a514df45015c9153685b032dc2a1c86f07c6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:02 +0000

    deal race apologizeapologise star pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+print(math.pi)
