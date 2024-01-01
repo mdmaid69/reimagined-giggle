75f9d5f5de3ca85e2ad42c1c3fdbbfebe5e63f57
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:29:30 +0000

    special rain replace occasional square

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
