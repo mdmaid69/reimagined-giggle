4c27b78b4e9522998903e64603c7fc850683e116
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:30 +0000

    performance passage rare continue spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def subtract_numbers(x, y):
-        return x - y
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
