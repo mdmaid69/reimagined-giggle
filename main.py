2f6e73d85ff46ff8d0279d4e4a845b3436370239
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:02:10 +0000

    moral wall Kk culture reasonable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+print(math.pi)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
