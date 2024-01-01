a9fdef162372a9ec6c557b6b6682531d535879fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:43 +0000

    chief nearly form beat along

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def subtract_numbers(x, y):
+        return x - y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
