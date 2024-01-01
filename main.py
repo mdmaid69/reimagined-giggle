0755df3beecb6633a8bf784c7a2d470d5b49f601
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:01 +0000

    holdon conversation failure curious possession

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print(math.pi)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
