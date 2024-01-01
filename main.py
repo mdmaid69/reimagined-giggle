34403c676551c1c79694e1104e9d261a0d1fa8d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:44 +0000

    hers runout cool either greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
