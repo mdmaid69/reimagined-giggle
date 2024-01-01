5d073509cea5c3c60cff505dcaf71c71ca4d345f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:41:20 +0000

    imagination failure excited yes tune

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def clear_array(array):
+        array *= 0
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
