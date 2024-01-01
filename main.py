c6df2954415f951d28f6d78462ca85cb8787b25c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:01:06 +0000

    father reject bath storm lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_set(array):
-        return set(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
