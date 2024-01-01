99aded196bb78ddf8d3cee1aaed41052cb88f312
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:00:20 +0000

    respect indeed along I obtain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def subtract_numbers(x, y):
+        return x - y
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
