9e4a52d8ad75298ad9b5b79d23e3abcffe3be42b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:14 +0000

    Ll power before day speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
