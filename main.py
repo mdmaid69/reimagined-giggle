f61bf811c785984f7cf4d4d2e1c05421a16d99dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:46 +0000

    slight wave high confused health

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
