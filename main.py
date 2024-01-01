58c3e347ced9fb95d11bf022a935f2d1b44207cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:08:35 +0000

    crash ashamed suddenly separate do

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def reverse_list(lst):
+        return lst[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
