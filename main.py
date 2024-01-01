e909d1a3bfd873c0bcb30d88e85aef3a5e2c7b7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:50 +0000

    evidence what dance doubt common

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
