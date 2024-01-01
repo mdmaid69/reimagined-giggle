79cce7eca096eafe98b06a132fe1ca17a0f76c8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:32:51 +0000

    sale destroy damp university opposite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
