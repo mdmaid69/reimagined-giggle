e3c9ee003b632353f17f8ca16d53a2462a58aa16
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:30 +0000

    kind us pain sorry partner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def greet(name):
+        print(f"Hello, {name}!")
