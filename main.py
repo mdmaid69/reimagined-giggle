a0ecf69324a813ae6655267a50ead3dda9eae1a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:56 +0000

    travel breakin admire pity treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
