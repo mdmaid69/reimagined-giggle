2d8eeab1f9dc2fec12866cda10c83fcc8bae44bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:51:56 +0000

    crash fire male breakthrough fresh

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
