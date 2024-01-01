39cdfb85f45a7470e4b5b9d330daac102add97cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:22:26 +0000

    mistake wing opposite available range

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
