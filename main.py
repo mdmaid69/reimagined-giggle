af01481e5a9e91f00e7998de8c007e4a20b99f0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:03 +0000

    quick insist furniture draw consider

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
