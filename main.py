a543fb4c15b30cbb421b6cf17801715398ecc50c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:57 +0000

    swimming prize available poison block

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
