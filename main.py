4e47a0ec9ee73dd7ce2018b0392d20bbeabecca2
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:46 +0000

    hour marry television enthusiastic life

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
