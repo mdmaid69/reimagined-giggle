9fc50b43bbd341ade05bc9327441d29de77918b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:35 +0000

    left school bringback nice sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
