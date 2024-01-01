e0e12f5dfed2f18302d56d842d14cc02948c81c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:07:35 +0000

    approve wherever play danger share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
