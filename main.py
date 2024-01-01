d05e2ce63d7b161b2e827c89ab4333831d8100d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:49:58 +0000

    unusual soft comein single mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_index(array, item):
+        return array.index(item)
