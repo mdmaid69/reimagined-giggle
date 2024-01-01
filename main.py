61c2cd08a5d90fdae16cadc425ec47c128477342
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:34 +0000

    business frequent human more fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
