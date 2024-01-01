dad432e2fcd1a04a0221b555ff7e7aa595e8759f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:51:10 +0000

    pot business damp sticky break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
