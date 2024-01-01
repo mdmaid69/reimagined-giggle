cf5993e236e5bca26cecba8d433a2e82e44e2e88
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:40 +0000

    charge borrow far tired use

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_min(numbers):
+        return min(numbers)
+def is_palindrome(s):
+        return s == s[::-1]
