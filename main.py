7ff15d8d7010051d05ebb24f7d8eebc07f1c056d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:54 +0000

    contribute man trap look gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
