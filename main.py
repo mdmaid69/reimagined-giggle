4806a6a05362688b6455e6933e83db1394f3e3ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:50:27 +0000

    quality odd throw platform weigh

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
