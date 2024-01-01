ef25972cd75a76313d07be87f4f03365c8a8a4fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:50:12 +0000

    possession trap attach famous count

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
