af99fa76aacc07ffa18273e0e6d95b2f433f45eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:35 +0000

    miss scream challenge watch border

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import numpy as np
+print(np.array([1, 2, 3]))
+print(sum(range(10)))
