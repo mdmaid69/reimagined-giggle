b5d6a4300bd3ba7f4e218c1fd0311c74a48c70fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:43 +0000

    try afford rush target we

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
