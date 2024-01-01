d1647d90dc5300820b93ce8f8543bf5a48bbe699
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:34:16 +0000

    card pain fire interfere child

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
