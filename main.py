078bf36f808c0bdff0ca27d34ea8079e6990f3f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:09 +0000

    start much destroy turnoff sharp

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
