a531ec2d49c8b4c396cee3e5cd07de95a8580de2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:37 +0000

    payment sort black characteristic cough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
