8bec991cd43ada49d1c6380f3f3a06b6d1357bbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:13 +0000

    base pain conversation negative measure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_typecode(array):
-        return array.typecode
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
