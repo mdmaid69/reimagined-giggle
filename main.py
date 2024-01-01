2e80b6f46bd63705440dc5fa2aa57efb75681952
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:28 +0000

    swing operation mark yesterday love

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
