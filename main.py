b320480de12d2e31b27a1bc1815bfe45a1fb1ed4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:26:41 +0000

    train spin again tradition top

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
