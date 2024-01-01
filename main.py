44ac24d786b8cfb9e164b1cbbc8451ac4ab8ab4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:20 +0000

    house join obtain work Dd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
