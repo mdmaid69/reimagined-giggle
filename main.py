2cac76c45ce1460dd2b9e1d80d8236f6fd8a53db
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:35 +0000

    use professional formal burst strong

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
