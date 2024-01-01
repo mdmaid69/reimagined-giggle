8ae8fa6c2612d0f7889c3d075f8ab7748cde763f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:11:52 +0000

    lonely doubt pool whichever business

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
