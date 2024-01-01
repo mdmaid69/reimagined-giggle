dd9c1b40bde5055acf77fed3bfa205886bd4b2e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:17:31 +0000

    reply bye discussion debt represent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
