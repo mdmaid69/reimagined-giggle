f6aa05c2ec85aeab013e1d613ca3573dcad2254e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:35:41 +0000

    tend achieve term sorry steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import random
+def roll_die():
+        return random.randint(1, 6)
