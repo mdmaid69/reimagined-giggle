a67d31f6805852e65fde01db15dba4cf60bdcf9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:02:14 +0000

    object relation number opinion tired

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
