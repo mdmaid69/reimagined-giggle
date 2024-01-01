e108a5c9393afadc91b890ef0a22d48827e72207
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:07:17 +0000

    passage treat so scheme for

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def find_union(list1, list2):
+        return set(list1) | set(list2)
