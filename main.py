b9f55f621bd1f573029ef7d1e1145d6469720c86
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:50 +0000

    support imagination manner stupid lesson

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
