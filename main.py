c76438cea7e17efb7b74b3bb872b8c379f90f4ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:17 +0000

    disapprove paper inside straight happen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
