0217b4365fce60517fd24258e050de9b0a636ae4
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:00:15 +0000

    nerve responsibility pride drop shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
