7bba13d3ec322e1dd51674508159715f74eab52c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:30:42 +0000

    taste best probably giveback sink

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_typecode(array):
+        return array.typecode
