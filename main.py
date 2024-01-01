24977f7f64149c55a756415c6d3c832bcf7fd25d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:37:58 +0000

    abroad anywayanyhow lookback(on) light love

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def is_odd(n):
+        return n % 2 != 0
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
