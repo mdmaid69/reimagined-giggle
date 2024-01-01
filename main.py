5ae85a7b4da1855ce3812870fe677227e1577ef1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:39 +0000

    wild authority argument force cheat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
