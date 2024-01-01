a04a3329301bf21534838aed53bb145fe0808f86
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:07 +0000

    anyone intend allow size turnover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
