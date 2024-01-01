64e81194d96fa2bd4f668ccb0e133695b96b2791
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:06 +0000

    getup defend gate pour content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
