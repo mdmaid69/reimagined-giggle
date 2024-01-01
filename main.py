eb9be9714b8af56a0cf9b8d6288a6fca04fbc910
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:24:28 +0000

    call moral sort film common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def is_odd(n):
+        return n % 2 != 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
