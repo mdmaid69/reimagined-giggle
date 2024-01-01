d3fcc324aa7fddb37d9168248bf4705b2650eac5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:14:28 +0000

    Uu morning increase either but

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
