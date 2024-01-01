ddabd8a706ef04871ce097a222961398831bffad
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:35:41 +0000

    drop past habit gas lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
