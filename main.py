a5d27a34b05c4c9033f2a105cf9763d578defcb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:51 +0000

    breathe praise record careful march

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
