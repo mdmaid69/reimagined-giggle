c79a0cf51801574c9a61a2e4618ef05dcfccf9ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:18 +0000

    bath suddenly book distant reliable

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
