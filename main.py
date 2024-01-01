0d2160a5ee0b35abdbb445afb013a52ae9cb702c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:13:05 +0000

    forget moment family spite poor

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
