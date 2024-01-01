535d5199719be46bc66a45c502fa01939327ef09
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:14 +0000

    slowly explain giveaway building flow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
