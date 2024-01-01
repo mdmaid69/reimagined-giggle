903e35ac931101b2d03f8b0637082c7cf59af639
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:11 +0000

    government minimum lead peace help

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
