b0743926b648a9845d8e5b70368978a3f6359a5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:51:20 +0000

    intend instead prize dish list

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
