6cab9b2e3866327b7eb77fb4533c787bf79a7d16
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:48 +0000

    pure safe exchange search please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
