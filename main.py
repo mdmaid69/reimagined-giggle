91849479a7f34b0acaef91d0e46328bce1a14350
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:30:59 +0000

    around can introduce callback convince

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
