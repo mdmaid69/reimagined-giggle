2317f27e7b876842a67075e90218e2773a933639
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:07 +0000

    we put familiar bath family

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
