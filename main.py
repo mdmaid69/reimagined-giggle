b0e85ecf28f60982d188a259ac43df33048b72e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:13:46 +0000

    pause clear regular food behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
