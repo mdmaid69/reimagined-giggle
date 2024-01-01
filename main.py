aa50899f57642e05eb0259208379d7781d25ff02
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:08 +0000

    meal sound heat slow skill

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-print(math.pi)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_index(array, item):
+        return array.index(item)
