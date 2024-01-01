38cb4ed656c78ad056cd47a4919ab47d856b1be7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:30 +0000

    relax bread himself finally consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def convert_to_binary(n):
+        return bin(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
