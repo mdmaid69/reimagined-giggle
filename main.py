299960a2aa22401d3cc9dff89bc795f8da0a8548
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:18:22 +0000

    comeup butter twist surprised lead

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def cube_number(x):
-        return x**3
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
