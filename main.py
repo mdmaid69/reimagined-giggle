18256c0039509d5387043289dec3cc01d691dbda
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:22 +0000

    means interrupt command mark attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
