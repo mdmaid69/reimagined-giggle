25f9a8dab33ae642e8e4b1f252c986f428a35a6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:33 +0000

    continue check message begin belief

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
+import numpy as np
+print(np.array([1, 2, 3]))
