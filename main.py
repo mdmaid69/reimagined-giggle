6e9fbb5d5e4c0c358bcff008788b741044e11434
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:08 +0000

    cough equal port will remove

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
