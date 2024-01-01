151f93450bf68030edcb99b0d540284d29b49fe4
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:29 +0000

    gas view rescue mean on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
