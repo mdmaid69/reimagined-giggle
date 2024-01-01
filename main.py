2a5748e43fd1d874edd3c6c99f63013d239edeb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:44 +0000

    good might through pronunciation ever

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
 import array
-def get_array_from_bytes(bytes, typecode):
+def get_array_from_file(filename, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
         return a
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
