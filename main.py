5140dbe868764c34225f6e6fe0415fa7074124e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:12 +0000

    trip save study occasional sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
