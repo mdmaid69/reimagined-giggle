aac5d6e2905024a4b8b253017decbf92becd986f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:33 +0000

    knowledge immediate distance head vote

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def cube_number(x):
-        return x**3
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
