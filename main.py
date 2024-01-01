cf1d286345f1108458bb6bc423738db9c1ae52ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:16:39 +0000

    provide whenever provide crowd afterwards

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
