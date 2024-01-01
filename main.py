f30f17eb6d576a132436f606fd19b4bac41e2d5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:50 +0000

    reduce variety waste remain offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
