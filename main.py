9c64f2b1b7a15c9f5717159b8058c3189b064578
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:16 +0000

    stiff complicated afraid under touch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
