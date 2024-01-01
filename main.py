630235c888d85163e6ff5e98daa3b63c75adf181
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:02:50 +0000

    bread runout shoot content shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
