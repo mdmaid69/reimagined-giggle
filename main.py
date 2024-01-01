14ef8aa2a8a8cf641861f77f19b539d0604d19b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:55 +0000

    artificial however narrow give knowledge

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_current_directory():
+        return os.getcwd()
