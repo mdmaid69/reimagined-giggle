c36e42e295eb6657f4007217ceb050635cbbe5d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:56 +0000

    realize at float meeting material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
