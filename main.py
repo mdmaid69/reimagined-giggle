a973d78d7f9d5fa85a4378d9a2c374c7930f9166
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:24 +0000

    giveup shoot crash thick date

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
