8b15cc83310646c0303c09f892efca5f0401b24b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:03 +0000

    goback(to) software clean woman very

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
