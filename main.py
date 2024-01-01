cc67d39d998ccdda980158aedf6ea57a114c3af4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:35:21 +0000

    Gg pick record slow unfair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def get_array_index(array, item):
+        return array.index(item)
+import math
+def calculate_floor(x):
+        return math.floor(x)
