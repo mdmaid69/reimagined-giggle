42c14eef33e1dabff4178c76557cf78bdf5a8f7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:03 +0000

    impatient refuse artificial strong rain

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def count_characters(sentence):
-        return len(sentence)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
