4d0342a08366f240dcc5404756d7f15c72f05a0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:10:17 +0000

    high Vv possibly takeoff wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
