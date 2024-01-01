701ea2b2147836fb86367e92fdc88a3017fc2b94
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:08:43 +0000

    please complete kind runover throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
