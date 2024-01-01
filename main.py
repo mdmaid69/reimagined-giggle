36f394ac87473d456bd0a9c313914c201c40860d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:44 +0000

    solve curious properly sure artificial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
