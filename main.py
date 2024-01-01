322432829360990ebe96995f68e8d457e842a61d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:22 +0000

    everything juice willing very pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import time
+def get_current_time():
+        return time.time()
