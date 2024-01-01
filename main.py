226199d3a7a49761a2e891bb66efb18c24106011
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:00 +0000

    trouble tool depend birthday centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
