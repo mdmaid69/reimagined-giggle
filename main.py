bd3c92a34b11b22e549bb8a5a1df8ffe2245df4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:02:08 +0000

    mixture unusual Uu draw bright

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
