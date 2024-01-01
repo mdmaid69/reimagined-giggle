89562106e37b6c12cd245f3065a471930a1610ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:00 +0000

    new drawing win hole demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_area_circle(r):
+        return 3.14 * r**2
