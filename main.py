d4f8504f0529dd29db6d125b3ef3a834b501dd37
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:46 +0000

    damage well inquire rank basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import os
-print(os.getcwd())
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
