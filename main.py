30256f3f3a933181e223556276b0daf8a3ac4f0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:01 +0000

    tax variety bury journey concerned

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
