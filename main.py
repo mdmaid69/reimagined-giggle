59ade1033a8e7ee9e45d91be1bc2f6cb9d852313
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:10:45 +0000

    recently empty to sail introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
