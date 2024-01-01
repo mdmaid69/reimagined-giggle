7dd363dafcf31b40f39fede860c8aa57ce12e92e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:47:15 +0000

    mixture top sudden every thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
