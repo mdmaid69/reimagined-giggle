0e83e762c08a50076a54f5b5f95351af2483c79e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:24 +0000

    trial announce circumstance violence drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
