ee0c72befc21f030481f2c04e2eee27748838d76
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:58 +0000

    breakinto I fast type new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
