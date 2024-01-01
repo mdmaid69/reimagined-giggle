6a70494e85327b8c9cc6392d9ea367dbeea03f13
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:01:07 +0000

    light invite sudden violence what

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
