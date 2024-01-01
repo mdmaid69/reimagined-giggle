472f12625fe06bdd0aed3af18cef30f22c29485f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:52:57 +0000

    getoff false both whole practice

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def remove_from_array(array, item):
+        array.remove(item)
