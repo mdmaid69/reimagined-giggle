bdbb8cb6183e609d303a5d5bfc5a0cbdd12f619b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:08:06 +0000

    in forget fetch only form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
