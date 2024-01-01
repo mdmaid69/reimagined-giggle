fe79aa5bec0c93fa74f464c48dd11e175484132d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:13 +0000

    cut goaway thing attention right

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
