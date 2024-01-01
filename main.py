267cd87413e6f66af54ddeb9cb42433fe4354ffb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:12:22 +0000

    565end ordinary gooff considering pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def add_numbers(x, y):
+        return x + y
