416dc078efb1761dd570dae846a6a2fdcb15bd72
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:42:38 +0000

    be bored involve afford bit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def create_user_dict():
+        return collections.UserDict()
