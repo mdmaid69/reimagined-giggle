fe501b2f066fe07d66913c1332b0588491a112aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:38:58 +0000

    equipment mark instruction blame dance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_length(array):
-        return len(array)
+def greet(name):
+        print(f"Hello, {name}!")
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
