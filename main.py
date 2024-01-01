936e9fc5fcda73bdb38ff2009f7b40a5aa0b21f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:04:24 +0000

    sensitive petrol shock present class

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import math
 def calculate_logarithm_base_e(x):
         return math.log(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
