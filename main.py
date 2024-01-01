90f4e897c765620ed73cbe4064dcf27f200fbf1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:17 +0000

    would pupil constantly justice pause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
