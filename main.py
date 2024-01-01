8d16901e20f9dfc3573ae771bac410f8af32d20e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:48:35 +0000

    exploration subject sail juice not

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def reverse_string(s):
+        return s[::-1]
