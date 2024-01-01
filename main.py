b5bd51ffb4b93163fbca4d51b3ef8f1b1638caac
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:24 +0000

    interrupt work catch grow to

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def cube_number(x):
-        return x**3
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
