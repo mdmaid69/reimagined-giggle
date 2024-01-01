6e4268ebf676bb3fe33d9b5eee91c17c02b5e2d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:43:02 +0000

    week consult should influence involve

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
