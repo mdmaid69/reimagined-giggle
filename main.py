844435a992f7c54af3e406568f3ff0ea4ae48593
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:34 +0000

    protect fast meanwhile reliable city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
