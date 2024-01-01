1c8bdf689e2ec5a7cb98fa85a36c87a03383dee6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:37:36 +0000

    through yet loan line matter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import sys
-  def get_python_version():
-        return sys.version
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
