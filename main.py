726c4cbd0cd0ac155b4944d1945355215a6f2517
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:37 +0000

    strike opposite improve moment cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sys
+print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
