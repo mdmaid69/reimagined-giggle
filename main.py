f17c882421f54030174c1367fdc754ec229cc740
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:13 +0000

    shape beyond flow praise understand

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
