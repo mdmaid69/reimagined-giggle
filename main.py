3357510e84a9bd18f0b24ac55b6c8c323d8d50d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:43 +0000

    point offer new almost live

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+for i in range(10): print(i)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
