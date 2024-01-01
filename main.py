33b8c74eafd5548a941889257382ff572b05890d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:44:42 +0000

    tend breath division mad force

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
