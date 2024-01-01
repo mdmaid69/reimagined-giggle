1ee8aa0ba72aec44ff1728e78ca0b6ceaec1d7dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:45:17 +0000

    appear frame exchange excuse comeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
