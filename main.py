1d2d4c4796addefa134a8da26298ee4cfbb13d69
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:13 +0000

    except beside company child discussion

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
