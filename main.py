204fccfbe51d8e4418d96646c7a907e1c54b5f01
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:21 +0000

    instrument hard distance target movement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
