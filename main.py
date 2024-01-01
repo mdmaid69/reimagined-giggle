5bcab95145e30d644a299c1d6a533b4778a09224
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:49 +0000

    passenger win switch fine exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_pressure(force, area):
+        return force / area
+def square_number(x):
+        return x**2
