26cb3ffdfcf16bfcbea4ff25f9e4fbf6a8f6afed
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:41 +0000

    second smell instead everyone tooth

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
