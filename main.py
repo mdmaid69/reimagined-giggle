d94c19378ae0f4f6beaf8c7a22f65105eb088f1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:00 +0000

    comment Itself illness provide sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
