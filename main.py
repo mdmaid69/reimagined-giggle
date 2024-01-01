6f6d16a76fc6cc59ff0dbd36c5207239be9c77f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:22 +0000

    save fly seriously relationship event

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+print(os.getcwd())
