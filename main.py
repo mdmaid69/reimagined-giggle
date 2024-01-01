84939c4c9042f837586acb2aa79ff8b90c2c7601
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:55 +0000

    pour brief dare climb lucky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
