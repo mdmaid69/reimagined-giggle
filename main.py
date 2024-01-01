2ffd622a47d38410d59935f37fe6824d9798028a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:43 +0000

    mind secret improve study if

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
