82e45e792bea05d4f548e8c381e4395a41735262
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:19 +0000

    claim few test nor runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 def calculate_speed(distance, time):
         return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
