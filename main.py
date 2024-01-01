c64a0b30c4d2eaa7420ec22f7529f9ea2f1f2324
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:06 +0000

    threaten jump fashion embarrassed exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+print(re.match("h.*o", "hello world"))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
