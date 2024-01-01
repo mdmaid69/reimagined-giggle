8d92cb5ff2926d748d1fac6d72b4cbb43adcb56a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:51:11 +0000

    tongue word blame catch break

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
