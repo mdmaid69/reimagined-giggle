9fd3c01bfc6827e3c79203e75275df129d1669bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:42 +0000

    race modern Mm practice badly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
