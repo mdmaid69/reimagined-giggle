21e1e61ed376a2c9d74df62ff8a2be9b568d17a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:38 +0000

    shoot nearly meet radio soon

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import math
+print(math.pi)
