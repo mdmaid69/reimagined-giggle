703f50b361b00b85563b338d33f1319b7ac3b40c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:58 +0000

    tap explain miss sit valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def split_path(path):
+        return os.path.split(path)
