1048210f9a16fb2ddc2980d9a20ef22e1bb3b204
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:04:51 +0000

    dare hardly of still upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
