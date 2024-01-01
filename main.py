be267d9a9edf6d5386b2c6d2f1a2e0fc6d0583d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:20 +0000

    copy acceptable dance risk doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+import os
+def remove_directory(path):
+        os.rmdir(path)
