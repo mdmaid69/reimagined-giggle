93da952b11ac9bc8f2f838b0a99542a62990f571
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:59:53 +0000

    but honest swim seriously draw

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
