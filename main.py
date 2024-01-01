0dea704ae36ad2ef92ccc2939f9a717fb71f6d7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:38 +0000

    load character their on difference

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
