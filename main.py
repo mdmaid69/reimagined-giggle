215d0fc95e6d2094af2b09a7121c0997426ab6a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:39:50 +0000

    forgive show sense 179bite interruption

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
