a0d9296d6d6a91c363609fb1bde42c4485003fa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:32 +0000

    rough spite difference objection they

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
