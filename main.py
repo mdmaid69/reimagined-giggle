1401809fc869f791a70edaf1940ec32e2db5d551
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:33:09 +0000

    everything send pencil produce smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
