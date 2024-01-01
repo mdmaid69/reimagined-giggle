bd2fba08a07fddb75959ce43b658e74e20e20a47
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:28 +0000

    blind translate character Oo draw

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
