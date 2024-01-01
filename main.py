9e7395d7f818b78c7c2cdf98374d77465ad35244
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:10:15 +0000

    short shut us takeout rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_speed(distance, time):
+        return distance / time
+def remove_duplicates(lst):
+        return list(set(lst))
