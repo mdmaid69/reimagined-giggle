a303caa334d781021c642f89eac74d682898695a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:30 +0000

    firm Jj communication constantly at

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
