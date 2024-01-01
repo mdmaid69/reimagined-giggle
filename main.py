be2ef3cc85053bff9887d2e6c0c46f6dea6ee1df
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:43 +0000

    worried sweep contain combine low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
