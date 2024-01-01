7363dc6e0e340eaa3a37ee6014630526b58c6970
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:41:27 +0000

    centimetre guess search care crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
