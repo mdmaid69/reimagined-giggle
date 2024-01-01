aec36f71cafc9e1c5d3ee098f63541e1c1005c2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:02 +0000

    571enough nothing exit sign dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+print("Powers of 2:", [2**x for x in range(n)])
