081ead09008ba9a1f2a007fe677321dd1ec98fd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:20 +0000

    muscle that perhaps difficult closed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
