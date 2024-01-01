4ee5306dbf520c135184389048dff1dc48dea9b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:19 +0000

    load failure guilty hurt activity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def square_number(x):
-        return x**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
