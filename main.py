1fce37f628ef93449ac769751e14d30ad9a8d951
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:44 +0000

    consist leave lesson profit wait

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
