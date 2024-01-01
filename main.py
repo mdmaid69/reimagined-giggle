0f68bc34c19e743eafa06cfc8668a48d802317b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:37 +0000

    neither plenty month range shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def convert_to_hex(n):
+        return hex(n)
