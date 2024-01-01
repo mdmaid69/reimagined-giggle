50183f2b17ebf28905c6940c30401a6cbb0c826b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:16:46 +0000

    improvement pipe turnoff prevent mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
