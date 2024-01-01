52af5fefe67cee5334cecd3f0504218c86056020
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:47 +0000

    plenty mind bush reach impatient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
