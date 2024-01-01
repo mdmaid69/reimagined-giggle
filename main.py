30a6dcc22d43466685dd109f2670479ab136e03d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:45:24 +0000

    backwards charge broad reduction Kk

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
