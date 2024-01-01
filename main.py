11b6a41f2077b58f467292a40c3e6d348be87f6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:39 +0000

    doctor aim band why successful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
