b5f30380b6fff61eb6ecc399f36bfab77e668192
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:32:57 +0000

    into patient breakin pen chat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def reverse_string(s):
+        return s[::-1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
