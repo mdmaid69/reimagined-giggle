bcb8ab0bd1cba7a4d278cb617996cd580decae6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:45:55 +0000

    decision name meet wait equipment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def greet(name):
+        print(f"Hello, {name}!")
