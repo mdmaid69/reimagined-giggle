6fc4c75960919af1608c15d31d8671831b8674a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:40 +0000

    hope tidy main shall fashion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
