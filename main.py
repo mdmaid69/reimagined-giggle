77c20ef2219a4a486233285a33d97907f5266b21
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:12:37 +0000

    through respect address most noisy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+def is_palindrome(s):
+        return s == s[::-1]
