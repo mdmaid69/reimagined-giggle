3e3ce17be6ef88326ee4d82237893f88e39a6bb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:02:27 +0000

    beneath mind hurry design ours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
