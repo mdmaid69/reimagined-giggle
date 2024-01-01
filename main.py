b8a40596d86d762b97193c4db50cfab495784929
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:15 +0000

    cover breakin equal before rain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
