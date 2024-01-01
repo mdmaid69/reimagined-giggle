ac855add4b1211e17afd758c423c487f66221fb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:12:24 +0000

    convinced mark lock soft fun

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
