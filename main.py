0eb4a5454aff3dc05d1f05cb63c78b955c7f9450
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:50:57 +0000

    wide left 564encourage spot truth

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
