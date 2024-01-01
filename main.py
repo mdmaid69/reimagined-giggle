82fb68cc7889e5a32ee1392c3eecb5bd49a11dc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:38 +0000

    listen relief process address confuse

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
