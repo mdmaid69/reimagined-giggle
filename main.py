d6150a5e7699034084af3af25c8f9ef0deec0301
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:04:40 +0000

    turn cap squeeze bunch pool

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 def convert_to_octal(n):
         return oct(n)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
