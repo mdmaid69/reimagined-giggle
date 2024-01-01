4042fb9b646281cd803c1410e03f5049d53aa7ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:42 +0000

    as lie pollution blind add

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
