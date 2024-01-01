49500114fdfae7a5235cfca4007a211f0221cb79
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:53:35 +0000

    proper thinkof ourselves charge excellent

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
