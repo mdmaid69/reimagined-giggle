cf7a5065b353595f0fd6fe0ef0ca0105ff93db03
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:28:08 +0000

    cool exchange fortunately rate plate

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
