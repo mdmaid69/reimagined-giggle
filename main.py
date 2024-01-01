a0670eb7265bd5cfb14545880621be5cf9a37fe8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:43 +0000

    wander calmly recognize separate lonely

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def add_numbers(x, y):
+        return x + y
