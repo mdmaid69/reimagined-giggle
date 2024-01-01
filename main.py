9635c5e94d9a0e9d52cec83b10b8fbfac2018b80
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:15:39 +0000

    necessary charge hate limit splash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
 import os
-print(os.getcwd())
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
