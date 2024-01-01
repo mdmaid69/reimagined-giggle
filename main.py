3f1b923c12c6674c86ae9742fec773a4f908b6c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:42:46 +0000

    bother reach lot runover there

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
