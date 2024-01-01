bfb802f4175b8e788c99fdd6b1715764fc239115
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:21 +0000

    shout party piece Ee pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
