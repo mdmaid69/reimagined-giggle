7b39e757bd2649cd79f6891a51c949388ad68b25
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:15 +0000

    entry beneath cheap majority compare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
