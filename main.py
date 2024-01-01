a6a912db78974e028b8b9efcd91a00eb88ab4c18
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:34 +0000

    warn remain development turnoff fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
