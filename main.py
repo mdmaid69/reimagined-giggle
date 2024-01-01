e5d4d89d8e46dc1d9cdc275883034105bc27e53a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:55 +0000

    warm Nn activity band cross

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
