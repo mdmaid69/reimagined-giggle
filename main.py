7e498cc00eee848ee83b471463902dac949201a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:47:33 +0000

    plain flat simple treat goup

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def convert_to_octal(n):
+        return oct(n)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
