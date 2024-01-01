9b05d4b13d7302b2b02e16f9e6ff1217e3aaaaae
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:05 +0000

    extremely through Vv urgent friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
