ab427c984357f39802f6ab0a03c1b7150e4878df
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:29:31 +0000

    confusing Ss workupto convenient true

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
