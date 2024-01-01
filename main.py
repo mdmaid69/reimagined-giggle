e0b2f42eea7e30f83c138fa0d4a89919e307c039
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:54:40 +0000

    stranger shock love list label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
