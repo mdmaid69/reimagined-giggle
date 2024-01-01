d195a3af71183316b881da0f5baf3a4f83ed64ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:14 +0000

    study kick what break possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
+import random
+def roll_die():
+        return random.randint(1, 6)
