6d0f13b7e4a900880206c66c249ec9a888a73c99
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:02 +0000

    final control past important be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def get_bytes_from_array(array):
+def get_string_from_array(array):
         return array.tobytes()
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
