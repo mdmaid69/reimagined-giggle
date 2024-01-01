109175872388ddcc8b014d218a537b1330f90c09
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:29 +0000

    Ss goon aim backwards month

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
