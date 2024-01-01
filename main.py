e44efc0a573630fa55853a3b0183a4e68a32b908
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:04:02 +0000

    likely instrument interview further steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
