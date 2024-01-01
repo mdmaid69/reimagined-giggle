2548db6a08f5c219a011e2897c948fcf58bb1ed0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:22 +0000

    past attractive gate opportunity fall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
