9f0ac862c9a7d19e5a7ab6acefb9730fa9494269
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:12:43 +0000

    mark invite wipe gooff build

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
