0b933d5c34a03ae568b3697842a7b25ecd422d53
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:57:13 +0000

    joke advice base butter keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
