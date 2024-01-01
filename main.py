64c1025c894fee7b27d56eab4a9f0ad32345423b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:02 +0000

    certainly course emotion happy independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import collections
-def create_counter():
-        return collections.Counter()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
