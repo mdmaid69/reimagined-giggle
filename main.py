5853ea7598f9459e1701b21977a2e5e0c53fef49
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:40 +0000

    difference description human order interesting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
