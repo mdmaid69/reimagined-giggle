ec0951d935f8ad6954718a25ddc2a156cdeb0976
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:12 +0000

    weekend Bb later kind provide

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
