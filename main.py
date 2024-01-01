e1217c3f83e93c39964d8c4b1419c3027ea5d028
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:09 +0000

    gooff ring majority pay broad

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def add_numbers(x, y):
+        return x + y
