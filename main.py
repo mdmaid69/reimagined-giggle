857882af80bc8c0ab4650cf2a7c9f0adaf8e3431
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:21:15 +0000

    argument and apart bound figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_string_from_array(array):
+        return array.tobytes()
