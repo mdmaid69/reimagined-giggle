3c873c96eefe7c11eaaa6866d90f127c0439ba95
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:41:32 +0000

    straight holiday develop trial dead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+print([x**2 for x in range(10)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
