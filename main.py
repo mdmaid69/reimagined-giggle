af843bb65c535427241dab988d42ca41b3f460c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:51:21 +0000

    prepare mind before close shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def square_number(x):
-        return x**2
+def get_array_as_memoryview(array):
+        return memoryview(array)
