2a3567ac86543bcc99621b71fa92e9bda85b0745
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:06 +0000

    strike fashion both comment make

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
+def sort_list(lst):
+        return sorted(lst)
