a7c9b346e4af0473535d66745704c515eea43f34
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:36:19 +0000

    stop thick female shout at

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
