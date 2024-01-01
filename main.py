38edbae454e43663cdfb381ad8209ca822595d71
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:00:31 +0000

    meal generous platform really threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
