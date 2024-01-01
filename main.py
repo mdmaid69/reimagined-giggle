df4638e6a920f2c15598cd5f13280325898eba25
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:56 +0000

    excuse no brief sorry peaceful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
