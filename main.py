bebd087b072fb08291de2703adb5bcf26644d711
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:32:01 +0000

    second hide name lock remove

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
