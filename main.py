2d1465e44d83b8717e7e118b5fd331a0ab3676c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:15 +0000

    us design land sometimes discussion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
