a59caa7fae728c3f3b37537d71b8f411f5043286
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:02:20 +0000

    crowd crime main keen threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
