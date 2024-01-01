7d68ab2979446def813c93e3045b71b155499ee3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:38 +0000

    upwards fairly wave exchange go

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import time
+print(time.time())
