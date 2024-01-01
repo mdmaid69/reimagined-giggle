03a10e62a8abd3ca09964e7df1887264fc72cc9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:22 +0000

    famous share usual convincing friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
