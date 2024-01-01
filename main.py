a8d56b10fc96af41835a74bc10f00d0fb7a56255
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:08:29 +0000

    song loud preparation scrap sort

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
