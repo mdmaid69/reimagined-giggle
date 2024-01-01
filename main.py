bea50733c331ba428712a02b1313ed5cd95262f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:03:47 +0000

    study wander this fire bang

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
