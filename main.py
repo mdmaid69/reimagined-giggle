4db48d2a125590d8be4e490b816dd36cbb4c44e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:00:23 +0000

    sick poison pause book wet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_directory():
+        return os.getcwd()
