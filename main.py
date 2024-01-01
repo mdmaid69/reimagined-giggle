ab89795fcf2695f5b57086e98aeebaabcff6e220
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:40 +0000

    night respect out wheel punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+print(json.dumps({"name": "John", "age": 30}))
