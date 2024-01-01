cdd00cd4dd1a729a1213fe692637d06cfaa3e6d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:44 +0000

    hand professional at address wall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_item(array, i):
+        return array[i]
+import os
+def change_working_directory(path):
+        os.chdir(path)
