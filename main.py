4ac601a306e0bc2bdd6968db2e87ef9836497fbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:46:33 +0000

    impatient teach tune think interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+from collections import Counter
+print(Counter("hello world"))
