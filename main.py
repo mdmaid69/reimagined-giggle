2615f9eb3ed1e42e8aa095265e9c49563adddc34
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:55 +0000

    present future fetch medium twist

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
