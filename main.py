ed7475a93a53ac641b80a1784350c4cb192e2a06
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:58:40 +0000

    moral angry hurt develop 572enter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import array
-def get_array_as_set(array):
-        return set(array)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
