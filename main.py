e67bb41f01d2bc086c7844d306a97fa602a19b67
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:52 +0000

    then load system join reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
