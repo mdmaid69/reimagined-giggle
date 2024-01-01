17b35e8bb9154d5df0aa4bd9faa30573cb079ffe
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:23:25 +0000

    injury brother idea separate fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
