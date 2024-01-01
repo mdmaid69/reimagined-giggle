1e5db3b45c2895e9fa38c17a81b2b74387bd5693
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:59 +0000

    rise process stare character plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
