a82afd9751481e35af8d7418730621a334e2b3a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:31:32 +0000

    practical suffer calm fire problem

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
