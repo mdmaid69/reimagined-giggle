541b5ef3e9674c5caad7e8af2a1aca3c77c92926
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:41 +0000

    guess discuss unhappy offer beside

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
