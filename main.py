532ccbeaaa9e3a6528b1d77b5bb49bab4c125a84
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:46 +0000

    steam satisfied judge chat object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
