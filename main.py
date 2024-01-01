1dd92c30344b7c50939c5a348ec8740c98f1d76d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:32 +0000

    normal book different pen ought

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def get_current_time():
+        return time.ctime()
