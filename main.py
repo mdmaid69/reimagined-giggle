d99262ecb3099afaf4dc46fcf6ff890d6c0601c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:21:23 +0000

    force machine sting calmly trouble

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+print([x**2 for x in range(10)])
