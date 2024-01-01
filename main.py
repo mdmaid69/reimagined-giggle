8457d292cabee8adbd0fd5e9a35d848351222118
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:19:26 +0000

    down slide owe exercise Ff

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
