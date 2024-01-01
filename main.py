828c1160438c404f9eab454331557d51b9fb7e59
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:02:20 +0000

    downstairs as balance deliver on

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def find_min(lst):
+        return min(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
