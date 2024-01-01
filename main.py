37d7d3cf9bd0a313a14526db4d7068da54ccecae
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:08 +0000

    control again believe touch name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_queue():
+        return collections.deque()
