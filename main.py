2a9076dd84c7cbf2888347f9c793931071da1320
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:10 +0000

    ring dream allow trick love

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
