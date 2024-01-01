22706ca266423fdb0f79b5b1249107f458612723
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:05:39 +0000

    bury sight chin substance risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
