d4a00a7babc1220ba15709a009b8295313d55249
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:37:00 +0000

    practise book possible interview runover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def is_even(n):
+        return n % 2 == 0
