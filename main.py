78f178045f8efa554407456e3bfb4b41c87481f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:14:02 +0000

    bound manner forget sink off

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
