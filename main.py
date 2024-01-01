0ac581c18e8f1a2a223d7fe6f62d153c04b10f45
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:39:22 +0000

    shake anybody getback abroad total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
