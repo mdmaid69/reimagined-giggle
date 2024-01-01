61c433c57972d1aa2d9c981aa360c047161c91aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:45:52 +0000

    loss convenient statement sit appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
