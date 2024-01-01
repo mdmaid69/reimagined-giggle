de6416f7c92969bd6b8e8b67c56b9de712431b91
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:49 +0000

    common instead disappear handle pen

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
