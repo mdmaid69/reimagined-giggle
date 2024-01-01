575e583bd89b8818c4f0f1e814500118020d88cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:52 +0000

    single available even arrange serve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_set(array):
+        return set(array)
