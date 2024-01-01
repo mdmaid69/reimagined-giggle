28f267f4c6c7208d7e3d067fa44bf7358fe15598
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:52 +0000

    roof plant fence difference refer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-print(sys.version)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
