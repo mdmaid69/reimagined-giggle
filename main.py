b0d1496c4c04116ddd57d3c743344a434b8bd323
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:22 +0000

    wild frame pocket strong except

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
