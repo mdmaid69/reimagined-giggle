e028a0448e1c848da31a760ef25cca9fd67db8d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:16:29 +0000

    stir special bridge convince think

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def cube_number(x):
-        return x**3
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
