c4a33a6052a03c6024a846138ef8be61c684bf1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:35:53 +0000

    smell long lookafter forgive another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
