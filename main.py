2aa3de6a8f11ae00d71d814a4999fa6ac3a4bcc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:37 +0000

    forgive occasional strict dear where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
