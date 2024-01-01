84a026ca3ea68afa2f538b258988e8b792061de3
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:10:02 +0000

    they action keepaway fly although

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def sort_numbers(numbers):
+        return sorted(numbers)
