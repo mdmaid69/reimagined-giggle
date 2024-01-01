25b25c1bd8e895766c71f617878781abe7a4b5ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:54:17 +0000

    push film brave creature surprise

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
