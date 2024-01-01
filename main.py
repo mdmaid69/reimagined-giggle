15e44853b9127c5ea19a5a30175208c02d6ea5f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:37 +0000

    coat negative acceptable advise swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
