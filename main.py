50f6cf3c244729bb236436cbfd7a5dc82c4a6dbb
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:59 +0000

    owe pity formal under conversation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import collections
+def create_user_list():
+        return collections.UserList()
