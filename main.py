bad4141ca479d2bf7e52fa299f468d3610e6bae0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:28:14 +0000

    shopping strength roll far start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import os
+print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
