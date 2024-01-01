cf92666a0fbdc4514f8101e45cf7ba4a38e3072c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:30 +0000

    calmly improve breathe car pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import numpy as np
+print(np.array([1, 2, 3]))
