74c180557a8e646a6bf3051c413bc576428e4690
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:36:06 +0000

    worth order only university mine

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
