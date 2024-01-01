5dec98d9dba4667871bd510d92bce49c68009943
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:28:04 +0000

    print plant town behind before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
