0c2ea541f338211eca2457dac1593b6ed47c220b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:09:00 +0000

    better centre square party outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
