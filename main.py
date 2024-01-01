f92eecc32361455e908024f949f48cb8cb72f6e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:52 +0000

    run into place gas miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import time
+def get_current_time():
+        return time.time()
