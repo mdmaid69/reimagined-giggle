af64aa85568077aaa1743d8e4bd00f4e16fd5fe0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:39:08 +0000

    bringforward purpose note ours we

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
