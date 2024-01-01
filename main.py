6411fc43f9f63ad15a0037f1d7b87d44bcac35ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:55:36 +0000

    try reach fly cross cross

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
