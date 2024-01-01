0f43d3bb364850e59d5256376635b50c320752e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:04:38 +0000

    cry still petrol mad appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
