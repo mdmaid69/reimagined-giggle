c44fcbba97e140fa1b31bac171618cea66b6ae9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:17:06 +0000

    comment rain that contact invite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def set_array_item(array, i, item):
+        array[i] = item
