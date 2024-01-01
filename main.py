301d0db1fb9f7bfa2fe5e277e078b873faa97d4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:56 +0000

    trick pretty punishment careful band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
