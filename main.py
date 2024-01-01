eaaf2cfbc6b9740cf4082411e5a6d37906f673e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:50 +0000

    prize stamp use weather separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
