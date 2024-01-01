0c98026cc7971b6bf35e19ac7effe0d156bb328c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:08:47 +0000

    would rain sell that plate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
