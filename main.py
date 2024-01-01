404217e09fcf2756f91d2b2859c2162a56e2db75
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:48:50 +0000

    smile worse band either rub

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
