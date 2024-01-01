baa17e8f4f844ef95ef736e50b55261b998e122c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:31:08 +0000

    alive class die bringforward start

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+def calculate_area_circle(r):
+        return 3.14 * r**2
