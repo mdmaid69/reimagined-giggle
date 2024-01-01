f4f1d9e8411af3578eeb467c5da365f1e1bebc98
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:58:51 +0000

    excited announce once involve laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
