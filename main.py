eba0d4ed19eb8c55b71fbcffdfebc656714a5fe7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:12 +0000

    do unusual summer cheque black

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
