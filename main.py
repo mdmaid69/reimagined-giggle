31a1e1dbd5fee4a53a37ea188eabd1cc5f4a99cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:14 +0000

    goaway film base worth port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
