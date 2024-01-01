904ff4f66412d63ad5861d227a7c7b7b39727233
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:25 +0000

    stay distant apply cry without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
