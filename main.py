386ccdcbd1df89cf288e061e16e2794efe9162b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:45:14 +0000

    guard hold match goup never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(numbers):
+        return min(numbers)
