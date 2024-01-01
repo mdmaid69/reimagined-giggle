c8dfeb3b7bda6562231508e8a240bf16a4cc8c50
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:13:57 +0000

    calmly poor fine male fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def find_min(numbers):
+        return min(numbers)
