fe56a5671cfe1fa826c6e64d2d9d2bfaf6c847ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:28:21 +0000

    sticky variety mess target holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
