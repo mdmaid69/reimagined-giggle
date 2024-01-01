0e2dc7e52b0d0fc0973c595063f41f3e63c5adef
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:18 +0000

    trial book question field may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def reverse_array(array):
+        array.reverse()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
