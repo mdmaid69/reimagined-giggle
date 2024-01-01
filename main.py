2b90f20886d6072c6a4b105edbb4e44eb607f31b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:55 +0000

    objection thought authority constantly official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
