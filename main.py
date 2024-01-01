ef1d96ca824146c73eb6e4b603d92431b28d46bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:27 +0000

    fill method persuade winter wave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
