fa60f478a8b0449ea7952e3536816dd016d6b314
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:48 +0000

    cover also war your maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def sort_numbers(numbers):
+        return sorted(numbers)
