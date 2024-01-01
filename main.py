b6ff06d33849044e899527cc6ac79b8f2e0589b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:07:51 +0000

    lie minimum march with tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
