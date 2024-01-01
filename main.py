3c33f331d895acacbaf999f3a488ec675fddfcb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:14:45 +0000

    prevent organization real expensive label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def reverse_list(lst):
-        return lst[::-1]
+import random
+print(random.randint(0, 100))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
