34a6d4b3d89a3bae265321899dfe355a030be31d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:37:40 +0000

    she mass disappear calm break

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
