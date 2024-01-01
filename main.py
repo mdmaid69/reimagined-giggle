6f81cb36bbeccfc957c716d9be374729d6737315
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:58 +0000

    centre art breakout share smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
