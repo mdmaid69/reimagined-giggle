3101aa35bbd371efa23c73080a8e20ebae5eb382
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:04:27 +0000

    unlucky reaction sick proof aim

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def count_words(sentence):
+        return len(sentence.split())
