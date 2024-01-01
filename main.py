717540bdcf5b12af39fb656699a70b9d75aff34b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:52 +0000

    distant workon concern rich party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def sort_list(lst):
-        return sorted(lst)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
