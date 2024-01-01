6b3d56234d1839328a498e8f5492233f8c70e619
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:13 +0000

    raise pair fit reach class

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
