5d4b99e9fae7b8d733a9d21eee918e440ff217f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:57 +0000

    term age course risk shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
