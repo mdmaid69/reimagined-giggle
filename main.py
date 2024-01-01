273f2e62187cdfe80817fd333aa0ff8ec963c531
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:06:45 +0000

    adjective country typical wish regularly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def sort_list(lst):
+        return sorted(lst)
