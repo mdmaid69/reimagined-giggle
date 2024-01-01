dc2f8dd9f110474280a43c71bd4ea234f37b1556
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:35:57 +0000

    door train difficulty future keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
