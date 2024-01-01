8ab6e371ac922787a39cf81ff54bb141d819f456
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:36:16 +0000

    bunch wise between with mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
