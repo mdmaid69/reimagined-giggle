371020190b311bd16f3a641a223903500b7638a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:57 +0000

    steep nor takeaway thick unkind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
