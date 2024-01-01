fbf403d68c569fbd72cd18c763ecea27e9f0efa2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:19 +0000

    eat tip over hit even

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import time
-print(time.time())
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def add_numbers(a, b):
+        return a + b
