fd67b84b1f24b4b9f62144a62818d58ed00ddf4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:19:56 +0000

    cruelty concern note fear issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
