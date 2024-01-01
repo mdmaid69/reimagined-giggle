f5b68ee4a554973b9403e31fbd4292e961776f5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:55 +0000

    practical attitude run choose sweep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import random
+def roll_die():
+        return random.randint(1, 6)
