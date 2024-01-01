d362a42ff41fd06d5b360257fb76f71f4273c64b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:22 +0000

    gap sort travel after disappear

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
