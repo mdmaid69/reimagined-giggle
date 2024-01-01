ff5e37a2c8fb7ffd59919372620305e2057af523
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:08 +0000

    song fire quarter brief complete

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
