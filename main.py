f7e85f4a18972d4a011c2855eb9bc3db3bedea9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:19 +0000

    vanish read thinkover new none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
