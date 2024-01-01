536b09960e84ef94760f35ea99479f0c37c989d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:12 +0000

    consist behave take reason church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
