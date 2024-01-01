0d243d13a5548f99763f187061cc7d5fabf669a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:32 +0000

    form otherwise thin difficult favour

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
