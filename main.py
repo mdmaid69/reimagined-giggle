c8803be54f79ab3d40fb1efb08b7b6a94b8e498c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:40 +0000

    major corner walk plant great

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
