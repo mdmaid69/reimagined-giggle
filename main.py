39b175ec65f086d71a7ef168db8d1b4d4f453f28
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:57:51 +0000

    ever now bar drop Bb

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
