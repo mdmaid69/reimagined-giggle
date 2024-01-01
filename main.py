34aeb7d6a7e18c041cd3c863d725504abeded7c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:24 +0000

    damp little report plastic distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
