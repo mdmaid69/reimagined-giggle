8274a9349f31a356ac21c134084c106f1662a2c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:09:59 +0000

    ear can group furniture nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_str(array):
+        return str(array)
