331b9355188f63106a28b8ea90f83a8001b8dac8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:45 +0000

    past dish tragedy bang suck

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
