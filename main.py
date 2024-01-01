7e0736f4440d4fd86aeb817e8b8dbd7316c2c8f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:01 +0000

    official drive rely side uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
