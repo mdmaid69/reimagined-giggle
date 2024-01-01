3e650c6032cf84d24271a0bf99c15ae8ecdbb8f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:12 +0000

    unkind gate injury excuse breakup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
