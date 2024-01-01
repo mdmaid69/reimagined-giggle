1bb2874e7c08d6f8b33c7d4b97582302ded33b5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:13:42 +0000

    doubt takeback memory dance dry

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+for i in range(10): print(i)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
