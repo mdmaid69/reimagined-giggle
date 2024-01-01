8cdf35f759bb47dce6e2dd757f9c4ef786489e61
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:27:54 +0000

    onto previous stay eat various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
