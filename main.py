ba5449e0445031e2a79d905002817c6586aea61f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:55:01 +0000

    character fortunate rich thumb at

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
