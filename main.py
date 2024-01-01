363ae87de35a19d6195aa65dcdc2e1d36a60290e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:10 +0000

    drawing getup breakfast butter common

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
