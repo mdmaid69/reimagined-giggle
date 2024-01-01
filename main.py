5148cc93706a6550732176756e030a5967ac489b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:44:57 +0000

    time tall intelligent warn sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
