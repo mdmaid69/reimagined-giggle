3e83589a4aedca914117a20f05bf44b59fa5e0cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:24 +0000

    fierce consult glass shall church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
