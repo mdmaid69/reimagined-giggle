e327498f930deb7e2fb909485eb0d1c2ec87205e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:05 +0000

    gradually his so pay trade

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
