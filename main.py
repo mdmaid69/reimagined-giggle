c1ba2ccbb3c692ca1361021cf179ac2a306769d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:22:02 +0000

    quickly themselves debt worried they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
