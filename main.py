bf96581bb583e32b33d4decd674c8a56b9ffd10a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:49:07 +0000

    tear raw rescue perfect available

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def multiply_numbers(x, y):
-        return x * y
 import math
 def calculate_hyperbolic_tangent(x):
         return math.tanh(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
