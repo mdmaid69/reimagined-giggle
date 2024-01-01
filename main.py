b5e00223187fbc166d2295fe560610c8ce51653e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:26:50 +0000

    ride gas crime busy certainly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
