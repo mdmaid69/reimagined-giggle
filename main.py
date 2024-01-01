362a13eb6f561d40489904899db7af9dc7bf466b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:56 +0000

    guard pure pause makeinto watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
