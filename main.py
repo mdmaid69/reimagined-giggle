89c03405bde7d655e008f521567ba0ada7e34a77
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:12 +0000

    steer tie anything argument right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
