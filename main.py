dfb324914b793d63c9b6ec7c243b1b2ce0d886cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:13 +0000

    roll bring mine tend standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def calculate_work(force, distance):
+        return force * distance
