311fa45efe3e90cff97914b552cbb0ce7033e9e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:52:14 +0000

    petrol message feel pour environment

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
