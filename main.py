d01cbd91a5445e51328147fd7c9980700f45ed94
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:51 +0000

    account glad ordinary next spend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
