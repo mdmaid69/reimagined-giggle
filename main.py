6e8aec288fd669af2fe2fb35e1e84fdf989d4673
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:14:00 +0000

    exact proud used because exercise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
