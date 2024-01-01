0f0e8d8744d158de385244cdfebe2fe345cbd8c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:22:18 +0000

    speak develop common stamp please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
