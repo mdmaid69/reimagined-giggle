f4c62af9f6fd9c276d822bd6bdc2c57c73d67d16
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:21:27 +0000

    ill even swimming why tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
