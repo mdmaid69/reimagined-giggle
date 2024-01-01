a79aa9a0180b06d407873b0ee22068e120a87cf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:48 +0000

    popular replace charge need bridge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
