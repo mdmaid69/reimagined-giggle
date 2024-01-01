0c49a43ff4ded19693e2c534aa8e45777c7c16c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:01 +0000

    regularly hate lesson lean health

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
