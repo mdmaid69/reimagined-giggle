a6b694a878ab6d330284c935682186527812991d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:23 +0000

    rise later past want look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
