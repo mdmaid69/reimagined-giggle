ea1fb4664c24b8a483d5aee1d313a6f4038cf6ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:32 +0000

    comfortable now dish friend right

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
