a64f00fe170041c1d9399d78058d4a6a1828e2a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:24 +0000

    variety warn contact interruption live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
