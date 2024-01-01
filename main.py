72b29e5ac811fbe9a014bdc344572de504c37fad
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:26 +0000

    this crash artificial value view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
