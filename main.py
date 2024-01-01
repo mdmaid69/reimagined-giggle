f0ae03ced59f616982c360f0231bbdf78fbc80bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:41 +0000

    bringdown draw legal touch anxious

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
