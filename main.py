c14d00c54726deb4ea393cc378a5eb978bc55087
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:28 +0000

    Vv certainly accommodation upset rub

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def split_path(path):
+        return os.path.split(path)
