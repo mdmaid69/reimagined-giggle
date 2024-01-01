cf9434d05aeca0663578d04c05bccef0ddfe8390
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:07 +0000

    giveaway breakthrough plenty pretend light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def is_odd(n):
+        return n % 2 != 0
