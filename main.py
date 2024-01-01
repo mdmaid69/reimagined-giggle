dcd1c54e65c80d0b33059809fe53f70b43c9d337
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:02 +0000

    room live process high young

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
