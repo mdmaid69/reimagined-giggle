2f497e8547fecac93d573e9847903da0d493dbc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:00:11 +0000

    clean onto please lean many

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
