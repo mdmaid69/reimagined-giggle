e9a2764f0f5ad4b0835c6906c3f249887545fdd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:58 +0000

    everybody owe snow aim Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
