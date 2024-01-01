e8b1aa1640cf8fc6eafe8c01455bed7930e2e95a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:31:14 +0000

    squash hold wind phone boil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_floor(x):
+        return math.floor(x)
