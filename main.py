aa2de6d9e81ed291473c4eac27e0ef3f93978189
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:22:23 +0000

    go popular attempt invitation fault

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
