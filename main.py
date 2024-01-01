1503221d66b29fee3b45c0ff48b967790513569e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:47:30 +0000

    unkind gooff comedown defeat spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
