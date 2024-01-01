664c161392ff4a94358b9743c93a7051deba451d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:31:31 +0000

    combine holiday produce familiar club

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
