705c975f9c0e1c84bea7deadf8fe37aa0f3d33f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:33 +0000

    hurry hair sleep influence exact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_acceleration(speed, time):
+        return speed / time
+  def sort_list(lst):
+        return sorted(lst)
