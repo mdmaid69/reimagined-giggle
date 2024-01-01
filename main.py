4838f94fe465880d2f76e0310db1a135f0ce0924
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:12:10 +0000

    goback(to) patient shut offend road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_density(mass, volume):
-        return mass / volume
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
