6dd6f75a23ef47c7ad609ea549f59601b45f282f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:20:53 +0000

    authority happy wind complicated group

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
