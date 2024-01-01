2ed83bde917a15b4171d184fa2b90b3b19965587
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:04 +0000

    last pass laugh chat lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
