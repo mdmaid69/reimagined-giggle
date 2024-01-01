c0e774f18be9ec533aa367bf2c5d8784ebd4f3fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:45 +0000

    close pupil keepaway holiday constantly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
