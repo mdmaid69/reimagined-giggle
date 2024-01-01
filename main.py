040b8c02a8e2e7b4b1ae7cc1df27af00078ee738
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:33 +0000

    refer pride tend noisy various

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
