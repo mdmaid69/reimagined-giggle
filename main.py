b06f5e5cb558a0c08434ad62271d7153fc00c5e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:14 +0000

    before use 569enjoy close bill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import time
-def get_current_time():
-        return time.ctime()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_density(mass, volume):
+        return mass / volume
