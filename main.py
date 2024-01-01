d2c14de9c241b927a79039f98b4ad6d14c0d4ff1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:41 +0000

    trade any her sail list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
