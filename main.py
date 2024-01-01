6920fbe708ff955e6a7b8f30af4c7dd394247502
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:40 +0000

    plenty have even achievement operation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
