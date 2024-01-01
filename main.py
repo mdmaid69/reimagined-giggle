23cafefbb6d0329e41d2b09b7ecc6e5227e3c9ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:24:01 +0000

    plain drawing motion either slightly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
