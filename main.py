ee94b10bffa8881176f5a5025b29b919e447c652
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:20 +0000

    strict land cruel house trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
