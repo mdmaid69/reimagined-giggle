ddc93196207e25c25bebc572eca81fe8f27d7adb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:53 +0000

    turn upwards goback(to) attempt sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
