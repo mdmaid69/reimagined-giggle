034755d88dfa013034fc203c1f0aeabfffee9554
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:10:07 +0000

    cry path blue room enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
