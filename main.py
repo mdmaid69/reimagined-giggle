4c1420302fd627b41a2a9801e14b016ad6804a64
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:52:26 +0000

    suggestion tend pretty open connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
