b3deb37e060f12d79e99b163b64aa2ab3843ae5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:15:51 +0000

    keepout legal whisper scream sweep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
