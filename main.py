27d0c79e2afaa2e3cdabc8ae433a6a487024cff1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:48 +0000

    surface head communication now direct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
