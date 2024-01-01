6743d3f5d8433a0a7d28f8847364dc8e00037734
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:44 +0000

    identify combine probably doctor number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
