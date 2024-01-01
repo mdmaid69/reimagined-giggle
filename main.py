db0e0b939c3460573a9676e1502285aba7fff893
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:10 +0000

    fact name gate juice remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def add_numbers(x, y):
-        return x + y
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
