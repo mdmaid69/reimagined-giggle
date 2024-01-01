a742b3e0c62d4d9d54944dcb4bf397b81566d473
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:53 +0000

    cost tap uncertain fear teach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def convert_to_hex(n):
+        return hex(n)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
