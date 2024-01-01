6104a99d8f004627efbd00c65cb5ea9f4654125c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:01 +0000

    your early miss bottom flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
