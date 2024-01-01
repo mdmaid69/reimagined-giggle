b44122ef146304383da561236252a7c3bee91630
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:12:35 +0000

    broad be trousers nurse sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
