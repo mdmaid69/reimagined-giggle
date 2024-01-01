2241a17daf4c6266ec31726f0ba9a1c22e7cb5ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:18:05 +0000

    die coat tough week guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
