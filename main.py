f251e794d6704f2e6bb54982305faae4d856f54d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:49:26 +0000

    high section extraordinary punish agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import collections
+def create_stack():
+        return collections.deque()
