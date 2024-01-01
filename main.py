f0231bb467fd882f4611b63f15fe30212a96f682
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:01 +0000

    freedom come thank female half

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
