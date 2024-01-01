795ca10ba4917b958dd3ae2ffe70d1d5a0061fb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:57 +0000

    common regret heavy information disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
