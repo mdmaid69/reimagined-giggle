4890612139a9dd484d4c977472feaf82bad3a673
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:48:00 +0000

    turnon defeat at worried whom

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
