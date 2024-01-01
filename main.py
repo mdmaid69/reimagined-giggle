84571a9db80ed48115c0f14344fccc50dae0c984
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:30:26 +0000

    ourselves sweet shoot hurt man

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import numpy as np
+print(np.array([1, 2, 3]))
