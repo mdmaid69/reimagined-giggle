c455bdc723bbab48c275d7bb8e54fb2661e29f47
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:27 +0000

    it marry speed law win

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_work(force, distance):
+        return force * distance
