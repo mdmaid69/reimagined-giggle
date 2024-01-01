8627cb6dae6729c92fac23888cb2700760daa6d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:07 +0000

    side rub top rain nurse

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
