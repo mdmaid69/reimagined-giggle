75cd3d035d7e6fee972f845c059925aebfbf9b63
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:12 +0000

    or ticket onto seem closely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
