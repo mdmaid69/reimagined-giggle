4b8572671d1770c123e9eda94b00e3975761f646
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:52 +0000

    little choose live lookaround hard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
