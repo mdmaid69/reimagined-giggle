cff18c8feb57cdd6c265fe080dcb1e349175d4e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:30 +0000

    majority bang Ss note that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_str(array):
+        return str(array)
