d384fdebee6e7dafa843ccc9e777455164d51ae6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:24:22 +0000

    fire compete talkoutof surface slide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
