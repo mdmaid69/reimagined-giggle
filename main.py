12f96bdea8cbbde58d3dfeb2c7a8fe7f82128548
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:05:18 +0000

    basic butter upset attract wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
