3ae083c88a5e9f99e2c6d6a5be8084f079669ede
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:07 +0000

    mark protection impossible public minute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
