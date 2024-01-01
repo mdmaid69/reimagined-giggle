3e07f505d972149c256530866b95de498a2def99
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:04:47 +0000

    benefit change wheel head well

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import collections
+def create_stack():
+        return collections.deque()
