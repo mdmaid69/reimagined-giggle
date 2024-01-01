abf60e1bbf6fdd48fa94b1ddf42450e90eedbe28
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:06:29 +0000

    deep ourselves capital gas software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
