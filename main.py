4525b70a59e111d8dcbfd75d4c515a21319ef955
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:53 +0000

    punishment free note sometimes interesting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
