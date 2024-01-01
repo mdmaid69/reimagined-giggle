f8771abede1acd8cfeba61ff41d2ba4647d71abe
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:14 +0000

    law collect please good left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def find_min(numbers):
+        return min(numbers)
