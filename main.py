3f9a78f6115939222fbd8f9a0453932a394c164f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:27 +0000

    steer dare lookafter too pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_repr(array):
+        return repr(array)
