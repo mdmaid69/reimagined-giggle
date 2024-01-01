197af7744b7d9004472d40c20397c303f5a99bd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:40:33 +0000

    wheel mess later play illness

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def reverse_string(s):
+        return s[::-1]
