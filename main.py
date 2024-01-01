21fb88a1307e971ffcb9aa46eb7b82f09ebf245b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:54 +0000

    near excellent pen from me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
