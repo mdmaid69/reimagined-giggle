e90099c5bd1d6bd9a9f17a66abe79c2ec1589513
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:14 +0000

    under ride impression zero whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def is_even(n):
+        return n % 2 == 0
+def find_max(numbers):
+        return max(numbers)
