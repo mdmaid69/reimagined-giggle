db1f04ea374dc8e7fa1a6cfd43444a64b76b5e17
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:12:08 +0000

    home most thirsty protest lot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
