dbe4980d93cd5758a9b50390cc294b01485cba6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:44:20 +0000

    only good active march clean

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+print("Hello, world!")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
