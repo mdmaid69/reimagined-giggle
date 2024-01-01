24fcd853b5883055328e7f003ef2091dd416a362
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:55:33 +0000

    day beyond muscle thorough even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
