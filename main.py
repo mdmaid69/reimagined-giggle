4a6be2f38449e23ef87426d4df0c1f01b9df5fb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:42 +0000

    defend straight produce matter by

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
