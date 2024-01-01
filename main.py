32bd7bdc3b6967cc9f2acb4224db7d8d13be17c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:29:09 +0000

    talkoutof fortunately write formal soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import json
+print(json.dumps({"name": "John", "age": 30}))
