cb73f2faa848220c4602df767d7396545ae9b040
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:08:46 +0000

    ask expect not trick remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
