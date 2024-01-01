efe19240b6eaf634f62d714583bce96c42f532fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:30 +0000

    fortunate out wave somebody classroom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
