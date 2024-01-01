3c0ff2c9032e5f4fb39ff77dd873ed29059ab3c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:35 +0000

    system mind car head production

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def is_even(n):
+        return n % 2 == 0
