df58b4b55d12b1716f6baf6653e3ea24b4baa982
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:24:26 +0000

    brush consider earth bring choose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
