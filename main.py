a2872759d3d11d30b7fa5661e4ad0bb8b5b3a423
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:57:31 +0000

    power list cheat everything power

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_length(array):
-        return len(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
