81b92c3a83cb14e4617363a003494f1aca677f0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:54 +0000

    tall precise able explain typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-print(math.pi)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_power(work, time):
+        return work / time
