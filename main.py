c6fbabec82baf1f412b32de1306d442e265ceb35
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:07 +0000

    action nowhere even song thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_work(force, distance):
+        return force * distance
