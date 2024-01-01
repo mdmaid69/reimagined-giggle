c87a5f6efa87f2f5bda7b4291c647ffd79a3136f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:07 +0000

    prize mess medium reasonable leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def greet(name):
-        print(f"Hello, {name}!")
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
