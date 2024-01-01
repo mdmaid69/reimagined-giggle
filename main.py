88d888389ef402750c25f73ec4e02af93fc86a47
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:03 +0000

    week fairly even ours push

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_power(work, time):
-        return work / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  def is_odd(n):
+        return n % 2 != 0
