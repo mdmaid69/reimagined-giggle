32aa59cf6a9963aa4aa29cfca798352b31750692
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:56 +0000

    connection control really sensible force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
