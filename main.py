908290ea08f1fa293564db840ec48aca79e6a16b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:44:30 +0000

    hope organize effort post praise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+for i in range(10): print(i)
