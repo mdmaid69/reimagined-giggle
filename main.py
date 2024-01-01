3024b91a3be1a21e9220d203aed40e4704e0e7b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:29:58 +0000

    to worst bunch ring mark

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
