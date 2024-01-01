82731ba3b2bf64dddaf48f1c426e6a129572a9f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:19 +0000

    ring cry conversation answer whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
