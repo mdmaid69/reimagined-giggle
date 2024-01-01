a74dc8f750d6783069842958ef221f5e6166ee67
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:29:37 +0000

    basic sting rare hard difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def is_even(n):
-        return n % 2 == 0
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
