817f585da8a0f5b306eee5f4eee550bccb791ba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:54:34 +0000

    after record keepup reserve might

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
