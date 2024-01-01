86c9bba705d6ab71b612d2b51eb71f6c982fa701
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:10:01 +0000

    evidence perhaps much plan steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
