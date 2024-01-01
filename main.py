53a2d4396fa845450042f4382e6640ed0027774b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:11 +0000

    shopping weather space urgent pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
