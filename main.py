594dc98ae1b64da50eb1b3417dab273a07a69a3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:21 +0000

    even display affect accept half

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
