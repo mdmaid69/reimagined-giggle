c985a32edd8a234e47d81c282e450bc053abe14f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:35 +0000

    various matter difficult record miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
