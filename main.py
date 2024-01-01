955fa0c93e8633fe0b7fe1ca731467c6e6f27df4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:55 +0000

    step callup exhibition knock contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
