e365efd4e4828bd51ed733719f96217c7dca6ee2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:53 +0000

    whatever to do finger 564encourage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_array_as_list(array):
+        return list(array)
