af3c2d6bc5b3cd11156417023eaca97cfc8b3828
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:05:23 +0000

    content sport next serve take

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+print([x**2 for x in range(10)])
