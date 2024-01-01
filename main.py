eaa78b8f636ca494249eb0a85931db2cf708f430
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:43 +0000

    bang contact warm share lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_max(lst):
-        return max(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
