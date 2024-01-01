0fab6029bdcf5d6c93c734827a3e397ba5e8c88e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:13 +0000

    harm lookafter vote arrival search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def clear_array(array):
-        array *= 0
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
