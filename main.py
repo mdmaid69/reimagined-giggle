7a23b47a64f0efc0e9bf79449637b9d8c4680abf
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:46:37 +0000

    often corner than whatever bringforward

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-print(math.pi)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
