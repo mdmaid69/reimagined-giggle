6ba488ba4e981f53aefd3cf1af57d67e6c73d09e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:14 +0000

    hard polite circle bite afternoon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def reverse_list(lst):
+        return lst[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
