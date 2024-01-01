92300410c6c1d96fa2597f986c8a0e0850f75101
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:55 +0000

    gradually low needle bang anxious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
