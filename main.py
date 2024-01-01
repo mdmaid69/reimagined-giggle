1cea600375b94d5b24234ac1e0daae1dc58f3990
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:15 +0000

    lower city into fit manage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
