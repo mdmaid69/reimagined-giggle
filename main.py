771eec6f4da8a4d67aae220bdecf627415428fef
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:03 +0000

    disappointed positive exciting nerve takeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
