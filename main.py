60fa67e294ce60a9fae8a3326f4c4bbcd8a9bff9
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:47 +0000

    left warning hour snow innocent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_power(work, time):
+        return work / time
