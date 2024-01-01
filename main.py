cda6725ada73b93d3d034612f612141672eed0b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:08 +0000

    importance scrape interview ticket tape

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
