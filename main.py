9fa433c4f0482512188019b700997d21299ca698
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:25 +0000

    loose influence black pinch inquire

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
