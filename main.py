a3b9fbacc7b75273d2258f7fbf7a691fea15b7b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:40 +0000

    nearly past regional slice wing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def reverse_list(lst):
-        return lst[::-1]
+print(math.pi)
+import array
+def get_array_as_str(array):
+        return str(array)
