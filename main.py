a2a935e9adc2c566ca7cb0dee5d2115f33a00de5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:21 +0000

    calloff well wait condition nose

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
