4b7c7b807708dd110a8c0a0b0032ebb369ae13e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:38 +0000

    lead task late favour ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
