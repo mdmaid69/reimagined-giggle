7af6eac7c0033b43da35feac7cfd3258cb3b19bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:02 +0000

    blame book earth harm close

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
