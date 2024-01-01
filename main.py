5ae563932042efd25a8706aeeb00ffd00d5cd3d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:12 +0000

    to aim hide weak steam

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_as_str(array):
+        return str(array)
