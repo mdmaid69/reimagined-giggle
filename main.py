2e54dd9c8fe9fd69582b9cf70ed35bc492bd6c4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:27 +0000

    along earth brief finger computer

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
 import json
 print(json.dumps({"name": "John", "age": 30}))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
