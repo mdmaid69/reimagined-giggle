9298dba9e42d20c86e5f9058be16773395e50812
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:26 +0000

    sore regular bad burst pleasure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
