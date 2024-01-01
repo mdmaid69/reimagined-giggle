1050ea8d9b088cc5482727071045b298d1e49e32
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:27:05 +0000

    return remember wise test rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
