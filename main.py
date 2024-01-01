2e0737cfa70e589f65ecdfdcaf462773bd3cc24d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:52 +0000

    cough twist service paper break

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
