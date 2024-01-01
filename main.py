e0665ae385aa1d6183c07c56a91bbef4231d28ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:08:18 +0000

    workout there instead confused imagine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_area_rectangle(l, w):
+        return l * w
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
