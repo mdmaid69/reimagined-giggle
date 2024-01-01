a5e3aca284109828fb7952390fd1c9ca4210d5e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:28 +0000

    trip right freeze send bit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def append_to_array(array, item):
+        array.append(item)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
