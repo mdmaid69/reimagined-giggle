9441f7f0a3ef13f66143f8e7effb5ffdd2e60e8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:15:51 +0000

    female respect tidy finally ground

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
