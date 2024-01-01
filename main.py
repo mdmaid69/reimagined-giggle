3543efa314d337bf25670f77d0e951df95af439c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:08 +0000

    idea until bunch anybody cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
