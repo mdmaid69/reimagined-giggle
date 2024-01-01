67325cce0b5a4f8238c69ec8893b1c18e860155d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:06 +0000

    combination invitation effect 179bite war

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
