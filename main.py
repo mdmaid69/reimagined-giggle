400e375b7db00d54152e365496a935ac6486a6d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:03 +0000

    meanwhile today error behaviour thinkover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
