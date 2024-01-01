85fbe6aaaa9565165149bb02a189ff752a982930
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:17:13 +0000

    rough weak callout hang foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
