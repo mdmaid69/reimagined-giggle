1e504ebb30cd8c5f83ddac7f54635ad3b258fe91
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:12:00 +0000

    hit argument try ilovedicks build

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
+def reverse_list(lst):
+        return lst[::-1]
