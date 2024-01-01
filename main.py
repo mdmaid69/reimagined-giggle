333b4c45846ea1f8ee735d8090d1cecb4e359ee0
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:38:03 +0000

    slightly polite second sight possibly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_list(array):
+        return array.tolist()
