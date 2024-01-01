6977df37cb01d6c047f45419863e48670e6f812c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:03:45 +0000

    content lock kind inquire slice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
