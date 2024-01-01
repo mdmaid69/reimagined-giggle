91acee5f67b13e139960eb5c322a3343c3c55970
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:37:04 +0000

    set best blame plenty light

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
