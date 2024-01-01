906ed2431efd926d07979852b34482fd11750034
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:36 +0000

    pool system scared water weak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import collections
+def create_queue():
+        return collections.deque()
