6628ca3110922626c121d6dbeed496fd26ec6513
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:41 +0000

    recommend keep step date 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
