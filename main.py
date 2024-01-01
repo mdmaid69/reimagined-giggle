843249fbda4ccaee4afbdd3404d0881961bb002e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:42:29 +0000

    hand thinkof breakinto OKorokay mood

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
