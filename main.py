f771ef5c6d3cf674fa45964f4eb4de7d6e632213
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:41:42 +0000

    he wheel satisfactory unite mention

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
+def convert_unicode_to_array(unicode, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromunicode(unicode)
         return a
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
