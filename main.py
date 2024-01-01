3db4a0a3c79f340f5dfc95f6c87ac9d205e89ca4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:01:33 +0000

    people thinkof mess Yy fall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
