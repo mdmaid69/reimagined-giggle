99f63ad46f4d810f25e3251d461b96f90a23afd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:38 +0000

    sudden Ii fire closely only

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def get_array_as_list(array):
+        return list(array)
