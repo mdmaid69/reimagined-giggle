2691af479cac643df5987dfc67d322d79020316a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:44 +0000

    feature since independent tear smile

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def cube_number(x):
+        return x**3
