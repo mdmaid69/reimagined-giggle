1c52fdfc459bd505c019cd8ab423a7e5a3149027
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:01 +0000

    tool add correction ago chin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-from collections import Counter
-print(Counter("hello world"))
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
