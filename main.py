1209aa0d5b5386df06ad3a11c114e1485a01081b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:19 +0000

    secret sad shock offend early

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def count_characters(sentence):
+        return len(sentence)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
