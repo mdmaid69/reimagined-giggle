ac5c7ed70517695834d09b035cf01ec2dfcf4ad1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:22:19 +0000

    suckme answer guess man report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def sort_list(lst):
-        return sorted(lst)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
