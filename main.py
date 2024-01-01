6e8b89347bdfa09f18955037ece89a6ab7361606
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:38 +0000

    prepare wind proof shock complain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
