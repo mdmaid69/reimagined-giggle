d389dcfb8d340f0da37d35d11ea628561da7efb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:10:27 +0000

    price example so study rub

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
