1c97b740fc0156e63e2e66116b4d6e80dd737ca4
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:25 +0000

    suggestion scare woman chin introduction

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-print([x**2 for x in range(10)])
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
