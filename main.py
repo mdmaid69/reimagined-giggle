3a65781db2d65c79c855e89c8e979a738093c42d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:32 +0000

    war this challenge conversation keep

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
