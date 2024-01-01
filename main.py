480ba3afa9f7c697eadcfcebd330de6ba548a50f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:58:08 +0000

    smile declare cold decide phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import sys
-def print_python_version():
-        print(sys.version)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def reverse_list(lst):
+        return lst[::-1]
