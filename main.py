61f36355869467e5281c98f9997597c69e1065cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:13 +0000

    machine relief lend convenient wrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def remove_duplicates(lst):
+        return list(set(lst))
