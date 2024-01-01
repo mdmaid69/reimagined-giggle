fd41a7a38629383d8e598446ac136a1add83328d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:35:53 +0000

    trap shut Oo him failure

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
