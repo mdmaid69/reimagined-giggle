2caa3f57a84f0bcd7a95bb2d16d9d69adb83a0ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:55:37 +0000

    serious rule stupid pump shelter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import sys
+def exit_program():
+        sys.exit()
