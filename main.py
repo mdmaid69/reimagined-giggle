45d5f9c9d553f51b100549f78f7411096a26207a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:30:12 +0000

    twist valuable different think suckme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
