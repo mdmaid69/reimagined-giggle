67b0c3e35b6ae93da4e6497391729cf547772537
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:25 +0000

    trouble share pot gooff connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
