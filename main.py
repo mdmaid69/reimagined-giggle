3562d88fe41d844aaadb75b44d341124b3a6cb95
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:24 +0000

    talk here separate kind write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
