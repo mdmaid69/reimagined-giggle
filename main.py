198b52c65764328a09a5f3a2d37f3ca443f8a6f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:10 +0000

    ever appeal shall fact stop

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_median(arr):
+        return np.median(arr)
