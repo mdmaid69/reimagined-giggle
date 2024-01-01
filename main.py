b5df760a136d94ef94aafe742f42ad80dd8585b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:59:10 +0000

    balance human healthy divide dull

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
