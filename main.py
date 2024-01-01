9b05cde048ea41c4b36b635ab5f68d8521bd1b1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:47 +0000

    shut certainly flood concentration workon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
