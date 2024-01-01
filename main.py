c3c33b6780219149d6ee61e3efa5b92205a91c96
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:59 +0000

    hurry model mark 571enough square

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
