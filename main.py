9bc78ec695ca48bd6f69a3ac894538d9752683e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:16 +0000

    giveaway tear either either weekend

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def calculate_speed(distance, time):
-        return distance / time
+import datetime
+print(datetime.datetime.now())
+import random
+print(random.randint(0, 100))
