70783255815ae467230091a66d2b6741c9087027
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:19 +0000

    recent story far influence drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
