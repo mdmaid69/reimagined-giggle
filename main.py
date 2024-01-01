881e3f640b6b6b4b5d1afa50a065243992daf49f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:18 +0000

    deliberately reply broken its basis

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
