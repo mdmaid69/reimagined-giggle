37dc7c700c9cd143a51838cb424a4d8a07d0a7dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:02 +0000

    sensitive software judge definite table

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
