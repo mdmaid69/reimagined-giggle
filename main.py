32ff6a7df25857176f3061bf1be7769ffd5aeaae
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:17 +0000

    lot patience around company period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def append_to_array(array, item):
+        array.append(item)
