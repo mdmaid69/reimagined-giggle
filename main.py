1918c692d21161757c3cec69ef3ed8917bd3a34d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:27 +0000

    everyone quick choose crime breathe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
