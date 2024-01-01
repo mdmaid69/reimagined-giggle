94cd550e135cba6efc430a33f314973a2c8355c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:34:50 +0000

    pen right slight takeaway whichever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
