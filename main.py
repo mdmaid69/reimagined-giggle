4fbfad3745697014088b4dbc95a4a9bf597645f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:33:55 +0000

    179bite pleasant search wonder past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_pressure(force, area):
+        return force / area
