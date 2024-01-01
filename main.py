76e59e1e877fd1d948caf5f64ae91a46cce56eef
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:50 +0000

    rare experience comeround variety boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
