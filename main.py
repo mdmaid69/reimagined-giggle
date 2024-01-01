542879c812aa910caf911382b608e11dee153597
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:36 +0000

    command modern need giveback tend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import time
-def get_current_time():
-        return time.time()
+import tensorflow as tf
+print(tf.__version__)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
