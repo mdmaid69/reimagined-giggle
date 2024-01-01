aaa79b0c71a313a5b74a44c835cf869ea6c614f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:28 +0000

    mess meal bringdown various narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
