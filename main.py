e16f8a56f662ba0df087da88d7e5b72e94a077da
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:28:51 +0000

    immediate operation mean probably exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
