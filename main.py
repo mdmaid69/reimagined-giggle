80aecf12742f87a64999b18902cb3db1bdf97978
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:31 +0000

    brave test reply raw holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def get_array_itemsize(array):
+        return array.itemsize
