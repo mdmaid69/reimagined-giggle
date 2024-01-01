f669e0f3f41565f4df0d68f4bd6dd1f106918ccb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:04:54 +0000

    survive anywayanyhow protest fine before

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
