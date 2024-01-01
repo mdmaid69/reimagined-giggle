285854af8fced61a153c1729ba545388ab9ddc42
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:47 +0000

    alone fence where task treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
