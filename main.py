83396094335f753d72ae2cbc5c21866a9749a307
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:56 +0000

    nose also welcome repeat runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
