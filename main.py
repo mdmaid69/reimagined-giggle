4d0a737de546725e8699ee5c25a7d27e515b1009
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:35 +0000

    turninto hers yes middle strike

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import tensorflow as tf
-print(tf.__version__)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
