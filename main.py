62c3879941ea585a4999ea25e7e9b2e067d82f05
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:46:05 +0000

    murder wash container there 566end

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import time
+print(time.time())
