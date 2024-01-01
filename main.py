a3e7401fb91ec7d98039661c31ecfb5803bf966b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:06 +0000

    sound crisis boat be even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import numpy as np
-print(np.array([1, 2, 3]))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
