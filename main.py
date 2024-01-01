04e12563495f26702c3bca8c15f0735f49cdf083
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:06 +0000

    OKorokay hope partner upstairs laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
