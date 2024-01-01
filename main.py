d5766c577d615177932db69d79988482adb66cd2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:03:40 +0000

    both central every callback it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def multiply_numbers(x, y):
+        return x * y
