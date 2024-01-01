7b83a19af6540c577010c78380469594a869c508
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:05:18 +0000

    film contact write juice otherwise

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+print([x**2 for x in range(10)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
