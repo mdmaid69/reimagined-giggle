623bc639fec027ff5a95fdca50da5f3cd35d608a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:37:16 +0000

    holdup programme face wide neither

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
