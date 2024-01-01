e7103bfb161e1992353912eefe344349e391f9b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:27:30 +0000

    switch scream shallow unusual prove

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
