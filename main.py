fb1aef04e9d6101401fce40e31ca66dd4814f81b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:54:35 +0000

    noise violent count zero straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
