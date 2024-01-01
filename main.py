13c30f3786b90e80c5c6fff24a0ab3fad5a813af
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:49 +0000

    difficult miss wing water backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
