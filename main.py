cb2578081a3abfc775d76df863005a2d5319c226
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:05 +0000

    family trap interest glad keepup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
