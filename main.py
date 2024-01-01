09c033b0d3376a5acdacfc3d3d1bc99dc6694c44
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:35 +0000

    difficulty record my influence afterwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
