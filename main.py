a6419c9cb132075b3684464993567c1140cc5edb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:33:30 +0000

    wall talk hurry stir characteristic

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
