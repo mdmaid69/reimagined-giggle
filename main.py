6ae55afd6c2f734cdffa6cbd07726f0f5bcdf6d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:48:13 +0000

    work crisis translate weigh fortunately

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
