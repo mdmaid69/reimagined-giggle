4a797a7e2700b11541c0f1e7192731fc5b7579f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:03:54 +0000

    content classroom consult message concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
