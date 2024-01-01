d6d4f6d57738ab011ecbf9e5bbe96306063dfa55
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:47 +0000

    murder getaway 572enter good description

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def reverse_string(s):
+        return s[::-1]
