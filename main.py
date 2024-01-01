c4dc917125f406b0ec72a80704febeecb3322bf5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:46:18 +0000

    mean apart hurt improvement already

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print(sys.version)
