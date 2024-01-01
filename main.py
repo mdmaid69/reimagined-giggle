3ddf1f6f6f095ea5bab8745fc821b559552d768d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:11:56 +0000

    gentle print giveup mouth precise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_item(array, i):
+        return array[i]
