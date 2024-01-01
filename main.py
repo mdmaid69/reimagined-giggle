dad2dc503b5c4502120d553f858efa4ba6c21113
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:56 +0000

    Ll 568energy 568energy identify though

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
