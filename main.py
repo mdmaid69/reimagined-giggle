7035c0c35d20afeed0efcfdb2572c45999ae9b02
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:08:43 +0000

    steam uncomfortable time very father

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
