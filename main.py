fd3c6d5ff18f2602e689b13709e5edc0c7b22637
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:23 +0000

    give unite short consult underneath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
