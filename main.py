2a93469e4056e192814836febcf0a27e5b9fa22c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:18 +0000

    spray hurry already school discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import os
+def remove_directory(path):
+        os.rmdir(path)
