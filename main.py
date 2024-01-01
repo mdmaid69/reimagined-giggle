84ffa251a41bdc66dad0249645e28c3a082811b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:47:05 +0000

    minor pupil dick trick hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
