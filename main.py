93e9b7568a0f37f2a76302e312b67bf45786129d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:30:21 +0000

    grateful performance but insist home

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
