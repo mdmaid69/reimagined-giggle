6f11d99ba723136dff77101b0591c3dea97271a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:46:08 +0000

    description light pleasant tear broken

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
