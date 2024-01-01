96230fdd405b38a971fdf5b589e4b7910cc02416
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:29 +0000

    tool ocean spread talkback Nn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
