1420df61e117713944431ebbae8ebc951c26c61b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:56 +0000

    pronunciation boil convince taste work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 def calculate_power(work, time):
         return work / time
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_floor(x):
+        return math.floor(x)
