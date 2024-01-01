c533d6cce80e976f133fbc7c5f2b39c5d140dbc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:39 +0000

    translate few shoot just find

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
