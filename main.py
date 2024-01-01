10a8bc3e778f6ceca83fc12ba3a0bff18b12bb2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:42 +0000

    insist combine bath safe negative

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
