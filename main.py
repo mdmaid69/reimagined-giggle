fef0f2abbc61aa645cb2ebcef285e45c1059ae13
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:35 +0000

    ignore target nor allow argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
