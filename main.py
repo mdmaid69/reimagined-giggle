d8521d2575a40c476a992be6c5084c022fb69ca9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:31:44 +0000

    design definite lend scale business

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
