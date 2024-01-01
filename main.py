8bdfb52e2be3e8f0029cb063f3886483191c4951
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:45:20 +0000

    person crime colour sensitive point

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import random
-print(random.randint(0, 100))
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
