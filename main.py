c87eb98f5d497ce1a79d725cf6e9191c64ddaec2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:48 +0000

    objection public available fortunately through

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_str(array):
+        return str(array)
