878b71e8b64a3573fb6bf3b894b7699730e844df
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:03:38 +0000

    passenger take sensitive knowledge usually

--- main.py
+++ main.py
@@ -3,6 +3,6 @@
           if n % i == 0:
         return False
         return True
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
