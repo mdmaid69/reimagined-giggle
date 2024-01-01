77f85cf4c050f111777e5d09998494833e29c2ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:32:19 +0000

    slowly expensive neither earth profit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import time
+print(time.time())
