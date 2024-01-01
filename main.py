2a16e4ef239862a76aa103ec8e5881287916f7f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:26 +0000

    dry male path press mystery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
