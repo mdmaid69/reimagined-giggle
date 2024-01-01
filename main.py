4b110be4b608e465383e396eb5898d86ca71e608
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:06 +0000

    say still great knowledge criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_work(force, distance):
+        return force * distance
