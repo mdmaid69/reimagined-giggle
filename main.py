d0801138cbb3a5ad09fd6f17af812a3afe47a641
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:16:02 +0000

    benefit late getback us cross

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
