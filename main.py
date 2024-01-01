ade370a3170830002f84df59e323c805b4a9ae98
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:01:56 +0000

    building while increase immediately after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
