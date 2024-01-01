d4553e56fb35c9d8f7d84bcdc207eb00f796324c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:46:21 +0000

    brave hold needle Nn honest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
