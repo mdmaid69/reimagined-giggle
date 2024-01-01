aa3221764877a96af16460702290789e56fc2b6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:46:26 +0000

    smell definite another laugh birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 list1 = [1, 2, 3]
 list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+print("Common elements:", set(list1) & set(list2))
