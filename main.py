2908ef0bfb47c77f2074c57b13ef7eef20fb24c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:44:01 +0000

    thick medium beyond waste quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
