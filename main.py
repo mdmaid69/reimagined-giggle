cd4d03fd6f1718c60913ace7066228f80a72944f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:38:08 +0000

    narrow reliable bang beside illness

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
