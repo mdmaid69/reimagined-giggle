8507425ac8811d1b8ea2a969b7d1082ce5cc1f45
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:18:27 +0000

    attach still meet seem speech

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
