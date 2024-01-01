ad42d9720a4df5fa1bc64b06aec33151539f32cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:51 +0000

    there which that mention father

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
