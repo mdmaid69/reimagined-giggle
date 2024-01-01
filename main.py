2eb79e14ef5acc93b3c8b08a3444b5137257ccde
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:14 +0000

    soil operate same closed press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+from collections import Counter
+print(Counter("hello world"))
