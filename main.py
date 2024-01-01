4962a89322c0579097133846b02f7407edaa3e35
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:49:15 +0000

    bet friend agreement Bb skirt

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  def is_even(n):
-        return n % 2 == 0
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
