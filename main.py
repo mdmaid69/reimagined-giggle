27e2e151aa7df63ccebcd18564f550a45acc16ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:56:44 +0000

    warning one separate polite nothing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
