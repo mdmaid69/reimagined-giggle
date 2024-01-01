f620dc8276516c53374fbc92f843f219b2b23d1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:48 +0000

    personal across in offer culture

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
