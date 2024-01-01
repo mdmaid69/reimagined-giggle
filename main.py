8bba2e933b1a6ea550099b8350155470f6676efb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:03:47 +0000

    extremely frame before print getup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
