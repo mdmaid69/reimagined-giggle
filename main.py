4d8985352c9163b3571d9d76156e7e6ce3e074c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:10 +0000

    record spot cross keepaway today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
