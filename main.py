d76f878520440f5898baa201390a68208ee32cf9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:37:37 +0000

    cover advantage union embarrassing goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
