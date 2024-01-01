888b641ee02be7d30864200a1c36e4ad4a5d7a54
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:34 +0000

    provide vote avoid myself importance

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
