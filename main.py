616638ce2b5dc8a22d262e1be13ed716177d8ff2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:39 +0000

    hang there sell free doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
