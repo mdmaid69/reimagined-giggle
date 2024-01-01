400fd2459c0deb5dafd055a8919cb1c21d6bc2b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:28:43 +0000

    whether organization attention careless information

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_area_circle(r):
+        return 3.14 * r**2
