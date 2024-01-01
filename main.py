d372963b48d03ee54a8f231f037ca6570cf8f1df
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:57:03 +0000

    shout blow runinto rain man

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
