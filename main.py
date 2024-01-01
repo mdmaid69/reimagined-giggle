ef7e704bcd993b689ef3680027d35d8b89ec22e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:43:27 +0000

    cough smoke port answer view

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
