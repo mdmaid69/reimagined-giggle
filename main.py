1e934843585fd77dabe45930f39cdea13bdc6b0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:18:32 +0000

    quick finally task tall class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
