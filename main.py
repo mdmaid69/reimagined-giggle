25bf41149dcb3818406a466ffdc89fb1ad7a6935
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:58:47 +0000

    find crisis like agreement children

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
