cbaf2d2cbff733b9e454f685ab7607a614bf24a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:48 +0000

    drawing sometimes beside design certainly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_odd(n):
+        return n % 2 != 0
