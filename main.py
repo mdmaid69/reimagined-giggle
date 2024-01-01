35e515b72464f8190f2e10f0d1959997d3b59c19
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:52:41 +0000

    favour house uniform rain belong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
