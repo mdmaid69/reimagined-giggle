bed4d5fbd8ccf50a6a25121d5f6c1832b0347f22
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:33:34 +0000

    across team doubt breath basis

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
