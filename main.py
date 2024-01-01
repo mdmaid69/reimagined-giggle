1af2568f876d6118918244311e476e1a4d8a88ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:58 +0000

    drop bus thorough where film

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def cube_number(x):
+        return x**3
