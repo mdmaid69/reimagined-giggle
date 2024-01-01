28e503d1435046e84027e1b6fddde794acdaf970
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:47:50 +0000

    OKorokay callup sticky sure design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_elements(lst):
-        return len(lst)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
