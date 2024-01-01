8e8dac7ce4f9d3e7187a95283b1f52a260c11b70
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:16:15 +0000

    sweet arrange visit foreign against

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+i = 0
+while i < 5:
+        print(i)
+        i += 1
