df5d9a201f86830ce6633fe82905ef762ccefd2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:37 +0000

    search town contrast fit opposite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+import os
+def get_current_working_directory():
+        return os.getcwd()
