bfc94798d2a0e62a12cdc842ea8626483c3165f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:51 +0000

    makeinto event lunch confuse table

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_power(work, time):
+        return work / time
+  def convert_to_hex(n):
+        return hex(n)
