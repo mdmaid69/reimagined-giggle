278eb13f4250448b1734fc404103ce8abd4f6357
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:05:01 +0000

    strange accidental return their pour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
