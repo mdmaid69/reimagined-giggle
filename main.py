e4a80b8e75a71b8170ae9290519fce66944f8176
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:53 +0000

    mine quick recovery however Vv

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 text = "Hello, world!"
 print("Words:", len(text.split()))
+def calculate_speed(distance, time):
+        return distance / time
