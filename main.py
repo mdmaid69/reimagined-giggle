cab70eeee544cf1f10344cbeba1908fed0a1c0fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:54:21 +0000

    tread niggaacademy care could comeoff

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
