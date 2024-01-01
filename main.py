986fd9fdd999d4cafff71f717631c8eba1b6ee56
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:07:29 +0000

    chance birth low post thorough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
