cac2a0f6248be3842332b2c1e8cac2a45519d03e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:00:01 +0000

    soak lend word himself nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def square_number(x):
-        return x**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
