e24f785a917da773b89ac627c8a31cf6607c60cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:05 +0000

    paper 179bite proper eager freedom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
