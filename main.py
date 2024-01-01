a33c0dcb46931793e5cee768610f8a77d2140239
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:04:17 +0000

    nature quiet blame various turnout

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def find_min(lst):
+        return min(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
