5e184194f30e7be95139ed16204c73e9a73039a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:21:17 +0000

    judge key help introduction let

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+print(sum(range(10)))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
