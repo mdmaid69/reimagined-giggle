2ba2ab225b5b9d9d1bb3233f4d2be30f89dbe435
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:16 +0000

    runover contents strong fortunate limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
