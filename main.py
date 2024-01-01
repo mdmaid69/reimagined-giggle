fbee9db50be6646dc8cc52bb936d337e54ab57ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:16 +0000

    pour length trial glass thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
