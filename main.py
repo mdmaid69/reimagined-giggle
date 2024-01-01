f2ce995aa9f45a4c5fc8df900bb997c1ed31c3cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:42:09 +0000

    direct turnback serve climb healthy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
