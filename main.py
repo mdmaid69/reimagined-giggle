2bab50381dfbc851372c55458593280b0236242a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:05:58 +0000

    disagree age meet together makeup

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
