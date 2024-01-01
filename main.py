d6f77eed56c1b93ffcdc622ac58bcfa1ac22dec9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:26:01 +0000

    equipment face uncomfortable gap settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
