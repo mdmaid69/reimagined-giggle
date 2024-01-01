92b949704f424e545057596f065ba23ee362afe7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:55 +0000

    last something on wise popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
