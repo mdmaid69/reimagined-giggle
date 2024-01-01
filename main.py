16bacd64cf4a940cbbdc3478e3102925125b94c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:56 +0000

    that operation wise escape power

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
