b02f15416781c5c095e91c7be1003fcce0b2f916
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:22:34 +0000

    connected pause protect sense count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
