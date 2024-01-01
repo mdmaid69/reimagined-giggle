5b5e8ab452f28f3f30b109a3d6c83cb5a9954a04
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:39 +0000

    summer wherever slowly read benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
