acd0db214b13395898ab87ca8813b21973cc0f18
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:26:36 +0000

    advise genuine opposite belief secret

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
