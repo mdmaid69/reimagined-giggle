2ab584a85be9f2349dc1c1bddf76de4cc2ec6ccb
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:36:22 +0000

    comefrom other command speak measure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
