8beb289c70e4b76dad9dab514931ebfbe0af1265
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:51:46 +0000

    issue office completely partner niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_set(array):
+        return set(array)
+import re
+print(re.match("h.*o", "hello world"))
