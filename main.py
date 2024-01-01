7496e1356109e08a28884f85c7d3cde337faac0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:22:54 +0000

    escape neat inside convenient small

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
