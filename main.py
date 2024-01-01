a506da9643d4f23e35529cc4a5607d826439909e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:40:16 +0000

    uponon blue ordinary by train

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def calculate_density(mass, volume):
-        return mass / volume
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
