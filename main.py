8c77b96d18a65db2a8e3fdb32c2167cc89338e83
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:48 +0000

    interrupt evening top upset quick

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  def remove_duplicates(lst):
-        return list(set(lst))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def count_words(sentence):
+        return len(sentence.split())
