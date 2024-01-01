ccd5db1e51f96f6193cb3dd7adec61fcad1d4899
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:36 +0000

    guide trap wave whom waste

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_as_float(array):
+        return float(array[0])
