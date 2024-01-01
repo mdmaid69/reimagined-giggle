c53589a8923a8da81848403e5585121494103470
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:32:50 +0000

    little mark represent close art

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
