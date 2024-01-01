85aeab800145df8d2992d8af89eaad7cb31a0d65
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:30 +0000

    mistake rush bend fear break

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
