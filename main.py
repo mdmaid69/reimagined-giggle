01fb40ffdd1858bc1f7ef2e39a8855398580be9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:17 +0000

    violent makeinto spray again uncomfortable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
