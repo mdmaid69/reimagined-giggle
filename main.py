360746040c68e6db3f3e04f7c5c376e09e50d806
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:21 +0000

    during discuss protect available emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
