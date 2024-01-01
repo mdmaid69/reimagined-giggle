1b70bb4230557a6e5577f2496e776495af46c4e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:34 +0000

    regularly talk slight door gather

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
