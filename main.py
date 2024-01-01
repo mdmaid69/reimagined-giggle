11d0f1cd388c98457c3909554cca04f115ffd419
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:40:33 +0000

    mine example breakin touch giveback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def get_array_item(array, i):
+        return array[i]
