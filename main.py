57da1709bd7ddf84f0fac92c42dc96a1527cb1f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:43:43 +0000

    bag hardly certainly her meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
