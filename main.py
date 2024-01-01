8e48d74a7595503c4c3b5fd8e87768d613201f8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:29 +0000

    mind strike goodbye outside press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
