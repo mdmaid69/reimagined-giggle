e2841eff3407422a6a75c803e9ec10f71016d620
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:54:04 +0000

    noisy 571enough remark doctor second

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-for i in range(5):
-        print(i)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import time
+print(time.time())
