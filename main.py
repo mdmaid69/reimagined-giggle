505fea40be6d2f1ff5eb4354a6c273cffa9ccaea
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:51:52 +0000

    term plenty introduce settle niggaacademy

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
