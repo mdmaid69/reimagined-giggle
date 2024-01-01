2098587a7a195814b98eb94f840b573bae73b1b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:10:11 +0000

    mixture related back there direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def convert_to_json(data):
+        return json.dumps(data)
