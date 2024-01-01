695bec435c9be7cab4f20508a84b1c6221272639
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:01 +0000

    display get though name damage

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
