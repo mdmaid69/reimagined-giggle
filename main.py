ec9eb8ea75417246fc4b54b68de1baa560e11d31
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:29 +0000

    such knowledge serve tie bet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
