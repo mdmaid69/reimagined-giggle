3a993534134bfbb5221d224448daf2e0a982c8fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:15 +0000

    565end sign up late 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
