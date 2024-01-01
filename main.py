56c661aeea0e22b3c8c763ef644179a3ae3db45f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:07 +0000

    room join used keep ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
