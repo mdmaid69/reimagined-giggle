5710c8d3c7ef18083848863ac56a759a8b906152
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:29:22 +0000

    home thank generally mix waste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
