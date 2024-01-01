5a2fca39683d8c0af1b376ca7f7dd85080f3b788
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:26:10 +0000

    place natural hand separate his

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import collections
-def create_user_string():
-        return collections.UserString()
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
