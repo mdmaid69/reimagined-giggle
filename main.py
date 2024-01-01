f5fbde596eb8c4860df25d467743507247541fa5
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:41:18 +0000

    year sad distance prefer bet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-for i in range(5):
-        print(i)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def cube_number(x):
+        return x**3
