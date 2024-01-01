baae0e2aa7214b6a27739e235ad996b40d8f9389
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:19 +0000

    meeting interest it appear nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def count_elements(lst):
+        return len(lst)
