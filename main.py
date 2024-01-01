340ddc3917f47b0013fecee11b21e9b4c82cea57
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:37:18 +0000

    argument belief hide fast taste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
