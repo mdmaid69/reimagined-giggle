4f96168f17bfc6f34cfaf064722dc675627cf1bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:51:51 +0000

    pattern shock myself fit answer

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
