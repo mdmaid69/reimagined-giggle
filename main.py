d6bc5c431ae083314bdf738a652a7c26af46876e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:32 +0000

    fetch communicate out always national

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
