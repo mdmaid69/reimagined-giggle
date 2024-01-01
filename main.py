4bec9cb93d618e5e26938bf4b74fae5ccf30961f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:24:52 +0000

    pour contents thinkthrough store hope

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def cube_number(x):
-        return x**3
+  def is_odd(n):
+        return n % 2 != 0
+def sort_list(lst):
+        return sorted(lst)
