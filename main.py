5f0a7f4ea978f2c399dd761e4002f11bb7876970
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:26:55 +0000

    spoil table name write rid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
