7c062bff55fce5ccf16dab80667ae5862999a737
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:06 +0000

    aim beginning 573entertainment door wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
