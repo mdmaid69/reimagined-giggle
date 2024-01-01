edac5a37532191a4031139ce81da4f4859c863ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:14 +0000

    friendly than close quit test

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def multiply_numbers(x, y):
+        return x * y
