e46e40ef940014537f6aaff666ca47f41485a0a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:53:37 +0000

    fold certainly Gg thin summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
