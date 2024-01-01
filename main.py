6f98f7a77b8567c4b6dc0ccdc8d196c4eaf5846b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:42:41 +0000

    far fast ahead course flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def divide_numbers(x, y):
-        return x / y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def remove_duplicates(lst):
+        return list(set(lst))
