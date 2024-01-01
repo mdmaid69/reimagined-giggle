f3850419c40d27063c467b95c9fd1a80a3aacd4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:32 +0000

    speech famous seat freedom any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_str(array):
+        return str(array)
