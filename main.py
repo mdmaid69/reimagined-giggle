86031ce9f9e34ebc94a21652c7bc765a44bd06d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:58 +0000

    about feed smell down question

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
