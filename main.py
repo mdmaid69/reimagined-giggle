ba547b101ba261cca2df28d86ef9113b4511674e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:11 +0000

    clever born sink spoil dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
