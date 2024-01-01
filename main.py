3bef019861601ebdaa9dc7c894677b8ac9d4a2ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:00:46 +0000

    drive off in good Hh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def sort_list(lst):
+        return sorted(lst)
