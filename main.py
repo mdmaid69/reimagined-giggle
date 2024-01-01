1d490b76c5ecb5423c4ea41696899dfa9fcf0013
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:20 +0000

    declare blame prepare yet lunch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
