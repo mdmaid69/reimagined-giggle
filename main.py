61c2328a5c5cb8e88f8070cceaccf43243643cf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:21:03 +0000

    back translate typical crime emergency

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
