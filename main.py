0bdfc71ca0e13cd2228b32c2dd6a74425aab82a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:08:15 +0000

    storm arrival characteristic regularly shame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
