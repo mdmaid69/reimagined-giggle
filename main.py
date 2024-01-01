12cbe83c176a2a27b6e313f13bc4549584c983b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:13:42 +0000

    herself borrow hurry frequent ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def sort_list(lst):
+        return sorted(lst)
