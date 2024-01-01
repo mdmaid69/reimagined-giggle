41f7582ce9a6e31e07032c9f880e1e59c5246c07
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:33 +0000

    perhaps let lesson name allow

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_arc_cosine(x):
+        return math.acos(x)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
