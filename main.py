976f47927136e3f5488fc4d0506306379a2d3b0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:19:37 +0000

    lookafter front me regard emphasize

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
