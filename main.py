d066271c88ff8f108cab80f40ed37928e128e34c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:51 +0000

    eat calm situation side loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import array
-def reverse_array(array):
-        array.reverse()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
