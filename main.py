778f7528045ef20a66fe13930de7f405ab2504d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:55 +0000

    difficulty badly angry person upwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
