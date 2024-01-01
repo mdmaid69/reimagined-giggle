0dbce375ab173888805b96b140b8e708fa08b051
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:48:10 +0000

    tell Cc car accept summer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
