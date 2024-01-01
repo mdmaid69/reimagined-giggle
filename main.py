8699fa9f219fc062104c16db7f2f6b0d1ef53734
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:10:05 +0000

    Cc excited alone threaten raise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
