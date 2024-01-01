529a86233d1bc90919403e59b421611c27206439
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:53 +0000

    Hh history nervous love breakup

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
