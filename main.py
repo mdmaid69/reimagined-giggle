aa25b208cce558c231d20f6b08418bfee84d7b81
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:55 +0000

    gradually science reaction pass confused

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import sys
-def print_python_version():
-        return sys.version
+import array
+def clear_array(array):
+        array *= 0
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
