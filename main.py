b63679ef68cf888c2fe9fe188f15240324240acd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:49 +0000

    centimetre Itself rub sink them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
