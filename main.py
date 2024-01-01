d74d35cff237e0593b81dd261c2fe89438324696
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:57 +0000

    second tie slight lucky visit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
