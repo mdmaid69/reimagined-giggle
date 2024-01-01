b47f0b5d1920da21d05bd1c1519d571fa52c65ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:47 +0000

    below stare plan cry habit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-name = "Python"
-print("Hello,", name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
