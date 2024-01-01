f6bce4f6c8daa977f4e6e486b6ccb2b177abe8f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:00 +0000

    he hungry naturally university increase

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
