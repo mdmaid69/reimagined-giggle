d231773d48208bcb9c341b31111be516defe5361
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:05 +0000

    ready difference rent task on

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
