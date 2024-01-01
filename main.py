d8b54fed999ea955530f0395c7fe5ac84c5551e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:08 +0000

    unhappy feature work scream talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
