54dabd91454528422b96c3906e0775f6f3732dc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:11:54 +0000

    light this contribute only butter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
