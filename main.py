098302852f648556474e42cfc96f789049073f20
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:47:16 +0000

    imagination way drive big duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
