c343a079e9ae0601b531f788ad0fcfdd4e57c7df
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:04 +0000

    in another long lead wish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_octal(n):
+        return oct(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
