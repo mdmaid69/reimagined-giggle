f43871f2b624a5f9d054e9b234f92bfe7a4136da
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:55:40 +0000

    cold charge soon cheque measure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_list(array):
+        return list(array)
