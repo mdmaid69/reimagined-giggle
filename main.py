18114064fedb6d4ba4c4b8cbc82b9edab6e3b3fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:13 +0000

    uponon paper base way slip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
