cb230269586c9a5281b36abade7b98c77239281f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:50:18 +0000

    tooth pupil concentration agree fat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
