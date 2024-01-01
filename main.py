d5a230a91fd1e7d40357d1f6968fdf965fc2f8e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:53 +0000

    comeback exchange constantly breakinto judge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
