1f9fed3edc394b7966ad36466d49c3efcf75b0e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:50:59 +0000

    wash up none substance drink

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
 import array
-def get_array_from_file(filename, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        a.frombytes(bytes)
         return a
-import sys
-def print_python_version():
-        return sys.version
