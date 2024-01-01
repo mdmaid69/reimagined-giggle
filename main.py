0d52c1de1d5a192a00201824eff6b6150f466e41
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:51 +0000

    emotion keep get information disturbing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
