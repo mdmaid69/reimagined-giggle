7bc29dd394bb8dd1e71bad6defed7b1c7a44fcd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:00:34 +0000

    clear progress turn air cheque

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_pressure(force, area):
-        return force / area
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def get_array_as_set(array):
+        return set(array)
