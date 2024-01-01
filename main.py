f3d61a577a807a35900e75daa9028ca4da820875
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:40:39 +0000

    famous exciting confidence apart lend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
