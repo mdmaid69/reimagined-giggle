7a29b3e533e369845a911a7590916b99314fa927
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:04:40 +0000

    detail card snow house culture

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-for i in range(5):
-        print(i)
-def calculate_pressure(force, area):
-        return force / area
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
