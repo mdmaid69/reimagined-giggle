10268613364ea6c2872075e79625fbbf28d0c018
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:17 +0000

    funny maybe voice measure Qq

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import sys
+print(sys.version)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
