8edc34b56f2c8321dd4c2dbf544078dcbffc7f5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:22 +0000

    likely relative interfere dare battery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import platform
-def get_os_info():
-        return platform.uname()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
