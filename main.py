3e771a452a4a3a21421527281fd052dcbf0cc58d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:24 +0000

    thankyou compete soft wrong appeal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+for i in range(5):
+        print(i)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
