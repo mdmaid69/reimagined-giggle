6e9e6b5e295a76729fe8a5176cc5606a9e180355
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:34:44 +0000

    nice though innocent would summer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
