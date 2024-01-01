fadfec937bfe3db67c7fb49ae42424d191598ae2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:38 +0000

    nerve wet star sight fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
