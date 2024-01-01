6fce6c748d9e9004c21f99e901fd0e5a3de37338
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:27 +0000

    zero confuse figure surprised design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import re
 def split_string(pattern, string):
         return re.split(pattern, string)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def is_even(n):
+        return n % 2 == 0
