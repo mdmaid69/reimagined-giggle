2cb9fcd06cde3ba0b4f8c6c3bc6065d8bf0f491d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:43:28 +0000

    effect increase hope worth straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+for i in range(10): print(i)
