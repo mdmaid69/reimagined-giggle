6a3889ed81ed500f028a77681e46341f54e04498
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:50:19 +0000

    agree feed label nurse trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
