e1174d5c070fcc786a6e68a0c7b4b911ae8449af
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:37 +0000

    brave runinto second meeting left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
