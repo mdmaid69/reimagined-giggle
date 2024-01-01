ad794dfd45cbd776bc8097f228f4e8d3f1cdba1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:14 +0000

    keepfrom trade squeeze press bag

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def remove_duplicates(lst):
+        return list(set(lst))
+def find_min(lst):
+        return min(lst)
