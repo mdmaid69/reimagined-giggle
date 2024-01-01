47251be85ddb89908a47ccab635e975e08156374
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:44 +0000

    grow progress move offend getaway

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
