15dae223bc957ae24269176e90b290bb74c4d5d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:17 +0000

    scream suit minimum whoever speak

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
