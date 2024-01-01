3b1721b72d2eac289138aa5dbcc3e3d66257d2bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:28:23 +0000

    height guilty reaction anywhere reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
