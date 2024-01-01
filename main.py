aea2eaca72de757eaab9649f26031b5025cf0cbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:32:58 +0000

    present skirt callback goon dress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
