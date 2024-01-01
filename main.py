c4ec4eaea97621591ef62a9469a03fe3cb6a636e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:10 +0000

    throw your edge maximum accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
