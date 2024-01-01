d08a9f56cde695de26687b808dbd2d2b6c90356a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:38:54 +0000

    team behave thumb pure university

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
