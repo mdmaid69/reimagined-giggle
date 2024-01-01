e7c3d5835b23d646f8674855c8bb52af9eaf7484
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:49 +0000

    aim city fire 571enough his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
