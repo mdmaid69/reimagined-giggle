f4674fbf1797ba45712a1b4186df712b25a2f96c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:40:16 +0000

    mine special so me workout

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
