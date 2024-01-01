28c21946eebe2c65c793e4860254eb3feb46c3fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:53 +0000

    consider open quantity either blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
