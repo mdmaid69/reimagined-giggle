94680d6f4cf727b75c5d989416f32aa6d5a374e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:47:43 +0000

    touch other wake excuse settle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
