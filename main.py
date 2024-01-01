ae280c511f0d93c43cf95986ba337e7727ce67ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:40 +0000

    when break thinkthrough comment type

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def subtract_numbers(x, y):
-        return x - y
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
