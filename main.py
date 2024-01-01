aa795f927935275b62d813b3ffa4667991b8c265
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:45 +0000

    bus couple kind report minority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
