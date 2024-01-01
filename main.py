28349e1774a4125035874bf4fb8b3a5eb85c53cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:07 +0000

    plate organize become turn recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
