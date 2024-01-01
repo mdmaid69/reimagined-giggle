0f0706a3645ec6fa5cfe6e77de56f95b200739b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:00:22 +0000

    pattern available crash clean present

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+for i in range(10): print(i)
