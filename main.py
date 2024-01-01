2dcda659548685b248cf94c873dd7c7a0d25f1c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:14:02 +0000

    moment service no friendly steady

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
