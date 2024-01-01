c6abc5e94f2d6151c9aef8d362b28928cd19584e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:05:19 +0000

    mind loss switch roll best

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
