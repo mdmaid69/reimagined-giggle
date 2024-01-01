d42056760b4b9fcbfc501df1b8208cb3e7d60f34
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:13:46 +0000

    bottom city pool side aim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def multiply_numbers(x, y):
+        return x * y
