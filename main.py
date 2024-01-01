75edb2c73a6444f69e1da505ecab92f145f36d4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:31:26 +0000

    honest development anywhere steer slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
