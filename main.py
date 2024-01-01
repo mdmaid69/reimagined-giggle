7aa63bb4d022ef6ee1d07d8c9ceef904f44b1a6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:02 +0000

    need bringdown around damage adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
