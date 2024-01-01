fb3e4f148cc4140de703d3d40a2d299c8e3d67ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:58:39 +0000

    reaction cruelty mouth duty admit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
