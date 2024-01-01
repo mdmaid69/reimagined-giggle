d30061b50564285a7a47bf9a3a5e6208295f0fa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:50 +0000

    possibly against short also description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
