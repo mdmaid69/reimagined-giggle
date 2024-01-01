4da2a1817725d1a237fa19be13ac00657c74a517
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:33 +0000

    anxious pollution circle personal glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
