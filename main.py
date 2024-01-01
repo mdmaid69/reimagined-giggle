cbc83ba6820b336011b371449392b8ea4e765ce9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:43 +0000

    refuse take lucky uncertain afford

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_volume(length, width, height):
+        return length * width * height
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
