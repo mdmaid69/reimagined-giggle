ab1eb030e8110501c4730763ecef266cd56ea56c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:02 +0000

    comeoff scream brave pause trick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
