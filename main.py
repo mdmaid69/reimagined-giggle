09535a91994b2a34bd22b70fb3334196a80632d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:37 +0000

    through division tour invite workon

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def reverse_string(s):
+        return s[::-1]
+def divide_numbers(x, y):
+        return x / y
