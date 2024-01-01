4e0cf4fcfe3f10b5c01d0260e06bdb25f83d862a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:17:38 +0000

    path border lookback(on) importance him

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
