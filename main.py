76687396deadfe7d0fd57529b26ffa96a3ca518c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:22 +0000

    Mm Vv straight bush Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import sys
+print(sys.version)
