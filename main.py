bdb58998a19711e0e9b097f5f9a11bc20fe1a0e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:09 +0000

    whether use fierce depend rise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
