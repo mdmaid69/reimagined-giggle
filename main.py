f03b14cf36c869f50293553b826514b19cfe5104
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:11 +0000

    involve original peace fun only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
