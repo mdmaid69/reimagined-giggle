b151609365ac4985aed73bbeb2d0e4a84fb6930c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:15:41 +0000

    alone block once peace nice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
