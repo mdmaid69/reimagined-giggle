36aae59a0ae9119de36cef7454bde84a35f9820f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:26 +0000

    excuse success trousers inquire next

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
