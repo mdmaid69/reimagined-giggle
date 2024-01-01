bff7d9013bf0208171a56a4778b90d72dc3d1ace
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:03 +0000

    sign mess accordingto direct success

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+print(sys.version)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
