95cb7665e68be953406514ccd7aa26fd516e28c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:07:45 +0000

    journey walk name take art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
