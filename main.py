47b416412702d5d737e030e0aea334b62aadf7a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:30:53 +0000

    support damage connected side attitude

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
