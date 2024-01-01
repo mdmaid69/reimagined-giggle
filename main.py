bd9e4dda4cffec7503855df6cacb29b3e5c07cb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:49 +0000

    the watch happy brave serve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
