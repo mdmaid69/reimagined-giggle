086cde9f5f9510708c8152b46e01a2f83d29d662
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:15 +0000

    cheque accommodation least area move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_base_name(path):
+        return os.path.basename(path)
