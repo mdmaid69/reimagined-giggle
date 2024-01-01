b7fc7f2a736d216bbdd9b03b65bee730660f35eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:38:24 +0000

    care which knowledge sweep rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def sort_list(lst):
+        return sorted(lst)
