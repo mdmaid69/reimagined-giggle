008bb25d25973540eac448a8902de3b82cafc166
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:52:06 +0000

    ashamed fold contrast comedown house

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+print("Hello, world!")
