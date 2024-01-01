dbfdecfdd02935e2dfce8350d623628450c2b034
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:43 +0000

    below result pocket fasten help

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
