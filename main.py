2ab240ffef886b9e9f06a9f34f72df0d397b040e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:43:07 +0000

    distant obvious rare rubbish cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
