a7a7fac208f98aa2192fc83d6088ed5adb89bec1
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:27 +0000

    tired not forget not basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def convert_array_to_list(array):
+        return array.tolist()
 import math
 def calculate_pythagorean_theorem(a, b):
         return math.sqrt(a**2 + b**2)
