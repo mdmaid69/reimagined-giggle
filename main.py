741a327225cafed10b32cc358849fba180995e92
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:36 +0000

    pocket pay Nn limit ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
