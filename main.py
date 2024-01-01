350f30cba21b4a5d5d18d9d26ce3b31c18c06e21
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:31 +0000

    them whether shake about tour

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def convert_to_octal(n):
+        return oct(n)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
