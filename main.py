a0a315aaaa25fa4958c27ce3adfd486c690bfe78
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:30 +0000

    jump base cruelty door opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_int(array):
+        return int(array[0])
