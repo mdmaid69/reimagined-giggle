1fb7af9680e61ed1f1e853ac442579c8e127235a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:11:38 +0000

    score movement ocean more because

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
