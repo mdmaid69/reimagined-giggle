300789db71a3ecd9a1827fba337917c372fdd609
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:58 +0000

    nor rid quarter squeeze typical

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
