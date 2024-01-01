d4f4bd9b5b6c99ec1899058082b649b55d680eb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:05:00 +0000

    plant some definite threaten thick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def change_working_directory(path):
+        os.chdir(path)
