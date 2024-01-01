bbca72dbe760a7440e39f9256eab74aa9c6bdfdf
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:39:33 +0000

    chance murder available oil announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
