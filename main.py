329e589f5ef4bc7692df7cbe91856df6b27cae83
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:37:22 +0000

    Tt feature length wrong drive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-print("Hello, world!")
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
