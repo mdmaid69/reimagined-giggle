417f10b7c667c4b388b04d0ab3bd453fc591491e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:51:49 +0000

    goout order arrange keepup switch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
