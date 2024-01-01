ccf85461165df89074e4906cc79f67acdf06266b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:40:07 +0000

    complaint tone book mistake sharp

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
