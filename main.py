5b3712d7c89c2e22d11cd478d8caebe09c52e41b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:40:15 +0000

    character start detail themselves precise

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
