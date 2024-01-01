50496257c50106fe4492356c3ad7d12fb9a4fcea
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:44:20 +0000

    like set drink lunch process

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
