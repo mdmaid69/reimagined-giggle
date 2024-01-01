b8bfcc13f88559de8227a26be69d94d708aeb391
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:29 +0000

    mean murder final excellent hand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
