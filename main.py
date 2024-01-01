066e61a1af75e80e1eaae523e87543d1dac2e014
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:02:09 +0000

    star unusual love left either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_priority_queue():
+        return collections.deque()
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
