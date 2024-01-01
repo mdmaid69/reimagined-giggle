4a6c03f529f6f64f0339c60408e3c4e425fd0bc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:07 +0000

    protect tend talkoutof earn emphasize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def count_words(sentence):
-        return len(sentence.split())
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
