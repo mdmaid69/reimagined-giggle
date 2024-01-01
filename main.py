4d959df52fb90cd72e52198e5812d73e60692d90
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:20:08 +0000

    glass firm occur backwards development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
