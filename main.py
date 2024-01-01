4cb1a62c89254503bf3cdb3a2db468063e39cba8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:13 +0000

    hope chin step communicate production

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+print(math.pi)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
