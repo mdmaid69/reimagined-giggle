3f30c35039014f648b5f7165201d15a22a9a3f27
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:51:25 +0000

    fast common dream stiff little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
