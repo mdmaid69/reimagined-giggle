7c0266ff6b4f71135ee1ee13d2b0ba23828979fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:02 +0000

    mark properly blow big plan

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_speed(distance, time):
+        return distance / time
