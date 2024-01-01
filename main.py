58cac04fe5f57c22caa0bd9d01c071144e00cd0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:26:40 +0000

    zero chief stranger name blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+  def convert_to_binary(n):
+        return bin(n)
