86b30eb74eda947ccc2e1ec940c76958ad1f24ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:13:58 +0000

    Oo Dd memory search match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
