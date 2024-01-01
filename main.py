0814a17b11e3f0b31fe2f3a0e2cc19603e6cedb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:00 +0000

    attract stop precise dead distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def get_array_item(array, i):
-        return array[i]
+import sys
+def exit_program():
+        sys.exit()
+def calculate_volume(length, width, height):
+        return length * width * height
