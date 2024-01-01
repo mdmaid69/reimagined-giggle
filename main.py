aa50cbb5f828cb6973ac8e612f5cfa6b3048069a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:44 +0000

    form unlikely show train yesterday

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_density(mass, volume):
+        return mass / volume
