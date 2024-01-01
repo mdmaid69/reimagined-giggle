1f118df23517a9f47a34619c2ddb9adf59f40683
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:14 +0000

    last sick let see edge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def convert_to_binary(n):
+        return bin(n)
