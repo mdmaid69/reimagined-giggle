d3f30567c5db90615b82a0e428a2a2813f665653
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:37 +0000

    gift emergency love takeout ours

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
