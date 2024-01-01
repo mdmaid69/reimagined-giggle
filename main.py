305bd61538b843c68f6df44f40d45686b1c37670
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:03:59 +0000

    rely defeat sight Dd harm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
