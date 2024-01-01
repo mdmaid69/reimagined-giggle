2aa5d77e3915ab280907aea91cc8c2507dfacdd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:30:05 +0000

    detail direct effect straight cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_user_string():
+        return collections.UserString()
