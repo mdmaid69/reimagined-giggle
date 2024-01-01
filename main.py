36b2cc43099782d36da65720481aa00ce31b5958
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:03 +0000

    interview odd follow minimum spot

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
