24b641bf8b5576cbf811b367c80d329cf946f0de
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:12 +0000

    morning ability prepare closely replace

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
