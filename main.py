415e3d50286a314f0630aebda4fedd20e148029c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:37:27 +0000

    food punishment tell full machine

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
