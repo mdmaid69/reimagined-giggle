0ce4ef04b48fa146cf7c76b257c6719510893bb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:30:30 +0000

    keen clean out arrangement worry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
