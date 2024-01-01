78327b2b9385cd1e648c9d91c5e0f07990cca466
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:37:20 +0000

    character giveaway turn chase comeout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
