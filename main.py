4228dbd44edf86408c53bdeb0325e06bb53b7450
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:41:29 +0000

    someone cut keepout represent but

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
