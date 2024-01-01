f5146a23265d7fe467eb19f08a24ef2be7a88a4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:31:13 +0000

    justice wave lean drop support

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
