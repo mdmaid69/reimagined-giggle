8a0c01610a50848ef5c8a9d1a9785eba59c2ab56
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:22:28 +0000

    every trouble weak anyone stay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
