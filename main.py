b424e6acd11693fe20db385eb8c36d0d5b72b608
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:42:46 +0000

    unable every behave introduce shock

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def reverse_string(s):
-        return s[::-1]
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
