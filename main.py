f724de59e2c32d64c792e11a719a0a0f29dc5ffd
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:44:01 +0000

    love skirt kick lock deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_min(lst):
+        return min(lst)
