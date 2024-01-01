e78c1d10604e441497340e766684df93d1c6b764
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:33:11 +0000

    tend greeting trust against beat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
