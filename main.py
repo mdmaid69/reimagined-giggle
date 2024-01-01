126a487d78c68471b252a10b3effd3f28ba0a652
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:19 +0000

    cover trap little winter clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def calculate_area_triangle(b, h):
         return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
