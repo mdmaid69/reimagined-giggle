00d1e86a45a6a8907b87d402c1f207eaac82d6c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:16:12 +0000

    dear cold learn regard giveout

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
