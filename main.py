7a8bb9d244390aff69a426bd3603a180cc97993e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:04:38 +0000

    tape young protect bury well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def convert_to_octal(n):
+        return oct(n)
