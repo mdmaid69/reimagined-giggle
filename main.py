1b52d240df330db9dc8a5f87db52da2e2a5722df
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:56:45 +0000

    name waste history instruction deaf

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
