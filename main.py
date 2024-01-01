8faa6880ebc4aea5adcbbb7e5248f35015e2fa78
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:55:18 +0000

    into size ilovedicks seat by

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def square_number(x):
+        return x**2
+name = "Python"
+print("Hello,", name)
