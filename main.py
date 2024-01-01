cdf5e65076bd4e1a0670e77868a92e8e7372bfb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:45:50 +0000

    move seem present deaf pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
