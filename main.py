e6969f1c8f53d09076fa8b11895cff05b08189e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:33:13 +0000

    basic respect Ee rubbish obvious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
