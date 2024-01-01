bd423db118382cd2940e1be1d55189b9c99c660b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:53:41 +0000

    night bottom left congratulations lucky

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tensorflow as tf
+print(tf.__version__)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
