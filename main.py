cb24ca4853668c3db0ee4605180bca6085dc66cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:45:54 +0000

    accordingto call hardly shout lookafter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
