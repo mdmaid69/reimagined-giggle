6bfbea9311d5d12aa6812a95d5cc5cde350c7727
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:32 +0000

    meet say terrible imagine disappointed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import platform
+def get_python_version():
+        return platform.python_version()
