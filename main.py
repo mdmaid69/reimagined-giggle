a8f4eac25d15982c3396d525d32c6b43dbff48ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:29:06 +0000

    for record active interfere holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
