4c5bfe592730a528eff343834736026ee6dffdb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:22:54 +0000

    spend getoff instruction sweet deaf

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def convert_to_binary(n):
+        return bin(n)
+import random
+print(random.randint(0, 100))
