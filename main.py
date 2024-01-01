b721038d2c7b0efc00a2ae72ff297442b71a9eae
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:30:46 +0000

    holdup callout oil shade limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
