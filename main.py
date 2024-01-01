c66c04afd12e655df5a5ce688e624020da7711fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:38:20 +0000

    business which criticism turnoff rush

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
