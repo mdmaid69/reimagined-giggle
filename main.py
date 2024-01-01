e5eb08d22bbf09ad4d0ee595e1fb32a3559cc5a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:38:52 +0000

    yours tear keepup mine confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_unique_words(sentence):
-        return set(sentence.split())
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
