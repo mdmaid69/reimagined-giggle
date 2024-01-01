8879811f9877e91249bd18fa74fd87cbdec78916
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:43 +0000

    drop success near sick fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
