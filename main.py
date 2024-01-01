58cbf2588614a74fcd16479d25e7e92ecfbb273b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:16 +0000

    calm excited certainly flat size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
