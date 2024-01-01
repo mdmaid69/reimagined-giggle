a358dcefadd4761f555e8461575a6099a4233228
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:27:26 +0000

    nor height for trip obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
