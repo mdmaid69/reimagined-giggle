18ea6767bb7e069f662d3d173b145d50db71b33a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:30 +0000

    communication treat doubt already next

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
