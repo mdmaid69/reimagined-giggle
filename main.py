0efd0a19f62de941495f038d218f3d47a7bd418b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:17:19 +0000

    ordinary never dream thanks survive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
