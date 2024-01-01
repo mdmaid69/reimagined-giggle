b0bb62b227613f7df3f59e7158dc8a096e48f2dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:01:48 +0000

    simple criticize fire rank speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
