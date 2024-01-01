dcf5ef8252ed535c8d42e89707e212dcd55c45bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:02:02 +0000

    declaration suit ground turnaway experiment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_octal(n):
+        return oct(n)
