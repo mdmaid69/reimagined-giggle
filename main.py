594d39b084ce369306fc78192d6f8428e79dca75
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:06 +0000

    appeal myself fit home repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
