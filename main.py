2d0a32972278633fceb2c63e4c64df6b8d4af411
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:59 +0000

    beside pattern rubbish guess foreign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-print(sys.version)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
