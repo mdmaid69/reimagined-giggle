59ed12f7837dc94a0db7182511ad888724bbdf98
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:12 +0000

    offend event this account possible

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
