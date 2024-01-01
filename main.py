da12b61af98b5b716c4948b75a2a146d47f22c10
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:27 +0000

    fence hardly turn nose dream

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
