6d42f86c5637ef439c1840db404662e70f28ee32
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:53:22 +0000

    soon produce recover direct breakout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
