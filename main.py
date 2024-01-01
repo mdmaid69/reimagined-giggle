d96548ed4aa339262d8200ec31c34274bcde2903
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:16 +0000

    dance drawing bang trap heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
