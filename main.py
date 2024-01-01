f682d2070d6bb18e51b20bf91b9391a0fa226523
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:31 +0000

    whistle I style sensitive store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_priority_queue():
+        return collections.deque()
