2096e0a410e709e1d03cd1703872c931213b405c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:11 +0000

    play no border wrong less

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
