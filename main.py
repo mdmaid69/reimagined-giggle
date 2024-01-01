7c5c60d88f65140f4920537f930c2dfe05723577
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:12 +0000

    wall direct address sink sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+x = 10
+y = 20
+print("Sum:", x + y)
