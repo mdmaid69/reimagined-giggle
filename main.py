71f2ee1404e84be4cb70a6fade9129893cca2420
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:44 +0000

    choice contribute equal deaf fail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def find_max(numbers):
-        return max(numbers)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
