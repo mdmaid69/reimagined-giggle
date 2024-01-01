436638da92a2fc1c4a1b18ccf5cb396ec6550cb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:24:17 +0000

    myself there left colour ashamed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
