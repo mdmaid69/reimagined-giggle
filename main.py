3e849cfd71e38f537863e2e958f25248830eebc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:04:25 +0000

    comment figure spoil protest takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
