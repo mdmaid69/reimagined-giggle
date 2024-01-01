59eb99d62d121965f11126c9dacfe6f8c4527047
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:22 +0000

    order sort Ss but thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
