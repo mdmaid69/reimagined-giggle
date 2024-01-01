edd2247c1784bd6ca906af5a3fc6246bd135a1bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:32 +0000

    before market ground pressure crash

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
