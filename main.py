d4201fe3d6d518ad29c6820fc2e757ff6a36a67b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:55 +0000

    survive straight punishment eye size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
