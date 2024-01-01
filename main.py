ad7975d416dd81e23be7f6fe0caaf0de22db43f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:28 +0000

    achievement miss produce its takeback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
