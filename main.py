55bda24dc5fe470671ba5290c30ee0bab85a8f06
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:17 +0000

    finish piece lend gate trust

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
