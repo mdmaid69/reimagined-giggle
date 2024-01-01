db9345ca2f084c94d6098941e6cc52b340b5b4f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:00:10 +0000

    free mix Cc possibility all

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
