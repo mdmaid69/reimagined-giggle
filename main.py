f821444e5e91fc7490ad36a8d782373ae0172ac1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:24:26 +0000

    cruelty big sink class roof

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def cube_number(x):
+        return x**3
+def find_max(numbers):
+        return max(numbers)
