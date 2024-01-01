7b2f6fd637a5eee8fe49d885ccfccf9f1e703051
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:05:10 +0000

    blame dare sound coffee outside

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
