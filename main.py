1655f6375886434923827c9c7e4ba16bfdbf562d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:46 +0000

    street raise sit responsibility change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
