77499ebafb58ab2c6f5c0f7efbbd9330460e6dc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:29:54 +0000

    rain friend pipe naturally command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
