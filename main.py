2e8adc652ee4d8616e98713f8a9eb90d2539c916
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:35 +0000

    one tight block dig scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
