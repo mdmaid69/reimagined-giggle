0e451a83201f1a72b9eae222f3ae3832b06e0594
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:20 +0000

    leave pipe smile scrap short

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
