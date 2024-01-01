43cb33e58d6bbb6606ccd152a46b45ff0ec77c76
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:57 +0000

    error throw telephone comein contain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+print(datetime.datetime.now())
