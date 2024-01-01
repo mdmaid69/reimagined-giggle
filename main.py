b88589cb1acfef75082fe06bf04b1ce3e8c46e7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:56:39 +0000

    range difficulty forward yours dark

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
