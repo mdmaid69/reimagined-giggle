e3155e7669daffa9eabcfd2fa19476f5e4210573
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:03 +0000

    main pour government spoil wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
