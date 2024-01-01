3763abc2d80a1b2b5db3de651d287330ec9ea5b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:38 +0000

    cool cheat science slice deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
