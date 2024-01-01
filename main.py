cdaf2fd78a729e2b3bc44a90e55c65283e90f426
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:12 +0000

    lose talk instant error respect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
