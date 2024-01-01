49c5b419e904a2e15f58fb85d6ba7bce796eeb10
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:28 +0000

    Hh offer sure backwards play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
