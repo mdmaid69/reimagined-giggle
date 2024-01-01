3967bcf236db55da66add352275df9f65b4fa1cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:05:45 +0000

    this comeup just touch pass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
