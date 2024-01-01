e9ac0aaec7c7f3e42e69771cccf056cb67d91d4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:58:14 +0000

    black invite support phone cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def clear_array(array):
+        array *= 0
