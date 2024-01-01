b01bc21a42e6d08600ae8304bf50691dcba8e9b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:57 +0000

    us cry giveback bush classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
