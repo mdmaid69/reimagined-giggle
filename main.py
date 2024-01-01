3ee8ebb2a08f43dfcbdcb7f18374c42cbe00c11f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:33:15 +0000

    respect winter phone be rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import json
+print(json.dumps({"name": "John", "age": 30}))
