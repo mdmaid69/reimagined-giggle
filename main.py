e6feaf37aa73604c8576fd9780777ed1cd59e4ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:19 +0000

    probably tired quickly spill also

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-for i in range(10): print(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
