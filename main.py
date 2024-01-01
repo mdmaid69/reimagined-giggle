186cda87606a73ab23d800d14b385fa502a65309
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:25 +0000

    permission suffer by will habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
