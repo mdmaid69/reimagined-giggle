1b0c93342a186e484da790d949d864876bdad2d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:23 +0000

    back eye form temperature bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
