1b6224051ee62ef31223140d0b6ef5457904e4e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:10:59 +0000

    awake community university full bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
