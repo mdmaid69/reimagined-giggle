c2cabb54425fd54004a562db2cb8c882eb4c47f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:21:58 +0000

    frequent comeround could glass miss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def reverse_list(lst):
+        return lst[::-1]
