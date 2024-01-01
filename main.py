7ffb661f4769a8de3763c410dd8970e66cbc7ff5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:31:44 +0000

    anything apologizeapologise off error sudden

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
