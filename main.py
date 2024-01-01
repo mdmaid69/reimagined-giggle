44df707d4841849bd1057433235fc880195c007d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:22:26 +0000

    substance in kind imagination future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
