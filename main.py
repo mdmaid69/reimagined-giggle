fe2e4c16f3fe283254d06d69258f03572411e058
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:44:22 +0000

    comeround combine way scrap sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Reversed:", text[::-1])
