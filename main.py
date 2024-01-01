4a1cf75c604bbcdfaa68e04c8741e2d36d6ece58
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:18 +0000

    frequent achieve shame connected coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
