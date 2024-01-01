ea5eb21e807c137bc02b660fd60723f747af704e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:09 +0000

    one similar when present getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
