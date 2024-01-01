2a4106ba530747f134ad200a9f261b012008640c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:47 +0000

    evening organization very risk other

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def find_max(lst):
+        return max(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
