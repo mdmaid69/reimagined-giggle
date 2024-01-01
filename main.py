c9881911bb7870436a2ee50f706f3410c5ba0198
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:02:48 +0000

    refer someone towards suggestion plain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
