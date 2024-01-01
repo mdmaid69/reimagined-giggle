fa9218474039215f9014beb9c935dcbd494175f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:11 +0000

    interruption happy neat breakup distant

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
