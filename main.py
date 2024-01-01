9ea736bde2687cb578c8b5ef5300ecabd8f3d733
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:32 +0000

    care pick luck little improve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
