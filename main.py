a6800d968d7aa410da26db09db61e3df4050f051
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:27 +0000

    classroom immediate difficulty himself name

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+print(sum(range(10)))
