da5259c71d8a3223f3d6a30c28413d6e28f03a0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:03 +0000

    throughout well nice awake allow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+print(math.pi)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
