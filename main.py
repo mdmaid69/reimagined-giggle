ce08299fee26c1fbb52624fa4e295ade765c0046
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:34 +0000

    finger somehow intend secret calmly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
