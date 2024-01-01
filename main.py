2e0ddef2e0bb075cacf6bdc677b56544e8fa2c6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:04:58 +0000

    talk minor another if pretend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
