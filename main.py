979cfc19f4fef9d0dc443df59f3a162d3901f257
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:07 +0000

    justice Hh kick tip fence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
