45fb45ba214377c667ad349eeff88cc1b8daa718
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:03 +0000

    minor combination hear straight makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
