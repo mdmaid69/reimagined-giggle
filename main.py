a19342db1002dec7c2c5d839aae369e2a3999baa
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:59:25 +0000

    properly Hh plant lunch care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
