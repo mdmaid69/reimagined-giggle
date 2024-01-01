57450f349c90a93817d044234100802cd5ecf81e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:50:45 +0000

    strike centre attract care accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
