4f60bffee87a124c4bbd59ccb961da26f5168579
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:48 +0000

    mail tip valuable picture slide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
