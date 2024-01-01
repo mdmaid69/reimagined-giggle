7743d4f8ec7985764690d9d73bc9ee3dfb3e8d87
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:38:01 +0000

    morning recognize mean ready pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
