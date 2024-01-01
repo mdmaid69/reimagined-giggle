1505482d9840c18cf9d4ece7bdbf10e5564dddb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:01 +0000

    no machine old maximum convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_acceleration(speed, time):
-        return speed / time
+import collections
+def create_user_string():
+        return collections.UserString()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
