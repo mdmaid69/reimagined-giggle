85276e1e325d3a6e323e9a4a572e62e75b2ad3a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:59 +0000

    start difficult have bottom brother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def is_odd(n):
-        return n % 2 != 0
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
