d099a3c4f11acab211c4c4b3ad550841fde8c1a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:36:36 +0000

    search appeal lie tip fly

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
