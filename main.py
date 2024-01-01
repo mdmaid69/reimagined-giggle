8a4fd04c4da7d85aa76847b34ccc670ea1efdd2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:18 +0000

    waste sick cause rely belief

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
