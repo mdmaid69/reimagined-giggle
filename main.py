b86fde05bb78faa46b1ebf61e3ff65a4911f963c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:38 +0000

    authority accept bang false alone

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
