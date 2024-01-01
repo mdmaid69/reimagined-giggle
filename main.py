2fe348ed1702703c47046e8dd8842fd74afb1ceb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:49 +0000

    mass start mess runout importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
