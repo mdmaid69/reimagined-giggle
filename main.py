6802b7c1962c767d68799e1513a79a4fa1a59b39
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:52:38 +0000

    objection rise extraordinary confusing cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
