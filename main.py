4424390dded35cb7559ecd72b0ba18ec6e2f0271
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:39 +0000

    speak expect probably but public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
