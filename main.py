9f765b99333f81123b5a972463cdca63734fd5b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:37 +0000

    affect same authority protect comeoff

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
