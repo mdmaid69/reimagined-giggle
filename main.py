80ce7ea4b3c5264ab47ff03fabb7f88ca3effa2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:10 +0000

    young complicated passenger steer last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def reverse_string(s):
+        return s[::-1]
