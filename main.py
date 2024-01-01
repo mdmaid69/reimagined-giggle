81511a9b71f7b8707b7e03de7283de2de19152fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:07 +0000

    wild scale equipment sticky callout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
