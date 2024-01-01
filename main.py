4370c5ad869e1d9ee0440205def4e52df16808ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:34 +0000

    speech comeback show woman flight

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
