8814ff7c9f4174e524c0dbbbd3550925c0857e87
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:13:36 +0000

    position system lay judge bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def reverse_array(array):
-        array.reverse()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
