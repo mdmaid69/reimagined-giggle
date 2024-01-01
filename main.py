154a9d4e07020ce9d5364ca1edac0a78dc314885
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:56 +0000

    care willing bite load father

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
