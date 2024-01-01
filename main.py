e2461a05694edbab547c9f0176af8a5ac74d2676
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:19:28 +0000

    estimate desperate crowded proud second

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
