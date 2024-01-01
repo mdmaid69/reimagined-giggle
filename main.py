a2cdeb00e6edeac790c4855e720283261a8bd0cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:10:39 +0000

    seat smell improve illness music

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
