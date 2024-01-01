71834617f13adbe7be7f5a6f431c35aab5e045bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:18 +0000

    this exhibition statement plenty actually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
