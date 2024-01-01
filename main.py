880db26476d5a37d05db134a8b32f3fa1dd585d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:20 +0000

    foot earth real emphasis light

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
