f4548f92499b6be23c5fb90de03b971e2fd02360
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:54 +0000

    tight off unpleasant pronounce approve

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
