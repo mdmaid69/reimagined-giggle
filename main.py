67611538b97c13a0cfb203d34c3eed15281f71d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:48:45 +0000

    snow offer Rr possible dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
