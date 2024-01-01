61b4932e6dc7870366b5033718c42f254cf10d2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:37:53 +0000

    unable reduce gap culture mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
