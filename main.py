32feedf6d0c2cae406e7a436b0b512d6d26bef2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:06 +0000

    people condition powerful man land

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
