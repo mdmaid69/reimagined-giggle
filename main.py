2de4774c050eb60005b08c978e6029dadb6e5a67
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:26:27 +0000

    note happen come delicate sight

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-def calculate_pressure(force, area):
-        return force / area
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
