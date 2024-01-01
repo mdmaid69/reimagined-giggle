377d5cf9f0681765174a5b0f8bd7ca3991a4854e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:31 +0000

    me once feature early load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
