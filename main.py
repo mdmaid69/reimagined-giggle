aa098b863db05c9b6264cabad5ab061abf074587
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:00 +0000

    responsibility Hh town round giveout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_work(force, distance):
-        return force * distance
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
