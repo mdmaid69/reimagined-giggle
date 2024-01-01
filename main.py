8061fb82bd5d36c9b357eab706d22744aca4cc19
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:23:34 +0000

    shopping holdup whether deliberate hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(lst):
-        return min(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
