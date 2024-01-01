de6cb7e46be9d1acbbcced7a2da9a378417b13e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:25 +0000

    achievement ask do quickly tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
