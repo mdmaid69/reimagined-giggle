3b642a55e75f0aa6fe1cd225993bafeb8d77571f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:48:22 +0000

    modern scared tired Rr learn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
