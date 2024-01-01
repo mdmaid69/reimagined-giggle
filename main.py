e039bc1376240e49ef9df05295cc392ba637a68f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:10 +0000

    train body plenty open mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_power(work, time):
+        return work / time
