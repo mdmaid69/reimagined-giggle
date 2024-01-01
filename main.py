061f40f2aa58e9e535fb5a7a8ceacbac9bfff4eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:55 +0000

    note famous equipment declare wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
