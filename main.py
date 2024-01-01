dd46e2d45dc1809bf5130f82a4c05e956f1214f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:40 +0000

    live announce experience usual time

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
