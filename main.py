eb02c6ae350a9a7c514176b1eaf308064e880d4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:51 +0000

    sport judge depend sensitive anxious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
