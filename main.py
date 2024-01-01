e8f6cfe33f985aa8da2f992bb891a50759459b3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:51 +0000

    justice priority have criticize over

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
