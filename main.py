44cf883bb5dd4e440c0c28b14568d80839bd7e4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:16:49 +0000

    on emotion bill ability path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
