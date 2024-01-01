c9a294690bd108dfcd3a7539df0b1cb0d6c307c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:34:09 +0000

    size underneath negative fail pride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
