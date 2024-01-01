bfb213fdef513ebdbfee390df42501abdcfb233b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:47:41 +0000

    undo realize cry smart strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
