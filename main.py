482b8069d2cfcc6b152e09feca425575f0b4ab5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:53 +0000

    getup separate holiday Ff hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
