5a1437c84cec908199ceb2848c878a8235bfed66
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:39 +0000

    stay involve fair exactly mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def get_array_as_set(array):
+        return set(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
