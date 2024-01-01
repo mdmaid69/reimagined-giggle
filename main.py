60787960e7dd8b7601a22df3d7e9b87e1efcb706
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:06:34 +0000

    instrument string that shut build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
