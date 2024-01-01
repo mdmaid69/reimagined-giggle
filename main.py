f405aa9050951e5d94d752df32bf9a2254da99e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:58 +0000

    thank arrive weigh separate burn

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
