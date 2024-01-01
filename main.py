e617d9bb1f715b9cefe849116075ba42292b4742
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:44 +0000

    forget recovery shut depend rain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
