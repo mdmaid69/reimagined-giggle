109bfc5835774437c327b6ea3201c34b658f4a78
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:33 +0000

    pronounce fear previous load cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_average(lst):
+        return sum(lst) / len(lst)
