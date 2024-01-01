dca1b8e98b1ef627f67eeca3708cee7dc0ca26c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:01 +0000

    bringdown recommend plate discussion back

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
