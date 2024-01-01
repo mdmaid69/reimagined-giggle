2c6d3e8bd1a6554150b2f68fb5251fee38baa35f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:16 +0000

    considering father path clever reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
