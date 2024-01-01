f4ee4692648dd0a71c33aba46a3ee365d54e649f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:55:04 +0000

    emphasize pencil row development sort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+from collections import Counter
+print(Counter("hello world"))
