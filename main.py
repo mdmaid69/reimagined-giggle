9e1884ff805e14baab87012ed8e09e3c5940c398
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:36 +0000

    impossible or takeout form tragedy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
