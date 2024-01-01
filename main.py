372099b5a5db9ff09ddd5a9c78081c5859069a84
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:31:18 +0000

    strange carry boil prize that

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
