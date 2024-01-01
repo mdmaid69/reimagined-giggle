0d8a8cdb72b41f49e868457f94dbf12273daae34
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:34 +0000

    downstairs drink reason absolutely nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
