ac9f62b1bf1405be486f18b27476cb8ab373ec1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:09 +0000

    how normal ignore collection exist

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
