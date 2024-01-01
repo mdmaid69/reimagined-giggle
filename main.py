d38f2be7551a9acfc8c2d35fefde94889679992e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:56 +0000

    step general practise compete off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
 import array
 def convert_array_to_bytes(array):
         return array.tobytes()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
