6ac80a851424c8e765bb6264f2bb94e6aec67d78
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:50 +0000

    buy song not life bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
