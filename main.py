75a74cc07730e548608aede244025820331b65bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:20:57 +0000

    church their suffer thanks geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import re
+print(re.match("h.*o", "hello world"))
