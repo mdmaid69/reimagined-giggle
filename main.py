7cf75e2f7eb45813d017c234aed84482a63f99ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:17 +0000

    goback(to) suit smile switch yesterday

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
