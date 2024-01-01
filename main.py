e7d7cb7c9b6f84760846cf8ed6ac8d0f05c0f79a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:09 +0000

    worst either schedule basically used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
