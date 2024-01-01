aac46d49e2999cc44e1d6ea318945adfd3d95156
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:07 +0000

    fire tip add sticky disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
