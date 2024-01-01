280f116e3e15d6b13a371e3085f48450eec89a8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:16 +0000

    heavy contact can finish again

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
