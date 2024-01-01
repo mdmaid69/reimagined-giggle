5b403bba299fdcde4c0074c91d7f5c004b50f692
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:16:59 +0000

    kill regret situation release careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
