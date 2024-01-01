ade979b717488436846e25da5b75226fccb1504e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:31:28 +0000

    maintain wear uniform neat warm

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
