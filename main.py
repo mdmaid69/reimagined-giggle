cd1e884dcefc19270ec8fd37836336a0ee4155fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:50 +0000

    against shadow flight steer either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
