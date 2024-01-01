81e5da9858ef7814109d74c36fffe5ee0fb18d5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:54:14 +0000

    visit lesson strange interrupt consult

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  def subtract_numbers(x, y):
-        return x - y
+def iterate_over_array(array):
+        for item in array:
+        print(item)
