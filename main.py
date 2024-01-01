594da6f8b9d45e86ed869fcd95d795e9e8c7b822
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:21 +0000

    judge language refuse bath promise

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import time
-def get_current_time():
-        return time.ctime()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
