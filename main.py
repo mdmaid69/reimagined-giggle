a9a5d34532ae8bb0f63922f388a7723c03e24b79
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:32 +0000

    flat actual damage sometimes turnover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
