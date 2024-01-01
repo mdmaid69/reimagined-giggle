ad49575661e50a700d134468fb6422b7baa08e35
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:20:34 +0000

    surprising stay imagine compare sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
