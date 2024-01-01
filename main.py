6fcd56c3845259098d25d90646e93b3225383b4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:53 +0000

    I inquire stiff nature suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def add_numbers(x, y):
+        return x + y
