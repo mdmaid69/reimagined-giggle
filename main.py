9443f159e2cfbf9e39c3513559bab949d92805da
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:01:52 +0000

    figure prefer stop choose trial

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import platform
+def get_os_info():
+        return platform.uname()
