2cd8d410dec2a1b94519c7be46baf4a7daa24691
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:21:13 +0000

    level limit ground beneath soak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
