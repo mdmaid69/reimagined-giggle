a63e1273add2a7415fa2d64cfce5bed0f340a051
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:37:32 +0000

    string lookaround without uncertain majority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
