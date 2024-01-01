3cec281f73132cd19e5e762da72daa2308308339
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:26 +0000

    cold back once belief 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def add_numbers(x, y):
+        return x + y
