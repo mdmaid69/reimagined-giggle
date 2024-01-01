216dbc1d91cb30ca68a16b15cd966405f6570a5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:01:00 +0000

    shall stone fly meaning flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
