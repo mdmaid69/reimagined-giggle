6c13560def7f77576ba5355779953440f3657fe7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:43 +0000

    wish separate never satisfied shout

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+print("Hello, world!")
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
