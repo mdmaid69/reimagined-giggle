8eb07052477cb74a15f0bf1a7dcfa6ec514737cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:41 +0000

    previous once hide emphasis spread

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def multiply_numbers(x, y):
-        return x * y
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
