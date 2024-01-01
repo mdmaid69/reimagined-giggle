ac590a0963815ad2e7c4d1accfbf68687e2016f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:43 +0000

    wide ought thinkof broken hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
