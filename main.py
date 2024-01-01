fa232996c994fadb4d2cb06e24f72cf4c82f0d16
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:20:12 +0000

    oil brain type behave bury

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def count_elements(lst):
-        return len(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
