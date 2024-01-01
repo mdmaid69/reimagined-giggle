4332190bc4d858202662e04af8e04bbd06e50cc8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:55 +0000

    day tool talkinto sight ability

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
