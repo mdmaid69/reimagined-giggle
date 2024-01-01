d80deffa8e30275a181605dd42e076d3c3a5ead8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:01 +0000

    final failure young share react

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def count_characters(sentence):
+        return len(sentence)
