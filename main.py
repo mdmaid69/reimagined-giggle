bbc386b52ff9f6786702815b6f8714fdc42f3d60
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:56:37 +0000

    pool no drive busy patience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_acceleration(speed, time):
+        return speed / time
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
