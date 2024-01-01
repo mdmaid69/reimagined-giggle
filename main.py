95fb6c3a979bb9fb52530e243ddbe686b8d025b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:02 +0000

    intelligent home single lookback(on) follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
