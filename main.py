240bc400ec5ce60ce65e7980d3c447434bb8cff8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:05 +0000

    that human breakup little only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_bool(array):
+        return bool(array)
