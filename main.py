d4ebe0081f80663e5812dc38840a6fc202011328
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:32 +0000

    leg arrive unit spot lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_directory_name(path):
+        return os.path.dirname(path)
