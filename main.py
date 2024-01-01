2fc84b39fdf66bbe33714920e4b0f21478d0d209
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:06 +0000

    opportunity culture many switch priority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
