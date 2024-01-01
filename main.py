260016745c983aafbed0effc6b4d00e49ee14006
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:49:53 +0000

    before relationship introduce activity platform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
