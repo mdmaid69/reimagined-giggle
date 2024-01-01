4a9000cc86aea7b6d14ba59e49fa5177a4451893
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:07:05 +0000

    spot reserve path knock film

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def is_odd(n):
+        return n % 2 != 0
