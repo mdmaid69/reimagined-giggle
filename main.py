edee8f0a5e8bf2e92d622ec7c4af1dca7452eb04
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:46 +0000

    again project another short sight

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
