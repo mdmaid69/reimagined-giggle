54b0fd4bc23372bccd9d8f3dcbdae853faeb5e78
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:15:05 +0000

    tread basic noise brother provide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
