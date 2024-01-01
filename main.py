c6882be5775ea2a87bee06ea56c987aa08df045a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:09:58 +0000

    tone butter young responsible spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
