72699e25462ad5bb3e39d651bafe9ebfd49f23d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:41 +0000

    introduction 179bite own narrow count

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_as_list(array):
-        return list(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_current_directory():
+        return os.getcwd()
