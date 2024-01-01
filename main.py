13fb67ac70012c4d30df424ba836b457044b7608
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:56:11 +0000

    demand invitation bend clear speak

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_priority_queue():
+        return collections.deque()
