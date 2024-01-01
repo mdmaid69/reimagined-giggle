5118860a48064d985fd083d97a607ee200c95e1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:58 +0000

    runinto sentence communication appeal on

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print("Hello, world!")
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
