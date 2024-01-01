15b302a036953aa5511f8330f437eee790a5332e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:29:50 +0000

    myself produce rich top value

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
 def calculate_hyperbolic_arc_cosine(x):
         return math.acosh(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
