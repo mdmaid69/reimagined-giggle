d4774e2226698112a7c17cc24a93d241ce3960d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:54 +0000

    needle well towards operation protest

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-print(re.match("h.*o", "hello world"))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
