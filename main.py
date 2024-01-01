506e856c153c3ee9b5c11a00f03a2315ec2dc5fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:22:01 +0000

    watch blow possibility rise Pp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
