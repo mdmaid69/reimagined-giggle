35e86a53172347a7ffc8dc7ec357995f34437eb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:39 +0000

    shock hair impression serve manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import json
-def convert_to_json(data):
-        return json.dumps(data)
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
