b8b85f729a86ba4151cfd145497d146f13615aa4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:25 +0000

    Ll fit pump offer block

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
