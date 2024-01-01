11491c36deb85b864a30f4a3547cda34562e1720
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:30 +0000

    become drawing chance star artificial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
