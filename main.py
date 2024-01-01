1bbc47b4fe97503913791686f3f12e51babf809a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:00 +0000

    block spite cope history major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
