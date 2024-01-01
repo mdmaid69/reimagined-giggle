e75cfecacb4bbfed2d2f6c90c56d7ddac358f6c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:57:18 +0000

    start roof direct tap goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
