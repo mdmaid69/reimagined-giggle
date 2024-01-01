371c5e2e3522048b4088bf8013f2a0bdcaebe965
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:09 +0000

    tool standard introduction ear up

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
