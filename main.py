1de5a19e80bb7e3e9b12e9dde6488ad2bcedaaec
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:58 +0000

    suit record phone no violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+print(datetime.datetime.now())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
