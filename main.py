1ac1e445f077fa919a9b3e27cf896357a5b566bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:06 +0000

    part radio giveout spare declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
