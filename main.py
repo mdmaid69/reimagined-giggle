a43a9a72b7a4a05a6755c91e8aff726266a83826
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:28 +0000

    pin destroy pretend pause appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
