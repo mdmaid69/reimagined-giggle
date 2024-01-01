fdee3bd579a10d89401b871460a4d52e95fa71f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:49 +0000

    if preparation exit journey ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
