c6b183d35bfd7e98c0c0e1ab191fa42a6ba331e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:29 +0000

    morning hour post smell ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def is_odd(n):
+        return n % 2 != 0
