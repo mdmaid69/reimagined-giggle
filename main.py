1d3819a18fc927a51739052dfdc271bea31287f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:26:48 +0000

    touch hurt season yes clearly

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_speed(distance, time):
+        return distance / time
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
