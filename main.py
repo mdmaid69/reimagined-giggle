a923415ef64b830e14a4748a7ef026989fc4733f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:32 +0000

    past look service takeoff sleep

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
