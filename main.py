aa92d5de25561f8542fa0bcbcf2f2aec3f55434e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:04 +0000

    show afternoon transparent argue giveaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
