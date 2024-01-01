4cddfc2565326dd6eb372eeabe9cc2d66369ff81
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:28 +0000

    trousers drive die Nn famous

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def cube_number(x):
+        return x**3
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
