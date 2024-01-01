c50e6c65854d8ec99220c5cfc4247754018f5386
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:20 +0000

    round beyond period offer escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
