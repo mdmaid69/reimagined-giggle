a2b75a9d73f8e72d993f26d3ebb8e447060abe3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:34 +0000

    traffic duty distance shop breakfast

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
