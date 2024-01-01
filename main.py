078821c4a4fb7ba83b4d4f8f9f201f0f965749d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:43:03 +0000

    true common worry ilovedicks common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
