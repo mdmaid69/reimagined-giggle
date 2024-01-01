6779b50f330f8f8980ac0c79ffb7d039fb206952
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:10 +0000

    treatment over last programme stick

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def find_max(numbers):
+        return max(numbers)
+def calculate_area_circle(r):
+        return 3.14 * r**2
