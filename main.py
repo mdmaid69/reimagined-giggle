9f0ed34b36e395521a11b977b2764e4c0663391a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:21 +0000

    remove rubbish sick reason serve

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
