339dee7cf40b7269b01f6182d635f51a9904c668
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:36:01 +0000

    responsibility politics generally consist piece

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
