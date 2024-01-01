1ca2cfed22be9bfcd7dfbd669644bd50341c3204
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:55 +0000

    scrap bang otherwise keep description

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
