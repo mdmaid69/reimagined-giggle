5d8ff67a5892042eb2635f801e1f383b27af35b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:34 +0000

    getoff crash turndown accident welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
