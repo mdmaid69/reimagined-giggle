d8b15d36747c0d044c540c7cf94b1e2b17e19143
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:02:59 +0000

    platform afternoon consist wish rid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+def square_number(x):
+        return x**2
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
