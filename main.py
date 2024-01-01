483c0df88e581726d4d93f641ea1f5a3af228446
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:26:02 +0000

    actually ago pocket want share

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
