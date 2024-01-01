8a76ec98acf3760e3a8f628c4762d50b3a1b5e3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:47 +0000

    cross mark thorough exercise from

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
