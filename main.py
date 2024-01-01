a16addf420261abed0fa6ba2bdc2b79bb6302a4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:13 +0000

    method chin cool least downstairs

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
