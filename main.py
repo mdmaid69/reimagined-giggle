02e0b1e571d600ef9da2423ca621e0aea041e78b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:07 +0000

    little cry block disagree whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
