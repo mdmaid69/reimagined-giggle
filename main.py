f2756023faf697bba46a3f3821fc3ce7657f77ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:50:45 +0000

    confidence substance adjective stop ability

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
