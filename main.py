d3897db7712a47859cdc6048d7c3f55b86a85c4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:30:32 +0000

    out flow arm stare discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
