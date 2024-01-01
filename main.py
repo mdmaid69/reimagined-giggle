ae89e43fd29bfe675d392e8090d0fe67fbdc4c06
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:27:57 +0000

    effect to grade demand sea

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
