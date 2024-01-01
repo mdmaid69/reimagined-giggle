589a87bfebf0fc7b3b820dadf774672ab7d5291b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:42 +0000

    thorough opponent usually careful price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
