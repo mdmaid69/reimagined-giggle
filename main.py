8961ccc463a3f132ede3e69f4444a2f78dba94c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:07:05 +0000

    suffer scene during previous building

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
