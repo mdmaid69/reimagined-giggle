56afa9bd3aed0cb53e8e814286a9f39be049008d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:04:26 +0000

    suffer considering measure pride either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
