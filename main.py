6e5108544c01e0e54a0b3816e6a7f0d757187472
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:41 +0000

    interested relation phone fork warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
