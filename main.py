1ae526e9239d4ae84966606254cd7778b66cdc43
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:59:37 +0000

    best black innocent takeaway print

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def add_numbers(a, b):
-        return a + b
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
