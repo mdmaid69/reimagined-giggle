935329cbfcced8100c9c8027577f38cd5487fbc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:04 +0000

    practice priority join arrange breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
