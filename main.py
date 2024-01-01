8655c34dcb5e373418210d7bcc0934b01bccced6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:07:31 +0000

    that hard open powerful fit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import sys
-print(sys.version)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
