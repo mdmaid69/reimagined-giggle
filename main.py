b22120d5ac6b5055541de8d0db3333c0b210c0ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:40 +0000

    little whistle trouble during paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import math
+print(math.pi)
