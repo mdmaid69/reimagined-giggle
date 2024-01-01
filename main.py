8c895b4d7efe2320ffb6a6f7475f0dbada43de31
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:55 +0000

    story embarrassed sick across support

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
 import sys
-print(sys.version)
+def exit_program():
+        sys.exit()
