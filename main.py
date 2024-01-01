cb9a4d72477c887505716370542016f218af50e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:10 +0000

    purpose level beat surprise weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import array
+def get_list_from_array(array):
+        return array.tolist()
