6443feb2a835c2f29e6882a4944ddcc4c4c0e317
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:32:51 +0000

    spoil Uu print you than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
