20a522b0c93c231e79ae50ef6f9fa405b68c9bef
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:24 +0000

    total sink gap handle pencil

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+text = "Hello, world!"
+print("Characters:", len(text))
