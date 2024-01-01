1342bf918d1c5019156456f812775ad8fca700da
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:33 +0000

    sore age give reaction damage

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
