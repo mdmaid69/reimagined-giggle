e1bd735921835d7f3846f815fc652fb257724082
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:38 +0000

    wipe nonsense laugh defend refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_array_item(array, i):
-        return array[i]
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
