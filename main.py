a72c11bd54b9b8c788b187be9087325ea89b13fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:39 +0000

    music in factory beyond noone

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def get_array_index(array, item):
+        return array.index(item)
