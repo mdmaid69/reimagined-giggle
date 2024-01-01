8c8cd548e21e3fc6fa87dcda9c58bf13c38f82c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:55 +0000

    generally history smile sting far

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
