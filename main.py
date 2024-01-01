dda46f69bc3cecc2038fc4ded993af72fc996a64
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:21:35 +0000

    street modern vote fill upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
