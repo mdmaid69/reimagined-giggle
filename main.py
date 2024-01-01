023b682b477eab2b290054af77c7a27ad89ec271
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:33 +0000

    bar vision maximum allow open

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import platform
-def get_python_version():
-        return platform.python_version()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def convert_to_binary(n):
+        return bin(n)
