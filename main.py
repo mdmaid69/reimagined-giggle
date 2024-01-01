87b220a55d78ef03e72277f5c15aa89885a707c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:09 +0000

    brief undo identify pronunciation anybody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
