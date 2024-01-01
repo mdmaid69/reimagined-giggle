0f12e294392eed94ff6a16ae8e586c5116ef4d2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:59:37 +0000

    emphasize fortunately this actual different

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_list(array):
+        return list(array)
