94bbcec8f4967aeba9fc691914a301d8b1ab0b38
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:38 +0000

    better sort recover home put

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
