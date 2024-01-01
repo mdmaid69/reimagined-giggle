22b9495d52f9601c639057acd1989f37df96d8e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:22 +0000

    experience on hope crime tune

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+for i in range(10): print(i)
+def find_unique_words(sentence):
+        return set(sentence.split())
