251055b7b7b6deb183b7b9b001e49354ea8e585d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:21 +0000

    hardly odd brother respect holdon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
