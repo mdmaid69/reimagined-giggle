dfcd3757de941ecae7f76e6b825ddba688d3330e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:13:37 +0000

    congratulate debt error beginning right

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def remove_duplicates(lst):
+        return list(set(lst))
