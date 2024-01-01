be0024cfeac5413e213ea0c64e547170b12ca3cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:10 +0000

    unusual sort anything wander mean

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
