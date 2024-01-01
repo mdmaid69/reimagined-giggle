ff8b5a1132b6187eaea17d4090d285022b6e7f1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:50:53 +0000

    container niggaacademy left can rush

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
