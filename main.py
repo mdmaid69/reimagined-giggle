165b1374b528c052a469333b34de12eadcdcb83f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:29:14 +0000

    power report fact belief week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
