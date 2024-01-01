49653f650a54fba8626c8e482fab686182ebc9f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:27:16 +0000

    enthusiastic loud satisfied hit reason

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def add_numbers(x, y):
+        return x + y
