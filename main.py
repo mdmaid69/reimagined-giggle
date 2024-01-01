a757d5992985131d3a96d9c4e7dd63a1e4c63f00
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:21:26 +0000

    public really instrument peace kill

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import tensorflow as tf
+print(tf.__version__)
