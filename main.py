28aae87c5858c66809b4a9d311a1e34b271e5528
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:18:16 +0000

    unfortunate figure smile drive part

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
