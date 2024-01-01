7f17143224e95d8725005f7505aa069f04cbcf0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:44 +0000

    train better workout round pause

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
