b026f575d8fd94a8af85c6f2854d293f4c9c46f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:42:18 +0000

    to star upset reply upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_error_function(x):
+        return math.erf(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
