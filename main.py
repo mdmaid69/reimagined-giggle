c9b6321b9105eed9e95a7c4a7c6e698d2d4977b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:49 +0000

    compare comefrom dry demand perhaps

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+print(sum(range(10)))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
