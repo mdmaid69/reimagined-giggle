e992068447bfe122f598f49394cfdbe64f63e840
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:57:16 +0000

    comeout careless price obtain point

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
