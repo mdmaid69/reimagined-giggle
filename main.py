d6035e7cc53c480b6e1f12ee727c40c18f907635
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:37 +0000

    pile to fat arrange escape

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-for i in range(5):
-        print(i)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
