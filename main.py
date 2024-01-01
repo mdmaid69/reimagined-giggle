78e0ca964ebb08ea19c60b95cebe0d6949ad529a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:12 +0000

    impressive unless accommodation distant exact

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import datetime
+print(datetime.datetime.now())
