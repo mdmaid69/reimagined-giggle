ffd793b5ca7216f9bbb86a17cba630c28c60d4e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:17:59 +0000

    closely house operate card criticize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import numpy as np
+print(np.array([1, 2, 3]))
