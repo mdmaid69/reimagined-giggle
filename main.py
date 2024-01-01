7db76a65692db28b2296a601eea6f5ed71009cf6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:29 +0000

    smart close must body if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
