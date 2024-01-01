aad8e94c7936f4bc465424bb510145f24812262b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:24:58 +0000

    only label show lower wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
