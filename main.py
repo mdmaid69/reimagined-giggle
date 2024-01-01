c093b52bd5541cd88f87c838734f4b0a5cb2bcb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:56:33 +0000

    other comeover will money excited

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_area_circle(r):
+        return 3.14 * r**2
