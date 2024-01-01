2d90bbc0711ca7f8d1b8378752d6137b5f1f0eaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:00 +0000

    skirt smile movement right anyone

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
