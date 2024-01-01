3a3a5e13608a213fb9573c89d81fb555ce3531ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:41:51 +0000

    hold loss use experience brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sys
+print(sys.version)
+import sys
+def print_python_version():
+        return sys.version
