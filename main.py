9409a7452250c071e7bb62bd62e5c1f945e97b77
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:16:37 +0000

    level chat glad comment cheek

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def calculate_average(lst):
+        return sum(lst) / len(lst)
