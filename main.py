d49f3017d8f5c798bfc254a2eafbbc78123cc5a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:05:56 +0000

    involve obtain turndown influence money

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
