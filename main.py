2c727ed3abbe117114ae5e30359a49a2e991d5a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:44 +0000

    slowly report high deep flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
