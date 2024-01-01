9a5c855d7c3ebc8142a4aab4e4c4ac38ff7f7494
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:06:55 +0000

    reason broken complete obvious around

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def count_characters(sentence):
+        return len(sentence)
+import os
+def remove_directory(path):
+        os.rmdir(path)
