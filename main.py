f8b99860b3e3c4a6f41321f4adcc0172c2931563
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:34 +0000

    keepoff meal free against process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_power(work, time):
+        return work / time
