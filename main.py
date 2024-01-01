5d9722b283bd2a4887b3db1abc9f7b199e708f2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:39 +0000

    age 568energy anybody night stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
