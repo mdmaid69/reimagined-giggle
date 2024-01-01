773f8875a4e6983bf4151dbbbebe23f8dd9122ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:42 +0000

    space difference clue hers almost

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-import getpass
-def get_username():
-        return getpass.getuser()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_power(work, time):
+        return work / time
