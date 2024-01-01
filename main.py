40066ed783af0aae5c4bea90d0d476152316f942
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:04:00 +0000

    probably wash Ll year general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-print(sys.version)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
