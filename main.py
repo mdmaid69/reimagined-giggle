faec1f187937b8bcefdb5a13da437f39718bab77
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:43 +0000

    shallow trust sleep treatment flood

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-print(math.pi)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
