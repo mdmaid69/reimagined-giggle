2b263e40ea3ead36eac06577a9215fb8990ef303
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:43:43 +0000

    wind meanwhile there blame left

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
