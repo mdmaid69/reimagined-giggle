3a32c8fe57ace2ce8ee4693607dbd8f34621f144
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:17:34 +0000

    personal run Mm grade air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
