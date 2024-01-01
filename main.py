0afeac2cc0dac12900106db3e58af0986f9c413a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:30:11 +0000

    precise experiment concerning touch race

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
