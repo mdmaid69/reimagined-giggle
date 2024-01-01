6ab21a5ba9234ac303e76fa98f93f7b6fc1cd911
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:09 +0000

    attract achievement history hear over

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
