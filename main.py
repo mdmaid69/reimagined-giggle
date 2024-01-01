d98bf99861179a062371494a6a4b33d395e0c9a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:48 +0000

    wipe snow difficulty eat progress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
