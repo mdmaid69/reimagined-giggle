8ce0ddc3db1649ede46711de55a33b6bbc415e1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:10 +0000

    shop why sweep Gg minimum

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_user_dict():
+        return collections.UserDict()
