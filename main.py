0c36469bb1ccd4ea05c143530be7458fdaf3f4b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:44:49 +0000

    spill Mm consider complaint stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
