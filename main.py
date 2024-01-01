2ca745345b1f700ac23e249f792a3bcf97e003f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:49 +0000

    relation damp delicate unlikely bus

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def sort_numbers(numbers):
+        return sorted(numbers)
