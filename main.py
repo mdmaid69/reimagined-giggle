e164ea3d445ebbe631a15c8745cba34428a1ab56
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:21:43 +0000

    getoff our spot shadow bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import collections
+def create_user_string():
+        return collections.UserString()
