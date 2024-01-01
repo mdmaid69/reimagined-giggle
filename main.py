f5462710ec053bfaa2ab1076767573d42ecbf8e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:24:01 +0000

    the interview feed early left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import collections
+def create_queue():
+        return collections.deque()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
