69f05b06363b1e239d486d858129287321ccc63c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:52 +0000

    for approve guess surprising underneath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
