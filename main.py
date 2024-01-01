b8ce7e6d5188376fd73b9d0060a31a80cf4d209c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:29:17 +0000

    total underneath ring judge repair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
