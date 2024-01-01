64579a6c697ef4ae5cca9e4a96e3853f685102ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:46 +0000

    lead already marry trial centre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def is_even(n):
+        return n % 2 == 0
