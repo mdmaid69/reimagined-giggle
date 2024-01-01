d51eb5994e8b96b8b231ae3fbe8df5e60975ee6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:33 +0000

    real to shape finally rise

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
