9d6cdd3efc28c7ec80b9c5fbbf29972f2bb3b801
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:10 +0000

    aware statement clue slowly prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
