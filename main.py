3cf815976e9f8dee128787813cd9778575b2f799
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:16 +0000

    bring getup contact pay neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
