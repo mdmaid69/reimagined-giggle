43a76fd12ad69fa91ff636b45f115632b611986f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:08:53 +0000

    damage along minor noisy comefrom

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
