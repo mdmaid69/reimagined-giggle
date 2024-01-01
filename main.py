f46176f971d1fc4bbf9c85cf4866efd7e23141f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:24 +0000

    complicated turn include meeting correction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_speed(distance, time):
+        return distance / time
