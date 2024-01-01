2f42d585d747de63d65f26a2cfdd58a44c4a660e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:09:00 +0000

    key perform recent Pp nowhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
