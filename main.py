df2ce9212cf5094e7ba6b1c308ae8ea4d4878fdb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:51 +0000

    message the gate surface new

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_even(n):
-        return n % 2 == 0
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
