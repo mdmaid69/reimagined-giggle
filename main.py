2f1ae2fffa9649e04d1d5fc66deb045fba7d2682
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:59 +0000

    stay wherever boring fight accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
