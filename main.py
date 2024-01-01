7e874dadeef4dd135fac1a8c23093d120c86e4c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:13 +0000

    shoulder base uncomfortable exchange bet

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
