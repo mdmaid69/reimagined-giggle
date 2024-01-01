8aa6809f2ca1829ca494fb510cdbf462a2b23b4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:45:20 +0000

    appeal ticket group time Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_owner(file_name):
+  def get_file_uid(file_name):
         return os.stat(file_name).st_uid
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
