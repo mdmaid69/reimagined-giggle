8086f1affa5d6fc3a0928902d82a81b8b3d0d5b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:54 +0000

    like fast radio than disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
