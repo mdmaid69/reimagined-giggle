e1a1bf9732d63205785ca9d00b7204a29bbb10c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:08:40 +0000

    unfair generally thanks successful office

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import time
-print(time.time())
+def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
