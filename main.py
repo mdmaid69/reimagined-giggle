dc44c9ba073182d1c0ab10dfe29ff9b190c846d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:50:59 +0000

    match marry Mm rid let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
