e4aebc82ae2423fd0276c91689d9c7186f5fd1ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:02:33 +0000

    drawing tread artificial hot increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import datetime
+print(datetime.datetime.now())
