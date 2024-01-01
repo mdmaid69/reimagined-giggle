92ac1cf5fdfb3a14ad3c2fba65ad8ebb1aea479a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:51 +0000

    regularly takeoff arrival rubbish get

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
