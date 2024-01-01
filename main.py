38f4ede12876b45d1c74d8d3a4ea806d9199a709
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:05:14 +0000

    protest somebody turninto university evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
