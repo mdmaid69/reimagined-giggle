39f0a0529ac76d667a7c338d210ef80d7b1cc634
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:36 +0000

    giveout mass bang nerve skin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import os
+def change_working_directory(path):
+        os.chdir(path)
