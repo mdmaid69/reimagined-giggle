a782fadf12eab4a378014b8a665ceeba8159694e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:26 +0000

    too rush choice muscle fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import time
-def get_current_time():
-        return time.ctime()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+print(time.time())
