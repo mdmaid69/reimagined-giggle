045bb6a8abfc83ac061427bd7d75d619313334ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:19 +0000

    sound its name of where

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import collections
+def create_queue():
+        return collections.deque()
