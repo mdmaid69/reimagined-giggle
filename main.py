7db2449b053b224d96df7627296ed26f6ed4e714
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:08 +0000

    book show term before chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def convert_to_octal(n):
+        return oct(n)
