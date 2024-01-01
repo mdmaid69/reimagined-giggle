e5836855d1ab4531bad01202d2db3fbbefcddd20
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:45:14 +0000

    bang bin mark challenge exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
