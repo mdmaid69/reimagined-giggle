178f9aa76972a12fb0562582a0c1722e4ee150e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:36:49 +0000

    joke field chase this bringdown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
