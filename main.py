92112ba313aca052c8d8d575b5c003520c2b2b26
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:56 +0000

    perhaps offend name height meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
