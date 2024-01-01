eb9d4f1f6e1ab2beb5eca96f54a6b96b32128b32
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:53 +0000

    together frightened calm maybe string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-print(os.getcwd())
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
