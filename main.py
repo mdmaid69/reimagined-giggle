9fa0b388de5186b00e7e81ab4865bd210a9dd115
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:40:07 +0000

    anywhere crowd although question forward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def convert_to_octal(n):
+        return oct(n)
