1e4c0d1b4fb4f7d234a670664ea211cc4e9a2b98
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:22 +0000

    she makeinto equal spring principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
