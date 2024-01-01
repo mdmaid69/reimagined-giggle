44ee3011f6c9a1d3ac299fa6e8c27cb2aacfcd4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:21 +0000

    meaning plan interfere neck them

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
