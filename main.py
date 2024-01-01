50c7e4ff40081612d565b17e9ec9b4df982dc72a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:29:25 +0000

    yes protest further thing tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_as_str(array):
-        return str(array)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
