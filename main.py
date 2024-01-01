829905f54cf1f3ed4b7d87d7d1774527478e1a22
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:51 +0000

    blind custom already call stare

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
