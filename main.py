c2e6ee7bd84f2355864b1e87482aa32b661678b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:26 +0000

    window Nn too dare mad

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_current_directory():
+        return os.getcwd()
