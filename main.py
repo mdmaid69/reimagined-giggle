7205c65bbe4f03b5f813b8768b3b030fcc5f2dff
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:18:46 +0000

    shut spite turninto principle hide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
