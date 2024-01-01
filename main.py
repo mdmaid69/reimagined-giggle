75379b42f40edbd6a039af146f699aeca0da4a2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:40 +0000

    match allow present period chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
