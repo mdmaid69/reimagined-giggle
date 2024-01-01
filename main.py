ea387de1f2dec484cc54f20b74c918cc12f8b97d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:25:47 +0000

    shallow sentence such standard Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
