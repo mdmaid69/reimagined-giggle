f688d6cd76f63fa14dc5ec2febda3727c986dbf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:31 +0000

    run state average spot before

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
