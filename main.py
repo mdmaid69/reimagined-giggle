8ac4303b56d6bca10f877e2d6746d38cb3f66728
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:20:08 +0000

    refuse though impressive accidental moon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
