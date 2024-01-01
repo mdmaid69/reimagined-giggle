fe6ffcd076e2e2bb6f1f74d87616c5e2de02479c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:33:50 +0000

    unit himself hit gas recently

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
