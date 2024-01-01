7a699ffbac4198b91977f9a1ca0d65277d570624
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:27:40 +0000

    arrival bottom share wire sight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def find_max(lst):
-        return max(lst)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
