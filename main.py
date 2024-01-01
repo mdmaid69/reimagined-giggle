6e12c40930695a9548746cbf8fdf0ac50e1258af
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:56 +0000

    thinkover pain cut aware play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-print(math.pi)
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
