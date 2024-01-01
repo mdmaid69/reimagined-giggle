f79051c34bae7c123fdfeda2a5432ca7132e8e83
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:55:56 +0000

    only several story sensible quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def square_number(x):
+        return x**2
