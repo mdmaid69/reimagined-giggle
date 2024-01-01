33f3026ca1165c71a289fb3e545ecb716781d383
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:12 +0000

    trade straight owe comfortable tread

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
