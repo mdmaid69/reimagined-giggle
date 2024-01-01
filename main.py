7ec79ad859d4f2435f18eaf0be1e1bfc56fec044
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:44 +0000

    support we sore its use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
