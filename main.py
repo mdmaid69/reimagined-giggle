b9f47ae8930cf87b4b8e6056367dab621cdea8b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:37 +0000

    squeeze every compare extremely important

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_typecode(array):
-        return array.typecode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
