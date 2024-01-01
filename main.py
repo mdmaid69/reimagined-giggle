b3642acf2ff45085cce26e01347eeaadf1043081
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:59 +0000

    small neither kind tomorrow shine

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
