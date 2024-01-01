b2ef5788923bac797e94d2cc275b9605fa2f0988
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:02:59 +0000

    post store winter blame right

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_even(n):
+        return n % 2 == 0
