10b4cf0499e75fc80190175a7bd44975ebcbf9a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:31 +0000

    strong difference knock could inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def print_python_version():
+        print(sys.version)
