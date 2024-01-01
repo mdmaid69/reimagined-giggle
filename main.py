8de3e03ffeac10cd4b179a6babb26e796215cbd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:09 +0000

    earth boring tool soon operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
