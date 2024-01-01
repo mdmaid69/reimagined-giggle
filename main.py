0222573474def99673818e9471f48a17293e12b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:51:55 +0000

    over advise hole scene anything

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+print(math.pi)
