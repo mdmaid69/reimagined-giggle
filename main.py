58ec6ffa2e6119b94c4ba638fe66dc00070e55b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:17:42 +0000

    brief head bag 572enter comeround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
