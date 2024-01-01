913a653e9d106862dd55c92cabbd59ce406cde2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:08:51 +0000

    against Zz attitude feeling delicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
