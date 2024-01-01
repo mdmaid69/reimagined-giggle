ab9f7060138a250eec3ead9c5efd7bd113672593
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:02 +0000

    respect major stamp certainly allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
