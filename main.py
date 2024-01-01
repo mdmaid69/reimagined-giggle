5951a529e32d5576545bebe5927918c4070bd943
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:09 +0000

    never somehow before consist gift

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
