93a66a3143a5c8619edf96a07bea484b262534a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:56:32 +0000

    back than direct runinto edge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
