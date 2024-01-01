28933adcf926195e3799f252231b75c4a7c4e0cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:12 +0000

    dry occur scheme around case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
