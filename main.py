cc8c4d49d24cac76f81d25ae59e1b03d0900b309
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:45:49 +0000

    glass dream note involve display

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
