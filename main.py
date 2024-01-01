a84b568fe3419b089443ca5ade6b338066436ecc
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:52:14 +0000

    store increase uncomfortable cross doubt

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_power(work, time):
-        return work / time
+def calculate_area_rectangle(l, w):
+        return l * w
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
