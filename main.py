13fc495b997b1221a159e066ac0bf1676b3a2310
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:26 +0000

    comfort aware he blame burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
