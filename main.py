46349ad47bfe8ad25de2a9769832467ac46d0e24
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:29:58 +0000

    person tour listenfor please sink

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def count_words(sentence):
+        return len(sentence.split())
