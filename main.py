e9742923b080d7121d56607d0488cb2916903a3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:20 +0000

    deaf tip appeal note now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
