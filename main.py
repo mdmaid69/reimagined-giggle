cbfb6d9d9687fb9909b3e17cfcbbcc94aeb3a736
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:37 +0000

    glass post feel fear compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
