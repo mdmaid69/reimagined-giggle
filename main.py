bfa0ffeaa76f0e1b3bc7a0851955855141f1c410
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:28 +0000

    something tune comeout OKorokay entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
