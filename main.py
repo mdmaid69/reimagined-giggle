79d6f64ac6772c2f3b4a9e6c85c0a88daad37249
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:40 +0000

    concentrate attitude cover regularly hang

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_int(array):
+        return int(array[0])
