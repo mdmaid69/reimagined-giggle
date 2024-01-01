2d40af18ac9d9ae59a581b364a5843009afccbb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:15:56 +0000

    hollow reply spare each foot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
