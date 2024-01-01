8674802bf240da518e16f8ddd0578863b5184df6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:29 +0000

    list takeout supply at share

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
