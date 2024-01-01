843b73fb721c2b92f7440d2eefb032c690929c7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:52:59 +0000

    statement wake suddenly sing occur

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
