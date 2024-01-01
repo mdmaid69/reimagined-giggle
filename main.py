227ab164fa48c83080a25e8527328fc6dd49b16a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:59 +0000

    criticism much keen meanwhile suit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
