01e0769db3bedbf2f793d69d99fea43672396408
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:10 +0000

    hard reliable wipe shut murder

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
