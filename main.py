0f27a016554f60b396cc85aa65b9181e8018ca11
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:52 +0000

    regional building food herself beneath

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
