6dc8bab4244e129b1a6d6bd760182745ac7accb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:59 +0000

    respect accidental mean imagine achieve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_acceleration(speed, time):
+        return speed / time
