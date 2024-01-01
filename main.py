6dc2a5909f2abbf9ed5404af33ec4c28c8264632
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:57 +0000

    worse deserve his exchange long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def divide_numbers(x, y):
-        return x / y
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
