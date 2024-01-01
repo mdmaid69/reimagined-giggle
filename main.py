4390142c0602ac1957397a793d210f61083e6558
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:10 +0000

    distance watch remember tight strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import re
-print(re.match("h.*o", "hello world"))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
