51a2e511d1566df46f35ffec0a1f69bbdd30b88e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:58:04 +0000

    handle talkback come afternoon wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def get_array_as_bytearray(array):
+        return bytearray(array)
