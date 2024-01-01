8eda0651e71c9dbf5759ecb5513aef8af2c4691a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:34:30 +0000

    can control powerful constantly service

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
