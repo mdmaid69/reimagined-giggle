85034bc4698f1296e025c5eda539834b77fd6c1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:55:53 +0000

    unlucky spend workupto damage recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
