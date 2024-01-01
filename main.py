65a8345dbd53046db7b6fe2e3f1213f9ed8e5f93
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:33:54 +0000

    work respect needle level similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_length(array):
+        return len(array)
