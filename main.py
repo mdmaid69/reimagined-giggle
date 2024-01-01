c0311ef4e94d7c5beac3285822aa33bbb4019b3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:15 +0000

    count base bye reason borrow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import math
-def calculate_tangent(x):
-        return math.tan(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def convert_to_octal(n):
+        return oct(n)
