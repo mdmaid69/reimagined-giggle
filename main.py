33edcdc6551750b6897018868c1128d23554d1c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:57 +0000

    bet port mend bath who

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
