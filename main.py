be73a0584b44782e387bf69315e4c12d6b601489
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:43 +0000

    straight thick scream interesting section

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
