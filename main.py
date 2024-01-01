f25b5ca2a2bd80e4b307db2ed9ab73ab74a30332
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:22:36 +0000

    buy familiar light advise surprised

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_rectangle(l, w):
+        return l * w
