40148edc80a82bed5bb2457d5a343a8cb36f0ca2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:34:15 +0000

    between book skin free university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def divide_numbers(x, y):
+        return x / y
