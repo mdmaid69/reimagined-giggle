c54f2534924a5734fbc58a66044676d98d5070ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:22 +0000

    remember anything getout rest there

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import time
+def get_time_since_epoch():
+        return time.time()
