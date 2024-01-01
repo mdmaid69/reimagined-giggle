d6bb48979be7ebd1e42622ce071a455d7ce9b15c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:01:40 +0000

    preparation Tt dark cry row

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
