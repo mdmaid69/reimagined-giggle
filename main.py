b0d898e16767d58e100825502e0197ac82ba8810
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:30:13 +0000

    only embarrassed means hair system

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_as_set(array):
+        return set(array)
