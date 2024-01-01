03ab62f2beb5f99b84a259f05d83fcd3f95e1a8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:24 +0000

    spray guard water past Gg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
