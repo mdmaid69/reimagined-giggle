b612a9db458736be1992c2a1e2c935dbd60befc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:07 +0000

    stare Zz develop floor basically

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
