22eaf0f31ea7cf2c04b7e3c3f298125e2d92d975
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:30:53 +0000

    error heat convenient waste score

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def multiply_numbers(x, y):
+        return x * y
+def divide_numbers(x, y):
+        return x / y
