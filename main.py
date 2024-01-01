60572cb903a7f932ca05f88045941aa515d4a608
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:58:21 +0000

    your wide mark fresh impressive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
