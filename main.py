98b9a1acec66bc20e3749a3dda09115fb182399f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:43:46 +0000

    bunch matter read available patient

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+print(os.getcwd())
