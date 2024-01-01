85c1ce71ba458b7df5789aa0fa22cb5fbbc70ed7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:07 +0000

    aware conclusion light call pen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
