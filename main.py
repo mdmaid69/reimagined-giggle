312f3ff0c6d5139203318820ffdff3bb2c17bf77
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:09 +0000

    wide protect naturally card guilty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
