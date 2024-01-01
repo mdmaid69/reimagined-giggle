7a09f416d669c08903df5d5bbc0f7749d33523f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:22 +0000

    shallow point vision weekend violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
