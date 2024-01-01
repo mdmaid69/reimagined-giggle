efc96f1e80faba6a9d069c0689d35013f5a923aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:09 +0000

    pinch rich noise phone land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
