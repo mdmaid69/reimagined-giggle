7ceb6540377764d703db9b4373f24f809d5ec464
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:50 +0000

    goup must hurry moral trial

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def sort_list(lst):
-        return sorted(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
