801c537ec724f2d4ea77a61fc7384085bcb53c48
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:38:13 +0000

    rain suit wide breakthrough arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
