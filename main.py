0c7b4b5035df0ec84d4a19f77aae8ea47f998072
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:54:42 +0000

    unfair but type time concerned

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
