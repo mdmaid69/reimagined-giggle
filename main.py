6c776c00bc3dd778e0b102972840b82ed2904daa
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:48 +0000

    small meanwhile string flat that

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
