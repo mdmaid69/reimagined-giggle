11fcd020aacc71f9f5fbc37edbde17e4fff39c0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:31:10 +0000

    sign identify combine stage health

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
