8c47412dfd3208276c41b46a7a323a4ad9e050d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:55 +0000

    skin we criticize congratulate consult

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import sys
+print(sys.version)
