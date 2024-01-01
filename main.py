d39da3944fb4bed744ea6995f903c1f02e2d158a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:25:34 +0000

    precise swim crisis takeoff beyond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
