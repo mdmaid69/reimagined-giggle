58416bceb9698da02667a6abea4dcce17099b511
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:36 +0000

    pronunciation turndown recent ago war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
