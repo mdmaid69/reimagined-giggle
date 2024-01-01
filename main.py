d8b042322bba976a964a71822ffe3b2612aa4c5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:31:16 +0000

    ready suffer certainly touch welcome

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
