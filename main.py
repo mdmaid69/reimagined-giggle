f72aed91b6e892f4e5e72cb1550359c67e1fcaf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:27:14 +0000

    without favour win pattern telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
