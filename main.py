733685297a8d641a4e21ad20fd8cc3d9b680ffbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:44:09 +0000

    safety land smell nice near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
