8d4951bfea816a666110f9267cf047ed13d3bdab
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:47:50 +0000

    remain spoil tone empty speech

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
