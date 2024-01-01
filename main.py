ef70a1b19534f078989abbd0da7a58f36ef6d398
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:50:48 +0000

    nearly goaway small light pleasant

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
