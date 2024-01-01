6cc429c6f0c29560496446fd5f4c5eb86705ef46
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:40 +0000

    simple regard mad autumn several

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def square_number(x):
-        return x**2
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
