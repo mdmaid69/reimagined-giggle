7e1385a222b748668777bfc345ea3a0e2226df39
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:09:14 +0000

    actual careless natural top praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
