0c27b032e4c94b80061f7dd42f07ea756fdb7081
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:16 +0000

    substance bury may score brave

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import random
+print(random.randint(0, 100))
