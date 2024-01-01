97b0c5bcbec3ba840448b5f9347c805fb0ce6313
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:58 +0000

    study right complicated bye sweet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
