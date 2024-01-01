75ef114bf454a1e3d351b6da2eabf64f440eb19b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:52 +0000

    trade support zero bunch block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
