4e352fdc747bac729dc3c3534cca16fda985bce8
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:56 +0000

    suggest worse original experience computer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import time
+print(time.time())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
