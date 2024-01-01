8aa4120046905457ccc44d8a9b0a9030776cf3a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:45:10 +0000

    persuade full represent example personal

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def sort_numbers(numbers):
+        return sorted(numbers)
