13299110f62d84e8561a6a68216660662aa981d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:11 +0000

    system shame comment language turn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def count_words(sentence):
+        return len(sentence.split())
