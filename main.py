8ec43aedcf20f9ca1a8faae04e0e69a29f74b516
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:50 +0000

    through stay attitude city preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
