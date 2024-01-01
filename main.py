afbce1effa74944a13250d261c8b7c535a220733
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:47:17 +0000

    wherever next dance ourselves dress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
