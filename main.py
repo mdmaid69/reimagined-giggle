fb302971c3d1696cf6529197a0695e3e4ba61cfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:51 +0000

    persuade male afternoon morning 565end

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
