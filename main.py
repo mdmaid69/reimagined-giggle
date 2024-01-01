89994ef4d268784e65b3b0498b038afb0aab2f2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:34:52 +0000

    risk reasonable obvious line surprised

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
