6ca576d0a445eef44684d95a21edfb9e3825d8c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:44:59 +0000

    nervous sharp record float contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import collections
+def create_queue():
+        return collections.deque()
