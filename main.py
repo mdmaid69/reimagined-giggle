fe9906f5c3bc28e306d2330f6fc9e7ae76c556f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:31 +0000

    where taste cinema attempt light

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import platform
+def get_python_version():
+        return platform.python_version()
