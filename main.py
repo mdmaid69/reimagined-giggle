bb4001894280361556ceb3d57d608f561c5fb8d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:51 +0000

    star shoot want only favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
