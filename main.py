d5d04328ae011c1cb7549f4109086d83bfcbfda5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:51 +0000

    custom birth care bar murder

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
