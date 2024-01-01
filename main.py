f49c4e11e22b821230618575dd3195f1407fcfc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:18 +0000

    lock need quiet though early

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
