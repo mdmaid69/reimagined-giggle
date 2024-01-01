1adbca86db0b3ef939408fe57d85e902f5c29e2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:26:25 +0000

    break marry combine steal getoff

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
