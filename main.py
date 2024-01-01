4a93fa32aa79d67e38827d6f0261d37d546bc4fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:11:43 +0000

    play rank combination smooth wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
