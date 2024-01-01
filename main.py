8b2196362f7844665c08e8460ac0c5d1f849c768
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:07 +0000

    charge Yy deliberately exhibition bring

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
