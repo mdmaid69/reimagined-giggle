0199270e2bd99a1f2a8082e4a8dc2dbe70bc947d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:18 +0000

    sensible shopping traffic spill bank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
