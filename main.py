bc6ea8205bb4a6312bb61b0921f8f8a8d298f38b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:39:19 +0000

    order club line aim chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
