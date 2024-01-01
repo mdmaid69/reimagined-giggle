856f4ecf6290684d64bc09ea1ec0f4d09e36ca57
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:35 +0000

    container rise splash battery nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
