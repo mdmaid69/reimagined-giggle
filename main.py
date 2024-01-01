4704655354b1de54d7aa39f456bab1801b8419c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:30:29 +0000

    age dangerous bit die store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
