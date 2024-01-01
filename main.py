78e245e64b98e69b0266932d2265aef5eae6b9d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:48:06 +0000

    better somehow total declare Mm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
