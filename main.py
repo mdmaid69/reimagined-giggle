eb404a8076a052d96c53a72f724a261db3325adf
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:31:11 +0000

    centimetre sure polite sting become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
