1f7450095483ffa0b265ec00c93b250887262146
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:07 +0000

    separate about complete prize repeat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_acceleration(speed, time):
+        return speed / time
