af99c7202ab29f158ee18ae3e50484d1111e047b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:55 +0000

    after direct during formal rare

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
