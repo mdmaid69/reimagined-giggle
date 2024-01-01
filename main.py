51fb55bb890b872fdbac8a8b97e37fec6b5886fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:34 +0000

    whenever invite quiet visit soak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
