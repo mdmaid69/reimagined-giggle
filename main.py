b426c0147989aa828e8df1e18ab640df1079bb76
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:43 +0000

    increase limit wrap yesterday cinema

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
