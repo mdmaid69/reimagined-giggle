609a8d101d76c0d5e92d8f62ecabdefadaa7c92c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:41:08 +0000

    avoid afternoon point false aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
