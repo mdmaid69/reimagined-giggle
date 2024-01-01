2e201612614f30663b2fe9db065a46474c253b73
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:41 +0000

    hand confidence new consist down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
