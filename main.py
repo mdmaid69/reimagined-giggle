d86336946f0bdc7a625199cbd58eb6049300e608
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:03 +0000

    mine slowly direct activity water

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
