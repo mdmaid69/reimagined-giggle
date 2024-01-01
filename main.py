cf7b3a0a83855d8bfa5ba399195efd2f29d646f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:40 +0000

    delicate still tool old deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def split_path(path):
+        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
