fb5f07b4d2afad0f6f97a92ed4b3bb5fe1d62df6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:16 +0000

    joke crime circumstance visit cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
