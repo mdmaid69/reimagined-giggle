8cfb4cc4f16300e979da91dc4300d09a7124bec4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:32:11 +0000

    fortunate miss children test accidental

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
