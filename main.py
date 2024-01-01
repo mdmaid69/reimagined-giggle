810aa811a75ffe34df6290adaa9a6eb6d055f643
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:20:38 +0000

    after exist fond work talkback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
