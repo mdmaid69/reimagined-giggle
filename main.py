564dfc803b541215c7ce6ee1f07c835db7d8568d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:54 +0000

    shut control new university makeup

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
