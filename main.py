19f9ae9e3ecb0884cbb7c7101b6cb9750c06537f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:41:33 +0000

    false forgive rid will beside

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def convert_to_hex(n):
+        return hex(n)
