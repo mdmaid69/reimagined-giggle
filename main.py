c58fa16acfba1c2f30811e743b34dcc19c318b1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:20:49 +0000

    herself rent rubbish personal apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sys
+  def get_python_version():
+        return sys.version
