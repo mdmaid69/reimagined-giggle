1cf1093834dd49afcae827fbe85f6b0ba5aa4fbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:50 +0000

    generally deep breakdown vision contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import sys
-  def get_python_version():
-        return sys.version
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
