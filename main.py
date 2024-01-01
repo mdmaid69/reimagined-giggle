53a2760f97dd8198991e2a501e3621fde9673b25
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:27:26 +0000

    song balance hit join talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
