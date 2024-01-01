83e84ddb922254378a84c222725ac39d5b0818e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:54:18 +0000

    bright goon justice public intend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
