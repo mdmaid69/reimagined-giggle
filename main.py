335612a7fdb9726002ee15a5461672904acfc9e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:36 +0000

    terrible many peaceful thanks lie

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_work(force, distance):
+        return force * distance
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
