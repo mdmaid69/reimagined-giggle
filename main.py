b14d9115fe77f567d309e8453b65e4f0f8aab0f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:19:40 +0000

    deliberate eye forever why straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_item(array, i):
-        return array[i]
+def set_array_item(array, i, item):
+        array[i] = item
