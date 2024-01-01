97abd84889e9c8c58b7559726aece7d05f552b0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:08:23 +0000

    table answer learn distance joke

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def extend_array(array, iterable):
+        array.extend(iterable)
