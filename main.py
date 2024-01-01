4679bde51789263581008fc0b5f354b6327d0843
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:41:06 +0000

    dream town thank complaint appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
