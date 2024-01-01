bfde182ec120f41ad771982c117f8a13bbd94561
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:42:42 +0000

    leave emphasis birthday doubt main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
