9e682817c6aad3f84049a1600fd69a8fe1fdb041
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:10:35 +0000

    ask by surprising home better

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
