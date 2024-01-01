eedf10f1e23db350e85ef1759a97287a1583d4c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:28 +0000

    cinema heavy regard uponon then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
