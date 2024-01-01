186a775ee6068dde4d54e98908f4f9a9c16ebfa6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:49:43 +0000

    dead silent whom back blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def multiply_numbers(x, y):
+def calculate_power(work, time):
+        return work / time
+def multiply_numbers(x, y):
         return x * y
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
