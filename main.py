c79029b5da74de45f5f276a02e7530602645aed8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:48 +0000

    sorry offend limit service cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_area(radius):
+        return 3.14 * radius * radius
