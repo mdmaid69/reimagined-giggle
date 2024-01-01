cb5cb034c03d6003a7993d205424ca92794cc998
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:30:54 +0000

    ours question attempt point loud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
   import sqlite3
   def close_database_connection(connection):
         connection.close()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
