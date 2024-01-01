25fd3e720330be4c076bd36cab6be0902860bd7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:54 +0000

    load light himself collect port

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_area(radius):
+        return 3.14 * radius * radius
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
