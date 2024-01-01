75becf00c0b41b9203334bdbc53eca03df84e8c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:07:30 +0000

    represent expect turnover load scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
