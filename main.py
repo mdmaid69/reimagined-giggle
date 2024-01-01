3c5f07b7352e43b36c8c84e42dacad2f9ae8865d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:48:47 +0000

    question invitation upset benefit operate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
