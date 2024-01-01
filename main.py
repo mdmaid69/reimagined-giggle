9c0aed1f8f74417066cb4360c4426c4975d78279
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:34 +0000

    turnaway artificial crowd deep shoot

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
