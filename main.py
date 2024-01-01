dd36b04674acb7434d9844ad910378cabcbb78c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:32 +0000

    goback(to) 179bite temper better turninto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def greet(name):
-        print(f"Hello, {name}!")
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
