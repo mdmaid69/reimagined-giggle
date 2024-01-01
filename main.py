60741782900c388bd15030fcd5ec49ba4159d11b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:53 +0000

    patience butter reduction sensitive father

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
