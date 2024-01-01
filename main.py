691bc2e9ea4796b2d70a0e0870254b93fc14f46c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:51:57 +0000

    spread habit share all combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def count_elements(lst):
+        return len(lst)
