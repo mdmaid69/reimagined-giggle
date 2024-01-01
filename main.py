278dfc7f7a17057ee2a6fcdd8a80caa63045f308
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:05:54 +0000

    feel early contact mistake weigh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
