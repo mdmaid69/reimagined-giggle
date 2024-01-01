bb3051a60688cfb22cf81f25cca7fbfef9a7d931
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:50:01 +0000

    takeover hurt tell female move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_int(array):
+        return int(array[0])
