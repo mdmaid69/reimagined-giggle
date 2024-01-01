7e71f1ec02514fed2dedef713e2b27f279cca911
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:02 +0000

    stare laugh certainly destroy however

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
