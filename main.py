aace1b39417ed67a72c4d0d56bf722850364eb10
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:23:14 +0000

    makeinto home collect female only

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-print(math.pi)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
