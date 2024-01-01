30eb6d2e2dd3121173747d8b71a3dad3353bc0d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:57 +0000

    maintain comeover you last health

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
