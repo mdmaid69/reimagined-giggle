f7f3f528228e353fc64d05c4cae6b05365676b56
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:06:48 +0000

    giveout rush experiment sign bottom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
