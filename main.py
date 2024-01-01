b7f9835e6ff2e9299a906c82895d587e7386cddf
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:28:35 +0000

    clear take service good onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
