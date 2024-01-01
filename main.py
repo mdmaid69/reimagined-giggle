70e2f796ce346c74260c9adb9a79064bf0a3338d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:23:44 +0000

    introduce house board plain target

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
