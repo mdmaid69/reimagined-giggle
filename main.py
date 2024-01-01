95f2e7a4996b59073651f7d0abdea2188b68c65d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:22 +0000

    him lower common interested busy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
