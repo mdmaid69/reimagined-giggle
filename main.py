92ca36af46411172e271684b08ac5c3198f08ce5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:26 +0000

    body game night talkinto land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
