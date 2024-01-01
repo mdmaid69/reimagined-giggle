5cab1829b3ca2077d66b2af49b13961b7b7f7f19
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:24:57 +0000

    touch artificial couple settle convinced

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
