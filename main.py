14e54cac65cf350b7a6ac04d7825bccfafa3a705
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:56 +0000

    never suggestion usual runinto nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_as_set(array):
+        return set(array)
