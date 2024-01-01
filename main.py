c8d2cc7007c49d4ef17d4f256947e8cada3ede16
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:43:10 +0000

    whoever describe home save close

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
