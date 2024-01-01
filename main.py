db20e13da811959a544effb53b6531e08bf3ca0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:18 +0000

    pronunciation congratulate neither rubbish snow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
