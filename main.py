22f5234adea87bd7e423e0e50aa6d3ab452313eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:56 +0000

    insist yet far deep since

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
