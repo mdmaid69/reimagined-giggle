1aac761f81bd4207aa5004f00ce22596071958e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:45 +0000

    sure criticize recently tax even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
