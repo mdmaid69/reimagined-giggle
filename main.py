866e1fa153a1b31f9580ac32278a1c12af2b2e9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:45:23 +0000

    talk happy tragedy top sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def get_current_working_directory():
+        return os.getcwd()
