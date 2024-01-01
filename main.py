9a0b650d1f292f33c1125d1c3cef3c09f59e55d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:33:55 +0000

    dress curious keepfrom on bridge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
