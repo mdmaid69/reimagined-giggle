0288488f9ebb9a2879c69ef842f365af50599ae8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:01 +0000

    medium round rich Tt shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
