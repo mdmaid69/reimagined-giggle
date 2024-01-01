5aec0b6f6d0f20a358b02bd6017292c80bb6c644
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:36 +0000

    rain medium condition bye operation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def reverse_list(lst):
+        return lst[::-1]
