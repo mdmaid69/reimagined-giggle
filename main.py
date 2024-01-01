2d4ff75fbd2940298a3e03a8466e998f41f92a42
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:18:05 +0000

    spoil expect risk smoke market

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
