7aa3a30828433e5b7a7ef63ef35ae8c348efbaeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:59:40 +0000

    recommend disgusting bored classroom break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
