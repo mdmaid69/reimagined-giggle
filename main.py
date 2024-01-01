f5f6111d4bfb97582c13f259f749776d4f21b5cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:37:52 +0000

    represent concerned soak advice sweet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
