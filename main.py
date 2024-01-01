e6a2644e2623733ceddda0abc39eead4a55fc0d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:01 +0000

    exercise Itself broken treat attention

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
