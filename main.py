65f52492cc204647151f4cbe70a116ec222808f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:53 +0000

    custom side float even eat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
