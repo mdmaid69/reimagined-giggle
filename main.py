2c8805a6c6fe351e24894cf4a83d2980440a6dd2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:47 +0000

    badly brief edge past peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
