6ddbc80e2b7f208f73fb328d20a6c4addf30a86f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:44 +0000

    confusing weigh gap goon object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
