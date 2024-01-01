b098eedca0ed35e9942bf9b14ccfa64744b59b51
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:39:37 +0000

    state from prize takeaway exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
