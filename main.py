c5385ddd2bee010ce7f0814b2346c4e1047b08bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:26 +0000

    comedown smell confidence time region

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  def square_number(x):
+        return x**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
