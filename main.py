d464b4c181a742a6779090deb873922775053652
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:22:15 +0000

    world post total mind concentration

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def convert_to_hex(n):
-        return hex(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
