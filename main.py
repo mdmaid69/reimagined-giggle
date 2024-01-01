226091b2c744b426bade7026cbedd56d6f5121d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:51 +0000

    word traffic tape it rest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
