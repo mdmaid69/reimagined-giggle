99ad6f6c9ec5138bfc302fe17abdb680859eddd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:29 +0000

    perhaps black burst juice fast

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import functools
 print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
