ca771e3372ff9762f35989f7607b865626d70b51
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:38:11 +0000

    divide platform on interview score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
