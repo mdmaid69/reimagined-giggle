23af9abc8d9d2b1e667bed3999ae5c8c519b3655
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:18:23 +0000

    cheap impatient powerful lost in

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
