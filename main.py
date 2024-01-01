1ee0b022bae7553663ca7b1d09831b2d60a4c831
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:07:16 +0000

    whisper burn deserve worth check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import os
+print(os.getcwd())
