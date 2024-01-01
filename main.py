2e2320b5f4552e4ce67dd8d5663cf8d61bb0f3dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:43 +0000

    thumb rent still centre usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
