39810524e2592af35fe3df9439a90b6cad03d929
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:57 +0000

    Vv top properly round even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
