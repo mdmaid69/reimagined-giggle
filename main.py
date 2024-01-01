57bf3b391a105fea4c99ede3e100873d36429cdb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:27 +0000

    familiar pinch ability beneath cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
