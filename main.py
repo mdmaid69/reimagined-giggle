a90abd4d89cb5633cc6e7363e15f6214443a8d92
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:27 +0000

    science spill learn onto get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
