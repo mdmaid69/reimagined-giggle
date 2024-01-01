7776ec3f5e41c74f8e304718ffb136bc6a1338d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:08:11 +0000

    upset entrance join lucky big

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_work(force, distance):
+        return force * distance
