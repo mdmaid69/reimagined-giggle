e4f194bd8feb88648b1447213d92155bc476bc41
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:20:53 +0000

    bright view professional love smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
