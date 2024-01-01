be6b83c04a6c63d80dca5940d6b9225b543f6b40
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:49:42 +0000

    light impatient mad main occasion

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def is_odd(n):
+        return n % 2 != 0
+print([x**2 for x in range(10)])
