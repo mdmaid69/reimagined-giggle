e05a813f4ecb269bc38d9acf4a40624935df9e7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:58:47 +0000

    care average range possible believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def roll_die():
+        return random.randint(1, 6)
