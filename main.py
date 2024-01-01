8430c9e9e1b344bb1fa2c3880f0461181c5e2b57
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:04 +0000

    now break scare collection pass

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  def multiply_numbers(x, y):
-        return x * y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import collections
+def create_stack():
+        return collections.deque()
