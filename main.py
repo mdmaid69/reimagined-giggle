1789e5c10d9970df662922641362bb39a1f70599
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:21 +0000

    important rub belief turnover punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(numbers):
+        return max(numbers)
