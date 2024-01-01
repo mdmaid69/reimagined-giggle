4e193e608fbd0ef268c1866ce8580f1deea311e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:40 +0000

    correct round choose pronunciation pause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
