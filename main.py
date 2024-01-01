cefe84c8f4eac6bcad0be39b17d2ead295d9f701
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:57:16 +0000

    file heavy reject bin sink

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-print("Hello, world!")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
