fc2f2956629a8810d61ea885ca27be7c8d625c47
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:51 +0000

    ought reduction defeat love workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  def is_even(n):
+        return n % 2 == 0
