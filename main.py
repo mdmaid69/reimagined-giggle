8a3eb681e840d357152c751824bb85e44a029467
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:28:18 +0000

    weather will gas style stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
