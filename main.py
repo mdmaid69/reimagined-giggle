7d400ac1e2bb97695e7368e2407f0a4a0d939625
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:03:40 +0000

    after hungry know after draw

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_acceleration(speed, time):
-        return speed / time
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def sort_list(lst):
+        return sorted(lst)
