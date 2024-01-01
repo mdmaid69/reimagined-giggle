676d2f805146eb4561efd0f9b7c21782f55ec4c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:47 +0000

    punishment responsible estimate tidy dig

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
