2932eb2e45104395c0ef7466d347bcc838fcb56f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:28 +0000

    call congratulations pen Rr her

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
