e21ae650760177b14fe8c05bd01095ca5963c49f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:36:30 +0000

    lead basic keep skill himself

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def print_python_version():
+        return sys.version
