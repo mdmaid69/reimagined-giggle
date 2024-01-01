17b678cd9f99a743622654b69f5b2fc4ac4ce9d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:57:32 +0000

    ilovedicks talk interfere cheat wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
