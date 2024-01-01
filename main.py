129d17394bebcbf62dede1018605cc670b130abb
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:26 +0000

    warning woman mood pleased Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def find_min(lst):
+        return min(lst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
