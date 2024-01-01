fcb1bec65b403fbb6d805f9a4bda48b467f42495
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:50 +0000

    coat we thin tone offence

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
