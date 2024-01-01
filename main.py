f4e941aa1728f52a90be51a61e79affd5135f2b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:22 +0000

    long broken copy similar 569enjoy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
