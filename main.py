f5aaaec6d19219f6121e03e98518f6a781c07a67
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:53:52 +0000

    possession steady blind pen understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
