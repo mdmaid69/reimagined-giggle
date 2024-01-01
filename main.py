9628e1815ae383facde57085963b8cc33a30fa19
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:19:09 +0000

    same apparently face right knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_int(array):
+        return int(array[0])
