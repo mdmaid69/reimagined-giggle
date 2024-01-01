cd92d0000750b1d64b5d71451870125eb4466aeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:37 +0000

    shop tip trick avoid connect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
