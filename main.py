00660ead9f8e169862a815681c2413facd5ee003
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:11:48 +0000

    pretty suckme holdon remain talk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
