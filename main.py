6e531f1adc43fa37da0b2b3dad21ff31d63a23a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:33 +0000

    frighten provide either turnup issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
