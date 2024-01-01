0cb0bafb19fcb41fc78b65ccd2110edec2971884
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:34 +0000

    trick there will business yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
