26ebf4818b412a2a655c4a59f11cea7bdbbc7a7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:12 +0000

    closed attempt squeeze bet party

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import datetime
+def get_today_date():
+        return datetime.date.today()
