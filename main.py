a1e3c8cbef03b14516e9bccc5eabbe349a6f2994
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:54:26 +0000

    will process Pp show either

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_circle_area(radius):
+        return math.pi * radius**2
