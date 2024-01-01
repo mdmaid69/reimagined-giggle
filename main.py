32d151af3cf7e9144688bfa92905e81d9ddaffd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:40:48 +0000

    count interrupt communication uncertain many

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
