11015a365036494881caed03f6bd636b8940d2c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:50 +0000

    pretty card constantly tidy interview

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
