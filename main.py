bdcd1f8ab29955e80d76498819f50a804e1b1871
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:32 +0000

    raise avoid may moment mean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
