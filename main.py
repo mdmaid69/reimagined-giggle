66672cc19e1ec6823f6584d69ebc076c590ec671
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:05:27 +0000

    average club achieve drop complete

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_unique_words(sentence):
-        return set(sentence.split())
+def count_characters(sentence):
+        return len(sentence)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
