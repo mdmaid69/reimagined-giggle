fafe363971e4c8c344b031eeb96ddaf9f09a0506
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:48:57 +0000

    everyone gas famous steam passenger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import getpass
+def get_username():
+        return getpass.getuser()
