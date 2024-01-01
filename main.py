414d696991f67f23c33060a2c5a4a5cafbe4c54f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:47:38 +0000

    alive getoff information error thin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
