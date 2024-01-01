c10686fe6c7150afa6dda9bed108b2202bf7c359
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:47:34 +0000

    call blame agree abroad deaf

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
