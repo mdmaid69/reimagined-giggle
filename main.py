b44fd91c6cef2c0202bc49d0cee3712bbefc68a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:33 +0000

    public chance risk underneath air

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
