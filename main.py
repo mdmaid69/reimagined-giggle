952ad41f0be98e57328be54e65c779a7a720f4ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:51 +0000

    lead key correct likely demonstrate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
