4083465bb700418c334df780fe18bd123c64251b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:57 +0000

    check call tense physical press

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def sort_list(lst):
-        return sorted(lst)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
