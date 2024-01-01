0510308d6ec4057da5c4e4cb6847caa6656ccc80
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:59:03 +0000

    zero true announce plastic area

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
