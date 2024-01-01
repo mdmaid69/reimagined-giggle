84213f76932f5a708ca3a6a1cb133a06c4ef7946
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:33 +0000

    kind pleasure deaf raise nose

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_square_root(x):
+        return math.sqrt(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
