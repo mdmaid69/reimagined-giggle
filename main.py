57d3e94ec0e61838637757845b418bdf790a31b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:33 +0000

    tip other offer talkinto butter

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
