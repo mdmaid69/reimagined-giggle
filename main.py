be0d1b69ff802aeca5b6cc8501f70c96e8527c0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:01:13 +0000

    rank closed finger split shape

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def square_number(x):
-        return x**2
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_pressure(force, area):
+        return force / area
