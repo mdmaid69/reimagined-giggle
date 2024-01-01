842e884a63c66e009af454a5a71268e823aff0ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:51 +0000

    upwards survive pollution slightly lock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
