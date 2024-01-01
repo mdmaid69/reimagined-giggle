36ae019881c88f70d296803c7608d5cb5d28bcf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:51 +0000

    statement smell affect gift contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
