d611bbe95ff037c8368a0014234d8815cf75ca11
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:44:12 +0000

    occur vision nowhere holdup manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
