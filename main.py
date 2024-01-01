e8706b03400ee1b10d158eb076c716ad0613bc87
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:04 +0000

    holiday machine worst good full

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
