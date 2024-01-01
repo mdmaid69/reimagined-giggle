b08c9ccdddb51da02187b9f0f93c2283dfd01e82
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:57 +0000

    platform scrap practice challenge instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def convert_to_hex(n):
+        return hex(n)
+import array
+def get_array_typecode(array):
+        return array.typecode
