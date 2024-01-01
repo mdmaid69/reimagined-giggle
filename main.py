b81857ed85559f70f1e2546c0085d4808f04d5f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:18:11 +0000

    maximum rarely breakdown angry fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
