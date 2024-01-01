b6d9a2db901dd0843b8d749e381d4fea552d22d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:08:09 +0000

    dream female cruel ocean factory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
