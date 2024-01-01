e327419f5a47f0d943b5dcb79c3aebf428f9d131
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:33:56 +0000

    interrupt polite noisy light runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
