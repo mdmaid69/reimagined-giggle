beb0b7f65b7da112916d0141289a2d10b03173cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:46 +0000

    fill car computer suckme butter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
