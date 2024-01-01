ce8d3433f8218557a25047606f99bbb43e83c3c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:58 +0000

    produce absolutely head flow steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+  def remove_duplicates(lst):
+        return list(set(lst))
