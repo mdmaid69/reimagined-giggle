5ebb7fffaec51bf14f606f26e44d43e2070acc8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:33:29 +0000

    chief professional cause Ll night

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
