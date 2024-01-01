c731d959f100844618abfa59f76c74af80988e56
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:16:00 +0000

    improvement group whistle breakin sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
