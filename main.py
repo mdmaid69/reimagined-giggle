4f1017ce224fb24de9715e9366379d3bf53da621
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:42 +0000

    sometimes turnover war yesterday intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import math
 def calculate_cosine(x):
         return math.cos(x)
