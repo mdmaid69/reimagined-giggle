fb2218dc1e138e4fcbbbacb7711dad16da4a02ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:33 +0000

    temper cover open certain jump

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
