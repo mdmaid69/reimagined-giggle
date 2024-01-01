3625c4dd1c12e5149c3b1d275c736d1a26568e0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:37:37 +0000

    preparation trial hair sure miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
