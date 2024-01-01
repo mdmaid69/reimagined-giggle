01bf401ca1c632bdcb9b73ad74e9c3d150997143
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:57 +0000

    improvement image bread some will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
