200c01a9b2f97d6cd3c9d5e673c2ed7582943f65
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:49:41 +0000

    well reach beat involve several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
