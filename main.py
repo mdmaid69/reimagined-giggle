4c45d10eeab0ac59882c99d40e8de7759e0689a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:59 +0000

    key blue near discuss cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
