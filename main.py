5af3a77de46a8ce87a23f33550b4fa58b0a4af29
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:24:13 +0000

    enthusiastic thick influence giveout lie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
