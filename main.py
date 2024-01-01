3f471037cde0b686e7922051b8dd0a8f92916f2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:30 +0000

    store sleep save content branch

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
