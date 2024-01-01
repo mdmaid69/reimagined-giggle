ec261dc67d74fdbbf4531a80bfc2f80c7a8c4278
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:20 +0000

    traffic goout challenge easy left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
