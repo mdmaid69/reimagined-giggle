d90fddab3a57c9de795c848e34dcd72ae003ce31
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:40 +0000

    future rough guess beat firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
