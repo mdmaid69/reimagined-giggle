6e8d615c1bdae1a5d63fa9ebc08608c91e4c8c41
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:16:08 +0000

    run touch land sister fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def convert_to_octal(n):
-        return oct(n)
+def get_array_as_complex(array):
+        return complex(array[0])
