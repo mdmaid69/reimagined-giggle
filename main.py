484d628ff3397699a08581bf7236e7cf2b22d062
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:03:16 +0000

    loud minor best face first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_work(force, distance):
+        return force * distance
