0d3c34103bc7d25aee49f8329bc17d64e52053dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:49:59 +0000

    us neither make breakthrough surprised

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
