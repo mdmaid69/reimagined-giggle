42dfc010ed634c022fdc28c76e7408b3f421a05c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:44:42 +0000

    tip actual now show tell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
