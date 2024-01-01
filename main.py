3fc53851edde4f570bf337d4412ce6263f3a6c8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:16 +0000

    cost prevent rich affect takeout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def convert_to_binary(n):
+        return bin(n)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
