ab95ea907744e757acc98552b5a9f27d2111f986
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:02:14 +0000

    count valuable hurry aim around

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
