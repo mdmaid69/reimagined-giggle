402ed1fc936f259ae764b5718e8e5d4f90ca6e7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:43 +0000

    possibly race later ordinary round

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_speed(distance, time):
+        return distance / time
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
