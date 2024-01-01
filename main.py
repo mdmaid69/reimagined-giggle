5eca083564902ffbf9ae301f5423d28f8e5d7dc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:40 +0000

    behind argument gas now proud

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_repr(array):
+        return repr(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
