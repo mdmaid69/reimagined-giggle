dfe2d25e70f6e5faee6ba0f7af49fa9034d06b37
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:05:39 +0000

    large used luck occur immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_palindrome(s):
+        return s == s[::-1]
