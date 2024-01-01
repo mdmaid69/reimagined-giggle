6cc2c330e1121e321a48eff878604c4c169dde8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:23 +0000

    attitude total freeze pocket probably

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
