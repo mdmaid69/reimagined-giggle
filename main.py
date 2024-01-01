7d603bfbe6117023cc2fb078a7e04c78ab730c2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:44:51 +0000

    unfortunate vanish strong comeup line

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
