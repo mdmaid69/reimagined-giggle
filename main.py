327a653003d5d976fcf71e08f37bbb2a246c8f64
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:35:46 +0000

    that read everyone collect guide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def square_number(x):
+        return x**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
