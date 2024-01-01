0ad1f72a1c689d26604b0848c2a33dd863a463bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:03 +0000

    letter but collection avoid meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
