fdac7f1b58637cc41189c7af79c8b583afbbb0ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:51 +0000

    complete square lookback(on) border now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
