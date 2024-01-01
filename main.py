e79761f300e78498a62d9d307710b46f73b37a4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:12 +0000

    young keepfrom answer promise small

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
