34a6fdc660953c8a52843cb3ade5178641a00eb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:07 +0000

    yours question appear broken hurry

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(numbers):
+        return min(numbers)
