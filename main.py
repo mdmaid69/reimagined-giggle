b574a24cc7712e3f4776be7a74c997d739ddc69d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:14:51 +0000

    nigga bet offence serve store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
