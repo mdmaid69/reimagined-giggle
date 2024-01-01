a1d677a27b42c41f440742e0e7ed8744319323b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:58 +0000

    accordingto visit cost swallow gap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
