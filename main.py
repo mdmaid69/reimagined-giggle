56c80783dce445ab0104f0ccb9c0acf7ddc523be
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:52:23 +0000

    stranger practice breakup common mistake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
