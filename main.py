e51e750de9350ca1d69c38c4cb8e9d16b6accbfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:07 +0000

    fond partner slightly home interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_hex(n):
+        return hex(n)
