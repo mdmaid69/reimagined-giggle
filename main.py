8184b64d87980b4e7f8f28a27d91af05c5cb62a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:24:05 +0000

    make tend such length manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
