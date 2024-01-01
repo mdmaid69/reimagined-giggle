a505f50e13e4233488421c186ebe4293159ad0d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:35 +0000

    frighten cover card protection instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
