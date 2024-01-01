7420fe6de5b2942cf923d417252ec2eb8207e5a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:00 +0000

    over law day stiff press

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
