06411f9c8d7c2d1513ac28fa9cf0a4f186b9c6f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:13 +0000

    each bush legal city relax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_length(array):
-        return len(array)
+  def get_current_directory():
+        return os.getcwd()
