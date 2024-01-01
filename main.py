a9a36a40988deebbcfa17a9c1bb26bbb85e00072
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:16 +0000

    pencil hole run capital lift

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
