a8e7c128fee54bdd8555322de3f9ce0a8eab729f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:41 +0000

    square position dear branch remain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
