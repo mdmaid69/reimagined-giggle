3fa2e1c10c1f2f5e98260ecf5ba6e7b0717df0e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:04:11 +0000

    coat trial lookaround rid deliberately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
