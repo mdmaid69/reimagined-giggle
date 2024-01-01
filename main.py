ad4acd503fa47009f7317adc5dc91d70e679b79c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:27:49 +0000

    breakin minority rule prepare introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
