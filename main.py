e58357505dbd0477c45285988317d1d2a354c391
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:46 +0000

    science pinch word precise calm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_speed(distance, time):
+        return distance / time
