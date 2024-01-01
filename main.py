adb6822c3a3ba1a979a0aa1b17419b4232bd4d5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:49:26 +0000

    sleep return winter lead read

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
