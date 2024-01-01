ea1b7137646fe2e5e2957d9330319c17fcef5c2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:37:35 +0000

    fast another improvement close declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
