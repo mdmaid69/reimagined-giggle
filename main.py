350ce002df596b8072df77feb67d11cbbd13e0a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:53 +0000

    copy excuse fit thankyou organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-print(os.getcwd())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
