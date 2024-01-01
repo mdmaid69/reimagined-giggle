76e2fcea2a440bf69c9aade53229d9337e97f426
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:26 +0000

    sort burn comeround grow laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
