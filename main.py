07af5acf7fd0dc2a4043c1d7ca247ed6025dd4ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:25:10 +0000

    keen hot suppose recent between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
