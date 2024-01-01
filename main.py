a9d0cba6ba41609def66ce3ff84083d70efa1cea
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:29:00 +0000

    taste comeback plenty weather lonely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
