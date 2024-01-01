db3dbca2e24170b6ae2353e95665f70fd1a6e89f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:30 +0000

    argument method grow anybody forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
