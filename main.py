8847f0ad15a0fd6f1e00715a7e0f40394f18525a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:18 +0000

    against intend professional sleep century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+for i in range(5):
+        print(i)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
