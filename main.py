91618928cb220e9a573db5838bdcccab66fc6971
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:16 +0000

    comein so next punishment exchange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def convert_to_octal(n):
-        return oct(n)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
