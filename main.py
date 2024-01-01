18194f06d57340c59c1214dd183c56b7e60c394a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:03:15 +0000

    but special turn trap go

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import random
+print(random.randint(0, 100))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
