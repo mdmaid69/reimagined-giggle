93a198322036c69dc9aa86a958c67c5abb237a5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:42 +0000

    bringdown skirt burst stare unusual

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def convert_to_binary(n):
+        return bin(n)
+def find_min(numbers):
+        return min(numbers)
