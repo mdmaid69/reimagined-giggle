710e1a161e7d46704aa1b842da005ae5d0862bd2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:13 +0000

    announce try solve principle add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import re
+print(re.match("h.*o", "hello world"))
+  def count_elements(lst):
+        return len(lst)
