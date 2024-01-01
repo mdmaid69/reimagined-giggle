5327dde0b71ee49c91813927b8cb98b50e5ee0c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:22 +0000

    lock mention impatient destroy poison

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_min(lst):
+        return min(lst)
