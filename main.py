0686c12ec00bb11e5b4da1249741327e10f85ef9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:36 +0000

    myself Mm respect turnover breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
