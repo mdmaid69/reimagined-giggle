efcf443ab38e520b0d54e1a6c154e9d1c9483c89
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:50 +0000

    stamp shut giveaway present sorry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
