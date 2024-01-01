d5fcee04888c31b7b77ef3e391dcad7a20c48480
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:29:15 +0000

    grow boil comeround tread instruction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+print(os.getcwd())
+  def convert_to_hex(n):
+        return hex(n)
