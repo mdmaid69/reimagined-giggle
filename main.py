7afe5cd7f976375e0511ab8cc852022e7e14929a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:08 +0000

    work combination serious curious pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
