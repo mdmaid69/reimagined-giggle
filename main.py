435d0b481fb86ef7e3a9e9792e678a2cf5f3afc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:56 +0000

    deliberate nor key affect rather

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def sort_list(lst):
+        return sorted(lst)
