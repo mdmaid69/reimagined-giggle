5d6d9b93982aac47e984e985aa365c7106e92311
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:47:09 +0000

    introduce generous reply fire mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import collections
-def create_user_list():
-        return collections.UserList()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
