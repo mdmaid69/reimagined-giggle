b2eb9543bbf9f7af6e396dac4d90e00da6b2632c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:53:25 +0000

    before bill lend goout no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
