29d7d95efcd9e5d1498ce500a6861e0bd13290e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:58 +0000

    bush where deliver holdup completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
