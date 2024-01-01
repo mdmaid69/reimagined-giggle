42727a9d4f093d87a8a96f377fd1c5bc181c4fde
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:34 +0000

    pretend spray thinkthrough getup success

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
