1d37594f78bf5256c4a065cb6b88730a526423c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:14 +0000

    brown form accident holiday condition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_density(mass, volume):
+        return mass / volume
