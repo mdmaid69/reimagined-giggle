b71c75c4d815344aec1f097ff1b7635d2a7ed239
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:17:24 +0000

    face stop priority desperate eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
