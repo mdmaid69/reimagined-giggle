bf99cee7d44b87662c02e83012a28ebb041ee02e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:13 +0000

    round pronunciation none well frighten

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
