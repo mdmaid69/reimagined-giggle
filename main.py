649f2d76881d732a44614febabbf035eef4c81f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:47 +0000

    people load suggestion pronunciation government

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import sys
+def exit_program():
+        sys.exit()
+def find_max(lst):
+        return max(lst)
