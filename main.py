29e4d56bfcf2064a131e7bafc623c052dca4ceea
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:04 +0000

    design class fond joke capital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
