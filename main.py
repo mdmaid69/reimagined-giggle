0bc7d98bc3e98ac78ff7b75b6d59aa0adb061ca3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:38 +0000

    finish crowd rich relationship road

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
