90c18dd5bfdf70cb846084c6ef8d890a1c22e209
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:09 +0000

    range Tt branch consider serious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
