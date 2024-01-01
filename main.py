f204cf445e58641da02da5b00f3f556fff8620d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:24 +0000

    war different hard away deserve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
