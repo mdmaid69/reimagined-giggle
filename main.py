bf895ae073a7d149a8e6af15a8718c1fb43129b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:41 +0000

    Tt measure week destroy share

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
