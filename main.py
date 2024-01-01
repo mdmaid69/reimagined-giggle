217cc512e5e504a45b4e0fe1541880b5f51cb807
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:51 +0000

    service again tune maybe result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_binary(n):
+        return bin(n)
