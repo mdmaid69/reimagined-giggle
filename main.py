954ae812956c63c2c14575ccc8f859ace0eb415c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:26 +0000

    blue it side worry disagree

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
