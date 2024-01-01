66f45933349693aeefdd5c0cbe5f46d3027b0267
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:41 +0000

    fortunate fold trap breakout load

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
