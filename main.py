d3d8f67eaa2315f611138185347723f31d05b48d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:15:27 +0000

    bang wild break treat second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def get_array_length(array):
+        return len(array)
