d781a3bf263e5de4d22b013a0d10f6ba0f0d8f5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:49:50 +0000

    day compare flavour now grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+print(datetime.datetime.now())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
