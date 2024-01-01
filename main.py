bc52fa4209c1ebacc10903329d42bbd86b60efb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:45 +0000

    lonely reach rank service case

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
