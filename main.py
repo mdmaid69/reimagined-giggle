e9357607f29698cbc1988a559e56a648e05e1fc8
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:20:28 +0000

    human mistake afraid bin strike

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
