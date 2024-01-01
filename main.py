16414e3284c41d25981a2d0ec6c5e2abb4e15e5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:37 +0000

    unpleasant speed choose attach holiday

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
