48c0d9b97a423423400b40bdbc4b89f538b25277
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:29:29 +0000

    follow profit weather power never

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+text = "Hello, world!"
+print("Words:", len(text.split()))
