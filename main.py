8f057d848f6ed2a43d3956550d774c60f635bcbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:55:05 +0000

    moral discussion nor strength boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
