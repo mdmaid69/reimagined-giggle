4557311493b955a2462bff5806efb0e732c27143
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:05:00 +0000

    inform comeoff large comfortable follow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
