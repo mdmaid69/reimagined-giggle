132a6125a4674f46ea76f480619a87d3dbf7febb
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:21 +0000

    shrink cold treat squeeze close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
