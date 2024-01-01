bae2e94162a533229661400e0f12d553b3a970d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:30 +0000

    average obtain flow week vanish

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
