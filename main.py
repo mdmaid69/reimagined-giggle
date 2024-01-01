3c875e4a56f93c00940fe868d017fd39c5daf48d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:15:12 +0000

    it total fasten pupil shallow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import numpy as np
   def calculate_median(arr):
         return np.median(arr)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
