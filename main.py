33e94f02081716067ffb755300001e20cc1c8158
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:06:53 +0000

    watch radio plan top twist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
