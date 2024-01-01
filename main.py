ebe1c07a5007ac289f9d367363168ebacd69747a
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:15 +0000

    bread thank promise still life

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_str(array):
-        return str(array)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
