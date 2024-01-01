7763a21a02a65a6ea9501fc56847026a5d608897
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:14 +0000

    flow repeat phone determined business

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import tensorflow as tf
-print(tf.__version__)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
