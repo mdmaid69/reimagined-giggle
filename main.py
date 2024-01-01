8f29f063f4921f91e98ac59a7f8b616d1de57ad6
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:25:24 +0000

    pump interview behaviour trip exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def count_characters(sentence):
+        return len(sentence)
