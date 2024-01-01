d79e253dd65fc18dc94cf172e1653543ef30e34d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:04:15 +0000

    chance danger meal drink tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def square_number(x):
+        return x**2
