59ce91bdc94e26198be016eb7ecd95eecd5311df
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:47 +0000

    572enter official herself hurt poison

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_directory():
+        return os.getcwd()
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
