893dca71646fe0072fcec219167a6f5da444e4bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:02:14 +0000

    couple cry interest before sorry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_power(work, time):
+        return work / time
