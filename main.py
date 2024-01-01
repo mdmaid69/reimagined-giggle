c6fce4cf57c2472d22536313c54c0cf05e1161ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:01:20 +0000

    fierce world Ss bunch shoot

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_work(force, distance):
-        return force * distance
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
