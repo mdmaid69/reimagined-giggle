6b795c8f4e94eecd696641493e1a4f091e14f35b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:49:21 +0000

    risk nurse out practical consist

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_speed(distance, time):
+        return distance / time
