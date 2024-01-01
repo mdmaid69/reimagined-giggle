c8e71f00d3a4b62fee62b4e1529df7cd16108b87
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:32 +0000

    minimum pretty light new block

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
