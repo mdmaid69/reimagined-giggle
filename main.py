c1f7526bdc7971f081fa64393c166a0f70968ef4
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:17:14 +0000

    shadow return care lonely positive

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
