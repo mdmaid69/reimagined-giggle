eceefbf58145d920348002ed11d86c9940ad1cf7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:23:47 +0000

    plenty dry better shall word

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
