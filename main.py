62e75a7317c4e275c2e5e5647ae14ee1231fbec8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:36 +0000

    takeback wonder pay total word

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+x = 10
+y = 20
+print("Sum:", x + y)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
