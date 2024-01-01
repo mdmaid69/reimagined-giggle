df23976ed01a2e325d090211626796c0827197f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:57 +0000

    comeround who whatever lost race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_max(numbers):
+        return max(numbers)
