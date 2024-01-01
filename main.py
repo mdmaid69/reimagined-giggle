398e0ae84f8e490e87ea60e636d1efcedb1d8a5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:24 +0000

    original free declare post square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
