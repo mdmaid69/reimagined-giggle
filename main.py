42e34f72e88e989ceb2f6fc560016ce9da243f0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:10 +0000

    solve practice aim become gate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def add_numbers(a, b):
-        return a + b
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
