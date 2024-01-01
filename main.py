fd7769f999566e14681aeb436583c73d4346b0a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:10 +0000

    treat dear consist bury this

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def find_min(numbers):
+        return min(numbers)
