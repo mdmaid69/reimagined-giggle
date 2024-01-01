d08ed2c751c938bc7fda53b2f04dc009ae3b71a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:42:17 +0000

    reject most expression steady touch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def set_array_item(array, i, item):
+        array[i] = item
+print([x**2 for x in range(10)])
