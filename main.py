430f8e89dedd60cd2a148dc090ca07a1673eec95
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:48 +0000

    once sting realize major unhappy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
