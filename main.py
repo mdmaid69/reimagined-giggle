de7f6955f136ad36f5cdcf05009f80ca1f63de9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:57:28 +0000

    fight children odd many reaction

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
