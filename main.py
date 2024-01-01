f1505e7a7d33a39c3a1cca4b82e17e3ac1245c30
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:04:16 +0000

    pattern below warn fetch rub

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
