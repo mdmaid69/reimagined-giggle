8b98aaa0bf9cd9909cca2a83234353ee79a8e844
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:38 +0000

    switch full judge announce should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
