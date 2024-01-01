b8ac2629529c353b71d32756c71396a73ae3eeda
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:41 +0000

    rare better afraid recent weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
