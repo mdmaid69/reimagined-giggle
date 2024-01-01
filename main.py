2f894927b6790ab87f6ad1ad7a9df0695620c46f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:28 +0000

    drawing now confuse pull gentle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def sort_list(lst):
+        return sorted(lst)
