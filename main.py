8665d21ecd582965c689eca73187454677b22403
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:06:11 +0000

    rain bored neither mix improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
