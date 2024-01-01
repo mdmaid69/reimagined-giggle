7fa24dee03d951b00f8f81dba79ee7e680352e7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:44:45 +0000

    tonight wing bit accommodation alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
