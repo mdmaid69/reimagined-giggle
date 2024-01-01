573b644869642897723c111fb92db91c34edc879
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:45 +0000

    defend warn task normal week

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
