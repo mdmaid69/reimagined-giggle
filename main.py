2a3b67bc778c58873ec3dddf27043f2c0ef7abd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:48 +0000

    pass cheat basis statement last

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
