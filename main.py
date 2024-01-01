4d03dc5b8044a4f0963804398f2aadff5f8d41ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:41 +0000

    club undo form film along

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
