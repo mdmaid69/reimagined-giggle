91b96b1d68f7caa1cd509c72f5d370adf713418d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:45 +0000

    sore cross depend extra split

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def clear_array(array):
+        array *= 0
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
