9556721778a395bede7fa80703606865418fa19e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:09:43 +0000

    direction willing deliberate across concentration

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
