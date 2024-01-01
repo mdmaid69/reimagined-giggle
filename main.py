4a7558c8cbe309df8d294b6fbeb4d9b90bb990cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:11:06 +0000

    shout wrong offer usually politics

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def is_odd(n):
+        return n % 2 != 0
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
