b1f02702d5df6e216d09161548c5fa4f64b346ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:38 +0000

    measure this wheel frighten respect

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
