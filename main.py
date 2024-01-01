c5f5363260ab0cd1cd00449d4112a74d37cbfcba
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:47 +0000

    breakin fierce public help light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def sort_list(lst):
+        return sorted(lst)
