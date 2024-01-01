2d24111cde9e68dcd58741c47191dd0f9443b7c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:52 +0000

    ring drive question anxious upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
