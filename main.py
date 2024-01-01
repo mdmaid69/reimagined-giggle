e81b8fc96ac0808eafc44d738714f1ef4c253c23
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:02 +0000

    stir out respect than welcome

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
