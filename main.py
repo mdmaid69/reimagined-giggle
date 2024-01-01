2f29da70e809f28f3bdaf98c471dc5af94e1f587
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:02 +0000

    luck mind seriously comeback news

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
