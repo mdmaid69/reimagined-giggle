f828a28280b6b3c73e2783f3729b4340dd6ab28c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:40 +0000

    several high pin and first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
