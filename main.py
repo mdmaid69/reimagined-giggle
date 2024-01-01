b17d3a350675fc8e03114fd4dbfde5b0b25f4136
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:05:56 +0000

    show much makeinto favour themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def find_min(numbers):
+        return min(numbers)
