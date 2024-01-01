6ab242b5dd0de8b9467226d2551df04093edd118
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:56:52 +0000

    complete splash either achievement total

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
