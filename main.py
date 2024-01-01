6db7d201d2f936d4827c07d37c6fb1e5e88468ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:40:21 +0000

    through properly fail turnup through

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_complex(array):
+        return complex(array[0])
+for i in range(10): print(i)
