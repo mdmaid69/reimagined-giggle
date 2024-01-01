49114e412c225aeddb27101e162c8b704e5c609c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:08 +0000

    this accordingto free agree violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
