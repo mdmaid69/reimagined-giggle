b8dcc17802b3ef465fc893d3801d2a02969596d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:10:16 +0000

    priority talkinto ready bush last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
