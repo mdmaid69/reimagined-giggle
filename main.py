a1f81bff386ed05c464823daeb31fe8f7e46b7ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:31:49 +0000

    injury mouth result reply between

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def is_even(n):
+        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
