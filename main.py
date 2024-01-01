7ccc53ccddc80d19c8e26b250131f7cdfbc5c96a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:58:18 +0000

    buy Dd medium doubt definite

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
