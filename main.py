10643b8bf11c44fb712f710aba2edfee2c0f165b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:13 +0000

    characteristic without organize tomorrow actual

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_power(work, time):
-        return work / time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
