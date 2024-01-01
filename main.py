b980d6380811acb7eca56d373f6d5438cbb33f71
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:31:11 +0000

    around demonstrate marry Ww delight

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
