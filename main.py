698654a35f52fa2464b6d9069d40cf2ae025bbab
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:22:17 +0000

    word refer twist battery pause

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
