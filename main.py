171ec71d7076470be3c609b0a095d812d9785b8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:39 +0000

    book seem pronunciation workout own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
