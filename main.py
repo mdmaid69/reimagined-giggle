0120c1129b54f7892019522bf7b42d3e18e40c26
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:14:31 +0000

    Ll other insist minority will

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import os
+def get_current_working_directory():
+        return os.getcwd()
