86c158974a6170dcc025cb8b9c103bba865620c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:43:43 +0000

    middle contribute workon tired drink

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
