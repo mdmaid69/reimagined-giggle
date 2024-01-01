65b4014954ba40d6e7b816ee154753488e42c63a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:51 +0000

    nose fortunate honest comefrom man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
