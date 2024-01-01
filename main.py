893fa79a7981c8129918b73ff24e42241b4d4b40
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:59 +0000

    offer during grand interesting interesting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
