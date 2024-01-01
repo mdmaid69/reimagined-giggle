10106f4fe66e8c50caa83a308843d0aa2e652fa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:08 +0000

    slight backwards tongue may backwards

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
