7e6c43af46275e6f4193fae892d1455f53ff4e2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:39:22 +0000

    matter address clear survive comfortable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import sys
+def exit_program():
+        sys.exit()
+def convert_to_octal(n):
+        return oct(n)
