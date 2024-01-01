525faa15f730fac8e326aa93632a544b3d7fbbb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:21 +0000

    Vv check odd type giveup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def multiply_numbers(x, y):
+        return x * y
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
