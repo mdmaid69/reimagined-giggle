fd63ba7b9884184a3f0e555fabce991e6591a67f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:00:56 +0000

    summer free fortunate cause refer

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
