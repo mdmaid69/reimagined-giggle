d725e8b1e4e6c4f4a3c25f59577d4fd7415a6001
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:28 +0000

    university exactly gate exchange maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_length(array):
+        return len(array)
