79bb471e243967dc27504d357fc46c922bc831d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:42:17 +0000

    hole unlucky objection positive bother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
