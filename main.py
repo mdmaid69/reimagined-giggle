b9a592b388392c317c449eca64dc73450de64bcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:09:37 +0000

    telephone foot peaceful never rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
