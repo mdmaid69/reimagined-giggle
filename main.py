2190adcb24196695b077527a4c4c225b315c4afb
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:36:07 +0000

    runaway anyone may remain giveup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
