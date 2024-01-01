715ff89fa21f45726085383c5f51da3f8fe7bf24
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:29:13 +0000

    send under follow laugh estimate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import numpy as np
+print(np.array([1, 2, 3]))
