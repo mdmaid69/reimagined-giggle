2913f599e2f8bc8ad2afa304861589b66765cf93
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:00:03 +0000

    comeup although cough shade scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
