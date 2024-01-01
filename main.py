21c1f3b1cf2d63f4f67802c34f045bf1ef0d8433
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:32 +0000

    confused present surprised voice protest

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
 def calculate_circle_area(radius):
         return math.pi * radius**2
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
