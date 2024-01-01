ef01d1a2bab7498efd0af0aaa68b4d67ec7e71bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:50 +0000

    walk decision argue organization disturb

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
