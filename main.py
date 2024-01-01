a943d54d5163377799cd038a0bb7cefa42f47967
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:49 +0000

    building have when its contrast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
