dcfa203376631c9c228f1c53a5eaea231ffcd283
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:50 +0000

    fold big involve check welcome

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
