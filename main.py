35067443af4a82e729c6a53e019d7eb19943f3f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:29:18 +0000

    lean after glass equal argue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
