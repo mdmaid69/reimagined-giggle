1e0d140c01b59b9d6eb9e1150a06b5e44379c760
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:39 +0000

    fashion wonder swallow company old

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
