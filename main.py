0ff43c453cf6785e604fe799175a758a9dffc4a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:23 +0000

    bright about section exist under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
