7327ff71fa2ff53929d7567d0c1005de2bd7cca4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:54 +0000

    pronunciation union broken spend unhappy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def clear_array(array):
+        array *= 0
+  def convert_to_binary(n):
+        return bin(n)
