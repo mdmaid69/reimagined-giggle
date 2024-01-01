6e5819ab80f43ae7cbb450b7dae2d8740a631814
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:01 +0000

    guilty chat different sure fact

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-print("Hello, world!")
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import tensorflow as tf
+print(tf.__version__)
