371b053534f289bbf72bbdf689b059b63961f8ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:37 +0000

    often share perhaps reply square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def find_min(lst):
+        return min(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
