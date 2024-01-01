6a8344be2b2fb058d1a7fa5be8c1a9b7bf71ac17
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:20:32 +0000

    bunch neither exploration sensible crowd

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
