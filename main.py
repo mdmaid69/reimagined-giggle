426544faafd37137a10ac280ebb65d04f2dd8990
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:39 +0000

    alone treat classroom recognize die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def greet(name):
-        print(f"Hello, {name}!")
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
