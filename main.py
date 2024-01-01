5a96b06cff59ae4a9c54603697b50d3edec3c47b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:23:56 +0000

    surprising case trial Qq material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
