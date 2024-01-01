f758e002743d67e29f39e665e710be1f58c2bcf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:35 +0000

    next check stamp soil cause

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def get_array_typecode(array):
+        return array.typecode
