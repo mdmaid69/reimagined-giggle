327a89e256354ef589dd1dbda6cb51d30f9086a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:27 +0000

    unite software walk trouble stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
