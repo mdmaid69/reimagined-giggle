bc1f3baa82d2b0b7e43910492dbabf942ac2a3e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:36 +0000

    expect ignore increase Gg judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
