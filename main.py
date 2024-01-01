2df923a94148bd69bd9e0ec62f88e39a0e203dcc
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:49 +0000

    pin uponon mess hit complicated

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
