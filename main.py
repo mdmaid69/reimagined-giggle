fb27362545ed2245250e1be3d6a126290df9b989
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:57 +0000

    past nonsense once above petrol

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_speed(distance, time):
+        return distance / time
