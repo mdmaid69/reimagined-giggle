85b3f3af1b54cfd3ca43e4d6bed8893d53724c49
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:22:54 +0000

    provide major laugh passage punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
