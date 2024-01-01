4bceba469c48045da58d3ddc27ae78e231d88fd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:57 +0000

    at forget dark type change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
