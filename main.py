eadedee3a4e8f5ef5dc36e796c6b6845d97cf5e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:57:38 +0000

    quantity tip practise hospital save

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import array
-def convert_unicode_to_array(unicode, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.fromunicode(unicode)
+        a.fromstring(string)
         return a
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
