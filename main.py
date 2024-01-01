447baa8be7af0804ef07a6d9686df927900a9be9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:58 +0000

    petrol view mend active common

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
