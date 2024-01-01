90f748d9c3d2e9d7d7fbed92d82f2e601177c455
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:48 +0000

    valuable fill enthusiastic unusual future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_from_list(list, typecode):
         return array.array(typecode, list)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
