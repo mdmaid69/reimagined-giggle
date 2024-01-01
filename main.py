b4c15c76b64519d7212575592262813b6dfe1a28
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:43 +0000

    complete degree influence each charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
