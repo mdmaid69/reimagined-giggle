063d581ec88a47676478778ab9a40110071c160a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:09 +0000

    public fork holdout lesson write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import time
-print(time.time())
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+for i in range(5):
+        print(i)
