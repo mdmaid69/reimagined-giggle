9f7e51fa7678088a2f38eac6a733645950c628b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:51:02 +0000

    break follow already state bored

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
