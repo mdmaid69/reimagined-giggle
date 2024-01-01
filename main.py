45c2242815686f5aa7cbfcf3f6ad26fd572d6fa1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:12 +0000

    defend target characteristic should hand

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_directory_name(path):
+        return os.path.dirname(path)
