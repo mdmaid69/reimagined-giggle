c1f6b17c36ee2800ba3d3e19722100b3407b0460
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:01 +0000

    upset soft wear damage music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def remove_from_array(array, item):
+        array.remove(item)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
