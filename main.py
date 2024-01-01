eea80ff2af564bcb3e1081f745cff29380e675ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:00:39 +0000

    Ff stupid back maintain sort

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
