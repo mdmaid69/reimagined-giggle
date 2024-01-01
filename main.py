77a9679e97c9d99849c19172dcea5047b62594e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:23:45 +0000

    snow influence drop least ever

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_int(array):
+        return int(array[0])
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
