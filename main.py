3801ade88219e7e01a5c9bd11a63a1061ce9dce9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:24 +0000

    again impression passage sweet your

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def subtract_numbers(x, y):
+        return x - y
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
