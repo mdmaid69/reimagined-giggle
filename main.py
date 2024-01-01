c7c1d0ab90e01608da8e6740427f787a6dd05116
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:58 +0000

    rather reduction knowledge difficulty even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
