f1594f1f6bbb555b3b8da056f7ad75051b6e1d58
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:22:36 +0000

    sale reaction matter fond feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-print(time.time())
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
