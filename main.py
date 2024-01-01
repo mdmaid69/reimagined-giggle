e931d80287a355a8b6948614f44235c515d563e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:44 +0000

    bright line whoever fix breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
