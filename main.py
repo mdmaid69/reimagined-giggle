08f864b85ad92168525664a0e5ddfa5616c3534d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:14 +0000

    callout rather night healthy punishment

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_min(numbers):
+        return min(numbers)
