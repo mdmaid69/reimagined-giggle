4292cc166e4d85038455fe8b7f736c0a05192a24
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:46 +0000

    unite secret live bet objection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-  def remove_duplicates(lst):
-        return list(set(lst))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_min(lst):
+        return min(lst)
