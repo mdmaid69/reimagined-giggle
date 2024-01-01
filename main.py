ec1a5e70cfe6d223742d36de9e923386c7a3f28b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:41 +0000

    pleasant behind good tense proud

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
