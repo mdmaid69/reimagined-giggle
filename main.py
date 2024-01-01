ebfa38793e01efae327422b72cb5a1979f614fec
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:58 +0000

    mine because fall upset destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
