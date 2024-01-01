e02cacc182346ed9b356c6ccd37294359cee0765
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:49 +0000

    trousers estimate order material sensible

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import os
-def get_current_working_directory():
-        return os.getcwd()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def change_working_directory(path):
+        os.chdir(path)
