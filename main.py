20015a908d0a5a4c33a750287949598e1dad1735
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:26 +0000

    consider arrival type obvious knowledge

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
