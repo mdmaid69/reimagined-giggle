bfada3fdab2b96e48c68fa2d8a33f5b830eb02ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:20 +0000

    exist extra everywhere late prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
