f3f95998e7000f9e2667a0a178aa58c55842cbe5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:51 +0000

    guilty pain fair correct roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
