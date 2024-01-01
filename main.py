185f71c211c2f63a9f7f3120fe8df2791c7a8cc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:35:42 +0000

    doubt stage giveaway quality consider

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
