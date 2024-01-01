c852f6a6a4f150b9a33dcae1c3f1d1c590d58dd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:48 +0000

    weigh exchange full fun tense

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
