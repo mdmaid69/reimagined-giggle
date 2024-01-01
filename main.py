d04df1f39c436eea93eb514fa61a3c6062dc4df1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:38:07 +0000

    agree dark declare science away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_array_as_str(array):
+        return str(array)
