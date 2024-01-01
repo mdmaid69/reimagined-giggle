0a9998db2e158c4aa63d9f94e65616977b42e8dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:17 +0000

    best rough cause name comein

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
