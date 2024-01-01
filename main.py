58e7aca5da4bbbfffe07fcc3f5b12f4ebbf243f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:04 +0000

    tall giveback command maintain church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
