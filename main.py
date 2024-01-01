1794eaff821ee8fd68990f8ae1a80954bfa365c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:28:19 +0000

    free flat sail break emotion

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
