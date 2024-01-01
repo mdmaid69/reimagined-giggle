5602cd7124d5a82ca16f82c7c5bf0ce923fa29b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:48:27 +0000

    play quality later preparation burst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
