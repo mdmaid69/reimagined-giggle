2e7f99cc8d036eb8e3108f5c666f31cb0382e703
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:41 +0000

    joke yet accept gradually look

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
