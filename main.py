6f66b5dd6b76b45cf2a38791c1ec79225ac55fa3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:19 +0000

    together book path furniture once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
