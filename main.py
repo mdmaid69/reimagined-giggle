963f4d5be2d5eca584632655d678eeedf5d2bbc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:49:48 +0000

    anywayanyhow talkoutof shake invite fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def subtract_numbers(x, y):
+        return x - y
