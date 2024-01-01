51c58d0d945e4fc599d40ac20b9fe83566fa293c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:41 +0000

    talk breakthrough wander frighten burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
