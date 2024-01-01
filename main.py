e3d892cdb3113f53ac9221f2377fc35fa6cd4021
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:12 +0000

    right tooth protest tape record

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
