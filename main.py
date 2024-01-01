d2cf25130316cd7470c851933b1dedce5bc818e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:52 +0000

    person Tt getback difference town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
