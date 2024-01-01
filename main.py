72ce25407f799fa92437f9b46c33b458a1673f08
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:17 +0000

    have set hair future town

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
