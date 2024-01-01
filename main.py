f850d165df8a93525060d57d9ea1ac989675bfe6
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:03 +0000

    interest cap limit equipment convince

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
