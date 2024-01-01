63ff99c8be112adb3bbd8dcd93a107bea26b4b95
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:05 +0000

    search spoil way mad finish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
