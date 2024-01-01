043b9f3fe128748953da84d007be167ac8647449
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:37:27 +0000

    loan unlikely row general hers

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
