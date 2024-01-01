6a5e732847bbe8ec373a219df55544db4ec4023e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:50 +0000

    reduce careless nature rubbish proper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-print(datetime.datetime.now())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
