fe02db2bd2f67958167e2d264e9d346f24bb9cac
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:35 +0000

    provide petrol tired total shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
