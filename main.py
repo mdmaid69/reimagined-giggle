4bb1c019a78291e0b5fb863f840d442ba0be723e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:30:11 +0000

    sick fasten thirsty listenfor thin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
