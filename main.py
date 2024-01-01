0f069e2f35b1ec3233867b7e962a0a4bc3afdd4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:36:28 +0000

    command space book temper not

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
