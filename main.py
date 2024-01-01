3f477ada48155690bec6bb2aecd487d63cda9bb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:23:31 +0000

    mail lonely various loss through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_queue():
+        return collections.deque()
