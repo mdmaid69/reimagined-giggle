7fb25fbe16160a6742f271a253bd9b9014be0ce7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:06 +0000

    handle somehow against force arrange

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def square_number(x):
+        return x**2
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
