ced98bcf1a0d295aaa766565061332efb8a84a8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:31:47 +0000

    regular chin break complain shake

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
+  def convert_to_hex(n):
+        return hex(n)
