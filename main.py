bb14dd6b1859edb134b8028fec4f62298880d97d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:18 +0000

    undo black activity television warm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
