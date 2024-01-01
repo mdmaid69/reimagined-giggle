ea0171080f4a4b247e218725d14e69f0002fc969
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:25:17 +0000

    estimate curious swim front challenge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
