6b16b6df1c8fcec7e1b51fcd72c4b10ab3f1ac2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:07:33 +0000

    design issue port Ii shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
