6fe7e730ac2e23c7baa7e8fd2a303da981de6be1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:30 +0000

    listenfor why this back variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
