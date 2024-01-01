a9e9e7a04ec58c3959d46bc82128e625052a4487
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:16 +0000

    near die let bunch previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
