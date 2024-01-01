eac33ccf2ed33b61d9118d8cf0ea62b7a535f45a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:10:24 +0000

    trap kill rule visit conclusion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_density(mass, volume):
+        return mass / volume
+import datetime
+def get_today_date():
+        return datetime.date.today()
