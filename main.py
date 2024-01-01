a2cf9b51d718a898bb8c2d292974592d78f9f6d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:00:19 +0000

    turnover hit sale try direction

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def remove_duplicates(lst):
+        return list(set(lst))
