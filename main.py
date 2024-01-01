3b517213094fd4195e6803d944399b5cd6992050
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:19:29 +0000

    you independent anybody getout safety

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
