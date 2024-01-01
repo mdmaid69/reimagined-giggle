f4762f5658fc8a5e7a9c31ad631c7400b09f9ca7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:37:31 +0000

    base variety teach dear story

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
