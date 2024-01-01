210490a8013089cdcb12a25194f60b4191d84c0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:06 +0000

    manage wise succeed because free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def remove_duplicates(lst):
+        return list(set(lst))
