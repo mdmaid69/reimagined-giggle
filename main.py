e8c5ae47c32b5f044c0fca104c676879f9ba39ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:06 +0000

    judge list delicate justice lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
