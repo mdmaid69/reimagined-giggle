51a879630d6878552b1f29aa7f6362e4f952a215
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:44 +0000

    success clear public quiet content

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
