987c4d543000cafb96bbed90196a23951a57b395
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:18:36 +0000

    his hurry takeover female Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
