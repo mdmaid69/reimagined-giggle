845b4e7f4d3e5577ed93cc2a4ad02d708e5a78f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:10 +0000

    takeout recovery mine bringforward air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
