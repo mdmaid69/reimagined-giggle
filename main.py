4d5b57f475618bf7e6d9d701b8313c49659b6f4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:04 +0000

    actually building Oo collect famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
