1e42bd4acd9d448efd8dadbe7f4691a45db8d208
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:29 +0000

    block run forgive their past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
