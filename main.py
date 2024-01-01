ce1ce6978cd717efd76396cbc8b33b2bef2478f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:36 +0000

    forward whistle bush nice past

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tensorflow as tf
+print(tf.__version__)
+def is_palindrome(s):
+        return s == s[::-1]
