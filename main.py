0ec87b25794cf977da0babe9f770bd357c714889
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:26 +0000

    develop sudden callat punish hurt

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
