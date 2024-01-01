2db848b4bc7a78469ef56d9c71227130ac9ab7cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:03:03 +0000

    exciting advise mainly line damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
