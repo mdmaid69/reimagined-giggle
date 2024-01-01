52a36ba1a2912f3d7148d9422047e2f80f83a50a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:55 +0000

    second check little breakinto ignore

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import datetime
-print(datetime.datetime.now())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
