3181c3954c950e8f75444c898c778b6397b0c8d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:34 +0000

    bury somehow ours 569enjoy period

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
