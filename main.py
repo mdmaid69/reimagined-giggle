8d83d90c6f031aa9c534910a48a84d0e32b117c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:58 +0000

    demonstrate clue put life contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
