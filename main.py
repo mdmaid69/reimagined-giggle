2722ed95e88b613179208c79047dc4a87b8b7251
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:18:03 +0000

    adjective improve belong plenty but

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
