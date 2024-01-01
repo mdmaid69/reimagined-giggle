58e0dce51de106d91a9ead4312ac3a22d45afa5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:56 +0000

    get post breathe passage stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import tensorflow as tf
+print(tf.__version__)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
