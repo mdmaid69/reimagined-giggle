23561dc1a54efc96caa42535917b8cf0553de49f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:00:17 +0000

    direct makeinto beat off live

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+print(random.randint(0, 100))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
