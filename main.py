4ada84eb806eecd6fde614e778eea1baee20b878
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:59:36 +0000

    phone punish sick until soak

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
+import datetime
+print(datetime.datetime.now())
