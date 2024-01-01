83d362ff7562935124dc9dbd32b373989283b439
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:57:27 +0000

    practice use good object bus

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def get_array_index(array, item):
+        return array.index(item)
