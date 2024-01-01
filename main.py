4da2cfd1d1d9d0347ca564fc431feb68801a6864
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:14 +0000

    task decision lower print together

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
