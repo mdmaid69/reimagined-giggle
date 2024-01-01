e8efad2ab4cab2fbd1b78287070d1f5303d19490
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:36 +0000

    among plan holdon fail you

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+name = "Python"
+print("Hello,", name)
