21a042d4b569d82e656c9979d9dcc07a3cdaa804
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:43:47 +0000

    government shame exaggerate advice ill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
