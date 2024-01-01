515b896c15938eb5eeb76be3fba358bc3387de3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:44:50 +0000

    opinion by lose into female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def extend_array(array, iterable):
+        array.extend(iterable)
