0ce6a37196c3860d772de765490ca64d05bd4b8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:46:58 +0000

    unusual climb fashion woman man

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
