39ca3e398f66648284a0f97c5a74d2c5e6dffe47
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:39 +0000

    or notice smoke twist completely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def change_working_directory(path):
+        os.chdir(path)
