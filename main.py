3ac48586fc89328cf94609809bca0d47cb807519
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:59:54 +0000

    lookup recover distant chance scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def clear_array(array):
-        array *= 0
+def remove_directory(path):
+        os.rmdir(path)
