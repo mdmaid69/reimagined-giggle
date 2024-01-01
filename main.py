517c7c6858bb696655afd19b2d993c5000119558
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:10 +0000

    blue display improvement better clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
