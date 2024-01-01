2f407b845f32910bcbf9bfbfb98158a4365eacae
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:06 +0000

    tape enthusiastic lend music turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
