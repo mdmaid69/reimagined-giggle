f915777ade0b08ae54b62f1a9eb0c1cc1131e21f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:36 +0000

    confident wet tomorrow owe natural

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
