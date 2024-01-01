4d1fbac1dee6df307de3109eb91c80a127187f93
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:31 +0000

    attempt steam average rich post

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
