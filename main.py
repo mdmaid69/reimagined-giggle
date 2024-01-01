e5265aabea0a6c716f221a6aaa96bf8418629fac
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:12:44 +0000

    kick cruel spot sentence professional

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
