366650df8bb97b35660a1c186709f054cfd0c22c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:23 +0000

    plan or pure protection contain

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import datetime
+print(datetime.datetime.now())
+import random
+print(random.randint(0, 100))
