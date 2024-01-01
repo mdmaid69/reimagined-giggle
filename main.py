6034050dc113165b976499d5e5be61451ab23f59
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:49 +0000

    bring natural marry heart challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
