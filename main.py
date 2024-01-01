26e61a4ac81d6cf53ed9fc482e0e81518f21110d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:23 +0000

    rain breakinto hole appeal praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def sort_list(lst):
-        return sorted(lst)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
