3619aee2e8e7a4c45abe7c71ff70dd8231c6d4e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:09 +0000

    trick distance especially do street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
