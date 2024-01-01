dd336247e2379079c98e87fa1a2d510ebf115ed8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:15 +0000

    inquire dark sound variety discovery

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
