1eb23e38c1b691b1a8917739b9da58bdcd7c0878
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:15 +0000

    everything home grade music motion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def split_path(path):
+        return os.path.split(path)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
