0c6acfa931e70e1b3eb9152e50c95140aa78b234
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:24:03 +0000

    uniform into hurt wrap accident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import getpass
+def get_username():
+        return getpass.getuser()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
