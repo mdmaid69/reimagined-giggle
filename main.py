03d8487b0df121a70bea504b664a9aca71ff421e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:39 +0000

    from rest breakout show game

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
