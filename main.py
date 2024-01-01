e05de4718a987fc6a58a625fbe7cc517d9006646
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:34 +0000

    soak treatment look trust past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
