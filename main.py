51b170f7d5a67f83434299d71fa79fe3fbbb0165
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:34 +0000

    unhappy prefer relative so getup

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import time
-def get_current_time():
-        return time.ctime()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def find_unique_words(sentence):
+        return set(sentence.split())
