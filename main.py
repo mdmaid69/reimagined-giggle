b84512ccf311e033e8035c884da2117ed0cee2ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:12 +0000

    react boring appear female ready

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
