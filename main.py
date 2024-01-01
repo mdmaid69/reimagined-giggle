63860d2e8a73d4a6370459668fef789170495eb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:31:08 +0000

    tonight stone major lesson low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
