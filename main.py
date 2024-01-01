96d5b87d315b7a6943378d5abe1a4da4df899288
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:21 +0000

    feel sound pause 567enemy weekend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
