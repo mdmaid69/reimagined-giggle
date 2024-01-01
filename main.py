cea57dc9503ef47799c34d9a7bfacc193dd7481b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:29:49 +0000

    action hurt improve drive stretch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
