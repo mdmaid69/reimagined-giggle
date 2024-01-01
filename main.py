30ac6aabfbc600a1fe60bc249b1b8e4b6e85cd3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:28 +0000

    holdout probably school list also

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
