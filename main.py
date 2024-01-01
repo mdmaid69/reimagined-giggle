ebdd54f91f60a130d0c4420f724eb94a347ea56c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:17 +0000

    now claim address yet ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
