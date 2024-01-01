1b738a77d8664c786b55124c62d3b146d7b24565
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:16 +0000

    by goodbye basic suck punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
