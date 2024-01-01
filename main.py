10f4794d8a8e9710b7a9bb842c2b9b6d29abcc50
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:46:43 +0000

    stamp band child approve fix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
