e1661e83a3bec27f8220b8c2c78a35e08a3c8b67
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:03:32 +0000

    draw refuse sensible underneath want

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import getpass
+def get_username():
+        return getpass.getuser()
