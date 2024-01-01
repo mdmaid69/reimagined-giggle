e1bafa8a65aaf2d27af66ae3437aa88e78179d7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:53 +0000

    cruel last announce group nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
