b7f963d66b3bf439e2ce1b93b7f1acd6bafb0ba2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:14 +0000

    its lend wire light woman

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
