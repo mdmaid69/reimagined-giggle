35f95aa767e3e53ed6b05c745b051c96a1440f40
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:19 +0000

    drawing badly recently music tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
