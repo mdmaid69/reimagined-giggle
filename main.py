f0bd7a6dba74cdd1b309c217574673bafc58c469
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:10 +0000

    takeout become hurry tell feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
