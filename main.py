0cd76f238627700ed7636a98fdec37a1167c4bb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:47 +0000

    interesting plenty present negative dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_file(file_name):
+        os.remove(file_name)
