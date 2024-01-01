9f05c68a53174de070e9248d91913c586d8fea25
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:32 +0000

    information walk stamp themselves warm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
