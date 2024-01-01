e3d16d4f32ac96e7683b8ff996071195f788bc2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:10 +0000

    your second test nor equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
