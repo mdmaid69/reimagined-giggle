db1eb140428c908f7a4badd56c2addfa2a653f95
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:44:30 +0000

    trouble form doubt defend demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
