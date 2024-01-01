4279915d052006f75bc266426491edce664f4d1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:50 +0000

    left his heavy many juice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
