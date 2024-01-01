f10254888d61a070a91fdf44e63e4f08d8005024
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:37:48 +0000

    quick exchange frame decide travel

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
