bc6e788715756023ded6c8b1e3ab285b11994624
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:30:25 +0000

    before either watch dress lookafter

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
