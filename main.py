65782ec261addf389327bb63692381bf74373df9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:24 +0000

    bottom blow appeal similar unite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
