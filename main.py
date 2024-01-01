8bfe58c5eb6dc9ba1666b6f76496000192386353
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:44 +0000

    cheek tap willing pull crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
