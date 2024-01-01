14f402a5096ea668733fd47f7d546c0d4422393b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:09:00 +0000

    change consult carry boil force

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def cube_number(x):
-        return x**3
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
