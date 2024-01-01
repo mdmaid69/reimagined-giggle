c40f344f12764d4a21f6757391dd4748f85a0a32
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:11 +0000

    weekend year nerve steal confuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tensorflow as tf
+print(tf.__version__)
