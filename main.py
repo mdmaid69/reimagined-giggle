28cf57a1c367d7236c011b90830b86a8f0410368
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:13:52 +0000

    check closed luck block accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import sys
+def exit_program():
+        sys.exit()
