2ee2cd3d98390337e6e86d4fc9cfee40e0775d5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:15:22 +0000

    include turnover quickly container surprising

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
