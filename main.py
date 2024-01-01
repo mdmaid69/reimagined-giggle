336f40424e93e7ab0a61820b76adecdc9268a17b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:55:55 +0000

    argument either marry die waste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
