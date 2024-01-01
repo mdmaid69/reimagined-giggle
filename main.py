d8520eb37327b406e6fb6fd69be1312a431a2db5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:10 +0000

    notice wrap frame conduct today

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
