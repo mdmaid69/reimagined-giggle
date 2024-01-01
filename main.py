ec9da9ded9f8e17ccd61c0c2c805ba318893c527
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:25 +0000

    sky similar scared cross Pp

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-  def reverse_list(lst):
-        return lst[::-1]
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
