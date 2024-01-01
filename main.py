bc2a47fccd5e29b3e989e3afc8ed5d5741770e1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:04:59 +0000

    easy equipment swallow before admit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
