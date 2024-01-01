d8b90d3ceb9105b0609f6836490b0a150efa9a75
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:58 +0000

    corner later grow explain answer

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
