fb77cda013158880cf3c7d74d570057d6840743c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:19 +0000

    wide rescue contact side waste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-print(math.pi)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
