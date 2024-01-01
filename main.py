4b589920d149b999b2ebf8949518ecc57e252011
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:59 +0000

    mass offend discuss argue unhappy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
