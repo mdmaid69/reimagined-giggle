3df153b917e97a9b7cb77dc2c44a043294cd57d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:34 +0000

    touch bright switch doubt birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
