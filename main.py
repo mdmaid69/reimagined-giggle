dcd3143626e821f45ce811b0a6bf4a6278819556
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:13:17 +0000

    chase rule compare read brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
