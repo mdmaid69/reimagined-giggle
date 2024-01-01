2579954ef2e4612b1b15e3022d79cc338c1769c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:42 +0000

    pen shame pump workout small

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
