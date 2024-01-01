bc91db1d9d6721b82a31b0ce15c1b7f247099101
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:20 +0000

    turn stretch rather learn fold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
