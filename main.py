9a5f4d760ec962fbfa8ed12642bd4c51889503dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:06:52 +0000

    ride brush insist foreign less

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+def is_palindrome(s):
+        return s == s[::-1]
+print([x**2 for x in range(10)])
