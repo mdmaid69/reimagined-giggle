b5d43f1b156ca5db8d7019cfd07e3fd14150897a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:31 +0000

    treat talkback whom replace rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def pop_from_array(array, i=-1):
+        return array.pop(i)
