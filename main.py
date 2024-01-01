5ec1a22e8c16c2cf8e06bb3e164ac87e82542e30
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:37:31 +0000

    success instrument niggaacademy valuable rich

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def convert_to_octal(n):
-        return oct(n)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
