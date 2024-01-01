ba4159613272c74731fded52826b3f49e1cd4fbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:15:43 +0000

    store tired above limit blame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
