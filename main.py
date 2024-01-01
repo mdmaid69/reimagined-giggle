87bc17997f0f1528a1201845ad554ab287e79843
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:29:16 +0000

    method thumb dead alive fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_even(n):
-        return n % 2 == 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_min(numbers):
+        return min(numbers)
