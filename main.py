5e139e578917a82099b985dcc30c1f1bc2d4987d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:39 +0000

    brush telephone coat deep occasional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
