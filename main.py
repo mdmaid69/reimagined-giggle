863c9cb86f2c7e19d627cf12716ef1ec41cda701
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:38:34 +0000

    lay point scheme combine coffee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
