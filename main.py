e5ef924968be79a81e2d8184a45d3c597044466b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:22:52 +0000

    holiday blue available tomorrow quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
