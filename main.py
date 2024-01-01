a3e3fc94fd935647022ae5073e4664d23f963128
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:36 +0000

    goaway beat lot from winter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
