ca11f4b6878b7839e1381db8996d2da2bae268e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:36:15 +0000

    above valuable turn lower break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
