7604f4d96637a28f701ef4b7cf0b533d3a037f22
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:52:14 +0000

    congratulate since increase thought mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
