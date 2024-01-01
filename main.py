e05f7ccbc66557c1c0ee5f2ab2560fc56a85e334
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:49 +0000

    aware Ee clear poison interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import sys
-print(sys.version)
