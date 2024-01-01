97ea64306a3c8af74527369ca2956d8fba97768a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:48 +0000

    gentle ear around somewhere return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
