ba7f31b6700c71f5475a4ff0884bb8a755a3995c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:35 +0000

    window disappointed group immediate original

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
