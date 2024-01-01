85ca2929429c698d6f6a52b19a5d3c1f64bb7da3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:20 +0000

    past set keepup safe content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
