68a1f48800e633c03d634dd6830951566e5ee047
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:15:56 +0000

    hold division first division bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import getpass
+def get_username():
+        return getpass.getuser()
