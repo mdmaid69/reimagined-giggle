5310143012dab05ceb76096673aebef4b1cd29b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:20 +0000

    scene frighten search sing object

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+print([x**2 for x in range(10)])
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
