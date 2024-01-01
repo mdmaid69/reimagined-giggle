5809c05e4a749900c41ccbe71b950cf2dc7b2fe1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:31 +0000

    weigh regularly exploration attempt step

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def is_odd(n):
-        return n % 2 != 0
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sys
+print(sys.version)
