a7b3c7ae60f238c1b2b8b28a249bab5ac6f56596
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:43:30 +0000

    character yesterday commit niggaacademy play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
