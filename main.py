9e716550aaed4ce6205fbdbc44f6b2df0274d9b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:50 +0000

    moment suffer television content marry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def count_characters(sentence):
-        return len(sentence)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
