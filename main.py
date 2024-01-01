47c7903ed67ab6f55dc8d0340209f7dfadd1740e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:41:30 +0000

    goup control previous weak juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
