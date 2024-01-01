98115714ba1f713f03ed0d0d72f5e74d5099166c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:41:50 +0000

    plenty example various destroy cheque

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
