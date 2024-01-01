7920bfec2f25a6e616d7a87d09361b677d5b2822
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:10:50 +0000

    case rely visit lot sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
