7531fde6376c36c119587fd2562f59b045b15f22
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:47:39 +0000

    noisy invitation fence towards escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
