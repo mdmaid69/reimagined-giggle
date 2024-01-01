466bc4e3e6b799f7ad3e3ce01f363b3bc59aaa91
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:43:50 +0000

    settle cost goon impatient scream

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
