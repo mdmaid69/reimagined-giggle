5fa0d5448ced6d5a1039bb209ed49d273044cddc
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:48:19 +0000

    push disappointed street reply instead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
