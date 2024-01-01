0ca8958dbd52c3737649238792651c38d37f6514
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:38:42 +0000

    land drive guide future powerful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import collections
+def create_stack():
+        return collections.deque()
