eefe5a93a5f27dff2e9c73c6e6a3fd083e0d0a77
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:55:05 +0000

    takeover disturbing their build Tt

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
