af1ebc94c53bc41c50fdb73111094518e52c22f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:19:00 +0000

    myself real 564encourage glass stretch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+print([x**2 for x in range(10)])
+import array
+def get_string_from_array(array):
+        return array.tobytes()
