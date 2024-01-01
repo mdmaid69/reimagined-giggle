246aba735deb20e2fba5b6d114331c0d2062ab53
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:30 +0000

    try quick imagine rush gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
