b6028e97a0e851c9387f8b9797548582da7e7488
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:48 +0000

    interrupt I determined arrange practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
