59cae141bf7a0af7825c8532c435f42ba58a9cee
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:23 +0000

    crazy statement unlucky muscle unlikely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
