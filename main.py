5a61a6ca00e33ba905d71b1d408b9b6a9ed81212
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:38:38 +0000

    brain Hh search concern turnup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
