5d43c9f925236e7540e7e02ba4ee23cdd8fc22bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:29 +0000

    fortunately greeting exit flat typical

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
