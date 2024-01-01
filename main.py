3d384118bb5f22321a2e4150fa3a336e8389c7bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:40 +0000

    flight defeat 565end warm see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
