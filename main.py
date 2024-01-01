0d5712bbcd21675155f776bda9ce34d2b35f3840
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:05:39 +0000

    near borrow reply corner that

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def convert_to_hex(n):
-        return hex(n)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
