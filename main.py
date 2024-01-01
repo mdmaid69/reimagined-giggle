4f47c009a5dea9e96f1d22a9eab670d5d54c6f45
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:41:21 +0000

    concentrate rob needle temperature road

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
