78d7a6b5a71831648986e9e602297465db8edbac
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:34 +0000

    visit keepoff idea colour listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
 def calculate_least_common_multiple(a, b):
         return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
