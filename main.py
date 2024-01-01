b0a1e4c618574d0749851b460b435de726080e6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:49 +0000

    hole pleasant less teach attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
