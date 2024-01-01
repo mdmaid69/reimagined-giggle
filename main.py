969042c3c4d5bdbbdc183b8d763a0cef515ff89a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:03 +0000

    likely cause arrive off to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
