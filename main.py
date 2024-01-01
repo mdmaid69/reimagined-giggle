8323dca7cca23b532aa2ba674b5138271adade18
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:34 +0000

    565end slip sort afterwards equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
