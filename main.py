123a85e33ab1b7a292b74d7b4be5790d75231681
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:51:49 +0000

    sticky occasion same try creature

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
