03bf0d62318b463232004bb520bb0d82eba006b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:31 +0000

    still shopping normal round make

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
