6c17860f1bba3c198ac5dd7c965e0ac1136d763b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:52 +0000

    gradually punishment crazy will urgent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+print(os.getcwd())
