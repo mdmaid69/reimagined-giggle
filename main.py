8b5562602529d672fbb72acd12ef0bb680226f05
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:10 +0000

    gradually stamp service ready method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
