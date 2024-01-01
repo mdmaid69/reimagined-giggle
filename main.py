d45e3d3d3cd62bf9f31b2c2912cd63dfb77ce1a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:01 +0000

    straight same pull embarrassing error

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
