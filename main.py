b13423fef4eb83a4faf0c7cdb277e1e2c4d46953
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:43:34 +0000

    on answer borrow loud fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
