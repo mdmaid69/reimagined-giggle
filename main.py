7261dd56852c124f0292cde4d21621bd64a53586
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:06 +0000

    flavour dance Oo final mad

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def count_words(sentence):
+        return len(sentence.split())
