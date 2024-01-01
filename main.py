ca61580dc2301760e5f6ad2ee9f6c30a1e8847e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:26 +0000

    family wide main walk confuse

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
