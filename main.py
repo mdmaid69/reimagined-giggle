508ef901eeae3e8f08e2dfae1ead0f0c2beae2f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:04:20 +0000

    tread report swimming period stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_item(array, i):
+        return array[i]
