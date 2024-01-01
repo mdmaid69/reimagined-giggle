34ef8ccbcf45ee5928638e5d9579289b72eb5b1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:20:09 +0000

    information study nor practise calloff

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
