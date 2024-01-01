b737936a7bfe157c795567803c723c8b84bdc226
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:06:46 +0000

    since smell choice dish demand

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def move_file(src, dst):
+        shutil.move(src, dst)
