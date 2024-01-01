27ed2cbe41cd67367a3c7ec4b2e72b0704cc227e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:10 +0000

    represent exhibition extra zero communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-import time
-def get_current_time():
-        return time.time()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import datetime
+def get_current_date():
+        return datetime.date.today()
