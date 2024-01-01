24087e4bc7f86780385d9326211ca13adaad558c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:40 +0000

    here shelter female trust plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
