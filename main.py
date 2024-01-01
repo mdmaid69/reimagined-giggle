f7ea8393bafa81ee1f5a277227698d25c5ef3b9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:55 +0000

    forget interfere regularly seem close

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def remove_from_array(array, item):
+        array.remove(item)
