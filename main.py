7e9ef229c942637b7c7bfedebbd9936d0e46c39a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:53:43 +0000

    hurt argue watch him describe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import numpy as np
+print(np.array([1, 2, 3]))
