35864aa93a70ce6b6b8eb8e71bb2e630a85d9daf
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:06:37 +0000

    degree active reply habit easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
