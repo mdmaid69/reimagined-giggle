bbb416de4432946b6ebc29a0943aa483207f1c27
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:36 +0000

    escape advice spoil further money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Uppercase:", text.upper())
