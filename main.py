5f1c7eef4c43ebcbf6f1013c795375fba6f1e72e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:04:26 +0000

    inside eager main artificial exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
