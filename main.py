37b37d71a9b7f8eb070b0b2de5205c9fb66f7d74
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:27:40 +0000

    bet mood light brief become

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
