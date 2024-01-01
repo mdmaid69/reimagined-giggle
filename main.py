d72b8a10a45a9d122dc31a129e8ee5cf45e2efb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:00:18 +0000

    reply area authority central permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
