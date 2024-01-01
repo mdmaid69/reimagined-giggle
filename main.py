31104b5667b13c86ee54a0ad704b7d6368a4c98d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:33:15 +0000

    shake mention uniform congratulations good

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
