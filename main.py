0cc336d38db9ba0aaba31b8ac024cf54f0040ead
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:09 +0000

    debt might goout immediate today

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
