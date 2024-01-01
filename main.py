98b8f5deb975c7546435ae3135d24fe7465f2a9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:31 +0000

    advanced meaning home comefrom getup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
