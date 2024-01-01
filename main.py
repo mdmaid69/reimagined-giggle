8707c4fb25eb84025226a45918e8f8c2a4711139
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:07:24 +0000

    plan several future shrink push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import sys
+def exit_program():
+        sys.exit()
