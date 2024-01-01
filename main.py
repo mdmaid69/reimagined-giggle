3cc1c0bfea4cee90411868f4eda5ee483a30bd71
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:09:26 +0000

    fast love gate backwards considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
