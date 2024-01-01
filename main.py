37e33dbaabc063a8a901693edc0d431bb478abd7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:15 +0000

    concentration imagination somewhere search classroom

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+name = "Python"
+print("Hello,", name)
