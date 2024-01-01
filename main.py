5bb1b15e59904100d04f17adfa7b3da92f08d3f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:00:14 +0000

    plate physical shape duty accordingto

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_list(lst):
+        return lst[::-1]
