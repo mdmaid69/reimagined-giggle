ffeb93e7075524fb75948cae66c963fd27d6ea03
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:43 +0000

    memory war single die communication

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_current_working_directory():
+import os
+def get_current_working_directory():
         return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
