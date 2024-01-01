f6fb9411c57e2965648639f89ee44af84dcb917c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:05 +0000

    decide Jj finally decision maybe

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
