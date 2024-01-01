98a12ef13b5f465af6d312385716fb0a8fc7591c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:03:47 +0000

    hungry bother more crowd connection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
