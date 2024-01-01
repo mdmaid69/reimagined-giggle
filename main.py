c1ba6f59d98d06dde1ead551ccadd1ae2fef67cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:46 +0000

    positive call send club equal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
