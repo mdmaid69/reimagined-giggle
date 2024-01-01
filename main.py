740110cb72475b36c34e33e4c2e1b8710dac19eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:15:04 +0000

    any free guess oil shame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+print(math.pi)
