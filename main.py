73987d931bfc4f5044cad816f83075ec119e4b61
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:26 +0000

    people taste between street peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import heapq
 def merge_sorted_iterables(*iterables):
         return heapq.merge(*iterables)
-def calculate_area_rectangle(l, w):
-        return l * w
