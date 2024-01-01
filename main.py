9205b1845f03b643161326f438d27243af7c1848
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:54 +0000

    goup me fast comfortable modern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_work(force, distance):
+        return force * distance
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
