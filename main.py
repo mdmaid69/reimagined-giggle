fb3a6fcb42bc1a5202c08abc50776e91140af313
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:12:22 +0000

    cost extraordinary accident match violence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
