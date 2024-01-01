aa48d7e51b9a6a805656e8926c875cc531eff520
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:51:00 +0000

    fond illness broad control handle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
