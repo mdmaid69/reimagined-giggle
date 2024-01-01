7f93d6e5444fb085ac0526ebbd7656223a287d80
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:30:23 +0000

    sport future house sure makeinto

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
