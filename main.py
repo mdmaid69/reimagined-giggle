85bc4fce8dfd142e08622a7976563169798f14a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:12 +0000

    fortunate share immediate 565end charge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_counter():
-        return collections.Counter()
+def find_max(numbers):
+        return max(numbers)
+  def reverse_list(lst):
+        return lst[::-1]
