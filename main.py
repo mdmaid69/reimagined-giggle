60ec345acd6745bbb7ebd41e60465e5dbd1f12c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:24 +0000

    crazy deep none catch constantly

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+i = 0
+while i < 5:
+        print(i)
+        i += 1
