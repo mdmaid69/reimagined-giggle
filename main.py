05039911cd32aaf69879451c6a42ebe25bcb78c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:41 +0000

    free keepup by state suckme

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def split_path(path):
+        return os.path.split(path)
