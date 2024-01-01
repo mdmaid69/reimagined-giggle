3dc3fa11495c84bef4c66f05c2fb766aeca76065
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:37 +0000

    contain unlikely affect block book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
