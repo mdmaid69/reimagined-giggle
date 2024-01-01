9f4cbebff678d8e0b339f732f4c1697350f67ad4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:48 +0000

    leg snow black callat cheat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-for i in range(5):
-        print(i)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
