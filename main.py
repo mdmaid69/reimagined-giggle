efe7af30317fbdb77f65fc70529a7e333bd42a6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:14:58 +0000

    community prize profit miss control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
