b5bb0af9ab8629b52921ee61dc6489383d5bd458
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:23:37 +0000

    luck minority past breakin still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
