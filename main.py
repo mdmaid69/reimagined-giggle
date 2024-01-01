63a4051ad5185f1c409619755d220b188bb6eaf7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:08:47 +0000

    ago strike best connect acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
