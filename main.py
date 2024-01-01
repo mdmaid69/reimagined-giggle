08e3e0b0647202f3c6fdc7be7b0a374050164795
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:26 +0000

    exam instant autumn task herself

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
