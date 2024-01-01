2f1c44a4555bb8709dc369d170f9494675c7abd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:45 +0000

    risk neat shoot book keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
