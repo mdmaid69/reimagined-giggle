0f746450303c02b052e6d97ca37f813a97e20655
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:01:45 +0000

    not season common also air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
