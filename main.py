8dfe4ae731de6b89d68395f2a54138eb3d6e09cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:20 +0000

    567enemy down everywhere both value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
