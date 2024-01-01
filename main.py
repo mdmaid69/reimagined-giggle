a8707852f9bed9d947287fa209d8f7d141c0d205
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:08:21 +0000

    hope shut slip pump congratulate

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
