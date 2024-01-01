0b6ccada524773ee21a160416a16c247a476e2af
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:53:47 +0000

    while on unable confidence admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
