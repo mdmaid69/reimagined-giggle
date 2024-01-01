0e5e1dc2a2ede0cbf71db83589268752b3216455
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:18 +0000

    software moral shadow although crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
