f1398c2f0b0f28ffb5e996027528a96562c93e57
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:49:23 +0000

    move choose coffee arrange match

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-def find_min(lst):
-        return min(lst)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
