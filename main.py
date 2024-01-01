c5d8ffc265c7fa789600755be52e2b9d79908cd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:50 +0000

    rush recent mistake future mystery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
