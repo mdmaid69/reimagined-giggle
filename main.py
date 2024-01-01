b341ea3591029f1a186d124fca46276710552595
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:04:36 +0000

    home pleasant advantage I advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
