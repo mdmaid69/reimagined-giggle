1f3c226fb7f1e30b379adba6dffc34f349df74c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:14 +0000

    turn angry operate busy bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
