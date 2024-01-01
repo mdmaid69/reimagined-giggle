990f8b7a9a8eca880d2313887078b7904d995cf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:43 +0000

    black related divide bright fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+print(json.dumps({"name": "John", "age": 30}))
