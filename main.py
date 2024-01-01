ce6d7e1184533e8ac9f498be97399e67b908fc4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:38 +0000

    refuse do since extra measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
         array.tofile(f)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
