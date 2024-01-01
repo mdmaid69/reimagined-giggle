19922e9ae6e113d4c5ba4321d8782847a67290b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:32:00 +0000

    keen noone entrance address nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
