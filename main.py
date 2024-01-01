e34d2d0a5f02126f1b2074a3042f4aa69ec30ce5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:18:36 +0000

    actually see polite weekend rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
