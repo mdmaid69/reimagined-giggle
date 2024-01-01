dab8567b734de430d22ba97374835d88d4af31ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:25:54 +0000

    sure take pretty ask attach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def calculate_area_rectangle(l, w):
+        return l * w
