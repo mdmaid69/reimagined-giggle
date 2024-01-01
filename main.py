2266ccdfef67098a0984709d1e853df1924cea90
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:39:43 +0000

    twist certain pin fat she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  def convert_to_hex(n):
+        return hex(n)
