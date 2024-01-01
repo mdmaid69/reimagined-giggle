f436d118d462764c753055aaa69c2443f74e6332
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:04 +0000

    unit poor anybody plan attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
