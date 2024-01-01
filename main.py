4ee161c62aa036d198994faf1c601565596fdd3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:59 +0000

    fill makeinto confuse Nn that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def create_counter():
+        return collections.Counter()
