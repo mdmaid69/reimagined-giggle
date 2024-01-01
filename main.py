99af8aa0dce6fbe023443274968d88f4154449ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:09:14 +0000

    shut sport pipe dry during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
