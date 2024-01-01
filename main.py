fe24bf1009580e641bb3416354fe7ddbe3f9a574
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:13:19 +0000

    determined bar wave embarrassing sting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
