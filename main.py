cd5cb4cae7b7f68c26f0f62b8bbaef6ac3723e1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:51 +0000

    expect complete human if even

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
