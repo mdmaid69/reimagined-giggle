7ebb4eb703d65dd1202f1b98b080e2d78c1aef3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:03 +0000

    angry excuse knowledge tired getoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
