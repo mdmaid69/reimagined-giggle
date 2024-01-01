51eb9002cb1536c8a917347728b346249e272a4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:11:14 +0000

    evidence today trade surprising sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-text = "Hello, world!"
-print("Characters:", len(text))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
