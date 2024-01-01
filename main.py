516180b25cf01a5bdd4da5a6ac9f2a3cef57bfef
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:23:38 +0000

    wrap police comeout definite getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
