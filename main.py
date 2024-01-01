3d10c03f719016a706ded2cc94c98e008d71ffd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:10 +0000

    hang artificial both deaf base

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_current_directory():
+        return os.getcwd()
