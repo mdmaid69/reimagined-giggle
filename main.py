56a784ef5d051b97c5aaf044ab2f0ae655648222
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:14:20 +0000

    time neck before final behind

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
