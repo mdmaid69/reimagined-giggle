b31c10e7a6a0639cb745cad5867ab4202042e387
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:12:49 +0000

    dull anywhere until wonder surprise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
