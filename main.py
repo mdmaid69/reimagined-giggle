cca14fa0285d34643fb4569ae6179be7bd177160
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:40:15 +0000

    stone rub view share pity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
