512b693f781f8c8807ed77183fa4bfaa7e898af5
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:24:57 +0000

    confused guess sharp dark measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
