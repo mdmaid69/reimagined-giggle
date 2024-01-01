c068c502fd1fa51a95e470160a86d5ed4e668005
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:13 +0000

    the appear file confused only

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def find_min(lst):
+        return min(lst)
