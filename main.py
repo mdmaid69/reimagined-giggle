74f505deedb0a58dc67ee2b733fe9feb5f451350
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:53 +0000

    content life century combination clever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
