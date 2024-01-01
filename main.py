a941a30db36b8165e9c536e661cac56282ddc352
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:27 +0000

    education estimate open my awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
