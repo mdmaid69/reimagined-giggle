2a11eb1e5e7bf47aece8e09744f3e2dcabf2c899
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:53 +0000

    list mix stretch Ll close

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
