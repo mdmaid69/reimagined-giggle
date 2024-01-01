c2037e1c94f82ccaf288e5b0e7c235b0da01b347
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:13:01 +0000

    home skill tall minor supply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
