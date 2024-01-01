674bc75db3dc8fb3716a2993dcbbd90d18efdc5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:59:02 +0000

    bite machine shock case label

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_memoryview(array):
+        return memoryview(array)
