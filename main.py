04e69595153a54bd9cbaf54678768b1c00630a2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:06:30 +0000

    hard second content example calmly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
