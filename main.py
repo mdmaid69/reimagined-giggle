a6a895105b81935f5c065ab2fac4c9f4f9b3ec1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:51 +0000

    regularly nerve last conclusion kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
