86dca98f44f6352876c8c67fc17698f73ccf2aac
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:21 +0000

    careful brush accommodation cap stick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
