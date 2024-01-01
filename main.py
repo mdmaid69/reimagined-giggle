b2bb6cc4d03daedf27820dc19484a5fcdca844ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:56:17 +0000

    reason sound lift however rob

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
 import array
-def get_array_as_list(array):
-        return list(array)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
