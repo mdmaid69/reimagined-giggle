94d130ad9815c0a5d09a756f94f38feab05aba0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:07 +0000

    its mark instrument provide light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
