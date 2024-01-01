66068d55511e0ca86d3c8d25c84e314d8170774d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:59 +0000

    maybe situation business need flavour

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
