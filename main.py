886e22b2f9414285ec01848c1cec4b96784a9999
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:05:35 +0000

    depend surprised ask shall nature

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import re
+print(re.match("h.*o", "hello world"))
