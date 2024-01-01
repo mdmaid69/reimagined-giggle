af53e1d69f107fa1728d0c04f5e89c1bab41bec1
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:13 +0000

    not plan Cc fit muscle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
