0ca10a1916db143d7208af8c87d2edb34e942bdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:41 +0000

    keepfrom judge add do any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
