16638d9577d131bafeb2578fcff7fe57ba33d890
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:42:15 +0000

    trade actually squeeze talkinto file

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
