e00d0ee5e01bdf8057c4489a0d98a0bae7dd0b1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:38:53 +0000

    when only invite lend guess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+from collections import Counter
+print(Counter("hello world"))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
