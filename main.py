1bc178dee75cde5d43ae63a8b5a6eab87cbe5b52
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:47 +0000

    care interfere accordingto confident attractive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
