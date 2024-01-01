5d192861f363d4152d4f9dde07072653986a8bb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:41:58 +0000

    accommodation getback measure should trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-print(sys.version)
+  def split_path(path):
+        return os.path.split(path)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
