24f69758fde57df5ab0da5bb4bcbb8a1b3f8eb4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:51:12 +0000

    trust guard probably purpose foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import os
+def change_working_directory(path):
+        os.chdir(path)
