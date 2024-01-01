b17f85ed125fcf3e9e8238587fb7905aeba73266
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:30:23 +0000

    cover along fair sad immediately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
