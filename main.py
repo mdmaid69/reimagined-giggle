4f81e64dff301406f403cfdb7e74ac662deb18b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:37 +0000

    comedown giveout 567enemy off such

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
