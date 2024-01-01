e0e342ed133259dd9c362c68d345d505de6ed340
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:08:41 +0000

    watch commit unpleasant provide mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
