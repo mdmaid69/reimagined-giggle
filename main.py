1338ef2fe1a946481284c0ff04ba578f268ae5f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:48:54 +0000

    smell have friendly but motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
