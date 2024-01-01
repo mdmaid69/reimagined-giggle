592bfc984889419d1699202b1fbe8a8a6253c82b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:39 +0000

    produce bright clean bath phone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
