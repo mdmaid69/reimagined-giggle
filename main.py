e26aca1341fb96449f937e73b10fb7de55accb21
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:58 +0000

    danger content own neither cough

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
