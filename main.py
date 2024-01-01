f854a5677cd43ce3aaf6b125cc0594079605ba68
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:47:31 +0000

    read murder collect Oo turnon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-for i in range(5):
-        print(i)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
