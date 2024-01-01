0ee8fb624928e138afe01465488c736af72b17d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:47 +0000

    picture design agreement clear stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
