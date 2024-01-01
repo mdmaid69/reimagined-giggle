bd462c053eb7843c669e4871c405eb223ed5d9bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:47 +0000

    agreement wrong except greeting picture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
