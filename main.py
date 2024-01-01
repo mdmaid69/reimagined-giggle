7ce239f8bbf9c6d7feae1624579c118fd538c8e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:05 +0000

    sale kick steam narrow male

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
