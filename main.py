ae0639c85eac0d75531595d3390d9e83dacc6d64
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:46 +0000

    professional activity depend callout birthday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
