7eb80702b0af7c5bf896d8eb6a56017c84bbd929
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:22 +0000

    process evening discussion tooth innocent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def is_odd(n):
-        return n % 2 != 0
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
