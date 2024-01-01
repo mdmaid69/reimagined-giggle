b8ea304eb9aa2f4bb9dd2e3afee20dd555c35475
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:58 +0000

    uncertain radio the petrol throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Square numbers:", [x**2 for x in range(n)])
