910f712e5f9c9756e28ab5189ef4f8bf363a546a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:32:36 +0000

    obvious stranger sensitive slice smooth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+for i in range(10): print(i)
