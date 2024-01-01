5ae18f92271d95573c32ca428a6a86fdde318c0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:22 +0000

    half last 564encourage neither balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
