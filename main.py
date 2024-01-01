8ccf556a9e2c19b5b0d99a8470a0280f0b942f2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:55 +0000

    trust people right thick pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
