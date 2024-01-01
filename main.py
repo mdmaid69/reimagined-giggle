b683e8b6ce5a6dc4b8a174c7e4c2f0e7e5805dd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:52 +0000

    left bringforward commit in achieve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
