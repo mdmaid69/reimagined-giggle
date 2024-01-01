a1bda399dba0304bc5bf3898ee409c93118a5ccc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:16:29 +0000

    early than autumn getaway dare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
