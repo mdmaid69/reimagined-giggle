e265ac6412442c603a12428225f17b3adcf203a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:35:49 +0000

    pass stupid tidy tidy pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def print_python_version():
-        return sys.version
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
