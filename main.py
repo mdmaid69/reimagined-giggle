a37044faf388072767c855ff4c5dd1a7e48cdd01
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:44 +0000

    gift day case range final

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def find_max(numbers):
-        return max(numbers)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
