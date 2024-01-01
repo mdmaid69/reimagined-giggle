c7f3590edf9e6654b8838cedeb058a6d064944f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:15 +0000

    fix debt day operate interest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
