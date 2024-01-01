2005f9edd9cddcc49f3746154471449f77932dd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:09 +0000

    relax me else increase understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_min(lst):
+        return min(lst)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
