7d6c344b1dd40a07deb6bba00f9c5f702d3755f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:36:27 +0000

    performance mention cost direct spare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_working_directory():
+        return os.getcwd()
