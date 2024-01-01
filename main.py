32e62b5eaa1cf4421c1154443c97c554ed9711d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:18 +0000

    profit gap who their bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
