688470c3de333e5d51f5a60098309655e6919437
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:23:09 +0000

    everywhere nearly schedule manner introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def clear_array(array):
+        array *= 0
