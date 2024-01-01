f1b3c0426218584ad6a73cf31c91409a3a16cafc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:16 +0000

    near cost sure field rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import random
 print(random.randint(0, 100))
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
