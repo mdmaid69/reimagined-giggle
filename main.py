e9510be3300bc4ba76fe16e88de5553c5ad511a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:53 +0000

    scream grateful comment worth record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
