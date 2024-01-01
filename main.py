1f8cee3552b8f34bf1d2c4427a61ba8d70fcaaf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:31 +0000

    frightened takeaway spill Bb situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
