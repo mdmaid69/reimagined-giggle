ddfc396797a4a52759d17ac82ed37d40f4d20a56
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:06 +0000

    both fail passenger convenient grow

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import tensorflow as tf
+print(tf.__version__)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
