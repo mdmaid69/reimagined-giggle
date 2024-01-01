e6d51026c8fb86f9b2fc118a6becc2516457eecf
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:07 +0000

    drink flight previous awake to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
