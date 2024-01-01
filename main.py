0c3949ca49712900482256e2b013b18965407190
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:42 +0000

    form risk mind stand pool

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
