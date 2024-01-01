9372096c1714dcb4fd2b6c27104c7a8b8a8f477e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:32:05 +0000

    force snow direct set even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
