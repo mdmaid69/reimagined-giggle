0e10e874b95c18a948288c85ba68af04921a85bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:15 +0000

    correct wish on skirt transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_density(mass, volume):
+        return mass / volume
