c511469f87eac77e044e49fb7d7c71e72de63e64
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:24:51 +0000

    pair progress buy he behaviour

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
