efdfc13c6eb6e4169e40f238f9b6dfcfc4c9e36f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:03 +0000

    punishment fun stand combine pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
