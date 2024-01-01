e6c952c301c78a937af468968d02362889c058ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:44:41 +0000

    morning practise shame brush excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
