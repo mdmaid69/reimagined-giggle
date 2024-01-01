b53aabc3dbe64215c51da671de4d047f7935db0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:23:18 +0000

    body field Mm strike lookback(on)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
