eb115afcb1665e4038bbdbd62aa5660fc3d5db74
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:24 +0000

    turnup ride estimate cover remove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def is_even(n):
+        return n % 2 == 0
