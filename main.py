14a5b15a524b717daef7d3d365df4f6675c23f16
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:04:36 +0000

    Dd bet expect run hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
