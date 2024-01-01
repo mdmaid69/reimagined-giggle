8337115b4eaf4b5aec9e61b4bc66b44f573fa993
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:24:05 +0000

    if spill interest surprise next

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
