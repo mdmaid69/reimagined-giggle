8c0a40018fde23531524a6655fe84b27fd4bb485
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:35:13 +0000

    sweet question skin job name

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
