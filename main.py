da78a2bd79ac52751d384b499bd9e38ab0356631
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:30:06 +0000

    visit scream ask job tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
