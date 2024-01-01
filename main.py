6105585b432361b6d60692b2a617f445b4ad1153
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:40:18 +0000

    clear anything blue form anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
