82fca1610796bc7402fa9e9b1c3b8682bb3d298b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:15 +0000

    not eat rub mass door

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
