275d4d419ce4739ca7b47334c5b8d35e2f737774
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:21 +0000

    straight shop prevent nonsense worse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
