d6f7b630ac26271ca354ada32258b3962614b5b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:47:03 +0000

    miss depend lookback(on) prove although

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+text = "Hello, world!"
+print("Words:", len(text.split()))
