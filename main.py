2dca4940f7618067cabe1bcff9a84b27e7c789ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:20 +0000

    pity abroad bother opposite tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_repr(array):
+        return repr(array)
