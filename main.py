50e11ae9fc23ef0febddc0d9d878d08f908f37db
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:35 +0000

    callback instrument listenfor confidence else

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
