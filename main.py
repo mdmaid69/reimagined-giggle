0c1b2e668c6c54795b8d7fe130ffdcedc6d1d11c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:23 +0000

    unlikely Zz habit cut any

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_min(lst):
+        return min(lst)
+def is_even(n):
+        return n % 2 == 0
