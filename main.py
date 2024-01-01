2910e4c5317a4249f42e6f9ec1d3e81b05ac59ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:38 +0000

    thick married simple excellent minor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
+def subtract_numbers(x, y):
+        return x - y
