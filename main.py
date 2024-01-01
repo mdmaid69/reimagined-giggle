e35bb00c114c1c4069bfb0f853d1f80cd238efd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:31 +0000

    job practice letter trap objection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
