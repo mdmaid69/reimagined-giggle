9492eefed7b72368188c08554dd49e15627862f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:42 +0000

    apply word pipe level interfere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_today_date():
+        return datetime.date.today()
+text = "Hello, world!"
+print("Words:", len(text.split()))
