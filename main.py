041d8630af809794a3472ca26c6a548b8fc6bb49
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:14 +0000

    trouble furniture message discuss brief

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
+import collections
+def create_user_string():
+        return collections.UserString()
