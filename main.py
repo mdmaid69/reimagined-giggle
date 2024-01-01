defe7777cd033c32b63d6ee9f5379e5428ec157b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:23:32 +0000

    dig doctor freedom song shop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
