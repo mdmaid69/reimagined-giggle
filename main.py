e247853c43a1576791f17664bc28341de50e95c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:18:03 +0000

    present sensitive bill there reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
