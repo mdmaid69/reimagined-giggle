f71f5effe47fa78ca32f919a9357473b8fef014c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:30:23 +0000

    damage confuse lead prepare advice

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
