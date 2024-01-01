d85efde705b90ee2860e255420ff2d3d6c9175b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:35:24 +0000

    path near high street band

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
