fde67c932c369532c12687e67515267efc4b1f65
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:05 +0000

    everywhere platform born congratulations free

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_file(file_name):
+        os.remove(file_name)
