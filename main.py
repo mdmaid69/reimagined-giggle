7ce3d1f2c061765d994d7bfc1a82a9265a8f5e7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:31:36 +0000

    record attract edge summer where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
