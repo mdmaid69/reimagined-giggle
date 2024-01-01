f64250e5dec626254e4003b40498d099fd96f256
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:52 +0000

    breakin test train comeover sad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def multiply_numbers(x, y):
+        return x * y
