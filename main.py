b4fcfc0d5155731b5f7192a99d2d096fcfc01e79
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:46:10 +0000

    thinkover noise sweet smile opportunity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
