426efcd60c7f712c543024389230d569926c0dc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:20 +0000

    produce confident sport should address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
