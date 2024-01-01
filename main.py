ea2c058621549c5aacfa08a3c6d8f474ded0b12f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:16:52 +0000

    glass lie succeed shut price

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
