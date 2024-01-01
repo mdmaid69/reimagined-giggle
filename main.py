a51bad398cb72ca209a3495f71441d02d639c8c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:31:45 +0000

    progress cover supply niggaacademy smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
