82f94b646361f136d36271938bb99ae6cc3420d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:27:35 +0000

    see dream whom even cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_min(numbers):
+        return min(numbers)
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
