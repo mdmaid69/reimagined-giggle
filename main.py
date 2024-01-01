f0e0bc5bd28360b3ed6888ebf9af22fb709ce1c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:22:03 +0000

    wake learn raise rarely among

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
