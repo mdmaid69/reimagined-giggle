de8003e00c0bd7b860a1de3431b319e22fd9db15
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:16 +0000

    cool final catch store innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_min(numbers):
-        return min(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def append_to_array(array, item):
+        array.append(item)
