f654cca792989d6d3c5ad298600ae1bb5142e180
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:38 +0000

    length wonder fall convince present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import numpy as np
+print(np.array([1, 2, 3]))
