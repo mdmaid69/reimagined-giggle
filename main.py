74aa142885c4a807ccbbf00fe3a267bb17ee10c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:11 +0000

    sell land bill bringdown age

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
