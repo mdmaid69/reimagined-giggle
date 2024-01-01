8e3cfd7655611e422c97a2fb5a9f5ed5fc4adcad
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:10:07 +0000

    plain basically change container blame

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
