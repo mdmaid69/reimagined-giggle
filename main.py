2ffae273e0eb486ce72e7423940cb675cc1225fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:13:11 +0000

    correction finally miss journey ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(10): print(i)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
