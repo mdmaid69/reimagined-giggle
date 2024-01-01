f2be8e11540b370550708b71972d429d8c7a5ee7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:12:59 +0000

    advise cheat contact science main

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
