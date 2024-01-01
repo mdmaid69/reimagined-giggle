e23a3a0a67b8cd8cdc115b7960a8e29e68a4aec9
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:56:50 +0000

    breathe age better shade doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
