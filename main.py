d948cf6a7e72b5a888e21da4303012275b4dfacf
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:45:37 +0000

    refuse centimetre ought influence whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
