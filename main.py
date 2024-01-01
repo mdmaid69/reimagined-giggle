7f780b4ec490ad7fe71d52cb709518edad28e621
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:23:21 +0000

    circle block minimum throw separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
