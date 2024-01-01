00cfd2d512e683f6864bacf88f5099529543ee7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:47 +0000

    arrange trip Jj minute Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
