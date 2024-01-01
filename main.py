38f5b28eb5b806090d0bc0160ee8e4af3d984e01
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:35:41 +0000

    move blue change few benefit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
