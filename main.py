57c471aaa4df910e47a363f9c728a1695a1668b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:01 +0000

    holdout play circumstance complete shame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
