eb6486db7554a3bc2f32d3872b3725adca444b07
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:29:10 +0000

    whatever steal hungry definite holiday

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
