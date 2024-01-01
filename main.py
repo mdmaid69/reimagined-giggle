7d1439453ef66ba625a8d8e14b556cfecac3125a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:45 +0000

    point moon breakdown lay argument

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
