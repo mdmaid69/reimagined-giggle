c430c7745b6b5498466f91f714ee458b82b0716e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:23 +0000

    tomorrow sport finally crime develop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
