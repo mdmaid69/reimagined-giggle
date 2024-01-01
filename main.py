7e971deec5e0eba5683a193e75e7e96306d6247b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:03:42 +0000

    turnback nervous further bus sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
