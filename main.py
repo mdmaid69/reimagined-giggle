d9dce2e63338246fb32af648c963be9bf2b1d2f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:59 +0000

    tap crowded hurry copy full

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
