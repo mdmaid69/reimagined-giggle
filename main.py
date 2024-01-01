d1fb8487e697f87dcf27a5aa5a6ff8c748c3d4f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:12 +0000

    interest bound crime remark throw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
