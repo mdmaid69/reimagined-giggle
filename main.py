4440404331bf2ea28bfc77a302fff33ac678ea3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:16:50 +0000

    charge possibility story lower trip

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
