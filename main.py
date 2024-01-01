6bb7079308950878fa54324a0a96330ad2894f4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:42:04 +0000

    ring embarrassed survive floor properly

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
