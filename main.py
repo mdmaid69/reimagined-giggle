98311d6551940eb6db1ae461f1cf5a2b0811d424
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:28:32 +0000

    mixture score nearly by unable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
