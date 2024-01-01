05d7a4c1acdef96964dc3e786ae42b062adb4acd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:50 +0000

    border operate straight formal change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
