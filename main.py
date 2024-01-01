6d6b105db5aa1a80b31fa38b6b3d9db7cb82eb84
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:55:15 +0000

    Dd kind evening intelligent every

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
