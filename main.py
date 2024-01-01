d257584de92d604c49c6a5dc07e094b38bb6573a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:06:54 +0000

    now upstairs invitation trouble Nn

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def is_palindrome(s):
-        return s == s[::-1]
-print([x**2 for x in range(10)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
