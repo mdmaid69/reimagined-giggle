372fb8bb198e9146a530e8253040ea641c163d48
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:27 +0000

    trick since chase stare front

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
