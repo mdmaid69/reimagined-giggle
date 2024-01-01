6c28fec65af4227b54b7d9a3d30d82237c754d7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:04:28 +0000

    before bit national roof repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
