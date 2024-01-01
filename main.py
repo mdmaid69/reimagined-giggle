d514c71ebb9b2486bcfc1660917fcac6ffff6d81
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:04:07 +0000

    behind beautiful pool too somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
