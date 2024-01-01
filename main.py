236235ad3cd6194dce09b76be2c80581c0028288
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:56 +0000

    share plain mistake neither exam

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
