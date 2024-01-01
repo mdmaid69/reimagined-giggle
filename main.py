6d898e6e339b6d88b263dc25a06484765d3fa12f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:01:37 +0000

    long wet wear flow deserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def reverse_string(s):
-        return s[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
