2bbcfcd74de912d4ffb2fa7ab353d193c927b549
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:38 +0000

    scream confident show again pure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def get_array_length(array):
+        return len(array)
+def find_max(numbers):
+        return max(numbers)
