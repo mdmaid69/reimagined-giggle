b53479d37a78564ea5ac0afa8b51e02e99aa175b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:07:12 +0000

    luck fail shock in hand

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import sys
-print(sys.version)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
