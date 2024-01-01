05045fd3c51dd3583ab7277c4224ed545dc5c47c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:30:29 +0000

    complain month support continue borrow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
