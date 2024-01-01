8d8e01ca16553d2ee78d2b1d6f4a59b9f38dd087
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:42 +0000

    comeout imagination become lay who

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def change_working_directory(path):
+        os.chdir(path)
