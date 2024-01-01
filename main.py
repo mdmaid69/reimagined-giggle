b969bc0ba09e4c8cba4c11adf02bfddadb9c9998
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:04 +0000

    board prepare many bound ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_even(n):
+        return n % 2 == 0
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
