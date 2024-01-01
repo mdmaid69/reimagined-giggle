4a47151786f062b6480352928ab517f3a12105d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:41:29 +0000

    fill increase sea press anywhere

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def reverse_list(lst):
+        return lst[::-1]
