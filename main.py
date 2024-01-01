5aacba7227ca3f4ae5cc023f72afb494be56bbf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:54 +0000

    spare become unfair mark borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
