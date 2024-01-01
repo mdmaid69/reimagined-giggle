e76b29a3d448426c1baaec30ab3a9dfada1f66a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:45 +0000

    present gradually wave back trade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
