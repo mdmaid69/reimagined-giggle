d27570f2a471a9a784fad5c62df89e69453dca04
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:50 +0000

    order delicate violence physical awake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
