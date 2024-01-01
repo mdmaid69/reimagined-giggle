04b1fbd6ed5ff0af44d842a53e71f828436071c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:11:37 +0000

    comedown high school lookaround gas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
