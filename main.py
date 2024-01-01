e2808b1252d23a426e76f06aa59727a40d247483
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:51 +0000

    Yy comfortable tap black watch

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_density(mass, volume):
+        return mass / volume
