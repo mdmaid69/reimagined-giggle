88bdfc6a8a7df187a14275452b1d392febc464f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:12 +0000

    tradition thirsty conversation really top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
