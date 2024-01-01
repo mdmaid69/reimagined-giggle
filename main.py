10767a7ab66769e5d8941823564d4d598342eddb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:43 +0000

    tax insist rather brave with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
