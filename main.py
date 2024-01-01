0ee05e710a4efa49f7f37cceb3cdbadce5117784
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:02:23 +0000

    thirsty success contribute correct translate

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+print(sum(range(10)))
+def convert_to_octal(n):
+        return oct(n)
