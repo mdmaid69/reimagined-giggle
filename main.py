a05dc25fae0e9e4289b7b79749bde92b97f9f978
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:23:31 +0000

    project drink wall lucky allow

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def count_elements(lst):
-        return len(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
