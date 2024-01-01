9f5bd96187b47c36bf0f0c382a8bab5da379c449
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:02 +0000

    practice like proof design speed

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
