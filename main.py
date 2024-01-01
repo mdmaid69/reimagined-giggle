eb329e38244241ddbc254e6fd583d836e133758c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:27 +0000

    paper mend herself takeback plenty

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import sys
-print(sys.version)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
