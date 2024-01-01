f5aa3bd2ebdea547688e4d502089724138b168f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:12 +0000

    while off road fix guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
