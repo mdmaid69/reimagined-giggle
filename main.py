fe77e024d95ac7d0aff61b6166e5b3f6a7b28518
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:38:47 +0000

    promise you level race part

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
