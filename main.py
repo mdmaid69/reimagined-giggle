565452d9843c29627b868f30d0b34ca167e8a2e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:27 +0000

    wherever circle spot quickly cut

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
