bc2a1ded43243f761428d3782b593ae9728e4bb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:54 +0000

    skin defend foot main happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
