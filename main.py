d392e95a3c33ae5ca0f3786a10bcb1d628f16bd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:17 +0000

    book second stamp jump where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_base_name(path):
+        return os.path.basename(path)
