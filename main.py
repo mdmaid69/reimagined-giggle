a3602c499bfe04bdb3c8c409d96eacc647edd0ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:09:28 +0000

    hardly Dd opportunity return excuse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
