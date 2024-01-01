aef6dec95f89c2dc764ad7a8fed3cfefc9d55f34
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:44:00 +0000

    together throw contrast bite translate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
