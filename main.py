b9273923f00d50909bfd4cb31e09ac7705ceaaf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:09 +0000

    ours workout rain rescue anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
