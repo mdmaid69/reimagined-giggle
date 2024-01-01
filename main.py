c14b5c5084dc009eda2ecc0cd96742f0d91f0fe7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:49 +0000

    tax cry substance trip distance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
