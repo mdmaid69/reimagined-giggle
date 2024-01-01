2798f7a6227cbb6df609b57bec2aadc657987f2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:32:10 +0000

    relationship attractive achieve choice neck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
