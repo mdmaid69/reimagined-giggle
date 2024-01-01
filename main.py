2995f22ed831565f1e9ba6c061d048cb55e67643
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:47 +0000

    sudden away press tonight worst

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_directory_name(path):
+        return os.path.dirname(path)
