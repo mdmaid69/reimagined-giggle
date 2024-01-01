c33b61d246f214d1105672f22e46b2ce93ce03bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:50:19 +0000

    shine payment up praise deaf

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
