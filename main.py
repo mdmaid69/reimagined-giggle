7258eb067e330b5d3bb7b51d948e9f7917a25bb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:18 +0000

    produce sign complete land alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
