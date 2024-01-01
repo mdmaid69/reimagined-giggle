317ed291acb08523056df2cee632fa92e6070eaf
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:52:02 +0000

    information thank worry patient much

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+print(sys.version)
+import math
+print(math.pi)
