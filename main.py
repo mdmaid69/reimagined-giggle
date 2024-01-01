67efe2be58e669ecf74ca8a969acbe702ea92162
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:11 +0000

    copy comein container trust interfere

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def cube_number(x):
-        return x**3
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_density(mass, volume):
+        return mass / volume
