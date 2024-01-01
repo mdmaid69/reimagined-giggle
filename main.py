89e2b962ea0f06b35073e1506ef4113f1d584ed5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:16 +0000

    speech wipe nowhere murder pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
+def find_min(numbers):
+        return min(numbers)
