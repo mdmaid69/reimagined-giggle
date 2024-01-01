2e05e46ef8ac1ef9c92709ada439962217bfdc33
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:05:55 +0000

    aim pleasant our brush breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import tensorflow as tf
-print(tf.__version__)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+print(os.getcwd())
