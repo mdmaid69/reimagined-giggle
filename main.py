3d46f36982e2d6fe50bf751e47332f0cd024637d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:54:01 +0000

    movement science switch interested hospital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import sys
+def exit_program():
+        sys.exit()
+import sys
+def exit_program():
+        sys.exit()
