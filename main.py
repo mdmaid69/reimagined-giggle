131e736e116a42fa1680fee640263479cc285c40
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:51 +0000

    either blow develop fresh ignore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_power(work, time):
+        return work / time
