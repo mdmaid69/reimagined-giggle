70a047f472fb66e8dea8111d46d56ecfca8b1741
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:51 +0000

    brush beautiful bring description frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
