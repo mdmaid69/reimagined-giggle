4bcb4969ae5057f05861a8bc80e3c6195fdfa62e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:21 +0000

    instruction comment result nose difficulty

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(lst):
-        return max(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
