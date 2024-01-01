3faabdcc8f77c3af7279d8345b12c9f154b38255
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:30 +0000

    spin considering lucky pour mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import platform
+def get_python_version():
+        return platform.python_version()
