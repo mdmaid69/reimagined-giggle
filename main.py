c0c6a8577e71297c66a8e9fdaadfb2825a127215
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:43:09 +0000

    comedown period road it plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
