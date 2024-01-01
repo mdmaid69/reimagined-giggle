7dcb248beab6e92117637602b64208c7b045e2aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:22 +0000

    thought disgusting mass general below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
