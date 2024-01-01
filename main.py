29b1294ea96842494081f11b58cace57e8d4746c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:56 +0000

    clear giveaway entrance phone circumstance

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
