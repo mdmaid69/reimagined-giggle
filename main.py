dfffacd3b8503997ec11748ae94b5e5f8945c353
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:39 +0000

    frighten up equal getaway thinkover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_acceleration(speed, time):
+        return speed / time
