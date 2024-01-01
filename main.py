9edd5df51b2cf8693342d3d867bc33a1d2ad0ac5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:26 +0000

    sticky brown table display but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
