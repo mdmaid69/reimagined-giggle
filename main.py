0af203e3669fb43e162716d1ff23cc18e8aae10a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:07:31 +0000

    hardly build increase evidence exist

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
