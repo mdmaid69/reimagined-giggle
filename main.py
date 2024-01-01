4781bed929fcda7ead99d19284faf42d6a006662
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:16:21 +0000

    petrol land sign free themselves

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-print("Hello, world!")
+import sys
+def exit_program():
+        sys.exit()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
