6d5457a247af0fe26ed27b5e4c9d4c4425a1328b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:59 +0000

    opposite surface need group deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
