3d344f5d1d87553611243d72e6bbe40e62c4e470
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:33:55 +0000

    point smell contain whatever grand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
