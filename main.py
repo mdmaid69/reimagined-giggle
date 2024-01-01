65e0d31ecea0a14a5fee40e217bcc23d4410c5db
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:34 +0000

    569enjoy education fresh schedule by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-for i in range(10): print(i)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
