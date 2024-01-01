efbe41e9b05e908d927f4790ce0288190c21a5a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:31 +0000

    many male see climb hold

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_min(numbers):
+        return min(numbers)
