3316b530ed1f0a88fe247c92143fa123b5dffe9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:04 +0000

    honest level lean top like

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-for i in range(5):
-        print(i)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
