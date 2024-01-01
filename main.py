7af87e3540bac3d5da5b12f670d6134e013bb955
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:50 +0000

    breakinto cheek disappear space train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def square_number(x):
+        return x**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
