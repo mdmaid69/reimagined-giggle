8a20fc88bbe687c13505aae0421acc435ebf0124
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:47 +0000

    cause accident building set exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
