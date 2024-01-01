3d4fc65b7fbbdf2e1c2bb8bda24b148ba8a97f67
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:46:03 +0000

    draw tradition Nn gradually regular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
