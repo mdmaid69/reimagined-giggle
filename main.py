c824c76330fa17a1ed999e5638de26ba1dda7023
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:59:57 +0000

    achieve warning present pity organize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_sine(x):
+        return math.sin(x)
