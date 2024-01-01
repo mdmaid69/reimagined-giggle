e199d22de7bdbaebe540affc4fa61d6b902d53a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:41:12 +0000

    apart mend rather away after

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+for i in range(5):
+        print(i)
+def convert_to_hex(n):
+        return hex(n)
