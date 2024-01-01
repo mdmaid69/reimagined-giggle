9f04fef8c5b95681a4d64159bff46627ee070e38
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:06 +0000

    when giveout side Nn safe

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def count_words(sentence):
+        return len(sentence.split())
