6d3a6f62d108308aeaf439e4a51fe467fd280eec
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:39 +0000

    chat year buy kind way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
