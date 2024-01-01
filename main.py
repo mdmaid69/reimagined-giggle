d5ed4b6bf3f1dcb6a8b1ac9c0b683386fafdc70d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:43 +0000

    him holiday side shut intelligent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def get_current_working_directory():
+        return os.getcwd()
