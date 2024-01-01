5ff231c69bff2eed39e103f6229fe54ff690956f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:24:41 +0000

    fork downstairs prove centre health

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def subtract_numbers(x, y):
+        return x - y
