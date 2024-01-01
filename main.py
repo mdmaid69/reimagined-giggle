7b89fa1f1b32731174f697548a7a04eb3f229872
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:02:27 +0000

    smile make call turnover job

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
