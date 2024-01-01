f32e0f531354f0e97a8e7b8e724835e192f31b01
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:39:33 +0000

    burn cough embarrassing order evidence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
+def remove_duplicates(lst):
+        return list(set(lst))
