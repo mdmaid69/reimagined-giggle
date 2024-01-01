37838370e1a5a294ad46fbe5003d67199c1cf98c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:15 +0000

    organize sensible shock brief nose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
