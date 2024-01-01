66ab26d915e3b842b30e026992eb5cc814b441c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:08 +0000

    recognize course willing scare frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
