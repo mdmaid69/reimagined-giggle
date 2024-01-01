ebc8d20ceb75f6f54049fa971f452b66bb2e3f9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:44:11 +0000

    sell pause proof save unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def find_max(lst):
+        return max(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
