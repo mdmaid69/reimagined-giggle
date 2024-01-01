0a0e094cfd52cbd056434e921da12bcbaba23a62
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:32:08 +0000

    ability object mass rise sky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
