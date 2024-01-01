63da8eb441133bc976f9f4825fb9c45cf19ac2a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:09 +0000

    murder flight convince telephone centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
