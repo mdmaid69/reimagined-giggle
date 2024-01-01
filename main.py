5666c8c59d17400157a763441cf7a12e520e24a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:46:42 +0000

    against whole bag pressure label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def sort_list(lst):
+        return sorted(lst)
