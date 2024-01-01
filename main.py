aede21ead039216b99ec730a1ccdb0b00826a4c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:49:34 +0000

    otherwise kick thinkof male feeling

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
