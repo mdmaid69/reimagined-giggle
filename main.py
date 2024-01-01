3fdc433812eb15101876815226689f3b47ce455e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:00 +0000

    crisis house slightly distance prepare

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
