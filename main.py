22fa6271d976681776d7bd68be345057a1f9bf83
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:19 +0000

    simple black glad giveup off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_speed(distance, time):
-        return distance / time
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
