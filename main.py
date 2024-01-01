ae892135c4f7e7626159a64e915b390e85bfd05c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:07:21 +0000

    activity thanks upwards winter mark

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
