312f651d8d3e4610f6277756391f352561cfe805
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:46:30 +0000

    object 572enter feed common attempt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
