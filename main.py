90c119b7d516160026424befca75f461d0a5a12c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:59:04 +0000

    include breakinto recommend paper copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
