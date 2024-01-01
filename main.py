ce51b652992e0cc716e3ddc34f5cb05b260970c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:29:27 +0000

    respect condition react lot Hh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def remove_duplicates(lst):
+        return list(set(lst))
