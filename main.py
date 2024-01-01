91f8fd30ff11849c9d171910d9b2b62a4bbd7e99
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:47:01 +0000

    sea describe under belief dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
