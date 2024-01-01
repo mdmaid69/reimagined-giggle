b6d4ee76ac36ffb783d94e120930857a420a180f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:08:20 +0000

    pot touch hurry combine out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_priority_queue():
-        return collections.deque()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
