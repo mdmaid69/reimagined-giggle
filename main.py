8e1ab95ad891210e39f3e00cc2e9bfe43a6bf4f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:09 +0000

    path behind cook able band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
