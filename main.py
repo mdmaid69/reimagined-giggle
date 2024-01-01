106249324e10bdd263340be24cd0fe64513d4ae5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:56:49 +0000

    consist cum can easy set

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def is_even(n):
-        return n % 2 == 0
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
