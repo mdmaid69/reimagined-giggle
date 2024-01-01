4a02487f366c8c987b60a72d103e9dc6df869549
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:46:27 +0000

    pleasure train issue ill claim

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
