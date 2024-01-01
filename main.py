2d11a6ad2ae69dbe68581921aee036d981fe3369
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:50 +0000

    flood shout slice measure tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
