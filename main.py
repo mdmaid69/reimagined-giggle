88ea0bccdd5d6053e5b57c4f196a9da3b743bdf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:22 +0000

    entry push guess accommodation shade

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_length(array):
+        return len(array)
