ea1edfacadc5d2c8446b1755062d5758b7797307
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:54 +0000

    dangerous ordinary doctor unfortunate protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
