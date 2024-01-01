13e309382047a92c694709286e0421cae9ce5551
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:41:16 +0000

    properly suddenly moment search by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
