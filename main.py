cfce542ae7d643316de9ed128f8ce6ffa06d74de
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:23:55 +0000

    getoff life music wise worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
