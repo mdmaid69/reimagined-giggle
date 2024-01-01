3d23ec021153d741ded365a38885952db27fee58
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:08 +0000

    cut keepfrom yet uncomfortable inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-print(math.pi)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
