5730c984b12447d34ef8ebac1a4271dba83167fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:33:43 +0000

    danger agree consist loan walk

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def square_number(x):
+        return x**2
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
