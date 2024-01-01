4b5a1819ced3cd6730862673d2aa3ad064f84ec5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:07:33 +0000

    wish runout tell I short

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
