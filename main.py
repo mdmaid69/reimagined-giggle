3cc2d95799d333986ebc1914ff6eb99a370309b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:34 +0000

    blue century desperate reach spill

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_rectangle(l, w):
+        return l * w
