35d4f73b6db91a2f9a9d4aaccf2a26d7d9704057
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:17 +0000

    climb trouble inside immediately underneath

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_sign(x):
+        return math.copysign(1, x)
