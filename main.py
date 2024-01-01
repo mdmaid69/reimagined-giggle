ccde4330f36df0fcfebdf56bd9971ca4e7e01c53
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:48 +0000

    voice breakup gap tone science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
