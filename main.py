26c60f96ae3c402f3782a31949274efeea8503fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:28:41 +0000

    winter big maximum difference turn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
