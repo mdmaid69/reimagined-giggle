8ec8381a74df297b755c63c0838815c056b1b006
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:37 +0000

    coat occasion play score car

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
