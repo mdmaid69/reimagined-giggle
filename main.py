99561f2fb0479dae8e7e2be07d57a83c95d87ca3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:01:31 +0000

    giveout laugh copy next pleased

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import os
+def get_current_working_directory():
+        return os.getcwd()
