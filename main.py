2b3489b3fb72728bf9826fa4d3f17d8b934b4d52
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:29 +0000

    start swing force situation appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def square_number(x):
+        return x**2
