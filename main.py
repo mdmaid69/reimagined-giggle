8f3d81b8496cf75e2cc8f023e04cce712bb3bf31
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:07 +0000

    holdon criticize talk famous accordingto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
