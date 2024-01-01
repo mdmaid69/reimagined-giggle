b9930968f4c0d6542cb1b501211a6f5e52faa309
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:50:34 +0000

    importance bottom under otherwise getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
