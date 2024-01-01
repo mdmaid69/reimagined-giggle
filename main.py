e48e486aea997dbcf2466c1160c01a9d8692767c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:25 +0000

    half thorough accept her funny

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
