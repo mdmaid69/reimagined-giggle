5c443a197511ebc70c4f43319426c2077f876a36
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:31:47 +0000

    floor punish once flat deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
