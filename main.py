75d580ea9af788e8f1c96b27cf1f137b394c0a0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:39:56 +0000

    realize follow borrow air key

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_directory():
+        return os.getcwd()
