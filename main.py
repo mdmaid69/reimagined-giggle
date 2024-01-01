c072cd680a24163d98a0ceb58c6a95066ad4303a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:20:02 +0000

    group your design noone takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
