71cc7a57b7653f2887290ca419b4fd338e9a6ae5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:10 +0000

    heart against generous trouble practice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def multiply_numbers(x, y):
+        return x * y
