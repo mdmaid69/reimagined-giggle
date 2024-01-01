60417251efb0a73aa01f656df290dd44b9d7f7d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:26:16 +0000

    apply influence twist value bear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
