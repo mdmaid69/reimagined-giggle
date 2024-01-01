31a48e8d48efd79f11e93d7c4b229c1e08d11db3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:31:43 +0000

    left Ff factory constantly image

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
