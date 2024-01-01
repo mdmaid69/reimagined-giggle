c73e255f3dae9b0d33e65061ae0ce7531ac0a202
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:46 +0000

    rub it comeback behaviour wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
