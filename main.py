0f270e9f3ecbd7def7b40258133830506923ec25
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:19 +0000

    awake next tune any emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
