15e303e836b83e81b03b73df1a00ac0466e5ed9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:33:02 +0000

    prevent secret splash keepoff pain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
