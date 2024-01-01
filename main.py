5ee987931ee67afc06fda65157360374c7360b16
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:31:41 +0000

    prefer close talk excuse food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
