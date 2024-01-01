8ccb4ee130cb0b85315089d2245ce337c42a56fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:07 +0000

    move school can chance lower

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import sys
+def exit_program():
+        sys.exit()
