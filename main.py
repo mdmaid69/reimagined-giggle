a7a99a41f3e75bb17f5f852ca1c46edbb0e0eb85
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:16:54 +0000

    amount house sail tap replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
