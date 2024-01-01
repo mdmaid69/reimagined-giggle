3c1b6fe8781f36520f166d3885884905d230e195
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:55 +0000

    ilovedicks yourself radio walk they

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-def convert_to_binary(n):
-        return bin(n)
+  def convert_to_hex(n):
+        return hex(n)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
