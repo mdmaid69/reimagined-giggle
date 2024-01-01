f362da43c3502ec26db0d798a0aa13a71333ae8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:47:17 +0000

    perform hurry star trousers shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def reverse_array(array):
+        array.reverse()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
