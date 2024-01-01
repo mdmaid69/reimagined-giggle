3ec4f106a4ba75282a43c4f905aa46d8672cd325
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:52 +0000

    callup record light trip obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
