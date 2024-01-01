8c2b590ff974e9691c0d46ac701ab8a9093baf92
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:18 +0000

    demonstrate watch basic old determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
+  def count_elements(lst):
+        return len(lst)
