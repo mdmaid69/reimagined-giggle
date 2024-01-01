a555e2d80f8d7ca0cefe01c42b9ed8e077bd50e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:30 +0000

    in rank consult will fond

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
