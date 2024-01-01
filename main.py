d842f8c07e218ade00eb3ea6a7b3a85cbd4e1680
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:22:50 +0000

    soak priority suggestion achievement proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
