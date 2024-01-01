26ed992fc57b980f198684e0de6c2eda6650e70d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:18 +0000

    trap shadow shadow watch cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
