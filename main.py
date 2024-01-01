e34b593a39b779034afef39e5c6fc210a3011ad2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:46 +0000

    paper telephone bet touch world

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
