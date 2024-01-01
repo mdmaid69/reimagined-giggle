1b3580d61678e199ce6355db438f2ca3849def75
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:03:04 +0000

    getaway commit pool tool arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
