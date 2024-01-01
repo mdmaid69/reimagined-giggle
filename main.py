2d0e14f8b49a433798deecaf5385083255005815
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:32 +0000

    explanation hurry straight early myself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_tangent(x):
+        return math.tan(x)
