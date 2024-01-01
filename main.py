4952ebe0ef560f65360c6ca24777ab4e8f8b480f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:58:17 +0000

    night high paper fix rescue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def find_min(numbers):
+        return min(numbers)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
