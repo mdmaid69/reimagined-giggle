beb99f8bb3324169ec932c9969e350324e6b83da
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:48 +0000

    simple actually taste continue thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
