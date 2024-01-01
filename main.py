88ff6c332e9b5cfe8ad8942c70fb0eac2b0b4cbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:02 +0000

    climb suppose present sound Oo

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
