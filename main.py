157296a14b1c8a471f33dbb9ea516c26b0550af3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:45:38 +0000

    punishment point though geton century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
