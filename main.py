1b6d8264fd16177c86457ec2e90a73c6444ae36b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:44:59 +0000

    slightly little male weak no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
