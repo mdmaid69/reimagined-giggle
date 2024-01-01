ec38b1e9c13258c72e9f38cdeca95169414569fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:28 +0000

    this proper during same will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
