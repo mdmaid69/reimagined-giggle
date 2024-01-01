df874ee831a3db521fc4f777f376d99c5cb01725
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:40 +0000

    rain make exchange play enthusiastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
