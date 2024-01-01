323718589a46f6f28d685316dfa78bca1ba8f264
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:16 +0000

    cross interesting correct buy middle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
