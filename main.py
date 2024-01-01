f7eb0c07bc60ca42167e660d3a7425b38343e103
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:25 +0000

    grateful bother nerve grow love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import sys
-def exit_program():
-        sys.exit()
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
