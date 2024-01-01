d63a8e3f9fe63840ee49664cb4d83ef1cf447a9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:16:46 +0000

    ability mean yes touch keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
