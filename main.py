426b898b3f231732051a43a473fb7b459a2bc1f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:15:02 +0000

    goaway OKorokay stare tip bury

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def add_numbers(x, y):
+        return x + y
+import array
+def get_array_as_bool(array):
+        return bool(array)
