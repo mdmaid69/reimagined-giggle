97730ac872574a3eae9397465696f2351efa5340
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:32:26 +0000

    dare pure put hang behind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
