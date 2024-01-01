44e894d168910fdc3e2108a5b797b98f73c2d6e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:00 +0000

    afternoon still cheque tap whenever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
