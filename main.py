1eebcfe4fd7b178d526328647bb5c18bed503544
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:02 +0000

    never middle vision mouth interruption

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
