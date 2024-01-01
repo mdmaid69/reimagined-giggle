8a8120ecf06722839a4e7ab6f1e58aa8344e93ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:44:55 +0000

    wake strange bottom favourite thick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
