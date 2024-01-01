05e99e855016ded0e5c16150b3bfba8804f45d87
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:28 +0000

    positive hardly interested branch wherever

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-print(os.getcwd())
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def is_palindrome(s):
+        return s == s[::-1]
