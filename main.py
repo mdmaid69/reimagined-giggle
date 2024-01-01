13b1bb979a0829f9d94692fe664011646e9d67cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:31:19 +0000

    terrible complain through shop no

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
