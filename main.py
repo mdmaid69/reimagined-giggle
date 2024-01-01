cef2c1d18b9e0ca1ee5c31b12086d821445e81b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:33 +0000

    sometimes substance space bye main

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def convert_to_octal(n):
-        return oct(n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
