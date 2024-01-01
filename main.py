2341b5b2db441620d506d01b8134579492884fea
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:52 +0000

    comeout actually sister payment crazy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import time
-print(time.time())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
