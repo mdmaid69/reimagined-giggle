9824f4976f911f90d8448d1f44af6842c00847ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:42:14 +0000

    discuss fly yet sensitive include

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  def convert_to_octal(n):
+        return oct(n)
