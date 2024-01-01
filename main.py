669b286241eee503fa8023fbb93515622b564bd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:56:10 +0000

    agreement talk new lookup description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
