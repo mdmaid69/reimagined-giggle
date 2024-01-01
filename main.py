4726b9016359b1dbeac8b01eb18c2c336b4f6071
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:09:04 +0000

    skin surprising central float confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_repr(array):
+        return repr(array)
