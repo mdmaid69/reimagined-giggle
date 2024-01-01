526dd4e468b1de31d6e575300a84d8d9e67d717f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:00:17 +0000

    tongue event spring treat fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import time
-def get_current_time():
-        return time.ctime()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
