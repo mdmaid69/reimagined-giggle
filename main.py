99eb26baba670c35125fc304c87ae56d09620265
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:52:52 +0000

    clever manage share pleasant hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import datetime
+def get_today_date():
+        return datetime.date.today()
