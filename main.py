e2e1ff39e9f21cf9d458bab7d245a5dc015094c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:11 +0000

    group workout delicate interest high

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
