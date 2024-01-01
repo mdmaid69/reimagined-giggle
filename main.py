7e9cc7adde121dcb3b341c0c72176c7267430b64
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:41 +0000

    shopping dance breathe authority ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def set_array_item(array, i, item):
+        array[i] = item
+import sys
+def print_python_version():
+        return sys.version
