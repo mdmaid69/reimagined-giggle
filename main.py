2e6c53802ab7c26996fb7409560da87f13760e03
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:58 +0000

    direct proud likely proud rise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pressure(force, area):
+        return force / area
