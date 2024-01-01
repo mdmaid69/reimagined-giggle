e7991e6e65a870cf74a73f68b838f0caae27cbcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:22:35 +0000

    each pin give often dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
