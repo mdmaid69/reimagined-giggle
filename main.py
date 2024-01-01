79b2d2bd937143fa499775e4b8ea07f36b8dd616
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:47:41 +0000

    mother reasonable deep present once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
