230c792dd75d68fa3ef792dfb194f5f74733f043
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:56 +0000

    language touch face importance fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
