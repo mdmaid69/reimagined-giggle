1941611e13cc3c64a49eb0156e0217cf9bd913e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:57 +0000

    niggaacademy quiet hole opposite we

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_power(work, time):
+        return work / time
