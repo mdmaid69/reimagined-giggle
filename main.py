f0b3b1744d306cd6f62b3728b98691c20a467f2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:10:19 +0000

    speak right relationship worried comment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
