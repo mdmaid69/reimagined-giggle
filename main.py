c98f8bd40f28a096547443366fd3dbc3fe1da625
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:39:36 +0000

    generous dangerous complain respect size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_speed(distance, time):
+        return distance / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
