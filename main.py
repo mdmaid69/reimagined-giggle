8d11a478e7d0f7993267e30ccfd707739750cf87
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:44 +0000

    disappear stone bet deaf store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
