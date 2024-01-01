c74892c628437cb123419ce0f563916cf6315576
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:04:22 +0000

    bridge destroy makeinto Qq situation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
