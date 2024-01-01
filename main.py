14e9d2dc53c5a84fc5d7b94168911e7d6d85b218
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:56:56 +0000

    train conclusion crowded preparation answer

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_speed(distance, time):
+        return distance / time
+i = 0
+while i < 5:
+        print(i)
+        i += 1
