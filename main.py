43aa171884e01bbd0ebcb2b69536bebc788422b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:52:35 +0000

    war disgusting murder seat field

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
