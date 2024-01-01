cf3a6e12af83a418910f642820bcf5c62c43017f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:25 +0000

    emphasis line pump few bringback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
