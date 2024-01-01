490788defb50f7510eb1668d428312e7ca1d6b36
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:55:50 +0000

    firm brother anyone wait transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
