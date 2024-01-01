39b40f2c1122c8c86684c351ba07c11849878564
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:32 +0000

    blue neck exactly forgive help

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import time
+def get_time_since_epoch():
+        return time.time()
