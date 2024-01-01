a6af60c082178c0d1bb59a899b67a644fe1848ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:43:17 +0000

    lonely makeinto sensitive wave invite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
