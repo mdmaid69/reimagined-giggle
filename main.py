6e04486c3ea803cb2ee46bee0c904a1f0f4535cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:55:49 +0000

    responsible win fetch shoot dress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_str(array):
+        return str(array)
