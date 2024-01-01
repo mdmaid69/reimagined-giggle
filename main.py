b89d842bf9f3d668ebedb5eabf9c7068902c2c47
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:35:55 +0000

    size knowledge suppose hide about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def convert_array_to_string(array):
+        return array.tostring()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
