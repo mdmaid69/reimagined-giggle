ed58b56f39d42b50e37e065817eebaafff2a03c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:46:36 +0000

    unite join charge up involve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
