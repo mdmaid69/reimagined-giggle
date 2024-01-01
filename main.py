b3383a08f2c3abc7b10cc1d02d830412689ad6cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:35:28 +0000

    fresh area secret pretty disagree

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
