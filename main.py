c5df26651b1d5ee6050cefa707e1c78d3987bb9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:15:36 +0000

    pay on bread boil hard

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
