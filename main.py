1ef71bdc8132db997d0ac9d02099ab70cd55a623
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:34 +0000

    situation short imagination fasten like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
