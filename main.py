91c83e4094ca0e769bfc826e45954984b6371cf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:29:09 +0000

    culture young forget union together

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
