26d86a8c8a6432480c4c8ae80487d4d259642ea3
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:38 +0000

    inside unkind basic classroom sound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
