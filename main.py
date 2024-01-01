238bcb0964951ce0720cda12bf79d2d8b24966d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:06 +0000

    clothes turnaway zero club task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
