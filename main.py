bb8f3605786067008d8b7f39e1799e86d0185f4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:06:43 +0000

    form clever journey identify difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_union(list1, list2):
+        return set(list1) | set(list2)
