ce57ca7773fdd7e8a0b60bbd02ba56c51214a9f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:31:22 +0000

    bin year mouth off tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
