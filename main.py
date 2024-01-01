e1298bb4044fa0a41209824e02bf716dd79d7ef2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:37:23 +0000

    mind blue rub responsible rather

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
