32bad3092f734d0783df05926d3eeb216b43aa8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:49:39 +0000

    proper great part worried carry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import getpass
+def get_username():
+        return getpass.getuser()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
