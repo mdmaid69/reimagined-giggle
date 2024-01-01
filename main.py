82677442acc957a34f0e0056f6d5ce00cd78dcab
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:17:38 +0000

    rob serious duty bringback police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
