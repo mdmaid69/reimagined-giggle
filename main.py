1103599aa8b888f2675c7f335d74f559ce758c81
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:19 +0000

    nervous mark accident choose hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
