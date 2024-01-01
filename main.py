94a105197cb973727cbeca2bdbd5062c323430ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:49 +0000

    downstairs hate here fine challenge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
