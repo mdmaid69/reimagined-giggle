7b5af523c8929e3a2d5aa5891682f4f56bb3077d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:30:56 +0000

    getoff free settle comein door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_pressure(force, area):
+        return force / area
