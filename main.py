f4a91ac1c0799060ba618c11694b077747a7a867
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:59 +0000

    imagine hard mark trick sit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def remove_from_array(array, item):
+        array.remove(item)
