130dcecedcfbff8ac4354beba4f0976e38fbac80
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:16 +0000

    wear glad practise as study

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
