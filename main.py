e3fa2335b79bdb6cad94fc7f41b2ba35cbe8bbba
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:37 +0000

    properly study used tune stir

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
