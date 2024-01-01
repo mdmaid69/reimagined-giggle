8fb9f32ffdf3a18f5b1d1685c06b6f41295273d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:03 +0000

    side petrol burst quiet impressed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
