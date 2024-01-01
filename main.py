b45b311b745b8c2ca6a62749fd6b09e6a17692c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:14:57 +0000

    somewhere decide you centimetre church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
