e0ec438e8af87d984f89b75471bd74103ef422cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:49:13 +0000

    translate after suit guess weekend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_index(array, item):
+        return array.index(item)
