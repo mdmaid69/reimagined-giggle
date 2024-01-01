cf70b26a196d65ef303f299c9b48ee327054f55f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:52:26 +0000

    game hour rank decide couple

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
