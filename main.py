d34c9dbfd632f2b76ac7fc7c34c6290f92ec14ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:27:00 +0000

    favour nigga room point forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
