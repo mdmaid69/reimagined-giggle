6236978e9d5180079cd3ee698ddbb6ec88e43f83
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:45:04 +0000

    turn convince chin seat polite

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
