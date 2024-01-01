b5324d7b3a6232fcebf35561e7fdc40c64f25ab2
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:18 +0000

    generally goup train balance false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def sort_list(lst):
+        return sorted(lst)
