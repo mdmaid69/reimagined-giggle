615e513baa4b7586d7206ba14865cb4bd301550f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:13:13 +0000

    wish compete defend emphasize well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+def calculate_density(mass, volume):
+        return mass / volume
