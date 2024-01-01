50fc7beb4f321a131a80c096acedbb7a230f53b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:55:28 +0000

    record fire terrible tune amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
