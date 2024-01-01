dfa21a40fa2d5be19a223d9dc17fe8a1d5019653
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:35:48 +0000

    display decide unusual him exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
