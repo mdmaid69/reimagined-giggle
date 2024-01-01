1c22a50f95980f937a172c5410613451390b8b76
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:33 +0000

    disappear touch stranger wherever snow

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
