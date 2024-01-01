ee0356eaa6b5a51d3bedfb124f4b4e2dea8e6539
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:11:21 +0000

    Oo close flow dry system

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
