29d845ca40af73d3618e0b543acc44da68d4d2d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:36 +0000

    condition mistake Dd delicate claim

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
