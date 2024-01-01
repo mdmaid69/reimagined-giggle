977bfaa3b167070bf506d71cc5e11331f67ab489
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:49 +0000

    smart set shut truth yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
