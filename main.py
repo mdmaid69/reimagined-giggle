eff31b704a516240a554f3fd1464ba9a85e8fd71
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:29:01 +0000

    distant table freeze so present

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
