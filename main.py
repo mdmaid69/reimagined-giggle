fa12b5e076b9b4a22edfb4643dbd5bfc95ab8bc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:33 +0000

    wall train close decide least

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
