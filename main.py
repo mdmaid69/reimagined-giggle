6198289efed243ddff89e7b498c201406ecb2a88
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:41 +0000

    boring waste pull path description

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
 import time
 def get_formatted_time():
         return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
