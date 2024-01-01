bc7b2df17bacf502f414bb2ed724b44b58487738
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:07:57 +0000

    so stage press Cc fast

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
