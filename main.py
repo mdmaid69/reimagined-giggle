d9b9962a9f77ac2cfa2ed47b0ed859f9fdbfc01d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:31:34 +0000

    want anywhere sore agreement view

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+for i in range(5):
+        print(i)
+  def add_numbers(x, y):
+        return x + y
