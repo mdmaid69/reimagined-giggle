6913dc22d0d75f1a77175452b04455cda55cec7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:15 +0000

    previous miss here confidence keepout

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
