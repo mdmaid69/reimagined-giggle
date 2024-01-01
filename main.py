70827c454c8c9dd16a206608a7c77deeb08432b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:19:14 +0000

    second giveout image confused radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
