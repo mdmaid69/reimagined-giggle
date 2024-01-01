7ba3ffe76a65250f634c829a5da0c44e6fe6759f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:15:58 +0000

    bottom its after on store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import getpass
-def get_username():
-        return getpass.getuser()
+  def delete_file(file_name):
+        os.remove(file_name)
