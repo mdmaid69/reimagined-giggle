63aa94fd802b43e1f8fcaacf5253383924427b4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:32:19 +0000

    hurt dead get it apart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def create_counter():
-        return collections.Counter()
+def remove_duplicates(lst):
+        return list(set(lst))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
