f8efd64033fdfef3ff4e32d88842bc71bf34403f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:28:20 +0000

    discovery help answer post stare

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
