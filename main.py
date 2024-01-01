37d5c7e00f394466cc785a4e23a495aa23cb3b62
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:49:00 +0000

    cut runinto quantity minimum section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_characters(sentence):
+        return len(sentence)
