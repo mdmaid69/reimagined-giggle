d9a90ca66b22d0e6af373599a1507be895c102a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:06:20 +0000

    reliable opposite our pause drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_queue():
+        return collections.deque()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
