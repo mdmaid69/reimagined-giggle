23b829d2609ddcc236699cf4b65150f80cbdb4be
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:34:53 +0000

    polite only store pleasant callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_pressure(force, area):
-        return force / area
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
