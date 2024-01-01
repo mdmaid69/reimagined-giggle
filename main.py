e68df27133a5add1c2c7f43808fdd922768e5fa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:19 +0000

    ground performance song machine tell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_max(lst):
+        return max(lst)
