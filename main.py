4831e6b424af5bb5546ff710dde5e2b56818c9a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:45 +0000

    hope lead sound objection bright

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
