4ab60342bcd3ff0b344a28797691f0ea90e9f292
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:55:53 +0000

    Vv contain here square thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def add_numbers(x, y):
-        return x + y
+  def reverse_list(lst):
+        return lst[::-1]
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
