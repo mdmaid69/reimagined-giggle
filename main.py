780e65eb959f93a124201fd6bb2ca440bf1b130d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:47:19 +0000

    offend count whatever estimate actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def reverse_array(array):
-        array.reverse()
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
