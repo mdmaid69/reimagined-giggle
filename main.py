8f4336da663e06a5ac8e80d96d1f977d14371585
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:32:50 +0000

    frightened relief suggest discussion move

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_odd(n):
+        return n % 2 != 0
