fb3d99bdf71ca4035efa88967a400723955d4162
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:16:48 +0000

    phone listenfor treat gap pleased

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
