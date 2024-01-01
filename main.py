8d4f5898816dd5b4c7f0d325b4f02f5bfbba912f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:42 +0000

    argue sick music movement duty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
