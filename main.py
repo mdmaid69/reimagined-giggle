fa2b95b5453f46467938a65db23691f43b372360
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:29 +0000

    power taste together once interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def is_even(n):
-        return n % 2 == 0
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
