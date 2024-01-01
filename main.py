d287e9aa0614389a276b7a39c1c2441c0868450c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:45 +0000

    week weigh succeed almost battery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
