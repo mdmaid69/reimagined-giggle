b3e9cf3415cad0a92511f6ee848dc34a80053a82
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:47 +0000

    ahead appear escape note correct

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+for i in range(10): print(i)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
