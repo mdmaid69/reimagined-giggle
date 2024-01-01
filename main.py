e1fd009cebd2b3b2759fc7f321976bd5b2a5c52e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:39:20 +0000

    line pocket tense ticket little

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
