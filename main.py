ec5f2b31e716c6cd9bcd966dca99e9955669eb9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:28 +0000

    slice involve force theirs loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
