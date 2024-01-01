285fa9bf0c816138b0e1e5700d22399541d6263a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:38 +0000

    book excited inquire wish equal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
