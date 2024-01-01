34d2d7a68f3e902c008c16cdad0e5f1da47ee633
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:35:23 +0000

    his defeat behind can later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
