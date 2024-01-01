46ff91513584e50e1fce5dc81a367074cbb5081d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:58:26 +0000

    funny waste gap Kk like

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def is_even(n):
-        return n % 2 == 0
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
