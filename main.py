79c01b119d2e8aa9da37b4385959453fe6e8f2d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:26 +0000

    sight corner error raise from

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def print_python_version():
+        return sys.version
