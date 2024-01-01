d949beccb25c012580b78f7e56e865950e602f8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:03 +0000

    float perfect workon thorough live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
