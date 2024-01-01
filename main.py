105b1c89ccfddd905f090d142184afc806ad5ba0
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:44 +0000

    payment straight miss thinkof choice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
