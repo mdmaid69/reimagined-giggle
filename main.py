8fde97b81ee68ff2c86c177700fdecbf0d3946f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:36 +0000

    ability lock gate contribute comment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
