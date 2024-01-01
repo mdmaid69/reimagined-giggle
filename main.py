280c9852737fff9fa94a8b2aabb0aa07921af84b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:01 +0000

    dance interruption prevent confuse everywhere

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
