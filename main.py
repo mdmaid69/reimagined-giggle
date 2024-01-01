6659ec19b4b34f1e644882f214566c5a1e4707ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:05 +0000

    getout content use poor clear

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def square_number(x):
-        return x**2
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
