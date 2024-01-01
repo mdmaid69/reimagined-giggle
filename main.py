8f5992dfed8c473abf9a8c33ff14dce16dc02029
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:23:14 +0000

    play owe comfort discuss force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
