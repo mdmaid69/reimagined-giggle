df8f09affa0bc9b971f96402a9c414f6b7991021
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:23:43 +0000

    reasonable pour stretch imagine behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
