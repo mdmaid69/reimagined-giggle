35e63dd2d08330fb1860f587fd5f3246e1d5fa6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:39 +0000

    probably judge always open low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
