adbb1a86c6b95f9e3c0655e74bc77b9ced81992c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:07 +0000

    wet walk congratulations agreement music

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def convert_to_binary(n):
+        return bin(n)
