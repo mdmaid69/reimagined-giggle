933452a7e73a130036c29726e44ae310fcb76ee9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:30:10 +0000

    water transport stranger fact material

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
 def split_by_pattern(pattern, string):
         return re.split(pattern, string)
-import numpy as np
-print(np.array([1, 2, 3]))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
