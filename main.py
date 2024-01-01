b9b1d1e1377380a8aff2ad224a9e4fff1ca23de9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:27:50 +0000

    ours container lock wait subject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
