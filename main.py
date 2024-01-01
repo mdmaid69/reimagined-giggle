aa4555b46fad7ab568e86ef01e6fef2c5028c4c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:35:15 +0000

    little ignore hello noisy succeed

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def exit_program():
+        sys.exit()
