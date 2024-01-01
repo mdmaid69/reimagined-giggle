8b9c0845ef250cd70f87d5ea84afdbd3710d1367
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:53 +0000

    active noone illness collect stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
+  def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-  def convert_to_binary(n):
-        return bin(n)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
