dc28341e98f6d4923b63a490e911be9dc2396eb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:10 +0000

    trust run object regard schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def find_min(lst):
+        return min(lst)
