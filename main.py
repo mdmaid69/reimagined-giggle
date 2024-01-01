38f8da7d637fdfb474ac97a42d32af69a505c323
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:31:10 +0000

    talkinto confidence wild coat runinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
