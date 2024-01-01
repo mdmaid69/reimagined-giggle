11e4ea01d13ef681aeb7ac0edf084e6f78b546cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:25:52 +0000

    face surprising race artificial create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import sys
-def print_python_version():
-        return sys.version
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
