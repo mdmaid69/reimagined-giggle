5a99d73beccf2a203698b328b22d57cecc3ff4d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:41 +0000

    position future rid refer yet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
