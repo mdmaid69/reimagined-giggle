22e61f062f6b0af3035ca441ee10f03450554dc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:22 +0000

    target provide disappointed still woman

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
