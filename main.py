48081d8dda521f052f31f0cdae4da8040d51e3f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:53 +0000

    goup command usually space secret

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
