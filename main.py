07f0940f08a1c1e77f30f9ebb51349a245d10f5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:16:06 +0000

    getup though rent in thumb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
