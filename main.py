9a9ee730cd43682074b2bc42d19c8f74b78786fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:18 +0000

    remember nigga burn mine comeback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
