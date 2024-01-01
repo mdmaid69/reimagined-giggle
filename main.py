f66ed23de5da488e008004b165d89b2e9aa55b2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:43 +0000

    agreement hurt partner target stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
