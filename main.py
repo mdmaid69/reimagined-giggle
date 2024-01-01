05436ccfd1d71daf0d7ab13e914c03bb3d26a744
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:13 +0000

    hold order hour thick fire

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-print(sum(range(10)))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
