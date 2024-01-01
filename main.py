e93f1ee73965fec843a27eade8d633f54838eb10
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:46:36 +0000

    famous sink table guess injury

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
