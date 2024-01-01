e613c4f73ec9bae2d3621b2c69a226779b3a2584
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:20:01 +0000

    callup false lot pause guess

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
