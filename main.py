d9acbf2b95d8d3808fcf173a0a1e601659dd6dd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:33:27 +0000

    get attempt wide still stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import sys
-def exit_program():
-        sys.exit()
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
