e70c7a7c033ae7fc22b28c5a9702a25fd814facd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:42 +0000

    invite along tell unable slide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+print(random.randint(0, 100))
