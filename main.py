01b21b5b63af03ba801ae177875a79a57f7488a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:46 +0000

    comeback harm everywhere circumstance label

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
