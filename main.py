2906dfded9d117fcd15a13a7da8339f66b76893e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:06 +0000

    serve content forgive holiday temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
