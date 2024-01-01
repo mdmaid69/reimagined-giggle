3e621326aed8b58ca37d3c61735941734e413a31
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:58:31 +0000

    Itself independent excuse land positive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
