838406920cbbd9e5463af477121c2225dba1d29a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:26:29 +0000

    fat criticize protect hand difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
