63ed0e5664c9af76096582bb121419d8f85cd716
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:24 +0000

    split used lay remain finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import time
-print(time.time())
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
