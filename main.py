c76c88f60c7d992d977729bb1de4891400a88586
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:49:09 +0000

    content cinema strong distance charge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import platform
 def get_python_version():
         return platform.python_version()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
