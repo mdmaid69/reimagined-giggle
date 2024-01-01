aee80529c671f3e38a84a20f264d0c6ad6aa6eb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:58 +0000

    left scene wing use rain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
