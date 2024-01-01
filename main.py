8c56d4684d15c572c9997e8527bc8065a6a65163
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:05 +0000

    step right unfair be embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
