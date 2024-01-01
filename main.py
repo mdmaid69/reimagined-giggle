9b97316b1b6f57cbde64e0a335a346c947ad45d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:31 +0000

    mine borrow interfere area as

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def convert_to_octal(n):
+        return oct(n)
