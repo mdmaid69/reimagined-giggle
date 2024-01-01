0ea073a0c7f208cee2af5bc9fbb5a84f7ecdd2a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:07:25 +0000

    let impression snow fun over

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
