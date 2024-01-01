54c7688f3c5637d20c68851ad69be92aea643fad
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:41 +0000

    why outside turnaway free Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
