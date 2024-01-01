409b8f58b5e4e3952415073cbb3229ce1666dfd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:47 +0000

    definite complaint present border advice

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
