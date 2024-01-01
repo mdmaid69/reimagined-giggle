4fa18070a5741aabbcdb966b2d8f7e78a561840f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:43 +0000

    reason opponent pick team outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
