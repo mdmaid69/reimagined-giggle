52c51932683e56254faeb9727d2f8ca5113cd4ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:24 +0000

    strength actually important blame but

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
+import sys
+def print_python_version():
+        print(sys.version)
+import queue
+
+q = queue.Queue()
+
 for i in range(5):
-        print(i)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
