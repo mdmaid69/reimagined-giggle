2e4cec331e2972a112d5ef2e52885e7296c40200
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:12 +0000

    building later it strict nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
