b475872681939d83160ef655d61d3449383e845e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:58 +0000

    last against heat lot direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
