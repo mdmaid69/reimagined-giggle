e86137243e5280f2126ad17294850f77b8bb48fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:06:51 +0000

    amount state unlikely answer succeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_set(array):
-        return set(array)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def is_palindrome(s):
+        return s == s[::-1]
