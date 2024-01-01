dd5c101ecc0cc55400baed73bb26972c43507c71
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:09 +0000

    suffer result discovery work address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def split_path(path):
+        return os.path.split(path)
