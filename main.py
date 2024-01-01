30331bbc44b60fe2716079b4b358bca254cc8ce7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:04 +0000

    round demonstrate around material very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
