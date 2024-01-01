347ff93fa55d43b813c7d8e89dbd2eaf33d90edf
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:10:40 +0000

    wave play storm single certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
