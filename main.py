09ae854fc7326d383ab4d6bc23f472de1e292e37
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:25 +0000

    callat except club cost keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
