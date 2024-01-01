9887e86bbb9abfa6d159ac329b9c5d651b3bf8e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:16 +0000

    not should mend pollution fall

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import logging
-def log_message(message):
-        logging.info(message)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
