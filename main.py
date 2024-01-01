170d9b87651e681dd6b9184bdbf2f75ee5144f5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:08 +0000

    sport spill refer scale pause

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
 import math
 def calculate_cone_volume(radius, height):
         return 1/3 * math.pi * radius**2 * height
