622d8753b86f887959e72d3cd918af373af836ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:20 +0000

    delight continue minimum exciting narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
