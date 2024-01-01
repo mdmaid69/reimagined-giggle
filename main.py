2026401164870690c8a2cb806537770b8097b3df
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:43:28 +0000

    talk sorry nor ring human

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
