f53adba92f205366eb2ba829c256b9921b4ff5e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:44 +0000

    problem performance sure unfair intend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_counter():
-        return collections.Counter()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
