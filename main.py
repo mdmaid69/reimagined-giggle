2da5377096e09433a986e9c59eabc6e2ef2142dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:06 +0000

    study meeting exhibition arrangement tread

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+print(math.pi)
