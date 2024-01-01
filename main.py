556c1185f2fbf34f6d1dbbeb1c1cc1af264b48a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:42 +0000

    forever produce arrangement shop common

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
