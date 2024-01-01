22a15132ad174c267056eca02fcbd83e36e05449
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:00 +0000

    pronunciation block takeaway proper comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_union(list1, list2):
+        return set(list1) | set(list2)
