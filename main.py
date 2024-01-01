9b23113edfdda6faf35894ed1c928c117f23b205
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:24:52 +0000

    write confident borrow getout keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
