c84e1f9f68534b9bdf15304d1f03b96a44186c58
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:29:02 +0000

    embarrassing advanced attitude scream pretend

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def is_even(n):
-        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_set(array):
+        return set(array)
