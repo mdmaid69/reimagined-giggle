0322620ba704c3cf481b672e61a1ef3e7b8e8934
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:24 +0000

    improvement for weak that cinema

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import time
-def get_time_since_epoch():
-        return time.time()
+from collections import Counter
+print(Counter("hello world"))
+def find_max(lst):
+        return max(lst)
