cbaa76a8d7f3336d20dece51602e37750bd71c21
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:30 +0000

    however no convincing getaway land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
