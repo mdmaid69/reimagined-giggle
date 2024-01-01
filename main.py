99b493f2dd4b79ba968295fac6fe9153ba09d066
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:29:58 +0000

    far emergency scream wrap shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
