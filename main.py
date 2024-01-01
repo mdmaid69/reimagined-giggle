ca18e6b3fbcb39f601eea8a5e895be0e551824db
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:49 +0000

    uniform anywhere shock deliberately autumn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import sys
+def exit_program():
+        sys.exit()
