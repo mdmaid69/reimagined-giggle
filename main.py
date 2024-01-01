2abe60e96fe1e93294ff520b44f6e03ae7a99827
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:22:23 +0000

    share accordingto mine own ready

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_set(array):
+        return set(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
