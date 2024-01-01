5c9f73a37078deaba0101d3e222be312bb926405
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:58:23 +0000

    knowledge valuable nurse cheat be

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def find_max(lst):
+        return max(lst)
