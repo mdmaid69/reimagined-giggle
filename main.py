4902c4b430a67623b79481b9ac15ed0d2653a70b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:47:45 +0000

    turnaway 572enter free question left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+print(os.getcwd())
