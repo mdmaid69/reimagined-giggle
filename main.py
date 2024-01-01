17d2406dc68f61687b6001cbd3b50d155615d6e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:52:16 +0000

    realize off care basic laugh

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
