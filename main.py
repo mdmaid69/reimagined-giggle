97ea8f09bb07688516c89945c422f3ee3a962d90
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:23 +0000

    control authority report or opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
