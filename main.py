ca6dda753ecebad33da5e29d4722cf3ef6996c41
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:32:31 +0000

    fresh rescue last large week

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-print(math.pi)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
