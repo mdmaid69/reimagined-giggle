cf7f94b04ffc897739e1e787cacb62e02552e915
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:43 +0000

    obtain excited right area impression

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def find_min(lst):
+        return min(lst)
