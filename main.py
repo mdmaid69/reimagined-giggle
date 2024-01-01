760ae034df5de1e9f1f462bea0204714123d97f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:51 +0000

    plenty accident related fun promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
