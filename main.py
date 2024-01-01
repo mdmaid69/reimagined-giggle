339dafe7e67feff3c2014e535bbd9f914680fd66
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:18 +0000

    poor getout father convincing blue

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def find_max(numbers):
-        return max(numbers)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
