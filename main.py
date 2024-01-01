a43d5b5e2adf7664a0486829252e16d8675eccd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:21 +0000

    last mess lookup increase pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
