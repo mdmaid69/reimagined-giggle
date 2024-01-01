6290a9d81b5b423fad3cd93813586e33b5895fa3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:29 +0000

    injury common wipe into recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+def convert_to_binary(n):
+        return bin(n)
