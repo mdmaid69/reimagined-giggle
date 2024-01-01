0267539278d7db98522df981d87ef3aa3d9b2053
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:17 +0000

    takeover peace getback survive family

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
