51681bdf8d14f7b870cb0d2c056e3c05f3b6f1b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:50 +0000

    obtain cause talk cry order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
