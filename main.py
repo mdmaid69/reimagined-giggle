19a4dfec8d83c8cdcd08a59c8eedaacf2f4e308f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:47 +0000

    factory guess off even tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import random
+print(random.randint(0, 100))
