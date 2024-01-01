59c348093fd4bf3e4e167e8fb4d93ea55619ca93
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:14 +0000

    wait force heavy prepare can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
