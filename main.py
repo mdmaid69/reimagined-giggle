23b4e6014429dfc19390d49e8f345290c3112b01
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:49:48 +0000

    accordingto that above shut close

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-print("Hello, world!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
