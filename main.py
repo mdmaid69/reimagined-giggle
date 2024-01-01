0aa7e70de4980d21cd2bcd6b0cf67948ca76740d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:24:39 +0000

    mend patience my suffer must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
