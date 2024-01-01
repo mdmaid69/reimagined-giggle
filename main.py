b24a590aa1b6446f8a1eb191250fccb0e14d807f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:17:31 +0000

    dear slow keepfrom commit progress

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
