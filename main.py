8f866079bfa582e6b5264a90c9eb603c01d4c8d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:49 +0000

    favour effect bite disappear his

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
