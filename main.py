1a90ff8616ccb089c873bf6b46ce4d7e538fb382
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:21 +0000

    exam mix getaway board gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
