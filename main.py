e5d5b2bbd2bf6d6b0d815157b4928ea414f35680
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:39 +0000

    deep story hard ill fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
