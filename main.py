af7f764afb297022a6c96a771f988dc1855ceb39
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:16 +0000

    comment trust fortunately add stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
