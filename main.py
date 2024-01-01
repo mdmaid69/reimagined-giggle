c129f2e9bb40631fb9c492020ab29adf880a65d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:32:43 +0000

    fork safe cut upwards large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
