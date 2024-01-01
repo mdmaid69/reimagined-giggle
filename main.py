d6ba1be518601c7920e3e6d647bdb9fde747e52f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:33 +0000

    anyone on own nose touch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_current_directory():
+        return os.getcwd()
