143c0e54a63a4c5b981f5d8ca5d61c1e6a7189a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:29 +0000

    summer somehow sign play stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import platform
 def get_python_version():
         return platform.python_version()
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
