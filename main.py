12feaa8d5370c55c2c93693a24e172a87ea532f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:42:21 +0000

    advanced favourite agreement underneath go

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import collections
-def create_user_list():
-        return collections.UserList()
+def create_user_dict():
+        return collections.UserDict()
