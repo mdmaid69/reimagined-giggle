2d013c3191181999f5671ad72271b3ed69f3ebbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:40:36 +0000

    last guide welcome total cool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pressure(force, area):
+        return force / area
