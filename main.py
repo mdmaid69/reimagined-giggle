d966ee15a5245dcfb1be07ce5a231e6b1d526cc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:30:43 +0000

    disapprove on Ff ilovedicks dry

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
