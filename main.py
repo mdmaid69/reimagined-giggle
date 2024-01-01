77040f7c3f5ee16ea7cddd53c3225ecaa79b5360
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:04:23 +0000

    back race tip godown ordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
