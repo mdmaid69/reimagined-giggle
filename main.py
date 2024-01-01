944012b9edb7ac8b662c3bd813876a75b8982847
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:46 +0000

    shock sting almost mixture tread

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_list(lst):
+        return sorted(lst)
