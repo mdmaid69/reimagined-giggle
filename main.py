495bbf84ae7c45343fd40a5208780e9e6fb28c6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:19:36 +0000

    grade noisy willing afford outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
