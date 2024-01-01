ef19ab707ad7b853b2e2d15e115d5fb505bbbc07
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:42:34 +0000

    natural answer to sweep strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
