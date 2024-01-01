2e82adb3eec90cd2901cdc11e65917e7824179be
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:16 +0000

    too thinkthrough copy either say

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def square_number(x):
-        return x**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
