e4e04222922066a73cc49f004d668901d7ce5401
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:54:41 +0000

    prevent defend convincing excuse wipe

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import platform
-def get_os_info():
-        return platform.uname()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
