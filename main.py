3127d9353a93b52230403693c0311cf73a960c2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:05:39 +0000

    flight standard past both Ss

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
