7a9217071727fef57743e2846e530ebc080f7ee3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:21 +0000

    Tt runout on learn take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
