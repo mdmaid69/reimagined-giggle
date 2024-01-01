35f48f9a1205c7ddbbf53c65e15c90e8bc265dc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:08:27 +0000

    sort home control permission sick

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
