576044b46bcd81f35d0e85413ba9ba529675aa2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:27 +0000

    hungry call boil obvious body

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
