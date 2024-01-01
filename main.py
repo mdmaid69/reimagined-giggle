cf7adf99bd3c99d9e14379f69e9074264049f0ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:18:49 +0000

    coat bush wave scheme mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_odd(n):
+        return n % 2 != 0
