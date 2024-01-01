c2ffc50c92f0204f136fd329b3c2ac8447feb7a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:46 +0000

    lower heavy platform proper special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import datetime
+print(datetime.datetime.now())
