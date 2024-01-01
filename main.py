dc798cbd0afd5c3f91a2f16ae8814816ac134de3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:41:35 +0000

    but try noisy so limit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
