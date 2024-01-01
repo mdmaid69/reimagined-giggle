e0b2a2768ea08fc550969cd24ae139757bbbfb44
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:38:18 +0000

    already attract wind wish guard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import time
-def get_current_time():
-        return time.time()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
