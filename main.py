ca164fe82ca3815e4dfbe93f7806d825ec36bfb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:19:07 +0000

    increase ticket inform hold tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
