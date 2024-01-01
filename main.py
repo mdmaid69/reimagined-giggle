4d0d7ffa7f8697ca9c859b11e124385642db4f9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:35:26 +0000

    takeaway open nature dare leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
