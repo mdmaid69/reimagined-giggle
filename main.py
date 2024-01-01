09164f0ca1c05139b7244f9bb69de2aade4637ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:45:22 +0000

    characteristic decide turn wide meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
