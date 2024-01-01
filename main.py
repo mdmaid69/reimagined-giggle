c315d0254f851277227fdc46d8d993f4ac605236
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:59 +0000

    off silent drop hollow supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
