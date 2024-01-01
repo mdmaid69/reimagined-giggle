88f8a561175dde7f76d2318e7444a17424256d81
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:37:31 +0000

    borrow skill quarter still giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
