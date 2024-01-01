1a915756232d908ca317d07f3620f36d8e5e5f2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:58:41 +0000

    watch both turnout unite protest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
