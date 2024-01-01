0c8ef7b4ea98dda6803be691136ed47742e8606a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:45 +0000

    agree hungry on mean age

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-print(sys.version)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import numpy as np
+print(np.array([1, 2, 3]))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
