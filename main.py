c47386f5c9008e9c884c7f2ea8bb5592a12bc214
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:31:28 +0000

    physical professional coat wide foot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
