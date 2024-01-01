0b0c5f63c741cd74a7e2f000babf75b01b73c070
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:19 +0000

    recognize suckme centimetre opposite meet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
