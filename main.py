3474dac8265b10b5961755ffef387a528c571dc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:33 +0000

    way start shopping least pleased

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
