c653f8e71b6664094de4105197852e655abb7548
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:01:27 +0000

    surprise block more tight claim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_tangent(x):
+        return math.tan(x)
