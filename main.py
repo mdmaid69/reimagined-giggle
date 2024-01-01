ee4fb279d21519dd4eaff9a64674dbea31cd3497
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:51 +0000

    influence school steep centimetre joke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_list_from_array(array):
+        return array.tolist()
