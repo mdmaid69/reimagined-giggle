d95efa713fa7823f8cf1dd6b88c14b160fd85cff
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:38 +0000

    refer chief impressed regional autumn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  def count_elements(lst):
+        return len(lst)
