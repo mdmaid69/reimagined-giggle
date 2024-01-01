aa6de820bc898f3441a52b5ded046c1e98396453
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:20:55 +0000

    ours wing independent unusual stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
