e138d617523bcef14f4f5d71f4329c569b425c66
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:43:32 +0000

    come important become sign shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
