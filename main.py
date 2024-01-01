1a60f5795f9c76847a6aec4a67fab019608341d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:26 +0000

    to impression appear snow wish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
