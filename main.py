21068acf8c8f2faa9e5aa6ad73683306eeb5a51a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:17:10 +0000

    passage polite circumstance central nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
