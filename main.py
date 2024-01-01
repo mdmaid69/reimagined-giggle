614c68e6f6c8e7215a61954b339600485fcd7466
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:50 +0000

    hold thanks heavy contain join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
