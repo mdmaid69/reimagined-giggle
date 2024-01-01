3cfa48d2ed3f9dce9ff1ebf83e46e726fc88fec8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:35 +0000

    degree the fix healthy fly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
