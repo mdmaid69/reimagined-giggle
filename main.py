9bd2d3f9b73acf0611af51459cb5114a9f0ca97a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:52:42 +0000

    hurt spin limit criticize demand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
