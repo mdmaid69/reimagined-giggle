36fbe148b1d3f369b035bd7f07c05d7e8132a7f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:33 +0000

    friendly exit onto supply over

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def convert_to_binary(n):
-        return bin(n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import time
+print(time.time())
