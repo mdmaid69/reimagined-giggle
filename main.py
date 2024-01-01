af1ef8cfe68d9c52cf6bad44bdeed659b169319d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:46:28 +0000

    sell matter rise money fly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
