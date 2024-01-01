92d033c80e501df5537c3700616d88686a9756fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:52 +0000

    put stranger weekend ahead trade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def sort_list(lst):
+        return sorted(lst)
