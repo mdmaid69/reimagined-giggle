44abc440d7e744cece3daf8593f1f43a58d4bc17
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:16:58 +0000

    truth turninto opposite myself breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
