2d9be6a891ebc08f9be1477150302b305ab3619a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:17:00 +0000

    relax contents compete trouble room

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
