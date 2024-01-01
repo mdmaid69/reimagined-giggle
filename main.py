976364a7c3f14767e184d678ffb0b84397b2f2a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:44 +0000

    suddenly crash calloff below brother

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
