38685a549df58fdb3db451f10580867166f17bc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:19:03 +0000

    lookback(on) light exit square among

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-for i in range(5):
-        print(i)
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
