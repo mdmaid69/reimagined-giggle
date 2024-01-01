36cbd50aaeb0597d7aa896bd620a2addf608f156
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:06 +0000

    line cheque related display keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
