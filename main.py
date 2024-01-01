4b3b1f8eece6ffec6c2c9a94c70ed9841c6366b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:00 +0000

    straight Uu cope measure sore

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
