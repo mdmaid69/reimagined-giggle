44655969ffbbffe71ca075dec19d16ae1335cd82
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:29 +0000

    long act age confusing share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
