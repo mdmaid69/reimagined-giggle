f31a3e3618177f0f9daef73f78026cd489a04d13
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:41:10 +0000

    fork objection manner single over

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
