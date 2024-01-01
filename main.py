9fab9a5e098385048c8da687589b6a1532b9dd61
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:31:23 +0000

    natural frightened demand division once

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
