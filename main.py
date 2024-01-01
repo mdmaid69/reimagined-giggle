f3b4d4c81d747b96bbb9dcb2f6303b344413932c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:49 +0000

    impression persuade back business support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
