9fea383597488e0f8d523ceb8fa3097638883b72
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:38:25 +0000

    wherever closely certainly pin learn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
