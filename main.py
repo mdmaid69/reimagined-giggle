417e6a2495fb8764a8354a817365db95780e310e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:58:48 +0000

    edge tool determined help top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
