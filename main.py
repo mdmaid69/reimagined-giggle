673a3694df20bf8d864db90a28983cdca52f0503
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:35:02 +0000

    cut could should surface later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def is_odd(n):
+        return n % 2 != 0
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
