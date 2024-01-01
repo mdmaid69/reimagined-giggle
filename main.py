14d002341036226602ed889fb4c310ea94ce74cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:57:59 +0000

    formal up forward pronounce field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+for i in range(5):
+        print(i)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
