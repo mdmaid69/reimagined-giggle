9c54eb366eba152141d25d3fccb44517fe6573fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:56 +0000

    expression attractive straight last wash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
