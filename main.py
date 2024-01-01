4fe966885db8789eb856ad629b2b577b77dc0707
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:42:48 +0000

    Vv tense subject unlucky turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
