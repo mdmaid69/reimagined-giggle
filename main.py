75ad79a348f5f1ac769dd29091c887550414598d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:05 +0000

    violence pity tread false clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
