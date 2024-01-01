dbcf89893d90f16d83afc00f471c7d3297ef972d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:01:04 +0000

    game say forgive comeup birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
