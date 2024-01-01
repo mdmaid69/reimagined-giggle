a907730bfc89537411706202031b915a416be6a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:06:37 +0000

    owe collect language please store

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_pressure(force, area):
+        return force / area
+def convert_to_binary(n):
+        return bin(n)
