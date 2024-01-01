1b68d4308a0ffe2123938fa63337d3f785abcf90
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:31:51 +0000

    abroad draw powerful tooth needle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
