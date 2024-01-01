a381671f713dd51f001a3c3ff955e5a60ed4901d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:31:15 +0000

    stop someone old burn judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
