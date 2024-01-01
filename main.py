5447e94161b06bd2b37cbd27858e6512ddbb05de
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:32:48 +0000

    characteristic minimum morning tie then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def remove_directory(path):
+        os.rmdir(path)
