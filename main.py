11822410630333a57f62a4c2652853d643ba2a09
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:09 +0000

    guilty drive him film or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
