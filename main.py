19b5531d2a72266e57b00dad671d6d855b757098
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:27:39 +0000

    uncertain getaway pain shine cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
