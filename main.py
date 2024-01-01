c8ae1b718b7b0f5888a2567b59486cdca0f21ee5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:14 +0000

    impatient the cinema contain lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
