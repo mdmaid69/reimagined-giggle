cc814d5165ac87ef41f5f9e08a0717f27a06cbf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:15:05 +0000

    pot shallow turnout preparation idea

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_odd(n):
+        return n % 2 != 0
