c35749dd850bb80105034c9fd74bc8418ca48b5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:15:43 +0000

    on hungry interest geton juice

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import random
+print(random.randint(0, 100))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
