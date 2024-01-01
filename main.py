664d892596b408e716ae3ceaeccdeb9021c63055
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:08:16 +0000

    name especially breakout ground quantity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
