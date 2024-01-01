c8bed66542c1d2371031069d1bfecdf0bac0d589
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:42:10 +0000

    than silent brief pass top

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
