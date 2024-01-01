edb6926c1633b910918390388b94987fbec4950b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:16:15 +0000

    slide apologizeapologise production empty Uu

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
