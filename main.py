8bc00ad93cc9c986fbc5c664ffbbb70806f171c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:20 +0000

    total business free slowly blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_base_name(path):
+        return os.path.basename(path)
