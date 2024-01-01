36e4280517d033432bfc9aac2421f06d15e7367c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:00:33 +0000

    Ii later shopping before set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def remove_duplicates(lst):
+        return list(set(lst))
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
