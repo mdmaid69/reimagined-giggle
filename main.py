c9df2397ebe66d863966f4dc1808969a172e04f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:11 +0000

    form moral nearly behaviour agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
