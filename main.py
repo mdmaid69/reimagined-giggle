6c9a44e60b1c780fcb9b3500164f89153df92872
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:25 +0000

    chance passage rid pipe man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
