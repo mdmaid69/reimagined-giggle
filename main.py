a91732eb6eedc71893fd0e27abd1c3629f15f19e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:57 +0000

    strict study vanish birthday course

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+print(sum(range(10)))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
