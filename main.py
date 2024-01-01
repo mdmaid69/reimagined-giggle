cc4eba45e4b8dd6b5697d870899116bc7fb126aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:49 +0000

    spray forget successful disappointed call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   def find_max(lst):
         return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
