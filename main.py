6a5aea6b2d7f3f794665be48ffad9d2c31c07abd
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:07:41 +0000

    mass deep no now trade

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
