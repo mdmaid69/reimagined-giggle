ff7944415cafd68c1d084d9ecbd4eb6ff2e1c95d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:34:15 +0000

    keepoff favour organization extremely Ll

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
