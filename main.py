f5ed295cd35b474550b99992bb567960296ff701
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:24:28 +0000

    admit none draw loan evidence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import time
-def get_current_time():
-        return time.time()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
