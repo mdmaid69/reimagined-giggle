a562f9d658ff8b30a7fd8c9d804d03fc2241f78d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:05:15 +0000

    room drawing strong always rush

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
