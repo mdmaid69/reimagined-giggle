d4fdcad12e524707c8a4725c7952c33d4e95c361
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:03:06 +0000

    comeup concern search glass actually

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
