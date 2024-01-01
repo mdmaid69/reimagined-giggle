820b2cc34c07221f8314f0791bcd54a33f3dd010
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:52:43 +0000

    turnoff shout slight complete possibility

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-for i in range(5):
-        print(i)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
