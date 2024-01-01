48c61f54f248d724d0ce84003c3916892e1a4e31
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:52 +0000

    bet limit second letter show

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
