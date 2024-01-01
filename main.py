d5276f66faca72a0469f650e2451375c67c5d326
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:30:03 +0000

    only then active already thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def delete_file(file_name):
+        os.remove(file_name)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
