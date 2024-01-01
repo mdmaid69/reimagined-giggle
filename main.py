45a7eb89a52b755651992e12676da69f4cc928c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:59 +0000

    scream promise forgive climb world

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
