fa3a631868bd6a5283b92eb7c01f38306a354620
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:26 +0000

    concerned direct exercise usual busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
