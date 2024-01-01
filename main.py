b3d17cae1cac21d650d495f6b772e9548a38bc5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:04:06 +0000

    public steal ability pin woman

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def reverse_list(lst):
-        return lst[::-1]
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
