52bc032e71c9b67a54eba9208fecb45c0274b884
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:03 +0000

    future thought turndown cross pity

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
