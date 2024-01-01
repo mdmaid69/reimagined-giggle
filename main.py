dabe9edc210f9428d9405a4370df0791312d6d53
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:09 +0000

    shrink poor cum underneath wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
