32af702ac9ef7b3c3391e083acec8d207c5c21e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:19 +0000

    cheat information delight original admit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
