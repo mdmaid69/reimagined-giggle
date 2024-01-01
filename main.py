b7cb1135f5075b1d3d286e5d739f2106afa0c85e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:44:48 +0000

    introduce knowledge powerful anything turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+x = 10
+y = 20
+print("Sum:", x + y)
