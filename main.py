7ddcc0514a4ac88529373d4fcee5af76337fefdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:57:00 +0000

    Kk anybody method through section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
