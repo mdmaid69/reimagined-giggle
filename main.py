43f1144344e65274f38dee9b7c922b3cb5698665
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:54 +0000

    if well best afraid difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
