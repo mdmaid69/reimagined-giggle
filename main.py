c78955e5f761d44fc26c6dbea4240d65e4768523
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:53:04 +0000

    uncertain getoff life to chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
