46a2ff9ade22fbe38540caf0041b5f9f5a894906
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:43:45 +0000

    black print flood satisfied name

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
