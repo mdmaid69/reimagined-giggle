dd855917458eb5f54cd2c6ed27a1d744386ede73
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:38:46 +0000

    picture mess appeal effect distant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_speed(distance, time):
-        return distance / time
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+for i in range(10): print(i)
