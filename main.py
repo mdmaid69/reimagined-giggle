6d6a1a5820c8276dff38f24a8c9802171811ad5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:40:04 +0000

    better broken behave upset such

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
