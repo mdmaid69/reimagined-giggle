a049ba700a68aad40bc84e9b3aa5a85e93890b1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:34:48 +0000

    splash cry change weekend full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
