15272f6f34998c1c1842d2d562575ca90bb4d997
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:21 +0000

    body turndown centre always murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
