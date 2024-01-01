75855054a512a26f32b227eaad5ed0a394abd8a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:34 +0000

    police wish inside female control

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import sys
-def exit_program():
-        sys.exit()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
