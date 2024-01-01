44b641f4327d0ea10e371263967bca163d053961
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:09 +0000

    nice sell 570enjoyable surprised all

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
