1130549b6206d186b25adc7a244080e213a90033
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:17:29 +0000

    die worth cross smile black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print([x**2 for x in range(10)])
