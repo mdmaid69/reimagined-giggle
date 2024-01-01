851b2b403276a4af4d6a8577df0563b6892c74c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:20 +0000

    swimming getup niggaacademy commit support

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
