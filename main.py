aafa776dae1b3b1abdd73c373c7d60df53d4133c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:58 +0000

    central above report reasonable ear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def count_elements(lst):
-        return len(lst)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_repr(array):
+        return repr(array)
