167dbd4ee00b1c63a4e341bdf1767e54295d776b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:16 +0000

    recommend voice mysterious would Ww

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
