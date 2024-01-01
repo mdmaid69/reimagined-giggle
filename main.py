6b2e24eee5b6bf9753e014f02724650c14b011b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:09:26 +0000

    wall goodbye him turninto calloff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
