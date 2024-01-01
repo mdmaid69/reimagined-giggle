7a0b8482a609c0eb35bd4f642c933e778f670afa
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:22 +0000

    force realize lean theirs search

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import os
+print(os.getcwd())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
