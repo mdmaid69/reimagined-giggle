3146d4942baad9d6fc914211fa1947fdc7ba216f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:42 +0000

    stupid play accommodation play display

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import time
+def get_current_time():
+        return time.time()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
