4cf88bb3049d86a95130cad9ffa469713c397645
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:05 +0000

    mistake practise method speech remain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
