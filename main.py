6f758f44d15b3ca40edae8ca5e06575c8e49787b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:17 +0000

    feel lie original break patience

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def square_number(x):
-        return x**2
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def find_min(lst):
+        return min(lst)
