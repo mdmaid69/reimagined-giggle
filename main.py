d7ee1ba13fb3a543ae0965c5e45fe11022dd62be
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:06:19 +0000

    skirt himself next confuse shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
