7023ad246853433455139d0cbc46611eaeeea704
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:41 +0000

    mother 567enemy rise late lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
