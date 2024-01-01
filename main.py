c559abd991fb15be50d5338d6fa37cce3eda4b1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:57 +0000

    importance purpose prefer safety across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
