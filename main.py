6a963a8f6de2b8458e4aeb7007566530e79f1bb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:00 +0000

    die personal when thinkover friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
