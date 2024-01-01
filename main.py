e8a38d500a7f487ffe8e0cbde10a2e996e074e1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:41:47 +0000

    step convince upwards bored mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_power(work, time):
-        return work / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
