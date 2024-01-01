96084b18deb88d858885aef522f8be74b4cbef31
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:22 +0000

    hurt way bread shine answer

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def add_numbers(x, y):
-        return x + y
+def reverse_string(s):
+        return s[::-1]
+import array
+def get_array_item(array, i):
+        return array[i]
