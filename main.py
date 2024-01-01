a6a8ae7804978df8a875a478c8a43a1b83be981e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:32 +0000

    close wet genuine abroad solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
