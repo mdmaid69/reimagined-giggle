a07ba3b50fa5b6311a17c770d9f477d808bdcb4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:07:22 +0000

    572enter trousers furniture takeaway spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
