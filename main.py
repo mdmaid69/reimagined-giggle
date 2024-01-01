9ab64db5bb70d09e287affed2c93e3e41906434d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:42 +0000

    nature live move usual demand

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import datetime
-print(datetime.datetime.now())
-import tensorflow as tf
-print(tf.__version__)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
