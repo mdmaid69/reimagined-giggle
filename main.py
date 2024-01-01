e34b56d220b175abee59a4d61021a1278501fb2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:57:28 +0000

    thing consist why chat opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
