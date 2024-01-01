7d76b36ab865e4ca6e1ea406832576ede071e87d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:45:59 +0000

    class duty connection serve dry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
