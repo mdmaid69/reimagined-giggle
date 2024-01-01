6de47c5ae49eff44f1a6cbd9fcd0119d12cf3cd2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:44 +0000

    picture work young information happy

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
