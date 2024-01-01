780a17bed4395a7f8c6046e87b4394b4cd97bd8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:46 +0000

    tongue right port crime pleasure

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-from collections import Counter
-print(Counter("hello world"))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+for i in range(10): print(i)
