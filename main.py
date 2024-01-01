a30438e51cd9d95313b7285d399f3b104848c5c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:39:59 +0000

    borrow talk search meeting detail

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def convert_to_hex(n):
+        return hex(n)
