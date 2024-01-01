3269fffaa06c5260409629f804da6b7248dbd093
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:22:34 +0000

    branch sleep react lead education

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
