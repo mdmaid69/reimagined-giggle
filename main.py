ecba338e404e28c4c090081097cf7ec6cead3850
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:56 +0000

    run pen other uponon reason

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
