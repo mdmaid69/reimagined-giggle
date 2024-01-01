193e40f0557c87e12821959c9d58aa5447709d45
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:23:16 +0000

    conclusion takeoff study healthy pump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_max(numbers):
+        return max(numbers)
