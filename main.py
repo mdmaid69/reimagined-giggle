cec7de7e8f3293827fae99d192e6a26c3cf8b88c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:43 +0000

    listenfor wash line seat roof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
