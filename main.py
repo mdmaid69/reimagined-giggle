4bf62f9b8fee4c6e24aa39c8a6583651d9b2d694
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:58 +0000

    fire hang development dry understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
