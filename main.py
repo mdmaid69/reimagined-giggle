9d9c7c510be9be8dd60ad98b5865ea5d417bdb37
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:28:30 +0000

    contrast height opposite even embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
