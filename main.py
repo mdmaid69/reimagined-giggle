c9a74c76fcf9af6e13c7185d6c86191075a59192
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:59:18 +0000

    range thing comedown interest design

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
