a860e832f5d2c994fddc7b94537fbfc1643e3be4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:01:53 +0000

    geton excuse creature young trousers

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
