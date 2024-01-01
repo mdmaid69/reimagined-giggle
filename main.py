1490d0a658ed8c688cd983a58fc7fc4243cf87ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:22:30 +0000

    learn fail main spread fond

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def reverse_array(array):
+        array.reverse()
