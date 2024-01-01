b01f706058be4998e38bb5884fc9fb0aad787740
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:43:22 +0000

    favourite smart race limit swallow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def iterate_over_array(array):
+        for item in array:
+        print(item)
