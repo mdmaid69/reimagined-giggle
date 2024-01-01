02b4e409f8c7cb315e126c4fe22ec49044db7a7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:24 +0000

    hot classroom leg manage exciting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
