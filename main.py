a75654154cb94024675ced81bebf629a57cd55b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:18 +0000

    always Hh push suppose nose

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_density(mass, volume):
-        return mass / volume
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
