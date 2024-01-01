32b408603e7bb54448b1eebce8a65203dbf94700
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:52:45 +0000

    Jj throw pretty custom spring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
