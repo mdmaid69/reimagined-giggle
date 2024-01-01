6a2ad6476a1490d8f50ee34adf1161a98853cab7
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:09:56 +0000

    contrast term runinto contents drive

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
