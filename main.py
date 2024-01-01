631a4d6a5aa5e5d3c7ef251a3ee1f97a11deb1fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:10 +0000

    receive lead uncomfortable scream stiff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print(sum(range(10)))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
