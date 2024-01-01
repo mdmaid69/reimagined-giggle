168de191e1e645ef5dfd23927a9c18f1e0652ca0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:35 +0000

    sorry disturb food proof not

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def remove_from_array(array, item):
+        array.remove(item)
