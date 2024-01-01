8ef84e4c5fd285cc7d7100aa71478ea82cb6de3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:03:57 +0000

    line keep go reject please

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
