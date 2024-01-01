799e10c8a7c9991123588d1d3c2a2c1f5355ff5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:44:13 +0000

    display stare pleased temper myself

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
