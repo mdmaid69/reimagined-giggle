0e381c9078009f10c32413311f9d0af630650f14
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:36:13 +0000

    makeinto balance for various cruelty

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
