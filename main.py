755ccf84dbda6e3bfb5d8fa5dcc57bfacc3b6e05
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:37 +0000

    combination ahead pity will record

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
