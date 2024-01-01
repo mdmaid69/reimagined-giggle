1cc4a2d3cbc58cfc4bde5cd174e3850495f7ab11
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:52:58 +0000

    partner draw maximum front sticky

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def find_min(lst):
+        return min(lst)
