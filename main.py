5cf75fe097b7364805fcc90f48d4f01c4252b49a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:10:51 +0000

    whistle walk Yy second strict

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
