1d1317d7da03d59d0105df6e93216fd682061718
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:50 +0000

    well mad ability cum sentence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
