32df928f41ffe6dfec5fa38b81e0b960a48a3f57
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:11 +0000

    married calm really thumb after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
