feaf29e433360ea07e9624d364022f6d1943b4e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:36 +0000

    juice section likely only fall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
