5c6d58493bcaff9ee054cddb83880574ad6bc868
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:08:39 +0000

    put rule show ago Uu

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_user_dict():
+        return collections.UserDict()
