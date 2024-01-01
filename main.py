6edb02e4065edd006a3e06d6642cdac14ea6ec16
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:01 +0000

    listen separate century delight guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
