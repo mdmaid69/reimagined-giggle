a40a9b54fd4b22619bcd3d2b6b20c7df4948ef45
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:03 +0000

    range greeting runout right manage

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
