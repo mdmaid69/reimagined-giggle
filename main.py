c733d82a8b009c476e08040400a2a9cb12b04666
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:34 +0000

    obtain serious squeeze pass affect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_density(mass, volume):
-        return mass / volume
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
