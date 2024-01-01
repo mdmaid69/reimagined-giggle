8ac6e102a6fd082a9339094a38f30c2b5ede33e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:12:32 +0000

    right recognize table reject criticism

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
