65e2e2f256684dd2324de64a39ead9f0d94449eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:36 +0000

    keepfrom bury class little boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
