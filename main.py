3788c73471831653143d6f6f52cb8c8bb42544f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:40 +0000

    eye height intend correction study

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
