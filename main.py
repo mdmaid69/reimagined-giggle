8df0d5fbe5febb8656906544123daa8bc07ee6d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:22:18 +0000

    sweet although male pick crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import numpy as np
+print(np.array([1, 2, 3]))
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def remove_from_array(array, item):
+        array.remove(item)
