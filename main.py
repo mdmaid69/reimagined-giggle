25b48cce935c5ad37589870b7c51e6969bd0743d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:52:53 +0000

    fine motion fork satisfactory boring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
