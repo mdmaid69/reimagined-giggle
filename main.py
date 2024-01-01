cecd1acc775fc269020d0722e23d74d9c496685e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:55:43 +0000

    fine recommend bet taste pump

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def multiply_numbers(x, y):
-        return x * y
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
