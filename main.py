81348f7a7708ced137b329b9d24368790f41a3c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:31 +0000

    guess shout full concern fortunate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
