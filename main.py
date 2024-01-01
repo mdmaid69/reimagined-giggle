0741a6f954192284e4de801036f9765104ac7a63
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:16 +0000

    test wall country collection top

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
