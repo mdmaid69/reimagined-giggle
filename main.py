2adb2edd6fda71c289c1676085a1e558d2e7f29a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:33 +0000

    wish beside great calloff area

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
