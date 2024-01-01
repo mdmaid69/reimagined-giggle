6df72bcb30a229d82a4623d173261f4a8103edbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:58 +0000

    manage no land 571enough disgusting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
