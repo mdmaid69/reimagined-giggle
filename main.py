01e7a367ca7b4594f6230279a6950e8146152f90
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:27 +0000

    degree worth wipe attitude question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import re
   def replace_all_occurrences(pattern, replace_with, string):
         return re.sub(pattern, replace_with, string)
