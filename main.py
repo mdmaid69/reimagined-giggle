2ad9c05a50c1f0d94787d758d869a826f747608e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:00 +0000

    event smell often hit game

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def subtract_numbers(x, y):
-        return x - y
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
