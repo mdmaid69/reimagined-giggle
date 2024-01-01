c221d64e62c1ea0842881d0f1a1b9d761dea2cb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:06:21 +0000

    concerned bush fortunately hard exhibition

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def remove_duplicates(lst):
+        return list(set(lst))
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
