95aebe8d674bf1e33ccbef9dfa36e41e922c104f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:34:48 +0000

    drive start put circumstance great

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import time
+def get_current_time():
+        return time.time()
