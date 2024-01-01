8181d91c79fde730030e20d4612f26bb9ff7d48a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:07:07 +0000

    hospital describe behind up connect

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
