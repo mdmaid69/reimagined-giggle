dde2c49edbe4f76515af33c9b8409e4e376e956e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:42 +0000

    bet advice usually quantity sink

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
