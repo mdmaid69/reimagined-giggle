34cc660b9c9fd470130eaf827c14e45caba12a51
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:16:14 +0000

    word fall tragedy draw plan

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-for i in range(10): print(i)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_typecode(array):
+        return array.typecode
