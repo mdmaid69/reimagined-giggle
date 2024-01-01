43b961698f5dc8c97e341bacc0e303e2a9b4c75c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:35:20 +0000

    side sound fence noise object

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import numpy as np
+print(np.array([1, 2, 3]))
