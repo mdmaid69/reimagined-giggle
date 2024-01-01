ada62b6f2d7a1813788ef370fc8a9da5c65f822b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:22 +0000

    over concern confused before better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_tangent(x):
+        return math.tan(x)
