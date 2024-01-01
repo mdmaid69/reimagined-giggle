58f6dbe494e24d02981b7ff5bd8cba49c1cba3ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:22:11 +0000

    advice error top brief order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
