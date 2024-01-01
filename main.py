ac13d3c935d8cf9c4016ffde906adddc855c3317
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:06 +0000

    themselves just dream arrive pull

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
