a0c9eb058b31c134c8c3827618120f011371de61
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:56:50 +0000

    turnback exam chase minor reject

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
