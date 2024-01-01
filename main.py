2868f46f7c5aa3a539318cb8f8b3044e17c9113b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:15:59 +0000

    day shape collection ignore never

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import getpass
+def get_username():
+        return getpass.getuser()
