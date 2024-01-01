96a0f7aba2b3a601cb44f18b6a37ce95d4e72692
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:33:41 +0000

    understand comein hope brother Hh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
