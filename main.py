d7b46626cc3427eaaf482cd01f1cc13cd3bd36b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:43:01 +0000

    influence ours country demand thank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def create_user_string():
+        return collections.UserString()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
