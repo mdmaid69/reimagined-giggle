ca17ad1c80eae40aff9cf8e80bc3014dc961cdaf
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:43 +0000

    health contents talkinto follow excited

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def add_numbers(a, b):
-        return a + b
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
