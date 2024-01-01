db314ef8dd0178df852d1a13de04cb11e426bee4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:43:37 +0000

    male usually illness quantity rescue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
