ab4a30b9362736b93785335d901e7ff471a27854
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:25:40 +0000

    especially intelligent young doubt 569enjoy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
