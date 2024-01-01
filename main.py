b61238561320b871fc72bb8068305148825ced2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:34:02 +0000

    Hh rank takeback event fast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
