61cd70fa92f89d44db6dcd4a3b431af0905a4595
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:02:06 +0000

    lookafter down ought steal take

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import sys
-print(sys.version)
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
