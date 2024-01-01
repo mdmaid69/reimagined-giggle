e45c16430e0fa17d201244d65b757b474ac1e1fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:10:05 +0000

    collection occasional Tt side tidy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
