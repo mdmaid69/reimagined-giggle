2446f2456c236a6844f1f1aa2a799c362eb60427
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:08:58 +0000

    tradition freedom reply brief debt

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
