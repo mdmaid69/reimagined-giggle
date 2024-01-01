39c723431026791ccf3bac8a086d0aa66218ed90
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:32:39 +0000

    attitude nature callback manage spite

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
