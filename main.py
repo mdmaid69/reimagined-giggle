4fb7802e4062ee7c5fe3984ba6f6cf0c78166288
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:15 +0000

    direct rich balance satisfactory yet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+  def count_elements(lst):
+        return len(lst)
+  def count_elements(lst):
+        return len(lst)
