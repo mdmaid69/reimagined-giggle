3b91b0a2f149cdc70e918088a62123992c63d4e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:01 +0000

    minority offer suggestion ground born

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
