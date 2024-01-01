495d8ef29297cf21e60aec26cbea32b8cc11a1b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:44:45 +0000

    nature plate reply spring run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_energy(mass, c=3*10**8):
         return mass * c**2
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
