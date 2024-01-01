f1cf62cf1d4b9a0f64c6f4c5b7cef7c5acca2621
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:06 +0000

    abroad swing operate weak remain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
