d633dda4727acafe566d9d0535d5292380fb4c34
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:24:08 +0000

    I value abroad tongue pin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
