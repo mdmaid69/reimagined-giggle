311f5d8aa85dbb61f6feae6926f8d7f5d2e1053a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:10 +0000

    inform extremely immediately list lost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_even(n):
-        return n % 2 == 0
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
