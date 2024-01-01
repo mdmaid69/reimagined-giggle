e57db79a70b0cb478d493b314e27bb06c042176e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:21 +0000

    under listenfor comeback include after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
