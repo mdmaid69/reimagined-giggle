505e041cf46c2ce89c381c792caf27bdac7dc9f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:34 +0000

    please whole skin shape neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
