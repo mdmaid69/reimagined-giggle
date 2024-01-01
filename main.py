8d5a695484b3882087b621743f9322dc9393a409
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:39:07 +0000

    authority comeround rare ring upset

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
