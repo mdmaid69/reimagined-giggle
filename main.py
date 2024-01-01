f33aaa813938c2705c75e3145e680ad8a278731b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:48 +0000

    bus prove few influence road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
