1db354ef960130ee9af842c50dca44cf628e704a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:24 +0000

    crowd wide agree certainly mass

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def is_even(n):
+        return n % 2 == 0
