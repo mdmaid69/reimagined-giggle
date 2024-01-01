a540f1b3bb417972442d0803992c613607f0796d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:14 +0000

    responsibility difficulty nose office reach

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
