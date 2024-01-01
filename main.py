4c51d8f234c34572e5a04deb2d190c151724989f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:04 +0000

    discuss distant think freedom content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
