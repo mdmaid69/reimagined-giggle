d87e6bab606b1e63a80f32301e1b0e0ce0a5e535
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:30 +0000

    too sudden shrink otherwise copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_rectangle(l, w):
+        return l * w
