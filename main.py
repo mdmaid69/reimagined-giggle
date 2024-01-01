1b64458c0b2cba3cf6c74997440308ce457fab6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:00 +0000

    easy exam early soil community

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
