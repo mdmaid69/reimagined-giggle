03585c5eade46ee59f911758ce51229761e0e545
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:06:56 +0000

    capital spot either clean form

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
