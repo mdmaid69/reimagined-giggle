88317690af257f6820b62b1cee4322201cb2f590
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:43:41 +0000

    makeinto passage pump sure prove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
