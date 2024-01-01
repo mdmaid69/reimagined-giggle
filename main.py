fe4748d2348db9f31324acc6b607187b17382192
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:51:01 +0000

    above nothing course dinner match

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
