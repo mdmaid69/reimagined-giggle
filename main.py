a676893eb19b576dfafd10629b88818d6747b8b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:09 +0000

    argument it failure breakfast tooth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
