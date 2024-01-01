40b942104f2bef366456f2f1f9179f048783267b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:51:45 +0000

    lay spot fine but rare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
