09d18ffef8881707815d2bfdc86f4b1f55cb2b65
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:26:48 +0000

    takeover 572enter instrument out weekend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
