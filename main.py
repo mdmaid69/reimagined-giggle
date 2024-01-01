1eb0bdcad28b41dc26c194b05210d8f44568b26e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:27:42 +0000

    length thankyou hit method disgusting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
