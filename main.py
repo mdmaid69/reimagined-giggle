197dcdfe77f850f821eda54a02e720c4aa7dd81a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:32:35 +0000

    comment attach exit cruel into

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
