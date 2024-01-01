2348578f0a45977f94689702b94593ad5f23c93a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:51 +0000

    keepfrom part mind earn move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
