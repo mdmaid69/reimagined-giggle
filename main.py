871fc352a8721dcdd19a9918de87f763b1120012
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:00:58 +0000

    room comeoff send crash scream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
