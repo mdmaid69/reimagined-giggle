ad60dada8a1e0930c3a2f6dadb4429f419eb03eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:37 +0000

    pen meal excited takeaway roll

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
