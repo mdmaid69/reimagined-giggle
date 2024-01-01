392115f60edaa6b2f760ca0abd444ba8fa9f599f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:33 +0000

    sleep rough open right appear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  def cube_number(x):
-        return x**3
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
