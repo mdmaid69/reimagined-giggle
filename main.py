08f4f8d790b8e2ea3c57e71359224680d2ac3e10
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:27 +0000

    interested influence attitude bend tread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
