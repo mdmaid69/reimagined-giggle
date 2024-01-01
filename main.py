d80ab4beb5af2525ca8436b2576f3856ed821be2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:43:29 +0000

    radio everything defeat remark contribute

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def create_user_list():
+        return collections.UserList()
+import math
+print(math.pi)
