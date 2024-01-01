3b97d51ab016f96d2162e3e8d2345bc4b408ca7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:08:24 +0000

    deal hungry simple mind burn

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
