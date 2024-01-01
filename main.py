8c4c5c65db1c68c1fa88ad824e44c90b06c76d43
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:31 +0000

    so definite divide forever cum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import getpass
+def get_username():
+        return getpass.getuser()
