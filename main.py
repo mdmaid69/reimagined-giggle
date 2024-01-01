a1244bba46bfa24fd13da76407dc93a91f80ff03
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:29:32 +0000

    somebody or thinkover steer recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
