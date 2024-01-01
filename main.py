a5164e79530353093b8404ddb68d6acc8f4cd105
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:57 +0000

    cut present total beyond square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def subtract_numbers(x, y):
+        return x - y
