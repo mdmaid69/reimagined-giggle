c90185b9aeb831ee0db8c4a25ac5be29df71ecf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:04 +0000

    herself thumb inform gas along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
