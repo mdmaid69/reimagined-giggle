1109ad664044ce568ad63d0378580b408758729a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:16:54 +0000

    often boil view break holdup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
