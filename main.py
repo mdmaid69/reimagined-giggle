e3b216844d701c2d20b87e444bc12cf2b1316cde
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:53 +0000

    smell tie card likely indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-print(os.getcwd())
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
