74dea27d3664796d99fd3abf978710290cb62f02
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:32:39 +0000

    pause everyone invitation nerve reply

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
