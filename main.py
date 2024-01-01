c6e6377ea3829fb87cc621b9aac54e085bd93b16
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:53 +0000

    past goon survive ticket abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
