e53eb94b8cbe6733615855747672544aa702670a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:09 +0000

    cum post interrupt close rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
