1e60d05d0e36abce6ca4b3e3a78d41cf211fabba
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:06:42 +0000

    many block owe waste declare

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
