c6be6e674b5ca3cf45864d345783c72c2e5d108d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:46:16 +0000

    interrupt spare respect look know

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
