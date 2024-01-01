86fd5c1c07c6399ce6509db9575609b1326e585e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:18 +0000

    wait area hurry accident brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def divide_numbers(x, y):
+        return x / y
