bb91d5ec7f4ef183d9dcaba446db790b5ea45399
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:17 +0000

    bush voice section mind shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
