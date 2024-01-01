d18f316c8ffc7adf96ef52f1215f04aa5aac640d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:37 +0000

    strike communication exercise tone pipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
