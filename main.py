f86654c40f337c62a58a2a23383f776d171037a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:20:23 +0000

    round beginning have passenger identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
