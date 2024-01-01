ed70bb71eff0c37105a04682fa6dbc57b8085c5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:41 +0000

    almost clue borrow main climb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
