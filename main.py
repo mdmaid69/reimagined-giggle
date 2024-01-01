3641f56757ea6f36c6a43605ace3274e947d8818
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:58 +0000

    equal ask yet national offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
