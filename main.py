0399b94bf783f9044065b9307d217ef31b4c3197
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:41:40 +0000

    quickly all develop level do

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
