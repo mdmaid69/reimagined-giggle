1cf6a412e3223086cb3509d62eb15abfae477a95
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:43:51 +0000

    shall week touch goup another

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
