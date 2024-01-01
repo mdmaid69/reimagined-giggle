bda946b00cbf6f352f90cef6a359a4a75e70fb26
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:58 +0000

    earn experience exploration 572enter 566end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
