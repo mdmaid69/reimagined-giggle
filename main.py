fbafb82a8741807e7b58279d685c1737244f3070
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:19:57 +0000

    sick change image innocent honest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
