b65804c9417ec03cd3a63a958eae110201aa9d3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:02:15 +0000

    relax yourself threaten ask various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
