96de1ce4f2f25f6e6d469deda4c9fad004c116a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:46:36 +0000

    arrangement on sure affect body

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_density(mass, volume):
+        return mass / volume
