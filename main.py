6d9c75b1d3ae61adb89ac50be1212db4882c1613
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:40 +0000

    pay mess ride escape search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
