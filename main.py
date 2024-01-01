b81aa6c501f582ee54494a991f28df93ea7ee533
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:30:35 +0000

    ever exaggerate where private lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
