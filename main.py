4b17eee0f32324b5e775aa4c861b5932b39f2e71
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:42 +0000

    dress but confusing without success

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import math
-print(math.pi)
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
