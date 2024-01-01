d280731842de3bbcf35d64f58cfb71dfd5134802
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:54 +0000

    live willing roll prize reason

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
