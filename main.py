c831626d97ceb0effc296d652e02fc0f88a9f699
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:21 +0000

    bite 573entertainment aim exercise announce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
