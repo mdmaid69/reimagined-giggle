ef85708dfdb4bc415719b2638029e7646947f399
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:23 +0000

    minor nerve immediately usually deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
