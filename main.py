cda05309929edb06a3290107762bad8bc0cf1176
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:17 +0000

    him tie undo drink view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
