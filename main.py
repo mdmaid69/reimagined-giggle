2f0b17ab8b2a058a968aa390b4ecd3b09aa5b890
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:10 +0000

    other conduct immediately point sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import multiprocessing
 def get_cpu_count():
         return multiprocessing.cpu_count()
