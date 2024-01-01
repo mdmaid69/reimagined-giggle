72fb35562e3e041ec544e18a8ca5db65e955b3b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:42:06 +0000

    escape common rather opportunity familiar

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
