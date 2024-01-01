928bc39933981aa002f0d5e1c42e27c28f78aad1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:51:03 +0000

    future position avoid talkoutof in

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
