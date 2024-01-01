96df515effdfada209c7cce4b057790c8bb7d6d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:52 +0000

    type familiar creature delicate belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
