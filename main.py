a6d409d107a29234a47d2d42aaaf50c74ff0925f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:28 +0000

    speed bear mend makeinto hate

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
