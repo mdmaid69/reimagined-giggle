3975fa44856355bb5cdea286f0bb8069e027ada1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:06 +0000

    confused benefit firm wish Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def convert_to_hex(n):
+        return hex(n)
