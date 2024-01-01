5a250929d3f87192a4201495a7dc3ddef04cbb15
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:00:42 +0000

    environment slightly instant increase constantly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
