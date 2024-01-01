f0e736393b5a3a52edff6dbabbf9a21400d7f417
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:52:37 +0000

    worry job power about common

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_power(work, time):
+        return work / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
