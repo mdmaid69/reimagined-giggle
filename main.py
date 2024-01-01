7771fc9223acc5f8537f9ea877c2e205ffc26197
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:22:19 +0000

    conduct label lesson behave niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
