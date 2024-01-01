468033c14dc0631811b528ddac13c8a1b59dc2b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:38 +0000

    decision impressive combination control satisfactory

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
