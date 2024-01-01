cdad07af3c7984ea4b0d0d6fb36b04b73027b0ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:01 +0000

    how wheel lookafter honest dry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
