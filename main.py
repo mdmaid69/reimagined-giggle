550b2e7427b5727b419010d8d73cecd7cf8783fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:07 +0000

    cover no welcome pollution practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_area_circle(r):
+        return 3.14 * r**2
