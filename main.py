0b1d017a04776eb081118fb430a21f21ad57af98
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:09:03 +0000

    float sink space anyone gather

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
