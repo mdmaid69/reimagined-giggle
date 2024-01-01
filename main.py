c05b12b10261b19c878149a2f6a8822f9abb136f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:04 +0000

    yours compare its fork general

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-name = "Python"
-print("Hello,", name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
