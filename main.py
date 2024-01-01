dec3b9cb3a04c679a6c2ea62a0f5d3d04ddce51d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:49:44 +0000

    disappointed treat professional we famous

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
