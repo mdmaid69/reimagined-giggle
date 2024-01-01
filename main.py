a6a0e551316be42faaa32b79123c3ff5d3ce30da
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:50:53 +0000

    good activity earth relief strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def clear_array(array):
+        array *= 0
