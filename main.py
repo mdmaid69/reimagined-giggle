41bff1181e626c4440415cc5afcfab53ef103686
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:31 +0000

    convenient stop active always operation

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+print([x**2 for x in range(10)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
