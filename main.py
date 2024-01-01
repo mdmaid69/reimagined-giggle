bd2d82950038387618ea59ed2fd0d904b1e853c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:53 +0000

    lead teach exploration how line

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
