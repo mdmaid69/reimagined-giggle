3c77ecd1a218c239f768ff67b9e8baf3131e2dd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:48 +0000

    character majority bringback cap curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
