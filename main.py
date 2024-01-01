f1ab1e4c0a49864c654913a6f5973ea22afcd6a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:24:03 +0000

    telephone much attitude relax guess

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def add_numbers(x, y):
+        return x + y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
