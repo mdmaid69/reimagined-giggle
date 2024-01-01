f9d5a42a95b834fdd14d53d6b291ffccacc1e476
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:47 +0000

    intend giveout lend Tt actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
