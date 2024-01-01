bc943b9a26b82e9389ae7417c9f2d9e37f2d2b2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:25 +0000

    everywhere autumn advantage year considering

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
