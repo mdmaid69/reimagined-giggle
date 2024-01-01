bcc314b78a46532acda1a93be0ef2bf581b38abd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:25:54 +0000

    fear conversation waste butter past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+name = "Python"
+print("Hello,", name)
