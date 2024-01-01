cd69b3f4684e6b79e0632278ef5855a0fedd9780
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:11 +0000

    transparent wave boat record finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_max(numbers):
+        return max(numbers)
