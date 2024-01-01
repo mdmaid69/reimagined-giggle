8d64be15dac6be512971be2606232f7293957285
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:33 +0000

    light talk difference us body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-print("Hello, world!")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
