28d89fc32b500bb8d42ff37084c77bce95e1220c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:17:19 +0000

    concentrate fill disturbing emergency repair

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import os
-print(os.getcwd())
+def convert_to_hex(n):
+        return hex(n)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
