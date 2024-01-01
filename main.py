33d01a2512d6e1c970090b10ebc57d8f24a3c11f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:57 +0000

    clear water reach guard couple

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
