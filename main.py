3a075aaa939f343e1ca3bd553a4eb244c7ceda0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:22 +0000

    organize partner scrape pull emergency

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
