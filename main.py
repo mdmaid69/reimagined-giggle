bd55dcd3e8be5b43a1dcb7134d8b1d38011cdeba
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:32:37 +0000

    murder callout pass recently excuse

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
