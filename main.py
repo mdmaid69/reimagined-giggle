d20442a85b33bd8f7986d8818e5d94793374ed68
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:24 +0000

    speech present funny concentrate blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
