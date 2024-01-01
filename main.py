1a3b95576f512d458b55c5afa4925768704d1efb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:00 +0000

    experiment help especially within sky

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def convert_to_octal(n):
-        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_speed(distance, time):
+        return distance / time
