0daea0889deca2f06f69ee62d27c3212590d00d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:38 +0000

    play comment tool accordingto thankyou

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+print(sum(range(10)))
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
