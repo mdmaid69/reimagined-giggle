3f06c6c8b32aaba03c738a671632b1d1e47dc60d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:22 +0000

    sting guilty keepup blame good

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
