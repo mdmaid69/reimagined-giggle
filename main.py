a94b36eae42f8efb429a84d94c0059c4a76ac059
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:59:52 +0000

    steam opponent comeup fit bush

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
