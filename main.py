e0ff3be0394b1dbd111be48d33614571d85dd40f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:58 +0000

    tip takeoff attempt press rub

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def add_numbers(x, y):
-        return x + y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
