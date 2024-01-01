4772d1039bb62e6b80194f7b7a9eb8c06f769174
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:51:51 +0000

    holiday spend cheque impressive foreign

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
