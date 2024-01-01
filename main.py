47e5c26f8ee25e16f66b0b8256ce61187cf405ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:40 +0000

    noisy feature border dark area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
