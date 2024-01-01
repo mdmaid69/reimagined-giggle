3aa91aff460b7349a6beb6dd86a442cc29ce802b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:31 +0000

    firm complete direct mark thinkof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import os
-def get_current_working_directory():
+  import os
+  def get_current_working_directory():
         return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
