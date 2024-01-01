650d8676a2f0b60b5cfaf98a140f37ef39e5dab0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:43:39 +0000

    tongue brush content nature smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
