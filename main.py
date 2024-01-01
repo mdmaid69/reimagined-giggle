5197dbe48c2d9ef00dd296049a4a20eed269180c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:11:13 +0000

    connected treat compare forward sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
