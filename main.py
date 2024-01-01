daea16fa566c5ef016d28d78a14421d0067a94e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:22 +0000

    nowhere development warning computer pleasure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
