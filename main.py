f6d35cb3dc7217aa4d5a8ef58478f1dad9aabc44
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:23 +0000

    be godown damp benefit contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
