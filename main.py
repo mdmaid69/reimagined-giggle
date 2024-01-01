af3a22a89aeeef54eb3c2061db10790ee6e0c292
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:04:17 +0000

    study rob father sudden club

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_repr(array):
+        return repr(array)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def square_number(x):
-        return x**2
+  def get_base_name(path):
+        return os.path.basename(path)
