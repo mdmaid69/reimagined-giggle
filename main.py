2f5a9dc8b7b6db5e9efeb391e7b43b986652ee1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:50 +0000

    climb give ordinary equipment yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
