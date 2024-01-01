ebc65c2245239141746ff245eafad4cea1e86fc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:37 +0000

    hole gather come organization count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
