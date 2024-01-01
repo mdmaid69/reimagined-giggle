0237bee6d58da77373538b60ebc98a94809e556b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:21:31 +0000

    evening unusual child listenfor dig

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Characters:", len(text))
