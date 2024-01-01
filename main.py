5ade92cc923e94c8c5e1f3d97f460a8d0251bc1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:26 +0000

    Zz it tooth chance majority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
