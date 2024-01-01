573c2e375327fcb9e54ba6530885f0cc7c1cb3ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:18 +0000

    change number sky identify type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
