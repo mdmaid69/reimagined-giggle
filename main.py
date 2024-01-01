15cd6698b4569d9c496b65254b2d5ec7b2011bfc
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:53:01 +0000

    preparation hard doubt handle minor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def remove_duplicates(lst):
+        return list(set(lst))
