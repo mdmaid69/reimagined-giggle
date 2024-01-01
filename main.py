c99f4c25023b9b7117417434baaa28b19cb0aeb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:39:18 +0000

    us rain public limit weekend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def convert_array_to_bytes(array):
+        return array.tobytes()
