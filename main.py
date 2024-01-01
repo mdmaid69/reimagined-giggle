4f378d83db45b1659c9b8fd7ca180fe00e805039
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:55 +0000

    tongue dull tear enthusiastic steady

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
