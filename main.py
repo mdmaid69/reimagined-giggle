2f618891db33554a5cbcf45edc97a36f27464cc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:16:30 +0000

    school manage injury style ocean

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
