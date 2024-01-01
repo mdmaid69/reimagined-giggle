31efaf329afc746f779ab19cbb9ac001385fab60
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:49 +0000

    head mess tooth pure Hh

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
