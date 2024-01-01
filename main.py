e2baab356e91b0456d8657ec8f5384ea6648a2b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:00:17 +0000

    separate steam once both alive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
