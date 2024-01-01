d6edbd3224874a17f7c2bdf5df69237884a929e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:16:32 +0000

    estimate so race even dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
