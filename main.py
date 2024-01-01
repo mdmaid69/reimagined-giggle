fab32064daa5bdff09ecbcb64773a894d5460d6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:30:52 +0000

    need include glad emphasis grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-print("Hello, world!")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import array
+def reverse_array(array):
+        array.reverse()
