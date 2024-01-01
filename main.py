9b839e6602734a62a8f34cafda49cee1b9bad714
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:32:15 +0000

    below comeup trick press with

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
