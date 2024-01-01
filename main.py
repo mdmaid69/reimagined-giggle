8fd667bc9f569955b42e9b028dbae7b91f15d947
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:17 +0000

    violence memory environment child that

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
