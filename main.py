5602deee3058ccd9400c6e52e689dc1397af321a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:21 +0000

    exit along contact equipment hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
