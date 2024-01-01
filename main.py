1f0a79c9e20ec9f5828626d6c416dde64b294917
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:42:00 +0000

    walk beginning gap lead comment

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
