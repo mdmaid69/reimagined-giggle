d2ab625e6d6bbb21077e6648da972581500b3386
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:24:21 +0000

    permission plan unable answer invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
