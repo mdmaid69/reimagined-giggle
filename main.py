eaea32f6fed9b7c044ccef675247cd88d904d0c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:32:18 +0000

    manner hour let thinkover night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
