3010fe65a5bc0c019513a58f3b81fec8d5fe9f91
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:19 +0000

    maintain factory related mysterious name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_list(array):
+        return list(array)
