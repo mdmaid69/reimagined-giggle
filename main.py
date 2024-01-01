ec01e87a6085778f06fc80060e2a5a3ac9bffdd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:53:24 +0000

    search nor opposite claim children

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_work(force, distance):
+        return force * distance
