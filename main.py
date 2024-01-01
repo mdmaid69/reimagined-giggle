8f89dcf295c7d615abbc62b1691575b38dfd9f60
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:15:32 +0000

    shop Ii card crash later

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
