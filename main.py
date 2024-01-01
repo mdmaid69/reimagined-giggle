ab13b8b38efbd20652161f454c27810abab35042
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:40 +0000

    Ff practical when crash positive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
