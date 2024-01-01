6e86029d4821f57c2d49703b44ed459ecc13aa95
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:21 +0000

    colour serve than fact deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def count_words(sentence):
+        return len(sentence.split())
