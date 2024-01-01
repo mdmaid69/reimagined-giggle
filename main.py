94e14fba72aba6fdebd8a82e56a909de0fbb1e09
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:22 +0000

    programme desperate squeeze reserve tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
