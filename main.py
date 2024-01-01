ab3db151e7046aa6489a5e013f91647ec68893e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:33 +0000

    read them reason lend lesson

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
+import os
+def get_current_working_directory():
+        return os.getcwd()
