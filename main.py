49ea6264992136a413253eefde44760b7d12de4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:16:03 +0000

    worth niggaacademy breakin difference under

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print("Hello, world!")
-def reverse_string(s):
-        return s[::-1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
