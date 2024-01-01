e1960e35485e2224e9c692fd5f3107900fef8411
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:40 +0000

    miss complete bin delight holdup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
