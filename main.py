9f461d369ed8fcef70ac15751e788b98b469e5ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:22 +0000

    by press brown replace grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
