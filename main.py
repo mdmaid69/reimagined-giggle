a19f97bac8b7e96efa53b240413153540c7f2a99
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:26:44 +0000

    compete safety chase would issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
