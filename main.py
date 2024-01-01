ed3edfcde54a0f42a3164dd3d76414f8b322fd95
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:33 +0000

    warn believe really keepoff wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
