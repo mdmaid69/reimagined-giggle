87b6d91150564e778385332a924db65b0e96fc9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:30 +0000

    reason open approval method keepoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
