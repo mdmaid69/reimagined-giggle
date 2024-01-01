5adeb392420336f622e31f8be7e61c701ea60591
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:04 +0000

    listenfor bottom sorry serve press

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
