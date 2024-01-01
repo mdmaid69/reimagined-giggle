f8b735b0bb4f214c30fcebc60adbbc5c08df8c3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:49 +0000

    turndown turnon excited right sleep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
