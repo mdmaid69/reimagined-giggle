a170dd418c77e73a1b17129d549d9a729d716320
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:24:22 +0000

    turnback worth bringdown believe deep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
