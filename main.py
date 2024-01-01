2bd707def80deec4e1a91a76f9406898186e0c9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:36:32 +0000

    unlucky press Gg tradition breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
