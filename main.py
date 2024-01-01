32d12bd7293545518750344e72970e1d933d0779
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:11 +0000

    reason thing confident praise nerve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_binary(n):
+        return bin(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
