73d915989e808c5233d2d16e0350221ca0b36534
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:04:52 +0000

    experiment push powerful whistle read

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def calculate_area_rectangle(l, w):
+        return l * w
+  def is_odd(n):
+        return n % 2 != 0
