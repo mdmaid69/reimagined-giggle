2b674c3b03ba36b67395bbdc1a650e8dbe8b8d0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:41 +0000

    track day necessary comeout command

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
