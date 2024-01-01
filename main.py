20dd2b15533be3104a39cef8be4e7d8aea86c850
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:07 +0000

    score deaf plan power expression

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+def convert_to_binary(n):
+        return bin(n)
