7c6d34bc051b3213d7db5b98cd80b537030393fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:07:04 +0000

    treatment defend stick clear slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def add_numbers(a, b):
-        return a + b
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
