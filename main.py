94053d334ad58c2b56462604aa9e9015c8d7d78a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:43 +0000

    embarrassing attention sea throughout light

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
