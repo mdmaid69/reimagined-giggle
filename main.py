94360eef896af2bfc229c6b632e528c0018f8bf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:25 +0000

    her fire badly try cost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
