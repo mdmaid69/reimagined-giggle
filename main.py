6c26959ed382e09508ccd3a965627ccb76b23906
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:45 +0000

    trap flood attention or real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
