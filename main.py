25ed2de6e408cae0fe3e1c8dccabb51ac31bc7d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:34 +0000

    frequent appearance brush sky notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
