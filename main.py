670cbe989168ace8d81f4fcc883820d3ea9df52a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:28:25 +0000

    only police influence above too

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+print("Hello, world!")
