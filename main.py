175cf8d09236b4963b2fb3afc15a906b01be0f7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:09 +0000

    Itself flow most perfect place

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+print(math.pi)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
