8d248b0b58491516c86500782a7b8d2f53816239
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:48 +0000

    picture trial giveout underneath forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
