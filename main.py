f22e376ef49b91cd3cdc8ae69eb9c45a18de785b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:44:11 +0000

    cost display win business stick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
