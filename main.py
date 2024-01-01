dd55c5905a71ec4bb58a7d5fbef13f9fb8678b6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:32:09 +0000

    own most cheque make fasten

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import os
+def remove_directory(path):
+        os.rmdir(path)
