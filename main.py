e44c6f57206e59dcc3c257891dc2603f80867699
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:39:47 +0000

    convenient squash pride class fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
