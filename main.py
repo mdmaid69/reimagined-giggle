bc347db99ceeaf8b43311e163968bcacd32f391f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:09:03 +0000

    expensive hope suit lie disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
