d6fb325d968b7a87a107a6dbbf03e9d4bc7f96ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:19:08 +0000

    bag really manner wall breakfast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
