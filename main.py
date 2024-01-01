fbcc21a57c3c41a75eb5eea80e36d976febf7eef
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:44:58 +0000

    569enjoy shadow scale serve satisfied

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
