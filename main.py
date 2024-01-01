ff1f0b118b0ab1f37013bd75f074b9eaac59143e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:46 +0000

    rest instruction label independent square

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
