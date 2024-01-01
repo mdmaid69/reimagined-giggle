bfb1c221bc850a17a94ddcdc12179586ed132773
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:51:31 +0000

    state holdon breakdown mixture demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
