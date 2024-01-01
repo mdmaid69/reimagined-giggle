4ee58677b8331c181b061bc1ba1c5d0c07750159
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:38 +0000

    run takeaway cope fall week

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def square_number(x):
+        return x**2
