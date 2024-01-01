089c71ad5a7809dfd3288b2f48113f808a686e53
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:01:39 +0000

    far worth buy environment getback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_inverse_hyperbolic_tangent(x):
         return math.atanh(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
