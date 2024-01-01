16c188685d7f9fe92321a205694dc188abfd66df
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:42 +0000

    switch respect ashamed call track

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_elements(lst):
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def count_elements(lst):
         return len(lst)
