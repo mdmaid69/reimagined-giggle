b30435fac25a837a150e37d6ae08da2fd9aec517
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:37:30 +0000

    tight repeat common mix in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
