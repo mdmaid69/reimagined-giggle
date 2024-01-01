0025a748cc8ef7752bdd2aa25d866a4a0c7717f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:53 +0000

    back result teach practical week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
