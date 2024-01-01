d8d53fbbf2457ac072cd7ab55f7fd3d2be17ad37
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:57 +0000

    Itself address mean glass hate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
