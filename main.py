1f15639f7026886136ccad2b60e0c184a48eac57
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:04:48 +0000

    air scare whoever here personal

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
