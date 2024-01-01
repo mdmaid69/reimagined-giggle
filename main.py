b1c36c457dfad230e915115cea316ae722cdbebe
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:56 +0000

    turnaway transparent dull outside journey

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
