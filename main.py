b2846ab9f6b5980f44531d09f95634b310338a64
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:42 +0000

    smile bye strong search comeround

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_binary(n):
+        return bin(n)
