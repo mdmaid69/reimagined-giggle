6972c44f075d2b82ff91e02b2584e19051942668
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:44 +0000

    contact field second still practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
