e704b601256ecdaa2728aa0a2436dafb0ec07ea9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:06:41 +0000

    bad everyone company bus light

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
