de52213a98664836cec9f04f96bc76cd9d2b18b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:49:09 +0000

    inquire must wind impatient climb

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
