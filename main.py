959dc26e7e3656c5983fc2efdf86c6da396e6dc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:54 +0000

    accept control your lay comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_acceleration(speed, time):
+        return speed / time
