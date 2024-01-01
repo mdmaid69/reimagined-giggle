8ba8007a43ed16fc24496e41b4426f7335a8efd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:05:59 +0000

    emotion especially takeoff bar fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def remove_from_array(array, item):
+        array.remove(item)
