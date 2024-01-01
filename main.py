9c41445772c6c456f145ddcea2518e319b5265c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:40 +0000

    accordingto miss fork each far

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def convert_to_hex(n):
-        return hex(n)
+import platform
+def get_os_info():
+        return platform.uname()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
