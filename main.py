68b79fbe6cdbc4d107ced24e56d8a69eb2422feb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:29 +0000

    ball himself smile job left

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
