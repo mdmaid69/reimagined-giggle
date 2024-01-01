ad6c73f7010675083a8fa7fbfcaa3a50c0847f4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:03 +0000

    convinced possibly recent alive stir

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
