91952a07f715cf4534df7a7412a797facaa39ec7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:31:42 +0000

    mention direct but afford shopping

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def add_numbers(x, y):
+        return x + y
