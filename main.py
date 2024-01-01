8e4882d20a909f5559cd170fd90aa14568415656
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:30 +0000

    unusual suddenly wipe rest prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
