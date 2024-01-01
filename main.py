44e3bd2cfd0c740a8f765721957d3b531cdba8d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:06 +0000

    return variety left judge skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_arc_sine(x):
+        return math.asin(x)
