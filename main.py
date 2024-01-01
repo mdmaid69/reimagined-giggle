baf3898b0d51495fbff51b53e6a743b68c2bcc72
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:28:47 +0000

    city myself direct calm shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
