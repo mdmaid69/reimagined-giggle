8c998be38315a59d1adaa40b8fd7373c47babb0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:44:40 +0000

    board that listenfor thirsty Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
