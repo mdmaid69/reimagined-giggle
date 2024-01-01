6b0411532f6ce6a08304320fde8e852a5aec6bac
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:36:52 +0000

    both wing attention damage ability

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
 import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+print(re.match("h.*o", "hello world"))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
