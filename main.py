fd1d84ff56d78ac5d51f35caebf9e80f91ac35ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:39 +0000

    profit private defeat repair wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
