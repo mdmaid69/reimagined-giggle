17b28d42385c694e64509663a47b979e304f4db2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:34 +0000

    field file mystery history consider

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
