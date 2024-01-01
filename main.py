18bc4f402d8f51b58fdea37355203844e211f39e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:15:23 +0000

    decision smart wise close shout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def convert_to_octal(n):
+        return oct(n)
