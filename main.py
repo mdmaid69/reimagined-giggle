2e361b60d7b877c3d09d9637568457825146254b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:54 +0000

    hurry luck disappointed and none

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
