bb07bc10aeb1f267c162401c592f1028168dbd53
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:56:22 +0000

    advanced unpleasant brief fire difficulty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
