abd62aca79338904602850b2585230b72f467b9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:31 +0000

    culture perhaps splash bush advanced

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
