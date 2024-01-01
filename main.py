44d851a9765b2491b6bdb8f0efcac39369b400a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:21:54 +0000

    partner tape instrument service lock

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
