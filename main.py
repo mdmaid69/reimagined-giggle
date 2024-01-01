bbf16c0651d25a47bdc9c03b9228b313fbdfb93c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:13 +0000

    force wave fill change form

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def remove_directory(path):
+        os.rmdir(path)
