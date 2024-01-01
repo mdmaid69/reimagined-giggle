23c594176cbf652868b7bd1e29bb38957d6323c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:39:22 +0000

    only or target after mad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
