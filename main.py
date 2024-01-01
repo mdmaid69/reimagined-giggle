5680f69a7de49ef6b8cb97e5aaa02e9b5bfb1145
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:22:36 +0000

    unlucky smile drink thick continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
