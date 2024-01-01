283b30b29bc6d7befa42fbdaae805089221eb70f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:22:10 +0000

    seriously fill travel situation most

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
