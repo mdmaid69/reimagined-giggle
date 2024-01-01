f4ec4db0cca91684e8bd3bb68b6f21f47bbbb0c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:03 +0000

    plenty peaceful rather exploration drink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
