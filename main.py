964bb05116985da8305622569ba8efb1d7227a3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:31:14 +0000

    rise future take slight whoever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
