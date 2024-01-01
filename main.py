7e9d2b717af83e0022966ba38d226a978d69d3d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:05:23 +0000

    afterwards thinkover steep whenever home

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
