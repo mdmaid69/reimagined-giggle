60fd0f371ef0d251d1fb25a4f099f65c8262d256
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:01:52 +0000

    though hour exciting state on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
