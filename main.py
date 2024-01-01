e37494f0585c078c6bf84a2ae294748ed6748f4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:07:45 +0000

    concerning suck hospital design sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_float(array):
+        return float(array[0])
