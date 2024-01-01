82568b9df14ec565e89692c9c4ec016e6c91ea29
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:27 +0000

    quantity post choice load whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
