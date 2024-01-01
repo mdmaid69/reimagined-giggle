36a175fa17d70b5cbc35fedf044cfa484ced8633
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:45 +0000

    persuade roll little careful tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
 import sys
-def print_python_version():
-        print(sys.version)
-for i in range(5):
-        print(i)
+print(sys.version)
