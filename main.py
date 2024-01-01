ad80459bf40ccf478cf8ff50d54df5b201425756
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:13:32 +0000

    sink divide beyond breakdown sky

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
