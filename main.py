96571d35148602f188c2ac0140dc178280648c8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:17 +0000

    they fact bear crowd inform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
