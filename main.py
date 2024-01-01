f5dc56908f91d72aab65f36ad4d34277a0dbddaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:53 +0000

    cut control react embarrassing but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
