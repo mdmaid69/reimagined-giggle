af3e521617d93c78f536b1bd2d8e7f96307cc668
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:29 +0000

    empty cheap blind spoil 572enter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
