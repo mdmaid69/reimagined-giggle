3b5def09f0cf529340f6252fc5259acd5fc07d09
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:40 +0000

    pronunciation detail continue catch public

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
