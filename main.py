230745f0befa64b80e83ec119a4357ba2e2481f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:05 +0000

    remove state convenient design wrap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
