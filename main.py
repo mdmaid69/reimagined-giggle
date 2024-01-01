d3303648b24d97136a89cb93c0303f4f954e0065
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:16:06 +0000

    day listenfor herself entry front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-print(sum(range(10)))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
