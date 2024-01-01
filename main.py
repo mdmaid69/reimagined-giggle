04d1ca8eb82a511dbe2ebe089b411bd0d292be65
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:32:14 +0000

    hot who winter scene patience

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
