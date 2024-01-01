6da19fd1bbf1233fb4a9aa47dafc38a2c7946bf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:48 +0000

    just sea full same Vv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
