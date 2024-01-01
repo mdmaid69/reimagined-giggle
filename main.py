0487bb0ee2c2d1a90695b69a37ff42d9f4406877
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:49:16 +0000

    thin want criticism organization Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
