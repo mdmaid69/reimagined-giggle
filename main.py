efc584c4ea655f33393e1c29e76838544b06a072
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:24:39 +0000

    rank scream handle roof progress

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+for i in range(5):
+        print(i)
