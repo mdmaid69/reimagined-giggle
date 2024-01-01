6792950df4eaafe2328525f58b2dce549d07e7f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:32 +0000

    though might summer light nature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
