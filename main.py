afc964d1797798149a2d9096a15b5a52b004b14f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:00 +0000

    bus matter also rob out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
