64bb696bab82da9fd140f9903499a0589a1cbdab
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:08 +0000

    pity keepfrom please terrible affect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  def subtract_numbers(x, y):
+        return x - y
