5cd16934dea57170a5193f65c3a3dc3d7eda6af7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:44 +0000

    trip shade guilty him rare

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
