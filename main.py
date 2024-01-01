448ac5082fbeafc1cf3ae5362c1038bcc70f0823
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:56 +0000

    everybody really far pattern clever

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
