ec8a3b22b3d396c944d470c5073517f86c507c7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:31:20 +0000

    black borrow key present when

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_from_bytes(bytes, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
