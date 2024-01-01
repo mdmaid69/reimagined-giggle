31f45ea20a10ff31b63b26e7d2e0678164aaa831
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:06:43 +0000

    enthusiastic compare them without rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
