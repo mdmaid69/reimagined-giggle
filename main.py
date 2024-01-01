98352cbc077df3d816a534de2885d6f59a2ea6f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:47 +0000

    trap many law strict weather

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
