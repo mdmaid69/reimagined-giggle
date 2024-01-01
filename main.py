2160a8ee2cf0c9edb5aa590bd858e0124a40ef7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:11 +0000

    slowly survive comeout cool change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
