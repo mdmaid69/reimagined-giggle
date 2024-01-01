8049791cf12de4b0de4f73c0483e867beeafadd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:42:25 +0000

    factory slide protest must fear

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
