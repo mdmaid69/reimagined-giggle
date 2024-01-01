84e05d88c9c473d0809a40c0dd003836c372bba8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:40 +0000

    private wide dinner anything Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
