4d69815b1e43056da225512e749f2c98e3f7eb07
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:00:23 +0000

    value worried settle tour afford

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
