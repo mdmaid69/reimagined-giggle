801e62e490f39b7a7ff4a2d4c79b285d1fd5352e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:50:34 +0000

    place treatment waste arm so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
