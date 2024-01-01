4a7f5107dbf4c9baa4e241a6be5fc32f3e434722
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:45:51 +0000

    crash actual divide its smooth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
