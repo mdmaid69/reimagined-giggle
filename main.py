01da61c362684cb8288cec1b794adfa11836ea45
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:27 +0000

    hate shock communicate strike exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
