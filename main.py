06169bbb6247b6496f35d4a818f22d92a72eb38f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:41 +0000

    turnback figure air laugh direction

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
