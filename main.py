abd95b3230e69a5fa2fcf8f1efa0f082921d68f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:44 +0000

    disappear whole balance front style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
