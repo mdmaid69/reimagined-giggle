b3e59bc4f1fbe50c955f8a16598ec7298a4fa954
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:11 +0000

    debt turnover vote seat 568energy

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
