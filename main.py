afd8e1c76c25e90a2c2a8ab5b4811c8ddfe89c3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:26:30 +0000

    friendly mark stretch road past

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
