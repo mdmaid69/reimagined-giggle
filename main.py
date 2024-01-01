25ab595fa970fe40d7943350d1c7e2195f45cf21
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:15 +0000

    dance rid right round warn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
