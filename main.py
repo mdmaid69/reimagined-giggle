e87d7b8aeded22c9713d97c6631179e054711bcc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:06 +0000

    material ill advantage sort my

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def add_numbers(a, b):
-        return a + b
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
