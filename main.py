a8e6e221a526ab827a09710f45fd2ae761fac322
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:05:10 +0000

    action waste yourself trade fear

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
