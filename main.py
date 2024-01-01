de0fafa7268487d6f2b69390166589f33d1b030c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:50 +0000

    draw by sweep offer medium

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_min(numbers):
+        return min(numbers)
