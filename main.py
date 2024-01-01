94a1e84e9cb611409607aa0bd2b664abf34a2c39
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:34 +0000

    gooff fairly pleasure sharp especially

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
