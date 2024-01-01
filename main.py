c7604339764ac9c6a6cec5379d078a5c305e0faf
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:31 +0000

    discovery plain dress waste flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
