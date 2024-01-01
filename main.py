b51a4b80fdb8388655dcc38ee4f853351f103813
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:44:57 +0000

    guess switch way tap stare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
