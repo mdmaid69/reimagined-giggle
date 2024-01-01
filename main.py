014626310dd733456b5e7effe9538336ef94d469
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:52 +0000

    deserve comeup front draw point

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
