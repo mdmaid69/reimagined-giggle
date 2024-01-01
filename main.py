c68bdf4153d3326da4ebd974c251def3eed32310
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:35:05 +0000

    measure past pronunciation find memory

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
