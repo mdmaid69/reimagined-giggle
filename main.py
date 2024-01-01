cc40abf5c202fa5e2c23c38e715b04606758cfd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:59:34 +0000

    new 179bite defend anywhere amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
