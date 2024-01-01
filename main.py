c761e9f33527ac8dc85bf08d3bec4fe25d3e9b3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:09:52 +0000

    hear pretty comeback square breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def find_max(lst):
+        return max(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
