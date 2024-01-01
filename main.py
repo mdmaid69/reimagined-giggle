98f702000322e256a561f451df616c0e6ef9f835
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:15:39 +0000

    cut giveup problem voice should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
