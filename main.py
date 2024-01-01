b9d7840122fbbc3bcd848342d98f74ba5aab1807
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:01 +0000

    mysterious waste something order office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
