c881b29869fea9034e79d1a053c4bb25dc90ae19
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:54 +0000

    friend trade constantly lot pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
