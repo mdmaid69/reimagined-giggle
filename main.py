f0ed11e4ba2171f22761771ac3fd9612d5e477b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:16:43 +0000

    height hit respect book moon

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
