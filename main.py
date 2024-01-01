2f1f503c5dae94dbeb3297a395dc172d4a9d23cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:16:00 +0000

    big deep copy notice godown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
