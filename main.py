cf42d82d16d66ca84001db8c576fa401eecac55b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:56:07 +0000

    look listenfor negative interrupt any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
