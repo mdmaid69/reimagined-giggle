e283223d50efae8792c6a45e717363f68a17d662
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:57:55 +0000

    comeoff stranger firm unit suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
