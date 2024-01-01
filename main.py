11d39503567ce65972afcdb31b7aa498614dee72
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:36:45 +0000

    change hang solve public score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
