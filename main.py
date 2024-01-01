556629bd0cbf0d2ab820f4602f1ed5dbfb29bddc
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:39:45 +0000

    original beneath underneath runinto strong

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
