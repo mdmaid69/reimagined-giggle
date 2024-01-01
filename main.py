bbd547de3ce903f2f8f667754defc8a5368d9e4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:00 +0000

    same extra safe naturally naturally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
+import array
+def reverse_array(array):
+        array.reverse()
