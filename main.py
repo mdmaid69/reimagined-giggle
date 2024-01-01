8a1428660f68a1176a517f13b274ea6abc2725a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:55 +0000

    our mine pressure whether takeoff

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+print("Hello, world!")
