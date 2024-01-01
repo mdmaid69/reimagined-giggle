120ac7ed9aac25bedc2e126f2f621d84baa5a122
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:51:26 +0000

    flat notice tear rarely below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def add_numbers(x, y):
+        return x + y
