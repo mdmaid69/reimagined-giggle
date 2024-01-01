bef64f81b46884d80cddd3b9fff14f1191c6fdad
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:34:20 +0000

    kind complete match surprise container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
