67cde83b92e3d90a0f71ba62bd5846242ea50fc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:21 +0000

    slowly either fun steady fork

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
