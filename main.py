b9e9d256e2790a071e8a46e4e38a4f593a34a897
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:44 +0000

    machine fortunately generally model few

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
