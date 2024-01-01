419c860ca25210255692bbcf6f3cd9cb0a1f3c28
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:09 +0000

    quit lock break now mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
