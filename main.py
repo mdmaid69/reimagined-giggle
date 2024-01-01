cecfa5e1be01c19dd6e0bb16d15f2b426cda3346
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:47 +0000

    behind sweep miss report lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
