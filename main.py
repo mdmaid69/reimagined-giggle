1904c30e355210b56697e0de9837121ed3790170
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:06:09 +0000

    dish unfair wonder frequent artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
