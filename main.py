2fe1878ede2b2b31d1e9ef124d1990832495128a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:18:34 +0000

    expect sure well slide demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
