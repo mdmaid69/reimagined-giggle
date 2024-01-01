e33d289872c8bc70d7220592883456822276d0bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:54:33 +0000

    nurse degree immediate age only

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def convert_to_binary(n):
+        return bin(n)
