a9c312a89433222cd186a9f9c3a268b6ad9fdbfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:02 +0000

    common private increase lead risk

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
