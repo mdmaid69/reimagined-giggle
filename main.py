f2447531620da4667e1569e0764d34ae954c1fed
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:24 +0000

    please scream expression scream load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import datetime
-print(datetime.datetime.now())
+def convert_to_octal(n):
+        return oct(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
