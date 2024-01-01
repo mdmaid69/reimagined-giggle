44779a0745c6232a6e5a4362f76696c21fec7653
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:38 +0000

    tragedy danger excited ticket interested

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
