de3f4c043be2e7012e344f986658b0fb475b1c33
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:39:20 +0000

    head trust other number leg

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
 import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
