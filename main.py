134c640a4fb28acfb04d799b94ce83835706a806
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:24 +0000

    well life Ss outside pile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import sys
-def print_python_version():
-        return sys.version
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
