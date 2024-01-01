4d1583a4be4964451a793629661b805a757c66d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:17 +0000

    effort special gate bury apply

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
