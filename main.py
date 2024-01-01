3d685a5d06d677327f2c87aa5f7d1729e5774af1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:27:53 +0000

    wish serious gentle view combine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_odd(n):
+        return n % 2 != 0
