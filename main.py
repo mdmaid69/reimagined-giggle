96c5440648718f928bfb6600155b8f93bd4db2c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:08:07 +0000

    frightened lookup original charge mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
