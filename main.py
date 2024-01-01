532dcf7c2e8b3f31833759f31f5d1afe89aa1955
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:49:06 +0000

    raise tip guess giveup anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
