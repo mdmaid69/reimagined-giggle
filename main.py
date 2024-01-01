e9a82b4e6f2fe31c4936454c824262b5fb6caa3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:26 +0000

    Ww old prepare plastic reason

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
