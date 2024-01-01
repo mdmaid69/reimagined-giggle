8ac041eb3cf349b671d8ba24fa22d361760a789c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:23 +0000

    free since let control sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
