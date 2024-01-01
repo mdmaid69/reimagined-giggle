504c9d5fbff36ca32daaebc1bac29021228aefd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:43:15 +0000

    shoot boring male cinema finally

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
