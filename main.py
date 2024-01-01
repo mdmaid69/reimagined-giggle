eb35a56abd4da3b6710844d58a8d75b91a4f9a03
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:18:22 +0000

    he apparently problem simple fine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
