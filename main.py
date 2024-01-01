3b7cdd7ed93247ca7592acb0b6a46162af0ea415
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:08:16 +0000

    surprising fair pass responsibility satisfied

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
