6064b468b5c7b89830a05ac660ff5b248a8b1b55
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:30:03 +0000

    lonely complain bang follow best

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
