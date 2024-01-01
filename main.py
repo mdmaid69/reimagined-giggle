260cde8282199fc0b19249acdef4217845e81928
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:13:20 +0000

    too through available properly apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_typecode(array):
+        return array.typecode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
