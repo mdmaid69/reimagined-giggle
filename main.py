b38ba725e663a9166bb79bc5175a79f0ca3b0ecc
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:34:27 +0000

    left car partner stay sweet

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
