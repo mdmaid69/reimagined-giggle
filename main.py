70cf294b35b5215791ca66b06de6c3984a9466f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:42 +0000

    grade telephone illness book beautiful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
