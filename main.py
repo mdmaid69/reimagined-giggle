06cd218e49c139f8cf0eb5150a3770410e7e5ea1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:47 +0000

    cut obvious schedule branch along

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
