aa0d2cfa047ce97a2be404fee3701c7a47af36b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:51 +0000

    branch comeup small because especially

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
