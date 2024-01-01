d88fa71c7dbc275acc2b53dc38553c9f2c3bbb6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:17 +0000

    than ashamed every minority model

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
