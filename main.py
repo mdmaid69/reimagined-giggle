84190428a4e936f2433484b99e01bfe242a48625
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:52:14 +0000

    finally check precise name disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
