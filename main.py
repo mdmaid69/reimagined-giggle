63d09810218a32174e9953f5d55092c65bddff65
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:03:45 +0000

    also use someone invitation maybe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def append_to_array(array, item):
+        array.append(item)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
