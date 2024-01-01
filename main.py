1fe42029684c9067b70fce92eb05125ff72e18eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:47 +0000

    pretty prefer compete science address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
