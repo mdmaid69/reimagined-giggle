684de97c2a7e7b1043526b787014e4f19d40e38b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:32:07 +0000

    university likely single love boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
