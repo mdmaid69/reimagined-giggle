08ea1a03c48d657cbfe7f8247ded787d26cf78fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:36 +0000

    pencil climb ticket start port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def square_number(x):
+        return x**2
+import sys
+def exit_program():
+        sys.exit()
