142329e3c6d2a96675261f7424dcb05acdc9653b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:33 +0000

    address job turnoff pretty range

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-def greet(name):
-        print(f"Hello, {name}!")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
