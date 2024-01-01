4b3103422cdce80fcdf88dfab13a1878e2343907
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:50:32 +0000

    left away pretend common check

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
