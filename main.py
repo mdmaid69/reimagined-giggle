b95971f90929c1c724d4ea9aef515c6b29a6a779
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:42 +0000

    or sense name tour could

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
