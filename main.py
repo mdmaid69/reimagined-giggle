2a4d6bb3871573764f2f3dc1433c3e1fceeacca6
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:47 +0000

    suit talkoutof careful create port

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
