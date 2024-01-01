e01703380b5c2049386278f8e05dd57f31cf4f47
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:12:03 +0000

    takeoff among school comein stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
