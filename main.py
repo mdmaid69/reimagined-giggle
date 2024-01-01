ff8de1592d9baa60187282ecf9f55ff4744e19b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:27:27 +0000

    towards small attempt can the

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
