653c962c427b26266a53c790461d1c2a56f7a7c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:55 +0000

    large answer peace race out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
