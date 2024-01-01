f0e2fddc066594d99491da7a8836100d6e2a5c61
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:32 +0000

    ask surprising previous any in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
