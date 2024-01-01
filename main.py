c3d0a45aa1e2fce1b36269c45a5a189e11d44deb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:57 +0000

    telephone watch where promise trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
