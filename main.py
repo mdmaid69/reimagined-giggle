97b49e61f57bd1078c598ec6b8770ebec5d0050e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:45 +0000

    prove human lie announce best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_as_memoryview(array):
+        return memoryview(array)
