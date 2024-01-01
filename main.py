ef35b25711ae11a765d1527b833a550d6f30bdb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:58:11 +0000

    fierce walk opposite life pencil

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_power(work, time):
-        return work / time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
