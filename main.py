d5ebe68c6454d3102f1027d2bb527abb86030568
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:19 +0000

    direction voice breakdown impression of

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
