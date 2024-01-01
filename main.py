cc5f9ba371aab505cbd5610fc27883372ad6aa0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:49 +0000

    ignore print cum kind software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
