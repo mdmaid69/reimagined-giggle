bf801c2daeb493335b6cc798778fcbf75ec6cfe1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:53 +0000

    beyond Zz to sticky wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-print(random.randint(0, 100))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
