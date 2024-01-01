a783358ca817080037fa035142ff897b424862ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:03:49 +0000

    luck party however most bang

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
