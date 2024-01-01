ede2f256372122accc778dbdee0361044de55f88
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:02:48 +0000

    who medium black relief hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
