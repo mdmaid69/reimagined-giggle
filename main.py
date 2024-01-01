94b19e4aee0a4663321447ccc1fadae8d5dd1a40
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:17:41 +0000

    mind exchange raw convince unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def convert_to_binary(n):
+        return bin(n)
