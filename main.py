95aa613c5291a0c42e4a7a87e1226b41672b0539
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:47:11 +0000

    brain odd team boat contribute

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
