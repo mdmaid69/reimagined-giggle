ed7234e50f0ddc92ec8f38640ce12035d12e3e8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:16:42 +0000

    suggestion no try 567enemy record

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
