1598d626e85e99db2a013561b980ca1d3130906a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:13 +0000

    takeout shape deal translate low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
