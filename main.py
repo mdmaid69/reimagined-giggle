c776b94e9fb01516ba3a767a33f54faa5b838f55
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:24:05 +0000

    takeover runover win winter partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
