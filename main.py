96221352d430722ab2180f40b5199a3ad6001106
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:18:11 +0000

    telephone sign cut mean accordingto

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
