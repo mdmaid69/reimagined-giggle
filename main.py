19281f8d3592dee1bf743ce0f898f431cba108b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:12 +0000

    tidy plenty relax example same

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
