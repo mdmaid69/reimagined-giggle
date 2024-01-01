c08e9296f9ef601fa107a66f17e6d71ebcf475eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:19 +0000

    mail error murder combination well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import array
-def get_array_as_str(array):
-        return str(array)
-import time
-def get_current_time():
-        return time.time()
+def set_array_item(array, i, item):
+        array[i] = item
