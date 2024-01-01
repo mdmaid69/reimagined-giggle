bf6607b0eec83ee1d93effb740a0a831aef74461
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:35:44 +0000

    quality principle unite involve plate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
