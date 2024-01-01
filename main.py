51165a3b2aacd7ff3f792c4f5e7a5f4a4730043b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:36 +0000

    visit mistake shame thank journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
