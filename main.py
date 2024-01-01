fac2bd9861d2b694197a9f771030236977596ab0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:16 +0000

    hard wire lend point top

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-from collections import Counter
-print(Counter("hello world"))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_density(mass, volume):
+        return mass / volume
