ae38a084218352da0c918c44b50435b80e586043
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:27 +0000

    weigh gift perhaps listen task

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_float(array):
+        return float(array[0])
