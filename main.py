575b20c3b70e78cee131fd8ead8c98484d1bd509
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:09:55 +0000

    steep around between clothes conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
