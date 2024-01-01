180b0907635ce37916b0e9fbb0df182c31c1a332
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:11 +0000

    tell blame bath mail ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
