1844e6c29b6e4566a970ed90c82f939265478e2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:49 +0000

    frequent make estimate great 568energy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
