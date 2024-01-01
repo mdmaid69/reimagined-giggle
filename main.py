289c28bf4ad613fa1152b23121b7c0ebbd86302f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:50 +0000

    pattern Hh pen band turnover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
