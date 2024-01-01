0bc80a1efd0194d5ba92191836a77f3bd97e4252
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:34 +0000

    tradition process opportunity guess tip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
