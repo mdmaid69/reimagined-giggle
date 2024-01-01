82ce1b8bb7fa9bb946ab20199d98302fdf939ea8
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:02:23 +0000

    lie related slight shoulder education

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
