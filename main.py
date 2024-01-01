72e19256f6136bb3056305e9fbe48863325dbe66
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:19:13 +0000

    bottom be achieve around size

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_max(lst):
+        return max(lst)
+  def calculate_area_rectangle(l, w):
+        return l * w
