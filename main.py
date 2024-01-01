85955e8910b3785d095c569dfb04438560568b90
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:11 +0000

    correction fence possibility uncomfortable create

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
