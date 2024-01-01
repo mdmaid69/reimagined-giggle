b2f79808e40ccf666bdc21bf9377f17e68d22386
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:28:17 +0000

    wash complicated properly base badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
