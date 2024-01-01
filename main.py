ad951de6d8168d6d71883842b78a567ffcb73f98
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:42:12 +0000

    hurry star place spend heat

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_binary(n):
+        return bin(n)
