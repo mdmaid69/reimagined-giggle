f429509de36f6ad80940903c7bca8a41bb98194b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:10:45 +0000

    nearly pollution bar imagination cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
