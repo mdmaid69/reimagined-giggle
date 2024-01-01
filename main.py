8e781a579e444053b0b7dac640f2958ec3550fc8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:55 +0000

    influence motion middle whatever ordinary

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
