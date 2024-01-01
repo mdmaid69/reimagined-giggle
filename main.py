e155fe345e5d2594115779aad3d0a5d042864748
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:51 +0000

    Nn before glass instrument embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
