480a6d3b820981c56ee062c8f4e9083dbc172e3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:40 +0000

    Jj welcome once settle contents

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+for i in range(5):
+        print(i)
