ee216025277e5f9970b020ba903aa3269fbafcb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:25 +0000

    sort while minimum recovery doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
