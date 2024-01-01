865972a94b4e2aeaaaa4f98db580ef82e1888144
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:11:22 +0000

    card serve minimum suggestion accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
