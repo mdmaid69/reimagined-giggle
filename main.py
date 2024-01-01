77d21af9fc742e13d317bc577e42d0e71bfb85f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:25 +0000

    live doubt stop hour joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
