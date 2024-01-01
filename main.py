03382bcfb8020c6bd56073fee46e71a543076268
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:17:50 +0000

    scene extremely explanation town rent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
