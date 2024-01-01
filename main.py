b1bd56de79dcc4459b8e1e638de024988fbf5383
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:51 +0000

    lose description possibly connection her

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
