0cc061f77e6bc9cad59cbf8b2a9b5b4e0aa0ffce
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:20:01 +0000

    away movement will become embarrassed

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_min(numbers):
+        return min(numbers)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
