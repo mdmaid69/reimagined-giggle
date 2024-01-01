fc8534c8e99663b21cf4455b145aaeae325e4e83
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:31:19 +0000

    outside main brother compare sorry

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
