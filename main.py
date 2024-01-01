7a760e0120f4a2f86533a18db56a71d424c8cc2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:41:33 +0000

    demand nearly object pure wrong

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
