ffaa90d1b8c56c92d462d618e658d54d83b00427
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:41 +0000

    unlikely more trip ignore service

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
