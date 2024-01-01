8854a7b41cdb0ac17ca87d85a53a2a169dfa61fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:00:15 +0000

    choice pretty country minimum offend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
