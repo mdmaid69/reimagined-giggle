8bae7e59fee007229fc7e8b90ac35eb69c2a1ccb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:13:58 +0000

    about mind sink total another

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-text = "Hello, world!"
-print("Characters:", len(text))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def append_to_array(array, item):
+        array.append(item)
