a8c070dbe45ba2d7cdfb7cd3e27b5554a5805b44
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:14:55 +0000

    machine die once Ee invite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def reverse_string(s):
+        return s[::-1]
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
