48692ca22865f4b706a1ca17084e584656426914
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:03 +0000

    plant yet string part talkinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
