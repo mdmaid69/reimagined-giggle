200535ddb1b2c2d6ee56de3afc265acd6659c633
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:49:45 +0000

    thirsty strength mean whoever fire

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(10): print(i)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
