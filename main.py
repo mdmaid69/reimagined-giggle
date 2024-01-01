af898a4ac3c99a3767918fd8b320eba83ecb4df7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:45 +0000

    shut forgive almost string sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
