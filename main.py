04bc3fa46f9a8ed6f17d260f81b19ae1d11a2b6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:51:41 +0000

    approve exchange start bringback majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
