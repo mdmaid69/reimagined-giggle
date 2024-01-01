0ae950d7dcf6bca4c2dbed6c7a439631a40d9e52
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:58:57 +0000

    ill use cause obvious considering

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
