730d8ed25351777ab184204d8138acecc970fc28
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:56:59 +0000

    design trap they mean so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import platform
+def get_os_info():
+        return platform.uname()
