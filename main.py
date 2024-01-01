a9c2a56a582b6487a361c5fa5ee532008be57b88
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:06 +0000

    maximum building message tear disturbing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
