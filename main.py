40e7460c154fced63e65ea8fd14c113b60bc5980
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:57 +0000

    bread invitation unite cheque switch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def split_path(path):
+        return os.path.split(path)
