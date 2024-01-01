00a3f1f76af3066bdd0fa58d583e030503d7bbef
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:40 +0000

    own blue feel hit mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
