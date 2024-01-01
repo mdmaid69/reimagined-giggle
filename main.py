5a695e303c95cf2669e359c4419683e2c54fb794
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:41:47 +0000

    turndown well base joke type

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
