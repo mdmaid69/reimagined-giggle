ad2707cfd3c0c968486df91f2fb8389f147267c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:18:23 +0000

    degree surprised rough common music

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import sys
+def exit_program():
+        sys.exit()
