1e7dadf626c97084cc915f4ee3d138dcc34cff20
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:03:32 +0000

    fault early generally door serve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
