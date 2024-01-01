f4811c7ca9ff7d4bb3930adfb7850f0747b4c0c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:18 +0000

    goout work succeed mind list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
