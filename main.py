041d9630cbd7f8738d627ffa31c2fbda588ba4eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:59:25 +0000

    absolutely cinema share burst exist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_even(n):
+        return n % 2 == 0
