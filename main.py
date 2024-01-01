1f13797c2f026a9ac1d0d450fdacdc5e5c260608
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:26 +0000

    impression since much valuable just

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import sys
+print(sys.version)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
