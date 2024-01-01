0dee3ac4397db4781fffc8cf8f067998bbfb2561
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:53:45 +0000

    plenty means fit no takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
