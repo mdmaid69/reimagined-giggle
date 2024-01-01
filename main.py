71dddea1d538bcd4ef56742426151a9d7d5e34b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:16:52 +0000

    go shop flow destroy in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
