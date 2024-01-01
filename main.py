f945bdcdc8beeca33d900d3f4548d160c2a308de
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:49:44 +0000

    change steer circumstance offend instead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def convert_to_octal(n):
+        return oct(n)
