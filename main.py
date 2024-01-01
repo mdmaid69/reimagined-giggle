60ae050678c605188f6d15179e4ef7712423d72e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:37 +0000

    for straight attention gate instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def sort_list(lst):
+        return sorted(lst)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
