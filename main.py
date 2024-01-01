5ec83ce4fd05a4c1f85bb91f347b67fb1652cc3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:02:27 +0000

    food real neat visit good

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import datetime
-print(datetime.datetime.now())
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def is_even(n):
+        return n % 2 == 0
