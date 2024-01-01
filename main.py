9452b16fb69e4816aa0f8a3d37dfcb0d51cce0aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:27 +0000

    pocket slight university month side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
