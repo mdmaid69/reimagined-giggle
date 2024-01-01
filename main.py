909c4adffb1c7626f13e4a67c4f8e4cc0be16a36
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:30 +0000

    minor sleep boil which immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
