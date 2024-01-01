057971a9ca7db420011b87258edacf4516e65a3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:15 +0000

    opportunity increase interview scream exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
