3ffcdadb1f1e1dbf8901fa7abec0bf4bb46b9bf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:00:25 +0000

    certainly convince business her aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
