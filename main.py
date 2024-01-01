14d3b7a3e26b1703d11e1d5ba714f3a9b96bc19f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:04 +0000

    front light key all duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def reverse_list(lst):
-        return lst[::-1]
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
