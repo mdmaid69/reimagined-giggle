bc7632f7394a0d70be50d427cdd59fdcef5f3e6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:19 +0000

    job dress satisfactory outside jump

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
