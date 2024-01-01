cfeffbc652396aabffa0d80f1179090cb90a42a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:43:48 +0000

    poison round yourself become instead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
