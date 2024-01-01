60883a57a7d33d1123e3fd16ac58a87f5a4e4064
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:53:06 +0000

    order convincing problem across bunch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def count_elements(lst):
-        return len(lst)
+def get_array_as_complex(array):
+        return complex(array[0])
