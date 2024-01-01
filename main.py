94daa27cdfcfa0291f0b1625347870fe0a01be2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:00:49 +0000

    dear nerve married Gg display

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
