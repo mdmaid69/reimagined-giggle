f56b007bbf93cd5383cedf7d42f06222c2c7e269
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:58 +0000

    for pinch file deaf she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
