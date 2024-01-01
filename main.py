60a5ebaf45b0c925fa993cdb49b9f58c698c1336
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:37:06 +0000

    journey thorough nonsense sky organization

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+  def square_number(x):
+        return x**2
 import array
-def clear_array(array):
-        array *= 0
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def convert_array_to_string(array):
+        return array.tostring()
