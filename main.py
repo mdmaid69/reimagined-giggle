b7d99d095ecd1a5b2aec14a6624838002491cbc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:50:39 +0000

    back criticize market splash reject

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
