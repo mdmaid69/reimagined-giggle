bbbe33bad3251a0854cb48ee488c785ab8b2be6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:57:28 +0000

    train call empty eat holiday

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def add_numbers(x, y):
-        return x + y
-def reverse_string(s):
-        return s[::-1]
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
