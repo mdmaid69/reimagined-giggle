e9a302f32aaced10a53835388e3f454629cb6516
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:55 +0000

    factory desperate offer dance count

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_string_from_array(array):
+        return array.tobytes()
