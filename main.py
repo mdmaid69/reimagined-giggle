b0b6c8c2130281f7989f6a96c1371a2ead60961c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:19:03 +0000

    accidental opportunity comeoff blame correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
