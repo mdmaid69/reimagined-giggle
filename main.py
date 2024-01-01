6a1b40e8e84cff874686f97ef81211e951d923eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:47 +0000

    minute square exact without realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
