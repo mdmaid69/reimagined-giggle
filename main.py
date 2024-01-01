f4c7d9994b63bdc27a24534c049882bb4e6051c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:30 +0000

    dick anxious behaviour demand worry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def convert_to_hex(n):
+        return hex(n)
