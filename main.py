810fa08e0124da239b2758a4f0c7ea5b27533112
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:19 +0000

    lookafter general generally circumstance slide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
