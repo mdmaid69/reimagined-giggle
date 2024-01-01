6918cf57107cfc27374b2a8784c878d129745490
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:21 +0000

    way bury stir physical thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
