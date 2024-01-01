1d2c0e6a0a9459042b9cc653076011335cce2f4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:53 +0000

    eager attention whenever comeback ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
