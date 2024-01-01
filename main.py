28d09d70f62144e04d7f436a394cd8213fe948d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:39 +0000

    truth translate tour wash rush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
