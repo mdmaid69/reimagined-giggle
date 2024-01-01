a0c369192f3aec99ec64be39a71bbe7491a9d0b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:53:20 +0000

    feeling breakup pretend march guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
