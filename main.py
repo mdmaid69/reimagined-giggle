55aef113c82996162c42a3063c3be1e328f0b81a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:39 +0000

    pour guess with label hurt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import time
+print(time.time())
