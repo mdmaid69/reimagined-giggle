260b2a423d50864db9ab1aad60509417c7bf27a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:37:09 +0000

    noone deliver news mail cinema

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def greet(name):
+        print(f"Hello, {name}!")
