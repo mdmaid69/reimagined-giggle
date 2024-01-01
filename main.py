485cb9959fb62d9a627724fe07df7638a74bed50
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:52 +0000

    goon interesting disappear production container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
