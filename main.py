d93ae7f0a9a8d2d6a2c5ed811224ea44242dc049
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:03 +0000

    standard answer long wander crash

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
