b357e84e9af1ce26dbb7b2a8fd0a07121053b9fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:19:15 +0000

    life imagination naturally mean one

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
