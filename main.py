bdfc0a6705fa18944d8e60860517f2fcf0a6e248
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:16:23 +0000

    accordingto day accidentally release test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
