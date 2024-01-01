89e84242eee9bc410174cf37549176d4553c4941
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:09:54 +0000

    thin OKorokay gather pleasure me

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
