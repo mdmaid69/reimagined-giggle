0a3039fd8fe9b09e789d76cc4531e5421353493e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:33 +0000

    sort smoke hit welcome roof

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
