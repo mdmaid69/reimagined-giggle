68c0ea849cb1f21ab3ed3261814cfce61c04a830
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:21 +0000

    bringforward unkind section equal name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
