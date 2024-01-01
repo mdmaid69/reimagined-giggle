efa52fd53b470f1c69febb1351c2737f82284e1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:36:40 +0000

    alive home clearly not exact

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
