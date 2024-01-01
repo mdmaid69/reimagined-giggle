8aedad02a10351fa533a03235eac065c9cb03a07
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:11:36 +0000

    suddenly fork behind plant come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
