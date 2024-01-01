c543c8eecb428bca80093e47b1c80d2bc24a247b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:08:54 +0000

    ago friend concentration matter road

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
