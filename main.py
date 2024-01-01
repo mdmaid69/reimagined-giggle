67a57cd8dd1459bcc552ad2cec0e4861d95dd699
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:39:46 +0000

    about OKorokay rent its cough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pressure(force, area):
+        return force / area
