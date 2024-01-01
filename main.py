3da9a14fa17094371943eb7b1cdb73f1c39462af
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:31 +0000

    opposite hold walk turn old

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def remove_duplicates(lst):
-        return list(set(lst))
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
