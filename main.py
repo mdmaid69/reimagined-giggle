63c6bdf1b481f50b4094e36d0f97e0424033be18
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:31:32 +0000

    each cum giveup wet mean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import datetime
-def get_current_date():
-        return datetime.date.today()
-from collections import Counter
-print(Counter("hello world"))
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
