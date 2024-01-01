655276a36da38f7b5bb2e9213e6d31cd84d7f4e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:32 +0000

    capital float Nn point row

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+print(datetime.datetime.now())
