8e193d6dc415cbe5af0141cfae062e0994353af6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:08 +0000

    only conversation bend bit slow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
