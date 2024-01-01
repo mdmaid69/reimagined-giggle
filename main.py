02b4aeff0bfc357d48fde9d7447fda9ebf54b1b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:49:49 +0000

    issue flight waste sort figure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
