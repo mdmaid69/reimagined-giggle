e4c9c75ede0573f88df2e72df35257ccb8de493c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:05 +0000

    consider ticket month cheat comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
