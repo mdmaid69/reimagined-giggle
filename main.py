4528e83ec434c626bb7685a240165540e36050ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:30 +0000

    accidental careful about fly sleep

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
