59eeaead201af04b40567bf3bc4978d8f660a3c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:04 +0000

    keepoff criticize willing first imagine

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
