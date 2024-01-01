b1266b84d3b21c8142e76cf6b5b15bc0fccbf494
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:42:05 +0000

    tend around turnoff risk bound

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def reverse_string(s):
-        return s[::-1]
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_union(list1, list2):
+        return set(list1) | set(list2)
