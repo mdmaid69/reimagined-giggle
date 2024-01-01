6269cec17da6f152dcea2b0e273b374999a7e94f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:08 +0000

    confused float unfair Itself balance

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
