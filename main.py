7c1cedb83cc832e10ba5802d6c329a9e1425b7c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:27 +0000

    everybody can ever forget admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
