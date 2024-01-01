d6ff026e0012ec761ee60dc0fb364af4cd0f782b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:40:41 +0000

    characteristic worry convinced wing cause

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
