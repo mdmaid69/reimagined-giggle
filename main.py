98b3093a638c41dd9b9d08b6c8a33bb195a8df2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:39:06 +0000

    shadow off comeout lie black

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
