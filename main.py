0da2d854c13bbf979c7c4c2abaa4ecc5c91b9983
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:19:20 +0000

    breakdown shop always wet strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
