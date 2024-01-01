b24d2fa2036bcaccd56d057f54699b42e742124d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:33:26 +0000

    control shine if open summer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
