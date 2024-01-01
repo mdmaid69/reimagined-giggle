fd451cd0cf1a109d1407ebbafe437f09eced159a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:36 +0000

    rest temperature available live quickly

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
