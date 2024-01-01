dcad4d17ea20e22d37a4498f0da3489439ded914
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:24:22 +0000

    talkback excuse interest pain tongue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import numpy as np
+print(np.array([1, 2, 3]))
