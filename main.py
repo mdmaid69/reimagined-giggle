4641a6077b0f3290675bfae60728d908a132f62c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:37 +0000

    world knock guilty money here

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def subtract_numbers(x, y):
+        return x - y
