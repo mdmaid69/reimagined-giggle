b67cdae66eab0a1e529ff199127afddd5c308377
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:34:37 +0000

    undo smile cum worth serious

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def sort_numbers(numbers):
+        return sorted(numbers)
