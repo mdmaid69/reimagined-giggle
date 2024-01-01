053a21bb25f959617ca77c42973cf33266ba1106
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:13:19 +0000

    university legal his recovery opinion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
