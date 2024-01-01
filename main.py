eb1ed85ed7f6c0a2b12bdaed5ee4c708941b2cee
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:50:02 +0000

    main profit ride judge too

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
