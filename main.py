d3195aa5f39853f61b67734ac417c796738fb96f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:24:12 +0000

    excited label leave embarrassing dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def find_max(numbers):
+        return max(numbers)
