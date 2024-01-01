3fee66ddab329278c346b8f632797ece65708ff3
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:47:45 +0000

    begin comfort offer carry proud

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
