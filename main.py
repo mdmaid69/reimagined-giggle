25471f3d964aada87d1919d4fee94c4e7acf4c71
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:02:31 +0000

    wrap tell equal justice wear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+for i in range(5):
+        print(i)
