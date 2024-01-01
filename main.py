89f8f0bf524bcd81bba14bc3541ce104ce0c6092
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:54:19 +0000

    after Vv public breakin glad

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_power(work, time):
+        return work / time
