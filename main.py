c6146a278cae2dfcb10d69f7048a0251c99859f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:13 +0000

    miss issue in successful friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
