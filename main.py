04a4d8725806094c46ba83942659df597409043c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:14 +0000

    address side either around add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
