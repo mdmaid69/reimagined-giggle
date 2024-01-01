9980dc5f3a6c1140ffe482abadac0cde67632dec
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:23 +0000

    eat post early over human

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
