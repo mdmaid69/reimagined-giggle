1cf23c0e11d90598d0dcc825b677001815834376
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:46 +0000

    back cover turnoff obvious beginning

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
