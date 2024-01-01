9b7dd6a27627fb7bc765825d1c44be888785cbf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:20 +0000

    how needle remain problem boil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
