325d678069e4387f451494eac0e749cfb542d45b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:39 +0000

    measure like practice train eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
