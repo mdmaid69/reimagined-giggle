e1f06e0b763d8d68bb2aa18f7edc495f9da5ea9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:50:30 +0000

    soon sometimes hard knock calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
