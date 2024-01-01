3d94b515aef520a6b85c8f0188a1fad407c61964
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:43 +0000

    trip touch shoulder short old

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
