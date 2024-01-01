1bdab96cdfe5744ad5f54ecb952c9e7de34260d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:39 +0000

    afford feel hang rich send

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-import sys
-def exit_program():
-        sys.exit()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
