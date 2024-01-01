82d34be17e66982fe239df350a5e845b0060fded
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:30 +0000

    judge view boil calmly consider

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
