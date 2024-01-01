46702aacc4b6197d661db0e13c7eaa78428f1f9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:52 +0000

    sensitive performance twist unit gap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def sort_list(lst):
+        return sorted(lst)
