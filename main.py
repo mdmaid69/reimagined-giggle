47931463e2ad11491fe3b26ba99afaa94de07d7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:41:26 +0000

    language wear crowd talkoutof practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def get_current_time():
-        return time.time()
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
