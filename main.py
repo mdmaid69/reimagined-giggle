616458aedff32383077e1adb5468ee99b904e84b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:22:35 +0000

    friendly travel persuade hope foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
