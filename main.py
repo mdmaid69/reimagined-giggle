a8067ef698d77afc8492964828ce50e74a8305d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:33:34 +0000

    scale film Jj argument address

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def convert_to_hex(n):
+        return hex(n)
