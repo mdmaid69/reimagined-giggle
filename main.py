97b1c3e2e0e3e47a48b6ffd6b02f9f7965ec699d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:37:24 +0000

    away rest commit disturb mend

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
