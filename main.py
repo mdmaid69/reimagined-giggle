cee4b68309cbf200011385479bdfea53a22c0c8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:58 +0000

    centre she downstairs in land

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  def sort_list(lst):
-        return sorted(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_length(array):
+        return len(array)
