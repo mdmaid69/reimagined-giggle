30b6d6c45705a0e93910ec01b58da0fd5bc0926e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:37:04 +0000

    lookafter steam group evidence together

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
