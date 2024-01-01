1ef1f7d67d956cf719ee402b52cf21adcdaf32ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:28 +0000

    ready innocent shake float classroom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import collections
+def create_user_dict():
+        return collections.UserDict()
