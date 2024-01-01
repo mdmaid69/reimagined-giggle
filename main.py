34bc0ba907330de31f3838f76b6b11cb7ea2a911
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:03 +0000

    difference again good display leave

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def calculate_average(lst):
+        return sum(lst) / len(lst)
