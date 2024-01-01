8a3497b8219eeb82af1f8b5d4d32b621033bdd19
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:51 +0000

    quickly she from weekend commit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import os
+print(os.getcwd())
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
