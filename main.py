07658fbe79c3b199e29c160e9a5ab1ffa21523f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:41:49 +0000

    tired fresh organization about lean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def remove_from_array(array, item):
+        array.remove(item)
