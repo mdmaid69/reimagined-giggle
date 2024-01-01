b42055478948775257932cdfdea59c06656c0b91
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:26:04 +0000

    related exactly comein thinkof evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
