399fbc35c0b3ed2938835d08cc77c4420ec3756b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:35:59 +0000

    should alone lookup already goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
