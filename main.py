212266130279c549ab5d6b9dc402e2a4f8db0e80
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:21 +0000

    embarrassing word poor practise pleasant

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-import math
-print(math.pi)
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
