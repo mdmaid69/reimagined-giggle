8cbb70c4c02d6327c4adbe83683c497b8b2584e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:12 +0000

    favour fault abroad scale expensive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import json
+print(json.dumps({"name": "John", "age": 30}))
