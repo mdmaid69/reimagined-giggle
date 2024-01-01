80af67e8d4df7342593812d3886b0f221823bbc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:55:15 +0000

    ilovedicks uncertain task poison heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
