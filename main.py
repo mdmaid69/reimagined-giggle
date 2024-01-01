70e30f1e107b3111afe56f3cbee3b4fa887f608f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:02 +0000

    method stranger form regional Rr

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
