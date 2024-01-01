62046794a81de7fc61e6b89f68c74a4b0738bc98
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:24 +0000

    question relief immediately fail thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
