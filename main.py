a986ddce4bd88d416a7cf8f4c5d7793e61b93560
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:05 +0000

    purpose lose town attract ocean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
