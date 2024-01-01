a51e5c363c0e8088151502bd9c2c9d22546d6f57
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:46 +0000

    fix charge wish perhaps hard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
