6b2e00777694e7b1cec498830db117c0f9432b20
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:20:13 +0000

    several recent national between comeup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import numpy as np
+print(np.array([1, 2, 3]))
