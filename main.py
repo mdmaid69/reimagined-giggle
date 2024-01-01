9c3dd7098dc287bb38f4d0443e37d311d7e6d0da
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:00 +0000

    emphasize serious broken part please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
