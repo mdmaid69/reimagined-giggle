0b68eba52a5bb489fa278da06e859613ae56e686
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:07 +0000

    buy me talkoutof period joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_owner(file_name):
         return os.stat(file_name).st_uid
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import os
+def remove_directory(path):
+        os.rmdir(path)
