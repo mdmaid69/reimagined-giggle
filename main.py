35e7ca6b66d062cba6d0a2230f0302d76983d85e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:40 +0000

    leave petrol breakdown peace fight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
