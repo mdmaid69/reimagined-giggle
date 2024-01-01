642a86213745953f339e0e9321bd37bc339aa212
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:04 +0000

    turnoff deal deep group Nn

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
