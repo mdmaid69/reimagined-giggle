a702b625f7d17436092f77ba22d2f877d8e01a94
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:32 +0000

    material wire float sell sensible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
