3a43111a64df1f9eeffc069a38fec859d0f201a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:49 +0000

    language explain guide hard shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
