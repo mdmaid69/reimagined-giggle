ad95c5cecbbfabbda238021e242d1287be0eb499
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:56:36 +0000

    world good subject wealth welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
