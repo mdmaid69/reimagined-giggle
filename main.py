d9ea8e4e4519f5367c7c49a8c532bcb18f7dbe96
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:20 +0000

    confused disturb guess respect complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
