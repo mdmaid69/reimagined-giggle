c0534eb813238b545f2f8d0a7ca55af3e88710b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:43:55 +0000

    characteristic understand afterwards force constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import collections
 def create_user_dict():
         return collections.UserDict()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
