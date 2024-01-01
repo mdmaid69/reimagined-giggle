a5935c0191802309bef638654c224044d2949b68
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:48 +0000

    immediately press continue fear male

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
