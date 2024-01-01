904e917be43db03083495f0960818bffca15ef6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:49 +0000

    custom soon intend runaway wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
