c4ddff7d82be872fc8538c22eb28fad8df03746c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:05 +0000

    thank defeat confidence any top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
