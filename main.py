ad29714330c08b209310625174b45f041a94d711
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:49:24 +0000

    fall underneath cut compare pleased

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
 def sort_list(lst):
         return sorted(lst)
