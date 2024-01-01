c446fcdba7e9e9bf9b0ea4e184893e4b38f2e375
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:15 +0000

    phone season correction top coat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
