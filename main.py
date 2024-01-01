5facb374f0c923f4a114d754dfe176a785e0851d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:13 +0000

    help rid compare exploration spite

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
