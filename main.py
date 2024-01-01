cd7231ae82684141b90686447e30afb8da04c3e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:25 +0000

    walk start breakthrough poor again

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+for i in range(10): print(i)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
