26980ddbabd9167818aac80a8f9b96b4a9891c69
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:32:18 +0000

    ball colour lonely snow congratulate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
