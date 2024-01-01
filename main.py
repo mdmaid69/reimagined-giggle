08e8fde5bfef06049dd3bbd35774cf5773b98ed5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:26:59 +0000

    another family design relief unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import os
+def get_current_working_directory():
+        return os.getcwd()
