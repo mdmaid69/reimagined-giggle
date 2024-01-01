a051ad6291b971829d9cbaa0520cae903f56b038
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:08 +0000

    school comeup or plan bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
