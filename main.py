5612d163f9e4ddbd435af00fddda45e611e5ddf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:33 +0000

    exit temper out rush store

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
