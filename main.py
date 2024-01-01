2f03fac59b40f7967a7e9beeaf4e38c1821c9735
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:42:06 +0000

    glad your spread own part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
