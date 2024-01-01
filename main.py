072bca0f800c7fa8e9ca84a5e52727a6a5271d35
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:04:47 +0000

    earth repeat hardly further hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-print(math.pi)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
