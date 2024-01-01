1f369890c96d6fd14f30ef938bed3443006596c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:08 +0000

    possibly intelligent blue charge before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import datetime
+def get_today_date():
+        return datetime.date.today()
