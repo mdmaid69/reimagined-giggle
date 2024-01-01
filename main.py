e8e4d7e2b4f0a05db3801df2be91205e2eed771f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:03:11 +0000

    pot ought safety world uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_pressure(force, area):
-        return force / area
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def is_odd(n):
+        return n % 2 != 0
