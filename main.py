be20734ffed9e82901315a3f67064925ed1d8ee6
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:54 +0000

    fear know sale completely arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
