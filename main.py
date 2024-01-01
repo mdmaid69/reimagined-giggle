b2ea73c4374327ac414e95bc08ca61d7451687a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:07:13 +0000

    remove mess much shame often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
