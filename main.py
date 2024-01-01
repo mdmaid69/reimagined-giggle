fbee5d8104498bef94c5dbdcd51d7f0f626e1d06
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:31 +0000

    appearance breakup mess basic recognize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
