264d0e18479693e0b892ad88a92eb3dce239626d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:15 +0000

    kind trade instead among down

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
