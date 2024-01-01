34c51ef27d365151b16394e7070e1e4158bf1392
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:30 +0000

    game copy somewhere reject wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
