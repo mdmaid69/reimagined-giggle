bed30ac421c2c8c76c35975ca7c1ae167dfc7804
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:59:41 +0000

    offer mix worse home central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
