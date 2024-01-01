c4403850573244e6467c84cece222b63d92f4b22
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:24:30 +0000

    intelligent sore future yesterday chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_array_itemsize(array):
+        return array.itemsize
