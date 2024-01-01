4b23a52b979fe84d0b917fa39417951f7c6afed5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:37:56 +0000

    doubt cry calmly battery unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(5):
-        print(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
