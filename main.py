9172deec2176a92f8de22c2ca4dcca656a240b54
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:16:23 +0000

    concentrate there round more second

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import os
+print(os.getcwd())
