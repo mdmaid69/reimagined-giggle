6338c3a6b1d7e30a68b94e0bfac028bc328ed539
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:56:42 +0000

    again giveaway way well give

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def greet(name):
+        print(f"Hello, {name}!")
