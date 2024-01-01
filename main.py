1e73db9e3601f835881280bea987f183860b862a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:36 +0000

    to cross error turnup profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-print(math.pi)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
