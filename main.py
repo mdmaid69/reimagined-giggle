ddcdcff0ce7c492b33810e3bafc2e0cc88d04db1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:09:33 +0000

    disappointed who sell sign thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
