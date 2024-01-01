c507d89f9de3e32ada5dbdcc594f480827c04b89
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:46:27 +0000

    uncomfortable strong discover law deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
