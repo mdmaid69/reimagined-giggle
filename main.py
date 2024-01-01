b9db2e1005ef2282ead6f3ccafcc26fa6fd85be7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:06 +0000

    base present rule office giveout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
