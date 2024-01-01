0477127c43e327fcaef222e055fb6e23da1770c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:50 +0000

    profit cost 566end smooth phone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
