e94eedf8c864ff20df1cd5824bbcf611a9e1dcf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:50:00 +0000

    pin excuse black refuse computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
