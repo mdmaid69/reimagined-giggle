bdca760f43c8c4e672dda938dacee66ff26eb599
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:55 +0000

    world defeat switch modern day

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-import collections
-def create_user_string():
-        return collections.UserString()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
