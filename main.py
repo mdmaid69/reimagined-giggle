11d3b52f6420129f5d86beeaf230edb8200da21b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:14:56 +0000

    pleasant to unhappy identify contact

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
