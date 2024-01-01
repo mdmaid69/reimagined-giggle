67ebfc0a569658a55ed834670d59350c3ec016ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:46 +0000

    perform notice scrap run neat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def add_numbers(a, b):
+        return a + b
