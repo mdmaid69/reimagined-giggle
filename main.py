0aa8c65aa16792c5936e8878a91d4d7d89dbc54e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:41:15 +0000

    book complain order listen attractive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def find_common_elements(list1, list2):
         return set(list1) & set(list2)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_power(work, time):
+        return work / time
