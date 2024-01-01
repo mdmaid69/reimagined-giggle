d9c7fa4eb59d10c044d032779cc2a6a2b82cf41a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:17:40 +0000

    smell behind surface distant steam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def subtract_numbers(x, y):
+        return x - y
