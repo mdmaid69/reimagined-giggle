69c06b0c34fd55e8bfb19fe589fb232b1331a2b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:26 +0000

    base month lunch out shoulder

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
