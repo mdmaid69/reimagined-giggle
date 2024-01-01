fcce8b7e911a84183f21de51c236932a72a10e90
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:01 +0000

    wash fortunately worth demonstrate promise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-  def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
