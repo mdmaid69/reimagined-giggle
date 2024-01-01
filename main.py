00949d94d855fafa747794ad818caff3769ed685
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:51 +0000

    laugh group scared separate reason

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
   import os
-  def split_path(path):
-        return os.path.split(path)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
