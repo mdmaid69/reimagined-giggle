04b37d641513c1b70970b3b5d60d20506a2c5d34
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:18:39 +0000

    reaction new rough job just

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
