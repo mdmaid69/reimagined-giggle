6146ac553361abdb56aeb422b7831af4e2579c06
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:30 +0000

    regional recognize lunch deliver punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
