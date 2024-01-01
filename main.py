9acbc11216ae514315709d6a16cc769581d49f20
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:35 +0000

    fortunate balance tall earth regularly

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
