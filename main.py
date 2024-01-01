daf3ae792a0f98f4a4c3ee529576894b51ebc823
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:32:45 +0000

    make correct somehow forget fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
