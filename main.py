ad7994934feaea453f3d105c654efd2520aff3a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:29:40 +0000

    apparently occur keepfrom both praise

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def calculate_average(lst):
+        return sum(lst) / len(lst)
