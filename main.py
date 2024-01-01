bde6978cebd98ca0a7651ab3a71f4aa742e4f7ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:41:36 +0000

    slight float earn provide middle

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
