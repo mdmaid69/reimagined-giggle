9df8620fe93f69fac800bc5cac42212120366891
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:18 +0000

    slide experiment hold whenever brother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
