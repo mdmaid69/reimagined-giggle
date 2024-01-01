74ffb612309b26673fc41b24340928adc644de04
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:06:35 +0000

    pride wide coat contrast proof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
