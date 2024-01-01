09cd48c5a1fe60831d28ab701488a1ad64f90325
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:33:43 +0000

    plate recommend basis fine use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
