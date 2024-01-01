727f1e106dd19a05a324ef7efbc15fe15fe3d70d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:45:51 +0000

    grand touch especially report ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-def calculate_power(work, time):
-        return work / time
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
