fe7b1d082695b720a055d123dfbe0e0e4dd40425
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:27 +0000

    bad dress less occasional typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def count_elements(lst):
+        return len(lst)
