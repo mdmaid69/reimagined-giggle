1f9119249fc7b9209a1cae8e10d3b23b911495de
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:25 +0000

    ought by train comfortable answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
