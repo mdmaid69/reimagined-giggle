9a92bcc4237f1902dbfdf5f6f2a9a041dda5c9bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:23 +0000

    club basic scared level draw

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
