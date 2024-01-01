8c0d3713d0f4d400105259c553a88c09fd9ef448
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:07:12 +0000

    someone he note building keepoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
