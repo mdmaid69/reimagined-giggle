c5a3537db2e2c3fb87884ca99cb921b557f7a120
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:39 +0000

    avoid fail rush dream pattern

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def find_unique_words(sentence):
+        return set(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
