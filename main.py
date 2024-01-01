62900d334f72870386c7d679df544100a88fa488
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:05:10 +0000

    appeal window spare effect hungry

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def convert_to_binary(n):
+        return bin(n)
