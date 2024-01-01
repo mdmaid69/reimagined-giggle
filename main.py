bb6e166ef0a103b99a1fc160b77e86b5078ea2bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:16:53 +0000

    cool hit female start little

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
