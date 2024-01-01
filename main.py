335636d18e822db5649be94c87a7afdbd16f3bac
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:59:49 +0000

    idea otherwise shut organize birth

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
