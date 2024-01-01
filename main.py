6946df0008aef4f5b6b218a743888e9f2225ec08
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:05 +0000

    concern least touch stupid order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
