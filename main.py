e4a2cbebe0aed1fad7d91af2d72ce2771b1f5c9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:07 +0000

    warning store exhibition past prize

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
