ff26d5ef31fed1b7149e2a6930feefc20e6df6d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:19:31 +0000

    poor turn finish past swimming

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
