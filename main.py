1aaf21ae8f37f7aee7272ebff9f8eda623a7ec28
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:39 +0000

    brain healthy suppose visit whom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
