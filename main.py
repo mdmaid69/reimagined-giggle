b7d7da5937d0fdc0d31998b21bf5aa16fa061807
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:19:16 +0000

    common care noisy slide belong

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_power(work, time):
+        return work / time
+import re
+print(re.match("h.*o", "hello world"))
