466aeb31603a833e2b87083afc6fadf3cbda8add
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:53 +0000

    seat original train give shine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
+def add_numbers(a, b):
+        return a + b
