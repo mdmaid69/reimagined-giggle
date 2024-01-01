0eb0f969f6ba09cbe31e2739c47a38ea8a46403b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:19 +0000

    repeat especially patience impression steam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
