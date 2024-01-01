8c0fc8cd050805f1df4b0a7aed068048e9881761
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:40:19 +0000

    famous butter base advanced work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
