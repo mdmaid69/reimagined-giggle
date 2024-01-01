d46481040c8cec4963425ad0b05c3f33acb9db62
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:09 +0000

    issue quarter whoever Cc plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
