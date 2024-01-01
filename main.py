4426b0258d5869e1b605ac54eb78e980915cc86e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:29:45 +0000

    appeal story die full exit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
