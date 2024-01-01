84f2bc09c386d78926acceaf1c1d395c90b1e205
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:12 +0000

    geton communicate label before priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_work(force, distance):
+        return force * distance
