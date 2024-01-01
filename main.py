84eba513c04e49b22f270d0dd4df11ee528cc991
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:29:51 +0000

    shrink level forgive lot great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
