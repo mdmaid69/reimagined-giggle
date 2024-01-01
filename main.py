9d1258b0cd3899609a2383248a88f3949a485774
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:31:24 +0000

    once thought still sing from

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
