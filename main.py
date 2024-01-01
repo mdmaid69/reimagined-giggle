694b44dc17825e597633bfbbb1ba8c4062790d91
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:12 +0000

    march plan Oo can arrangement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def multiply_numbers(x, y):
+        return x * y
