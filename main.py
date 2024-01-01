093b5c96bed8c16bf3dbf388d2cdc9365b584d13
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:01:21 +0000

    tight avoid stare divide character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
