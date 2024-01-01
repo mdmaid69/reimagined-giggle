bca8a6ae9421791ef7230298ffb8dd601330540e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:34 +0000

    offer treatment breakup check anybody

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
