78635b57080320535d3c2082efe5fb8e1c42b44d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:53 +0000

    mistake stamp police order prove

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
