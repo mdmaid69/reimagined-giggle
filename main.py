b51f4252554d124a8c830c2a3996d34d73da2046
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:43 +0000

    mystery must scene concerning rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def convert_to_octal(n):
-        return oct(n)
+def sort_list(lst):
+        return sorted(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
