44b2dc0b734bff95df849d56f8fb5cfb0a76e21d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:18 +0000

    love rest thirsty willing worst

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
