f6e6c53ff31a32228ddb24c0d56443f9d63456d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:58:42 +0000

    shout trouble involve arm all

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import os
+print(os.getcwd())
