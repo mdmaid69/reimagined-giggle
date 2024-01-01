b354464f43f3a5798314a39fcc1950f73fd4185a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:08:47 +0000

    represent weigh father hungry measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def add_numbers(x, y):
+        return x + y
