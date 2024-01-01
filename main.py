201712ffb41f94462156e8c92b47a4d4c2fa89a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:02 +0000

    stiff fire exercise blame love

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def divide_numbers(x, y):
-        return x / y
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
