821fd2a559382ab8333683d6593c921ed0522230
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:33:08 +0000

    scared justice reject trade remove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
