2b88867f1796b400562e15fe560f33ab6a73dd3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:33 +0000

    hold important safe description description

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_numbers(numbers):
+        return sorted(numbers)
