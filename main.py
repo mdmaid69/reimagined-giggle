3c89da6d7f6917244458ceb1e6515aa31b6e1b5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:33 +0000

    Qq team lookback(on) disgusting shoulder

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def is_odd(n):
+        return n % 2 != 0
