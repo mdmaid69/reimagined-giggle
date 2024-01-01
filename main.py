a83b477969e8148ef83378713bdf73279f85b143
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:44 +0000

    pronunciation strange pencil remain chin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
