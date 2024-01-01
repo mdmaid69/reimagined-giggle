73b3bfdc9b32a3cf513e6024dfc805490c5532c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:03 +0000

    disgusting tell cut thin deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
