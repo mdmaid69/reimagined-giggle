9c5b58affd29aae2b5863315f381225080788477
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:11:42 +0000

    share contrast this brain oil

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
