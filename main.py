4aae36902d3b7c7384ca68c08c523f13bae50ce3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:24:15 +0000

    reach admit turnback report condition

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-def is_even(n):
-        return n % 2 == 0
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
