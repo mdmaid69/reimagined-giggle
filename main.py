bef8c6b55d4fddc19626f3e966ad5c8a30e11829
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:48 +0000

    new weak window set pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
