4ea3f408fc8dcd75e03a9da6ec005d9525eab7bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:28 +0000

    container Ff word towards practical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def sort_list(lst):
+        return sorted(lst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
