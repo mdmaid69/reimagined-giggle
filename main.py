bd44d7a4b1a1ed7a0e75e493cf38e3053cea2428
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:05:45 +0000

    alone line cinema safe moral

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
