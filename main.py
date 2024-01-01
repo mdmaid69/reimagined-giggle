820898462165dd4ec60a812ee24f89a74f0bef20
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:46:08 +0000

    idea invite experiment principle arrive

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
