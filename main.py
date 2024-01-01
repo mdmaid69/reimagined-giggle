8f7d1d98aadd3b6904a58d0e7e60adea267fb465
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:13 +0000

    radio teach rain wind hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
