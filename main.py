623148977588a7be93376ff91cf1812c2b3712aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:49 +0000

    safety them throughout judge sore

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+print(time.time())
