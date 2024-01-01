56ab416dd3fd217c53187282f928555efb5bff76
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:19 +0000

    false reply rough pick story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_str(array):
+        return str(array)
