e2e3c7b27a14df33760a93c36ceb2d6ed3b24a53
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:37 +0000

    lonely attractive justice lend deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
