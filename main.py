75487e394c96504d164947c56e46737fc040d992
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:02 +0000

    569enjoy thinkover team accidental loose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
