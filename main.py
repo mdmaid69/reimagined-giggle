6a982a1f036dea4502e14ebd97f5a67000e243fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:39 +0000

    detail regard now equal somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Words:", len(text.split()))
