40a8db6cae2f96407ae78009c41c0b0feecc3961
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:12:02 +0000

    speed abroad quickly out shopping

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
