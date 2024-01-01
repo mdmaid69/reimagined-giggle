a796fd6d81bd4d5fc318236bbf92658a5d4a6016
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:12:10 +0000

    excellent gift maintain modern several

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(lst):
-        return min(lst)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
