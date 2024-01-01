1506396290f0c576628c65d17abe98ec0512034b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:51:30 +0000

    interesting crowd argue eager bringforward

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
