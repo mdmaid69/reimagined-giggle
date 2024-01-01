ad92572b319b2dd8c0eacb8bc8b624bec2616af4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:48 +0000

    married juice reasonable I entrance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def find_max(numbers):
-        return max(numbers)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
