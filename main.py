a442a7ecf2f47e9164d97e66ee9e9ae4b647b3f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:39 +0000

    afternoon break seat blame image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
