7cfc8a171d49e5ca7027dd52a79538ee5a5568e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:20 +0000

    activity prefer tragedy bill make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
