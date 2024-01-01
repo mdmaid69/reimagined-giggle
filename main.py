b0be12b7303b8af47181d4e8cc472dbe2c8f3615
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:05 +0000

    follow meal scene coat eye

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
