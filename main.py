8085763f5a722eb26efb12868c87a196ff43cd02
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:46:28 +0000

    behaviour defeat allow full story

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_slice(array, i, j):
+        return array[i:j]
+import array
+def get_array_as_set(array):
+        return set(array)
