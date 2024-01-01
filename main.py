e1d1d5edcd3fab9bf7d7b4ffd48e853e40c9e97b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:21 +0000

    debt keepaway handle address indeed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
