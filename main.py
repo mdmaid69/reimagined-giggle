f2f38702dbb864badfe034f6caf3764551d3d816
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:31 +0000

    say cry breakout disapprove mixture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
