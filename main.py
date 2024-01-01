d4809be4625a41fbd4a515016739e36ed2aa71eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:18:16 +0000

    pause reduce dream inside subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def subtract_numbers(x, y):
+        return x - y
+  import sys
+  def get_python_version():
+        return sys.version
