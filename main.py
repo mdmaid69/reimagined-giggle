642c34869812d32b8070ec4639ea26f6a5f2955b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:37 +0000

    bottom quality Cc scared label

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
