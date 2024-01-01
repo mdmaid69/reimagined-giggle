5be20c2b3beb07bdacc7fdc35d582a078433cece
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:28:22 +0000

    rather sleep pure listen write

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
