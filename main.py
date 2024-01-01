641666f87cf6fda3f38dd5abb5c0f76580d86d14
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:05 +0000

    concern test sweep develop bringup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
