f1086ba78e96f8472a75bcc07300d5ec878357c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:58:27 +0000

    country cut organization voice possession

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import tensorflow as tf
+print(tf.__version__)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
