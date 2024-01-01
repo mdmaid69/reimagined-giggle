5e56c4c28d3ee0b9239c0b7d149661385274076e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:37 +0000

    steer model care thought hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
