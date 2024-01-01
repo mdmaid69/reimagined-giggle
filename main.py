7f4e912eb323f186c8b5c54ae01d83bc1eb43830
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:19:32 +0000

    paper experiment Ff lookback(on) method

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def extend_array(array, iterable):
+        array.extend(iterable)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
