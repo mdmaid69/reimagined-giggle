90c9899789c27af9bbece08afad9325a2bf596c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:37 +0000

    prepare Rr top teach cool

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def is_odd(n):
+        return n % 2 != 0
