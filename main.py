271d0a24e4ad933a5d7d06aa7dffdbbc84db44cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:54 +0000

    whistle him argument trade below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
