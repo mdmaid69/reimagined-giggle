f060caed9e00d951ad6e57981b7274116272f69e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:59:12 +0000

    pay board turnover breakup Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
