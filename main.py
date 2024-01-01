1920e916c811be4ea184e5e00ed3f8b1ef69c5e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:33:50 +0000

    production standard keepout platform building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_set(array):
+        return set(array)
