bc52307505d5f771f5f5c52558b2ca7200c8eb6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:47 +0000

    follow reserve and Ss use

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
