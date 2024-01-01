cab5c32a894aa00f3fde04f49ab010eff3d2a7a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:09 +0000

    picture mean support unkind language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
