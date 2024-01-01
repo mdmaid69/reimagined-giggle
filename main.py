d2088390b5afd2261624b165bd40e2d16b31cbb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:47 +0000

    remove while fit familiar uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import platform
+def get_python_version():
+        return platform.python_version()
