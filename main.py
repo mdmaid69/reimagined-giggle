9843547a8b8b3d0e71aa6f8f425eb1ea1d7f69ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:42 +0000

    measure ought public exist past

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
 import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
