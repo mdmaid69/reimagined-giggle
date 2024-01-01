70c1b04ae6d278a1240f367b26bbc71fedb58fdb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:56 +0000

    turnaway step Rr almost eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_user_list():
+        return collections.UserList()
