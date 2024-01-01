d9aea94a6bf879e8c01311464c4926a7db9852f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:44 +0000

    collection authority this confident act

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
