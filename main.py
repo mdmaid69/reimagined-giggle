fd50f6c7b67709addd5c4e9b82c8f19ea990049a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:59:22 +0000

    load come that exactly day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
