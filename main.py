ca5bf535bc5eb25c8df8ca7d5a7c5035eacc9cbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:38:57 +0000

    patient dare last crime slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
