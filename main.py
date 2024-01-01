6fffd895c937e72d2f6a312bd4d0da1d340737f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:34 +0000

    persuade never money accidentally name

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
