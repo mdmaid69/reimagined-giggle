27db950a63b2859985ac7f428e0a19afc18b0c6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:49:40 +0000

    march yourself good turninto unfair

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
