82dac49ce3f7793ec6179ebbaffab9d8af5892b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:50:35 +0000

    choose anxious talk murder crash

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
