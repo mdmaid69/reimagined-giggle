41611986d6c48be5216795cfe89c3b5fa869187c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:13 +0000

    change cap slight again from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def find_max(numbers):
+        return max(numbers)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
