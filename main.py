d576fca185f335fa69dddb48adcfe1fec732fa46
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:30 +0000

    cause state creature formal keepout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
