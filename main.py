ebab35b0eaa3c4c4033b93501003ea8b691845fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:35:55 +0000

    dinner power unhappy gap lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def count_elements(lst):
+        return len(lst)
