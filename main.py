a3e06411609dcc4f53a072123741d6e33eff4b4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:05:31 +0000

    question getout first trousers information

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
