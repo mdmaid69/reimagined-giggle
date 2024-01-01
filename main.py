f603fd9166185e6fa9810ce71b51c7b982261307
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:30:07 +0000

    brown home connect search company

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
