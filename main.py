be321505af5a34a869fb7f4aa4d42583655e7c71
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:16 +0000

    flat rid container manage careful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
