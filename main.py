c8fbd778375a4a7a12bb4ca958845e3283289e6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:23 +0000

    lookafter tidy cap miss support

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import os
+print(os.getcwd())
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
