652e49b183308ca578f1e0264c9bf3217ef3c05d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:19:08 +0000

    disappear conclusion load demonstrate wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
