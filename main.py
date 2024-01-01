caba09e2e5e3aed6f5aa1b226388053c2485931a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:14 +0000

    performance stamp cost steady tell

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
