0a27867b752403ce82343c9b7a869e845ffe6efc
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:27 +0000

    odd state joke yesterday up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_tangent(x):
+        return math.tan(x)
