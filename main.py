0aae9a858cdf8519222c4f053ab9d8e3e951bafb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:38:38 +0000

    peaceful breakout check talk keepout

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_min(numbers):
+        return min(numbers)
