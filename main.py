68831783991785c91b760025e74965682e2c859b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:14 +0000

    burn neither fashion children but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
