5c7dd495b25ac85eea4cbd9d76e92616a84e4a06
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:05 +0000

    stamp exploration breakin aim makeinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def sort_list(lst):
-        return sorted(lst)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
