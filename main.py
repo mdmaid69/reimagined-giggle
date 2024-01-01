35ff2c448bcb5982ab3f396aa97bf18afd9c28bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:25:12 +0000

    aim tool slice section hurry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
