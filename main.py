45a91d5563388a59fd58b0f659e207f6abcec988
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:06 +0000

    ordinary mean badly condition turnoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def is_palindrome(s):
-        return s == s[::-1]
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
