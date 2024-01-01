8f6ec831cf1ff5362e51e778051c92a80e2ea45a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:32 +0000

    comment tell important otherwise drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
