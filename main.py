e41f376b789fac0575af3140d1a20ac44c7f406a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:30 +0000

    worth chat prepare bend especially

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
