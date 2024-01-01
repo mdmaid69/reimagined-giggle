484df51296a4d55dd39d9c2ed77cac4592c24f70
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:01:31 +0000

    lay influence kind eye lookafter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
