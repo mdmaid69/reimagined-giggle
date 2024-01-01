92f4fbeb1d21c7dd37c8e2a8daa4edf0967b1bdd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:13:25 +0000

    value or lonely best lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
