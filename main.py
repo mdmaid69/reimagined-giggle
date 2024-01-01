6f4eb6710409cc09b76aa46cf730ec3bf3f4559b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:40 +0000

    free suit combine lead throughout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def convert_to_binary(n):
+        return bin(n)
