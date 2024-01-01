827c89cee8bc6a7f20d8b89826e7e03402803931
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:39 +0000

    before slice body attention handle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def find_min(lst):
+        return min(lst)
+def find_max(numbers):
+        return max(numbers)
