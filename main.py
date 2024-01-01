3dfe7a05dd439a0ffe919669afed62bf5d578ebf
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:53:15 +0000

    concerned tool talkback should survive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
