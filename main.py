41dd3ff412ff0641f8667a7c20592bb1d2ba8ea0
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:02 +0000

    tonight constantly have pipe mainly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
