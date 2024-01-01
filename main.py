67a7f4c1b9f07bc51627858a9432d036759e18a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:18 +0000

    leave protection television opposite petrol

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import os
-print(os.getcwd())
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
