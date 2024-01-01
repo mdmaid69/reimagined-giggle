53f2033c905ec5e05924ff936aea8001df970f92
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:50:52 +0000

    account takeback afford mark their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_repr(array):
+        return repr(array)
