1346010637d0cd2baa8bff269d70f1141a15d460
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:39 +0000

    can goout strict yesterday suggestion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
