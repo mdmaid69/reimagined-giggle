643a39698eebdd8d2b7d9977195c4384bbb27cca
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:29:37 +0000

    able giveback kill combination cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import array
+def get_array_as_repr(array):
+        return repr(array)
