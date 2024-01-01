324587aa49e7df682ef03906b24de22e40d8a4e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:25:42 +0000

    born nose murder perfect and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
 def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+        return math.factorial(n)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
