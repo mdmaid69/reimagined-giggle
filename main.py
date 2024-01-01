d2b00417e77c2eda1abeab2d77f2448e3e1f2f3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:23 +0000

    birthday split difficulty disturbing work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
