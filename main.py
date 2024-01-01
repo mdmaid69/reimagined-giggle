ddeb0779be8a20bbbaa929f64b75c61a66eff3b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:17 +0000

    runout total want precise its

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
