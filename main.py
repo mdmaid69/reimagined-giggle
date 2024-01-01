b395a0f4ba993a098bed168889cece39795df336
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:13 +0000

    runover male upstairs similar Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
