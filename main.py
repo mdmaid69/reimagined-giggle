e24ee439c517d685184bad821cc2bf90cf2e4921
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:12 +0000

    hear brain criticize copy original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_frozenset(array):
+        return frozenset(array)
