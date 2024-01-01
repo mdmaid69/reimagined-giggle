38e8efa70b7a53771745b7e10e60539264920eb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:02 +0000

    foot influence possibility blow good

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
