467d74bcb7cdd379f80e902517c8faf82d260110
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:11 +0000

    surprise Itself best opinion all

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def reverse_string(s):
-        return s[::-1]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
