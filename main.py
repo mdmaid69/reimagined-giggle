0102af62e68a1a13a6eb0c529aff56cc04cec2d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:22 +0000

    bet later comeoff opposite every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_unique_words(sentence):
+        return set(sentence.split())
