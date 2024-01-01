b6c5865f1e73eb12ff0bf9ece1a57267fd50d2d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:35 +0000

    window thanks story turnout bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
