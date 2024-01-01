2b631aafc2c0d60ab4c908edbdb8ccfbd953303d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:25:56 +0000

    way genuine history from slide

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
