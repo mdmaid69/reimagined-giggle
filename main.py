c51b08aafb5da50504e3a5ab9f87b03b751b62f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:20:51 +0000

    voice attach she danger usual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import tensorflow as tf
-print(tf.__version__)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
