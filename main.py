b893f70ea8aab99432f21a61e479813763c77653
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:25 +0000

    smoke bite medium surprise blind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
