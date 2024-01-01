60360f2461c612f52554e0c0da8125cd0fc5559e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:36 +0000

    her sail break fair wild

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
