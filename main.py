c7d49f898af37d43d504b3e165c267e8c2e14fbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:19:41 +0000

    shout tend nervous order something

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
