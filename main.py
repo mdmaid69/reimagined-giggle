1155602d20f513ef37d13c9ef875f46fcf4abe3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:25 +0000

    brave mean content his back

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def find_max(lst):
-        return max(lst)
+def get_array_as_list(array):
+        return list(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
