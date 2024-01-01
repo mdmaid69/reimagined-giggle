78147bdb805297587d5e068e2760bdf325ac7ffc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:52:52 +0000

    strong their shape early suck

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
