d7daaf461c981e3b9fb81202dd47ade211625e00
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:29 +0000

    information off sight university bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
