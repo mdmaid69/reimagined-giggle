35398ea6790caa9bc523777620d8c33b6aaff76d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:22 +0000

    for 564encourage cap affect feel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
