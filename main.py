5e2da9d222e53b6438b05b52ffe4f48959e100e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:31 +0000

    wrong suckme acceptable off turnback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
