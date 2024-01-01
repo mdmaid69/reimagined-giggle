84b8e90d59d5148fd4f42dff53ea9500697d1e66
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:29 +0000

    measure release owe damp height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
