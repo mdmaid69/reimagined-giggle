733224a14b047e68243d91a5772ca710744f1bd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:03:05 +0000

    centre female thank conduct second

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
