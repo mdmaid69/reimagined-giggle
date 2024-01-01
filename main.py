dde7707d6184e457dd3d32c3aeb521cf08ec4a4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:13:05 +0000

    drive search storm mass sad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
