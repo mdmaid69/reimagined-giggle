9781fb9a7870b958532337de542000995ed5e8da
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:55 +0000

    bad goaway however past movement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
