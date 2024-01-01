028930a04d65f82efa72b565e2032845699aeb45
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:16 +0000

    yours within secret impossible rise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_repr(array):
+        return repr(array)
