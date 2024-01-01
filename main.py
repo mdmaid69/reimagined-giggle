e226fbf35eac56a3d740bdc136584ac0e6d49a58
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:47 +0000

    cost close pinch regret insist

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
