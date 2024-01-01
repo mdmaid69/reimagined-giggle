9b92c87bfee28a10798cfaeb2674c6b54fdefb86
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:11:49 +0000

    doubt heart trap order comfort

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
