7c05cfb3ca9afc669ed4a25045ee211a49ff3a37
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:32:05 +0000

    noone disgusting everybody mistake fetch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
