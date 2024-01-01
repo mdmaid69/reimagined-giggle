970b4c33599e2411c660e3a491ed878174626bb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:02 +0000

    noisy confident manner Dd wet

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
