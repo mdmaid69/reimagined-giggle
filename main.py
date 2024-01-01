1fe461a1a5606f1f89395008a651f1e50e73972b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:03 +0000

    runout breath language environment equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
