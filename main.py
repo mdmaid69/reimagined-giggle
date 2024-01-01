373109fde77e811626a2a862ad90c07403336d6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:07 +0000

    chief blue affect better tonight

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def convert_to_hex(n):
+        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
