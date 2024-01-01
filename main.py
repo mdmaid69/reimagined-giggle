5e5f8b43b39b483ddf04bcda4122d565c541de32
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:06:20 +0000

    lean material minor impressed worst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
