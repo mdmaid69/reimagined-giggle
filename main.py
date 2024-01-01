aab543dade95b7cdf7b92d91befc947bde91c9ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:04:36 +0000

    basic generally spin boil sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
