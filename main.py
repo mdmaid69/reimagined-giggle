fbdf75d87fa5b9b84fd854e676913206555f7fdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:46:04 +0000

    instead benefit principle dull honest

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_dict():
+        return collections.UserDict()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
