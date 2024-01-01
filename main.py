b31587956f2ed60550d3db82e33381b57562cfe7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:11 +0000

    create sad skin valuable teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
