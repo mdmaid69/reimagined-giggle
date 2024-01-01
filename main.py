1cb33fcd306fd72d9e2a82fd9518595956aca4ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:08:24 +0000

    fasten offer cheek reasonable second

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
