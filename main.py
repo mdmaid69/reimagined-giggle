16c10baca45ae1d8b46302b883743a5defd2091c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:43:04 +0000

    connected waste butter OKorokay why

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
