1619a22fff8abc591c971ead7fb4337924317647
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:54:58 +0000

    finish tone complicated sit language

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
