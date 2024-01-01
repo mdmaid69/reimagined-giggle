07e0d86d72ab26d438a3db7933cfa5c7aa6affca
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:36 +0000

    finish mother unpleasant tonight detail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
