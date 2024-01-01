5aaf65f1b0531d318ad32e775d7c17a97e59c49f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:00 +0000

    hello cross takeout unpleasant slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
