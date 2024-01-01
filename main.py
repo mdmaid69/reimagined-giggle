317d3787298d4de1f82bc8186edcb49e537bb5a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:54:49 +0000

    close judge tool home understand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
