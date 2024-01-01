6c79c53910a341b7c5b29c8625200bcf7a558049
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:49:32 +0000

    along pair yet now narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
