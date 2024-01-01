c2bbf390093934ac54637445d88de39c5314f04c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:49 +0000

    bend boat keepout point rub

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
