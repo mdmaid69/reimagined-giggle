8946c0a66c408b92bc2fbcb1092c80a15435af2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:27 +0000

    belief comeout importance breakfast quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
 import collections
-def create_priority_queue():
-        return collections.deque()
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
