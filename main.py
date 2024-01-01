00d3923e7b9b6e5680d436897455f4cea098d7e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:30 +0000

    fast my complicated keepfrom left

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
