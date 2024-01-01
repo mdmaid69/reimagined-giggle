791d7597980f4c31d757e1176c60f94d1e48bb24
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:48 +0000

    regret relief increase ground mixture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
