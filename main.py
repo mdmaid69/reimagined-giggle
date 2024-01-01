3d29209000849ac87e8980f521a3cd4dc239f1bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:34 +0000

    568energy relief mind worse party

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  def convert_to_octal(n):
-        return oct(n)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
