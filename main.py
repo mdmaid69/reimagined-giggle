e99b2b55e42f442e66e71b4a6800ae61e8165155
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:54 +0000

    light habit collect will spite

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
