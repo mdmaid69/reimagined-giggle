770764e6dea99964c812a5ec83fdcb3b6ca5cb9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:06 +0000

    sound clue himself serve shopping

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
