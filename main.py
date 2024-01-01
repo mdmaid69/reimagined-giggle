f24e7bf72a2bf30ca406969f5ed8bc39e51a7ca1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:12 +0000

    person clear weather sort thing

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
