0533974e7469bb4b871f4b63c1079181b7fa6bfe
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:51:08 +0000

    move ask service issue breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
