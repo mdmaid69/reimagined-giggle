be1a5a3ed4647a8643e83776d75acbcff1888378
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:10 +0000

    keep impressive loose prefer choose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def add_numbers(x, y):
-        return x + y
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
