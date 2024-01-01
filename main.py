6b3ae6a4e4b31034540652f9c72fcde6ccc615ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:04 +0000

    Hh frame live interested doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
