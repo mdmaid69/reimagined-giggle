56588842f33a41b578ef33512b533a9dbbf4f45a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:04 +0000

    speed organize standard feel position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
