9ec59bad3730899e74181b9dce182a987956b69e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:45:36 +0000

    direction Rr forgive brush language

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
