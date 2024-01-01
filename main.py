f33181b83344198302be3204db5d58df5436766e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:11 +0000

    relative share yet soon keepaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+def calculate_work(force, distance):
+        return force * distance
