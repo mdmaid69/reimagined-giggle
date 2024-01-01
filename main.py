e2b1bfb31ec4e9a3f39f12df4890830f1f3670db
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:45 +0000

    oil comeround strange here above

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
