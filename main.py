a7268b8d6ade529ac79991b2a0eede3b15af1202
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:32:17 +0000

    gentle their best awake effort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import time
+print(time.time())
+import tensorflow as tf
+print(tf.__version__)
