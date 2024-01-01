c1965d4403e847e44bf7e0a16931d9c827457474
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:46:57 +0000

    lost home also giveout true

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
