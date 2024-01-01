ff06086e538961d4be78f806d86ed77a997bec0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:01 +0000

    I relax opportunity 567enemy attract

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
