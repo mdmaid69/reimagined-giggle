5b1eda51f50adeb13df226c1e251385234a6019b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:03 +0000

    matter surprise real commit ahead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
