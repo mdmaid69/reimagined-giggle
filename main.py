a05b0246617fb2744f2b78050318630389ff34bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:51:48 +0000

    road spray offer survive raw

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def cube_number(x):
+        return x**3
