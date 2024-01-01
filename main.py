cde1f56dbdee1fc1e38f58082f53ca7dbd80d688
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:30:33 +0000

    know operation safe wave join

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
