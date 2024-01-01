cd036138e2e99b6c252b6129014eba406492d7da
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:03 +0000

    result sight obvious spread single

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
