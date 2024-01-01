c0fe476aaf497fccb1ebf5938bcb55b56bf7a900
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:31:10 +0000

    cope view recovery trick unkind

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def square_number(x):
+        return x**2
+print(sum(range(10)))
