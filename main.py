39de13ac74633ac5d428722c9f20cb65d8fb6d93
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:59 +0000

    direct free refuse old confusing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
