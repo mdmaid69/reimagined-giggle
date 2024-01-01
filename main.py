657fbcdeb7feda26cea5a9748a7e9b4d1de8b98c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:50:30 +0000

    guilty start precise murder cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
