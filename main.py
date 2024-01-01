51d4a7ed76c3320f9b3522f2152fabb204de96c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:06:40 +0000

    natural scale country light disgusting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
