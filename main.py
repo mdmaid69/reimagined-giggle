9102b02a899d55b60925f355378e67a46e7ab695
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:11 +0000

    kind after at error achieve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def multiply_numbers(x, y):
+        return x * y
