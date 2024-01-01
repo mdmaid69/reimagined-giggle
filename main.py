78a691fde8e13245248bb808ed46287ae34fb922
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:47:44 +0000

    breakup serve tour treat brown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
