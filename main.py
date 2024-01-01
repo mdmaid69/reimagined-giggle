1c42e19789d0c88072a7af52c1ac9ce9e897457e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:39 +0000

    someone listen Qq feed set

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import sys
+  def get_python_version():
+        return sys.version
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
