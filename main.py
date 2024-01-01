375b47ab5d81c86e1f2acf57700b5358561623b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:22:56 +0000

    by Ff typical belief nice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
