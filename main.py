55be3c17c7e7fffaf32afffad7c213bb0946c12d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:32:06 +0000

    education suck proper beautiful combination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
