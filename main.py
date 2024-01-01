6cd07b59940b374f347aea08bd8210095acd09fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:07:26 +0000

    mind frighten escape shape treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
