35bd56277eafacb147d07911bc400f5f1fef2322
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:27 +0000

    promise taste just sport thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def subtract_numbers(x, y):
+        return x - y
