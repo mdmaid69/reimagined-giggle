cec8c107910c5e51688909ea388a45c625ec1aa2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:44 +0000

    police run flat flow through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
