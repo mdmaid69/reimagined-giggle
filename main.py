e06c1e1e23f0df3b2f3d35c5f637d03ef2447d81
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:29 +0000

    nigga forever doctor treatment success

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
