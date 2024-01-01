c497469eda3bb26498ee361ff8ba12e0bfd308be
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:54 +0000

    few steep 564encourage disturb himself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
