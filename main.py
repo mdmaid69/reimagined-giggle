aa37c7b159dad5755a4dc5490818112c1b5fd7ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:46 +0000

    even fast once radio sink

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
