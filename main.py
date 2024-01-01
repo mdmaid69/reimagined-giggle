1021d1b9f95b8a5f78e292932846f8513a92305b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:57:33 +0000

    question variety just welcome trouble

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
