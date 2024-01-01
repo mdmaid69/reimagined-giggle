d49387173b4a8c8cefc07823c373b071a2f52ca7
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:17:24 +0000

    total advice exactly emphasize 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
