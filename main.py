32e9176888d601fccd41405ac586b203fdbe66f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:31 +0000

    frame bridge except who plenty

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+for i in range(10): print(i)
