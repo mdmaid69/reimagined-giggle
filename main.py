3d8d519efc5efdb265946ae4b4a1a76ebb0d340d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:28 +0000

    back giveback realize brother mess

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
