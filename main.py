7a15202f191b0afa065ad40fb1feef931e87717f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:55 +0000

    discover object Pp expression turnover

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
