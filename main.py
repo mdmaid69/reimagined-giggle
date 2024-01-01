74df50869d1a0e2030f3ff85aca02fe08f9ed754
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:48 +0000

    waste evidence twist thinkthrough fairly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+print(datetime.datetime.now())
