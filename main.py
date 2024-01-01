72ed8b235c5743a3a6497855bc84f1fb10f5ae8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:41 +0000

    ready thorough tone this high

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
