2192bf686dd259252fe0b2b2104812e089097f03
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:17 +0000

    us holdon still march blind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import random
+print(random.randint(0, 100))
