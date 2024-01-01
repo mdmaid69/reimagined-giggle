a2b3d0cd205543643fb062d05791342a19b7d882
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:56:32 +0000

    persuade match keepaway nonsense ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
