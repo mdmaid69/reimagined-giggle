0b2ed8514d8f720b62c75ee3a745a64950cecfb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:06 +0000

    go either minimum first dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
