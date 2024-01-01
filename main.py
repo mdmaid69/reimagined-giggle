9cc0fb476eee2169010f136cd9e43da5743cabd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:26:35 +0000

    really tear walk care comeoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
