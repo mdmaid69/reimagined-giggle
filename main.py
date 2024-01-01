d7655b84f190e7df741f65e2d13e1b99a41e08da
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:58 +0000

    speech follow Oo ability bet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
