c7b8beb56e11f9bc0ad9b309e96acb9d769e3ad6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:43:15 +0000

    doubt obvious slip breathe display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
