695fb05f4ee2eedb1b50ee317c6dfaae2510f7b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:30:16 +0000

    generally human maintain only trousers

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
