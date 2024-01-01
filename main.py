d11ea95de946bd2874e70506280837d364261b00
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:41 +0000

    point regularly partner group discuss

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
