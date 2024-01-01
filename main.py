2ad61901e134f7f5c70071b7c5c67eae255369c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:24:29 +0000

    open full 567enemy emergency interfere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
