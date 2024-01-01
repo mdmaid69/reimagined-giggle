013cf1eb7a1de7b56bdffcd0a16e2244d9c28b29
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:07 +0000

    offence original contrast wet Qq

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_area_circle(r):
+        return 3.14 * r**2
