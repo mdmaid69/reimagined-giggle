6015b544e052f6b287f8627479635ed3faa9e7cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:01 +0000

    order punishment perform before most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
