fd7c42343a918593b58e1018127734e0e1f13345
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:36 +0000

    patience embarrassing ordinary bringup plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
