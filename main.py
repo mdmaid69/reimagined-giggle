444aff5122358e8aa8b584e27dfc64c5df3653fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:03:32 +0000

    relative colour truth accidentally godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
