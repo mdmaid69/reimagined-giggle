7459fbdc5c775cdd070cb7b1d694a5192a24a596
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:44:49 +0000

    take method official attract too

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+print(re.match("h.*o", "hello world"))
