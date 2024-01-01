7da22a0968c3e76063f7033c507c354f2762fdc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:11:12 +0000

    dress defeat off hair smell

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
