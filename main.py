15b56b43c2d14e7c243ca61e5de4be69c060bfb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:42:49 +0000

    knock collect forget future street

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
