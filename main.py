faeca1e10b3f92d66e31cb72b44c2402814b8c96
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:26:56 +0000

    sink compare sleep Cc absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def convert_to_binary(n):
+        return bin(n)
