71992dbb5b8976d572251df56cb7ad9e3cdbfa1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:34:21 +0000

    nonsense after suit interruption rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
