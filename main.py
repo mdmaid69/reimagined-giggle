7d592014829a637d78b2b45820130e1f7145b7f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:16 +0000

    please cough everything bear brush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
+import datetime
+print(datetime.datetime.now())
