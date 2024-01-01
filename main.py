a840ef8cfcb2848df04096ea4759a2caec1d6d9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:38 +0000

    brain shopping circumstance please length

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
