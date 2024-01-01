db8effae43b5c241bd81ad3c9cea0161aeb115fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:31:18 +0000

    sky badly pinch fortunate meet

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_directory_name(path):
+        return os.path.dirname(path)
