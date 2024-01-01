3289728193d727ca9b04b559211814f8b2512b8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:51 +0000

    centimetre minute convince bang feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import getpass
-def get_username():
-        return getpass.getuser()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
