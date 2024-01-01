1a5d0db2b0b476254397415cd3cfcfef8c0d91e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:58 +0000

    point stand us image further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import sys
+def print_python_version():
+        return sys.version
