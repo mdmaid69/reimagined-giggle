aaaf6ed0acada92b152d35c63a8a72fe63b31565
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:55 +0000

    belief battery injury keepup neat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
