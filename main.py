639f51eece0fca7eb9c426a4e1666e8e54d5d85a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:53:40 +0000

    on certainly careless increase Dd

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
