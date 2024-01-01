5ebbd529556089e61b17410ba134bc56362440eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:20:01 +0000

    form find bet safe he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        return sys.version
+def multiply_numbers(x, y):
+        return x * y
