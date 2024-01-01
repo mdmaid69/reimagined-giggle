e53d65eebb5ede0743a3e12be428503849fa3841
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:15 +0000

    know manage suckme Pp himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
