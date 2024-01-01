fba5cdd4a7ab5b15a868738b46af1ca6ab216e8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:36:07 +0000

    black arrange right natural match

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_words(sentence):
+        return len(sentence.split())
