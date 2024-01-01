60a427d8c385c3636c0a5ebf62b42af50070ce94
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:56 +0000

    return belong formal measure claim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
