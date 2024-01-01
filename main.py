e21e7f64da9a708c2e45db200a0622a1867aec0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:59 +0000

    comment post subject once look

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import sys
+def exit_program():
+        sys.exit()
