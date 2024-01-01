918e093b659983396b4520bbffe92b9f6e5e2eb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:58 +0000

    cut chat tall opposite describe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
