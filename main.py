fc0b79fb35533306fb9b87b29b6b1214b6e68d2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:45:42 +0000

    chat stamp clear noisy correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
