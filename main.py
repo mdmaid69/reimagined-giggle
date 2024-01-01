34a814af32ecf532bf5984adfa2fd8d449c107cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:45:40 +0000

    record workupto press trade bother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def split_path(path):
+        return os.path.split(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
