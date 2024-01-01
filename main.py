82e4a0417d2953738697185216e4a8ebf9abd777
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:06:13 +0000

    operation roof squeeze affect size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_current_directory():
         return os.getcwd()
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
