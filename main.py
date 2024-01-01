118154d48c319cdfdce848b76aec617fca0d2e85
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:49:10 +0000

    statement 572enter change sore war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
