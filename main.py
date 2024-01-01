f959b8fe2241015b0069c680181e6e6277a920d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:06:39 +0000

    print trouble gentle attractive declare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
