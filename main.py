e1d6230f554f91b641ffba20fae6a601f051dca0
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:32 +0000

    their organization recover prepare person

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_area_circle(r):
+        return 3.14 * r**2
