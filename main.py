1eece66f126528fe57b52b4f0f689b28790d3bac
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:51:12 +0000

    fear play holdon minority rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
