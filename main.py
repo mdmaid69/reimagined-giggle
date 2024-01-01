bb3ded4bb3794579243800145803fd168a5a6517
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:58 +0000

    oil forgive terrible behind subject

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
