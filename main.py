69f16ea358e673034fd5173fb89edb64e67aec18
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:17:53 +0000

    half convincing cheque favour introduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
