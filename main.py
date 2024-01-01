66963a8b26c2333151a88e39f256ed99e8a31755
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:34:18 +0000

    aim please herself set increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
