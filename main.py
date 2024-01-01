ef259fe92deff7a517265591c42333ec3966dae5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:49:46 +0000

    trousers anywayanyhow bother cover smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
