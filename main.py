2dba53ceea01fbb489682cafb501c8ccaff02abd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:31 +0000

    approval doctor scream book onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
