1eb35ff724358d40d876a3171c3cd731a8db52c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:56 +0000

    black from breath cry prevent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
