fb29d6357afae7a298852d1e2e119abac964c0fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:02 +0000

    dear full pity address experiment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def append_to_array(array, item):
+        array.append(item)
