aa743320850aaec0616628e92adaa752faa265db
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:04 +0000

    thinkthrough police direct base load

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def square_number(x):
-        return x**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+print(sum(range(10)))
