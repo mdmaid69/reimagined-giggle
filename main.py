5056a024d52183c92a37f85b499776507294adb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:43:36 +0000

    herself port feel apologizeapologise tune

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
