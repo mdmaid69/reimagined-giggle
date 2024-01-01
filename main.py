71915a2bcaf53be0b9c6169668a6fe0fe114c97f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:14:56 +0000

    slip delight nature creature dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
