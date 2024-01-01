371a2d8420458d8ab9fe01f97013d8382b9bb8c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:26:45 +0000

    scrap calm appearance beyond reserve

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_acceleration(speed, time):
+        return speed / time
