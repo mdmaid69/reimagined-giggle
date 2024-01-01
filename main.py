01705784feafde6c91f5f32397229c0d2963daf9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:40 +0000

    train bored guess brief time

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
