fc0e3d95dfba1a0ae04e89e11b543104f3b313d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:26 +0000

    thing certainly wave otherwise bored

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
