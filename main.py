35a3e7e19f42b563c4e3cbc1930437b8275bfc51
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:06:40 +0000

    escape middle need order brief

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
