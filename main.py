5b250d75b63d21dd6b6943728c96cf7f16c8c6d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:04 +0000

    again without people trust firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def print_python_version():
+        print(sys.version)
