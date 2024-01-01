940698e0ecadffcdfaa05a8ffddf52f91afb8ccc
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:13 +0000

    disturbing beautiful treat paper debt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
