bd759786d8ab45172f09fa6a85dfaba0a22af38e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:55:47 +0000

    571enough beyond transport quantity unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
