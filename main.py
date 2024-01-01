effb9b3446b48878194e4dd11cb0025c2731fffd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:32 +0000

    task zero pass message ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
