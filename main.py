1596162dd9beea0628318f5a62c17d8f83885561
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:13:26 +0000

    imagination whisper fair record quality

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def divide_numbers(x, y):
+        return x / y
