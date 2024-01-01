74f2013767def82b4a5426c631b0f931f74f1558
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:12 +0000

    possible step holdon light interesting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
