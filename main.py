edc84d3c16f3c898afed8eea5563f803320a2097
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:03 +0000

    substance shop into help tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_min(numbers):
-        return min(numbers)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
