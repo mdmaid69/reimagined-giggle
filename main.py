4078ca10b83613a74f3e0889173c7a3cba834d0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:49:36 +0000

    takeaway house equipment 565end brain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
