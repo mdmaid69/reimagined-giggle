c31516c81e0467cc23b581fbc78be8c397dd4535
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:09:49 +0000

    fat ring against charge motion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
