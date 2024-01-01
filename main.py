0c5fc277a351d27104050099110f9725f2aae0d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:24 +0000

    adjective breath outside scared same

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
