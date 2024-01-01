cf9e7f6fab1af5b07f70bca9a4dc228944f6280a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:54:53 +0000

    nonsense way mix harm too

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
