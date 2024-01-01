41d02ad82392fe820139fd7ce888e9065e988b68
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:03:44 +0000

    law attempt area simple through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
