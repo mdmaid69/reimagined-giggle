cc17fa326c57c46b4502b2ffe0529922c10e7005
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:14 +0000

    miss perhaps level row odd

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_area_rectangle(l, w):
+  def calculate_area_rectangle(l, w):
         return l * w
+def calculate_perpetuity(payment, rate):
+        return payment / rate
