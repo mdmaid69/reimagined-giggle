682610e4cf0206eb9972536fb47a34ac8ce53810
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:26:37 +0000

    lookaround height bother excellent how

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def reverse_array(array):
+        array.reverse()
