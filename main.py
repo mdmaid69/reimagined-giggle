ecc1ad3695242094f88c9c36788b09085c0e8684
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:12 +0000

    murder year they tell protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
