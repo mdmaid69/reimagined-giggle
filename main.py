02ec8eab5a822b44362466f704bbaaa342276241
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:37:04 +0000

    war rule child remove early

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
+  import sys
+  def get_python_version():
+        return sys.version
