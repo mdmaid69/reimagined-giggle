c48bffb2d218741481e8c83d495eebc10a1c7d0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:51:37 +0000

    through impression innocent contain fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
