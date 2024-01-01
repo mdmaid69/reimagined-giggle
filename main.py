c00e833ad8d7acc2f5d74d779f8cabcf260a3fb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:09 +0000

    motion fine understand borrow stupid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-print(os.getcwd())
+def sort_numbers(numbers):
+        return sorted(numbers)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
