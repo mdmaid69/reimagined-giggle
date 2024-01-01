7b7f9c5dd6bb4f19a232c09332ff144bd6b03f43
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:29 +0000

    warning spend survive tread cough

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def add_numbers(x, y):
+        return x + y
