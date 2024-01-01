7578d9bb7d554503874ade0f2749f079ef11d698
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:56 +0000

    fault flow demand defeat breakinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
