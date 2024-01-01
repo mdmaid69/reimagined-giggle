70e203d905af98b023d4062764126d806d871c9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:26 +0000

    generally travel whom state hello

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
