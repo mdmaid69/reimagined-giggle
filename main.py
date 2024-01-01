0db04972050611132cdb69e6fcc0fc593d021b70
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:25:17 +0000

    expect tread frightened practice ahead

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_density(mass, volume):
+        return mass / volume
