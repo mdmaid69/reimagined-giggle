63797bc996a8cf0a58cf20e405a2f6b3be5be30c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:38:44 +0000

    slide occasion guilty easy just

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def reverse_list(lst):
+        return lst[::-1]
