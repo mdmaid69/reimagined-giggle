cc6687d6987accb7230b59326a00b684fbdbf877
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:26:16 +0000

    remove dress typical unite anybody

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def add_numbers(x, y):
-        return x + y
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def sort_list(lst):
+        return sorted(lst)
