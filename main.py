09b36dfe81ce2f685db2f838349611e09be40ba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:58:31 +0000

    classroom comeover normal normal order

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def find_min(numbers):
+        return min(numbers)
+def calculate_power(work, time):
+        return work / time
