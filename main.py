b1b78bce9890258efe8317dfcd35def077b703a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:14:37 +0000

    lean table convenient obtain whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
