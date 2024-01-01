081f12f427b468b4fca503c9420bc381784a24c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:55:14 +0000

    bridge long backwards trust Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
