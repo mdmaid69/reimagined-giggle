4e037e9c5137eaa36d03047db6b1b37481389a92
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:18 +0000

    welcome would comfort taste type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
