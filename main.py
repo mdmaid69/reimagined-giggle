1708abb88b46c9c73c7bbdd3828af65650a5fa6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:39:21 +0000

    plenty recommend intend country main

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
