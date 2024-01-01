f9eee5a2aa65b5b3b5d1954053c8258a240e25a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:59 +0000

    lost wind funny he spoil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_pressure(force, area):
+        return force / area
+  def convert_to_binary(n):
+        return bin(n)
