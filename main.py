fd4c4dfb61f03e9f4d0c79c10ad210eb23050653
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:03:22 +0000

    tomorrow bringup increase creature taste

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import tensorflow as tf
-print(tf.__version__)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
