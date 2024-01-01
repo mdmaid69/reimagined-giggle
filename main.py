43f9159c074578b7b6f3474eb5ff1201292d0bbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:22 +0000

    steal priority mark secret lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
