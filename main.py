9b83c6d4d63b4b7ba60aa406e8e07ecd18440178
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:35 +0000

    treat criticize pattern silent juice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
