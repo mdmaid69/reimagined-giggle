65375747289b5b8fcd3185045fc20240b633458e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:02:34 +0000

    ours somewhere holiday be fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
 import array
 def get_array_as_repr(array):
         return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
