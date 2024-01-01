2ac3a77d237d77ffc25ffb52ba3855e9c021ae68
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:52 +0000

    eager hospital moon everything fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_int(array):
+        return int(array[0])
