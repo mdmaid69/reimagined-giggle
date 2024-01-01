dcae174f20e2077432a8f7edf3b45e89a4397511
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:11:16 +0000

    plant up office turninto snow

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
