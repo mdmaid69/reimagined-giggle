9033166b89c05ec88921e5c0c583002486c1550a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:21 +0000

    sign talkinto almost badly remove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
