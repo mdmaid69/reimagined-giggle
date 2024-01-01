6889e08a20fdb808249d6d5ea997a9722dd8c3b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:41 +0000

    loud pupil brush solve shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
