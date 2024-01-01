2ffdbb78c5a7fbcc640c53fa5486851547152eff
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:15:40 +0000

    friendly hurt declare stiff write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+text = "Hello, world!"
+print("Uppercase:", text.upper())
