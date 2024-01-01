8e6397baa59e104f8001a56f4d6b259630fe67a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:26:13 +0000

    risk top Oo goodbye condition

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import logging
+def log_message(message):
+        logging.info(message)
