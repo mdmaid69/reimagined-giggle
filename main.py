db332ddd419cacefef5ded9c921581d4ef1eb95c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:50:39 +0000

    waste whatever build bag sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
