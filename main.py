617f60b95149093bf4148d26d7d743b8527550c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:35:19 +0000

    stiff beyond bet unlucky certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
