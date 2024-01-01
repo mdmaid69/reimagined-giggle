a05f0a4288c20f2972ff39637d3041748ad96324
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:23:52 +0000

    makeinto hurry cheat Hh average

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_work(force, distance):
-        return force * distance
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
