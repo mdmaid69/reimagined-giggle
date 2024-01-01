70456dbf8109af8d579358846f6a016ac4e178cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:22:02 +0000

    bag pleased sight message storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import collections
+def create_priority_queue():
+        return collections.deque()
