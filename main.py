da9ddee43f3f7a46eb2a526d8321597cea49896b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:38:45 +0000

    still regard on floor length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
