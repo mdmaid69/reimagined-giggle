4ae5c400b4820765c8a7397e8c4d9926fd6ae242
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:27 +0000

    bringforward explain question duty port

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
