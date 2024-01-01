0c01bd79c524980fdba66d17ed4d225f71e6fc93
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:57 +0000

    damp father live aim think

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
