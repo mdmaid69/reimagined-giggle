458a525c41c7db6dd7b9dc5ebebd27a3bd3f27b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:10 +0000

    course appearance hide profit sensible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
