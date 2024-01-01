ff87d444118cbf1b6acc315b23a9cfaf8718a000
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:51 +0000

    single advantage exact basically by

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
