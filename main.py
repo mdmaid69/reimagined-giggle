bbd23c5c030d2bf83096679df8097b318ed85c7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:10 +0000

    port awake doubt correct strength

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
