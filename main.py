d05857ec3c9f081e55275aef81a7b026785e51ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:40:56 +0000

    determined smell mean soak smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-x = 10
-y = 20
-print("Sum:", x + y)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def append_to_array(array, item):
+        array.append(item)
