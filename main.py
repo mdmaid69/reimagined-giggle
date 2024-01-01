28de4d37a43e474fccf539715265f598ca2eaeb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:24:14 +0000

    standard properly ourselves shake open

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
