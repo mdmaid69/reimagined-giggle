5d27bac8fed8ef3ac17582b45f0aa0847ed6f5d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:57:01 +0000

    realize determined bus thinkof float

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_cosine(x):
+        return math.cos(x)
