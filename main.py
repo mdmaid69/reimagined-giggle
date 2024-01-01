c5cbb3275ecd6e270ec0a7d9d0a663d51b275c77
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:08:41 +0000

    because get tone across lend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
