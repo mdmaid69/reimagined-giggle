634bd7ad11bfa29bf08e393d7f35f07f598267b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:05 +0000

    exploration terrible job clothes address

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
