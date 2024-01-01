83ea945ec72ff84007594705efed1c8143779638
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:36:54 +0000

    mix family part politics point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_pressure(force, area):
+        return force / area
