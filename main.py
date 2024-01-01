878f56dc19a3a47d82fc148c2920da9b3c8ee5b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:11 +0000

    disappointed empty grateful read drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
