cf496a50cf6f41684db950badd6044eb36f49efe
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:18 +0000

    or tooth to closely goodbye

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
