566700d2e9437740d9289370d6eea9105dfa88a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:13:03 +0000

    law rest quick quantity answer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_floor(x):
+        return math.floor(x)
