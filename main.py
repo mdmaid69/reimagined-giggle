5f727ada8a8d9116bed82f0b1ddad1ed59297282
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:33:41 +0000

    insist prove sorry cap rest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
