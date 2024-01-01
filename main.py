51c81d54f6cf4f8151962ab419d3672b7ccd2b2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:24 +0000

    Bb depend break sink band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
