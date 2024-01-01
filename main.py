d85ca1858a5b61f79b20f7b3f5a6a528356051d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:22 +0000

    activity mail destroy all punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
