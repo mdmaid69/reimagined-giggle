b6d1384cb3fca25e35f6e8b5c34b98c493586884
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:08 +0000

    concern talkoutof turnaway improvement spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
