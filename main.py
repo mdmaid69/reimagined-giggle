971fe6a48173cc8474ccc24971efa395ba207fee
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:47 +0000

    skill straight risk idea during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_float(array):
+        return float(array[0])
