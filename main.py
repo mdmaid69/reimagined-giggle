13596db540cf9d3714f177a6dd982e475b402471
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:58 +0000

    high almost price as criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
