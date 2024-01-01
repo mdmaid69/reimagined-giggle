ba002c37d45917aa8727785a749628e58c152e75
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:20:20 +0000

    schedule total Ii decide birthday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+print("Hello, world!")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
