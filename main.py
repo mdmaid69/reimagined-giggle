a721aa3686fdaa2eb14ca0432678a28c723dc69c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:17 +0000

    thick lost somehow giveup square

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
