eb952272d6e7b73db3a97e23b63377c9410427a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:58 +0000

    shape century smile they freedom

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
