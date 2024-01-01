3fa1beea834aaa3237140c40e0322b352c6d264f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:17:47 +0000

    gather note tongue remain ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import sys
+  def get_python_version():
+        return sys.version
+import os
+def change_working_directory(path):
+        os.chdir(path)
