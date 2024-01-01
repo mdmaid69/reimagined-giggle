e0325fa71083f2be224a0b07e8940731b4de003d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:06 +0000

    in plate sudden fail trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
