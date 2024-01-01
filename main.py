68446f514bb281ffa496f651084b6bb2263dcdd2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:04 +0000

    but fit clever even interested

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 def calculate_distance(x1, y1, x2, y2):
         return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
