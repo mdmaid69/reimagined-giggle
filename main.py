36dfd484d4358a4f06944d68086658652302cac7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:01:56 +0000

    afford shall manner nor buy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_density(mass, volume):
+        return mass / volume
+def find_max(numbers):
+        return max(numbers)
