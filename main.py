67f66401fb8ea82d55a64fec389ace8e99e938d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:15 +0000

    flow paper partner famous plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def delete_file(file_name):
+        os.remove(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
