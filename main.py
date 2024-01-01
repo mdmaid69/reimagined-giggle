4a34244660bada7e634c5aeeabac5b1c623c38e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:25:48 +0000

    joke outside organization well comefrom

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-for i in range(10): print(i)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
