0ea03322fc0ecb64fdab1a57a0f72ebddace81e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:04:17 +0000

    nothing such brother ought accidental

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
