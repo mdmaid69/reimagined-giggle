d81dc132dd5179a17d316ab5c47181fc3d4a3d45
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:51:15 +0000

    connection replace explain slice geton

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
