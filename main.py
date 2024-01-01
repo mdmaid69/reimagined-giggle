e5f72f7da89e3e02ac60c51dc0bed5a23ee85fca
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:21 +0000

    hit record welcome remark could

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
