7fed8ca8fd983232f0c5d1844795c665b3dd75a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:35 +0000

    scheme fast himself will pile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
