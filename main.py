4c92ef1780a5951e2fe3bb01305b91a2055a8f2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:38:52 +0000

    hold almost goup stiff clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 def calculate_force(mass, acceleration):
         return mass * acceleration
