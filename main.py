8d79121a21302592b3f03d121507c5a444cc53fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:34:46 +0000

    loud bringup powerful frame apply

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def set_array_item(array, i, item):
+        array[i] = item
