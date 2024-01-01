6bcdbfd1992e435a025e13fdf8eded2babc11756
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:32:04 +0000

    real second pupil loan add

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_directory_name(path):
+        return os.path.dirname(path)
