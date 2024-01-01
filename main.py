9945befb7395db2f906533b4f2a092995e5fd0e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:47 +0000

    pollution dear again grade over

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+import array
+def get_array_as_str(array):
+        return str(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
