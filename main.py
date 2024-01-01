4c6f8f9e70eab956d10cacfb690d744b93fc9c4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:23 +0000

    special friendly direct prove direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
