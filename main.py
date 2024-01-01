1e5713dcef390dfceadcbdf0bb2c5c47718fcaec
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:33:51 +0000

    spin disturbing sure importance rent

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def exit_program():
-        sys.exit()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
