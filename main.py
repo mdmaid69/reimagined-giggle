fea89606e18b5c7150ef91841fba8cbfe9318e6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:11 +0000

    even mean repair measure importance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import time
+def get_current_time():
+        return time.time()
