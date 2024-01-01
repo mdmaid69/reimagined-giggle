f18f35cfbfeecff307a4ecbac9b28a5bc9684ebb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:24:19 +0000

    will rank trip night give

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
