0978d3e427f468f7301a2e8e291aa57b783be023
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:19:30 +0000

    declare better use chase entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
