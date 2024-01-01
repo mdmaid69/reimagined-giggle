13e52f444ce7084a7705e892b80180603d9d1add
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:49 +0000

    dream fact left group change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
