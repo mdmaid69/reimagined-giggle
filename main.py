8ea80a61f723ea79e4f760f414982af700120c45
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:01:59 +0000

    excited weak file afternoon react

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
