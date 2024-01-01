bc2d96804f4eb198f19ce4bbf4d2cfa408b92031
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:08:46 +0000

    attractive sweep giveup pronunciation team

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def is_odd(n):
+        return n % 2 != 0
