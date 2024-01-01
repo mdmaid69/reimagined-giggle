499c1c187fd1f795e7d00b1c651e87289239e120
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:45 +0000

    mark work if disturb run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
