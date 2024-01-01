3aee2b1f056db52e60361f0da96aca76a2c86feb
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:02 +0000

    give picture scale question instruction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
