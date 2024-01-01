c3cd023e205750c1330cac44df9b456fe809f316
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:31:56 +0000

    new accordingto old sick paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_error_function(x):
+        return math.erf(x)
