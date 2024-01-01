356f391f81678a6d19c743c37f03a43e5cf1dd23
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:39 +0000

    fall find breakdown along disagree

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
+import sys
+print(sys.version)
