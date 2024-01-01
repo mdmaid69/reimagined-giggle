ac81ea4ad005bd233491c5d0270a696ccf09155d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:00 +0000

    party basically ill keen close

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
