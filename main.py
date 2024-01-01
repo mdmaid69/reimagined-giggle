9d4160410a7b9b41a1ab545d083e3c63e241b9a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:50 +0000

    tough sing pinch load might

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_list_from_array(array):
+        return array.tolist()
