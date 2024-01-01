e3ea0276167afa96ef600020d3d175e9dabdea32
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:02 +0000

    broad damage mail leg run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
