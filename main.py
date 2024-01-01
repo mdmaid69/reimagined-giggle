7290d7867a243f6f683c8a7166c1eb30102cc0fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:47:39 +0000

    cross crazy train move uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
