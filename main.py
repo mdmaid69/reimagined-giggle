8cff3a0173c511bcd8da233701aa7aa6841b33e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:50 +0000

    deep past shame cheat can

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def multiply_numbers(x, y):
+        return x * y
