8df42b33eb8d79a3ac4a6bff53068d3d5b59db86
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:58 +0000

    beyond moment nerve satisfactory distance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def change_working_directory(path):
+        os.chdir(path)
