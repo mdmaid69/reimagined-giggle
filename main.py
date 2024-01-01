26b223706c6bcbc88565f46b484610dad295bca6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:24 +0000

    unable drop day transparent rich

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_sine(x):
+        return math.sin(x)
