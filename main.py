5011539cee606989fc5a4aa9d6bc0b871efa494b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:09 +0000

    come priority earn loose powerful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
