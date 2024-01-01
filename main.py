615349519fd585e08af38cc186fb42094e7e3f80
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:23 +0000

    change scene getout owe everywhere

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def is_odd(n):
+        return n % 2 != 0
