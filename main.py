8e41d21551b6c04b98dc9a8fb8cfd73a6afe4298
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:54 +0000

    divide heat calloff listen relation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
