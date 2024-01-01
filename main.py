161d4d81ce690e2f364fd0faaa282d0dd316068d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:15 +0000

    condition crowd turnout fire mysterious

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
