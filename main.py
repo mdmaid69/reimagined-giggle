0d21bb7ed665d35b9166043f00befcf66faa471d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:42 +0000

    careless record name there sure

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import math
+def calculate_sine(x):
+        return math.sin(x)
