c8ae6d99521e90c09a9cd5ee36cb63f6213f073e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:49 +0000

    bet wise consist people risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
