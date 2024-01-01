5afcd6e7180d13fee8192889cb9b0aa0c9207f40
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:17:09 +0000

    flavour general ride injury discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
