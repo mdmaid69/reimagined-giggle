3ad33435452b18c32c743f71cd010e3dbaf3fc01
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:34 +0000

    inform test fear dangerous have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
