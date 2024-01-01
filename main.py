0bfdbd719780aa91d857a215bf9505536d59141b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:11:25 +0000

    left round come ear out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_user_list():
+        return collections.UserList()
