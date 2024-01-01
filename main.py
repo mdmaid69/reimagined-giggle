5f4691df66607cb12048c88052f0dd741cdc939b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:52 +0000

    character upset depend butter furniture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def get_array_typecode(array):
+        return array.typecode
