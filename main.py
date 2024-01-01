d4b9c48f47c188559f94bc746516ad9ec7643e91
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:33 +0000

    phone law show live lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
