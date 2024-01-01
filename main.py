fe12bde45adf68d243eb958b32795c43dfa792ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:31 +0000

    progress error tune public behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_set(array):
+        return set(array)
