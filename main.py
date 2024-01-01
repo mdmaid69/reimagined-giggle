c4736d50e935ef64278f10d57d640d80cb8afec7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:47 +0000

    describe positive supply payment difficulty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_list(array):
+        return list(array)
