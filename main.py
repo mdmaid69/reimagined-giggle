3cf5fde1d3e0011586f56324f0b174a6d6383dde
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:09 +0000

    estimate grand add suckme as

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def reverse_string(s):
-        return s[::-1]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
