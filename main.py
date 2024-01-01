1c6a476b9c522fc303c40616ec263d8e0cb0e4f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:23 +0000

    keen dance smooth back test

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
