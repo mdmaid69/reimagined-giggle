d33929660aafb41bb46b7fc6894f8779665f839f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:23 +0000

    beside fear claim getoff lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
