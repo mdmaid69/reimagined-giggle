ec7b6df78badf7a933ed624173ff967acebc933c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:58 +0000

    ill finger radio back nonsense

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
