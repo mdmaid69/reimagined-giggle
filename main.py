330f2ea925df142d5de2fb10ffd3c42b30f27ec2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:40 +0000

    friendly sell similar private arrange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
