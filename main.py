9dfb3d94ffd460abb18f5b2d069048858f3e0fd2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:03:10 +0000

    duty chin exhibition fashion calm

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
