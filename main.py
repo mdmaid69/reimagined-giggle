11aa0baf2ace10e1ded1c336fb2b4980a573cd4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:29 +0000

    joke promise backwards computer all

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
