440b3065f26e3728b9b35e9d6efa2308186e35f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:07:50 +0000

    stranger makeinto worst goaway backwards

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
