b7d9bd7d7d64d7e4382a04ab795cca3c0c2bf4bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:09 +0000

    polite anyone related aim valuable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_dict():
+        return collections.UserDict()
