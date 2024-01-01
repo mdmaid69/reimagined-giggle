dd3b91443e73453df2377621913066519da2bc02
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:44:57 +0000

    gather sentence gather importance than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
