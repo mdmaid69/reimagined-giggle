c8c29518efe77f66550f89ea58967b6dc2c96f8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:47:01 +0000

    part innocent scrap height roll

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
