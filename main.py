a9633c328b0bee46e232b6f1d31ad2816f03d4de
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:46:58 +0000

    beat rough connection mainly deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
