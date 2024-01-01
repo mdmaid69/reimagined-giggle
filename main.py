daaf735598c9ac6f77e444f0468e00e122efef0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:51 +0000

    again deliberate without same search

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_power(work, time):
+        return work / time
+def convert_to_hex(n):
+        return hex(n)
