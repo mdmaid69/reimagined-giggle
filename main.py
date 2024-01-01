1ffe893951df5b03556f9349d9fb91c71d2909b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:05:17 +0000

    letter couple passenger sticky trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
