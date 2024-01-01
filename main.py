ca7e19fd00d67c62bb6d66321ab7c466f5753976
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:17:40 +0000

    programme surprised quarter plan there

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
