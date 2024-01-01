307b9cc67fbf057307a087aef41fc185211ac223
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:25:23 +0000

    there escape operate send flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
