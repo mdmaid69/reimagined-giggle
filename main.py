1ae47f28cc8f1be3327fd3e5c385d64294d6a100
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:40:08 +0000

    support case arm consider bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
