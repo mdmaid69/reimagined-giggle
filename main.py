dfb2844f5522d85884a679597aea738f34fbd92b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:50 +0000

    set exam takeaway conduct ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
