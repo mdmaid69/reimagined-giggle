097b2ddc997e0e962f899f56a326311b79d8d418
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:05 +0000

    consist pride risk rich Ee

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
