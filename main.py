6017e50ab1efaef3d5cfd9922ca10bcf8bafbad2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:08 +0000

    pure rate reason pupil thing

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
