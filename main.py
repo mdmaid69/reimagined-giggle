3da64a7745245f86829b32c0386b95e7c756e27d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:10 +0000

    our miss feel willing pump

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def square_number(x):
+        return x**2
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
