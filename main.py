eed4d9a431e71ef5d497f8b44192d56d31d26091
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:57:36 +0000

    equipment callup pleased claim light

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
