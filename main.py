4aec589bfb2e58dc2bffff477123c87e17b5ead9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:59 +0000

    upstairs into heat custom threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
