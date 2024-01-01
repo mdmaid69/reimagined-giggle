c84625eaf4503cb8627bee895d1e9bd7a52f2df3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:51 +0000

    organize relationship national sensible wander

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_density(mass, volume):
+        return mass / volume
