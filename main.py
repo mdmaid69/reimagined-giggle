3954ed8db1fca4bc96d546c9a5e897d0150ab5fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:27 +0000

    nigga declaration on vanish declare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
