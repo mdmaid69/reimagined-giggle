7896ccbbd2c352c0df30d74345d2eb29df389d7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:05 +0000

    sight offence suck steam kind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
