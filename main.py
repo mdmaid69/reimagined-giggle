32ca6b1e20a835c1ced921aa0e0ac45d5801bd82
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:33 +0000

    both touch front wish think

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
