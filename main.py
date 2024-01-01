bb67a64bb086e36c84738bf68feb4b1972ec1359
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:08:29 +0000

    rough collection man news beneath

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
