61ab29ee206d2569ab4bd017bda13e51998fcfc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:41 +0000

    material plan keepfrom successful sound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
