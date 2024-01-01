fb87b93eed13264e1f1415f792ac88564689b27f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:45 +0000

    trust thanks circumstance purpose centre

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def get_array_as_str(array):
+        return str(array)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
