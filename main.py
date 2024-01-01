b4d4c84bd057904a97e7c1aa8d405511c47a4a27
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:40 +0000

    hour say for attract unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_array_to_bytes(array):
+        return array.tobytes()
