acf750a6314b5c53d829503dcccd3c2ae96b9389
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:59 +0000

    force beyond hardly profit ear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
