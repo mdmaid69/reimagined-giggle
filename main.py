6f6d5e87368626782146a2038ffdf64a7e2d494d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:27:17 +0000

    bend effort absolutely stamp OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
