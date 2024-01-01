505c5a66fb05ae5439d20f3018115b745d0e2c68
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:20:47 +0000

    tomorrow feed apart bright exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_volume(length, width, height):
+        return length * width * height
