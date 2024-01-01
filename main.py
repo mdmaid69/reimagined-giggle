71d28440b0954a052994c1da073773020b7259c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:32:26 +0000

    thirsty spread takeout neck stamp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def remove_duplicates(lst):
+        return list(set(lst))
