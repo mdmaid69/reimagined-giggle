99e4b61d08031fd3abf34f89b4fd3715991b00a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:53 +0000

    butter excuse turn brown before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
