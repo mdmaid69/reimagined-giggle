60cbbd86c2b519e37e9615eed943881855f4adf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:10 +0000

    shopping pile exciting past callat

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
