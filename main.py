780b80f4183cf9327f3923c0b00cdef094cb478a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:30 +0000

    great hang use ours independent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
