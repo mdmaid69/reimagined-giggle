868de34461e520a31fee4bf84db841eb8ce84ba5
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:44 +0000

    bottom introduction schedule debt press

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
