77d031d9da93472e843409475f9f24d50329908e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:22 +0000

    shut care union pipe whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
