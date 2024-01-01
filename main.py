34564a4abe34117287e59262f341a81e0897273a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:14 +0000

    comfortable duty backwards total disappear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def cube_number(x):
-        return x**3
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import os
+def get_current_working_directory():
+        return os.getcwd()
