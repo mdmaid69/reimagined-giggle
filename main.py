9f24cb63b888d68b6f0b6ec4232fff526d025a4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:58:43 +0000

    nose moment active joke intelligent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
