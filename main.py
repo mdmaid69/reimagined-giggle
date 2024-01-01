59d77968511c0b51d0b2a381dc6b1af880bdca64
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:57:40 +0000

    remain expensive very flat instant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
 import shutil
 def delete_directory(path):
         shutil.rmtree(path)
