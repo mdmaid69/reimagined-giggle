765d12682ad2f7a1fa0be2cdb1bf5cc39aa5eae3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:51 +0000

    finger average centre head Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
