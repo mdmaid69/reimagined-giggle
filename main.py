37cf6dd728fd39064364269b38094c5348f4f330
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:44:09 +0000

    remember Zz nonsense concerning phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
