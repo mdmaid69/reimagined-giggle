cfbce41c85ca01cd5dfb0d5a607837bbe7c76d23
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:31:18 +0000

    film guilty suffer send tomorrow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
