da35eec98d3c88cf59591be5a593b58a0df9da44
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:00:22 +0000

    treat worth autumn in convincing

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
