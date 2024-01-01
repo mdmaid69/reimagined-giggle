eec8948084fa9850a38aa211913b09dcde87360f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:23 +0000

    start read adjective battery relation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  def sort_list(lst):
+        return sorted(lst)
