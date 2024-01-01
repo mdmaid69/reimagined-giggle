9e008f4f8ff4e337eda5ea65855f7366df4bb1d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:34 +0000

    hang pretty more generally able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
