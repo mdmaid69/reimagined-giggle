40a63ec5fd52a9cce4bb905b950636eef4d3bc31
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:57 +0000

    raw benefit open lay wave

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
