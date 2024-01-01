04e655e37a28605fd3ffdc34931e9d937baa271f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:29:54 +0000

    power turnoff scared over enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
