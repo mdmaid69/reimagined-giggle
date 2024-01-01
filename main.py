bc606e89d67626dcd598f8bcce97ab4a6e80867f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:30:06 +0000

    patient eager late special even

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
