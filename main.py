a373551cf8b2761a89b38ef867db19bef01190fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:42:05 +0000

    goon everyone spare cheap dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def convert_array_to_string(array):
+        return array.tostring()
