0696dba3d8beac9928448e00851ba195352293e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:02 +0000

    level tread keepaway reasonable kick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
