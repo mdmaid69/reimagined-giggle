56eddbe6b27e6469c531796893287a1a3cf6e5cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:45 +0000

    comein rarely impression accept add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
