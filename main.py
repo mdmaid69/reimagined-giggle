c6f34d1b8546fd9aa9b331f897efdf2fb5b24c62
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:14 +0000

    join mistake sink but unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
