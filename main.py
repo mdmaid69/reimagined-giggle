63dbd2633b18af563c8b957896ba4da444f99961
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:14:26 +0000

    reduction table favourite share should

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
