45dc3cf85e5dd4ebd03d5020b4402d03ef324ceb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:59 +0000

    shut giveout ear fold hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
