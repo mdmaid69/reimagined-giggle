8933bbc9e037af0cdd1c4a6bc420f28c834d5351
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:06:47 +0000

    neither Ww influence workon luck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_str(array):
+        return str(array)
