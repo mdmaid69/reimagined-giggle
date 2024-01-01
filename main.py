a0b695e24b84de514d5a00407b691682b819bbe5
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:24 +0000

    produce own far afternoon fly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_density(mass, volume):
+        return mass / volume
