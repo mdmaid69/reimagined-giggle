c8d6d2903b1c639c47f062298c3865a10c1b8296
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:13 +0000

    proper finish part quiet flat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
