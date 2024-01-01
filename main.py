a292f17e69f872c8751355efe3c1a766a45358ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:29:04 +0000

    offer store half degree part

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_work(force, distance):
+        return force * distance
