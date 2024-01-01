5b2abce64618123dc54df447dba464bd05351e86
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:51:15 +0000

    clue guide pull shout love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
