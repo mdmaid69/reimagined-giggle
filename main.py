9a8085157b608800cb32c7574b39c891c45af7fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:51:05 +0000

    pair stay face open various

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
