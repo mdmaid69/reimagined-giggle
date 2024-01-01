abbfb75d30f3e3992697537465ceef39c91aa65b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:45 +0000

    equipment upset score correction chat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
