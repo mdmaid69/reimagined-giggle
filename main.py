5d0b0f1e8a3c80e30399f29a00f13d4387a7297b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:40:46 +0000

    perhaps port legal sink swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
