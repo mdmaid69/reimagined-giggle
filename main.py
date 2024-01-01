42595f54150e1208dff467560d6b5978e31df356
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:06:44 +0000

    special commit plain above deliberate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
