43d11ec1ec68acdfefeb7a5babfc8e217e1a170d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:58:21 +0000

    tomorrow sit down report spring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
