880ce494d55257f0ed3dce41a6aa1b2087354480
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:33:44 +0000

    tense anywayanyhow go object circumstance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
