f671a11543115b62078b2da32a7b4419283e3464
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:47:08 +0000

    slice careful water recognize crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def sort_list(lst):
+        return sorted(lst)
