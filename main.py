3824b5a00568731f174c167dad01ace1832be71b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:04:27 +0000

    any cope succeed deep well

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
