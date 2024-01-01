052904ce6f7ed40e072441f6786303a1a3463e48
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:12 +0000

    deaf order burst hurt spite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def square_number(x):
+        return x**2
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
