ee90ea7388946af672fd5a6b2d02767bc678b4eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:50:33 +0000

    confuse beautiful friendly steer choice

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
