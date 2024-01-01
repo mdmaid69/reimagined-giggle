74293bde39be7a36a5124f13a14f50bd6064d084
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:37:12 +0000

    brush crash community bury improvement

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
