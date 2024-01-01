0e06feede62494f72cfe9ac2a494857998e36fd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:46:13 +0000

    impression stranger division getback wise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
