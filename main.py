75741f3a3e72ac65202715258d9e04cd53aa4f71
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:45:08 +0000

    I because form comment union

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
