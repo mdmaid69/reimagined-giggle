a04a8f47f00664ebdebb4f6c21b55378c82726fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:52 +0000

    continue invitation turnon house worst

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def subtract_numbers(x, y):
-        return x - y
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
