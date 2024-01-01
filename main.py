6ff53c7ae92c71dd5e262f15984552eaa3650b38
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:09:21 +0000

    personal circle hurt experience object

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
