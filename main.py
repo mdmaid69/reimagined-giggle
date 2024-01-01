abe2b323b65cb76e9d7dbb77b4f3b301069eab49
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:07:41 +0000

    club frighten lesson born round

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
