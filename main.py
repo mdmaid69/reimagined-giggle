e33ee8b02f9bb105395d76a3dd6eda2be6209a67
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:17 +0000

    workout term lower that basically

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
