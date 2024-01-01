7ba17aa4e6f0eee004869a1d740cf0c5f60b359f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:19 +0000

    poor transport you schedule free

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import os
+def get_current_working_directory():
+        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
