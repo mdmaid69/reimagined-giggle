7aa3904891c9330ef8eda0c758c21f3cb7278644
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:13 +0000

    lend lie thirsty aware beyond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
