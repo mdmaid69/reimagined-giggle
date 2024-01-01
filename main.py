d326e9619ca6ef8e4fa090ecfaa128f2c8a8b379
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:37:31 +0000

    complaint impressed only real cry

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_current_time():
-        return time.time()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
