429bbe7dbbff24fcc0b761fb28b3aae0d2cc8131
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:29 +0000

    reject still during increase mine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
 import array
-def get_array_as_float(array):
-        return float(array[0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def count_elements(lst):
+        return len(lst)
