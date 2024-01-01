27cb969305f979ab8625c74804725298f4ac672c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:29 +0000

    accommodation my chase emphasize voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
