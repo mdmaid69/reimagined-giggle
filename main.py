01a3743180a9820400302256a70a0fd15dba64a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:38:49 +0000

    save level could no art

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_min(lst):
+        return min(lst)
