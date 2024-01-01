3e07cbb1fe9c7dd9e7fc18cb869137ff1c0e1f60
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:35 +0000

    happen glad house letter exchange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
