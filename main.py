00512f5afbf4c9bc631c96efd111c5a9c926406a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:51:47 +0000

    unpleasant plan Oo only bear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
