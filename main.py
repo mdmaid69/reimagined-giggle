239ad080a1962c3c94aad1590da2955d7b59afc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:00:33 +0000

    forget port uncomfortable line turnout

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
