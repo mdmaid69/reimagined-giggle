5fd4417aff77e1f69b9b911be23b13b575efe17e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:29 +0000

    joke relax tooth address pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
