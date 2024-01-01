cb5be5b40530cd139511a2d37c482ab06f47459b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:03 +0000

    food meeting shine OKorokay familiar

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import datetime
+print(datetime.datetime.now())
