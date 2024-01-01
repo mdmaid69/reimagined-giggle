a15e366e8d85164bc6f1ecbaca88f0f4c4a1fa28
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:26:29 +0000

    ring health firm trousers do

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
