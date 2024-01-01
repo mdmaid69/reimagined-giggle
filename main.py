042a5be6f77868f1a0ccb847997251c2b1b36b80
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:37 +0000

    part approve generally foot success

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
