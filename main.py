5caa6671ad4ef43208ee1dcc04b9bae35ea26125
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:41:10 +0000

    freeze swim radio cut keepup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
