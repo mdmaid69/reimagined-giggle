8150f3aaf6832b255cf9b6a295db64bf26953a17
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:47 +0000

    hurry copy bottom design increase

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
