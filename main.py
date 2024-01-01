7b8c057ac95c59147124a5fd943248ac2d26b948
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:21 +0000

    Zz steer rush proud community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
