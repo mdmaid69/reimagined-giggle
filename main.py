f690e91ec1735a69b6a67a355892fed9af426ae3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:06 +0000

    pin father object exact cum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_acceleration(speed, time):
-        return speed / time
+def get_array_as_repr(array):
+        return repr(array)
+def find_unique_words(sentence):
+        return set(sentence.split())
