00a69d340be0d692604c96de95ce44c2d3b1cc4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:27 +0000

    situation blame reliable state influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
