d2fe9e971b0555a7acedd020b345a2b8f42070e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:33 +0000

    firm listenfor murder recent yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
