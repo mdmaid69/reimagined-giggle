460e5a36cf336e05a8953462343400e3f642d6ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:19:56 +0000

    stop real interruption sick uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
