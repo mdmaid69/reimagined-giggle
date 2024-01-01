11bb1db0ccaa9b1e6e4c813eab4a9bb459d1c130
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:03:57 +0000

    back key pause serve shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
