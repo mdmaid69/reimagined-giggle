77bf02ffdec40453df2addd1b5ef0c1702b89a95
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:12 +0000

    business hope getout task face

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_user_list():
-        return collections.UserList()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
