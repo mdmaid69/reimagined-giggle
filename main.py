69785ebcec65191dba787d8f13577df380d79989
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:42 +0000

    comeover lost damp scare money

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
