41feadf21a646d3fc426b629827856cd74ffa714
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:26 +0000

    general tip something tired environment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def split_path(path):
-        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
