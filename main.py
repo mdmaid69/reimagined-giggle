a590a8cb966c10281215c847e6431c705f360a98
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:04 +0000

    standard possession spend test takeaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
