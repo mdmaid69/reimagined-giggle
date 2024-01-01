883c281a6397463a99b738caa59199b19600d4da
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:01 +0000

    wave speed test petrol post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_queue():
+        return collections.deque()
