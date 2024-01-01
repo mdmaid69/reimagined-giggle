7b6333296dcfcba718a8825e74f9db74ef06529c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:24:22 +0000

    house future wall mixture strike

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+for i in range(5):
+        print(i)
