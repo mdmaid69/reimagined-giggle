aa6350bb5c7220d0351f547d44d22ad784f31293
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:58 +0000

    separate about cover notice congratulate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
