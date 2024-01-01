9c490f0d07f2a871f2e5722eda6f1591b4ec3ff3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:10 +0000

    figure spill period radio sit

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+  def convert_to_binary(n):
+        return bin(n)
