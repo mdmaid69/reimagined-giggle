53d7a1016012d0a37143c7a5f74f34d2baec3d04
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:05 +0000

    generally common male he relief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_length(array):
+        return len(array)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
