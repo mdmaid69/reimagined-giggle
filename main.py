f90f802e3532b07c833be3ffd3662d3a8eb188ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:32 +0000

    share embarrassing small together special

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
