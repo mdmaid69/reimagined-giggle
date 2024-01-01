bdde84ddcad0d6bd8bcdc611e6f74e94aa3ecf3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:04 +0000

    service impossible back part sad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
