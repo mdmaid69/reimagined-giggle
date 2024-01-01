b1d7ed8806122f77954320cb2b5fe1c59be3a550
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:16 +0000

    stiff plenty but step sink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
