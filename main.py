a852362ccdf91e1a5dd590314b70e2afbb50fef5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:50 +0000

    feeling entry mean chance or

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def find_max(lst):
-        return max(lst)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
