00e8a7654c82a82479f72ac13f87fa1fbd202c7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:08:10 +0000

    dear breakinto score across bath

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
