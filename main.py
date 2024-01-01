bf312ef84ad45e665d465d48adb3e7479d37e07a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:11:40 +0000

    colour accident judge wet modern

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def count_characters(sentence):
-        return len(sentence)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
