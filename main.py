b73776cfc04fba87090ee9c6b2f50ba889a5a6ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:03 +0000

    crowded cause agree nowhere interview

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
