aacd1f1a838c2147a4cec3b6fda07026a8370a66
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:07:58 +0000

    on just only serve pressure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-def convert_to_octal(n):
-        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
