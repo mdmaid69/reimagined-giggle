003431783fbeb2ca87d8e5ad6a2f55a8b3afc130
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:24 +0000

    dream act busy plastic true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_set(array):
+        return set(array)
