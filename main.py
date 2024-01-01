530fba61657290200da701ff2a1a4cb12ab6cd41
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:34 +0000

    flat national callback search law

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+print(math.pi)
