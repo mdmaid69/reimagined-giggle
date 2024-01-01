70a1b595faee8026a0c5ae04f1d6f14da66fa149
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:58:57 +0000

    motion whenever runaway light lean

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
