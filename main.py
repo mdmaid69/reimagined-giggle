8f59754a0e71499ddddcb1bbf45e5293c5c56f41
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:00:22 +0000

    same port path forgive serious

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
