cc001dfa6ffe1aff16abf5747169113150302f80
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:05 +0000

    wish here condition fall stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
