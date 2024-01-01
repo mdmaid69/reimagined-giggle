1c86f60cc43df44298f9f08d770387b4e73d02b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:43 +0000

    climb hope basic protest progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
