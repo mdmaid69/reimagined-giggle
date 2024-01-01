f605032680b75e39598163d83186c030d3b94005
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:54:42 +0000

    offer ability emergency inform like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
