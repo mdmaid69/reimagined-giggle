98e02310bd7f99bb3b0955114fa692a0d686a0b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:44:29 +0000

    material into heart calmly love

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
