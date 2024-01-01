b1fb28292b155b2a4c206c71aac63ee4da290591
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:55 +0000

    anxious voice shock swing exercise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_power(work, time):
-        return work / time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
