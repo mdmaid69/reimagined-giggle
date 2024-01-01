f117b000103e4af5ca85a3369bb37c3ccb05564b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:05:29 +0000

    mean with under children maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import tensorflow as tf
+print(tf.__version__)
+import sys
+def print_python_version():
+        return sys.version
