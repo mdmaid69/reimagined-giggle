fd6cbd291b9f1fe647a1bf0f17106bc3822bf746
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:19:37 +0000

    dance hit accommodation progress theirs

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
