6a97bb096a8da59dcc6b8d8ba9f997544631483e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:39:17 +0000

    nice fence sink swimming all

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  def sort_list(lst):
+        return sorted(lst)
