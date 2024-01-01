14e9f14bd7345a744d62aa4f09e357d715f7ac35
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:03:22 +0000

    turn several development explanation near

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_acceleration(speed, time):
+        return speed / time
