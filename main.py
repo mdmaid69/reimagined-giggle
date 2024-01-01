c2d9c4c46091fc829792de824f0abba2b0c43a7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:15 +0000

    I future workout minor Kk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
