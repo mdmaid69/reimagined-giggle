5152aa9fa83a9800e0e565c7cfb76d1a5af0c2d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:55:17 +0000

    stranger ilovedicks spin splash most

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import sys
-print(sys.version)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
