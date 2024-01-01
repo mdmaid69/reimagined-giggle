f6bb8a740cb89aab47125c0f7afcb8e67bce24a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:51:47 +0000

    juice offend profit interested never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
