b7e72b5b24c5349f33ceb56a657440bf45d33792
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:10:05 +0000

    late left deliberate regret quickly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
