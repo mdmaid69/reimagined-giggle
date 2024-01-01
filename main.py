17ccc9ed6b2c519e420c91b2d83ec1e1601db522
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:08 +0000

    instead happen tone force colour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
