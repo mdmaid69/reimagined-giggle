3ef3b0a4107a7cbb4d2a20147ec5457fb464e212
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:00 +0000

    wrong as dare issue room

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_error_function(x):
+        return math.erf(x)
