94e31f8d6f56cf2557d2e7c1285fcc93cea31a0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:18 +0000

    571enough level lose loss if

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
