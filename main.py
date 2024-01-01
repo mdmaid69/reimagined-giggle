46b7898ada0bf14756e649303f4429e5f83df064
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:42 +0000

    lookback(on) rare little appearance risk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
