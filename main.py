97dee750fccbc298c86b2254ce3336697cc778c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:42 +0000

    convinced emphasize room strange let

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
