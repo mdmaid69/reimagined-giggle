95a62f89f879f56e8c39d3f5a55deff353d678ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:55 +0000

    its more sore content direct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
