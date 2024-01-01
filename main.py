46ecc42b50f5c14d08e2a0f40a581ca2dd11641c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:04:08 +0000

    keep hers further weigh of

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_min(lst):
-        return min(lst)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
