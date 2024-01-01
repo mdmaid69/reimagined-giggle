4e9ba55136dd3c87a817bb4af689366eff9e1106
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:58:44 +0000

    final pump regional flight away

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
