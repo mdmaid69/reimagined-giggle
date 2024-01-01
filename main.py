3d5a4c24ee88667ff982b35bf1092a676794bfb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:01 +0000

    collect this criticism exchange leave

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import random
+def roll_die():
+        return random.randint(1, 6)
