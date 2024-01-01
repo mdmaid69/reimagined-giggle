27f4377a88137b92434a4ca1443c003b265bd5c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:35:40 +0000

    by tap goup maintain pronounce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def add_numbers(x, y):
+        return x + y
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
