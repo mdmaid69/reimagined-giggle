7311b7e8791a69fb9fc9dff043ef5ec000fac4c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:07:22 +0000

    approval judge house goaway general

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def add_numbers(x, y):
+        return x + y
