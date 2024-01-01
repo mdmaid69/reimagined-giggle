ed1c53bb14b250eaecd727612c742feff95655dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:21 +0000

    hit moon lesson influence smoke

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def add_numbers(a, b):
-        return a + b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
