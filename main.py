d3be2abbd861f53c6dd7fd52fbbf084f97379064
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:28 +0000

    convenient hurt call gradually extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
