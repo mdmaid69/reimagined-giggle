616f1187acd83f54677b911b5b9981b54afed684
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:38 +0000

    full hope label band each

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
