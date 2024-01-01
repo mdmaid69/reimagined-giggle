d3fcb4f655d3eadbe51e2cf6e2541bba42642527
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:25 +0000

    exchange wide worth everywhere take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
