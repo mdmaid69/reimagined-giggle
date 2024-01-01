cced3f9979951f78814cbaf7252e6eeefe655523
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:56:17 +0000

    nerve follow exit interview introduction

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def count_elements(lst):
+        return len(lst)
