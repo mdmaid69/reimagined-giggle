699785c51fa2c7346949898f58ff6dfd1a376226
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:29 +0000

    bet once war unpleasant on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  def is_odd(n):
+        return n % 2 != 0
