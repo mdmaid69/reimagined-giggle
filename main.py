d55b5352e96194de5752f835b4123aadb62dd6a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:46:42 +0000

    over touch cheque dare comment

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def find_unique_words(sentence):
-        return set(sentence.split())
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
