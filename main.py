6ffadffbccb85718a9c739bd2d700177975480bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:41 +0000

    count confidence entrance aware relative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
+def square_number(x):
+        return x**2
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
