b5f7da1851a2e4bff0fd003c3021825af4869d49
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:57:12 +0000

    fight rough defeat eat sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
