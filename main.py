315fb7f7914dc69ce01d745141ecfa09fc768b52
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:40 +0000

    treat prove definite nigga dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
