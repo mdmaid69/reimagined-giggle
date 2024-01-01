cf75ffb68f5b33d6dd131fbb1f13cb68ee0afaad
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:44:27 +0000

    sea something careless home stare

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
