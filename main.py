19ae9847080213dc6141b7a27fd3c2a1b69831e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:31 +0000

    flat convenient boil me belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
