66cd282d106da49e9b6039d8bbebd45a5908417f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:23 +0000

    level unlikely stir add progress

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
