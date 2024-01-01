b6903debe6172443d4d9e8ea97ed2e1ede569254
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:45 +0000

    realize lesson ahead nowhere colour

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def is_even(n):
+        return n % 2 == 0
