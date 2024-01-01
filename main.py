eabeafda7e31ebc4e929003765acc65725c7d697
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:51 +0000

    error desperate stupid second interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
