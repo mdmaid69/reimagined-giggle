41deb2e09da745720e519e399a28698358d18b3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:42 +0000

    crowded precise onto though exact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
