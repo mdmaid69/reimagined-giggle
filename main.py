755cde4c1b61c8e4a3102b126bd6b0a4b176ca3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:30:48 +0000

    write connect if central watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
