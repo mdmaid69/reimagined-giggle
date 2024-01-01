44f1d6d0c636a137c946404673fd9791a571d8a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:34 +0000

    prove broken hole confident under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
