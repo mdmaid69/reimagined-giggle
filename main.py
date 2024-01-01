46ce10716105a6af8c229bb8456bb4b6927ac602
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:28:06 +0000

    reply below praise sea factory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def convert_to_hex(n):
+  def convert_to_hex(n):
         return hex(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
