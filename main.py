63ca29c52e87282e14f055b2cdcc2aee9e81b00c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:36 +0000

    tidy tense than 570enjoyable anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 def calculate_quick_ratio(current_assets, inventory, current_liabilities):
         return (current_assets - inventory) / current_liabilities
