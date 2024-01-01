dfda872430cbbed4f5d83da29cac3c2073a92563
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:00 +0000

    so common workon brief speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
