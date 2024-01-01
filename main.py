7cd1f5ff1ff24d35583bc5b541572982d65773ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:34 +0000

    damage gather course within hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
