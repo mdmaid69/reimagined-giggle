dda02c3ffc1d250bbae3d437901bad46052882bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:56 +0000

    party post badly wide awake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
