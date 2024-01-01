ca04a0ff6fc91b212b27c1e14dd8fc982458f4c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:39 +0000

    either exactly cause notice alive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
