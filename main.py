0bff365948e382eee3507cc54a26edefb215ee3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:09:37 +0000

    fix someone grand turninto or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
