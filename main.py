a7dc9a53b723fab20b81877775a9510a2bd177dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:38 +0000

    means ourselves room character protest

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def is_even(n):
+        return n % 2 == 0
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
