f065d5d25ce29967be25506574f33facb035ae96
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:53 +0000

    grade flavour sign just pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_priority_queue():
+        return collections.deque()
