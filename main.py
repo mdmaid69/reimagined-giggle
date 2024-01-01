d6a919d690838bd6010ae41af69a48bf4cd7a14f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:05 +0000

    neat runout relax wherever stop

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
