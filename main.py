4a4eb1455c10d640a0007f5ebf7103f3d350d2d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:12:56 +0000

    confusing tongue thankyou precise least

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
