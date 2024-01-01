748870d94d44b94af7d2234f600f9f7639c68826
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:24 +0000

    education together to place collect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def remove_from_array(array, item):
+        array.remove(item)
