61d74f3e5d62e0f9aa40a5ad877611c740356925
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:58:44 +0000

    convince blind up broad goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-print(random.randint(0, 100))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
