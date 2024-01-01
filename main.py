ec479600693bc58bb25e4b14aec65e3500ba8371
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:45:38 +0000

    picture whether wish area talk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
