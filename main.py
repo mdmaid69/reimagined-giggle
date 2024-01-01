03d6bd9697c130854e0de556d34c9fd40e4d4283
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:58 +0000

    mine pocket vote disagree chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
