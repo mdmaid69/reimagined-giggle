221c284f8627766b1a369db161274715bfa1284c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:33:55 +0000

    equal female spray cut interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_density(mass, volume):
+        return mass / volume
