b76e3e62429cdc2bbb21fd88997ef4a87f41f892
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:47 +0000

    back callat skill talkinto Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
