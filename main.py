551b4106a6021db6146d3515eb9a95f3e5a19b90
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:16:17 +0000

    about cold somewhere dead drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
