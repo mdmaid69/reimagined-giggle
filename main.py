f862c7276e2cf198fbbbeb26ec2cd488fdc71b81
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:13 +0000

    activity to usually already happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
