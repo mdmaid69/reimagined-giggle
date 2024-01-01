7984fb3c24c3066a320a3f3a9fc94512e102cfdb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:04 +0000

    really run Zz throw mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-print(math.pi)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
