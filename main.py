c193a215713941241857161cf02392e79d6e3d1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:49:44 +0000

    cost visit here throw discovery

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-  def is_odd(n):
-        return n % 2 != 0
-print([x**2 for x in range(10)])
+import os
+print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
