0754023dc2817809e7058ec4b36c52a8a91feff8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:38 +0000

    mean through skin around activity

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
