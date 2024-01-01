9d596e00a7b5fa5bd20f77309cd56a62ba9a8742
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:45 +0000

    division loose takeaway anywayanyhow accommodation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
