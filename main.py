c3d6362b499ac34ee72d90e9ca608979b0a6737a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:27:43 +0000

    further they whistle war getup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-print(sys.version)
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
