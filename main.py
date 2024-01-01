9647c765cfc380bd55e2aa09b615b37aa5df3e39
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:40:27 +0000

    method represent hospital by side

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
