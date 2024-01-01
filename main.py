08e4fd696df966ca3a77e3a20333d40cca6fb510
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:30:00 +0000

    attractive turn impossible apply visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_set(array):
+        return set(array)
