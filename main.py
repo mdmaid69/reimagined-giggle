43163ad1fabab1faa80fb8708b160c4504731e08
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:05 +0000

    woman used front include simple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+print("Hello, world!")
