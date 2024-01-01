263914a627dbb214a1ad9ad09c646bd7fe1d182c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:35 +0000

    remark trousers see bet firm

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
