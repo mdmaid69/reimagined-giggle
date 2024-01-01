eb5023fd0554fbcc49b1faed9ce79757406b3fb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:26:16 +0000

    sweep learn dear holiday say

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
