97c12606d9e060694b8c721edaf6ef067a0774ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:10 +0000

    my explanation crazy base knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
