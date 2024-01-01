a54f5a9c24d7f0670b81266e68bad82bdce9d1b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:10 +0000

    fact abroad contact scheme do

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
