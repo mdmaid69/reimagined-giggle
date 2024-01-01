a00d9a68b1c8264816cbc801e2d310464b8d320c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:38:16 +0000

    worth clue holdout whistle explain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-def add_numbers(x, y):
-        return x + y
+def get_bytes_from_array(array):
+        return array.tobytes()
