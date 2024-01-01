c0ba3ff1d12784d55ae147091f64c680f31b65b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:05:39 +0000

    minimum plan minimum do food

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def calculate_acceleration(speed, time):
+        return speed / time
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
