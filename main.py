fdf1ac03ccffc40237b1f07f9be895f8e809d491
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:52 +0000

    can payment exercise hard life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
