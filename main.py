d54b1c58a6957b65326d20f90228c6f3f62eb509
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:17 +0000

    goup guess mark over politics

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
