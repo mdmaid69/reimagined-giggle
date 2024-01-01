6ad9062096862f97f1220192c54edbd2e1f3cf6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:17:46 +0000

    support guess handle healthy please

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def is_even(n):
+        return n % 2 == 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
