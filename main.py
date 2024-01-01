e879d65d86056e0f70b3ada1df887e52cdf03e61
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:21:46 +0000

    reaction list noone university failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
