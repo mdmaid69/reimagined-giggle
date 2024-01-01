2bda263893cd96aa56785ecd1b6c3cf9f3ecc141
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:04:33 +0000

    about 565end uniform snow thinkof

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
