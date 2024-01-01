5a004e0e1dc37043a47c5718a7080326b6b06829
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:31:28 +0000

    fold middle ignore order long

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
