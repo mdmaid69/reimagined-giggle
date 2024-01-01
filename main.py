79be4e1a0cb7fb44e1971d44ab206faaebf05d5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:03:25 +0000

    nerve future health travel valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
