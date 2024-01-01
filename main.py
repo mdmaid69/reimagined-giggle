32f29e3b455fab8309a29058cc667c0d4072e589
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:59:21 +0000

    opponent acceptable peaceful female where

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
