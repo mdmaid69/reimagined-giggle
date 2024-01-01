e32193a8db54eba66d11cb1bb80ac3b8ec5462eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:41 +0000

    university impressed concentrate name fact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def cube_number(x):
+        return x**3
+import numpy as np
+print(np.array([1, 2, 3]))
