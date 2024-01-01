e06164abc14543679be8f836726afd59ff14a5c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:06 +0000

    tone wake dinner important ring

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def convert_to_binary(n):
-        return bin(n)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
