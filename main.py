f921bcdb89df56eb93e97ef5f69df62a68227adf
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:00 +0000

    knock team occur butter form

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+print(random.randint(0, 100))
