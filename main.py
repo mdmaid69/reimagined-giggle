910210636d0bff648fa2e776f030bf5324f8a564
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:42:55 +0000

    ground Vv book exciting possible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
