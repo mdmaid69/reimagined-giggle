c8c39404cd2f5fb054a74a9dc61578bf0bba8c74
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:18:07 +0000

    convinced without result embarrassed dry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import sys
+print(sys.version)
