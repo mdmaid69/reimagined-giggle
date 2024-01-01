7ebe6ac29f0a899e331904b68c8fb7a17601d01e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:42 +0000

    control contrast lean similar order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
