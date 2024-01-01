4f87a08650469f84dcd4474de5d336bb86ecab33
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:48 +0000

    as government he although show

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+print(time.time())
