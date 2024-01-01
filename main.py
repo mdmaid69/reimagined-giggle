3271700a8aca6209ee53289d75b2bc1ccb0087e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:47:42 +0000

    since absolutely moral break sense

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
