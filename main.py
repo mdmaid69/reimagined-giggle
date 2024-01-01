3ec9d2472ac57e6579f51761db06f8884109af85
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:14:31 +0000

    569enjoy improvement consider vision bend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_max(numbers):
+        return max(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
