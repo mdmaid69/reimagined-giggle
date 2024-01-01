c42333331b7e33a8dea6768a8b1af7a5f7d5c6eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:46 +0000

    person anyone clothes thing noone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
