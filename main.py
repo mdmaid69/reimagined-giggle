16084b28cfbffc0b7555e7be3bb3c397f6b0408a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:51 +0000

    old car wait main tragedy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
