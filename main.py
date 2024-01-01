81dc6b24bd5f030bb1ad407533416aac8eeb1720
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:59 +0000

    power snow school serious mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
