ba0ac6da22cd92cddeacb0628a8b0b13f314d090
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:34 +0000

    nerve bet so gradually small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def remove_duplicates(lst):
+        return list(set(lst))
