910171455b3d3ba55ae83cacdd5b92b3926b1af8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:26 +0000

    trade arrange introduction flood knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
