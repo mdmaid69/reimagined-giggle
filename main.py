d0d878084656d6107ab84083a51b959731397671
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:05:15 +0000

    weather once walk dangerous decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
