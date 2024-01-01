243b60277a122bc686eabad5aced3a36c5345766
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:06 +0000

    instrument top will private culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def reverse_array(array):
+        array.reverse()
