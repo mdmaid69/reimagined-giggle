a487a7aa5bf8922c0482d19a8fc29fa7cfda5cdf
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:45:00 +0000

    calm string punish swim second

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import sys
-print(sys.version)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
