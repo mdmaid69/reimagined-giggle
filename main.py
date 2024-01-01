62ab24e62ce9eb142998d2069c601bc853cf5717
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:31 +0000

    month ring next weak allow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
