444fbb5a7c9546294dc89507fa362c7619a2abe6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:50:20 +0000

    steer raw flight traffic stop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
