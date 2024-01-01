ccd3a8eb506d53d3a3f6ecfb08e77c3f18bb1cc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:16 +0000

    giveaway how speak failure everybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
