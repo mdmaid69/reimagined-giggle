3a8b0779b4e11fe7056c2261c9c0472ba5fb9f8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:06:01 +0000

    height male quantity tone worry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_pressure(force, area):
+        return force / area
+  def sort_list(lst):
+        return sorted(lst)
