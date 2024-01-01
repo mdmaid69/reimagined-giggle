879a0728b8e4ac5fa035f5710ff4bfccadb1acc8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:44 +0000

    breathe attitude unhappy rich lonely

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_acceleration(speed, time):
+        return speed / time
