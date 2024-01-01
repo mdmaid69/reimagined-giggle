2fe0d2adc1cf00270348240028ff36a236327f10
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:55:19 +0000

    inquire used room admire point

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_pressure(force, area):
+        return force / area
