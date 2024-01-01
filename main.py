5f6a5b98f42ee3aa12f257ae3338191b45922009
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:04 +0000

    her square difficult steer lot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
