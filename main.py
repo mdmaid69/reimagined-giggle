407448044a025c233e60f8aa40d85e3f1466c585
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:29:18 +0000

    convincing key dance surprise shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
