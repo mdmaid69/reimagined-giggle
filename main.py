3c6d8b748bb66336f64896a712cd3d5c0cd81d68
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:48 +0000

    please buy sign peaceful getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_work(force, distance):
-        return force * distance
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
