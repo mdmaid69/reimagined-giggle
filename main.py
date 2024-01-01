6c92c3215451e6ee94377d8b53fd18aea45a8358
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:38 +0000

    around polite floor defeat emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
