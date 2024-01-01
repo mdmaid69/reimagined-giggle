b38870d9d4b761ec0d4720d91c42530d36c8a759
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:22:23 +0000

    neat peace bread Uu than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+print(re.match("h.*o", "hello world"))
