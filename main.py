492b6ce2d41c9ffe6e348072153295ed5928fa52
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:10 +0000

    include oil likely tone repeat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
