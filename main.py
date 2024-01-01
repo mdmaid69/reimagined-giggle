6c49ce71ee30d0284c7e3d851b0a557cca7045e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:19 +0000

    my pretty kind live show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def is_odd(n):
+        return n % 2 != 0
