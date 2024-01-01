cd71d26fb5a816bc07d7620bc86e8f0c04b31515
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:05:59 +0000

    replace trust wait street confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_tangent(x):
+        return math.tan(x)
