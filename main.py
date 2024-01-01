a6631a2bd83fd32dfbe60e0a49e6389e1093a256
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:59:38 +0000

    surprised trouble split search effort

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import datetime
-def get_today_date():
-        return datetime.date.today()
+print(datetime.datetime.now())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
