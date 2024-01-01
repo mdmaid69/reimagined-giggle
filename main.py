b63eecd2d5e8fad3ccda60872376563ba1ee2553
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:30 +0000

    catch certain as invite rise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import array
+def get_array_as_float(array):
+        return float(array[0])
