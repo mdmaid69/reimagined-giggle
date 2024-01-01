b9cc448eb7253eb24e20d16085d82521f04e3aa1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:14 +0000

    group Kk type passenger use

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def reverse_list(lst):
+        return lst[::-1]
