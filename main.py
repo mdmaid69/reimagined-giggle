1fbd554d030f96f8edb51d4096e05e48ef950e78
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:47:07 +0000

    choice laugh behave official wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
