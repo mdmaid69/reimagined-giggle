291d6c86d4cb400303d5a8360cfb1ea03b30ea53
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:31:12 +0000

    well exaggerate hide raise recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
