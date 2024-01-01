0f69fbeae93c40c2e01e12970342832a003aa502
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:38:00 +0000

    cool send breakin doubt Oo

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
