891616705f80838b0616a9e5746c1e7fc9af75a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:37:14 +0000

    behave smell control badly play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
