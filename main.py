e798b0daba356ccbcdb136b2e1520345c7e04896
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:15:50 +0000

    judge slow catch each main

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
