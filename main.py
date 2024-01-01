09c3b376ee7890999a91deebce2702435e940d6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:06:31 +0000

    fetch yes fact whistle pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
