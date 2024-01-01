db6093eca3018d19d9bb76906fddcbeaaa80a938
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:29 +0000

    speak occur issue laugh declaration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
