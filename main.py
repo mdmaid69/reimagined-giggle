57017333964e34562ec9edc3ad46ea398bd20c50
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:58 +0000

    near unite reliable agree sign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
