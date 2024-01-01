0d82037c8bf5b76461e2efbb902cbcebcd59b4ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:23:39 +0000

    divide workon second lesson sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
