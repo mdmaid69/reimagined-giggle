9777d4d49986272e932852ab7279a77fbbb37365
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:29 +0000

    line original neither geton describe

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+print(math.pi)
