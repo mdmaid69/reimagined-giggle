b9f21ae89a2fe02ae4f2637a594213185b72599d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:15 +0000

    since please whichever show furniture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import datetime
+def get_today_date():
+        return datetime.date.today()
