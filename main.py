5944397b382303328c157bc96d406d821553abaf
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:46:04 +0000

    city get treatment enthusiastic flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
