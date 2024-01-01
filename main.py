960814e2464c8583885d9a76072204c390f3111a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:29 +0000

    answer next constantly workon dick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
