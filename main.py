4c2e240a55380b61f7a7a81bcc85a8c0359d8021
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:43 +0000

    pleased someone defend cheap special

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
