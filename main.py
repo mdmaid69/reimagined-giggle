a0665a5964557fb411993faea0f1ca5ee2f95a1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:59:35 +0000

    male flavour opposite feed so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
