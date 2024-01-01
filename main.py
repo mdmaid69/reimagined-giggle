b6038dd5fe8f6e57ec4c598d723ecaf9bdd1dd6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:23:09 +0000

    seem workout describe violence benefit

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
