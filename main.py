14a6f941b497f394bcb9d3907b02f41b6f425dd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:16 +0000

    lookback(on) tie favour name arrange

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def reverse_string(s):
+        return s[::-1]
