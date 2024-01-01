0dcdb31078e9223c182803c418803b486b8b9489
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:24 +0000

    treat fight contribute body minority

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-text = "Hello, world!"
-print("Characters:", len(text))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
