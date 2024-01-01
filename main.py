e8d28b087be4d1e3a41cc613ab246831a4ab9dd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:59 +0000

    attractive relax pull opinion recommend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  def is_even(n):
-        return n % 2 == 0
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
