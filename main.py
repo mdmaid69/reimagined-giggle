2d7b16cb7db58464e3b1fbac0e026c2302d23960
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:06:29 +0000

    study extremely shop connection hurt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
