86bfeaddab020d39fee784dd075bc7f4dbbd26d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:31:07 +0000

    wet lie regional can unusual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def reverse_array(array):
-        array.reverse()
-def divide_numbers(x, y):
-        return x / y
+def get_array_as_list(array):
+        return list(array)
