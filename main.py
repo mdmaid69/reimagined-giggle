ef3a99cf0278be62ee851166d65bf27b109a2c79
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:38:26 +0000

    television proof punishment reject convinced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import math
 def calculate_complementary_error_function(x):
         return math.erfc(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
