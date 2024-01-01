4e4e850564511d5d4fb1ae56c84ef622dbc0bbe4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:44:17 +0000

    reason vanish difference decide free

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
