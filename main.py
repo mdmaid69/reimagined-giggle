18043614a87b1194f0a9beb346350df43b32c309
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:02 +0000

    side teach Gg away ought

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
