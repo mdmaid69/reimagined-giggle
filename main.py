4af70366b8f66ed267cb96baaf1cc78cf1b80ebe
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:31:09 +0000

    disturbing 571enough question bag tired

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
