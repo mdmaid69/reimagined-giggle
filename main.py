4f05e8f12a9913969e0298dc5733e1c4203cfdb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:59:19 +0000

    temper achieve behind finish support

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
