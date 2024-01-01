5d1dafb0ab61dfcb83994fde9ad46c5dd409ba3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:54 +0000

    back argue decision consist influence

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
