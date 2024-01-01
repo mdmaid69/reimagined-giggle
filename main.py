03397dabc58e551164dcce1a5e369abf62d2896d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:55:16 +0000

    immediately anyone cruelty them bank

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def create_counter():
-        return collections.Counter()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
