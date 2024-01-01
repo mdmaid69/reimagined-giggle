b6d6069b523ed72e464f80668a1d71dc6b7be4f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:29 +0000

    recommend apparently lost swimming love

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  def square_number(x):
-        return x**2
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
