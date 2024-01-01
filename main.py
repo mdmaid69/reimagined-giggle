9aa86f2388d4f8edb4ca79a4c8c5a12d606aa95c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:05 +0000

    speak control emphasize hit through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def convert_array_to_list(array):
+        return array.tolist()
