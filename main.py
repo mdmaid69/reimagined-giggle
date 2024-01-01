784f083117010d75c15cddea68fc83f0d26f9926
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:35 +0000

    characteristic reply her goaway getaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def convert_to_binary(n):
-        return bin(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
