bb59199641cb82563c71f48b985acf0097133b95
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:26:09 +0000

    circumstance slightly special already mood

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
