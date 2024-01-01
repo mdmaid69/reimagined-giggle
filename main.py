76f95e35acc343aa5222f63fc92865aabe1a0673
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:00:15 +0000

    wrong choice lie pick completely

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
