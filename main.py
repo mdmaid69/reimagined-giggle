b12954cf81a1cfb22927bcafa2721315c36496c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:13:05 +0000

    mind thinkthrough continue notice poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+print(sys.version)
