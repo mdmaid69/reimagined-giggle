12be41c3c585500e3683db01526afdd4b669e40c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:10:59 +0000

    successful upstairs us moon hurt

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
