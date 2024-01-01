3c97be8665943d9a7609bde1206e65c5e541cedd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:26 +0000

    eager manage no pleasure table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
