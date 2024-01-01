03e7989002260f2983492ef12606bafd2a13bad9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:37:31 +0000

    why development night worth ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
