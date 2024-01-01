1935ad398386b5e0ed726f55a4d5598da4bb02b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:47 +0000

    second look sign any arm

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def add_numbers(x, y):
-        return x + y
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
