f1f9a1af7ae2543a26b9d62fbc558b4eb61647ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:55 +0000

    birthday kill obvious description against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
