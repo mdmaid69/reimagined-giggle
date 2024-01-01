aa892bd98ab1d51460c3de665a5bd56cb4b77ad3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:08:45 +0000

    valuable abroad relative past up

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def reverse_list(lst):
+        return lst[::-1]
