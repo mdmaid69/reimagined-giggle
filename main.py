dae33ec3e29b333e3aa1eaf27e5d19d5b2c3442d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:52 +0000

    should uncertain steal colour public

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def remove_from_array(array, item):
+        array.remove(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
