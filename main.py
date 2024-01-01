194b6b54f24c03de6f38d983e0e5357a31978a3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:09 +0000

    wonder law home thorough play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
