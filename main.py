afe89cdff6eb8869068a0009c314f50e83e5fac9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:02:54 +0000

    quick talkinto square late shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
