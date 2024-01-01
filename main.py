55539f2f46bf0461eef82fe8dee1fbaf8414b9bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:07 +0000

    factory begin after would sick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
