ca0e00b48d14f9a6e55f3080f8ca5a07583f6e40
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:34:11 +0000

    rare properly process entrance allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
