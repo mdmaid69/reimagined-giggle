c70ae2942e55704f00226b342bc3ab9efc26e2c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:46 +0000

    cause less base lock unite

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import logging
-def log_message(message):
-        logging.info(message)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
