dc7153ca6a9468d1d650074cfb59f1fa68563a97
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:44 +0000

    acceptable shallow play number grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
