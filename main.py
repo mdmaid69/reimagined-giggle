272b3084bd583f8c534051b4dbaa19f0915c91ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:29:05 +0000

    although away copy press deep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_min(lst):
-        return min(lst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
