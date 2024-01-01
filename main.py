1d2ff70c861208cdee3795e8e5db3e47d375f5a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:04 +0000

    convincing off unhappy nerve race

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
