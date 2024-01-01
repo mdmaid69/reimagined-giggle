c8a5ed3508a1530ffe77a8da657d9fce437917f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:57:35 +0000

    talk declare Bb blow change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
