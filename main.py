f313af723e91d180c6f18d9cc6dd40a126820bef
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:39:54 +0000

    confused fetch welcome blame space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
