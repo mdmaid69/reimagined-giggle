ec7f71bf5acff99b34d110e13950d963567a707f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:01:31 +0000

    violent like lose destroy related

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
