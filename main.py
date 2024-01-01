0662895bb155c6d0f8873da88b099c6473f546bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:26:25 +0000

    only figure best cross foot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
