1b65ba26934e9bf3fb9e6d4a57fdec7a73ddc575
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:16 +0000

    the deliver bill sort continue

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def square_number(x):
+        return x**2
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
