a78be56c3dc0d2f35e70445b58bcd8e2e861f35e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:50 +0000

    recover emotion stretch start height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
