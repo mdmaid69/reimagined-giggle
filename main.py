eb15186bf61e9be3e02f9d771833da1a2d303d60
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:12:43 +0000

    bright capital raise centimetre load

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def subtract_numbers(x, y):
+        return x - y
