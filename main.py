84637add6c8d005b645d5de2600349813ecf3993
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:20 +0000

    listen loose attach office whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_pressure(force, area):
+        return force / area
+def calculate_work(force, distance):
+        return force * distance
