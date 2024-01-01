962877578c98c33692c7a91e9779125c3feb9dd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:49 +0000

    freeze rise guilty remain absolutely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
