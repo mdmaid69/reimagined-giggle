9a4786be83540b694086fc393d382d1a3c2a29d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:22:45 +0000

    lookafter shrink pool joke strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_density(mass, volume):
+        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
