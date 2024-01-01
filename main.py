ddc90fffc56490e49a8e34da4d67cd68c13d580f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:23 +0000

    contact compare doubt clue bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
