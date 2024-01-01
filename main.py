f411bb4458ae422d5a5c964487f78bf7a067effe
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:32 +0000

    praise care brain pollution cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
