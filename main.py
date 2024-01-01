40f754a12a2424230b01b736b66d1890f7575b48
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:19:39 +0000

    small ignore actually sentence Rr

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_current_date():
+        return datetime.date.today()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
