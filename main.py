b23f860652cd9cbbcc0a69f463bb52333f237c65
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:58:46 +0000

    single over correct smoke future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_itemsize(array):
+        return array.itemsize
