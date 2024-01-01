63714f2d6850732d52a429a6b663f4750dc7f24f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:38 +0000

    fear great central afterwards lock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
