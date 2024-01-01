2f52a91e899114ec0b1c4621ab3304271425f235
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:42:50 +0000

    whisper score follow experience would

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def clear_array(array):
+        array *= 0
