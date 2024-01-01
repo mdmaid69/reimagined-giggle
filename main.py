3de99c1d68d7f0c19d8f7c86d56d97012024aef7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:02 +0000

    scrape blow store expect start

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
