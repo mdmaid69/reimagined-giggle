f5bd3330d1660477f8aeeda44c5b71ce2a0963ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:55 +0000

    point stamp close wire theirs

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
