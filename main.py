7ccb3ac5020d5ac167731f74f71cdb2162d85225
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:53 +0000

    tool if obvious relationship to

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def convert_to_binary(n):
+        return bin(n)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
