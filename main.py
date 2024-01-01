a4b5c897716c8fa227330b16e3def1d0bb9eb631
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:26 +0000

    allow start surprised thorough laugh

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
