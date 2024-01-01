30bdea120ea5620009142d322aa57399c3e2e5e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:26:50 +0000

    score stone spin finish gift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
