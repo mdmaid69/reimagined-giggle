4a6782ebd9b099cd55000532d67fb43392b804eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:50 +0000

    eat style now previous computer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_base_name(path):
+        return os.path.basename(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
