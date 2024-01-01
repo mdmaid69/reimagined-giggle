7b270712f6af021f71ec9bf9c3f156680dad754e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:16 +0000

    middle goup may clean odd

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def insert_into_array(array, i, item):
+        array.insert(i, item)
