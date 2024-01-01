1dea91eef3da17c72e7898d59a6d92ae81ff53ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:27:53 +0000

    interested university reject organization among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
