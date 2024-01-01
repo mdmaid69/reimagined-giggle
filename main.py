77c8cf6f5680df0abc117bd2c8ed58ff678b2b63
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:58 +0000

    light effect fence content union

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
