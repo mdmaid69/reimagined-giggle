481679efd0c5eeaa9e8eeb6e3d9a2d7f05f9a333
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:14 +0000

    close invite flavour post stupid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_complex(array):
+        return complex(array[0])
