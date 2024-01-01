f1eaa7066a4eff8a6eefb9c447c634a80ac01490
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:25:00 +0000

    plan anybody runover already cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
