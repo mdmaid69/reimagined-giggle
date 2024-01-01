4b31cec59ba1cd572f7ab76979350d045c006b0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:33:23 +0000

    crisis under label enthusiastic city

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
