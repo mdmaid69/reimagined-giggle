a64ed63e170ae1e928f02f60910bc70eda17f78c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:57 +0000

    can clue return spring many

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  def is_even(n):
+        return n % 2 == 0
