24ebd544ba88d779ff55368312ffa4bd03601c32
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:41:08 +0000

    wrap comeback reason transport lock

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
