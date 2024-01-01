0a8bc06de915aa173eaebf1401f62e4157d13ea3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:35 +0000

    point clearly may write season

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
