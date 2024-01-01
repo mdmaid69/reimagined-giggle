21e8b02d3468094a2a056410ef7ff703b6fbab15
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:58:56 +0000

    shrink keepoff smile talkback exciting

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
