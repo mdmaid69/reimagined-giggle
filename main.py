3d93653689221a5e17f84a309cc3d304eed690a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:33:00 +0000

    curious anything bread war clean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import re
+print(re.match("h.*o", "hello world"))
