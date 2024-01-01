65b7acdd67db62f96f7c0b08f2a74a9382d57233
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:38 +0000

    busy simple flow bite quarter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import os
+def remove_directory(path):
+        os.rmdir(path)
