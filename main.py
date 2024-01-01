015373c2e0e82ff9acc51d99cc05aa79ebc3a358
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:42:43 +0000

    rush probably who board description

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
