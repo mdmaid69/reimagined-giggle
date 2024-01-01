445331829e9e163660d7c4dfc0b625e863f72e52
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:55:59 +0000

    571enough inside field frightened scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
