8274d1fd9be20758c7bd5bb79bee5ca74dcaf8a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:20:57 +0000

    direction machine compare Gg national

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
