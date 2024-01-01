5bbebbaf2537a361bcff76a1527fe67f642609f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:00:01 +0000

    pause own touch allow rub

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import time
 def wait_for_seconds(seconds):
         time.sleep(seconds)
