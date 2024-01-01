88b63e6e0aa40346596dfa64d4458cd710252090
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:06 +0000

    rarely ticket war giveup talkback

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-def calculate_work(force, distance):
-        return force * distance
+def add_numbers(a, b):
+        return a + b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
