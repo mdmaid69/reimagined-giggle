f4aa3a5c5b74c9769bdb42a8a17ec11a99c03977
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:27 +0000

    copy take copy improve tragedy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
