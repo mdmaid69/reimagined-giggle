7a36acd785bbb901c0e3af88c00bd9438c3d24ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:40:33 +0000

    Bb regret cut mistake below

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import re
+print(re.match("h.*o", "hello world"))
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
