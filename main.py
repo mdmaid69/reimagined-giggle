881ae5db6c3fa8887058f78bc698c01ef2ed2a04
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:17 +0000

    slice exercise provide last any

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
