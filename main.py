07b9d3e125a415767f3d4d1c38edff8a7c435ba2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:57 +0000

    workon worth fact show backwards

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
