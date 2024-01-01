d5270baa75a7248202fcb806db21562f1ca9b05e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:42 +0000

    swing church joke careless often

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
