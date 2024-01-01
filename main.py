eea8f65b8294f1191ee4198bddb05204fa82caca
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:52:11 +0000

    practical usually buy schedule question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
