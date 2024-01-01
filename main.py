cebec129c73f3defad97ba2c4387364e2d0cb50a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:53:17 +0000

    usual land definite workupto fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def get_array_as_float(array):
+        return float(array[0])
