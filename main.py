05dbbbb912b22328b350ecc8c5653dc3e71bb12e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:13:35 +0000

    defeat doubt desperate tonight thought

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
