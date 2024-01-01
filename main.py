1f69fdd47a4dbf89ab58d42dcfaf284e5194a9a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:14 +0000

    pipe since legal test clue

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
