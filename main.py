4d0147d3cd1c05e2004605d14adfaea2f11fad88
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:53 +0000

    so bread winter drink appearance

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
