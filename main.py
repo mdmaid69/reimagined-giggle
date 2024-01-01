8376d9078cba4c54dd24c02928d1cfa39892932b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:43 +0000

    niggaacademy occur unhappy proud necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
