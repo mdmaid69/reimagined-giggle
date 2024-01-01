ee98964f95b3bcae079e763ac2ce6ddcc987ca74
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:12 +0000

    fold doubt without much steep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
