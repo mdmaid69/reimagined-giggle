23fafacd4e690833d09c065839240b8d7b2acfc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:51 +0000

    leg worth plain various downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
