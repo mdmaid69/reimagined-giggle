481aace15dff3cb61aa5fead58eabbdcd4697f3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:32:43 +0000

    branch ride type unhappy light

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import array
+def get_array_as_int(array):
+        return int(array[0])
