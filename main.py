9c184cae8d3294997cce717f08f3e697264fb44e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:05 +0000

    their criticism Ee spare vision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_words(sentence):
+        return len(sentence.split())
