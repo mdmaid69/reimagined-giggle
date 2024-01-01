a2c488c264b4aeaa8a7569c5588e03ee7c3cc241
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:04 +0000

    before dance late impression eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
