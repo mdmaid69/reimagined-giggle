eb96c2333720bcbe7406547085da95111eb6e705
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:56:43 +0000

    capital pause interview yours pay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
