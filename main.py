8a8bf0e557584ccbf32f74c91f6bffd23d612025
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:39:19 +0000

    tear scare description pile scream

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
