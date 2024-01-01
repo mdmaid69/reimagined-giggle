db45ffb9fb313c5a2a66f035425cca93f2abad5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:36 +0000

    takeout recovery practise level stage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
