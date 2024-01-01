24b9e7a2aad79eac309cf303c284bf5041e27dbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:47 +0000

    contact phone long hate cook

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
