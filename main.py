722f8d52ef7cd760cf234922949666f19d7ecfbb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:36:13 +0000

    steady kill uponon maintain talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import getpass
+def get_username():
+        return getpass.getuser()
