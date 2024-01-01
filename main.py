caed069b1fdddfadeba1d7491d45768133d7d306
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:39 +0000

    either unpleasant family roll shock

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_speed(distance, time):
-        return distance / time
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_force(mass, acceleration):
+        return mass * acceleration
