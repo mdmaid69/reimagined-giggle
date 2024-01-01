6f5813cd7edae407dd386f954611b851d482baf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:38 +0000

    bound point somehow dry content

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
