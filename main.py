4f7c5736166f4a8a1e991c1f75ddba18fc8445b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:33 +0000

    both piece division noisy desperate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_set(array):
-        return set(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def reverse_string(s):
+        return s[::-1]
