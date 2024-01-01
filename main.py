41b936f48366645c8c19831865ac46e9c7b4c34d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:52:28 +0000

    attempt translate ask any rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
