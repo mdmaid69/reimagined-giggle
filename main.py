540ea3c9f4fa909d56c78bfe30b09fbe34236a7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:36 +0000

    roll expect Mm light Oo

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
