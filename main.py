b72227a2405e8f4900c38d3fe8f6e32f12eaca79
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:17:41 +0000

    before pretty organization undo length

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import platform
+def get_python_version():
+        return platform.python_version()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
