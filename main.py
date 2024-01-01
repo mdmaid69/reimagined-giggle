d9f59002dc87c40a456c82d80816612f41b2d64d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:37 +0000

    both store wait influence usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
