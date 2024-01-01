1167173d6426d45470f96d980394b6cd408d8d83
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:37 +0000

    tap march pipe keepaway best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
