497b9e177353fd6318741ee319d058d51b3434bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:06 +0000

    past defend shake wall mass

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def find_min(lst):
+        return min(lst)
