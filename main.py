83c08cf82f75bab843031d762d839c92e5398110
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:41:35 +0000

    whatever talk wind butter deliberate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_cosine(x):
+        return math.cos(x)
