3c73442fcf68df6f619b3cc891aa3b2b5b7f40a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:47:30 +0000

    direct woman gooff takeoff suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
