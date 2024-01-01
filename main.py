f14cc44df603e0ea5b603b15cf61835a89317a41
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:04 +0000

    hardly note my detail turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
