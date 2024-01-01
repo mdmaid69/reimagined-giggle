ec00e63c6740f275b7344bc1b636e99dde155c26
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:56:41 +0000

    tip every each slice safety

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
