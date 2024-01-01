8a2a71c76bfc73a83dbcc334b87c57c0b9ed4eee
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:09:52 +0000

    pause mix lucky swing action

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
