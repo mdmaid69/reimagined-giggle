cc335ae30bdff4d84389f2a8751a16fab8d86f4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:00 +0000

    also foreign body limit estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_combinations(n, k):
+        return math.comb(n, k)
