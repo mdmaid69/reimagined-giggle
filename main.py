66c6e7b20d8727467b0f3e2a88f7c641d8b8caf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:12 +0000

    original Zz capital book after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
