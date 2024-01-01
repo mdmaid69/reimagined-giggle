c445e4876837a537624797c88ce7cb5dcafc1650
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:41:18 +0000

    than exactly catch star sensitive

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
