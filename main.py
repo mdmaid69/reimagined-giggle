2a15123b8406ca6ac07ece20ca8165640c59762e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:02:51 +0000

    performance file immediate luck pool

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def sort_list(lst):
+        return sorted(lst)
