2769ab9aa435ef5a9e2cd31487d7ccf1be61f15d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:28 +0000

    comein strength talk model contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
