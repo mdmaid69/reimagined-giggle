37b1c4d25021215d22f2a92c19675540fa379b4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:17:15 +0000

    turninto easy tooth admit anything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
