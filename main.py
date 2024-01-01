2d87c0f4e6c25a8dbc737e7e89a777531314dcc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:23 +0000

    afraid file partner history education

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+print([x**2 for x in range(10)])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
