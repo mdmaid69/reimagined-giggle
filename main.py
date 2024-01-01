0005764a0e4a2f391342d0d518d95d12e5afb1f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:43:26 +0000

    artificial break artificial steal mend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
