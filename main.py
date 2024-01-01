e0c84eb34f9e750e3d820feb927f9b19602f2233
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:30:23 +0000

    score sort unable pronounce nearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
