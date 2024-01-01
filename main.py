e8ad6120f83226f0baef879d0083e87ef5e545ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:54 +0000

    over late Qq bend hurry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
