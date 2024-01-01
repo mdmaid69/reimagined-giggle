06a249c9e9ab90aabd5c0dfd4dad32786cea2824
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:40:46 +0000

    getoff disturb badly connection outside

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
