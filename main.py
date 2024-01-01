8a98f976c68e5bc842fba07586e146391934c6f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:40:29 +0000

    rise dance plant shop set

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-for i in range(10): print(i)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_speed(distance, time):
+        return distance / time
