6ca72fd052854d3dc45391e325c75bb34c2a5aab
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:04 +0000

    extraordinary spill impossible criticism feeling

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def count_words(sentence):
-        return len(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
