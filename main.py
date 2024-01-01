6dfa41b373671670ce79b41c3b9cda362921d787
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:18 +0000

    brief fire spread plate real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
