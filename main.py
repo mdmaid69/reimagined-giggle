5352be3fbbe125f5b839738509a3060b6bafcebe
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:03:37 +0000

    Ff slowly wheel amount bright

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
