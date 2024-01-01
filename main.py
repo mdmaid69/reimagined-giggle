d3ae0cf18939956d6d328e2e04c4212068c2891b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:53:43 +0000

    any game now town spoil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  def convert_to_binary(n):
-        return bin(n)
+def count_elements(lst):
+        return len(lst)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
