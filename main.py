b8866fb30d088a378233e93d5ec0aa721de97b20
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:32:34 +0000

    emphasize nurse keepup stiff intend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
