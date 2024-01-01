14adf8cbdb6f5207ead64621d022600186d69b96
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:24:47 +0000

    judge turn pause fall important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def reverse_array(array):
+        array.reverse()
