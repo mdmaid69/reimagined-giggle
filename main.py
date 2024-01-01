0bc62af73d0406a4ee6b6abd46414ed3512efad9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:09:53 +0000

    cheek matter mail second dig

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+print("Even:", [n for n in numbers if n % 2 == 0])
+import time
+print(time.time())
