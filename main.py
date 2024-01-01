2ea4e4feffc4b2459dc68ddcae09d8c515536f62
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:18 +0000

    teach practice absolutely square official

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_acceleration(speed, time):
+        return speed / time
+  def convert_to_octal(n):
+        return oct(n)
