96e70266c8bdc102bba09b6ce3dabaa2c31e485a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:34 +0000

    reaction fear comment profit power

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  def convert_to_hex(n):
-        return hex(n)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
