7617e97306a765227af75e5839ed5551f73188b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:24 +0000

    mistake term believe balance generous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_pressure(force, area):
-        return force / area
+import json
+print(json.dumps({"name": "John", "age": 30}))
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
