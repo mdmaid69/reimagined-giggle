e66107e82b695dbfe2fcd96b6bbe91ecdf44dd65
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:48:15 +0000

    young water clear period land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
