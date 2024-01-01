456022ee2cac6aa96167d2779ed12b23eb25f858
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:38:16 +0000

    hold authority upset ours fall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        return sys.version
