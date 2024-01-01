c77bb36ddaa412236c0167f623d97b06b81898ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:08 +0000

    medium distant practice replace pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import array
+def get_string_from_array(array):
+        return array.tobytes()
