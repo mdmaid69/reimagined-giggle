c8fa03614a2d9c45d6e4f81800d03f5dc09fc583
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:27 +0000

    566end here without weekend mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
