1c7375fa075f76c3aceddb255fbf7dc904ce5266
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:15 +0000

    try prevent clean equal music

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
