ee32ead8183ddce8ed3ecb9e44166374b3a0bf42
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:41:28 +0000

    exit sure pity culture contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
