92319015b29951b2a961de1a9d84d4dc2f486e9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:34 +0000

    worse think software ago number

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import sys
-  def get_python_version():
-        return sys.version
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
