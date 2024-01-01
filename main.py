ac5bff24fcc42c3a3976a686de1ca87b32c61f85
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:11 +0000

    score clear level smile add

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def is_odd(n):
+        return n % 2 != 0
+def is_palindrome(s):
+        return s == s[::-1]
