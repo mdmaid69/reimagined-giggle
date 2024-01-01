7a95de469f144113ee8bc1a8558192661b616c41
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:27 +0000

    black hurry partner couple fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bool(array):
+        return bool(array)
