840fe12ccdfcee37ab972b1c083fb3995dd12b0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:49 +0000

    black arrange understand mainly miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
 import math
 def calculate_arc_tangent(x):
         return math.atan(x)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
