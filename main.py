5ce93ab93ce375ca19bb0c4ff70d7477e4ec5a7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:39:44 +0000

    tough feed pretty spring plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def count_elements(lst):
+        return len(lst)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
