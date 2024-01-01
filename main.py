31ae1277e3c5dd8709a357b3fc33227ec5ccc7a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:48 +0000

    less continue wrong your forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
