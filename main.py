b068182ef3a7e008eff049f426385d850de056f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:06:25 +0000

    comein shop transparent black pull

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
