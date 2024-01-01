34af8c569b67313c823ce2af5e2e58df7b60fe3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:06 +0000

    secret grade beat deep smooth

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def find_min(lst):
-        return min(lst)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
