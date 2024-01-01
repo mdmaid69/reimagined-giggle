b6a7f1a18d1c6f9e0fff88cba4238f437dcfb001
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:50 +0000

    peace unhappy greeting expect happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
