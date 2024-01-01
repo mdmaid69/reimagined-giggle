623d1d7b3bb1395dd16a3182a736c4da4f617b79
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:48 +0000

    voice know direction blue ilovedicks

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
