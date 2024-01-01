9a4a9de42029891831e01af7c49effb61f426774
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:32:56 +0000

    material confidence meeting remain accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
