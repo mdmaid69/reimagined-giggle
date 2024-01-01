4eed73a94962da34417411df5e741cf80f4c01b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:35 +0000

    team enthusiastic profit comfortable direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
