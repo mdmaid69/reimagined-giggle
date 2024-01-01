a41614fde73ef448942e7954de6c0b26f3d73a51
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:53:31 +0000

    sound skill break give crisis

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
