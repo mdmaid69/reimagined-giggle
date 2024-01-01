73c8779763068467a9bd08d27d5005689c20151f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:45 +0000

    motion couple keepout win everywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
