8e2b565ced1cf45eabfb1371413b28970d780c25
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:24 +0000

    pretend clear wave address give

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def square_number(x):
-        return x**2
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_user_list():
+        return collections.UserList()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
