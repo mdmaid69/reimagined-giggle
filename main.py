e87ec1792996897907dae0954998f33ab1d04415
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:14 +0000

    dick aim light danger corner

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
