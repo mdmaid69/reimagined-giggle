d9332afe712a3774375e8fa911477ea343a37c12
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:04 +0000

    towards easy compare note face

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
