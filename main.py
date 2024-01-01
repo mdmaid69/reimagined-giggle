ebaf12f277e2509bdde4b7f95f898d29e89c8170
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:01 +0000

    angry wind it more tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import sys
-def print_python_version():
-        return sys.version
+  def convert_to_binary(n):
+        return bin(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
