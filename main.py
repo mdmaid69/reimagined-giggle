9cdfda9dec45969e54832f0df701eaa1eb014df2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:02 +0000

    tell pain mistake gap when

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def clear_array(array):
+        array *= 0
+print([x**2 for x in range(10)])
