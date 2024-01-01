f5596ea56c4e2327f150f3f53b0ef27796347dd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:56 +0000

    nonsense see getaway tear heavy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
