a60bbc70d9515b270e047293bb0933fc4ab15dd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:45:06 +0000

    burst underneath dinner makeup nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
 import array
 def get_array_slice(array, i, j):
         return array[i:j]
+import math
+def calculate_floor(x):
+        return math.floor(x)
