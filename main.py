42aa4c757f3414bcd6bab604f125779530cbf383
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:29:33 +0000

    fight type ability bridge natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
