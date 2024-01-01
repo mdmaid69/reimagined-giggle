a6fa4fb62b2ac2f85c42d8f8d86d0681ea8c1ff4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:18 +0000

    kind total fact shock pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
