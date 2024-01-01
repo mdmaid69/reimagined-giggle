56dedfdfb0877a45c8b7fc4cbaac7ed4e237070e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:47 +0000

    hard he reduction market report

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
