d0cb12e34fcbb88961fa07906c27af1657627909
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:52 +0000

    piece will example round night

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+name = "Python"
+print("Hello,", name)
