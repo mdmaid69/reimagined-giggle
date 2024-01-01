fb63e1999f8f6431bf97504a19a86016add9cd90
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:00 +0000

    wing cheque hardly generally alive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
