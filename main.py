f488f2fc960d87e230380710b7eec53a30596242
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:31 +0000

    talkback strength education total mine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
