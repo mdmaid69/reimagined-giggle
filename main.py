9864ebef0b00907798fc6ebc22c02bf0374aa743
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:36 +0000

    arrangement majority just there urgent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
+import tensorflow as tf
+print(tf.__version__)
