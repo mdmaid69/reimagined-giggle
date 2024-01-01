181b514a23fb681e0140830394adc29ca83c41ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:06 +0000

    must keepfrom flat rich quickly

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def find_min(lst):
-        return min(lst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
