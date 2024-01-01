903c8d9aeb3124ffec6eb2d5bf47d94bf6b311ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:51:47 +0000

    cheat carry once turn expensive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
