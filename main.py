7f44d8aee1a62f6cbd0d7df8c1e1d0d45929376c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:36 +0000

    hate comeback thorough lie content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
