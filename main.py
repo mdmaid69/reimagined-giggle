a88a71cea0eaddb81ee8525aad2d924b12c27456
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:01 +0000

    peace follow various praise dinner

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
