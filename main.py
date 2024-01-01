5fb9df19e1c9e35f3a33b60b76ca6429b659e246
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:52:10 +0000

    trap spread lift education tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
