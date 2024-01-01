62d77f8875e9e09b831915072563fdb0dd93ad28
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:45 +0000

    left beneath on come stupid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
