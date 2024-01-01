78fbea014f17f55022f0280730838c212b7cfb45
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:23:19 +0000

    dance interfere manage drink business

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def count_elements(lst):
+        return len(lst)
