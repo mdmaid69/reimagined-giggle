e623e88813202718e0262260c001f16e7e8cf950
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:16 +0000

    unit more difficulty naturally full

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
