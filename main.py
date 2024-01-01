38e0c4c2a16ac3ba3f9480d4be39cb3cd70cb286
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:35 +0000

    education hurry maximum talkback than

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
