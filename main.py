bc3e7ebfdb7bcd30850a3a927673a1f8960f78ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:12 +0000

    chase towards connected whistle fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
 import array
 def convert_string_to_array(string, typecode):
         a = array.array(typecode)
