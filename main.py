a4764943d961ed51b426b3d6384cb0f67ea0135f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:28:09 +0000

    born upwards Dd forward store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
