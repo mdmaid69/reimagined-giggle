e1f851cc64f94682400427b5ca085e83b3416f50
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:11:17 +0000

    equal ours discussion low trouble

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def find_min(numbers):
-        return min(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
