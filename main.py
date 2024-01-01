b1260b873cfbb45854d34ca77b49b1d4dce19ffd
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:50 +0000

    person prove thinkof importance minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
