6761983bd7e603f3989005c285d14df004fe56dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:01:52 +0000

    behind takeback guide bring war

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
