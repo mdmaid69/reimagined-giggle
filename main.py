1ce10d9d58ebcbb8c71fefe658f989d25874a74c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:34:14 +0000

    accident turn generous go vision

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def is_even(n):
+        return n % 2 == 0
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
