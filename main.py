d0d5792e039c1b17b76b9c7c208ba0fbdaf52b7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:55 +0000

    television keepfrom damage contact among

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
