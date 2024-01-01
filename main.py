4c84eda5c6e671aa3c7a2091f1c06631fe91f5df
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:01 +0000

    poor wise software month various

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(numbers):
+        return min(numbers)
