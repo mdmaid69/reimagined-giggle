75a53b847b5235a4a664e0936a59f2979dcaf227
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:06 +0000

    experience about whichever genuine lose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_error_function(x):
+        return math.erf(x)
