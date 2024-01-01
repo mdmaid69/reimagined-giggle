4a77575628d408905b1d6df3582cfadb4ea14dc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:56 +0000

    lean hurt urgent average write

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_octal(n):
+        return oct(n)
