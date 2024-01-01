2f2a180aa0f431fe2d3d8f15b01d8c455303fb60
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:05 +0000

    skin suppose character condition common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
