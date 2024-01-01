7e2fa2dc1626e03abe66b0020491b357d8948089
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:07:29 +0000

    legal month science long hospital

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
