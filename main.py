888924a7376184fa8505c71c63a46ad963f4b38f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:37 +0000

    produce question light afraid importance

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
