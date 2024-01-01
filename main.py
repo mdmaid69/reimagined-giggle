06d21e7ca8fd4171b856ad96c9e4ec781d5242f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:56:54 +0000

    tooth base neither hurt comefrom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import sys
+print(sys.version)
