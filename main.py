3fd5f7aa2442404a78c765f1c2f078a01603256b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:47 +0000

    show knowledge follow rest trousers

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_pressure(force, area):
+        return force / area
+def convert_to_octal(n):
+        return oct(n)
