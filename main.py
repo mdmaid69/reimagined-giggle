42702e811ad53ce591cd8548313aea26a2db0513
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:49 +0000

    basic raise used usual happen

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
