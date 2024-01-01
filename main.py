c1e934966020c08bacd896c459444b61bcb68623
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:53 +0000

    pity work next smile false

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
