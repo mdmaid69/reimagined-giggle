35b416e3b3bd6ef76612d8c9d8360249e32a085b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:11 +0000

    defeat possibly hope piece inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
