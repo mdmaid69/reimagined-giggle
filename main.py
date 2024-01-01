a7e92d2af540895186d1a51dbddc06b7c305028b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:12 +0000

    burn sure tonight this physical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
