feb4ba32e5ae3380c352f295cce6a2667f53a490
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:54 +0000

    involve include takeout price minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
