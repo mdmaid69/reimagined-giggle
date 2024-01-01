cb48c5afa7025795815e664c3756e2a142888585
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:55:14 +0000

    steal basic begin year anyone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
+import collections
+def create_counter():
+        return collections.Counter()
