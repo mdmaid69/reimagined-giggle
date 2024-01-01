aca34062122317cc3e883dbad8bfb9d380a452a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:28:50 +0000

    test chance avoid post flow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
 def calculate_present_value(future_value, rate, time):
         return future_value / (1 + rate)**time
