d50c9083bf8e99a4172f1c5f14792f8efee702b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:14 +0000

    print suppose shrink shrink stage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import array
+def get_array_as_str(array):
+        return str(array)
