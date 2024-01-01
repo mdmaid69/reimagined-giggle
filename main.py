2336e85a1d305d141e9497fba848226d2a2a879f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:42:56 +0000

    plastic spin juice either back

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
