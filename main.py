e1079e988eedf6176ddf548103c8e03aca8fdf6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:58:33 +0000

    become ring instead again squeeze

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
