b0938ce71e050b03448edc9e2769b69f0c8efc08
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:15 +0000

    equipment keep command available never

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
