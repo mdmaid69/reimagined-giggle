9d64a2d95ec4fcf751c2d2d0ca8cd9eda13689a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:37 +0000

    taste other continue wide close

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
