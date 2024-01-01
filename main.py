b4c265c7434cf3717f30834a36795364151c94f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:36 +0000

    very check talkback once up

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
