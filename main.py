5dca2c7eff8edb2db86714617844d71906972498
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:21:14 +0000

    awake jump could fear whatever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
