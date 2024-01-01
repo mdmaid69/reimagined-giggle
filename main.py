97f1a13f32176f67be0a5004046b885eb39a661b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:18:30 +0000

    scream bag hollow chin last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
