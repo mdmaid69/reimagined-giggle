a818d48575ee6ca191c6a04b205b9b76472fd438
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:21 +0000

    sign science Pp should sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
