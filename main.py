55ebd2379ff0b7abe144266bf50d1547712a1411
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:19 +0000

    many should argue pure still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
