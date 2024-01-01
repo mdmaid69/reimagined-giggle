7f74c06e49f1da73e9d1c2e47bda7606550b3024
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:17 +0000

    mess cost concern trade well

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+for i in range(10): print(i)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
