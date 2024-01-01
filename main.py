bf828a762c8593c73958beeb2c79145c5b0186e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:43 +0000

    convinced luck practise together strike

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-print("Hello, world!")
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
