30bc0bfbf91d93bfd0911eab0d3fa09e798faf97
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:28:37 +0000

    kill callout morning over steal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
