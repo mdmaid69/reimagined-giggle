8b24ad7b9ed39f19f406119adcd4b01094002ff1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:41:46 +0000

    produce than narrow any 566end

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
