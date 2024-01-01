9bf07fba6027393a0ed4bf50f0838091e113fa0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:46 +0000

    near deserve smart reasonable town

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
