4af8844599758dcdc9d8a96eb4b7efbeaa5bb073
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:07 +0000

    interruption act stick drive offend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+print(os.getcwd())
