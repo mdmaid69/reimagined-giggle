a7515b5f79396355cf9e50754704eb027d80abf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:05 +0000

    pleasant objection section environment news

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
