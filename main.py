85fa2568f89cdc29236b46c47696936249bbc76c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:00:09 +0000

    put achievement onto legal forget

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
