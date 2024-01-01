82543dfdbca9493cbc2afdf844b72b2172a57e92
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:08 +0000

    repeat rain open position home

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import time
-def get_current_time():
-        return time.ctime()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
