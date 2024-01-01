cb4401c372e6870fd1dbd268dcc859b81b5d4fc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:47 +0000

    turnout judge ground breakin otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
