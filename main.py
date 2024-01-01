e47ebf781184bef20a36db667f06fe90155d31c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:21 +0000

    concern breakup healthy give belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
