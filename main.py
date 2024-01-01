2b71d491e08e30f8118a919d4e450e0185f19d60
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:15 +0000

    score sight Ff violence mixture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-name = "Python"
-print("Hello,", name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_unique_words(sentence):
+        return set(sentence.split())
