2f264b877c2059c254014e56a504655e6698db55
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:09 +0000

    much dead private aware conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def extend_array(array, iterable):
+        array.extend(iterable)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
