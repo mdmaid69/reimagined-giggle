4aad93a738801cfddf50e89a32f71a711344d37c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:10:21 +0000

    teach peaceful mixture store train

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
