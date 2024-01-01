c965b782921bfc5325c5b598429ccb485b82698d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:42:18 +0000

    rain godown effort change independent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def is_odd(n):
-        return n % 2 != 0
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
