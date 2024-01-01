d854b90b20cd8a64fc4b159cbe1d59b106bb9718
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:38 +0000

    downstairs represent about section gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-print(os.getcwd())
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def reverse_list(lst):
+        return lst[::-1]
