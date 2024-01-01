c97314dd808bde47ccf30c7118a9126aa96f7009
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:54 +0000

    process port decision share round

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
