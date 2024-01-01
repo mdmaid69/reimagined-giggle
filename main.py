f30b2dd8e681122b64b17652061f87ea66b05b2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:10:09 +0000

    occur do tap find determined

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_max(lst):
+        return max(lst)
