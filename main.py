fc0162fa287ad0f72068053739f8384307d5f8bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:12:35 +0000

    defend unless pay worry me

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import platform
+def get_os_info():
+        return platform.uname()
