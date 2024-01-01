a77d0021d4a60e6270f8b2906c3bb81e70cf7694
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:19 +0000

    need excellent trap private cry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
