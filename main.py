8c89ff7e256bf8b67cd9c44dc684697f88801f9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:45 +0000

    band conclusion rain owe surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
