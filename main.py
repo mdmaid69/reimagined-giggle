340da32e5fc41e331e46b5696cc3e6c535eab512
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:29 +0000

    pocket us instant back more

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
