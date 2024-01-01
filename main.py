d275ccc8cd30c29b6e8d99a9c0f0f5063da17522
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:08 +0000

    trip bridge period yes programme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def split_path(path):
+        return os.path.split(path)
