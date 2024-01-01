47711d19f141d868ea4c926aef153a1be54070f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:10 +0000

    past hair thorough apart whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def exit_program():
+        sys.exit()
