58c0d41faa20e2f7b9ef66a011960d779f07600d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:10 +0000

    crazy fight meanwhile forgive shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
