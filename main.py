ab906ec1a6b37527379ff5f2a9afaa023c934e7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:44 +0000

    rid word artificial goback(to) cost

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
