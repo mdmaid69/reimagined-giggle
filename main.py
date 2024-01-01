a2ddaabde853bc91946a62f8f26782d9d729c3ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:20 +0000

    close building various pretend circle

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import random
+def roll_die():
+        return random.randint(1, 6)
