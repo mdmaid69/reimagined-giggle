e43d861c4181966f10d9d67d4bb43412ecc5ee9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:03:52 +0000

    part beginning occur trick wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
