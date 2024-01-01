31e799e37e0e78a8a09bdf9e24a8bb8cbc22af52
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:15:50 +0000

    bringup coffee careful absolutely takeaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
