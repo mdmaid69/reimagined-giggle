143a38bd4c478241446a0d573bac3e5b99f48553
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:00 +0000

    side discussion permission warning trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
