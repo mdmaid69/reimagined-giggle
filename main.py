91b497eb9470a00ce52f89c22bea575ab7034c97
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:40:32 +0000

    field force mood climb fact

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
