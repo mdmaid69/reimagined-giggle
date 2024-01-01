c53f3c447166b73f9bd5ebeb774cc8c501086fc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:27 +0000

    reply do genuine breakdown star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
