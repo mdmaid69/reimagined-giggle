0cf8929ab02f37dffe36c71a06445657ebb63c70
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:19:43 +0000

    low communicate dry leave attitude

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
