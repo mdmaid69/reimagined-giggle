f947e15a118f6aa79790ca31ace9ba8fa08dbb55
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:59 +0000

    surface discuss print bye job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
