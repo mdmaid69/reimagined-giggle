bd9fb09c3057bc455a339567f4490df0191d2e27
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:32:23 +0000

    turnback impressed programme character themselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-print(os.getcwd())
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
