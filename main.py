5b26b35724700fbad14a571bd84e05891b0af001
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:54 +0000

    underneath noisy welcome comeover second

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_index(array, item):
+        return array.index(item)
