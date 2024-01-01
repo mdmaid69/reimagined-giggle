e1f6db95f5aaa75cd8f2791e1d072eb185990f70
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:58 +0000

    computer trade school mess display

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def is_palindrome(s):
+        return s == s[::-1]
