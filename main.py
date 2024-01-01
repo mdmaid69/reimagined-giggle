81ccdb572bcd75c1e43ebcf910209b3dd1f36c55
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:30:02 +0000

    base unlikely phone trust takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
