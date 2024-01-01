c9dc4a1d6d6f9188b5dd162f15d22c4a5063176c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:30:20 +0000

    prepare own discussion die smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
