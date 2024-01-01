cec3d7de6753aa81aca721187ac0a8a91df2813d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:59:45 +0000

    squeeze catch open note bury

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
