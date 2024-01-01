17488a75723b7b132b007e6cc8093f1924005bea
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:53:41 +0000

    vanish later crisis region female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
