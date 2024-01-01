58df685ef932bb69b4f8d282fcd50dfaea2d41d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:16 +0000

    right surprising complete respect become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
