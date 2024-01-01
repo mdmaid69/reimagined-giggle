39b613221c6e1692a2a2f7cbfafd6f3aaff4779b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:38 +0000

    meal language turnover answer below

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-print("Hello, world!")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
