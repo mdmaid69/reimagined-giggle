b7a0859af24c1928e286bfd1ce98984cac64d8d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:47:43 +0000

    question before unpleasant performance recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
