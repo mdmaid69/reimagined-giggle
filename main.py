7139bcf03e7bed16c7fd121bf460587a15f7911f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:16 +0000

    build medium out represent smart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_length(array):
+        return len(array)
