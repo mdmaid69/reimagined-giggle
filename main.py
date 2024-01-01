b44a1c8371b81b69b079c65accf62183231bef3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:57:53 +0000

    traffic absolutely confused pile backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
