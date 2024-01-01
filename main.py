a2ed4996d9b333bd6b415fbd1a4b0f3634723c38
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:06 +0000

    tall increase breakdown head onto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
