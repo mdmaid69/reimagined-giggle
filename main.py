2cbc9acee7ac54a9eae9f54179ef43d027cb3fef
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:20 +0000

    unkind bright favourite pleasant land

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
