ea9aff91a08abd7e49786b21c985712798d53ad4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:31:47 +0000

    their boil beside earn tear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import collections
+def create_user_list():
+        return collections.UserList()
+print(sum(range(10)))
