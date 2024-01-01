91d0d2fc7d1f4dfa828353e24015a7ac898286cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:08:07 +0000

    through national represent cold float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
