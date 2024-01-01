d3fef39dd68332b9db885a3c398910a556b84af0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:07 +0000

    pipe know neck Tt business

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
