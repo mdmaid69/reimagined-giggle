4c46621db486fd347089687253ecc8abd61007f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:06:19 +0000

    plan funny never comeover mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
