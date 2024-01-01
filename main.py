b9a152d829cf0cbf788cbca7dff293c04cf78841
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:24 +0000

    suck pattern shop thirsty Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
   def get_file_group(file_name):
         return os.stat(file_name).st_gid
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
