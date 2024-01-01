388b3d5cf38846c320b2ec27985670b1e3cc167b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:56:18 +0000

    objection try convincing address remark

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
