7bffecf0db711ee6faa6d708c34ecd2561d29a87
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:59:04 +0000

    relationship exam average especially decision

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def calculate_acceleration(speed, time):
+        return speed / time
