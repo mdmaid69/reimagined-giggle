1cd82dcc234bbe77a4e3d9953d76bb73dd0fa785
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:45:42 +0000

    discuss back limit he nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def multiply_numbers(x, y):
+        return x * y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
