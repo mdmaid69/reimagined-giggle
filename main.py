c817581788a006d9971ea7e61c79fa2b46af0762
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:29:48 +0000

    guess limit base Zz clear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import sys
+def exit_program():
+        sys.exit()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
