d54270c726829fbeb785f5c87d49cd5ad4d6ea0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:34 +0000

    just female pleased careful floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_typecode(array):
+        return array.typecode
