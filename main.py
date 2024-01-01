7a25bae6d5b2e41c407298a195fc818aa93bf70a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:18 +0000

    turnoff while exchange ride possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
