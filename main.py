0f7aed13dd6198f052a46ab21a8dffe1c0274cee
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:37:16 +0000

    experiment sort refer loss goup

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
