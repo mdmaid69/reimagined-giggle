bbf429d418615bdedd3816a866988273d5b312d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:39 +0000

    lost plastic suit talk land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
