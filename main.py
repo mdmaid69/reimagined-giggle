99923a674ba0c41aaa8b870041aec27767a5ec17
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:06 +0000

    careful straight bar niggaacademy risk

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+def find_min(lst):
+        return min(lst)
