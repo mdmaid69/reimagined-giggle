774389b0069a04a1317955c6cb62adfa3ab9668d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:16 +0000

    smoke earn region stamp equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
