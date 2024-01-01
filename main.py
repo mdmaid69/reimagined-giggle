aff3bc23c3a8755929d065ae86952aa68b4081c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:00 +0000

    surprising other interrupt really well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
