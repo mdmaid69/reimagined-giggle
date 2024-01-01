0998c7aaff6430b88abc21193d3f1712a4f54620
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:42 +0000

    regret key prove race base

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
