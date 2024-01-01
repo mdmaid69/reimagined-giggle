abccf472f868301e924edccd154fbb43a054773c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:52:07 +0000

    hope always announce alive confidence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
