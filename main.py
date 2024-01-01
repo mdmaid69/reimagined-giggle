9e6cecf8f00cd5d87c56deb4c4dff93e18909e2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:36 +0000

    television ignore ground contents start

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import collections
+def create_priority_queue():
+        return collections.deque()
