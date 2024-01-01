b22c2c21e29af0a93edd2c99b8d6a4b60db95d5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:27 +0000

    possible so mind contain minimum

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-  def cube_number(x):
-        return x**3
-for i in range(10): print(i)
+import sys
+def print_python_version():
+        print(sys.version)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
