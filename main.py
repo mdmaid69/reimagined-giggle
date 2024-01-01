bdd93815ba1846b60b45590eee02fa4993f70368
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:48:14 +0000

    person abroad sleep travel moment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
