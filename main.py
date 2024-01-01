091000dc894ee9fae4bc2636e6ad607c46f86c0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:35 +0000

    rain affect Cc dry bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
