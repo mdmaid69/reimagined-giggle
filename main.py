fdb5072a5d2c5997cccef03dd9bfad80508c252e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:54 +0000

    cheap direct ride cost getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
