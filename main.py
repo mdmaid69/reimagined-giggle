67845785126acf404a7f20505d373916d8e2282c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:51:34 +0000

    after power straight trust climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_work(force, distance):
-        return force * distance
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
