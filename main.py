ebce5031463c7c3f3f3a92a7f71a94410ce852f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:34:02 +0000

    answer claim half I exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
