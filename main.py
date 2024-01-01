9f42b0d1a6ab8afcdf0dc053a714bc39e3d2ae65
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:20:25 +0000

    imagine yourself imagination will day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
