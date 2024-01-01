f859270911485dc81655e25b2e1d72231206af96
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:27 +0000

    rush should sound secret giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import os
+print(os.getcwd())
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
