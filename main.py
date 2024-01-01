1f044ee49fdef330e7a09cf8cbb6d1efb0be40ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:28 +0000

    anything trip knock necessary relationship

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def print_python_version():
-        return sys.version
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
