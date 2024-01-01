3aa24c26bb8d12479edc8390c5fbc890a90c7142
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:40:43 +0000

    alive marry money grand breakout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def print_python_version():
+        return sys.version
