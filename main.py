22a693285244b5ced0357589ac23ab713e421628
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:09 +0000

    bit bank hurt statement contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_as_complex(array):
+        return complex(array[0])
