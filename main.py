3727a41ffa74d6ea5081879c75c0a21f8f17d583
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:12:29 +0000

    seat point return hollow silent

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
