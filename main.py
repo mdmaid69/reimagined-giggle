8cf9360ab7ec851ab251de44919396c46cfca2cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:14:01 +0000

    closely boat hand watch nose

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+print(os.getcwd())
+def calculate_density(mass, volume):
+        return mass / volume
