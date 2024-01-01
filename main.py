62eb1ff3fa4a38def9cbfbf35c4ae0e14511e9de
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:41:12 +0000

    pretend middle dry introduce take

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
