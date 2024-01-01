473967538fdb9642cd25691c98fb3f4d2c093fbf
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:35:57 +0000

    over greeting quarter repair trip

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
