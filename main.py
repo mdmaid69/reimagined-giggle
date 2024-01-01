b8b66c1eb233e10e1b1ad35e7ff193b8271d00b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:12:06 +0000

    copy exist news ignore too

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_speed(distance, time):
+        return distance / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
