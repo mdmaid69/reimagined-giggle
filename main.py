c965ce3e3fd53c147eb2ea7f19759a6101a818c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:59:14 +0000

    most his afternoon estimate ourselves

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
