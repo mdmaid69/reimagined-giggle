bc9486e411e0e1c1c2a69d8df0f0044aaa0742e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:38 +0000

    ashamed message remember best fall

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
