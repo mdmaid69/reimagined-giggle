041c21dded986f324817548e9e77ccaf910a63b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:51:43 +0000

    heat find war blue birthday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_speed(distance, time):
+        return distance / time
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
