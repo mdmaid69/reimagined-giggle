fd3ecbcf974012281d93720d9d341a3bad4ae7e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:00:41 +0000

    guilty obvious fence gate improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
