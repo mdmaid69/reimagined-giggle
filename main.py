e92f485711f98ad9a439631559a59439076ea640
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:24 +0000

    accept accident vanish lift strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
