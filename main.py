40b4ba1626d88a92a5f8ff27f178acb70fe5ecb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:22:20 +0000

    summer see pile friend shadow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_power(work, time):
+        return work / time
