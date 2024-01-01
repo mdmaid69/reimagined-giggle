8e89de674344c72bd1bf40efa73abb9bafaed6d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:40:32 +0000

    manner wash present traffic common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
 import array
-def set_array_item(array, i, item):
-        array[i] = item
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
