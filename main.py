618629b30047ec89b53d5ddd70a53f095019b45f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:03:42 +0000

    punish pain combine bread unfair

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
