daf74d4334e66d8b9fc1844840c44b14c3cce0b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:10 +0000

    lookaround home operation various short

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
