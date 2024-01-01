667538775b1f7c703a66d8e7b4b2cb8cde057249
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:29 +0000

    physical crisis tooth opposite recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
