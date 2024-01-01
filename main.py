d24b3c4e038b0fd17160185a2533afd6fe11b36c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:50 +0000

    shut trust stone cheap shock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
