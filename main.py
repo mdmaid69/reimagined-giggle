379f23b8a8112275e8053fca6bced2300bd29529
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:26:52 +0000

    uponon though sleep uncertain inside

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
