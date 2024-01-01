1735af71d5f6b973a935b23fac0ba648618eeb4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:06 +0000

    firm down emergency young pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def extend_array(array, iterable):
+        array.extend(iterable)
