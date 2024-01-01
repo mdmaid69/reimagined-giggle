2963bc60a488c44e1584716afce82ecfb7f8021f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:13 +0000

    on soil lead realize before

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def reverse_array(array):
+        array.reverse()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
