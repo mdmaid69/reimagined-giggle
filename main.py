187a73aef7abf92d28d44fcc93afa5dbcb45ecfe
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:53:03 +0000

    Itself help uniform occur rough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
