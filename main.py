772ffcb5135e270c50c0a7294e76310b25ecbbc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:44 +0000

    scared Ff delicate emphasis average

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
