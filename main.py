f5de95a45af36ed2b2eebe3f9bed294d494760e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:33:25 +0000

    plenty keepaway mess mysterious team

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
