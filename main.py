c2919ea6df917d812a749611fd8cffa7c79c40d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:58 +0000

    secret sweep substance style culture

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
