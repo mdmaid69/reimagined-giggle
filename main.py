c7917491d9b72327b05843819b78866d5c176e33
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:29 +0000

    uponon plain sister traffic offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_exponential(x):
+        return math.exp(x)
