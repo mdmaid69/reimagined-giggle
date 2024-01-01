9c685a864a75840f18f5a8882128ea59a18da967
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:25 +0000

    ought design identify event touch

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
