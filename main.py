2cc82219b352bc1b89e475070068a04a3d822246
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:14 +0000

    basically suddenly course angry involve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import platform
+def get_os_info():
+        return platform.uname()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
