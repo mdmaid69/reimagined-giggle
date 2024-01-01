082a6690131d867659f9866bdc3e8e533c55c33b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:42:38 +0000

    expression sensible main cinema university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def sort_list(lst):
+        return sorted(lst)
