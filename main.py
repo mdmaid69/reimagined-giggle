6a200472bcaecb773dbb2d9c2707ad33c82f81e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:26:35 +0000

    wall sick but shout increase

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def convert_to_binary(n):
-        return bin(n)
+import numpy as np
+print(np.array([1, 2, 3]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
