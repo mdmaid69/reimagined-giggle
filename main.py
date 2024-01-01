be1542df1fceec907ed2d30a07ae850b8371c4a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:16:15 +0000

    step film voice afternoon thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
