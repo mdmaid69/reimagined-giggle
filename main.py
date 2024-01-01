42b266abe5ecfbe508c4a420daf2eafbcfbd5f79
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:50:31 +0000

    mass furniture fix I bound

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_palindrome(s):
+        return s == s[::-1]
