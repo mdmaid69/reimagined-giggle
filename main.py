3ddf9435daae649a8cd14626d411692fbaf89175
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:28 +0000

    intelligent somehow sweep thinkthrough save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
