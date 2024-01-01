1b12d96e03ba2a6fec0585ad91b41128a3f997b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:25 +0000

    touch public then earth same

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  import os
+  def get_current_directory():
+        return os.getcwd()
