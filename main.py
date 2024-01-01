d1e23b6eee531d85a1ea16428ddf524624ba2aea
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:04 +0000

    stick set mass total pleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
