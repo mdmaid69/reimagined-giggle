353f36aebdc653199806de3a9e240fb79b7c503f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:05 +0000

    other birth run example meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
