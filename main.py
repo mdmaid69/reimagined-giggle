d4de0e054075b4fd7799c0f742db49bec87685b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:16 +0000

    mean adjective straight beautiful flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def square_number(x):
+        return x**2
