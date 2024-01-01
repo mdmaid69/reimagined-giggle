1796a051f9d9da3a65f8b00c2dd4f4c875033785
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:15:57 +0000

    welcome comment party mixture left

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
