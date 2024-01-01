8498de006f190a14bc1a3b3898807c586d06e1f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:20 +0000

    usually mark age run sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def is_odd(n):
+        return n % 2 != 0
