25092c5753b1d9843f366fe051222e6f541b061d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:41 +0000

    race among guess rest hear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def find_min(lst):
+        return min(lst)
