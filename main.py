e2c4feae04407b3b12fec4b8e420962fab944342
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:25:16 +0000

    sweet hole turnaway realize unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def multiply_numbers(x, y):
+        return x * y
