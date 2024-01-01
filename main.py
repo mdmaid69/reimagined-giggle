2e1ec68b695bbdeffb60bce3ad29ec734c8fc72f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:11:57 +0000

    hot giveup book crowded neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
