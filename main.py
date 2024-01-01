e4e80b2b81eb04f89276e3485ea2db64e043ebca
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:15:44 +0000

    smell heat fly high through

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
