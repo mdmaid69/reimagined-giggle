ec8b5a00d5bf175f4957c6d5ea1ef915be143c2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:15:21 +0000

    result wash bang influence snow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def subtract_numbers(x, y):
+        return x - y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
