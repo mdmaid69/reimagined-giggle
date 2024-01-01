52e8cc0270945ee780e1774ae7beb97bf42b6e75
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:51:40 +0000

    dick name correction none official

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+print("Hello, world!")
