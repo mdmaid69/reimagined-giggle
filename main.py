58b502033683c808187c3c1cadf73204d428ee83
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:47:52 +0000

    566end anywayanyhow emotion man cum

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
