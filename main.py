cad6675b68e5647af6d81fcffb42e7477ee073b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:06 +0000

    stamp file debt can between

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_acceleration(speed, time):
+        return speed / time
