ccc222b23a4f12d5e6c25932933ed28fb292aeae
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:48 +0000

    point settle hot cause no

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
