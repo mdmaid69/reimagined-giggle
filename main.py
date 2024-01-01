278d595480a6cc11db5d071210d819da8b4a5fa3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:05:10 +0000

    cost drop profit big door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
