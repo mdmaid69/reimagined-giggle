55f1e15d0d394e352c250e7ecec4b2af0548e10d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:29:10 +0000

    close flight clean hand recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
