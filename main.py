7dabf7a6eb2434791d7dcc9c502cb63e558e7ede
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:40 +0000

    day skirt appearance case makeup

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+print(sum(range(10)))
