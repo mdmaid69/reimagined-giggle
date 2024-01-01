5638b6b9886efb95e551aaa369d82c91e93de680
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:07:33 +0000

    criticize secret geton workout thanks

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_power(work, time):
-        return work / time
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
