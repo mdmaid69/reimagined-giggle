bc1c296ecbcdbde682da25ac2158aa02b61abfd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:05:42 +0000

    such cool hang put free

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def multiply_numbers(x, y):
+        return x * y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
