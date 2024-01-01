dcc172bdc6a769d42907e18a94fbf2a13f512f0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:59 +0000

    balance concerning kind ticket inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
