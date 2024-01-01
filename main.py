b14d7ab71a5ecce5607fdfbf9315f04d51305164
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:35:55 +0000

    bread cross letter dream to

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
+import collections
+def create_stack():
+        return collections.deque()
