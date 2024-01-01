429f410aeaca9d7fe1ab376ab6ad613bcbc3bbb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:28:33 +0000

    ago workon butter become but

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
