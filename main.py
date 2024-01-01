71df19ad48aef2299f321151ce1d4289366d680a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:10 +0000

    thankyou before division war birthday

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
