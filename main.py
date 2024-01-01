4b4eb08edfdb907c63acef18a8333671ef8e764f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:35 +0000

    giveup wave ask rent comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_length(array):
+        return len(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
