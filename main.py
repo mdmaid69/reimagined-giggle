15c3fed69f395346e7aa0f473797fcb287e63ea0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:11 +0000

    borrow stamp disturb room the

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
