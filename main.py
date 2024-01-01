5d1d5555558493f42517bcea6142f4ad55f5858c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:18:29 +0000

    moon game clothes drink or

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
