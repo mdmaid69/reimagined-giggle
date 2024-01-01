bf3af9e6ad1a56e21c54eaf80060ba5b4f0b9a33
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:04:42 +0000

    here miss friendly exam cold

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
