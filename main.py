53fd87a528e3a89b7648910e457d72855ec81bcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:19 +0000

    entry when believe Ww manage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def cube_number(x):
-        return x**3
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import os
+def get_current_working_directory():
+        return os.getcwd()
