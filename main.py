e5c70bf4d3420b5f478f2ce5b6b5ba91ec14ee79
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:10 +0000

    injury than wind giveup far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
