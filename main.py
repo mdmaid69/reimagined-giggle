7f36a467840e50606e75670bb76b7e7a9bd9fdbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:54:02 +0000

    strike clearly day sick lie

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import sys
-def exit_program():
-        sys.exit()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+for i in range(5):
+        print(i)
