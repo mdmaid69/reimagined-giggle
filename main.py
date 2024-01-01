e0a4e90e68abf4fcd29b35e275cf642ebeb8f407
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:25:50 +0000

    turnoff influence state mass close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+def print_python_version():
+        return sys.version
