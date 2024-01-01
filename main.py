dc558d24545f577dbd2c736cbf647579caa18f9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:15:47 +0000

    sorry limit this read wash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
