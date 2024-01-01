639a79f0d4b3a98bb5024796817226090b533820
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:28 +0000

    safe instruction attract school high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
