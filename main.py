436d1f94c4bebfbeaa6e074c3d2a8b91828e0c19
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:53 +0000

    meanwhile equal forever lose raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
