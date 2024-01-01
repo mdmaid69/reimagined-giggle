d052731ff779990272735097ed32b2a0fa866c61
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:10 +0000

    aim deliberate show develop breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
