4bd1fcbe096039381286c38998994841f3033771
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:15 +0000

    furniture steal murder who wonder

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
