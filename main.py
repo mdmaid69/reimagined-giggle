0764951a348402ed56f654a1e63a94afc0a0d1f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:19 +0000

    comment market worst in traffic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
