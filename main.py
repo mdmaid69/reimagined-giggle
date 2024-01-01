7ffb7509786ad7545478851ce972ea0f462c62ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:42 +0000

    move bush wide day except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
