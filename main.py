fdc50eed6665962b0cd77c4569c1756ffcef6a0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:29:56 +0000

    family sign noisy stir anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_circle(r):
+        return 3.14 * r**2
