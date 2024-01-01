0299e75ba2753082fd968ffcaa7d4742bf9ae156
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:01:49 +0000

    reduce blue return beat hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def find_unique_words(sentence):
-        return set(sentence.split())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
