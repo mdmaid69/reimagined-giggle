ef4cee68ca3f6b434568573ee81bdd4411f177c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:19 +0000

    evening home second present charge

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
