dbc244a42fed9bdf6842c10c12c384d72a98cc78
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:26:51 +0000

    read home Ii repair or

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def count_elements(lst):
+        return len(lst)
