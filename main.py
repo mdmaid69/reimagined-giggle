430b633ffd8768bfcfcae288e1d8ea91cce4a675
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:40:08 +0000

    base feature several inquire confused

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
