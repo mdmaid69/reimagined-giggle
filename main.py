1942259edb8d8fb054e039515e9709f805376613
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:33:32 +0000

    spare protest sing stupid strike

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def subtract_numbers(x, y):
-        return x - y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import getpass
+def get_username():
+        return getpass.getuser()
