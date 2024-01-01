b3a9aff55985f006d3e24ae2fac01c778d72f78a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:08 +0000

    evidence major careless before experience

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
