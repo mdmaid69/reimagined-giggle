4621bba31885b8610b3164ae6b902f848185d205
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:31:11 +0000

    fetch sick shock stranger visit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def remove_duplicates(lst):
+        return list(set(lst))
