0889589237796843306fea75c90d47ee182bb011
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:32:34 +0000

    popular cook nose matter transparent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
