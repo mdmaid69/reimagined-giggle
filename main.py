6e5bd2eaf9860aaac3f0d911b8437cdbd60ba4ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:09:49 +0000

    plate passage determined call by

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
