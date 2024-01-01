88ed356e2d3e56856635bc202142e866ba87b29f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:28 +0000

    police day on book definite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
