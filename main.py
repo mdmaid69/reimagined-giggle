c2d1bc07b1ab4422e07848f3d25979ce2c4617c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:51 +0000

    room whichever fun set now

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
