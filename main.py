13d5b736b34cc4b5bbf515043ab9f6ff1c2dbc50
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:10:19 +0000

    write attitude collection us limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def get_directory_name(path):
+        return os.path.dirname(path)
