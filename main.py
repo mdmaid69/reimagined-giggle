835d0f09551eb67d3b2e85d6b05a8809c4850abf
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:30 +0000

    affect normal curious free trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
