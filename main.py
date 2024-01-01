d511f04bc942b94674af3d50abddaaefb1fbf891
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:08:09 +0000

    569enjoy pretty inform scare flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
