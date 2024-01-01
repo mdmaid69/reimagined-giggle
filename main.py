7bfd6bb1b0e32dd2d3752398104f29fc03ba176c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:18 +0000

    meal fresh bringforward side next

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
