2dbf3fb22e458925cdaede005a59930dcbeb9c42
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:30 +0000

    choice good blame communication wake

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import sys
-print(sys.version)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
