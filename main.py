c2dae52cc496cea244df910bb25c4794e92f87e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:33:18 +0000

    lower information memory Gg over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+for i in range(5):
+        print(i)
