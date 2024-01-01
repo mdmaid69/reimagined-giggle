cd412bf1cab3532915f6f5e662c9f1dce4c85f46
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:10 +0000

    weigh tense 568energy shop estimate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
