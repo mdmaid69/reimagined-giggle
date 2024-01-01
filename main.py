02e6a631f8474512b06e3f024659ad4e4adf0d68
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:25:17 +0000

    pen blame hope workon private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import array
+def get_array_as_str(array):
+        return str(array)
