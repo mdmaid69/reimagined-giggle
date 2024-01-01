bda182cd4c06e904e588298b0c787c4ce0bf4f87
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:26 +0000

    attitude conclusion wind bit cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
