82242b3e2ab464db7b14250474247bd1fc57851a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:54:09 +0000

    say pinch blow shake confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
