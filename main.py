6fddc208c2d3558abee36eb7b2f1a13711186742
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:09 +0000

    beginning never by ourselves scrap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
