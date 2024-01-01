bf94aed6b0595614b9b567ace0505653bcc1a514
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:44:48 +0000

    forever cost shake either before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
