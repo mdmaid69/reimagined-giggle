6117e84d01dd9bd544f3a314fe62b0dcbabcd675
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:22:10 +0000

    protest doctor entrance almost variety

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
