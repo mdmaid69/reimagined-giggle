46f7518cedf924fe3f8179f303129b253b9ed88c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:47 +0000

    pretty Ss memory sail patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+print(random.randint(0, 100))
