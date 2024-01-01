6f31fbad20074785bfd586931ddf25f3c3777ef7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:52 +0000

    partner leg kind comeback contain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
