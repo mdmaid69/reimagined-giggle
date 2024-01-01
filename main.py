3922470a74a7adb3667671075b2d08b0968a84f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:31 +0000

    meet successful design bin switch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+print(os.getcwd())
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
