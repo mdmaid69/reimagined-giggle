46587ee8bc8094a1fef6c9ec681b03358b430e60
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:30 +0000

    stare pronounce bored include giveup

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def is_palindrome(s):
+        return s == s[::-1]
