be62cceb4e0488fb0e43a901af8cc4a2030e65c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:32:04 +0000

    share calm how fashion stay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
