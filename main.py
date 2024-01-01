e3c41d8f60d597c3ee8518b53611b275d00bfdec
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:53 +0000

    clear exploration pay meet black

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
