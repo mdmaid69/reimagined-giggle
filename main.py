04f441c143ea2bc96c451f8ed216ed9784cdaf5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:25:27 +0000

    question circle any contain bottom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
