a7219ac886696d463e70d9a98e5e13ad861b3421
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:58 +0000

    moon fat have feed at

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
