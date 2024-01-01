58c2dcfb32235adc44221bd3c63f1a600bd42422
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:15 +0000

    completely form sit repair reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
