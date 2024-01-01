43dc293d794971819109b1208552cfdeb0ccee40
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:09:32 +0000

    contents children blow drink Itself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
