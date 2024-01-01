93cd2b65a67b2bb44dae0940fd59c199209049a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:12 +0000

    body right keen fear divide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
