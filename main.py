f599b4a4f824a0c7e4a80cc8577ad560b282fc7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:07 +0000

    combine bar mother protect fun

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def calculate_work(force, distance):
+        return force * distance
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
