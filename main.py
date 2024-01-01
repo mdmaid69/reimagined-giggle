61c5763e3e81a6b3b8b1eb0e232aa398e824e9a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:38 +0000

    talk attitude explain fire male

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
