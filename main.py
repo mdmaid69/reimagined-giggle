23c37f14123c48a31f8b6e80691d0fda6eac7c95
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:50 +0000

    ear during spin state aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import array
-def get_array_length(array):
-        return len(array)
+def reverse_list(lst):
+        return lst[::-1]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
