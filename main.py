bb0eb32d241cfa5fe844b10de9a75de9fe255d01
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:53:25 +0000

    mess match chief wide wipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_acceleration(speed, time):
+        return speed / time
