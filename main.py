9b5cafc44caabf609a59f2dfd87e3561329d29f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:09:45 +0000

    cry list suffer to listen

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def remove_duplicates(lst):
+        return list(set(lst))
