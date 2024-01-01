a13a70a4048ebfcd3461642339d9f79fc7cf044b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:27:33 +0000

    tense anybody tell Pp charge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def find_min(lst):
-        return min(lst)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  def is_odd(n):
+        return n % 2 != 0
