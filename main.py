bddb2ec37350ab2aa1a96e55f748f5076303fea0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:00:27 +0000

    allow range getoff next takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
