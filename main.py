4afecb09b2409fffc8a332d2c711e5e4b1fa3ca5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:04:20 +0000

    tall their study combination adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
