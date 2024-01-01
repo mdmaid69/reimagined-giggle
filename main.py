ce13a26fc784f63461ac5ce53e0f34122d41e1bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:03:44 +0000

    tomorrow beyond swing yesterday hurt

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
