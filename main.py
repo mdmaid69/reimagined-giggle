e6a3691cd32296a8f195dd303b40c9eada7ecc5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:28:49 +0000

    interview expect mean paper error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
