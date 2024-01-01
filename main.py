e4e78e88b6011ee995158a61bdcb9c3b4646328d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:32 +0000

    relief godown recently close fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_ceiling(x):
+        return math.ceil(x)
