edb3122154cddf8a4ab401a9747a54a7b74c1bfe
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:19 +0000

    talkoutof bang cause sharp strict

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
