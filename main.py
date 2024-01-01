b50b2907f9bd8d35ed4071ba7048951d5fbc51a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:24:41 +0000

    mess getout later distant plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
