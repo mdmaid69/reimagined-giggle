e1cc788d1ea462b5e9a471583b0996ae865e52ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:21 +0000

    pour hurry immediate Rr purpose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_item_count(array, item):
+        return array.count(item)
