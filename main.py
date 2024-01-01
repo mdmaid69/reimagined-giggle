e055cefbaa7780decbac2d5b322aefa15abe1a07
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:43:14 +0000

    holdon anxious danger convinced club

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
