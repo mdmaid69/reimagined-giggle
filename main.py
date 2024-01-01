67a2dc67279fca178dd71e499ff5ecaa5e6940f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:00:13 +0000

    conversation door wrap disturb phone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def find_min(numbers):
+        return min(numbers)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
