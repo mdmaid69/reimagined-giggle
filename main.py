929134291a20cdefaf02ef25a0adb54c9d06404d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:07 +0000

    waste average somehow previous translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_length(array):
-        return len(array)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
