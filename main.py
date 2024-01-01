6ebf281de3854a186f12004d6962ec76a3411096
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:17 +0000

    earth them modern head urgent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
