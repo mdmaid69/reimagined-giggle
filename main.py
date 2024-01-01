639ea65d14444a0303c49345b8ef0122810eea94
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:22 +0000

    exciting dark bet authority watch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
