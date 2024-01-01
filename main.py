00d50b56c483b53fa9621c864472d1e964827c1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:33:28 +0000

    progress extra responsibility sentence reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
