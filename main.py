25f236030a678108115caa148968bbf45425cb2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:42:08 +0000

    round advantage visit hurry meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
