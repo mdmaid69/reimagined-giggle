375c30609691ae83d9eb81a043d90ea944c79a1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:54 +0000

    around standard spring stop against

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
