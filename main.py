ca6bbfc17fcaaa52cc8492f411f6eeba362ae546
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:12 +0000

    ask fit create our secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
