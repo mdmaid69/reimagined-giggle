e2ccbb3e6cc73388f0c15c6b4eab211fdb9a57e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:41:57 +0000

    disgusting month sing change mean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
