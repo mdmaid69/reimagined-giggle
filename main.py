88e2c67b8210d7bdcfa46f8fd207cf89e4788db9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:57:58 +0000

    material event wind up here

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
