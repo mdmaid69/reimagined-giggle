bc0f9a3088c53e1a63254eb0472b01b045360c13
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:25 +0000

    shine previous head disturbing practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-name = "Python"
-print("Hello,", name)
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
