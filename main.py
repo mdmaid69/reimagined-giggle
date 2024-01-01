93a63673c4c39e940f2d9440d3f2ad5d1b99eebe
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:23:35 +0000

    afraid break Hh boat understand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
