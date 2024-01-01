cd736ca223b05cb6eb49a9cefaeb3470bbf3de4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:37:54 +0000

    reduction shock path lower wish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
