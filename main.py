44ab5e71534a4807ca4a987408e64dc6ae790be6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:44 +0000

    rob excuse cost regard opponent

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
