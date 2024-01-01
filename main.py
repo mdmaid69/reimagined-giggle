930ce1d47bacce98a7a804adb3aa316b8b2eb01a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:57:08 +0000

    near weather among need half

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+print(os.getcwd())
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
