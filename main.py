7693d8ebb6a5d5fad412799635219c9dd351eba5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:22 +0000

    pick many extremely dry demand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
