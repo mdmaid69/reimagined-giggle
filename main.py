8ca7543ede3d272a882024f70bc6d0c79c3f6dd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:53 +0000

    violent study lock march prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
