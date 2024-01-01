60c92010b127bdd10122c63b91bd27e9ec824f01
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:30 +0000

    straight otherwise slip emotion notice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def remove_duplicates(lst):
+        return list(set(lst))
