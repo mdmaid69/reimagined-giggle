77410281770b626500554896e455546fe9e5a291
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:31 +0000

    band downstairs gather successful might

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def get_array_as_int(array):
+        return int(array[0])
