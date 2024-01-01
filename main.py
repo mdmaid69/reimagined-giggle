9801d654d1b33c267880ab718668abda715c8e1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:39 +0000

    underneath correct shrink stick suck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
