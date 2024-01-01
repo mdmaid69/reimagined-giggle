921c7937888f31bf6656dfbd2f9c7af530267f84
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:56 +0000

    circle mark guess concern beat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
