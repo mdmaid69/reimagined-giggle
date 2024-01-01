7613b7e3d8226dd85850609c1b23bef810c794ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:19:34 +0000

    matter shout flight environment most

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def convert_array_to_list(array):
+        return array.tolist()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
