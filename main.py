67073a8183ee50fa344499502d6e4bc1051983ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:19 +0000

    brief attempt deep disturb drink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
