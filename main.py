0c9e1594a90d4eff17612885174717bd044704ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:50:23 +0000

    declare transport behaviour simple any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
