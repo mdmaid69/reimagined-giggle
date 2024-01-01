282cd835a9e8983974c09de995b5a00752b12cfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:40:31 +0000

    quantity rush 564encourage nonsense persuade

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
