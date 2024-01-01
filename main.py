a037fe994b33da93fbb809fd523a26947cd43c74
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:59:51 +0000

    size use supply before shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
