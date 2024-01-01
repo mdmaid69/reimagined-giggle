aad169413a0496a997ef1b77698085667413cb6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:16 +0000

    amount skill snow there question

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
