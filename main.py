39993932a1ab114c3d85289e17d2ce26c294012a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:22 +0000

    arrangement violent invitation fire store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
