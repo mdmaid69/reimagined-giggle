7e25e95fc00a0a410bbade830dd8d61a65e3f14f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:12 +0000

    old weekend example about whistle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_circle(r):
+        return 3.14 * r**2
