f0d06362e82bf1838bbab24524cafcaaabca43af
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:51 +0000

    clue speed break attitude especially

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
