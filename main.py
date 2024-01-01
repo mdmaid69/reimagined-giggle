e0616be06b540240ef204a4865c2d63f85c35533
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:41:32 +0000

    future transport Nn least picture

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
