a2ff5a9c49f6c0916db222fca83ec6543d16a408
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:37 +0000

    nothing about us change well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
