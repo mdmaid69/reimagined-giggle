271c74ccbe305ee9f14c29305186a1dfa560b4de
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:12 +0000

    first accept zero circumstance movement

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+for i in range(10): print(i)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
