1c4b6eb1874a3000efd532ceb3d086fda422fa8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:12 +0000

    regard dare apologizeapologise cut goaway

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
