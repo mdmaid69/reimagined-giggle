47566d2bfb4c4f2aeb9b9a6fa03994e0b88e9f28
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:04 +0000

    preparation sort professional interview avoid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def is_odd(n):
-        return n % 2 != 0
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
