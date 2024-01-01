722e6e43cb990eeb6670a24d85691b71a088273e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:07 +0000

    factory solve occasion collection own

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
