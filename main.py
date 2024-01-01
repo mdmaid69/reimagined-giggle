bf8ebacca04dd72c06cce6f9373a10d572a8e834
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:47:58 +0000

    heat foot exhibition several shine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_min(lst):
+        return min(lst)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
