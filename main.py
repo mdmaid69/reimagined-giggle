e11cad30c231f03c0c7d9c19b616038c7d2716b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:23:28 +0000

    giveback stir centre wild pretty

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_work(force, distance):
+        return force * distance
