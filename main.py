510a330aced8673815b0d3e644f36e328bc70b7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:56:28 +0000

    concentrate present rush stamp whatever

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import sys
-def print_python_version():
-        return sys.version
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
