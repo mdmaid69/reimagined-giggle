779162aac0cd612d01804e8cdba093a4b864b77c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:07 +0000

    trap turnout Qq emergency scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
