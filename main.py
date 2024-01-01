764c3c5a1b0a00efa9c8c4959ea27a474e4d813b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:41:14 +0000

    top callback big fork scare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
