f61033d61b990bad0f59a8780e20f43fe34f54d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:14:08 +0000

    sight nerve risk history reply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
