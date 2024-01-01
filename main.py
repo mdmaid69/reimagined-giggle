96c43f8d471cf7866cd7f60d302335e35f8074e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:42 +0000

    interview sister opinion experiment label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
