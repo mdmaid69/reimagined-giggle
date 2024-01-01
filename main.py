a9a20d8a68a322d213d7fb291e4e037a8ff32b68
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:09 +0000

    pump range matter yes move

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def find_max(lst):
+        return max(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
