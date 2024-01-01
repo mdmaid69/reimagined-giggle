3bdd038b65e349e6f474fa83fa2ef6d56ca94dd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:39 +0000

    left by very block warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
