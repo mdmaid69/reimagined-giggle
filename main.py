2cd699833a01a148b0e1ce2bba7f541a5f0ad50d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:02:06 +0000

    need tomorrow laugh only turnout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
