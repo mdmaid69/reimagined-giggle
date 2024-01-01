4796dac9c26408e7112d6c49a655e1bf6febcc57
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:43 +0000

    really shout pupil afterwards section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_as_int(array):
+        return int(array[0])
