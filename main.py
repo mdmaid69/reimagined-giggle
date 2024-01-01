5becced915bbf467c6015fd7eb00dc7821434ee6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:08 +0000

    of protest organization this Hh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
