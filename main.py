f8e1f09bcf87d486c936310f35688fe966497bbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:25 +0000

    experiment quality lead label breath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import json
+print(json.dumps({"name": "John", "age": 30}))
