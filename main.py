5888897d1fbf73202b99d5099505e927422b64f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:50 +0000

    cinema soft home either successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
