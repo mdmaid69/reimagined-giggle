3034b45e29b288509bb391d241019ea7391ab9eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:53 +0000

    careful often conversation rob passage

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
