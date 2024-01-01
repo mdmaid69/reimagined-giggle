8d8acfc17789e692cf4e84b43c3215b3d3821204
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:23 +0000

    work picture track process fight

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def find_max(lst):
+        return max(lst)
