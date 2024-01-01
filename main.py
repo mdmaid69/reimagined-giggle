31a17e2ca1d1bb55c5c49183a7574c04fa49d1dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:57 +0000

    custom comeback total boil win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
