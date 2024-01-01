f32ea7e54a137a1b6813e9e6233da3d8793595cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:37 +0000

    look target advantage unless middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
