32cc56711dc916f2c4f0ecc0703beb3b725f3f3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:27:43 +0000

    patience interview die environment miss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def sort_list(lst):
+        return sorted(lst)
