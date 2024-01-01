e9ce7d8aebc76fab79b732511c620749a4ff0ffd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:57 +0000

    welcome until office mistake place

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import datetime
+print(datetime.datetime.now())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
