edebb2a2c5ab746ea464e33c50efc60a2783817f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:29:06 +0000

    independent comefrom maximum average trip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
