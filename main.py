6889d756781fe6064e15ff261b226ebcf4c480cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:39:29 +0000

    tough shop somebody error kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
