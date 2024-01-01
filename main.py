c7f5343ec3615cfe3c3950f0bc093b3e42ea59e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:54 +0000

    friend feed previous mess closed

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
