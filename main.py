72fb14829afea21e89ae59c45315e6ceae43e875
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:53:50 +0000

    close direct off reject call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
