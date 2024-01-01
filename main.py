65259109ee5747a34c0f6faeff47fd716346c32b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:27 +0000

    discover clue attractive telephone ear

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
