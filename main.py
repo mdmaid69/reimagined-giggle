3984a1e2e7e386e71754d117fa08a73e17330c34
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:05:53 +0000

    print top passenger fix father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import tensorflow as tf
+print(tf.__version__)
