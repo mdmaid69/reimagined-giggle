e4599321284e026e01a3053473d58c24a1a9c9eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:03:45 +0000

    word collection trust for cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import collections
+def create_stack():
+        return collections.deque()
+def find_max(lst):
+        return max(lst)
