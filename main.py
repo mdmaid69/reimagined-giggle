25f3d22ee2e8c320a70bf2374fd5f28afb06fd79
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:05 +0000

    protection circumstance him confident brief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
