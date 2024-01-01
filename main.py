365a4c23b8e10a8eae79f3c596f89dd917362e88
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:23:06 +0000

    force persuade difficulty future definite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
