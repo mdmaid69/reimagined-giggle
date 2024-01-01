a5314d82226ecb804373e22da1ebfaf88600a543
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:37:02 +0000

    giveaway unite dream recommend OKorokay

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def calculate_power(work, time):
+        return work / time
