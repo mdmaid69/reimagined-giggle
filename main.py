aebb5638bc30e0495de8872d43aba905a4010dd7
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:31:55 +0000

    wet repair passage border more

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
