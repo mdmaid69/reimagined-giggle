eaaab8dd1cfc8bf50140f2d6e0f4ac717348c809
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:42:41 +0000

    breakinto of trick present buy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def find_unique_words(sentence):
+        return set(sentence.split())
