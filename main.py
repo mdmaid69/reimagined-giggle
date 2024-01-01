ac6890eb5570fbd53a7569cb8b618dbd52474280
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:31:49 +0000

    wait stay normal callat opposite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def is_even(n):
+        return n % 2 == 0
