e74f97fe20c010d1be179a90f568da8e11379880
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:35 +0000

    model rubbish later swing forever

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
