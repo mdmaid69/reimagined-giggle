316714bc49cefa0d57b66953d1bd35ef2ee0419c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:56:57 +0000

    coffee smell poor drive sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
