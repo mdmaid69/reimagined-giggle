4961bd237537280200606e2fd8d9bb4a41989238
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:57 +0000

    past thing command side getout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
