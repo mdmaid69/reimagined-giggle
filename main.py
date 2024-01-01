90e584d32d98c59f3bda16aaaf00d4c48f9f98e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:29:35 +0000

    board sorry less bye by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
