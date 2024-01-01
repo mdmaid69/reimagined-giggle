d5c0f83c19a8602841c577e572c3c291c92b5ed0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:30 +0000

    one ear light step future

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def cube_number(x):
-        return x**3
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
