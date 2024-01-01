3b74adfa76ef481057ff9e4b80d455515c1b2bca
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:40:14 +0000

    duty week worse step warm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_density(mass, volume):
+        return mass / volume
