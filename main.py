99e3cb31c75d5903b485bdf73c4fe1ab7bf62dd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:03 +0000

    pull bin snow object style

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def square_number(x):
+        return x**2
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
