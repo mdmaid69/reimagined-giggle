dd5cbe0f07007a26f2777c1f6fba78044e4b304a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:15:24 +0000

    smoke success payment invitation sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
