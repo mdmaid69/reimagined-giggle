1b6f42d97dc7c00f19f0c9d71ff930d463795fa5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:32:29 +0000

    accept prepare moment work yourself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
