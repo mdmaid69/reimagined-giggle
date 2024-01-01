3c38550fd1ff576d695793fba6a77e1b1a7ac2d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:44 +0000

    treat effect against suit month

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
+  def calculate_area_rectangle(l, w):
         return l * w
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_exponential(x):
+        return math.exp(x)
