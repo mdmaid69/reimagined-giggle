62ceaedf426609f34f178ece0aae0288f80aebbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:00 +0000

    bringdown attitude risk bury only

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
