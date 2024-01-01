b1f11e0be258f9e16f0ffcf53a4fdac038f89bdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:59 +0000

    565end unpleasant getup as female

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
