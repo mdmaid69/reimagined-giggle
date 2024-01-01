476959164c90a60eaf534c39c82737de142923c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:13 +0000

    show responsibility over pleased however

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
