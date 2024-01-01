bc0999509e2f2a94016076f54aa799695b57239b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:57:25 +0000

    anxious object high bridge operation

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def is_odd(n):
+        return n % 2 != 0
