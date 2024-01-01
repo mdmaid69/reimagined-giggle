e728e56faf4666b6468a39f1971226255a735fda
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:44:28 +0000

    discovery trial wash accordingto somewhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
