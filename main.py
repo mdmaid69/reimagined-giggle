d18796baa16cfd8d6f2102ea4c9991d796f4fac9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:42 +0000

    mouth regret loss power Oo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
