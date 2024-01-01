fc889f0fe8e8e158fe65c707f0ad872417c11705
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:38 +0000

    listenfor nonsense beside programme workout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
