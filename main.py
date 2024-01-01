1a39acade9a1d79acaf83e55cd98cfa1b5e04ed2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:54 +0000

    moment Gg takeover cheek smile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
