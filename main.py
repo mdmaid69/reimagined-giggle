8f1d85449f514cee30309a0ca9b91c6a1218b94d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:30:08 +0000

    Ee wake thankyou suck even

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
