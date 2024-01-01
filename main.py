d730da5f220056ac171ddf602b05c338e124c68f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:02:00 +0000

    steady unkind please go lucky

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
