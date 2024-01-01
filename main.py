653c5e8d8f4e4246c4d50f35d02d4eeeb737268e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:55:11 +0000

    undo finger down sentence ring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
