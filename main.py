a8dc3a992c6e39b5263d375f00fa969824338a3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:53 +0000

    fix wherever early afford story

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def find_max(lst):
-        return max(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
