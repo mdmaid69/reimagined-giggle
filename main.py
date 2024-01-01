cb9eb613ddadbf0e83c8daa926e32e1321eedfa3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:56:54 +0000

    pair beneath hurt then guard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
