962f4d8566c9e5269e46255dc6a5ba0629a33b23
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:02 +0000

    mistake develop whichever lonely pair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
