7fa6a249b6408866d2268beb685f02a76a2f36c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:41:29 +0000

    trust turn best dress Vv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
