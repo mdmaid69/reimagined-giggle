fec4bf0ee958d3853be90bd9934dc4fba310bd8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:44:19 +0000

    us effect branch him cry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
