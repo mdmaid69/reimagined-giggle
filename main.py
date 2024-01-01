f4292a6e62efc32d2ac71deb5d669f1b2091eb93
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:32 +0000

    tired to task task actually

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def count_characters(sentence):
+        return len(sentence)
+  def is_odd(n):
+        return n % 2 != 0
