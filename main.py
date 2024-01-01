328794e3f322307b19522afca822302e3cc47b60
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:16 +0000

    limit underneath work which exercise

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_odd(n):
+        return n % 2 != 0
+  def sort_list(lst):
+        return sorted(lst)
