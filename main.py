d2da9484643a2e830104fe25fdc96de9eef95135
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:02 +0000

    bound show hard sticky secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
