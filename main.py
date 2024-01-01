9e93e33a6e9b7a15fca3505a5b80d4816fdcb2e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:20 +0000

    effect increase but alone paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
