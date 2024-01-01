a69bedb62604172f2628078b295742dcb792fb23
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:23 +0000

    tape example thing probably group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_density(mass, volume):
+        return mass / volume
