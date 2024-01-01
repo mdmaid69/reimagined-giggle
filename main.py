598ce0c4d1b9268cf012d6227b66410e06f0ddc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:27 +0000

    touch fit branch makeup meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
