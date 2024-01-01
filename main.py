ae6b5bc143b95358bec8339a81cfc155397c992b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:46 +0000

    along mention stupid might pause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
