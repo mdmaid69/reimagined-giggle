7ee8fec08135ba5e92171fb17118e5efed661a43
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:05:27 +0000

    offer loud call later boil

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def is_even(n):
-        return n % 2 == 0
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
