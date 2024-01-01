10d86f4c9bf1c3a0a4e56739159ae6d80bc32da8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:01:02 +0000

    gradually street piece eat lay

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
