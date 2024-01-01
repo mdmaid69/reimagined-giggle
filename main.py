63ed0994908bdbd7d2d2b788d97d386c5c18970a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:14:25 +0000

    watch present steer blind otherwise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_length(array):
+        return len(array)
