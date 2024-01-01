857c12a8360624d867cc7255eaf7f01b24ca4f60
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:48:32 +0000

    this confidence bet false negative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
