23261fd3ec8eec83f187e46476e52d9f08987227
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:00 +0000

    cross arrange march factory appear

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
