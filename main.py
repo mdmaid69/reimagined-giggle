9aa748179bf6da7e01e74057b810a2fd32af0c4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:57:14 +0000

    avoid trick hand handle earth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
