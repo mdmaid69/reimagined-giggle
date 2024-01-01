1ff1ed5f91e175352e7e50a610aa777a62561bd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:06:20 +0000

    towards funny free steep connect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
