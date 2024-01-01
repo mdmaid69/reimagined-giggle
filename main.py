6e288caaf832dcb530786f14c1ddb3ad2873802c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:14:45 +0000

    busy professional snow health fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
