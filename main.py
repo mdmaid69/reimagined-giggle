ce884a5776525be138127b33b3b92910fad72836
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:30:23 +0000

    whistle steal start every previous

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
