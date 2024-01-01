a72d299bd2f1bf5b15d16eb4fd5463e0ba157253
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:14:35 +0000

    back admire rate offend mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
