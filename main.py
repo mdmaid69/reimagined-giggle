4f504a69d2fc13c428705447133ed8f9187b8064
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:55 +0000

    sometimes birth solve stamp steal

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_speed(distance, time):
-        return distance / time
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
