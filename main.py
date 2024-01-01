cc1687af744393a21dda06dbeca43e9c9a6d8374
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:16:16 +0000

    greeting hang contain city afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
