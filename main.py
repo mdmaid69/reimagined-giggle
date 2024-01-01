21b6a0eae527e5eb95b3b4670d7a9914f0d5a895
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:44:41 +0000

    direct miss slide busy wet

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
