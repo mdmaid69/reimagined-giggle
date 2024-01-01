9c0e1d0d9697cd0a791682cbf0948cdf1c6f3d9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:23:40 +0000

    become patience anything so boat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_area_rectangle(l, w):
+        return l * w
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
