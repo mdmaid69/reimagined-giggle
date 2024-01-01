f23bd44f95cffa12253bee5420caf6caba5ed252
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:27 +0000

    tall aim in OKorokay journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
