7344cbf1730343173e9af25a6366b18869109ad8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:47 +0000

    age hungry use soil demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
