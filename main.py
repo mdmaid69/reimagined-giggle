290d67e1b1a2e9b2215312480a938587d36737e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:09:28 +0000

    strength scream mind feed thirsty

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_current_working_directory():
+        return os.getcwd()
