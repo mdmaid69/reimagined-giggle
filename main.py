1ed6837e92089a7af6100a296dde19231af4fc91
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:10 +0000

    572enter drink card gather shopping

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
