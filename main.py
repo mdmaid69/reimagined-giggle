7073f0b0db428b0fd1e02fa3499c133acf83e835
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:03 +0000

    certainly road sport genuine dead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
