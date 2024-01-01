89707afa902d8659a52ee7d99cfa63cbe2fbeb27
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:14:01 +0000

    national phone earth cost cum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
