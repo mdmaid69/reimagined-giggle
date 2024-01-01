d72fd2b15c22536f951f12b68d033ad618d2533d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:51 +0000

    drink comein food odd square

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+print(os.getcwd())
