e9707c2f238937a4157cea1508506f2e6c2f084d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:17 +0000

    importance mark cinema somebody cover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
