e60535faea35b387f9a05ed569a8edd20ffb50d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:21 +0000

    have tidy telephone cough feeling

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
