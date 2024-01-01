5dbb80c12e8251339fc2b118789706dd7701497b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:15:50 +0000

    contain represent friendly collection gather

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+def is_palindrome(s):
+        return s == s[::-1]
