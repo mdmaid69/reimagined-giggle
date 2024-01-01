9a67c8bf1867cad2d8813feabde135ade18ae1dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:35 +0000

    however start finally cover outside

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
