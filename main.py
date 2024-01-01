e4976d138b8ba0c6e2163129f20ae2bd7bf7de57
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:24 +0000

    thinkover protest rise least paper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
