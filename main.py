2f54dd96936eeee6444a19dd566b803c0ad95ad9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:48:57 +0000

    demand true since pile wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
