9063113b232e60a1d87170794399a8b895daa2d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:16:55 +0000

    suit announce here effort carry

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_work(force, distance):
+        return force * distance
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
