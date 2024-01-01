3064b6718e78af7d890473f9d87ceeb38e7579a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:19:21 +0000

    spring tongue earn message pause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
