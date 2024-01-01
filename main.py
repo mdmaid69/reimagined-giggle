0c8cdb77282009380afcd11c551d2f01166f8fb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:29:46 +0000

    appearance watch freeze smoke second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
