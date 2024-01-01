78c9fc195c2eb284693f90725d59ba9ff8aace2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:18:50 +0000

    there month answer justice everyone

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
