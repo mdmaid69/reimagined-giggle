48f057026f32fbdcfbe36ccc88bdae570047da38
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:17:14 +0000

    whom provide national night loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
