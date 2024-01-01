f66f306ae2eb63ba40c58a9f40a5b1d99bbb19a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:15:25 +0000

    address protection runinto shop negative

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
