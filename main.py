63d1fcc55a4c7adeed7bdfbae901bf22c43a5687
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:38 +0000

    shop sorry square character below

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_index(array, item):
+        return array.index(item)
