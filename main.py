aa748c17c9d9c42b926d87d61f725f6bc367037e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:42 +0000

    please pump bin because lot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
