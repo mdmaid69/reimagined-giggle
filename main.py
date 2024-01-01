22a22054fa6ad64fc256052015f4f54a6e911c19
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:38:59 +0000

    tap ilovedicks bye principle way

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import time
+print(time.time())
