ca310650dfc185fc89b22d5a09478c06ec06b3c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:11:54 +0000

    left skill complete mine change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
