96e861a9e8d534e01f9b690704e47e038374ab40
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:53:30 +0000

    breakinto start bye callback declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
