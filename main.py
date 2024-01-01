ed8d8b68300f0ae7d8279bc31202d3ed43917003
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:22 +0000

    turndown both away reasonable fasten

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_work(force, distance):
-        return force * distance
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
