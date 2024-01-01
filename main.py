e9cb54d3a3acd58599e4e63ef18593e5557ae073
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:28 +0000

    neck group way act drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
