9df98c12c4252cb308c1851d3c13bc1886a63ac1
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:22 +0000

    upstairs stage land soft clue

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
