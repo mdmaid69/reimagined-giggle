8c7e711325fc93c43a7491509267d5549ed0bd31
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:13 +0000

    fit mistake tool until taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_binary(n):
+        return bin(n)
