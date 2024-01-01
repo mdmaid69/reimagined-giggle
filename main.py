75c3016caef2188d9c9d51305025ce6e4b4aa6a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:22 +0000

    friend moment intend tape swimming

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def calculate_perpetuity(payment, rate):
+        return payment / rate
