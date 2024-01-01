27bc8afef9141b2fa1a0725d9393feb552c0e0ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:27:32 +0000

    while could wire concentrate knowledge

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
