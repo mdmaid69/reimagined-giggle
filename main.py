628db77181d56581231d8090b0e9d7397c5de564
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:19 +0000

    hair pin keepoff dangerous confident

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+print(math.pi)
+for i in range(10): print(i)
