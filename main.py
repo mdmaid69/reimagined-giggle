b36124f1047b2bfc31db4b1a4ef48e3c4e513f9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:09 +0000

    practice reliable second window bin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
