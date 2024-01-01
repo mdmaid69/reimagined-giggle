99a27dc36467c5e8bd8f420df67b74c4311e8e30
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:04:41 +0000

    range more some bury advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def calculate_area_rectangle(l, w):
+        return l * w
