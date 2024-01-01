96080d793f8c4e7e010071ed0845729e5e89a116
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:48:02 +0000

    base minute grow prefer drop

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
