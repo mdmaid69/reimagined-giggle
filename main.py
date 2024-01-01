22436802585cdbb0388bd13df32f6033e2eb0783
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:06 +0000

    breakup university crash smile lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
