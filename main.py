bb74c56239e5709f95874073e7a078981bfcbde3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:23 +0000

    surface write receive pause increase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
