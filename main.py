cfa4513312d8621331d2baefa84d18f1779c07e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:09:47 +0000

    chin breathe connect foot trap

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        print(sys.version)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+print([x**2 for x in range(10)])
