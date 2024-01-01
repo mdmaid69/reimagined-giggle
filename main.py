e87f08b301a7140bb5e3fc162e9706b136553173
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:27:30 +0000

    relief cost could summer public

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
