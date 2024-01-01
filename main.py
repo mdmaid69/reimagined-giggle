4ab77492419efb00c32b6c0042e6c783dc27dc26
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:49 +0000

    night recently could bag quiet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
