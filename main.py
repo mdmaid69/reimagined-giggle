afb3af79c3fbbbbc26946e9c21ed4feb550164da
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:31 +0000

    how regret dry record insist

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import re
+print(re.match("h.*o", "hello world"))
