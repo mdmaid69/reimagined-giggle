4a2ab8f98527047251742cb8ab698c5c949acec8
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:44:05 +0000

    566end fold anxious ours set

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
