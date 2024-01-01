2f90aefea1dc17c8eb27a4e16d5d4307bca9588d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:16:35 +0000

    reply grade low attractive offence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def add_numbers(x, y):
+        return x + y
