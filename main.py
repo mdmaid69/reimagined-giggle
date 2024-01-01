47c802fd4a3b8aa5ff218b8caa753a9780630db0
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:09:29 +0000

    add dinner war cough tune

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
   def get_file_block_size(file_name):
         return os.stat(file_name).st_blksize
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
