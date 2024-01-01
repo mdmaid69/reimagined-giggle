2dbb27f205898643238cbc7a61f24d3aeecab048
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:57 +0000

    hello on refuse long public

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
