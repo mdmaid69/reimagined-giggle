8724ba194b67ad01f25e3ef22d365abc1c6f52db
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:38:53 +0000

    mouth slow undo however maximum

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import time
+def get_current_time():
+        return time.time()
+  def cube_number(x):
+        return x**3
