a1a5020f35c310f961250d4f09dfa82b10bcc49a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:32 +0000

    cheque gooff compete understand comein

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_speed(distance, time):
+        return distance / time
