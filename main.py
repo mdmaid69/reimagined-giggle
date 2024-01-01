d02dd636d9796bad255f4b364d798b3009d15398
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:52:51 +0000

    trap treat rate violence trial

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
