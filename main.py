3c2368fec2042986dd146ad314da7298fa3a0884
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:18 +0000

    peace dance water high space

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def pop_from_array(array, i=-1):
+        return array.pop(i)
