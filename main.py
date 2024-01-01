ca6efbd61fd18dbc31b834edd7f271f6f399c677
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:00:39 +0000

    black lesson sale through lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
