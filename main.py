16bafbbda903159d490e9d6722a49e92b08b05bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:05 +0000

    office solve sister burn buy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_list(array):
+        return list(array)
