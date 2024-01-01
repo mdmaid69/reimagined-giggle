a12debdb35c9bec413a5eae0ffd1938b527545e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:31:28 +0000

    city circle anxious advise rid

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
