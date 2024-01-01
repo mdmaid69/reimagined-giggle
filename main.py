6685f38b70594acdb4fb80acaf235b5ad6321b8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:49:54 +0000

    definite wind measure splash appear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import array
+def get_array_length(array):
+        return len(array)
