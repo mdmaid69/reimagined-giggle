b2a73fb65d7069b620b8413c01f32b3666fd67fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:49 +0000

    practice plan quick feature regard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
