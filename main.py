ab0b1c2339d72eae35b66a5d866f056e7a923b16
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:52 +0000

    them precise protect argue hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_typecode(array):
+        return array.typecode
