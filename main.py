d3e87b00abf0243b8ee1b3837cb9cee5ce7c7b2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:21:51 +0000

    question to 566end happen date

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
