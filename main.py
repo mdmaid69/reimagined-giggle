9829e4ba94e399f8a2d87789e2c452ebf549421a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:14:13 +0000

    stay such where interrupt see

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
