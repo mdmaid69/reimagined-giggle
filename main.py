68f85b0eae3c9c3be3359d0e98f8ac84c4529c7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:47 +0000

    outside rather star permission comedown

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
