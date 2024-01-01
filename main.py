e1d8811b5446e3f1a633dab1cb69c2c84a451c27
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:04 +0000

    load unable finger meet present

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
