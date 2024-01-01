f50e6c83dc10c7c71201f08dc5faa0fecc0d5765
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:38:28 +0000

    marry swim decide measure stage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
