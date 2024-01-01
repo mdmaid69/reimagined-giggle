5cc8147d090efda8f891a180a05b6c219bae5685
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:35 +0000

    lead subject confused capital serious

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import tensorflow as tf
+print(tf.__version__)
