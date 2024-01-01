b9b43a61da454d625520a681e5d869fed1cae17f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:58 +0000

    case environment clever judge tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import array
+def set_array_item(array, i, item):
+        array[i] = item
