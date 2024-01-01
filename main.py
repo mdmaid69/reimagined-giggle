4ffe1b731beb87d943425637fdda28a01443daf6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:11 +0000

    still tired soft once welcome

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_power(work, time):
-        return work / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
