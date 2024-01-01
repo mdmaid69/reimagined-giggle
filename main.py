b3c8fa7a9a49c151077b8c891cf97a51daac6d3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:25:28 +0000

    pause on edge danger exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_itemsize(array):
+        return array.itemsize
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
