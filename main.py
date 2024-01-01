074ff310ffaa4de404b6fc716d0f5c5ef9650783
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:42:40 +0000

    kick lost miss run afraid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_octal(n):
+        return oct(n)
