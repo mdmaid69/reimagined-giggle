aa8d9a117b24396ad23bd70ee057b10549323c7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:29 +0000

    emphasis advantage excited advantage Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
