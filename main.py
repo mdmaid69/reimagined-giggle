17f63d004c4f441a7f4264aef3838142acbe899f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:32:52 +0000

    back Oo convinced lot first

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_area_circle(r):
+        return 3.14 * r**2
