2ae624adb89f530f2d3ac363eaf5292536fad7a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:13:31 +0000

    niggaacademy slip yesterday try cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
