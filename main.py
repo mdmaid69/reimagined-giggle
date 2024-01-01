2d8dc2674067668db6ed11a6ba0222b90b4570f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:43:48 +0000

    dinner silent example hang follow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
