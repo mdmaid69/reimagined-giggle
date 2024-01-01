fe758e48f1dc01983eadc305c8055e4a310a8afc
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:01:34 +0000

    relation belong authority approval dry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
