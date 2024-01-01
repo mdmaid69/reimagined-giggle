4e4bfc06bc3c0899a5a3dd6ad8e816e14915c28e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:13:57 +0000

    kind thick intend lie pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def reverse_array(array):
+        array.reverse()
