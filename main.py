c762336606815e7aacd380562031a0e66ebde8ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:29 +0000

    exaggerate straight wrong trick pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
