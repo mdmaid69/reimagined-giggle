af82d66b84cb0df5dfdf221180014662466a76b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:57:05 +0000

    scheme anxious thorough flow high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
