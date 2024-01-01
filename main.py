0e88f76b91243861a5fc3034eb786099c37ed9db
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:12:28 +0000

    add turnon set drink extra

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import datetime
+print(datetime.datetime.now())
