1594fa770bcfff57c31b1e8210abeb25a590a8e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:26 +0000

    middle comeoff suggest bottom whether

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
