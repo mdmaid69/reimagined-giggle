6307a3e52159965aa327cacf710083ff78e08824
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:04:47 +0000

    either noise improvement free match

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def reverse_string(s):
+        return s[::-1]
