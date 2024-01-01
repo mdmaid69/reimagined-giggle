4a865fd6e01c6f283a0c277cd04c053f6a3dc4d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:25 +0000

    pick hungry connection aim mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
