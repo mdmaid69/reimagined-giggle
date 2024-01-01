8eb749cc5ee3e0ed1e42b87dd5841695a87b0398
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:24:24 +0000

    guard anxious target free little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
 import array
 def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
