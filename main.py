63757c4b59ab1a4d73e5b9107c5140ecfa55c838
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:47 +0000

    unfortunate bottom life set onto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def is_palindrome(s):
-        return s == s[::-1]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
