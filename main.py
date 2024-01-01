a215a6b12827e3cf09d2e9b0b0b3368bd703caa6
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:46 +0000

    dick off blind fight home

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
