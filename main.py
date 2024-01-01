ad03fb899b1a03e4fe80e009666b98065f03d692
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:11 +0000

    away pinch immediately pile depend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_area_circle(r):
+        return 3.14 * r**2
