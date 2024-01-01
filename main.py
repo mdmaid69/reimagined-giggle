0f157b6b3a84510e4bf61431069a45bd803c0fc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:54 +0000

    authority no know poor control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
