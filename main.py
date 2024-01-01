0d2a9787d31cac6a00231d09988387c3a9b720c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:50:51 +0000

    pity combine insist soon able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
