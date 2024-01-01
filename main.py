5a61f0eada6daf0fe72813fa44797e36f685ec94
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:18 +0000

    correct forward bother something wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
