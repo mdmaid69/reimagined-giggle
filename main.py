928b9af802e064f665fc74c6a384c634a8583521
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:52:55 +0000

    necessary attract praise lock unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
