dde4c06ad81a1043199b3585f093d7bbb484097a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:33:05 +0000

    possible hope makeinto completely man

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
