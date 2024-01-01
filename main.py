afbd258ad36918a235cfffa8b3a6995a363f2873
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:30 +0000

    no bury bringforward state run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import array
+def get_array_as_str(array):
+        return str(array)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
