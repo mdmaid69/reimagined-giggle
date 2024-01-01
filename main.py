945b30587a393a04eb12be827262572b21c625c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:49:30 +0000

    point runout above control ground

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
