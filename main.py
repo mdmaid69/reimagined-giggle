3ae0b298b00f3356f4737f878022778f380709dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:33 +0000

    honest declare love image exist

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
