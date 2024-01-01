05e7fe993bf82ea1507f967f55b4ec2f80b02921
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:06 +0000

    invitation strict argue declare city

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
