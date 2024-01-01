c13a32782b25710e1af587e7f8aae892304c5cf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:52 +0000

    new day closely sister plastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import sys
+def exit_program():
+        sys.exit()
