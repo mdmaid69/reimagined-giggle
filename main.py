3178c674d09db080315687e2d5b91b58c7c7b09e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:22:00 +0000

    extra Oo punish object so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
