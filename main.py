e0a69cd540ab55b1a27590082b7aa4a826140144
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:57 +0000

    572enter eat separate typical dress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
+def count_elements(lst):
+        return len(lst)
