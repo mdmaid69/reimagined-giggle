6afcb4a62f1866e571b596ba998f7f5aeed4957e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:46 +0000

    stage cruelty reject art simple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
