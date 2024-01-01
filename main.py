fd4c2ba9764db65146c690541b1e9d911122ab47
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:25 +0000

    test good crowded depend theirs

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_directory_name(path):
+        return os.path.dirname(path)
