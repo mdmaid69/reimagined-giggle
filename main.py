af3fca2e630d5df4cac3d7df7a9418b606590584
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:45:37 +0000

    fierce consider pleasant fear punish

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_index(array, item):
+        return array.index(item)
