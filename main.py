b1a58644bf72973783db8e67ca2cef722a4d8e10
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:24 +0000

    off much across imagine tongue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_user_list():
+        return collections.UserList()
