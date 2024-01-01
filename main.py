6f23c13f30ad142df15ddd721cdbf0cc9a21485b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:43 +0000

    surface value argument total flow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
