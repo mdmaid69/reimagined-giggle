cf8b36180cf3637109ce549fd0b1722e6b7f102c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:46:14 +0000

    bother interested lonely usually swim

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def subtract_numbers(x, y):
+        return x - y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
