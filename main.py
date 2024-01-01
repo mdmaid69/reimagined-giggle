24a68ea2ccbaf800f480fe6031d9fa17319877b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:32 +0000

    field range Ee ring cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
