62a85491efb39003a89effed453c38a908f9693b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:36 +0000

    contrast night dish label frightened

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
