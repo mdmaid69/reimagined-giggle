4178b62d8f259258333409a26d69dc20b37785c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:40:34 +0000

    field lost leave bottom break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def append_to_array(array, item):
+        array.append(item)
