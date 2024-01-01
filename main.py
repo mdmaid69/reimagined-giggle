4478c0fab56f7060307eb6a951b38b45c12823fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:01:37 +0000

    comeup pair unhappy hardly wealth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
   def check_if_directory_exists(dir_name):
         return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
