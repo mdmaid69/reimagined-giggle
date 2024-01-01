8533849d7bf8377db7eaba66b9ce68ed0884873b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:32:03 +0000

    habit common big stare serve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_bytes_from_array(array):
+        return array.tobytes()
