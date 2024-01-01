b6869d97583538330108f7716c213066cdb76208
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:43 +0000

    push giveout excuse above consider

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_directory_name(path):
+        return os.path.dirname(path)
