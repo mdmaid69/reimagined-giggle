2c60f516f382fb1492067fd8a5414dcb9f1523a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:04:29 +0000

    furniture care willing ever it

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def find_max(numbers):
-        return max(numbers)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
