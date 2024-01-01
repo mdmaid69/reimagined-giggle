24c38d277871da2be96cf18775673e28ce291043
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:52:33 +0000

    along represent warm stick payment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
