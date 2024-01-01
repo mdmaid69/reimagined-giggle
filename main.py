96244f7dd88cf0108edae1ed37f4696f98cb4be5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:49 +0000

    uniform coat bring now even

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
