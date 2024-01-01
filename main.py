8300ae50d98a8dde084cd029f861f58e9efacc3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:38 +0000

    afford more along healthy they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def find_max(lst):
-        return max(lst)
+def get_array_as_set(array):
+        return set(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
