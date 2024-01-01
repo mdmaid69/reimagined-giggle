1bca030b144dfe0195bcbc922168001b7a429c1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:54:06 +0000

    either distance whisper just exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_array_to_string(array):
+        return array.tostring()
