cc6907c282c6119bb6b034a337496729576ac537
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:42 +0000

    workupto peaceful adjective description stretch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
