2323998a250fe73a4a4f44c2a20b73c2f7ca1336
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:06 +0000

    church comedown fall scheme expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def split_path(path):
+        return os.path.split(path)
+import math
+def calculate_floor(x):
+        return math.floor(x)
