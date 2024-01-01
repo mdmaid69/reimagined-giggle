0d314c6e370bdae308cf9782e89ec700c45cc615
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:01:16 +0000

    holdout dry respect willing anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
