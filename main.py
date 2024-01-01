389a53439721d9d3ae92be53a7cd1c7ca42dec5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:59 +0000

    ill place borrow bite spill

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
