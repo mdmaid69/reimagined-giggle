f7083ccfb2b86f692acebce720d29aa2f3c3e481
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:42:33 +0000

    us gentle gooff wash polite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
