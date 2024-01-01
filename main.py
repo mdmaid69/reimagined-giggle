f37597c77ededf2914b87f9f5643c0e30f9faa32
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:39 +0000

    town keep glass brush considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def convert_to_hex(n):
+        return hex(n)
+import collections
+def create_stack():
+        return collections.deque()
