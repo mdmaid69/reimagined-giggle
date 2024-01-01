1e968b8abaec798145eb3c1f0fc84437a3b68c52
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:05:58 +0000

    bit attitude appear pretend comedown

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
