ec0591ded46017de5bc80379c8b3520be4dde0cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:26:20 +0000

    silent definite charge freedom expect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
