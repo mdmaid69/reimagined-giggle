05fa2808912054b756e93a99db09293cdd2032d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:25 +0000

    afternoon past from getoff reject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
