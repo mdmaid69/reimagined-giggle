e8441321b4bd6de6331f2b3f704e009ae7aef50e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:51 +0000

    will success shop most eye

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
