15e95bed970e7b897d5a569d3350ed280185cca5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:12:38 +0000

    comeup limit arrangement name story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
