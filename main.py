8d9b7d4732b765415552f2acd705894337598cea
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:17 +0000

    respect print responsibility blame bang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
