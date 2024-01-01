95a7b6922cef4ea108c951a5f097687ff6dde8f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:19 +0000

    university right hot plastic mother

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
