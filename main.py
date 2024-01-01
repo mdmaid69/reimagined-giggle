9c260b4401db6fa456bcb1eac62dab9242b20489
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:34:36 +0000

    refer colour visit sad emergency

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
