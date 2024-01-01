dea46aa96d2429008d9155d348436d0bea48ebc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:06 +0000

    climb proper contrast have after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
