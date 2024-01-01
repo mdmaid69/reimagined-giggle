030a4c84e1437a719b565a769da7f353d54b79aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:31:01 +0000

    early bang stir sight settle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
