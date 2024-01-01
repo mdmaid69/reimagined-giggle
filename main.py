33668ea57ca23f67434c0a4caf454b6dd2dfa04d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:22:00 +0000

    rather basic belief develop development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
