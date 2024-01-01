1b28e9c879df4a4d7faf728f853859532df9c02d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:19 +0000

    day bye hurry schedule memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
