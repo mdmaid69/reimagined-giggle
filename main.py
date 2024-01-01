56670a8ab85f1c9d6f96c1181d7ca7d4dc3ff2d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:20:35 +0000

    Yy private various disappear minor

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
