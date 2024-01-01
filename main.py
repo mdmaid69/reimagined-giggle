464e274d38a8528e25b2c17b38fd0915ad1fc326
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:56:00 +0000

    night make reject connected load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
