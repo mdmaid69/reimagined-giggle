b0c8fb8ab5351533cd6971a03e1f7ca9798a80e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:51:51 +0000

    turnover down little threaten pen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-print(math.pi)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
