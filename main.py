6c5f7b42e59bbef50c38acdc0b85866ff6494500
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:39 +0000

    hate under under stand behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
