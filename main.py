353e44b92ae3c3ac45ffb156d54a96905388b924
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:22:42 +0000

    reply beneath region debt confused

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
