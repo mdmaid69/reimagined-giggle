6d10127f5be018edc7285f51c995a7ba15965c92
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:03 +0000

    rich memory eat discuss call

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
