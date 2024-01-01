e0e07d4c37607945d76fb3e18aea5c06ee9f91e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:49 +0000

    petrol describe connection advice cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import sys
+def exit_program():
+        sys.exit()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
