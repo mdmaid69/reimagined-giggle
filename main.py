d8baca1cee6a520502682c037ae5fef8c212ee39
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:23 +0000

    me office keepout sensible serious

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
