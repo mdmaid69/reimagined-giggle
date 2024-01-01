000500011bace36937c3854ceb0a57f388332317
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:49 +0000

    view nonsense after angry accidentally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
