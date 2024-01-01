0236b5ba1f5fc79047b5c204001360d6a0d6ef8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:11:23 +0000

    section confuse whether replace afford

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
