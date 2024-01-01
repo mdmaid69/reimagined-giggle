34a0bdb1fe5d47916dfeb835e58f4e3bec86023f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:41 +0000

    and young mixture blue peaceful

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def convert_to_octal(n):
-        return oct(n)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def convert_to_hex(n):
+        return hex(n)
