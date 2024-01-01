297f99b99468eab252b61028b73a18a7f93fcf8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:46:04 +0000

    correct child tip attach prepare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
