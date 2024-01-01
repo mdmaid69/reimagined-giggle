db268ce81b550a3d4a850dd2f909da97a29b5a96
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:54 +0000

    system since attract work decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_item(array, i):
+        return array[i]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
