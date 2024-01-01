0086eafc9c103c4d211ee6fe2113feafa980d679
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:25:37 +0000

    travel have feel before minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
