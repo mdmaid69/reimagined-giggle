ab202ceb62c74bcec6803afae1cc0e565635ce58
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:37 +0000

    personal history chief cause public

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
