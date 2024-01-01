a7c34450e2940f7f7c1a6b0290b04ce49dae8daf
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:37 +0000

    571enough tidy level crazy over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
