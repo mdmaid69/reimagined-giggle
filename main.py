1ad3b3b83e27a3231102dbca2848d1a4b51c362a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:12 +0000

    comein imagine school inform punish

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
