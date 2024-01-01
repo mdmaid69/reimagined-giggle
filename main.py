cca8b8e471bd0bdb3038d2e31408e8d4bc62741f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:01:41 +0000

    necessary breakdown fair emphasize mine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
