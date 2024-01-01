41fbdc4cc0886f1068af6d628c7deee57d471b79
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:35:34 +0000

    eager wash country message opponent

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
