cba338928deb7097d8ece3b22d722f6d878336ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:21:26 +0000

    obvious bar organize surprising loose

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+from collections import Counter
+print(Counter("hello world"))
