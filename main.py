95ece132606df7d61ac876c12dfa86c2e4c82d12
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:48:33 +0000

    anyone failure duty trial reserve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
