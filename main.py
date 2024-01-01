de41ff686ef075ac4cc5e445c80d61d791ff05be
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:31 +0000

    usually slow very plenty anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
