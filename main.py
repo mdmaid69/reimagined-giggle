6e09c19bdafe84540a9d580773630e53e41f5c07
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:39 +0000

    hospital clear Nn test previous

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
