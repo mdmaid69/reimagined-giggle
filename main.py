7f752bb834a12be21db48b87615daadad749d090
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:16:53 +0000

    runaway but consist dead back

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
