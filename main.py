cf5fb0cbf463840b4f120eeb18b0b714bc06fba5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:41:44 +0000

    curious new carry treatment scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
