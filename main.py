a1731c52d4dd9f84ada7640da070c785b38fd083
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:13 +0000

    every keep warm leave bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
