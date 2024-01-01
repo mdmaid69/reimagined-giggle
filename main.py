b6d2fdf7ffab63e991efb377e13f3aae2eb414d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:12:12 +0000

    information purpose fix both crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+for i in range(5):
+        print(i)
