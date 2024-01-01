28ead5d5b7118b87045de7b76f03a8701e00fa9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:25:11 +0000

    period help thought clearly waste

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
