f7390060aee8a5240faaf3f0458f2189f2382374
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:36:03 +0000

    drink narrow clear sense trap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
