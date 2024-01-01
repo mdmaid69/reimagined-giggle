a08a7afdcc6dabf963244788acce2a15983a7b2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:42 +0000

    on anybody weekend flood go

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
