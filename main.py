0540b200494420f7c461145a7d660ca7df85a52a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:59 +0000

    yourself several connection sticky speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import time
-print(time.time())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
