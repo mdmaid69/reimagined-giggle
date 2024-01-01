a510a9e8d8b6f186ae8e48078afdf7236f72ab55
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:41 +0000

    regional realize close reasonable form

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
