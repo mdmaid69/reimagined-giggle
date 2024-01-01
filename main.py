b77672b1b8c379e0dbd9088fd8cf0e0683f6c496
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:08:04 +0000

    hurry string cruelty apart ride

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_unique_words(sentence):
+        return set(sentence.split())
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
