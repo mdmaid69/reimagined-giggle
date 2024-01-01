9575ca09097e82b03ea42d25aba8f9b9cd6e96db
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:43:37 +0000

    cut activity lookback(on) hurry really

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
