29ca3c0521aff4df0d43e15672852c1afc8f6ed1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:51 +0000

    every behave party proof welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
