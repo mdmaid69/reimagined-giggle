ec21d82c16c1993e9a3d767de0b755e249065ca0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:34 +0000

    doubt breakup look centre check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_itemsize(array):
+        return array.itemsize
