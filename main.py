f491caa02e3faee353cb6fa19984738b4f1565a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:25 +0000

    rescue work strange convincing vote

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
