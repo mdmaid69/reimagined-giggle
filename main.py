925e735ed8150bcc222cb7144e018134a3444f14
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:30:01 +0000

    rank precise floor stay ball

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
