69f77c896d55a70570ad86bb4de58be971f6a858
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:18:51 +0000

    between extra regret be better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
