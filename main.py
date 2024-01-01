490e2ee83ce8af26273c912e5f7b7d3b5d043a07
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:42:14 +0000

    anybody accordingto fashion miss dream

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def get_time_since_epoch():
+        return time.time()
