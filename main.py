994daced0f0643067d07ab3d5aab04cb3546625e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:10:51 +0000

    team replace centre lie frequent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def find_max(numbers):
+        return max(numbers)
+import array
+def clear_array(array):
+        array *= 0
