04c7e186e3079bf0afa10581b74129a701cb8304
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:55 +0000

    scene include have we fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
