c9dce84b60f9f92115f83200306e41ce70248a99
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:35 +0000

    ready process pollution organize happen

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
