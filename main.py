8c96ff220c15ae93a73fd5a6a7f7ffb467deae91
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:06 +0000

    beyond wash sail disagree discover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
