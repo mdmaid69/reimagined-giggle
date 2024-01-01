895f5d73265c7d53b2308649f144b04b3c6de5cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:23:55 +0000

    giveup purpose value action skirt

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
