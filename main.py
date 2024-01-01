a353903ce9e0c8757f423163ee644a9b1c61611e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:30 +0000

    usually excited himself ours foreign

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def sort_list(lst):
-        return sorted(lst)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+def convert_to_octal(n):
+        return oct(n)
