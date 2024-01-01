6f92800aa4fb6da15684a6699413c57965bc1811
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:22:24 +0000

    cut clear doubt certain sea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
