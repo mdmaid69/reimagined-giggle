266d14168e930e762f967a26d955353fcbf49f67
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:21:58 +0000

    crazy professional firm bank quarter

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
