aadeea35b83fe3d3c92265974b0c2c86243ad730
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:23:38 +0000

    influence otherwise duty similar steer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  def square_number(x):
+        return x**2
