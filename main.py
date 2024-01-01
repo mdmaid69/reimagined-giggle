0d84f9e0743d8599629fdf2d36c0c256062440c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:58 +0000

    point realize trap narrow blame

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
