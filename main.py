4ab9d5424cd68266cd3e585b0b6eeeb7b27c6551
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:13:09 +0000

    trip front chin murder pocket

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import os
-def remove_directory(path):
-        os.rmdir(path)
+print(os.getcwd())
