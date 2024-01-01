a328fd99a796d4cefcbdce7f98166e7e7f4b04b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:58 +0000

    until stamp goon comfortable event

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
