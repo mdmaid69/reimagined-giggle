4cd8e4eaa97a7710d26dbe25c3d690a451334fbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:23:17 +0000

    what replace emphasis callout cross

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
