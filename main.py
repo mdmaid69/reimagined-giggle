474942812bbb090f4d72f823a439349513e9939a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:11:17 +0000

    figure contact still pain sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
