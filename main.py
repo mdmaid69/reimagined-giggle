e1936b81600ff54cecb7951898cb40a1f56c8c34
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:42 +0000

    language game represent remark report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
