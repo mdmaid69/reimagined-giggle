5f6cddade70e5925530a97b9212757a296113cb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:24 +0000

    class journey agreement breakin talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
