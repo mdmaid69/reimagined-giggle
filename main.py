135328c4d1913bb9ae2d43f3fe69e96cfeb946dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:31:04 +0000

    butter exploration terrible date both

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def convert_to_binary(n):
+        return bin(n)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
