14fda6b488bb5efda6a847c4132c2707649f7dff
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:05 +0000

    reasonable reach dick uncomfortable importance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
