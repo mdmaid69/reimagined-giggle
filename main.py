ed6b941d4c1db647242577e897aa79ca4767a63b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:04:31 +0000

    thought settle sing charge treatment

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
