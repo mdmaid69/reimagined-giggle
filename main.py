9c75259fa0b41d882d54fd9d951b3b1de69fd7a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:03 +0000

    worst science activity bank spin

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
