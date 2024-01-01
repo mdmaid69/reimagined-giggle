94316ebc7b247608dd2db6dd37e49aa16c79f854
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:33 +0000

    easy ashamed female yourself behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
