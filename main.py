a7d57659ced5494922b401cfdb0e4fd7eeec8b3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:18:58 +0000

    unless soft instruction both flow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+print(json.dumps({"name": "John", "age": 30}))
