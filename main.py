3252d91355598da46dfee641eb22850a4b2825f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:42:23 +0000

    view pollution refuse worried set

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_area_rectangle(l, w):
-        return l * w
+  def is_even(n):
+        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
