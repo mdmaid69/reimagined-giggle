49b7ff099e8aa07429ada8f19f1f89a05e738acf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:36:33 +0000

    lead stamp dare neat shape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import sys
-def print_python_version():
-        return sys.version
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def convert_to_hex(n):
+        return hex(n)
