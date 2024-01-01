03b3d4ac443d94fb9db85d3b044b979faa03f99b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:51:12 +0000

    should prepare smart his spot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
