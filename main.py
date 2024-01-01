bde82042161a1878088f78119d8000fa857827c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:18 +0000

    Cc star anything basis main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_sign(x):
+        return math.copysign(1, x)
