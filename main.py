d87723b4e1b85328e5511b60cd182bfe1aa82351
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:43:13 +0000

    on pretty visit simple minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
