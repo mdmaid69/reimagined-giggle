b749cfd369e765572e54c74c38fd426fed0e2cbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:14:03 +0000

    paper city thumb content defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import array
+def get_array_item(array, i):
+        return array[i]
