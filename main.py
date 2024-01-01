e67b2f097d5af6185d9957ef3a36c8b8ad2d49c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:57:41 +0000

    to early obtain grade bother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_as_list(array):
-        return list(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
