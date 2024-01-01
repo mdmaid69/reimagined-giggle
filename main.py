e792b31d9134b5e26e443e1dc48527137ab3d60a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:45:49 +0000

    wait conduct another law copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  def count_elements(lst):
-        return len(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
