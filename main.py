635b0970cb4dd8a7efae7984850ce10e1c128e6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:58:44 +0000

    last takeover lie autumn basic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-print(os.getcwd())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
