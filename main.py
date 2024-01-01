82e44eb233a2d98b112f0803f43b55508f904f78
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:34:20 +0000

    now each official why paper

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+print(math.pi)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
