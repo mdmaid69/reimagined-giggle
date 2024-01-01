8050e6990d335724b7a51e791cf2436078906aca
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:26 +0000

    flat before uponon wear sometimes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
