5fdf9eaeb8025d1f39756b9646d7e02d13681ae6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:04:28 +0000

    wind movement attract provide gas

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
