e65d0d042f064d1678c46ed28d2cd0be3d7f5ceb
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:01 +0000

    Qq act clearly time neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
