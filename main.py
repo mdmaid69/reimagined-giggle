3dc368e5cf560d668f4e1bc98aef7f08e039ea0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:21 +0000

    dream smooth sight towards sudden

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def square_number(x):
+        return x**2
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
