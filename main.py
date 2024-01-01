2473bef306b8eecd1564a71a1aab0ec69ef3551d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:57:44 +0000

    various importance chin future second

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
