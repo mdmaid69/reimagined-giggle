b591e37ad7e1101e0746837dc32f0e844ceb7230
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:10 +0000

    season different coffee rent absolutely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
