3c42a25ab3352ce54d800ff12cc1d39a1914a799
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:26 +0000

    float colour already simple apply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
