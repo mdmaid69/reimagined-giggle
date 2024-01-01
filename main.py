961304d4e1a6a6d1c1efd3c65cd646bec4c5e7a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:24 +0000

    work ago wall way mind

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
