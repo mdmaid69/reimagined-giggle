165e532153089c4bafe002da7b315ee81d2fcd84
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:03:10 +0000

    no goback(to) information thumb between

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
