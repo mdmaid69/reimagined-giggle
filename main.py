cfb8d83faeb9f9fa5c384f85c79ca2cfb941bebb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:58:18 +0000

    pencil that high talk tread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import tensorflow as tf
-print(tf.__version__)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
