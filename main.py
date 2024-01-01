de4de9532d3e292a0e8fdcb819e375f9dbbfe57b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:51:20 +0000

    care father scrap nerve curious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
