dad9bd63e3b18f6706e7c7d980d740afef8b29bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:59:49 +0000

    recognize affect drive during division

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def reverse_array(array):
+        array.reverse()
