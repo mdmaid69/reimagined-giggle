d2a090414a18453f9ab27feed5e1ec8abff13849
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:19 +0000

    usual father control order date

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_repr(array):
-        return repr(array)
+name = "Python"
+print("Hello,", name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
