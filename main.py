e258eff6e2c87542b510900f3f51dd0923dbd115
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:33 +0000

    experience create attention tall fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def append_to_array(array, item):
-        array.append(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def count_words(sentence):
+        return len(sentence.split())
