2b5f4890080d85ecb6299f53d699de343e4cb547
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:21 +0000

    challenge dream fire maintain get

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
