3cfe655897afafdd1b8d038eb64069191a295795
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:45:27 +0000

    scream while little good business

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def reverse_list(lst):
+        return lst[::-1]
