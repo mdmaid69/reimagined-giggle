930b07adaef81f03d864cfda89a5123ed6e29bab
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:30:08 +0000

    comeround clue statement pipe entrance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def square_number(x):
+        return x**2
