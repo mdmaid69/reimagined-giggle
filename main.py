c04b1d06809ed513c26877781d16a7c46c5b0b37
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:46 +0000

    tidy switch support stay shade

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
