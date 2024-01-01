245016b8895abe39ad93e7ae9a477c7095c715d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:22 +0000

    I nowhere worse invite usually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
