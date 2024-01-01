1a58872f707ba9365016aa6fac566987eb0fe76b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:06:08 +0000

    used pull bang wind score

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
