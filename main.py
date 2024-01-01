aa0cec1a0d642a7f21907b964a9480cebf2995db
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:27 +0000

    afternoon sting getback any must

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 def calculate_eps(net_income, shares_outstanding):
         return net_income / shares_outstanding
