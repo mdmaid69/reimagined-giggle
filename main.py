93097ddd7eb43a55aa894366c2ef3164e412fbeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:04:14 +0000

    train if nigga be defend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
