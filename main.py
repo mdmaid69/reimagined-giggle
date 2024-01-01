17187a677fe4cbf4d77ed2723f55ff645200affd
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:20:15 +0000

    spend aim space story fine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import numpy as np
-print(np.array([1, 2, 3]))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import sys
+def exit_program():
+        sys.exit()
