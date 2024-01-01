4d0cbfa22bbe7e0f2c7e5f4c52b78059472b7dd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:15:46 +0000

    direct inside meanwhile visit modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
