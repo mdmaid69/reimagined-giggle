5c3a6141b1b88d887ecfdde070e51f069fac0976
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:03:02 +0000

    getback correct pattern stick disappear

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
