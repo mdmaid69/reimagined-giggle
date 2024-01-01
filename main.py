0dd6dc07f7b401fed6412dac3ace8123e295c2aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:41 +0000

    fail quick may print discover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
