2fcef47112b6cd115027a62b7a1ea899ab8fae52
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:17 +0000

    heart work whistle string shop

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
 import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
