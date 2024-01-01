949457f2b3d1725c2025e4f20170376c995454a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:09 +0000

    suckme dead see ear never

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+for i in range(10): print(i)
