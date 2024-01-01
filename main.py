800b0132d188d894148df5202d81b16a60b106de
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:19 +0000

    demand kill unable purpose objection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_buffer_info(array):
+        return array.buffer_info()
