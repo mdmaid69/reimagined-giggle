fa63bdbc76ac0cdd2615d0e6fc7fcaf43b6612d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:37 +0000

    release respect the drink line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
