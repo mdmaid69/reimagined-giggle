5682e9f6ad79255ed06de53c0acbb27aeda3ecf9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:30 +0000

    door frighten behaviour weigh fold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_variance(arr):
+        return np.var(arr)
