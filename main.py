4039221862f58f0dc10274a74d6fb256a067504f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:34 +0000

    cut whenever sorry ought wish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def cube_number(x):
+        return x**3
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
