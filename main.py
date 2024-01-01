d22a3392fc74f6ffa929318107aade41e271a5a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:43 +0000

    plenty arrange comeoff group interest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
