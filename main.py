4afc73a69c16603ea42cee0b904ef2e72c4f768e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:53 +0000

    suckme communication mainly approve colour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def add_numbers(x, y):
+        return x + y
