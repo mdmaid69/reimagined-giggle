90e5dbf389e3ed1137c997d523cef4ac2e0d4fe3
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:25:52 +0000

    565end light clothes copy hit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
+def calculate_work(force, distance):
+        return force * distance
