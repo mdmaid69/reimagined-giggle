68919b5a1ce34dabbc44728ccc3f22d63af4a0f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:26:50 +0000

    achievement keepfrom without tape lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
