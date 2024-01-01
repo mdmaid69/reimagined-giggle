bc961c52eacd56792969dcc5c00bfba007b4c029
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:26:23 +0000

    gift comment keepfrom clean win

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
