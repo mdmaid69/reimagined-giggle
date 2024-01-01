71c6e7b59c9ea57c1d6227e60b8c6c3e9e6ed862
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:30:15 +0000

    promise 564encourage regular treat through

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_as_set(array):
-        return set(array)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
