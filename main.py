f94781716e6789dcf6c3ea1fef00952d3f09ff30
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:55 +0000

    rise turninto section watch switch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
