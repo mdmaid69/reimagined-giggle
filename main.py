f33bb6f5d8d59d787fb3f7498c832609ff27a269
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:35:13 +0000

    another memory minimum bringback best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_tangent(x):
+        return math.tan(x)
