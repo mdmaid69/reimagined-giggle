2133f2e5ede6b115fdc0d627931f49ca44fad8b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:33 +0000

    tense you actually somehow unhappy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
