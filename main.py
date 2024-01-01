3d3c8c1197452ea352ae4d81a36902bdc530a302
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:53 +0000

    turnup Ii anybody past cry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
