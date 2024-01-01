342a5b175c08d7f40a0134873dca0ecd3117a3dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:39 +0000

    appear spot defeat drink hide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import numpy as np
-print(np.array([1, 2, 3]))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def is_odd(n):
+        return n % 2 != 0
