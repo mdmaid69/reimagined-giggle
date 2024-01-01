ef155c4592c5e2c9451a7681373e5867a70abb60
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:39 +0000

    brush very interview tax suit

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
