657be9885bfbde4c992ece1bd4454a56fc18d767
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:53:32 +0000

    part feature lost unlikely strange

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_work(force, distance):
+        return force * distance
+print(sum(range(10)))
