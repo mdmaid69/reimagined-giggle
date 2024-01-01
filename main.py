d6ab4914dbada2ef900c87cbdde791f09a3a96de
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:26:38 +0000

    embarrassing gooff experience this natural

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
