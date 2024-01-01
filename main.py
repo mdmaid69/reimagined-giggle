0a60dd14e3ec5e95e277515b629c96c39d724d75
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:39 +0000

    worried payment private possibility night

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
