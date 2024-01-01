0844b20c1d33b52206bebab55b0ce6b32d81941a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:13 +0000

    best possession empty himself progress

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  def convert_to_binary(n):
+        return bin(n)
