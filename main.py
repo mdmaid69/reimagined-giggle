453bc5c75939ff55cf8f818ff014cc5536a5d47d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:15:49 +0000

    Rr enthusiastic step typical promise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
