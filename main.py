371ef33d72876d147825410cb5217e05759eb090
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:07 +0000

    unlikely sight warn she need

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
