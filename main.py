5213234523af1e2d790cc0be82f2f241b4bf7c9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:48 +0000

    drive dry remember through control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
