a993909b1e5a61cc75319f997fab2ca34555898d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:10 +0000

    climb live Ee unfair without

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+for i in range(5):
+        print(i)
+def count_words(sentence):
+        return len(sentence.split())
