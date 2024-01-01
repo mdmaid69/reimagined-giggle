9882d40767ad44908dd12ac796efdc6980870d10
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:58:23 +0000

    soon depend care exit store

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
