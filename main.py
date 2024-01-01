1ba204b6572aedd75f8bd2b5f42e4174241d3cf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:07 +0000

    present ashamed careful will refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
