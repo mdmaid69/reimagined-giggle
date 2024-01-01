e1f72b2a68831a808bba9cd04bbff6fba3b2a089
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:23:30 +0000

    most instruction somehow pick congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
