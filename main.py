4227e812db60913837500d4d2c49fe5e97bf23fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:16:32 +0000

    finally hurt pay chase include

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
