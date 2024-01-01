8c304badc8567ab9866d65af5cc356d88d80a6b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:50:34 +0000

    careless steady clearly ring long

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
