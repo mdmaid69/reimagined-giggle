2c531a0496ab2cd81e51884d73d7fe63468c80f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:58:30 +0000

    willing fine provide slowly breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
