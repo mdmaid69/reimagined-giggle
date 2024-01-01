d56bb4f956f56d744fa0a24ca992d878fc24a08a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:27:19 +0000

    connected trust furniture bar minor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
