08df763ff7aa2bff13f6b2b4be02212b88b0f71d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:05 +0000

    meaning cause prefer bound science

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
