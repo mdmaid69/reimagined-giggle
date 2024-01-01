54750defc8d1a092dd4cd6d748fb84a81c257151
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:32 +0000

    slice emphasis bang trick would

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
