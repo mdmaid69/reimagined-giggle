76b7c7d97fe8fd4d988b5088ee9ae5d58ae0d8fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:28 +0000

    sleep punish ourselves fierce print

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
