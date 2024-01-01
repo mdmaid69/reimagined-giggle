2ac84c2b6157ecb1951fd994dd7c732270a22713
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:24:30 +0000

    border clearly discussion indeed sleep

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
