db4070f6db2f8eab20e05bb2963e36201cfc7a0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:35:27 +0000

    eat level giveout by brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def multiply_numbers(x, y):
+        return x * y
