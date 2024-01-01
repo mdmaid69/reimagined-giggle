a011b94816f1f988237acdab13c7339da30bba3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:42:43 +0000

    pronunciation almost dance face test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
