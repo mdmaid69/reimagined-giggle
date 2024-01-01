42513b32e712d7afe6c30159db68a7db8ab91b4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:35:41 +0000

    interesting plenty from breathe compete

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(10): print(i)
