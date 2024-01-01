e39730589ec4daaa77d56411ad3d1ad0b7f871b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:02 +0000

    in hang put free yet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_arc_tangent(x):
+        return math.atan(x)
