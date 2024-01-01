ecce37c605e1c03134516a8415b5887d161d9601
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:53 +0000

    taste such outside area live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import collections
+def create_stack():
+        return collections.deque()
