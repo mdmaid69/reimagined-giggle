c9b4ea367703d3bd4561a370a671d89140fcd0b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:22 +0000

    dance drink business will possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
