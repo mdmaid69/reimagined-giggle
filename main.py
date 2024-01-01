7c204c4d8f5a3788507eefb9943adc31620cdf6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:36 +0000

    shut borrow remember scrape male

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_item(array, i):
+        return array[i]
