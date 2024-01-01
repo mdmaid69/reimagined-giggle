c3d171ea57dcf21e106a5139a29207214abf3e19
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:45:33 +0000

    disagree trust fond yesterday compete

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
