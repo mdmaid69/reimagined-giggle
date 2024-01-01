2ad13c37b9b8b72200144ecda8e18edf676fd311
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:24:19 +0000

    once step describe twist bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
