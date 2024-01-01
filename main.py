5edd545c45080d84ee49fcc678797e092ae70829
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:20:07 +0000

    off best repeat general distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
