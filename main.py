a9df6d964a03a46060dcc1cd555fc2ddb111c154
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:45 +0000

    secret occur see considering complete

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
