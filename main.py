ddaa3b249ca15968eb7564d7d79deefeee273700
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:52 +0000

    poison house uponon influence damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_typecode(array):
+        return array.typecode
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
