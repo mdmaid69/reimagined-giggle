b2f59e55e2ec3825e9bf7e483a92e0d2b0ae7eec
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:34:57 +0000

    what wish pause cross tour

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def split_path(path):
-        return os.path.split(path)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
