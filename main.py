0c4ba3e55d882efa6bab181534a839ce855cd161
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:56 +0000

    know embarrassed shout separate takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
