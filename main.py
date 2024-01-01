81ee985333eae1f0fc3b3c8366e3f345d67b1326
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:45 +0000

    off sell usual you natural

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
