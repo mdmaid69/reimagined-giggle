a9d44a081fda448d4d0f141ba73c89db71406257
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:52:41 +0000

    anxious help relative brush pinch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
