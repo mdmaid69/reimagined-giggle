4c50665d92962367420ca53d66213e94db508733
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:58 +0000

    street spring wherever film subject

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
