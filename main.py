97289c94f3588733ad088300382f5ee41b810afd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:28 +0000

    criticism badly platform production protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
