96209dcecfa662d22c2ac3e1eeb430b5018190b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:39 +0000

    appear awake sail slight heavy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
