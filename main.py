5089cba8679dfb3929c15e5374dfa930b313101b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:25 +0000

    last walk include lookback(on) hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def find_max(numbers):
-        return max(numbers)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
