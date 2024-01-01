5089da41ee415ad213061710e47e0f032ae908bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:18 +0000

    funny create whenever birth sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def is_odd(n):
+        return n % 2 != 0
