b99c9d690aa139127c163728c3825e9ac84fc032
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:02 +0000

    sort run talk keepfrom human

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
