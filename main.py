8065156dc9d22135867b8ff7f4a7927ba881e4ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:12:31 +0000

    interrupt majority deep not succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
