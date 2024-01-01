6e0132d58137f3c462107b4c381f8d3a9132e833
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:00 +0000

    violence stamp right tell sick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
