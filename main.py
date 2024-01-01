e91bbd9bcfcdb914b9f91cddb6def75f35fa46fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:44:22 +0000

    sure consider Ss comment face

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
