be791f7e1b173e3ea3b5f31baa3fa5c896087d1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:47:02 +0000

    take workon decision rule pattern

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
