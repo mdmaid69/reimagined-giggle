cb2f53f29c914fa336965fb4750e09ed62a33780
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:53 +0000

    late year thanks mood offence

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
