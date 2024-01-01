f6d68f723e235b358db04ab9713b3baa5a16b009
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:03:47 +0000

    unfortunate check art whatever judge

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
