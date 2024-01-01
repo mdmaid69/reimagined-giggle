acab630658b614e587f4a2b295a2617a21dff136
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:55 +0000

    steer however over worry however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
