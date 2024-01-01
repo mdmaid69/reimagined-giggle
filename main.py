0224cef89006e9e52d04ae7df4ec3e680b29a621
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:23 +0000

    there exciting comfort keen favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_float(array):
+        return float(array[0])
