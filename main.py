a45a9bbec85496c4a105b0e23cfc24924f44a482
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:09 +0000

    strike way abroad spare suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
