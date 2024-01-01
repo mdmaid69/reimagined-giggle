8dfdec3358682ac5effe19706d10c14832f61630
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:19 +0000

    cool after organization more upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
