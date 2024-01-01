aaa050c8f5210dfd6ca53768f7d42a0c863b1478
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:30:17 +0000

    justice sale result risk each

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
