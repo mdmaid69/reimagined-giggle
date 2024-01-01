dbbaf8eac6aa009748ef67676458f3b3ae70bb64
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:27:51 +0000

    big various quantity season confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
