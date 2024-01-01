52c58429f1ded7c42ea80612ed8eea1c8a85b126
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:09 +0000

    bin practise father defend tax

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
