df0fb8c519a5c7de9e9f5e65ee58b4cd79c85e82
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:30:13 +0000

    Uu skin keepfrom emergency edge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
