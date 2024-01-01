98597c208c963a0d536a5320b4635e417c3a9db0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:21:52 +0000

    post strike rough no meeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
