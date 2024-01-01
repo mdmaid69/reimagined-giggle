ce2a4f1597bbc4e3216fad1be9e623541a2c254c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:21:35 +0000

    indeed road begin be middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import json
+print(json.dumps({"name": "John", "age": 30}))
