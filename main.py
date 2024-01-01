534cdddfb6f95872687947aeb679dc5e079829f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:37 +0000

    bother beat square worth callat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
