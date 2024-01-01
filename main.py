f29cc8c308921b76de8bba0fa69a98747fe56880
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:58 +0000

    temper thumb disagree damage condition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
