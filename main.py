539878cdd345c9265358e03bff1b32e476d8f2a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:45:46 +0000

    provide collect used closed form

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
