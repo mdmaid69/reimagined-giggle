cbc6dab5b4a1cfeaf05ceabd06f61a3adf02af9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:54:18 +0000

    concerned permission vote bridge once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
