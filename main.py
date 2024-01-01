cfe3d491392d1307efa82c8fd368686c1831768f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:05 +0000

    deliberate behind create form undo

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_queue():
+        return collections.deque()
