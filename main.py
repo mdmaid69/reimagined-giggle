bd2c4fc4c1fddcbd1e40647939981d9a411cbd13
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:31:46 +0000

    ball himself roof right measure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+print(json.dumps({"name": "John", "age": 30}))
