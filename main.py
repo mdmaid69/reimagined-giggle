2bb420d17dce6941e93f1d5b04dc02e613726e0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:06 +0000

    minority bear walk available past

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def get_array_as_str(array):
+        return str(array)
