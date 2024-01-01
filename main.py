650e1416975ec92e362fc42a557d06e8d60092a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:27:56 +0000

    point stiff doubt and correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
