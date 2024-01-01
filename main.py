b0ab10698e6485a28f9cb5ef1e578ce8c296a58f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:45 +0000

    ball knowledge deserve disgusting design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def subtract_numbers(x, y):
+        return x - y
