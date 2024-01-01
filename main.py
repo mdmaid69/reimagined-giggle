4009e8f52b1703c13c06213aed90a89c048168e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:25:39 +0000

    roll bottom hate important certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_list_from_array(array):
+        return array.tolist()
