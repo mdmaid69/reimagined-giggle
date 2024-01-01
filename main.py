50bbc9c2d350a98952c4b022a4be7487b6db6184
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:24:05 +0000

    large operate zero television permission

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
