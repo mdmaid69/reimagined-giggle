2260ed0a05ecce52cd48cb24dbd9c11156c35c27
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:29 +0000

    low blame quiet themselves sing

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
