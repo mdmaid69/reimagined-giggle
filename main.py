9ad261f57025922624cbe3d19c66aa7644318f56
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:34 +0000

    beyond safe means instead instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
