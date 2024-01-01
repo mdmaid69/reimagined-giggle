e8ea3b759c2bd876d2813700e3c91a375e04706b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:22:23 +0000

    welcome bye them home authority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
