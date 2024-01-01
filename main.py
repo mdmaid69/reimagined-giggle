0fa9b3fef236c963ed310ea35984a43d56ca0439
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:46:01 +0000

    remember thirsty needle material secret

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
