28d92e11dc6db344c339503d6e58757373c592fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:32 +0000

    interfere holdout unkind for refuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+def check_if_array_contains_item(array, item):
+        return item in array
