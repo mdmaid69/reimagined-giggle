58656f402b59e6f1f0207f2998d46e3b28f163d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:31 +0000

    congratulate discussion centimetre break unfair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
