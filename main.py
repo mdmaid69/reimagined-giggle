42160bd715b13b5977eb11e47a02ecb05fc37f05
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:28 +0000

    heavy Ss lonely precise life

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
