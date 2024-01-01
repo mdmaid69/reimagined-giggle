d72aa3c8e5040fe6eb8177110e7b9428c42be31b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:31 +0000

    anywayanyhow short power fear long

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
