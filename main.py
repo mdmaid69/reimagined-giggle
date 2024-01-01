167ab01470f794efa22727b7949189e5b70847fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:14:29 +0000

    finger sensible stupid one relax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
