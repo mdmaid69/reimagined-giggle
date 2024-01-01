a5b28cdb3d578da14f48f362e3e541dfa2624ee7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:35:53 +0000

    several stir stamp big meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
