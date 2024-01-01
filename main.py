6b7a214a9fbe163fb64c22dca36daa270ea047ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:09 +0000

    sound prepare various as why

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
