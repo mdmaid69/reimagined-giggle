580bee6ee55ba2022b67e128161d66f805e9ce6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:11:41 +0000

    phone dream long us violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
