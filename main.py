270729eeb577dd99521d304b31ff8fbd8fc4c581
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:04:55 +0000

    sentence mass attractive message Yy

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
