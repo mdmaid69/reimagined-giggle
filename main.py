935ac93410d9a8531628adc6b4a5ffc3251dbe98
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:21 +0000

    arrange appeal thinkover visit will

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
