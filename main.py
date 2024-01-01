2d2963cb10e4c75375f05d4db182781e61c64845
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:44 +0000

    space 567enemy factory deliver careful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
