ed12e81523304c16115c9fe542ef43f20cff7aaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:29 +0000

    still tired far copy flow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_unique_words(sentence):
+        return set(sentence.split())
