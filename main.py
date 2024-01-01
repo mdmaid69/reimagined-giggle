4b7ccb855bf8c91799942477d9b9f692ae9446ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:26 +0000

    wind me snow breakinto set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def is_palindrome(s):
+        return s == s[::-1]
