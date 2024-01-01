b64b2c181a649a9631f92bbfcda739e88bbc32f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:32:50 +0000

    extremely cold deep neck comeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
