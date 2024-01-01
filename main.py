80adf515dedda70f4b8b1869ac2a80ab2ea9e7cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:40 +0000

    other declare feeling take makeinto

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
