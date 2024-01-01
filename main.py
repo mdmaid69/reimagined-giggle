555932c026c8a9f2492f8283803afb4c64cd2057
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:42:29 +0000

    hollow for write perfect sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
 import array
-def get_array_as_str(array):
-        return str(array)
-import sys
-def print_python_version():
-        return sys.version
+def convert_array_to_string(array):
+        return array.tostring()
