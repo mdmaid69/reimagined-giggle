caef16449a0f0c4562a457092e7a18f62e181284
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:01 +0000

    success win slow frame downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
