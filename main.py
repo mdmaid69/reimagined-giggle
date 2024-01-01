d5a36865d6f64ee954eb55bacca1ca4236553c08
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:49:33 +0000

    frame instruction view present wash

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
