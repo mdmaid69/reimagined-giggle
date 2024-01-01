7fa1059ea2ad77759810bef8e404689977427389
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:57:45 +0000

    lead also night usually interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def find_max(lst):
+        return max(lst)
