68756b63e81378db2545450c26717b613fd5c5f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:50 +0000

    easy consult hurt the risk

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import math
+print(math.pi)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def check_if_array_contains_item(array, item):
+        return item in array
