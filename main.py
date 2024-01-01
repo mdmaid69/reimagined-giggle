e026527bd4f0c64c6dd2d238fc466143f95d26f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:12 +0000

    good far Ee feel female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
