4aa2e30cbc715742d4d4c6c2b4cc659ceb386018
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:20 +0000

    break emphasize stop straight shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import os
+def change_working_directory(path):
+        os.chdir(path)
