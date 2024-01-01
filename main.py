8dee93c5f633e746158c84c6f8f1a524e03857bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:55:07 +0000

    turnoff taste nonsense trap brief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def subtract_numbers(x, y):
+        return x - y
