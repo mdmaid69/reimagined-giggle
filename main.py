6a392bbb45a37d04d82c1db2ed238f9cf4dfea1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:49:50 +0000

    occur state event strong runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
