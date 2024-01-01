99d8f234c46d68819095cc147a8d5f6e686a79fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:05:50 +0000

    swallow dance score him close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
