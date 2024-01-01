bcaed3a663e280cecd630b93bb5c21e3f5efe8cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:14 +0000

    flight objection string warm person

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
