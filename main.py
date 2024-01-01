6e5c99436123a20008f848d9ead555df6bdb2c89
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:43 +0000

    empty reply ago result suck

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def greet(name):
+        print(f"Hello, {name}!")
