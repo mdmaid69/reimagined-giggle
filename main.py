ad9e04249f2a89cd4fff0d6db0feeec791ba0f72
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:49:31 +0000

    special type complaint spoil breakthrough

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
