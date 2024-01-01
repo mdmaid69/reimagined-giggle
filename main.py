d6e593b73a675e3f441a189e036b4427ee884c66
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:13 +0000

    clothes convenient warm arrive difficult

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-name = "Python"
-print("Hello,", name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_error_function(x):
+        return math.erf(x)
