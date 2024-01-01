b4eac1e2643e3427e9ca1de73720291791c9bae3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:09 +0000

    next strike they flow combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_typecode(array):
+        return array.typecode
