9e577e72a95b36d76e6999d64b16511a06653e3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:08:57 +0000

    contain away course have trouble

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_pressure(force, area):
+        return force / area
