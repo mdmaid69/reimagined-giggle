d63d0c0907dee7a622e9fe4b62419cc82e765af9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:18:43 +0000

    fill walk beat think bright

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
