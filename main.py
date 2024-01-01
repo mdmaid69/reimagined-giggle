7292db4413cc56ab7ebcfdec91f66b1315f2df71
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:17:19 +0000

    post public steer support year

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
