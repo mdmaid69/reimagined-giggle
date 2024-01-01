cb4f2589c2e145b8da858ac615ef0684258b40d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:09:01 +0000

    blame word mark answer city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
