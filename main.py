07785abcf52b1aa0ac2809bc7679ad9b95a63e19
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:27 +0000

    print brush bringback clothes honest

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
