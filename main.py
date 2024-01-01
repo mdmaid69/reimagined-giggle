49e5b7ad2af730ba2d459be823891a9db7498410
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:46 +0000

    recently angry turnon transparent related

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
