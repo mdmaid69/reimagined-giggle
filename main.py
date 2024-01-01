1c7e5a974117a07723351f4142b79705af768954
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:19 +0000

    beyond wheel compete fine change

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_volume(length, width, height):
+        return length * width * height
