222f3eab84f347753a0f9c18f89da781af2eb9db
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:20:49 +0000

    main piece risk produce fault

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
