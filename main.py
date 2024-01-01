706f88e9d99edf866fe4b15285c6acce0e895751
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:18:51 +0000

    level include roof combine loose

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import datetime
+def get_current_date():
+        return datetime.date.today()
