2340619b1f3c20f282e30208e823880263ea3080
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:00:58 +0000

    someone glass myself should thinkof

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
