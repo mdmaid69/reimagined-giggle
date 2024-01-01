570d28063a14b61d7513c390f92cb1721f2cfd5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:16 +0000

    voice wander train available quick

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
