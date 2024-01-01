0a5b61a1d0218f9541e04cef745245710019e0c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:10:24 +0000

    funny shine plenty will interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
