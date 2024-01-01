3b83e523758ebd01e88c817316f4191c80ef8115
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:32:44 +0000

    plenty familiar badly fear sea

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
