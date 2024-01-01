7f5bc754cb0187817699d5d7dc80d0c758dc772b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:10 +0000

    wherever scene 567enemy type record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
