9dfd3654a1e478115fef1e515646d4793fb81b74
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:57 +0000

    conclusion scrape future dare bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_length(array):
+        return len(array)
