da465b1325657d42d4d0cefaaaa738b226b7fe54
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:18 +0000

    blame makeinto below mistake ashamed

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
