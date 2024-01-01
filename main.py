82f6c594035e5cb1beaa5a13e54a6cf113e3f5a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:23:53 +0000

    rather cap nor defend probably

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Powers of 2:", [2**x for x in range(n)])
