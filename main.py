31aa8a43e64ae4988593ace27c4a2ffa17523526
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:30:42 +0000

    how enthusiastic stiff blue you

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  def remove_duplicates(lst):
-        return list(set(lst))
+print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
