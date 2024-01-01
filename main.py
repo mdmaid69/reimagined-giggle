d185c27340499ffe4834a382a360c7287866300f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:16:25 +0000

    tend stare around watch comeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
