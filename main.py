4a89a0e7581f9328a6e2515223357dcde1bdf90b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:57 +0000

    inside study trap finger call

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
