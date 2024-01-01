e5ffc665f54a47ad4075b9a550b8374c9027fb55
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:41 +0000

    dull soil the away range

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
