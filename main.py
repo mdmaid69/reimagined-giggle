a80bc791d3d9f89361c11af91541ff99005de4c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:56 +0000

    beside content inside else OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import os
-def change_working_directory(path):
-        os.chdir(path)
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
