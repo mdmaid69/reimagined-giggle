6774ca9ec504210d098d0727b0b9331e61857634
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:11 +0000

    spread worth hold about approval

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
