c83069dc3633dfdc630dee582853195451c68de9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:39:00 +0000

    wake load deliberate trap trip

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
