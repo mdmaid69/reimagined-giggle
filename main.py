659a542063c955dd1b2a5b266bc349dbf6cb80c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:51:06 +0000

    favourite dick back defend field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_float(array):
+        return float(array[0])
