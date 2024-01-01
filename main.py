84784e3bdf8aab0fc394dacad5c25a03c0fbbb37
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:51 +0000

    operate immediate thorough class well

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
