885e0ce5d82b94cf36851373e7ae311b4769d054
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:24 +0000

    brave tidy father lonely wide

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import sys
+def exit_program():
+        sys.exit()
