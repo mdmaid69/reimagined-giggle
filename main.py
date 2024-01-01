1aac4ae6a6864be8cde47c246cf24567acbc1058
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:21 +0000

    extremely cheque judge bury fetch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def count_words(sentence):
+        return len(sentence.split())
