7a52f9d06609004818a6f72389112f5ba1c7f444
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:25 +0000

    their rank usual through there

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_repr(array):
+        return repr(array)
