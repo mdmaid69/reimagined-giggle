3e185b49913e3492e2fb6ae99734dd6ffe45e8c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:37 +0000

    may forget can interested down

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
 text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+print("Reversed:", text[::-1])
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
