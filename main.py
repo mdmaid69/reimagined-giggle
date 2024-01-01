e72a6690059fd79354bf45fc05a1801abecc210d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:53 +0000

    future mysterious declaration will disappointed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
