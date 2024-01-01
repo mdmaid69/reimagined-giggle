0a5c0011cb0ccb03a7701c2fbcc4ddc3dbfa8805
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:06:18 +0000

    trap use several fall figure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
