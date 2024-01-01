43bdd8f4662cf5976ccd91e91edd2944cc948b4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:58 +0000

    stretch trip cinema correct fair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
