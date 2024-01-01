50c27a17c36acbb40d060fc6ab13752f1615cd35
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:41 +0000

    recently action seem average force

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def convert_to_octal(n):
-        return oct(n)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
