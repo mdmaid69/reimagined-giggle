5b8a884b1352e6f65262055f47d631a3814b085b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:55 +0000

    average spill total command month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
