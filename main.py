104bb5681d25291a3c382cf0d6868c44895b57cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:36 +0000

    print Tt zero add communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def convert_to_binary(n):
-        return bin(n)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
