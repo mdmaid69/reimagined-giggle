bef1bb3bc87aee95818abd3a955b5d8bf1733ba4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:49:35 +0000

    class influence method city couple

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
