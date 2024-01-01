da7b582fc0785215f2d1a37e9319710dc416f16a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:19 +0000

    wide plenty female sister university

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
