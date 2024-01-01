93c373f99bac6d3359895e761703053a20673b1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:19:24 +0000

    little distance power nearly only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
