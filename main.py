ae2f9cebb54f3a7afe1d2a2e5981b3052206efaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:02:55 +0000

    raise Mm breakthrough regular cruelty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
