e3732bdb501712d54081175f191b381dfc204b1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:03:25 +0000

    direction change anything complete right

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def count_elements(lst):
-        return len(lst)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
