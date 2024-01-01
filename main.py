d3d4b66b904bd15a43e29a787e166786e902312f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:09:02 +0000

    legal sudden quit until wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
