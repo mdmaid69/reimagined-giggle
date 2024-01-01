ebe47a9ac4fe96a5b3d39529bd36ad39483e5048
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:21:15 +0000

    dull message live proud protect

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
