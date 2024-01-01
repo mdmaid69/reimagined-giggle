80adde37ee2bf9a048d35e227da2bc86839ef3ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:05:18 +0000

    excuse stare through perhaps dark

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def find_max(lst):
+        return max(lst)
