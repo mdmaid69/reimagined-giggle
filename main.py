f29f807a34dc0cfc362c4d063aa7b793ec1b6d7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:38:41 +0000

    flight either remove well statement

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
