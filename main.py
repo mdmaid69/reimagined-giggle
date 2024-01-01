9e13e8a169209dc97a7526adceddb7101b85b20a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:15:12 +0000

    favourite family transport level public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def print_python_version():
-        return sys.version
+  def convert_to_octal(n):
+        return oct(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
