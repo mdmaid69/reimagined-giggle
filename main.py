d82fc2bbf0298138cc0ceb283331db8098b17ae5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:15 +0000

    stamp pen legal organize train

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
