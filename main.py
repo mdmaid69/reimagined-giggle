e1348da04100421190b55b304cd5bd91cf3c8e28
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:26:12 +0000

    bank content schedule tomorrow rely

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
