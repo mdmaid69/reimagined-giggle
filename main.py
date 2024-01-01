46c9e8148a1ec23250d2f5415dcbb59168e548ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:21 +0000

    unfair age body wing battery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
