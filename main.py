27d79bbc844802a2386b8d0336ff133b4d5ecc1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:14:03 +0000

    sort wherever getoff subject duty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+print(os.getcwd())
+  def count_elements(lst):
+        return len(lst)
