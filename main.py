7e104e029bbfb0b5c952089d9615fdf9bd2ef4b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:59:00 +0000

    block test impressed tongue uponon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
