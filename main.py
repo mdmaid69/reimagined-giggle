20092a332141de020700a6fc907dacf29c768e6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:41 +0000

    promise border fit rescue uniform

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
