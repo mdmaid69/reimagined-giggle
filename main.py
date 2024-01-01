dc9b64218d8ba2be81cbdc0ac983abe1b8e8c72e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:55:59 +0000

    place top fear gap several

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
