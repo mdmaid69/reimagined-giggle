f3c5c5bf4382f91a4effe896df722063bdb65177
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:12:18 +0000

    underneath crash process year tape

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def multiply_numbers(x, y):
+        return x * y
