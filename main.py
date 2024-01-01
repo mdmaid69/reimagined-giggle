046ef3c3aeb4beb1f0dc68635d883ebe9746b3bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:44 +0000

    mood blame early butter even

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
