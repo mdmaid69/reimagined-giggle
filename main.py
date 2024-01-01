5deb1b577d9a57a44cd1db7f224c52139fdfcce8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:20 +0000

    workupto forget before complete crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+def get_list_from_array(array):
+        return array.tolist()
