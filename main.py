ad30b6200c4355f22c75857f0ef03dedfd369e89
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:53:19 +0000

    lookafter hers summer quiet important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
