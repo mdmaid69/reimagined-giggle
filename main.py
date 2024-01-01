81a4fe01f92151d4921b0c3de9ebf74f3e599563
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:33:04 +0000

    getup fetch really order worried

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
