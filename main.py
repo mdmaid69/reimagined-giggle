cb0f2673ba720263fa48b86cd1862efbd44e76cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:27:53 +0000

    gather clearly advantage stir squash

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
