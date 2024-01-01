f811e27fe65fc7f2e7a74fa331d9277c6eefa298
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:23:56 +0000

    bet runaway temperature violence pressure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
