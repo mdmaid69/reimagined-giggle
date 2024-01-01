58ccadd8e5c97b5c50918d72c32d6da9b97bb4b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:32 +0000

    popular command dream mixture official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_speed(distance, time):
+        return distance / time
