4de1d146f7275b52df9f67dc988e63e943e7d96e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:13 +0000

    fast distant represent deaf edge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
