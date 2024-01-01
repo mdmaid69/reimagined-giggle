1cf40949776a9982e4bd18a81a3dbf2e1295c668
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:00:07 +0000

    first claim deep no custom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
