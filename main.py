a0c2a6491f4af176d68dbb38d537d38fec454a86
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:14:35 +0000

    threaten minimum open Ee swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
