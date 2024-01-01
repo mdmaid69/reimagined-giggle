67695321431546d4dc04d7eb632b682449fcb7c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:05 +0000

    together his command visit system

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
