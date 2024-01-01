88f6a1e3338ba8b1fb7bc58dc6c8dbfc36e7fa9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:39 +0000

    there suck communicate unit its

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
