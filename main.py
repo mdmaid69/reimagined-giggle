2f045bf023aa0cd7bf267081ceaaa0e0f5eac38b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:48:12 +0000

    pretend 567enemy direct splash secret

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
