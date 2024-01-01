ab31195b9d4591ba98457c68c55c6a6066960f11
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:00:22 +0000

    comeoff rather slow wish steady

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
