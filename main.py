13accf97fd5c0fce4e908089e92aa056bf8caa4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:29:54 +0000

    slip by lookup hard television

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
