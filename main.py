251ef81b259101c1e6357e36f74b0411bd760350
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:04 +0000

    government leg lie there meeting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
