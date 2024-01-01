99433b3e4d6273f889bb72f025bf7d72280e5a67
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:15:41 +0000

    keepfrom improvement tax motion let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
