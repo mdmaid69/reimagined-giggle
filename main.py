99bcd03750baa2cf8cb98718641cb508de69ed31
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:42 +0000

    sudden strength attitude practice honest

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
