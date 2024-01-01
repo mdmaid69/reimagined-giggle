eb250266cb397e40bd0d1cfdf67c9cc663b1eeff
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:27 +0000

    available man official broken accident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import numpy as np
+print(np.array([1, 2, 3]))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
