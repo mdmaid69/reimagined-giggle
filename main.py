8a6b0732bbc1d99c95fccdfd06fef7b01f4d1bdf
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:41 +0000

    trousers breakout upstairs variety off

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import time
+def get_current_time():
+        return time.ctime()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
