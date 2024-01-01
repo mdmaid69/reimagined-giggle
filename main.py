fc23577d7f2fe5edc3b092b5151115149a2957b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:13 +0000

    lesson exist over division workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
