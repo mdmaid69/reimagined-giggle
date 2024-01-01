8c8b51a5aeabb5a0a63c02491c242c9bfe51cf5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:20:22 +0000

    height report find dangerous finger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
