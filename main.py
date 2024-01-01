4d4235c32aea54ca1b8d626aa6110633d7d9373c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:16 +0000

    neat marry understand promise fact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import time
+print(time.time())
