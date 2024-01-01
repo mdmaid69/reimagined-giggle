09f86a32b4272c48247453c6a5f91363a4e14a2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:54 +0000

    soft hungry reach job until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
