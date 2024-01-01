6928e0980230bde29bcd29770f32dd64922efff7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:02:41 +0000

    progress offence break book different

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_int(array):
+        return int(array[0])
