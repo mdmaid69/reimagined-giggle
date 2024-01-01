4396cb7bdb55855512894f19e5e272500da73f23
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:04 +0000

    goout point insist correct turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
