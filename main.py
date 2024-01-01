7b66fd3ebfc47f26fc6e4309238e956a9e33d16e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:58 +0000

    touch Pp switch shame proper

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
