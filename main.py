2da0cae17ffb0fc580f50236b91eadacaaf16175
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:09 +0000

    before solve flight face major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_length(array):
+        return len(array)
