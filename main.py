1135975f449ed607fab66672e46918b5f31f3a4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:04 +0000

    actual judge recent other pot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
