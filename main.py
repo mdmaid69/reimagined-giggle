c0db9f5b0050a612018b663c4d4cbd74e951d8c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:59:34 +0000

    exact involve autumn fat swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
