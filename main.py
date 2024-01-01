72ccd5b879f2226bfb69db603c668bcee63982d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:51:06 +0000

    honest separate heart safety bored

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
