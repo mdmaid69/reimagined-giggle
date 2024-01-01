ad7f800e1c5cd4d44f7123a2de93b66209158cc8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:12 +0000

    discuss they quantity set this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_power(work, time):
+        return work / time
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
