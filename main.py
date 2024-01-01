dce998f5069bf330f9f698dd116aaf5a7b851717
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:33 +0000

    relax around him effect interview

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import sys
-print(sys.version)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
