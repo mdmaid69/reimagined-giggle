81ebba370502241eae17e145e3def86b222f217b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:50:53 +0000

    attitude life hand getaway unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_itemsize(array):
+        return array.itemsize
