475e9881ca49e807f3140985af684e2d5ae2713e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:29:10 +0000

    result sail means statement deal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_array_as_bytes(array):
+        return bytes(array)
