01752721c2bf90bf5f743b9ba0c18848f054b0e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:03:19 +0000

    touch cheap pity sale interest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
