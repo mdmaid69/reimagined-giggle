53d4e54e16953ee9da49f84a0cdbd9327ca66b23
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:04 +0000

    art clearly comeout slightly comein

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
