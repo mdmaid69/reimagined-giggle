60760e2dde94982e542bb7cce0ca38ff3101076d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:59 +0000

    squash simple idea hit proper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
