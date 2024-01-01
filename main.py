eff6b7e0b514efc5c329b0bf5e364d1c046b667c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:45 +0000

    lead book reliable sort remark

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
