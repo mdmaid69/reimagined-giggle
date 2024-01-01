ab927347b27e8fe4bfad1ea4697c40893056ae51
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:19:36 +0000

    borrow proof quiet good answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def subtract_numbers(x, y):
+        return x - y
