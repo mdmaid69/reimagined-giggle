1bc45b3ceaf04d900097f0d385ac5ae209b756e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:44 +0000

    idea point estimate rare hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
