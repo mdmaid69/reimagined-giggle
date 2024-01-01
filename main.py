489da53a502ece9ea82bf94d466e70fae2685e4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:32:03 +0000

    cut block wide short learn

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
