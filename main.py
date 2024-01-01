d150369d99eb61d7024c89dcc0cfac29f7c1cc3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:39 +0000

    sound board theirs have experience

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
