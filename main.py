a9bf92d1db0f6553d76100081e0a35683a846de5
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:45 +0000

    keepup mark fun treat judge

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  def cube_number(x):
-        return x**3
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+for i in range(10): print(i)
