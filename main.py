2d0dd09c0bf95c8dd593075904ab53c27028fb70
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:23 +0000

    only from hardly already goon

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def cube_number(x):
-        return x**3
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
