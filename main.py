86fc4538c29e373737da92bd60612e03e3c1f338
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:04:14 +0000

    hear less used plate explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import platform
+def get_os_info():
+        return platform.uname()
