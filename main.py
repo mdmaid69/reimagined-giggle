f424a24aa881ca4b6fc7c39c0fa0caf3f75ee8a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:11:07 +0000

    spring Nn exaggerate free dry

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+print(os.getcwd())
