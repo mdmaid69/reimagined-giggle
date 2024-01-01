6b6e5b79d79e1c20f4e084c5ccc6ed26a3f9f83a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:19 +0000

    once change male church breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
