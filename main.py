131bfbe31f09a6283775ead5e693da08316b90ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:45 +0000

    claim usually welcome wide healthy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+name = "Python"
+print("Hello,", name)
