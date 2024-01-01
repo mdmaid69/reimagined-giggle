78c4b0d2e3101861d107eacd0eb9df18f1d92e3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:27 +0000

    Dd if freeze in place

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import sys
-print(sys.version)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
