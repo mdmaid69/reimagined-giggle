3e76e55fd2691b44c8fa2c61e94ad402b4ece99a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:29:57 +0000

    relief sign share minimum cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
