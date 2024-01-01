8b83bde6334ed78bd12440f69e8054a4430d7e87
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:07 +0000

    walk shrink back information sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import time
+def get_current_time():
+        return time.time()
