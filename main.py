6d41d24badab6f97072ac0dd495f8f128eab8784
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:00:34 +0000

    constantly tear smell none soft

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
