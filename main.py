00a731c90c5d05ca2af15c418ffc46ba7bed3f2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:00:24 +0000

    stay really discovery Cc secret

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sys
+print(sys.version)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
