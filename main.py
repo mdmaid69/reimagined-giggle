08c666ed44c32006cc6063ec1fe87dc4363bd3fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:07:48 +0000

    eye rough order damage bringdown

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-for i in range(5):
-        print(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
