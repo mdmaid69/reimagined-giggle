826fa563710fee680c081e63ddf9dfb4968ddfbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:21 +0000

    order Ee connect rely build

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
