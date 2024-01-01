baeb6edb8048f1204e417e243a2c588ca14912a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:32 +0000

    message urgent use wash arrange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
