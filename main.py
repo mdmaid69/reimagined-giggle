59429a24a88ffae080217584b0f7c253b5b0ddcf
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:54 +0000

    exchange hit cinema roof tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
