ac2d7ffc45d5516d448e643c8fbc4b0134d23697
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:44:10 +0000

    godown round cause ride wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
