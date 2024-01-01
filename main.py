6352c565e77de87584088b17821043df09c41b3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:57:21 +0000

    reject fence warm afterwards appearance

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import platform
+def get_python_version():
+        return platform.python_version()
