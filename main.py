22b6c5e3773ae613d5540999509ee50278238bae
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:29 +0000

    tax personal immediately environment kind

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
