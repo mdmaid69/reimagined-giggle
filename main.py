f9d676c8559c0d6b5a2e277a4739f880d2dfe1f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:20 +0000

    several touch divide fear too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
