4b6cb304c3eae3a9ba97d2d55b0a73efbb7ffed4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:30 +0000

    among through strike complain lead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
