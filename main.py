8bf85d38e796e67cccfa7b357653b95f53f0f374
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:13:38 +0000

    place however better advice unkind

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
