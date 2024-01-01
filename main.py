94c38d5b6b11af6150d848133bd41e9044f09a30
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:35:49 +0000

    sink permission happen party hope

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
