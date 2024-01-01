c87b3bb11764f13e2fc2b601c2f7a0503b087307
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:25 +0000

    band unlucky suckme hard comedown

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_list(array):
-        return list(array)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
