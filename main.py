5d918092210ae3abb0ef05e34b746c9ddd9973f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:06 +0000

    instead fire impatient justice thirsty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_array_as_str(array):
-        return str(array)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
