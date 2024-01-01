7f8c9372663ac01a8da1d9cf026ba42777db6966
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:18:57 +0000

    but height fetch keepaway ground

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def find_max(lst):
+        return max(lst)
+import re
+print(re.match("h.*o", "hello world"))
