34844de7837838eabccee569cce58f3968eab577
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:31:53 +0000

    ashamed maximum hungry final introduction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
