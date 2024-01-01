bdc0bc89dd306383f98923096153feed6004102a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:38 +0000

    difficulty shout cheque reply quarter

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
