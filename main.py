860df3c943a6e8f6a4baee9306333c7ad25dbb4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:36 +0000

    sign quantity book contribute so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def convert_to_binary(n):
+        return bin(n)
