1c50752364c8ff84d6c2a52b0caeded62013d6e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:36:28 +0000

    burst late crowded up large

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  def add_numbers(x, y):
-        return x + y
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
