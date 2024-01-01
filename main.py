dd76da33aede53433195aaa30901c4574ca09617
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:26:01 +0000

    computer have accommodation by will

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
