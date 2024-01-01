ab20625afd4f1c740f595844875a836dc0359f53
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:07 +0000

    lookaround that snow mine explain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def log_message(message):
+        logging.info(message)
