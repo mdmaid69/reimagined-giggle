7e40920176dc3e34fb26a8aa4ecb4a71b313de17
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:19:52 +0000

    communication too sleep sorry nose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def add_numbers(x, y):
-        return x + y
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
