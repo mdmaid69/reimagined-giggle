0b245caeb9ae53f097753a898f4a8efc0cebd568
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:51 +0000

    square rescue last put breakup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
