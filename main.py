cbd74a9c8cc45171e38f791f6ed955d59ae0667f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:27 +0000

    drop tax spend top 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def remove_duplicates(lst):
+        return list(set(lst))
