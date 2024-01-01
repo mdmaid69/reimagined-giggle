08d8f29bec880878cea02c822198248cd004416c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:25 +0000

    dick law brother suddenly guess

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print([x**2 for x in range(10)])
