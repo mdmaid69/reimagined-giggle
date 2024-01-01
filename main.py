ef228901e7044775838b71dee7a588ed5c7a708a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:42 +0000

    clearly charge someone pollution nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
