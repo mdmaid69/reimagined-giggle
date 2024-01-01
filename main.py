4ce9d05c32fdd5d9b822af479f4271c470f1fc01
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:50:48 +0000

    delicate dress push bus crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
