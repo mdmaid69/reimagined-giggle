90c9b95248d24a1f227a83049dc8a44b8dafc141
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:26:38 +0000

    region joke pollution conversation bored

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
