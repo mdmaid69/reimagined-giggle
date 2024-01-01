ee65bba8a876bbf60c4fd7dd23ca0c73020b1c69
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:06:31 +0000

    shall stand aim produce share

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
