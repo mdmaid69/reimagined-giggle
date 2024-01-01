7bece975ce9640d028849ae4601d9b9a347c391f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:19:02 +0000

    concern tell none open suppose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
