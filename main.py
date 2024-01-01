54b4626642cdca5006e856684bab04b2ea40ff82
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:44:48 +0000

    empty house walk custom interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
