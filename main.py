3e3330acfb19c7543006e5067c5ae6110525030a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:32 +0000

    enthusiastic actual legal considering between

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
