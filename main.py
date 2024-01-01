34880956f5c1ee52b82b7ab3cf4a55f5effe8f76
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:16 +0000

    century furniture forward kick unpleasant

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
