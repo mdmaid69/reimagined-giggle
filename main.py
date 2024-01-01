b47bc35ef90f4de3b7fbcfb002ff83f9ee9839d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:27:17 +0000

    like pretty related brief circle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_repr(array):
+        return repr(array)
