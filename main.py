af3c9aba1fa1a1f1b9e3af1371c9e16291b7250a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:53:35 +0000

    567enemy except that room strike

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import getpass
+def get_username():
+        return getpass.getuser()
