3c20e42e9d8d0673b36753c4b109962610746b60
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:53:30 +0000

    process funny ticket emergency start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import time
+def get_current_time():
+        return time.time()
