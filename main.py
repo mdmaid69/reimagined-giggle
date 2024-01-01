66902de7ee05499eb19fd3caf192265037d7bebf
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:22:12 +0000

    coat enthusiastic glass arrange side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import datetime
+def get_today_date():
+        return datetime.date.today()
