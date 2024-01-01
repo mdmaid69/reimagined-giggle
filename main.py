b8944245b751f39b1a779a62b09cb8d03d42344d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:46:51 +0000

    raw season 570enjoyable violent wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def clear_array(array):
-        array *= 0
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
