d8f16b313c610b35d62e642d42cececb344277ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:15:11 +0000

    copy 570enjoyable free survive above

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def subtract_numbers(x, y):
+        return x - y
