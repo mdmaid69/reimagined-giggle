68e8304b4f8849fbf7437693b871b2a9cae14a46
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:18:45 +0000

    train approve own first delicate

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
