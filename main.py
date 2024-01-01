21fe943b42b90c08559fdedaad91785057885548
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:57:34 +0000

    average try sink read flow

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def greet(name):
-        print(f"Hello, {name}!")
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
