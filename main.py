6b41fe33de9235a838a4f56a2697ca86e90aaba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:56 +0000

    laugh team away bother each

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
