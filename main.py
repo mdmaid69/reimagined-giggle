1a2a6ff9019f8f2b38f19741719a3d167d87afa6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:13:13 +0000

    nonsense wonder miss weak geton

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
