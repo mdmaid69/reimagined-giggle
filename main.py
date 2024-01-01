64a7b10506739153563d060e6d9083a7c74b7285
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:39:37 +0000

    study set sense guess minute

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
