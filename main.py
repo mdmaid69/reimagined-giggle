5bec8557fd7d98fba9274e4ad9af7be38e178534
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:24 +0000

    sick plastic report more new

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
