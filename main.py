024f52f1738c6ba2575794a4beb2b7fef805f425
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:35:17 +0000

    there extremely obtain moral education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
