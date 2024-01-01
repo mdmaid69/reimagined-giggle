332005073d26f6799ef5a03a1a29a3062ba1514c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:38 +0000

    strike beginning cool regard wall

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
