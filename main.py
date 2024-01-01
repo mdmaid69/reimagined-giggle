db272112c1be8c697dbc440e2ff4a9c5b3eea169
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:23 +0000

    Mm may reduction stamp wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
