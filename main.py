1906a5e439603141a1902037147b47a43b592178
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:01:19 +0000

    uniform chase explanation influence brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
