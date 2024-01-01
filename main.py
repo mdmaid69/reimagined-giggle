797a0a2723b3050d746441b0c68ca029aadfb885
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:39:15 +0000

    eat address problem strict wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
