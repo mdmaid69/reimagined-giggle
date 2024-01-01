25e3d77bb113f529f4110a3040a035a43fdbf85f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:35:53 +0000

    think well failure order not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_power(work, time):
+        return work / time
 import array
-def get_array_item(array, i):
-        return array[i]
+def convert_array_to_list(array):
+        return array.tolist()
