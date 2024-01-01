b16a33f88c326d615fb6a34ad291563123f8025c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:47 +0000

    environment sort crime getoff stretch

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
