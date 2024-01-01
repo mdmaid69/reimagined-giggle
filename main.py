f1fa9fc636859341b4ada9795dc6512831ed7d68
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:59:05 +0000

    weak motion communicate discussion can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
