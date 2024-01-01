6d80d045936034bc13eab8d0a9e9333801801113
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:46:20 +0000

    Ff comein clue tie climb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+def find_max(lst):
+        return max(lst)
