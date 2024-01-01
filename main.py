76e3a74f4083da6e9d46ef75566a163430d3e75a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:28 +0000

    defeat university many earth square

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
