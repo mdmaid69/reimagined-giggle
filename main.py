191c44e6cef893c99ea8da4b06f43eccd7bf0668
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:36 +0000

    breakfast hurry slice breakfast task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
