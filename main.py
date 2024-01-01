3ea37b3fcf9f0a8dfb5e683320d770e2b69437a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:00 +0000

    better today compare fortunately available

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
