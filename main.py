a48fea9b113be3e07760e57fb694591db21182f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:49 +0000

    reach admit everywhere taste why

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
