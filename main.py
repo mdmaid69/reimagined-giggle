918b97319e2b35d943d57c1f5d71c062929aee8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:00 +0000

    kick produce weigh dinner common

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+for i in range(5):
+        print(i)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
