20203f02f6b88376c342ecd738af1e65366c3f9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:22:05 +0000

    flood school even expensive but

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
