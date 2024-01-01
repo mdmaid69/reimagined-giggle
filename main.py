be5ab869feb746ae6aefbfc0990a1f4b71d58fcd
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:36:18 +0000

    carry bet age Jj use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
