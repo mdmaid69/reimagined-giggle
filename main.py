e8d43ca068e22066c84d3e9e840f7433d614b3ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:30 +0000

    delight air right form fast

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
