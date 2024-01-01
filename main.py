592f6fc6dd8a860feaaaa4c43f89161a8491d0a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:57:02 +0000

    reason guess close straight air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
