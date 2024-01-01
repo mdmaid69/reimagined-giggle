4b49baf11c8305dc6b54caed1287366eb71ab9e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:44 +0000

    service getup noise collect goup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def reverse_string(s):
+        return s[::-1]
