941a601e60d38c4851e3c06e995940d5fb03ba76
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:33 +0000

    loud treat bath wheel turn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
