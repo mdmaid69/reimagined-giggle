cf782649440e60f3325b66f226d7c8cf46197585
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:35 +0000

    increase makeinto duty task round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_string(array):
+        return array.tostring()
