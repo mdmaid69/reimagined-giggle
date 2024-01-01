0a93717ee4e72944044bc6f6d0e225e83665718f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:49:24 +0000

    force interview move dangerous message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import os
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_work(force, distance):
+        return force * distance
