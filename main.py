1a2181eae0e3542721086a4297c5a7ca7235b07b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:49 +0000

    make pollution separate develop rain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
