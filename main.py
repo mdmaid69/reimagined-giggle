d23c46c6661f1b3b1de361fc4b827e0324ab8841
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:22:58 +0000

    opportunity question name demand forever

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
