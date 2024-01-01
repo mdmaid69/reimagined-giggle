8e16bd5598a9c6d1673f7e1cb7a7aa62384347cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:29:56 +0000

    question breathe juice represent distant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
