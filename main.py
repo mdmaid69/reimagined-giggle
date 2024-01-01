cd6ab77124c9d4d8a32e40c96b95318fe5a11566
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:24:10 +0000

    geton turnup crisis meanwhile many

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import os
+def change_working_directory(path):
+        os.chdir(path)
