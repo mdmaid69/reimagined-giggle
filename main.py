b6567180377be69822ddaefac2ad797dfcbe8607
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:14:54 +0000

    software each risk trousers dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
