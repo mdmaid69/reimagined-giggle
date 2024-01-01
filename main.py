159def2c88077a77ceed33e97762d930140438af
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:42 +0000

    order long need fire expression

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
