6fc88c5375d350eeb7e36cb46c058c151e73a4f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:10 +0000

    present many criticism interrupt soon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
