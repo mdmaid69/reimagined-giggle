998b0f0b9f1b776afa69deebb2a66527ba5c3271
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:55 +0000

    introduce education climb spray enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
