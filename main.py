7741b903dff294d41bfd7a67e0fa5bc98a2057e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:47:06 +0000

    telephone care miss book turnout

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
