510d826ca5a5fa38442149ebf6c38369f0b8b28f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:22 +0000

    agree town bite hello moon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+def get_array_slice(array, i, j):
+        return array[i:j]
