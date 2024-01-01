d3287d6b0d56fd5beb83f9bb23214d6bf2b16950
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:43:20 +0000

    567enemy better natural delicate stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
