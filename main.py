babeb86036a8691dc8edc823e80f46ba7909eb00
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:17:05 +0000

    available Kk piece squeeze aim

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
