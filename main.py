3a239005a4f90b031095a2e0dc4696ba0b6e1499
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:18:31 +0000

    disturb squash situation thinkthrough position

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
