b7c0efe62a58f7030d2a730b80753bcf29ffb191
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:50:00 +0000

    kind climb immediately practical pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
