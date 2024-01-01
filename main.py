e2a528d9ce32524310b56cabec032028af1013cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:41 +0000

    march target healthy bill workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
