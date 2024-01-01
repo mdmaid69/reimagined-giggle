a47ed53747d03a93c4bd8028159c31e56dcb72e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:28:41 +0000

    yourself considering stick generally own

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
