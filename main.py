476dc4ad42398ea651716372a192093452011f31
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:14 +0000

    error steer set underneath difficult

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
