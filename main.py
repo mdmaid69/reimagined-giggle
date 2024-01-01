37bf1de7542c485c8d4dfe025812e7ba66667cd2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:25:41 +0000

    voice debt sense continue match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def sort_numbers(numbers):
+        return sorted(numbers)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
