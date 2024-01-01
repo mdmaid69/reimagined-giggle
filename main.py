be4203774a8fd027cfaeac731711be88db5a3e60
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:25 +0000

    other land fault his guess

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import math
+print(math.pi)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
