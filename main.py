0a2caddf959da77878f95a7e3d3e5cdb1654883a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:04:11 +0000

    interesting show acceptable Itself Ll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import os
+print(os.getcwd())
+def calculate_power(work, time):
+        return work / time
