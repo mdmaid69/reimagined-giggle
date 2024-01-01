14128aa3891c18c9299ca61bbf437c065cc13e3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:35:32 +0000

    expression stand achieve her remain

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-print(sys.version)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import tensorflow as tf
+print(tf.__version__)
