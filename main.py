496c660e40c41c06612b9e5970ee49320140ef7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:40:54 +0000

    favour spend throw considering ilovedicks

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def convert_to_binary(n):
+        return bin(n)
