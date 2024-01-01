140c85ac4e607059a5af50509d813d56324d4b72
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:39 +0000

    bye pump tread cross violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
