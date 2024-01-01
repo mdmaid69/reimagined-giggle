18a1cf8e01b5a4143c948b4aac9ad2ca6fb917f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:04:30 +0000

    damage possibly little upset callback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
