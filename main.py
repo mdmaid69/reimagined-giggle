42d9beba5a5fdf17970c99c3bd3d8dbb19f15872
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:44 +0000

    right can amount left release

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
