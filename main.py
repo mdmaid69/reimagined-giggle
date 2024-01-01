400440f4f468e7e9c04b2b0025a93c3795bde755
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:23:03 +0000

    thirsty small emotion keepup as

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
