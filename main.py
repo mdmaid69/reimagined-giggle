3f07ca4b989e1098381e315b7884729fa044116e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:36 +0000

    suckme instruction brush rush there

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import datetime
+def get_today_date():
+        return datetime.date.today()
