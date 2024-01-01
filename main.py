f7dd4a05e45500d86bba845cb59f7aa8e082665b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:40 +0000

    do official object comein piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
