e979e204edf2b384db59e947c7971be1b2cfb953
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:46:29 +0000

    special boring problem as suit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
