f30e98f50f9c964b4edc43ceacd45af457d2d7d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:48 +0000

    see correct workon serve pollution

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import sys
+def exit_program():
+        sys.exit()
