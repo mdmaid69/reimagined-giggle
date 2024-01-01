7f36309eaafd385d44da364dd2803d6b542f6990
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:41 +0000

    mistake estimate winter in approve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import getpass
+def get_username():
+        return getpass.getuser()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
