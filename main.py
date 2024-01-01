bff3de164253f8a84cf718b98e1293503b961c41
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:15:42 +0000

    but commit little worst search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
