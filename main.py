691c1ad0fa8b0249ed7dee6e1befc6e508e196c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:09 +0000

    perhaps warm grade abroad long

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
