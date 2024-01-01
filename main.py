67e71ad8b6937eaefb4f5a35a142bc773eb609ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:25 +0000

    child where ilovedicks exchange turnout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
