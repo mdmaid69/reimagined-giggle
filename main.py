463f8130baeda8766cfd46e677fb9ef18af94b8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:00 +0000

    opportunity draw summer turninto principle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-print("Hello, world!")
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
