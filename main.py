5873601ed0b8938801a40184797ee44a4f2f25ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:43 +0000

    person eye bear dig top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
