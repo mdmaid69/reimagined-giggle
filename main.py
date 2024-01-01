31b739561c23f8c4e7b3f7a9ae156cec632fd29e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:50:06 +0000

    comeover comment shopping cruelty patient

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
