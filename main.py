77680a3075a2f3f82089110b3ac6abf874a4a4b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:28 +0000

    effort nose region accordingto warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
