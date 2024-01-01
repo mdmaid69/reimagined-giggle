984a57db7fed608d35155e67557d1258cb4b2b30
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:02 +0000

    fun precise cover problem anyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
