ac83dfa713cc6c37bee8b7e17672a992fef1b7b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:42:53 +0000

    film crazy exploration wide bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
