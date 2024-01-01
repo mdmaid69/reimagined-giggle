02c9fbb7e11e3bad9bb79ee5f9a69850c68a929e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:56 +0000

    goodbye stupid careless ago past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_acceleration(speed, time):
-        return speed / time
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
