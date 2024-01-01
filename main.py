64035a002c4de93ea3f193d53cd7e25d60268091
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:51 +0000

    imagine one law me clean

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_density(mass, volume):
+        return mass / volume
