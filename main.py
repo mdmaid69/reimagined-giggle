f4146f474cdce26dd54e62b4d6f11ea2d098bb78
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:52:08 +0000

    out street frequent speech confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def get_bytes_from_array(array):
+        return array.tobytes()
+def find_max(numbers):
+        return max(numbers)
