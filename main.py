17f15bc67aa5c5375416ce5cf7c4c0f187349206
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:27 +0000

    takeaway wish bet absolutely my

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
