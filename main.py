d4c13b80a8cba1e41a949582be2dd48c54377473
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:14 +0000

    declaration control true impressed breakin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
