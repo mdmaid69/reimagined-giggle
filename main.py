f1f465dc6aa020e593cd1cd04a0cbf5688b90b40
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:23:53 +0000

    display without drive left on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
