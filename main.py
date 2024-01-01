d402b9824ab2ba3986432ee06b6e39a434eb8b81
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:24:03 +0000

    although figure hard approval persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
