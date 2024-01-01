b02fd58b1e531b51e8545f2d4a83df1ac377f7f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:02 +0000

    operate bring favourite desperate hair

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
