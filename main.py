994110a65d8d1dd81bca3ef950303894a9386a11
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:53:00 +0000

    plain actually pain sure sad

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
