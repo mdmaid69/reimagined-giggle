886a500f5180befeac52f87e09925d31b22536dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:36 +0000

    deliver zero probably bad Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_tuple(array):
+        return tuple(array)
