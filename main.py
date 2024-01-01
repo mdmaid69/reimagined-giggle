9b525804fa22f05436a7d8bf8d0f181753d1a011
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:00:38 +0000

    tap address idea common apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
