2d2dbebac92239f58691caff5f84c25c1e339727
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:45:47 +0000

    contain herself should instead tear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
