93f38299d111e4db2e8d4ef11e9f898247f662de
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:14:49 +0000

    rank extremely proper steer runover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   def find_max(lst):
         return max(lst) if len(lst) != 0 else "List is empty"
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
