810b22b4587e4fc354fb4cfcf13b8154a428abae
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:42:03 +0000

    strike exploration copy best tough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
