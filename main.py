7e53ed88e9510a4118a639673bc699366aedadec
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:40:40 +0000

    proper description tradition loose voice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def convert_to_octal(n):
+        return oct(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
