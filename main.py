1fd80e758ab4805c32681e68f63a10025f686a3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:59:54 +0000

    vision time comfort back improve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
