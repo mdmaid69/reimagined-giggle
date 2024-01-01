b5c392c4e390a57ab13ae9e281af9d56e90ff94c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:00:47 +0000

    run show survive division bush

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
