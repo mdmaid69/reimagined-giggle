676e6d67c9555c2671246b34b3e6b0d2ad574b05
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:54 +0000

    positive thick statement expression to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
