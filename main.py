f028bf8e015717546f66a30358d6d0a5089347a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:47:35 +0000

    attract improvement right shop imagination

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
