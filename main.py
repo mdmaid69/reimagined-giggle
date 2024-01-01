c591f960eb02afd07f8d55762bb43b44ff8e43b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:16:25 +0000

    crash tight private return address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
