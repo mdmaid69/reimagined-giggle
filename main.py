4023c5e270c3267ee8ecd10a40faf528f10dbd9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:20:23 +0000

    justice exchange free decision 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def cube_number(x):
+        return x**3
+def is_palindrome(s):
+        return s == s[::-1]
