f54524cfea64e0db44c1e01eea51f6dd693f8087
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:34 +0000

    criticize follow goout in cost

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
