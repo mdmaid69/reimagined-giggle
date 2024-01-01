076c4f37578e31f9f5a48dd4250a76d6fc486789
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:01:39 +0000

    strength turninto wash collect responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
