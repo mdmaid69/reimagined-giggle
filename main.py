f6feaf332d7182c96cc48bb9eadc0078b1773257
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:14:53 +0000

    boring present deal side several

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-print("Hello, world!")
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
