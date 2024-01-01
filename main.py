b34175a8a3f8da3d05fa109a86355797358523e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:05 +0000

    past guilty moment issue objection

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
