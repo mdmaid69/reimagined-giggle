1cfe5b764f27b33a5ab639f027063cfc9dce0a7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:34 +0000

    breakfast year convinced touch approve

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import time
+print(time.time())
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
