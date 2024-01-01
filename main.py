bde3a1e40e6dd4e44e7eab8f8ad65e769ad92a38
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:33:44 +0000

    exchange sink warn hot might

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_volume(length, width, height):
+        return length * width * height
+import os
+def remove_directory(path):
+        os.rmdir(path)
