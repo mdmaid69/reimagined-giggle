4359fcbb894d4ffbed7b669d48ee1a0e3c26a02f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:06:14 +0000

    because explain comment seriously plain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
