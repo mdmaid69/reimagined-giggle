0fa82d6886427fd2ac489c50a08ecd11e91c0d85
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:47:45 +0000

    bringback reduce advice soil pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
