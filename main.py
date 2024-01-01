567e32bbe31ad99f54e28f13ffbb25f42f81e2a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:56 +0000

    important over shopping guide rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_string_from_array(array):
+        return array.tobytes()
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
