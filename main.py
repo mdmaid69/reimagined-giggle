2c7c3120c497c7baac49a984afc916a11380a486
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:38 +0000

    beautiful bus sleep cheat unit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
