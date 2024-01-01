1908813398e2bc1d4396176ebee8f28d2f073c69
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:00 +0000

    journey building prepare sorry occasional

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
