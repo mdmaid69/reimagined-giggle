cb0e84ad23e9acb4fce08c9a3486e29e287bc717
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:09 +0000

    total bring medium off team

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
