4985983d2b952b484ebdfd8485425608e1daac02
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:45:06 +0000

    provide describe single practice tradition

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
