510bafe848c02077b161d7759702f5aee771c431
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:33:38 +0000

    switch cross meet its answer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
