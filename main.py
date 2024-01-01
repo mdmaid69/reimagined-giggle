744933e43d9606bb1667b1dd67bca52d546cc1bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:36:23 +0000

    glass rely organize develop pronounce

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
