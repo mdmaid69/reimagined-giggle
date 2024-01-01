09e801f3577b3f7c296de59cdf83294bb81c656c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:59:46 +0000

    hit private badly depend information

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def get_array_typecode(array):
+        return array.typecode
