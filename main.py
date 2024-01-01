874879dbd884cebd881a12ee930b722dee634356
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:38 +0000

    game community comeround save miss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
