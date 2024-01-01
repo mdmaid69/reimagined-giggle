1abcb8d7aea98ed0c834c7777d8873d1badf793f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:21 +0000

    like pin payment share out

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
