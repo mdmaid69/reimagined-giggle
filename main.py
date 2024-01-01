805bfd1dad9a5007f02e35a5ec33e003f355e69c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:00 +0000

    sound also pass attempt Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
