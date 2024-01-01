50cd497549ee194cd548de0a6a7f0bfe8d42f352
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:12 +0000

    enthusiastic contact can taste ahead

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_as_set(array):
-        return set(array)
+def convert_array_to_list(array):
+        return array.tolist()
