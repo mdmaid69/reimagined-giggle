bcf428f9c1b5adeaad59092eefa4e3e6b028ef63
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:26:21 +0000

    partner blue work suck often

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
