2a66e060ae0853830e6f46a6b23afa6c3a426fb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:50 +0000

    go table impatient healthy block

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
