186306fa42193bbd2bd9381c26f037f29e7e4f9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:33:09 +0000

    complaint today answer suit luck

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_current_working_directory():
+        return os.getcwd()
