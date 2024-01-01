c3af046dc0430d88ecd423a9d4a0369318cb2ecb
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:01 +0000

    callout suggestion file pleasant black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
