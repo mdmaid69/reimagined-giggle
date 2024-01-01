415cb76eb270bf7cd6d47d4ef2f08a2eb1743af4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:43 +0000

    laugh 179bite risk success community

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
