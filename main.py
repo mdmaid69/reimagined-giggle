b7ad4d1a89729bda3fb3bd2f78f2d9f84b0e8d0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:15 +0000

    cut attempt add whichever runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
