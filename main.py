95ec80c2f412ef564f0c0e43f802ad6b65d3ccbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:54 +0000

    try disagree listen sort appeal

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print("Hello, world!")
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
