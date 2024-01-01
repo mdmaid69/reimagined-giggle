116384c8aff5ca38b0a862ba916443c2e2b7b996
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:29:52 +0000

    me moment beautiful example control

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-print([x**2 for x in range(10)])
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
