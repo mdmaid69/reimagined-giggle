b29016b564f2b291498fbdafdd8b342ae0fc033c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:14:01 +0000

    cross impatient fill wake church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
