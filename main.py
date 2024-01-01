003df062fb9f28e703baf59dea03a25e0320fe59
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:22:14 +0000

    target beside tough shopping independent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
