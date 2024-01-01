a258e514731b17850d4f57b7fa9c801317e3ae54
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:35 +0000

    crowd touch great meeting rate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_work(force, distance):
-        return force * distance
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
