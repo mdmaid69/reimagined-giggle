52b120d14e728b063b5b6b811c03c65d8bb2eaeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:38 +0000

    example can fit under head

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def clear_array(array):
+        array *= 0
