6e0e458a3502646e8cef1b2d7e54adee66615ac3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:08 +0000

    day 565end someone direct guilty

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
