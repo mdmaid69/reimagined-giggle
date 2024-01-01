2885f04f640b1127c32612b376eb5a5df0b5cb76
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:11:31 +0000

    object shut guide will inform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
