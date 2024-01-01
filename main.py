2396efeefe103e725d51aa1e1471ffa23ffc3fae
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:23 +0000

    classroom strength idea plate refuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+print("Is palindrome:", text == text[::-1])
