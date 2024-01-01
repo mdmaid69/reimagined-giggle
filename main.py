e28bdad8689882db5ef7084a7c214059173db070
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:47:09 +0000

    range several consist minimum meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_density(mass, volume):
+        return mass / volume
