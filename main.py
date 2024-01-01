2fc970c764c8eefcfa7725b26a261de951a3281f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:42 +0000

    drive live dress better great

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import re
 def replace_all_occurrences(pattern, replacement, string):
         return re.sub(pattern, replacement, string)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
