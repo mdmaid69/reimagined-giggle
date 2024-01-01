1eb4501d849a2a05954c0b8cf77f549958c67c6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:45 +0000

    wrong straight head sad giveback

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
