8b52ee546903844e6efc1a19eed308aeb6aec21b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:08:47 +0000

    meet way interview where limit

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def square_number(x):
+        return x**2
+  def convert_to_octal(n):
+        return oct(n)
