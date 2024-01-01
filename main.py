d19475af1f68004cf7b093d527a3532d98348708
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:55:30 +0000

    read really contact just have

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+import os
+def remove_directory(path):
+        os.rmdir(path)
