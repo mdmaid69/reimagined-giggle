4b93c8c0a81f9a4d254ea1edb4935d9f8fff3fea
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:47 +0000

    contact turnback speech sleep similar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import time
-print(time.time())
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
