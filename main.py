60e29df66c603c368c3b15b951ddbfbaab851be9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:43 +0000

    blame everything invitation dance protest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
