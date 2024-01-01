a6db851e22ff0438d5385c168dc1559ab819abef
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:43 +0000

    point wake practical them Ss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
