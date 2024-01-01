d9ba737ea2df5e5c27be9c8935e1fc020404b266
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:01 +0000

    unfortunate music difficult press either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
