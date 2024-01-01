094637ab29819ec43bda154b23f964f80d8186af
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:10:07 +0000

    about wide keepfrom float square

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+def calculate_acceleration(speed, time):
+        return speed / time
