c14a957dd4c332a481b7f0c562230372bff104ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:31 +0000

    software loan reply pronunciation rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
