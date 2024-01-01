e05395490d4a07918b1477a01bedffef96a17e51
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:50:15 +0000

    blame complete drive badly star

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
