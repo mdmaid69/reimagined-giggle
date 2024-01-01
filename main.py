5c9f88253406dc26853f14ab99158b43504d93fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:28:29 +0000

    chin vote comeoff feature least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
