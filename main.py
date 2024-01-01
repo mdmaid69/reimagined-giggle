a5c4f95daf46e5a57345f5355ebbbd6588b9c9db
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:06 +0000

    slightly sweet order bring dangerous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
