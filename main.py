9164b6bb2fa69dfdee163d2c4d0383dd62d67150
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:28 +0000

    laugh drive develop prove reasonable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
