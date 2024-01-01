1f86122ed30e1403cc7c3432d7e165bbe78d97e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:35 +0000

    mess modern surprise compete in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
