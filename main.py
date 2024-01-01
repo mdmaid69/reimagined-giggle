90ad1dc3f6af31767d8d67c3e3d93f76f550ffbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:20 +0000

    legal school travel fun trust

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
