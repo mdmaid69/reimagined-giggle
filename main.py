8b38a3689b804a27adee6f425dc635c0be9ca271
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:29 +0000

    tread corner science slice leave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-print(re.match("h.*o", "hello world"))
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
