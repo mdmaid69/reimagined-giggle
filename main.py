a7b8858b2332ab6cb36eecc4334b27b7dc1691e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:05:47 +0000

    warn reason language rain word

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import collections
-def create_stack():
-        return collections.deque()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
