539511b39ee0b6e122214898e29126af44244dff
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:29 +0000

    cause coffee himself matter man

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  def count_elements(lst):
+        return len(lst)
