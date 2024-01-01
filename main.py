d5e85264ca2e74e25162ef6d3eca8f4721cd8216
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:14 +0000

    remain tax bad burst minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
