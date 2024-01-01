e000d6c1eeabbded27327dfcba76df62d01b58ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:42 +0000

    Nn pause health she event

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
