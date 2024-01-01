4d123fdc64e86af5280da8486d98577fb2f9891e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:06 +0000

    honest pencil remember agree defeat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+print(math.pi)
