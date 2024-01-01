088358c6c3724d53b525adcbb1f0be12f1c2bfac
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:35:40 +0000

    ago pick while guess bringback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
