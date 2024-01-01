5a42773415fd457cf070d0a8eca80f649e8a588d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:43 +0000

    while doubt but die somehow

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
