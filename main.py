9b7ea71d1102fc545a1831ff07a890f1bb790569
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:00:27 +0000

    do attention load crowded keepout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
