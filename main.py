846be37b459da73509db5ad9d603ae7918f8efd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:56:28 +0000

    head godown degree wrap repeat

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
