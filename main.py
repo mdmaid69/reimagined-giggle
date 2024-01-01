59f13fbb288e9f878e1a02af16ee08a8a5725899
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:27 +0000

    taste wear underneath buy settle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
