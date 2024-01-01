5a373361304c397d354bed56064caa3ec1854ac9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:32 +0000

    hear disgusting best attract flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
