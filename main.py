ddc041195b1b2573ab455b50d5b5f6023d529e5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:48 +0000

    fast stop human along work

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
