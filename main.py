c57894220262cdd01edf77e29c26de814818af59
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:29 +0000

    majority catch turnaway ability fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
