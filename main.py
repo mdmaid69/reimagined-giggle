07d0c104d6c63c1ed31c6457182e0e36a88df23c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:23:12 +0000

    load independent bend kind fear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import time
-print(time.time())
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def remove_duplicates(lst):
+        return list(set(lst))
