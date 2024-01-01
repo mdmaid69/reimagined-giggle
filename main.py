dfcb4e1802c548bcfd8cc78ee1ac1f57313fb041
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:39:56 +0000

    experience union he sleep him

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
