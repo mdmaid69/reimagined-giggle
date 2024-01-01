70b2c02b063c5137e9310ba9d31e06a399a4f593
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:11:59 +0000

    comefrom dance scene position away

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
