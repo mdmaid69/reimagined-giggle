15da84957a73900dce1f412c6372700922d3a5db
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:16:21 +0000

    begin ball through her belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
