11c226ebfeb9bdf66a1151129d76302b0b6b1f24
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:54 +0000

    ocean crisis still with flood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def square_number(x):
+        return x**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
