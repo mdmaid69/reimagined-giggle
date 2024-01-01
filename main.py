e6a39763a04f25d8b2c39ebbebef3d48eff21b83
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:11:44 +0000

    expensive trap like float destroy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
