9428e813ff4f117326590a1a1b4e9929dbb57012
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:49:07 +0000

    free Uu share ignore part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import sys
+print(sys.version)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
