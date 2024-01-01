188269aea31ff114c720fed4961cbe2374c74e89
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:52:10 +0000

    awake neat dear approve child

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
