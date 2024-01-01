f5e42afd346269e775e0f49c09ec62a39491760b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:46:50 +0000

    behind apologizeapologise tool come film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
