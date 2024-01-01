2d64f00c533302fad30d4dc15efb9433471c267f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:29:17 +0000

    immediate search union except condition

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  def square_number(x):
+        return x**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
