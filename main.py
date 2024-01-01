50c6974ea4ac9e70e63a04f6b9a43846bdfd0610
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:26 +0000

    brown expect breakdown stay cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
-print(sum(range(10)))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
