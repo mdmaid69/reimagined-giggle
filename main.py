bc2a4cfb4252e94521915df0ea5322aaefc52ecb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:36:45 +0000

    drive typical male influence last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def remove_duplicates(lst):
-        return list(set(lst))
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def is_palindrome(s):
+        return s == s[::-1]
