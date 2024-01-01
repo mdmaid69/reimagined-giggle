a4e2dc422d4ad17fdd3b220a750e224d97da147e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:32 +0000

    thinkover funny emphasize shelter between

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
