998fa90f88187a33609043f025243548aa47eb2b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:51:10 +0000

    afternoon stamp bet pick standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_int(array):
+        return int(array[0])
