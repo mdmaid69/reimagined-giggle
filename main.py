ff1b35fb0a9bb233d639a0b6624d7e271ca94e15
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:46 +0000

    include cry like hear keepup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
