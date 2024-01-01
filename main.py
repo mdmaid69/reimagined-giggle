0cb331664a4f54cbee6f6b1d1a9b12b7e183a1f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:14 +0000

    rate only comein few turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import datetime
+print(datetime.datetime.now())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
