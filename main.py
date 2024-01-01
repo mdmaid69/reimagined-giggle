3c45bdac3a92441377300c1dce2e4993acfcbe95
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:17 +0000

    split how steer start protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
