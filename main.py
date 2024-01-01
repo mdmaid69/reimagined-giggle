60d54922f9bdeba4f3d038b5d04cacc200e3fb69
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:30:00 +0000

    deal blue spring uniform recover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-print(os.getcwd())
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
