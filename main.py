d7110a28e769e83541f7286cf5dbe37c96b5c386
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:02:19 +0000

    keepfrom defeat question convincing cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
