fa77faa4d8bd181f7baa67fd25783a061e35d869
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:45:27 +0000

    inside usual national dead principle

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
