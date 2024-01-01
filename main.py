9aa3bf483a49b9e8e63eed23764c171d54ec30b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:25 +0000

    succeed expect nothing break below

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_str(array):
+        return str(array)
