fc84b28ff15227bafd3e9f8d6427d1b853132d7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:00:05 +0000

    need motion type breakout story

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-name = "Python"
-print("Hello,", name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import numpy as np
+print(np.array([1, 2, 3]))
