9b0c72fc81e826d8d1cfcc0b740947fcac8003be
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:20:26 +0000

    thumb needle feature behaviour large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
