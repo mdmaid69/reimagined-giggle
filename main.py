802e4df7f7cf6950500d9d8241e7d4b211a52f4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:25 +0000

    effect judge poison ourselves describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import os
+def remove_directory(path):
+        os.rmdir(path)
