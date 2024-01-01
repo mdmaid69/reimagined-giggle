bbc73cd4af2152463933d5ae93968ee01535ff4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:25:52 +0000

    meeting challenge leave glass during

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
