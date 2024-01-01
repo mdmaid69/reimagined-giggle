eeeb36323d4015d2dbb1f28041e5793fdb0b2dee
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:23:57 +0000

    man keen challenge possibility stage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import sys
+def print_python_version():
+        return sys.version
