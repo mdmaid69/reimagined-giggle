92cb7e741b9741803b439ef98bc59a8d4abca1fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:59 +0000

    about talkoutof soil combine confident

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
