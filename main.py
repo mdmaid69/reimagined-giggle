173876435f4ab3ac6caceed40f41567d17da268f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:53 +0000

    pick pretty desperate pass hot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
