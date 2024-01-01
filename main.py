e505022b5e3ddae64ad08feb89b1504aea2ee7db
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:44:22 +0000

    professional note your mixture well

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def is_even(n):
+        return n % 2 == 0
