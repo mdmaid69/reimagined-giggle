06fd71fbec4d00b5d714f23d42e190791876a733
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:50:24 +0000

    welcome lucky admire read walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def sort_list(lst):
+        return sorted(lst)
