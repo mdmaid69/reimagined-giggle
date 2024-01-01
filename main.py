e08800d9def53dea562b97a2bcfb01ec016d7f75
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:46 +0000

    press believe need normal nurse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
