564e3ad36412bd56295d1e961232233eb5d18364
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:55 +0000

    plan half fresh gas anyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def reverse_array(array):
-        array.reverse()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def greet(name):
+        print(f"Hello, {name}!")
