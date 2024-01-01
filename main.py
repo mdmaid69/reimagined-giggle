a5e9b9eed39e5a0e87df81be6184b280fb573d03
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:41:58 +0000

    platform healthy tip string tie

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_current_working_directory():
-        return os.getcwd()
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
