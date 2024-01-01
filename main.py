de4d54d1b2f0dafb50728e95922bef77b8c2908e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:44:24 +0000

    boring slice thinkthrough comfortable taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
