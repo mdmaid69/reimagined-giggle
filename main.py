731b8ec029d72983efe53e358dd4eb593c620b47
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:10:21 +0000

    tear aim guide comfort soil

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
