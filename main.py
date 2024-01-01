ccf79d0b08498cb1bf00ba90d69031e713c779b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:07:32 +0000

    direct flood translate weekend positive

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 import array
-def get_array_itemsize(array):
-        return array.itemsize
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
