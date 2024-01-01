1c68b33233380e5b38ba9193e03edfc5223b7857
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:02 +0000

    confuse discovery pleasant fierce narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(lst):
-        return min(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
+import collections
+def create_stack():
+        return collections.deque()
