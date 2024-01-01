59af7ea92d1f525367a04a8c9bae5c83d198bdb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:36 +0000

    old report flight mine neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def subtract_numbers(x, y):
+        return x - y
