c349e26ceb0133c8dee6247c6ff8144d364a93b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:24 +0000

    satisfied feeling pass black lend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def subtract_numbers(x, y):
+        return x - y
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
