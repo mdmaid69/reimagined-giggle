ad6d248fd07f0e15536baac4884b9aad79be17f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:17:27 +0000

    refuse thankyou fold chief question

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
