8de369ef26c4745f02964c7d3dfe52f836b30110
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:01 +0000

    fair blame act turndown black

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
