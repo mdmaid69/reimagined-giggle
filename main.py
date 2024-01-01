25a06c6b66a635e527ee42f7740f285bd2801918
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:28 +0000

    stage steady throw fear suddenly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_speed(distance, time):
+        return distance / time
