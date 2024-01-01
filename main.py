a85a1cdeda664770f5a3c312f52ea176b149ad34
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:51 +0000

    change sting base measure gate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
