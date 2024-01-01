c08fd3f0694fe748e0ba14b38184cfb64571f166
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:28 +0000

    bringback sensitive majority fortunately border

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def convert_to_octal(n):
+        return oct(n)
