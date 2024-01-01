4c58c7f5810056c52529fe7ac789be8340be479e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:15 +0000

    copy before touch create claim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
