a25a3a9abb17b377e16df086706366b791035090
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:00:58 +0000

    loud wipe Itself wherever roll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import array
+def get_array_as_repr(array):
+        return repr(array)
