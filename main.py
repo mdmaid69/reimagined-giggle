e96853182e2d235596197da841b6ceeff27fae53
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:05:25 +0000

    guess similar cut about advise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
