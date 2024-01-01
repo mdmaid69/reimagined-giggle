81288dc72d33fa18790e551838b34bfd99e653de
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:29 +0000

    before special part safe tap

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
