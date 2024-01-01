e338a44eb69bf42d3986190901f01d21506cb4e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:20 +0000

    idea thirsty takeaway anywayanyhow contact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
