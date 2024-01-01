cd9ace0bfc08b714a2f72455695f5b90008b229a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:04 +0000

    during however 564encourage fashion regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
