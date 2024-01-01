ac11a6079ee4808bd62088c73e7be93c9b2b8b2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:43 +0000

    myself guide consist love say

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_length(array):
+        return len(array)
