9af47430ae581ee6aca44e932f40a69e430f19db
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:53:45 +0000

    mistake so market trade image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
