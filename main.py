b7c5e3f1c78008cf6cf26820d8459619906f1e33
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:45:32 +0000

    first actual correct relax guess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
