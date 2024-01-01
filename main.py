0b115878c8796bc2b71e06688c035aad27a3c8ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:42:21 +0000

    charge hold time fall sensible

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
