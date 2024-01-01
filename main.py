67155272c19764d0c420e2da888b7f858394c58e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:47:52 +0000

    whistle steer takeover correction meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
