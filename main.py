d632879635270c11fc442e9db90df0ffa9a47472
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:06 +0000

    comein seat fast description hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
