5f708e0b2b4d902b68f4421b3c29c04a725babc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:36:51 +0000

    march loud dress stamp opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
