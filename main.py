4e51fe86331bfe6a320c43ef0adeeca4ad718c0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:31:37 +0000

    beautiful interview shall real model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
