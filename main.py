0fe76c608005b5e2e927511f4a4763418e927414
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:51:20 +0000

    increase downstairs ours square awake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+print(random.randint(0, 100))
