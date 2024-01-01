e13ee980f77f1afea7eaa69380c72a71509e8906
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:31:20 +0000

    information use false back needle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
