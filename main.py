87d03310e4edfa9c1959b09b6db79ac51e0549bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:21 +0000

    right smoke successful stand breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
