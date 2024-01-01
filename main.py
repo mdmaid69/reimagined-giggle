a950baa9d957b5cba82dfb4abd3fde00b8b0e31c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:09 +0000

    comfortable willing hurry voice minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_work(force, distance):
+        return force * distance
+import math
+def calculate_tangent(x):
+        return math.tan(x)
