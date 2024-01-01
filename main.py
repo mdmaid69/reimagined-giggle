af224d6f43a39291d2bee697a7a1f654c953b0d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:42 +0000

    pleased professional adjective report turnaway

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
