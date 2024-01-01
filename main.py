18edc49a2cf82180c78febf5f61f5eb386086d47
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:22:17 +0000

    slip future touch Hh bringforward

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def sort_list(lst):
+        return sorted(lst)
