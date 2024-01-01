bb2003a4070653fea0a9d3f645a6c8eb2428a4b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:28 +0000

    woman fall fight blame activity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import time
-print(time.time())
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
