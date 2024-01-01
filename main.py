1e501b5b787f08dc9b8d7ba891ad9a4aa2e65652
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:41:22 +0000

    cross as rest spot blind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_queue():
-        return collections.deque()
-def convert_to_octal(n):
-        return oct(n)
+def count_words(sentence):
+        return len(sentence.split())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
