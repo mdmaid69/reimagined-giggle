93a818ba6895ef63bbc047989db616c7256111c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:15:19 +0000

    share compare body label accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
