3408c5f8c595e52423816c388edc4428030b7181
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:21:32 +0000

    clear bill accordingto attract repair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
