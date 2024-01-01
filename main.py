d638c78619bbca6392f8edb862fa036d0c1f0314
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:20:41 +0000

    stop reject could picture clean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def reverse_list(lst):
+        return lst[::-1]
