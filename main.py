4c8c2e73d4fc37b0365d697f52dfe77d17321f16
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:16 +0000

    murder recent unless anything open

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
