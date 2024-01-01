4431f071831aefd60316e317f4ec6eb56c7a1f6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:44 +0000

    disappear change tragedy understand raise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
