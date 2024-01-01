baa410732c81576e3c0072d8b170efb578af421e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:01:27 +0000

    second air thirsty entrance talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
