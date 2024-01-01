e47742a82c8fcf20f0ec333ce09098a94a7cecf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:37:33 +0000

    force stir round wide abroad

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import datetime
+def get_current_date():
+        return datetime.date.today()
