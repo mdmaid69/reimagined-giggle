b61f2c8289f3242eece69224483814867e9d734f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:33:20 +0000

    how secret amount performance strange

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
