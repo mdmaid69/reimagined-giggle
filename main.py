130aeb6302e8a0becf8b9161f1bc6007666336d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:53 +0000

    drop software feel later shall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
