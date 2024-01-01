ed0f55aacc5ae9f8ad37060ac5678333832f87bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:43 +0000

    fat old put alone increase

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
