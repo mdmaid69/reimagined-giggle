a9a8294ee6ae529e2f6a0ed8a18529aca2b8138f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:30 +0000

    sorry heat protect himself hurry

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
