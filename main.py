d73d27226378a95e57d78e01d939abc93fccd34f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:28 +0000

    level above take refuse bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
