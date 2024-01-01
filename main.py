93e065a34b13cf220be93f8541df2cb05e3c1107
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:58:31 +0000

    yourself once emergency wild birthday

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def count_words(sentence):
+        return len(sentence.split())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
