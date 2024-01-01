87cc328768238325aeafa3c3ab83fc5ad7ba3fcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:28:00 +0000

    likely trust character light guilty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
