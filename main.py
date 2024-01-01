00adf1553078bfc15457ef308317a3414245e2bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:10:07 +0000

    bring in day grade copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_as_bytearray(array):
+        return bytearray(array)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
