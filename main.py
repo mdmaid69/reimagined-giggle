7c555ef02d4f7011dfa03b4391fe1f1acf3e9126
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:03:40 +0000

    government common theirs dry tap

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
