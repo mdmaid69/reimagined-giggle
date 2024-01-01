d6abcc454a62da1e68dac07c8411e87e444e28e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:41 +0000

    meaning spread cause claim obtain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
