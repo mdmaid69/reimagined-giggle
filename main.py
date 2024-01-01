73a735b997d5cf33ca8f9b0c3fbeb156acbaa728
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:47:24 +0000

    form excited burn wear slide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def get_array_as_str(array):
+        return str(array)
