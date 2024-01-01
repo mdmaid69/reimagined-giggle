81a80f577ff7b65656b67d0aea9aed82497c5155
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:45 +0000

    Mm contrast part takeoff through

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  def subtract_numbers(x, y):
+        return x - y
