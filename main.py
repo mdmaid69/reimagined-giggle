4b17f01ef5c706d6870e14b7ff5e0d1540e43cdd
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:07:44 +0000

    throughout pleasant start song clue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
