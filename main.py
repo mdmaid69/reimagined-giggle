1f84f04f6400cfac6100cd8889dd6e798921bd98
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:37:12 +0000

    holdout disappear adjective regular greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
