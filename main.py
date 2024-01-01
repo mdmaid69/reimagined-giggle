53834f07158db320494c3953f5b2bd40b5867fc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:58 +0000

    free angry comeover issue brown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+for i in range(5):
+        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
