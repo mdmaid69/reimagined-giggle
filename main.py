30e74e24e1cd287f1aa1451c70ab00ba1659ba43
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:57 +0000

    show door undo bottom whisper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-def count_words(sentence):
-        return len(sentence.split())
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
