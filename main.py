ecba15e010f57bc8ec5ff194723beaad1bf9581b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:32:27 +0000

    firm confuse cheat arrive smile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
