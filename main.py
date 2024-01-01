021994651a4e91eedb7f2950660c14a5829752f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:30 +0000

    tradition fill interesting giveback our

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_area_circle(r):
+        return 3.14 * r**2
