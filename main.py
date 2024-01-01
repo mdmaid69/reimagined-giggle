2665213b8db5e09f7ece36352c4d0377f88fabd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:54 +0000

    clear steam approval punish destroy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
