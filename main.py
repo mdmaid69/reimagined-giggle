9b43cb80e2c78329a0fa015a6305286e4d463da0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:24:57 +0000

    dream birthday store port send

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
