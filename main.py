da2d4e620cf981d541bf554718315a195c1ed0ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:14 +0000

    after meal month step original

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_base_name(path):
+        return os.path.basename(path)
