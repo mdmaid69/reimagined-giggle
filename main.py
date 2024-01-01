a82f66b40cb0a52b036d7201b9d1ce7a12eecff0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:31:15 +0000

    comein marry conclusion awake quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
