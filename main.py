fe28327e9996cd31fec4229ffa9baa06eb759cdd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:41 +0000

    true rid seem talk arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
