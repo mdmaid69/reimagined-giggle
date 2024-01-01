c3ec2bb440f2baaffa8fbd2c187e3f7d4a1da7c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:45:19 +0000

    ticket memory minority someone excuse

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_min(numbers):
-        return min(numbers)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
