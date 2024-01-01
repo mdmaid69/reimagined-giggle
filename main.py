80466a562a37bc113869746292348a13a67aaf75
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:52 +0000

    fear interview bet getaway development

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-print(os.getcwd())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
