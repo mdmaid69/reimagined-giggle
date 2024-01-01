acc57c4d2bd5b83c5c612bfa957566a168749edd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:32:52 +0000

    every brain march raise from

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
