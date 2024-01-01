5209567951cd33a4d3081803c8b4d8d52031731e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:14:25 +0000

    look force spend bag dry

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
