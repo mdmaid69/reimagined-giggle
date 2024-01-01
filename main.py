744a8d8c2d7ad0cd462ea2723f3136a04df7ad9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:43 +0000

    broken loose easy unfair nonsense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
