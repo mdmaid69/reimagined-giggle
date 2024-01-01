4e514e80491bb74743727d4d9527b71d152f2bb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:09:28 +0000

    pride tax tough length in

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_floor(x):
+        return math.floor(x)
