dff7f2407b6cf3cb9b4cf80c0ee1b70a1cecbf80
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:46 +0000

    forever trap happen rubbish dinner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_density(mass, volume):
+        return mass / volume
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
