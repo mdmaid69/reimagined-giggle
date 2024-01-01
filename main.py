9ed111e5ef353a3645b29d5e3d8d4fd38c7955ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:35:42 +0000

    culture guide impressive less line

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
