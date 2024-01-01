8d2432c951f28ffcddbad7bcf5f24db9e17556b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:40:30 +0000

    card building glass listenfor or

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
