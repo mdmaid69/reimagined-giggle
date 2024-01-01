74fccb22351dd6dc5910bb379b360d60b6a59dd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:01:47 +0000

    apply shrink normal switch tread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
