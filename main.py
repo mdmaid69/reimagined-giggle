fce0b2bf093cdcc96340bbab27b297e7d5ca2cfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:21 +0000

    floor minor convince willing close

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_user_string():
+        return collections.UserString()
