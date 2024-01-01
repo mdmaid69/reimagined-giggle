a734c13152c825bf8d36f628ed4a24ef241e50bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:26 +0000

    steal wish concentration trap order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
