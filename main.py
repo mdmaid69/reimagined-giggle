bcb149208e392102196be08b7fd8dd15cd70e762
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:10 +0000

    suckme rob spring tip curious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def log_message(message):
-        logging.info(message)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
