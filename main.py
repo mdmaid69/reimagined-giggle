27b662c1747ab07cfa47fe43f1c9a79aa3cc1efc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:45:26 +0000

    over wake kill safety basic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
