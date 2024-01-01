9efee6631d2b2b4352d1082f4d77852245fde468
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:46:27 +0000

    nowhere test train confusing explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
