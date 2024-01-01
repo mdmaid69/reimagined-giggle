54f08d22db6489f8195423e17745e59f8e719097
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:30:21 +0000

    attract post plenty yours fine

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
