4f54a37904956f346d15dd8c9f52e40cf41c2fed
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:33 +0000

    immediate flight scream whenever before

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
