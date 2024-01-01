95809f4395b07d106f8eb8b29bad159970f543fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:22:01 +0000

    never opportunity giveup cut congratulations

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def reverse_array(array):
+        array.reverse()
+import json
+print(json.dumps({"name": "John", "age": 30}))
