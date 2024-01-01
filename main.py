a56b757adc4634fb6551925656e18a3831e87f3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:24:18 +0000

    complaint both turnup extra they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
