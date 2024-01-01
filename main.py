92ef7b55ec9e000b02e1adc570bc1f392e9c601c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:51:12 +0000

    heart Ss quit woman increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
