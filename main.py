3f002a9929780affeb064732f141ac0b844ba046
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:58 +0000

    Cc imagination generally official typical

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
