3ac437919ac294d158d5d3244b5715a08e548f17
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:01 +0000

    present where hard pleasure willing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_item(array, i):
-        return array[i]
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
