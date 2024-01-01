e8000eb1f2bc0e349f57cff43184d341ebc13f04
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:52:43 +0000

    ahead broken Ww acceptable abroad

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  def convert_to_octal(n):
+        return oct(n)
