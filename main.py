7130fd3b839a4ea2d928e2bb237be76cc31eaf7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:58:59 +0000

    receive present use bringforward rent

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+print("Hello, world!")
