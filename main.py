67c55c9d9a0a3fb49626b677d08651086b6db094
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:33:36 +0000

    comeoff sign apart hold afford

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
