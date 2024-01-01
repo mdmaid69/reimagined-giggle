11500e65970c62651c2c8b79c7cb1b83d8a0920b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:29:52 +0000

    contact morning love please few

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
