6153ce9fb17c8cfc232539e2204015ae80b95951
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:23 +0000

    return equal window through cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
