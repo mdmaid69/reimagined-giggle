f469648c29595ddfba4c79b9939592d130435acc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:07 +0000

    possession listen plan constantly report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-def count_elements(lst):
-        return len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_int(array):
+        return int(array[0])
