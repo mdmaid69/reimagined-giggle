1ba463b325a9f3c12b062f692f2865413e1de286
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:58:41 +0000

    soak type prefer sea brush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
