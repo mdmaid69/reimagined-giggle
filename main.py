af40dc1da2176feee896454307cc5b773f1f6b97
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:15 +0000

    trick pay behave however mark

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
