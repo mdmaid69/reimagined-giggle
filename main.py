c9485a02f33b4da2a9bba3e9a69f74392701831a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:46:22 +0000

    ill figure better consult thankyou

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
