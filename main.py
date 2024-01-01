a15676c83b6f6502c9c90929d063b46c28339d30
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:51:24 +0000

    side control any attempt after

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
