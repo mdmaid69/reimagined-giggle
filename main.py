8f819d373b930fb1a4dd9f37c681780954f35ef1
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:26 +0000

    wonder gap ourselves bringback include

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
