65c6392a7f5c1056f9bd97c5cfd9f39ac05e7d87
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:25:20 +0000

    transport protest petrol opinion anyone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+for i in range(5):
+        print(i)
+def find_max(numbers):
+        return max(numbers)
