1d8a226cc92d15222635e85794575d6c92e8ad3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:38 +0000

    quantity behind shout forget keepfrom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import collections
+def create_counter():
+        return collections.Counter()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
