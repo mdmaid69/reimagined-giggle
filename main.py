fff8b72ab9326a95822f910aec4e9c1eeb726fc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:50:35 +0000

    afraid maybe perhaps creature keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import datetime
+def get_current_date():
+        return datetime.date.today()
