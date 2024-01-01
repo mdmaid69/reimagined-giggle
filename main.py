b7dd21e8c94f89f8b37cf8d66daf62491bf0c29c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:03 +0000

    laugh measure ahead match deliver

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
