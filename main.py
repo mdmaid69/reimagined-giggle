4a120fce5c875fa4b9fd2278b6a62e0491b0f3d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:38 +0000

    correct rid meanwhile hot cruelty

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
