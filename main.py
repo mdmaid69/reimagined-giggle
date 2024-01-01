a2e59d185a67717d8de6d945ffd6c5c9ca7bab25
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:08 +0000

    cum relationship learn lend imagination

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
