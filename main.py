39541c8df1a415eff9be13597b6b12153de83577
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:30:41 +0000

    disgusting address bunch accept miss

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_list_from_array(array):
+        return array.tolist()
