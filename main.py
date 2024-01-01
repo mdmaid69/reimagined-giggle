f6a989e0f86e4b1ceeba264c108492583b497de1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:54:22 +0000

    holiday accordingto abroad wish holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_area_rectangle(l, w):
+        return l * w
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
