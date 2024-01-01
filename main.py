2c9552f1c2fe8fc601198100049585b7453dbfeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:24:55 +0000

    share good Ee ever theirs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
