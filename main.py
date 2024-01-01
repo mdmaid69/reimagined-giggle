682fcf3e9fdaeb03cba290b843b0ef9d6edb519a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:39 +0000

    pattern show pile talkback crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(5):
+        print(i)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
