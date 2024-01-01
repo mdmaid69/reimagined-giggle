5157e855e1b529691f472a81c820efecb9ecdf0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:27:12 +0000

    whether ring place weigh later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_list_from_array(array):
+        return array.tolist()
