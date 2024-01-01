019308c6ad52cb91a5b332423cdb3cd4171eceac
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:45:40 +0000

    congratulations meaning impression play die

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
