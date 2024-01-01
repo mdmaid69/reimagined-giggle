554050040ddcfb9d8ecb5377c77d2b1a4b43f4fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:15 +0000

    finger love official state card

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
