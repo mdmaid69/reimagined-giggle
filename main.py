8c13c63b88ad07ddd0a46d6e3816195dee3e8e64
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:58 +0000

    see safety make regional rubbish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_max(lst):
+        return max(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
