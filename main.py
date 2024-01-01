b3ec3827ba29873413eb7be569232bd28d88c224
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:55:21 +0000

    some feed poor lunch juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
