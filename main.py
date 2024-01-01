de6f27fe1bb603da3e6171a33b84a852d2f9d28f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:41:08 +0000

    short little message male scream

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def reverse_list(lst):
+        return lst[::-1]
