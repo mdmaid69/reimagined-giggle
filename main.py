65c5b4ba65300613a84d05aa06d48a0c8a803fdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:51:05 +0000

    conclusion every forget its performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def square_number(x):
-        return x**2
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
