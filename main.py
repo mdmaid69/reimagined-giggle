18f11a101bb43fd2537e5566c0778a9967b0db9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:44 +0000

    way prize contents pencil emphasize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
