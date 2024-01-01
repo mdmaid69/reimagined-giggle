c8012a9f989dbcc68a2a6e0406d8526123b08d9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:32 +0000

    independent typical right giveout convince

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  def convert_to_octal(n):
+        return oct(n)
