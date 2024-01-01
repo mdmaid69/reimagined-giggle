fdcb69cd1e78b877de6c950b1468f36afdb68f83
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:27:41 +0000

    last that deliver thick Ee

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import collections
+def create_user_list():
+        return collections.UserList()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
