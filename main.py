c43a91859fb88a077a2feeae05f054f0150cf104
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:29:26 +0000

    most provide follow bringforward exciting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_max(lst):
+        return max(lst)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
