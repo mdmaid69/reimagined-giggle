55113ec8ef2bd232e40a3e32cec605069ed7dc5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:21 +0000

    leave pay oil exchange Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
