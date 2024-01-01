755895683a331e323b6b14d35e4db45bb9e193bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:52 +0000

    nothing keen pick curious cover

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
