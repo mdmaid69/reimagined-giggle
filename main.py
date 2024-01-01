4749a2efe1bc55dc1e045d2e9bcadd0fa8d4163f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:17:49 +0000

    winter very damage soil aim

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def is_odd(n):
+        return n % 2 != 0
