d9a039c23577221a10d14bdcff5a0ec230e51228
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:26 +0000

    behind shine lay bored speak

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 def calculate_circumference_circle(r):
         return 2 * 3.14 * r
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
