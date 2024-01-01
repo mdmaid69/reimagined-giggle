9cbc7931a5022ae5d3eece16e64c5598f5740c8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:21:48 +0000

    whistle willing break negative target

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_list(array):
+        return list(array)
