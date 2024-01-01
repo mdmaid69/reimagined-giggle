7d1f0d7d52f3f55231cbf7ea3455a399e7d53e55
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:34:22 +0000

    become introduction minute drink undo

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def is_even(n):
-        return n % 2 == 0
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
