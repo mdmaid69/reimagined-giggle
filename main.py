6c29238c5a22ab820a18d9e321b1395f1eeed39a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:10:59 +0000

    exhibition giveaway fall rise only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
