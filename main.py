cd1c4021c844812db2d109dc36d386d72e98ee62
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:13 +0000

    quiet hold pour crowd borrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
