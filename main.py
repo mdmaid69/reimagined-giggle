5c090d4473eb7fcbabf8b1ce05cb12523173d272
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:05:04 +0000

    relief appearance soil practice invitation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
