74515ddda7990c6e08eaf7927765f94726cfb31f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:05:12 +0000

    hollow late out follow Vv

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
