b0323ce134dc912f905c2866be8d2c3c40a7d31a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:05:37 +0000

    blow right line trap basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
