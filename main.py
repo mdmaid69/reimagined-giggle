6c4416e31b54095d0fbc59b82a5c354b0b770d9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:19:08 +0000

    accident benefit find birthday pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def add_numbers(x, y):
+        return x + y
