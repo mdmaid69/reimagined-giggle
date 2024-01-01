c892662e7fa6b50619b3419b65465456fd15a5dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:07:04 +0000

    shame move charge complaint before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
-print(random.randint(0, 100))
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
