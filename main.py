8091df3e953932729ed055b46aeb25f537631fa6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:57 +0000

    standard before touch snow load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
