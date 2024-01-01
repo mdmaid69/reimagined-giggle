6fa623c84bad667ab8af9d5f1f628bd1389b6861
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:09:41 +0000

    successful admire himself goaway confused

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
