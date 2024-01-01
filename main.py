782523e43318019b87b7bbd58d0f45c4612953bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:50:37 +0000

    remove breakout so disgusting bad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
