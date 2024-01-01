bc513c50e2e2f99a04be549b94564d327ff03946
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:42 +0000

    bread light guilty time arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
