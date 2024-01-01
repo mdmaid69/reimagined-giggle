bf57e15b34a2356c589e02b8064cf70ba292a3c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:25:34 +0000

    clearly sport fold release sail

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
