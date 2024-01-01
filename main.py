e96abfe09581385ceaa7d2170050255b4f4b8ac5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:57 +0000

    protection great tie blow hollow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
