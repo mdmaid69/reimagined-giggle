f0911e59c62d87d30e66b6ecb815cf94985447c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:04 +0000

    quick speak takeoff OKorokay comeout

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
