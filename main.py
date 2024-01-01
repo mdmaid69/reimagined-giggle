55c5e4712e8d179c0dff04d5cd5c2a6bb1533071
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:11 +0000

    realize block tomorrow capital demand

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
