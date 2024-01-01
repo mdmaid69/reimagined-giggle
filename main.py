2ef0c632a1b8a84fb6a73b43454d6eca16bd174a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:55 +0000

    cheap busy Qq sweet honest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+print(sys.version)
