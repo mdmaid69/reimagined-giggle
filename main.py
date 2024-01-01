8b381f9dcfe59fb6243866b5b993fe27cd252d7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:28 +0000

    release let blue judge popular

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.ctime()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
