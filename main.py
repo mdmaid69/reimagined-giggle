dd501b4c871d5fb40fcf303ac5266848f5626cd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:48:20 +0000

    command cruelty break pinch disturb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
