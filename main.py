8de7176a08251c86091068dc650a96e4f27ca884
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:17:46 +0000

    post thinkof miss people neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+print(json.dumps({"name": "John", "age": 30}))
