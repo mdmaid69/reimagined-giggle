7945148a1375069f2e2a819928e5bfaa2c7865e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:34:11 +0000

    fear downstairs Hh interview compare

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
