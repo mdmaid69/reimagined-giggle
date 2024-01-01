8ee8c8cfed4ff6ac6118f2bcb2bd18eb6979d898
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:53 +0000

    travel reject attitude guilty Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
