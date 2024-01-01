878684097efebe3f25a0710eefa2c6c01feda4b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:06:36 +0000

    begin trade Dd middle clever

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def calculate_volume(length, width, height):
-        return length * width * height
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
