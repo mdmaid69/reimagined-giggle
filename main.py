929ad124f3b1103bb7b17e156fd0a93f3a85608f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:42:11 +0000

    practical secret computer rain crime

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
