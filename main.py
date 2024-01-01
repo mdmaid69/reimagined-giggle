76eb3e002cdda0b265cd5c57127965b2a4e88f23
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:55:09 +0000

    image unlucky community juice tap

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def sort_list(lst):
+        return sorted(lst)
