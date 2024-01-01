812c110e7243bf75bb27269a5b1e4c2555d1268c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:31 +0000

    off Uu properly likely borrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import platform
+def get_os_info():
+        return platform.uname()
