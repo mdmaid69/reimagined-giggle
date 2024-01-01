a4cb7f750769008eabfa43e3a2f603b21c0ac848
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:43 +0000

    need success public tip slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+def square_number(x):
+        return x**2
