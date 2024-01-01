8a7e8d74a124cf09b51bf804528adcb6489e8c6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:48:03 +0000

    justice wide pause nor injury

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_repr(array):
+        return repr(array)
