ac15b380192917075bf161fd420865b4c45018c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:28:00 +0000

    tone beat shadow burn past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
