bab2e212e64b56cfd62e2c9d566bc8b637cccbf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:30:53 +0000

    takeoff rob tap crowded success

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import re
+print(re.match("h.*o", "hello world"))
