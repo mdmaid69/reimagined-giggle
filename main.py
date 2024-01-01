1ba95dc9c8ba029e3c44b80ac2c51c1c282ae6cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:58 +0000

    experience community workupto pile rush

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def create_user_string():
-        return collections.UserString()
+  def is_odd(n):
+        return n % 2 != 0
+def find_max(lst):
+        return max(lst)
