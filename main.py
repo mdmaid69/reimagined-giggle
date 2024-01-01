6111ac7b2b312f0bcd4594a9327a28b1f65ccd55
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:55:40 +0000

    wide fail fit rough round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_hex(n):
-        return hex(n)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
