672eb88388839e3dbe79e82c2c2cdc73ed5b1242
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:46:52 +0000

    people though exchange enthusiastic but

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
