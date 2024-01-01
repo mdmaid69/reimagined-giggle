61b901073d9fd8d41ef5c8ef53de0342f7250571
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:43:45 +0000

    home much statement suckme desperate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
