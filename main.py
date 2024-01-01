65e4d2767ae313a6062d44f046209d48121db045
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:59:00 +0000

    chin print bend stage cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
