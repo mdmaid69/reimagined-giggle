61e0d4e14ad0f8f4e439c3c33a6582dba6a61948
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:00:24 +0000

    crash him with last awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import datetime
+def get_today_date():
+        return datetime.date.today()
