2b33a0f09940403e7111d51780146249156a630e
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:37:54 +0000

    notice away bunch shut habit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(5):
+        print(i)
