67ea9c5520b2dae587036322261de84f42fd7356
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:17 +0000

    there straight getout swing nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
