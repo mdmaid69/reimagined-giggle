cf66a79d660cbf94c2569808a0cd7b51109400e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:14 +0000

    nature speech fail bush message

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def is_even(n):
+        return n % 2 == 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
