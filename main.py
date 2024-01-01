6c1afaf423b11cedc6c4f0eb826d047beee85393
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:18:01 +0000

    rob match pain mine breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
