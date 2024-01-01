2588cc683844770c66d2bfe27287e4e73a179906
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:42:12 +0000

    form take scream attract else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
