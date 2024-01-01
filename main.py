b5c6d045ec9277c0d935404ab63158893acb6254
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:02 +0000

    raw cost slice cut break

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import json
+print(json.dumps({"name": "John", "age": 30}))
