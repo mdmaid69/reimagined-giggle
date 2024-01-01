d717af0cd6ebd50f015778aa53cc5b3aa5a0d9ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:00 +0000

    manage bread though computer study

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def greet(name):
-        print(f"Hello, {name}!")
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
