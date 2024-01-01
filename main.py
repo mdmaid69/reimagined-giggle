f543d09388edb8523af59496704c51da01f231e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:47 +0000

    drink gas black cold line

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_error_function(x):
+        return math.erf(x)
