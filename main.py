8c0cfecefe0bdadc10680f421465b89141ad8502
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:20:55 +0000

    ashamed reject underneath official offend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
