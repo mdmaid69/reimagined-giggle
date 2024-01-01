05b7db334263404a4b3bf9a9cc8dae798e6ee92b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:49:02 +0000

    decision aim over wish face

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import re
+print(re.match("h.*o", "hello world"))
