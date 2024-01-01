322642ede5846b25dedd5a52118a7bbb5d171d8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:44:24 +0000

    extremely party complaint or certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
