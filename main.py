642101db75e863876d7a44fc12b7a73bbd259c00
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:06 +0000

    present attitude used address fall

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
