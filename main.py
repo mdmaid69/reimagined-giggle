5b5ef65212a481f491c8a7baed56908c6f1b19c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:23:02 +0000

    idea exploration estimate distance such

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  def sort_list(lst):
+        return sorted(lst)
