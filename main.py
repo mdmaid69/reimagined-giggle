53080935fb82d9d9e0a340d05e6268e49cc8696f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:59 +0000

    tape fairly command delicate myself

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+  def count_elements(lst):
+        return len(lst)
