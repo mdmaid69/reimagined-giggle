e864c8d91e984362e75d0edf6c228e993de3b172
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:24 +0000

    understand similar narrow present terrible

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+print("Hello, world!")
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
