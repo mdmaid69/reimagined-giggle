2ce3d87042a57be9dd3c659c78e803de8e2558d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:05:49 +0000

    turnup inform close sell perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
