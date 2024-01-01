bb365bbc37547204725d59d9dfca70417768de0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:50 +0000

    positive space difficult achieve grow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
