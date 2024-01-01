1c6f28296849915ed019c52abb4ab42e13617e58
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:56 +0000

    short key light exhibition reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
