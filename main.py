f78f16105cf509b381ac66d3cefea79b6e93593d
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:11 +0000

    answer nowhere stranger guess urgent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
