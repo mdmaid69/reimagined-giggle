a9a7c6260ee9cffe949065471eb58b758c33862e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:36 +0000

    inquire contribute test likely ill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
