fbd16e8dc63322ea9b7dc1a6581b4d33607d44a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:25 +0000

    born favour goback(to) giveaway tone

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+def change_working_directory(path):
+        os.chdir(path)
