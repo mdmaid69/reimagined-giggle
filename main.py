40139a72d4ecabace516c6ec7a75736fa2b146f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:05 +0000

    section depend answer against temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
