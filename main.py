ef71592458cf0b4b8a5923358a147c3a90c39a20
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:06 +0000

    perhaps opposite dream light address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
