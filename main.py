5b4971f95476f3c5cb66f2dc58fcf9c1e03ff379
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:27 +0000

    after indeed probably Nn lookup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import sys
+def exit_program():
+        sys.exit()
