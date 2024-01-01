16b7e3a4361f262f2aab487871620a3d742888dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:20:56 +0000

    temperature runover noone order show

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
