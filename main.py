c3b084fdc6d07335ce64e0b2afdd67fa60b6e861
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:00 +0000

    her keepout content embarrassed crazy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_rectangle(l, w):
+        return l * w
