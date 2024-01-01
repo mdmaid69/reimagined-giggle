6720473b4b84c3a4029535d63b5796b7d2bb66ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:51:04 +0000

    fall suck wind few juice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
