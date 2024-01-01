e07d2517bfb2d3382ebcdd64092a79fc5a1c6ed3
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:29:14 +0000

    share really proud dish production

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def subtract_numbers(x, y):
+        return x - y
