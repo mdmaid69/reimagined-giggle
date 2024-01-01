2d4c064acc6f558e91ea983b6df807c2fda42e2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:40:28 +0000

    good risk famous emotion settle

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
