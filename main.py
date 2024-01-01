ae1583ceeefff79aed33de3dc33dc5602c88977d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:42:44 +0000

    fall share harm order record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
