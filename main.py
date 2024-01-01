0fc9a76df0c1fa53d064a59e519796e7e161b3b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:40:50 +0000

    why test along feed bread

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def append_to_array(array, item):
+        array.append(item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
