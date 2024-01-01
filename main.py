301980c7ad3daed15875c650f6d685694ee67a04
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:32 +0000

    divide face turnout escape must

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-print(sum(range(10)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
