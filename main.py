028d5aedb9820df68b6530d0de0ac84ac166e7a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:41 +0000

    wander practise fail label colour

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-print([x**2 for x in range(10)])
-import collections
-def create_queue():
-        return collections.deque()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
