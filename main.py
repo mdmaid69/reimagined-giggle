607c88700da4aaf35b945f9649b1ff0f14ef3730
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:01 +0000

    along agree calmly needle laugh

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
