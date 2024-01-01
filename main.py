bcf93f95318ac80e9799460379c1921e28f7dafc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:59:48 +0000

    long what mother myself equipment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
