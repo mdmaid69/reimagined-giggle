c166c843c3223ea8733dd7424654a6f15dc0d438
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:56:39 +0000

    mark funny main friendly hungry

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
