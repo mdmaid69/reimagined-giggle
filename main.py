24c96250607ee1837df4305ee9d0eeac6f70678d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:06:48 +0000

    interview all noise bright miss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
