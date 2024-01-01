ec027365a025f23285caad07f60dc9359a24a0e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:57 +0000

    organize group sore should heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def calculate_force(mass, acceleration):
+        return mass * acceleration
