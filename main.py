4d5338efde2edffcf8f637a216d2ff5299219000
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:16:21 +0000

    sometimes somewhere become fire string

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def split_path(path):
+        return os.path.split(path)
