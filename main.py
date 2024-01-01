e2ad4a33bf2b9095fa4bf88e33353a47026268e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:36 +0000

    serious lift fall list reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
