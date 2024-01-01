c26c090e271390fa557a41f78fbd7764dce480da
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:55 +0000

    cope sea advise Hh state

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
