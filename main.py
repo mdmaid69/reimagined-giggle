c3704afd7bd931f96a6ce429361cdec5ae883539
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:43:29 +0000

    future giveup finally equal regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def reverse_list(lst):
+        return lst[::-1]
