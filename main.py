27f7f27d1363cbfbbc8d449b285cda3d8d6d6222
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:13:51 +0000

    actually experiment Hh autumn live

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
