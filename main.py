f85e8e843f93b1b9bcef9d34dade469357d2d90c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:13:34 +0000

    deal callat pretend they might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
