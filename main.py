188ef104008a49fa99b92cb27fef48b0b8acd542
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:06:41 +0000

    force design wander crisis laugh

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
