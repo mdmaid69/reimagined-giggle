5d09b0a52a08e915aa8104e568d7543f2fd46815
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:24 +0000

    report sure time offer curious

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-from collections import Counter
-print(Counter("hello world"))
+def find_unique_words(sentence):
+        return set(sentence.split())
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
