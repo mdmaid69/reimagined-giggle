11cb1075f2937090cd6d53549f5d8b7f74e04c5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:17 +0000

    bringforward I return 179bite sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
