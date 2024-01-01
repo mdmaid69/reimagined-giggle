e690badd58e30b20205ad6e403cdd0f8f5fed23c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:53:15 +0000

    complain your entry back design

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
