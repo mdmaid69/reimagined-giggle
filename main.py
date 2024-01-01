04537e9e1f34dda25c6a499ebd0774d19b0d7192
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:05 +0000

    share error only justice until

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_min(lst):
-        return min(lst)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
