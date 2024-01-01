a7e2266773bb1cbb871fe7795c7e3ebe17b83e30
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:35 +0000

    do appeal unless preparation unlikely

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def add_numbers(x, y):
+        return x + y
