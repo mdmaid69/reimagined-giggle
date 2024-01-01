7b89ae2b2f9b216649edafbf31f73c2f8e226737
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:29:56 +0000

    Mm shop justice unite sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_float(array):
+        return float(array[0])
