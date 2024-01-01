5fdde6f0e0b2d4ab74e096cefd7352702ffb27e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:07 +0000

    shake on left flat Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
