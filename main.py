87236c17084f3ba42af6e714bf6911cfe3f0a665
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:54 +0000

    everything welcome secret sure opposite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
