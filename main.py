1a4f63843fea67ddb7592b4f54a48621864f4419
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:57 +0000

    wide figure present shop bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_power(work, time):
+        return work / time
