8701412111e80c0e238d21af97a80084c528bf76
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:58 +0000

    566end impression environment slide fire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
