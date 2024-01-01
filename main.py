07e4be69a3f241486c1f11d2afee62b62755717b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:05:03 +0000

    beyond back somewhere pocket relax

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
