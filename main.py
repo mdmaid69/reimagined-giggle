c7cb28e096303cf8bcaae45f2122ec21a5466e06
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:26:31 +0000

    plan among owe art fact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
