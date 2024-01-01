0717f292fedf83c7abad50e24364bada4f13203a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:02:58 +0000

    fall tape own object run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def greet(name):
+        print(f"Hello, {name}!")
