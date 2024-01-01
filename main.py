c244f4569dc1645e7bc9d5cc7400fa0211070ab0
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:02:48 +0000

    memory exchange smell tool activity

--- main.py
+++ main.py
@@ -3,6 +3,5 @@ def get_days_until_next_year():
         next_year = datetime.date.today().year + 1
         next_new_year = datetime.date(next_year, 1, 1)
         return (next_new_year - datetime.date.today()).days
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
