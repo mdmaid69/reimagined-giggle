659b9e3894dc6f92baf2ffc71464fab7f8679697
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:16:17 +0000

    leave getup lose bin basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(x, y):
-        return x + y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
