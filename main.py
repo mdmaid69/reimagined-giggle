45aceeecb65ff17862508c87e81294565bf74433
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:08 +0000

    load unable separate slowly smooth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  def split_path(path):
+        return os.path.split(path)
