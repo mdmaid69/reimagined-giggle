6736fd5d3467bf27cef4bceb3d6b6c4a8bd7deea
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:22:48 +0000

    impressed usually light place noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
