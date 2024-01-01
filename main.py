fb62cade3ff339380f297678e3982884e4248b2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:05:48 +0000

    various dress view downstairs hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_time_since_epoch():
+        return time.time()
