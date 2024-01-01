1a586cd1c865c0824b405bc1824caeb126d9c074
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:49 +0000

    wear healthy pity thick lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
