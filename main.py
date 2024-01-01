717b463145aec520e793a3232fabb59811c85b31
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:42:42 +0000

    lot lunch rain eye class

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
