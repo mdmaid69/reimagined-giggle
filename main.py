8d61ffb23d838c302d2f4e3af1dc1996889df310
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:45 +0000

    foreign ticket admit sense push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
