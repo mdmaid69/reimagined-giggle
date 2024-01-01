d9e54426157fdecf195816442220d841cf2f2bfc
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:31 +0000

    steady else murder his Yy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def square_number(x):
-        return x**2
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
