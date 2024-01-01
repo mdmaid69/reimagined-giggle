a89437c42d7a35b8524eb2ba1f34fd9b4f6103af
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:44 +0000

    miss platform usual always alone

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
