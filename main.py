ec2f37bf011220c0d7941c3cee57458088cc5615
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:29 +0000

    usually mixture from strike yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
