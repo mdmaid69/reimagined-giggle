4ccaee52819c6a01158d9e59f5745ba73171593f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:51:49 +0000

    sharp future definite whole convinced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def add_numbers(a, b):
+        return a + b
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
