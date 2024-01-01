699a47f20f4e3b5fc5f9f544ceaf59774480e97b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:10 +0000

    treat idea aim unfortunate change

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_min(numbers):
-        return min(numbers)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
