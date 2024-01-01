ef2d3e9b8ce059ccb5e92df42c370127265c467d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:00:12 +0000

    deliberately period except full film

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+name = "Python"
+print("Hello,", name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
