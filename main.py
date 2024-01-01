069281b3e155461c4f9cb45131da78d66c6f0945
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:26 +0000

    error separate alone guide statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
