7887febc61595a7ec13b8f46e8fc8cf3f02df2fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:08 +0000

    succeed habit high lesson common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
