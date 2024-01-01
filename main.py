da92e248920d91826d9b96bd17023a7c1b47487d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:03 +0000

    ground meeting stiff height keepup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
