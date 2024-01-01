b864f645ec564650e60e0cf1d6365490dc2597b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:43 +0000

    light certain pure relative reduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
