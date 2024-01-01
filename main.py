6a4937c32ae37115123fada4802aab64436712b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:12 +0000

    exit ring deliver peace equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
