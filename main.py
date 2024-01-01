f4fbb51396655c4205b262a9b0434f6f51068428
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:28 +0000

    ilovedicks he secret battery dead

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import sys
+print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
