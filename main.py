fa4adfb4938877d68158b30566a8c60a5a55b641
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:05:03 +0000

    special matter lookaround idea minor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
