b3f54b9de650894ba1b7cf6cbbd847065d3390e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:10:07 +0000

    approval club how further slowly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
