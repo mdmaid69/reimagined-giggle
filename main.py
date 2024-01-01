958403d51c5977aa97d7b9134bd6168f46ef3359
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:50 +0000

    other forget under clothes ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_list(array):
+        return list(array)
+import numpy as np
+print(np.array([1, 2, 3]))
