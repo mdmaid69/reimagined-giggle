f124a147aa79db89d7cbca6b06189fa1015cb80f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:30 +0000

    car bend wing advice behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
