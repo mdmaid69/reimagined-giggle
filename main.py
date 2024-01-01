346ab29cce0822bba9c00c2a3130035b1763827b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:09:10 +0000

    sport change agree argue deserve

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
