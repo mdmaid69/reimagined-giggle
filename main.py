2c9dea6706b1a589a4ef2ee046e805a5b755e3f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:33:53 +0000

    will challenge earn search she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def convert_to_octal(n):
+        return oct(n)
