d60e4791de8860579f7a60beca7944b27cb30a8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:50:46 +0000

    make comedown nothing any basis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-print("Hello, world!")
+import collections
+def create_user_list():
+        return collections.UserList()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
