76667cadddbde3f8d7ef37796400eb7d9baae60b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:24 +0000

    year poison trouble name before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
