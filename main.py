01ecff964bc49c7ff5e9cfe252f212a38c196358
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:39:18 +0000

    gooff phone further trick takeaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_typecode(array):
+        return array.typecode
