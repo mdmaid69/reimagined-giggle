e0f1419325c3828be843b51ab2ff7b5055669919
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:04:09 +0000

    comeround face future comefrom bread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_user_string():
+        return collections.UserString()
