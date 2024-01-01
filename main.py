43745b91f6eb173c9d226610fd25a985df18a474
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:15:21 +0000

    lookback(on) already convince quick naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import os
+print(os.getcwd())
