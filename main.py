9184c396f78ee38c5a0c81327e82d5e76b634542
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:05:20 +0000

    stare honest any desperate however

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
