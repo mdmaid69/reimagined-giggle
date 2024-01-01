3042847a8d244e4a6a814871355a032b9394fe98
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:46:34 +0000

    bang otherwise cool once ground

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-print(math.pi)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import numpy as np
+print(np.array([1, 2, 3]))
