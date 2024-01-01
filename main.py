b89e466bd2d027780c341bbfd525fde29f6cbcde
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:59 +0000

    improvement unit foreign develop who

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def subtract_numbers(x, y):
+        return x - y
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
