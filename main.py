393a17610375038c708a9fc148d2c4a996dada97
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:42 +0000

    result our example drop bar

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
