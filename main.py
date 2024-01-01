5424ad6244e0e884188e7bdb4d80dfd15bcae570
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:47 +0000

    party talkinto maximum turninto turnaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import os
+def change_working_directory(path):
+        os.chdir(path)
