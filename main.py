159f8ce946be454db3653d80ffb28cce94d7d495
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:35:11 +0000

    branch piece congratulate holdout later

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
