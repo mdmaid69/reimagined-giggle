4070b638407d0c229ed4f3595afecddbeda60862
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:47 +0000

    study see chat minute left

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sys
-print(sys.version)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
