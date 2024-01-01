e6a9fd24f957882ac99b046b361d5e9381b34207
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:08:20 +0000

    moment reasonable hope change public

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def reverse_string(s):
-        return s[::-1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
