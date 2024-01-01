b62df0c0a739933b6575e865991497dd2c14b064
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:22:48 +0000

    yet children value home gas

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
