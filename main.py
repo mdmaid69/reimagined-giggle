78876cb3bb91adba1dba9579ec0e231f76686195
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:17 +0000

    top fill young study extraordinary

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
