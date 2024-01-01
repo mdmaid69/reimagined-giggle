bc961ae48e17dc09c42e9c62dc92c6e64775944c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:21:04 +0000

    bang whistle stupid terrible ashamed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def greet(name):
+        print(f"Hello, {name}!")
