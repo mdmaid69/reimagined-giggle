37869a5f463af736d55b7450000fdfc3229eccc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:26:41 +0000

    declaration telephone load night cool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
