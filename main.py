40d04efecc80fab59bdc8add62819417fb711854
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:52:54 +0000

    extra interested return holdout nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
