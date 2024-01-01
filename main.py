24736aa1c7305ee3c24671079956d2e446656089
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:30:27 +0000

    onto model vote attempt spite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_power(work, time):
+        return work / time
