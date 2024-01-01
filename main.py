b38169976a36a22864c88b854b3ca102f2a7d742
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:28:50 +0000

    post control dress ours keepaway

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
