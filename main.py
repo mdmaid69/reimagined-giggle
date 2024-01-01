d77efcac221a2b6b9d5037893a56d258b990f06a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:10 +0000

    relation unable create complete chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
