aefa97ee2306a7735b5d44d1a91406cf1bab5893
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:04 +0000

    especially seem tragedy able comeout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-def cube_number(x):
-        return x**3
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
