f6aa999c70823bc3a1602debd70a9a0ba544cb9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:04 +0000

    protection concerning any equal left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def is_palindrome(s):
+        return s == s[::-1]
