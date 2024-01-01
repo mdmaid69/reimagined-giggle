4ab1182fa01476c86d0dd5cfae5545eaaa1afd2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:54 +0000

    interested poor closely getup laugh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
