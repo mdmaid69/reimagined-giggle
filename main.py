0af761cc51a463f1c054d5f378a1535913238451
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:06 +0000

    mind turn bringup confidence performance

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
