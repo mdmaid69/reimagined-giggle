19254e6c96317e2df222ea1e2fc6e1d5515a3600
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:16 +0000

    cross train cry middle medium

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-print(math.pi)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_speed(distance, time):
+        return distance / time
