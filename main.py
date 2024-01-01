cd9b16d125fe9a7f5e04320f85d383bae1884ef4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:37 +0000

    estimate getoff manner holdup borrow

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+for i in range(5):
+        print(i)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
