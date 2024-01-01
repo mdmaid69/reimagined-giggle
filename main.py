60ed5da01ab8588d029849ce00923c9b44c3287a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:21 +0000

    cheque material usual tap appearance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
