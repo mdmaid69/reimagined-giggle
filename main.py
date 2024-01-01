e7948dbc9550a9233e73ba78f4d0bf35ebbc7e8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:46:56 +0000

    round entrance weigh town every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
