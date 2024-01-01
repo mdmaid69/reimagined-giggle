7ef4e05c138615aa7b47659e2b25134013a02da7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:49 +0000

    scream cook trust cover suffer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
