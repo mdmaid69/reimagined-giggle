7d1e34eb358e824e316d0e7a642e4c8fb2eb60cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:55 +0000

    drive pronounce breathe fast cruel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
