edfe5878a5e23ea2d8dd9f02e47b45592b6cbb3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:52 +0000

    light careful card rich pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
