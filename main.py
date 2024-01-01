a3c87c3ab04b2fbe376373f6df35072996fb230a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:51:47 +0000

    chief turndown dangerous twist goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_octal(n):
+        return oct(n)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
