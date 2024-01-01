5c4087faac3f45f21cb34603e99fefd565b601ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:30 +0000

    advice left then convince ourselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
