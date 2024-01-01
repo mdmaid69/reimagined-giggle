c577db0a8200a8bb0353a6abed91ee53d0f479ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:02:43 +0000

    crime shop long swim swim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_list(array):
+        return list(array)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
