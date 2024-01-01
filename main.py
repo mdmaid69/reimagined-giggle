ff2a3a9c32da2256e0df8c043452265837240097
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:28:10 +0000

    speed turnup doubt undo field

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-print(sum(range(10)))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
