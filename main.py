c856f4b771d31453127d7ad6d0478c0b7cd71e98
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:09 +0000

    punishment workout land tired fairly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
