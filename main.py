912bb19bf257b75924af09d5b3e87abb32fde5e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:40 +0000

    obvious tool glass illness vote

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
