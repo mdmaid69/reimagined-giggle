519ab072c55a4889241bee4418debe80a5352f7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:36 +0000

    scene pipe reason noisy spill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_stack():
+        return collections.deque()
