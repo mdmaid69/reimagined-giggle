b7145f6b878eb0100a26fee8a73219a7047ed7ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:04:41 +0000

    apologizeapologise back hardly group pipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+def check_if_array_contains_item(array, item):
+        return item in array
