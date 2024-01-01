0d6ac69825fe99952482660478fdb7700b51f4b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:13:30 +0000

    what hurt chase getaway than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
