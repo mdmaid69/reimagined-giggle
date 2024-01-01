f637a7da69ed48f49441d852f2961fd51084888c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:00:21 +0000

    for nearly professional borrow accidental

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
