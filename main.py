aa34cf0b27f3d6bbbbb0e0c24c60d1221c53dc59
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:30:02 +0000

    goup squash affect ilovedicks sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+for i in range(5):
+        print(i)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
