30c4cc7379d6710a78bcef0df3010825c83274d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:44:52 +0000

    stupid bringback home priority Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
