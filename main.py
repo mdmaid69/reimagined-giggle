5f6134dea14d5967647e29415da8520ffa175ba5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:39 +0000

    lose push hurry 572enter although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
