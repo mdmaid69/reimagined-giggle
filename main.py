15e12ed6f35f352f570d9312cdf1e0442daad72b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:11:21 +0000

    other talkback unfortunate this certain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
