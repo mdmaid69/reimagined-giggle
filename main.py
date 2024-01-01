232d54eb76a4aee11a50ebab2bfce5ab4c7ec9f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:21 +0000

    opponent voice sensible hollow book

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def create_counter():
+        return collections.Counter()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
