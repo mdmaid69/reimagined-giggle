45e26012858a595d828e49296773e126cef75b5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:33 +0000

    lookafter remove label disagree run

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+print(os.getcwd())
