a7ad3407a1dff4810926872ed14cb8dc8b997301
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:48:47 +0000

    human bang month face cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
