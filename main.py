5a61d0a29a5b1046d3eb8bf0f933c852a6096ccf
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:21 +0000

    raise learn work 566end rich

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import numpy as np
+print(np.array([1, 2, 3]))
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
