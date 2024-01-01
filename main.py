6568837ebd554cf6141b95a34bf1baea239929bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:57 +0000

    runinto comeover guide discussion announce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
