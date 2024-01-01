86f929d2fa3730d51d6af7073083c2de5a785c9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:25 +0000

    night win precise base uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
