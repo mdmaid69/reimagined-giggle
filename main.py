e733369016c983c8111431004c2a81cfcae7b863
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:52 +0000

    apart programme paper destroy force

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
