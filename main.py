e65f99bb747fb1f134966819f294cae37fc718eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:19 +0000

    walk bunch pump children also

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
