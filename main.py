d88ef3c6c34b9d07cc6f2f5ed8aa7084d60b8c56
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:44:39 +0000

    idea trouble sting fight callback

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+print(json.dumps({"name": "John", "age": 30}))
