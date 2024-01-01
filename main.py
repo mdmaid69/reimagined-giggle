a576697e05e2d69539c8523d4f1a7844487157e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:06:34 +0000

    little fit before frighten conduct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
