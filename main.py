75790c7b0b56154552a4ad44eaa61c5f1deb400e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:19 +0000

    juice although reasonable object comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
