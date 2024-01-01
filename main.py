61191bcf7864bb014841ae4bf2f62004969a012b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:50:44 +0000

    shape worth environment price turnoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
