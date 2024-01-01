bc6baed43a844d4ed45ab3e90fcdb0ed49c9af34
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:39 +0000

    nonsense accident plant me getout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+print("Hello, world!")
