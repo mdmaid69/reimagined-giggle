dd64854ba1266f280e92c275cf7ccdbe0d73bf8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:11:02 +0000

    black underneath high check identify

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
