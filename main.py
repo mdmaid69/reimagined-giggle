a5ca3e09b21ea0c747ccdc3eb9e82d79ab6edebe
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:52:27 +0000

    waste would really few heavy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def sort_list(lst):
+        return sorted(lst)
