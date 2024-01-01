9145139a6da309cae462806599b783acb2369efd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:23:14 +0000

    print distance relief include possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
