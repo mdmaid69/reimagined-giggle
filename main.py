063bc71a8abb970524e7c4aadb508195a4bcc673
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:03 +0000

    feeling quiet possible natural print

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
