3cf5ef39201cea205d2514b2f757f0d022b9fe85
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:13:28 +0000

    completely achievement breakout squash score

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
