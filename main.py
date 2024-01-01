f70a048fda19089701c982165ef5d3c465c72ddc
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:42 +0000

    clear hit first solve friendly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_set(array):
-        return set(array)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
