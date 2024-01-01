cee2102d2a930da6870155a59a50ad2cb6d2fade
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:54:22 +0000

    complicated fit experience operate connect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
