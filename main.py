f606c89aaf25e1ac37541aa274d6141ec6747d16
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:30:06 +0000

    impression fire wide correction busy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
