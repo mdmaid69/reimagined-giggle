bc269cca01152c771b29754c57740dc19365544e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:12 +0000

    design shrink office guard grateful

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
