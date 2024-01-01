5ffe3a6d52e0afceb7ab0a6039543cc00fee6741
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:37:52 +0000

    habit crisis slow mind thinkthrough

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def reverse_list(lst):
-        return lst[::-1]
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import random
+print(random.randint(0, 100))
