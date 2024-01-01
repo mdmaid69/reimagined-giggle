7277b3a06165c8650fc621239c6d5ee413cdf3c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:27:18 +0000

    pity the each accidentally study

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
