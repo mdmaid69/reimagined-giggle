44578f105f23e63a032b5811e793e12e7a58dec5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:02 +0000

    embarrassed flight post apparently press

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_as_set(array):
-        return set(array)
+def find_max(numbers):
+        return max(numbers)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
