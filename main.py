6dfc0ed914611ee742ba18f6f1f788fa37854c09
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:09 +0000

    manner suckme together which related

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
