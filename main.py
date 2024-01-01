e030fd38ede133f69d36ac9488d4e5618867bc98
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:31:13 +0000

    detail run plain furniture thumb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
