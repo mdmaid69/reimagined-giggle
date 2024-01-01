63a8d98198a459fef849122f7872d791b2462178
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:01 +0000

    well condition by band gap

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
