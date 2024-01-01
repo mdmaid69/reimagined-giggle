c0c28e38283325495fe83cf0dd70c37bbc4864b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:50:12 +0000

    there they concentration responsible suffer

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_current_directory():
+        return os.getcwd()
