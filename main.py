0f62cbfcdf110659cc7e2c029190976e4f09a6c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:24 +0000

    favourite reaction defeat high education

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-  def add_numbers(x, y):
-        return x + y
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
