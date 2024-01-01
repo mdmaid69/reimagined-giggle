f71a06a50cd9a343e26be1d736f3673bafd9249d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:30 +0000

    whoever print turndown myself discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
