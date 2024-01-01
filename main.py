4f0a583cc2c2c2832989c9921fe64e8a4761e958
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:07 +0000

    bang road wide deep callat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
