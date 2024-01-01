a1500c59b8bb7d3713f7b00ea91917d8510e93c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:45:23 +0000

    understand capital smooth worth turnoff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
 import http.client
 conn = http.client.HTTPSConnection("www.python.org")
 conn.request("GET", "/")
 r1 = conn.getresponse()
 print(r1.status, r1.reason)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
