3764dd947f3f7684a3adf9414d559d6da50bcdd7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:27:14 +0000

    brave goback(to) near minor know

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
