dee5559d812cc3834b02beb8b1759daf08bb702b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:12:43 +0000

    and steep 569enjoy guilty transparent

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
