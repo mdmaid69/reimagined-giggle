2238ab3850859c5b1441087d8a244fb371f01a61
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:26 +0000

    accept underneath classroom character relax

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def subtract_numbers(x, y):
+        return x - y
+  def multiply_numbers(x, y):
+        return x * y
