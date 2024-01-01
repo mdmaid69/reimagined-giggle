9ab03336c26b7d75d99507483373b32b3b01b7a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:21 +0000

    disappear grade sweet prize issue

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
