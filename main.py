136bc398f5bcfbc882ef90dd0797ad348e36d15c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:36:30 +0000

    forgive community possible importance return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
