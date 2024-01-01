2b56cb27c73ec1d00cbb61f5f02b9766da5dabd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:44 +0000

    like idea turnout forget window

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
