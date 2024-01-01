50bfb0a2f3cf32dc7b6d39babe034069999943ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:19:55 +0000

    pronunciation position pride movement replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  def sort_list(lst):
+        return sorted(lst)
