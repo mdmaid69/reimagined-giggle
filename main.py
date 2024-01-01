096d45a6fa8495bbf7a9ee6d0ec25a4557c2aa29
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:46 +0000

    equipment bunch sensible path fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
