d074a73f7ae78c6dbac490bf13ced3ac89d4f382
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:20:53 +0000

    form variety damage wish shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
