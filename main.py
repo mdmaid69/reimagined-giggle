d9227118b3536439be00e36d9377c877d6e07394
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:45:42 +0000

    pick emergency result repair tip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
