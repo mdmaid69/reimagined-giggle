831533c4183f4bb215f7dafea7941e69e3ea54a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:00:32 +0000

    seriously moon law practice frighten

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_pressure(force, area):
+        return force / area
