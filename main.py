a1c338990365316331c7ee561ccf2a9140f00441
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:18 +0000

    case limit justice substance exploration

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
