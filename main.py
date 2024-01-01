e4a647bec8f2b436a54756496fc98e8b089db4f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:19:16 +0000

    sure command price important imagination

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_power(work, time):
+        return work / time
