b41507f0648193fe3fcac23cde33435dfd371cf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:47:41 +0000

    hold as charge Uu hurt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+  def convert_to_hex(n):
+        return hex(n)
