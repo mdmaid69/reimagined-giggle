02ffc06f007ab06bb7ddad3a9891390e2c07ffaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:52 +0000

    kick duty occur tough comeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
