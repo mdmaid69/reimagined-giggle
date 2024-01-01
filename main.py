a91e7b5bdcee91d015f06861105c1882f7eb7247
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:41:03 +0000

    to mainly dance lean effort

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-for i in range(10): print(i)
+def calculate_pressure(force, area):
+        return force / area
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
