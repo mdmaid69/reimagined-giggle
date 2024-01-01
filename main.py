12d79684f72c330f6474b88af7e531ba2e465549
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:19 +0000

    talkinto doctor time introduce happen

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
