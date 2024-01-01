05726d99d7be41cc55f9cfd3459a283a76f05f0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:44 +0000

    turnup mother regular shrink praise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
