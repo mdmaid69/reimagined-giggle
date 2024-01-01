e87e3602f59539dc5a8c543258dcdd78f2953d42
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:22:01 +0000

    arrange confuse sometimes wave punish

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def reverse_list(lst):
+        return lst[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
