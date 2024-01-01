e4e00ae0972dde381f897005023c0576cb4fff01
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:25 +0000

    suggest confuse pinch blame official

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+print("Hello, world!")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
