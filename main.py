06c3489d36805dcdcdf98bf53ace3496dc0a4664
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:46:23 +0000

    rough zero pause interview meaning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
