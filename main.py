14bef85fdb4ee8d24f29d9248865be3a611adf18
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:49 +0000

    send pleasant wear turninto can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
