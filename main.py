2c1162e89390eacf98b16b70513d9cdf12b37bf5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:20 +0000

    runover admit principle unite wander

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+i = 0
+while i < 5:
+        print(i)
+        i += 1
