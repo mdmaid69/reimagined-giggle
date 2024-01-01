ebe84dfa18b090622c37d7e22e16773b3e8cdbc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:54 +0000

    standard needle principle shop round

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_power(work, time):
+        return work / time
