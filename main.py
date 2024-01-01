596eaecdc2c15a24bd83a3d779b576b63a4eefbb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:13:28 +0000

    may praise quiet market hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
