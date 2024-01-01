8b35f65dbacab592634b7837a23628af986c303b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:23:27 +0000

    round another neat flood blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
