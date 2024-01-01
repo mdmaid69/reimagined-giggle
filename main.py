08acd7c2d5ef08e897f2f7754e86e0569f4f6e77
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:52 +0000

    book complaint quiet burst public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
