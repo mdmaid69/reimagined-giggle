a75b046defd93b4b07a75838bb533fa1060f4e34
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:10 +0000

    turnout turnover base reserve personal

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
