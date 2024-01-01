9f4a6aefd4f911b272413e62dbcf21ad9ea34761
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:13 +0000

    space frighten their even its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
