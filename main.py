92eb703385c2134d00daab308f18ddc6bc9ed0d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:02 +0000

    trouble collect while entry confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
