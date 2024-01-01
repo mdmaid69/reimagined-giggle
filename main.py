ba53bc4781bfed56f590ba31b8d3f38b146c4853
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:08 +0000

    secret rain splash available lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def move_file(src, dst):
+        shutil.move(src, dst)
