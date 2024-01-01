fdbe8325ccf0d1c9aedcf5ebcf9261fe0c883d3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:30:28 +0000

    them clear other set onto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
