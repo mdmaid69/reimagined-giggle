eb1d48352d3c696e675b1dc708b5fa1bdf1fb98e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:59 +0000

    beginning level contain pick brave

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
