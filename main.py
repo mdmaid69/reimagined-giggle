79c6aa3b84a70c8c902131e7e524394791c8fcbf
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:05:36 +0000

    list pause account opponent wrap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tensorflow as tf
+print(tf.__version__)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
