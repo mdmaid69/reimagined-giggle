f642399d0243377dc91a4cb194005dc1893a6ccf
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:33 +0000

    meal division touch care keep

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def square_number(x):
-        return x**2
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import os
+def get_current_working_directory():
+        return os.getcwd()
