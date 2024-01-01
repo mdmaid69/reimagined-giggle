187af68d6adc9ceac892c1492cf249176e6c10a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:50:02 +0000

    lower someone trap admire ashamed

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_area_rectangle(l, w):
+        return l * w
