4c7f7bf79878768357b13e1841589221eccf53a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:43:55 +0000

    far instrument complete appeal protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
