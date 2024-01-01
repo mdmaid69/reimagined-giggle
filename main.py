a71f8e3c21f02b85486f06d89ccbc68731eae33c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:07 +0000

    thanks pretend shadow share recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def create_ordered_dict():
+        return collections.OrderedDict()
