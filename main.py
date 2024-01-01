bf82f5a956dbc8432521c5590bb11b0d37b43bb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:45:18 +0000

    speech science oil once ask

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import random
+print(random.randint(0, 100))
