dead9fa71549be4a084453d460ec039c9e004d31
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:05:49 +0000

    argue before drink temperature ilovedicks

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
