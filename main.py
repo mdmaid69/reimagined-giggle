3d7ceb6b9f6de986226afcefc261baf198529f92
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:19 +0000

    pain crazy feel meanwhile pretend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
