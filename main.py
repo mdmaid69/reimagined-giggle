7903d8ee58ae01a12228abdfa901415be97488de
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:22:08 +0000

    touch organize such into whisper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
