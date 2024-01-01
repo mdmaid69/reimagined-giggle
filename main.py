275cffaa82df0378bf8970f43b7f005e627a58fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:05 +0000

    hair runout extra many finally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
