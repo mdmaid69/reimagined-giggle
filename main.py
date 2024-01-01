fe48261cdb4cc710db531f6398613df961ad22f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:28 +0000

    slip rid organization loan crime

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
