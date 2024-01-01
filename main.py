3e1be885fd8fa5fbb459c0ea4dd33dd90ca8020a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:40:59 +0000

    throughout borrow private fly prepare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_pressure(force, area):
+        return force / area
