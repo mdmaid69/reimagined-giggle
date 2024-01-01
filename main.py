4247872d30301cdb4a5a11ceb9747f8bc3fa2b8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:16:16 +0000

    clear pair prevent board degree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import time
-print(time.time())
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
