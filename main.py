e5746dd0a944caf523074fb2d4b7f27f95b81d69
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:04:53 +0000

    round goout destroy broken reply

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-for i in range(10): print(i)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
