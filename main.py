d1433591bcca1c6461d2eb60a130764b15435327
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:49:41 +0000

    stamp brush bet willing fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
