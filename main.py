d5f9434f0b8c22c2a5666ea68f3ba6d6fb70b1d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:30:16 +0000

    smile cinema own concerned schedule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
