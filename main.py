8cc3db70c4cc77f6fb681ab4efefbca041f7cfa7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:47 +0000

    record join block tense fault

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
