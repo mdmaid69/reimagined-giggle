e705c7ef9b1edfc0d51b98e9b11865071e590053
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:41:16 +0000

    wheel system recent proud ashamed

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+for i in range(5):
+        print(i)
