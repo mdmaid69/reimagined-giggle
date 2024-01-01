7c119dd3994a0c0a2bf5b5c5afd497630a0f8c34
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:20:33 +0000

    address cheque ready gradually cook

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 print([x**2 for x in range(10)])
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
