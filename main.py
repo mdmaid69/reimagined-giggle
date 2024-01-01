4fa686e4270f34ac61dcc0f3447e5dcbe7d1e366
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:33:32 +0000

    570enjoyable warning strike behaviour bear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def remove_duplicates(lst):
-        return list(set(lst))
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
