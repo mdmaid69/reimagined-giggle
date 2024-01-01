f352fcb0c55833063721528db45049cbadfafc42
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:45:58 +0000

    train wrap perfect in fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
