354690a7c4f23002f1c89ada58c89413ebfac5fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:29:19 +0000

    raise stone start clearly difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def get_base_name(path):
+        return os.path.basename(path)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
