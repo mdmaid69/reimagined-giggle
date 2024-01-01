46a7c5a7caa921f10db2acb9b7ef9234504b8a0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:03:32 +0000

    good bet road match oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
