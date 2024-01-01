90e41e31f1b6ccf65f0a320de10b1f4ea11c957d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:39:37 +0000

    everyone concentration raw from square

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-print(sum(range(10)))
+  def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
