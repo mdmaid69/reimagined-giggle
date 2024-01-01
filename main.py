c2202e74a904ed06c0ebe4617d4fd84cc873b9bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:49 +0000

    pronounce tense concerned foot absolutely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-print(random.randint(0, 100))
+  def count_elements(lst):
+        return len(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
