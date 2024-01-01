f14edc0885d4fad292008fbddf29403481f921d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:43:06 +0000

    standard brain autumn pile quiet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import time
+def get_current_time():
+        return time.ctime()
