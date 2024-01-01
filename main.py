71d517d2311a1d2e8a222f4117b6a2a86784dfa4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:45:14 +0000

    food whether read each whistle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
