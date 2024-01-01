b389545ecce014cbbd19383f5aa53cab1e72edb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:44 +0000

    we contrast mean alone tidy

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def count_elements(lst):
+        return len(lst)
