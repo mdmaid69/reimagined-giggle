cbc6a320676c32413562581584eb5892a6a76bf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:13 +0000

    hardly upstairs case impression repair

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
