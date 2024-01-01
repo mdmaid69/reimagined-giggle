3d1cc0073cbf7c6b018cf63d1ebd0703085856fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:47 +0000

    tap include thinkthrough straight answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
