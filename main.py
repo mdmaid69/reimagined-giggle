13ec797918f968d35b308849f4c712dfb7449526
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:26 +0000

    length scheme train afford vote

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import collections
+def create_user_list():
+        return collections.UserList()
+import os
+def change_working_directory(path):
+        os.chdir(path)
