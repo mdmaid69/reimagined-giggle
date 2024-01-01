1a4041c5d6136ab1eb2f3de378ff7449ba39f820
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:53 +0000

    comeover thought string bright explanation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
