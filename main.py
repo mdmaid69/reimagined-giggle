44d3a60d717de19ca702a2acb0f9fd90dc1a9130
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:27:06 +0000

    back drink getoff shop successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
