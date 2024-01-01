f4ade04c01eca89812838083d837333bf85c3856
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:20:45 +0000

    meaning achievement principle scene proof

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
