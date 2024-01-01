ad4899c24bccfe79c2dbf5f1ea0004b642bac160
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:44:49 +0000

    swim slowly Jj trade tidy

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-print("Hello, world!")
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
