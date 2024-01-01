4026095469c2adcda6c805640dd39218695f0bfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:13:15 +0000

    rest wind sleep official carry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
