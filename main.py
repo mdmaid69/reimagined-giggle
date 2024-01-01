67affc5907cf5d5af4da734247ae2ad41e335ba8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:48 +0000

    doctor jump confident mean slowly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
