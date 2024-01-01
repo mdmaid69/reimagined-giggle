891e01e65452e216dd186b5924a5c09cba93c94e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:40:04 +0000

    sign reduce them sister stir

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
