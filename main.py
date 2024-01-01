b0a67bc57cd40ca0e2567b3f61aec20e5f738fd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:16:34 +0000

    tooth know tough plenty 570enjoyable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
