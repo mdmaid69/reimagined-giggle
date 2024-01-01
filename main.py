735c12a9a7918718db1b96ce5cd6edca0ca8a86e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:53 +0000

    odd offer similar promise cook

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
