0f591993c18734cda3f98fb3ca211b0121b8264c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:04 +0000

    score serious or difficult sharp

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import numpy as np
-print(np.array([1, 2, 3]))
+def add_numbers(a, b):
+        return a + b
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
