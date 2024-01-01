db46022abf526953911d48fe50367dde03c326cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:52:29 +0000

    with keepfrom type can relative

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
