e2341e0e6ae3335e72e8d5cf377e99ca3b5a26f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:45 +0000

    success loss gather imagine generous

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
