60a3bd306822b4954f41f2758e4a3aa6dfafb340
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:11 +0000

    night let method broken prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_pressure(force, area):
+        return force / area
