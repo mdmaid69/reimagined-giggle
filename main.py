7af27035444fae676f5a87ae5ce6ad59fdbca692
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:37 +0000

    sea danger fly man quality

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
