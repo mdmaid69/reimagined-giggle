d1e7c37f4637a59b432aeb745d66dda603b2bc35
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:32 +0000

    clue strike solve closely question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_repr(array):
+        return repr(array)
