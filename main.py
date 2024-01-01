3e9c43091a793e7f8a70ba7b404235a5da414126
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:54:22 +0000

    blue instrument cover offence first

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
