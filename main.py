5f755f57cb021d71a63b5240e23e827dbfcd6112
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:29:35 +0000

    opposite direct discussion under private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
