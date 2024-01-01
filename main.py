aad93e7b9f95d1ebc07693c4054925badda612e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:09:58 +0000

    ring somewhere muscle disappointed quarter

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+for i in range(5):
+        print(i)
