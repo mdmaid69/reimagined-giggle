6364fe0fb28e9325cd52a6ca961d4bc07b0c5994
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:08:16 +0000

    thinkover platform deliver pretend amount

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import numpy as np
+print(np.array([1, 2, 3]))
