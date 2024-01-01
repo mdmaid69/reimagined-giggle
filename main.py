1d50ed9a4cf4df885fcc8a9704437cd86e3b6b42
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:48:08 +0000

    they obtain and top difficulty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_length(array):
-        return len(array)
+import re
+print(re.match("h.*o", "hello world"))
+  def reverse_list(lst):
+        return lst[::-1]
