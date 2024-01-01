36afc2e08cac941467fc517d9fea961a64829714
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:53 +0000

    club turn arrival produce injury

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_to_octal(n):
+        return oct(n)
+def convert_to_binary(n):
+        return bin(n)
