36a49853a53ae1e3fddda4c737310862a0c4fced
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:00:22 +0000

    slight principle Ii emergency welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_itemsize(array):
+        return array.itemsize
+def reverse_string(s):
+        return s[::-1]
