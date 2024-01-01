64c780cdc7d61f9a70f684d1be8571328695566a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:10 +0000

    choose exactly term man takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_typecode(array):
+        return array.typecode
+import array
+def reverse_array(array):
+        array.reverse()
