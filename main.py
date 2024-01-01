21c834c9db6d0e14d6bcf28db255d0770e9314de
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:44:05 +0000

    row exit pupil suit history

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
