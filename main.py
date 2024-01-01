f78b3dd623974246f1954d0099b32303d1d7a941
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:15 +0000

    age concern sometimes left into

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
