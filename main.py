21f7e70a67d5d8b9a0137bfa5544c35db6dcf6f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:03:23 +0000

    godown put reply identify correction

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  def convert_to_octal(n):
+        return oct(n)
