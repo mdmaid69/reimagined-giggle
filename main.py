25b058cf9f1b784521577fdb6b3b685738fff7bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:38:40 +0000

    today collect lend arm dig

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
