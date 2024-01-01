1f33850c8391c0202e7fef8c9110b77ac080b040
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:40:10 +0000

    excited wrong treat 571enough drive

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def is_odd(n):
+        return n % 2 != 0
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
