ff607b0e5305212f021435584a4c6281f2a5879a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:03 +0000

    under difficult punishment steady breakup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_queue():
-        return collections.deque()
+def count_words(sentence):
+        return len(sentence.split())
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
