e768c892b0f90f4e5b677e8fdc3fb210242294c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:32 +0000

    improve season skill handle temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
