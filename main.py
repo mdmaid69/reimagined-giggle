f306f02e1a3d63a57a34373e011b68f5ba2047fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:57 +0000

    sale Jj none black love

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_speed(distance, time):
+        return distance / time
