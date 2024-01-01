67f1ca7cd9d2abacb6da20a73d3b084b395e2d42
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:17 +0000

    survive mad line climb shelter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
