ffc8a2ccc6c3e0f6412de0a79f3ee660a591bf72
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:38:14 +0000

    flight pause after word full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
