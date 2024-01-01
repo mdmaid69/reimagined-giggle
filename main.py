a523b97f0b15c985e129ec4b0a3e0ed2094d1887
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:27:34 +0000

    danger choose manage person rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
