c88547582eae7c2c54c21c2a8eea940feaea0df3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:35:21 +0000

    thank ordinary him shade cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
