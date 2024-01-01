5bf267e5a4b3fa30784cebbab99e91b68a4a9326
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:59:15 +0000

    snow large every lookback(on) properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
