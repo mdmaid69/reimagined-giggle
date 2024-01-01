680d8cc31aba52a6fc1cb94331ec0548d6b7f432
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:51 +0000

    refuse experiment perform still imagination

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
