a2e99317d944444d95b65ef10ffff2c32756fb66
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:40:48 +0000

    manage person regional score long

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
