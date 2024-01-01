15fa247c032001ab5caa1e893ab11aa599a9eda0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:18:54 +0000

    plain fill hope tall software

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
