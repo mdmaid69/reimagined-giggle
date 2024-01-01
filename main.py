29571fba0a4febff56cdf901d4103cfafd5221e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:08:12 +0000

    opposite connected lookafter position than

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+print([x**2 for x in range(10)])
