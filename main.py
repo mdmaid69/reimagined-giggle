8737a79f0046d7be322ffa2b131cffd83c9a75bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:14 +0000

    cope note unpleasant far shape

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
