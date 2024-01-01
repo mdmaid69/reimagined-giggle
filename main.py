a4b6dd2525fd50fb32391ba839dfd822243babc9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:07 +0000

    fashion pick fence dream tense

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def reverse_string(s):
+        return s[::-1]
