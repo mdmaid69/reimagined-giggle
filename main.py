44a85056f1e184a6944f6a41387ce4e0db8125a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:11:05 +0000

    better pipe criticize blue danger

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
