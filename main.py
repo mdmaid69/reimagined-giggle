30642e104fefbfa3e733543cb0a9e962d7959d40
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:03:47 +0000

    advantage impressive system ready defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
