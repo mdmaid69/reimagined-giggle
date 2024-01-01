552f4b123cf35dfa3ab48a686b36126cfd9703dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:33 +0000

    put feature test software scale

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-def calculate_speed(distance, time):
-        return distance / time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_area_rectangle(l, w):
+        return l * w
