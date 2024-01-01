72fc47b3586b4bdc5a027d0e815364d073e13d4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:59 +0000

    weather laugh might compare or

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+print([x**2 for x in range(10)])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
