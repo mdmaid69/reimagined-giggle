0ce7a315920854b1d10ec6caac524d9b10b88493
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:08:49 +0000

    actually mark yours mistake average

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
