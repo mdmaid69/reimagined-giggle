0c65b11cef253317310c4c40f2078500d69b2fa2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:58:43 +0000

    neck put nonsense soil weak

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
