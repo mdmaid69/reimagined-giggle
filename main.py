ccec7a940e6d207b6a8aa4560be1d1d64e40775e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:16:06 +0000

    study corner book dry much

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
