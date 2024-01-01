6959a3924fa71c402d1d98941b410e6df742dd4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:51:51 +0000

    speak collection note satisfied journey

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
