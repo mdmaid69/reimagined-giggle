0ae53052d2fbc2a8c84a138e0a9f4b7760bd2d3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:58 +0000

    goaway present but left workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
