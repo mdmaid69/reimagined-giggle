98cdd6bde5ce1d54cd20e3cc1b896349f6b4523a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:10 +0000

    cross scare soft protect nervous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
