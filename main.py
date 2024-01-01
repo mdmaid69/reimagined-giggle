6a57fe672013f648c39937a22f375bdc317492ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:50 +0000

    do turnon now work suit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
