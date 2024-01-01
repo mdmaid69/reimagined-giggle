abeb76dcd7b551505f113316ae2fc11a2beaf52c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:08:59 +0000

    low disappear handle flat principle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def subtract_numbers(x, y):
-        return x - y
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
