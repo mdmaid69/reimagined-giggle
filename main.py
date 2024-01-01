acc0bb77f9c7f0c075d46d29eb842619c63603ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:08:34 +0000

    please able comeup my test

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def find_max(numbers):
+        return max(numbers)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
