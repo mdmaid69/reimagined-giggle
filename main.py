e8fc85568aacb723166e17f538cb11e63e909378
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:05 +0000

    order takeback tune fire joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def find_min(numbers):
+        return min(numbers)
