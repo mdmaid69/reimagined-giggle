809d66864e0adea9c56456a41485263a00324bf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:05 +0000

    government heat bottom majority spread

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
