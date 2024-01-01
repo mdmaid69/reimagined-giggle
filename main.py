a9d85a34fe47e6d9b6800e2d3e0c97f04bc9216f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:43 +0000

    frighten mixture excuse base road

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_python_version():
+        return platform.python_version()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
