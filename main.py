8b0b23c775d1830bd4b2564c067eca7972b2b779
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:04 +0000

    switch opposite people priority pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
