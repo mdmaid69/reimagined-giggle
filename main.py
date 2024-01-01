3c37977458fcb77d852abb2a4d94d622ed297893
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:39:17 +0000

    regularly Rr content remove why

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
