20a1447c2a8b7776c32173aaa34ef7d9fc023468
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:32:16 +0000

    find below care congratulate suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_str(array):
+        return str(array)
