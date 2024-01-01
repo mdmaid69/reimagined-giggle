f271e7d0c25a0e28c4f757cd9f30b61e14b550a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:04 +0000

    already fear dish small majority

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def is_even(n):
+        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
