6adde3ae074c06a83ae364cb25862b4d2e9228ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:26 +0000

    later present future calm scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
