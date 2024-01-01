e3d6bd3d21e5841cb96905330555595724c01c96
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:53:31 +0000

    set enthusiastic hang tone enthusiastic

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
