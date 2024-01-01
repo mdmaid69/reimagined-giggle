1ebb625db325ebb9fb5ca419d0652095d3d39011
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:22:57 +0000

    team school noone question fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
