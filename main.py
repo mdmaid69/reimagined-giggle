b5f5513433f3a6037bf717570c597b3b0d5a6a3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:57 +0000

    blame sit fold uniform shade

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def convert_to_hex(n):
+        return hex(n)
