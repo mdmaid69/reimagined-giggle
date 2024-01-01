476a26ecd72bef93cb68a50bad5507eacb537cf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:53 +0000

    exam next black city correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
