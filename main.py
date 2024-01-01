6752c4de4a888371c0f7d4ba590464b620aaa887
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:55:16 +0000

    tall fall scheme by school

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
