d7b1d8c9aba715d3241d7fb4a1ac2372b11a470a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:43:35 +0000

    interview comeout copy muscle each

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 def calculate_density(mass, volume):
         return mass / volume
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
