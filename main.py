327dd35e701b852ac3ee796333182bb06f07d8da
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:40:28 +0000

    rough brain night admire take

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  def is_even(n):
-        return n % 2 == 0
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def reverse_string(s):
+        return s[::-1]
