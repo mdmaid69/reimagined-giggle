7da9c5c472dcecb296aefa41ea3d54db612a2b45
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:08:52 +0000

    aim manner motion challenge Rr

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
