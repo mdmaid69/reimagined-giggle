4997e067e593852daea1bbf20a3d18044cca56e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:50:45 +0000

    communication average cover range so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
