e2511f42152ec4ccc42a8d38d1e8d32177000fcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:13:09 +0000

    so hit other giveup game

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
