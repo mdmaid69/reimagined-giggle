da5fe34ede0593a5849f9ab00090a0e67d04c454
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:37:06 +0000

    nor style honest passenger spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
