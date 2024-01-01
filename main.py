014e66f49becc0c0803a06a376c983172e772bf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:22:01 +0000

    into weigh shock 566end appeal

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
