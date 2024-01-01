6c188692b85ebafd94d2bc95b8e4da9d7d369761
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:18 +0000

    pain total favour single geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
