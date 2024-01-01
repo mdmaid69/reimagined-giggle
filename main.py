a0a967b0011143d51fdbd862d9f5714251383226
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:42:54 +0000

    before remark nigga deserve live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import collections
-def create_counter():
-        return collections.Counter()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+print(math.pi)
