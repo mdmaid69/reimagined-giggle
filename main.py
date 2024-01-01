c5ce95cae6db2d448394dd35f6c343f6b1e5b5f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:10:56 +0000

    getout announce peaceful punish ourselves

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
