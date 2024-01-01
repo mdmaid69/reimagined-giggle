1cb6fd26b72bcc6ed9be8a09b82e9aa6ca44ebc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:02:22 +0000

    like cruelty community abroad organization

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
