c6ca9f999f88d22dae63f0988f93118d00354093
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:22 +0000

    funny period maybe limit hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def remove_duplicates(lst):
+        return list(set(lst))
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
