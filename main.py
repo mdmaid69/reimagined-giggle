a2c0627ac7735c064b8c3d6f8f7d195c7356541a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:02:17 +0000

    stupid church drink means spin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_pressure(force, area):
-        return force / area
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
