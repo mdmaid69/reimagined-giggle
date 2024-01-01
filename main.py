5d6142505a8f120df11de8153dfd7cff67dc3a40
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:49:06 +0000

    powerful miss woman expect help

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_speed(distance, time):
-        return distance / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
