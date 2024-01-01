668ccde7fd559d24aa3b01e2566fd9bffd0fc093
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:09:19 +0000

    serve follow private autumn suggestion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
