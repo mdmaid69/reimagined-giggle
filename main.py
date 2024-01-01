5ef1bb230477c438c9eb6fbd448d6205f26b945e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:55:22 +0000

    teach over go smart rate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
