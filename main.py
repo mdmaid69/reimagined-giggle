4f4e7dde649f51c2d4aa059816beebd1803f4f22
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:49 +0000

    image prove example female mystery

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
