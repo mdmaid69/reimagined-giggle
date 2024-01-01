8b47802c748cfd68bcf183097a65bc3fe1a0b871
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:49:50 +0000

    fix dream declare means relation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
