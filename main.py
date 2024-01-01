8d4364beb865f0e7c7c12cf131a283363024e081
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:30 +0000

    treat single past loss inquire

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import os
-def remove_directory(path):
-        os.rmdir(path)
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
