ac8d5166fb7062cb12a2319281307d4eea6429b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:28 +0000

    several house yesterday intend future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
