52028e742e44900cdae01d8901b845d74762f95e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:54:55 +0000

    open risk after expect immediate

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def add_numbers(x, y):
+        return x + y
