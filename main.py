a6234a2664ff1a4c4aeada3d2a332126f406a47d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:42 +0000

    disagree practice roll part bottom

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import json
-print(json.dumps({"name": "John", "age": 30}))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
