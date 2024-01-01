9206c5233a52c97dbd417d79da85ab48d9c2a0c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:44:29 +0000

    better holiday geton now strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
