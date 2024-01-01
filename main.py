abceb1833dd4b91fd2d51e1c748c958695d6dede
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:52:44 +0000

    precise pain woman forgive anxious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_typecode(array):
-        return array.typecode
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
