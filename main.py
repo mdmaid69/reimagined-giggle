5380a570bae5535351f4168af45529e3d05a12bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:27:42 +0000

    what road interested demonstrate sweet

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 def calculate_return_on_equity(net_income, total_equity):
         return net_income / total_equity
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
