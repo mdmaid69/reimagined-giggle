88f25ff0c20512255c3fb392083d7cc806613a8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:44:35 +0000

    any estimate crowd figure hot

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_min(numbers):
-        return min(numbers)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
