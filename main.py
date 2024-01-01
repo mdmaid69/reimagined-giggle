c89ded4247fb14229ecd270df6bd9e88296a494e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:55 +0000

    cut hurry we wrong speak

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
