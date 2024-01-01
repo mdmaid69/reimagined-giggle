efd2ed5b10337bc254fcf3a7c839854369f694c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:37:01 +0000

    payment surprised majority still character

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
