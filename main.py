0341cefb4104175812254ece029b1b999f54265d
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:30 +0000

    minimum central will couple night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
