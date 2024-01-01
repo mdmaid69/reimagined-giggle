5c99b37fc8bdb7653fa2a4b96f6874214eb2e4df
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:42:31 +0000

    exact half throughout across skill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
