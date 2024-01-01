9b55ae91020761bdd36572f3fe7e0e54f720a483
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:44 +0000

    dig treat whistle keep heart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
