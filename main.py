5594f3f962fafd016ac3c185675319bfe5ec9d30
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:52 +0000

    high protect fond steam wide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+print(math.pi)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
