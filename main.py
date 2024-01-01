e21bf49b3cca44855855da179ab8eb20a63fb6c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:52:48 +0000

    prevent deserve test before question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
