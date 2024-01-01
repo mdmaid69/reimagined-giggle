055b030de3dacf0708f96f55f42570f11948e88a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:18 +0000

    here holdon clean visit unusual

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_elements(lst):
+        return len(lst)
+import numpy as np
+print(np.array([1, 2, 3]))
