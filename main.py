78634611bfd600cf4c461a0b3cb9056f0a462db3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:28:32 +0000

    embarrassing soil real sensible accordingto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
