7479cbd692b0a843463173cc33a9bae274c044dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:44:47 +0000

    clean will dead wonder supply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
