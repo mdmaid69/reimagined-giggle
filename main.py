8e05847f3fcf7f0d6a7658903bd6bedd10dcf877
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:28:39 +0000

    slow name pause grow beyond

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
-import re
-print(re.match("h.*o", "hello world"))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
