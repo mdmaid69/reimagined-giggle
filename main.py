27878b846b3f1c5fce82a6b6087e44e2a6073347
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:38 +0000

    swing become best whistle start

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
