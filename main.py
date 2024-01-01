ad5a9a51abbc93a7b58b28c1e70ee3fc7c631e3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:01:08 +0000

    warn area reach middle cheque

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+print("Hello, world!")
+def is_palindrome(s):
+        return s == s[::-1]
