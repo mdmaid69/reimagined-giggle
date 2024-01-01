2158f3a1427f9143fabec77cb45f56ee8378cf6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:17:36 +0000

    street woman scare stupid permission

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
