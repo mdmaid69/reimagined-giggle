710096fe6025785eb4ec4edfcd067f16ae3daa2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:23 +0000

    search even disturb respect minor

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
