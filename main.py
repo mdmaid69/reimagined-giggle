a263f2f0eb1c782c3bea1cf346e1577bbf5c1b1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:14:53 +0000

    block side relax bin part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
