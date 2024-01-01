4446b95546b66fb0d715471cf9cfc53f4a3172ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:02:44 +0000

    further getup message performance flood

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import time
+print(time.time())
