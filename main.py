4a9add22ea5d8b407730ddd89f035375732e4045
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:38 +0000

    production television shade report blow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
