1194b94dbed666275b2c7c1338b31206c3e8293a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:33:00 +0000

    party still small disgusting spite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print("Hello, world!")
+import array
+def get_array_as_str(array):
+        return str(array)
