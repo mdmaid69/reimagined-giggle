25d7d8f91966891bea89127890927ae7469ca2ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:02:51 +0000

    edge word action ring substance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import array
-def get_array_item(array, i):
-        return array[i]
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
