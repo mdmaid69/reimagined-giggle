27ea41488d5959a82c99e5f347a80319785d65f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:47:37 +0000

    stretch lean argument band comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
