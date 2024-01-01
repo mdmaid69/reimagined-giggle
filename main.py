6f391f7d99896ac1037003971f25c1b273e0a7d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:45:29 +0000

    this worried say seem grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
