43b33ebabb2449c8d7425cd18f9708cd1a4b06b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:10 +0000

    image plate mistake crash introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
