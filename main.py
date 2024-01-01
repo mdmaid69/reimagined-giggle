4a32849e03205b6e38417d31168a6e087f77ae60
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:50 +0000

    pain tie 179bite blow water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
