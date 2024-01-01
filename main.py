02883f8135dc0050b804753f37037503bbdee8ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:07:22 +0000

    risk godown deep ignore taste

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
