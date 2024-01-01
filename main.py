f87c807b0d94b24a2df5f7a6ddd15e14c9dd2ee3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:31 +0000

    tomorrow us comeback time all

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
