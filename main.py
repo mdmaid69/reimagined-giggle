53d1c1434a34b435974e6645c1cb509c5f57772e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:38 +0000

    frame suck put trial exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
