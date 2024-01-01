2a9bc5169c356303565597e6e840c525a24797b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:59 +0000

    plate extra Vv find theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
