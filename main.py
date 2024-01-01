038d587a5955f83d168e9b81595a102ca6303e4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:16:44 +0000

    boat Ii mistake petrol declaration

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
