b5a16494d2796b4540368c908204dd1b0ff01297
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:45:15 +0000

    within scream platform herself 565end

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(10): print(i)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
