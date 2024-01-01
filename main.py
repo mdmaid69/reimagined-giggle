5607231061c5a8a1a6b25befd561c909ac9805ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:08:57 +0000

    reject black square wide conclusion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def convert_to_octal(n):
-        return oct(n)
+def get_current_working_directory():
+        return os.getcwd()
