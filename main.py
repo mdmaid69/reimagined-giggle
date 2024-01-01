928059054a3a65a0d167f285fd6f84e48544c2b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:52 +0000

    organization square fashion tap train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
