b1b1a6d820559471d219cf0d17e805ec8e37cfcf
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:24 +0000

    concentration left bend independent twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
