577250ceb06085066405d435b07f43b77b3393fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:31 +0000

    cost feature takeoff phone continue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
