14e5bbd8f24af25324732e1a79a6abdba693bad3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:12:53 +0000

    run any fast whole tread

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
