0c3d48b279281d43cad20cfa001bc97f07dc88cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:43:13 +0000

    excuse Ff waste ought Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
