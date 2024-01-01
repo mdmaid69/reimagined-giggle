76d2339cc3779528c033e01aec335d343e48d2e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:53 +0000

    carry hide OKorokay turnaway chin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
