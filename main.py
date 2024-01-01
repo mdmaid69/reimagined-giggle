595744311ee8d8741e24d40f6e049fde16bdc0df
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:56 +0000

    service range business minimum themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
