b5ac1b411c06384ee59c42391caab84750fb955e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:45 +0000

    strike offend shame sink pencil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def sort_list(lst):
+        return sorted(lst)
