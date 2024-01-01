e9e7d32ad3b11116a87188a74b0746fdbdde9c6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:28 +0000

    home work food bridge plate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
