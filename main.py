a5bd2506a05ad6dcc1dc64fd6dde88009a0c0b7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:00 +0000

    properly hit sad sky address

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import os
+print(os.getcwd())
