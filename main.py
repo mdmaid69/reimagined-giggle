b12f615a17826a5798c5664050058b6531447ad6
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:45:21 +0000

    bill vote turninto flat search

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+print([x**2 for x in range(10)])
