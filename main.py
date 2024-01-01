a9a37109a2f64e4c58ab926bbc05410937463998
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:47 +0000

    confident might swim right plenty

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_queue():
+        return collections.deque()
