70499a413a6ad2b18001b97696f0e490d192e3cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:52:28 +0000

    deliver in shake case ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
