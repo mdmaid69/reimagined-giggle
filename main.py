c7edfe91d59bcf967965b72f09cd6ddfb45b54ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:03 +0000

    protest spray passenger degree find

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
