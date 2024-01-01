2199572a85df83c2bba993ff7e355013f46ada78
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:39 +0000

    than exist plenty cum compare

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
