c97ef40558c7f9bcd9d6ccbfe1e03a04f36cf284
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:53:37 +0000

    indeed standard spin gate wish

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def is_even(n):
+        return n % 2 == 0
