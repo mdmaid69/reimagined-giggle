4cff005dcc4333080485d7580ff707ec6baf35c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:57:59 +0000

    tap minority getout risk food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
