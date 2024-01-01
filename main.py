2141c407ebaeac49fb4447642d49fb235427af28
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:10:02 +0000

    offend comment any sweep exercise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
