d0f1f563253c8e949b4232a74d4f26e27eda3476
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:55:33 +0000

    nervous shock impatient by face

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_list_from_array(array):
+        return array.tolist()
