1a04924f798d36018a02d31acbe44ae27db76a50
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:26 +0000

    birth culture model target fork

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
