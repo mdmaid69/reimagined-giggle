2a544a163d26ace53031aba3eeecbc49d9ae3da5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:14 +0000

    soon thinkover pronounce whether shrink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def count_elements(lst):
+        return len(lst)
