356fa7bf1b67c3646aa25508ce61858027e1c6ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:32 +0000

    limit Itself many favourite not

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import tensorflow as tf
+print(tf.__version__)
+def sort_numbers(numbers):
+        return sorted(numbers)
