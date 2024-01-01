258c9a1fb7f12c459c0d89e660dbb2b21f4a6aa0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:06 +0000

    rarely order affect hang destroy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
