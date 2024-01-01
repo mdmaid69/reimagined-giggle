7e2897c986aa30b1571c467975cd7f8fffce14c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:24 +0000

    mark yesterday giveup treat contain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
