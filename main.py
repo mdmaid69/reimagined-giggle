5ea5e6f9fede5c2d6d67f53877f3383a7eb31571
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:11 +0000

    great knowledge later admire load

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
