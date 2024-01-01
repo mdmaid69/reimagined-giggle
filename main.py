fd8775ef1cf743bd88c15c0476bf3431b22f1216
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:37:05 +0000

    cut see Itself spot make

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
