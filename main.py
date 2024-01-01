5a4137c9850ee6319e745e99a1a0b161a64e64e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:58:35 +0000

    science anybody block petrol dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
