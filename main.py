38d02eb82461e70948996d8af28a3ea19bb216bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:43 +0000

    state position damp dead punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import hashlib
 def hash_string(s):
         return hashlib.sha256(s.encode()).hexdigest()
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
