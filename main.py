e1bf3f23b9072085c3478006a1f6b9a778c74d83
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:28 +0000

    will each worst mine cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import time
-def get_current_time():
-        return time.ctime()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
