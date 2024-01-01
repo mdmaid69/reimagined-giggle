ebf2409edb6c8d8fbc92b0e64fd6f4b6b10d2eca
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:12 +0000

    allow fun lookafter tread thankyou

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
