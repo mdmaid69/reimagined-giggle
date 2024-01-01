ea41b6eb311b883a628dcdc43a5162f0185d9a60
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:14:49 +0000

    music beginning wish bother apparently

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
