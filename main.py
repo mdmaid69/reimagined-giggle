2acc76df84e3ce75493ed5681478851c4c9971ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:51 +0000

    narrow trap any sort concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
