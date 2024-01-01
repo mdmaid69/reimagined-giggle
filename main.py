12c8ffaefe3e2bc69ed81e243a7c54730c9bb586
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:54 +0000

    trust describe doctor act case

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
