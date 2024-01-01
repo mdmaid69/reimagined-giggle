fa256cc463fa284735a1bde76cf2d1ef366225d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:03 +0000

    mood independent tool Vv best

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
