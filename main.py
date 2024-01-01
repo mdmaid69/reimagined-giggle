58eeb9e2ac66583d3c6ababe204cbdf17cc0d23f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:56:39 +0000

    getout tap rid holdout off

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
