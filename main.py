413d165b2c33fbd53d2ad053e0ccde1417c4ac65
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:47 +0000

    plastic wild ought stage question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
