895d17e957ceb7a5c00c2ee60afd4d281b75c332
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:43 +0000

    pronounce push comeout trip beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
