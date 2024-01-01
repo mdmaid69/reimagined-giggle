23224f3e193ae48b9b8c67ffbfa318f8f8908961
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:57 +0000

    chin mention comeout ignore Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_max(lst):
-        return max(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
