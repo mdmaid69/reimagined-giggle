63b07ad9e1f0982ec6634f0cd301897be213479f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:47 +0000

    label shoot plenty tool embarrassed

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def is_odd(n):
-        return n % 2 != 0
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
