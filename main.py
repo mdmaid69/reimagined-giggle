4315296f6db4e24b61b6df0353ed5dd297ba1444
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:47:48 +0000

    behind fence authority convinced scene

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
