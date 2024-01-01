c62fb8fbc47d777f48e111cee0f66d5bd4115e69
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:07 +0000

    mark glass keepaway dark our

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
