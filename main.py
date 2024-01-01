40455e5c9ba8e8684213fc3f0f4bd32f16b6d902
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:17:11 +0000

    sad actually care everywhere support

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import math
+print(math.pi)
