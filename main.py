e8ba6cd78dcf3362331c9ee6b2ea92fb16b326a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:59:18 +0000

    accidentally certainly line success twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def get_list_from_array(array):
+        return array.tolist()
