b0def3c572b2ac17c07f5c5d5d01db2b97417dd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:19 +0000

    beginning cost badly run question

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+print(sum(range(10)))
+def reverse_list(lst):
+        return lst[::-1]
