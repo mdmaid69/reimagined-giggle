b0a73a218b0bb2dd6915a5e3e4e3e0f1d0d082bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:04 +0000

    however relation slice sudden creature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
