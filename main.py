2e5c6bd108ee014123c3b466b0a14e66a9b3bdc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:40:44 +0000

    save consult walk object science

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import sys
+def exit_program():
+        sys.exit()
 import datetime
 def get_current_date():
         return datetime.date.today()
