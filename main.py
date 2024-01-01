b1b89e3231b0ceccdd92586148689dc970a7c1bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:38 +0000

    command comfort favour wheel available

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def get_array_item_count(array, item):
+        return array.count(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
