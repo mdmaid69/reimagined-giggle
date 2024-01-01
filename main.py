92fafd68a557ff3737a2d96c2aa2e2075494b402
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:53 +0000

    if punish debt comein correct

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
