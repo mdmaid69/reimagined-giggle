63a0f18d76a46899b1d630de46310ec08dce9837
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:07:26 +0000

    round confused water amount centre

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
