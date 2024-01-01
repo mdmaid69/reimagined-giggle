82a50e65fbc477cb215725aef6e55c9e86fcac18
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:06:42 +0000

    lean all ring Tt line

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
