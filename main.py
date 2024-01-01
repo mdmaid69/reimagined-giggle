7349b7d85f9bfbbfb055eed949db575b9c35c26f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:47:33 +0000

    pity command circle pay crazy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
