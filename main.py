ab826b6d21d5d6b3149db4e41e154b324051d3d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:03:46 +0000

    change worried mouth game over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
