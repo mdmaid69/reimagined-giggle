94313451534a01c10a47d0d27573df78b0cf603c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:05:48 +0000

    most swim walk fear discovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
