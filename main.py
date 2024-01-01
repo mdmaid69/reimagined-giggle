538dfde4a7e5eabbb87c21c19db89dfa8c995e40
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:31:11 +0000

    side react cross play badly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def clear_array(array):
+        array *= 0
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
