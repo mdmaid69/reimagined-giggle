8f274aa16ba9f36b764b4d1a34c8cff6bf5f221c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:01:28 +0000

    reason always dangerous address nose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
