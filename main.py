837af05516a13969247a5e3eb0d367e14dcdc586
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:24:12 +0000

    fortunate dead view protect honest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import numpy as np
+print(np.array([1, 2, 3]))
