5574c86ac2b77877f35de665d32c97edd67f4f97
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:21 +0000

    juice join maximum rate save

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
