9ed47d6d5c3d286395e6e1df22e2704b729334e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:42:56 +0000

    sail result thinkthrough point healthy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
