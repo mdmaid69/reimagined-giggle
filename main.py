c9051b90edbf5bfbee9d1dd3f264a54707fa21f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:44 +0000

    top sudden cool begin address

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import time
-def get_current_time():
-        return time.time()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
