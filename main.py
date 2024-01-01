1b9ec7867059782ed5379c571056a876cb35da76
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:37:52 +0000

    pretend bend down while nonsense

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
