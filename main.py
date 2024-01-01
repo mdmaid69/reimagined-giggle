c1558010106ac3a0ff12deb6bb96b37939e827c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:10:45 +0000

    lookback(on) matter though to replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
