ad440344b3aa1c6b0d35d4ab681e67ada87eeb49
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:39:55 +0000

    effect zero priority shame for

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 def find_frequency(sentence):
         from collections import Counter
         return Counter(sentence.split())
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
