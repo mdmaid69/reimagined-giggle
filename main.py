9c9a6da6ce3e1b8b4f867ae4ce539c6132dc9006
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:20:37 +0000

    space process cough live floor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
