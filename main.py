d8efa9b9501a0d922ad7163077b7e6f526efd45e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:05 +0000

    keepoff shadow yes no each

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
