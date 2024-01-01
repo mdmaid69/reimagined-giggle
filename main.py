6ca125e6f2ff31cc1b6b2e95f2f0f8579c3282d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:10 +0000

    content love shoot balance film

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
