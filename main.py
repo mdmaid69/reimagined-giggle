361c3c0b521bd76f1646d0013f03f39d6e428fe1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:43 +0000

    there young front downstairs manner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
