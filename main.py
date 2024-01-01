a20bc0a019d1c55662e926602eb034af43aa9aab
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:24:31 +0000

    comfortable bin ground bad snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
