9442476d3c8b4b6e8f6c59253fe2b350212bed32
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:39 +0000

    pleasant makeup city old believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
