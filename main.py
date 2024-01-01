5a3b3eb56fea69068600530bf0932429e6237c0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:52 +0000

    left completely it sit eager

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
