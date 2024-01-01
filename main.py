2149d545b80b2eb238574067a6efe82d7d523f64
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:11 +0000

    open measure basis actual rent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
