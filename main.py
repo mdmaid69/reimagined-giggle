43967a5c4be44bf9d47568767268e2b5d9624efc
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:13 +0000

    towards name best live side

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_density(mass, volume):
+        return mass / volume
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
