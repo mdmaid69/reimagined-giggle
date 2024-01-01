dd361a27c922092d8b6f84011a684dcc52ffbd0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:13:44 +0000

    uncomfortable pause turnoff cope pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def reverse_string(s):
+        return s[::-1]
