ce461badd9d1e5bf98e313f11326211d0040a8a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:33:58 +0000

    wake come back sport plan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
