d0bd4b749b33484fd88d47a682adc4f485d3a9a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:23:29 +0000

    trial full practice event talk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def get_string_from_array(array):
+        return array.tobytes()
+def count_words(sentence):
+        return len(sentence.split())
