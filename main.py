b2dd05941de104b2cdb28565de2dd2f5ec96d6f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:45:11 +0000

    watch Ll probably common drawing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
