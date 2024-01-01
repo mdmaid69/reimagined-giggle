3ae7f2cda2a5d294b488c2fd8eedcd708b46e1f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:30 +0000

    swallow once support underneath question

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
