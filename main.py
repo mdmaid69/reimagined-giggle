3cea4a914b806c8dd1cddbeee7913bfbdb235713
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:17:09 +0000

    birth vision pen typical turnover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
