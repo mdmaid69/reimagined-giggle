eb98d6f8efe46b980495bb88c3a0176c30553ef1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:06:26 +0000

    use square rare doctor claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_list_from_array(array):
+        return array.tolist()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
