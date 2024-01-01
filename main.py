00e9d1a4ad5db8e3046cad12bacd448e54b633ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:52:01 +0000

    advantage train supply difficulty some

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
