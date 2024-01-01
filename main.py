c2b957006d945897571a2eb4bd2091921a37adee
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:15 +0000

    present answer live 568energy lunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
