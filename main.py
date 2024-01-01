3f31e170a538489b6e2e0aa248bf6886a84dbd32
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:41 +0000

    contact advise afternoon insist appeal

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
