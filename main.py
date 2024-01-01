2777c9e735c8c987015af1fed1101f486c933134
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:34 +0000

    fear line figure wing thanks

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def clear_array(array):
+        array *= 0
