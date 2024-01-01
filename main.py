4fa70fbdcf94e2d755e9e67cd23e51bf2b4b9b5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:16:13 +0000

    frame nowhere so suggestion explanation

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def is_palindrome(s):
-        return s == s[::-1]
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
