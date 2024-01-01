33d1d0516028ba21fce274fbe71a30c08285be41
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:37:34 +0000

    bring rank stop straight post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def reverse_list(lst):
+        return lst[::-1]
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def append_to_array(array, item):
+        array.append(item)
