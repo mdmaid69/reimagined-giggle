88bcc72b361bc9318cc2ad72b6ecd88c92f0829d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:13:10 +0000

    deal reserve serve second undo

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
