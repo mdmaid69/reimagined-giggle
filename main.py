b922071464be25ab40de0e78b7432d8f485c3614
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:09 +0000

    like spare can no human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
