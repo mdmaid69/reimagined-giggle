5263ddf18ebc3ebadb3d6b848fa31d5b8ba9b5d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:36 +0000

    cut detail design bringdown stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import collections
+def create_user_string():
+        return collections.UserString()
