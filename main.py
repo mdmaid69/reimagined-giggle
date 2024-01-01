a7ed44f9be94e057ecc926b81e6252e2b0a906e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:39:07 +0000

    change as desperate alive foreign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
