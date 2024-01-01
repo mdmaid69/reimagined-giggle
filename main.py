b0a3e16de664fbd068dd7de2dc5bd661980f6c68
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:05 +0000

    tip hurt space even order

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_as_list(array):
-        return list(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
