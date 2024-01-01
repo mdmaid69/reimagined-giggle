7eb3c9693090c00753d9af6dc9063296e060469d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:03:15 +0000

    experience lookback(on) lucky scared charge

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-def count_elements(lst):
-        return len(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
