5dd44d5c88bebf70416eca320f0ba54019694d99
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:51 +0000

    sleep brave rent copy never

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  def square_number(x):
+        return x**2
