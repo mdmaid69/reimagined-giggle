de9994c1007d27c2e58e8c77b7d052dd5e8738e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:57:23 +0000

    scream scheme control shop cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import numpy as np
-print(np.array([1, 2, 3]))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
