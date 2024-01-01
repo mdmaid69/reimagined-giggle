627e4832d1b6999d3ba8d7751caef5813e32e750
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:43 +0000

    mine about store slight unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
