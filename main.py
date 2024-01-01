632094eec52df3476edf94f44d0e93608df9dfd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:39 +0000

    birthday Gg afterwards example test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
