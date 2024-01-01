f6ab2b81c0e92a38735cf11e987b584900350ee1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:25:37 +0000

    behaviour last process favourite even

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
