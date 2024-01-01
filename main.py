eabccab22061e5c85ae4d666d2d59071bdd4157e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:30 +0000

    unfortunate except design large model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
