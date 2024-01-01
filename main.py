bd0cd82034f2c1a57dadbb3dabb213f5ea9ebb10
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:31:28 +0000

    deliberate open season usually yours

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_min(lst):
+        return min(lst)
