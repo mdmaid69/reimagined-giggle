4baa1f217ad0a4917a382b2b2445ed83e89e3dfe
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:27 +0000

    pause generous consult lend determined

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def convert_to_hex(n):
+        return hex(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
