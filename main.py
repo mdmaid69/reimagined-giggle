fd38327dfadecec84d6f2e45512db22d946844c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:23:36 +0000

    pick precise tear mail cruelty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
