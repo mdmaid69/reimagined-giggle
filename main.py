5a4816f6ed671766f15a9bd5fbfb811a61600025
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:24 +0000

    advice extremely middle instruction along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def get_array_as_repr(array):
+        return repr(array)
