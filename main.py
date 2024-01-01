8239ab96b61434ca4a0fba3242bb3e1a87a42c0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:02 +0000

    remark excuse back subject equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
