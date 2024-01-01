ad9addb41a5f717e3c95b2904f1a9541e6b92eb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:49 +0000

    breakout gentle spill motion mouth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
