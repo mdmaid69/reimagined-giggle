b7aefd756a3bc286ccdc45200eeba9bc0f13a520
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:03 +0000

    great attention behaviour into shadow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
