ceb8156c1bc68556f3b4396013b48d15520a9ff4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:14 +0000

    cross bottom warn length him

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
