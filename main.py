ec6ddec83395eb25546272d5459c3c6b5ceaf86d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:44 +0000

    his behind company getup uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
