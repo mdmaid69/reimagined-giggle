dcb09b5f239ff980ff5587228ca2f133ccb0593b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:26 +0000

    temperature what day recognize pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
