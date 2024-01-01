f1b43d7f6624b2b22ff2425e44d3da7c9e12b885
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:11 +0000

    scare dress scrap order say

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def reverse_array(array):
+        array.reverse()
