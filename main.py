ec6878d11a895d0c0c19a9b3644f3310545906ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:03 +0000

    breathe cope university clear sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
