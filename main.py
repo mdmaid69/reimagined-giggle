4af0bc1f2071147c3d67e46a24439a8da4d6f1a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:05:13 +0000

    clothes father sick classroom respect

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
