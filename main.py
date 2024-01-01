873492078a6f18242041cf1ac061f4b3f9e14cd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:22 +0000

    shop tight smell expect yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
 def calculate_acceleration(speed, time):
         return speed / time
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
