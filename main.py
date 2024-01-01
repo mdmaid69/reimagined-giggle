56aaebfd11d1ff9296dfbf00bc7de44b081b6ecf
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:04 +0000

    just consult forever impressive scene

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
