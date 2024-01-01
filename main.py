e2174042fcaca79965e53a11531217aaf601bfc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:12:50 +0000

    fact union calm sore however

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
