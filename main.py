1507f20e35d2ea7329612b0c68542cc4c19b4a2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:20:53 +0000

    leave disappear kill government watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
