ec2947121315872dc3d56eeaf70562095a1b7a61
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:30 +0000

    far row bath die tonight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
