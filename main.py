ccfd6dc01a2a8ccfc99731362771000301e63166
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:31 +0000

    actual worry guess call dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
