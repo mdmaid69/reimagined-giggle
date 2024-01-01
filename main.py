e1e812205ec285ede69496e44c10b4b642aed397
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:19 +0000

    however fun measure suppose properly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
