01ae9be6d1b3673440155255c48bdda97816c0c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:22 +0000

    unlikely to some operation underneath

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
