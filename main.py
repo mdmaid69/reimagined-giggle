391508bc53405423ccdabe1bf8f9fb3ef346dde8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:17 +0000

    warning present turninto large bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+print(sys.version)
