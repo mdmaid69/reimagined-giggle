ce0cc9cff7d2e2b067b51d65cdf21fae3c2e4b46
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:45 +0000

    excuse wealth bet nonsense yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
