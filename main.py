d0eeb9c0f9be723339f66c7b1194513dca3cbaaf
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:15 +0000

    fence eager successful sort friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
