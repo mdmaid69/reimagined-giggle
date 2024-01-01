9b48d3f3655d03ce798fe39cede3ef48c9a1ba14
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:58:39 +0000

    572enter plan impatient thinkover Ff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
