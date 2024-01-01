a8dc468c9c404319caa50527d1ba34aa4cb6b904
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:53:02 +0000

    blue connect now situation separate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_as_int(array):
+        return int(array[0])
