50a9f2b5e3f6f02fc96fb1f8ac68474740c98855
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:28 +0000

    second seat reply wish date

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import time
-print(time.time())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
