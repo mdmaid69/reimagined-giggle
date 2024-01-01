72a00bcb9f1e366be7394a76ef12b7bafba3383a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:50:23 +0000

    unfair pay refuse wait attempt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
