b7276f037763546e537633d7a58855075e5ca9c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:11:30 +0000

    miss radio pipe leave breakin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
 import array
-def get_array_item(array, i):
-        return array[i]
+def insert_into_array(array, i, item):
+        array.insert(i, item)
