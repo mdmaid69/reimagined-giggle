b2183ac531b6c805a14a386f126ecaa2753d77c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:44 +0000

    single supply quiet large shop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
