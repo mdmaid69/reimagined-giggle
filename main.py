10fd57459f1a9f655af1f69d5e35eb6a12604112
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:56:22 +0000

    considering hope idea account yesterday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
