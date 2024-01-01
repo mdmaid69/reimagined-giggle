e6bb4d1c417030b674ad9391b2f7b4d1668e982f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:35 +0000

    actually hand temper doubt moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_list(array):
+        return list(array)
+def convert_to_octal(n):
+        return oct(n)
