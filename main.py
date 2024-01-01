12a240be816b9b92085b4ff948e525bb7e91cd1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:51 +0000

    borrow that nearly nerve responsibility

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def square_number(x):
+        return x**2
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import array
-def get_array_length(array):
-        return len(array)
