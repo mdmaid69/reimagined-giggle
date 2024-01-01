8e1ac20b9ecd6d1d699f86fd01c8d2c70156b948
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:21:08 +0000

    section marry error introduction cruel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
