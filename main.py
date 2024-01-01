813fbe155af90321154017e125b7789ddd2b8c4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:17:46 +0000

    sort finally receive his Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
