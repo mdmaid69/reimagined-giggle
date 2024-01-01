52b44e822b875cdaf089e72b67f1d07c04c7bd8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:19:45 +0000

    while way pleasant exhibition admire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
