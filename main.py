2218b1efb54df356b36ee08f1df852e55f2c6ce6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:13:23 +0000

    cut land correction word himself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
