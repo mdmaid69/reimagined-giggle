cc27a469ac5ce7a19a8e6ae4d57e48fe5015882a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:02 +0000

    cinema past means public workon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
