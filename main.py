8dde5ed3643fafc84cffe62e447173485d20071a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:40 +0000

    special attempt surprising birthday clue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import os
-print(os.getcwd())
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
