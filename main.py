93ef650931d4aa48b3cfb8ff68777177a2a54dd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:23:56 +0000

    pot push like pleasure force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
