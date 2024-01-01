ad21cb4fd5229287e246a9d93d7657d1095288e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:52:44 +0000

    learn year normal begin extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def is_even(n):
-        return n % 2 == 0
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
