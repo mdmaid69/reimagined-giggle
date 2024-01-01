c86cd148b8980f580e8d1dd595f4d078acfb271a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:32 +0000

    down dry great them sort

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
