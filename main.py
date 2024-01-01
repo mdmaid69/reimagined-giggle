cb1a770b72f1e91bafad6e96d49d200c3598d528
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:33:46 +0000

    solve practice form forgive door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
