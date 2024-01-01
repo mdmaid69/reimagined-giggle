d93fa346dfdf2372dd0a4529e3cc06fb601a8a1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:26:19 +0000

    identify surprise sea gas pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
