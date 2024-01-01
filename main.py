e056f65e036e4f46c8f913a8b7f51a13b50d60a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:22:32 +0000

    operate imagination mend typical sell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
