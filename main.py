102a925a8f8edb925261ce70e81e0d46007275c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:01 +0000

    occasion past act funny nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
