c7dce110db84444e9ae8a5653a58586db18589fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:20:34 +0000

    definite interview tragedy yes explanation

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
