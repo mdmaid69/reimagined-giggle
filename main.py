c37d659531db021339a39de831180f13179ece6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:53 +0000

    shoot ashamed plain workupto the

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
