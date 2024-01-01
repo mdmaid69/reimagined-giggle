fb50de8cec3c7fcb7ec53032ef7cd1f119876a6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:32:20 +0000

    away consult bear uncertain term

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def iterate_over_array(array):
+        for item in array:
+        print(item)
