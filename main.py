6206bb8b59d5a71f3f0f31b4842c993c8cb225d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:28 +0000

    vote drop attention lucky round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def remove_directory(path):
+        os.rmdir(path)
