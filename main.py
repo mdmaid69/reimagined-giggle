5123dc935e08e00964dd91cec73f4af5b54a17a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:33:54 +0000

    remark achieve just will same

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+print(math.pi)
+import os
+print(os.getcwd())
