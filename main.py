26839c2d5d0595daec94d4f11bb7ebb49c52ba8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:56:02 +0000

    imagine block understand concern standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
