16d603ad1ebf97cb4cb046b58296986e6aa98752
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:14 +0000

    vanish it combine emphasize tidy

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
