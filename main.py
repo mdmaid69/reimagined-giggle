b9b2b486116d2a98ba5037919cea5f9655dde4bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:08 +0000

    holiday complicated authority woman art

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
