0e87af74dcb97b708b470507874d4d16346de7ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:15:02 +0000

    official sight view lesson black

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
