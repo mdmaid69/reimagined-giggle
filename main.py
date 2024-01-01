83c33601901b525f7643e1b596ca00bbafe2080d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:25 +0000

    take wheel as boring laugh

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
