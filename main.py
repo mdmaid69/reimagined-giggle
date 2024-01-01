fa5ece7f2a977b541a5ea432436945802d975650
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:27 +0000

    none report speed coffee confuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
