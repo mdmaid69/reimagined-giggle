b50d01cc98b58baa93565c743a773f9e1d43ee8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:12 +0000

    565end production difficult side cause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
