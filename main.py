7b99ed3ce90bbbf0a90a2e4ba6d59373043af8c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:01:02 +0000

    of inside sport land answer

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_as_int(array):
+        return int(array[0])
