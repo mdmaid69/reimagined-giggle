b2c520356718ca6fbb4e68ecbffd42527fd630c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:57:34 +0000

    paper place art forgive touch

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
