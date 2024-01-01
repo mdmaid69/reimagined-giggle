982e4118962a497b07faba5a228578311b6c7d4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:52 +0000

    natural dry call how other

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
