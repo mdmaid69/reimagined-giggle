8bc837c8b2068745a70cbef234a44443e5fccc7e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:20:40 +0000

    train branch allow thing report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
