db2b6f3d08f62c175f979f0a7215af94a2e31e72
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:45 +0000

    foot godown weather path popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
 def calculate_debt_ratio(total_debt, total_assets):
         return total_debt / total_assets
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
