0449455d58ea339e2081d22e5d378f9fa3d6c32f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:12:35 +0000

    damage surprise concentrate Cc city

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
