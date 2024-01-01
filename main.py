d00e7d23f0d57ad6072382907195a8173c6b2651
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:16 +0000

    sticky Qq brief temper pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_palindrome(s):
-        return s == s[::-1]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
