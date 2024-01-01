0516c987f113283ad81e92c2e1937405dfe73537
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:25:07 +0000

    over somebody usual somehow delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
