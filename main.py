a900bd9afb273ef71e67e7bde25eba52573ae704
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:00:23 +0000

    broken pretend disturb problem more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
