e17f00986061ce6ec9916cc1b85beb51dcddbeeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:33 +0000

    lead equipment holdon battery use

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
