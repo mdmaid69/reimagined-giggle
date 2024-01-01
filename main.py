b3bc832a2cf4eaaefc9477b32c05e51e03cb08c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:44 +0000

    look generous present lose colour

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import collections
-def create_counter():
-        return collections.Counter()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
