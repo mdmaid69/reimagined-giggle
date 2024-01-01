5376c196c425aef969feacba82b48990e9d4065a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:18:55 +0000

    pity ill somewhere punish kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
