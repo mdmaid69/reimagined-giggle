cc52ece132bfcd8e560d4f2ce5f5d0710db27221
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:43:11 +0000

    Cc gooff suckme complicated runover

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
