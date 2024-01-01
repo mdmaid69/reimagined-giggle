899f489f18ee3febc5c63f8c96a245417a160f72
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:06:29 +0000

    take worry cry objection approve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
