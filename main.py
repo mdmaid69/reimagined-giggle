9b7881ced74c7b120fc5a2c2c1b95a4c53ee4ecf
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:51 +0000

    extremely official squash honest certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
