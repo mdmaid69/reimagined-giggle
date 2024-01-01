d31553a770cb1b146f53e08faf43d90297e4ac67
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:57:41 +0000

    early track time replace claim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
