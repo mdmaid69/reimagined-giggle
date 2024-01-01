4e78f14721334ce8526c15b527b6b23f5367b77c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:24:01 +0000

    the slip catch film speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
