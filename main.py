9ec24148a80c51874670d11a0cd26e6a25700e5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:23:07 +0000

    so impatient basically careless perhaps

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+def calculate_acceleration(speed, time):
+        return speed / time
