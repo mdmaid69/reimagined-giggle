b5615eb5a776840face8cdf14e0b4f13fa3a6843
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:55:23 +0000

    state gas main road whoever

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
