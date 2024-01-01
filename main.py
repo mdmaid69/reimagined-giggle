1db5afe90d5c4b9703e9da03386064bddb9c1930
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:12:45 +0000

    female talk business 568energy job

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_logarithm(base, x):
+        return math.log(x, base)
