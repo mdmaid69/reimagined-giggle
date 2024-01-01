06d0cb53a629fff9a75053d0c9b86bca7ca24668
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:51 +0000

    modern thanks trouble skin precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
