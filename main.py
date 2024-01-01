3e79957eb342c6e0a6ffffee4725d5adbaf55e18
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:56 +0000

    charge catch phone succeed true

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def count_elements(lst):
-        return len(lst)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
