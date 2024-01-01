5ff8175c3f7893350248691b1cab5346772e9eec
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:19 +0000

    phone doctor goup exact care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
