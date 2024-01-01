492f947d8afe1db320ac505493a4ceb785995e49
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:00 +0000

    body question hit major Oo

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+from collections import Counter
+print(Counter("hello world"))
