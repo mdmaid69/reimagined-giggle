64c43db501baae26d328c57810718294b9ce2fc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:11 +0000

    tap onto usual brave future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def square_number(x):
-        return x**2
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
