4eea55e5c4bd18d818750c4376738cd38e46aba5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:45:27 +0000

    still physical arrangement push interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
