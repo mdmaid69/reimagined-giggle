54e2be54e281f5c5988045f7e149330a61dba0b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:11 +0000

    side sensible space behind speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def square_number(x):
+        return x**2
+import array
+def get_array_typecode(array):
+        return array.typecode
