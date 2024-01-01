66cd448233dc168c48afb5e36e659c23c938bdc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:07:47 +0000

    generous strict trick note style

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
