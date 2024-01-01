c057bcab601bc552f54685a426a1121e60f51da1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:30 +0000

    arrange enthusiastic principle summer concentration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import tensorflow as tf
+print(tf.__version__)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
