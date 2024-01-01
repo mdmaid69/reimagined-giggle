cf92471a2e33ca400138efc85d6e4c255cd6ee27
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:24:38 +0000

    show wait deep unlikely persuade

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
