54245053c79382b7f7ece06dd2da5a108272a124
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:25 +0000

    unfair turnaway accommodation hurry scream

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
