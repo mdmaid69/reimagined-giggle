e2a8188f5398831a2aa9ffcfffa8e2c7275ddbe9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:00:30 +0000

    block far object share identify

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_area_rectangle(l, w):
+        return l * w
