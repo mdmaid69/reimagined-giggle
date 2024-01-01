4e59188abc3f575b8b087d48cf6ba27d532cebe1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:49:23 +0000

    swing relax them block odd

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
