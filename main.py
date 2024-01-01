173d65fffc418c2e8040b9f4f5d42ee06b688890
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:22 +0000

    practice support arrive work cry

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
