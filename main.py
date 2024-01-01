14ccb66c1e767eb7c51353f6394a9dc38073d3c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:11 +0000

    divide past evening regular delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
