2295c714a81011abc85061391994c4266260b954
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:28 +0000

    tall meal few tap positive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
