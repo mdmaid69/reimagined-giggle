a46d1b49beea04ccc38f50578a8543f863a73639
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:03:26 +0000

    change accidental strike lose gather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
