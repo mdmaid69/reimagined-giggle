29e47d0b6a94f65c1eecc013a0ac237ebad78393
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:08:13 +0000

    plan wish blow support forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
