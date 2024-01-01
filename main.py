a2ec839e1d2c32e7f61c944b3e468a51c3f94804
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:17 +0000

    approval improvement owe glad tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
