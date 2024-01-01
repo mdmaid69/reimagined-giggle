53020f780cf5149bf834923838684193a091b8e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:20:10 +0000

    568energy Cc project bringup duty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_item(array, i):
+        return array[i]
