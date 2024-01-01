87eb3f6d0cf454661ad4b13ec16516f40e2dcb2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:39:02 +0000

    balance telephone statement nature cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
