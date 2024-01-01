34b7fc623be1682054ce1cad2c8d32552184e383
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:36 +0000

    bear supply everywhere model offer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_user_dict():
-        return collections.UserDict()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
