b9421b3fa32ddc1cafa15a2382d47408269a78ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:39 +0000

    phone squash clever extraordinary discover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
