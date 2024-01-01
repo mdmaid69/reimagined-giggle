1bc335cf9e9883433ba1516140d290e4541b9265
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:58 +0000

    proper principle holiday order gap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_set(array):
-        return set(array)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
