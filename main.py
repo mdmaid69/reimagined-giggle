7cd4be7ce5578643dfd97a375c1b67d4e76f7499
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:34 +0000

    pronounce giveout detail same know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
