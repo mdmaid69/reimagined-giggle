48780ae3c4ee2e3f4d10dca88480d6725c512cfc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:27:45 +0000

    relief extraordinary book only second

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
