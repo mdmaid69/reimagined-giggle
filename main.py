7db777a3a8ece6fbb482c428e4d8f52cb26d0e17
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:27:51 +0000

    scene invite tape lesson rent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
