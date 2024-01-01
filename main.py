09ab456bb7bf537825b18c57ed17e30c1f10474c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:53:54 +0000

    tread return hardly problem unit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+print([x**2 for x in range(10)])
