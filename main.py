0469b6915f1143ba0df1a7121731b24e51034954
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:12:58 +0000

    mixture accordingto fence file scrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def get_array_as_frozenset(array):
+        return frozenset(array)
