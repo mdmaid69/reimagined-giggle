adf781ce8eaeba72c1dde202026640e57d4f0ef9
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:23:44 +0000

    warning of behave quit nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_float(array):
+        return float(array[0])
