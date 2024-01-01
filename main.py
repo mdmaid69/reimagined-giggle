71d3e29a518f1ae54aa019603ae9e585e1454ae0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:28:51 +0000

    tear swallow comeup good whenever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
