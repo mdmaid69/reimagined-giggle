e4ce351db5445ab3bd5499bebe93f9f67a0eb3c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:29:12 +0000

    reasonable introduction partner demand none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
