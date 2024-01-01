bbe81d101afab022943158434dbe6f6029b1d79d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:37:17 +0000

    wonder subject knock invite belief

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def add_numbers(x, y):
+        return x + y
