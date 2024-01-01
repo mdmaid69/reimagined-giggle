11deee0df76a72c43f0211250b400abd78c669f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:10:53 +0000

    building trick serve later neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_list_from_array(array):
+        return array.tolist()
