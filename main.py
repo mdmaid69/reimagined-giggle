924d1f6ee75591026141d6c0a9bf582f091ea6f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:40:26 +0000

    heavy drink easy punishment talkback

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
