cf181dd63c4a34af0f1dcfddee382e3e630dae2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:06:38 +0000

    guess town profit recommend reduction

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
