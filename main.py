613edf2c940b2040dcc7c6010615d5840496b436
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:26 +0000

    shut me unite whistle manage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def calculate_speed(distance, time):
+        return distance / time
