f25130ec381be8ef49bbee75a308d508f80722b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:13:21 +0000

    suddenly early before suck holdup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def subtract_numbers(x, y):
-        return x - y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
