3b6e91da6c19e14c72b527074eae002a15f08761
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:03 +0000

    means pocket replace however line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
