ad58155db2e76af78ac2d81a3d32bbaf8bf1d9e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:48:37 +0000

    goback(to) gap noone material original

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_area_rectangle(l, w):
+        return l * w
