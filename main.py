b1d81728e3b103dbda69e8d89a5c0768c8e7e6c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:52:55 +0000

    fashion glass plan turninto meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
