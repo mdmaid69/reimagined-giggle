1e7d849831363a929f964a6ea8319b749e4d9252
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:31:34 +0000

    true anywhere view punish aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
