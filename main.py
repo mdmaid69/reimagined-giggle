aea4bd009b410768384c85e1f0be7fdc4f8c4653
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:17 +0000

    problem bunch medium average chase

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
