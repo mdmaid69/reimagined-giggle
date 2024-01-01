ee3bb42597a64b0d64f4b3b32924b33cebc6013c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:46:06 +0000

    maintain side beginning ordinary equal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_stack():
+        return collections.deque()
 import os
 def get_file_size(filename):
         return os.path.getsize(filename)
