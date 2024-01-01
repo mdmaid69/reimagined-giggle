cf60e3805676a528bf459896bef90697461a6453
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:22:56 +0000

    black if drink set back

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def convert_to_binary(n):
-        return bin(n)
-import random
-print(random.randint(0, 100))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
