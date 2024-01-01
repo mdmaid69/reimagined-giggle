c1430a19731819ec3ed488e85d8d7f7444cbc68d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:05:55 +0000

    ever back issue perform divide

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def remove_duplicates(lst):
+        return list(set(lst))
