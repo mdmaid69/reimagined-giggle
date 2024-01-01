96c94a58293a4d5b8371bc0d810b17f009f0b62e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:35:32 +0000

    receive it ill press boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
