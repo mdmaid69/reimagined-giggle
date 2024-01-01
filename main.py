120bb1d5a87999b9de773df165071f18bd13252d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:19 +0000

    ordinary involve love pot add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def convert_to_json(data):
+        return json.dumps(data)
