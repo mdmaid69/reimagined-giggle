5b39e6b2d951ee394c40d1d08ac998d5f24a2eca
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:42:17 +0000

    within pretty suppose operate train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
