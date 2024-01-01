99ab352c8ad6cb99fec5b33256b64d4ae2c4e227
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:26:07 +0000

    trick measure same will cross

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
