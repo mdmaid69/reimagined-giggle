75ed58c547b0ac0455bc172cab59dded87c77aa1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:50 +0000

    recognize present company holiday certain

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def subtract_numbers(x, y):
+        return x - y
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
