0310903ab808c21b00c234076dd2b4973098456c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:33 +0000

    waste we unlucky impressed flat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
