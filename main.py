60ef0dfb6e1b68b7ccd762a17641bd64241906a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:26 +0000

    have better university fairly neat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
