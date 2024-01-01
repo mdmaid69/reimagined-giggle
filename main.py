fd21b79ce2a58e0594dc9dc9ebd8e0ad40f96de8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:29 +0000

    meet damage extremely unfortunate creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
