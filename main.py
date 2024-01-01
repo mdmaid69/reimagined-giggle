cd7aec1bae9b91b447df8a6d5742979e061e54ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:29:13 +0000

    sense sale aim discover wide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
