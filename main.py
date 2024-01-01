cb419a1d0d740d076ff3d91203449772a8006db7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:18 +0000

    store communication future company excellent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
