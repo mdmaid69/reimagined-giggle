981062b318b061646bfcaffeb2e42d4b2b5983fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:22 +0000

    pass man print you schedule

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
