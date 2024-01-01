e951880a1fb6a1ced6ca4507fde6cab78885af2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:30:28 +0000

    accordingto thin common produce destroy

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
