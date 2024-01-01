91b738afb2e2c7032dbb6122a3b48e2cb98a2fe4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:29 +0000

    turnon bored ago absolutely progress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_power(work, time):
+        return work / time
