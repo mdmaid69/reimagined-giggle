5b1ac1a1c27f9647776792b4a272d2944ee31dcb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:59 +0000

    this air land miss character

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
