15343281e998b222b104d55c87966905c7df35c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:23:59 +0000

    action trouble wrong beneath precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
