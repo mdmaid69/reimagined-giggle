f7890ec17d6f9f864694e6c206f1de603fa7c1d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:14:30 +0000

    geton deal theirs answer big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
