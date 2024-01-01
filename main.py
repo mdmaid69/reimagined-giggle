79f9c2a9d96efcf6580eed8633a57c3631c4d88e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:01:22 +0000

    properly second fast pretend fear

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def count_elements(lst):
+        return len(lst)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_factorial(n):
+        return math.factorial(n)
