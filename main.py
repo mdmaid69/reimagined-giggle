7906027ce6fe1386bec295e9379e708804be6261
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:59 +0000

    sort tool question but cold

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
