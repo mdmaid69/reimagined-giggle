c200df6b36ebdf193f5c0bd40bdd75ea70f72579
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:06 +0000

    boat medium regular brush season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
