0f1d5e791635ee17ef41b0e1d1dd1bda9a8bfc37
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:09 +0000

    average line information play one

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
