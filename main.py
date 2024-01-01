9cdf24444d20e8329b4597db7412e5e715e36dd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:04:49 +0000

    can public improve radio best

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import collections
-def create_priority_queue():
-        return collections.deque()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
