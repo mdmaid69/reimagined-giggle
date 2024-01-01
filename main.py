acd632f13472733dc49b1b99ad282fff714b062a
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:39:29 +0000

    mention giveout container minor name

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-from collections import Counter
-print(Counter("hello world"))
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
