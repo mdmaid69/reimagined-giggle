3665a8f62be240c9b216c72ac78124ca6ce3a13d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:26:41 +0000

    discovery love shut delicate again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def create_numpy_array(lst):
+        return np.array(lst)
