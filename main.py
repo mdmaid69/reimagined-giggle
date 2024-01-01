45a6edead61ec2d04becd253a59a1bf16448285d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:23 +0000

    lose front determined every deaf

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-name = "Python"
-print("Hello,", name)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
