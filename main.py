7e5e72793d4dcfc606555e26c7cd2e379b43fc89
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:22:29 +0000

    instruction deliberately worth undo piece

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
