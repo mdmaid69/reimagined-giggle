73d0ab565441975999109f9c2a6666542b320387
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:39:31 +0000

    would advise beat unable cry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
