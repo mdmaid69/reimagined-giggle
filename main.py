14bd00d8aeeb9cb69bae49775bbe4d1f5f0a0650
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:00:50 +0000

    rough shop he challenge almost

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
