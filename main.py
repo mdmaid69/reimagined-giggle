701256f8e388762072fb913c6fb419dd16dac025
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:36:55 +0000

    ride polite up there model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import collections
+def create_user_list():
+        return collections.UserList()
