feb0a05f95cb1ea530be8bfd658ffb31f9847bb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:09 +0000

    away used turninto stage like

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
 text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Characters:", len(text))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
