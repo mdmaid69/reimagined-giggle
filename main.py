76633a30bd4a7e06a326d05da39fb46b3856c71c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:04 +0000

    contrast set date Qq fierce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+print(math.pi)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
