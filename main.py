3bb68e362a3fc4ff6d324f619d15819a9b53dbda
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:00 +0000

    talkinto squeeze object dry increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
