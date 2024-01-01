4700c7d2cff9ceee2ac313efce7f79504ffc56e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:48:42 +0000

    share hardly against wish know

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import sys
+def exit_program():
+        sys.exit()
+def find_max(numbers):
+        return max(numbers)
