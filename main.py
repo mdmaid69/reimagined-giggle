6c930b451b01d2939366ad86aa994e62c9aa811b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:07 +0000

    emotion uncertain storm persuade check

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
