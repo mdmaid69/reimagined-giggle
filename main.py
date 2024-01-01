11ab052458485b60b9373f38a090c30f622433da
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:21 +0000

    answer pretty law public although

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
