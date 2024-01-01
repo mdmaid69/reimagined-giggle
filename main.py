f7e9af06c7a535796b0a615dae3e6a97ce4e67ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:19 +0000

    sport run when crazy balance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
