c3be6acbf9fe8ab4cc1963da73a195163391511e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:09 +0000

    tight shine nerve why find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
