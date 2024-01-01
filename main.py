b5477d5d6ac1e246c8855a3b16a6f25637e595ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:05 +0000

    scene water money I science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def remove_duplicates(lst):
+        return list(set(lst))
