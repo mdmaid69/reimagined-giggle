44b58a3a589806d2b0915498dc5c5a23fc22f538
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:46 +0000

    fork poor male gap search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
