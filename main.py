49571a90fb8d8c7a0655a74824955056e261e14f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:16:10 +0000

    dig design place vanish fill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_itemsize(array):
+        return array.itemsize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
