79e680b9ac900a6fe757a8aeced16c93d251136f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:26 +0000

    still deliberate tape correct software

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+import os
+print(os.getcwd())
