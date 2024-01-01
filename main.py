ff9fed313ce34f5424267bed654cb5759ff5641a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:35 +0000

    kill attempt interest attract science

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
