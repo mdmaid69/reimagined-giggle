b7b806fe6850622c45ba3dbc555a13d534d57f0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:22 +0000

    temper any by evening whom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
