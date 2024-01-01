97cf840fb607dc5e8e5dbaaf3d2e2553f159d1de
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:03:08 +0000

    deep burst need sort quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
