a2b7349770cada7fdde6e2dbb6c7d1489ff72a21
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:03 +0000

    bus there stupid successful family

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
