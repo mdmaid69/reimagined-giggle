96410d2af9c6d4065ac2b1c3eff7c0be4c5c8d05
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:28:01 +0000

    even by nurse else apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def sort_list(lst):
+        return sorted(lst)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
