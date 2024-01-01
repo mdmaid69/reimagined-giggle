9cede770811772a88502c417c40c509475c37f61
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:35:26 +0000

    partner determined beneath section workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
