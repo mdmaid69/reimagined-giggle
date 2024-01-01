758b016f9da150f0c17ca8ff4450cd3f56d4b034
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:58 +0000

    substance face mind outside satisfactory

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def cube_number(x):
+        return x**3
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
