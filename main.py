a508dd14330c4f819a6d74b20ba0030c96652496
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:40:49 +0000

    steer react breakinto no dry

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def convert_to_hex(n):
+        return hex(n)
