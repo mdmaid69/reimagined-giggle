36062b2b5a1cfb4d3bea8600aaa121a01f1dd994
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:56 +0000

    basis about notice often underneath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
