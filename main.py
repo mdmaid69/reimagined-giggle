5a52951a83cba0a4602ec18be67cd3214cc51d89
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:18 +0000

    rise set supply advice official

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
