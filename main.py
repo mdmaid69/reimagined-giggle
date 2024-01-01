340d346e39163f30fcfaa1ca4153c3608fdc6f60
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:42 +0000

    certainly cost accidentally cut tend

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
