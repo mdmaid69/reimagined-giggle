fab1afdf14251536cfe6a56fb2977565fd8d3c28
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:09 +0000

    software such about disapprove promise

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_acceleration(speed, time):
+        return speed / time
+import time
+print(time.time())
