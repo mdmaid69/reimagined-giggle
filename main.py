2af6e3c4251887af4a02dd1e76e0c86d433df146
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:09 +0000

    like breakout regret proper weigh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
