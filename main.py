25e57c212ee5f97bcda449713714b36aa3156e9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:49:22 +0000

    warm behave responsible dish name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+print(math.pi)
