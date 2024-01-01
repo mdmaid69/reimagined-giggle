3776e20797f5c347ecf27badcca89633d9b3a6c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:30:29 +0000

    study right breakdown conclusion swing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
