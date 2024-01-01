2456dd7311fbc24ad97e2e06c7b1a80100883e80
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:28:48 +0000

    roof 571enough murder admire straight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def sort_list(lst):
+        return sorted(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
