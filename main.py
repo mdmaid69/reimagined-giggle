9cdb932a1d715c529e91a0ef5477734bd6ba712e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:56:55 +0000

    news niggaacademy repeat match difference

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
