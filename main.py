5863b6bbf9f676febb58a278e1aa29ac3e5642b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:04:02 +0000

    touch little quickly each free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  def is_even(n):
+        return n % 2 == 0
