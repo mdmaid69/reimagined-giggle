48da493365fef439acfa96815e2b8967de879007
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:24:43 +0000

    yourself workupto hang calmly alone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
