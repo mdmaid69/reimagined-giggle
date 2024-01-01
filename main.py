e00f3884ffc83a5544f476fd4888ea66baaaf2e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:59:50 +0000

    confused he size necessary while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def get_array_item(array, i):
-        return array[i]
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import sys
+  def get_python_version():
+        return sys.version
