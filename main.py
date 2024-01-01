9dd24ef3eb644c8188871a8a2d6b040251e70f42
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:12 +0000

    now date hard cross disturbing

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_index(array, item):
+        return array.index(item)
