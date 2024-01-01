c0547fcb7c5456011ab14e017e64693cdee11158
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:36 +0000

    cheat section apologizeapologise allow survive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_octal(n):
+        return oct(n)
