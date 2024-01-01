695bc19f271f9b7ac5bed8bc34806a8be90f29d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:53 +0000

    direct exercise repair entrance talkinto

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
