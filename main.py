451320c51f0a32da3e0ca7b62c414aca74ce2e3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:48 +0000

    today match exercise welcome comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
