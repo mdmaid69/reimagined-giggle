3ff0278b177a248aec69eb848c839a265bbe268e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:39 +0000

    meanwhile Gg Itself comein before

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
