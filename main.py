3940ff276cafc8e0357e7ef58d1771807448f0d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:39:07 +0000

    experiment far related class wake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
