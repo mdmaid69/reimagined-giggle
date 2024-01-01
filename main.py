8d9dbbd563e3e8990e2d22ca604a85ad28840a73
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:50 +0000

    estimate crowd prize union insist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
