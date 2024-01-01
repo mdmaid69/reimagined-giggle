95bb2081f953f68c94d0a163b500e5ff06313948
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:58:00 +0000

    leg tap fill official total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
