6b2fd38cb2f80551bef80ddbac8b8fe700c808b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:42 +0000

    discover stick aware smell happen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
