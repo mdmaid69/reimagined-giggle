befd40e6640b929fa7237ce4868ab71a4090b428
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:50:52 +0000

    Zz shallow know settle extra

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
