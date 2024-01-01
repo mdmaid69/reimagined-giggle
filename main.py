3522b927dca2431c5bf7541cc6f2cf41fbe287fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:10:33 +0000

    Yy brave ride during swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
