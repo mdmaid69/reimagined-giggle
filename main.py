17036ae532363c72b7c5566b6d8c90f6ffac3ddf
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:28:18 +0000

    572enter earth wind money on

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
