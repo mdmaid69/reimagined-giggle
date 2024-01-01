6680458ec370bbf342699d8903c48fe041d1bb94
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:10:47 +0000

    left false load majority yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import sys
+def exit_program():
+        sys.exit()
