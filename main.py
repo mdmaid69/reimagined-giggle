5185f0b73fd95fe53e97b6aa97c58964e4efa1a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:20 +0000

    prepare class finish town whom

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
