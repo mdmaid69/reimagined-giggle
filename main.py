de4d35bcac5b5fce76bc807b60b02428855b2675
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:30:46 +0000

    example judge class sensible rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+def generate_random_number(start, end):
+        return random.randint(start, end)
