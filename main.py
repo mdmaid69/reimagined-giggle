2d52c13be747477bcc377a8459c817a41240e69f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:38:48 +0000

    someone necessary fail arrive dick

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
