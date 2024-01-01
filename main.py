8bfe84aa3dd92f347414224ece69da90adaa9f99
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:26:36 +0000

    joke future regret match possible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
