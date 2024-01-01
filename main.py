266dd00a9a6a81d09ee01664ecd5a0723532adc3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:24 +0000

    scare factory genuine be affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
