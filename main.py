68c68e1b6a38644cf52ec9cc60acd52f26979b07
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:38:40 +0000

    slightly thumb heavy duty lost

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
