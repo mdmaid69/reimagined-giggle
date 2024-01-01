94bb921bd494db2ce7078369f7fe5482c27afaf4
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:41 +0000

    light noone wide mark bend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_arc_sine(x):
+        return math.asin(x)
