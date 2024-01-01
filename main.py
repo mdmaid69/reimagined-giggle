09ba905322a56a3b5a5a955ebeded9c3b34e80b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:33:44 +0000

    negative bag content damage ready

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
