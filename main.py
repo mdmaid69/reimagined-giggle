320f70cf5a7fc8743b87cd946299048005d758ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:22 +0000

    ocean turnout drop snow lead

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def is_palindrome(s):
+        return s == s[::-1]
