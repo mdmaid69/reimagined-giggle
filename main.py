feac2e30e7570a5182c66555b858349cbcbe0017
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:11:33 +0000

    before whether wave themselves thinkover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def greet(name):
-        print(f"Hello, {name}!")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
