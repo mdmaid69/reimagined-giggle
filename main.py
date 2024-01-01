f1595c0d722fb0a83a824f9ada292303d76a3ff0
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:47 +0000

    tight character familiar book radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
