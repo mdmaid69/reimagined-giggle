88c70d3a32f32e85802379e44758908bcf5ba7cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:52 +0000

    please brown sale meeting takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def convert_to_octal(n):
+        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
