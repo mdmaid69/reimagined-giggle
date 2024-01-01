9e43511791c80fae0ae23ee61bf9d0e2bbed6f9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:05 +0000

    anybody serious sea opposite inform

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
