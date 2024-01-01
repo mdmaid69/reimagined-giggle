11c06abb03d7164dd955b149d2c7d8832005662f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:16:39 +0000

    whether make mad noisy one

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def reverse_array(array):
-        array.reverse()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_current_working_directory():
+        return os.getcwd()
