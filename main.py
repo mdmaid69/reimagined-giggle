a88b39cd08e532cec40f39ea7249209aff514517
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:29:47 +0000

    month first make dear base

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
