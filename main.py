99e3f0a38f2e2b5cb045781a52fb681d49889292
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:43:47 +0000

    Hh mistake back keep genuine

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
