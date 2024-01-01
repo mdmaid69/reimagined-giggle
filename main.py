d65195eb9bc595db3ea44919307e9f0da542c3ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:29:23 +0000

    only ought the in key

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
