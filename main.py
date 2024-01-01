f260fa6ba9e9fa42b878a3535afcf3528c3d97ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:55:52 +0000

    go sore between find callback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
