3a8fa193f82c6f547445e05d6589e9ec54f22f2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:33:58 +0000

    shrink allow limit nothing you

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
