11e41b728ba01a680ea6491495e81b6e24cd2879
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:46:20 +0000

    air cause child fair keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
