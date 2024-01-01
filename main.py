0ed3b4b85b2120a3e949e99b730641ec580089f6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:44 +0000

    getaway knowledge away harm gooff

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
