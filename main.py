e1540439f1f3397b0dd7c20f2f215e0acb8629b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:41:58 +0000

    his beginning season Oo lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
