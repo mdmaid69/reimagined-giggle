4ed1a3281cd0d26244143ce8158bfa4b0acdb488
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:04:53 +0000

    instant concerned thirsty party concentrate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
