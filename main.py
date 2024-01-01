9e02283079e89e199b845ab842b3d9bafee37462
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:07:33 +0000

    keepup arrange tell exchange correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
