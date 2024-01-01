769aa3f4bb75f6062cfdf5e93d4d4ffb4d88add0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:30 +0000

    still opposite rush nice age

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
