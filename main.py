79b594d76ff6f86dc0757ac73a5373f753cf17f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:53:07 +0000

    good appeal list knowledge odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import os
+print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
