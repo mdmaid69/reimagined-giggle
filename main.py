435cfd6b5be4700f8812fe70fec78f4532b3147e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:07 +0000

    hear healthy level pretty talkback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
