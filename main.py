e86aebc31b13b767eef025900b7b47031c54f23b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:10 +0000

    bar moral further takeoff star

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
