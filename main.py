cb1ee693b6dc7cd88d2f4b4312e2d323cfcecf0d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:07:26 +0000

    high fast hold entrance drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
