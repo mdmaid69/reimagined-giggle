1222cd0b281974874ef18ff2e2cc6d29f47fd6fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:54 +0000

    dry muscle responsibility last account

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
