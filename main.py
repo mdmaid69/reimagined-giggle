a3a8c97c3a9de963d61e40db238f6fcff5c21d71
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:33:50 +0000

    age string principle turnover comeout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
