ab21fa8a29f117f0d59100f4fa69fdfc6d028619
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:10 +0000

    break region lead seriously live

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
