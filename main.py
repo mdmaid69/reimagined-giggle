bcc8f445b46d1f4db711ca5bc75d8513ccade482
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:48:56 +0000

    protection smell impressed future impatient

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
