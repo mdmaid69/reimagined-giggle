583f7c78c32dfaaa4b4eee52fc6907fcf51b2ff1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:57:48 +0000

    mess better finish squash main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_area(radius):
+        return 3.14 * radius * radius
