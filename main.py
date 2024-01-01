a8cb4cdd5925238d11cf4bd19b84cd833fcf782d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:50 +0000

    operate furniture release rid apply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+print(os.getcwd())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
