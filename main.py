91ce4723c630118c0e2cd3d5d08d56a478f31e3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:36 +0000

    unfortunate pity protest lie downstairs

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_average(lst):
+        return sum(lst) / len(lst)
