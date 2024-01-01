2e46bd9c22f025e8d693f089b75372289dd08739
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:20:54 +0000

    scare moral passenger wise friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def reverse_string(s):
+        return s[::-1]
