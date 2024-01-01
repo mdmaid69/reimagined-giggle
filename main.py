884ce6f4cb6aecd1a358666012404da5a580175f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:58:34 +0000

    mine gooff anywhere workout task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import platform
+def get_os_info():
+        return platform.uname()
