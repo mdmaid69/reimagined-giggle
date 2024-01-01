163104e6f0c348bdec6de911a5573578d565fa69
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:35:15 +0000

    tidy do mystery reduction replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import sys
-def exit_program():
-        sys.exit()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
