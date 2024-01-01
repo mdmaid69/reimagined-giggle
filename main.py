34341e3599784ee97d6e35e69609e391c3fb108a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:06:35 +0000

    fierce silent store reliable warn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
