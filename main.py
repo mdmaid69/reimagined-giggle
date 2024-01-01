fc56b91b567ce3066331fdb87a9b210c344947a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:36:24 +0000

    apart position us pin get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
