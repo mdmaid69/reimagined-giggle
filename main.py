6ec5f022681fa53f4583904ccf6405035583af2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:20:50 +0000

    reduction doubt emphasize song total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
