d5931bd97f5bd2d6b5ad94736d51886918cac7b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:32 +0000

    organization smile alive operation since

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import tensorflow as tf
+print(tf.__version__)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
