9df72e29f5a092e66aa4acee8a5c1efbc983b90a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:25:51 +0000

    even class move excited rest

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def get_time_since_epoch():
-        return time.time()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def is_odd(n):
+        return n % 2 != 0
