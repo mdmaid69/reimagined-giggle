696fd95630cd45e9d291c287f9a0aadcc443cdb2
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:00 +0000

    solve announce task famous take

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
