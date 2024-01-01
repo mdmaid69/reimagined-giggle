a9906c8f120a770f4201dae50a72513cb8e8643e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:09 +0000

    general turndown giveout different disturbing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import numpy as np
-print(np.array([1, 2, 3]))
+import re
+print(re.match("h.*o", "hello world"))
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
