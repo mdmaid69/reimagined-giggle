d36b42f757100488ca4701b2ce4c5f2f128705b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:42 +0000

    open pattern start breathe Cc

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-print(math.pi)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
