f24e4166f897980cf08e1989bb1f1712b91655ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:50 +0000

    doubt heat wind least bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
