a3562d8b2420b3f8750bbfefbb7db4d709b78fc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:08:03 +0000

    responsibility patience soft past material

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
