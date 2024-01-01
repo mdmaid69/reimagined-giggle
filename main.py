64f1696d7b3aca23768575577b25901d3bf12e54
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:46:21 +0000

    suckme use confidence towards importance

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def calculate_average(lst):
+        return sum(lst) / len(lst)
