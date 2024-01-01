b32513d4cfe4770237ba3a99882b1dd57d32ecf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:11:30 +0000

    handle real takeout fat big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
