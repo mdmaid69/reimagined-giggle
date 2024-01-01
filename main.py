770202af907628560b28c77417be2e2825e8a415
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:11:55 +0000

    when see fortunate collection takeoff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
