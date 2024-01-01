e7cb31d82dbfd0a1df53f2ffbb3280327bac0df7
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:42:38 +0000

    small violence entrance fork actual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
