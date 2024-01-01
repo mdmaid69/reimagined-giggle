a2d0299265601d13e86fbef6b119c8b39d1d6a27
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:03 +0000

    bear sport colour male holdon

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def count_elements(lst):
-        return len(lst)
+def is_even(n):
+        return n % 2 == 0
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
