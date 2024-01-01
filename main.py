3e1f483bfe21b6d12b14709debcbf9b6539cf26a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:57:10 +0000

    band love evening block future

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
