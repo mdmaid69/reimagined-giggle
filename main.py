846a1c02fab722ba0438e23739ffece7cd3867c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:09 +0000

    create other remember prepare goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
