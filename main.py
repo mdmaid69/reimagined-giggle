5c5fc414abc7c9aa64739e772191e5fe54d57130
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:30:19 +0000

    ring before catch thankyou admire

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
