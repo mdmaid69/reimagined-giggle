7edcf97cc38fcac822366608db62f253df2a43cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:03:16 +0000

    breakup embarrassed clearly me afterwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
