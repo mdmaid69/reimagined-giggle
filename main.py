5807d6f14cf6cee6c5813d858f215b1ddf071e9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:06:12 +0000

    estimate crime maximum block distant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
