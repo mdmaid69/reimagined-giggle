ea258b5ce78aa88d09fba56dd5fba26e66bb70b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:55:11 +0000

    create ordinary present brush pity

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
