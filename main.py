1fc527fa4895132c044b99b1684d9c962e64e868
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:34:10 +0000

    workout usually file clear sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  def is_even(n):
-        return n % 2 == 0
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def reverse_array(array):
+        array.reverse()
