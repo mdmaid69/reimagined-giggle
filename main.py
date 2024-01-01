be453e7a551b44b1f90b392e1bfa927a3983429a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:11 +0000

    police normal like manage wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
