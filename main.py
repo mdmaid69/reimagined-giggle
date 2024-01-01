5461af771a4139887e8c64c96f25a6238ec466cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:45:43 +0000

    danger above later enthusiastic fasten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
