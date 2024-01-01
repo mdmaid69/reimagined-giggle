ff2aa765b6b2880082a86fa6eb545e42eb17192f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:23:35 +0000

    Ww prefer under trap cause

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
