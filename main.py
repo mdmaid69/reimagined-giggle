39eb34feb549971f2694687f8e9f15cd3d3b3a2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:59:26 +0000

    sale bored nice anywhere apart

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
