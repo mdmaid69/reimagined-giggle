af3d581a0d9c8896168d722881893abfeedd74a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:23:13 +0000

    small winter considering key dance

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_index(array, item):
+        return array.index(item)
