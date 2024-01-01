23202d0a9f83226d905c35b75ed041881145026b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:39 +0000

    before emotion immediately trousers tear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
