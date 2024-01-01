87472d8d1116f189705a5de893099352f8ff1cff
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:11:24 +0000

    impressed offence tend relax plan

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
