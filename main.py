aef66fb127227107d97d2034cf18e0174cab2c4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:05:49 +0000

    war sorry careful scare split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_pressure(force, area):
+        return force / area
