cfcf59c7faa29a1818e68f6f840ffd335ee06692
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:15 +0000

    night interview conduct star neither

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def calculate_area_rectangle(l, w):
-        return l * w
+import random
+print(random.randint(0, 100))
+import array
+def get_array_as_repr(array):
+        return repr(array)
