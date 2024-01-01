c6e8b3b218b402cd123279ec9a562c669c1e5eed
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:04 +0000

    getoff conduct borrow fresh yet

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_current_directory():
+        return os.getcwd()
+print(sum(range(10)))
