ebcf61b9b0aae25f74539caa9a19b44e7143bf37
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:48 +0000

    peaceful sink pick tax peace

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_power(work, time):
-        return work / time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
