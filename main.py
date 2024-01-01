363f7381c38ba1231e25dd50a2447109790116b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:52:48 +0000

    poor plant advanced available slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
