f6ca1fdf142060eb90c286d404f53d18433ea2d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:35 +0000

    winter himself sleep until door

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+for i in range(5):
+        print(i)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
