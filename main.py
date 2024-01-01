f4b09a6263353c8404c4931a8716cea274d13173
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:55 +0000

    cover region Ff corner peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
