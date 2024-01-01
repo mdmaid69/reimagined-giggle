fbdb7d5d248514c969baf0288bc2ca65f5101da7
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:51:57 +0000

    weigh post reserve aware middle

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
