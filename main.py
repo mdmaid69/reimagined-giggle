56db3a90e49ab52fbfe86dc87a358bb02269f511
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:37 +0000

    stare famous sink arrive recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+def calculate_density(mass, volume):
+        return mass / volume
