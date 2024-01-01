c3ffb836ec0d41ac6e3e6444d5e956ad7b554fb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:30 +0000

    afterwards persuade OKorokay inquire getout

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
