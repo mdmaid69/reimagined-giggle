4386a7e690899d120b804d002b4644091479fcc7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:48 +0000

    comfortable about behind add middle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
