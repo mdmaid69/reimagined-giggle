cb4354a2245bf94f03dafcffc44f6dff554ab5b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:44 +0000

    involve mark back fact satisfied

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
