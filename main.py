2a2317e1370298e117f1b0ecd53a5d23bab8edf8
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:58 +0000

    getup service shut his give

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import collections
-def create_counter():
-        return collections.Counter()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
