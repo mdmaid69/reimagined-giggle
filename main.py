e0fcec90efcf5b56466bdd9db4998bfc1f750faf
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:54 +0000

    switch attractive spend finally street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
