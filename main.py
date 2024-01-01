4436c2802bbd9e503c0414c540dff2d280f2e9b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:27:48 +0000

    worse climb seriously ring sensible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
