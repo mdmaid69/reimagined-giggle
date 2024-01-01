14a62eb7a3a90101f2dc21b1310c660d94786dc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:00:33 +0000

    hate slight amount deep life

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
