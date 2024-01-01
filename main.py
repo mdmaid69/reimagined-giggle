54676d991143a3c2c7c3034f7e6ad92980417634
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:41 +0000

    off survive finish familiar deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-for i in range(5):
-        print(i)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
