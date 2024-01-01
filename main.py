9af843dcaba7ee6cc2225eca30462d0ade2707ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:43:27 +0000

    chief when pretty shrink behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def get_array_as_tuple(array):
+        return tuple(array)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
