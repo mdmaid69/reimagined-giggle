dd19b6795b00a29ed5352fb39831158f2ca06a57
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:14:34 +0000

    snow time good clear clean

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
