afa80381c6ab1100891a2344f9d325155be8b580
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:50:21 +0000

    accommodation swimming unkind stare emotion

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
