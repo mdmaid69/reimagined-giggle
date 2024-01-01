cf8d98f4fe34d06f4423f2e79cf8614a82e264d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:22:21 +0000

    insist pleasure sort bound produce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
