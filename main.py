30ecc85e47fb42b9027375b41c1247e0935859e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:06:07 +0000

    effort thinkthrough summer pretty price

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
