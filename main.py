20bcaf3fabd69d5e40bab97a34f97cc924364c94
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:58:10 +0000

    switch course urgent gift sentence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
