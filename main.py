e30ccd35fb7706991385e4f782624365a67e286c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:05:11 +0000

    interruption disgusting crime goodbye vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
