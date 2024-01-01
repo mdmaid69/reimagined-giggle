3a009199de47973ee74f996aa9f59d0fb7526301
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:23 +0000

    cum love disagree aim fix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def get_current_working_directory():
+        return os.getcwd()
