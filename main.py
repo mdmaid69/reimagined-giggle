a018aef2fad2bd5ed1adb8eb0081608f1e20b256
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:52 +0000

    bringdown force either turnup across

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
