31c17c059a79355b58bf0905ba2c991d3aa26b3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:19:31 +0000

    breakout introduce treat lean sign

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def calculate_acceleration(speed, time):
+        return speed / time
