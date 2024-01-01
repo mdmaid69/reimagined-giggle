d16d2d689b7fcbc132c57904ee70c58e0161d0c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:54:10 +0000

    famous sleep read plan game

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+print(math.pi)
+import os
+print(os.getcwd())
