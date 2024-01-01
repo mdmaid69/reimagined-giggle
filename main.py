0aac6296cf0b0f95b24bbe82ac405f254bd0482d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:18 +0000

    Ii snow miss get better

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import logging
+def log_message(message):
+        logging.info(message)
