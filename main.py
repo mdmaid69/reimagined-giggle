5f6ef215f0cd2a83d400aad4a54371d969a27bfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:50:11 +0000

    fight take add any Kk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import sys
+def exit_program():
+        sys.exit()
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
