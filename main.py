81f70f57d607265c9ae8c8a2b30822e21550765b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:23 +0000

    will march some bright object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def count_elements(lst):
+        return len(lst)
