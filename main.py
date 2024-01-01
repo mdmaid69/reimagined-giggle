8c05c971b1df0f4147d8988b0d5e95803a067a5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:19:45 +0000

    evening spring priority development introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 n = 10
 print("Square numbers:", [x**2 for x in range(n)])
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import time
+def get_current_time():
+        return time.time()
