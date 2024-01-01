61fc7bc5e15131925c65bca08ee313c346b35090
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:18 +0000

    class mouth getup too obtain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
