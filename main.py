f47e25a3911406cabbe234dc0dfa7e8d40f518ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:33:43 +0000

    paper common home trap store

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
