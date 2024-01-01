88854ff61fd99737856899dfb115de8076171ee0
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:17:02 +0000

    problem callback past anyone table

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-  def subtract_numbers(x, y):
-        return x - y
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
