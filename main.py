2c3bb66b873f3f6c5ccf149b167cec2e0c4f2c5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:36:55 +0000

    exchange one towards can recovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_area_rectangle(l, w):
+        return l * w
