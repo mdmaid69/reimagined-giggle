0ccaa069ab22afbcf76fa7209457243615098c01
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:47 +0000

    office breakup compete tour lot

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
