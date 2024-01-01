88a72d265a52c585c9bf6644cec27f6983220293
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:58:16 +0000

    about oil friendly introduce estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
