483a3f21e2995faefb3b7006d94bbeb31f18280e
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:28:51 +0000

    careful little famous complicated plenty

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def cube_number(x):
-        return x**3
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
