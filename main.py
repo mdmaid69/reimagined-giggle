624cef958acc0f77d7d41ddd6531fa7337de8397
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:50:44 +0000

    connect complaint today police odd

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
