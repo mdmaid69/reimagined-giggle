11ef7cc084cb54dac19a39dddcf1cb09a05e4ee7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:29 +0000

    motion angry trick callat miss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
