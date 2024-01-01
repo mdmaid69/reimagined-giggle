fb4bbe868b3d7aa9d387c6b121019435d750261c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:55:03 +0000

    only which ring unpleasant challenge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
