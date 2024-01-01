a14344d87d8df0c65b50a252cb111b9acaa79966
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:34 +0000

    satisfactory cut not all anywhere

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_speed(distance, time):
-        return distance / time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
