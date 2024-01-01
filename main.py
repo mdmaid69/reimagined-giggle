09ab927eabdd022de6851ece7dcb75ee1e5d4774
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:37 +0000

    exact environment sad disappointed round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_rectangle(l, w):
+        return l * w
