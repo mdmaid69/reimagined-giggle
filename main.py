f5ad914078b27c06a6d06954bcb7cc01ed7b5018
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:42 +0000

    certainly official emphasize uncomfortable band

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def calculate_work(force, distance):
-        return force * distance
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
