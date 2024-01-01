c96b6a0a1b707403763e19bb70e0e9af2715b2cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:12:46 +0000

    sell one shade fly smell

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
