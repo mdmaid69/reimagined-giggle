ad8c50b44cc2d1428754037e8883cb381912591b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:07 +0000

    cold bear behind boil plastic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
