9a5e0ff17a7c604f7bfa52227627ee3084f1c738
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:45:47 +0000

    politics regret persuade search imagine

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
