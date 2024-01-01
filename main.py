aa4247b606a479764b5f9fdbd75f7e2c1b198917
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:15 +0000

    foreign comeoff spill smart typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_directory_name(path):
+        return os.path.dirname(path)
