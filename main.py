73c0ed8260f3b98348982361c8eccb4027efa9f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:57:04 +0000

    middle pretend television recently somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
