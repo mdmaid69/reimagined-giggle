42a39a57bb106077fa87f1a9aa459d31f2a3ac53
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:17:04 +0000

    bet sore again spend tonight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
