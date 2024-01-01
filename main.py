8a69fe697e5c0a7ba63b4ac2a950751168512ece
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:32 +0000

    later far heat regard memory

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
