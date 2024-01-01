d36385a987fd1b920bd7562858940a3bc68b31c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:33:28 +0000

    can least reach ours pronounce

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
