27c7223a4afad4f3b8bb1e3e64c6512968ff0e86
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:35 +0000

    want freeze strict fat final

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
