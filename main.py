b0e69bb3be7009e53aa3cde480aacae01c94b7bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:53:12 +0000

    pressure everyone cut correct only

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
