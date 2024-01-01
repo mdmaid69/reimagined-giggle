3ac0513e22c02fca2b20f3ad2fabcac52ec0c0be
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:33 +0000

    hope gate measure memory alone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
