1d500ea94d46c4544fc7fc67b9b4d03b8a18592e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:11 +0000

    arrange deliver friend house deaf

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import getpass
+def get_username():
+        return getpass.getuser()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
