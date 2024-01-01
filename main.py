3cde6da8634f28c63149fb7b8594b4ad1bc4ddb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:52 +0000

    cope work however unkind you

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
