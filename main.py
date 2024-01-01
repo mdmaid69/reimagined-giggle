e00bca23ec201415471719f675643fdcbd93ec61
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:39 +0000

    benefit through thick store separate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
