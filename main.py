137bd0275456598521fb65595362e3b33c204bab
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:49:49 +0000

    tired development hurry attitude refuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
