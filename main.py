4766caf64b351d139f8deb9ce046f0dce49aa65e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:51:38 +0000

    apparently young exist offend level

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
