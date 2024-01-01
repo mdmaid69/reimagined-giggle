5209c16de30f4128de2577836d36876f98902f18
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:56 +0000

    impressive want win past normal

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-import sys
-print(sys.version)
-def reverse_string(s):
-        return s[::-1]
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
