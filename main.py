22e5296d6b8ca90a574dacfea61a24b12593cc15
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:08:07 +0000

    contact except consist mean thin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+def find_min(numbers):
+        return min(numbers)
