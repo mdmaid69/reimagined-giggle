a11d7cbbae74ea34ca451ecc448514a5b53f9e3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:15:03 +0000

    welcome swing team inside under

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
