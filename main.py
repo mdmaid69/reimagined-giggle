cbc87c28bbb457b7a628b32508206f0e272f4b3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:17 +0000

    naturally quickly breakout should plenty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-def calculate_density(mass, volume):
-        return mass / volume
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_power(work, time):
+        return work / time
