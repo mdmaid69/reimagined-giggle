2141de2b897bcfaa3fb6b739acfd152f3a4cf84c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:27 +0000

    well experiment centimetre black include

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def convert_to_hex(n):
+        return hex(n)
