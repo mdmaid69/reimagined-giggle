18c7ecff73a5ed3fa51538c4a549cdf14ce499b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:46 +0000

    chance comment scream frame discussion

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import time
-def get_current_time():
-        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
