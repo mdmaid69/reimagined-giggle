981cbadaddadd081db53176f46e91dcd4308d08d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:37 +0000

    beneath system poor store book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def subtract_numbers(x, y):
-        return x - y
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
