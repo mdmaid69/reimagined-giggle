1d285270b801fa302ab18a702306aa220a40d5e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:42 +0000

    manner manner bank pupil thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
