e0695e7c6b76214730f3875cae12f6251d866a68
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:57:38 +0000

    recommend opinion storm or passenger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import re
+print(re.match("h.*o", "hello world"))
