9649d306e08f46ac34497e9cb7d6ec604c997784
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:48 +0000

    bottom getoff minimum control several

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print(sum(range(10)))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
