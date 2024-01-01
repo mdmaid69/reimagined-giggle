5a160354338248f9e6a3a5e495d9bafef54d0d2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:29:55 +0000

    again near regional everybody question

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
