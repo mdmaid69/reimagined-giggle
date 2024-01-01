34d100d185369380b81d140d3d6e25ad1b705360
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:56 +0000

    exchange thanks land winter our

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
