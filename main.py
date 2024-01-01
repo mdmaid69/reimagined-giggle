4401b133b35ce5dfcd81118342807a2baa82d7c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:11 +0000

    journey lonely scared grateful point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
