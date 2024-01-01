0bf7601766bb3444f2cb418e99c4d6271bf017d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:00:22 +0000

    them comment although exactly Jj

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
