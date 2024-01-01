e0cf0a077d585fe18061626643040a62d9824bc4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:47:41 +0000

    plant once nice aware comment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
