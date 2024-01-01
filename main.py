42884173322e7389bbb3dc24f9eccb6efeafe5b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:20 +0000

    test person suppose summer them

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
