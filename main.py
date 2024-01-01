ef4fe7280b863a74c68fa4d3858941cce6648069
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:45 +0000

    getoff deliberate discover forget behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
