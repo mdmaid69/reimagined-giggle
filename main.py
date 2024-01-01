7459fe8b6fa229f57fe10952010a02f797416b56
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:39:13 +0000

    grade sea takeback considering region

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
