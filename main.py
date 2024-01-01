cf511a4a7732c2def579757e020466567e601237
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:33:42 +0000

    understand well score apply last

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+import re
+print(re.match("h.*o", "hello world"))
