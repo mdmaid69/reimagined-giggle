561bccf86e8b1df0893d18033fc77602e25d3b93
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:01 +0000

    common formal uncertain best last

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def sort_list(lst):
+        return sorted(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
