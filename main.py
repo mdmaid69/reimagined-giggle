8fc104946db689e065bb091ccbb90b198b1b1be5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:57 +0000

    window ball present simple correct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
