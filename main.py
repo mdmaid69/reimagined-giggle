c3308f752b28bbf667096758feb8aedcb13026a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:04:31 +0000

    cruel chance eye noone problem

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
