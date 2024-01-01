9f346b5e2290b5ec9eb0515cba5219c9072c3d54
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:11:58 +0000

    sing little reduce what accidentally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def add_numbers(a, b):
+        return a + b
