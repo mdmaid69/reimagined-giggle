b09b3875d85eced9a865df9bc405871b3077a822
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:06:17 +0000

    satisfied rare stop possible tall

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
+def calculate_work(force, distance):
+        return force * distance
