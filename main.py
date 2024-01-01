1cf1556e74016eef45b1d3d20ff177c826621019
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:33:45 +0000

    weak trip argue tomorrow breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
