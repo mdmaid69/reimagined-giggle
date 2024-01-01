28d5904724bccdd73b9fc0e0d3dff558acc27bbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:58:22 +0000

    roof still shout none sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_min(numbers):
+        return min(numbers)
