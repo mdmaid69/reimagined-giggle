4360cf0929fc6022cf58a81f2fb9f8c76d5d3bb0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:58 +0000

    strength shut pinch that temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
