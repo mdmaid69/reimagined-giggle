ebf250ec9c71154bc3740a41f1ba8b0b0175bc13
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:00:29 +0000

    holdout point important problem bottom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
