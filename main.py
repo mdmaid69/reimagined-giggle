34d989643d65bd91efc61ebca9f6c5710126f679
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:32 +0000

    careful expensive almost attractive tie

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+def find_min(numbers):
+        return min(numbers)
