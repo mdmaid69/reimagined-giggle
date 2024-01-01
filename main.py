ac406d48519b3e4f598603cc05797ca06385bb75
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:00 +0000

    talk willing accident lock on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def calculate_speed(distance, time):
-        return distance / time
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
