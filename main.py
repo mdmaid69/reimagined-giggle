0cd17fe3ea80b93ed39f7bafc78fc40a09be56dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:09:38 +0000

    smile floor object recent later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+text = "Hello, world!"
+print("Words:", len(text.split()))
