98e16fb1993a11e9e416390d8218959c7e2ddb5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:18:33 +0000

    avoid hers glad law certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
