a8cf1e5a56226a9984adbebb311ec71b3a7317db
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:13:11 +0000

    him speak further lay release

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def find_min(lst):
+        return min(lst)
