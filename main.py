fcda5cd13daa57cfa01b956257ed15050f57c351
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:07 +0000

    chance message test table favourite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
