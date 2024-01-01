9a56643363bba2ae7fd2c06500e601a545091cfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:03:09 +0000

    contents breakthrough telephone number value

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
