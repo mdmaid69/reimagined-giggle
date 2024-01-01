35641cd4ccb0bb3f2a41a8f93e375d79c7c1bade
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:31:20 +0000

    tread same effort many giveaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
