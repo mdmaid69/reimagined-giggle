0b1c9f5db08be3b55520a5109cd9ba76c4451e00
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:20 +0000

    square distance petrol finally object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import time
-def get_time_since_epoch():
-        return time.time()
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
