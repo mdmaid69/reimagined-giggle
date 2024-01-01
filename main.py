434dae46e20fb6553a47494773e7693a9a9b1d50
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:08:59 +0000

    Qq near peaceful spill back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
