8e77d5abc70c4e7ce9626bda3a0b9d743792a205
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:21 +0000

    term doubt live lead section

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
