e14f82b341d9f36197f0d3595b006d4a3b01949b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:00:07 +0000

    use weak emphasize moment comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
