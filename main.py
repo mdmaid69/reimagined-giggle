98911e4d91634e0e559b0648b16d461350fca09a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:34:16 +0000

    vision positive watch throw recommend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
