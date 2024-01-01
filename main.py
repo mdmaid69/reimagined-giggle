d0e1e9950b1963592c1aa31111b052a04cfe96a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:16:36 +0000

    worth us whoever certain offend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
