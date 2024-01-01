89a6d56782959d8bcde84c3e494e652dbbe8aa8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:55 +0000

    loud piece talkinto repair then

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def add_numbers(x, y):
+        return x + y
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
