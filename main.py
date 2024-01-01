9be63db8f21eb758669558d5d4af5f97fc1b97c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:37:55 +0000

    scrap blame bread punish level

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
