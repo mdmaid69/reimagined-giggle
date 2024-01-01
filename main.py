3734138360072ca73fb2f1d31da7be5241bcdf17
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:55:24 +0000

    takeback share use her body

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
