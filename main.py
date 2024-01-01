4d177cba8005d03590a322879257edd4134603bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:49 +0000

    motion every live guess trick

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
