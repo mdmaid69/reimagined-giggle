a49c497f80a32754b43a5a760c7ee4195856115b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:31:04 +0000

    explanation spoil respect now television

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
