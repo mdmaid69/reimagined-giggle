0f54b78bc77626cd9a1ffa07aa91bc9cd0e2666f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:50 +0000

    since tall life exploration interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import os
+def change_working_directory(path):
+        os.chdir(path)
