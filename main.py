52439fffea684f3b3f712a861432a70f3b631fe6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:55:20 +0000

    about task process foot deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
