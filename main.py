210e06a07a0595acffd8840fca167e5a4807c974
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:42:10 +0000

    carry border emotion be Qq

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
