65c49ebf23d022d7539aed5e35d37bc0743dd366
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:27 +0000

    continue weigh cross walk chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
