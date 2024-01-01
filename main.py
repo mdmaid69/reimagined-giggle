f9535defa692a76dddcb5c3ce08381ce93d63b80
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:23:45 +0000

    seat bar frightened sure wing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
