00eee4fdbd3368ab62b2d9154ed2ce413b9adb1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:30 +0000

    refer since disappear wind pronunciation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
