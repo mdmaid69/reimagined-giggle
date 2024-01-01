0cd50e16123cf70df0c7e8a4d8d9d05629aab21c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:28 +0000

    tomorrow silent independent rest Pp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
