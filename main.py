dbaca4ae0c48e8776d5a1631d3ea9a6ab9543aba
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:51:02 +0000

    police degree acceptable visit climb

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def get_array_as_str(array):
+        return str(array)
+def find_max(lst):
+        return max(lst)
