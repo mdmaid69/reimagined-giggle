79121b945b297785625de9a37760cbac716dabf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:48 +0000

    defeat quiet wash ahead care

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
