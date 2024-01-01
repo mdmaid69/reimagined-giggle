28d1487b96af2432edf856459d619051ebacb09c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:03 +0000

    thinkof work after phone excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import sys
+def exit_program():
+        sys.exit()
