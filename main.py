5efbca52009f34eda80b1ec4334fb0d1b1052c95
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:41 +0000

    agree plate today cause field

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
