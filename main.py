022c88417945069e3a9b1f1bb138e1b8a7237fd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:51 +0000

    instruction final skin report add

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
