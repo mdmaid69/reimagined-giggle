12135230bbad8668fa7d35938a41ec88c8623c15
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:46 +0000

    stare reach pretty increase give

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
