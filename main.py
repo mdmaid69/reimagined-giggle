47a1170488598ae75c1b2b498277b4b745dd5f41
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:26 +0000

    search piece turninto cheat cut

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
