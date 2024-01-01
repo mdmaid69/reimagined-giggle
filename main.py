630881c5127dec573542a8342e5b066c7fee8606
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:51:42 +0000

    nurse damp learn mad get

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
