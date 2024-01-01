2a0960fd773ebe2c520a3d26079a0460fde6e492
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:11 +0000

    chin pump comeup estimate excited

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
