3832de81af300d8fb698fbf295068b9119374295
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:07 +0000

    suit container outside now image

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def is_even(n):
+        return n % 2 == 0
+  def convert_to_hex(n):
+        return hex(n)
