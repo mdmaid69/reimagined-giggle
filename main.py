15b6ca4bf6c0044a00294ab1b1d8793a1dbf3041
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:24:52 +0000

    pain appeal although unlikely disappear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
