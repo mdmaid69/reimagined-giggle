1d04c987ffe6909a072e74d119012b86c5c0834c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:45 +0000

    cap risk thinkover activity glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
