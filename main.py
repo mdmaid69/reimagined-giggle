ebcf3aa803508b53772224528e075b0bdfcaeb79
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:22:53 +0000

    mood formal create tradition earth

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_str(array):
+        return str(array)
