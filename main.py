ef4363fcf162bd755cd7e3e77ae2b91b746db438
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:59 +0000

    visit obtain ticket quiet circumstance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def calculate_acceleration(speed, time):
-        return speed / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
