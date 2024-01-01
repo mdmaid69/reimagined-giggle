8597cab4c86d8d57bbd31463edb2ac2c46a12678
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:23:11 +0000

    cook worried anything message estimate

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def convert_to_binary(n):
-        return bin(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
