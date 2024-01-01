223f02fe96c3d795d42c6f0670ae80682e1a3f75
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:02 +0000

    play everywhere press yes nurse

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+for i in range(5):
+        print(i)
