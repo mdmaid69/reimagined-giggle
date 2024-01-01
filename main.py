51516721c008a815c084e9334125ca19c57357b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:40:11 +0000

    wet business by tread wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
