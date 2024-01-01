dd0a4214490793364b83c725840da63b249e5adf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:01:35 +0000

    communication bored opponent word worse

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def reverse_string(s):
+        return s[::-1]
