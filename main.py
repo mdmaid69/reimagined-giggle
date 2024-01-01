147c02661cbc8a4459368638e9b40208503570a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:44 +0000

    pronunciation tight shock bother comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  def square_number(x):
-        return x**2
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
