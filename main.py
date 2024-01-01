d6c83b732bff91fecf9d71d74748aa13e7bdb8f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:12 +0000

    them skirt stick meet wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def is_even(n):
+        return n % 2 == 0
