0774ae9db81799c62612e93c430c1145d838f657
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:01:55 +0000

    fact country cut inform Ii

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import re
-print(re.match("h.*o", "hello world"))
+print(sum(range(10)))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
