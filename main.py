49efc75a49a6840f58c997094945ffd30673e863
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:22 +0000

    challenge enthusiastic father bang deliver

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
