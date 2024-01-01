7af59d3ec11363b2bf2c6ba446ad67ee613df112
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:25:13 +0000

    loss near importance seem ring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
