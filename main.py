9bc10454838588503045a07fe2bfbd6fb73e2c3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:29:23 +0000

    last free invitation rate hit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_str(array):
+        return str(array)
