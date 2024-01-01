c5d53c59f4ecc1e15ebeaaf929fba8382be0ea89
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:41 +0000

    profit occasional instrument waste bottom

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
