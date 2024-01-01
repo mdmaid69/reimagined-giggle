3c0b734bbe636eeb78dc6927f856cdfa08b9b475
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:38:26 +0000

    amount thought mine bit copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-from collections import Counter
-print(Counter("hello world"))
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
