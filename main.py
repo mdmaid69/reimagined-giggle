a1d37d9532209a81fb1c456aa73efe0e09111c44
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:19:49 +0000

    reach wander getout needle begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
