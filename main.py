06d97123f4219035f28a751ad7077d2ac4ff0dc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:13:32 +0000

    mean 567enemy excited plenty convincing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
