9f98332bb2fd046fdf2f8dc90b19dc29c6758b03
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:42:03 +0000

    seat declare work measure people

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def reverse_string(s):
+        return s[::-1]
