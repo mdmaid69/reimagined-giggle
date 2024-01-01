a360a5a9dc8773741176296fcacdab4b0f2a35ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:01:54 +0000

    treat write snow temperature refer

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print("Hello, world!")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
