bd7d2b89e1ccd1df9a249b8114bbaefbfed67ab7
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:55:54 +0000

    all paper young sale rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
