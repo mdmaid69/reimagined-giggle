273706dbdc5bb90c49b4e780c183a41d5ea48e8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:17:59 +0000

    practice stranger honest no favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
