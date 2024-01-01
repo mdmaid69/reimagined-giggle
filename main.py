8e53aba89c0a93eb9e0a199a4cfd6e5ce4bd1c0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:33:30 +0000

    word lookaround attractive disgusting clearly

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
