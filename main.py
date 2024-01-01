f7b28e98904fee71df4fb51f107d62a1c74fb99c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:20 +0000

    interruption godown label fight feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
