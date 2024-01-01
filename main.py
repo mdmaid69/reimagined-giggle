6cbfcf0516c34d84cd8ef1441ef9e6e5c1857afc
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:37:14 +0000

    also safety rob through consist

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
