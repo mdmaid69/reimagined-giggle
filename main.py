18edc7fa10766da1d6e9ecc721b93d594dca670e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:26 +0000

    Cc each neither start join

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def subtract_numbers(x, y):
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+  def subtract_numbers(x, y):
         return x - y
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
