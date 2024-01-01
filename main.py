eb0670b3d82c3b55cb307d6b30503f76461531c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:28:46 +0000

    bringup fork hate use art

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import os
+def change_working_directory(path):
+        os.chdir(path)
