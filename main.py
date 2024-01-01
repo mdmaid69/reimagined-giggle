99091be06502067047cd7cfb58bd98e382d3c60d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:23 +0000

    holiday float keepout together various

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
