418244b96c4416ebaab453f3dae037511b6f022a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:15:48 +0000

    trouble 569enjoy I public certain

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import sys
-print(sys.version)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
