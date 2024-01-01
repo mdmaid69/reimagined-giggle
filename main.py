4c7756ad6ee4b5c82557cde2ee37963e76546cbf
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:08 +0000

    breakdown contrast coat win contain

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+text = "Hello, world!"
+print("Words:", len(text.split()))
