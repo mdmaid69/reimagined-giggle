b7fd2e1cceecfe6b72d5c7daf33c50ac5401469a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:14:38 +0000

    only back to soon control

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
