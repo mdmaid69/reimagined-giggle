24d1dbf1ea6ff7e5e9164027ec657bd6d893a4e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:21 +0000

    night pool admit everything set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
