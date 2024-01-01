0a6a179fcbdd5c165539a4deee84cf47155f2747
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:22 +0000

    swim wing no head boat

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_volume(length, width, height):
+        return length * width * height
