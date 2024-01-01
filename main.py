7a3755fbee618126d4561d7893ca635f6320b37b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:49:38 +0000

    567enemy middle load company cheek

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
