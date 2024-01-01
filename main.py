ff7f555298a7bebb48a279d6536c9c1bf3675859
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:37:41 +0000

    politics sweet Tt criticism very

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_hex(n):
+        return hex(n)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
