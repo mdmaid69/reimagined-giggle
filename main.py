5d7f685415ad77d6fed8fb9012b4e07a4dbd6e7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:48:49 +0000

    affect keepout warning complete along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def is_even(n):
+        return n % 2 == 0
