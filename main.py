35eaa85fd9a15fc13fd6331a092440c9fcd010ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:34:04 +0000

    least reply goup few worse

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-print(math.pi)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_slice(array, i, j):
+        return array[i:j]
