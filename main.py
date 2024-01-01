005297bf01a693112711a364f24374f58fc1917a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:56:30 +0000

    obtain terrible understand nigga surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
