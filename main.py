1dc60bfcbc9f8ab2a5cb7fb8bce0d061bfd04313
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:18:05 +0000

    before whistle weather broad bottom

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
