e7dd467936e36ac3df54d5e889173e6c66a435ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:22 +0000

    short later rain soon lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+def sort_list(lst):
+        return sorted(lst)
