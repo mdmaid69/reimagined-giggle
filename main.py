0840cc2c80be42241eea7bbc2c7741012aadb986
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:38:16 +0000

    appearance comeup tool means heavy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
