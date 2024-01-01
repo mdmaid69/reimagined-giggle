3ace0eb5fcc13fe576949b55a626fdd282400510
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:10 +0000

    night wide child order use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
