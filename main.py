87cd6e622adc394181b9b2c7a8bdf3168d59474d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:52:05 +0000

    goout goon trousers measure better

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
