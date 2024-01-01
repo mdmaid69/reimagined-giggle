6e886c39c2e8086caedda2aae091c93da4f815f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:29 +0000

    aware polite getup organization skirt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
