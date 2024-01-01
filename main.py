4df731a5f912eee6d916e3d82a602ba91a4a4ac6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:17 +0000

    wealth community flow notice wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
