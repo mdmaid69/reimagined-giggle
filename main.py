cff08af8c84db88e0bb3469fdca25aa3c412256b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:34 +0000

    national receive fortunate of wet

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
