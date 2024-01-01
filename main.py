34db932bc96a4dfae71fa136a44a2801e319a46d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:39:09 +0000

    move level environment believe mark

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
