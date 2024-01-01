30031820233659b92407a9c29971d635c700b6df
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:29:12 +0000

    watch bit soft past underneath

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
