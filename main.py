8797b98d228bfbffd53675134eb19313f7aead2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:17 +0000

    pretty Mm occur way emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import re
+print(re.match("h.*o", "hello world"))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
