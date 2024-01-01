50cd47e8f32b50a936c7dfe77c8692215349ddc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:31 +0000

    pick choose message appeal gooff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
