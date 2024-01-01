d8e262d52cd4be70d9ccf15beac6a1e67f909652
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:38 +0000

    call object artificial calm trust

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
