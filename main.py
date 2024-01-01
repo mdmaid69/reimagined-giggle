a98c929d18a0048ec0287bd9c8fafc11f2ad20da
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:41 +0000

    occur speed keepup bring call

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
