7f0d287f760918618a3a29584fa2ca0eda573591
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:22 +0000

    flood task copy juice row

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
