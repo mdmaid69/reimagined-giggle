f29e7c22053358f4743367f6ee21cd6aae298e6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:58:38 +0000

    tidy getoff unable Zz boring

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_set(array):
+        return set(array)
