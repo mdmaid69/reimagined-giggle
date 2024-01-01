149818f48d9a7afd98ae177fb0ed4a9d1b522cbe
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:21:47 +0000

    eye part treat indeed guard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def square_number(x):
-        return x**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
