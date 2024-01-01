844c4d2ef87221476a9f79c08d65c8539a843d1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:39:59 +0000

    573entertainment offer clue early tip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+print("Hello, world!")
