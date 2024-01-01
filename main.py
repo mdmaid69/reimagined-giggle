be191468b510daeb9503133ae9b34889f32857f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:05 +0000

    however size final fit trap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import math
 def calculate_logarithm_of_gamma_function(x):
         return math.lgamma(x)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
