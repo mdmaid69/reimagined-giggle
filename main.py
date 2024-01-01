5fd5e2efce2f02e83fe7236c2263412a2e3ac9a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:42 +0000

    petrol circle health arrival catch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_characters(sentence):
+        return len(sentence)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
