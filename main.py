0c3af9f20bc57f9f076568e9215edd333c60c60c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:49:29 +0000

    beside bury calmly disturbing rough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def count_characters(sentence):
+        return len(sentence)
