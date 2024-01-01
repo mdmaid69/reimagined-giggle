60cc88320e1a0d4859bb4156722454a26d9203a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:49 +0000

    behaviour belief bright affect swimming

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
