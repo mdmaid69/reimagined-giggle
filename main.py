fc712ed862efec086ce46e6f05dc11bd9016a18f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:13:51 +0000

    action draw word complete trial

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_bool(array):
+        return bool(array)
