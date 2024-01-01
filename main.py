89670714d99db2a37d7f270f20c67de87224d9c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:02:33 +0000

    finish total unit team 571enough

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
