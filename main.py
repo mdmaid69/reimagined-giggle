71aa7a52c807e78a960c80a307edb7d16c48caa5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:44:21 +0000

    strict sometimes poor desperate yours

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
