35f6b898c7156778b1592257704dfdf82547e3c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:10 +0000

    method search compare thing same

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import datetime
+def get_current_date():
+        return datetime.date.today()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
