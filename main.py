1929ffda8754d6f9b81a9bc2370820ad9afda012
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:56 +0000

    whatever appearance careless indeed laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
