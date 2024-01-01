ff09a2ccb95549e8e633938be3c8b2d3cf728210
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:06 +0000

    team base obtain train sometimes

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def find_min(lst):
+        return min(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
