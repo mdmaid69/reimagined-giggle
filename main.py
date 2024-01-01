4b32654509a1ba3874bc68fc6133b60c779b0d81
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:34:46 +0000

    section breakfast hope further advise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
