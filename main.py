4b42c9052faff1c77b786ea4b8ca0ac0dbdb8882
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:39:13 +0000

    person closed freedom thick ought

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
