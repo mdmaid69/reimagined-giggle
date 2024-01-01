dde7c3df51d69d204e8031ad7defa3d67333df58
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:59:39 +0000

    home surprised car supply basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
