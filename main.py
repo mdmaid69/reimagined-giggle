e0d28bd04d3171fe36315edf67185b200ec0b43e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:58 +0000

    scream radio during display yes

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-print("Hello, world!")
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
