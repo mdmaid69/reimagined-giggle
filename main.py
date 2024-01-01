7f4e364e5cb8ce5b705c541f03a95a0bc61a443a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:51:57 +0000

    over near mention 564encourage concerned

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
