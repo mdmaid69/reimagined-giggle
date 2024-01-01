9b70a4d02378ea247cb6a69f166a144e228216fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:21 +0000

    draw such turn people phone

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
