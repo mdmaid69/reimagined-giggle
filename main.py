4b9e4b5a383c5dc7617828eb286f0f25157ff058
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:12:42 +0000

    radio meal contact someone hurry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
