316113b024e8224eaba3fd81f0601c911bfd9d82
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:05 +0000

    birth thinkthrough relationship plant mad

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
