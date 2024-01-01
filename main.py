094100f12ee76254f184a8493ed1cef83e428072
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:29 +0000

    rough regard authority hurry middle

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
