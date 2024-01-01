53972db76ad36b195199fc71e2e83adddbb42daa
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:12 +0000

    wealth return thin nice birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import tensorflow as tf
+print(tf.__version__)
