f211ca6c3556543e1fda6f850692abc4537f938f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:33:19 +0000

    bother ground other leave dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-def convert_to_binary(n):
-        return bin(n)
+def get_array_as_complex(array):
+        return complex(array[0])
