43c16b40cd4c7544fdc22a9c13eba91504f5521b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:33 +0000

    describe busy human any steam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
