eee762376bd301c077b464c95932c8ab788907bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:44:00 +0000

    bit doubt us when commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
