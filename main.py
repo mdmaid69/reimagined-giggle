24de0fd133b64988374228ee633c41ea7f55596e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:05:14 +0000

    plate person thing bit intelligent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
