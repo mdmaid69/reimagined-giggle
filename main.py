196595f3d76d6eba240d1870b3b9618f628dfc9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:40:36 +0000

    pressure murder whistle respect able

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
