a5765dfdc3f7722a13cf04875adaeff6db33751d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:29 +0000

    should easy job fast choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
