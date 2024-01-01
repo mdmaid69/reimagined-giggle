063e0a808947ed0eafdf7fb4a25da14b09cdcd81
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:54:03 +0000

    each provide escape thing return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import logging
+def log_message(message):
+        logging.info(message)
