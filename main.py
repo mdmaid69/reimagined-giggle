be1c4fb25d26b38bb2653feef883aef41b69610d
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:03:01 +0000

    Oo whom late spite tap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
