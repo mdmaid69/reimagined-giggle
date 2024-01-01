7ec49012542b7e762a1bef9bad930d4a4808fd5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:26:51 +0000

    kind much basis otherwise 572enter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
