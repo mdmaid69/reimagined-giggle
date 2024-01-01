f4fa0eb2e2d5bb2b3e901ef61f4b667c0f4e7ed5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:24:40 +0000

    release lookaround ought worse case

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
