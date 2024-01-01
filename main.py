563f0abdb041869b1b15f2040ce1762ecf910ceb
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:51 +0000

    guard clearly emphasize likely if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
