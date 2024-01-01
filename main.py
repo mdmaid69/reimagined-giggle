ea780f9c52dafcf5c388a29ee4b32e9f450f1fbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:31:41 +0000

    later detail month behave taste

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_current_working_directory():
+        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
