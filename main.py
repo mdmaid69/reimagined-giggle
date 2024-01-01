6694fee6f1adfed7f4a02d2465826c7a1b7e21c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:03:14 +0000

    price cold knowledge giveback check

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
