302359e7bf42bc32da9498489d878ea096191f5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:20 +0000

    shelter petrol lead gas refuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
