319a68048cd81544287bf5803d1f2672988fb4ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:20 +0000

    ahead statement main embarrassed project

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
