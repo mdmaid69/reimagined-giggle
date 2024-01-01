80c2562f89c98fb6a5c8607ce17f09888286d30c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:57 +0000

    tight line able almost pin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
