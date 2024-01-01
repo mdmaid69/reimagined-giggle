6498fe35f2c98978c868bde51a82ed757a6ed970
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:02 +0000

    belong improvement convinced classroom disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def is_odd(n):
+        return n % 2 != 0
