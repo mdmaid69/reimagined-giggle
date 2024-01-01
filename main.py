8fa46aa00800ff3991167a90f00aec77759d3c08
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:39 +0000

    collect order aim century every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
