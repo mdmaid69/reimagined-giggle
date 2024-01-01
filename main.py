d32205eb6ecea4323c469dac2de817104d73d747
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:01:45 +0000

    outside geton child cost powerful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
