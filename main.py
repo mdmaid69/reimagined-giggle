93db9fb9c67bf2bf23c8132e41dd377e0b6c0bac
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:44 +0000

    congratulate hair project thought below

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
