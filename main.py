21eeed538969122b9fa80936019f822345827c73
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:02 +0000

    alive confuse bringforward freeze fond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
