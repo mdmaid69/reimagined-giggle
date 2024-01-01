a3c45a0f334bc0665f4dffa8e93959dcb6c2d01d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:07:43 +0000

    autumn otherwise region give doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-print(datetime.datetime.now())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def find_max(lst):
+        return max(lst)
