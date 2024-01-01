dddd276b0b051b041d49c5097a359b82c4d22e4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:47:27 +0000

    tear pattern window show begin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def is_palindrome(s):
+        return s == s[::-1]
+def find_min(numbers):
+        return min(numbers)
