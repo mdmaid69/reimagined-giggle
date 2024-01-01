1c32b2c0335e86fa5648369a8b1654d6288527d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:30:05 +0000

    worth ahead cheat school interfere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
