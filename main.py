797907968f91af279f47652144d61f3b5f32c466
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:08 +0000

    thinkof 568energy side bother truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import sys
+def print_python_version():
+        print(sys.version)
