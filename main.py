a2563c916c3b0c034639f8641e3b2cb66fa164cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:30:18 +0000

    below getup least lucky area

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_floor(x):
+        return math.floor(x)
