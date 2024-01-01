058bad89ce1dea48851fa9a6a0b9549e0fc169a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:13:53 +0000

    thank shade figure giveout better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import datetime
+print(datetime.datetime.now())
