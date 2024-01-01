b3c544e5cdfbe48f11cbb154fda1f813fdbd5ef5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:33:09 +0000

    performance easy dry by worse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def add_numbers(a, b):
+        return a + b
+import os
+print(os.getcwd())
