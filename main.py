acc293be965fb613fe48d83e24e346d6b39e330d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:53 +0000

    discuss suppose board opposite pause

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def divide_numbers(x, y):
-        return x / y
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
