875afb145f5ea91926cd9fba1184f23f44f4e466
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:51:56 +0000

    post definite story finish basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
