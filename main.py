1f21bd528ced9e12f33a951e18d31970ad6f5f46
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:33 +0000

    loose Ee moon train sale

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-import os
-print(os.getcwd())
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
