83504747a6c4236ed92946faa3873ff9e547e9bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:21:05 +0000

    early class professional anyone neither

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import time
+def get_current_time():
+        return time.time()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
