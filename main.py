62770012b4b4022f3f4e58497345122166c7aa97
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:59:52 +0000

    body Gg stamp tidy respect

--- main.py
+++ main.py
@@ -1,14 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import array
+def clear_array(array):
+        array *= 0
