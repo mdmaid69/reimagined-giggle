5d77c1a7dc74c5bff042cb0c3b7e14661d8126f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:55 +0000

    belief comeout Pp slice hot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
