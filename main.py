303c8f0f86d7b04944d77c5675d700d6b8165268
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:16:32 +0000

    definite stare guess ahead suffer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
