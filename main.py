f8bf92ac464c40cbe68b6a84d5ddd21672b8b7fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:27 +0000

    frightened tour tap keepoff mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
