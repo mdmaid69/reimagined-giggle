e37dbb7ca0230c2a1111cc57b7e6a2c4e87a47c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:57:38 +0000

    instrument bringup rough to centimetre

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
