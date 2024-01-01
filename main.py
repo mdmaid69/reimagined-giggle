6881b8ae9e6b2f2271430e5bdb27659fe78ddcdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:28 +0000

    settle boat drive until circle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
