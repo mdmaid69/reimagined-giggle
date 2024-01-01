549f37e8e49b107e3dfea6b2bbb6990026ca6429
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:05:03 +0000

    laugh surprise advise before contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
