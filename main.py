ac83361b47846e2f1d29547a49a4ce32519f819f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:47 +0000

    age all downstairs last ignore

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
