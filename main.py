d8ce402e9a596993874232f558af3b8632f952ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:58 +0000

    power breakfast mention smooth ball

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-print(sum(range(10)))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
