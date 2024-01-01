8aa4466538ff03ebef71474451f01953fbaf78ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:23:36 +0000

    grow fold callup contribute influence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
