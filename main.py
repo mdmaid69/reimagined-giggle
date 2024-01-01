e7e2e537b54de1858aa4cfc7e33156b73daee575
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:39:48 +0000

    she getback complaint grow opportunity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-for i in range(5):
-        print(i)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
