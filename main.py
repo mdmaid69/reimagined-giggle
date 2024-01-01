8141ed270559930e81023a616ab03be53f9f896f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:10 +0000

    expression least tap announce form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import array
+def get_array_as_str(array):
+        return str(array)
