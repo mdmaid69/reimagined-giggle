2b0312afba8f0981256da23bd3947b7a239f2c9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:58 +0000

    error down flat ocean port

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
