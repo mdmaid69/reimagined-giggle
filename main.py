dab84695384b8f9a7b7b64ccf1dde937419e5f3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:09 +0000

    right giveaway university error relative

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
