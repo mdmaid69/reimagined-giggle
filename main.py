6355bf269c86e33d109a0f727d427657db6f8211
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:26 +0000

    reject Jj clearly tragedy Jj

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
