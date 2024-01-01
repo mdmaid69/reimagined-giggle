0c1a427d7fca7c3d81fb7f23c3ad34bf471c6af9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:31:12 +0000

    hard bringup likely wide address

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import os
+def remove_directory(path):
+        os.rmdir(path)
+def cube_number(x):
+        return x**3
