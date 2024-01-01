2515109751949cb9436765ad186f2ea667f89e76
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:49:43 +0000

    takeoff high bread tongue compete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import numpy as np
+print(np.array([1, 2, 3]))
