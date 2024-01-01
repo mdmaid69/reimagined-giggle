5e0c10998520286847bea0f5fdebfbb83a820ca2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:10 +0000

    remember without impression fast shallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
