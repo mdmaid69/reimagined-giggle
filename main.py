82fcd2cd82b57bb6402c391fc84217c60b5fd8b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:53 +0000

    main school regret even bill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def append_to_array(array, item):
-        array.append(item)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
