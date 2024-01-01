7c4f2309fcd7f9607537859b231a3cb8b91a9d22
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:55 +0000

    theirs trade suit lift paper

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import array
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
