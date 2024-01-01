c1b22bdc879fa59f47b5989877a7716ee0510a3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:12:02 +0000

    secret taste deal underneath best

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
