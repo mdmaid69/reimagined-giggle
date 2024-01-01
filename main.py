5abfea404c7528f28e7f3d71e72a8de3039a583e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:09:06 +0000

    cry spring earn central lot

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+        return os.path.getsize(file_name)
