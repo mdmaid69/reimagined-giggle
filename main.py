ecbf9e01c53b67c2935473f646a04930ed0fd242
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:54 +0000

    coffee violence attention Ee case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_current_working_directory():
+        return os.getcwd()
