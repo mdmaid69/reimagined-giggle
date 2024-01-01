64160f1df942dff0599b5c960e37a5f9d59631de
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:37 +0000

    idea willing giveout run else

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
