f1aacdb1268809bde6adf68836f8b67db26a19e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:36 +0000

    let write cut bite single

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  def add_numbers(x, y):
-        return x + y
+def add_numbers(a, b):
+        return a + b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
