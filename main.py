cca59a087f586c01fb9047975e40492c7fdbbd70
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:22:02 +0000

    shadow sort fine guess similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
