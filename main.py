84946f0b1bd0d55e6677c97d5f05ae54606f7912
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:51:47 +0000

    closed something shoot personal nature

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
