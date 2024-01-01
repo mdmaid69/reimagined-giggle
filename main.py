b311aa95a531c087988e1cbb47b0ad738d336b7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:51 +0000

    no search disturb down impossible

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
