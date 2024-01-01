8b7cc48a0a049d11ba7b1ba8bea5456afeb1a594
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:09 +0000

    life office spite progress payment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_words(sentence):
+        return len(sentence.split())
