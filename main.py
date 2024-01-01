84d4d1b26f5cef4caa6719a3c4cc94e219fe0177
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:04:42 +0000

    suddenly remember bring getback hold

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
