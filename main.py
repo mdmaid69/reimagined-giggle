4653418a38272c6ff643ec238df32779d71b3e85
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:48:18 +0000

    noisy someone earn our always

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
