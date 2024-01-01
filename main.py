b6f3643cccfad1309ba6bd807aac67caac71d68a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:10:36 +0000

    Jj hold somebody problem order

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
