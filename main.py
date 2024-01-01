69b22cc6fc998250cafef62624d03d512f3ec959
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:42 +0000

    honest violent smoke blow contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
