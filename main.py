3391aba8fe5ee751a82016e7c101da94de076728
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:32 +0000

    accordingto takeback shame basic connected

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
