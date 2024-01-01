4f248886ee09184800fa9661259744924eb79c1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:35:35 +0000

    strict contrast star law think

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-for i in range(10): print(i)
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
