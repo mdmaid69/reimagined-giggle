d2fb9b8bbad162311b0f8503fa4e3908cb50d062
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:04:42 +0000

    best from delicate lower kind

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
