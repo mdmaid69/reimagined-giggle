fa80b0c1b0225d3547422c6bdf0f59d82cdeb19a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:42:09 +0000

    excuse region reaction correction bright

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
