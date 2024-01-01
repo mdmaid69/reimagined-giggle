7c87655f44d20be7beaf1d2c486153ebbf5bd4c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:25:39 +0000

    be exercise event frequent switch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
