d41a67423576af15e3f3956c270b126bc631e51b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:11:24 +0000

    fill other team busy have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
