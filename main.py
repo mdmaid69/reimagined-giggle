8d6291cd0651d836ab54321523c99933f805bd3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:10:27 +0000

    chat off power clean whatever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
