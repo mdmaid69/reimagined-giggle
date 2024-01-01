da20d46b437a09852d3ee67aaa3d4d4350d632a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:10:57 +0000

    underneath backwards confuse lucky pencil

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 text = "Hello, world!"
-print("Reversed:", text[::-1])
+print("Words:", len(text.split()))
