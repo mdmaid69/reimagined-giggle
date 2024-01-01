6db16b04001642636eb7537ef9946b162fd5213e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:39:26 +0000

    condition regularly piece pocket unless

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
