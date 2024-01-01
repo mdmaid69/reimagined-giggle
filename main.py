09be70096ac45e67e6814b8b89a7c45a7a113647
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:08 +0000

    goout bottom winter hardly emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_length(array):
+        return len(array)
