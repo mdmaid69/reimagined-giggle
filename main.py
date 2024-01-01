d1a025af27e0f1f77ffe276dce4d5b33efbcd9cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:55 +0000

    contrast downstairs somehow pump getout

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
