da7f7aa631a31f122e247a256f38f4bd6f87480a
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:48 +0000

    unpleasant collect picture minimum walk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
