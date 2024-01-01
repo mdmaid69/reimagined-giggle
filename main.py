51f681b8eea06383daeba65ea1cd90adddb4f8e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:22:51 +0000

    show earth top possible knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
