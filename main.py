11f54b33137382e59d9432754692f941c64280b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:05 +0000

    press write 568energy sort professional

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
