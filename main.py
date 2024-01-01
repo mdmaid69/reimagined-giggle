c3ba94c366465101b19c7a3c3f2b872ed7132858
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:02 +0000

    by fall turndown makeinto border

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
