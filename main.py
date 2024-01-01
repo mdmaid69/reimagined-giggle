c3b73648270bee1bd1409539df899f569a2c7966
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:54:36 +0000

    score moment prepare shut steep

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+name = "Python"
+print("Hello,", name)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
