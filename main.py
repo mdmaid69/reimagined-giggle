c64cf052b71c90324d6d38ae30f24f0f79ef9bf5
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:05:47 +0000

    suckme become where exercise strong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
