38e7133344be889ba95c7c6f78d9aaf40cbdb624
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:29:39 +0000

    among official field away everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
