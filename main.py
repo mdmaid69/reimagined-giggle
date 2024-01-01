c07adafd39960054c7ac9e4133ceb9a640640c7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:33:51 +0000

    plain stretch persuade thick change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_octal(n):
+        return oct(n)
