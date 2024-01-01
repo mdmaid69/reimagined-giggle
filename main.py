46d17cee492b7d8ee6edcb7edae68272e6fd6b86
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:19:50 +0000

    if hole hardly compete 566end

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def subtract_numbers(x, y):
-        return x - y
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
