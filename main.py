a7a3b58df624c3ec8f9d3436487e5a8c78d5ff54
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:41 +0000

    takeout treat wire clean and

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
