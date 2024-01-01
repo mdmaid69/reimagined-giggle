a9035aa1a554edec94862a1379909a84269dcaf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:26 +0000

    off without switch protect join

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_list_from_array(array):
+        return array.tolist()
