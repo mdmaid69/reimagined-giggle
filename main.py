cba4addc7b9e712909a5a17bbd2b40519919e7c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:55 +0000

    anxious minute sink release attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
