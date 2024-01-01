6980e38319cc2721cffd088d6c0bf05a4d452104
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:33:21 +0000

    city today act wind ocean

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
