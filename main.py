9c9c01953f2248b50e1705b0b5c0292e60e512d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:30:35 +0000

    emotion comeout OKorokay stage attitude

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
