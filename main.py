4980bbd589dbe0611b5a1d46adeed4142b12404c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:48:41 +0000

    best immediately ask limit practice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
