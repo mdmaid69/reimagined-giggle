9394c017c386904accaa704001682023c51cad83
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:12:44 +0000

    break food sweep watch surprising

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
