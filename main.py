e53e529fff76b93a26fb8dfef6504f66a71f1b09
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:08 +0000

    do comfortable state race rather

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
