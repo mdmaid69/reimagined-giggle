3a3a6cfd06d3b69a7d0023c4585fe81351c8f904
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:26:14 +0000

    566end need should lookafter exaggerate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
