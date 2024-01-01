b4d129e16b9d69fde05f1fa13e8b9359dba00f9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:13 +0000

    mess offend direct scale fail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_current_directory():
+        return os.getcwd()
