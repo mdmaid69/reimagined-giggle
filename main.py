0efe5791b2c3fa154854ec0f0a669abebe211db2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:31:06 +0000

    chat comeback takeover support call

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_acceleration(speed, time):
-        return speed / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
