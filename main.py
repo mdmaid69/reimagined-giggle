63ceee5b57ed855549e24a107d8026adca2985a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:41:51 +0000

    forgive trade slide uponon wild

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
