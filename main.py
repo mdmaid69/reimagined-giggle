8ec8cdb5d654f3b282ceae35448a147c5e57f790
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:12:29 +0000

    yours OKorokay actually godown unable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
