2fab647a7dca477c81d5589852c0f01d7a11f4ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:07 +0000

    scale thorough copy bet label

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(10): print(i)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
