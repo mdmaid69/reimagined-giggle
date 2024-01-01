6dfeeff2f04b082e56b9e79e48bf7529211a9f0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:26:53 +0000

    pay takeback market steal keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def count_elements(lst):
-        return len(lst)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def square_number(x):
+        return x**2
