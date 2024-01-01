5a1dcc5635035850ae7ea915b4d37046de028a3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:04:50 +0000

    bridge hot freedom runout press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
