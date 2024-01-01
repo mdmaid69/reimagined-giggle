940e170c83613a80411650bda05a56cda7167dc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:14 +0000

    search stretch frighten disappointed out

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
