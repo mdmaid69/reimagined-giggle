45a6d79ba5c61bb7413486b0c57f8702d7036295
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:54 +0000

    content description workout operation previous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
