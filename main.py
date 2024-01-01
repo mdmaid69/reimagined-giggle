937a1adba958843b53102a09d6ac8cc68057bac9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:44:13 +0000

    thanks tour also long tired

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
