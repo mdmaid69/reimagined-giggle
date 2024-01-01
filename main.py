573ada0c008ea4fa61907162ff3fc80094acbe45
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:29 +0000

    shame anybody lead approval mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
