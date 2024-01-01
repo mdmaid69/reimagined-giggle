b758554151d035b45e2005b6f7c71bc2047bd09c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:46 +0000

    ignore progress morning fill snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
