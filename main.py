6725da8d4ceb7fde6b8e3edd430b69605c101f01
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:24:45 +0000

    build scale exact wire both

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
