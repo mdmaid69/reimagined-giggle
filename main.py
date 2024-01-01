166be44b8f9f919f5b8aac7f9f983781a3eb813f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:22 +0000

    lunch computer whenever sorry excuse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print("Hello, world!")
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def find_unique_words(sentence):
+        return set(sentence.split())
