01134ed3eada1f347167af56550757ce5832fc5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:50:10 +0000

    guess settle noise space record

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def create_counter():
-        return collections.Counter()
+def get_array_as_repr(array):
+        return repr(array)
