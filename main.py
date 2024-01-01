8bb5e1a657dc551865d10903c5352cd63f7af2f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:09 +0000

    touch deliver still nigga human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
