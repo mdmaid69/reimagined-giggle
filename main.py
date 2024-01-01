c0f16ea405d8f27d8b0319a4074342cc7be4b09e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:35:34 +0000

    glad actually estimate within scheme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def split_path(path):
+        return os.path.split(path)
