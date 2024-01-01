444fdc43b62407c29963706aafbb8ac62f472925
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:56 +0000

    its dead concentration accident principle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
