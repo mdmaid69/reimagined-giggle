88e19381724da29f7dccf050068a98b20a173d14
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:02:10 +0000

    plan nigga remove compare table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def remove_from_array(array, item):
+        array.remove(item)
