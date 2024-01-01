c6966de981c3e79003eb543f300de4fb90cad110
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:47 +0000

    backwards decision further mystery responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_itemsize(array):
+        return array.itemsize
