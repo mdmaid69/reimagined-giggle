a460809e952d469ca1259327c996baf2f63e6719
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:43 +0000

    follow bit organization strike sensible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
