579ff83935f948487e84f994e08e3c59da77ef5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:19:14 +0000

    Nn also pocket getaway any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
