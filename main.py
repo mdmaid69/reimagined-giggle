55a87d76fb23e0aabd6aa7e6fdc3b598b50a0925
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:35:16 +0000

    deep can eager cinema organization

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
