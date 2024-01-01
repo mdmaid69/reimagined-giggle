88578e729141b262e9e3adfeedc45d0b967ef0cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:00:03 +0000

    cruelty workupto bang chin behaviour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import sys
-def print_python_version():
-        print(sys.version)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
