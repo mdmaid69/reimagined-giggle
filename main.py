7c13fd16114b04b5d4ad87d70cda52c45227b224
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:21 +0000

    neat again breathe decision table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
