414a8f8006aaee4fb9794f1df1014b0c4f7fd4ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:38:50 +0000

    decision dry collection colour everybody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_force(mass, acceleration):
+        return mass * acceleration
