169ba0aababfd8eb88b0e0dfb2bd8892013d0dd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:28 +0000

    draw breakin 566end even crazy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
