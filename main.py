ef363f98ea5d88d1c36aafa044829812c818fe07
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:14:19 +0000

    winter passage in light principle

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def cube_number(x):
+        return x**3
+def find_max(numbers):
+        return max(numbers)
