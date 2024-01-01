5b748f300c934f0782d6252e9136fd441f309d07
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:43:32 +0000

    rest various near content storm

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def is_even(n):
-        return n % 2 == 0
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
