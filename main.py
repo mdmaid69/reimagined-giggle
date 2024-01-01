954735ac3f818675f168aaba24cfcda0d44046b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:29:27 +0000

    argument scream middle pupil medium

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
