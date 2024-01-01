03e193c2f2004ce99bc556583bb833fc03c4b520
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:23 +0000

    possibility fierce level takeout rule

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_list_from_array(array):
+        return array.tolist()
