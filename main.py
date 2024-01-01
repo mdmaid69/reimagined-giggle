f662f43baa2a9f5bdc42c29bed5c8946776311ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:35 +0000

    whistle event too tread rain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def convert_to_json(data):
+        return json.dumps(data)
