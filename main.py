9312803a333c895bc77882300887eb901efb5e31
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:30 +0000

    flat list past workon goodbye

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
