4ede4fcf955fa79d104d316d3d95c7e9852b78bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:10:29 +0000

    564encourage only cum rely minimum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
