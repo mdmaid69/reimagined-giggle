c5eb6aa218db7847d9c90bb9f1eb4b67b63ec57f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:53 +0000

    sink ordinary show string standard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
 def calculate_circumference_circle(r):
         return 2 * 3.14 * r
