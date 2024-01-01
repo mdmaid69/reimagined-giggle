ac4a49a54341acf41ff40e84b9294d7bf12a40c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:19 +0000

    waste lookafter settle lookafter view

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  def convert_to_hex(n):
-        return hex(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
