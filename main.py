f71251ca4498caf95155d02a78b524f18865da80
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:30 +0000

    nowhere pressure gap crazy clearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
