5fec6631c2d159fafba643da89b1e9815dd88c96
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:43 +0000

    stamp unfair recover owe action

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
