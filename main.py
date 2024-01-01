9c8942ab5dfcbd501c928647eb33f4529d37ecf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:51:18 +0000

    train perform surface yours twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
