f74d42e3e70b13684c623c013672ad7aae90431a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:11 +0000

    art traffic kind before lead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
