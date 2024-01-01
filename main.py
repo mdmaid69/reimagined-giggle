7cc3728f94b419c712c32c51dc5d6a0efaa46ac5
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:36:22 +0000

    lend to order smile practise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
