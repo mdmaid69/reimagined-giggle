a89a69eb409d54a5f700c32ffd1d860456bb89d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:24 +0000

    flow surprised belief set offend

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Cube numbers:", [x**3 for x in range(n)])
