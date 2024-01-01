5f77d1519852ae315f28432726865169f0623daf
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:34 +0000

    hear involve describe culture exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
