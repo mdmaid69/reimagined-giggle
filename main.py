c3bf3040fddd7390e78af4119a11bc180a0f3639
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:59:26 +0000

    before pride arm stranger human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
