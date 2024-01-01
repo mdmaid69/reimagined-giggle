097f620aeceb01fc7d35710e7b8596b2666df68d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:31:28 +0000

    increase use keen surprise combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import threading
 def create_thread(target):
         thread = threading.Thread(target=target)
