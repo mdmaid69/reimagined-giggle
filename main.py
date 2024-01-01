2eba7fccac92872d4cc9b20e4e286cf55753acd7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:59:26 +0000

    private pupil length left bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import array
-def reverse_array(array):
-        array.reverse()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
