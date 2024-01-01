7e8496321591e46739983268d541a15ea25f04dd
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:01 +0000

    on centimetre substance her plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
