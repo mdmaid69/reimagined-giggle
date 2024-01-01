b99da9eb56851f08098d98e54c45d9d21ef7b893
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:19:40 +0000

    declaration wild slip warn violence

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
