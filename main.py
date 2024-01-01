986fcebdbbf3edff0e156643055e32a6c7778308
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:57:48 +0000

    escape steep spill discovery nurse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
