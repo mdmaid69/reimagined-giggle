7d81e0055d4842ea79593d92fcde9a8f01d548a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:06:16 +0000

    open pronunciation minimum health period

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+for i in range(10): print(i)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
