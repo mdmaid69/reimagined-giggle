5ea135fc33305020ca88ea124e0c26b8b28ae328
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:53 +0000

    hers getout vanish hit broken

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
