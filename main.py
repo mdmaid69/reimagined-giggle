89b20b9984742c3b6f5eaa8802ef79e474fa7090
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:02 +0000

    I comeback block mind shame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
