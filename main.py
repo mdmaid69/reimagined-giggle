91bf52472884260f01240800a0d6997c4df19104
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:07:28 +0000

    smoke ours day refer concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
