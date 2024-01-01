83e07ab186647537cf22f196de96d7065ef43464
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:38:36 +0000

    only mainly if touch exploration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def convert_to_octal(n):
+        return oct(n)
