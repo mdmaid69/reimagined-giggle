204af2dee57ec1fe8458f09f8d81a1d3c3f1649a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:10:36 +0000

    subject foot confused rate occasional

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_binary(n):
-        return bin(n)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
