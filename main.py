72873d10c7b9c0ef0d74a02b2c7b86ebd83ef479
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:23:32 +0000

    disgusting film reason platform mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
