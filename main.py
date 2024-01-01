dbb541c563cb060311a640cbea63de86cc00c214
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:38:15 +0000

    people little keepup shelter replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
