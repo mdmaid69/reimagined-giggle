16929b6679236f1bf1095cb45ad2d0b833f73cb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:58 +0000

    low curious control fork whole

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
