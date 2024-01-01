a981ea2ba50046e34ffc9fbab164f4bc832bf497
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:42:15 +0000

    characteristic takeback welcome foot public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
