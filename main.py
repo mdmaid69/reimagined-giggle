4253ecb2c81d6ca1b62e4c231f1a77b0a08a6770
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:53 +0000

    comfort may familiar total proper

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import sys
+def print_python_version():
+        return sys.version
