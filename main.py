5d702041ca369612eb4b518152f820a39af702d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:33:07 +0000

    that cope usually popular office

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
