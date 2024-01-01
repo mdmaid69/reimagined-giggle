37f2567e0b010e8552e3bd3f5089b9989b04cdfe
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:39:39 +0000

    save rate picture insist sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def get_array_as_set(array):
-        return set(array)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
