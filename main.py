368294cd7cc9b62c1125299f089cc5186f67468f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:18:32 +0000

    several trust refer typical excuse

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+print(math.pi)
