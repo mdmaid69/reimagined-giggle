b6788c590e59ab43652f47bb43dd44f46fe38f0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:32 +0000

    disgusting silent touch callout Ww

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
