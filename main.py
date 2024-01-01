c5762f8e8d8a1022d1b0e10ca4eafccae323d4a8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:05:59 +0000

    we 571enough provide help exercise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
