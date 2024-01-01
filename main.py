23648fedff7230448abc2a55edd15073e7d567e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:24:24 +0000

    fill mess rescue wish friendly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import collections
+def create_counter():
+        return collections.Counter()
