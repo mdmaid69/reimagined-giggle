bb30bed88f600c2fac81a1a421827b1ec7577033
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:38:48 +0000

    I need will afraid sure

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-for i in range(10): print(i)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
