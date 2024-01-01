89824fc8ceab061350aaf4325949be110c6c0686
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:10:24 +0000

    use blame direct quarter provide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
