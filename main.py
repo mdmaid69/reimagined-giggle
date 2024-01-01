fed1e564147f27f38d050fed804f644f4e0ab5fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:50 +0000

    meaning tired succeed inquire before

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
