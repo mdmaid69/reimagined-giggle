d79e021ba85f9870c48cb3fbed63d2b2fd289d56
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:01:22 +0000

    read accordingto worried sign cheque

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
