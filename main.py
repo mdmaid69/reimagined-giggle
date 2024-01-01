1beaaa5b93c86a12ed98008ce9fed0d5c2fbb537
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:36 +0000

    normal move tour print wealth

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
