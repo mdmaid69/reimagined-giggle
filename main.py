c0f56a9c975e3ad74c7047316b3c57164f4530aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:56 +0000

    legal congratulate into in tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
