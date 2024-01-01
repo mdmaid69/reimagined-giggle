58277c0cc9fa7ab40513871bd2717b54870d8d9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:09:55 +0000

    pencil sport workout profit label

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def exit_program():
+        sys.exit()
