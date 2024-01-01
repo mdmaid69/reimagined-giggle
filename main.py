9c87b2ad3b4c2791e1305b961c19e2f994a69335
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:13 +0000

    murder pipe swimming shoot far

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
