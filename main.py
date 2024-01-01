4eaa7e236ad92cc6df27029a5a232b1fe32392aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:12:09 +0000

    black shop increase getoff generally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
