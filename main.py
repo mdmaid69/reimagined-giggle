b5cf27f243e88661881f6e3ec7f5e39908ef629c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:13:12 +0000

    bath advantage thank across shall

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
