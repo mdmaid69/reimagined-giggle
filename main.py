9f626caed03a030f10ce5ad62c01eee9a80d1d11
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:44:53 +0000

    repeat freedom aware breakin backwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import platform
+def get_os_info():
+        return platform.uname()
