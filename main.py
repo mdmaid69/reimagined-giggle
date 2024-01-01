e0ef908c5f5d560e62873f6712144873a14ee8f7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:42:17 +0000

    various unfortunate limit hurry course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
