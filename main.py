804de185bff2af5ca79a0f98b5e61481a9d68560
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:47 +0000

    in or rid century let

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def add_numbers(a, b):
-        return a + b
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
