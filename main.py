a401527b7b89c300b337fce28f6ab0c5a14e393a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:52 +0000

    trousers brush pollution feature indeed

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
