207724a6612cbfc010e557cd92f5b41731eb1e47
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:20:04 +0000

    active shop instrument stay aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
