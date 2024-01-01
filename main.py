cf375590d534f8cc9c581418fa30d0768c95fe2e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:57 +0000

    please ring you aim sight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def count_elements(lst):
+        return len(lst)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
