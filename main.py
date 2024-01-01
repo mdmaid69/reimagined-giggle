aff18d28abf460d54780fd5914ace61a8dbd2baa
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:39:11 +0000

    own hair clear question plastic

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
