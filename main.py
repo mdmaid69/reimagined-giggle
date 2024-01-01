8e0a04020324cffa9e2eb716660faaa0be34c69c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:39 +0000

    argue watch travel complete thick

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def reverse_string(s):
+        return s[::-1]
