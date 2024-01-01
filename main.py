6c847865c45894657998668997b8454f518600c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:14:43 +0000

    birthday noone step genuine bin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
