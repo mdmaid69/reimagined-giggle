7a576925ca560a89a6c1c22bc1b1ea035113b85f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:14:18 +0000

    clothes arrange label greeting test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def cube_number(x):
-        return x**3
+import array
+def get_array_as_list(array):
+        return list(array)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
