cb7559931fb586605fd93851bcc4e54ddd41d1b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:19 +0000

    suck environment meal effect succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sys
+def exit_program():
+        sys.exit()
