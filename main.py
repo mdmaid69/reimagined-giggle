79a63f143db8e0deb65012fa281607ed7c982d1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:06:15 +0000

    chin score agree size correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
