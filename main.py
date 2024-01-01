e79bcaddddbe4c5e7c5760d3470313b815e94f4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:43 +0000

    count pay breakin friendly although

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
