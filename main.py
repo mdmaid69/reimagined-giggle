da2d1a6ed0e60e378a3d4eeae5eb23a87347e1f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:22:16 +0000

    conclusion serious waste trust head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
