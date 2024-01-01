77e4c9c25b80a8ea54584e42935e496c24443594
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:35:51 +0000

    view aim machine will amount

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
