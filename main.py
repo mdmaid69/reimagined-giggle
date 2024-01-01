132cafb20cd170573fc73e39e47fbcb90124910c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:41:37 +0000

    invite stand far in mine

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
