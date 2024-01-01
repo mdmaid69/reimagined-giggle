620405b90defbe9106d7d865458fc9c7c855cf90
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:28 +0000

    reach constantly rough sad communication

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
