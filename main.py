7b40a7047ce58e1b09356b7bae8ee77cdf9976cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:47:03 +0000

    depend suckme path trial square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
