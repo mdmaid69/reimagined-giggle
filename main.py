2eaeb56dd4da20f8f1b81aadc7df10dc2ff04771
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:19:43 +0000

    travel university simple no somewhere

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def get_array_length(array):
-        return len(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
