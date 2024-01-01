72ee4207a722fb804556b6b4de8b42091cb253cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:30:14 +0000

    petrol phone into increase nowhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
