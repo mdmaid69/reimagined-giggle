b87a3b3d423253a59d0f9d02693a85e30ef4d75b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:19:13 +0000

    other natural positive bit weather

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def remove_duplicates(lst):
+        return list(set(lst))
