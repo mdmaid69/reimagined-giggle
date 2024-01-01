59535c9635e173ceab2c971df237200d08ed1bd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:03 +0000

    giveaway breathe line regret truth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
+def calculate_power(work, time):
+        return work / time
