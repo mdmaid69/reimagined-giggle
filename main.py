e71fcbcc2254269d4b793463e428538a99903b3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:13:23 +0000

    band near benefit history treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_sign(x):
+        return math.copysign(1, x)
