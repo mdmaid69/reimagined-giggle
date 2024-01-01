ad486640c696e9c10b12ea27e7d9fd16ef95bdfc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:06 +0000

    worth cool cold section strict

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+name = "Python"
+print("Hello,", name)
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_set(array):
+        return set(array)
