1d60bbeea6da0ed47f1e83b599f6c52836395ab0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:46:21 +0000

    information cold share broad Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
