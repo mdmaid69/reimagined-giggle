327c6ce70985a25fb2c9f94d7dd02d9b3cac815f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:09:43 +0000

    suck block expression swing loud

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import collections
-def create_priority_queue():
-        return collections.deque()
+def square_number(x):
+        return x**2
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
