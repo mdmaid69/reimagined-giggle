1d996980118601d9d5e89243b09e82bb66be781c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:40 +0000

    tip absolutely escape long joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
