5f48b11e07313f8fd2c88333b8b6dac5dd6fdd24
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:57 +0000

    glad air callup wise give

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+x = 10
+y = 20
+print("Sum:", x + y)
