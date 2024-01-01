7b3e214a376cc8bc77765f15f97931292723e05e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:42 +0000

    stiff trust area profit violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
