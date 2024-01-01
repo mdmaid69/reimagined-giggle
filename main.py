53bcdd559ebeed6adca1be9e14454495773a6685
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:29:18 +0000

    bank tooth direct profit illness

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
