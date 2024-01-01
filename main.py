3ac824bd7b387fe78138670a1fd23c1a2d7944b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:50:20 +0000

    sale dry care opinion short

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
