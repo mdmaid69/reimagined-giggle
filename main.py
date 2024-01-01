8a49f6f78fc68e49395aa7f9982f66bd75211f48
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:18:07 +0000

    official method certain pencil display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import time
-def get_current_time():
-        return time.time()
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
