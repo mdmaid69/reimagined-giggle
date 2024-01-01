2a6b3ac63ae2bc85ab459011016d3101205fc472
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:03:56 +0000

    defend doubt pleasant shine welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
