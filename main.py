7d975d9029a0531b151b61e5069917dacedfc63a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:30 +0000

    shop exhibition sail scrape sorry

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
