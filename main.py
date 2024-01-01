f0ed9ac0a6f732082573d28424ff907606b8f6a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:42:51 +0000

    fortunate drawing home high ourselves

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-for i in range(5):
-        print(i)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
