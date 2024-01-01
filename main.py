e59e9c5f443dd91e66c413f16b16a7d0740c6638
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:48 +0000

    advise ability although father shout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
