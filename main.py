b7bab7bd9955cc805ffbdee65bf437e2c0eafd44
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:51 +0000

    meaning rate yourself Uu beneath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
