5884c8ab4110c1047adbfa2025aea4688a5c1b8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:03 +0000

    rid replace suggestion rest sick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
