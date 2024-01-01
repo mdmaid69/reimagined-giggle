f1bf592c0b724ac1327d5ec5382091ed9e3d9d90
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:00 +0000

    hair plastic matter bottom what

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
