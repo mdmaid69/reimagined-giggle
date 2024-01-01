e130ba4a7890ea80d036cbcadbf9bc68f8059e98
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:18 +0000

    back closed fresh scream sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def multiply_numbers(x, y):
+        return x * y
