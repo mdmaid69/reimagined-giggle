836ea40ffa215266616c323e42c5a5b617a452c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:32:21 +0000

    corner show no everything turndown

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
