bd1f085da20d2edb783134b43ac9dbbbaa9061ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:37 +0000

    Cc study scrape rescue empty

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
