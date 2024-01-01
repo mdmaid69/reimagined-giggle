a921d83b9e5411fe63727b25f5953afe91d1c323
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:15:00 +0000

    command breakin pause lookaround space

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
