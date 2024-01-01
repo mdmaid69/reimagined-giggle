378ac93cbbd5c89f360a2268852220e2f0828558
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:20 +0000

    something protection light telephone uniform

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
 sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
