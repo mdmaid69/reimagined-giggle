b8b37bc0cfebd96baded291b3e3e8fc8fce57782
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:10 +0000

    violence scare afford underneath indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
