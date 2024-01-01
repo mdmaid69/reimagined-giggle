a44b7424a16fed9dd906dc428637c67bdc3497ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:43 +0000

    level ball lookaround trade exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
