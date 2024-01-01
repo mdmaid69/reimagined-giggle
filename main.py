ba7bbcb01f724828809c35770732ae8b043eef6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:11 +0000

    steal method battery into performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
