8ae0136bdf2fab396ab2751b6d8edcc4a6cb0743
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:04 +0000

    passenger habit bush below lose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
