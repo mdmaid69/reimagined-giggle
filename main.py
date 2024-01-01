d0535177ed9a201cecc93e18ac5230f3d8300a28
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:55 +0000

    accidental reduce emphasize Dd forgive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
