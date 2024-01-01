a8b1afdcf231da66dfd3401c1c59a163a4b7adcd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:04 +0000

    purpose threaten balance petrol there

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
