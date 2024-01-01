3e46ad74cb2ef08ff084020be05ff496131547e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:11 +0000

    house turnover brother object talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
