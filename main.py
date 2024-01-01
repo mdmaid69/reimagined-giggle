8ad29cd2878d0cd6ea952c359cd81ac0b5476457
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:25 +0000

    act platform favourite still admire

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+def count_words(sentence):
+        return len(sentence.split())
