8692e4d450460611ea4346bda44f1ef6e3874e75
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:41 +0000

    ahead safe breakup understand city

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
