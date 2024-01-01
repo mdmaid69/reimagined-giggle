9913908465a3fd781e3b1cee36897101a2b3e9ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:30 +0000

    its Gg trade tall takeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_square_root(x):
+        return math.sqrt(x)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
