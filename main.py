d17392ef18dada57d8500d134ef407a47ec89c85
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:58:11 +0000

    compare me independent warn easy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
