3d7dd9cdb70b9ebef876b0e9fa1dec04a7ca3b91
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:09:48 +0000

    public ocean watch ourselves pair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-print(os.getcwd())
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
