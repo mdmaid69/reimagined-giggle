aa81af8e4b62b5ca80add9f87319055ce94de457
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:20 +0000

    either read deliver breakthrough she

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_float(array):
+        return float(array[0])
