9ad9c9d62bed813907e580960a16c27d4e1b41c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:28 +0000

    Vv tap impossible battery please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_speed(distance, time):
-        return distance / time
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
