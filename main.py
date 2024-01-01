cafad2610c2179f7a2e5bb3b2d762365aeafd2f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:05 +0000

    tap getback also turnaway once

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
