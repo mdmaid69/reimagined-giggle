8dbf237da2bd176d8cb9c6d8361b8385821e73ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:39 +0000

    bad ought scrape meet private

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def convert_to_hex(n):
+        return hex(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
