24d4f6ed03f7156b9c27fcccc90b2cbe30ededf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:56:58 +0000

    relax impressed apologizeapologise shade cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+print(json.dumps({"name": "John", "age": 30}))
