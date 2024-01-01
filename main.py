42e1f29e9411bf6683f92f93fc5136c8c25ec1ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:21:22 +0000

    lookup suggest bite awake understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
