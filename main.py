722164b193285e28c4febfbe0c71b10b8cf8c88e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:54:05 +0000

    put meeting petrol flood steal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
