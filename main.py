ce8f0b08a8b37a803bcc66aa508648b7c5076279
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:56:35 +0000

    run challenge well outside tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
