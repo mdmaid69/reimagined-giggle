7af29ddbde6534684aeae33a151ad8d12950f0d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:38 +0000

    fierce detail for goup loose

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import tensorflow as tf
-print(tf.__version__)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def reverse_string(s):
+        return s[::-1]
