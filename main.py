8ce07e4fcc83ed6fd3a5bf0d64e64e8a0249ef46
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:24 +0000

    shadow stay memory taste warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
