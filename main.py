a3116a08d57ecc74895fc11a22bfcc43a7746599
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:21 +0000

    middle me comein kill pronounce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
