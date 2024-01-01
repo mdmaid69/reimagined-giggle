d22e42ba5a47f45b86a084a854f4915bbe3d2369
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:04 +0000

    hard pour active target nurse

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
