326b75719dd8c853556267e0e0d9acc1a07c59b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:17 +0000

    fail warning make congratulations narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_min(lst):
-        return min(lst)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
