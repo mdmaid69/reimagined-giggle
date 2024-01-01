5608b614b5d89a80e7fca9eb20af4220bf7e65e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:22:40 +0000

    loud pain system correct prove

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_elements(lst):
-        return len(lst)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
