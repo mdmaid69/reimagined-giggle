c6a9bc500d355a1a38347ac01660a0df9b643fad
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:27:57 +0000

    relative survive attempt fierce above

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
