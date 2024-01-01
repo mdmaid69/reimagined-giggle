b53f2919592ac5508ec3a72433cc7004459f9b06
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:07 +0000

    glad pattern close whom cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  def cube_number(x):
-        return x**3
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
