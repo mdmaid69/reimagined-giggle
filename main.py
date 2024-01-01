7da58b0004851ffa95e539d0a5a938f72369f434
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:27:32 +0000

    lunch watch rule phone authority

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import collections
+def create_priority_queue():
+        return collections.deque()
