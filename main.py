97fb26cd6fb941b96f1a456e9ca58575b00a3561
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:54:55 +0000

    chance edge stir course keen

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def add_numbers(a, b):
-        return a + b
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
