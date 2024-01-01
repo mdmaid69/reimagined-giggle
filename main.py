a4eb810c83dae8a532d89a2f8706a73f4b23691e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:56 +0000

    official feed myself might receive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
