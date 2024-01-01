fd25e43cff740e68bf39acb62bb56257909cc904
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:42:33 +0000

    rest project constantly control history

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
