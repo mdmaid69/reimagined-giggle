e468fa520543fb3ccc0efa8453e2f3de93cc3728
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:36 +0000

    bear trade still spoil pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-print(re.match("h.*o", "hello world"))
+def square_number(x):
+        return x**2
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
