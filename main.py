ff57d1c1ebd8f2f8da52ac1d0d34ad4120753a5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:19:14 +0000

    kind owe avoid occasional far

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
