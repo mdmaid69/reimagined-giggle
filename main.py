fc1ddff84d322c36f1279663d0f51ca0e1d1abc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:03 +0000

    cause month illness fear fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
