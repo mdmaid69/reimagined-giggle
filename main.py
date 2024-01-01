415f695c6ad624bdd395c8da6ffd65b73543b108
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:37 +0000

    getup force church stage frighten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_even(n):
-        return n % 2 == 0
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import sys
+def print_python_version():
+        return sys.version
