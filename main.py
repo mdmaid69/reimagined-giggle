2242ea187ca324a10375357b604af84d949928a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:56:40 +0000

    sad decide the do ignore

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
