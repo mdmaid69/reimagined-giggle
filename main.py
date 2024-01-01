76e40aea17cb809c08595e4cc4f92f011baf6e21
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:55 +0000

    feeling basis list yesterday duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def get_array_length(array):
+        return len(array)
