bf338b409587e935005939ea4fb549275137aae1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:03:38 +0000

    goodbye nerve later confuse estimate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
