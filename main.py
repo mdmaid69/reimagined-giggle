ea5fb07e119bacd462730062f5addd520148433e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:08:03 +0000

    note deaf instant convinced strange

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def count_elements(lst):
-        return len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
