4a30ef816c0ff77f7afc54e857b924515e746139
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:46:22 +0000

    passenger calloff limit section circumstance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def add_numbers(x, y):
+        return x + y
