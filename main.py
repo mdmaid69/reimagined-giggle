f045e2f93b55639d0c4ba342b843626eeecc62a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:32 +0000

    press cover anybody past furniture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
