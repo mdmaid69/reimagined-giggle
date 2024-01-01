4e9355989f30a04183438157a34d1973455d9c55
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:46:04 +0000

    produce pull gather content swim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
