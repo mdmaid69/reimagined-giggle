f39e99e3c311d82e04822d59da8724af884f747a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:52:08 +0000

    special guilty cheek easy alive

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
