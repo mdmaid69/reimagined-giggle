2162a5ebdae11adec1b8a7263b2e6528ad10ed0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:11:16 +0000

    mistake community copy permission Zz

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
