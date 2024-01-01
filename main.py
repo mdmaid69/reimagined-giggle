21e1c786a2df2c52fd07f2c9ed7744cda9563b05
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:54:25 +0000

    solve pocket lookafter clothes reject

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
