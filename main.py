6fa1ee7691fd38a07d5959ff51df2dbcb91c9121
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:36:03 +0000

    building replace big nature future

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
