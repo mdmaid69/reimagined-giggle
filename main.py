d5a908bc1f6db25d637641a878a2cf51f6ed648b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:32:14 +0000

    left relation turnback 572enter reason

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def square_number(x):
+        return x**2
