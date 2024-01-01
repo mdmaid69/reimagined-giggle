e7adc57a436599144031bd21655db1d2d9e73fa6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:21 +0000

    by proof woman because smooth

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
