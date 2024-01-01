0cc1251fc1615061c5fa504a98db1dd847f8d62e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:58 +0000

    support interview could keepaway fear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def convert_to_hex(n):
+        return hex(n)
