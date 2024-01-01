7a4426c24ae0e9d1beb7c849d67189d06f528a19
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:05 +0000

    bin Nn getaway fail workon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
