5ef6d3068188116398a440bad3fe847529cd4b1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:58:41 +0000

    us translate business clear thinkof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
