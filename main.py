6c0a16bf10a8b382161c1c2aa9f2a16b101f3260
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:56:32 +0000

    shall Ww breakinto right apply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_density(mass, volume):
+        return mass / volume
+x = 10
+y = 20
+print("Sum:", x + y)
