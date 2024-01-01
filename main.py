8e8e3e193a5df6c284e14779899bf8b2122f6d1d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:48 +0000

    turnback event familiar model object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
