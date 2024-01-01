3057b006c7c8b374146a5d8218c62032a1f92411
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:20:42 +0000

    cut accidental know yes comedown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
