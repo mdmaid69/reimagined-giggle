fcb67140a6ede383f4f9d42969f73a07e67888e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:35:02 +0000

    wide thick breakout dull plastic

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+for i in range(10): print(i)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
