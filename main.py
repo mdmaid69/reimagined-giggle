acc19414409c45a487434012aa4ca5df00abe601
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:41:41 +0000

    majority discussion ordinary minimum imagination

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def get_current_working_directory():
+        return os.getcwd()
