97a93cb51b5ba611ccbb6cb94fced6eb6a6737fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:28:04 +0000

    related pair after smile free

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def convert_to_hex(n):
+        return hex(n)
