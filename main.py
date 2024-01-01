44b6d8aaa8d00e75eaac18f389022f9175746ee9
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:19 +0000

    hair kind tidy great skin

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def get_array_as_set(array):
+        return set(array)
