ccd192814ca140d4f22bd07d0c94dfab3cda22a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:16 +0000

    common exact during undo fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
