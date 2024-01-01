4c5034a91869c9b421176a69663e7d53b60495e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:28 +0000

    expect noise education principle immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sys
-def print_python_version():
-        return sys.version
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
