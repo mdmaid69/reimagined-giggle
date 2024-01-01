34afd2d5969cc0149ca57b4843ad7f983874a1cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:14 +0000

    mainly accommodation rain pleasant design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(lst):
+        return max(lst)
