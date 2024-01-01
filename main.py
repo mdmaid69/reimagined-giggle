c8386cbf06a626abdef8e05f8d85dd789a8552cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:13:36 +0000

    roof error method last cost

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
