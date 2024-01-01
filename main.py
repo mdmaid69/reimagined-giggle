4fcde52132144cf64b2da0608eeb2eab710bdf0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:05:19 +0000

    believe claim marry ilovedicks generous

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+  def multiply_numbers(x, y):
+        return x * y
