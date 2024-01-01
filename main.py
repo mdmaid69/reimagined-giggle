a00f13e8a0f956e35520488366cecba8fde134ac
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:11:02 +0000

    flat comment fear butter secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
