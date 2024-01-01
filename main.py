7425521c7500d564818819f3161b9688e0fbe577
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:29 +0000

    position protest cross uncomfortable reasonable

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
