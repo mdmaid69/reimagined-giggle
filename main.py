39dcaba47c83c54b89360992b177031f65aefc61
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:10 +0000

    church though borrow keen burn

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
   import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
