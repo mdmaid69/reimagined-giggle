343724dba610db170909ff62e2469b8ee9d5d44f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:33:05 +0000

    forever improve after suckme characteristic

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import array
-def get_array_as_str(array):
-        return str(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
