a64d7c6543b1da76afd5a57a86ed0a3b3fb679b3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:57 +0000

    care great drop wealth direction

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-for i in range(10): print(i)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
