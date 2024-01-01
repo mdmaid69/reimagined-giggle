af8b5c54ff93246e6e2e1484b8e0dfd7ca77ad96
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:45 +0000

    turnoff brown soon still production

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
+def convert_to_hex(n):
+        return hex(n)
