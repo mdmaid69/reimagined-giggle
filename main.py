5a803eaee3fa774e0182e069de6a97e6612cd8e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:18:05 +0000

    pressure smooth and thirsty run

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
