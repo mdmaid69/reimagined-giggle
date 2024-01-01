44a5135c4886e1691813771e6e14038686038af7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:13:16 +0000

    alive nothing loud goon behave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-def count_words(sentence):
-        return len(sentence.split())
+import os
+print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
