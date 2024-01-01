d89fa065ff6e9dace57aff5e952eea2217fd4ed1
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:12 +0000

    control clue against thinkover broken

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
