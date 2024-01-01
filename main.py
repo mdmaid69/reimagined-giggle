e5e79a5b8158629fb0cc81845a3727702d04f5bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:28 +0000

    pause letter fairly lookaround number

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
