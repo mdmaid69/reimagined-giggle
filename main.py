2693e66a20a1f3cf238a56ce74568b68a2210c95
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:53:29 +0000

    advise sometimes previous please bank

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
