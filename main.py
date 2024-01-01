a44777643a5d42fe27a08e4faffe1861ba7ab8ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:39:07 +0000

    nor might turn defeat Hh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
