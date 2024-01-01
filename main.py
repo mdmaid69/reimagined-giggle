4e8be492e5c1f4f2f25768c62d0f38226063dcae
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:27 +0000

    voice such achievement popular communication

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
