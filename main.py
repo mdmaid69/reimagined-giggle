6252f81b63a04915b29d331621da01d4ee3e2ef9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:33 +0000

    moon skin Ff forward goout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import json
+print(json.dumps({"name": "John", "age": 30}))
