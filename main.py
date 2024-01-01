b0d01038003b17534f39f2f7b1608574b9d00f08
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:56 +0000

    answer closely hold but Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def count_characters(sentence):
+        return len(sentence)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
