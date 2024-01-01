b302db3cd161bdc20756a6b3dcab4e7d9dc0ca6f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:15:23 +0000

    understand make own trick understand

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_average(lst):
+        return sum(lst) / len(lst)
