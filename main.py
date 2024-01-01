a85cef240bb1f990f613b9ee51825a91c92635d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:47 +0000

    scale sure wherever announce cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def cube_number(x):
+        return x**3
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
