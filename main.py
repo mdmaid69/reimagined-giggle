469645e1e0701e195858f0fa02aad9840597c0e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:13 +0000

    Gg colour several serious advantage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_power(work, time):
+        return work / time
