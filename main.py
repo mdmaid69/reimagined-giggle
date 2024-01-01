1255cbac3f5456cf477edb5b373513dff56bf531
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:27 +0000

    breakout unlucky drive dangerous recovery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
