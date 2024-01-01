c3b03075e4334726227ea5218cafbef7ed1413da
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:47:35 +0000

    tough which confidence play holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
