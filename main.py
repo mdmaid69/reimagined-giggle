a7b7bb07a159870a5c2d398b74231bf79af8b0ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:33 +0000

    twist relief convenient future believe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
