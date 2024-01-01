b33c717d5109af4f481ebb0b6754cc7d268d14b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:37:52 +0000

    charge discuss his joke save

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
