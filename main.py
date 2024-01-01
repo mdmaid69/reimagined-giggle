42cf0cc5b72165985b1eb96e5ceb59bbaf8db8a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:12:09 +0000

    angry welcome so open worry

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-print([x**2 for x in range(10)])
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
