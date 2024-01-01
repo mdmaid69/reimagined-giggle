e0e333b12ee0df2108fb42ebe4901fa3d10138ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:12:48 +0000

    copy lookup Ii day far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
