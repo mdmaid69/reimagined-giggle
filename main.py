618b61b9853c94086552fc53befad9c544404013
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:54:47 +0000

    keepfrom couple replace dress none

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_min(lst):
-        return min(lst)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
