4d78e8ca3f43ace4928f11bd9d8c433068e30c6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:31:18 +0000

    payment earth level fresh birth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
