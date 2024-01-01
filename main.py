979b2c1b86a93c9f50db69f546a01e34b7ffcf11
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:09 +0000

    really tough worth workon punish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
