474c385e05efac6c63832f3f2054720f8c09077a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:21:35 +0000

    feel ride total score comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
