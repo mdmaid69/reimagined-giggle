39ab050cd678bcece3dc7814baf294e1f598831c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:59:53 +0000

    kill dear negative freedom squeeze

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def square_number(x):
+        return x**2
