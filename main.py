e861473684f240a35504b29b77103296bb9ded29
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:14 +0000

    calloff hurry goup information wipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def reverse_array(array):
+        array.reverse()
