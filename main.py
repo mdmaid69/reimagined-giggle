a354b4c71d05d2cb8b356b743023137ec9a80720
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:49:26 +0000

    work comedown tense week exchange

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def multiply_numbers(x, y):
-        return x * y
+def cube_number(x):
+        return x**3
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
