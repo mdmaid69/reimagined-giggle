ebe565243e1705a6cc45f8a992323d96b601ea5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:39:00 +0000

    aware breakin have disagree maintain

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+print([x**2 for x in range(10)])
