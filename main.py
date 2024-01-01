8ada6c849aba3c5a823b18ec093e80c97261b41b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:59 +0000

    especially future dry switch lot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def calculate_error_function(x):
+        return math.erf(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
