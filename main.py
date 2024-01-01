00017f80c30f44e711a065522a611f97f42afa7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:00 +0000

    activity breakthrough build forget transport

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_power(work, time):
-        return work / time
+def calculate_density(mass, volume):
+        return mass / volume
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
