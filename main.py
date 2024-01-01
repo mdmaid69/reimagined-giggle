2beb786afb18b36c4b857ad86bbd927e9edd5b97
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:58:26 +0000

    practical unusual comfort side comedown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+print([x**2 for x in range(10)])
