a57e9edd6a442a61a2fa23b7c2467c786d1b687c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:09:49 +0000

    minute tool hurry exist opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
