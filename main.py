f89bbcecc7df29b1cd5fce3de0921509f8496f10
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:57:16 +0000

    means bringforward pleased joke claim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+def get_current_working_directory():
+        return os.getcwd()
