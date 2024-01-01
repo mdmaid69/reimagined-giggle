1ee90fae36dd2cf006141bfc977ff2dcc5112abd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:22 +0000

    authority precise level injury suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
