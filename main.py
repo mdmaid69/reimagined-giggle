fa15197b4b7633113d18403cbc2422fb3e488866
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:58:01 +0000

    circle still summer bread agree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  def subtract_numbers(x, y):
+        return x - y
