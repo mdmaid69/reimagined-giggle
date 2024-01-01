1a54f6b61f3c7a1017b68f4ec86778c4ebf9ef19
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:30:57 +0000

    large taste vision port Rr

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
