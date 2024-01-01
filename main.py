bb01d4927927345814bbdfce3d306f3645b7cc1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:42 +0000

    bin method last snow proud

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
