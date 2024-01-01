9c399945d5a6951b85d2b3052482c6154092a180
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:51 +0000

    switch description spot fun impression

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import sys
-print(sys.version)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
