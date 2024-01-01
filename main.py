ef0f07905de0258b8b3dfd40994b76256ce4bc04
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:52:02 +0000

    oil wild friend bottom off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
