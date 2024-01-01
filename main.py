aca353455466621feb12ce295c5afa6cbbf3da43
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:20 +0000

    care shake picture present owe

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
