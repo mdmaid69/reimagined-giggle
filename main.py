3943ff625f8b5b2c7c6a12329c8a7d8c2a676795
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:07:33 +0000

    identify either cheap breathe find

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(lst):
-        return min(lst)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
