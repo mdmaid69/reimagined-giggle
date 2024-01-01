b46f1cb51f4613231cfd36450217d12bd2c752b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:57:29 +0000

    century tragedy age goout obtain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def delete_file(file_name):
+        os.remove(file_name)
