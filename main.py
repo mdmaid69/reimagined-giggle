b40a9a2d3e247c4208288bc7d280fbaf9174aa86
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:03 +0000

    trap quit turnback ring dance

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
