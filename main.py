4e50a09ab4abce426ace6ff1c8799ed6186261f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:29:28 +0000

    top tell fashion old improvement

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(numbers):
+        return min(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
