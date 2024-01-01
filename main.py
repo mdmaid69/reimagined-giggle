e4d0f5ad7faf6b3995007e66d6ede842283e85a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:59:39 +0000

    choice niggaacademy little similar dick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_max(lst):
-        return max(lst)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
