4a075248f13ce45575759a4b509ca4a2d57953e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:28:30 +0000

    trick church compare Mm sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
