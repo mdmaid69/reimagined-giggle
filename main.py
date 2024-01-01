eaee2e6d4949643bd72fe80f79e2e9d89f2f8b4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:25 +0000

    split present recently there doctor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
