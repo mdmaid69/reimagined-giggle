473baf034687a69ffefe61756b72f703d886616e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:08:39 +0000

    unfair pity and wing crazy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def convert_to_binary(n):
+        return bin(n)
