44d8529f3964066d2616cbfb23f3d7cb75294cc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:43 +0000

    excuse exercise remove draw our

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
