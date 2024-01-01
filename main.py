c6bb6e1ec89ffc073561a797997ae6cac266fd07
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:13:27 +0000

    force radio large convinced off

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
