99781f30fee95b710b04a01dec3eadc4447f61a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:44:03 +0000

    hide criticism nervous afternoon from

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def get_array_as_bool(array):
+        return bool(array)
