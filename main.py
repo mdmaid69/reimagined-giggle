34ddceb0e9a06368835aa21fa4eca02433750a8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:04:43 +0000

    land behave by fire pride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
