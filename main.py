3eea95257b52ef432aad4dab0a223e17228b4be3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:14 +0000

    reply badly voice reply bag

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
