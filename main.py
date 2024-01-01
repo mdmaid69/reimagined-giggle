9ce96e4b0bfd9ac20fb8e811d5362f40851904c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:06:49 +0000

    thought whoever turn history rate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
