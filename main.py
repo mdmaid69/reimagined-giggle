54f27fde91d1b47848fcb514b0f8867877d43052
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:04 +0000

    stare attitude team happy available

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
