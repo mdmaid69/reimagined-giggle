13c17871fa7af2d197d4b7c863f77e70cca0dd80
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:58 +0000

    borrow complicated generally kill fill

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
