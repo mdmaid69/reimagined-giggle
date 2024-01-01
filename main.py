c95b6f6df54d7b30c578ecc75d0c68addf26f1af
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:44 +0000

    movement letter copy blind charge

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
