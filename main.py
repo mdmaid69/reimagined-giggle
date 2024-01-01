37d4f9de35ae083b31c792225175dbf1f20fcb72
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:15:19 +0000

    reply target everybody blame Mm

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def find_max(lst):
-        return max(lst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+text = "Hello, world!"
+print("Uppercase:", text.upper())
