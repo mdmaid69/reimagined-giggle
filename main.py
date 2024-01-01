382842d99ff2c53f6049f5b8674e05b3467bb624
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:12:12 +0000

    plastic compete ear pity worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_speed(distance, time):
+        return distance / time
+def find_unique_words(sentence):
+        return set(sentence.split())
