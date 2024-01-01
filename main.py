28f0d86938e75c1a4c0d45b0641eecd99b357217
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:02:17 +0000

    exactly owe count insist better

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
