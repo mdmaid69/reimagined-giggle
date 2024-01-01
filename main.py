7fccd6b7afec5ec0403f3077c141ebd64d258ba7
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:57:50 +0000

    guide throughout education flat copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import os
+def remove_directory(path):
+        os.rmdir(path)
