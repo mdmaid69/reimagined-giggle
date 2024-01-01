88cdc7f9044a27c9bdcbf61d01fd15965b1a08d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:45:31 +0000

    suffer tax boring so side

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_list_from_array(array):
+        return array.tolist()
