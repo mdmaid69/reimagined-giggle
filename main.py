f0a15c1e261c126e820b304b72de28fa4e3d585e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:29:42 +0000

    be dry usual well education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def reverse_array(array):
+        array.reverse()
