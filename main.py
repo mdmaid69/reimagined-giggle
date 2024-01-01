a95e0b77dfef5059675d42bdc46eeaa1dfe27e7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:08 +0000

    politics special left old disgusting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
