0477edc9f3f07c692854f74965c26efff9bc10c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:02 +0000

    ahead because offence suggest love

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
