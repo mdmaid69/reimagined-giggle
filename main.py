f95b00be7c053a15121a6782e74722a253dcd47d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:18:13 +0000

    further only market recover brief

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
