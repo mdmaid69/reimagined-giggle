586bd786a4ce0687154462752e2e7f8e709d336d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:25:58 +0000

    lookafter space wash emphasize course

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
