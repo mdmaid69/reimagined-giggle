25d111011c85127c084de809b158b0316ab78f55
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:27:02 +0000

    burst wall order everybody search

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def create_user_string():
+        return collections.UserString()
