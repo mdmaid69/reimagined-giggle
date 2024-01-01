1f6418f5dc0c086c5f5fc36ecdbe73d031fe17c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:54 +0000

    sound crowd obtain read greeting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
