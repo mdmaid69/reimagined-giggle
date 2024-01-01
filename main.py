b200eee37e691994d4c3a70448f7e96d5ca4ceb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:42 +0000

    stamp repair discussion laugh recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-import time
-def get_time_since_epoch():
-        return time.time()
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
