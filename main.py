4d8f141219ec34d532cbcedee692b83804089446
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:48 +0000

    far main reduction receive listenfor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
