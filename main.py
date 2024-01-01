2580689f5b168dcae75c542b5f789f67c7c9dd22
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:37 +0000

    compare happen on low have

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
