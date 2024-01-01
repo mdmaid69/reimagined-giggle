1f9b37cfec4ae5c007802a87888c9a9521322e03
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:07 +0000

    dull bunch partner reason idea

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
