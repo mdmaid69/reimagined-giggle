a2bb4630ac2fb2f05001b7d8aea4030f3fa744ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:14:55 +0000

    comeback meet mysterious wealth mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
