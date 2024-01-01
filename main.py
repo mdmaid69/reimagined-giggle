67a80f8109525c223a2226cbcc30f38d0bb2d2fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:44:07 +0000

    century giveout human dangerous imagination

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def is_even(n):
+        return n % 2 == 0
+  def convert_to_hex(n):
+        return hex(n)
