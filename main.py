1f9f090ac5eb8f69f70987f3380ac9ed8ce3a60f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:43:41 +0000

    relief operation bet rate last

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import time
-print(time.time())
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
