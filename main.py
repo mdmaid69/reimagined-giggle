870bb1d2ce66b2261130c7603b52ea6f9d27b3cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:51:37 +0000

    free blame stand real emphasize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def count_elements(lst):
-        return len(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
