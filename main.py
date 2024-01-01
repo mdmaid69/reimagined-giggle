7d42442b9e0857b271c69f30af84b46ff0e7e5fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:53 +0000

    cruel spray under pretend flat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import time
+print(time.time())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
