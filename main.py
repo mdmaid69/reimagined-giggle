2d2f58c5f33214b8eed6e54a1d8806cf749f0c82
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:38:48 +0000

    hide idea path middle wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
