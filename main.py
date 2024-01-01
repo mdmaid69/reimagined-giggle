6e679efe8b37c9acf844935bd0823b8a58acc84c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:54 +0000

    quiet study proud figure temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
