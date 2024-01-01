f52be264fee43e62bd4320bbde39f07822c29459
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:22 +0000

    its traffic sensible purpose address

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-print([x**2 for x in range(10)])
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
