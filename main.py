7e0d981e50c8f87bd530dafd58ba8c3b63321e7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:00:56 +0000

    land change store begin top

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
