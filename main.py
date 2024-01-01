33f2442ff33e61e98217ba30ae8d028544c122c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:16:42 +0000

    himself chief back false above

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-print(sum(range(10)))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
