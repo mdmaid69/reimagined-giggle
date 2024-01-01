1ad4f9de7e1dc5d301dc642f3f9581eac3480b39
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:28 +0000

    valuable true simple praise create

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
