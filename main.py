0df63a60f7ec6d770be37237c4306883ec3569a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:35:23 +0000

    equipment solve behind laugh fresh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
