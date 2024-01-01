3df6aba9d2ff4d54341bf91b1e7539edd7753a07
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:26:01 +0000

    black teach by cover development

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
