997d9f40dcf5661c49ca19b3cde71df826838044
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:10:50 +0000

    increase interrupt type separate job

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_counter():
-        return collections.Counter()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
