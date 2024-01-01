127a0bb3ee2b3e29aaa2f9f90d78c66060bf5334
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:47 +0000

    fast naturally better raise welcome

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
