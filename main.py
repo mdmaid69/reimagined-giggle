123d7b553c5d441cbbfcc9e9e7be2a263309b72e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:02 +0000

    takeover store praise risk brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def is_odd(n):
-        return n % 2 != 0
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_index(array, item):
+        return array.index(item)
