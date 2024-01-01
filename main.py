b2364876c209d5a9e95e995b8853ba8a1cdd9883
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:05 +0000

    fear recognize bring talkoutof straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import collections
-def create_priority_queue():
-        return collections.deque()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_max(lst):
+        return max(lst)
