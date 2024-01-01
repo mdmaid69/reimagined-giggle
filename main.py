de772ada426639205b9d3085e5a74414b2e1a1b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:25:10 +0000

    offer object interested understand pollution

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
