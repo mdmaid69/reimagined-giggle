449731cb6e7d1891f2f9bb592e32086abceb4c1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:10 +0000

    smile feed bottom motion reasonable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
