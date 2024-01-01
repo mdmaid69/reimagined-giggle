bdd631cb5a64c1eabf7e9ab4938eae9e2165b13b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:27:29 +0000

    team female foot mind lend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
