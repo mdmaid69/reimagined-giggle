ba05f799d97e15f6aee6955adb49a0b546f94097
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:20 +0000

    enthusiastic finish such empty subject

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_density(mass, volume):
+        return mass / volume
