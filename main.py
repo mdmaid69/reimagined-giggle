5928af0056093e55149e9d7bfda0883656b05553
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:39:59 +0000

    effect warn warn joke normal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
