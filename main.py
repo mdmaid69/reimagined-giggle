5ccd8464ff14184046f1c39a3edb7712b9e8867e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:53 +0000

    method nurse order mean concerning

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
