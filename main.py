7a48f56f050f309580ebfe926893413883fc5ed2
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:25 +0000

    stage accordingto manner miss argue

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
