551659d70ae210b84b1cea2afd614091ec86c707
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:53:19 +0000

    boil gradually look healthy control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
