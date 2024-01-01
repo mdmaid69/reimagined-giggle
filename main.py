ed7ff86aeb1b3aff40b3d5b0c2610d1dd43e341a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:14 +0000

    dead content gift shock thankyou

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def reverse_string(s):
+        return s[::-1]
+import platform
+def get_os_info():
+        return platform.uname()
