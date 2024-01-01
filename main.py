7a9c1a5c0bfe6b159da6e8da47b55c55b816a31b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:15:38 +0000

    platform regularly everything base listenfor

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
