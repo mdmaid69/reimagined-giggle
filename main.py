1e0d9aa1e8c719906d55a2868fdc3aa12091b091
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:13:47 +0000

    time century question law strike

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
