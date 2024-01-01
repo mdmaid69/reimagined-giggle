6a637f482b6c49e653fa353d7150ce39da8ed3a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:38:18 +0000

    pick model curious result progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
