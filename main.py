907af2301729fc4b5cec7bd2a2f98c7cee12a1f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:35:35 +0000

    block heavy makeup extremely concerning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import os
+def change_working_directory(path):
+        os.chdir(path)
