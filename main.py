ff7942b08cc6fc3bcad115761e37e7be02554070
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:08:15 +0000

    agree pride wake complete demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
