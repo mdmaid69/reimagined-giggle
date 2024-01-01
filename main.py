dc9f81ec58afa974a4f57b67cd2a4a4ee551a31c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:24:09 +0000

    pity peace pay though look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
