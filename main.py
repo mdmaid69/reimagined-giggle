292dc132cd83a46b35ab77991218e59c9d67d131
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:34:07 +0000

    safety weigh like scream Mm

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
   import os
-  def get_file_nlink(file_name):
+  def get_file_number_of_links(file_name):
         return os.stat(file_name).st_nlink
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import collections
+def create_user_list():
+        return collections.UserList()
