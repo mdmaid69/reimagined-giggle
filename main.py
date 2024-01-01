c8c5364e6939a8ca13d5d81f7466de416cfba5ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:01 +0000

    rate tight usual wing contact

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+print(json.dumps({"name": "John", "age": 30}))
