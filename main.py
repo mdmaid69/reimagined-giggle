ca62f7056a0447172c547c8ca01a2c6d7d4ca317
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:32:48 +0000

    rush wide something disapprove genuine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
