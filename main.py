7aaa452e725605300813f7f5786dba3d337b5bd0
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:13:50 +0000

    purpose shade correct wrong your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
