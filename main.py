d569368c3449e8e9ca1256712081dc56c539f482
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:30:37 +0000

    direction willing drop tax space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
