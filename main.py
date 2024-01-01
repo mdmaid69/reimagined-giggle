4439394811eca43be21f5367cdf3f2fef4aa2b5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:54:19 +0000

    should soak pleasant forever recover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
