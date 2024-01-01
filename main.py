e87f6bf9754505649a365ec60583131cc3c8cf88
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:23 +0000

    turnback Cc more keepoff live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
