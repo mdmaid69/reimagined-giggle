c1edb6da540cb0cde5db3623d11a81b3c957808a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:25 +0000

    union unfortunate mess wire release

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
