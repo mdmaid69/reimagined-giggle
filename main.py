c33d864e618e7ded082ba8d2a2c41abb9c42f1c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:49:20 +0000

    apparently spring influence change characteristic

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
