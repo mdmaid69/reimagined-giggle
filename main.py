eb0d3263040835dc8f6bfb64417a44309a8880ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:32:38 +0000

    design life crash exciting colour

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
