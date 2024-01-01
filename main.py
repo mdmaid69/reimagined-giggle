9451d821015fc949ec5fea9469db2f5dfcd523ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:13:32 +0000

    climb no absolutely guess advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
