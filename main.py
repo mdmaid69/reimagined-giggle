cb11b44f79818907eddf07cfdc54e0782ed63639
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:57:59 +0000

    rest float please turnoff bet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import math
+def calculate_sine(x):
+        return math.sin(x)
