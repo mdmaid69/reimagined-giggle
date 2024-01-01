4f63245510c5eb142784bfdf5e06f8d8b7470aff
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:36 +0000

    stare create try sale well

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
