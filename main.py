138ca6c4d63985b74ae034ee1a6020c565c97f1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:16 +0000

    wave 569enjoy check shout about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_list_from_array(array):
+        return array.tolist()
