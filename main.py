03e5a5f0c8ed0ce388bf147e84ef9292c88b82a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:34 +0000

    stop remember share priority temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import os
+def remove_directory(path):
+        os.rmdir(path)
