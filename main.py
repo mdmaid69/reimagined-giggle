9fdef001e4ff2ea112153cf94315ffc411442e41
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:04:48 +0000

    suppose furniture abroad morning circle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_priority_queue():
-        return collections.deque()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
