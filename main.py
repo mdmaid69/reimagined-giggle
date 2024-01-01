e9b41a10b214b9708123964b579b3e2e2e1a5428
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:06 +0000

    compete crowd day wind feel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
