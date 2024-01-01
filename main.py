29e17e0de492fe6506ea393825096151f4ef74f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:56:23 +0000

    criticism steal bet otherwise congratulate

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
