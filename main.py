2a1305de097dfa2512546c1b1c439bccc9c37255
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:57 +0000

    clean dull break lay pen

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
