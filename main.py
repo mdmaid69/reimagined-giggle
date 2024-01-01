5064d93f25d4c0e092db0ad8211f04e5eed6bf79
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:29:06 +0000

    control personal cum there further

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
