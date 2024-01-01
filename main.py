6e44c1ea7fbeaeaff27cefdd3b0dd6a70f3fec9c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:14 +0000

    again card message thanks mail

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
