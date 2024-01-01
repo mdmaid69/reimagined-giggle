bf788ced1fa1acbd9b8ab3708bd4c8a3f5cb8276
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:33:23 +0000

    somewhere score plan which see

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+print("Hello, world!")
