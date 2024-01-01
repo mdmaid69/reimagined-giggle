322dca1cb60e3c3578953a33bc855f0065e561ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:30:23 +0000

    silent male instruction profit plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import array
-def get_array_as_str(array):
-        return str(array)
+def pop_from_array(array, i=-1):
+        return array.pop(i)
