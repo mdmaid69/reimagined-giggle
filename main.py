d515e54aaad5686a74ae7a7b3c21d308ff9c0a15
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:59:10 +0000

    wear emphasize steal will niggaacademy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
