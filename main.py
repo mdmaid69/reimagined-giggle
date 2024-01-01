9be7a02aa7c1f013f3fd1c31e57b7cf20e335cb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:06:41 +0000

    hurry sweep answer object against

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
