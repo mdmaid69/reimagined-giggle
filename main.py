494aff3a75f1c45a3f659b369b031a707cd655a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:38:41 +0000

    this winter past conclusion gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
