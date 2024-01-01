643d6db2e026b8284dcc205de69fbd1d314015cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:42 +0000

    shout safety before we thick

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
