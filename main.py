cf26cfe597b7f4b94dfc4b1086a08397ac96e1fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:06:07 +0000

    Ll keen muscle cut anybody

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
