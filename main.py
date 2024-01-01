5bad24c7fdf710d555152f25c228c131018b16e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:07 +0000

    variety instruction preparation general balance

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-  def cube_number(x):
-        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
