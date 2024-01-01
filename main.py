0bd0f61aba8a980b660cf219273c2d9019fdfe23
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:23 +0000

    cold damp physical shadow exciting

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
