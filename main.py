4c294c6efda9151c386cb4dab02053368ed1a5c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:13 +0000

    manage lookup bringforward callat complete

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_working_directory():
+        return os.getcwd()
