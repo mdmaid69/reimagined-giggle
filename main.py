ce544b98b0042f587a4b0792dea34e4663f9b67f
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:09:34 +0000

    creature wonder closely anxious twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
