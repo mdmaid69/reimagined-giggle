fa29a591bd5355f4d0c03c4244acb85ca560cf37
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:42 +0000

    language float bad anything workout

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
