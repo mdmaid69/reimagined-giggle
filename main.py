876ce991054b4207d89471f517b1876d9319fbee
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:10:09 +0000

    opposite glad usual present tongue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
