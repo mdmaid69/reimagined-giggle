0f4f92070747717adcccb01e48ee1e6a603fc9ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:47:18 +0000

    skin risk train promise go

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
