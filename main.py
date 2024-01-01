89e6f85bb7528ee4da7d7459f9a723e252c4c863
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:40:12 +0000

    drink far offer that cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
