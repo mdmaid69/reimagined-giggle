b9e9e309cb9cfd06b44f6b0f9994987fda797358
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:14:54 +0000

    extra dinner off form clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
