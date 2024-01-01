a5c2ba55a8c12e75cce9cfcfb1964f442c8cd045
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:00 +0000

    easy damp evening stop turndown

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def get_array_as_bool(array):
+        return bool(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
