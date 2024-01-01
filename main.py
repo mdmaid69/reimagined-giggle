5a58f64b3966a8378749b6ff4c85f96183c3f93d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:04:20 +0000

    tip stay exactly drop manage

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def greet(name):
-        print(f"Hello, {name}!")
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
