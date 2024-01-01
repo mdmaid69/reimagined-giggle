9598d42f406effe00b40dac5cbed2178e737ebc1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:16:59 +0000

    collection control suggest debt black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
