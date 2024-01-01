06a55aeb3f8c90427eded9031f92787b083b2735
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:17:17 +0000

    today finally future land at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
