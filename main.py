3e16962597e7f38fc2eb135bc08bf997d45a66a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:49:41 +0000

    want house software why report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
