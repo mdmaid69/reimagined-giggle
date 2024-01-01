86edbaf082555111b1a622ea81cd74bbb8b8a12f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:39:15 +0000

    waste very star turnup bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
