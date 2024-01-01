61c4819c64dbe41ed118df47f5dd78259f3b12c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:00 +0000

    everybody difficulty fix Gg people

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
