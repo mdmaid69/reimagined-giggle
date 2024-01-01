b077de4e07b6452e0b1da31f295fda1882e51047
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:13:30 +0000

    single close chin will tired

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
