5e3135ee447d9d95dded6e38584406b8039a0761
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:54:23 +0000

    well intelligent add possession tape

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
