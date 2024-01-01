23ab32b9993fb5d4716125067626ae4edd27b7fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:43:42 +0000

    head combination set closed listen

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
+for i in range(5):
         print(i)
-        i += 1
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
