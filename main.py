9194a5923930cb529d01a508477917453e22a250
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:09:13 +0000

    exaggerate rent lookback(on) evidence feed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
