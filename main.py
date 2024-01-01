22165edbf6791f153615186924d74c351a62b6f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:47:50 +0000

    right interfere satisfied mind environment

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
