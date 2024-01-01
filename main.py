c48241cbdb477bcf6bc544c448d22bef97529c7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:15:47 +0000

    measure sport hate crash waste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_power(work, time):
-        return work / time
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def count_elements(lst):
+        return len(lst)
