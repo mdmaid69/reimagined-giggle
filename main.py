1a2e5992c0f80bfd5850fbd74a5808ee4662c459
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:57:16 +0000

    custom person eat possession fall

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def sort_numbers(numbers):
+        return sorted(numbers)
