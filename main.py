48cc0531f2d8926caf6774577376f5acaf3462b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:25:40 +0000

    square period mine now tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_os_info():
+        return platform.uname()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
