a69b5027396d0af8468f75a2067823f1b2ace6d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:46:34 +0000

    father purpose search key damage

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+def calculate_work(force, distance):
+        return force * distance
