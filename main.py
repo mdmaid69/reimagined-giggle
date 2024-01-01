709edf48093d4272367c737a2634ad9b304b6988
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:28:35 +0000

    classroom slight port business present

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def convert_to_binary(n):
+        return bin(n)
