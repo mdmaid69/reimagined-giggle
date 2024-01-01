33b9ad44b4a3974f890d688a654354dd40677b19
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:34 +0000

    Dd pretend uponon nerve sure

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-def find_min(numbers):
-        return min(numbers)
+for i in range(10): print(i)
+import numpy as np
+print(np.array([1, 2, 3]))
