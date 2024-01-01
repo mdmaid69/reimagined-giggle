7ad1ea594f733fd13314f15ad65c41d235076323
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:25 +0000

    damage discover until that niggaacademy

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_current_directory():
-        return os.getcwd()
+print(sum(range(10)))
+text = "Hello, world!"
+print("Reversed:", text[::-1])
