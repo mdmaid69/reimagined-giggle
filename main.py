6aaf3fe8579fbaf37ae37db960c1295bd6ffac9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:42:15 +0000

    child lesson obvious second care

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
