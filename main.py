2f2d66f420aa855bfa3e92c16783551205d79ec5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:25:31 +0000

    now finish trust fortunate reliable

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
