decdbd4669bc77982a54e6a359ad7c5f9a1cd529
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:12 +0000

    crisis breath minimum weekend top

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
