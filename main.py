f33fac841f59fe2a7f3f9d581bafbc36f4ff6bc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:10:38 +0000

    play shoulder contact successful pure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
