f7eb38934b44e5c3d177288b3ca1d6c967005843
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:43:56 +0000

    although peace wise some ride

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
