a947284cb9e161c9a5df60dabaeccd6a03459fcc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:46:41 +0000

    curious crash commit teach look

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
