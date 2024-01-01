01e0a8652606265ebac1af119c43941491343510
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:16 +0000

    cheap natural whoever possibility Qq

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
