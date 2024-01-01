f2b9dc15d1c12855b21723c91528df22759a5d14
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:39 +0000

    cost house clever open present

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
