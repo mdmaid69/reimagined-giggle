c2f6a5afa6d42c3b493fc8179938dcfb648c57bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:57 +0000

    forever form cause lift custom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
