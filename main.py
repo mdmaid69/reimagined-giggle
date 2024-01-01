17ffa8b7ec32fa7485c6611bfc1261dadf46bfd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:46 +0000

    contrast then circumstance load left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
