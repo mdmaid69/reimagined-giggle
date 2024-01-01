3c08d626a9e2c89acac8d12f32fa0f62b742bd2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:17:34 +0000

    brain cope thinkthrough dish judge

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def is_odd(n):
+        return n % 2 != 0
+def calculate_power(work, time):
+        return work / time
