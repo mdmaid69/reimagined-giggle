639f0e6b13c9ccf898f4387d21b28e94a343d935
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:15 +0000

    medium combine rise to wish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
