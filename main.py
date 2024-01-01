c7bc9dbca78becb9035ae4f98626ebea5911b071
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:00:05 +0000

    otherwise backwards sea truth phone

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+for i in range(10): print(i)
