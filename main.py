d09224331b68498e6098ba1d0cf9144d183c9eff
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:39 +0000

    major fly bringdown progress sleep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
