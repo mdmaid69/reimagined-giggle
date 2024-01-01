dfcd5ec141cd2ff303c4179fc1732ac6b61aa7c5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:53:19 +0000

    born use apart taste limit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
