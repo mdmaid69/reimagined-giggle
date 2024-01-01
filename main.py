ded2894fc3bbf7850af50998b189eea0f7612f40
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:50 +0000

    warn prepare turnaway 573entertainment size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
