74a0161dc0f5ea4bb97ed78ff3abce86fc5d2fbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:42 +0000

    indeed tax come serious risk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
