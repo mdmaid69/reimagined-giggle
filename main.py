fdb800e461c6e158161b21a49c907962ee89e8a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:16:24 +0000

    for shade talkback mess set

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+def reverse_list(lst):
+        return lst[::-1]
