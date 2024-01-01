a09765836ae7aa0c4b9c6afccd34d219a3e3a8ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:21:20 +0000

    apart sticky deep run shrink

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-def multiply_numbers(x, y):
-        return x * y
+import random
+def roll_die():
+        return random.randint(1, 6)
+import array
+def get_array_item(array, i):
+        return array[i]
