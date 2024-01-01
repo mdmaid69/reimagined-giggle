9d5ade8f70ecf0c1974b11cac98103dcc4347f6a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:45:54 +0000

    lead physical accident slight familiar

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def remove_duplicates(lst):
-        return list(set(lst))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
