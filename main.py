78b40c7911b688092f39a18df6b1f4d2ec7e1d8d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:22:31 +0000

    bye challenge welcome Ii kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import platform
+def get_os_info():
+        return platform.uname()
