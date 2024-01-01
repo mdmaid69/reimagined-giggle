7fbd0e2630c81ee66811e4543a175bfc5378f269
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:05:44 +0000

    keepaway common different lucky spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
