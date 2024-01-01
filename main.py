fb6a094830e3fce80a81a6aec6f4b53e6f9aa3b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:15:14 +0000

    still defend name search children

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
