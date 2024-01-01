596a361fecebad42eb7b7ee029aae1a139b7b776
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:14:30 +0000

    either tap cum spill capital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
