02ceabb3098b8ec474b7fdaa9bb6b730fc333092
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:15:11 +0000

    talkback limit sort mother workout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_current_working_directory():
+        return os.getcwd()
