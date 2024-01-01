d31a15add98c0a1493b3f370e586ec4653bac7c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:01 +0000

    finger admit each vanish quality

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
