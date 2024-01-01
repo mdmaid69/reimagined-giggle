40eedb6581d7c38be1d5b179716be875aa1299cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:09:04 +0000

    rely impatient today recovery likely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  def is_odd(n):
+        return n % 2 != 0
