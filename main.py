546381be94db5c17cdcdfa552f3263eebdf347a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:22:20 +0000

    ring lean lunch street as

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
