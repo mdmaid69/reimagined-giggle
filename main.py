a1f975a448254abe861fe6bdfb0b80b43bea4958
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:00 +0000

    goout recover at taste proper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_factorial(n):
+        return math.factorial(n)
