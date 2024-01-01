7725851173718e2f3a47a5ce64b4a3b4d0caeef1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:39 +0000

    shame tired let mass strength

--- main.py
+++ main.py
@@ -3,6 +3,5 @@
           if n % i == 0:
         return False
         return True
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
