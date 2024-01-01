61275a925754ed560a2fbbf382df968ebfb04d44
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:08 +0000

    imagination height attract height uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def add_numbers(x, y):
+        return x + y
