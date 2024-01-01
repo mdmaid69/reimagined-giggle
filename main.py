bdf2fab749793cae901650d43aa4e6bab76da73c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:41 +0000

    she authority sorry length downstairs

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
