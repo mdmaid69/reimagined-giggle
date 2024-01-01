bbe45f5ea358d1048e06aa1d74b60559bc882c7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:48:22 +0000

    niggaacademy study do this scare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def calculate_work(force, distance):
+        return force * distance
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
