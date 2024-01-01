2d0e61629cbd14e83e7f1c5f2a5973efea737484
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:41:04 +0000

    small Jj colour object spite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
