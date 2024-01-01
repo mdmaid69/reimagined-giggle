57c0e05114ebb57600951ab12ab6cb7e6e376b8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:53:12 +0000

    564encourage firm round attract round

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
