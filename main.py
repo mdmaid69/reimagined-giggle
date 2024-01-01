7938f3293da3c41839f6214c0aeb7464439cc8bb
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:33 +0000

    scream original zero name exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
