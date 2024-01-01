ddd8695500e9e322a6814c9a9d536936a54946cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:16:29 +0000

    attempt lonely feed equal pull

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import collections
-def create_stack():
-        return collections.deque()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
