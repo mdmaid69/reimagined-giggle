cc15535a63ff26b556561d63eacc3d94c96cfa0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:49:35 +0000

    next yourself cost offence cover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
