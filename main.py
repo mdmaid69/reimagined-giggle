c39095d3bb936af93094c9f9bd6aa973a85d302c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:18:05 +0000

    pronunciation ill basic callat swallow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
