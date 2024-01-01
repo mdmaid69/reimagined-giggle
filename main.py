c8d05ade2788b30f1a9d4694be0fb6dc8af8b380
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:51 +0000

    start tear relation term important

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
