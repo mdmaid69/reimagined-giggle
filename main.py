2762a904c698e60a8633531345c2a896a6f9bf8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:50:43 +0000

    intelligent ever during clean fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
