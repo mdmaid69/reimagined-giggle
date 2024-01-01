8767da7068a95a1e0c57dd06e63c7002fd1116d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:49:54 +0000

    judge afraid feeling undo later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
