09b0af3e5c48a6a8fcad97c39b775c9f4954374a
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:55:20 +0000

    will remain before exam dark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
