8b4662e535d3b025d771b43ce93ec5665ae8a11e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:07:20 +0000

    usually terrible inside cool slip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
