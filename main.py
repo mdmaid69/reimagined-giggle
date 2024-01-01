798a2d0f07e61a0f7d5e49b1d36cea6edd128d71
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:17 +0000

    support damage giveback if identify

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
