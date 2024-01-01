307d04637900387cbec120ab0429aaf107ccde7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:59:13 +0000

    check appeal sit battery fashion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
