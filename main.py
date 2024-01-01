1101b4548f99ee66fe58eb10e60cfc2434f88967
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:44:45 +0000

    interrupt figure afraid hang fact

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-  def is_even(n):
-        return n % 2 == 0
+import numpy as np
+print(np.array([1, 2, 3]))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
