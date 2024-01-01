6ba155ce2b8c471b6861617a7ee38b2dcebb3602
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:24 +0000

    rush she everyone tax tired

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
 import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def get_array_index(array, item):
+        return array.index(item)
