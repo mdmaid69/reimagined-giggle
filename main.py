97cfbfc71287dfe9047f1d690bebae9563db6fdd
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:52 +0000

    appear of else follow himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-def convert_to_binary(n):
-        return bin(n)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  def count_elements(lst):
+        return len(lst)
