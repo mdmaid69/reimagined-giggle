c32ad6796823f57f60e6d603fa605f76a02458fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:11 +0000

    represent favour miss rest blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_time_since_epoch():
+        return time.time()
