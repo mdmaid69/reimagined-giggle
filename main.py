97cb70ed9bf0c5738283b4bab5875ba877c35202
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:58 +0000

    telephone risk ask comeback it

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        print(sys.version)
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
