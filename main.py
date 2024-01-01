684316cbd95e00711a8e0527fd5db986f7cc3476
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:51:19 +0000

    path meeting influence warn story

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_stack():
+        return collections.deque()
