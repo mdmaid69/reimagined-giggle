65e58213cec30ddde2fcf0ce3528347344d4aed3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:23:14 +0000

    always recovery shake achieve sing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
