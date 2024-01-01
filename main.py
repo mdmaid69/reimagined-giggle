c2980b1826ae17bdef886ed81bd94e9c5b1c09fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:51 +0000

    fill pretty press reliable near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def find_union(list1, list2):
+        return set(list1) | set(list2)
