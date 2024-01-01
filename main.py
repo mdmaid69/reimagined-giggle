105c2c4329682b42e70b2256673ca4c40a323ff6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:30:30 +0000

    switch official protest land pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
