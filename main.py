93afb543ee6b19b0c1202f8cbe40cd13f413b569
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:09:35 +0000

    both government spare deaf comefrom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
