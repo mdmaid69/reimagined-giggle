4ea2845b5ee73e000a44c4fb354492f236fafe92
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:59:47 +0000

    involve careless all fit themselves

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
