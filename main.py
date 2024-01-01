1bea5dfda1813002b9bb8236910cba8c12f4700f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:37:10 +0000

    contain yourself replace indeed right

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
