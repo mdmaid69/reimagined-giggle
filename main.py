f75b3f6edb74f66e7e21de7e4f0d26ebd44fe297
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:02 +0000

    butter total be before scream

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+for i in range(10): print(i)
+def calculate_density(mass, volume):
+        return mass / volume
