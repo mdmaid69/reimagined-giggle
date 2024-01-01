2da87039caade0859584c035ccc75ca673e79948
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:10:50 +0000

    congratulations available weak tip comeout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
