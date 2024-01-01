4d95469475a4d15cbeb37adfa426518ee48541f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:31:07 +0000

    on now compete understand Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def is_palindrome(s):
+        return s == s[::-1]
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
