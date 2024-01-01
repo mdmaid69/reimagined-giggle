c6cd21521ada71ee628565b1075e2dcde8c6e45d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:39 +0000

    concerned amount exciting ill pair

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def sort_numbers(numbers):
+        return sorted(numbers)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
