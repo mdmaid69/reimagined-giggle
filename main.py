1256b386bcf57072e8c080f94a6a6702687f5025
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:37:28 +0000

    marry edge division activity squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def sort_list(lst):
+        return sorted(lst)
