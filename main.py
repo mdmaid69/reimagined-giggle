3bbe457dc5197a3ed14adf41e505b19fd023acf7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:56:22 +0000

    understand neither contact takeback up

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
