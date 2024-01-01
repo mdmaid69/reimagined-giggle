01fb23ed2d23bfff6ad050f7f2fada485901b6ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:05:20 +0000

    age learn win squeeze all

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def count_words(sentence):
+        return len(sentence.split())
