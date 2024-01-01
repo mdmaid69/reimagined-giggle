60f0bfc572ac8c4c3ca03a5042535608ef26f1b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:10:41 +0000

    artificial how them weigh ready

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def remove_duplicates(lst):
+        return list(set(lst))
