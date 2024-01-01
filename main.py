a04400433d2d57e81d19ddee2b262a154ee63667
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:44:30 +0000

    fact contribute greeting wish it

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-import tensorflow as tf
-print(tf.__version__)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
