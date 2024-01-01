c5681e13caab32f7d50271e6bcd6a5c88cc59d42
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:22:26 +0000

    board open file example may

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
