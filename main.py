32bcebb3aaa0abcbae947e4ef92f7c15388dd854
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:26:59 +0000

    arrival break rich runover turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
