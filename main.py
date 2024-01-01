9f04cbf593c5b63edc3e6dbdb85411ca6ab65fd8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:39 +0000

    now child I miss convincing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
