31561bf8ee98f61aab6e5fca9539efa86010ea91
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:07:23 +0000

    attractive chase upset vanish yes

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
