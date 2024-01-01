ff86789509b701d95381d133b1f0361f72bca614
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:55:16 +0000

    basis thought introduction trick become

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import datetime
+print(datetime.datetime.now())
