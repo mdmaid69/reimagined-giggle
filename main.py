6d1076645c122413c53ff6e7a6d31b8f0cbfc9a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:44:51 +0000

    violence under rob glass fine

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def remove_directory(path):
+        os.rmdir(path)
