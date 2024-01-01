c87bef88deff08808536fd3245f93da5bd242df7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:23:21 +0000

    tear cool picture beat completely

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
