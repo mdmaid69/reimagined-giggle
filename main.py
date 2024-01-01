d333f473b7a85e010c2560e6044afcb6156ba6bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:50:32 +0000

    tax carry platform regularly themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
