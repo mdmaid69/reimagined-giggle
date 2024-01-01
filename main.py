7f26af2930c80718e9666b3f37a8954721892897
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:52 +0000

    estimate confident finger like fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
