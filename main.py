c3f1d08d65cd257681731a53cbc3add66aa61063
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:07:00 +0000

    meeting wide than coat match

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
