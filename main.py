299cc45c36e94cc5ab0916e115d1b0bccd727107
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:01:37 +0000

    decide bear curious characteristic instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
