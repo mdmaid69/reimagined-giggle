240479c2471912397954c9e671440fcea99a9b16
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:13:29 +0000

    snow embarrassing provide goaway central

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import collections
+def create_queue():
+        return collections.deque()
