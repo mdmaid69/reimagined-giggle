38b109a05524f013862e81bf1adac333653b1096
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:46:47 +0000

    callback combination careless judge adjective

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
