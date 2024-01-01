e5ca73cc7569de3239299aef94607c43ffa766f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:05:38 +0000

    whistle prepare almost straight sea

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
