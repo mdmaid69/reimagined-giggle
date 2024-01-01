901ca84ed27a2f32e756814441a3012f5d650420
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:47 +0000

    shut neither stamp other pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
