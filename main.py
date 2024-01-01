1293ccac6542ba0f6e9280e0f45af1977604abb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:00 +0000

    smile petrol list ever run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_priority_queue():
+        return collections.deque()
