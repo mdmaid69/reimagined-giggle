561a66122614a8e763bb1d5dd068974a63d992ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:51 +0000

    block risk warn swim takeaway

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
+        return os.path.getsize(file_name)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
