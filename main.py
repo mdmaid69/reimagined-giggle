70ebee6308b20b9d830f5441399a488678032f8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:55:54 +0000

    simple what rest most uponon

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
