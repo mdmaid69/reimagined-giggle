01a40561efcbb1b4074a998eda927078299f39ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:48:02 +0000

    constantly float answer from gentle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_stack():
+        return collections.deque()
