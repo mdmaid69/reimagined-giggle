06aa2a5f3f58275501eec2d028f1cfc8623a5ad8
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:43:40 +0000

    cope here miss nervous of

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
