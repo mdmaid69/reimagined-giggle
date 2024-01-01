6d2e2caa6139715e1630d72027069c943c763833
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:52:12 +0000

    support surprise serious door post

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
