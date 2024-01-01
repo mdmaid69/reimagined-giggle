90ec171fd67f982e4505548a9060e3e3f42db0fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:47:26 +0000

    though hurt during 569enjoy size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def convert_to_octal(n):
+        return oct(n)
