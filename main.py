7df48eb0a47b5387368a507283db5fa6f3aa6362
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:11:44 +0000

    awake nigga beyond himself flight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
