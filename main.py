9e91a452b577047f7ee010e04be51f74bb747a54
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:45 +0000

    weigh consult minute whoever wish

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
