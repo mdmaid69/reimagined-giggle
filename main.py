be2bca74705a349f16c976c6c260467b186771d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:44:28 +0000

    congratulate 566end now depend earn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def cube_number(x):
+        return x**3
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
