0e51e1dfa28fe69253ec4ced0363e9b61038b11c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:59:01 +0000

    year principle share apply next

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
