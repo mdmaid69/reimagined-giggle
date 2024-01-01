4b047ad2e7a7e0836670c11e725da866eb7b696e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:13:25 +0000

    petrol Uu opposite concerning stranger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
