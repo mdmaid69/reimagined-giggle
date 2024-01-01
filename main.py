9399647aa2c2f3db2cd41519064311d1612cb493
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:13 +0000

    secret culture wash whistle brown

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
