55afc8870718af7cb21e2b57602783d431cc9955
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:15:25 +0000

    able wrap always promise generally

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
