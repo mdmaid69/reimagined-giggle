46c9001a1da988b86cd0063953006e59a0dadf8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:37 +0000

    thinkover extra pipe admire secret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
