3963b79ce7cb9569c973ea6ebe4a002317c6670a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:41:34 +0000

    everywhere passenger in argument every

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_area_circle(r):
+        return 3.14 * r**2
