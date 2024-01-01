bb7613a8a8b59c27495ecd18aa9a6cac4705435f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:49 +0000

    sit cope another skill stand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
