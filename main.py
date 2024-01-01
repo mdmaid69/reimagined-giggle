71aadbbd35dac719955577b04eb1aa8a58294007
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:34:41 +0000

    charge often care curious cross

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+print("Hello, world!")
