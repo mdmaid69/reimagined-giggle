e10fb8b393ae911c551b0b03dccab25541cdc262
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:40:09 +0000

    sign punish male punishment general

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
