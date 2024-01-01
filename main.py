a1ae836217432716a24b2024ee8538429197a7d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:49:14 +0000

    warm far need herself rest

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 def calculate_work(force, distance):
         return force * distance
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
