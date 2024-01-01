3f05643f2c7f2665bac2adb10e3434065c28e103
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:57:01 +0000

    somebody trust suffer prepare aim

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
