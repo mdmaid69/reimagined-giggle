7ec3b581bc4b7246402b4450c6067ffa655bb84d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:09:44 +0000

    one careless secret convinced takeback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def subtract_numbers(x, y):
+        return x - y
