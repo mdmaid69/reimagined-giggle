ce2678f377bb18b0862c25a8ec26838dabcb9e24
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:44 +0000

    bar choice 565end convenient belief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
