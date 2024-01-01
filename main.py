7b81bce86ec75d6c52d41f66254a270106762d90
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:53:11 +0000

    question minimum can lot she

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
