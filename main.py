b6835afbf362e36e34bd8c4f6c512e3cceaa409d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:21 +0000

    low more sign line ahead

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
