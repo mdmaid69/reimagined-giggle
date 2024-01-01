0c2cd949b8ae0a4137def74ed58adbf7d56da89b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:50:04 +0000

    blow 565end debt away meaning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def remove_duplicates(lst):
+        return list(set(lst))
