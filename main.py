8251ae0fb131d4409e6f94c44292f88013687585
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:02:44 +0000

    before direct pool course tongue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
