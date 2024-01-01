9595f3c6206cbf4a93b6c4b4944fb50427601e93
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:51 +0000

    recent protection mend lunch with

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import sys
-print(sys.version)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
