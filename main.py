ea79d80bdb46e94fb32ec03f5a6b335ddf8b1205
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:34 +0000

    teach service sell with clever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
