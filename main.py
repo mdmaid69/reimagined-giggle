8c5773e750cbb5a5e38162c9d1b4d8047b8ac48b
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:38:58 +0000

    anywayanyhow hospital history laugh bang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def append_to_array(array, item):
+        array.append(item)
