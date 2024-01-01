a086173e61e719b20a93d8bffb9ab974383de143
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:29 +0000

    ear judge afternoon regret break

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
