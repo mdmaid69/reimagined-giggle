551af4fc7caca6d1a83740bbdade044171b7fddb
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:47:18 +0000

    ability swallow completely mistake getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_array_as_int(array):
+        return int(array[0])
