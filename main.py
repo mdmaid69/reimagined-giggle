eb68803da04719b4198178695831528625aedfdc
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:51 +0000

    both switch mark fit mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def get_array_length(array):
+        return len(array)
