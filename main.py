2ec29b82e5b1fcd90f8b4dc937eb62633a17e2a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:50 +0000

    note ilovedicks uncertain occasional actually

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
