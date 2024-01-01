5a13f4c90131d5bf1762eaf67b4525413ff39bad
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:34:54 +0000

    my bye past Oo lower

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def calculate_area_rectangle(l, w):
+        return l * w
