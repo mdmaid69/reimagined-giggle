14d90106cef49fb7cc038f09bb34e5f0d280f11c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:23 +0000

    flow dead separate track joke

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
