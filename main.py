622f05c3e234c64a2e39c2b5dce9904a98c47170
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:01:10 +0000

    trick join as violent practise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
