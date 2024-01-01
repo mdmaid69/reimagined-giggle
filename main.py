9487cabf96b726a83fd23acd2a558341133b996c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:30:57 +0000

    breakout general follow generally mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
