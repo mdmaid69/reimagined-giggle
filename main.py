643d069db892a3ff7813d03c710f60675f76a6e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:04:22 +0000

    sink plate swimming stop bus

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+name = "Python"
+print("Hello,", name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
