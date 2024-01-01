2b712bb2b38fa96d3616be0b705bbf1cc97bc580
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:53 +0000

    fun narrow real fall hospital

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
