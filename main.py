e7eb89737fa0e6320089c3a4890f063e862ad786
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:45:23 +0000

    century rescue pleasure punishment impatient

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
