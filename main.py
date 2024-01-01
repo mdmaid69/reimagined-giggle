f3dee10acac30d9553d4f46ff6ca699a456244d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:20:59 +0000

    high final past story advanced

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_work(force, distance):
+        return force * distance
