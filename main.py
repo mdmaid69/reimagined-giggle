9395ce0eaf80ccb55e7d2a712f4db08bfa5a9926
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:00:34 +0000

    original innocent regional any whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
