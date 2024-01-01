79565efd01383df10a34ebcb2594c14f40f0f114
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:45 +0000

    into on forgive study thinkof

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def multiply_numbers(x, y):
+        return x * y
+name = "Python"
+print("Hello,", name)
