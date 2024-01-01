8a2bb9e0c9ee5d578be41cca692f76796afd8203
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:20:00 +0000

    bend cap cold nose on

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
