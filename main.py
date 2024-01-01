4d8c595747c8ca3b6350d019d987780d45811a9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:29 +0000

    art anyone our wash write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
