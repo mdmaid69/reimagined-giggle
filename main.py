673390fb16a6af6c52c5c37e51273f2e8ca45ce0
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:43 +0000

    figure action gift result push

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def get_array_length(array):
+        return len(array)
