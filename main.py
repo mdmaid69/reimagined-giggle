d920721e8eb044c034d379d9e0d216ae00a3a0b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:00 +0000

    classroom early beyond combine entrance

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
