0e98a928427e52a92c9c1915e4e4aab1e2f9275f
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:29:21 +0000

    fault suckme wipe play freeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
