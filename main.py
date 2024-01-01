3f0ac1e04ef41b6c240238d8e2e5c3f731cc3060
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:27:35 +0000

    such turnup produce discuss steep

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import sys
+  def get_python_version():
+        return sys.version
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
