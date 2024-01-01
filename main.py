152ff6bc8a3af505871728683fa59fcdea816ebd
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:17:02 +0000

    you smile sky recent movement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
