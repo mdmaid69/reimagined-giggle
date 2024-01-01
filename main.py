0bc542befbf2ea97b8c8704193071cac3a7a4c09
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:16:32 +0000

    part near below foreign Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_tangent(x):
+        return math.tan(x)
