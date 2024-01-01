52d86b8c8a640330f470c50afd5624e5121e845e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:36:46 +0000

    communication safe test his ourselves

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def add_numbers(x, y):
+        return x + y
+import array
+def convert_array_to_string(array):
+        return array.tostring()
