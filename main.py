f89357d5af2ed69bc64ad4c7c428aec3fa0d01a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:06:50 +0000

    drawing stop only charge unlucky

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
