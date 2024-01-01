8ec8520ba791dbb45f73c898fe186023d107cf48
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:16 +0000

    while pronounce everyone attention smell

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
