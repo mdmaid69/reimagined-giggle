b290363bb63c55d42e644fca55bc2c98debe0b3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:28:14 +0000

    mood react yes future bringback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(a, b):
+        return a + b
