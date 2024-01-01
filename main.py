8bd35c72ef987b4c767c8aa9e8fea043977ba941
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:51 +0000

    same as culture train priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
+def calculate_pressure(force, area):
+        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
