e118eecd7243ff0a8c29abc855bf9c8651f69f17
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:22:51 +0000

    trip reason or count place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
