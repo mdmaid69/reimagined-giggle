81f0a3072962cc5a1c48f37ecaa159329c47b7a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:42 +0000

    education vote mass from behind

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
