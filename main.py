4b7ea0889d48edbba471fe1328338f241e5171db
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:16:29 +0000

    put finish lost wish foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
