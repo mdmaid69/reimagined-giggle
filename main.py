80ed5aa8b716b1339ea57f5d9509e88ce7697c74
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:52 +0000

    pretty bring smart record opponent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
