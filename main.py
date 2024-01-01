a16f13c388c949770f2cb3b55b25e1fe2ea11f7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:11:03 +0000

    lose push otherwise emphasis steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 def multiply_numbers(x, y):
         return x * y
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
