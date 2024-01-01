cb486b859e44dc5d1738ff40903fa3eac9a917a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:56 +0000

    tough why nervous uponon loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
