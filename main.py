a22672a6446d7dd0decaad37cfe4755a14ccc80c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:35 +0000

    holdout table acceptable be eye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
