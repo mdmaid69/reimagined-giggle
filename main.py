f7d690089023eaa996466f48c9c481b0361ec277
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:43:59 +0000

    unpleasant often opinion limit sink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def convert_to_hex(n):
+        return hex(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
