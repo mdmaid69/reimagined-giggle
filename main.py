9ce3191e7fe97bedf3ba6a6c564f08265b2f7d5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:24 +0000

    objection eager escape performance avoid

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
