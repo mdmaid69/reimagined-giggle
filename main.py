0b4ab24695c672295eaa7a6273d367fd30895bd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:17 +0000

    Ss damage contrast same silent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  def multiply_numbers(x, y):
+        return x * y
