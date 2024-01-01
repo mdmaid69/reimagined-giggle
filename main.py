2c41bc5fb122d6f86e003998d4279a3c73a5f31e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:35:43 +0000

    shame care off nerve fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sys
+def print_python_version():
+        print(sys.version)
+for i in range(5):
+        print(i)
