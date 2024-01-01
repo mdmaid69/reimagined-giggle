f5c900a95c80bdbcb32dcf0159ab753a52264ac0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:22:17 +0000

    once closely lonely shadow argument

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
