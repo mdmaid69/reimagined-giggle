e542a35cbf4b119ef3d6b843d6aec7ab8745482a
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:46:04 +0000

    bit either fill sale trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
