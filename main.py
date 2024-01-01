a8b0483861b8c6e24602c6e3411fd3f80fbae48c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:18 +0000

    people suggest case live occasion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_length(array):
-        return len(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
