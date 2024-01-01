bb4184c3c6fbfb5e78809f211fd02c978942006e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:49 +0000

    before mix basically information any

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def count_characters(sentence):
+        return len(sentence)
