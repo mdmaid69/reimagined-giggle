66fcacf544dad6bfc4a567444bfe973d68c5d490
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:40 +0000

    sad find takeover accept confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
