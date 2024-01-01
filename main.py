98f39ed47e5af84c34b2834d2df508a817d8b4e4
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:36 +0000

    protection move order hurry closed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def clear_array(array):
-        array *= 0
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
