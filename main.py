9fad75fe256fc82a9dd0c64fa2cec6a58ff323fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:11:05 +0000

    go well Ii pupil which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
