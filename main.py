fb116d88f47e60f264f6d1b3dd2c93209dda9c7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:04:37 +0000

    last believe Dd power way

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def square_number(x):
+        return x**2
