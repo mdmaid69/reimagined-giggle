834f139ca33a2a455fde2c8dc23d3e0c610ff20e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:39:10 +0000

    pencil successful content owe fair

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+print([x**2 for x in range(10)])
