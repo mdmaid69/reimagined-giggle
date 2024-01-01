c5022695811034bcfe63dcd105b464fa998c4f83
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:06 +0000

    frightened demonstrate loss godown jump

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import array
+def get_list_from_array(array):
+        return array.tolist()
