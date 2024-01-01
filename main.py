2edf60321c5c5c7d850903cdd25d5d3d12ac235e
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:23:18 +0000

    throw band if same popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def find_max(numbers):
-        return max(numbers)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
