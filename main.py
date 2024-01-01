480aab8db7e8f810c7e5ae0a43e366e634ee4d14
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:47 +0000

    interrupt edge giveback everyone wind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
