a87f6016c4fb434d209fe1aeacded1e01d9be799
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:28:43 +0000

    impatient callat cut myself eye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
