5161c2e0caec2991af46c1c526f899bba1fa24e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:24 +0000

    study so now brother clothes

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
