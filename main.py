6d84d254ce3f49a5f17baa576b439761f9eff5ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:15 +0000

    then tonight pay cough print

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import collections
+def create_queue():
+        return collections.deque()
+print([x**2 for x in range(10)])
