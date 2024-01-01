3242c686851a00498c4c9c8c58a9bc0ac755f53a
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:18:28 +0000

    date immediate error include many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-print("Hello, world!")
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
