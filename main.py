76ff5921747035166cfbaf9759a980efe44f74df
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:29 +0000

    distant short party practical government

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def reverse_array(array):
+        array.reverse()
