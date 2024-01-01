152bb0d4dddd8cc19ed05a2024275900d12e37ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:49 +0000

    speech success indeed difference contrast

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
