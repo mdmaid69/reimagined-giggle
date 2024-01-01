fee9ef8c9e0d6523fd0e36a742c21a68f1a1d947
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:26:09 +0000

    minor also improve hurt tooth

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
