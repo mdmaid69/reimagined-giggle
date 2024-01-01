51bdf4ca023a7c4cfc71835059d39ea207b1cd8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:05:43 +0000

    hurt arrive Bb getback work

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
