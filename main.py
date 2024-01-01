fa505e78e3bc8520c60cf511ff3a8360d0a406e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:55:58 +0000

    exhibition takeout depend scrap talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
