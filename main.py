d29eda0b8c1e48987479f5e8169a9f13c6f90954
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:09:14 +0000

    underneath pretend speak discovery suit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_unique_words(sentence):
-        return set(sentence.split())
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
