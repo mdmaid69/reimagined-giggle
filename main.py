f4f12d3cd8ee5eda95e1f63d9210873f2c20412a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:31:57 +0000

    stamp branch question unit fail

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
