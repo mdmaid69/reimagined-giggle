cf0aa028e6cffcc9682e9b8b3e8a4b90b2d26428
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:08:37 +0000

    passenger price they visit ordinary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
