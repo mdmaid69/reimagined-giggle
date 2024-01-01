e80371d40e72122e8393807e4e5705254ec72005
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:14:28 +0000

    write behind purpose sweet mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
