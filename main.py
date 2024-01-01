29cad8a58e06df24687528a8056ae0a5c127e106
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:41 +0000

    advise getout communicate juice explanation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def get_array_slice(array, i, j):
+        return array[i:j]
