1716ba769b69a82551b3f41a3ff4805510e73418
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:12 +0000

    contact afterwards turnaway somehow peace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_error_function(x):
+        return math.erf(x)
