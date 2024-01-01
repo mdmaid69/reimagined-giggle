2b4494cc34bc6b9c9e81d742226a7a84e39c4118
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:51:28 +0000

    sink wish travel delicate Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
