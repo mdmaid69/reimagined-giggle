4d565755a11cca3b48fceddeaa0fc8d2bac2a37a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:13 +0000

    majority steady pretty friendly air

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
