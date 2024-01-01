b4564a15ce03d327af8d81bdaa6c19faf51934ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:34:42 +0000

    afraid protect share gooff Yy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def is_odd(n):
-        return n % 2 != 0
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
