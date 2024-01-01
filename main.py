eb6713839cd43cce8a1dc2560a463e64fc403731
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:46:07 +0000

    bin else reduce measure poison

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
