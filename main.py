878339a82be2f904bdd8d6caa838348e23f627ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:17:31 +0000

    emergency stretch than treat centimetre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
