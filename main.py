c2062d8619ab75f5be699dd28109e613d962fec0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:36:03 +0000

    complicated sleep way climb thin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
