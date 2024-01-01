ebcf0d04c6c305e4255af747bc6f44261e5e20dc
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:59 +0000

    regret responsible Tt ability receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
