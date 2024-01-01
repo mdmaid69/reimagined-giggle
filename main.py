08d92524ec9d82850ef2ff620f0bfb275a7bee4d
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:12:52 +0000

    environment survive head country study

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-for i in range(10): print(i)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
