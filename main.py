341d9b5903168a294d969eb708e4d823a109a627
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:19 +0000

    certainly hard pressure object before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
