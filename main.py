375bec5f4520949c7885584d2229cb3470b55cd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:33 +0000

    skin Dd left trust male

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
 def calculate_amortization(principal, rate, time):
         return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
