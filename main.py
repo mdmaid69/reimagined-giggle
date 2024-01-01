243730c89981c0a30be65eb4b6359a621cb26afd
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:41:18 +0000

    post acceptable concerning obvious I

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  def convert_to_octal(n):
-        return oct(n)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
