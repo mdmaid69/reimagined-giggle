9d08f7373e0781112ac1f4dbca88246d551f2aba
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:21:26 +0000

    who tend proud visit inquire

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def cube_number(x):
+        return x**3
