f62a028c951ee2ecb3a846c87282cbd69c63d41c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:05:40 +0000

    to direct breakout contrast usually

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_odd(n):
+        return n % 2 != 0
