0221cfe382526c121a9cbaec212938889fd2626f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:25:35 +0000

    must OKorokay callout science love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
