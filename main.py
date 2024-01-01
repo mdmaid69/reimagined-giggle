20c2b7467861c59a17701191aa6276bcd6731d4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:48:43 +0000

    excited successful number tired frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Reversed:", text[::-1])
