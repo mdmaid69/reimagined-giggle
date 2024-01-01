009b1fdc6e10a28a3819544f9994bc5a6f0c6f58
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:56 +0000

    entry mouth taste behind case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_area(radius):
+        return 3.14 * radius * radius
