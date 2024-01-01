f837b0b24d67de4fd4387aec9ef373e318203121
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:20 +0000

    whenever aim rough frequent stupid

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
