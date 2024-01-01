aa33b2d76f6aca31499a3cdb338269b0ef3d2f98
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:55 +0000

    vanish date share later close

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
