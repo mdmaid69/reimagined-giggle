c353bb05891dacf76f4b1ef294e91f43c287ae4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:08:08 +0000

    suit ilovedicks office breakin tongue

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
