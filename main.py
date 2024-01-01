4d1c4af3fd8fee4c6432edb3b28ea833e4afd4a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:44:30 +0000

    exchange quiet before shop behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_tangent(x):
+        return math.tan(x)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
