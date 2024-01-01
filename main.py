08f73039e1e9bd62079b0fb4e7b92a2ed8920de5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:01 +0000

    sink usual piece smell search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
