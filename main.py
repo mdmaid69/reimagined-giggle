451d91a5f59a41cdb4297ad2d452a35318f9d643
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:16:51 +0000

    shut base politics muscle view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
