c237825f79832b5cee561a46622f1456ffa65a66
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:37 +0000

    sky hungry reasonable plenty discover

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
