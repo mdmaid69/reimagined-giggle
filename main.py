adc1c8ec317e9872c641869f786b22ae301807a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:15:58 +0000

    early imagination mark noisy real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
