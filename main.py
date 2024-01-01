a73a6cdbf8380f0062cf1f712ee17bcf948df754
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:27:34 +0000

    delicate kind soil discussion mood

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
