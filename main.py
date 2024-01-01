e523f4fdfe6922be4f5db215a5087ddc9f185dc8
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:53 +0000

    occur handle treat either mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
