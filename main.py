0b94308fc7f1d17902f872051d4396dfbca0ec09
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:07:54 +0000

    fail suddenly welcome little many

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
