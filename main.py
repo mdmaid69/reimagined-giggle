56e995e3a1536e5165eb5ceba15176fea8499c52
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:45 +0000

    clue smell part tune special

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
