34e524ca9dca84668c98ec564828dca51534e017
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:29:50 +0000

    hollow realize possibly nice turndown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import os
+def remove_directory(path):
+        os.rmdir(path)
