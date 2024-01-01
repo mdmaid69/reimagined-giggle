a84cd84e43b8834d0d259dc92f3c6e4d91332d1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:35:55 +0000

    how impression flight fun such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
