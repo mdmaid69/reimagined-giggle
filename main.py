1a6feba938de9128f607a5a24a84a1d566191120
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:00:03 +0000

    prove talkinto fair awake meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
