5e95a86e80f5207565a665cc4a11c66f141e3bd7
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:31:49 +0000

    satisfactory entrance well claim scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
