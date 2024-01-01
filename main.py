d15c3bbe5991dac92d152cb23f8f6a9f3bcb4127
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:08:47 +0000

    size length hers mystery shake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
