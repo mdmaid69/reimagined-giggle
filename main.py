3a65efc8733a92a7b393024997212a5180fcd71d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:44:39 +0000

    slip passenger love travel man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
