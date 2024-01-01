5ce682cc3ec8daecf2606658ca3cb2a487b26e1f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:56:58 +0000

    language survive land nigga report

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def convert_to_octal(n):
-        return oct(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_area_circle(r):
+        return 3.14 * r**2
