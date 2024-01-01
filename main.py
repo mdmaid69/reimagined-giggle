ad814adbc6ecf3d05ad531d1e39d75f470537e59
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:45:40 +0000

    interview takeoff guilty dead talkoutof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
