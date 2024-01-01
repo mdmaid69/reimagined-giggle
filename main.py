79dac890a1db083d5c2959a42954dc7b28cce4a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:37:48 +0000

    total period strong block detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
