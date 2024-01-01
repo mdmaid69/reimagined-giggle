4961079c485a4adeb6f4696358a36184f8809a45
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:52:43 +0000

    bill explanation sentence cut strict

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  def subtract_numbers(x, y):
-        return x - y
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
