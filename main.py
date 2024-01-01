f96270a40bb269849968e1aa250e85d5b439a5cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:18:17 +0000

    please out hide popular concentrate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
