fb885c26003584bf683f5615463ce98c13dc66d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:57:19 +0000

    combine uncertain poison spoil sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_array_to_string(array):
+        return array.tostring()
