63b66185bbda03b833f8e326fc31bf19485ec586
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:22:37 +0000

    clean each land appear improve

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
