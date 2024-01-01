3fdf50912cf45a75e2d809a96666b278458c78ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:32 +0000

    situation comeover bother commit one

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
