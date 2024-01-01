b6631b8e176dd60f5bef7ea061eb94bd82decd68
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:25:01 +0000

    through sure temper image corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
