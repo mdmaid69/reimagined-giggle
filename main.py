52633c67bf3a8c6c4470fb7a2060ec22b08a7976
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:55:36 +0000

    pleased waste independent minimum voice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
