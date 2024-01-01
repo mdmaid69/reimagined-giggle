3d743a2f3bfd70886631f26604773d738ca26107
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:01 +0000

    raise climb sit beneath Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
