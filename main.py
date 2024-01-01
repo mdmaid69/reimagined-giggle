58ceac290bf16aa9f7315ac171d9e6b2368df118
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:36 +0000

    sight honest safety actually drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
