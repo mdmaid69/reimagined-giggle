937fc588de9c94ca92e863e3130732c6b2979149
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:52:27 +0000

    medium find official through below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import collections
-def create_stack():
-        return collections.deque()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
