be3fecf67391fd7ab764d7690eeb868491c88cd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:46:19 +0000

    people force smoke approve earth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
