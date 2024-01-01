3e9424ea8752e63695931feaca953d9fb7c5a6fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:26:04 +0000

    burst definite join discussion turnback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
