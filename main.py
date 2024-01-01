ceb773656bc8dfdc64aa82cc3da029a747a3eb2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:28:15 +0000

    unable sensible science school unkind

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_today_date():
+        return datetime.date.today()
+def is_odd(n):
+        return n % 2 != 0
