eec5517087ed3109d71dbac44120d8445bc10be4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:52:52 +0000

    reliable mess kind confusing television

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
