a755d06cff86584995ce2aef10130c0956cbad36
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:42:49 +0000

    demand shall like translate sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
