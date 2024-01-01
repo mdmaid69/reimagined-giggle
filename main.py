d4a2266e064a1a7a058939743ece68018a3683b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:28 +0000

    goback(to) admire usually piece very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+def square_number(x):
+        return x**2
