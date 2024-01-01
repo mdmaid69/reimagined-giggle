51d045a4ab35316609cf590eaa24af0ded4e936c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:51 +0000

    total before star female substance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+sentence = "Hello, world!"
 from collections import Counter
-print(Counter("hello world"))
+print("Word frequencies:", Counter(sentence.split()))
