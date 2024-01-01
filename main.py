1c2cb512bddb8a94c4d3be4f302f0660d7055a68
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:40 +0000

    hit turnback impressed that test

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
 import collections
-def create_user_dict():
-        return collections.UserDict()
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
