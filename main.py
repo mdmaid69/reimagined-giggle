fdf3503d08352bb174514a36bad85262b026ea5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:46 +0000

    566end half question suck build

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def add_numbers(x, y):
+        return x + y
