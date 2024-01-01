2ec14736a92ca728b7e93d1ad6da4add87e61836
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:46 +0000

    unusual workupto will whether excited

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
