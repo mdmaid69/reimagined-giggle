ee46fde171a52675bbd36b618ec120f0bbe8ea94
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:20:45 +0000

    part guilty arm turn medium

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
