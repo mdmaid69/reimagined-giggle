d84d60a4727f206113cc43f9355fe6c6c6b93c6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:30 +0000

    split cost school direct disapprove

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
