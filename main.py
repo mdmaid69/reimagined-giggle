c4ba3b0dc5a1b5cd287922af95c42eca1e6b3f0f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:58:16 +0000

    run compare pin heavy young

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
