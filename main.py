8e67a7f92b8787d9d046adae9cb76a0d889b62d6
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:16 +0000

    set surface happen disturb Nn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_set(array):
-        return set(array)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
