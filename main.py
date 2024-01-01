a59dd6e8be3030d29362a6fba10d157d9d05adea
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:11:53 +0000

    mysterious moral bottom brave vote

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
 import collections
-def create_queue():
-        return collections.deque()
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
