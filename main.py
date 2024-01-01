508417cecb72da0c112047aebea2d8608f6a31cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:58 +0000

    explanation hit refer different world

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import collections
+def create_priority_queue():
+        return collections.deque()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
