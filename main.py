d0bd5193dc925e0014ded0dd97ad108e01ca3190
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:56 +0000

    turnup purpose problem thumb related

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_complex(array):
+        return complex(array[0])
