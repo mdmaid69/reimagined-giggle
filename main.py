0bd8fa233667c7a6a06369de0ce8b67a8636ea41
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:06:47 +0000

    bringback Qq accident birth cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
