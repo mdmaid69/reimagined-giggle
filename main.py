c7a316f4f90bfabc4d017326f743cc51fe5efcfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:57:41 +0000

    bath power same muscle dangerous

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-import sys
-print(sys.version)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+print(sum(range(10)))
