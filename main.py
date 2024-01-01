00310614e1a98ea0c12b2c9da349cc619a4132fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:48:13 +0000

    himself possible trouble precise giveback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import math
+print(math.pi)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
