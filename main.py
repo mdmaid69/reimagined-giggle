fa1eec97ff6754995c02a1f8e3a8ba2ace52f994
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:52:30 +0000

    study grand apologizeapologise share fail

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def pop_from_array(array, i=-1):
+        return array.pop(i)
