80c1c7d270ac836ad341022c567471be214bf47c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:22:21 +0000

    smell mess experiment than keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import os
+def change_working_directory(path):
+        os.chdir(path)
