419b2d52b066537c2b6e1034c08e115ef8c5899a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:18:22 +0000

    sell pick straight minimum sharp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_min(lst):
-        return min(lst)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
