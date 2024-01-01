3d65e6e3b0f6f4cfb37adf163a5460d7771abbba
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:22:46 +0000

    none giveup advanced certain split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
