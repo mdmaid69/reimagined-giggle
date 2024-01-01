b38b32aaf48cf302b36dd85943d53676fdb8be8a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:16:24 +0000

    rare quantity hate expect time

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import time
+def get_current_time():
+        return time.time()
+  def remove_duplicates(lst):
+        return list(set(lst))
