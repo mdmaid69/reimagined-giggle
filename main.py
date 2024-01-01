24d2b4664322f9e5e50567f084434e95d358520a
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:19:41 +0000

    pause breath underneath past stiff

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
