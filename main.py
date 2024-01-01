78ef3aef74dbbacd86dfdcdf908d5f5181b476c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:30 +0000

    wash near free plan do

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def convert_to_binary(n):
+        return bin(n)
