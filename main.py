18b79dd58d0f8091811133853ffc6b0a79b906b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:07 +0000

    save loan criticism slice discovery

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
