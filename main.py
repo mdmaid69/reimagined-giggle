a14d2c731edbf9fd530ea912c69e402f4f3cdff8
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:05:40 +0000

    distance Kk think Ee announce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_area_circle(r):
+        return 3.14 * r**2
