5a8ad152c2eca0b9db9aee4133a3fd974ad363d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:39:14 +0000

    and bottom walk translate ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-def count_elements(lst):
-        return len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
