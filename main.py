706e759223e92b93f940b7cf9489f7679b3ca85f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:22:56 +0000

    add later getback programme angry

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+print("Hello, world!")
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
