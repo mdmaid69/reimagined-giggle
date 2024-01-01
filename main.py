7c08fd6cba553e524422d04215e9ac25e48c8c29
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:45:09 +0000

    once them yourself Mm unlikely

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
