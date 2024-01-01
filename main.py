cbc3cc515c020a4ec497b8c4c853dccbbb5e4eb3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:55 +0000

    chat best machine let demand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import array
-def get_array_length(array):
-        return len(array)
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_speed(distance, time):
+        return distance / time
