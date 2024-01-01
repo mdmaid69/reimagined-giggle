8ca0210faee7c1d7cab79e9f127707a8452f415b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:32:57 +0000

    throw regular clean everything second

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import array
+def get_array_item(array, i):
+        return array[i]
