f16a5218b5093df8744990edb4dbeac03ae630e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:01:10 +0000

    same level correction fasten because

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
