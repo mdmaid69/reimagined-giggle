4155344798014109b5b266279683101cd80b5893
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:55:48 +0000

    underneath drink deserve active sight

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def reverse_list(lst):
-        return lst[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_speed(distance, time):
+        return distance / time
