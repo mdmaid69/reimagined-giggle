386bc7978b0b4fc9a1a4f8761610e15f1f263562
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:31:40 +0000

    practice gradually characteristic another arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
