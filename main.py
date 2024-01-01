0656e3b99a4193212139854bc6996c736a78d428
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:44 +0000

    right spend base such then

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def clear_array(array):
+        array *= 0
