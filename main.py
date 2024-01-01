a59a4b198c7723b79abb46bb689269a89af1f7a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:32:08 +0000

    tool smooth rest fresh programme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def is_odd(n):
+        return n % 2 != 0
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
