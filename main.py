81a673d02f418cd39c1c049e922018474ca10ee1
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:35:07 +0000

    last base rise left active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
