df7ae5774c06fc3c78aaf972e67a186d7b686c34
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:25 +0000

    protection thing develop the production

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_typecode(array):
-        return array.typecode
+def get_array_as_set(array):
+        return set(array)
