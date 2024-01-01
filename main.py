bf9b0ada643a5467eb38f2ef784d50eba8a2580e
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:15 +0000

    expect positive behaviour damp welcome

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def square_number(x):
+        return x**2
