c84a63bbbff51e61959706acb568304f99bd0292
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:31 +0000

    both blame payment anybody shoot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
