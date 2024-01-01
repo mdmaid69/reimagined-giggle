013804c06226b5dba0c432b10c7cb05bac07cd58
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:24:29 +0000

    talk wake breakin flat danger

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
