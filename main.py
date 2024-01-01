3eed5cee521994bc6e0e846447b169319650351a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:17:09 +0000

    join thumb winter dinner spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
