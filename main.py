8f6ae0b75d1feea317e8af705c52e17115a401fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:47:12 +0000

    number immediately hour criticize just

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
