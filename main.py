3b94665ff4a741d81dca934100870e1d7817abfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:35:38 +0000

    discussion do count makeinto suddenly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
