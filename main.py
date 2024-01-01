6d3030d5ee923793d3098f0b0276f4764d1e1242
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:10 +0000

    tongue breakdown during shut minor

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
