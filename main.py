d0db2f72e9ebc30e59c2dba194467aa2f07cceeb
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:07:15 +0000

    goodbye purpose runinto trouble method

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
