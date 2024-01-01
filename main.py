6d0afa339858f157e37a3d41089ebb434f00185f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:01 +0000

    cross brown upwards far habit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import time
-print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
