53b3c80675d9b6bd5aa74453edecb125f7d5140c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:41:02 +0000

    callout ignore race interested Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def reverse_array(array):
+        array.reverse()
