7d98787da2c4949ec549268054ec6b16724cb1a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:27:37 +0000

    takeover little independent catch below

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
