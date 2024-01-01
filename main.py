10dc2e37a9191f31c53fb59972a910bf47f067f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:08 +0000

    drive contrast week forget lean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import collections
-def create_user_list():
-        return collections.UserList()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
