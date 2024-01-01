5fdb1e735ddbb7941ce704cabedc2e4dc58b203a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:44:31 +0000

    prove tired can age spare

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import os
+print(os.getcwd())
