7b7623a5eaf2ee729687cb25104b5a284555e965
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:57 +0000

    wind eager very cheek fly

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
