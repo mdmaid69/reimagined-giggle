f77a810f1e7c6d6b20639c92c3aad3db4fd5477f
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:38:39 +0000

    meanwhile together beginning neck who

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
