1d9671c81f37d945d19c2118c1e69f9eaa741fac
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:00:53 +0000

    sweep disturbing clothes nature space

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
