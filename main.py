0089b4c3202e75901349c94e588b48ea6ed7da65
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:41 +0000

    cope plant basic young imagination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
