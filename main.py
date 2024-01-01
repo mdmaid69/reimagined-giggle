c12bfe64d3cfe8576fc45c4eba71a15c29a97577
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:09:02 +0000

    Cc machine him pity meet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
