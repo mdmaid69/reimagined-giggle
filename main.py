a7da9d58cf360fa4c7d920cc7688f0207388e80c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:08:50 +0000

    visit feeling window stick creature

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
