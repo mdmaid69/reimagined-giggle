6226f64a35e676508d94dedab97549b4f3ca252c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:54 +0000

    repair minimum join rely start

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
