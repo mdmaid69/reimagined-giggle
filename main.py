5719d021134af27517bdae75d0bdf39b2694010f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:13:25 +0000

    range pay closed Mm fierce

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
