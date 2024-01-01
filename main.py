fef840258d89c071b14f8444d174ffc31b01d611
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:51:43 +0000

    free available exaggerate strange fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import os
+print(os.getcwd())
