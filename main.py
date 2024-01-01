6da9e296aa23832cef7810e5d0a561159ab33783
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:05 +0000

    difference sentence record sort principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
