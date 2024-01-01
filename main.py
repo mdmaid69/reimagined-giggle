87240dd590619c6c19c3a114b2ca1cd6fbd6782d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:35 +0000

    tape keep beside roof history

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def find_max(lst):
+        return max(lst)
