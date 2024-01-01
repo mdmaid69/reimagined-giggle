431a9c2423e31b79c6ece535de49038fe51aedd6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:56 +0000

    excited search 565end feel unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
