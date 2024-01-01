175614cf5481395bcf10f7c4c657fa10243c9764
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:49:34 +0000

    protection disappear path punishment blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
