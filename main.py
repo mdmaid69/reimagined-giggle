4aa5d2a0c1adcacc22405aac6564da4b19248aa4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:02:14 +0000

    closed even neither mouth kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
