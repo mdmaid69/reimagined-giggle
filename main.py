71853a0a086ad12fec8299263d6bae670a3fb1ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:54:47 +0000

    reasonable shrink central this believe

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
