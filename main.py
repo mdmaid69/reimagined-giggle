07ee452e1fb6d14c0641b0fbd0a4fe33206d7922
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:16 +0000

    OKorokay place direction floor yours

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
