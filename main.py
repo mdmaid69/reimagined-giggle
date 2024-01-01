32a4049d14a1e97f1dcb533c64e135c6d88c5d3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:02:38 +0000

    contact file him switch home

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
