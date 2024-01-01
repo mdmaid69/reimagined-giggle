6d202c00113bfcbb46d039a16ac823aa51f1d11e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:07:39 +0000

    cruel attention share feature angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
