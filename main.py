fc2bef72a51891389a2f637ff7c667a20e325509
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:55:29 +0000

    murder night sort offend brown

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_numbers(numbers):
+        return sorted(numbers)
