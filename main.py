3031f413509f559f0d040a7318d833ae9bb4c402
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:42:56 +0000

    distant conversation bringback narrow lift

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
