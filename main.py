69429934da3c89a0a9d4bc47095eff8f56a5b296
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:50:05 +0000

    rather after measure comeout suckme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
