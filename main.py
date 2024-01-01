de724b8b5df41c629712ebbc60c9cefebd1b7289
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:06:48 +0000

    remember crisis open trick juice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
