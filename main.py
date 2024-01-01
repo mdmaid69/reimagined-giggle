7758b603fa55682c68c3655c49a3e697075eb63a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:07 +0000

    spare estimate tongue ordinary manage

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import numpy as np
+print(np.array([1, 2, 3]))
