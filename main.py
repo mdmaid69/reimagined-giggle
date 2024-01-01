da7a592ded9ad6df7965c858cba3cef4d7709b0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:32:46 +0000

    especially carry address agree disappear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(numbers):
+        return min(numbers)
