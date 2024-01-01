d6f71c5fd9718289b7975c6505c7f9ff05f8a063
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:06:32 +0000

    description plain cross satisfied fall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+def count_characters(sentence):
+        return len(sentence)
