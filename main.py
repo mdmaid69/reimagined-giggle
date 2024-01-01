cef08b13adb3572926d36057d4d74b1318a86681
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:37:28 +0000

    grateful though smile produce press

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
