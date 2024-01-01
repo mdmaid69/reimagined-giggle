96baf230b00bf44fd75c270618313d9a145a7abe
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:13:30 +0000

    official connection talkback average themselves

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
