a712696357fcdfd4a85453f90de271893dec58ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:22:21 +0000

    table pot could hungry art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_hyperbolic_cosine(x):
+        return math.cosh(x)
