3de845de19398efd02334835239896d967431efc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:16 +0000

    by himself sleep bin easy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
