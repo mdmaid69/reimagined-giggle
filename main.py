614783f6fea69fabab6c43ce00132bca9f102ad7
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:38:02 +0000

    sink minority suppose trap government

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def clear_array(array):
+        array *= 0
