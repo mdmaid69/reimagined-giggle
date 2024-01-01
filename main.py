04321dca41c121615d2ecf02a58cadae2ab9c7f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:15:14 +0000

    spare majority strength remark 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
