bf0e6cd778e594a815f35c8ddf7e7639e93629d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:14 +0000

    himself other drop ring movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
