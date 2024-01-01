c511eec68a10f99ad97a65f423df140dbce03567
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:12 +0000

    run music hole possible turninto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
