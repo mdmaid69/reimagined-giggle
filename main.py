6f9fb114486a234407595e5876a371146dd1e459
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:20 +0000

    night odd sail change thin

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_density(mass, volume):
+        return mass / volume
