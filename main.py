d2dde71d21a1cfd01bb35fe1e2e4e9085565f558
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:53:50 +0000

    reduce how drop cut sign

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
