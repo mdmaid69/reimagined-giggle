35cc19893b2e784bd1ef0be7a15d3d32ab2d918d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:47:16 +0000

    imagine later sleep develop pull

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
