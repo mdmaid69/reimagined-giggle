e54fd30735d002f86d00aa1a84b0d3ad701ad585
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:55 +0000

    heart lead determined violent twist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
