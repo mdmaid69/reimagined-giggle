797d1a74ec393e7a2684e2920fed783db000fa26
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:38:28 +0000

    takeout relax invitation tough file

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_min(numbers):
-        return min(numbers)
+def reverse_list(lst):
+        return lst[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
