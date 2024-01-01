3c556bd3150669097c11db67cd94c64c1f5e0376
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:54 +0000

    immediate science off whistle show

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
 import array
-def clear_array(array):
-        array *= 0
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_unique_words(sentence):
+        return set(sentence.split())
