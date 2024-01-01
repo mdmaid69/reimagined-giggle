4cf10ef23e7cc30c70ef61291c16a6513a9a354e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:21:19 +0000

    564encourage persuade official pupil apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print(sum(range(10)))
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
