94b8fa0229672efb1782d466ef900998418c085b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:32:25 +0000

    exhibition thin smile public suggest

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
