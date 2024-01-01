6eb697e340293d56a763c1db42faed897d8199d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:02:04 +0000

    doubt here edge regularly earn

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_power(work, time):
-        return work / time
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_frozenset(array):
+        return frozenset(array)
