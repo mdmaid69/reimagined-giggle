8bd154891dbe29d4df200223d0e6b580905e10b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:22:00 +0000

    square shop moon splash corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import getpass
+def get_username():
+        return getpass.getuser()
