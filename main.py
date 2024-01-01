334bc921eafeb8d733ccad40d8902f62b33dd718
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:57:02 +0000

    length weekend over pick shut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def convert_to_hex(n):
+        return hex(n)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
