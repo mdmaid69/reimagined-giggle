42adc4ede7cdc10319b48c97b1cafb373ee4f4a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:46:48 +0000

    collect science rain Kk immediate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
