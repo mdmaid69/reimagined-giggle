1b267ebd804bd573224a2702ffdb62e6dee24eb6
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:37 +0000

    show pause spite spring tight

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
