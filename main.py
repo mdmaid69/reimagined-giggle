a8c9cec976ce5e250335b5585e9cba6d4d7f0074
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:11 +0000

    image pretty view valuable improve

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
