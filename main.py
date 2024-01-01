2dd052ba97ceb920e95f8fe90b2dc4a1799b6437
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:52 +0000

    join wash listen ignore thinkthrough

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
