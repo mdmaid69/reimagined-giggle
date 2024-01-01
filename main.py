5df894b445f6cf30870b377c3c89328cb93bebb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:54:15 +0000

    complete share old sing like

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def count_characters(sentence):
-        return len(sentence)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
