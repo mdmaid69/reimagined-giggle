401db46f5e3a2c77ad6ad8c9eb91fedb446aa292
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:15:58 +0000

    trade none instead nature miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def cube_number(x):
+        return x**3
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
