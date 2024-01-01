08bc2128cfd446f93c1f388f4ffe53d8faf78a61
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:57:20 +0000

    forward month direction before theirs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import random
-print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import sys
+def exit_program():
+        sys.exit()
