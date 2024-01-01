8edb93c077775ccbb024d2745289b724ae780ebc
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:14 +0000

    slight opinion violent full lie

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
