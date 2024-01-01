a44b4ed1a80fbcac74d4a04bc467829af3ef64c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:32:24 +0000

    weak expect cope argument mail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
