dd2e0fded72365f5c013e3ed4a228818b393efbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:32 +0000

    fix avoid trousers record confused

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
