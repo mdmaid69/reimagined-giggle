8d32055c48c38bd6c506caa33f9cdf56849ccd90
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:56:02 +0000

    along anxious keep goodbye inform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
