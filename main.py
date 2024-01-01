f47537bf51586632d5911b677375c762a308b83d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:09:54 +0000

    perfect taste half finally downstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
