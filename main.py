7514fc1cdc7fc852483e7fc18d16468c7dc9b6ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:24 +0000

    punish frequent some trial Jj

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
