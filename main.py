eee7e72061542e5f6d5e53437591fcae31f4944c
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:15 +0000

    female slide whichever hurt plant

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
