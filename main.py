36f1a93fe10caf47ee71e2e05dfbb9465e289653
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:33:48 +0000

    silent be quit follow fine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
