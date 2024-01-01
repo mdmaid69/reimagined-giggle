c0473610fcbed54f38a2860e11e0508d8d925f49
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:41:23 +0000

    strength where hit 566end class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
