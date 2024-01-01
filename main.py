fbcf05326e4c6074a148395d5f882415e15a7906
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:30 +0000

    release factory order accommodation bin

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_length(array):
+        return len(array)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
