8b53cb52d9e9966f4a55f2df057e9f33ac49da16
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:21:28 +0000

    unit furniture branch Hh dangerous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
