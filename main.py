072cb56dc5c686740d68c2c63ed7fdc973035a43
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:45:58 +0000

    balance section experience test plate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
