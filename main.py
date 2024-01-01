b10f138b2751ab84d0564d96c712060c3d38e832
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:08:57 +0000

    present shoot plant fork ashamed

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
