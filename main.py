3b9a22a1b99cdba9ac72a1672c60237fde72ff47
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:23:30 +0000

    satisfied say meal discussion disapprove

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
