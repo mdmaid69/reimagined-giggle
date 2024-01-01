a304e2659bf9a7e4b9bc128be9049fb5db1cff58
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:41:51 +0000

    mess show confidence down measure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_item(array, i, item):
+        array[i] = item
