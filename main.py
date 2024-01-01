636ce1539b4a53fc8fb21a0af91ad85bba5e9835
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:48:34 +0000

    health use low emphasize turnaway

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
