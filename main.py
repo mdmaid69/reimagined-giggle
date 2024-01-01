a15b3563a49a8fa0a7f77b9c39e890ae28d42a11
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:47:37 +0000

    weigh concern arrival Pp issue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+print(sum(range(10)))
