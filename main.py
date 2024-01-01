881ac14a38495752be3eda16d232f3b6c4f7afba
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:48:36 +0000

    suck cut test fly react

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
