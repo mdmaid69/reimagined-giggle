97b81c09a15164663eeb87701fc4f8d8ec04e030
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:16:59 +0000

    cap level trouble second need

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+print(sum(range(10)))
+def calculate_density(mass, volume):
+        return mass / volume
