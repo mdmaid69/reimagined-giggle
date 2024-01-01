d61926aef5cc7b504dbdbce8c47fa0d22407b82f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:33:20 +0000

    this concentrate fetch impossible promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
