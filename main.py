fff33a0d3b5d3d52aa38b84849d081c06155260e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:02 +0000

    average drive reach well terrible

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
