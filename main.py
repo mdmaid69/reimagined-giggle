2cd773303aec0ee5e4acfb8325eaa0f0d8ac460d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:30:37 +0000

    risk complete complete advantage will

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
