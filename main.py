3f56115c6b0a05da484cb940716619ca9575707f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:37:49 +0000

    never school heat table urgent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
