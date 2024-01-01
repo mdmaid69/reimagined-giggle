d7171b5868de9b2620e603463e59909679cc9a7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:04:55 +0000

    surprised insist bath autumn schedule

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import datetime
+def get_current_date():
+        return datetime.date.today()
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
