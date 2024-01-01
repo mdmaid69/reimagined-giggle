c9d75bf737b996df009c6948d6c449b18f204154
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:49:13 +0000

    wing section divide conversation listenfor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
