0085930f861d17e703a245199a65fef0ab3399b5
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:28:56 +0000

    switch communicate month talk beside

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def count_elements(lst):
-        return len(lst)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
