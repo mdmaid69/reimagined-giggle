cacdfb17e6c4df3c4c78295e119a39d3f87e5e13
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:16:05 +0000

    apart rich lay second force

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_permutations(n, k):
+        return math.perm(n, k)
