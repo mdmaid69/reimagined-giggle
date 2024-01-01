eb15f5032982a63cd06f3a5974e52feb90f040f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:56:46 +0000

    piece appeal rescue politics figure

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
