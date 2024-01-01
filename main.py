ca3819a5759d43e2b5487a3ec07cc37eccb96e29
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:16:49 +0000

    couple pay wash breakthrough correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import getpass
-def get_username():
-        return getpass.getuser()
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
