1075b4a4931e6687404f61f2fcaacf7d4664cd81
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:26:11 +0000

    little equal welcome little generally

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
