0941ce55080e98411975ceb8e4055fa405ddf53f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:39 +0000

    closed development might by bet

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def find_max(lst):
-        return max(lst)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
