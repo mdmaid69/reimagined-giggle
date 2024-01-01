45694647d62ad60916a6346e16efb7924bf75a13
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:34:33 +0000

    central choose apparently share correct

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
