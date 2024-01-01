1ba711c20e9f3c8c30326b76c69862ca4956a921
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:49:24 +0000

    everywhere back somehow respect accidental

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
