1900018096df0392dbb97613e578d6ce4eafc6fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:34:12 +0000

    closely pain project close pinch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
