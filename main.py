78a27058c872a91ebaea5dbacebf9c01ef502b13
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:17:09 +0000

    duty unfair realize street stiff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
