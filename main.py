f0a282816a99c4f9f576c90f34df072d27e3d4a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:02:18 +0000

    interview provide central free wind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
