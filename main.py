87a89dd922dc35c915c15b0fdda4dbd2bde2d39e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:42 +0000

    keepfrom tip mind keepoff guilty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
