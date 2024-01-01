27cebb533dd707f2d98bcb4a1c72391d685e13d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:39:09 +0000

    where rich use load building

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_error_function(x):
+        return math.erf(x)
