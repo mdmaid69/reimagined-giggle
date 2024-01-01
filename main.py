e7f433a136506e95b0e33d8786aa802d4fc5584c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:27 +0000

    practise job pay letter fault

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
