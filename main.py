5b3aff90b955e884771acce593de7aea0af55109
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:34:38 +0000

    repeat uniform rescue dish muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
