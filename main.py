8fc5117419f16629e52e56c28ca75ea0119cfd6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:54:49 +0000

    space record suggestion business tidy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_list(array):
-        return list(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
