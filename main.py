7261927d58f0f4e92934c3a395f44fbb516321d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:30 +0000

    trust preparation date lucky escape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
