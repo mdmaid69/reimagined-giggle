9e340b7c56cf3d749ebbd9eda7d9a3e7a1f2deea
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:10 +0000

    convenient whom broad legal plan

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import platform
+def get_os_info():
+        return platform.uname()
