1007ae78ea36239be2adf5120b3bada5f06103d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:58 +0000

    moon swimming material himself letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
