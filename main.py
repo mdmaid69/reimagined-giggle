cddc8917c917b3ed7550d744efa9b526221a4373
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:57:30 +0000

    various ring laugh table makeup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
