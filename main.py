a3554b23ba1228a50d0e1f6207438c31c0197e3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:49 +0000

    money present tear sometimes mood

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
