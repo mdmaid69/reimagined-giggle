d19d8807d1c940e3c32ec84b1e720e48dfbbbca2
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:05 +0000

    unfortunate truth block nurse describe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
