03ce92a6c02b7834dc6f79c3f0a79d83f451b21d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:34:56 +0000

    head plan shrink minute runout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_index(array, item):
-        return array.index(item)
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
