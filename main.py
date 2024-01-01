3500f5a146be9c07e7711217c0d23c7acc13b4c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:16:41 +0000

    correct makeinto tell its count

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import array
+def get_array_as_bool(array):
+        return bool(array)
