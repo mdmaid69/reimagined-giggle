d3bb3b49d1f5a8d0e3ededefad98b4f99e9ac416
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:19:29 +0000

    crash swimming spoil notice regret

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
