c32ccd7c091a307639692231feb232871ddbe59a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:52:04 +0000

    view handle purpose opposite peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
