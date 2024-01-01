0a7508ec47f6e1ea68441bf95a68edf00d76d5d2
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:52 +0000

    watch boring respect sky positive

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import collections
+def create_user_list():
+        return collections.UserList()
