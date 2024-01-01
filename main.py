37049a7dc060f4856bd269698315f5dd2bb92e67
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:01:55 +0000

    aim popular I card beside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
