6019b6d7df011f80ed953c6eedb23c07c69f2721
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:41:23 +0000

    owe turnoff mainly bar pause

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+name = "Python"
+print("Hello,", name)
