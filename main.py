2fc684376df73ab01dcc742100d22bd189911e64
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:00 +0000

    turnback punish mark disgusting his

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  def reverse_list(lst):
+        return lst[::-1]
