82f26cd7c4b57384cdd3764bf8092e069bd03782
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:29:00 +0000

    reliable explain begin delight next

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
