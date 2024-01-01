81414bf898e3043dc7141ae4b6f22a7be580271d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:38:29 +0000

    doubt negative cover impatient risk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def find_min(numbers):
-        return min(numbers)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
