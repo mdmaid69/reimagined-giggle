10677770f0ddc3d07d32c152c12ec326ff212187
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:31:24 +0000

    turnup sink smell run suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
