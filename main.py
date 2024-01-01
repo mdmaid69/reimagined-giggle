c552e018f58e242db31cba7ce1e9098a995a4d00
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:42:00 +0000

    Mm opinion exercise plan guess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
