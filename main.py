c155b8bbff62dacb756514c0697a647de66895bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:04 +0000

    reason clear hurt through floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def is_odd(n):
+        return n % 2 != 0
