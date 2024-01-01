83f63283bcf30ad679f6f470a80ffe638c230644
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:08 +0000

    air holdon within tonight stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import sys
+def exit_program():
+        sys.exit()
