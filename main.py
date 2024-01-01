ef0d9cbf9359d9e7fb99d97535963dff80f4ead6
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:38:20 +0000

    father Pp remain bunch sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
