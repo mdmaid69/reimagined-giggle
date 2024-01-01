86364f159e18e59bbbf817f35ab44dce05ad0a46
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:57:12 +0000

    smell responsibility slight shall lesson

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
