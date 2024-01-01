59aaa8c3e6692e6063c9d4a3d2d0de43c526d503
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:03 +0000

    copy repeat doubt explain spite

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
