80977e2fcb6db999b1ee99d1af3bad589791deba
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:35 +0000

    measure neither empty shoulder nearly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def divide_numbers(x, y):
+        return x / y
