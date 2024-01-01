3bd99f64d10fb39b6b05c37785ecd11034e17c77
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:32:55 +0000

    off cause even problem remember

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
