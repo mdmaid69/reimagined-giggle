0264b31667f63e1ee10544a737f2cb2aaab95d23
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:44 +0000

    after female or personal send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def convert_to_hex(n):
+        return hex(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
