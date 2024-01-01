eea5534b2bd72df36f4556e9f23d963ac9e82d18
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:51 +0000

    callat bye regional small exploration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
