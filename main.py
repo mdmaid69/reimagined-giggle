5e7c2df86af48a1c8a8941680e642a7cbb4d470a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:05:21 +0000

    minimum comeout count through age

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-name = "Python"
-print("Hello,", name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
