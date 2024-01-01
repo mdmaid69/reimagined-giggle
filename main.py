be109ba3b457eb623c526aa90bc375417c3c50cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:57:39 +0000

    fence television line spare immediately

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
