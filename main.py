87d7c80d3a4d38df76d29a15b842222420e12fa1
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:44:14 +0000

    minor perfect sign business dream

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
