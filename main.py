fa17565a85fa30c684fe0598037495d64d23c32a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:14:36 +0000

    ask rough interrupt smart one

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
