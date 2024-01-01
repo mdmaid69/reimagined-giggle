95cb05f2c5fd225e59584f862bf3cd09ef483897
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:41 +0000

    lot concentrate area crowd full

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def find_max(lst):
+        return max(lst)
