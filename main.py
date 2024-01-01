9dfd6313f34c0af8f1d328146e176e9029fed123
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:37:25 +0000

    damage disapprove university formal miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
