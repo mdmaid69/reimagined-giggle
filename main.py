5b84f65e63a19cdf6b2711ad7ec63741d331647d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:10:36 +0000

    present number for curious eat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
