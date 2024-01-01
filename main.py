a02348a1fe84ce2ffcddbf9b40d5287a72e92693
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:19:40 +0000

    aim slight drop pretty greeting

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
