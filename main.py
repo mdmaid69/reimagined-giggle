8b5e62e5587cfc93389b4cd593394ed60c38f087
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:05:35 +0000

    authority blame defeat goon wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
