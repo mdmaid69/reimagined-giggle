3b25652c98ddfdfcfb2cd9f2ffaf4f2e97ade9b4
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:30:50 +0000

    few argument art mass smile

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
