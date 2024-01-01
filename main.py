c7663e153a8f41aef7841706cef9e72da9dd6b26
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:11 +0000

    variety city stare bin professional

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print(sum(range(10)))
