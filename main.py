8b9025ed3490edc03d310023e00cda2b885a52d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:36 +0000

    prefer we pleasant emotion instrument

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_os_info():
+        return platform.uname()
