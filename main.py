124ef0279b48651699627b438fcfb001b5e03d47
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:01 +0000

    live shout cheap dance square

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
