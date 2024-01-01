c6328cb802c0cf5907d88f1eee32315bef936ce6
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:08:21 +0000

    brown famous land fold pen

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
