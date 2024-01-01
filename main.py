4684f4b1f2a919c94267dd0076e44719df188273
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:14:35 +0000

    birthday rush watch breakinto giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
