b73493e0a969f7417888aa6ffbd0e41a65a34639
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:54:49 +0000

    normal gradually usual workon lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
