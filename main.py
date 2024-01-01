b2cc3343fff98073ad0c9a79cacfc250e964b826
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:18 +0000

    science band repeat certain type

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
