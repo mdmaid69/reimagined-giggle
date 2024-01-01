8164de5cd02881afe8c645cf9064368e11094100
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:59:07 +0000

    attractive keepup name strike special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
