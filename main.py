1b1a4ad5ed5a26edb67aa3ac7e8f4dad108032e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:20 +0000

    twist keepout official nervous bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
