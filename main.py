98f5ad29fb5b34a7ec8d1a9074aa800dfb4cd459
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:59:04 +0000

    impatient reasonable suggestion mine do

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
