605e3a7bbe6496567e930e7edafa576516d33c95
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:47:02 +0000

    operation may amount calloff write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
