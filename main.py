d9b2f37ef8366757dd4cba1d13d47922ca0a7c17
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:19 +0000

    come muscle grow entrance Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
