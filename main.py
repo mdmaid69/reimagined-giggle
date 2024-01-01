a02a87b38d562bf5beb4305c38bb4b806588b005
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:54 +0000

    exist destroy comment confuse bath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
