cb85e1e33f298179997a3a8e405b28e0044a3c5e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:37:13 +0000

    follow would flight study criticism

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+def cube_number(x):
+        return x**3
