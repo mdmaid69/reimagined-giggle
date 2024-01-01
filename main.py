2833199211bc8e538f42b4eaa1d11d076ebe5537
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:03:56 +0000

    rain onto present will storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
