2095ddb7c49ba3267804b1b57eebac95b412e322
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:58:41 +0000

    different spin work reject before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
