a22220f47e49b20a8155b5e431d02aabf49c4b35
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:55:12 +0000

    arrival today equal genuine file

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
