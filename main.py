486bd2b6d78085059d252137dd7a386fa8a6362e
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:19 +0000

    listen quantity yet country fork

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-  def is_odd(n):
-        return n % 2 != 0
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
