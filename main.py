bebece9385c8369376263c8d21871abe50408051
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:39:43 +0000

    passage stare worst suddenly front

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
