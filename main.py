49af81bd8446eebacd447f0a17465a3dab1a3105
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:38:18 +0000

    after organize mark relation snow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import math
-print(math.pi)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
