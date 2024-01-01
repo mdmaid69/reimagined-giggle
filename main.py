37b8062001b76fb41b4de658125a515bd06a03e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:58:29 +0000

    pride attempt bite later geton

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
