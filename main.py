169e8444b1108b757a051fce082d3e8c1cd213d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:03:20 +0000

    tap total delight twist hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import array
+def get_array_item(array, i):
+        return array[i]
