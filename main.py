d142494316c2a14c0fa807d0f9e55eeffe75e8c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:36:50 +0000

    platform set reason aim tour

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+def add_numbers(a, b):
+        return a + b
