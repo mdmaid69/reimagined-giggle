f9abcf45efaf00c24145e3dc4b70a4a110f3d18d
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:56:00 +0000

    total succeed hold lost concerning

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def convert_to_octal(n):
+        return oct(n)
