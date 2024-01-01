a9a7c96cc1f6e57a39c6260d558bfd0a39edac44
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:07:17 +0000

    blame live block job certain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import random
+print(random.randint(0, 100))
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
