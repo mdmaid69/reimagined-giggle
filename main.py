29863a4aa47de52c2204a7b264b68f2ae00b8571
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:30:31 +0000

    service anywayanyhow cook open building

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
