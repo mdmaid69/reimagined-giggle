3aefdd6efd99377d4e5541b524028c012fe1444c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:15:45 +0000

    pair prevent criticism balance everything

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_power(work, time):
+        return work / time
