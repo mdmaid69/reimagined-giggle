e9248cf0cf4ed5221084ff6c418cae17cc598dc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:01 +0000

    matter guide frighten cry warning

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
