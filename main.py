551f479492ef95001219c1c424435d36e3afc288
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:29:51 +0000

    scrap separate share wish practice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
