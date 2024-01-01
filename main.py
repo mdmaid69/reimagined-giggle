6b5fcdb622150028ca99e6769088a0666e9d1b10
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:26:36 +0000

    so react bunch goup comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_work(force, distance):
+        return force * distance
