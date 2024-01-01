f2724a65e5f28e2cb248e44ca2f4bd1650859e84
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:55:47 +0000

    shock waste ball stiff future

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
