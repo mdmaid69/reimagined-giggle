031aa4fba37dd0e532fd3f85e7fc0584cb54c81b
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:49:14 +0000

    him instead might tend project

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
