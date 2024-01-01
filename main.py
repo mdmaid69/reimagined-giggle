7b968ebe0a0f4e05da83d96fe0009109d071e7d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:27:06 +0000

    distant least fun battery lay

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import sys
-def print_python_version():
-        print(sys.version)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
