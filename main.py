7b12abb26746eadf20285919f7321a8d2d40d80b
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:07:26 +0000

    contents through section cross limit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
