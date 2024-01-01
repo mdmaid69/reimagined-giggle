985a1c59720f8384ee0edf656abfab4f4c9bf134
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:17:00 +0000

    lock price successful while clean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def get_array_item(array, i):
+        return array[i]
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
