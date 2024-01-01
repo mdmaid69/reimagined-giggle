349969b9d5eeee9aebce3aef5161ae1b3b1ed02d
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:21:02 +0000

    stage few idea politics thing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
