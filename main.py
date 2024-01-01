bc83d2671f72b9c22aacdcac2ccf67f1d6f0bd01
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:06 +0000

    inquire but centimetre admit tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
