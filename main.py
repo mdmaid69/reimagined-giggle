77f52f0e94b327442c2cfe3508ef6790fe46393c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:28:13 +0000

    during spray reliable activity settle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def remove_duplicates(lst):
+        return list(set(lst))
