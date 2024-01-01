9b1a90a4b32d870afa3ead19af10d27962b8bc9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:58:20 +0000

    treatment cool as comfort ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
