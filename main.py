247654f29db0e12dbb86dacbda15587c99e74f93
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:05 +0000

    arrangement twist beneath building gas

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def add_numbers(a, b):
-        return a + b
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
