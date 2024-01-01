51eeb84d6560685212a45c1e383b26d848d9caf0
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:35:31 +0000

    unpleasant check add mass gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import array
+def get_array_as_set(array):
+        return set(array)
