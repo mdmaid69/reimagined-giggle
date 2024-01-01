2f9bb68e511751dd926489e938f8b4c15015b222
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:02:37 +0000

    finger positive lend joke file

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
