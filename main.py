101b3d4c4b9dfd3a1532874466396d5b4e48ec5b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:51:21 +0000

    blame confusing touch onto anybody

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import array
-def get_array_typecode(array):
-        return array.typecode
+def check_if_array_contains_item(array, item):
+        return item in array
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
