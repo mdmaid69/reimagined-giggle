1535a705264e6eb7c9bbcac2c2b279dda7ffe140
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:12:26 +0000

    minor crowd flow buy greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import getpass
+def get_username():
+        return getpass.getuser()
