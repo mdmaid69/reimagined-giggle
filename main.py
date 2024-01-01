ee4846f0e2490e6fc85421309c30c418990609fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:44:55 +0000

    cool address at rise turn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def reverse_array(array):
+        array.reverse()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
