9dbba0747546a10e505f07246b51651d6fed98ef
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:13:48 +0000

    satisfactory couple point silent display

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
