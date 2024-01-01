ef527234f5af242b3b9cfc05e41a9b25b923120d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:15:32 +0000

    cost solve Nn without goup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_union(list1, list2):
+        return set(list1) | set(list2)
