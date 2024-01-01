02d020856ebfeba51e5aa615e3239fc9e56dab82
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:32:51 +0000

    afterwards matter Zz neither temper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
