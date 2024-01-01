be7371d26b473d8c2de88f546a77b0fbbadee3cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:22 +0000

    explain bet drink Ww train

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
