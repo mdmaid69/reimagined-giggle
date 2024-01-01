41752b427c26e1365cba2c78a7c3fabf4caf0152
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:34 +0000

    minimum plenty turnback short regard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
