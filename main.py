eb73904c376c7c7fa817050840b2c6c53563d497
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:53:14 +0000

    call to introduce strict foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
