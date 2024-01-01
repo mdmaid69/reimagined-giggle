aa24238c928fbebcdd98b7c9c0e2c39ae1d92347
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:46 +0000

    decision balance alone water represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
