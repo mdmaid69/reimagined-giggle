3c8d2c8314c2bf3c629c4084a573bdbc3149cc7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:59:51 +0000

    afternoon 567enemy born bunch grateful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def reverse_array(array):
-        array.reverse()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
