ccdd35819ecee1051db64fe6e824dbbf15f516cf
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:00:56 +0000

    schedule operate walk level kind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
