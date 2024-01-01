fd2df30e90ada078c7f510e36887da4a48c51ba1
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:44 +0000

    become prepare unkind so butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
