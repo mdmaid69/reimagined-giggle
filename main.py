245e97831e054cdda0588595009f882b9407fba9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:45:00 +0000

    amount exaggerate pay Zz this

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-name = "Python"
-print("Hello,", name)
-def calculate_work(force, distance):
-        return force * distance
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
