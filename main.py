55a5e5f3e4533fe33fc70849fd4cecbcc4f4ca6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:04 +0000

    deliver obvious sticky best various

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def convert_to_hex(n):
+        return hex(n)
+def find_min(lst):
+        return min(lst)
