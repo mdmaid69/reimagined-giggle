30abe0ed3222d02854abc3b6657d72c41358bd69
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:07:39 +0000

    argue drop new teach official

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
