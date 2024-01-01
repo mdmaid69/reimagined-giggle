e0adda11276776157232bb4a9356238439c0e22d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:42:50 +0000

    course call read correct long

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-print(sum(range(10)))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
