8091337ff1ca0f0b1fea3f5c78ce6c9e0d5ee4bf
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:41 +0000

    hard mark book office stiff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-print([x**2 for x in range(10)])
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_acceleration(speed, time):
+        return speed / time
