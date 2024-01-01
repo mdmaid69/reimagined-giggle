3ad37b912bf5b62e97244a8c918820b0af74fdb1
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:13 +0000

    worry arrangement succeed front line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
