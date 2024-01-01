9f659ff9459dc7f2e175e2dc16317cd9e8b50b9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:27 +0000

    tight talkinto 179bite turninto persuade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def square_number(x):
+        return x**2
