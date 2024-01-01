cf43223e61ab2075db3bc6acea94b759858ef161
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:43:20 +0000

    achievement opposite type shadow about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_int(array):
-        return int(array[0])
+name = "Python"
+print("Hello,", name)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
