886bce454918d65e3a6ce9db7acd2d284e228212
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:23:19 +0000

    shall holdon how determined whatever

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
