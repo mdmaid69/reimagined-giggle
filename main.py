1b52fa213f0944f3c83c64823905034675434753
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:14:42 +0000

    bringdown suddenly sit willing rate

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def is_palindrome(s):
+        return s == s[::-1]
