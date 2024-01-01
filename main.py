fefb9710c8c323a681b74948b122e730d0ed36f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:44:11 +0000

    wish hurt calm refuse strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
