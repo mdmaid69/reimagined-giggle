8ff114c9edb23a0005d41fc607660fb2844e7bfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:00:56 +0000

    few cheque complain forward problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import sys
-def print_python_version():
-        print(sys.version)
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
