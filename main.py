d716e2cbd08851c84e804ae51931116e1e4d107d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:32 +0000

    happen touch anywhere form whether

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
