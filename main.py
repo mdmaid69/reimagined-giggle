216c805970b2a3801c5ce40f1060d6f2cf8c1106
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:06 +0000

    something left factory tear lose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+for i in range(5):
+        print(i)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
