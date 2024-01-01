19ed99aefccbbfdbaa54e137c03ff55899a72d0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:55:47 +0000

    destroy bank apparently order shade

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
