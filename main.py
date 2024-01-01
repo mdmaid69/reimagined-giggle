a2b9fef6a7852460d06877286b0f926d8cf0ec14
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:36:10 +0000

    brave business state pretend part

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  def sort_list(lst):
+        return sorted(lst)
