3db2943142f8e3a4ce97f2b994e4419d0b0277ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:43:45 +0000

    bin insist most experience development

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
