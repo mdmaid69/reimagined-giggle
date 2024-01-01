49f9ed831b21e53f33c6ff5a20702486a6ddba9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:57:23 +0000

    sweet tidy process bringdown on

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
