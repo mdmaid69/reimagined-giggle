b28c3dc91d35ef0a08740257dce85375257b9e9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:50:44 +0000

    risk recover movement compare scream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
