68fe9ef351c35f70404987d54ff9ff4655bbbbd1
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:13 +0000

    sink happen clothes know turnover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-def find_min(lst):
-        return min(lst)
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
