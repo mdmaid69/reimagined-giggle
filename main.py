299449592d249764f7399001140dd50fbeaff6b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:10:49 +0000

    shop giveup aim narrow grand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
