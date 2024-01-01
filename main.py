6424097e0d2a3cefd94381eade430f85ffd36789
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:36:07 +0000

    snow pipe truth opinion possession

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
