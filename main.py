e3e3d7f861a2072ee103b15fea160aa65fa6e71b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:50:02 +0000

    emphasis rarely backwards word reach

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
