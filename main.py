9ed60c5d0556bd9cdfa0f5de6176a6dd53aefe08
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:01:08 +0000

    safety breakinto mess trousers mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
