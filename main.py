3a67f47999370fbe23bbc0442bd6e77105a6f054
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:46 +0000

    whistle some forever sleep close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
