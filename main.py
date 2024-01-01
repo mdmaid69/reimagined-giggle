47a4353516aeaaabe592e4791704bc1286fc1d57
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:06 +0000

    suffer support fight also make

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_characters(sentence):
-        return len(sentence)
+import getpass
+def get_username():
+        return getpass.getuser()
+  def convert_to_binary(n):
+        return bin(n)
