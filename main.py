05cb515f95d214166c50e2456ce9a15c878ac18c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:40 +0000

    divide minimum need admit hope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
