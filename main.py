0482e9088382ba51ff7616ebe7ade326581c60e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:49:46 +0000

    reply trap conduct hit disturb

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
-print(os.getcwd())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
