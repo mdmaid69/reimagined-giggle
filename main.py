73aa9a778fb978b7ecf7e80aca33798d589bec8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:10:38 +0000

    Ff confidence pretend suit noise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_stack():
+        return collections.deque()
