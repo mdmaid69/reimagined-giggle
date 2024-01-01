ae06a080818c2c1a48c2bd453ffeeb7a98d72a91
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:42 +0000

    phone clearly cold contrast speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
