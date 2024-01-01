1b45cc8a0363d43d89600e33e257c1bff4625b4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:07:52 +0000

    ask begin unit order least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
