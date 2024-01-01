bb6f031c7f5c01db9f6e4a345f4fff7ea49a6bfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:34 +0000

    try ourselves combination connect choice

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
