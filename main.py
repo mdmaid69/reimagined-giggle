510d009642ef8d1ccba0f8800a510690eda4f67b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:54:03 +0000

    light Ll turnover over support

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
