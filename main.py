b7c9b2e61fb53705f5e1ab3b4690791083d77968
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:22:03 +0000

    afterwards takeback complaint wild their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_max(numbers):
+        return max(numbers)
