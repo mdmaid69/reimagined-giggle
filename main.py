cdae31d6838ce77b0d81231798b5b4836ce930bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:32 +0000

    worse crisis team minor evening

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
