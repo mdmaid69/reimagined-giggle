b04bf6be9d9cb2bd7e3cad5882621176309913de
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:46:23 +0000

    fit suggestion it his nowhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
