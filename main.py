475917abbebbce26094602ab382fb817ec51ad31
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:44:12 +0000

    embarrassing bread straight long its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_current_directory():
+        return os.getcwd()
