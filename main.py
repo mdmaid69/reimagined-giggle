7ea3107cfe43936521e211236a4320ff2be786f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:56:35 +0000

    there shoulder dare evening refer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
