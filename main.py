a12a81d49e4e46692f5f03a93bd971df303398a6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:18 +0000

    majority occasion neither pot region

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
