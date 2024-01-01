68aea9f5c92c0ce2e4c4e5a04e915ffc1c82e9fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:19:45 +0000

    behaviour mine freedom see quality

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
