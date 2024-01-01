68d3ee8f9792eda66cefa56203373e0c0ebaff9e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:32:47 +0000

    juice attitude may mess friend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
