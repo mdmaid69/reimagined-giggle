82fd21084c2d3b5de050e09e083d874f3f26c893
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:46:16 +0000

    run instrument subject nor protest

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def count_elements(lst):
-        return len(lst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
