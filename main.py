17d5e7ede01bccf1d5eed27271eb1d34acb392fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:22 +0000

    backwards exam content branch thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 logging.basicConfig(level=logging.INFO)
 logging.info("This is an info message")
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+print(math.pi)
