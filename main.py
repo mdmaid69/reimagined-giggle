744d8c99232e1aa82d4c27e440f6f698cf34969c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:15:37 +0000

    ride judge cruelty gap thorough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
