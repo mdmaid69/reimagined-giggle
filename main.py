f14ba5b8fd796e29ed84c8b6a61ed793e957e7ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:25:00 +0000

    connection fairly operate dull quiet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def is_even(n):
+        return n % 2 == 0
