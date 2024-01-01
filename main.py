ca47d00e253312b861ecaf1938ab48ea92b09eda
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:50 +0000

    makeinto 568energy interested even beneath

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import sys
+def exit_program():
+        sys.exit()
+import datetime
+print(datetime.datetime.now())
