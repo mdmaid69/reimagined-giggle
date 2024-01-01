e359c2b281d74db549cacb1a9ac87cd290bd33df
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:02:44 +0000

    drop fine post borrow else

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
