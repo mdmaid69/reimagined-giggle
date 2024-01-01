c8c8ea6be43fa590cc5fa6731f3ab55ffe1a8678
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:06 +0000

    foot pretty memory getback giveout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
