4630c25126b4238150c2340e64fb5defa9596bce
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:34 +0000

    far narrow rather nor last

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
