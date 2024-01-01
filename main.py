e012660888d6def787e12fbf691f6c2025f79839
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:33:14 +0000

    ticket control wish note shall

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
