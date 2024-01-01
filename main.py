cf5465c4cb25a6af05f2c8d335dc2a65c1225e72
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:16 +0000

    split healthy contain many discuss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
