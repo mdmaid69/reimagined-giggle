5912be9efacbc4f069792b86119855e2902d5b9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:18:28 +0000

    spray wrong cap squash Ss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def subtract_numbers(x, y):
+        return x - y
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
