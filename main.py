8e72e4fd9ffed96035a41f7132e4decc6190eabd
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:54:34 +0000

    stay comein spring else work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
