b9c5556eb542594424c72d7741e8950f1c4db956
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:02 +0000

    improve whisper care adjective ride

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
