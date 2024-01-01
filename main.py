aeb6dab537f191607d9039e05cca1912f680b309
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:01:33 +0000

    high vote cruelty rush good

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
