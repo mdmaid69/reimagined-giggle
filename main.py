d7f73a96f46941e5a755e9be108e456882e8d176
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:32:20 +0000

    uncertain search quiet live type

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
