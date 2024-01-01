7d1caa46ec4aa2a08c52415fb73522e1c8703cf1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:59:08 +0000

    noise by talkback float building

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_palindrome(s):
+        return s == s[::-1]
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
