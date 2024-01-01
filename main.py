3baf162f94ead40e0c4c27f44f3bc25b061eec99
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:30 +0000

    should not performance pollution reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
