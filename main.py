ec5b7dad1dd597db31ab29731a84f7cae4ea9b44
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:48:38 +0000

    ring pretty last advice tour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
