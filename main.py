20ce538ba42510ba7984d25835ab40636940d9f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:58 +0000

    proud lookup plate the warning

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
