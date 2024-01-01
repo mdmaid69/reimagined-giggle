147355c1be88443d23008e839a6861a0f113b4c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:33:53 +0000

    impressed health active plate low

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
