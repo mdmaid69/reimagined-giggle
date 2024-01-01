c7bf89c5bedeb9d1e9153f916fb4a5cbf52bdd7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:33:30 +0000

    damage approval honest concerning same

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
