4730cdf27bbaa6b4ab157301a7aa336b9042d615
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:36:40 +0000

    likely mess damp sleep concern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
