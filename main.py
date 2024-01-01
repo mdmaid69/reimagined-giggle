ff2cf89b0f578455cddf9585e61219e0fc72173e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:09 +0000

    hurry comeback trap bank Rr

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
