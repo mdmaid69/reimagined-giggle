c4f9e314fb822f05e4c28319ac49181b1dde8c9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:59 +0000

    afraid load thin face minute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
