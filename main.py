0d1423a07a3a0096c7d7528f18cec7559d00ae3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:49:56 +0000

    office appear argue land belief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
