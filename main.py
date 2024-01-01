00cab1d888f8e715a1d76588e55db5ae66ee3eb5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:22:54 +0000

    question discovery way squash chance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
