b2046cb09e82cd7f1a949c44480eecb8f07b1052
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:35:06 +0000

    study late safe press take

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
