4f296c8262405b7dc0fbb329ae51e9e3fc3f9622
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:35:42 +0000

    pump enthusiastic narrow dream lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
