1b718449c5de2ac6295373dd306c462ae8c3cd11
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:45:16 +0000

    treat week full division warm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
