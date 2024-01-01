2de2fcc3588596cfcdfcb393a69fb02c895dc006
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:58:43 +0000

    unless suggestion probably complaint Bb

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import array
+def get_array_as_str(array):
+        return str(array)
