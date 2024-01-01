77024c5ce7d462e3fdceaecd7e7182047a61aad6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:07:44 +0000

    often possible waste edge prove

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def find_min(numbers):
+        return min(numbers)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
