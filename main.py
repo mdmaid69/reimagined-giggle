3b9ce1a7f96296997ecbb62195022ec6147a8d51
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:53 +0000

    sentence prize circle estimate pick

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
