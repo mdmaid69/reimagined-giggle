c17c86cea3c70cd65fa894f2c1babb546aed82e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:24:25 +0000

    change weekend laugh produce law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
