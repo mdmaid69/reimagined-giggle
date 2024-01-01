7fdf02d2c7d0dd424e6779ad807cb25f136087ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:56:54 +0000

    present pollution Oo turn principle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
