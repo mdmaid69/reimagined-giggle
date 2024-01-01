871d58e1362ee9a77e9db42ee72acbac7af7f4d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:29 +0000

    dinner comfortable lookaround accommodation considering

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
