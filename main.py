7c8fb2157a6490369bf2d1369942f5bfd888e474
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:01 +0000

    principle train left line word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
