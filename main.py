ea44892aab91c770178a9ad2cdb362479c3cc70c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:34:16 +0000

    mood pollution imagination plan pity

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
