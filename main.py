f9fc0369e80092a407804d67bc7114a990539319
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:14:41 +0000

    brain fortunately turnon recent low

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
