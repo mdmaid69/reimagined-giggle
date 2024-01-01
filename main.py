d0593b92c2b9a0fb7e5ba6dae380d1f9cf51d79a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:18:27 +0000

    sensible intelligent each Ee exam

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def remove_duplicates(lst):
+        return list(set(lst))
