9ccc239f594eb271f7c8fdd2d1cbba96c06797ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:41 +0000

    present whistle battery our attempt

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
 import array
-def convert_bytes_to_array(bytes, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
