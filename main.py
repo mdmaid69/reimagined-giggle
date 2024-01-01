ff0165842289f64d7f957c33d91d1b0d88b4f343
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:15 +0000

    desperate some sense thankyou independent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import sys
+print(sys.version)
