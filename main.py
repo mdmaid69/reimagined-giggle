947366d638361940dcb1431a05012ce3f468da8c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:47:46 +0000

    forget turninto connected goback(to) distant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
