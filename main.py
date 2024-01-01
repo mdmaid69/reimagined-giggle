604f2a01524a079f5205d202f6bfc326b10b0156
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:58:39 +0000

    chat turnout shout unable car

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import random
+def roll_die():
+        return random.randint(1, 6)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
