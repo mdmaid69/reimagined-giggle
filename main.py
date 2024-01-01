90dc02dc6bb256424deee292f2860f6f841abd52
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:47:00 +0000

    continue limit describe frequent fact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
