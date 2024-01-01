24bfd0f6040a76fbc076f4ddc4af0b99e41dbae9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:21:36 +0000

    proof paper mainly top aware

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-print(math.pi)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
