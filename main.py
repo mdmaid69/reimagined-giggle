e5ec5371d8c847f083a07a10ee487532c29bbcf2
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:51:58 +0000

    that sound straight clean breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
