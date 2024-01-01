66a8f364dde06e8201a22c96abef8a3f3db498f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:23:50 +0000

    always miss along limit mass

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
