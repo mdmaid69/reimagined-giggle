c87fb0d996730c40640106f86511db4dccacdae2
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:17:28 +0000

    call Ll invitation nowhere exact

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
