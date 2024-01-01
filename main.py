1a0c632c83a1a029781068a11b8551d001fc3ea0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:03:51 +0000

    sharp rain fit opposite realize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
