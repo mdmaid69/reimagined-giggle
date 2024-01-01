4ab11de3621663e03cf01559d7fbd329196a26e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:43:34 +0000

    regional file honest short what

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
