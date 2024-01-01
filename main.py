d4737ed08657d2596c0561314fc3f59d6d8275d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:39:17 +0000

    degree eat later indeed behave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_itemsize(array):
+        return array.itemsize
