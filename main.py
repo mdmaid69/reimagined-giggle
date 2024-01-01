678a08c1a21bfdd7eaf1b65e1a90baa3d4b42ec8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:08 +0000

    difficult exchange deliberate patient reach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
