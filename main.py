2b41ee67445a05b5a39d5a68ab9df2f192118dfd
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:24:24 +0000

    worry instruction naturally common wave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
