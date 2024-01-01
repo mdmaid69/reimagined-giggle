6fbf9779eedbbfedf4b84541494f94199cb759be
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:54:39 +0000

    talk ilovedicks evidence chief night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def subtract_numbers(x, y):
+        return x - y
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
