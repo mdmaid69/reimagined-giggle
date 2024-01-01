9a35907bffc2f8a6370c757e7faf1a0a21c13dad
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:54:42 +0000

    stamp influence give Zz space

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_pressure(force, area):
-        return force / area
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
