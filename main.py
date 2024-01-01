590de6e411185c55657a794441225938a23bcf11
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:47:08 +0000

    answer reliable actually prefer trouble

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
