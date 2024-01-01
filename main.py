b2c4260feab75348fcaba7906e181b24ed12b61e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:46 +0000

    hurry otherwise sensible important wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_max(lst):
+        return max(lst)
