e3c65fa824b2edef6cb8f0219f98d9a299686d84
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:08 +0000

    support many truth light worst

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def convert_to_octal(n):
+        return oct(n)
