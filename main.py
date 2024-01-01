3f5d3b9bf2b9e7570e5e54eb750029fd51196602
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:06:56 +0000

    serious see pause report direct

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-def find_min(lst):
-        return min(lst)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import getpass
+def get_username():
+        return getpass.getuser()
