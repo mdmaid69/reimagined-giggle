c14e08f892a41ea1aca946044ce39eec66bf2ed9
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:45:05 +0000

    bar certain delicate politics very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
