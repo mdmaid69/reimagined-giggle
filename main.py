63ab70a3e778daec5a1e8e6918828aac90526a02
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:25:26 +0000

    expression whenever centre bye downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_sine(x):
-        return math.sin(x)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
