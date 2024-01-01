70032192d1affb3c56966c3a761b77b92788968f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:10 +0000

    criticism exchange minor ring whom

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_counter():
+        return collections.Counter()
