d86a5fc99e4cd08da883e2520e7452978009dec7
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:16:06 +0000

    aware about sight description also

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import time
+def get_current_time():
+        return time.time()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
