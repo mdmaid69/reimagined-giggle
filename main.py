8f2ff833d4775e0753a2da82175bf73114ac9deb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:53:41 +0000

    phone getaway Bb influence bringforward

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
