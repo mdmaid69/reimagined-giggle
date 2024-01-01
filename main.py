25ecfc1480522f1ced802446c0f1c97f589c653d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:21:37 +0000

    aim tight godown comefrom guard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
