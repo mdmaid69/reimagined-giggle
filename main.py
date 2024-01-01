b272a194ad28a9fbaf60f3691e75f887baee589b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:23:12 +0000

    sale fine so conclusion force

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
