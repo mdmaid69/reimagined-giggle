d00bf71950a3138a563e2184c49c4a9c422b9d3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:29:44 +0000

    suit bin appeal step influence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
