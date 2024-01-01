5b9fe559b12ab11c774b196ce35c1863a0500436
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:52:03 +0000

    voice dream congratulate various connected

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
