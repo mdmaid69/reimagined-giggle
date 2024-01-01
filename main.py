f0cd626c0c1822e6e4a90dbbba60ab3801d6e063
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:23:29 +0000

    flow instant town holdout enthusiastic

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_bool(array):
+        return bool(array)
