90a8b2adde904451cc83717c517aa900f5b627e6
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:26:47 +0000

    seem suggestion unpleasant treatment split

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
