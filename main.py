210aea616f2652187882d2667fe106beb74e330d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:12 +0000

    comment band boat opportunity begin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
