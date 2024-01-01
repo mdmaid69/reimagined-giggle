035cb9a1db71c1d8cbd58611181b00b5d0243a63
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:47:21 +0000

    pinch succeed scream within age

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
