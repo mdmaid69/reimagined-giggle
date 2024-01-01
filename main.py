8a924c3844af06a74d71ce8b64aad8d4cabee6b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:29:30 +0000

    value succeed up climb obtain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def remove_duplicates(lst):
+        return list(set(lst))
+for i in range(5):
+        print(i)
