2e01250998295762b33088b42c2df644c46354a0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:38 +0000

    572enter claim sad seriously the

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def subtract_numbers(x, y):
+        return x - y
