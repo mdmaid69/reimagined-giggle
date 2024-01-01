573d0225fb663eab77563b7f5f677e781f1bc9d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:07:20 +0000

    oil subject artificial hit performance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
