4c48e222ca911a6c181ea6b312a39bd5d17f0308
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:22:46 +0000

    upstairs holdup joke rain study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
