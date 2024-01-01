d5d4786260938e9aa6ffcb1b49bd156d8fe6da7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:52:08 +0000

    hers lay bath turn keepoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def square_number(x):
-        return x**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
