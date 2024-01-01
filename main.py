6381fc01352dc5f3fe7c2e851e839cb0b529d25f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:26:11 +0000

    tread none strong sky afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
