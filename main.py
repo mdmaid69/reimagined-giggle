60cce4d6756ef59b4dd8494b939bcdf6c1608559
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:49:45 +0000

    accommodation public uncomfortable thin classroom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
