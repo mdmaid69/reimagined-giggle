64988fc0fe71811b9be9cee108f4ad66654def20
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:55 +0000

    turnup invite town after favour

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
