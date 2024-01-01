8e7ed4c23b505c8c1cb800ad9efe2a9cc14fa557
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:03 +0000

    method wind turn peace flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
