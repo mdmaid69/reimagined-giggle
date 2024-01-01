ac662e6a1bdccf9b5ade96dea282a964e7c74ab3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:40:26 +0000

    join giveback operation destroy prize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
