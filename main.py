56e1eab4fd80d6747c194315a279bb89f207c410
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:01:30 +0000

    hand nervous surface full worse

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
