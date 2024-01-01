02e4d8ba798f5532840fa7e7ff294ef6b4b89e52
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:50 +0000

    this whistle limit none fault

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
