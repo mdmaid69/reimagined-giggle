c6da9a13add55ded2598056215852f3f5795e317
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:29:10 +0000

    past public spring cause woman

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import time
-print(time.time())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
