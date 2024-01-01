ae27fa11744dde928028ef6ddc9d89afdbcc8456
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:37:15 +0000

    dish run ocean neither estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
