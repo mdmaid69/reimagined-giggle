5eb2e351380496bf5ad9c04f7bfaa705d2520d3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:38:22 +0000

    crazy Yy powerful 564encourage total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
