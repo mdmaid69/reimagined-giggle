047123b649906335465627b417e0ab299f188f8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:18:26 +0000

    wherever guide sea opportunity error

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
