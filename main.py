f290c6180fe355b8b67730fb6c6a7dcf35f07f2a
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:43:25 +0000

    rush mother proud hear smoke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def sort_numbers(numbers):
+        return sorted(numbers)
