3dc9716c698694815e176f056e04cba75b326242
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:24:47 +0000

    train trust tip little stamp

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
