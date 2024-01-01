be3361d84da61b2e91cb0ecba43db22af2b3a7c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:52:39 +0000

    check 566end group though side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
