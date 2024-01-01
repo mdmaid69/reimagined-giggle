8f740fd21b84c2f76bc624f39b642deab175fafe
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:30:00 +0000

    pronounce Pp prepare hand large

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
