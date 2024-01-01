6e07bd15c49e7365ba596614e30fb192869ed4e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:22 +0000

    recover consist measure cause possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
