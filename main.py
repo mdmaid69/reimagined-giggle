f354826f144d7938f63bdddf5a62af7526181718
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:26:47 +0000

    whichever later office rule exhibition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
