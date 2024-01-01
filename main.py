867987098f492ce0e88262301f9a71168528039a
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:38:09 +0000

    communication set copy plastic Cc

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
