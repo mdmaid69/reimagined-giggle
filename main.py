6083a9ffe28840a1a8dc9dfe676e12c448bd484c
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:21 +0000

    find towards rise possession walk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def add_numbers(x, y):
+        return x + y
