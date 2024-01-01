f2c29112a51735b08a867b0fd26c966ef291ac9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:11:33 +0000

    runinto work already spread separate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
 def pop_from_array(array, i=-1):
         return array.pop(i)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
