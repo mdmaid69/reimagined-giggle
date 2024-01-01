dd1f7f72e57383b0c2c9f8e74b2580c20423b47c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:06:24 +0000

    guilty stand behind 566end remain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def remove_directory(path):
+        os.rmdir(path)
