430a800473daf40187bc9ee8615950ff851063c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:45:35 +0000

    advantage traffic person journey yesterday

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
