6465b5bd2681043dc5b5ace7c96b54b819b34754
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:33:11 +0000

    tread honest worst plan opposite

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
