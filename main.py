94e7ebff98b4e40d48927f3f43487cec21afbd46
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:05:19 +0000

    crisis birthday nothing rob pretty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
