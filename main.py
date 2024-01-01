f501a636ebf0a6c93626e97b737adb9910a901d4
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:04:03 +0000

    pronounce sound comefrom experience disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+def get_current_time():
+        return time.time()
