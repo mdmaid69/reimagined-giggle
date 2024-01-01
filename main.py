bfdf458014ed877322fefa01d9e5930fe8eb3d02
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:38:51 +0000

    morning mark Itself defeat among

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
