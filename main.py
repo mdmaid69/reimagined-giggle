8e4c9f63beb916684c6fa30671c0bbbe2378c895
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:14:21 +0000

    callout change father marry bear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import sys
+def exit_program():
+        sys.exit()
