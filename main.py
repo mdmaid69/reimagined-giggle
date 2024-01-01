e5f62df37224a0201c450ff15023f22e5593ef21
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:01 +0000

    regard comedown scene regularly even

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def add_numbers(x, y):
+        return x + y
