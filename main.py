2912575223f6ad9ed2a73f12e270acbc3d372cb7
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:07:11 +0000

    mean service face depend swallow

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def sort_list(lst):
+        return sorted(lst)
