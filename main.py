9ca085d51155825e48af8cca80f906ce9d4a22ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:53:07 +0000

    sad father march find cheque

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def square_number(x):
+        return x**2
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
