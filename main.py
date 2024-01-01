3a8c60567eb4cd2370b9c5f1ee9fe3681c24c630
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:10:11 +0000

    will further either month sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
