4c8c4f1b2bc4e962c26bbd02954648cfeb9f0c31
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:22:33 +0000

    himself waste battery under design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import array
-def reverse_array(array):
-        array.reverse()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
