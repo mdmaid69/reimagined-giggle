7e88ff37263cfcef0c457b143a7ab63b0a9f2a23
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:34:32 +0000

    fail occasion pause general mess

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
