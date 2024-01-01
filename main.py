3140cea26c0ef77e88cbbdb84a73dedc6dc0963b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:27 +0000

    mind stop reduce leg female

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+print(sum(range(10)))
