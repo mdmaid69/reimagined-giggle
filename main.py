ab5aac54b583c61f86ef49b5d1292cd71c2ea131
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:00 +0000

    talkinto official damage some flood

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
