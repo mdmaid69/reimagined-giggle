f533039dc5efbf8dd6f837ae7d551f1497ec238a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:17:46 +0000

    angry communicate effect arrive early

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-print(sum(range(10)))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
