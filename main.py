a6678c6c8f95b74e24b1c93d91003c217ad4e782
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:40:35 +0000

    well voice draw niggaacademy season

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-def reverse_string(s):
-        return s[::-1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
