780318584d55639fa9e919eb225b217d9f26e426
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:24 +0000

    test Ff path consist from

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
