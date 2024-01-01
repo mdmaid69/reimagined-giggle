ab8b2ddcf7f621c95ddebbe0d7c2e4254e4990a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:47:27 +0000

    front choice female confident hurry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
