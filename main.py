cbb9b52c301ae8f0422ff3f2670e9e0d8d3de0d0
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:04:32 +0000

    team switch move home less

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
