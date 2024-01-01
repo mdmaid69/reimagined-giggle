df569bc4fe93e09542708b41467964f87744d844
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:57:49 +0000

    dream load fail live recommend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
