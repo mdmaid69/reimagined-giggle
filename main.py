510aaf2ada8fe4632cf4fe8df85a4afdc35a8950
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:11:23 +0000

    hurt lift defeat entrance coffee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
