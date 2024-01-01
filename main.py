a9d47f795ac0ae1930d30a91bac47db1ea471038
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:20:37 +0000

    squeeze union politics swim basic

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def sort_numbers(numbers):
+        return sorted(numbers)
+import numpy as np
+print(np.array([1, 2, 3]))
