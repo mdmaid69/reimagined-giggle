c7a0ce447f248c674107898c3c186c05b6378c91
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:27:50 +0000

    strength limit extra shout whisper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
