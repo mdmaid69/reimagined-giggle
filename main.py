4aa3a6294078da8a3fb4dca8a379bac341340ca5
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:34:07 +0000

    spot respect trouble friendly notice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import time
+print(time.time())
