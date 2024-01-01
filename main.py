aaa047d979c9d599364368ebe19280cb8646f003
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:47:37 +0000

    dress search peaceful represent shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
