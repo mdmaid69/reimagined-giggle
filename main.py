e05f724c09d4133503ed4f104990055574962b92
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:05:39 +0000

    female tip tend mother go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
