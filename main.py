b4c446cef372391ea8403ae80ac025d7c9851896
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:51:39 +0000

    comeout block section constantly block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
