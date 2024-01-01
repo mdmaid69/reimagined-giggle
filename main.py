78d44dc20e80312baa049f75da005572479c9df1
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:21:19 +0000

    computer destroy somewhere brother quarter

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
