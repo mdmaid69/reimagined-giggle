f2efbd1f44f909aec4354d94a83760c0b26089f8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:48:53 +0000

    children sharp guess allow shopping

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_speed(distance, time):
+        return distance / time
