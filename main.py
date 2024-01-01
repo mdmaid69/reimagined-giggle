9844c6343f57917baeb95ce11cdf0bf01eaba30e
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:59:01 +0000

    nor file strange charge ocean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
