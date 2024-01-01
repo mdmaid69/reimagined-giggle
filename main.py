289338c1178e5884ba250828ee19bf5ffed9de9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:19 +0000

    secret what uncomfortable concerned now

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_pressure(force, area):
-        return force / area
-  def reverse_list(lst):
-        return lst[::-1]
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def remove_duplicates(lst):
+        return list(set(lst))
