2689b98320351c6952f2f79bd49b535d0a5a8b0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:52 +0000

    return whenever congratulate well introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
