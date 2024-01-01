8e1d56868c9a6b3b594bee9e7e96b96d4ac98543
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:07:45 +0000

    butter legal splash Ee achievement

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
