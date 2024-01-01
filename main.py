baf8442ee0720ad9d883ebc27ea59dbd488ce897
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:47:20 +0000

    dare lesson imagine load please

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def is_even(n):
+        return n % 2 == 0
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
