7e2d08d4274747f32e5fc558ac5bf9b49597777c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:17:43 +0000

    perfect steal shade getback split

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import platform
+def get_os_info():
+        return platform.uname()
