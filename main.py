2c752d1454aba23912203ba6456d47efa2e6761f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:49:43 +0000

    once advise funny Bb Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
