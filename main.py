661984d49036ec2b05c84119a67de4a52b16a274
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:08:53 +0000

    pattern speak rush unless smile

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
