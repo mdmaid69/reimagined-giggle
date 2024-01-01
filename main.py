64e0aefbe99157fdec74a309a4dbb109ab825b63
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:39:22 +0000

    sport roll cut otherwise another

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import array
+def append_to_array(array, item):
+        array.append(item)
