54cecd36b1a59584efe2b8aec1fe5dc0033a5699
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:28:54 +0000

    telephone mean for course slip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def find_min(lst):
+        return min(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
