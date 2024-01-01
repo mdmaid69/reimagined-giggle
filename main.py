94d9d674be67b4b5ca4fa9a2ba9759b3d092dd4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:11:52 +0000

    sting mother breakin bother force

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import array
+def clear_array(array):
+        array *= 0
