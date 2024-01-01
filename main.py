5c7216fa3c9a65786cadc3a8289d4ae0f7000123
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:56:21 +0000

    concerned correct radio sweep after

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
