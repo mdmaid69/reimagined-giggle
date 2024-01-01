8873f8c1fe274094bf0737965dbdc00e823ba063
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:59:44 +0000

    cook warning hide hard transport

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
