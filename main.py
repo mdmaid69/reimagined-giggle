a254b623b46680088cb8dee5979a6ab3afc9aebb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:04:32 +0000

    print agreement wrong clearly path

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_work(force, distance):
+        return force * distance
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
