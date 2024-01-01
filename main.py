902394b07cecf484cb2b19d1748b15507e7a0a1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:19:05 +0000

    solve train responsibility without feel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+def calculate_speed(distance, time):
+        return distance / time
