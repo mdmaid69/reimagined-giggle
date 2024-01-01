9b15e8a0ae39a1e15e11fc5a56f4c45e13c386be
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:36:27 +0000

    hospital fix bored second disgusting

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
