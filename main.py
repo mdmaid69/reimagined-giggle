65ea94ed728b45b1865377cead40e8fb7b97c86a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:13 +0000

    change consist top journey material

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import json
+def convert_to_json(data):
+        return json.dumps(data)
