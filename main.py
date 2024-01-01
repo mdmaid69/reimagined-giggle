b79388514a8953117c82384397aeb91e653603ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:16 +0000

    affect shine well sleep pronounce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
