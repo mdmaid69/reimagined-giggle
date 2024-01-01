ccfb7f9712d7b854b7a72c9f5c50db3625d2253d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:27 +0000

    theirs later move from sharp

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def multiply_numbers(x, y):
+        return x * y
