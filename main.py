353817ccaddc66e2348575691480d538c6c8b1c9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:11 +0000

    giveaway after blue rent project

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
