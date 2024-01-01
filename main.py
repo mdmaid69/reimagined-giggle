030e2bfcc7521b6f496e35f3caf050c87fae7938
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:47:56 +0000

    foreign record way comfortable radio

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
