ce2d1450339bdfca3b5a4d0ecb6dcb0205476d50
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:54:57 +0000

    description bury ordinary fast fair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
