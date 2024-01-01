a191da8e9f7f14f47a08caa9c1c2ff889d86ad04
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:31:44 +0000

    meaning early complaint deserve smart

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-print([x**2 for x in range(10)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_area_circle(r):
+        return 3.14 * r**2
