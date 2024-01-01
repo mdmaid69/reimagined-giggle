9499a19e4d7f7a024b291d80614b2711b5d5f4a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:29 +0000

    space sensible fix height pretty

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
