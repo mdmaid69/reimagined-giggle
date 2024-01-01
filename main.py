fb1d2232e4e5e9bba4e9f22261b755ac9baaf80a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:28:48 +0000

    tap swim level connect nurse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
