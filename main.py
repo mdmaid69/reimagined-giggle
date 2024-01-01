c73c095663f5c1117f200da0ec5fdc4875b2b8e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:30:23 +0000

    enthusiastic close unhappy difficult or

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
