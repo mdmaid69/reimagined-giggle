dcfba5c14bf3d1a0cd6902f380758e385b4dcc40
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:34 +0000

    pot port attitude shut passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
