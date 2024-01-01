1bf530fe63b3e353449233ea78ede03f3dd23092
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:36 +0000

    suckme drop either tap sore

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
