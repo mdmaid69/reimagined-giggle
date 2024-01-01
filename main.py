c499e8ee874c5946a3b4d48cf4ae282197b39792
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:37 +0000

    mine share sit many tense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import sys
+def exit_program():
+        sys.exit()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
