c68ba81b8d289cebe11c79b3b652ea28c4856107
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:55 +0000

    common together change go during

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   def calculate_perimeter_triangle(a, b, c):
         return a + b + c
