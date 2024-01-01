d4086d8ac8a33f732deb266a6f5e14c0dad6da8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:28:18 +0000

    club post deserve break attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_array_to_string(array):
+        return array.tostring()
+def reverse_string(s):
+        return s[::-1]
