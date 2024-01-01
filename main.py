8ad2eccdbc15711c1d874cc73b7a007a34701f4f
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:00 +0000

    pocket comment teach practice perhaps

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def find_min(numbers):
+        return min(numbers)
+print([x**2 for x in range(10)])
