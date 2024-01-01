4f378fe68f78a1464f94f46dddde974b394a86ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:43:58 +0000

    probably own awake than instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
