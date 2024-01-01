6ba8c7f8cba0541d64457a06a040728cdb308822
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:05:08 +0000

    flavour scene sad major under

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def add_numbers(x, y):
-        return x + y
+def calculate_work(force, distance):
+        return force * distance
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_bool(array):
+        return bool(array)
