9aff94d8c0642c263fb868d68f22347dccbb1c46
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:41:10 +0000

    straight it number promise spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
