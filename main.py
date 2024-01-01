36afc2e61c0e3342fe6b5b07f374df8817010a80
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:15 +0000

    declare identify smooth pool sentence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
