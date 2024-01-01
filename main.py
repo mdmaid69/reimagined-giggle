e20d39ac7a12793d873e02d811886216e3089c02
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:46:39 +0000

    hurry advice label free guard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(numbers):
+        return min(numbers)
+def calculate_power(work, time):
+        return work / time
