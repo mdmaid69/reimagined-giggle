e2de76e8b49f2e14c72afca745993302a0e48419
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:46:58 +0000

    file write 570enjoyable before hello

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
