0412fe95295641b84845697e0fa2afa423bbf92d
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:47:45 +0000

    round equipment work until progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
