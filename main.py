2789efb1e1af9f1e00945109156dbddc06a95509
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:05 +0000

    question direct attention confident pause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def convert_to_octal(n):
+        return oct(n)
+import collections
+def create_user_string():
+        return collections.UserString()
