5c09cc395948e54290af66b92ce58afca5894334
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:07:56 +0000

    worth occasion reply whistle blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
+import os
+print(os.getcwd())
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
