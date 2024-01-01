7cedbab583716f16ed2a7274c703fcfb7e35af6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:48:00 +0000

    see visit culture perfect dream

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_index(array, item):
+        return array.index(item)
+import sys
+def print_python_version():
+        return sys.version
