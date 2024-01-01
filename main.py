a7df2e7289fa41969ed2ea8ac85dbc83c3d4475f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:36:49 +0000

    tense gentle best count stage

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def count_characters(sentence):
+        return len(sentence)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
