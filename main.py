fd1973f2f49735fb5977985eb3aeab67d12f6247
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:10:01 +0000

    passage still proof later convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
