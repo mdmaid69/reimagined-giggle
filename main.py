f4cdf10684911e46368c462e584b4240d3701047
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:00:25 +0000

    force man breakout perform thirsty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
