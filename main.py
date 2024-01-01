671ab45e3979b82d3b21fd3d391ec1f1b2135797
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:48:32 +0000

    long such slide list eager

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_work(force, distance):
+        return force * distance
