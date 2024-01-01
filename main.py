4b875448f5d7d7d0aad4bcb9a9b73f4d03bcbe6e
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:35:14 +0000

    breath average prevent split angry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
