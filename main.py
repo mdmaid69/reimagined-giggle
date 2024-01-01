ff53d8d7f2a161cd6d765602430f176291639243
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:12:39 +0000

    beside develop deliberate circumstance anxious

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
