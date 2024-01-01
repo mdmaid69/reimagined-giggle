fd0806ae80e640eabd33b92068fd4fabb7a85ed3
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:36:14 +0000

    ask slight debt mine preparation

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
 import glob
 def find_files(pattern):
         return glob.glob(pattern)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
