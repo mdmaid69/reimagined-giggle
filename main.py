1e8c4eb052b116feb4187d33cf03b9ffa8c2c31c
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:57:16 +0000

    past could properly land bet

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
