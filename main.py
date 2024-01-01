a563cd21f6c2af6696d6d10058522155fb4cc9d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:14:45 +0000

    or state save job rush

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
