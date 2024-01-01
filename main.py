e74d6ff8718500cb9ae5f5fcc21eb21f5e22b880
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:01:35 +0000

    development but any pleasant however

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
