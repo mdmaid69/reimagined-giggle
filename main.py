633042e76d04a5fcd7d9647311467fbee14f3111
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:55:09 +0000

    slide field sort bar at

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
