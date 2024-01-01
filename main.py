2e4f7539fe63d8b1421a6fe41f89d7cc49379f11
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:54:34 +0000

    listen switch people song exhibition

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def reverse_list(lst):
+        return lst[::-1]
