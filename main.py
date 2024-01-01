2396744cc4959b1358b2d1e39e9977d14e1db43b
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:50:58 +0000

    result disturbing project difference though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
