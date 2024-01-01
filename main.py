c2172ae2594aabe0bd96b1f167141974bb270e6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:24:55 +0000

    duty control noone bottom month

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_base_name(path):
+        return os.path.basename(path)
