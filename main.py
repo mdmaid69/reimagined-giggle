69cb6e274347e03d4bb5b256ab5bffbdb87a486a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:46:57 +0000

    fly town mine negative soon

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
