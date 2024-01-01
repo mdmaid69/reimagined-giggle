c0dac6c4a9d46f3545ac5c109980018618ce4138
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:30:06 +0000

    worried house final surprise disturb

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
