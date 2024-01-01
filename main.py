cde1f645fb781ece60618507d88bf5cc972e7295
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:14:36 +0000

    there 566end oil promise problem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
