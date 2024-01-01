9e1201c1defed291b8d46421ebc79ba355e7d6d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:12:15 +0000

    day seriously pretty sky cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
