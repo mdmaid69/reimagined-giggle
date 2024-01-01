51f7ae2b166d16142a2f81482eb128a4f837442c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:26 +0000

    comfort breakthrough clearly dance flow

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
