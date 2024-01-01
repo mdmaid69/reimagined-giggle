b3aaa141bebc3290dcd66db663e15eb9f0e9d56a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:52:42 +0000

    spill complain move wash final

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
