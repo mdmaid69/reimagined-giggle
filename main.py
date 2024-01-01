e406212974869dca0c3e71886e46b4ecb6fadaf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:15:36 +0000

    plastic border wild suddenly mess

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
