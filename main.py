8ada58acb9ef23239500ac103bb683b43ae7da32
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:06 +0000

    limit take clear smooth invite

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_as_repr(array):
+        return repr(array)
