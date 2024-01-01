fc14605a3dec36b58e3995ff8e2195a4d70d24ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:54 +0000

    mysterious common thing become stay

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
