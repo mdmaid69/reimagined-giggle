d8b21a74c72838a5bdcb89475b9046c0afbce9cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:03 +0000

    imagination argument runinto steam store

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
