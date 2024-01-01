8ab6cbe0acc9067f5e2dc59a0f92cc23d977c022
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:01:15 +0000

    will judge post refuse transparent

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def is_odd(n):
+        return n % 2 != 0
