6526344250d480a24e6064c0bfe5c8cf73d8aa01
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:47:15 +0000

    like pull wander stamp mention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
