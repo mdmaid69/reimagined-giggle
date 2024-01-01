5735dfcaac186cb2b223d00366fef16d098f7c4e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:25:27 +0000

    worry hole fond lend deep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
