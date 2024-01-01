605ede12e0de4cb37d66d67e831a352930ad1477
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:59:23 +0000

    present wave close Tt recover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
