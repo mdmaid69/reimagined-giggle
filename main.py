71fb4dd527f56fb50d82f8716fd9eab1f94c5e89
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:40:35 +0000

    flat fierce this smell safe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
