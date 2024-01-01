5fa5468cbdb0c384b4fea908e1712c025a941884
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:09:31 +0000

    Zz interested the key lookback(on)

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        return sys.version
+from collections import Counter
+print(Counter("hello world"))
