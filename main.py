7df8451d08b17cbd0ed067a352b50dfe0b2ec2bc
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:48 +0000

    likely place plain goaway that

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
