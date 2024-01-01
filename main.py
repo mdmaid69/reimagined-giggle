181f7db008829b5faedee09f4199c02e6ebab986
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:58:22 +0000

    hurt exaggerate slight capital plan

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def square_number(x):
-        return x**2
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
