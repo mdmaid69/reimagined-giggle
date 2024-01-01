9485440f97a720269af015b994f3a90d3aa2bc42
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:52:49 +0000

    warn runinto picture factory can

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_current_working_directory():
+        return os.getcwd()
