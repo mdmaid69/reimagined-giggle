340237090ee962e3e6b1940d41f0c900d32651e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:50:58 +0000

    order drink wide try middle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def get_array_typecode(array):
+        return array.typecode
