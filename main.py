19c7682feecb3524eb148169e5d3757b105dd1d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:08 +0000

    already distant early change treat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def square_number(x):
-        return x**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
