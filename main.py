97f7f7e23c3d0b62f4b336ae59f45051980c0128
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:22:37 +0000

    uncertain fall be minimum clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
