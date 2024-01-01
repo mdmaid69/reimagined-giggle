dbf3eb8a26a986ee190b1100af92d6dbc776ffee
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:39 +0000

    Qq sort preparation save stretch

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def divide_numbers(x, y):
+        return x / y
+import random
+def roll_die():
+        return random.randint(1, 6)
