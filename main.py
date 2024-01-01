847874e05bb650f5c76619deb2f9d06ec6114a6c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:16:47 +0000

    mean law switch reject 565end

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def convert_array_to_list(array):
+        return array.tolist()
