74c97a0f13ff37667547e010e98d5a6f3e5eb4f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:18:24 +0000

    container various bring play number

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
