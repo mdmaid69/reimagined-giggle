0985c3789d20571deca61a45a44194f2ec1fda5c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:07 +0000

    character song estimate pencil nowhere

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
