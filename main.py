14768b031fe5c0dd090f5280618725c48c2e0e71
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:23:53 +0000

    nowhere comein murder backwards pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def square_number(x):
+        return x**2
