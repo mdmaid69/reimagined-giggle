cb71fd703856019b52dc53d3d898abdfeff76841
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:59 +0000

    talkoutof visit precise image summer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
