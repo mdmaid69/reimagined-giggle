8eb95da60d2601f8115670a4946ce805ec91c4b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:10:26 +0000

    usually schedule window heart light

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
