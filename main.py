12bc8bcee28533c28e1b439e8dbd7a9e71237d9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:37:50 +0000

    course wind visit crowd several

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_power(work, time):
-        return work / time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_volume(length, width, height):
+        return length * width * height
