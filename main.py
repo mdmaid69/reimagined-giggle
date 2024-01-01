7059cab7aae47598aa435aa07299832809f85563
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:22:06 +0000

    smoke sentence tip behaviour wave

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import random
-print(random.randint(0, 100))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
