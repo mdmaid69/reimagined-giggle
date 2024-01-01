ea05a3cb8ec2776a950c43a0454b2e00cf89df47
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:15:18 +0000

    central ready or tear custom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-def fibonacci(n):
+  def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
