de99887331e4d7298366c41c78101eb6b3a6da59
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:35:38 +0000

    sort pause touch whistle film

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
