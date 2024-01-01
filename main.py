210916804581c91727ba582a193b111413bd2aa5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:21:37 +0000

    common choose wear by day

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
