823fb6878ed4513c225bd12e7159558aaf33293e
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:28 +0000

    ring criticism wander reduction contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
