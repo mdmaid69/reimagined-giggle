ce63be4115328191c18e77067271c60f78504092
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:45:19 +0000

    off design lift congratulations shallow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
 import array
 def get_array_itemsize(array):
         return array.itemsize
