68fa2ab2dd6dca6b9d9ba21ed1f278b957400f54
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:06:46 +0000

    have down all nose sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
