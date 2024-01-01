071b228df3a44163ce02000730c7f38d7d123579
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:40:22 +0000

    translate how previous trust test

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
