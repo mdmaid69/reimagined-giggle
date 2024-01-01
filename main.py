26be7fbff302a5fcd39ec1ad33ef19da0513f16b
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:10 +0000

    cry sweep lay aim myself

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
