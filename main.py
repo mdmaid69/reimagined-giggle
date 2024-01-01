6b91d488387e5811e41f5ca35ea39e326a943014
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:33 +0000

    public make force success entrance

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
