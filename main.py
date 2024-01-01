a92817f176806c60b02355381ffeab4daa145fe2
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:37:58 +0000

    include over sensitive scared slightly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
