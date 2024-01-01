4aa164c512c112c006eb5ce57bb2ba1742f3c68c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:38:25 +0000

    lean face progress fit statement

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
