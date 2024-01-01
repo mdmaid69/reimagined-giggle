71cc3bebee0d28450f89540dbcaf6e8b007d4502
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:15:01 +0000

    journey callup brain down friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
