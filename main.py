846ee1bf711416b3d502ab1546944acc9e416fbd
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:43:30 +0000

    giveup excellent clear aware help

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
