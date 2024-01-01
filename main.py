3644fbf89a72caf441ce61db437f23961ea46c55
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:06:26 +0000

    perhaps solve defeat whichever laugh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
