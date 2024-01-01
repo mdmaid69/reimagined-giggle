56735a932b584cc00b38204337edc7a77b4eb2d7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:55:03 +0000

    chance people tight takeout age

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import platform
-def get_os_info():
-        return platform.uname()
+import sys
+print(sys.version)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
