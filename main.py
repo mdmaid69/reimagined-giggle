eefd3f7698a828f3b3adfaa779c8918e2043b019
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:15:45 +0000

    official pen runinto represent image

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+for i in range(5):
+        print(i)
