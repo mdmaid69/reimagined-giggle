163c4fa523390935d5bc5731f16ab899d67e8f6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:49:40 +0000

    workon authority I over criticism

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
