fa73c93bb2d2fe7da317e69eaa606fd23f387336
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:41:58 +0000

    demand protect challenge next comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def find_max(lst):
+        return max(lst)
