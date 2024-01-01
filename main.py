35bfbfd3ca4c8249d585ee33d71637ba29c1354d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:50:09 +0000

    noise about report pause plant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def subtract_numbers(x, y):
-        return x - y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
