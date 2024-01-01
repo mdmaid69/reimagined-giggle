00d7e8ef4eebb2b11b4aab021b6ef9837a1b93ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:22 +0000

    considering makeinto touch only waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
