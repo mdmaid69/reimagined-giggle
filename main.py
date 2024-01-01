5606d22becba7bf2aad2d1f202c32bd66f69c13a
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:49:34 +0000

    similar nervous heart believe tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
