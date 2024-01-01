9892c5943331241fd619a6cd190a6adf2f60073a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:24:08 +0000

    step lookafter journey exist clean

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import sys
-print(sys.version)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
