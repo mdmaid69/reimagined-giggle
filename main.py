d0a5b94f2a67c5c83f0a2e95d8bcd03d842ad444
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:19:29 +0000

    advice post satisfactory heat all

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
