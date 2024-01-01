59b848096a38aab16c44990e6354d803eafc3b1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:27:53 +0000

    something this begin thankyou Jj

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def find_max(lst):
-        return max(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def get_array_length(array):
+        return len(array)
