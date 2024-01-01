9dadd9b0ca7b12ab5d87606fc8af44599d8692ab
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:40:38 +0000

    number refuse case afford demand

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
