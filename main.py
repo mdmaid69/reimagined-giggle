3ca1fabe960dc67ce1a5378b7b81b16bbd39df82
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:30:33 +0000

    slight something even rain evening

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
