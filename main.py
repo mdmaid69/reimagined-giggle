e1cf311f3506cbd9849b3d3aeaebfd5f06871af7
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:46 +0000

    my Kk whistle lock record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
