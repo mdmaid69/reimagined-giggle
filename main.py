ed70943b781125565dc99adea98fd7fd0a09db77
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:49:54 +0000

    business almost closed believe even

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
