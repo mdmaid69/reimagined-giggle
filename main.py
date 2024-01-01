859d98fb881b0fd01336615d52fb371923c4ba6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:51:36 +0000

    over straight father cool content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
