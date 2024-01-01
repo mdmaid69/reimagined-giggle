c3b85291e473c2c0f52b8083e362d3899c4f405f
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:48 +0000

    general leave 564encourage keepout prevent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_min(lst):
-        return min(lst)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
