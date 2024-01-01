44bbef18aa26f26b3771e042495f01e333ff5e6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:40:16 +0000

    direct hand smell black snow

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
