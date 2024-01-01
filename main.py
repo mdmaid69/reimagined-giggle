84570ba391bce45957d254901ea733532a589204
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:45:41 +0000

    passage well back trouble love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def add_numbers(a, b):
+        return a + b
