377168e555aac409b828b7a40092a6f82c6f56b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:40 +0000

    admit conclusion no shade fetch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_current_directory():
+        return os.getcwd()
