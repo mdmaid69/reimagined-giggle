62bb7b88b72b9a1d4de57fb39ebe6c0ba81beae7
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:33:24 +0000

    worry familiar neck instead season

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
