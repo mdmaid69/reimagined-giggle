8804c2c0704f48b47fb132f79ebbd563c59cd71f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:26:37 +0000

    effect excellent weekend soil flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
