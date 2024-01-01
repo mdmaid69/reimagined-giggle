7f6234025a8e2eb2c6469bb9f85cec7df5ea3618
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:55:47 +0000

    nonsense create culture it outside

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import collections
+def create_queue():
+        return collections.deque()
