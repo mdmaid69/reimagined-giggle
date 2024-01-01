906cb995455468822759966c97ccf88bcb101a37
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:17 +0000

    uncertain excited average star message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import array
+def get_array_as_set(array):
+        return set(array)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
