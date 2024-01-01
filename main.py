c030a63690c43c51776eadb2cf4431a03a9aadaa
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:45:34 +0000

    popular border fortunate favour kick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
