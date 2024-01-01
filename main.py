6decc837918d2a6dd28e21326d85ed995ac0f703
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:26:28 +0000

    correction break shopping public on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
