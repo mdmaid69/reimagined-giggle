e451d7aa2c6a91dbda4a0097c9edb668bd91abb4
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:01:45 +0000

    skirt practise spare enthusiastic foot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
