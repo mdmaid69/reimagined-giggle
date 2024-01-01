6179f94c367c5e91c25496889b30a0fdf143fd0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:47:22 +0000

    turninto pupil stretch pocket praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
