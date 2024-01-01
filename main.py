25f923287c06b02e14ad533d14a91344f060e863
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:31:45 +0000

    very Qq squeeze criticize meeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def multiply_numbers(x, y):
+        return x * y
