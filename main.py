a4fdc1105529edb2ee4d793f46e1784ba43bf3b1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:58:06 +0000

    teach giveaway perform hurry meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_speed(distance, time):
-        return distance / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
