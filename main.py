fd49878b5cfae510481b0a9afd707b0e5c666862
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:20:58 +0000

    person side care your turn

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def find_max(numbers):
-        return max(numbers)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
