569bb6c038364d365357770cb04dbbe71860dc56
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:29 +0000

    pile press treat smoke themselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  def add_numbers(x, y):
-        return x + y
+def is_odd(n):
+        return n % 2 != 0
+def convert_to_binary(n):
+        return bin(n)
