d75e48a31b608920fa12142c5692d13d4693a49c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:41:13 +0000

    learn manage attitude push emotion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
