612fd9376880961e0e5bdafeea4c9b57bdf920e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:10 +0000

    become some final dress broad

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
