4a334250ade9fa838be5a258ebe8939f3f3197fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:05 +0000

    actually store must exaggerate point

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import os
-print(os.getcwd())
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
