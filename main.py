afc0a817fd6420b0d6c05b5e4dab2b1b964ff968
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:19:18 +0000

    nurse experiment his coffee offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
