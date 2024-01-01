20c65dc45ae65b7ea3b54e06dd565e365647bce6
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:58:16 +0000

    when before trial air run

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
