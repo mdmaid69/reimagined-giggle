c715fccf876f03002c03d82c60f93aec9b3d0375
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:33:34 +0000

    cheap surprising platform drawing mail

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import math
-print(math.pi)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
