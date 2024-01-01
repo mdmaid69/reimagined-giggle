f7dfa895bdf2b4c4279bf92fb3192789e2ab9938
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:39:46 +0000

    acceptable sense myself depend advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
