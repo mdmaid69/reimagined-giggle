0a4270766879c5022acf9592f51ef90fcb8713e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:15:51 +0000

    copy have circle turndown turn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_bytes(array):
+        return bytes(array)
+  def is_odd(n):
+        return n % 2 != 0
