8d785f9da21400dc87a80d00f9d2971a08761f71
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:32:02 +0000

    pretty everybody slice here without

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import getpass
-def get_username():
-        return getpass.getuser()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
