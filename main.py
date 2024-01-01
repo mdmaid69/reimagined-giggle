bbd8474edf939b99d0fceb08b55cb0098a998445
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:54:23 +0000

    swing take street tone possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_typecode(array):
+        return array.typecode
