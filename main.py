f91a62813e2cc3d1bcd8ffabd3cbd9fafd740c6b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:00:41 +0000

    not like meet dish comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_item(array, i):
-        return array[i]
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
