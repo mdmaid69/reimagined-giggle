fef9c0c07f91cd338abeba50a867a85baed0eb07
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:23:11 +0000

    develop step up wish angry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import array
-def get_array_typecode(array):
-        return array.typecode
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
