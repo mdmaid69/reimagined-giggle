f283880b6df906affd068c050a856c503f313724
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:32:01 +0000

    turninto picture level clothes second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
