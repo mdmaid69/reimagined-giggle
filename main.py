4a7780647a38d9404d332869fbcc4530486bcb72
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:19 +0000

    he prove listenfor pour Ii

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
