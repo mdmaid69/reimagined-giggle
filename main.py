58a0d9265688a7f429de217ddab01528463109bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:47:18 +0000

    approval impatient straight equal transparent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def remove_duplicates(lst):
-        return list(set(lst))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
