acdfe33a407cf7219279ccae509ee9cf62a67496
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:03:52 +0000

    all entry really firm determined

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
