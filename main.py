3f44a40f0084f6026ec5c6b30f3c31c721e253d3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:18:43 +0000

    tired announce dick fly somebody

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
