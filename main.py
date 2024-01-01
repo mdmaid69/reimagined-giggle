9a758b89f444b02d9b14b34df052b6db26bd9396
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:19 +0000

    slice set embarrassed strike frightened

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
