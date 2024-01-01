22f5529ae4d015f941ba592959beb71b881260b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:50:34 +0000

    pleased side properly that only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def add_numbers(a, b):
-        return a + b
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
