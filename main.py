4955376ba26f61298827a01eac4c1e66e95e8912
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:58:59 +0000

    period handle recognize unite switch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_union(list1, list2):
+        return set(list1) | set(list2)
