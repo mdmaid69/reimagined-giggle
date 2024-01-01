763ba2f21f400503a781bad42dfbb34388142edf
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:01:52 +0000

    private food attract satisfied intelligent

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
