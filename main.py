1a71f17f24ec9f3f4f96db505802ea0f28aa96ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:54:21 +0000

    trap drink common intend trip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
