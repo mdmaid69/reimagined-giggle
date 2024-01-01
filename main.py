d7ac7cb471078163c388f085072d98821a78a858
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:18 +0000

    mass its later later brain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
