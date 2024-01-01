752583dd2e5a3fbade2ca7a41156c777b4da14ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:04 +0000

    marry fond empty tour tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+import array
+def reverse_array(array):
+        array.reverse()
