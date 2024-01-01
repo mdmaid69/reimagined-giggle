267a1995b422c1bb035df04eb09967edd466a19d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:09:41 +0000

    contrast lookafter principle woman will

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def create_priority_queue():
+        return collections.deque()
