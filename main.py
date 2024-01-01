15e6db7450ddaf64b1c24c9ec2b3d00e103adf88
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:22:21 +0000

    his anywayanyhow describe match office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
