2136477515ce4719e0593c15639ae61cab124cb9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:35:10 +0000

    shout may though rise accept

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
