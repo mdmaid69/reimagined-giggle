45c2724a6ab95d617c4e20a3625aa2eefa39de93
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:11:01 +0000

    mainly realize work arm watch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def calculate_speed(distance, time):
+        return distance / time
