f9ccfaee6fa4c78612d39f2a3e702da4a84259f4
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:13 +0000

    sign pull soon mail callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_uid(file_name):
         return os.stat(file_name).st_uid
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
