a6c35dc183c406192a1bc5c33fe7490b0fc37bb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:55:20 +0000

    pleased watch arrival rub must

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
   import os
-  def get_file_uid(file_name):
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_owner(file_name):
         return os.stat(file_name).st_uid
