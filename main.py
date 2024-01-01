d7303be07cab52766f184d1dc370bf28b430da1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:13:34 +0000

    swim Ww holdout hour computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
