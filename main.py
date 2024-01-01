5b917527cfd4ba447a42c34423dd7558aab1cd36
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:31:43 +0000

    present word reply mark land

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
