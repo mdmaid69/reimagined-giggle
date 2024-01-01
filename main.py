f05f1fd4f150774f5367afa5552525660606d45e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:48:38 +0000

    next attitude bag kill light

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
