3e4a60f53b2916fc7bc935cc06b961cd2551a50b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:52 +0000

    work begin brush write concern

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
