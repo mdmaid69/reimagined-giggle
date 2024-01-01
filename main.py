9c365deaa6fe2ffd773da52fdb57b09bd9d95779
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:07:59 +0000

    572enter fire thorough face heat

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_area_circle(r):
-        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
