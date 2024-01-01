9d1b8ba50f4e93a6071ca36498d2375842df47b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:19:50 +0000

    vanish knowledge record juice at

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
