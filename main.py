4c3dedb57989cc126452c038691450ecc8428533
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:59:26 +0000

    various kill good quickly tragedy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
