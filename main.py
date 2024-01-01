ef75f47463a44f524bc9a4553d71b78e77bf5e96
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:10:53 +0000

    opposite long its tip fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_error_function(x):
-        return math.erf(x)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
