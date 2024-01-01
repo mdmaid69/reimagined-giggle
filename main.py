e5c2caec63274d8a4646d4acd17f4b304a599400
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:23:14 +0000

    state dull steer supply but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
