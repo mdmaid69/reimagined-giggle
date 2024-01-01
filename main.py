6fe9f4abe7a62af552215aa49111695264ab3c3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:05:29 +0000

    here loose cut performance debt

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
