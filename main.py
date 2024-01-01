78dca807cfe1589b701c6bda2eb707352407adca
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:53:46 +0000

    ring common charge connect fast

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
