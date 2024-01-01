ff06b82989c6c26df65a75ee1ef1bd20bb958489
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:22:52 +0000

    shall right duty science advantage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
