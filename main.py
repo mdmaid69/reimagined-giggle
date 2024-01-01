4c52d35be2b99499557ab915d2939ba62a8a36e7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:08:49 +0000

    onto space regard neat land

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
