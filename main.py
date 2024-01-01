821b92991ebf9fa5a07d79c627323db04282f387
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:20:18 +0000

    expect win generous must question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_min(numbers):
+        return min(numbers)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
