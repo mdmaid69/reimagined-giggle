8320a9b061b532710732845468fabd718264046c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:06:50 +0000

    strength plan keepfrom black introduce

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def set_array_item(array, i, item):
+        array[i] = item
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
