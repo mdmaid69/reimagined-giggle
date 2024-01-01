99c9f0743431bd6a7690e6bef509ccfc90f0d71b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:49:22 +0000

    agree touch me afraid opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def sort_list(lst):
+        return sorted(lst)
