d47062838670d16621c0980cbc6333cc17f7c29e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:21:18 +0000

    official estimate please joke night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
