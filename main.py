eb3947e0c947e22d9d26f42b10f07defce24cf87
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:59:20 +0000

    holdout describe heat pretend authority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
