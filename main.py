15f1399d6d65281f4a288799fde39cbbeb359067
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:09:06 +0000

    lesson kind comeup meet uncertain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_density(mass, volume):
+        return mass / volume
