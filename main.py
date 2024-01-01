3a898c9b9150e634e025ce4cdb724b3cbc961278
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:14:37 +0000

    disapprove address shop fix foreign

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
