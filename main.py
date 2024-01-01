695171c2b9ca8a509144db201e8180407a73c2da
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:08:38 +0000

    curious comeup holdout soil commit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
