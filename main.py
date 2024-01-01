fe782fd75b86696004f6be69e19caa38c1011e7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:30 +0000

    accidentally obvious wind that backwards

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import array
+def get_array_as_int(array):
+        return int(array[0])
 import time
-def get_time_since_epoch():
-        return time.time()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
