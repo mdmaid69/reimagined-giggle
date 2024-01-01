1917e82cbca3471cc9a9c3f25dd24745f246a25c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:39:37 +0000

    kick scheme instant circumstance sleep

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-for i in range(10): print(i)
-import numpy as np
-print(np.array([1, 2, 3]))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
