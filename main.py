962f950ae63cd50a10d9245baec8c23f38c2345c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:35 +0000

    bit period watch fond different

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
