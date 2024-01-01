f2a4dc5dcac463717b6f61f2eb1c4a79e8c4a453
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:35:16 +0000

    deliberate snow nor recent fair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
