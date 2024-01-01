54c6b77e91770a0b982e47be43ebaccbd43dfa1b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:03:55 +0000

    possible form top address violence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
