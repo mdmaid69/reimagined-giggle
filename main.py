b86fe1cf8f5ad3f2fa13537b89c46383b98e35ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:10:38 +0000

    welcome exam stamp similar shrink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
