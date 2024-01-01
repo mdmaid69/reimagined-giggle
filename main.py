fc084b6fd3fff09d973f385024556eb114e39acd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:00:18 +0000

    inquire people custom innocent government

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+  import os
+  def split_path(path):
+        return os.path.split(path)
