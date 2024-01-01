5cd0072322965ab353772a889932bc56462630ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:18 +0000

    discussion embarrassing habit patience class

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def multiply_numbers(x, y):
+        return x * y
