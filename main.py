3ac12b4db9cb77c49e78eb7d1f4297ddce03a8cc
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:29:38 +0000

    estimate thin band contrast safe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_power(work, time):
+        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
