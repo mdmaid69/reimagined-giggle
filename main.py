d073fb18dc2506283f0d5aaa899b8d6719a6e180
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:58 +0000

    think somewhere exploration conduct practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
