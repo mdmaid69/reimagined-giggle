beaa07676c324d91104aaf33ce7150c5c85395c2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:28 +0000

    reasonable drive uniform stop considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
