3be3da3a00b429143a62858f764a1a2cabfc6044
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:35:00 +0000

    tough wild complete place into

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_floor(x):
+        return math.floor(x)
