c58342b04028e80e529b537497be05ff211e1e33
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:18 +0000

    satisfactory reserve suggest finish only

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
