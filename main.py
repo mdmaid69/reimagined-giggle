e3b18238d66680bca61b5aed89622814d989140c
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:43:48 +0000

    possibly dress goon culture instruction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-print(os.getcwd())
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
