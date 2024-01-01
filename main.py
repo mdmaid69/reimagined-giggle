7cf0624592673810c521e1b1be7a4cab51d3da8b
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:31:25 +0000

    commit notice sticky knowledge than

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def cube_number(x):
-        return x**3
-def find_min(lst):
-        return min(lst)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
