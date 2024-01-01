9f49d544572340cb35f8b426223f80d3efd1b7de
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:52 +0000

    thing blue pool comment disagree

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
