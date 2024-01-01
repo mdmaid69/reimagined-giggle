2ac9ba2417385405abf93634c407d53f41ec3b3c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:33:49 +0000

    card married comeout glad may

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
 import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
