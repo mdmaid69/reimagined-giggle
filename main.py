f5b9c7e6b527360cc4ba7f9d104d7438b3d7b78d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:53:30 +0000

    appear drive of for laugh

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+print(time.time())
