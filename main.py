e5a47c5df859e59d11e70d875278f658978b0f80
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:45 +0000

    new might extremely congratulations line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def cube_number(x):
+        return x**3
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
