e9938d983b8202c78e1b20e48ddf93fec1cbf918
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:08:13 +0000

    thing responsibility approval clean argument

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_work(force, distance):
-        return force * distance
+for i in range(10): print(i)
+def remove_duplicates(lst):
+        return list(set(lst))
