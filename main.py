8c7a539b6a6564ce93b8ba6d270201663003588d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:48:35 +0000

    copy Vv pretty generous herself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-print(os.getcwd())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
