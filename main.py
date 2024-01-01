01a876adc6bbb8bd5b402b0da87f589a0e75be7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:25:01 +0000

    offence cause treat basically sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
