1981f39b3bd4c9afce4b0530f53df3986592040f
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:19:16 +0000

    against Cc platform geton turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
