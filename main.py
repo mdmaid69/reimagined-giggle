4af1f9e45ac7f55e7912ceb40727d3c53bc9b817
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:11:36 +0000

    uncomfortable luck bunch impressed them

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
