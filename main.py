0b8740a21de4bcc164e106b1f4a9f3e0fb6eeec6
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:27:55 +0000

    worth plant either proud character

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
