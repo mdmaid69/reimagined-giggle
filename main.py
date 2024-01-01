177f1d554d06ccd4cbf30f1034f3f77711d9b51f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:46:14 +0000

    wind will mess bang card

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import array
+def get_list_from_array(array):
+        return array.tolist()
