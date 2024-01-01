1ebc9d6b34917adafd4501e328de9a13ed2a0240
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:43:54 +0000

    describe tight half tired mail

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def multiply_numbers(x, y):
-        return x * y
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
