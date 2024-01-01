7c46dd44124417e796587c3b8dbd8d0270c5a691
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:43:01 +0000

    go translate building demonstrate that

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
