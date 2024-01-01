19811b143f4af7a89b11881b139e8be370d753bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:39:11 +0000

    television practical near wind unusual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
