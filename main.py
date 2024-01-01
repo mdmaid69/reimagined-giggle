155d736e5e7ae93541fff26b481223b302819e5d
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:50:58 +0000

    hot year hate goodbye estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def find_max(numbers):
+        return max(numbers)
+import array
+def get_list_from_array(array):
+        return array.tolist()
