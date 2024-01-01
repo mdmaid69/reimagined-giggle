2612ebf83edb7106acb6da72cad05eea85e77133
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:34 +0000

    as Tt fix tone worst

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
+import getpass
+def get_username():
+        return getpass.getuser()
