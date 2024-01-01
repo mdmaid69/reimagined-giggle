36a0aaddda71817eab445846588b3eb2c807cfae
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:24:42 +0000

    under takeout mine support straight

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
