fef52feef1af269b4e9bfaba623cdbcddccc7cba
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:34:53 +0000

    achieve there passage present discovery

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
