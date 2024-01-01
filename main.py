c49b63ca02cff1df22c3d9e1a432da6a96260054
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:22:54 +0000

    careful share politics dress steal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
