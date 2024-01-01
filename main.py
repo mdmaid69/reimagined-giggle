22b13074fa127bdd90279e458d125cadb4ac80d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:13:06 +0000

    whistle distance 179bite bit through

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
