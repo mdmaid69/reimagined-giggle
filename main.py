2f2b8aec0acf4fae6b8a6adade12456289723c91
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:30:04 +0000

    concern about comfort pollution comeback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
