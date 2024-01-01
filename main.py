1954fcd3e24a01b338de719086a228db04fc5c76
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:01:04 +0000

    getup makeinto offend bar heavy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_perpetuity(payment, rate):
+        return payment / rate
