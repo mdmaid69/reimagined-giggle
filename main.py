9ab8075136bd33de814eb94cb37dd39718ed3cd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:13:02 +0000

    should Rr ordinary law mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
