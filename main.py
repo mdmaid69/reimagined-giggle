2d4541f9bfdc13b06bd5178a6d4b6e957f74cb9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:25:47 +0000

    position challenge emphasis one godown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
