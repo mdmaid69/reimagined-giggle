8d41ddddb3cfc0a68430aa3105530a1b48a31b9b
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:31:10 +0000

    it mean load various refer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
