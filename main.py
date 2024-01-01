e4ce1fdd5cfba181e79dcfa420195e74085f583d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:00 +0000

    date shop welcome correct giveup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
