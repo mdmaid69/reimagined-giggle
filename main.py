a614d8a3f292c8370da8c3fe8c4fab37ef59ba74
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:35:46 +0000

    trap waste obtain world confident

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
