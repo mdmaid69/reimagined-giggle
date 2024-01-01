a14ba9c3385b149cc25b5f24dc6cab54fce871c0
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:12:45 +0000

    direction shrink immediately care ever

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
