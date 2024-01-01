9be78484f472f00b4ee16061099b7df222e0f5f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:03:13 +0000

    politics fierce likely except advise

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def subtract_numbers(x, y):
+        return x - y
+def count_elements(lst):
+        return len(lst)
