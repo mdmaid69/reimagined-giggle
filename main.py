7073afd8a64dd298070a0910ba3aa9076ea366bd
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:18:01 +0000

    disturb target male spite this

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
