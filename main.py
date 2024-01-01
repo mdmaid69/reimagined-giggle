da78347d99ed05b35f7207e95e37719d2f4f33c8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:24:13 +0000

    fat bottom forgive count out

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
