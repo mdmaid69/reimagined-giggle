8c4e3c8075082de16ae46d1b351dff1cf663ae65
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:29:31 +0000

    swallow nose respect while Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
