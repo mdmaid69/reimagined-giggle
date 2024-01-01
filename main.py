20b56de991d514b72b6469137c7b5c8b73c1e514
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:17 +0000

    life statement contact Jj day

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def count_characters(sentence):
-        return len(sentence)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
