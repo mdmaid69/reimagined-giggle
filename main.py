636936bc0cebe0a87d1b15622552e874e1a551eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:26 +0000

    somehow pile hang future already

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def find_unique_words(sentence):
+        return set(sentence.split())
