0f3824b82a9163a6c169d7fe97cfc9f217e67efd
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:19:52 +0000

    think Rr around ready strike

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def subtract_numbers(x, y):
+        return x - y
