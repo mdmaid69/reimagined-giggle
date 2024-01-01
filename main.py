6b0dc965a3c3598adfdc22aea7879a4b32b05239
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:10:42 +0000

    breakfast improvement practise somebody deliberate

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
