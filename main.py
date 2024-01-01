7a01b5d97530f519f9f1fb03a577273b2d82376f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:05:27 +0000

    contrast cry relative workupto left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import time
-print(time.time())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import array
+def get_array_length(array):
+        return len(array)
