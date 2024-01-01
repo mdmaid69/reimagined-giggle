ba028b93c3d22d78e274fe3715c48e58ccd6d240
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:30:01 +0000

    later chance unfortunate force still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
