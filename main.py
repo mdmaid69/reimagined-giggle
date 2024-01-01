3f581cd85f9b089add93ec1d59618202f254aa8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:19:20 +0000

    oil away cruelty violent callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
 import array
-def get_array_as_str(array):
-        return str(array)
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
