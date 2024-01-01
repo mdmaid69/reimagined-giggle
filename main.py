fb4b9203b8fb0292660e013533a583b5e7003a4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:46:28 +0000

    immediately steady regard there think

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def convert_to_binary(n):
+        return bin(n)
+  def subtract_numbers(x, y):
+        return x - y
