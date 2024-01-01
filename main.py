8d9e9ec96d88f4666d7c20dae62ccb421116de2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:12:20 +0000

    forget test professional answer reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
