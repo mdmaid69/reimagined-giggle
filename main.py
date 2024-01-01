b8206615ebcc37eb1b5bdc431ef44fd093120261
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:49:40 +0000

    hope picture draw light exchange

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-print("Hello, world!")
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
