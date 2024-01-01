950cdbd7ab3ce6a7b098cd5fc770e6262a2e2449
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:34:20 +0000

    previous exercise business embarrassing she

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
