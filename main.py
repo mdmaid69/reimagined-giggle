6f4a0869a14735e18f309a44ed3ef6487dd83d47
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:30 +0000

    centre identify only hard mark

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
