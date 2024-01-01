51d328b847385c3498142df10c56e78ee675225c
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:25:48 +0000

    future trip leg pleasant university

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import math
-def calculate_floor(x):
-        return math.floor(x)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
