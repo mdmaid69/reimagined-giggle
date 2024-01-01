2d2b01fd3f2f962b5a8397d6d7671f67af09de3f
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:18:33 +0000

    but awake month hate one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
