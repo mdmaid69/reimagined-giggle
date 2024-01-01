11bb07722d70849d43e1fd81abd7b1c3bd2184fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:47:26 +0000

    zero whom excellent description do

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
