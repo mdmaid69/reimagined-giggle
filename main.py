d896921d9c1cdba848362df4998427ea924291b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:24:41 +0000

    blind put copy class force

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
