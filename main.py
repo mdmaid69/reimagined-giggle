ee16f5c65b26a60ba8b9d411b7320d9c57c9f7d5
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:03:34 +0000

    side remark balance unit far

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
 import sys
-def print_python_version():
-        print(sys.version)
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
