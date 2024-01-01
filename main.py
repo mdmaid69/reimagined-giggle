f52c820407d538d5697ed7dc41f192a2fae891df
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:19:26 +0000

    hurt find Ll drive exciting

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
