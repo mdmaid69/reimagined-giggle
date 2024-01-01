f94ef158f49bace3e85a11a40b54f184d5dbce16
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:05:24 +0000

    sensible birth lucky change family

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
