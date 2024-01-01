4d579a9f8d99ee36238820ffdf8c2de302eac99e
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:42:04 +0000

    bound fit after amount inside

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
