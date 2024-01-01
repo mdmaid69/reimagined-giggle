eb25fb94b17d03f31c7a8220c074cd9c27bb73ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:46:21 +0000

    comedown giveup false dare physical

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def clear_array(array):
-        array *= 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
