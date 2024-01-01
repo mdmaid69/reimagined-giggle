7f34e34ec0ed27acdf7baf4976ebcc8409d6f9e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:09:25 +0000

    capital remove probably turn tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
