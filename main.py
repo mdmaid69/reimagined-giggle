f4b25bab947ccaf3559d19364255b8e8ca2dfc0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:34 +0000

    children usual tough score correct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_volume(length, width, height):
+        return length * width * height
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
