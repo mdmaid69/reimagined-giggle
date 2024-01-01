5b102edb1b0bfe7699fa24fe6cf379332a995123
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:24:31 +0000

    guilty without suck average influence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+def calculate_area_rectangle(l, w):
+        return l * w
