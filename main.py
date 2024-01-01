ba5891659e09e8368d9be82b62fe44b3bd0950b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:33 +0000

    connect university light run table

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
