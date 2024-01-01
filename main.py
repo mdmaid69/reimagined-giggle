e02286af4c93f7aeb171bd8a92b9b289c107e26d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:41:57 +0000

    direction least cover total standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
