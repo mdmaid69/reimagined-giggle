6cc1fbfae911a0d349b6578086e8b20986ab67ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:31:20 +0000

    future difficulty design measure petrol

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def cube_number(x):
+        return x**3
