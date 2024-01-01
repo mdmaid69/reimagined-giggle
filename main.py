53603af08d4479c4dbced7c43566593313ca3862
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:07:44 +0000

    rich fact complicated run breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
