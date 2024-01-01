3b5a479ac226e617f30d865b4436b54df0b39464
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:09:14 +0000

    pronunciation sweet correction negative quick

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-x = 10
-y = 20
-print("Sum:", x + y)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
