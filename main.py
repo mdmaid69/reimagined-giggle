879aa8490508f895bacd44fb2dc21bab073948ec
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:22:27 +0000

    before now cross bear wide

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import collections
+def create_user_list():
+        return collections.UserList()
+  def is_odd(n):
+        return n % 2 != 0
