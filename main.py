7db92114069fdc9911226c845557e67f8a3c6470
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:48:30 +0000

    avoid judge chief evidence activity

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
