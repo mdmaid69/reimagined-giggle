b638e6007a7b93f97feaadcea027230243dc91de
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:27:39 +0000

    protection accident although allow value

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
