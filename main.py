bba9b1c53782615a8256a4cb3f9487ab035199e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:33:36 +0000

    it downstairs modern tell telephone

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
