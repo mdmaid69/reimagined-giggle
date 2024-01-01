c4ce44c595f6b1c673d67dc606e50f7bc978869f
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:10:19 +0000

    bringback easy Oo record float

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
