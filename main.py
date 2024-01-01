4b000643b68ec560ffcbee621a5e1ffe4a3c90c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:58 +0000

    together include Mm successful port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
