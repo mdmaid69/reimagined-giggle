0b85e0f23f3f506c92bd07a3abfb3a87df2ec5b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:11:49 +0000

    hungry there urgent speak bored

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
