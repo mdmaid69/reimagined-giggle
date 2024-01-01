337bbf0ef8292634f4e3a89dc0add4ef4a2554cd
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:41:09 +0000

    team wash that cap moon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def reverse_array(array):
-        array.reverse()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def get_array_as_bytes(array):
+        return bytes(array)
