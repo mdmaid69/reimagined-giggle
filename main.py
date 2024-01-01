27badcb21c3c343fb3e7660d00fcbc84b2bc33da
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:57:12 +0000

    strength here class stir grade

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
