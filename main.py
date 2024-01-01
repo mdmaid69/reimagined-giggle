9a47267f2586c0e601b5fe8383d091f601e6cb01
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:28:44 +0000

    whole begin impossible closed work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+import time
+def get_current_time():
+        return time.time()
