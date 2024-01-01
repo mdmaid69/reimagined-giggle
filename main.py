9dbe9602842d5c44b643dd824e2d79601055bb53
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:22:34 +0000

    picture influence listenfor approval cheek

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import time
-def get_current_time():
-        return time.ctime()
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
 import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
