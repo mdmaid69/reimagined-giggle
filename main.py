9d2bcd0a4fd847524dda51f13cd821be7335da5f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:08:19 +0000

    ground burn simple fly matter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
