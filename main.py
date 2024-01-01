62bde35c6ab1abfcbf81e5da39eb93b2487e05cb
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:01:23 +0000

    steal risk patient lookback(on) common

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
