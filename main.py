e4bfbd52b5cb7e456a5da5056877e4aedac8e549
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:02:22 +0000

    task middle sing turn interesting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import os
-  def get_current_directory():
-        return os.getcwd()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
