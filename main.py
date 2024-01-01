d691577319cb927f3ffbb3307d0b93d2b89cb130
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:22 +0000

    year rarely smooth ourselves small

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_volume(length, width, height):
-        return length * width * height
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
