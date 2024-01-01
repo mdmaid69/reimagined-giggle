5d0bb6e5d1a1b9ed25c03e86e806badea0f88e0a
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:30:36 +0000

    damp discussion trick connected bright

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
