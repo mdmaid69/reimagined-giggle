15d48d516703dde1a52dcfe20601a94027b97a32
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:02:06 +0000

    shine computer free speech broad

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
