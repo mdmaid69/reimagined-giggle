35aa8c88cc2b8425cd57ea3fd6336d43b77071a5
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:31:03 +0000

    comedown official throughout break train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
