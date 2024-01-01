8244b95323558f45f1b2a13ecfd6d090fe6ca0e5
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:17:43 +0000

    perfect of 568energy turnup language

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
