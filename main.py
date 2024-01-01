7e666c47d6318d9518f146c93f8112ce2577c471
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:19:37 +0000

    same recognize scale secret me

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
