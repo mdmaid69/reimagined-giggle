c327f83ca21102bfde820a05ed9658eb02a21787
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:16 +0000

    legal contact file holdup clear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def convert_to_hex(n):
+        return hex(n)
+def cube_number(x):
+        return x**3
