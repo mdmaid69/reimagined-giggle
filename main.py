d76a7ef99d5217ad4e42b54fd8dad0bda4edcd66
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:13:57 +0000

    change accept no wash thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
