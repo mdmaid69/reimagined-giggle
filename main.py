ba793f30d30aff1651cc58980cf247fa782e7126
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:08:25 +0000

    his gentle mood event also

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
