730a2ab578443a9ff1bdaf5c934e2d64e212d019
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:32:21 +0000

    organize conduct throw bet godown

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def is_even(n):
+        return n % 2 == 0
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
