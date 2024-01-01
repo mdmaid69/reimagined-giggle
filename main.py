abb91d76c51c6221c602ffce083ba8ee36836666
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:53:19 +0000

    answer spite steer steady onto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
