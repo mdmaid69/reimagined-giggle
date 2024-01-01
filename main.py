4f68e4329fc8f95f195efc855c2a28e7542600ff
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:39:30 +0000

    note greeting comfortable opposite evening

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
