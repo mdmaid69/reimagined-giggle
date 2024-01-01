c1010767740c413dd038d9199c8988642514a683
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:20:12 +0000

    please tune shadow talkback pupil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_min(lst):
+        return min(lst)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
