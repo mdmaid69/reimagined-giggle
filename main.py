2fa8a4d179671548db1e9d1b17cde2507e15608a
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:19:00 +0000

    traffic far shelter move attach

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def convert_to_hex(n):
+        return hex(n)
+print(sum(range(10)))
