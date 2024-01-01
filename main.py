2d1132335f5dfc95daeae26f168e798a6acb6db1
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:00:59 +0000

    part activity official major post

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-name = "Python"
-print("Hello,", name)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
