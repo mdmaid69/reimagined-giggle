8a0b572253ada20e7d3abdf8c4cdfa6fb4b7ecd4
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:16:51 +0000

    in reserve noise turnon frame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
