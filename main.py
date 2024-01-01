73f4e52a57fde12d7b743e547334dc3db998572c
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:08 +0000

    company evidence interview defend burst

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
