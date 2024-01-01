547acc180ac079de85cb151b43287209f8ee239d
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:27:31 +0000

    sleep warm blue claim hate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def convert_to_octal(n):
+        return oct(n)
+def find_min(lst):
+        return min(lst)
