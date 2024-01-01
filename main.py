48d2e0aff16c1a59e980ec5cb95f76a743a8f953
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:37:56 +0000

    owe juice block hope gooff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
