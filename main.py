8d9fe1e1b96e17d90be319ff6cc81df2a4dbd52c
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:37:20 +0000

    side connected alive right situation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
