e307df1c5fab1a0f8d74e1962eedc93e7120bd58
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:37:52 +0000

    scare make argue report marry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_octal(n):
+        return oct(n)
