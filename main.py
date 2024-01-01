af7556e34b95244708b4129c55b7279e954c3935
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:53 +0000

    you everything separate warning check

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_directory_name(path):
+        return os.path.dirname(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
