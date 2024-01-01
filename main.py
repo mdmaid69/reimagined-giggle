43118238512dca67e71ef4b892cb62cae4fc65f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:37 +0000

    please declaration fair failure interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def cube_number(x):
+        return x**3
