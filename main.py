72b59188ecef8f41f76b55c4d7a23c7eb3550b59
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:01:10 +0000

    morning except team mystery answer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def is_odd(n):
+        return n % 2 != 0
