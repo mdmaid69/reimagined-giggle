892451445c0fc243e3f1547053015a313ff353ee
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:14:04 +0000

    bin contribute prize print heat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
