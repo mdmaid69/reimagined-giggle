f6419180e7171cb608149ec869fb5f1802808212
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:13:54 +0000

    bye suck add sticky part

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
