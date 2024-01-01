2acdc9ee0cda22a22fff861ddd0b16dd8265881f
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:52:15 +0000

    increase pupil cinema discovery drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
