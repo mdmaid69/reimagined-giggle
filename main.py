930a693c8294861940ad377ae2d5fe02e9457d61
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:56:26 +0000

    objection visit mix announce nowhere

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import numpy as np
+print(np.array([1, 2, 3]))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
