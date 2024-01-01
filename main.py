2ee96c95da708d9ca72c4686de16950d77e33f06
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:08:28 +0000

    naturally up regularly on sorry

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def add_numbers(a, b):
+        return a + b
