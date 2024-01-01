c3adb226e43f5d478f2ecc792799fe901fd2f26f
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:28:28 +0000

    call plastic wealth escape frighten

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_floor(x):
+        return math.floor(x)
