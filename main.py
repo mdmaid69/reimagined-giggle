2d8f934d4449829b2f0eb1acd30375956a5477a3
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:01:27 +0000

    solve single tough exam accident

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import array
+def clear_array(array):
+        array *= 0
