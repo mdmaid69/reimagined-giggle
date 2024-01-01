99d59511f994fdd12881b618f9004e1132c6fe19
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:12:24 +0000

    fast unit contents mix rain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_work(force, distance):
+        return force * distance
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
