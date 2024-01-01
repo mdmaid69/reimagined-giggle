78bcc66c016a90ccd9e07e8a8c47a69697185aad
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:17:57 +0000

    soil conversation mark autumn unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
