5c56f2f3d4c4af759fac84748c472a56ae2ee8ce
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:25 +0000

    keepoff what shut immediately shape

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import re
-print(re.match("h.*o", "hello world"))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
