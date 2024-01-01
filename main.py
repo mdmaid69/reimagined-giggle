bdaf02e9fc24ae725381e791310607578fe9d4da
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:23:58 +0000

    clear comein convenient style through

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
