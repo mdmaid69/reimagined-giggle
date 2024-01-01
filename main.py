f7b3a91938ef8b95436d48efe95f53f9edc451a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:32:29 +0000

    pronunciation roll better share tap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
