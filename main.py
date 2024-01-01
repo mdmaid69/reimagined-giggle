bca29c8229d6ffd95a9f22612c32bd7e2aa298fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:25:08 +0000

    mix sure sign patience area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def calculate_density(mass, volume):
-        return mass / volume
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import re
+print(re.match("h.*o", "hello world"))
