e44a27cf024959febb1e24b5657cd88907d26f40
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:04:23 +0000

    announce see feature similar similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
