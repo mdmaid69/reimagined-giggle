30a92db23e322ff07df3137612d4b3d263380630
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:44:48 +0000

    world spring feeling type wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import json
+print(json.dumps({"name": "John", "age": 30}))
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
