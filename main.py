5ddd83b6fcd2e07dd65e074d0b9f145c8d4a51f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:20:49 +0000

    tough cruel its case region

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import sys
+print(sys.version)
