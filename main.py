8a0a1ba7d47fc7b1f303f2b1e6a6fa09b2acd49e
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:23:53 +0000

    confident breakdown shelter they goback(to)

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_length(array):
-        return len(array)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
