07dd11d8354a7d2cc117c52c1b6b02b70b1ef369
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:28 +0000

    pretty entry condition deliberate still

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-for i in range(5):
-        print(i)
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
