be673b60d86671fcaf3bb4ed04468f79d2926b44
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:43:36 +0000

    turnback admit safety exaggerate shallow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
