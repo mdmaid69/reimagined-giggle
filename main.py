b09d8b237054a7b61074517f0cb61e327f64555c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:35:38 +0000

    them phone pain recognize quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
