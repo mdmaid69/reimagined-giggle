6e9b45abc035f139ea071ec20184aa09a5c58586
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:38 +0000

    mystery cook offence comeup crowd

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
