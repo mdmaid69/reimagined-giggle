f31f310ffee7f64ceb6c5e40bb0febbee9023a67
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:29:55 +0000

    introduce necessary it frightened task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
