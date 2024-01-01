9a4a57a192703631fe98674c175b07424e646885
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:35:23 +0000

    freedom balance please laugh guide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
