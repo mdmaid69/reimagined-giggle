659d3874f9c065f5f7b02e07bf8366a3fabb4fee
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:54 +0000

    central search neither wash experience

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
