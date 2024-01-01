cce2266a4220585b06e1f6f0bc14ae703a989db8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:36:03 +0000

    pen model angry light honest

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
