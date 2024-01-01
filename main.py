425905a2e2cd7990d81c01ff56d90ee16e69adbc
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:21:51 +0000

    claim happy cross unusual length

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
