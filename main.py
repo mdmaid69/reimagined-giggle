eedb664ceece69cb234c2b360882cbda1a46ca65
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:58 +0000

    law easy land thing effort

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
