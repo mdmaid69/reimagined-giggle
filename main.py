dfea89d361682e14787d9b74fac76072e0443ccf
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:06:31 +0000

    he report increase so nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
