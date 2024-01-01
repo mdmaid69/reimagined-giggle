40907f7211b107aa509a0ac91e808174c17e4448
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:25:59 +0000

    yours promise run bottom consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
