202ddb38e31450b59fefcb8759fa042e384981a9
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:47:07 +0000

    straight weekend rubbish wealth unable

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
