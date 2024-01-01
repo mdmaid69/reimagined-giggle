4c8444365c3624ff9ce047e8bf6607e50e96db75
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:11:45 +0000

    offend borrow performance culture star

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-  def cube_number(x):
-        return x**3
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
