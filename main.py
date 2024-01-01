8b528579f2aa4f6494d46b146b2747faa2186856
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:41:52 +0000

    autumn turn rob collection shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
