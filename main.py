154ca18df52b08ead8f79da71bfba94cb1f9453e
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:08:01 +0000

    her lean stretch lookafter watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
