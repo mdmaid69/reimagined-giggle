cf0c725c3f7561467087c1a24e8fc687e77a1242
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:28:13 +0000

    back cook day nose least

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import time
+def get_current_time():
+        return time.time()
