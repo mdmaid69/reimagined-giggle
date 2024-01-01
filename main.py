8db3b6d6528557080bc30747810935b4b1df679b
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:03:36 +0000

    area godown someone runinto roll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
