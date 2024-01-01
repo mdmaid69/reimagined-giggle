d5cbeceeda67dc9a6aa9ecff99f6039b932eca1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:13:23 +0000

    aware tired list group thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def split_path(path):
+        return os.path.split(path)
