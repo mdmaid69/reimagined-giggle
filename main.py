8208b8a80d0db8f95fcae0a27cbb4839eb08d732
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:41:25 +0000

    impressive if give dead speak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def add_numbers(x, y):
-        return x + y
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
