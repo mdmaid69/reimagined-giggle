0d9556d99180f7355a55b8ca049299e12dd004b9
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:47 +0000

    fast its or everywhere total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
