58f5abf0730cfe628fa29117e0da1125dce421a2
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:53:06 +0000

    Mm quick notice Tt alone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
