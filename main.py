940998c3872a70ee915c73b94ebd7a9aaad1f8fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:46 +0000

    sick appeal pay between how

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_priority_queue():
+        return collections.deque()
