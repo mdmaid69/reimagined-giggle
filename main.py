4c37c2b78c54b06edc1ceb415ca1e861cf5d51b2
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:58 +0000

    strike early kind excuse about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
