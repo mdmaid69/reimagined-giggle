7c837b13c357119fce4e0963f1eadeafa4d5021d
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:19:41 +0000

    programme comeback laugh ahead over

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_length(array):
+        return len(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
