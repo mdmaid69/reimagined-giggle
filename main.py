112e0de24dd9fe48436856d4145a34b35d2b6347
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:53:20 +0000

    generous foot increase point top

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
