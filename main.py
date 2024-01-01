cf79171a072075960cfb350503c32dd68dbf6970
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:01:43 +0000

    straight here influence needle object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
