482971b921676e54001c6350007aeac655a56f18
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:13:32 +0000

    feel everywhere obvious brown poison

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
