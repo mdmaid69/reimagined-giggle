b551ea325d09321cc2c096cd8a23bac3146625e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:59:18 +0000

    children remain exercise contact quit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-print(datetime.datetime.now())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
