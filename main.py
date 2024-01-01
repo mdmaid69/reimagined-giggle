33684e204754c7ca1ea54f3becb4695a900ab63b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:43:00 +0000

    exchange fast refuse fresh hang

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
