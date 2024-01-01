703d2890a9d4945bde18df0b744191750cc05fe7
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:46:13 +0000

    protest trap perhaps relation touch

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
