553d19a23cbddc934058c4f76b6ba9211f19c014
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:10:38 +0000

    afraid drive hot maintain grow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  def square_number(x):
-        return x**2
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
