3aa749433363443cccbf81b145f5bc14a881a123
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:39:35 +0000

    challenge collection hair man treat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def is_palindrome(s):
+        return s == s[::-1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
