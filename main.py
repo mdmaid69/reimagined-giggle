4123ecc03d6461c8f823106a74a3d020171e4985
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:50:34 +0000

    which calmly upset then remember

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def find_max(lst):
+        return max(lst)
