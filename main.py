ef86f7f963b3caf6c335faf30225c4063a42c299
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:57:28 +0000

    something feed principle any term

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
