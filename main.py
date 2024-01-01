9a7718e41712803f1610e4da9896bc691a93e5ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:39:25 +0000

    dress straight picture satisfactory person

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def multiply_numbers(x, y):
+        return x * y
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
