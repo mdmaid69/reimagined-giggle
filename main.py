1f5bb7be233be808eff320746efde91d7ecf239a
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:04:40 +0000

    use burst study fight book

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
