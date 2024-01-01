405ad734a4e09fc073946cef3a70f2637479f611
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:44:46 +0000

    interest content comefrom cut delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
