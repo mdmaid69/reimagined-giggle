a2399201f4fb2ebd6138ef9341e652105280d671
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:44:45 +0000

    completely walk emotion smell lead

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
