e8038713b867aab90069baf6b3fbbc67b4e3bf1e
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:10:52 +0000

    else cut 567enemy yours I

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
