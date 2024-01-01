937acdeb5985033b5d423304aac703f645bd64a1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:23:03 +0000

    takeoff gentle strength callback narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_permutations(n, k):
+        return math.perm(n, k)
