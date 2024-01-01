39234f90d9eb37bb73cd9fa340565350deeac77c
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:36:44 +0000

    content slide sleep top later

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def sort_numbers(numbers):
+        return sorted(numbers)
+import array
+def get_array_as_bool(array):
+        return bool(array)
