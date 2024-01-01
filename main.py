083866aa3376ebaa347b5724bd89b04de8a9db63
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:34:03 +0000

    hide order lesson female special

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
