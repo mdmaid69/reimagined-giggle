ae09451f93aa9ba3939c8cbf7308f2f33f865d2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:21:28 +0000

    move thank party after friend

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
