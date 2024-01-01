8168a50cfbab99e1dac47aa44abe6778b62f54f5
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:37:18 +0000

    might achievement moon until spring

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
