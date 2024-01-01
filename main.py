f5b4fa6da4943afd9b7d99668ecb327c254b8824
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:39:28 +0000

    sink hope turnaway nervous determined

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
