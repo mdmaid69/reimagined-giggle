313e715d28b89909fd0a86a4789c692330095b7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:22:22 +0000

    suggest ago getback quick age

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
