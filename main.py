be8e382d560874be0aebb14390149370551c88ea
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:04:28 +0000

    adjective introduction profit pay announce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
