512ab88c45f0cd2bda23633790e35a6389c7be53
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:30:21 +0000

    shock tight talkoutof mark admit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
