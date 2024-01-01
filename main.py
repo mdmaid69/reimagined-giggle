be07c4625c6fb920440ff2e4c6a918189ac55b5a
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:01:34 +0000

    stiff explain loose though scheme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_buffer_info(array):
+        return array.buffer_info()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
