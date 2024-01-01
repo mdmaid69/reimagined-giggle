5dab1de3921b2b56d7bd5e2e935d7bae693a8d97
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:29:08 +0000

    see different head emotion bush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
