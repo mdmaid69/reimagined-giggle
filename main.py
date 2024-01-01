77d5eae6df819f0128d81f14c6378f7db7ade911
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:06:55 +0000

    shake free radio crash shake

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_item_count(array, item):
+        return array.count(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
