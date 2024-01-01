231052ab1573e666817ab5b674c65c3fbfa6935a
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:01:35 +0000

    cross fly goodbye part marry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
