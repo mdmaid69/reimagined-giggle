c1bb75f67ea2566737228cd0c54c02e8dbbf1c30
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:42:46 +0000

    minor mistake press press successful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
