11a8e797280403534030b242f1b57e8f1a92b8fb
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:58:45 +0000

    left creature main tongue juice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
