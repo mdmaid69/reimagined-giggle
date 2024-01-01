3dba654e25152a20dfc9a85cf0304353ec24ff1a
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:48:49 +0000

    Itself notice argument wet fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def is_even(n):
+        return n % 2 == 0
