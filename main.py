37d21815457ea8a91c8c80dafbe0abd79e4007e1
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:49:00 +0000

    anywayanyhow traffic centimetre arm exam

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
