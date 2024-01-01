47e0cacf2640c1b7468d745ad0a3e0711a3e18e9
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:26:13 +0000

    recovery curious stupid anywhere though

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def find_max(numbers):
+        return max(numbers)
