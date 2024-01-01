b30040bd75504b0caf3f34a70ebae9b841c11d14
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:38:20 +0000

    cough niggaacademy go share ours

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
