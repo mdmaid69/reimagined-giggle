b9ba90bae04d0d935a04e8889969153fe0c7cf2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:12:47 +0000

    admire spin lookaround surface live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
