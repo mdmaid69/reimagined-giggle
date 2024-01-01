f7c773e575a4ba6904a77147359ade8c72fb4e7d
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:51:17 +0000

    pinch seat backwards comeover the

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
