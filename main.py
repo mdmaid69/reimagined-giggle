a940c6649038486278781885c5b6a928c8a84a90
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:52:50 +0000

    avoid different space quickly doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
