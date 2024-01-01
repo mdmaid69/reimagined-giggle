4e689d11980c7a20765fe3d4f9a4f338dc4282be
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:05:26 +0000

    wise march allow edge maybe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_pressure(force, area):
+        return force / area
