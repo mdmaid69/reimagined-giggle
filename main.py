b8a2b95bbe0ea91de4540176529e5ee2b11e19fd
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:53:11 +0000

    empty set label exhibition art

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
