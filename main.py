7f741c96dbffdefb6e7355a81b2ac5eadad1eaf7
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:02:07 +0000

    destroy sound exact nice address

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+print("Square numbers:", [x**2 for x in range(n)])
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
