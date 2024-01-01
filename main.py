c3690459df39efc22a84372ec9cfd1a9d484d583
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:48:48 +0000

    new warning sit gooff wash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def add_numbers(a, b):
+        return a + b
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
