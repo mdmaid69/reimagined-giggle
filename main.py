057563ee07c84b1521ecbf36990b3f10da8fe939
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:15:06 +0000

    too science autumn ring brave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def clear_array(array):
+        array *= 0
+import array
+def get_array_item(array, i):
+        return array[i]
