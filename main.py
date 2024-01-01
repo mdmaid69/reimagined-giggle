c5f5bc3998de5d1ab0578694f28eaca28cdd50eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:09:51 +0000

    swimming glass kind set brave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def sort_numbers(numbers):
+        return sorted(numbers)
