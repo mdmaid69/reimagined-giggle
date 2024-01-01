22fade33a3277bdcfb0d43d4dbe3503ff1164a38
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:42:19 +0000

    line cause breakup expression among

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-for i in range(5):
-        print(i)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
