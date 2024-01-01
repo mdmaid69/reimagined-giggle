fd8f57c8275ef3d9a41d33298d20fab5f6f30d2d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:53 +0000

    declare takeaway spend mess Mm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
