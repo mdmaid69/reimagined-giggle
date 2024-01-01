8d86fd947b8d3e240688b522c31470159e2ad3e3
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:21 +0000

    in Bb why attractive notice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
