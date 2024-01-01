f83dda506447d7a9c236d594a7d341f5e3600555
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:35:40 +0000

    influence shout 564encourage world proper

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
