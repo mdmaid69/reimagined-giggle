343469b5cf7faf32e04f3f6864e4b38506797f77
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:51:51 +0000

    crash past case fight comein

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
