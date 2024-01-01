bea870adde74b16a06474f2cd599da0b47c02e7c
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:40:31 +0000

    joke grow pipe find skill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def reverse_array(array):
-        array.reverse()
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_union(list1, list2):
+        return set(list1) | set(list2)
