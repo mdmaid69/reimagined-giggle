eb2f03248e2a48f4df0620b804cfd8c19371c90c
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:55:57 +0000

    when callout nearly regional save

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def convert_to_hex(n):
+        return hex(n)
