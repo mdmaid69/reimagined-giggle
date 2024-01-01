e47c574e5a068a12c818495fcb887d032d07cfe1
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:11:34 +0000

    but smart forgive lose fairly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
