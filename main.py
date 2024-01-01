884936e4d0dd115eb32e8be9ff0be4e75f705023
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:32:21 +0000

    meeting preparation medium away peaceful

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
 import os
-def change_working_directory(path):
-        os.chdir(path)
+print(os.getcwd())
