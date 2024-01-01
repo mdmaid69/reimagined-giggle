ed33fb1d978735863c15de9702dbf2917625c10d
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:43:53 +0000

    his visit burn flight shine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
