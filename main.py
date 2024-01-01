4b5fb8ae6796d1e0423e061e5d42a33d98c69b63
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:17:03 +0000

    about all divide could level

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+print("Hello, world!")
