5508060384a0554ca0fa56064c6ab7c1b03b87c4
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:05:55 +0000

    try soft business shade soon

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
