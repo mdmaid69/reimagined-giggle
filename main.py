08b9b9bf82187ed7ffca8076c8da4ca2051ca771
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:43:19 +0000

    heat anywhere tie square range

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
