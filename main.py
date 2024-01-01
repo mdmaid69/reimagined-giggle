822095017b341dd756bedc32808accaefbf25fef
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:43:50 +0000

    suggest let pronounce safe hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
