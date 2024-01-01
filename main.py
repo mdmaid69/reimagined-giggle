55b68769a2ca1d75b934d5ece6906229acf057eb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:11:35 +0000

    comment decision rise operate accommodation

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
