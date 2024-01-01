35485435f0ba19b0e1d86151fbfe0c852bed1d30
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:23:51 +0000

    push brain eye challenge suckme

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def is_palindrome(s):
+        return s == s[::-1]
