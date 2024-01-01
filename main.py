8fbc6d67b36b3e9a0764dd9345775f02d6dceada
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:56:11 +0000

    record emergency separate attempt number

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
