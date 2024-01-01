0288cc15baadeb9f762755585697b8e93421448c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:18:18 +0000

    task under block figure many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def find_max(numbers):
+        return max(numbers)
+import platform
+def get_python_version():
+        return platform.python_version()
