d9113c34fb078efb61a946cfa72a0df0e43f62d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:04:56 +0000

    past child noisy deaf stupid

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import time
+print(time.time())
