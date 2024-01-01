c31b7781f0b3cf072354330e8c9cf320bf1b0c2f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:11:48 +0000

    fairly move custom centimetre importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
