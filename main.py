953385b8292ab811c95ae65f112215e8bf12796e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:06:43 +0000

    knowledge whenever along charge present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import math
+def calculate_sine(x):
+        return math.sin(x)
