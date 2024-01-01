01a394474545d985a7260740259b45e32cf51553
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:32:00 +0000

    label soon genuine smart method

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def exit_program():
+        sys.exit()
+  def convert_to_octal(n):
+        return oct(n)
