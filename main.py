032ff8d93770e32515cfeee8aa4963b9fc49d0d8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:31:41 +0000

    trip characteristic coat feeling law

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
