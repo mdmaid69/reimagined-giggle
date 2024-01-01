dccce802c8b69bae183bd2537c8705775be91fa3
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:27:50 +0000

    hit lucky wave sale trip

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
