be30ba3b577471be748a4295e82e881b7f0979e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:23:53 +0000

    extremely bet uponon heat Ll

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def convert_to_binary(n):
+        return bin(n)
