38bf200494d387e4c1b54cd90f7cab1963d8d62f
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:17:48 +0000

    card disapprove black tomorrow speed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_list(lst):
-        return sorted(lst)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
