5e5308614ed584c6266900896b4867280122ab25
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:11:48 +0000

    ground hang land back basis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def multiply_numbers(x, y):
-        return x * y
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import array
+def get_array_as_bool(array):
+        return bool(array)
