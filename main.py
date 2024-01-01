4cf6c138f91ff79c65eca32ad82fd3fd1c5e1a0e
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:15:43 +0000

    admit fear up takeback love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def is_even(n):
+        return n % 2 == 0
