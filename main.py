b9e321d155681946570736dc470a5d61da1da93b
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:17:13 +0000

    569enjoy belong argument manage label

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import array
+def get_array_as_float(array):
+        return float(array[0])
