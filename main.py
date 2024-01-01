541cf10a01c9e7d788fd1f06e692c1d17a650bb8
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:56:36 +0000

    delight disagree present with little

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
