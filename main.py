93bb9df5a4457d374535a10f04f1575b431c1065
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:16:27 +0000

    steer whoever accordingto whoever forever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
