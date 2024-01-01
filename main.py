73f5c7315e53177039f5cdfd1d3767e83bb98529
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:36:59 +0000

    drive bang cover size reliable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
