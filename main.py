940c7569e374b76dc3bcb31f9238981b227f5059
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:25:39 +0000

    watch motion definite ashamed then

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
