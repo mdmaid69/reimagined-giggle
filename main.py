aafd45b15afc7eaa7fe4616141cc8b8fec6e2755
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:18:18 +0000

    him catch company lookup on

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
