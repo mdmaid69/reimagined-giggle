1ac6de599f96951f9c247bc0c0c7607c00604708
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:46:09 +0000

    solve repair suit stiff path

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
