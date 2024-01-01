2b0940f1c9435aaa4a96484761f898968315e924
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:41:21 +0000

    high freeze block theirs promise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
