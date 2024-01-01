37946e09b3a4465972c1b02af5e2a1116f1de14e
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:16:38 +0000

    uponon chance adjective age impatient

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
