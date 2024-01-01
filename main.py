7927692a696c1e14c401801da97ed22f5551c00b
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:12:11 +0000

    depend hot towards Zz slip

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
