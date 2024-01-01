9d0e1f48b649433b3629b740aee82d073bc9674a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:54:27 +0000

    platform occasional sport climb question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
