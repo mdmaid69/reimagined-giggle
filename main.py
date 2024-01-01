e41fa6268867a42a44c50a2a718e1e8f35526b4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:00:45 +0000

    large whistle goback(to) port tap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
