56a7489aef5ba253bfd5182db52e152684f690e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:01:54 +0000

    replace tall difficult poor team

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-name = "Python"
-print("Hello,", name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_int(array):
+        return int(array[0])
