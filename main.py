17ae0b6777929633f5453d260729d5e54f77758e
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:06:09 +0000

    thought Pp dinner possibly roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
