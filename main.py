647e2d8deb1b51b14f75a681d582619d851f635c
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:24:32 +0000

    cut spend please impression tear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+x = 10
+y = 20
+print("Sum:", x + y)
