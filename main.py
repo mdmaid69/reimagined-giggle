cc6921e8216d7c07e519724f71f6e94fcc5b0fd9
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:18:19 +0000

    dance Yy forgive combine tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_power(work, time):
-        return work / time
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
