903cbf9bcfce986382c31e872d48e81342e4df6d
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:35:37 +0000

    rarely boil of invitation Nn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bool(array):
+        return bool(array)
