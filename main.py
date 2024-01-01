8017194682a6f08c840cae8f96689fe77c6355c3
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:11:46 +0000

    argue street protect poor pick

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
