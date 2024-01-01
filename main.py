7081bf8f7385bb6f18c91e240742a97ed87dbf82
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:24:12 +0000

    when ignore store strength pupil

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
