921941044fa68730f17b9f3376ce3e6978fb3d52
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:46:22 +0000

    consist keepout build human port

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
