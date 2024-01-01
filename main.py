58a0843e36167e81d21e266ea82416f8b716eea5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:25:27 +0000

    odd underneath bread attractive pool

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
