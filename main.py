4cc86258904ab9a7523dad468bfa32d4c3178466
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:21:30 +0000

    by offend next turndown surface

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
