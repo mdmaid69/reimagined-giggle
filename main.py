3a80e73cfed2e5e85e0ceaf673a4be3d24d1117b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:39:23 +0000

    bill cross limit once fall

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
