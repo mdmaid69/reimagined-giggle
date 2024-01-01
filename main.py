3e7d173d2435a7cdfee090c7ca8340f5107e4691
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:46:19 +0000

    breakfast computer hope transport scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_volume(length, width, height):
+        return length * width * height
