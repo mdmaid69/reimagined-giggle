da0c8b09f9282fd994470ef447f0b34a1d48c277
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:08:27 +0000

    tidy direction keepfrom disappointed flight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
