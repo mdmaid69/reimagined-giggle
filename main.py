0d8f6f9af609ad5eb34dd0d2ca45888cea5d2088
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:19:01 +0000

    bit swallow choice train can

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
