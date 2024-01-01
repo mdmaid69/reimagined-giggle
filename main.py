76444731887200af4a56dd48c48a2f05e88fe63b
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:15:35 +0000

    tip secret himself especially breathe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
