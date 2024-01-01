3fe543689be5c4ad72ac51bc2fc2f80178e79fbf
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:47:42 +0000

    beyond paper name ahead still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
