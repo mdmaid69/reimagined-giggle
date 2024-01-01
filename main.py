d57b7b71f86f35d29200d2ec5d1cb14f6a444f7f
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:51:36 +0000

    unusual until wave influence century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
