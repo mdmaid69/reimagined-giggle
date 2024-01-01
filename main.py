8e7c9f49c0ce633d98e56a2fb7f6971cbad263f0
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:53:08 +0000

    guess change morning pour comeback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
