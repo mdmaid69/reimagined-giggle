b4da353c0b2b8902efc6156462ad1272cc66ae56
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:21:26 +0000

    occur building picture pool practise

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
