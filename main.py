4e82749ad93fef4e722dfc2fcfb6247634fd063f
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:36:58 +0000

    wing formal they for dare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
