f5416e5577604df59c5491b249cdd6c98a4ace09
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:31:16 +0000

    declaration famous confident bury community

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  def count_elements(lst):
-        return len(lst)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
