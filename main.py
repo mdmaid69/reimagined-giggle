f43ec7a7ae85fcba4dd777e6431a317e6c2aef97
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:04:08 +0000

    single guilty exaggerate next deliberate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
