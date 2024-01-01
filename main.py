126b2c967112c579d77ce2b8564eaf7384307c61
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:52:01 +0000

    thick demand invite just mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
