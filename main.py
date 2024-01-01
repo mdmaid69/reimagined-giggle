e788b5417e2519bfad6ecc761bc33cc778ab5dfa
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:12:33 +0000

    seem paper typical emphasize less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
