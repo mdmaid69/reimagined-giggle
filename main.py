50b5a7341a14107c56fcd2500a426cd417a87d7b
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:30:27 +0000

    butter rule move habit naturally

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
