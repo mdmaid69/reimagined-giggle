248eb0a8ef37e53aac2543b1651dd0a60e57379b
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:30:30 +0000

    speed sink line dead outside

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def count_characters(sentence):
+        return len(sentence)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
