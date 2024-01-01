59d2ae2598159971678884fea4ef4a55f1791089
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:46 +0000

    formal measure join favour I

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def is_palindrome(s):
+        return s == s[::-1]
