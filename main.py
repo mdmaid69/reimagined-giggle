36df95554b7668ed1da9f2134e8493b8f9de6be8
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:00:10 +0000

    front consider range cheap difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
