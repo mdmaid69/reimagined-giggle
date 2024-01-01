a1130c388382e56d25863ab0c9ff0fb3bc566a11
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:12 +0000

    smile light male dangerous Ii

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
