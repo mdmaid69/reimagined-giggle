d3918db5046afd0b3fd040397b2716a81e2e82ba
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:12:13 +0000

    polite may judge by getback

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
