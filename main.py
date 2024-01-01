53b19454a43c3c6b78c0f234dacad08776a31e05
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:19:48 +0000

    any from preparation enthusiastic borrow

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def square_number(x):
-        return x**2
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
