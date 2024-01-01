09bb7595cbee30cf0177eadf441c6ee0cd8353b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:38:47 +0000

    steam shelter ought negative among

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
