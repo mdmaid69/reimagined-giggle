1817f247d168932ee6aeabea32a2fa67119f0bea
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:02:32 +0000

    cry note cover walk tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
