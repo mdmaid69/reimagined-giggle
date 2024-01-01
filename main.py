8d62cf5637ce0fdb5a8ff8fc533404d16c410f58
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:04 +0000

    whisper beat continue test eat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
