7262760e7a409b742d56f7ebce56fab701070c4b
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:23:21 +0000

    car perfect belong mainly to

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
