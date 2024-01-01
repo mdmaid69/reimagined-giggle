1824a3272223b2cffc96854bbbaf538392a614e2
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:45:58 +0000

    question 573entertainment fold band run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_power(work, time):
+        return work / time
