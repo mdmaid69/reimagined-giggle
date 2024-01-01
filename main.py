b660406609edece29f5c9a316a1e9384071d7341
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:34:54 +0000

    sport tidy close generally aim

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def append_to_array(array, item):
-        array.append(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def convert_array_to_string(array):
+        return array.tostring()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
