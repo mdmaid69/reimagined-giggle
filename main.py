e5082bd6a7013f8e6a8ca344f85252b711bb8d36
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:40:31 +0000

    bridge line obvious imagination yes

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
