cb9711174aa91238ec017974c88887a7d328928b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:59:55 +0000

    apart crash popular control challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import collections
-def create_stack():
-        return collections.deque()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def divide_numbers(x, y):
+        return x / y
