deed2a1aaeea2e202b0f8da5bdf5e63a00a853d1
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:21:28 +0000

    stretch taste hang possible stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_density(mass, volume):
+        return mass / volume
