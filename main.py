8b9599f5064d464ff64df9602d7c00190a49f8e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:51:59 +0000

    cost depend stand comfort affect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import collections
-def create_user_string():
-        return collections.UserString()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+print(math.pi)
