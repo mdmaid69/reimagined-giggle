fae28d8ae80956ef7feaeed6895001752c25f383
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:13:59 +0000

    benefit responsibility always classroom now

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
