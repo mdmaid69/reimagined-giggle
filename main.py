4a12c1104fc6d30744ac809807188a1a636a1809
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:38:47 +0000

    listenfor afford shopping worth scrape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
