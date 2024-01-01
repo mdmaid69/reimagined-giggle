a76903cd4b7b8e291b34f55ee31b6472e5ab8d56
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:06:07 +0000

    564encourage frighten shock getaway cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
