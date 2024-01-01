3a320bd9346fca04fe92d55a11874a2ad54bd272
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:41:24 +0000

    speak interview flat bite friendly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import time
+def get_current_time():
+        return time.time()
