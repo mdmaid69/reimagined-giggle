7de4d9d468c79a52b19e415ed0874591a7299394
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:03:13 +0000

    show welcome pattern joke definite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
