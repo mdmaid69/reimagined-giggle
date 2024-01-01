c7a98687146c39f679dd29b5db8880f6d8ef72d9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:20:29 +0000

    unlikely content react ill share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import re
-print(re.match("h.*o", "hello world"))
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
