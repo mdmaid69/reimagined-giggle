4dbaeae3fa683dc59aac97364e52e1b0e737627d
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:57:21 +0000

    wipe part round nonsense opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import numpy as np
-print(np.array([1, 2, 3]))
+def find_max(lst):
+        return max(lst)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
