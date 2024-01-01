858a7251e85e259981f49b2cd049223d1db4c3fc
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:11:02 +0000

    combine official smile label 572enter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
