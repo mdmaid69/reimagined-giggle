9e9e3475cf81e088a77af12aacb0625e217a5820
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:42:29 +0000

    noone sure information cheat snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def add_numbers(a, b):
+        return a + b
