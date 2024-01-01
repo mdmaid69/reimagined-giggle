9ce0c724753777360d9884eb45ff3aa6ea83a3b6
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:59:14 +0000

    suppose want various not Nn

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def remove_duplicates(lst):
+        return list(set(lst))
