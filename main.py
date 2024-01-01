ddb26ce462787996004470f23314d296f4e06808
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:33:45 +0000

    command brother stage image bit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def find_max(numbers):
+        return max(numbers)
