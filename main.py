6a488a52df55699d85d1d7dca29aac6367f390c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:36:07 +0000

    oil name scream hospital love

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
