6debbfebf1faddc0273694d7b432aa2608442735
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:25:54 +0000

    deep programme arrangement glass mess

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-print(sum(range(10)))
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
