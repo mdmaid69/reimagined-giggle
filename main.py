b24bf9fd2176532dc1c420ce4ecdd8c9194e8262
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:30:55 +0000

    mysterious child rush size wrap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import re
-print(re.match("h.*o", "hello world"))
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
