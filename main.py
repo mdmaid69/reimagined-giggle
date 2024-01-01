52ddc81fca297e981555a06574d6787f0f8f2e06
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:38:38 +0000

    conversation share carry flow match

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+from collections import Counter
+print(Counter("hello world"))
