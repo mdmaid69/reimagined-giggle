fd6753e9dfe294005b03c3b6d5c2febbdc727b3b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:01:32 +0000

    public block market turn decide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
