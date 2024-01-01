f371a3ed6558d6630c98325dbf4beffd2176d8aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:07:42 +0000

    other stamp neither live rarely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
