c4e85e72dc48a56ba925f58982d454c7e62cfee8
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:13:35 +0000

    damage comein mad lay start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def is_palindrome(s):
-        return s == s[::-1]
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_as_set(array):
+        return set(array)
