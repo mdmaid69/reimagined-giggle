c51d58b49e97ab7d14741012695e956a43029c84
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:44:21 +0000

    importance choose average square borrow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
