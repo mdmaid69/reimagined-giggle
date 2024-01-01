b323af2264e34df2759618d29ac96477be63a3db
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:07:35 +0000

    sing heavy will that like

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
