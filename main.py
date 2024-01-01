450c4bd195ff67280a531b7f15152b38179abd4a
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:42:57 +0000

    someone provide crash travel mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import platform
+def get_os_info():
+        return platform.uname()
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
