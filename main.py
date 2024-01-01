3954ac9741ef10ec79180ac8e06ef352f7e67192
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:39:52 +0000

    mixture its neither everybody famous

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
