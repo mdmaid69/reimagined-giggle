7c0b5f3d1628f68181ba941aaef95c322927aa83
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:07:51 +0000

    usual old treat great 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def calculate_area_rectangle(l, w):
-        return l * w
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
