958112f2316fede54e8ddc0c86571434e8b26ee3
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:42:25 +0000

    clean partner comeround top weather

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_index(array, item):
+        return array.index(item)
