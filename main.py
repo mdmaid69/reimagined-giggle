0990ad8c10e568d7cc32fa6518bbb964a70bf047
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:45:47 +0000

    week reliable join aware spare

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
