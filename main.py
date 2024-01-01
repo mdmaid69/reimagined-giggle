930ecf23707271132db7b3730cbe2b56bdc591f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:17:23 +0000

    keepoff moral range promise part

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  def sort_list(lst):
+        return sorted(lst)
