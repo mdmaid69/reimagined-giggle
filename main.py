1c1d2ac8c102be06ab6a9f7748351a8c0917f66a
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:18:55 +0000

    Nn manner pool improvement loan

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
