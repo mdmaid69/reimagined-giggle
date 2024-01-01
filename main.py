c3c2fbaf553641f79ed4c838ccf55a523adb9441
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:55:41 +0000

    ignore exploration manage scrap rid

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def divide_numbers(x, y):
+        return x / y
