3416e1c0da5a19063bff22e29daeb8aaaf122d9a
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:52:10 +0000

    fat base alive flavour official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
