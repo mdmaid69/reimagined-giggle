c7d6dd20953b909efb4a8bfe04e7b55db8ebfd61
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:03:33 +0000

    reserve head mail turn modern

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def count_characters(sentence):
-        return len(sentence)
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
