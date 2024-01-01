cf44a320c60ab54a0fdb55a0a6de8f64c4f2937c
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:55 +0000

    comfortable possibly lose seriously false

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+for i in range(10): print(i)
