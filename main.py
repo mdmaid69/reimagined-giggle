6f972d7cda486c5086484c8c10759c4b369aa0f2
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:14:56 +0000

    whenever truth cap notice call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
