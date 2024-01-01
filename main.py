8e05c50789da1f37007e0b29838c1cc5bfffc63e
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:10:04 +0000

    extraordinary damage run impressed unfortunate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
