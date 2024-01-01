76927f4ce48ee2e3a888b7a7f1cfb23b0507c554
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:27:39 +0000

    already score discovery art other

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
