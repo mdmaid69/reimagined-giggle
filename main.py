7a37ef70de9702c2b1762c3d215a34701b2fbef1
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:41:17 +0000

    even disappointed sweet breakin at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_list(array):
-        return list(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
