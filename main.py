ad88ffd89a3c175bf748ef5e1d0ed6da4fc5428d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:58:56 +0000

    frame usually basis board period

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
