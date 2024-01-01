8e6b234eca293a9e68e91cfdc86ff739281ac245
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:41:00 +0000

    vision car comefrom lesson Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
