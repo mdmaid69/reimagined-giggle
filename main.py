ec0a6398626d300ee9244621a02500a204f87840
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:56:39 +0000

    manner attempt only risk but

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def subtract_numbers(x, y):
+        return x - y
+  def calculate_area_rectangle(l, w):
+        return l * w
