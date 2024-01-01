7b81def6061afd3345865aecdbe6fbb3b2f76bc6
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:40:49 +0000

    eat contain act book could

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
