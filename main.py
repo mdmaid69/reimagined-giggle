448b21950f98fe29a9186ba61fff8ace39196b31
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:20:37 +0000

    somehow in practice once to

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def calculate_volume(length, width, height):
-        return length * width * height
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
