b5fa3682ef245789ccc31415166489a89ac14dd5
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:55:43 +0000

    quickly lose action separate boil

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
