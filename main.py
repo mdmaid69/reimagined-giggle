94f3f5b1ac465bd535cd14ec912b8dc150ec235d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:45:30 +0000

    attempt black example also broken

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
