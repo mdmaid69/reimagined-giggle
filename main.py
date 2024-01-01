08ab4f50bd34d7428d6f0d972422e1e356d627b0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:25:45 +0000

    goon along break priority easy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def is_palindrome(s):
+        return s == s[::-1]
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
