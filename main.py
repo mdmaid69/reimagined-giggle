925a2646c0c09ab2d4491117f5dd715c965124fe
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:20:45 +0000

    unfair project back error face

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
