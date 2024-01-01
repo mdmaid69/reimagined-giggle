1ec599a4a6d28e908b5032a07385630935f9b41c
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:36:45 +0000

    slow area ready opposite development

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
