b7d6d358597549ea07c1302efa9ece9525513984
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:45:31 +0000

    generous Ww suffer believe achievement

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
