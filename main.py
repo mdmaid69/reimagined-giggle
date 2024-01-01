64c4c8b3697703cf997e7acde460ae339fc03525
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:20:46 +0000

    carry tonight ear consist bus

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
