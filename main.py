b3ad2f45307569e1479f213950a5867ba95e6e80
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:53:53 +0000

    smart anywayanyhow valuable cry available

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
