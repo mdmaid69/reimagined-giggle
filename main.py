ec4703baa0f4f6cf5518be733fbb85c3a1785b41
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:21:06 +0000

    succeed moon bush manner each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def square_number(x):
+        return x**2
