b2a0a786e724f02afa3e1363c8e0c58d3ed47bae
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:03:58 +0000

    breakinto month politics start man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
