274711d1e39f3e1ad3f0422b927e7b1b94367da5
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:31:28 +0000

    rest tough problem remain size

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
 import array
-def reverse_array(array):
-        array.reverse()
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
