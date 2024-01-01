9bdc08b99117b9689dc841f3309bdb872da834a4
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:47:17 +0000

    bet lend material because flavour

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
