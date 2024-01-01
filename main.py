dd0debe11a18f192d11cfe567f577192baa24cc2
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:13:00 +0000

    over at and young search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_min(lst):
+        return min(lst)
