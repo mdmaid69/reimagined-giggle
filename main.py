8533f78c0191a48434ed2e0e0fe4d3b557b66ed0
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:39:11 +0000

    holdup wrong emergency gap place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_index(array, item):
+        return array.index(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
