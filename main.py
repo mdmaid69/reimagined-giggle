6512a7c3715eaf5c19a0885a83fb664f350cb550
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:18:14 +0000

    makeinto voice finish swallow never

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
