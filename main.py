6a6fcbd4a175e624cd4ca88d5ba14ce33b314894
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:49 +0000

    cause shock measure practice murder

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+print(os.getcwd())
