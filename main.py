23ad708f8265b1368dc1abfe48c0541501c178ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:23:00 +0000

    yours mystery remember waste science

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def get_array_as_complex(array):
+        return complex(array[0])
