7e5dd14553f4739f751103c39587afae0f70e426
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:30:20 +0000

    lucky police sleep television future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_work(force, distance):
-        return force * distance
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
