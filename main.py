eaaa16245c76597785f43526a71114338529d23e
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:22:26 +0000

    brush hold stupid something accept

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+print(sum(range(10)))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
