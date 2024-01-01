f76321b602b285ad25863c9e6be84699cc17ebf3
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:16:21 +0000

    preparation because charge pity service

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-print(os.getcwd())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_int(array):
+        return int(array[0])
