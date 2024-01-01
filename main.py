85088317118b0f0cc22d9fa9119d68f4ae69ed7a
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:07 +0000

    professional cover rush commit list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
