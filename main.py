4c2bf362ea9f580ffc6f1b45d61c8c3a9f5efc65
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:40:44 +0000

    world shame taste hello advice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
