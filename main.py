b9c26f34a6b2efd0c5a8c041a540698ba96ff142
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:18:33 +0000

    cheque rain terrible lose for

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
