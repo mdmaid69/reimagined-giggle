6ab3f7cf11cd4df7a849802488895b0777508122
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:26:07 +0000

    expensive pen face sensitive power

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import sys
+  def get_python_version():
+        return sys.version
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
