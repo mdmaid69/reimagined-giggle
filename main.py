453db3e17224af0f3cf533c52354971728bbe4fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:34:58 +0000

    human sudden off uniform search

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
