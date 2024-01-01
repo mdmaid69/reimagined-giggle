a07d55c7b751d2211e8d97487624630bbcb03ebe
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:13:36 +0000

    takeover absolutely be back land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import math
+def calculate_floor(x):
+        return math.floor(x)
