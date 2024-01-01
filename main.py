ceaf932bc7b2740b7f15ba56a50830be2a25ffe9
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:48:21 +0000

    argument many bringup let turnout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def count_characters(sentence):
-        return len(sentence)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
