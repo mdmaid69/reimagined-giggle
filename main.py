8de9d4ea3fe93d3157cb7f07aeacdb1502adf9c1
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:20:15 +0000

    first thankyou frequent programme occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
