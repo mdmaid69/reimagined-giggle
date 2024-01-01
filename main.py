bbd9f8281f0b083aaf3c25e8486e404eccd46cc5
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:07:12 +0000

    nose sign test dare class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
