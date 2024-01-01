9ff6bbe7849580abfd3b31b27abb8495354cd488
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:52:21 +0000

    key load another coffee male

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
