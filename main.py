fbc35356c05be8a89cac0b2db0c51415819aef8e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:35:42 +0000

    comment sit negative sail mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
