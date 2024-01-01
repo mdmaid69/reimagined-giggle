e5ca15058723eed7e5f87f4291b8d1d70f81181d
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:25:09 +0000

    nature while actually cruelty move

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def get_list_from_array(array):
+        return array.tolist()
