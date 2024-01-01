eb05c9e12970a6dea5b8c174e2d4ba4014988f14
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:46:42 +0000

    trousers prize tongue authority actual

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
