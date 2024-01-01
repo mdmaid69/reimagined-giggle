e293a54fcdf9a899a58849ab22adc817f125b124
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:56:23 +0000

    frightened towards crash plastic fetch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+from collections import Counter
+print(Counter("hello world"))
