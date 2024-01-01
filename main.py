3336c2739e7f0e83563fe4556fa344085bc376aa
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:39:32 +0000

    pen home whatever skirt shade

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+import sys
+print(sys.version)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def get_array_as_int(array):
+        return int(array[0])
