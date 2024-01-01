83965d82f3c95f426c5270d9c7f08adfe3c44c9f
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:38:06 +0000

    accept gate wash place lend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def cube_number(x):
-        return x**3
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
