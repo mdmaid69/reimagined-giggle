5c301e057eee283b21a497a41f5433b83067d9b8
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:01:25 +0000

    summer future scheme trick raw

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
