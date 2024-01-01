7dc0c8a49d1ba9508aea23ed5a73606cfa7b4ec8
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:37:50 +0000

    famous one instead wherever floor

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-print("Hello, world!")
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_pressure(force, area):
+        return force / area
+import os
+print(os.getcwd())
