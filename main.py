c758ca51e7ba17518d56db7fbd6de3c80646f207
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:37:56 +0000

    thinkthrough swing drink loose answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
