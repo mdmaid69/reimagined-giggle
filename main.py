ae33bf9e542025b32213730641801836fdd43243
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:16:29 +0000

    just hit heart tread zero

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
