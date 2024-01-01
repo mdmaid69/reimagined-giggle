9f797cf856b05d4091476702ba01cb57e0376f9d
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:17:06 +0000

    trip strike everyone scene ago

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
