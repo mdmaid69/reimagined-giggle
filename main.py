8a60c08c267577e67672ef1d6bf8cb8499cb7e81
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:37:55 +0000

    whichever far spot lock Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  def convert_to_octal(n):
+        return oct(n)
