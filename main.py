e63473d0ea85665d7848761645983c64496525ed
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:38:53 +0000

    likely through report pot nurse

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
