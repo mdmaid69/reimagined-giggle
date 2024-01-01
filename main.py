182eb923cc2b8b91dd7dfaa5fc9ca535b3fdc510
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:38:57 +0000

    store watch case cruelty night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
