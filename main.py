816906347a6132b0ec8d2551d7571698c308f9fa
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:00:05 +0000

    ticket talkinto towards hard 573entertainment

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
