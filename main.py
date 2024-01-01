17b556f36a90ed5dd5703ae2538aa8313caad904
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:25:55 +0000

    prize regard embarrassed keepout introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
