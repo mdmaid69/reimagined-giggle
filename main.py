5e505417d8fabd2ba28864c2ee707cff33d0a468
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:00:20 +0000

    school squeeze house experiment nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def delete_file(file_name):
+        os.remove(file_name)
