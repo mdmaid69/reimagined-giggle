cd406461c3b2a5542bec161fed52cf36635eec86
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:26:33 +0000

    570enjoyable terrible separate comeoff true

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
