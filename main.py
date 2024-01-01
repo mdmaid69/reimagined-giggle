b0ab8269657ed6f692b6187ff69849a2010bda1c
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:30:13 +0000

    scare recent ride idea drink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
