8055e53700cc4e59d6f0f029efd4492125930e3e
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:40:38 +0000

    convinced father vision mind laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+def convert_to_octal(n):
+        return oct(n)
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
