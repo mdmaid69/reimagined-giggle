8ca23fc83e66832b12b15f6b17990c194612ab2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 05:23:45 +0000

    near niggaacademy finger fierce embarrassed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
