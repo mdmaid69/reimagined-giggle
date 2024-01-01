832daa6ec7d34db44880667be593a0865305f68b
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:57:59 +0000

    pull owe lonely set release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_area_rectangle(l, w):
+        return l * w
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
