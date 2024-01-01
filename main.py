058051e003db9c02a7266ee9a996f72534425b3d
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:00:40 +0000

    heart treat occur least case

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
