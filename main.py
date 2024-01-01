2dcccd8bc6126727058a52a5d344c200f7fca38d
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:55:48 +0000

    bringdown pronunciation minor violence ask

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def append_to_array(array, item):
+        array.append(item)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
