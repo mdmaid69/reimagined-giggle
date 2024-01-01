c196bae6d811dfb2a43e80b8784c93ceb6995fa4
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:18:17 +0000

    ring journey Rr hold pick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
