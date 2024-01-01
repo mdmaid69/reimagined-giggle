2ff52a6ba3ad944531f98d66fcc85953d88c63a7
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:27:56 +0000

    accidentally until beginning tread phone

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def calculate_perpetuity(payment, rate):
+        return payment / rate
