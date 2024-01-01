bbd615c68201d8174541b295832a72327bd380c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:41:15 +0000

    unite fit believe work giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def cube_number(x):
-        return x**3
+def calculate_sign(x):
+        return math.copysign(1, x)
