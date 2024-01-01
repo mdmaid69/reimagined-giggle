4dd51ced3c891e9cacd5b6d84b455ceea724ca43
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:05:13 +0000

    weekend shout never handle us

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
