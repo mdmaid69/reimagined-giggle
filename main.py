93d159e334a0fc83c0213e01ad402b145087b88b
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:52:31 +0000

    sad heart usual squash again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
