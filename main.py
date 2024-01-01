6a6f5bb6aa4c540a461db6d3cfcf2a231d05b924
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:14:16 +0000

    shame goout interrupt considering change

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
