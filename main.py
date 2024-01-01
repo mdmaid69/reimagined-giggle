cbe68519e9c178b1407bd067eb0ae5d50e3d942a
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:09:13 +0000

    really word visit Dd round

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
