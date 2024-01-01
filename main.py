19b24dc180e976cd698a25b387bff19033b18a17
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:42:51 +0000

    to character lot evening do

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def cube_number(x):
-        return x**3
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
