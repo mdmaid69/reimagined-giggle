3ab7a38131e669c78b65e6545c3d13f4de869e0b
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:14:06 +0000

    cold neat suggest clear bye

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def convert_to_octal(n):
-        return oct(n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
