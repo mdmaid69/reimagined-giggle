59692b265d72e20ec590bd91e5399a92f6696fcd
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:10:38 +0000

    getup slice young confuse extremely

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
