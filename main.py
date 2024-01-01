af744ce4e28767d86e388d6308676f50335f7767
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:14:49 +0000

    whistle bright idea black factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import random
-print(random.randint(0, 100))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def add_numbers(x, y):
+        return x + y
