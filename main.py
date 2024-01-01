2946871cc5ea8ae2f8fd684a5bff1c8c2aa6ee0c
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:08:53 +0000

    nowhere meeting concern bury deal

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
