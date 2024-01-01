4f18ac6935d527b4e59bb1de86012c5e98960af9
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:02:28 +0000

    reasonable careful hope foreign mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_string_from_array(array):
+        return array.tobytes()
