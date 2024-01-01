28a8fed9607dac94bd63b6df4aa5e785f79b6574
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:48:35 +0000

    corner national somebody question contain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
