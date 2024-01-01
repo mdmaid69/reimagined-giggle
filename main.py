673b0c4e49443379f3d1fa41d86babf7ce247f3a
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:39:06 +0000

    situation light air talk measure

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
