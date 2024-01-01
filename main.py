7788fab9dbc92be119dea451f5e40676b3129cc0
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:09:46 +0000

    flight false sick interview study

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
