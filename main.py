203248608324f82ca55ab054c46b2e5331db5419
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:54:12 +0000

    letter perfect traffic compare example

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import json
+def convert_to_json(data):
+        return json.dumps(data)
 import array
-def get_array_length(array):
-        return len(array)
+def get_array_index(array, item):
+        return array.index(item)
