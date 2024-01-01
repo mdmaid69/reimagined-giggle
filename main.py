0dbbb612a1fd8b52723795734948b8e1e7823dd3
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:12:07 +0000

    custom pencil reason official hit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
