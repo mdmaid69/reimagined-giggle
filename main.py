c15cb61dc56b9bdb04362288e79e2f1077e3ed47
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:37:04 +0000

    welcome rob mine throughout ticket

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
