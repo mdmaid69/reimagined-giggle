21bc0938303c10a902cb56dd56f800a55124b015
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:32 +0000

    rich dress muscle sort communicate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  def convert_to_hex(n):
-        return hex(n)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
