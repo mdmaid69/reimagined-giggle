c151b547eefbe0fe5e7f74e1fb29dcaad1acfa96
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:26:35 +0000

    wish about avoid underneath start

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
