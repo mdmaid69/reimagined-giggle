61875f9e3208fd2f4ecf07ecfb258f18c7427b56
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:43:10 +0000

    dream unfortunate this wing later

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
