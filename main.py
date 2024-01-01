770bd1a12581a238e1281ffa3ad14ac003dc0826
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:00:58 +0000

    instruction building over doctor acceptable

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
