5beb75aa39d61bd9ff1a7b54f6c3f51753ad6a89
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:03:12 +0000

    place embarrassing control deaf sudden

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
