745f430c8e05f67007b91d042e63c2635f77314f
mdmaid69 <robot@alexsyw.me>
2024-01-01 11:13:47 +0000

    otherwise remark married result female

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def reverse_list(lst):
-        return lst[::-1]
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
