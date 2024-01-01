c0e2b8ad9a1feec2b97f1ba6ae6866c11601e372
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:00:29 +0000

    match though onto smoke sleep

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
