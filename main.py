d29665c7ba9ac3fa180ff98d08134fb370a086ad
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:22:37 +0000

    him shout copy address ashamed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
