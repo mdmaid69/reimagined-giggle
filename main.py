4070dca47fabad243417a1b584dd21f968481b34
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:48:17 +0000

    keepup art scream forward charge

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
