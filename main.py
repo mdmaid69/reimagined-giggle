486dc2d28c99e80be5f21fd92d495f0f3b104483
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:46:30 +0000

    window list steal completely food

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  def get_base_name(path):
+        return os.path.basename(path)
