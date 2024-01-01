7f20acb926ed7b33ab1ee4f8466d6cb15733a02c
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:40:07 +0000

    priority smart confident bottom naturally

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+print("Square numbers:", [x**2 for x in range(n)])
