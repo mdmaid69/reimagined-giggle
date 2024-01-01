65afe54c97207a4959f082473dc5008ee6a79032
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:00:34 +0000

    to Oo willing scare place

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def remove_duplicates(lst):
+        return list(set(lst))
