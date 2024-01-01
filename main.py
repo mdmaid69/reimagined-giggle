7f735197a3e604ff9447c97495480a46c48038f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:30:27 +0000

    sort contact valuable getback behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
