644a32fb95f451b1d8bf1142c2d7d456bd6a7ab8
mdmaid69 <robot@alexsyw.me>
2024-01-01 21:30:05 +0000

    reasonable tap under climb score

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def find_max(numbers):
+        return max(numbers)
