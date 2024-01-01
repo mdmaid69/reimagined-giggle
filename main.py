f6fa3df32f1dd04d4d0ce11bc02bb7549b360fff
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:23:27 +0000

    getoff vanish tight history possible

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import tensorflow as tf
+print(tf.__version__)
+def is_even(n):
+        return n % 2 == 0
