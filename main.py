92fd0b0764cef7a17a965ce15ebe49997b76fb02
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:24:49 +0000

    search worried breathe company list

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
