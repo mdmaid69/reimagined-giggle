6fa59cf070462f3908b765219a5c2e67759a6290
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:53:37 +0000

    stamp bad information refuse able

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
