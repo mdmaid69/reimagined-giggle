96e897c55ffa3b8f7c5ff77ac9bf802cc00da32f
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:09:28 +0000

    argue ride sea nothing damp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
