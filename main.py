0dc283abad0083bfdb559676f42e199fa106565d
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:53:22 +0000

    check short slip unless bag

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def cube_number(x):
+        return x**3
