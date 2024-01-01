d247f4005ada562e9ead42043bf0959a33d1e706
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:23:33 +0000

    thankyou act joke swallow ill

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
