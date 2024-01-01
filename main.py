86b4ece068e0371f80c02849ef98ab696207f58e
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:47:39 +0000

    account eat giveback freeze breakout

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
