71a84bd3f3798394d54aa74ff2ff3253340804ae
mdmaid69 <robot@alexsyw.me>
2024-01-01 12:57:14 +0000

    Itself brush offend study suit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def is_odd(n):
+        return n % 2 != 0
