ec1a8bd2de08aca4a43cd57545f06b9a21f7062f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:34:07 +0000

    never recent cry react offence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
