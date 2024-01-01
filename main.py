7195edbf874d0b9eca708c09f64b3a34eaa8d325
mdmaid69 <robot@alexsyw.me>
2024-01-01 10:27:24 +0000

    unhappy position prepare after introduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
