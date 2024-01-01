4845b04048218e903a2e53c85e293cf917a56d48
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:59:50 +0000

    ignore expression Cc stare breath

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
