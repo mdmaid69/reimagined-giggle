406e5e42f447a8ccba18895fd00506bb3cf552e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 19:45:33 +0000

    his concerning lift just behind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def find_min(numbers):
-        return min(numbers)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
