ce2bf5f294f2b20ceb7f0b612102c0919460e147
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:28:03 +0000

    suckme influence lower estimate low

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
