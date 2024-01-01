7ce3f45d62307b4e6ebbfa776445ce9c614e133e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:29:28 +0000

    minor keepup bus keen century

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_set(array):
-        return set(array)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
