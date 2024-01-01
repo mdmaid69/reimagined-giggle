d26a1f717ba8467bf08eb48088f43086b69221f1
mdmaid69 <robot@alexsyw.me>
2024-01-01 20:00:37 +0000

    deal detail same turnoff formal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
