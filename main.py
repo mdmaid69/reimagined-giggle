e649cf293f322318752c00fcd8e7d80f370816ca
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:06:56 +0000

    come attitude his laugh favour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-text = "Hello, world!"
-print("Characters:", len(text))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
