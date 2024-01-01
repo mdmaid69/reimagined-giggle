1327e6b5d3e1e13df8ef5de88b286eab25771ead
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:49:31 +0000

    measure blame least break somewhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
