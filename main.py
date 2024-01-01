9933ffdaf52d452d6c5873d00d07f0a07dddf98e
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:53:39 +0000

    first much trick store worth

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def is_odd(n):
+        return n % 2 != 0
