cd8fd3b6e9076b268e49802a562d274a1b3a3c38
mdmaid69 <robot@alexsyw.me>
2024-01-01 04:01:17 +0000

    practice sea event related consist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
