162aad961d0f4f5e600dc8cf2eef674252d73548
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:27:08 +0000

    concern government school brain sail

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import random
+def roll_die():
+        return random.randint(1, 6)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
