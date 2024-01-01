0225458a9f68c18e1120a4b3887dd17d23161900
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:56:09 +0000

    rest big back runaway still

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
