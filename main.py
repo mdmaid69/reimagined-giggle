e017563cba04e56053b6f3f5a9b86876d919bb71
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:26:56 +0000

    regard preparation rob wish place

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
