4a0771bcf02dd4836395983e6dc037a8d150956c
mdmaid69 <robot@alexsyw.me>
2024-01-01 16:43:14 +0000

    draw thorough dish event keep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
