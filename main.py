11073c91a521a3e71272087dc4ddff1be857d5e8
mdmaid69 <robot@alexsyw.me>
2024-01-01 03:17:26 +0000

    rain cheque who avoid rather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
