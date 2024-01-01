0422298b3615e94dacd7fe0c9912dc276f41360b
mdmaid69 <robot@alexsyw.me>
2024-01-01 02:42:10 +0000

    respect ticket tonight size sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
