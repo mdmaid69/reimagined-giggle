2cf14a059443fd2f940145860420f7676e9d79c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 08:45:15 +0000

    crowd round shrink divide far

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import sys
+def exit_program():
+        sys.exit()
