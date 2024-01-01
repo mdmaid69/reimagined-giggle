6c73998f78c0b59f1674a93e286483c5daef6757
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:05:16 +0000

    ignore trick deaf male manage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
