cd54f7d8cdd89d36ecfcb1894f31afa2839872c6
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:42:59 +0000

    club embarrassed call admit Vv

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
