1ff6bc74c161e9f61dcedb38e226fba1a46d8c08
mdmaid69 <robot@alexsyw.me>
2024-01-01 22:38:13 +0000

    environment fence national under aware

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
