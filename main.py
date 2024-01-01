eb1561139dd6ba7333b578c4d5f55f19cff288f9
mdmaid69 <robot@alexsyw.me>
2024-01-01 01:51:03 +0000

    today science everything borrow food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
