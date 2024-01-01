7d7bc9089cfc04292b5f29b300521af9a258b556
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:17:49 +0000

    dead purpose keepoff people rent

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+print(sum(range(10)))
