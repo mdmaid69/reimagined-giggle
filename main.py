5c4a51aadf7ef9dcd86032cdb2589b992bfc8fff
mdmaid69 <robot@alexsyw.me>
2024-01-01 00:04:20 +0000

    first trip platform afternoon act

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-  def sort_list(lst):
-        return sorted(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
