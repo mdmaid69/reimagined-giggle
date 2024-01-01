ea9fc39763cac01a7534a35e2073e62bd36e10c7
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:21 +0000

    scream during contrast snow bar

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
