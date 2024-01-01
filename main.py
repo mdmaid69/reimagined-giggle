2b530961a64b3c9504e71eb15c00706db9d9b161
mdmaid69 <robot@alexsyw.me>
2024-01-01 17:05:05 +0000

    bag neither past gentle sink

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import time
-def get_current_time():
-        return time.ctime()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
