49202a72280a8d7f6d2dfc44cfef6a039cd4fc8f
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:45:28 +0000

    afterwards across peaceful damage centre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def reverse_list(lst):
+        return lst[::-1]
