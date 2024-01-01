1f52f489b1ede83a3c7abf5622817741b83c99f3
mdmaid69 <robot@alexsyw.me>
2024-01-01 09:17:15 +0000

    hold truth easy call handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
