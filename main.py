90b8fec8e33c40a168a7e2e2ff3b6bd14f764565
mdmaid69 <robot@alexsyw.me>
2024-01-01 15:20:53 +0000

    cough beside maybe dance snow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def divide_numbers(x, y):
-        return x / y
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+def find_min(numbers):
+        return min(numbers)
