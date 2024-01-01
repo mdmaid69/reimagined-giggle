e6a0facef1d38005c1b417eb2d4156afe9bff7e0
mdmaid69 <robot@alexsyw.me>
2024-01-01 13:31:56 +0000

    powerful joke previous fear no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def clear_array(array):
-        array *= 0
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
