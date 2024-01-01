1d0016223a17d061cc0bc87a214c24f36c88bcfb
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:59:53 +0000

    standard Qq another exact culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import os
+print(os.getcwd())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
