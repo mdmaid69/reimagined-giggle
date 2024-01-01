184eb217442ac65dae21836500307abf2ac60f4c
mdmaid69 <robot@alexsyw.me>
2024-01-01 14:48:41 +0000

    type meanwhile patient glass in

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+def find_min(numbers):
+        return min(numbers)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
