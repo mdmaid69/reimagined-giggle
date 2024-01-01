b5d1e907147cd2dc82065391e2e28278eaf7b3b7
mdmaid69 <robot@alexsyw.me>
2024-01-01 18:46:08 +0000

    there Zz Gg familiar effect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import random
+print(random.randint(0, 100))
