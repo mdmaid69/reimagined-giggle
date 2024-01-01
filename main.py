731af63c3934ac19f5afc1e3c9cced8c0b547f23
mdmaid69 <robot@alexsyw.me>
2024-01-01 06:23:13 +0000

    spin wind march estimate cost

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
