699fd03769a76e70a1577a2b74efb0ddc4fc9e2c
mdmaid69 <robot@alexsyw.me>
2024-01-01 23:36:04 +0000

    ignore concentrate except fence band

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
