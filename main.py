827df63d59b6f1f7c6d2178ce1bb91f50264ed90
mdmaid69 <robot@alexsyw.me>
2024-01-01 07:13:48 +0000

    mark loose fast blow Bb

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
