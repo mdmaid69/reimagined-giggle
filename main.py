e837fe83e584ab62fc7e65d298524f046b2d7922
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:48 +0000

    average base round responsible store

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
