60f0aaeae8d291a6ae2fd13bd95e68cdd91ad452
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:22:33 +0000

    nature rent bright connect past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
