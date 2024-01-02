b62e3f7fb27593a9f975f28187c6dbb16a34d8a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:45 +0000

    the bored music stranger store

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def divide_numbers(x, y):
-        return x / y
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
