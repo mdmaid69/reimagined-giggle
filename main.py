36637ca1033e2337931c22e795ce59745fd71024
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:26:12 +0000

    yet wind crowded field message

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
