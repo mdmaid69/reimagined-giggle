ba33e36b258475c42141f63401dae523523dbe84
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:49 +0000

    attach quick phone feature sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
