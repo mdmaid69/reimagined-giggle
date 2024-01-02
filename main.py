ec0ce97d6ea488bdc9b59f46a65ca082bf277f2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:39 +0000

    aim many shake whistle suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
