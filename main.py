ac31d7e562e50f2fa5fd07328faa654825a4c63b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:04:30 +0000

    include experience software row turnup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
