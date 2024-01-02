e3eedba4b04805d63013ac684879977361ed05cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:04 +0000

    cruel clearly less beginning apart

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def count_elements(lst):
+        return len(lst)
