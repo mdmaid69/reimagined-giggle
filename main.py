dd241c17e2520b8a88dea9385a5041c0f9ff66ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:37 +0000

    family smoke win noisy run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
