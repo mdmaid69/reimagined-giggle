743597b4776f7ebb2cca0cdea12a21eba31ebc89
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:57:00 +0000

    trick announce manage broken nigga

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
