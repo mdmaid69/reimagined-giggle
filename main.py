76722c73d4f69e76a9cfc25a75d4a2ce62227c14
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:21 +0000

    close children perhaps equipment fire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+def calculate_speed(distance, time):
+        return distance / time
