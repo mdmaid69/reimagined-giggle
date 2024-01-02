5254dc7114c33d748580930320a199fca3678954
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:10 +0000

    whether well top demonstrate number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import array
+def reverse_array(array):
+        array.reverse()
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
