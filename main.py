5fc43b06e86c4e1a0653694176c21b40f3a82167
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:56 +0000

    mix radio average family minimum

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def reverse_string(s):
+        return s[::-1]
