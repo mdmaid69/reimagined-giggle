60f835c09b9efe0c9c63e2ab7e9591b98708188d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:43 +0000

    doctor land condition principle occur

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
