eabaf2c8dd7929f5d93deedf9ea2ec23406c1793
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:14 +0000

    at forward figure find successful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
