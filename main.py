39db0f88ff4fd1737f9c859b306c94e77af363b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:36 +0000

    drop lean lookafter combination track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
