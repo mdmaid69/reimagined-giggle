d93f5956fbf107b08179bfd0194d7265da9c4870
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:40:03 +0000

    protest like keepout uncertain month

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
