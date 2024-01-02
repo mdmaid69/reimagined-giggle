ea8d59f605744bb70d8dde8cd36cc63b7aeea1f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:12 +0000

    minority flood load shame hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_error_function(x):
+        return math.erf(x)
