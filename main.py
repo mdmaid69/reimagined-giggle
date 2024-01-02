2789d9afe9112f2c18be0487576339b558b2961f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:53:04 +0000

    avoid inside official human offence

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
