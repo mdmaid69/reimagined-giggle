ad6dd283fb17751a86e24abe96022851eaef9d68
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:17:07 +0000

    advice bringforward car bet feel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
