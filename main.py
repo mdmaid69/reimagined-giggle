389c62181701f09ed26ca1cadad4bb5d43fb26f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:46 +0000

    popular kill since share grateful

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
