f7c7d7c13f0b25c20966eeba814b8e9bc9b8dbf2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:30:54 +0000

    combination language up so stiff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
