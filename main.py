0458da88d33911959976a4e88ee866a4fa3f294e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:25:51 +0000

    should common skill recent storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def calculate_power(work, time):
+        return work / time
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
