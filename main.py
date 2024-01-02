5b99acc850f3eed1b0dcda08a2336943ce81b41c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:10 +0000

    correction finally new improve doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import sqlite3
   def close_database_connection(connection):
         connection.close()
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
