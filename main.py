05716b396769e6d1516a7d8752495ddd9374c6b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:30 +0000

    free sudden declare large new

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import time
+print(time.time())
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
