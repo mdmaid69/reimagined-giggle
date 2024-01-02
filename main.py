9c5a517d998871d7d5bb1336eccc97e3ab383aff
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:05 +0000

    mad copy send worst correct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
