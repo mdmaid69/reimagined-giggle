4f3ad4d712864576b43ee079613f96e8c2b5c418
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:54:21 +0000

    cinema scream splash tap dear

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
