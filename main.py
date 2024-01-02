332ee51724f220929bca3467c4b54377bee881bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:26 +0000

    trick theirs eager otherwise rich

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
