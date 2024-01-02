2bc9dffec357cacdcb2b2fe304f4717a017e9d29
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:44 +0000

    save left practice occasional real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
