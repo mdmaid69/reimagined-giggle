53996811bbba8e3215ae91b2dd8203b08087198e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:07 +0000

    condition scheme rub effect pinch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def exit_program():
+        sys.exit()
