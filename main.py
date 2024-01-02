201d9f8e32b3c4fbed15df7cc9d943a601e74760
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:00:19 +0000

    goon full impressive try reply

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
