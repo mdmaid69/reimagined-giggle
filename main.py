07a787cfd3f284448945ec37569615e357e45b27
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:51:39 +0000

    split display welcome can Cc

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+print(sum(range(10)))
