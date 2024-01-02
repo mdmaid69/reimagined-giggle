194ac093547f3df55a82ef3193d9e32999a1eff6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:44 +0000

    everybody pen sail call combine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
