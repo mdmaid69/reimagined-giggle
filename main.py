84033a04110d51b645895f5343aed69f485876b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:51 +0000

    tip part takeover remain too

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
