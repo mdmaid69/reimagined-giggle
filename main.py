9c5dbb44658468e4031352a4b3b3c63db1d33c98
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:42 +0000

    guess news little fall OKorokay

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def get_array_index(array, item):
+        return array.index(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
