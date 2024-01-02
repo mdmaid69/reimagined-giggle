4952c9cb2be4dc48d98a9377bfe820d3a54f2a00
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:07:37 +0000

    punish section time rest thumb

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  def reverse_list(lst):
-        return lst[::-1]
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
