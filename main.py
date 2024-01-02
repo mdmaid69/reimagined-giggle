8fd7e8050f0dbb463e6c50c1975fe256a002ee57
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:18:02 +0000

    invitation perhaps new little almost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
