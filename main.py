69b28c27eaae23b5fbf9f44b97f1e6c5de52295f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:50 +0000

    soil best demand instrument handle

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_hex(n):
+        return hex(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
