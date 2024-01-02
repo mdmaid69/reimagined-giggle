2e629329539f443e8fd11ada16b0ef1ca1071c9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:44 +0000

    sad best worth mystery human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
