4c1be3751ce8412b08a85a8786c78b06702a56b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:33 +0000

    high fit history furniture mess

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def add_numbers(x, y):
+        return x + y
