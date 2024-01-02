bfa8dbe09b96de4f7b22587cf7634dc67bf46a67
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:16:58 +0000

    spoil obtain eat guess freedom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
