397fc594d835a913cf21b001e6f3bf3d2f0bacb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:54 +0000

    left type appear weigh thick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_acceleration(speed, time):
+        return speed / time
