c957365dcc0b950e5b83551d90c8ef01009adeb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:20:42 +0000

    music turnoff rather information house

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+def get_array_as_bytes(array):
+        return bytes(array)
