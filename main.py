8625d2bce85425627f1db6bc565ea7e22af83f4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:21 +0000

    admit flood develop part warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+print([x**2 for x in range(10)])
