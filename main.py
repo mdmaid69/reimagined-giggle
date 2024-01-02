9258ee45fe5ccfc31f3d38ea95f76f7b32c79768
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:42:24 +0000

    outside copy walk recognize adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
