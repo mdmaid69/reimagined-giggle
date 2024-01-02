d5b171e520ff34389e7f633d009b80020bc90503
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:11:45 +0000

    holdout train attempt church would

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
