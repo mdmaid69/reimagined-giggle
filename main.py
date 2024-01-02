90d4b2a37e5050f17260c13f04b1f60451c4d9d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:34 +0000

    visit laugh patience cover reserve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_directory_name(path):
+        return os.path.dirname(path)
