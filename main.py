c79998ac0baf6dc5549399acc26a98068b0037ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:32 +0000

    Mm naturally show steam guard

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
