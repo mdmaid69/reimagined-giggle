5dc1f7240311c4979eeb4a150ececfed378c1cbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:36:19 +0000

    cut warm justice line strength

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_graph(x, y):
+        plt.plot(x, y)
         plt.show()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
