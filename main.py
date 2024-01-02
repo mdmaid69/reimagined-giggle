ea05704e30b6169dc9f345e6926fb84ef63a582e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:58:17 +0000

    ocean ilovedicks breakthrough behaviour pronunciation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(numbers):
+        return min(numbers)
