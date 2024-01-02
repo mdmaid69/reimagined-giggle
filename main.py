58318c4fb0ea82376e701a2f1f0d29618fdf8467
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:53 +0000

    whistle declaration one steep everyone

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def is_even(n):
+        return n % 2 == 0
+import array
+def clear_array(array):
+        array *= 0
