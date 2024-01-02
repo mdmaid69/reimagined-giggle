01f7e83f69b390b906e508871e8638497721f23c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:51:47 +0000

    height ours need represent rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
