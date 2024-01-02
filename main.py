cea7efff66fb30c66c7f8653b0096f8570932406
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:25:16 +0000

    exist Kk sharp correct list

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
