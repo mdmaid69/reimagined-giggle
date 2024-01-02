2f64f08a9d7383713beece281ad340d9580c1645
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:36:02 +0000

    suit flat disappear language tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
