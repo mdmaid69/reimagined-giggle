bc7cc9de9091406f56bc7175d5f310942b8f3bb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:51:28 +0000

    sail proper club deliberately attractive

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def is_odd(n):
-        return n % 2 != 0
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def find_min(lst):
+        return min(lst)
