eb6de289135c0e4afc693d9223c8941468279889
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:55:33 +0000

    kind coffee bringdown valuable pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
