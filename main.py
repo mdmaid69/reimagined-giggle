77b39f1d723267ec4a4408ed3ffdd776e8a4d4e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:35 +0000

    chase shock march couple connection

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
