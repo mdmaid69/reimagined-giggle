6c7bf3b97c130a7ec30b6ab9fc2701bc396efda4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:13:45 +0000

    sleep worth promise seriously worried

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
