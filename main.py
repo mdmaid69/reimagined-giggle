55347b1417324a999ae1448fba1412bafa312b9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:42:24 +0000

    man ago present by already

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
