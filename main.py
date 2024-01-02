bea82de5e0888e30ddca3abb7284d7bb6ac75bf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:59 +0000

    recommend demonstrate tooth Oo fine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
