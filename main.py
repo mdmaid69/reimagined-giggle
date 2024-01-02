4dec7c9242d5838cba9d47b6dcd2113e990aa4b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:59:21 +0000

    wherever opposite design carry consult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
