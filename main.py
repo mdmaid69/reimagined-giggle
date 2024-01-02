e132f59fb212a0b2430077d5b35d70af010b544d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:59 +0000

    positive taste past up busy

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
