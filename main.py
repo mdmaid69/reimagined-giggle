8e37c196c60fe02de77085d66de4b3e00088ea4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:47 +0000

    herself takeaway ought bringback ours

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def square_number(x):
-        return x**2
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
