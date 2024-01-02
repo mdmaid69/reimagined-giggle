74787aa20953178ec74cd3a4d03a1a1751d9b639
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:02 +0000

    release base climb immediate cool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
