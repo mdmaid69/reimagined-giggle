c44c1d8f31ad69ca2f5eb1f2bde24cc055b37a24
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:08:11 +0000

    pot bang such she dry

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+print([x**2 for x in range(10)])
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
