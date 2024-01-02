b6bc3efb33e5a78432a29b8e89960974de777ae0
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:20:12 +0000

    container standard odd improvement opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
