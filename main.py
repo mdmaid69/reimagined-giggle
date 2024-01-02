864d11b3d1b9dbfa656384bb1dccf2ad19d359d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:49 +0000

    blind whether smoke to join

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def remove_duplicates(lst):
+        return list(set(lst))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
