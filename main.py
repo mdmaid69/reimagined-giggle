9c4ec32951f92e955d83b6f3423534c92fd48a7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:27:46 +0000

    some miss trap shall present

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def find_unique_words(sentence):
+        return set(sentence.split())
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
