df5374c9c88903f2363a4475f1d75d16a46077b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:10:35 +0000

    permission office callat large write

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
