5b7df3d0ed3eeedc518d0bec019fa2ccc4614104
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:54:44 +0000

    seem club decide interview basically

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def divide_numbers(x, y):
-        return x / y
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
