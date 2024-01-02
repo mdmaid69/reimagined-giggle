f4f12d192873dfa8b330449314bbaf738b3f4bcc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:07:45 +0000

    contribute promise comeround little themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
