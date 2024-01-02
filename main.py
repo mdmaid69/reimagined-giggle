53be2d4a9507bec2b39ff64f4b84256fbf22a123
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:11 +0000

    of why check field also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def find_max(numbers):
+        return max(numbers)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
