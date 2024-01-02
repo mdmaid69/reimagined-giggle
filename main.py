74289ce3b4893c0bee1fbf16f36a584bb80b039b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:02:17 +0000

    careless woman why control usual

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import getpass
-def get_username():
-        return getpass.getuser()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
