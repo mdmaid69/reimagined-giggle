e062d2acae47d1dc6fcf83cb80438b622859f3aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:00 +0000

    round either duty Ff relax

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
