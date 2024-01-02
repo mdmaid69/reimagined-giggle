f6b89c40493c5879d274f691ca74e383c932eb4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:01:20 +0000

    remove 564encourage Itself down birth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_area(radius):
+        return 3.14 * radius * radius
