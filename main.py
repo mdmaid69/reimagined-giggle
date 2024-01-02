31d0d6d7a667147d066d3f0c6dedc0fb5a7e56d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:29 +0000

    off criticism exact high transport

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
