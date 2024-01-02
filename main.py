c7d268d464f93c060086f7d3338121b3dd8a746a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:58:18 +0000

    accordingto pen glass close considering

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
