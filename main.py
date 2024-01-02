91cb34bc9ae8ff0026718a8efa758721aaeeb654
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:31 +0000

    rob ball old goon no

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
