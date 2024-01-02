347c6535dac35376f7e23e4e376f29aeb1ab9d40
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:18:27 +0000

    tread leave what juice shock

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
