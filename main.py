fa871adb0e3473da8932741881bc1fbefff45f9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:24:34 +0000

    top hardly shut Ww appear

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
