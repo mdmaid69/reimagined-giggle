eae527ca48ae96da95490e8fff1d7b916545d7b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:21 +0000

    guide female aware reduction ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+import array
+def get_array_index(array, item):
+        return array.index(item)
