934f9e3d34fd6e2db65bf7342bf2bf9d596a1a3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:15:56 +0000

    makeinto over game different entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  def multiply_numbers(x, y):
+        return x * y
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
