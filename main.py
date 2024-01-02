ff4ce3860038df54e1fb8a9f3ac004ab992e9253
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:33 +0000

    reject actual model pause kill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-def calculate_pressure(force, area):
-        return force / area
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
