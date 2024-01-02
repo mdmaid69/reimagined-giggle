c042ac3580413c4599e8e5f11c403dd7cc3e4294
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:38 +0000

    similar lookup concentrate above and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
