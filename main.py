8617144f9818bad563cffc89541019d07af40999
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:50 +0000

    mistake operation fair perhaps case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
