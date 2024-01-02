23f7a280c528cda527bc9c246e70648a9ac08745
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:47:29 +0000

    otherwise reduce warn reply inquire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_item(array, i):
+        return array[i]
