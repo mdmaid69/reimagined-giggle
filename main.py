10a7aca7a00046f34cffc02ecef0519c2f7c8fee
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:53 +0000

    past left straight equipment rest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
