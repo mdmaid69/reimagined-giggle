664fb811e1527277bc2bdf051873b5d3e228756b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:30:18 +0000

    favourite bill suck operate entrance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
