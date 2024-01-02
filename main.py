fefa4f5899201bc5844355c6c8592f5513f85db8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:13 +0000

    large crash read cold attitude

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
