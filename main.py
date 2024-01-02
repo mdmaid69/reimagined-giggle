71af4aac2a44fffece723416419c144fa766342e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:17 +0000

    dull live her mess thorough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
