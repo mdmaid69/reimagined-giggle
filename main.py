0c2984f30e32919161fccdab62ea29f3d6d02b0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:25:58 +0000

    part this old address friend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
