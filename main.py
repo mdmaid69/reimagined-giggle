661e44db8cf0d9f54fe501ed2ac8f356b189605a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:04 +0000

    promise order press call what

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
