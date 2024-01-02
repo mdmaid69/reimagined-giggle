d2eb855c2458e968b9f4c427217504ce3da19a0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:31:08 +0000

    scream completely connected convenient bunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
