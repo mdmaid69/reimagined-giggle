abfef31f986b5c7538468a7fe405c4f7ca714ac2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:03:07 +0000

    have soon mysterious giveout Mm

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
