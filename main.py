51f8e9ec40534ec960b1cfa8f6f7c198f1f76e14
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:17:36 +0000

    hope geton average cough immediate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
