d46786fbe162e0e666e84c35aec776bc054444b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:43 +0000

    film line offer bye impossible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import array
+def get_array_index(array, item):
+        return array.index(item)
