aaa8c3ad855e128bd3455aecfaf521f74f12440a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:06 +0000

    responsible force plenty freeze surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_perpetuity(payment, rate):
+        return payment / rate
