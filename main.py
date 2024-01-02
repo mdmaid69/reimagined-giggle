59a812b6cd44ada32c98639fe9f06a1b39515ede
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:17 +0000

    innocent rely him rubbish combine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
