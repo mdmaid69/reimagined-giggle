fb3ee1ba4733ab5146f256f0108c7c00d2bb8105
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:08 +0000

    plain sweep fair kill peaceful

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
