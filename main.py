230e714913bdc9a2134d8423fe853679d4400c25
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:40 +0000

    corner transport space lose bag

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_current_working_directory():
+        return os.getcwd()
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
