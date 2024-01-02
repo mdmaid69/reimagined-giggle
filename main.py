5243534a320af6e84a7ba53c706c606da19390b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:02:09 +0000

    takeout obtain left miss regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def square_number(x):
-        return x**2
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
