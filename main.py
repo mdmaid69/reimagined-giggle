50cdf0de160b218db31a5eb58daecab1276d3c62
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:24 +0000

    obvious natural price edge middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
