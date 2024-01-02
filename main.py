b6a1ad726f7bd8db40339fe4522dfc73dc0d3839
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:10:50 +0000

    571enough achievement somehow earn relative

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
