e7f854bb4ff530a6f92c6b2ca1f36304f10d424d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:51 +0000

    hand decision negative fact print

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.time()
+import os
+print(os.getcwd())
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
