9110d879ec35cc88db2ce0e1f43b15443e1b8177
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:46 +0000

    conversation path moment separate ready

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
