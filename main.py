f718ad630cc583a06b590ffbb2e132f8cc120868
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:19 +0000

    vision damage mysterious fresh one

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
