ecf13722cb10e556365e08001bba950f3a971045
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:15 +0000

    past brave centre welcome condition

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
