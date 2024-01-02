dec62445ea3c613f955e01cda2c6bb91eacff6da
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:32 +0000

    further only exercise along downstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
