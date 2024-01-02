8cc03de24e5486eb0893748ae30b389648df7e07
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:16:28 +0000

    fast sport do far little

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+import sys
+def exit_program():
+        sys.exit()
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
