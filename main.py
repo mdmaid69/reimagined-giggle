1b81a6e2051a0829b001ed33a353448b14ea1f57
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:00 +0000

    swimming creature stay band over

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
