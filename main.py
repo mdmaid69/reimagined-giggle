8a0dca5cc6f47a890e6450a71c8d61399b15a81e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:55 +0000

    brown draw variety afternoon breakup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
