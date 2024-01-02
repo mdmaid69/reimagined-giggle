6ac2de45fc9fd3fadff2b29c3ce469c431ec2677
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:06:13 +0000

    regret port noise inform pump

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def remove_duplicates(lst):
+        return list(set(lst))
