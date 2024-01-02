e3aee5ba02d0a1a7f2fbecdec350c5afabb6bb23
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:10:18 +0000

    coffee so free 572enter right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
