a4a47f9eb4c9a5cca908cb9953ddd5f5fbc7337b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:08 +0000

    record concerned check office by

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
