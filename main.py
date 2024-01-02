b70d3013df75a6243fcdb04ca1c36e3d47f8eb38
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:05:26 +0000

    could cheque bath if loose

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def sort_numbers(numbers):
-        return sorted(numbers)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
