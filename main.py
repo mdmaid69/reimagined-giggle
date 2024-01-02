35cc4ece1248f1fcf74236110a128916e89b9d9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:29:23 +0000

    talkback sharp guard practise her

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  def is_odd(n):
+        return n % 2 != 0
+def find_unique_words(sentence):
+        return set(sentence.split())
