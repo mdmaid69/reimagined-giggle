5f530105e80e0fa724cb429b39a0a94253774462
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:15 +0000

    necessary answer worst clear its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
