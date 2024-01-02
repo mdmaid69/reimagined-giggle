ccdc317094948dcc6ba89470a9e98191ceab1a0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:05 +0000

    goodbye split distant ill return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
