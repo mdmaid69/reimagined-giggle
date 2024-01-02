78c1841982b5f4bbc6ef59758b23fc2dcaa35fa7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:09 +0000

    matter the after wherever spoil

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-print(sum(range(10)))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
