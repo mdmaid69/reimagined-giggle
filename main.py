06437b39237141916f044e0f2ed812819f3e8424
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:38 +0000

    might draw one comeoff raise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_pressure(force, area):
+        return force / area
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
