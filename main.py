305f3a97687dd99c542a19e9b1606b0a3db3dd67
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:16 +0000

    corner juice loose score respect

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
