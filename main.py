0aa8464293702c900d674ef60d0c9a19a9c6f536
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:01 +0000

    into knowledge introduce wrong Jj

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
