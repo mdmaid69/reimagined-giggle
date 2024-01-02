61b63384060e8195e2166ed286649c6a9bd7e208
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:36:19 +0000

    smart sale wash appear computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
