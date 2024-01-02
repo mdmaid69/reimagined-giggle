34bc32530a8770585edb5a07a1d7b53bf5f10f10
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:46 +0000

    experiment cover sharp dish less

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
