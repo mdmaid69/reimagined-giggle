a25ff4cf11d05f08dd2da2a73770735fd7cb0e86
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:10 +0000

    right rather support letter wake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
