9dd334f194e3236f749deb03ba4ef152f81c1b51
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:16:41 +0000

    software objection wipe top performance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_string_from_array(array):
+        return array.tobytes()
