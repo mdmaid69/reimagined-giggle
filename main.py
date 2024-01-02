2803c6f5a686f6d630f6b035e8e871550f342fca
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:32 +0000

    tread dead cover gift copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+for i in range(10): print(i)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
