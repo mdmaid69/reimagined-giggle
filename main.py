b9c8c9df91ea7aab7510591a6d26e865920791ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:41 +0000

    die wave nurse exit giveup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
