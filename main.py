030a43df4a45aa42f93ddb749fdedc22b4975bb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:52 +0000

    wheel moment plenty tell eat

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
