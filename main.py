513da94adebd263fe7f28bce631cbf7dbb97cb3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:42:00 +0000

    bottom like question favourite attempt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def count_elements(lst):
-        return len(lst)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
