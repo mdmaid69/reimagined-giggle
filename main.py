064e6a3d5d643359ce71e32fa858067e436a7a5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:14 +0000

    truth intend hang seem congratulate

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
