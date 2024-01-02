5c60f8a7e1a2c6ad3b64b2d2898f47a42225753a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:25:06 +0000

    themselves myself strike habit shame

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_speed(distance, time):
+        return distance / time
+def convert_to_binary(n):
+        return bin(n)
