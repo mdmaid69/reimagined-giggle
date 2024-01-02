849a959bb4c6c84b7eae2dc6663d6ee598bbcdb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:32 +0000

    treat let lead improve wheel

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
