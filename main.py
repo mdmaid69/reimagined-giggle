82f6d7724c2e97a1922a4210cfc9aae72d9ac884
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:10 +0000

    show will example flow good

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import time
+print(time.time())
