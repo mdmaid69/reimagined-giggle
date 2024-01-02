b08c2b0be6d07819883e6386b94b2622a3ed5124
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:04:11 +0000

    relax such birth impression fast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
