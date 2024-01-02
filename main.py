8ae3032e0f793bda6ed4d66b54989963e31dacc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:59 +0000

    bet accident call prepare wide

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def convert_to_hex(n):
-        return hex(n)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
