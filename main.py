3d933350ffd79602b07852fc58b48b1d49579724
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:28:57 +0000

    statement whoever mistake wind character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
