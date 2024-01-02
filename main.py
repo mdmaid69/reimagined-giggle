9f71b5f0e9b554ed90be2f6a1d96b08c03fdab14
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:22:29 +0000

    speed than straight Ff exploration

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print([x**2 for x in range(10)])
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
