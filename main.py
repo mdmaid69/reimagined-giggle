ad3e096056248515ece28cbb440a62a48e80b5e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:34:16 +0000

    quality backwards little lookup apologizeapologise

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def convert_to_hex(n):
-        return hex(n)
+def is_palindrome(s):
+        return s == s[::-1]
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
