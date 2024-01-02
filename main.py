356098cd35d4f8e1ce4b33088e1118e1e7531d49
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:15:20 +0000

    ask calloff bite close work

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+for i in range(10): print(i)
