2629fa486e4135472d1f1824ef4e9ceb7de0e55a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:04 +0000

    exchange round finish design children

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
