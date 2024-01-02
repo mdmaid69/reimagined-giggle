37461009e624064e184cd7d3fa85cc34ab6298e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:45 +0000

    opportunity whatever high model separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
