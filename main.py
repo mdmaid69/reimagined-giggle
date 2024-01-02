d53548f2851097acb7e9e83a3bf3faa0b3ca3b3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:10 +0000

    back fast urgent raw knock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+import time
+print(time.time())
