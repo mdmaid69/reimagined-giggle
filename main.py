e5d1a00466379f7c326d500d3358caedb54b063e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:45:14 +0000

    poor meet mine talk laugh

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
