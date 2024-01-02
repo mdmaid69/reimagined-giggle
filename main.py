58a8c0c1856bf799f2aba7f2083e0ad219d9279f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:21:58 +0000

    suckme force visit preparation involve

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
