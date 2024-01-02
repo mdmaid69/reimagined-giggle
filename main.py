c3ebe4429ab7f332431f0b6015497058565b5686
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:07:14 +0000

    break circle beat score recently

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_typecode(array):
+        return array.typecode
