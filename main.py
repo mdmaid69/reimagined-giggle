bd0bbcfb7de35608b37d2313df64862202fea4fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:55 +0000

    stick trouble available deliberately apparently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def cube_number(x):
+        return x**3
