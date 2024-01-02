704c10b7ea7aa5a01ce0514e83b563748a9845b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:31:21 +0000

    excited increase approve onto ought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
