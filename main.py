cd6e5e371f61297ec55d9bce553ad9abab9449bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:17:06 +0000

    friend pick regularly age train

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
