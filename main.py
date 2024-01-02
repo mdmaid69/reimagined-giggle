c312fec9b423857256e6d77f69273d683bbf5255
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:40 +0000

    cap just close guess upset

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import collections
+def create_user_dict():
+        return collections.UserDict()
