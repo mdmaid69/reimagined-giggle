5bc5a04ccad75f3339693970e5e4dbfbd20d6d0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:17 +0000

    thought offence doctor hand final

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_work(force, distance):
-        return force * distance
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
