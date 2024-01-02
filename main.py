43ea19111dbdd6428200f4b9963cd67a0c4340a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:02:26 +0000

    while rare whistle talkback also

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
