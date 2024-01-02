e7c80b05bcf04c9e9f6b19fcc6265624a01fdca6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:53 +0000

    still start comment confuse corner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_user_list():
+        return collections.UserList()
+def calculate_work(force, distance):
+        return force * distance
