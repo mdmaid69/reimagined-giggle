851c2d6dd41c3195256de1c49fcca21e034f536d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:24:07 +0000

    lean section woman tip boat

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
