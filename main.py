a99f9b3e78e588824cfe5d495680e69068307e96
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:45:23 +0000

    artificial night goback(to) together loud

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
