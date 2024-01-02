fa694e88af4ad6c7dadbface1bb9cdf25f57b79f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:39:02 +0000

    any floor expensive rain suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import os
-print(os.getcwd())
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
