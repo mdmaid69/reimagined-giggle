fb636737431b39086ebd19cf49f435e66e4e6e00
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:01:52 +0000

    crime turnon sweet keen family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
