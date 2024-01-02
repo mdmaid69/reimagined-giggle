b3bd75f816006a55d72bec86efe1b116539afa7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:26:54 +0000

    brain watch price test mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
