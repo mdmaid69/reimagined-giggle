befc6bc9336354fd51ed95180e196a054e6e10e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:56:29 +0000

    way message contact sing supply

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
