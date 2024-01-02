9b86b251d07def1d00ccfcb1fc06633c98e99975
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:31 +0000

    lookup man plastic careless convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import time
+print(time.time())
