50bfe2c45450951a01473cf5441cc1d2b4f61469
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:44 +0000

    present score will compete damp

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
