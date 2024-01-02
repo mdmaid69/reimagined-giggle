27e9800a8d7512291aa081b487bf633b5c37973f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:41 +0000

    laugh colour appear careful not

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
