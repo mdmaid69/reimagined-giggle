1ce75b3dc3c3b8304594c6e49b7ea8bb469b7f86
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:02:56 +0000

    pool certain environment giveout previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_current_working_directory():
+        return os.getcwd()
+import sys
+def exit_program():
+        sys.exit()
