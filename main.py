a51eeae9ab39ef0026c9385392df42abae536604
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:38 +0000

    first funny use worried wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
