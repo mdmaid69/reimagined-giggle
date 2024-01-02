c007418fedb44dcfa2ae229b36e6be89a1b01ee9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:42 +0000

    switch run ordinary judge like

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-  def square_number(x):
-        return x**2
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
