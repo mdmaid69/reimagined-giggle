94c21fe53773caaf343999f056610dcfe1b4f4a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:20 +0000

    excuse happy experiment mouth dry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
