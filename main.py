54822dda176032db28d48f3aee1d3cded6ac0459
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:06 +0000

    its serious house act instrument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-print("Hello, world!")
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
