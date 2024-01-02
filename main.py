0851ce3a0229c9de5f19b01e5b53204e9c8bc47d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:04:59 +0000

    understand pocket from approve blind

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import os
+print(os.getcwd())
