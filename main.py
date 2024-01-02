030e290e7ac7f73773b5369d0442e05ff8d4bf70
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:42 +0000

    record into of event rest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  def multiply_numbers(x, y):
-        return x * y
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
