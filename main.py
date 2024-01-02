eb498fee9136fc895e2f0148e38702b09a78e4aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:04:45 +0000

    good opponent low beneath where

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_rectangle(l, w):
+        return l * w
