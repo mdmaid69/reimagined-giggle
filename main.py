b8c7551abad7ea3648d2d25cef07d169a2f475a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:47:48 +0000

    practise tense absolutely definite letter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
