f76102db283d9444aeca47c329be9430ac98a167
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:06:03 +0000

    raise size thinkover combination comein

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
