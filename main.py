be44ef00b94332b58fe08a1101b234740539944f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:33 +0000

    schedule indeed sad what live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
