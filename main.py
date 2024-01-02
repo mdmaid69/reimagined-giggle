8a114a8d8401a6d849886f31216d494276fc0681
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:55:24 +0000

    accordingto behind luck quickly entry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+def get_current_date():
+        return datetime.date.today()
