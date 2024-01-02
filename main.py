723379a08b06063247789b1cd79ebee7c48a2ba5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:50 +0000

    arrive clearly pause bar better

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
