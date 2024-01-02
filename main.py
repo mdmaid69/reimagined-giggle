9723cdd00aa0ce0f295d91f344f197b024f61464
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:23 +0000

    sense feel active show illness

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-print("Hello, world!")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def cube_number(x):
+        return x**3
