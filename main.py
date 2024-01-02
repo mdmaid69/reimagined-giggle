79d52e678a9b57fa9c79d98519b109a35ba55a2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:18:10 +0000

    school turn lookback(on) pleasure crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_average(lst):
+        return sum(lst) / len(lst)
