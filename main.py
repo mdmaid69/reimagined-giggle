b3c68c91423f2d59fd40694ae7d491bf80d8c545
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:02 +0000

    short original reply healthy lucky

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def is_odd(n):
+        return n % 2 != 0
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
