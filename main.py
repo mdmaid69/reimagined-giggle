9c3444127d4101910d4d7f3a7a3b4f6d6fc8acd4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:21 +0000

    hello mark block off cut

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
