ce6cbe94d2eaca723fa616d7c476a552339c11c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:13 +0000

    back through grow test giveback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_ceiling(x):
+        return math.ceil(x)
