e408387cef79114da1208c472c8f850ca8d2b94a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:46 +0000

    continue final shout tape emphasis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
