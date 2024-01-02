0a59c96376ad819f047170e53a780dab46eb1a74
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:18 +0000

    consider comeoff inform central avoid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def remove_duplicates(lst):
+        return list(set(lst))
