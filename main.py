18b1ce950395350c8c087f648b6bae92038fd033
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:08 +0000

    best hit roll total not

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
 import array
-def append_to_array(array, item):
-        array.append(item)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_buffer_info(array):
+        return array.buffer_info()
