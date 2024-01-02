b52b6c9866eee54227351c00754008fcf46d1d72
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:28:22 +0000

    theirs vanish programme ability way

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def is_even(n):
+        return n % 2 == 0
