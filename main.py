55a6c06d59beb2490db3fc3a3e8f6b9b1a4707d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:27 +0000

    generous consist reach cover mad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_floor(x):
+        return math.floor(x)
