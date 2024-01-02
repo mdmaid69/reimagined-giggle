dba3bb49fc78cca840522c9a511b9512e20960e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:35 +0000

    wheel lower well comment turnup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
