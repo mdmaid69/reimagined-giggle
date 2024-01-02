c12e1b51ead71cdb808d8e70a5cfe82c972ac739
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:25 +0000

    through tough cost crime recent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def divide_numbers(x, y):
+        return x / y
