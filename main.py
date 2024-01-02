f173766bed97af523b925b162574e1de82e53519
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:38 +0000

    brave cruel comefrom kind pile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
