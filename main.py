c1abb260f05180a055fea4b0bb00ad46ad0c05cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:29:39 +0000

    today air drive himself cruelty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
