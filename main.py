8c8c08d90931326ddaf8029de7d7eb62e56d6776
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:13 +0000

    bar fat share brave wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
