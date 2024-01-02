158609c3f59d94130849f3592a20630db2910dd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:31:49 +0000

    embarrassing direct connection buy wild

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def find_min(numbers):
+        return min(numbers)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
