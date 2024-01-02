779bd36b0588cc0a147239119287aa223247be6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:32:23 +0000

    every shame announce wash fit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
