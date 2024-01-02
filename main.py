ab8802c656e254e512587c13facca146287881ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:56:56 +0000

    spread gradually also cry judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
