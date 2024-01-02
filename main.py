07d443edc41e15761a88fb16bf1e0b2174241267
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:45:19 +0000

    fight thorough settle wind project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
