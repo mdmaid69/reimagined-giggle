a89d76443811d35f41e45e51730f914d30375325
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:37:32 +0000

    no interesting slice welcome appeal

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print("Hello, world!")
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def square_number(x):
+        return x**2
