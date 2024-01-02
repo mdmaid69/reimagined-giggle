d7c85b84f7e054bd77f9430576f93c89b2d83322
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:13 +0000

    term against thing sea day

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
