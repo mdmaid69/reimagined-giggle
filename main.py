e5aff790a5b193c48c6f349d5a5871906b73dac9
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:43:25 +0000

    shop Uu outside bite public

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
