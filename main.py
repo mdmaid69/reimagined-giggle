46177cf4c2d7900bf405ecb944be742e76a6316f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:40 +0000

    nearly building every afternoon bend

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
