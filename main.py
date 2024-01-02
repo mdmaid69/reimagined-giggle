dc96916de022e945f124295b5d984bcd9309e51a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:11 +0000

    weather instant improvement dish rub

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import os
+def get_current_working_directory():
+        return os.getcwd()
