9a295a726bdcfb5ed19e373f73a7a61de89e5fb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:16 +0000

    tidy doubt tip comment rank

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
