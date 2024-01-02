4456a95e030e60a641def584c79ce93c7e8c700d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:44:20 +0000

    accordingto sleep circumstance steer street

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
