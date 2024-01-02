0747c2fafa9c44720fb9c5c014e28b45662f5759
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:00 +0000

    straight goaway organize mistake confident

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print([x**2 for x in range(10)])
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
