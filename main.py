5f4e36a801c45995458d2d5a6d339fd788b7902b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:21:14 +0000

    declaration risk break pure neat

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-def is_odd(n):
-        return n % 2 != 0
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
