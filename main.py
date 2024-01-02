61ac8a72bd31d30678a1a35ee3ffcd77002618e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:16:12 +0000

    join responsibility address light house

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_density(mass, volume):
+        return mass / volume
