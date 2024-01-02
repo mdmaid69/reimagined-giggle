3e22336025c8f22c3a541bf5f75b5c71b1e7d8cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:02 +0000

    debt birthday react wish past

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-def is_palindrome(s):
-        return s == s[::-1]
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
