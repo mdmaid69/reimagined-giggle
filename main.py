604c7453bb6e4bc397682668a3cde3ba17092c8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:43 +0000

    cry reserve arrival feeling drink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
