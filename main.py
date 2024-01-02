53804f9b85e27d0a461d54a4fa8c0a3a529b39a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:52 +0000

    doubt stick compete ride university

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import random
-print(random.randint(0, 100))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
