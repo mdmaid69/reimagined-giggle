c7a58e5c64bccc16aa0f3208ca1206c3b52e1117
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:48 +0000

    sell human arrive hear wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
