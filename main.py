1a4655cd1edf248378c15bf2587a8a57a707ab80
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:39:26 +0000

    act during outside soft trick

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
