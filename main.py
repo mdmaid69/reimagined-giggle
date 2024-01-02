062bce03a8c3b4fb38da5b2dd590bf0dd77e3eac
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:44:24 +0000

    indeed develop compete type size

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+print(os.getcwd())
