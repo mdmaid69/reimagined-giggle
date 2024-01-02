620c1d0fd04926d4e2016cc325e687079cc6f66f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:15 +0000

    several talkback persuade neither temperature

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import time
+def get_time_since_epoch():
+        return time.time()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
