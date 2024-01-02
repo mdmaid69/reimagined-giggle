0a573e97784643e21796b9c46838dd9762fd0838
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:40:26 +0000

    treat artificial line cap miss

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
