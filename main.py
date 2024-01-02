de61bb815a0d371434c6deb9df20d64768f6d46a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:21 +0000

    comeout artificial movement cope mainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
