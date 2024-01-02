5996dcb671b2ff84a10648121146f109cc54aaec
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:51:09 +0000

    direct that determined guess either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import time
+print(time.time())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
