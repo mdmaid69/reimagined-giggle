6d05044256b52e4bf5fb5735eefe792b89184e26
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:43 +0000

    decide improvement tomorrow perform hope

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
