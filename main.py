539cc6e085de33c2ab3fa1191432bcc96dd49747
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:43 +0000

    Qq them sight beginning ear

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
