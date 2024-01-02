75c9b3fbca3594b34e519519c77202323d81ca81
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:25:09 +0000

    height bar least dance goout

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
