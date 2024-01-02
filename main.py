1dc784f7f5e6f66aed700143b71b0d4bd7931a27
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:04 +0000

    onto evening Ff necessary nerve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
