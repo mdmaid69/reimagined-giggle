ffd338dec79849d57b05f4b54f8c87786ff7c5ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:02:51 +0000

    calmly exercise hear begin though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
