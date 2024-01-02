8c225785d18d00f08b35e6f60bc98519f3593b8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:43 +0000

    trick spoil favour to finish

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
