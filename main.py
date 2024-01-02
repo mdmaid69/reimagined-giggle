877f4cb60dbf6a042e91b7dee81a4dcc094943e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:56 +0000

    raw point present share let

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
