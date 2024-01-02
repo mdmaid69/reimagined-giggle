8e67a5af50cd9ada249a8af65cd4460f5c94db37
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:08:45 +0000

    set the suffer party commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
