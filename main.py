92d2c19c2136f41f86265aa704b3e8f0e410429e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:52:51 +0000

    wire centre direct confuse tense

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
