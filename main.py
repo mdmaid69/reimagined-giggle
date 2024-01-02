f4d59a6a68a7de799077b79d823791c09fcbfe80
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:36 +0000

    set stop close sure shock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def subtract_numbers(x, y):
+        return x - y
