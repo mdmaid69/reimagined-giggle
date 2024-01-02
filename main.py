71ce37c08c7940268ba26c30a692e598c4bbce0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:04 +0000

    contact the region pain introduce

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def square_number(x):
-        return x**2
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
