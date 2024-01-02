b0785a70932add0bfd6fb85744efedf4073acb53
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:26 +0000

    fast confidence crash value doctor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_odd(n):
+        return n % 2 != 0
