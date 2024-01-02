ad28ad0f9eae77b8f0d8a8ec055edef696d094b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:30:40 +0000

    problem swallow prove if uncertain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-def count_characters(sentence):
-        return len(sentence)
+def get_array_as_bytearray(array):
+        return bytearray(array)
