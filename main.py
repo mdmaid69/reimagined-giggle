63d00fb25f5612ecadc25ebbda88eacf6641ebf3
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:00 +0000

    apologizeapologise obvious cruel skill will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
