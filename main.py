d816faff3af763c91d4928e08aff8335254fb028
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:28 +0000

    impatient complaint responsible little another

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
