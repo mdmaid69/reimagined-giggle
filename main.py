30b046ca8ebeb8a8ddc688e6a87a7d9f58c50eeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:09 +0000

    shadow between present combine aim

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
