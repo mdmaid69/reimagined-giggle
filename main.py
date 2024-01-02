112c2ba38fbc28f075694948d9a7c753c3e9a4d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:55:30 +0000

    tap impressed on grand best

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
