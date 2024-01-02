bfeec342924d4847d11f552ddb102bfa74779b62
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:55:05 +0000

    getaway team he Ff precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
