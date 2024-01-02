53ffb818d5c81e5a1968ce5d1181f1dc433aabf6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:55 +0000

    correction pencil board reduction write

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
