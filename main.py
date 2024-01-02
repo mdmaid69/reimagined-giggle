5e2cf72a2a75cdd8e64deaca72748551c483bc04
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:36 +0000

    into neat dream face wide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
