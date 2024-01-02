142420c2cb603f181a74df56faec82e1742b829d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:22 +0000

    breakup short fork shoot tour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-def convert_to_binary(n):
-        return bin(n)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
