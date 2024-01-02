9ba071764dba47f6c13ea75f126523201bee3d6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:09 +0000

    new flat desperate bank correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def multiply_numbers(x, y):
+        return x * y
