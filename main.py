ee95b5741afd9e0efaf243284ca653d726bedba8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:13 +0000

    interfere too runout goon school

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
