f503856a2d82e0b9e594b2f573f174ddbf2add5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:12:18 +0000

    new listen operate change so

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
