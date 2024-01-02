39cc9c4c06dae2cb3a8ff583c14764d12d3ac522
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:21 +0000

    reject letter back storm radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
