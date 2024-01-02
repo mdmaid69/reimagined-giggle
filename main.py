59e2d30eabbeca8a16554d28fd921a3cfd60aefb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:27 +0000

    score today new without wipe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def append_to_array(array, item):
+        array.append(item)
