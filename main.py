c3695b305c030f785af957df197fbaea63b09bbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:50 +0000

    basically proud no priority lay

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+def cube_number(x):
+        return x**3
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
