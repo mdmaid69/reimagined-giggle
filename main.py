f85548d064da69567ee7febc91ba9910a0b7515f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:29 +0000

    steep where runinto off turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
