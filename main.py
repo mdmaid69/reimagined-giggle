7cb9a17f490b0aa3837f5c2c3e8d25a0dc4d33fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:01 +0000

    guard always break yourself land

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import sys
-print(sys.version)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
