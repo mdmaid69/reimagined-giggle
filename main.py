e17b29ec27faa17b151955bbdbe1885b97a99a30
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:57:42 +0000

    noisy race spill sorry approval

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
