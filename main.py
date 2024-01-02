c72071fd6e6b0d1f40ba07a9b936a0d64e0d93fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:02:06 +0000

    congratulations bring scream rush model

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
