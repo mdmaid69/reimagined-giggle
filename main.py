1dc43620dacc8b7dd1bbec630c5c8a595dcfec6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:47:31 +0000

    month beside opposite peaceful step

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_priority_queue():
+        return collections.deque()
