7a39ad41fe22bf4a20d9ef999e06efd312f7fe80
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:07 +0000

    goback(to) better confusing unlikely otherwise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
