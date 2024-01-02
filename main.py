b282fae52e76047cf048f59d4bf26e17c2ecf9b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:30:18 +0000

    strike confidence necessary emergency surprise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
