3d69f21eeccc69baada565548dbbeebc58b2a44a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:21 +0000

    mixture second thing interested between

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-def sort_list(lst):
-        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
