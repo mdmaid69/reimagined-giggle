a24fb68f716837ad5c284ff845b2a4d05cdffede
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:46:23 +0000

    fear her sell forgive name

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
