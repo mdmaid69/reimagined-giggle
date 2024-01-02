76337775178f90ef66643d31c33a97c00195441e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:19 +0000

    relief cover bound promise sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
