964880d62fb4c37b3c49d7546ae66fac77ae4c93
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:45 +0000

    interesting bottom doubt silent back

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_power(work, time):
+        return work / time
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
