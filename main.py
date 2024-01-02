0e432088aa1cb9eec6ba5ae316cd226ffa2c0f0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:30 +0000

    takeout roll no appearance bringup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
