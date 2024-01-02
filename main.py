a351d28141655e60e8100ca6d3124d2e694898c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:55 +0000

    form scare sail belief proper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def convert_to_binary(n):
-        return bin(n)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
