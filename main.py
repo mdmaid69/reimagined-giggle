30c1db11fd3e006b6450011123d8c09fecb61fdb
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:35:42 +0000

    fast absolutely opinion keen to

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
