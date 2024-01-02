047e16593834fe9419852f700e92e1a15fae7a3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:01 +0000

    doubt month touch contact share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
