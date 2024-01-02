887ba63133f5fd6927dc8a15090256bbf6e5fd0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:47 +0000

    related side improve hot taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def add_numbers(x, y):
-        return x + y
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
