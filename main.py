5a1ee87dc2de1ad3f6be67a30153a3d9ba84759b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:17 +0000

    harm blame convince method tragedy

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def reverse_string(s):
+        return s[::-1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
