e1889fa8ea41b357015bc18fb947e4a890341797
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:53:25 +0000

    already century cope warning criticism

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def convert_to_hex(n):
+        return hex(n)
