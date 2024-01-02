04683d687cc1c3b8424c533c4930f2036989f93a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:52 +0000

    neck head discussion nearly react

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
