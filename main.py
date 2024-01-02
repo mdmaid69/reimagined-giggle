336762e46cebf623bf80d5af310fdada91d8f54b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:46 +0000

    wrong eat drive necessary plenty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-  def reverse_list(lst):
-        return lst[::-1]
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
