d9ffa7d64cfdeb389d8ebc8e103f9c8299321492
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:40 +0000

    contact call cross understand bringdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
