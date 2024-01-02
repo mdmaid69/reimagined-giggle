cf66686b63e6e64ebb31ecdc3951ddf6a782d4e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:48:00 +0000

    blame bad talk hungry minimum

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_inverse_hyperbolic_sine(x):
+        return math.asinh(x)
