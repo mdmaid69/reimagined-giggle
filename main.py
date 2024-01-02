0183a96820f8ade4da12a2719ba34c8851d61136
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:53:48 +0000

    short battery spin expression basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_unique_words(sentence):
+        return set(sentence.split())
 import math
-print(math.pi)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
