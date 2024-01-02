6d0a41df85ff7aa9be6e7dc6b3a832a371ce2b14
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:20 +0000

    genuine storm knowledge probably platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
