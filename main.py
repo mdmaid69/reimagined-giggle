3eb03a9ab038daade2df96190c7f26fb6633ffbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:44 +0000

    ought obvious entrance turn news

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+  def convert_to_octal(n):
+        return oct(n)
