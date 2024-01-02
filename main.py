93d58993dbf915f07b66ba96a7c64d4fb5a98536
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:03 +0000

    fond announce by long among

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
