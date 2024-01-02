bfae2ae7c70603f63dd44bd746e245bff2e6436c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:28:08 +0000

    off hit middle could lesson

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
