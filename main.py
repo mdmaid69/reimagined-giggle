ffa81cd2123e058cec51d837e6ed1c268ea05c6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:07 +0000

    test stop tax exploration comeround

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import math
-print(math.pi)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
