e79223b98ff1c0db817be04749d56c6d153d0fc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:51:39 +0000

    film of nervous whole brief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
