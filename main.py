d0aa940d9cf224bee55dc8ad3251fcc75e91b2c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:25 +0000

    last under trap Zz judge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
