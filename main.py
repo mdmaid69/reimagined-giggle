36f27e8f061ecd9da0c0c2e5543902390cd7e219
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:14 +0000

    Ll brain priority soil interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import time
+def get_current_time():
+        return time.ctime()
+def convert_to_octal(n):
+        return oct(n)
