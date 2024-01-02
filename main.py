b6eadd81624abd71d251e094e84f81d7a3931fc0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:50:06 +0000

    spring voice show level answer

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
