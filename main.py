e0de9ec1db94946f72bb28730ded3c40784d4b50
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:08 +0000

    means ability distance above trick

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
