effb422ded3fe99c717c10fd8d83c8c587ecf013
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:09 +0000

    him area hope character communicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_floor(x):
+        return math.floor(x)
