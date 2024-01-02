f87f71a6c4e24f9aa3ecebfe589ca0be6602fb62
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:31 +0000

    relax term whether anybody Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
