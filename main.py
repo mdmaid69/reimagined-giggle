d66f0d72ff8758231b18be422872ad919db458e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:03 +0000

    opposite regard tie snow pool

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
