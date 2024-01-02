eb9fdbc012ead740cfced91f08bee6be69f44f5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:53 +0000

    foreign rub balance everyone comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
