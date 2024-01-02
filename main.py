bbb0ae0017182957d2dded35e30245e42f6f8a9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:22 +0000

    understand interest action bet advanced

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+def exit_program():
+        sys.exit()
