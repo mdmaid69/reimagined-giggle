78533abb9b7913dc9fa26de1192b953e436ad592
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:37 +0000

    male formal oil result reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def get_current_working_directory():
+        return os.getcwd()
