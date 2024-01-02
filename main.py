561e781f91b2e485ad214515635d216f17f7ac17
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:46 +0000

    hair hospital thank dangerous level

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def set_array_item(array, i, item):
+        array[i] = item
