76630db2c859741267b26dec5229e175690f13a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:01:10 +0000

    trade guess opportunity middle age

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_speed(distance, time):
+        return distance / time
