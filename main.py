d1d3bded4e78a8f34c905fb3c0fa4ad6addf34f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:09 +0000

    sure couple discussion period Ee

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_force(mass, acceleration):
+        return mass * acceleration
