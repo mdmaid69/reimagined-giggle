a93ddfa61083397ba8ce078b59fdf1a053fe3e84
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:00:54 +0000

    high talk lock swing ability

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
