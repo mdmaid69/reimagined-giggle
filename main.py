55f3fc8aafdc9940e3f15e5b257ae5307b59a93d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:36 +0000

    imagination picture general stupid state

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
