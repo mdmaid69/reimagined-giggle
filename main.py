af149cb3b4e5f900dd121fce6cca197b4a1348a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:25 +0000

    none breakout account culture trip

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
