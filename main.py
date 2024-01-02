4c9b494f54e4d6778bae0d66a32536ddc19fded2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:50:26 +0000

    paper Yy wish far what

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
