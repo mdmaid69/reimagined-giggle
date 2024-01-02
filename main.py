e0b8993b02a236fcdca00a812dc8a3f3517c7a0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:50 +0000

    radio neither steam probably load

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
