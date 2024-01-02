b27bfb02a35fc2ed507a0567f1ab23b588c7831f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:19 +0000

    warning beneath convenient risk cheap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def append_to_array(array, item):
+        array.append(item)
+def calculate_density(mass, volume):
+        return mass / volume
