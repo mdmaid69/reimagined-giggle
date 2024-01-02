2ec6550ca448c1652be3850626b6caf3eb12fbf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:43:04 +0000

    inform freeze personal rank model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
