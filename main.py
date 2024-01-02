155f8e97f2720fea83103be79b995821e4ac3a82
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:11 +0000

    scream everywhere storm fairly allow

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+import tensorflow as tf
+print(tf.__version__)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
