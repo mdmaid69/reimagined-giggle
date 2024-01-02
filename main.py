27db05f3dd7cde03bd07559254375c1350c0b034
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:52:56 +0000

    clue yesterday tidy sharp too

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
+def calculate_acceleration(speed, time):
+        return speed / time
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
