98b8c7d7e95d18c39f62b500b0939e2c6988b138
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:57:33 +0000

    whistle wind young the result

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
