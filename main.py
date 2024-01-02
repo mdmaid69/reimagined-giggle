9724326d328e2e0f49322d8ea1cb03222fe53e49
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:47:24 +0000

    office general active deserve foreign

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
+  def multiply_numbers(x, y):
+        return x * y
