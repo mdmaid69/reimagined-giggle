2a85e623bc52e7ecfbb62a21c0e5be5b333cf168
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:30:41 +0000

    free sense grow angry stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
