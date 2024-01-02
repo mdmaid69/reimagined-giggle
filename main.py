0605dc9463bf8eef94a143942c353031aa5cf22d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:19:59 +0000

    unfair rid prize wrap work

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
