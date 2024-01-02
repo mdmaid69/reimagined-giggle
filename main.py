8e022a3c30b065d3ea06f0d7c1c5344565698bff
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:34:24 +0000

    message regret now storm truth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
