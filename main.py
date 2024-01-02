4fa88124f352f64147666317f41b97e736cd71f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:40:00 +0000

    push street like basic cinema

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
