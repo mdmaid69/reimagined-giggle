6cd8dd230adef9a6c5b4ca7f1835a9e8f06efd5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:50:38 +0000

    spoil claim guess look service

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
