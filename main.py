8cc067897268681d6d6de3f4620e8283f7640af4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:24 +0000

    market turnoff coat convenient vision

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_as_list(array):
+        return list(array)
