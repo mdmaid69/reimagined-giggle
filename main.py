23ba53d514dd5d85589365f1eef4962488929d80
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:01 +0000

    succeed since meanwhile shallow single

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
