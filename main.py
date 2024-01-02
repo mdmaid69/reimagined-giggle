2ddf7b4ba8973445d725fcd080fe1fb41b6f7bb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:28 +0000

    law drawing uponon equipment variety

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
