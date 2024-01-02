b49a463df9b15bcb7ad5568516a85f1d15161306
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:40:44 +0000

    wash cope chance pleasant next

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def calculate_volume(length, width, height):
-        return length * width * height
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def find_max(lst):
+        return max(lst)
