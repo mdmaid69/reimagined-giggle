b1971ba206a55c7ad7097c87c5cd2227cbc91253
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:00:30 +0000

    not fit there kind physical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def get_array_as_list(array):
+        return list(array)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
