bf581c846ab27b5776dea369cf2fde86f5311be7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:35 +0000

    mess it top disturbing list

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_float(array):
+        return float(array[0])
