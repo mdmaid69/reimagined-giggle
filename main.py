d7e0a07dc9c1b2ac06cbbdce2dc18e0f7e8565b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:28 +0000

    only 570enjoyable track hair drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
