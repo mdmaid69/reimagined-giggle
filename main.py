546c2d1a5d622bd97b1163190a3e7b2d8405cd87
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:50:55 +0000

    often quarter reach rub contrast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def square_number(x):
+        return x**2
