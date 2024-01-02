fe3b0a804465415bafb225d1bc1d08b6aee3391a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:09:06 +0000

    favourite down lookafter while female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_priority_queue():
+        return collections.deque()
