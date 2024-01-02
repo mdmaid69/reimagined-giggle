7d357e0cd021af8f9887d78a82f980d999aa91b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:51 +0000

    later boat hungry deliberately pretty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+import os
+print(os.getcwd())
