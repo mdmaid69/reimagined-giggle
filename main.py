b2c5782331b8aa5c906f35e7a087ef340e16b995
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:45:32 +0000

    rather interesting brush meanwhile people

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def multiply_numbers(x, y):
+        return x * y
