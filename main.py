2123a81df36b36dc9d5aeacac17a1c0686a090b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:42 +0000

    treat shut confusing corner stiff

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
