a6d14eb7dfb049707ec129dc8ff7c5bd33785b75
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:52 +0000

    comein makeup confident shame other

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
