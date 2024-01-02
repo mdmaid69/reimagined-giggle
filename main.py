6907416c016a73ab60c151f2a06e1e2a7fe72b77
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:43:24 +0000

    check further alive hand spin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
