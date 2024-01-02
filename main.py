b7405b434795c4415361def63089424953fa5c4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:55 +0000

    risk before string your lucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import platform
+def get_os_info():
+        return platform.uname()
