3cf28b3614a590fec1af9a1cb3a3d0309c40c3da
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:06 +0000

    congratulations escape loss favourite satisfied

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_unique_words(sentence):
+        return set(sentence.split())
+text = "Hello, world!"
+print("Words:", len(text.split()))
