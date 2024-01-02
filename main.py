7df4030756bdc5cd7f5a97a9892f004e53b3876b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:46 +0000

    time suit suit contain Tt

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def multiply_numbers(x, y):
-        return x * y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
