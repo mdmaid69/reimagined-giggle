ee7714e8f2578de4e6fd1b6c088ef90b6932369d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:50 +0000

    twist joke card awake arm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_work(force, distance):
-        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
