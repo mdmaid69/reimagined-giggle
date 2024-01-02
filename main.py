85247627e4a031d6abf731fe06058d9f9856352f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:33 +0000

    clear will sky bite instead

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
