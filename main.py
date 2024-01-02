3e4f975b6f4866f931bdaebf90b8258e606ff7af
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:16:35 +0000

    wild oil appeal brush escape

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def square_number(x):
+        return x**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
