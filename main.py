4d041a8b35529486c5aeebdf960c5bb6e2c845bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:50 +0000

    comment reject concern glad rate

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
