ad9fa8c56276cd0bba9a1575b9d5fc3367f9339c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:16:04 +0000

    recover instruction stay excited pleased

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
