cb33c333a7cc97e5ed051d7934068998d082ab69
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:08:31 +0000

    whisper path injury kick beyond

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def find_min(lst):
+        return min(lst)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
