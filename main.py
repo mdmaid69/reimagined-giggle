cbf976bd46aaab8dedf1cc51f29f86478ad3dd4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:12:30 +0000

    spill hospital big pronunciation mistake

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
