b35bd61284f278b74fd7d9a8e54a0046a15b8197
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:07 +0000

    Rr behind smooth poison control

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
