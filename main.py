330a39af66fd7d58fac1e22fa8162eb353489a18
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:46:29 +0000

    sad question law game drink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import tensorflow as tf
+print(tf.__version__)
