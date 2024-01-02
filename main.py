d12de60fbcc21afecc95cbf17122748a414ff450
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:10 +0000

    justice argue once proof represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
