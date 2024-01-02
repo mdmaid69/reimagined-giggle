60aaa303143c754bfc0f4ce00461b1182505088a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:34 +0000

    music neat children wall tap

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
