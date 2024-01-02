4dea5e85f5f4ede5758d0aa9cd021bdfa95aa4d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:52 +0000

    lead concentrate chief keepaway begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import collections
+def create_priority_queue():
+        return collections.deque()
