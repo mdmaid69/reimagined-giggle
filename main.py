c73e2a2602a396a89a50e5fc2e6e768c6005948d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:46 +0000

    left mine freeze quit gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import sys
+def exit_program():
+        sys.exit()
+def remove_duplicates(lst):
+        return list(set(lst))
