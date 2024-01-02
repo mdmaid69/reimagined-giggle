7f06286ee44eacfccdde5e18d0ef0b3bd281ec26
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:53:10 +0000

    improve rubbish weigh drive whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
