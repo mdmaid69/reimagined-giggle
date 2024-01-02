a2f0971f0a7e2d018add2ca5e0380acd0d5e5c93
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:57 +0000

    put guilty me ilovedicks maintain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def find_max(lst):
+        return max(lst)
