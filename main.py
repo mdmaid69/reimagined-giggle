7c086daff9dacb0f957db69758d1e8645b83fb9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:58:25 +0000

    sick suggest fine live message

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def count_elements(lst):
-        return len(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
