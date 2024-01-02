4c25ce24e06e1c210dfce4a5d130084fafd98e02
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:34 +0000

    murder breakthrough loud pull mistake

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
