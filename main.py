82273e70835613cee777a90109be9825e03d0474
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:15 +0000

    Tt among rush mix road

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
