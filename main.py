d42a4fbe085b26a90f2d5bb8d8b493d5ec1f8ece
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:04 +0000

    peaceful bit general marry replace

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
