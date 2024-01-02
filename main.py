26dc8a3958fa12b8624048d8578a751e2ab10182
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:39 +0000

    doctor still gentle simple computer

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_volume(length, width, height):
+        return length * width * height
