8ae532c9f1db067313c1c7c933d0b0d9bbe9210c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:07 +0000

    unable label direct floor usual

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
