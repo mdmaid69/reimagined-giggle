1d0421e77bcb073bc134664ad0ba0abcd716d8fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:45:01 +0000

    learn fun issue bringback board

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-def calculate_pressure(force, area):
-        return force / area
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def is_even(n):
+        return n % 2 == 0
