56f04473e88c932d77def2b510d975cbdfe3c3f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:54:15 +0000

    opportunity statement not finish obtain

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def find_max(lst):
+        return max(lst)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
