2e01c1c0a85d0d4d8a64b1857cc008f37886afc0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:03:05 +0000

    party touch unable fill healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
