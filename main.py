7119e0f44cadb26d352a62f794a6f1bc550154b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:23:55 +0000

    commit shallow follow collect calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
 def calculate_roi(gain, cost):
         return (gain - cost) / cost
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
