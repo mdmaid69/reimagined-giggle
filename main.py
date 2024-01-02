e7a15605f025e9cb4974c6656450f41cfe09e12d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:42 +0000

    correct exchange 572enter but find

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
