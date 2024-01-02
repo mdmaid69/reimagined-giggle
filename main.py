7f5ace2c57f2b3719516537981b041895fd4a4fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:47 +0000

    protest guilty Gg next pipe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import collections
-def create_stack():
-        return collections.deque()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
