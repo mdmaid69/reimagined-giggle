bd2e39c0db6922ae17d0c666e0e2a8dae7305eb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:08 +0000

    tomorrow sort promise kind between

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+from collections import Counter
+print(Counter("hello world"))
