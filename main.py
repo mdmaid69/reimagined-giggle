2ce675a53a6e01338513ab7070bf0de429fac66f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:33 +0000

    greeting model about move report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
