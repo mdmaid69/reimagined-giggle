edd9f7716db1350d2de4d24241ec01144ee1efaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:30:14 +0000

    will scene exciting thank win

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def cube_number(x):
-        return x**3
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import datetime
+print(datetime.datetime.now())
