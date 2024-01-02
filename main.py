f9a10dd982f714a63f7d8526395396df633440d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:12 +0000

    what degree commit leave keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
