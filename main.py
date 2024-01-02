41f11739f7d47018df56cd33508660e2d2ff7ffd
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:27 +0000

    form deal over guide letter

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def sort_numbers(numbers):
+        return sorted(numbers)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_error_function(x):
+        return math.erf(x)
