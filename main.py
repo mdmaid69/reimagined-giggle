ee63e90e2ff5d14bc5a08530453850ad5c8303ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:58:57 +0000

    throw while without cold approve

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import time
-print(time.time())
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
