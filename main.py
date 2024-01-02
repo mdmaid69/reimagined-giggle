a5572f3a7aee54981bf8dbd4ab542834e1cd70ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:49 +0000

    flight majority pleased store meeting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
