f972f6bf4444ff1bd7707244fff8dacfd35c9b82
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:32:57 +0000

    bad serve suffer typical right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
