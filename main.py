8638de7ed7169485a09021d3ce8bbe005d8e5220
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:58 +0000

    physical majority well mood aim

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
