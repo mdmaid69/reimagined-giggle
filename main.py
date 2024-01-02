943167d50fbbe60b0924ab581836347a9f7182e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:27 +0000

    basis mean brown nonsense somehow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def divide_numbers(x, y):
-        return x / y
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_set(array):
+        return set(array)
