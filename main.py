c4df309105ced3270c9f063515c4c9459aaecbc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:26 +0000

    surprise famous ordinary opposite after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def square_number(x):
+        return x**2
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
