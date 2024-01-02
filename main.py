2ef03ba01021ca6d788a418b5a4858a63e484776
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:06 +0000

    passenger near fat reply song

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import time
+def get_current_time():
+        return time.ctime()
