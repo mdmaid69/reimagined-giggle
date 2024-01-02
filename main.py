4f606725a9d4648c225d967cb73b1e3c7410f3bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:15 +0000

    alive disappear remember equipment teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
