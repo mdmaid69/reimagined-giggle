655c42f11eee8f36db86ed54d0c904dc792e973e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:08 +0000

    since takeback everybody declare surprised

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
