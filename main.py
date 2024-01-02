f453e47246a286729df1998c9dbd6c46c6f20343
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:41:34 +0000

    search machine press sense straight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
