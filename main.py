e1289caa696437d69428827e431e9035919f92be
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:34:30 +0000

    perform question achieve concentrate exercise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+import os
+def change_working_directory(path):
+        os.chdir(path)
