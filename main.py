09e496794f83d9edc8b16ffb5900d250d5c4ec61
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:22 +0000

    stare achievement cruelty hurt badly

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def append_to_array(array, item):
-        array.append(item)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
