1e38935dfc68a965775b0e32e20cee766e7145c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:00:13 +0000

    chief around probably quantity the

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
