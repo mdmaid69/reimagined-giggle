820ab1f4e8e8ef09dbe49de0c5d8545370315f25
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:59 +0000

    town sometimes away noise keepaway

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
