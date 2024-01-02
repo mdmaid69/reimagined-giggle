353b60a00cceed963a290a247e23bc4a9d20dc0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:07:41 +0000

    maximum before cover talkoutof gather

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import sys
-def exit_program():
-        sys.exit()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
