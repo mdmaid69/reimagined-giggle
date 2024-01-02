283d8f7af2ff12ed9017ebc83a7007f0a9cb2661
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:35 +0000

    pronunciation likely label coat weak

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-def count_words(sentence):
-        return len(sentence.split())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
