728868292818c5ce7f99d2bfc1da8970742ff5d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:48:32 +0000

    thorough 564encourage marry pronunciation church

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_acceleration(speed, time):
-        return speed / time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import re
+print(re.match("h.*o", "hello world"))
