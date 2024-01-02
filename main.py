4ba8d562def6f0c78a201b712cf0b310e58204c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:26 +0000

    giveaway sick nice state bath

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import platform
+def get_os_info():
+        return platform.uname()
