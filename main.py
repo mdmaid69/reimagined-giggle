2cc33659dc68eb89f0c38a908bb0a4d023acfe3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:43:40 +0000

    supply pile type valuable alone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
