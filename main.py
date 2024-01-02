5aa58b8d84d0ae48c300dd69ff61dd92605b7cf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:09 +0000

    expensive cruelty noise theirs aware

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+print([x**2 for x in range(10)])
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
