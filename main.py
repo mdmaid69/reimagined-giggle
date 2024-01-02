0aaf4ea41bb685f86051e635870cab281a3e3b58
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:07 +0000

    oil rain go talk natural

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
