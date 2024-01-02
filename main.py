2c104a52b5cd15ee08a75e2cfba3e2eca46d80b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:51:50 +0000

    fear transparent talk cover exchange

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
