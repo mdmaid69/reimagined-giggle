e3bfbfe64c35022a556a6de1e4b484b6fd27d28d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:50 +0000

    drop paper hard remain display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
