02c92e769b9fc087e8c5929c2aa3033cd549ad03
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:57 +0000

    Gg occasional pronunciation human chance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
