9d45074eaa8782860e8e209184fb40431c7fe669
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:49 +0000

    hole depend sticky embarrassing completely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import collections
+def create_stack():
+        return collections.deque()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
