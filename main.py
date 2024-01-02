81232e3f188e8915d62448e1fee1c8f91fb4234d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:59 +0000

    remark keen advantage fact forever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
