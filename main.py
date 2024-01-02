c73b7c6deb013b6a722ab7e99f4e2735eeb0d170
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:17 +0000

    control adjective also fat chin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sys
+def exit_program():
+        sys.exit()
+  def reverse_list(lst):
+        return lst[::-1]
