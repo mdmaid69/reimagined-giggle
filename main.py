bcab4ad6efeb047993d8945205235e074a82c43b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:59 +0000

    trap education passage hit grade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import platform
+def get_os_info():
+        return platform.uname()
+  def count_elements(lst):
+        return len(lst)
