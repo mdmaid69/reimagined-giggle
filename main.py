76a84a6d5318826bc79075361dd3d072d6d87b68
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:34 +0000

    friendly add whoever form gap

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import time
+print(time.time())
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
