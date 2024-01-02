3c88e92cc4720aa7b917a446593d1ceb9f79ff60
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:19:53 +0000

    569enjoy towards pick temper travel

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import tensorflow as tf
+print(tf.__version__)
+  import sys
+  def get_python_version():
+        return sys.version
