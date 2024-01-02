e58917edb86a3884ad92b435473ac106a135ebc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:00 +0000

    deserve funny solve peace school

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import datetime
+print(datetime.datetime.now())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
