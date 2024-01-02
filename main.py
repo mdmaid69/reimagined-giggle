cd01f8c1367ba72d8b6672bf5fb8fa9366286e1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:51 +0000

    relief university throw process community

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
