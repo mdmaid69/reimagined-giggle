5e92df93195e29ef6a140885d8b586404f4ade8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:17:44 +0000

    object drop community total influence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
