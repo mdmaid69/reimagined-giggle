4b8f446f673178f08a2c03a7d601be2497fc1abf
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:04 +0000

    string love summer go threaten

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
