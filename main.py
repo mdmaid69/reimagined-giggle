8f19c35b9fdb3ea07b9179b4622aa6667d7d93e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:52 +0000

    inside beat law correct few

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-print([x**2 for x in range(10)])
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
