66df8bf6d8e4361e927ca4e7fe032d7844f78fa5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:12 +0000

    solve cover shallow discussion cross

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
