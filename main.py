64475ffe8489a38ad0cc8a5fdfafde61a51f042a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:12 +0000

    mistake track view natural noisy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
