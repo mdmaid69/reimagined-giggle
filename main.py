108a3f119b448b45abe045324a1b73caba951882
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:26:50 +0000

    hit proof sound while vanish

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
