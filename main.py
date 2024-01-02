07b56e0077293a24351ba101c4eb8559e9432d41
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:40:28 +0000

    earn unfair do couple exchange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
