8c4699dcccfa2988241c7ec2b1a9d39ffbd38018
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:37 +0000

    everywhere start Ll raw office

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+def calculate_density(mass, volume):
+        return mass / volume
