d79d9ef2eb36f461b42ea690ecc82199ac3fe7bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:43:08 +0000

    unhappy small light invite occasional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def clear_array(array):
+        array *= 0
