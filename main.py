a028d2d1c38a0eddf1067cf81af45a5fd531688c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:58:30 +0000

    spite entry slowly measure family

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
