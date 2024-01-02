1623af93a9e6b7cce1b9ca857215737e6cf620af
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:11:01 +0000

    new club angry probably set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
