5f15914802eefa38535c3adcc2553fcb33074d1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:25:01 +0000

    meet complete balance class purpose

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import math
-print(math.pi)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
