d4fd180347d0daa87088248b5fddc9f6e581e86f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:41:07 +0000

    dry worth actual well suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
