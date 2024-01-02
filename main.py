4c006fc25456d6ee862dd03ae5fb127e839195f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:48 +0000

    common shoulder star strength prove

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
