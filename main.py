48f167b239155f458fee7eae368d9c6004fc1590
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:09:41 +0000

    middle collect suppose furniture keep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import re
+print(re.match("h.*o", "hello world"))
