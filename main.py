7fd836a5591995b5a89ecf0d25e55bd86079370d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:39 +0000

    trip little return 573entertainment partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
