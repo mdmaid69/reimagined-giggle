75fdee75c0e550b79e7449f36e2bfa1dc4e00acb
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:19:08 +0000

    science show perfect pretty operation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
