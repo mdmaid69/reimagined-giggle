693c4edd70170ea741d879b9d741fd9fdfa37885
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:29 +0000

    free successful agree behind prevent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_current_directory():
+        return os.getcwd()
