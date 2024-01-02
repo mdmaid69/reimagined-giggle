f033ff15894a5ebf49f0ee6d202e3a7376dd73ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:50:35 +0000

    race place from taste ear

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_sine(x):
+        return math.sin(x)
