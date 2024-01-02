a5524574610adb8ff2fd7d64dcbe8bb82a048286
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:37 +0000

    check spill ourselves emotion set

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
