d56ff41224fe6fca08870d3e20e6cdfcd3d9d55a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:15 +0000

    mend luck clue thirsty knock

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
