be046631aae96069afabdec69c0ec1565d50f6ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:42:11 +0000

    bother recently succeed safe offer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
