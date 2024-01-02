a2b61ee96164537e1920047c24ef851ab6e7d2b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:30:56 +0000

    possibility pair shock scrap bar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
