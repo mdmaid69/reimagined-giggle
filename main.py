b14472d0622fccdd41af1b2b66998e50f12c03c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:41:29 +0000

    runout taste swim today everybody

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def find_min(numbers):
+        return min(numbers)
