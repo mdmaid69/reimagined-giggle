209ec4336e6b9c8bf06c8ff6f9db1bff6712cbfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:38 +0000

    towards influence note plan circle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
