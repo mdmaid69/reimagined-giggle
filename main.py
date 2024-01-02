960d155e78afa39ed9098a0b06c3548105d890ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:21 +0000

    sea settle thought hit contribute

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
