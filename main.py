eaced2997e240c638ba1edcb7e74bea715e4ebf2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:59 +0000

    flat right pull check replace

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_as_list(array):
+        return list(array)
