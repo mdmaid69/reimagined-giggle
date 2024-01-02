56552fc0525ea69ae301e5c1a6c91a611e503269
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:29 +0000

    soil friend area attention when

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
