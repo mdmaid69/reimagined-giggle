69a952143db19a1d5bc1cd9b4f70376684863dc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:02 +0000

    breakdown description actually more miss

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
