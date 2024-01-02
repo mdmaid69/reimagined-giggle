b397449af3d5b82bbb2fa624c35e4548922e072c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:39:52 +0000

    pressure destroy Tt little question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  def sort_list(lst):
+        return sorted(lst)
