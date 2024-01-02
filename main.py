27e261506d6a5ecab6276f43ca7345c4bba5e580
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:03:51 +0000

    weather hard challenge ilovedicks challenge

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def add_numbers(x, y):
+        return x + y
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
