14c6e2048f7b74169cb3d5194cf1673dc9877a31
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:14:10 +0000

    list bring off hospital plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def divide_numbers(x, y):
+        return x / y
