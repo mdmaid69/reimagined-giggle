4ca9833b0978df27899e61de60f07062cbf0d287
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:24:01 +0000

    hot nose centimetre gap disapprove

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
