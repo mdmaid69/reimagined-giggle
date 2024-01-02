ab7eb0bf54c6c60041c3e4a485157905a7837962
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:26 +0000

    whole plenty dark badly collection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
