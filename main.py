18a0fe7ed17fa9049732af032823d05aaa257697
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:29 +0000

    clean fight marry forgive plate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def create_queue():
+        return collections.deque()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
