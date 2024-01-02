851010219199b8385b306adbb1ed84b4e4f646d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:23 +0000

    Bb Cc boil summer fall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
