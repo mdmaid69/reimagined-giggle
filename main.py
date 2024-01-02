ccc0d5dc9916a4caeb5e7510a2ec32e637f32b73
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:52 +0000

    fat that block choose attract

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
