098593169dfa9036364e39f01b19c34afc8f8895
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:47:31 +0000

    ourselves ring thought Uu check

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
