298b53b1bbf0f2d305dcf69b54f6534dbcc2e449
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:18:41 +0000

    connect excuse meeting visit exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
