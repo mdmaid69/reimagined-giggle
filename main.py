d9d401bf69345f00aa4e0c341dfa2a0a1b06df4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:10 +0000

    wash plenty slice right quiet

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
