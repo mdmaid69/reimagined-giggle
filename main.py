cda2f153af88e06c9bd682748abcba1face78e87
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:34 +0000

    table over community stop calmly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import sys
+print(sys.version)
