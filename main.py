b940ed4c4f357fac0ac931b1477711f8aea29358
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:29:55 +0000

    lot whatever soon completely and

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_string(s):
+        return s[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
