9b0ddbcd8d6f6735c9263cba847667dd7e77771f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:54:46 +0000

    praise radio even excuse runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import time
-print(time.time())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
