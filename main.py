1cc5394ddf30ff96ed48d43db77b82ed83c5db7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:51:49 +0000

    act workupto division opponent false

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
