5079af0188eaab80d114e2d07ef97e2deb1bb7b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:34 +0000

    nearly bar drink rid suit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
