58acc5796a7c33efd5b85e36e3e99b02178b12c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:40 +0000

    copy quantity cruelty snow danger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
