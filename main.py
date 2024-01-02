f428468486496af2ba823b576e93c3a5153a07bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:21 +0000

    explain commit Kk bring majority

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
