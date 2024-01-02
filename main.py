ebe56d336320b27c2a16f8ba44523fc7b2de1760
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:43:02 +0000

    right care chin fold store

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_item(array, i):
+        return array[i]
