2a13f90d4cbc067285080496a4df702b182f4f2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:57 +0000

    tool some comein steal space

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
