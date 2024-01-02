0ad4e9d6ecc116c985ff0ff74f543ab1cfedb65b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:42 +0000

    themselves apply new talkback science

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
