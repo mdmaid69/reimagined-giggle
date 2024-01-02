2add2245044da65dcd70bc9cd49a2203672b6682
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:40:25 +0000

    necessary wealth reliable mixture blame

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_base_name(path):
+        return os.path.basename(path)
