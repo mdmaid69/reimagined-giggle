fb114b61e3216a5fb391d40f4c5a57cf5515a04a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:21:09 +0000

    join actually chief model whom

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
