972538de49ed2a587e4bc7552419000713b3faef
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:10 +0000

    mine reply hate contents address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
