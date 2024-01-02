e0a8f5f81310a96cc9ad5b36d53184735c944458
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:31:12 +0000

    transport little power fairly left

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
