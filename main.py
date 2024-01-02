1c0f1e2926e7fbf1045bd1b107962e816d636f30
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:47:30 +0000

    defend mind goon prove height

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+print(sum(range(10)))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
