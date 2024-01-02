2e503c3c5ef850976bbcbbe7798aabf84af0b277
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:10 +0000

    miss remove public suit basis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
