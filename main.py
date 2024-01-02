7b550ef5b2868e4985f980765c60e17ad189dadf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:46 +0000

    prize challenge fill rank afford

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def convert_to_hex(n):
+        return hex(n)
