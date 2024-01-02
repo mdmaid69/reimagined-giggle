fbf0fbbd3b3d5422bcce81e53eabb3d780ac1dbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:20:37 +0000

    division bank spare unfair shut

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
