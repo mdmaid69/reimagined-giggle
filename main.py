2425c3e44f3a804a8d6facd00efa94807c18da35
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:20:47 +0000

    goaway fail for also natural

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
