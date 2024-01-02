7ea409de53636b057eb06d3ae28a41baf7fc3d1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:22 +0000

    charge transport attractive where everybody

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
