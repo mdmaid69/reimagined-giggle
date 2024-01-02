487007fb640496a37577316a757761595221327c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:16:58 +0000

    shock thinkthrough original rub lose

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_error_function(x):
+        return math.erf(x)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
