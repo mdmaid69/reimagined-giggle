be2b79bd61bc734bbdb0c2ea6cbec484718743ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:33:53 +0000

    until throw medium bye adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_itemsize(array):
+        return array.itemsize
