b45c1cb676af2b18244e4ecdfeea8bdb99551a54
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:08 +0000

    difference coffee voice likely OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def square_number(x):
-        return x**2
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
