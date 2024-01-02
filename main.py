de6b25d23532be27daac1ad4e48476721fc3b1d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:33 +0000

    school over tragedy wind edge

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import time
+def get_current_time():
+        return time.ctime()
