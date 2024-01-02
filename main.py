c94c2f67d947ea89cd129623d06f5979cc8b4e69
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:16:50 +0000

    development across brief meanwhile uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def multiply_numbers(x, y):
+        return x * y
