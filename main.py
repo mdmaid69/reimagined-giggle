b13bff90f500f65588b68e6d50ea97dea6c7bd0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:21 +0000

    order around arrive smile move

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_min(numbers):
+        return min(numbers)
