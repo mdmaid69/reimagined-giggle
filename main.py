932e16bf0aba02db8aabd5c656c2129df9ee5e25
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:05:35 +0000

    false keen within Cc number

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+def change_working_directory(path):
+        os.chdir(path)
