0b74db5e430e1b1e905f19bde5f004dee4ca591b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:39:30 +0000

    comfortable lesson description market earth

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  def count_elements(lst):
+        return len(lst)
