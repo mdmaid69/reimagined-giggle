e702fb06d5170df0a00bfaf97e3821e9bf9810b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:19:22 +0000

    unhappy Ee operate destroy runinto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import sys
+  def get_python_version():
+        return sys.version
+def find_min(numbers):
+        return min(numbers)
