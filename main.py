a7cd24b99994c1adc1baca5f38ccab54ef89cea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:55 +0000

    goup 569enjoy public vision there

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
