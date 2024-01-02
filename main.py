80493b4968f900547eebde851146c8dcde4b6a11
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:34:31 +0000

    there turnback unable anywayanyhow position

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def find_min(numbers):
+        return min(numbers)
