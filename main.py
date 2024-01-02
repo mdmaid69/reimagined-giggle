0b6c8a38f99c325869ad81f59e42ae8b467a3098
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:48 +0000

    train help deliberately sleep break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
