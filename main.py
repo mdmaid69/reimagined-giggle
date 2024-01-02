2a5a9ee37d6e39f82c284037841f5b1e9aec93cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:25:35 +0000

    within punishment urgent walk transparent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
