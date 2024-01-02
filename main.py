f2e4690dd4a8e4703201b85383cb3b91ef1fdada
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:29 +0000

    door pen slice sentence point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
