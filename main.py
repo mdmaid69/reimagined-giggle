24e57430450f0ab90507cf72a38aa917f81d9e08
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:36 +0000

    score key sure mean either

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
