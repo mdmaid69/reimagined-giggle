9aa0bf2af6bc6de90424b5830982819c3e3ab5ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:00 +0000

    arrangement pull person old aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def count_elements(lst):
+        return len(lst)
