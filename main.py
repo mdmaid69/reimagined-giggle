4e6d6b91f08f04453a564f1c57f158e2579e0d37
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:32:25 +0000

    perfect art through appear along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
