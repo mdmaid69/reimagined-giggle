411bae71c31fa9a7e7619bfe21d6572efd61cbfd
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:44:26 +0000

    somewhere spray search to while

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
