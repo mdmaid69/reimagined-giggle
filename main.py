5645f6eb140a048ea8521d88b39691e533a675ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:34:08 +0000

    hurry slightly concern Vv succeed

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_area_circle(r):
+        return 3.14 * r**2
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
