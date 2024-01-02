15cfa1518b04d8141c783ee1c2eac2ee0ecf773e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:31:56 +0000

    several inquire themselves Cc match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_max(numbers):
+        return max(numbers)
