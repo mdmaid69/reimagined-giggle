cac862d9a26e8b6a9b6e57bedf996f87be1f84e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:24 +0000

    cause turn along connect destroy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
