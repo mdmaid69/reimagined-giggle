9af1e197b275e0350196eb6df21c4418f681b9f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:54:57 +0000

    effect impressed might future afternoon

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
