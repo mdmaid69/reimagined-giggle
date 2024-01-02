4b587a2f883a5f5a890c7de77e64983bdbd2ff11
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:46 +0000

    aware sell study organization issue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
