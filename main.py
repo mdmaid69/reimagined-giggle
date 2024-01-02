29bca4a57cc7a4228780f65709baac88d43a68df
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:19 +0000

    scrape emphasize say safety male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_combinations(n, k):
+        return math.comb(n, k)
