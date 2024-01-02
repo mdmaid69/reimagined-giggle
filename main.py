8e15f23d2c19754822c2f304b79ab3b870996be4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:49:42 +0000

    yesterday chat comeback splash clear

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+from collections import Counter
+print(Counter("hello world"))
