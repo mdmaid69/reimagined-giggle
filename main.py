3b9b86f940f3d5872f9542d5793f8f6433d67823
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:49 +0000

    somebody appeal factory little workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
