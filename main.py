cacefe31f91f8839e0b604d39a44895313f1e3d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:05:39 +0000

    learn level left need surprise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
