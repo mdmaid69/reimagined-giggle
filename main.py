707b925de7b90cc2f41df390079f72d465ba6861
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:00 +0000

    fear zero divide wheel be

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
