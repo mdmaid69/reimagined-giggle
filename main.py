fb36cd722f362ef375ce9105c6c6dc8b37514f36
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:18:30 +0000

    refer operation arrangement wind only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
