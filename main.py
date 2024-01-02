e237ad9242254d86b9b02c0e9bb5427ff01bafcd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:04 +0000

    traffic entry light she light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
