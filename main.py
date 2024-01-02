0e23148e9adfa2ca851d894c4b27cd76ec527944
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:33 +0000

    wake love will study constantly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-def count_characters(sentence):
-        return len(sentence)
+import tensorflow as tf
+print(tf.__version__)
+def remove_duplicates(lst):
+        return list(set(lst))
