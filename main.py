cbffef955cac6a48bb35c484e0523c2302937d70
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:13:10 +0000

    keepaway invitation comefrom sore journey

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
