722e19dac3551f10d4d8ddd038bbc594aaad8e2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:12:10 +0000

    comeround generous entry discussion frequent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def square_number(x):
-        return x**2
-x = 10
-y = 20
-print("Sum:", x + y)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
