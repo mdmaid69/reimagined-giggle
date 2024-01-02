7ba92b4548edac2e4ffe34bdfb469c839c851da9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:35 +0000

    damage loose search crime culture

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
