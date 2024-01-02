e6337d84550b77f1781cf28fb6814ec2694b6d28
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:52 +0000

    smell moon side shout tool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+x = 10
+y = 20
+print("Sum:", x + y)
