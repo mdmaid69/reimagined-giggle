f519808d57a28d72a3060718af7400ab508ce3a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:45 +0000

    instant 570enjoyable yourself hurry perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-def reverse_string(s):
-        return s[::-1]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
