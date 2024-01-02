96eec10b840a62d76716a1f87de15137fd8b5f88
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:48 +0000

    confusing music alone mention complete

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
