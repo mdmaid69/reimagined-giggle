4b355fbbb299c648a53ebede88957114d07e5380
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:09 +0000

    accept inquire computer effect away

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
