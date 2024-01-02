ca87943a6a2ce4a2bffa5b493df4ab949128bc4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:21:54 +0000

    home judge underneath kind below

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
