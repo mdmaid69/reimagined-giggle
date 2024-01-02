0e70c331a6fa985c893acbe218b9ac5400f2be40
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:43 +0000

    owe trust split once shelter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
