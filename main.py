c0562263b0b7195fed69896409e0b88528301815
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:06 +0000

    near shock fast coffee produce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
