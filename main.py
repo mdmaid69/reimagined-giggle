d3fcae8468f43024d391aa0b6d309cdca6854e69
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:06:10 +0000

    suggestion zero flow turn perhaps

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
