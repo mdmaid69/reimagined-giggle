57ebd5109f720e42c9b7d77c49a2d4bd2d19a511
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:01 +0000

    build blind listenfor will fierce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import collections
-def create_priority_queue():
-        return collections.deque()
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
