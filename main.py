31ae381d03064524613e7774d9cd3aaf28cc13f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:46 +0000

    wealth from job statement rid

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
