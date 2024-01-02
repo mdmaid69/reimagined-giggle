dd3dded23db151dbf2222c79980a148829a9426d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:34 +0000

    respect study address war breakfast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import time
-def get_current_time():
-        return time.time()
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
