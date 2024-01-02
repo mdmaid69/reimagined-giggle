a366d15c2ff17ed55e40d9144a9d939a818f1cec
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:28 +0000

    react blue object anywayanyhow wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
