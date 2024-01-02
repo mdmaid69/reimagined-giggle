d860298ac76021c8eeb8760cecd12ca93423b3bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:48:26 +0000

    main turn gather giveout dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
