9bd623ca58a4b93b7c7e25175a373ff8f19c516b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:22 +0000

    let character store office figure

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
