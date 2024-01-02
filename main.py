b5427c997a021d7206bab99fe7697d05b1dae8d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:12 +0000

    form blind excited convinced stupid

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
