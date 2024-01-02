273a88cad0186b6da0e02eadd9aa5d462d7ae562
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:28 +0000

    square label expression weekend why

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+for i in range(10): print(i)
