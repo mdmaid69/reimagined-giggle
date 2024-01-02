c58d97b185720da3178f5a848e21004679d53a6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:44 +0000

    suckme complaint comefrom wave see

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_as_bytes(array):
+        return bytes(array)
