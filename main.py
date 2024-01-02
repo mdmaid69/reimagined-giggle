1b0383db5c80429607f6c7bd7625bcb8812ee3d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:38:27 +0000

    advice goback(to) comeup jump workon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
