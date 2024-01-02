8ce2691c62ac06edcc4f54a594a78e185ba49d1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:23:13 +0000

    difficult any decide collection appearance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
