7c0f55fb69f5502c8a4f9e39e3014707f1dcade2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:06 +0000

    stick really money shake corner

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-print("Hello, world!")
+  def get_current_directory():
+        return os.getcwd()
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
