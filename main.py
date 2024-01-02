b926604aa939bb9760a952de001e6bd637cd5f7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:02 +0000

    build report issue check month

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
