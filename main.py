a01841b16e1801987b4e387c38c00681292a1f22
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:13 +0000

    keep fear cry on ought

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
