5876956fa283c781ae815b32fdbff46572a7d0f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:01:23 +0000

    slice previous high save responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def set_array_item(array, i, item):
+        array[i] = item
