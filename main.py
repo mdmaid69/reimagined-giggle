c1f8d31cd81ece0a5660b16ede7c1980d37ce337
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:31 +0000

    attitude big judge within but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
