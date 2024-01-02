e100e28c3cdc90c683cef52314daaf21641256f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:39:05 +0000

    shoot grand shoulder unless fond

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
