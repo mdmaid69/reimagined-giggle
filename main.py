7a5f8f00cecc8428d918981ea7c6af30fb17ac1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:15:02 +0000

    though lucky autumn tour shame

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
