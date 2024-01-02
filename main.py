2f2b9bbf9de2a3e144817c4894ef22fc58af09d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:06:29 +0000

    abroad now born whenever doctor

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-for i in range(10): print(i)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
