b12d9fb2b4f15e553fd8f387e7bb98e3b57e41d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:19 +0000

    only touch long welcome somebody

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
