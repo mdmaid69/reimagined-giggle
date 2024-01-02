5f74030f54114acf9e2cbb37c395e473fdb833e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:28:04 +0000

    about prevent comeout better brush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
