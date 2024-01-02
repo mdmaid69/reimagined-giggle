b7f6eadc08ce2e366447c5308514b2b69e2c0b49
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:41:00 +0000

    congratulations switch rush remark direction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def convert_to_binary(n):
-        return bin(n)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
