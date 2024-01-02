7cdcb4217a6de889657963fa5805d4a5d4e92437
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:25:29 +0000

    peaceful completely curious keepfrom hair

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
