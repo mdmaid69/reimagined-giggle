2ff3d13246e91d11391bb2a5211e5ccb3a3ea269
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:53 +0000

    hurry correct care tall test

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_average(lst):
+        return sum(lst) / len(lst)
