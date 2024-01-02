35cc6d1078823d9e159da1aec240349559479c9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:47 +0000

    invite bother university answer accept

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import numpy as np
+print(np.array([1, 2, 3]))
