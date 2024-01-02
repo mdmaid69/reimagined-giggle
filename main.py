e0ee4e898e36fd99542bd94244ba575597494b93
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:19 +0000

    definite burst acceptable creature hard

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
