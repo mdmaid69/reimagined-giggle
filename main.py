316223a1b0aabdcfa14338358768a35b0f5c1405
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:07:12 +0000

    lend thank block roof now

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_perpetuity(payment, rate):
+        return payment / rate
