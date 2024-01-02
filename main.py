4b67882a63da0f86ee40066ec9f4b160da56e080
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:04 +0000

    imagination surprising used towards drink

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
