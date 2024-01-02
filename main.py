02fbcce1fffb1be309d70958241515de4e8a4c39
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:16:08 +0000

    war ear scrap certain tool

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
