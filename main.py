0abdf4edb6ecae4127d284641d70fad10adcb14a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:45 +0000

    rank freedom meaning cross anywhere

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
