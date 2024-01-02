90fe6b12b6e3b6c86076cc5e7fdb2c18a71ed28b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:48:34 +0000

    what top common burst sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
