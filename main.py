a20a51426e8ee2d9355668f93d9b3f2a960f5f0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:51:15 +0000

    innocent condition modern kind fit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
