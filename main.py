e6bdd06ae82061601416a68dde96859811c3ae76
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:21 +0000

    own difficulty doubt runout under

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
