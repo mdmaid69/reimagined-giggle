dd813c377a70eaffb180f8fec2e7a4b49b04909f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:47 +0000

    along task backwards news tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def cube_number(x):
+        return x**3
