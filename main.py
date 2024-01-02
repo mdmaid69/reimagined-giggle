95ad52939cf744c90bf620b199161cdaed2b9efa
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:48:51 +0000

    mainly mainly collect finish murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
