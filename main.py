53973add8b88f07bf17c736485b63498ab33dbcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:51 +0000

    firm estimate promise demand means

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
