26adb69fe813437a0a74516ea21b2de7c52af1f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:52 +0000

    coat plan determined talk suggestion

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def remove_duplicates(lst):
+        return list(set(lst))
+def find_max(lst):
+        return max(lst)
