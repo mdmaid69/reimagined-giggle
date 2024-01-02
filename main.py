e91520aa32296c6584e5c262ce130c6156e44aa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:27:16 +0000

    sorry turn improve cook tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
