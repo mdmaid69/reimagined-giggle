1cd63e86ea146f6979cec477728ac94f9d8e2f22
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:27 +0000

    thick information extremely thinkthrough fond

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+def calculate_area_circle(r):
+        return 3.14 * r**2
