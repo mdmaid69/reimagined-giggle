8d040c4c850168b10bf06585251144b09ad1f606
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:28 +0000

    base long word live sound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
