ea6510e3f2565afe84f4c1f8b4df446c1e9bde19
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:25 +0000

    rank sense record advanced classroom

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
