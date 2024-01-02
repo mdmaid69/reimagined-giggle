12e62e972f5d93eac4a1b4a6b271d79c66fff76c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:31 +0000

    just interesting learn nearly over

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
 def calculate_acceleration(speed, time):
         return speed / time
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
