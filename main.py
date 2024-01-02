664ee492e3f1a6d68faff809abe7f3724111326f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:36:11 +0000

    container grateful finally opposite separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
