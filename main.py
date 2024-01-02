b556ba82474a13713951fc86ef8abe30a9c22e78
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:00 +0000

    tip dish possibly regularly immediate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-name = "Python"
-print("Hello,", name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  def calculate_area_rectangle(l, w):
+        return l * w
