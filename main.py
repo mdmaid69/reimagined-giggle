619460726a199c3b117095e39b5c02c81bf22fdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:12:25 +0000

    figure square recommend cough software

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
