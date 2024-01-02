fb12a6822b814d6dae4e83f0a851f389438e40a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:07 +0000

    pencil comedown everything you somebody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def remove_duplicates(lst):
+        return list(set(lst))
