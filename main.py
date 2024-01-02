e01ebe8d7139ec741bcaf5ecfc77088c27fec528
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:11 +0000

    connection worse land possibly without

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
