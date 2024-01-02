ff30c3b28697e7d332127764ba7ad3722104518f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:13:56 +0000

    slice create quit kind boring

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
