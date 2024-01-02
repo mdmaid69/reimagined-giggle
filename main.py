e7932fa1cae6dfdec61a1b1453d5546a7432624c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:37 +0000

    careful matter whole live shop

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
