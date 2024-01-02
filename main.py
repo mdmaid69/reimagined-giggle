073d1e347815882b46428537cc12c04c8a7fdf6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:21:05 +0000

    ashamed progress collection support mood

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
