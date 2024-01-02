23a27ec290f49ae42123319d42805cf20d763d09
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:17 +0000

    dull sell band early home

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
