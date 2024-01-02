52ab5dc16cf9a4a7a2273f0f02ba64bbf4745b6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:00:17 +0000

    chance live scared hate church

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
