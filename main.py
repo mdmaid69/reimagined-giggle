8de1faa16bd2c282445d1b554c5480c5d0dea6b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:05 +0000

    flood takeoff stiff badly land

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def convert_to_octal(n):
+        return oct(n)
