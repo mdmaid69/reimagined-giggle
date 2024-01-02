0e3716b19ba4a63a355d8fb3a9f69d35b1dc3504
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:27 +0000

    satisfied least taste choose window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
