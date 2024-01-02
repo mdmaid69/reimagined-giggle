cb88ef224dee3234649b2633680a4cdb2be651b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:12:53 +0000

    squash rough office no priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def sort_numbers(numbers):
+        return sorted(numbers)
