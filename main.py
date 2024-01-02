e843a2980237c393fcd5b647684b26016e3b5b67
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:16 +0000

    impression congratulate exact chin model

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
