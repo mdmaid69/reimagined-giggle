155ffb547a0103941dd685bd009e449c0823b45f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:35 +0000

    push their bag clothes Cc

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
