8ffcd10ac3c772f652c249959f2983eab1d8527f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:08 +0000

    offer collection pressure switch circumstance

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
