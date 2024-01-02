ae109cae255a79f69c0ef58dfd4097320f0bfb50
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:10:36 +0000

    Nn order crazy congratulations empty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
