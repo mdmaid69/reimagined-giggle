086b24a90eb7314dc7126bb306ead55b60c8d048
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:53:24 +0000

    talk ticket ignore holiday welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_as_float(array):
+        return float(array[0])
