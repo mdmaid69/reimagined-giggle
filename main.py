6054283275540dba56a8639ac7c4d380a7c0c26e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:35 +0000

    minute mysterious angry careless waste

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
