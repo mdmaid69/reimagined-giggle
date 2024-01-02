7aa46706d5f395565c1d73c212033d0db5e50433
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:53 +0000

    related head release capital question

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
