a639301f8191f9211c706a43b973427c560484fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:43 +0000

    culture hate flow little clever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
