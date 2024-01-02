0260269fb7064532fd923fcef57460463238ed48
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:40:49 +0000

    call hardly second recently plenty

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-for i in range(10): print(i)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
