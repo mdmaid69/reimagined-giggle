da8ad38fa456b0a39f25d41ccc5051bd27c6a503
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:11 +0000

    admire rare motion some whichever

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def convert_to_binary(n):
+        return bin(n)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
