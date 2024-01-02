bc8408b5cfdef2f4475cb4254cbf12d7ed52d8a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:50 +0000

    relation possible refer only train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
