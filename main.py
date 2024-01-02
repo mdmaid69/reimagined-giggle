5e7f232b408a18b1e9174b1aee908e614b987d8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:56 +0000

    safe during really equal see

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import collections
-def create_counter():
-        return collections.Counter()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
