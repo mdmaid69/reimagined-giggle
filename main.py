b93bc06a7be28d97ebe8c51674fd6b939891d096
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:26 +0000

    lesson runinto expect bang bunch

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def multiply_numbers(x, y):
+        return x * y
+def find_unique_words(sentence):
+        return set(sentence.split())
