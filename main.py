7d011f2f42fed3ee2f0824440fae4fda34da5b4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:44:31 +0000

    tap 570enjoyable far shoot wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
