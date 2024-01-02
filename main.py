4fea84cd02167dc51ffea3f9087fbbb8e7452dcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:57 +0000

    pity sort sometimes sleep motion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import collections
+def create_user_string():
+        return collections.UserString()
+def is_odd(n):
+        return n % 2 != 0
