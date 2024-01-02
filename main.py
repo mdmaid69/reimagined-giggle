94bcac3ac4345179bc1b19601ab560f6c07aa428
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:32:09 +0000

    runinto might murder separate accept

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_octal(n):
+        return oct(n)
