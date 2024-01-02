02b4f7314c0b926fa8d15c11d9bb08ccc91264eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:21 +0000

    surprise software odd charge Zz

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
