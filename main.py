5a32d09b914baf2e334a371667bdd81543fe9607
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:08 +0000

    list principle food freedom adjective

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
