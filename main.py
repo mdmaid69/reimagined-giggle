1959fd5b581499723152dd74ef0dd543250c225c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:03:02 +0000

    underneath safe error tidy whistle

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_power(work, time):
-        return work / time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
