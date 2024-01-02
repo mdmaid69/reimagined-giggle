b1eb586ac2a6edb64e8fc62f7049a454eb22a2f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:58:18 +0000

    present escape light confidence funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def sort_numbers(numbers):
+        return sorted(numbers)
