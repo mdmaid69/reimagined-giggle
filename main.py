b994e18844a3aff1f6e6e803d8e7a91e502a7128
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:27:29 +0000

    female shop dig shine shadow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 def calculate_speed(distance, time):
         return distance / time
