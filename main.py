70b8110988d044d719309ff0643eb8aac2cd7bb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:25 +0000

    failure mouth scream giveback secret

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
