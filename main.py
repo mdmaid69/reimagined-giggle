b408c3ff47457bb0fa808ed1a27eae748dba31d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:24 +0000

    design night shock equipment interrupt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
