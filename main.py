604d20102d049f6dab17e83ef0772f55d866e961
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:43:53 +0000

    compete that mistake will tap

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_power(work, time):
-        return work / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+for i in range(10): print(i)
