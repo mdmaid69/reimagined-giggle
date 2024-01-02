af1fb4d2b017e32181258ceba03b18fdf64af6d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:55:35 +0000

    bus gather admire team legal

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def sort_list(lst):
+        return sorted(lst)
