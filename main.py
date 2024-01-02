09889baa5f311d4b9a80d3e2cf3256109ce8838a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:11 +0000

    goaway set tongue general Dd

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
