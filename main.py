a819524e116675823f40a5e500de052a7dc0bf5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:38 +0000

    react children confused port turnon

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
