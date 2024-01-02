9e7c5943a4e4aedfab7cc5d2f3100fb1dc0d657d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:41 +0000

    school promise wake that neither

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
