5302c68b897d238e818ad7529b3d37516156f42c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:07 +0000

    shoulder shock bite sink breakin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
