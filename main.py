85f63fbd71aa7f8b5ad0bba0e02fe652fd901ab1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:43:47 +0000

    defend persuade professional once OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
