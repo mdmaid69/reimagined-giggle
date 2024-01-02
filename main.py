7e11946b0ae8f4eb298f87788b12ef9ec3ac34b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:37:43 +0000

    mark detail life bear avoid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
