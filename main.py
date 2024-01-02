d60f80b77890970b706c859af7cd1eab8a396da3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:01 +0000

    kick fill peaceful add past

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
