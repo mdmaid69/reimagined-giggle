5909f6f4c442865c5e228c9ca1bd1db28d8ff1ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:51 +0000

    complete future straight there conversation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def print_python_version():
+        return sys.version
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
