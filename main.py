beaad86828518fa47fd415da76753415742e57b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:10:18 +0000

    position leg pleasant suckme frightened

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
