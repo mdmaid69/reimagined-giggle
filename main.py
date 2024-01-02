ff2b1846d8f663ab8e71c695ca43cbed8daf8f6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:22:52 +0000

    shut turninto little sale smoke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def get_array_itemsize(array):
+        return array.itemsize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
