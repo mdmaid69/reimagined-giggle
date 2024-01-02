be452b4225d5b5dc891b8cc714212299fabd08d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:55 +0000

    situation point interfere size consult

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def is_even(n):
-        return n % 2 == 0
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
