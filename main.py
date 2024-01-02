20f054b581716a17942372b20649bc519635068a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:10 +0000

    turnon thorough sweet wherever cover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Words:", len(text.split()))
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
