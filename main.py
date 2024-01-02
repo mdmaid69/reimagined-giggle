b651a1090e9fde13f9d265bea8fd05a36794f8a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:06 +0000

    wash personal owe smell exhibition

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
