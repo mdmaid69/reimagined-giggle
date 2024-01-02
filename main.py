9c1a7d4692290e5eb2ceac701cbcf803f3bcc568
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:13 +0000

    sense drop sail previous forward

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
