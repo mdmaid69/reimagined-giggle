62b37f61aa86b6627612454d65fccfc8ed84b3ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:51 +0000

    flavour error back he near

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
