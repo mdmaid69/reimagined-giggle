00a307de4ddce0b9e042879c11e4dc8b742c125b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:06:38 +0000

    inside importance back dry sister

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
