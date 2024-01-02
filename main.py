0e05c76ea3143d7f615aa8f8c2c155b0246cd70a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:19 +0000

    settle increase unfair valuable recognize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
