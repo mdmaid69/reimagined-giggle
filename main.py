3410c8e464f51e5b80cdaff6fdcae4b12b40cc27
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:05 +0000

    cry sky buy lost glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  def cube_number(x):
-        return x**3
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
