55df75338a21cb75e4c51f007dad0c56f58700db
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:51:26 +0000

    takeout grade takeaway process confusing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
