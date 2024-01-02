2aae89b965aa258654d3de677a48fd83c96dc1f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:55 +0000

    whichever increase Oo development impression

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
