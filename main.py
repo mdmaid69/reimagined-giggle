121cd803cbf16bce474244790e4e15cae4ef2b08
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:10:48 +0000

    necessary consult hold his watch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
