5ef4d9997649857842dff69a8e7f6e337d623118
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:42:02 +0000

    influence peaceful ride then turnoff

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-print([x**2 for x in range(10)])
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
