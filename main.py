21706d5a3169f029b0604aabdad1945f13eccde1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:45:12 +0000

    doctor fortunate score interrupt takeoff

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
 def convert_to_octal(n):
         return oct(n)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
