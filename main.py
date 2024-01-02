affd8b41753b41401bd539f867eb4ede9d8bfe76
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:20 +0000

    properly interrupt anything healthy need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
 import collections
-def create_queue():
+def create_stack():
         return collections.deque()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
