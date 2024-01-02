f5350ecec9f7cacf6e44eda3df571648ec638f14
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:22:30 +0000

    drop custom term exact correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_min(numbers):
-        return min(numbers)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+text = "Hello, world!"
+print("Reversed:", text[::-1])
