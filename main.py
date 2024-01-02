664553f452a258dd80513257af4f9d85cad4dd99
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:42:59 +0000

    water uncertain anyone breath success

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
