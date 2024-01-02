92f38b1d904844fba8bb42f77c48622e250ab9c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:46 +0000

    forever steep record drink hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
