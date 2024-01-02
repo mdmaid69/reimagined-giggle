0d3f735ab0d2abe67756e76815470380a4ba44d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:12 +0000

    Gg public refer that best

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
