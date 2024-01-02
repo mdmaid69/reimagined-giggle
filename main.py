e0001724006ed95e49b608a65816b60e85081b7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:43 +0000

    suddenly positive lock lend base

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
