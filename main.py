26088c6e8f99072446e5593bc27ee72f6c60c31a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:20 +0000

    develop Qq getup welcome start

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  def convert_to_octal(n):
-        return oct(n)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
