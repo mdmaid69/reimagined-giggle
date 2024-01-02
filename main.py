f2f77d88f59abf19a0cb18f4a8713eed66ff0bb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:23 +0000

    confident example system close either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def remove_directory(path):
+        os.rmdir(path)
