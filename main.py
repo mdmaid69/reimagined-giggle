c190513aceed9fa901ff02d01a78bb10caf44992
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:13 +0000

    go fine especially bite suit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import time
-print(time.time())
-def count_elements(lst):
-        return len(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
