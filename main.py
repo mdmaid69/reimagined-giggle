7458017b05441d80e6e3baa49d7d6284b1e8490d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:40:17 +0000

    equal experience return blame sure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
