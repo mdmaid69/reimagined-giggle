969a3a1d398c66b82fbc474604b24f1ef4a93e3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:52 +0000

    sweet sweep level form hang

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def convert_to_hex(n):
-        return hex(n)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
