635b63cd7467a58aaab518fa9baddeec82822ecd
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:03:04 +0000

    only stretch factory issue star

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
