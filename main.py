3efdd4ef8fdec75d9ba6d2a66c1a0dc3fee8684a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:00 +0000

    unite distance free dick smooth

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
