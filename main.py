7ae663da64dc5af83777524f9370d8f071f147e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:52 +0000

    count bringback after estimate if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+def convert_to_binary(n):
+        return bin(n)
