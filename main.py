479dae1cbe5d3e946b61e06e16deb759e859330c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:40 +0000

    dare blind thank Uu bush

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
