f5ec11f63ceaea784afb4c0fd788a80ac6c36bf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:39 +0000

    visit peace Gg excuse into

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
