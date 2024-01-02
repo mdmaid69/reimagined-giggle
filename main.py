c18d28d6fb59afe619d363f73324bc7d32a1b609
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:03 +0000

    mistake comeout flow further less

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
