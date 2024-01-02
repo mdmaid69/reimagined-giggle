14c5d09075cc8f03d5fc5f66d7f99e477781a947
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:12:43 +0000

    bottom breakthrough respect stranger hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
