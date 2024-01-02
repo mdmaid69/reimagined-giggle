ab82f874f225a96c874782e91691468c68e427b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:29 +0000

    couple getup vote violence bad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
