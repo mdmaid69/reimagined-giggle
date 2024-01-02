f97f8688e7c24eb6aca06df20941fa47a0d6ea0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:04 +0000

    grow programme report interrupt avoid

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  def cube_number(x):
-        return x**3
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import numpy as np
+print(np.array([1, 2, 3]))
