75d753e70d4242c33df97f3cf6c05525d3402539
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:17:51 +0000

    minor consult return port replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
