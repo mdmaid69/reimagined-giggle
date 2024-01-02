95ba24207691b08e22d1b1c4d830373b901da4dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:01 +0000

    tie consider centimetre solve describe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
