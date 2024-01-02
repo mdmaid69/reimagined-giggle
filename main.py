13ced3814198f83cb032f352704ec1e3d9019df0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:03 +0000

    hit drive dick mistake fierce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import sys
-print(sys.version)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
