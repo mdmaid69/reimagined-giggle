f9b8a4470a24c4c32cfc1b93a02c2cee4bd977d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:58:04 +0000

    match matter admire obtain search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
