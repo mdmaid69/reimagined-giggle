076b3854e802ba1f28ef65010d2b754ee7e7e5d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:09 +0000

    explain size commit communication pupil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
