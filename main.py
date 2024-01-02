7809a3cedaf278c836e9debe40327eec4fec746c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:45 +0000

    reply always nervous anxious wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
