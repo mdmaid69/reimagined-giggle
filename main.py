1e4bec1fdaae22ac3d52b9b52321c76ddfa1170a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:24 +0000

    union Nn getback work become

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def cube_number(x):
-        return x**3
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        return sys.version
