b6e85e1ae94df257980d4d91a13d32e756c6193a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:42:16 +0000

    ill remove zero major instant

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
