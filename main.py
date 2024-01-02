290b11109d6bf616c794e6837852e92806155662
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:51:34 +0000

    injury emotion list broken breakfast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-def is_even(n):
-        return n % 2 == 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
