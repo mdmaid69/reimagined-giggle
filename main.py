64f00d11f4b2e6958d8c481c6ec00991839f1a0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:49:28 +0000

    past bath pair her total

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import numpy as np
-print(np.array([1, 2, 3]))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
