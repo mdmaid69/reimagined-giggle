0bdbf9665364f6e4fcd2cbf23e58fb5bdae52790
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:50 +0000

    Mm classroom rest boil twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
