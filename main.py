0ea0d8e699e32349b39d2e4c61de4c14ea3fab2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:59 +0000

    safety guilty command pretend admire

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
