a8dcf040821064f8bf6292e3b9e322aa589fea37
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:53 +0000

    window raise television mixture conclusion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def square_number(x):
-        return x**2
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
