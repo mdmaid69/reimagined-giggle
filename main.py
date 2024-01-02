8a3cebd4f6cb361d654f4210f0e67d577d7d8029
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:14 +0000

    person temper operate course load

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  def reverse_list(lst):
+        return lst[::-1]
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
