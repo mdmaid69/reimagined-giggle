9bc06e4d308455b7acdf11eee445e8c56b8e71b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:23 +0000

    gentle anybody about mess estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
