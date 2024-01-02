394232dc82393006aa469e05d132ec6d7b3db9c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:22 +0000

    back Tt shut fortunately sensible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def is_palindrome(s):
-        return s == s[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_sine(x):
+        return math.sin(x)
