2beccc070065dc15a7082dbfa7ffa2318e572357
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:21:19 +0000

    ticket obtain touch heart usually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
