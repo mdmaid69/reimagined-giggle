14b547449b6c978ff910b553fd7504f3967e86c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:00 +0000

    public callout square question Hh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
