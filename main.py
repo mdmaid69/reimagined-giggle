96b04269474fca9946906b96c85695b08ab51237
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:21 +0000

    rush strong usually welcome demonstrate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+print(math.pi)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
