e039e92eec3fde02f11632d0053584ff8c163736
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:36:55 +0000

    disapprove unfortunate attractive keepup drawing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
