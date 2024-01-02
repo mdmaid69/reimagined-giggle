0f850c87f94baf54ec07e188bba26d8c11f2e45c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:40 +0000

    stupid until wipe lesson crime

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
