0f45e592459339be30b1e1938dd046789b7050a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:29 +0000

    now wet contrast level wake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-  def add_numbers(x, y):
-        return x + y
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
