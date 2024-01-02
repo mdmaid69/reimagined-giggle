df32b9ed2415e418178d162f51c2fbf46f02ac1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:39 +0000

    fast suddenly thinkthrough wash workupto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
