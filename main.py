aa11147ea50155986ee8659d04f4e76d78562b6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:42 +0000

    guilty lead affect constantly shout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+def calculate_speed(distance, time):
+        return distance / time
