8132f72cbf83fd0c8e7bdedaaf8a10700a34a6d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:31:53 +0000

    address who possible wander might

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_average(lst):
+        return sum(lst) / len(lst)
