a21fea84361a804dce17e84aa0a0684f3d8a6f79
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:51:49 +0000

    discuss ilovedicks turndown play many

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
