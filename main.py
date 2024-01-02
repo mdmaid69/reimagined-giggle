ee3c8b46f7235f268c83c52b4e02d1db3006234a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:54:24 +0000

    pipe man survive display letter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
