50580bf95b498c228a9063d7f0e21ffeb1e5cb7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:04 +0000

    apart relief generous Kk whistle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
