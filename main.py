cf7f828a9cfc3d3393174da9ef36c2f8e37e178b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:37:46 +0000

    low unfortunate dress because turndown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
