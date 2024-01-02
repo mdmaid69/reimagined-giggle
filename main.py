ea494341b04d3ad383d8da10d71662b2654ef748
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:42:58 +0000

    frightened break regular thanks imagination

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
