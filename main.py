d0c8c500a6faf7b1422507852e0b4c09d3c3b17a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:51:53 +0000

    central gentle goon injury drawing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
