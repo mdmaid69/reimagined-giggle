6a2c0430a90755abd253590e1c4adc9cadc1f98e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:33:22 +0000

    card along type limit mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
