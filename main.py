8796d3f2ee3832345e51ebc98e6554b7e3306696
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:42 +0000

    boil lookback(on) neither independent face

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
