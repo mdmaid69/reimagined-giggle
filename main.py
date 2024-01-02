2720616f5805e366fd1496dd7670d63a9407f304
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:10:09 +0000

    forever tap straight cross holdon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+for i in range(5):
+        print(i)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
