d6f1e5ebfc5f0fe5d2f33ccf52dcb4819efe7f02
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:30:03 +0000

    flat score fresh recently for

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
