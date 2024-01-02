be8133628d376e183f9f77831198594139b73a74
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:13 +0000

    usually doubt minimum trap church

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
