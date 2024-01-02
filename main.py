8fb7628e4739d867978cd55bd7b515bb62b1fbf6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:42:06 +0000

    turnoff die he comfort do

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
