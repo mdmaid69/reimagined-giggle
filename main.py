659c2e947a9d7c69774d689909b036af9da49f1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:50 +0000

    record possibly emphasize collection row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
