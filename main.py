37665c48805830273e011b6cddfb951d19db8c14
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:36 +0000

    precise weather keepout no sure

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_palindrome(s):
+        return s == s[::-1]
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
