553b27dc68e8c698b5c65371dcf7aa790a61931a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:40 +0000

    break turn under hand steer

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def remove_duplicates(lst):
+        return list(set(lst))
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
