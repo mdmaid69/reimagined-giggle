b5a2b8b907c11e0def18d8af66f6f61a834e5187
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:38:14 +0000

    book I arm goon have

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def subtract_numbers(x, y):
+        return x - y
