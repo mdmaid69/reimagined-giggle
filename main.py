e229ed6a7c7f770015020b3cb6e58136fb75f2e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:57:01 +0000

    loose measure angry pleased before

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
