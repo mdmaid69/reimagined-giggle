35e86db4d8efcfa07c830cddf84e38471fda1090
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:32 +0000

    meet taste cap advice risk

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
