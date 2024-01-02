1ed4a6641fcf2d69ac7400ab953d3a933b7cd834
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:45:26 +0000

    sorry weak argue crowded frame

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+def remove_duplicates(lst):
+        return list(set(lst))
