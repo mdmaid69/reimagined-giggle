73a598e479d1ce65368eb1348c1c34576ed35b28
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:24 +0000

    priority range sport skirt whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
