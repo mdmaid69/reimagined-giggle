7a0e1c833197b95370d9b6e84c7f152a00f816dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:46 +0000

    journey dream behind favour leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
