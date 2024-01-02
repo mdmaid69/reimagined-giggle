8d1173bf918f5ded5fe412b06dfad552b64b6f26
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:25 +0000

    apply black convinced land embarrassing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def count_elements(lst):
+        return len(lst)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
