8514b03edb9393a64836d0a494957d8ff84d068b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:27 +0000

    connect result score unkind design

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_area_circle(r):
+        return 3.14 * r**2
