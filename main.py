acb01b5926f646bd965eb1f943dfccb042e145ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:30 +0000

    contact always correction Mm measure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
