6a3166ceef85603fb1dce166b9ba915c060cf568
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:01 +0000

    scrap correct row breakdown protect

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_list_from_array(array):
+        return array.tolist()
