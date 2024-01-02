249eb1d6180eeb4a92825852bef90fdd155464cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:29 +0000

    fold stone night scale design

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
