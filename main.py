01021357888384aee26df1f65313a1b5d60022be
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:30 +0000

    period now deliver after already

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-  def convert_to_hex(n):
-        return hex(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
