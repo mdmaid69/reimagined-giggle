3a727f4c7e43466cbc0abdc02db610d880ceecc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:02:25 +0000

    value shame instruction Nn about

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
