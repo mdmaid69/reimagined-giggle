1135ab068ac48d393f51ab4e8ea605f849f90d5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:02 +0000

    hers police pronounce lift hurry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
