12601662d7493d4d1bd221450b445cc74be8c97a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:44:00 +0000

    thirsty it drink little pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_acceleration(speed, time):
+        return speed / time
