ec8178752fe2e580165cd9827efa6c95d6d59856
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:21:30 +0000

    platform cover private really fresh

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
