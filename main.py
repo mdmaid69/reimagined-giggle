811e62f3a31cb952675514239b9414e37ef8a372
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:28 +0000

    foreign same Cc first cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
