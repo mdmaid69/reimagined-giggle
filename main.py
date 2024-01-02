9b77da681189b70abc8b7de4c8aeedce84af0190
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:44 +0000

    smile impression side apparently feed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_speed(distance, time):
+        return distance / time
