16891f49d7c3e3bd4ed5efca79a5faf7266fcfe8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:23 +0000

    row chance workupto Zz wonder

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
