601f347215f6aab70f17a0466708b8b21ea50cb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:39 +0000

    up high match trick dry

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def split_path(path):
+        return os.path.split(path)
