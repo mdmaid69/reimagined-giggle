ebe69ee8dd1acda820043adbd0f5c25504e70b3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:59:04 +0000

    intend mistake like scream delight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
