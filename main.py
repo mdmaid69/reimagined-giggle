070c249247048779322882b520596c8a758f427a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:48 +0000

    mess or thing injury moment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def convert_to_binary(n):
+        return bin(n)
