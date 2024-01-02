cd17f509733b83029f7f9684e3b580db59f762eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:03 +0000

    shrink water water sight strict

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
