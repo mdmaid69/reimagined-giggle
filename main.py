6294e6108449d9b94f340c81aac8ebe57af1f742
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:16 +0000

    able market limit report unit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def subtract_numbers(x, y):
+        return x - y
