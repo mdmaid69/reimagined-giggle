96f9da9ad2e7fe42911d76cc1facc55e71064fa0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:32:12 +0000

    name detail drop official post

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def convert_to_hex(n):
+        return hex(n)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
