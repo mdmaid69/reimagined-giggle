69bbe616909282388e2bb1d75cbc1a3ef974e553
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:02:32 +0000

    total good niggaacademy we able

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def check_if_array_contains_item(array, item):
+        return item in array
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
