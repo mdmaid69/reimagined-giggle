9732c4e179f485effee7032d208f3b8eb1225bf3
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:01 +0000

    worth pay valuable cheek turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def find_min(lst):
+        return min(lst)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
