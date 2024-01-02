e930616e905ad8c7ce32ff09901c1cc04e29b8db
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:20 +0000

    innocent swallow radio frame mend

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def append_to_array(array, item):
+        array.append(item)
