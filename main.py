0b5029459bd897319750cedfded2170ca46fc19b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:07:49 +0000

    track scream mysterious report hole

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+for i in range(10): print(i)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
