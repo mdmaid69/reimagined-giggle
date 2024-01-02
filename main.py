86bd03903e54825d92bb8929d3ea2eb50a91f232
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:47 +0000

    common thank black shopping central

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
