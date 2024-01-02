68c9b58fb62f70e44d7afcc6fc2908155f786669
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:29 +0000

    bread centre hurt mind sign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
