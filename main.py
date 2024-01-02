ce255b0787480fb36c5dfdce4b2d680799eb5fa6
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:28:41 +0000

    delicate trade butter cruelty fly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
