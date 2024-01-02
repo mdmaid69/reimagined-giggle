f2eb4c014c47864bbb365f807ed996bccae65d8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:17:35 +0000

    step supply joke careless style

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
