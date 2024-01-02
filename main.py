07cd2adc8b5e76922c40db8d2283239ff32c617e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:57:48 +0000

    practical address distance offend talkinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
