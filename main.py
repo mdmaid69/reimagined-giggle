42a080d45a056871995cafe8e7ee8edec38c491b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:24 +0000

    get give difference floor surface

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
