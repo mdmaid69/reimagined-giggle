9654f26b74adf046d7c082da4b3131c5473d151f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:33:36 +0000

    bringup into also abroad control

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_item(array, i):
+        return array[i]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
