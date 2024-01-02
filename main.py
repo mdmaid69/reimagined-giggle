ad7f099132984a1a8f90cae11c99d6b8cb63200a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:04 +0000

    score butter murder rid blow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
