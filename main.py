a8d117df8ea0e8faa7502bb45898e7b1e0699e68
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:02 +0000

    machine switch present lesson mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
