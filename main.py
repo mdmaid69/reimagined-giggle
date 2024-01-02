895bcc2dc8ae6232d4059cf0eb044c8da7d7c10d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:29 +0000

    serious wall mend lookafter wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def convert_to_hex(n):
+        return hex(n)
