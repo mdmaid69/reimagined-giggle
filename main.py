df0850864f0080dd3a8b32e28e5e511a1e5b436e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:43:07 +0000

    bit find stir responsibility politics

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import collections
+def create_priority_queue():
+        return collections.deque()
+import os
+print(os.getcwd())
