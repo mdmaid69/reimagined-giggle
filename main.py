bee301ad4ba9fa36173cfa63f2491f060e3c34c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:36 +0000

    getout neither rescue coat tread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
