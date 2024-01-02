adf5fa82340040e6f0168e581eb72db618dfa482
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:25 +0000

    match answer rub yet please

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
