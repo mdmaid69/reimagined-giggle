fbb0b4e05ce6134fa4ee89a82ef9e05bbf879a78
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:13 +0000

    there wrong last ourselves eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
