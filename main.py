4e86f493e8f0e351ff1434316f42d9bb42c330f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:57 +0000

    education lay vision mess matter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
