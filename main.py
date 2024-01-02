a1b974c809d7350b5135f874b75d630cc4975eef
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:36:57 +0000

    survive sport certainly interview pupil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
