80cb0195d890475af4deb8388859d885910f6637
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:47 +0000

    group collect whatever herself know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
