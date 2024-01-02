59c135443e0bc32dd06f2cebbf6a2a20234a9227
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:05:09 +0000

    interfere original need whom guilty

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_pressure(force, area):
+        return force / area
