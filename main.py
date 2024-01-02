5ad64a6c8992d0cec21a7eab7911b5bd5723e542
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:39:03 +0000

    intelligent angry emphasis minimum out

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
