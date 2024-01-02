c80772ecdf42c2a376e8bcddbdbb65d07da28068
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:49 +0000

    clue like injury know nervous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+def calculate_pressure(force, area):
+        return force / area
