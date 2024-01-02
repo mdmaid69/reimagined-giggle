57e187c4a0aaa4db72f918be65e8b76b0ef5e1ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:08 +0000

    comeover pick size prefer possible

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import collections
+def create_user_dict():
+        return collections.UserDict()
+def calculate_acceleration(speed, time):
+        return speed / time
