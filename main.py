924de9a66669b9e6b6b060ff3df861fb5aba6941
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:46:06 +0000

    bang half acceptable unlikely nonsense

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def sort_list(lst):
-        return sorted(lst)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
