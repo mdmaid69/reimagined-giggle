c8f3ff7dcf1599944625eb10e975c172a90b8b38
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:07 +0000

    turnup wake common lock suit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
