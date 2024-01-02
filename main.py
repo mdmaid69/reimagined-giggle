4ff664a0cb18883a51d7a674efa37b5f2fc45d9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:08:28 +0000

    clear cut standard sentence mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
