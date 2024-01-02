b66abf127191b33bf4a3a1db78aedd67189e691d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:50 +0000

    degree immediate can aim crash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import numpy as np
-print(np.array([1, 2, 3]))
+def calculate_work(force, distance):
+        return force * distance
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
