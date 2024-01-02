d1656f93351739884669e1ea57d22903d149a541
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:06 +0000

    classroom takeover loan square only

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
