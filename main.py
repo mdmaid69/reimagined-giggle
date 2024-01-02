b1d3438eba2ac5bab48a1b969dac390969d107ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:47:56 +0000

    on memory shopping separate scale

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
