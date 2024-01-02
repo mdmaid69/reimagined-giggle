1dda506a6db0eec66587997a1923b7568cbc124a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:06:16 +0000

    to number into straight strange

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def find_min(numbers):
+        return min(numbers)
