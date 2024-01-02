308dfd82bd9b0477439294bf09c30b09992f38f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:14 +0000

    march song great near equal

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
