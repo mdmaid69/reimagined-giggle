9bc6bdbfc9b01159b9e3743811475010241d2d24
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:51 +0000

    attempt add judge concerning holdout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
