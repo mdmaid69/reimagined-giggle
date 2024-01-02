79aa7811e4ce694be279a36e59ad7d9791794b40
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:54 +0000

    bend properly rush neat strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def convert_to_hex(n):
+        return hex(n)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
