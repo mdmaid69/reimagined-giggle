dd197684f6f541f2f15a7bcb079ce270ba14158b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:07 +0000

    water add bridge war treat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        return sys.version
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def is_even(n):
+        return n % 2 == 0
