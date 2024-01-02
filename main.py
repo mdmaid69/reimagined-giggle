dd53cd8c429050efec1a9ac2c64a5b8136640db7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:09 +0000

    loss drop slow 565end event

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def count_characters(sentence):
+        return len(sentence)
