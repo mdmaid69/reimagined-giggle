df05e29cfd16fc8234369f232f851d8b22464b06
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:51:19 +0000

    comment poor here advice control

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
