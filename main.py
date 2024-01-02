2cfd13c0638ccbb646cff82a939dbfa873ad04e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:51:26 +0000

    if store become tomorrow characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
