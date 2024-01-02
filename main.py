90c4ab3bad6ef8d68c498f5ea786da8c20237fd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:49 +0000

    good disapprove climb naturally average

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import datetime
+print(datetime.datetime.now())
