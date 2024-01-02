7307c7b0e672be6fbaa0c58a49a136f3288de660
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:52:09 +0000

    shock politics invitation soon right

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+print([x**2 for x in range(10)])
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def get_array_as_bool(array):
+        return bool(array)
