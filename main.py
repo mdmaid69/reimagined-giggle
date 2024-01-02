a134fe4d014f0fb98946bc9d77af4f4b05897ba0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:38:41 +0000

    hear firm fork lookback(on) earth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
