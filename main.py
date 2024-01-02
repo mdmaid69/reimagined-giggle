6832118a7b0c81d4f0289de222e2ef6296e31f84
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:24 +0000

    must straight hurt base pass

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
