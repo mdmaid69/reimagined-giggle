cf11e1c322b0eaed23daf21464d8685840231ca1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:05:11 +0000

    mass bunch recommend art pronounce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
