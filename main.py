ee00202a13c92525cf0e9096e227449b4a8df243
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:25:08 +0000

    whistle start pump manage pretty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def multiply_numbers(x, y):
+        return x * y
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
