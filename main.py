8a324c05ab3c19c3b7a1dc897f8c4e935ecc3a13
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:14 +0000

    holdout principle mark notice message

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
