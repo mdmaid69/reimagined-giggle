65cf8a667acc0a3adfcdcc008ab55e8875ff2c34
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:00 +0000

    interview taste reserve remember belong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
