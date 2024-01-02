584f54343a679209cb8cb34e692619526cece4a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:34 +0000

    male part interview comfortable year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
