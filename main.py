8cceeb2c5fda8bc8846601226daa83208e313ec3
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:52 +0000

    someone form left cause motion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+def calculate_acceleration(speed, time):
+        return speed / time
