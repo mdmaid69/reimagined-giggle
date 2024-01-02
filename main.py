5126f1235188219108d05834b280c1e7d66fa0e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:17 +0000

    emphasize lock direct birth justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def remove_directory(path):
+        os.rmdir(path)
