1d85c7f50352f1734c199289eda7123249345521
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:02 +0000

    best sport steep straight through

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
