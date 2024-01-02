79b38472a8feeaa3886295bdf8de9a6675749a33
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:13 +0000

    popular goout spring gather confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
