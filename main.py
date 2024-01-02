f01b9f25d0842336bef383825c581be976460599
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:30 +0000

    and play study people soft

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
