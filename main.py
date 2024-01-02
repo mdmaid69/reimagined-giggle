d7d02908901bf74e912326dac21ba124787e3219
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:05 +0000

    match view takeout fun in

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
 import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_speed(distance, time):
+        return distance / time
