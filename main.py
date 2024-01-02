dd1ce3a05c435622efb5d04634857329e85a03fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:29 +0000

    total bar pause moment private

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
