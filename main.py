8aed5b78774a8430081cc6c715385e7d527fe533
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:40 +0000

    male dark centre character listen

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def calculate_acceleration(speed, time):
+        return speed / time
