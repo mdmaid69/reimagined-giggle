c9eb2bd58885097f5f29d18284c8567491511fe7
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:41 +0000

    compare tall government suggestion yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+from collections import Counter
+print(Counter("hello world"))
