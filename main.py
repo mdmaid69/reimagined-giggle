d426f3626449704e0e5c53b0833d163a69a2e33d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:34 +0000

    runover contents guilty couple too

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import os
-print(os.getcwd())
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
