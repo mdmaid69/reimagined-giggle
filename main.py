dfd8002187e0f182cfe140f5c546a04b94aabe7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:04 +0000

    swimming instant mass pride hour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def find_max(lst):
+        return max(lst)
