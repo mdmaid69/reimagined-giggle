a45ba29d1434ba42531476a1780d43d7a1c28758
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:07 +0000

    what ordinary fairly climb embarrassing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_hex(n):
-        return hex(n)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
