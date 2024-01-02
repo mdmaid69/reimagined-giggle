00a894063ac424d9cf0d2e0a0dd3e3f2afbbfdbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:20 +0000

    fair doubt firm entry mother

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
