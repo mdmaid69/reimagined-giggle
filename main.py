c5560ec8ea575a82068b3e6f601b560a103ca56e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:17 +0000

    giveaway muscle size leg meeting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
