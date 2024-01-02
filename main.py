2146c0a4103a1b919a142a8d99a6e9deaa6cdb1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:27 +0000

    swing detail pencil impression bath

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def reverse_list(lst):
+        return lst[::-1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
