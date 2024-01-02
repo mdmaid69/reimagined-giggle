b703766c4eaf89bff2bb6d92f6a557c37a8b0050
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:11:28 +0000

    hers complete false use crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def convert_to_json(data):
+        return json.dumps(data)
