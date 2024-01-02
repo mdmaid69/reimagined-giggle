3c108956b8789ae24b6f2bc2768245e4ebb666ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:43:08 +0000

    most noise to keepup ground

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import time
+def get_current_time():
+        return time.ctime()
