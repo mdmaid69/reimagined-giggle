9a90cca58daf6726d3ed0bd9151c702c62a56eb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:19:02 +0000

    satisfactory poison key mine situation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
