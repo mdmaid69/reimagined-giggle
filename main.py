383e67b64c13ba77ca3f41311d7161a4527421e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:40 +0000

    doctor consider goup unfair 571enough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
