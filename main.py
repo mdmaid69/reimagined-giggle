7210a5bf3f75a2ccae414e54a5cb56c39eb4c89b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:04 +0000

    attempt bottom ahead day smell

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import os
-def change_working_directory(path):
-        os.chdir(path)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
