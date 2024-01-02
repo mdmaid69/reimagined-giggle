e071898c67c0ed6554dfe7d1dc9634275e1d39a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:53 +0000

    drink beneath succeed badly thinkof

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
