88a22d59d5fe36dd42e0e49140ddbd3ad5d7a4ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:26:56 +0000

    hang welcome create close tear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
