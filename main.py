f3f2eb11e3e8fec941c31f9404903b2d3c8f3ba7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:10:09 +0000

    label over thick extra reaction

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-print([x**2 for x in range(10)])
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def convert_to_hex(n):
+        return hex(n)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
