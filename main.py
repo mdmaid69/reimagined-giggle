8b179d4d1cb9f91d383218a14080a8ce9efd7fcd
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:40:17 +0000

    behaviour exchange press direction smoke

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+print("Hello, world!")
+import sys
+def print_python_version():
+        print(sys.version)
