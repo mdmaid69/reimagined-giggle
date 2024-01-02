ce970dac4c246016b09f9687ec0024790ed89887
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:46 +0000

    pure hate grand extraordinary busy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import array
+def get_array_as_set(array):
+        return set(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
