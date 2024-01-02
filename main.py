739314dd10395b48c62572680152eeabd0c7cc65
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:17:10 +0000

    breakfast shut connected allow every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import platform
+def get_python_version():
+        return platform.python_version()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
