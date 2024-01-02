afb0f505ab60b19bbdfe96a7af732ac8949094dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:17:31 +0000

    else child appeal brave song

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_elements(lst):
+        return len(lst)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
