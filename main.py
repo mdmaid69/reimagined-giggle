bb5987522d46be557c7e737446ef94df716ff137
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:36 +0000

    brush calloff sweet dig comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
