1f2b41c50b6ec0127a24912b94916c2d3d5974ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:29:32 +0000

    debt either balance complete share

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
