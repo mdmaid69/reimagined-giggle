0f4acf23c2db370d55102eb35c1c89071cf0a2e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:03:52 +0000

    sense benefit escape frighten trip

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
