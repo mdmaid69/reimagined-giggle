e697b140aa204911f49626380266f4daacfe15bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:15:52 +0000

    Rr arrange damage police frame

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
