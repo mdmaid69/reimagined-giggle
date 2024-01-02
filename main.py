16a8a5208f5f6052c3c75bf10d7a80e7c0e53f5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:43:49 +0000

    nearly through trouble his accident

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def sort_list(lst):
+        return sorted(lst)
