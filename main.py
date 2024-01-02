cac42fbc7234d4ae6e3ea930d73657d351974215
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:36 +0000

    though destroy Zz physical well

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
