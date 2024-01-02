57c1616586f4505f3cf326760aec4c1d49edfcfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:39 +0000

    bunch he ignore tight bin

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
