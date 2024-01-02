68de54a686d0bc447fabf9fb3633448fbf588052
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:03:49 +0000

    Ss bang out talkback record

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_pressure(force, area):
-        return force / area
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
