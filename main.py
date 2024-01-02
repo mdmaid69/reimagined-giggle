a7372f4f82df3eacca2832f82723e45f73f5dd5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:29 +0000

    affect thinkover notice sea debt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
