1f167c03154c48ae8dcfbe1231d505540f28d5d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:24:48 +0000

    roll most right present hold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
