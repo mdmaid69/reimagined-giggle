849255244151f1af9bf98be9d8740b51de8413a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:26:51 +0000

    runover bag call manage instant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_array_as_tuple(array):
+        return tuple(array)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
