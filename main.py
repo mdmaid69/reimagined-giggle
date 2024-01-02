4701309b56885c4a3449d6054aaa8b1144207707
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:32:38 +0000

    survive pronounce measure rarely fasten

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-name = "Python"
-print("Hello,", name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+for i in range(10): print(i)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
