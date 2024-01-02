738ebd5d2e87b1a034bb1405b2c52cf7388541ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:51 +0000

    trial giveaway quit change environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
+  def get_file_size_in_bytes(file_name):
         return os.stat(file_name).st_size
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
