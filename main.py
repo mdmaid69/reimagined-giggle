c6bc296d7856fe678c46927a6221685f00867b54
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:55 +0000

    fast perfect shoot offer recognize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-print(sum(range(10)))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
