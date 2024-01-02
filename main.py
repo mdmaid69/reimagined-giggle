61ca3ebe8909151c6b1a1afa43489e690665c7b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:27 +0000

    nose common block welcome professional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_string_from_array(array):
+        return array.tobytes()
