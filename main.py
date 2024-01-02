5ddea475c230299ffa1e7e338745021b0c457f24
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:03 +0000

    generous herself motion poor I

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
