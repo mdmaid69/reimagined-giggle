fb8befd4c7cacdafe69d44b78a1f8d79d59ad455
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:37:57 +0000

    lock music wind beautiful trouble

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
