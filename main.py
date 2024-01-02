1d6a725b48f4dd558ba0d2f8ca6ccd4004bd4a58
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:58:05 +0000

    gift long theirs study air

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
