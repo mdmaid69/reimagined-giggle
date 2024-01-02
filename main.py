eb6939da5955a85cd2dffd08fe35880ffad372f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:53 +0000

    they eager along disagree rich

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import numpy as np
+print(np.array([1, 2, 3]))
