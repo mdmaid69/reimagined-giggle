4869dd144e3865cfdec3f259d016636c40d3b44b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:48:17 +0000

    man approval win tidy over

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
