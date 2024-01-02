e1eea760924f2ee605b8ed1fd8c54ad5b7234fc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:40 +0000

    argument argue more command count

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
