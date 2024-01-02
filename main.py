3be4913af829ddc1e860b7239d506333f6e0dff2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:35 +0000

    habit shop price branch somehow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
