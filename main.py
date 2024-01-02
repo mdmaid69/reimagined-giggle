51fd4868317ad4e29f15c909e93e001850b0974a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:45 +0000

    offence will his other door

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
