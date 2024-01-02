2b0d8671a2d438b9386f9fd2fa148a0928000dd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:37:55 +0000

    character means reply height later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
