d62e930547994febd9b929d8d40c33cef2aeb16e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:32 +0000

    lift mess outside discover tax

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_max(numbers):
-        return max(numbers)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
