635bc7cad4b1fab9ad380f7acb978ec18cf2c3d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:53:16 +0000

    completely heavy compete complaint plant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
