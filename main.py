bffdc9b71df8f95c19b408d6961f63eccb8e635d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:11:35 +0000

    length belong regret count frame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  def calculate_area_rectangle(l, w):
+        return l * w
