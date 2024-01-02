042c500a38c7686f11e0b16d90cbbce85d181362
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:06 +0000

    steady quit imagination firm Dd

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def sort_list(lst):
+        return sorted(lst)
