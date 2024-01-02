110d974709cb240ca7ca532501c4555ec8eeecde
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:29:24 +0000

    drawing organization fact land especially

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
