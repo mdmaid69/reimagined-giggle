71bb106dd4c7ee3d1d1f04688635ed1d29ce9abe
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:23 +0000

    valuable rubbish estimate worse experiment

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
