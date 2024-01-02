dbcd2fda547d7b8261e57d75f48e1b2c49ca14bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:50 +0000

    both hide attempt pour sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+def calculate_power(work, time):
+        return work / time
