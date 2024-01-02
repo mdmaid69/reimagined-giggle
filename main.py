287191b36ef9e8da1b0e47959391f1d8396d9d4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:55 +0000

    left impossible paper hello water

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
