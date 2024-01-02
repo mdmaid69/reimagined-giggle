9bbd38488b8c22b6d979d05ac310f885c73eccc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:26:08 +0000

    remain keepoff big tense quality

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
