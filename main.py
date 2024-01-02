23f30cceb6b5b7247dd9e48fbe04a38873e1ab98
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:51 +0000

    top only check lunch pause

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
