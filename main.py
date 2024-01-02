5ba80124c260a2230d1c2a79ca6549fbe1af6802
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:38 +0000

    sense record pocket low furniture

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import array
-def get_array_as_repr(array):
-        return repr(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def calculate_work(force, distance):
+        return force * distance
