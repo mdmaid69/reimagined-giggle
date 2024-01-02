92cfa1c2db9d62183035b2ff7dd9fe4950286861
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:43:12 +0000

    accidentally education medium doctor cross

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
