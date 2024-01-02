4ab925d49f776c15e2c282f18695abaa4fdeb703
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:03 +0000

    circle forever neat worst equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
