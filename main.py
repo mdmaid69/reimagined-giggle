955cecf9bf6ea28effee3ae4eb8f177061c9a750
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:46:53 +0000

    condition reach call not hello

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import datetime
+def get_today_date():
+        return datetime.date.today()
