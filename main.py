0904d42d9dd93e829a0612b5d3eed2b046a4e17c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:30 +0000

    rarely thankyou callout test call

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import heapq
 def create_heap(iterable):
         h = list(iterable)
         heapq.heapify(h)
         return h
-def calculate_acceleration(speed, time):
-        return speed / time
