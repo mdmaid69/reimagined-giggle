60d498a9948053c185fd8f682452a1b43b3c64fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:29:46 +0000

    whistle deliberate later live intend

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
