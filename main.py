0653834d27ae671d54ca5029a85cad6126f67174
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:30 +0000

    thumb hurt regional goup spend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
