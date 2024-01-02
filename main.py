cb7b6d2519dc54278e1c8b013f63bb3161769983
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:03:26 +0000

    snow mend destroy run plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
