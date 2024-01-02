cc6fb52ddbf795ef7fa2e9675588b57048a909a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:39:12 +0000

    shout 573entertainment suddenly scared on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
