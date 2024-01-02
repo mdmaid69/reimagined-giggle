2e6956322699dc5b1f802a70f236936ca0feaf56
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:04 +0000

    open drop burn proof may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
 import array
-def get_array_as_str(array):
-        return str(array)
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
