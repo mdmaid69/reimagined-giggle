709eb60cddfae5e3d03fed7afa054010f90901fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:48 +0000

    buy luck print month solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
