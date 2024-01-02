e8bb211d51f6d030736695f47b848968fa8e529c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:53 +0000

    speed then callout risk display

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def get_array_as_set(array):
+        return set(array)
