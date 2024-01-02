1babbc3572ad969517dbd12d82fa02d25cf501f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:23:32 +0000

    hope play rather job forgive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
