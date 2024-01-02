35946685cae06983e7e2f8da438b478bf8130eee
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:55:51 +0000

    free material remember crash weekend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_typecode(array):
+        return array.typecode
