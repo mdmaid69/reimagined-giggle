2cdfc26650a8085330f690880a2957da6ec6b59f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:02 +0000

    world clear inform file punish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import logging
+def log_message(message):
+        logging.info(message)
