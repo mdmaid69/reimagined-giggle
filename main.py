8ed514285a2d869a123311e8871151341aec1687
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:32:15 +0000

    fit aim underneath guess getout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import sys
+print(sys.version)
+import array
+def get_list_from_array(array):
+        return array.tolist()
