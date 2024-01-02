09c6b272e94e47b13ae347176780b42fb8751ca5
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:53:36 +0000

    accordingto intend doctor accidentally either

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-def add_numbers(a, b):
-        return a + b
+text = "Hello, world!"
+print("Characters:", len(text))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
