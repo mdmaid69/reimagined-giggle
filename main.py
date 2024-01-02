4acbfad312f2e7b2ddd6c778b64f1ae11c49e5e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:46:00 +0000

    regularly artificial road post similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
