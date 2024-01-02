be620236d07e53122902d291d08cc27d8ca4f9c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:59:53 +0000

    information key part likely difference

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
