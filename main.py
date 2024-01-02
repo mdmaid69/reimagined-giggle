cddcf009c761603c1e3c277055255ebff8e33dc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:21:16 +0000

    bottom early seat stage correction

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
