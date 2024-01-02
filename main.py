f79e2c15c6d97a6ccaea2d9707b1e4325019ee3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:39:40 +0000

    design remember piece still noise

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import collections
+def create_priority_queue():
+        return collections.deque()
