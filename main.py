8a0b0512842ebc4e99dd09afb85c1c4feddb3a22
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:31 +0000

    while detail will owe sit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
