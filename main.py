74b41997a89c7af85b03120f398e86c40274dff9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:13 +0000

    of breakinto bet port no

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 text = "Hello, world!"
-print("Characters:", len(text))
+print("Reversed:", text[::-1])
