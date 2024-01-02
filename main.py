bfd0a0097a15a2e2238b26d8c094a88528b00c23
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:45:03 +0000

    announce often shout pay against

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
