78ef9c87c43488ed133cbb869e1a16f98e046997
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:10 +0000

    brown nor somewhere by ought

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+print("Hello, world!")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
