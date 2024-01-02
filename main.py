f447f0006a573a0d5a54b0762f8418c0364bb2e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:16:15 +0000

    value day favour turnaway such

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
