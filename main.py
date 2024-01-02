9d2c07228af1164d7011e933d66568aa533fa972
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:04:11 +0000

    pay Gg nowhere mean unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def remove_directory(path):
+        os.rmdir(path)
