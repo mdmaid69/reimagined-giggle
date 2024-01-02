2187162a9f16219e4f64741f692e0ef0ccad176b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:04 +0000

    furniture press satisfied design remark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import time
+def get_current_time():
+        return time.time()
