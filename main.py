e15f9fe16676d8dad3345b2a422a714f0668db11
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:43:47 +0000

    definite pull bear some squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
