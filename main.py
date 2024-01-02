21c46e1d159131768171665650456f01e122fa33
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:43 +0000

    miss accordingto neat morning report

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
