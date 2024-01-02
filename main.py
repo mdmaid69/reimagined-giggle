834860c789d5427e82445dc0101a1753a5013ea5
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:26 +0000

    traffic new keep matter cheat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
