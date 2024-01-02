deb20e946e99a27319a9d75fbe7b25e9e3fb09fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:12:16 +0000

    goback(to) runover effect smell getaway

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
