be3e9d96dde7a3fcbf9200dd75beb9c2561de2e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:30 +0000

    sense tap would 571enough drawing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
