8c48aba48d92878fc95ebf6a31b6c582d188901c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:03 +0000

    information tune convince fond violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_counter():
+        return collections.Counter()
