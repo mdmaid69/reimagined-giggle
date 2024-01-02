4b9b375331d00ed8d8ca6391484f82352b915118
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:21 +0000

    frighten pull breakdown stare fact

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_pressure(force, area):
+        return force / area
+import sys
+print(sys.version)
