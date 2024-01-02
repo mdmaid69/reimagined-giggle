01d1e3344e74f3ca6be1f6b75ac76803796afbe3
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:05:51 +0000

    compare payment agree appeal pen

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import collections
-def create_user_dict():
-        return collections.UserDict()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def create_user_list():
+        return collections.UserList()
