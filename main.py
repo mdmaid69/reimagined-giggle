e30af535bed367cb9d0f4841bb613c451c3b3c49
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:20 +0000

    active type beside interested history

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
