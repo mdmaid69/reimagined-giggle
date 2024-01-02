6509e727b30910f871cc1bc4c6f2e1b67ee28928
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:02:30 +0000

    float effort wonder behind go

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
