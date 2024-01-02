9bb52025b187cb9e06c5045074bbb5f3363f7ff5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:17:36 +0000

    relax tough explain noisy once

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def split_path(path):
+        return os.path.split(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
