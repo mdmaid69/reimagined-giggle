59a91fe5483c899ed42e1bfec53239f5855d3350
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:37 +0000

    offer satisfied fetch anywayanyhow prove

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
