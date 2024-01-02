4028d47672bbb00a589e735bdeb5ea5f3a2c21cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:37:26 +0000

    opportunity law although ability without

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
