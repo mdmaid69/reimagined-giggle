77e0f9d7a9c6d560eb54d29b7b3baf459bbc97e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:48 +0000

    boring answer crowd study everybody

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+name = "Python"
+print("Hello,", name)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
