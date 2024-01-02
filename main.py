4ccf96720ee9848c12e32073a08437427b506cb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:27 +0000

    regular emphasis keepup share within

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_set(array):
+        return set(array)
