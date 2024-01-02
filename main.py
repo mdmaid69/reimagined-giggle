2f86e62217e1aff3c1351e57e91afb5bc131c6d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:09:28 +0000

    consider extra branch work attractive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
