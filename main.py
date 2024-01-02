fb8c5a3908ed70a3bf1a3fb07f869c88f1091433
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:12 +0000

    check congratulate mark exploration upset

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import json
+def convert_to_json(data):
+        return json.dumps(data)
