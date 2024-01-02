abb3b641f82c1f454bb1c417de632ff534c0884f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:24:13 +0000

    practise cinema birthday blue mixture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
