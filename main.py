7b122a1247d33e45bbfc204a55df91f74a059bed
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:38 +0000

    old score force ability consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
