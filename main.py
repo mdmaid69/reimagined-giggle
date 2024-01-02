215c6b05b718318cabfd91f1d42e5c48f9ae2bed
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:13 +0000

    zero town agreement argue finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_area_circle(r):
+        return 3.14 * r**2
