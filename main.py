2ec21ef041346302c10288b53df6a215355e99fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:52 +0000

    lookup meal hair star apart

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
