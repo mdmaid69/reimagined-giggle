de03044bdd8851c4c313b25061d49776596fc508
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:26 +0000

    watch company fork complete gas

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
