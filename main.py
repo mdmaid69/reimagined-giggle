1e2de7603330bb0767a79e4642e2c05b88a9e344
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:47 +0000

    bother habit figure state winter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def square_number(x):
+        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
