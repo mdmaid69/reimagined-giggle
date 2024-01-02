bf289bd36a247efa73978f07ea9d9afcbdb4bc88
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:14 +0000

    holdup priority voice right system

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def cube_number(x):
-        return x**3
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import numpy as np
+print(np.array([1, 2, 3]))
