a10ec68e4ae1c02cae56e1e277b6245be78e3a7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:07 +0000

    fault appeal season why loud

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_length(array):
+        return len(array)
