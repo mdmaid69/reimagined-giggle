62899a16c765b426e0aa2e41b14d5bf0b7eee788
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:44 +0000

    everywhere stupid satisfactory expensive tap

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import sys
-print(sys.version)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
