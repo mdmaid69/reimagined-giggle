36b5ac9037879574fa8dc137997a4b040af2c387
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:22:50 +0000

    programme special male soak connect

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
