51735441e7418f8128683b67e2cd9568c9796617
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:55 +0000

    imagination scale gas passenger up

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
