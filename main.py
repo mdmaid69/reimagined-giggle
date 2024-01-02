383488a37e7436c1ba79e9aca9fb015c56964c43
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:54:35 +0000

    confident worry grade suffer wear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_python_version():
+        return platform.python_version()
 import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
