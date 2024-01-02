304d98be5d4da79a86f53fe80d7339e4c4a027e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:23:38 +0000

    mention scrap Uu sign open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
