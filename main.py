febf2ccc687c975fec43355678adff0afbf7a1b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:50 +0000

    stiff getoff talk danger easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
