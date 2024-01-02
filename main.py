9d0246e71b32ffa6c188500aabef1eef81355363
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:09:16 +0000

    look soft lookafter corner apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
