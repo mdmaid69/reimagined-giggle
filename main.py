7e53cb243d92e31a6b66056229113b759fe6e7ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:20 +0000

    spare purpose getaway duty hardly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-def remove_duplicates(lst):
-        return list(set(lst))
+def append_to_array(array, item):
+        array.append(item)
