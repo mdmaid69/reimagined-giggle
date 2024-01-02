2979258cb02082422a173af5403859bb9d92a4a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:56:14 +0000

    laugh disagree stamp keepfrom turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import array
+def get_array_as_int(array):
+        return int(array[0])
