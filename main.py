f99ed22d98f864455e0b7691f029b3b39c139def
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:51:58 +0000

    Kk street copy pinch float

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
 def count_elements(lst):
         return len(lst)
