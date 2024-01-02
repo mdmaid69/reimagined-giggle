eddf77d71f3cec0c221a24921f28923e4ff93f59
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:49 +0000

    bar interfere something book she

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def count_words(sentence):
+        return len(sentence.split())
