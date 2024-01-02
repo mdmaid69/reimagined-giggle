73268ea608890223a6ce55e4c782961bcf77c862
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:53 +0000

    tour petrol outside Ee know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import collections
-def create_user_string():
-        return collections.UserString()
+def get_array_as_str(array):
+        return str(array)
