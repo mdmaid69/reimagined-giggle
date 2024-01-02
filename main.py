210de77c1758c393c1f7fc9ae6d6df2befdb943f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:52:45 +0000

    excuse rare Tt shout instrument

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
