2a48a5c5a19be82a490df11642655fd1b2c75c80
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:32 +0000

    Gg actual depend flight emphasis

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_directory_name(path):
+        return os.path.dirname(path)
