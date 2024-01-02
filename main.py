a9837d09015555586c8dac8cbb94de5722e8697e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:13 +0000

    wide probably thinkof smell 571enough

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+name = "Python"
+print("Hello,", name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
