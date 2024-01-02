aafbc735d03e4cd639f360b522fed96c2d079168
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:52 +0000

    write tip show clear survive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
