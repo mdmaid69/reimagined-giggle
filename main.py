8e8ab776a346bad63d59abcfd91e49e3e879328b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:50:11 +0000

    distant desperate level though match

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import math
+print(math.pi)
