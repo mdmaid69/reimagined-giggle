46061434202460c912094c3a3e78ebbda79403f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:21:14 +0000

    run respect right admit fashion

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
