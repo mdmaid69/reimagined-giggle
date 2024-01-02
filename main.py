98096dbee3d5b376b3034e4029326c71819c2c85
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:44 +0000

    off pupil lunch match trip

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
