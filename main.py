42bddbaa4d40ed5ae5d385eb0a72f49624ac18e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:50 +0000

    shock nervous comment acceptable post

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
