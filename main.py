43c6fb9c187e1b696099484a60840aa35258a221
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:52:39 +0000

    regular set tap coat one

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def multiply_numbers(x, y):
+        return x * y
 import math
 def calculate_absolute_value(x):
         return math.fabs(x)
