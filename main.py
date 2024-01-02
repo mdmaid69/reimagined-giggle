944359428f47563bf4af3c769e0f14bb135b863d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:20 +0000

    thinkover 572enter scream concentration letter

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
