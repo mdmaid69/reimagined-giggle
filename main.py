9eb3b608b8b71926ecb35e97cbfa51523628e6da
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:24:41 +0000

    centre refuse collection middle second

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
