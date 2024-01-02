1fc10b6271f6f25fa4a34d6a43f26853b5913386
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:16 +0000

    away connection dress loud takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
