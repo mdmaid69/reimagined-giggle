cf7e2026468f75c05c3cd9c6299e5a226072cc89
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:31:45 +0000

    imagination dream convinced moral thing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
