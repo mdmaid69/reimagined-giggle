49d31117d3c9f65c01f005655029649f40b0b034
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:09:54 +0000

    confusing curious experience form speed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
