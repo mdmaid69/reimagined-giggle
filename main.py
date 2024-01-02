a712408b299d493b59146f02ead756e51c79d9cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:20 +0000

    after week interesting mother protest

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def square_number(x):
-        return x**2
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
