3f21b86772dc7953ffabf9a537cb4a9b9e5467f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:45:20 +0000

    true gradually squash put whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import collections
+def create_counter():
+        return collections.Counter()
