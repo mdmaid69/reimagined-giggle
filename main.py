57dc63af70dc0fabade00cbfd4d9ad6f1f7bc587
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:43 +0000

    wide accidentally further bar tune

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
