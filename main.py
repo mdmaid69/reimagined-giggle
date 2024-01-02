1ffbfb57f4b0689d2c8c1b90048e97d370436294
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:32:29 +0000

    shade start long swim accept

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
