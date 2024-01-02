50a5104d45b57822e9182c10edd244d56f617cf5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:48 +0000

    book mystery geton moral cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
