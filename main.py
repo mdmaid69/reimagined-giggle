f923fd87b4e616856767a392a3bc28bb9861b4f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:55 +0000

    decision mark square turnback makeinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import platform
+def get_os_info():
+        return platform.uname()
+import os
+def change_working_directory(path):
+        os.chdir(path)
