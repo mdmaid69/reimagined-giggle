113cfc84199a1d44a6da70b8923516e5be85c6b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:03:56 +0000

    theirs best seat skirt Itself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def join_paths(path1, path2):
         return os.path.join(path1, path2)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
