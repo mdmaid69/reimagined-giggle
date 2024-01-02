3525170fc51aa605aca516a5b6f15c5838c214cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:42 +0000

    rub well responsible block badly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
