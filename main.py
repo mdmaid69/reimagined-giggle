ebda072eed0c85be96008cdce085855eb4067b25
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:40:14 +0000

    skirt top what release off

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import time
-print(time.time())
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
