462dd70754e93b1e4b5c165c0b3c597886f1a37c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:27 +0000

    very let clever he embarrassed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
