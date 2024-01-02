670f25ff9916913728d04e93996fec6b4a9b332e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:23 +0000

    either lookafter ashamed mean accidentally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
