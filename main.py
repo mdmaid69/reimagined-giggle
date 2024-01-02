965695d7243d7ab01762f6c79453dea7ce20c0ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:06 +0000

    spring bus fence regret climb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def count_words(sentence):
-        return len(sentence.split())
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
