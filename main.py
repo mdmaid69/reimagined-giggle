202244fc910c73f58b06060e73d09c5b15015496
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:33:18 +0000

    stop light main change neck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def is_odd(n):
+def is_odd(n):
         return n % 2 != 0
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
