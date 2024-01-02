8b149c7da558d6324f73bea8a19f70f8343c76c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:07:11 +0000

    decision quick difficulty smell trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import os
+def change_working_directory(path):
+        os.chdir(path)
