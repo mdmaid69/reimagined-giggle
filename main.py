435eba7681fd50c7fcb1d8b22a22e8efc428044a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:59 +0000

    complaint really press passenger shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def is_palindrome(s):
+        return s == s[::-1]
