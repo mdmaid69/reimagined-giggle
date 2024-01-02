2df6cd15d6ce3d47c2d217e5fe5ec754d9144cb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:00 +0000

    field variety wave entry turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
