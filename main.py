5c21d09edf8422802d62ae4e8b828ad7c1e67a86
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:44:31 +0000

    study generally left goaway exhibition

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import sys
+def exit_program():
+        sys.exit()
