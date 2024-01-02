4846e605a21e8f3394f437fc609659dde09d32ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:00 +0000

    equipment each raw maximum word

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
 import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
