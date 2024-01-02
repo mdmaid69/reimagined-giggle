48e1a55b5d1abefa0157a149d36ad117863b2396
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:14:45 +0000

    pencil like under thanks level

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def find_min(numbers):
+        return min(numbers)
