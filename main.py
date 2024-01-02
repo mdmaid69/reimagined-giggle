ca598dc8c6d29aeedf539be5c40573e5c3b9ddc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:06 +0000

    section bite welcome allow cost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
