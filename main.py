e3cfe7d0fc589ac45e00ac1b06f78a7837eb51c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:19:55 +0000

    base makeup back whom profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import os
+print(os.getcwd())
