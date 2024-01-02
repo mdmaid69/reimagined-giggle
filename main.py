fa4fc814340ec9e518678e09ce2abb05d7edab83
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:25 +0000

    pull getup best post giveup

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
