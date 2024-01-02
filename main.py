576dbed853794520a8eb82301f186cb532acf8e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:09:17 +0000

    information stranger foreign travel bringforward

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
