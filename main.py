a338902acdeaa048a074beac75b2473de5666a69
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:56:39 +0000

    report satisfactory separate disgusting thank

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
