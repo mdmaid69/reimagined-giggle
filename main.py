d1738036022aa7deba117ff080271ea7f3452a42
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:53:58 +0000

    interruption pleasure accidentally congratulations wealth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
