3ebc22c86517c0d9ccf5d9af57d55e61a59f3f75
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:06 +0000

    frequent offence shelter should replace

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def convert_array_to_unicode(array):
+        return array.tounicode()
