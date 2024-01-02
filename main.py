0fd46d8311127d6c067509ad547ad2c9a5a57a83
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:29 +0000

    cut even regular rent store

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
