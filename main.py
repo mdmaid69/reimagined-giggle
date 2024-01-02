d1e4939f5a0741f0217da9ae52922f31730be208
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:10 +0000

    precise confidence scared patience borrow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
