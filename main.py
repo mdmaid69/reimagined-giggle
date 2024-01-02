df83c9994ba9eb27b09bb4d5eaa6f6082efdc392
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:39 +0000

    train face like proof hot

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+def count_characters(sentence):
+        return len(sentence)
