d97f9dffce8c672e03e7196528b2ccc4b8c14668
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:27 +0000

    black issue talk moment programme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
