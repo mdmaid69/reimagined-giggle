47064062cb1a7aa28073ce0830a651c2795770e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:27:57 +0000

    behind die mention wrap anywayanyhow

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
