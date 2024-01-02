ecf6969797b31a2a746405dfac2e112626d9392a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:56 +0000

    fit question sound purpose getup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def sort_list(lst):
-        return sorted(lst)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def is_even(n):
+        return n % 2 == 0
