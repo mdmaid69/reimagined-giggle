9421e418c404184e9c0fb7577ca15e9b8ef95d71
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:50:07 +0000

    illness spite forward wing shape

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
