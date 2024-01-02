3b855333db166f046380b61993b4ff7a3f5997db
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:27 +0000

    offer contribute convinced plan provide

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
