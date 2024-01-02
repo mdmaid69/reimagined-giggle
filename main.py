994289b7c1801291d2bd9106d36c449f2d4189c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:20:09 +0000

    single apologizeapologise support scare mark

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
