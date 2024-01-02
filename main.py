3884029cee900acf0ba35270c46acc2644cdbdaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:28:58 +0000

    less finish excellent 571enough size

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import collections
+def create_counter():
+        return collections.Counter()
