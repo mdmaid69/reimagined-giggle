84aa173f037ab5f44a800febbde5bb8f12531ce2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:20 +0000

    polite star throw side steady

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def is_palindrome(s):
-        return s == s[::-1]
+def sort_list(lst):
+        return sorted(lst)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
