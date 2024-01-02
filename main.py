ef0f83b7680810b23c90de7511dbb576fcfc4736
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:47 +0000

    power now copy judge out

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import os
-print(os.getcwd())
+  def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
