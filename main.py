d569115dce673f3ecf19a85572aadc6b74ef6bcd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:00 +0000

    clearly hang rare their turndown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
