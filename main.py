786f927ade3adde346969838de819f08c66ffcd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:12:01 +0000

    pay influence anywhere stone at

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  def convert_to_octal(n):
+        return oct(n)
