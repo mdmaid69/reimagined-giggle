5b68179f2186bee7ef1aa136e732bf0987c7db24
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:24 +0000

    spill sound base kind calm

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-import json
-print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
