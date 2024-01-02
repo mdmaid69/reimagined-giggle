b9d2b09045fabb9e3ce9c8e654f6619b73e29b91
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:55 +0000

    gentle damage exercise surprised tragedy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def convert_array_to_string(array):
-        return array.tostring()
-def find_max(numbers):
-        return max(numbers)
+def get_array_as_bytes(array):
+        return bytes(array)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
