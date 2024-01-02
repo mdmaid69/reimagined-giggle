49627f292ea073307cfa7729d97fbecda53a7650
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:50:19 +0000

    sing speak sudden lay shallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import os
   def get_file_modification_time(file_name):
         return os.path.getmtime(file_name)
-def add_numbers(x, y):
-        return x + y
