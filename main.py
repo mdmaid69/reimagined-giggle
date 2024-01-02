5fd278b050d2c6b695cd19dac239a4cef165c27f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:54:39 +0000

    although well half store proof

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def subtract_numbers(x, y):
-        return x - y
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
