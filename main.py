c5532dce0e3d63d2cf9fee7b995aaf1cf709789e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:17 +0000

    picture guess please feel slowly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
