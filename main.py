d660025822c7d10739b392ddff29da6cd5a7c943
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:58:31 +0000

    estimate naturally responsible century Ee

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
