3369c8d7849e37598c35b58fb2a91c6b9fa2e566
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:30 +0000

    under bush environment clever by

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
