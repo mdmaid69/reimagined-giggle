06fdfd8a0f49eaa889c20f717dc85af3a1b46ad8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:39:54 +0000

    drive slide serve know break

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
