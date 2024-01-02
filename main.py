474b1fd6d654bc244cf04b40ec3a540ae6ef8b27
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:40:34 +0000

    gift human definite food oil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+print(math.pi)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
