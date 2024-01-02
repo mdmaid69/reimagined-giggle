8413c4a5ca4f4a242a2c5f2c29089e4d74ae4a68
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:19:00 +0000

    dance listen cap perform actually

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
