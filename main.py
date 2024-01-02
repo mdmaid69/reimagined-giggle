4a0390a25184f18288e92808403c479fc22f4793
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:20:35 +0000

    dress turnon common tragedy sign

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import collections
-def create_user_list():
-        return collections.UserList()
+import array
+def get_array_as_list(array):
+        return list(array)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
