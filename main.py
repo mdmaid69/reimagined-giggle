11a7adbd46e392bf8a8252619036656f49c0a758
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:08 +0000

    either ready try down 567enemy

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-print(sum(range(10)))
-import collections
-def create_priority_queue():
-        return collections.deque()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
