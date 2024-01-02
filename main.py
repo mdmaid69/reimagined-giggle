1fb4095064a41f514ddae2f94e3117cb5ac87e96
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:56 +0000

    warning comment combination school hold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
