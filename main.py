8aa3c4d531428931689b98db814997be2594b2bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:15:35 +0000

    road attach grade calm listen

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
