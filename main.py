137784a833b17610a1b8f3ee964ca5ce729ab17e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:49 +0000

    frighten cruel peace recommend unfair

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  def remove_duplicates(lst):
-        return list(set(lst))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
