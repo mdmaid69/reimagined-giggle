0619704bc682669ea7b1c81f11244453985eeea1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:24:37 +0000

    healthy ignore warn general formal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
