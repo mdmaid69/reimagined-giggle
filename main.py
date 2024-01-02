fa43628cacaa34b36e1c271dbbb3317d146f8b0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:55 +0000

    bar anyone calloff target occasion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
