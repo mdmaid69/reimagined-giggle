7391350c4208deff1a8f2f2a90096b5da902c163
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:55 +0000

    thumb room consist inside common

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
