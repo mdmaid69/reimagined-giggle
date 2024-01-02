03633fbf8d3a4949f8f595eddeb9bb73198a900f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:09:43 +0000

    without unlucky under breakin indeed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
