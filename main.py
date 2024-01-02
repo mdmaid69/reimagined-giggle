07401a5049c4d6efd4304ac907515e3a73e4d7af
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:40 +0000

    season extra own invite man

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import array
+def get_array_as_int(array):
+        return int(array[0])
