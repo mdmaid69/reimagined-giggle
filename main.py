a25f5ca6e8159d009caa83cda75ff9710504d11d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:00:47 +0000

    increase part friend carry true

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
