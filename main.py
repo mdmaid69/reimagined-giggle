0f77c9189bdec80881685331e78def7ffe0365e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:51:46 +0000

    common can talk complicated wash

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
 import array
-def convert_bytes_to_array(bytes, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
         a.frombytes(bytes)
         return a
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
