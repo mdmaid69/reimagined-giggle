d19cef5b55f5fc5fbb849caebfb1c96f9778ce74
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:54 +0000

    thing second cap suddenly cost

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-def convert_to_binary(n):
-        return bin(n)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
