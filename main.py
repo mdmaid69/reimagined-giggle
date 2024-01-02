a6043d35946088c07f1068b9d5fe14898a62df35
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:17 +0000

    illness disapprove imagine only mean

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
