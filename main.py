a4c8b73c194d04a703be04097b6319a9e086df09
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:33:41 +0000

    wise relationship possibly dead inform

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
