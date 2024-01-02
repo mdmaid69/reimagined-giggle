370f714a270f7523835fff2b51038c00b12a4efc
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:13:37 +0000

    will unlucky right close their

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_area_rectangle(l, w):
-        return l * w
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
