a4ff9dd29c6e12a5ff260a467c1c7dbf7e556e43
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:17:11 +0000

    whenever Rr talkoutof debt rare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
