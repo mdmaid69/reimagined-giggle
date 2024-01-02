925a7a317143bf2d8a232336e45fb70aaf1e0f37
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:04 +0000

    chin government print move most

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
