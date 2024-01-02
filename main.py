56ba28803bb65ded0ae1b220ba2fdde55e8ca3f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:49:59 +0000

    respect worse message sick convincing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def calculate_power(work, time):
-        return work / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
