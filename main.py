029484274b15810f1e83ae6ff29dfe47d6ba7e4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:39:14 +0000

    taste minority large justice proud

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
