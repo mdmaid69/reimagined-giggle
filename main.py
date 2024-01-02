a8a8c31c997bab1077be08f34c0c3f0ff48aaea3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:43:27 +0000

    basically hardly quarter pour half

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
