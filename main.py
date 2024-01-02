e556cafc0ba26e2f375ae8a3ecc3b415dfa75e06
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:02 +0000

    street thinkthrough hot refuse high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
-def sort_numbers(numbers):
-        return sorted(numbers)
