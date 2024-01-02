07b0062a991b01fe49d7c59b5a172441eb1bff11
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:53:59 +0000

    present study weather skill themselves

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import tensorflow as tf
+print(tf.__version__)
+import json
+print(json.dumps({"name": "John", "age": 30}))
