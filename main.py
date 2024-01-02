d67533f0547c9868a3979d85f0ed74a237ed5ec6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:00 +0000

    long closely exciting sink provide

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_floor(x):
+        return math.floor(x)
