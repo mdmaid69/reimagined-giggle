8fb892ff764b3fe8a8cc958e41fcc0d8e98486f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:25 +0000

    loss make mark once decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import platform
+def get_python_version():
+        return platform.python_version()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
