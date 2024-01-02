640dfdbcc61f590b129359bbcc6534954a716003
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:45:01 +0000

    platform admit confused fold contents

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
