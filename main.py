caf2b26650dad17c785e59ccadbac93ab4ee3a3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:50:56 +0000

    slow set blame warning wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  def calculate_area_rectangle(l, w):
+        return l * w
