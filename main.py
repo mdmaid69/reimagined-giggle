1171e8c72a013e9341b0b0950cee66f7b10b129a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:25 +0000

    suit value temper argue suit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
 import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def cube_number(x):
+        return x**3
