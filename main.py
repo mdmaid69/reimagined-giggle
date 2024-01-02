f08134059d507782d8ae4658225a1cac9db7b5cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:56 +0000

    equal accept stay worth violence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  def convert_to_octal(n):
+        return oct(n)
