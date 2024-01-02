2b85936ae350424ed338004889b5bc21cbb3e420
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:29:33 +0000

    sight swimming post float holiday

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
