13be81c750ae13374b30461be960c140460fe73d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:47 +0000

    soak while disappointed wind murder

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
