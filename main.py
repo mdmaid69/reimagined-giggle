5af01c215b57f3662433de8b5c36d32bbe932c35
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:57 +0000

    such gooff floor through extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
