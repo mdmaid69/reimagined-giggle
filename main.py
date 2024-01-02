4cf7baa8c05d11e99d5c5981a81825518f3fdd7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:12 +0000

    influence effect moment she Mm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
