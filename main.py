2ca7c03d0920bedb049df54f03ba9c49dfd802a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:49 +0000

    might dark formal hope awake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_unique_words(sentence):
+        return set(sentence.split())
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
