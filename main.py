5f65a3af3d1675315658cec729155d1167fa4842
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:42 +0000

    breakup slide ignore accept fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+def calculate_power(work, time):
+        return work / time
   import os
   def set_environment_variable(var_name, value):
         os.environ[var_name] = value
