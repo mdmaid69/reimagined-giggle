8a1a5bc1e9942fe6a004620240b8b2758491c553
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:33:03 +0000

    abroad introduce shallow party away

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
