0375522fa67db1991e38ca5f3a3811ca328e2e23
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:54 +0000

    recognize present conclusion crash blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
