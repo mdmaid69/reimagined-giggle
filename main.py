feb501eb2b1f735035a299655c33ac295288bc34
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:13:53 +0000

    wise defend them region police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def calculate_power(work, time):
+        return work / time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
