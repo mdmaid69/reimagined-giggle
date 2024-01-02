a8a6516b2dd64d8a61c85f91038aa64afd638a64
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:15 +0000

    spring Vv confidence method fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_area(radius):
+        return 3.14 * radius * radius
