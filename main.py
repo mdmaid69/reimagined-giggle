c87198b948ca1e576854403f596144519390676f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:18 +0000

    already actually down fast recently

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def square_number(x):
+        return x**2
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
