f74d791132ed405437a8af94074963154091fc5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:01:09 +0000

    recover comeout original feed picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
