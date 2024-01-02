3c9a6d230daa64b986ebdd8e7a70a2ead2819254
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:22 +0000

    copy debt judge oil dance

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
