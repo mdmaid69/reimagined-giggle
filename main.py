ea1622432ac55746d0ef7801032f8b67f623f0f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:11 +0000

    theirs brain runaway unpleasant female

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
