f07d25e79d589379ce9b7c573c7a9437614c9e00
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:38 +0000

    against run getout closely fasten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
