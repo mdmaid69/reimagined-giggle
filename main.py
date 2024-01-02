d247847145c95cf218bfdb696116a37ad571dc5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:59 +0000

    telephone sure bringdown other manner

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
