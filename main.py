c36a44d1fc0573173f88c79be78397a23769b0eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:39:13 +0000

    thought classroom discussion behind tonight

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import time
+print(time.time())
