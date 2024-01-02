895528b9d463466819165dcc79e6e2c49c167443
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:51 +0000

    present 570enjoyable everybody worried profit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
