c669e0f470a3eaf91c0c4da9134c296ab6c8f334
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:50:46 +0000

    wherever keen steer switch major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
