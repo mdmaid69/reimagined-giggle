3594f6b2fa7b93020e1a9fa8daad795e8aac594e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:44:47 +0000

    physical sit show Cc whom

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import queue
+
+q = queue.Queue()
+
 for i in range(5):
-        print(i)
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
