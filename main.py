c4844855c1d7ac925a29f793d2b7258984ee4292
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:19:47 +0000

    calloff serious touch Oo wire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def reverse_array(array):
+        array.reverse()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
