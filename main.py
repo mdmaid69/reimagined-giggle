a04af7b1201fedb99ad7904de4bc28978363b0a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:17:32 +0000

    centre claim dry long successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def reverse_list(lst):
+        return lst[::-1]
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
