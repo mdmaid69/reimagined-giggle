4caa4c19e4945b074968844ccc5e5282d8c97d79
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:52 +0000

    win unlucky bin this term

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
