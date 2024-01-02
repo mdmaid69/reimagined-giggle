f05701d60eafb3cbe653207cb37a064d0ce10c45
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:24:14 +0000

    bored swallow emotion fire size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
