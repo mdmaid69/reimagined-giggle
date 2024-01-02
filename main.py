9927b41ef3b13c8c3fd40b9bfad649136c23b674
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:10:04 +0000

    admit identify find Jj worse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
