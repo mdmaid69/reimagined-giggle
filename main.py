f01a60e8c611b4869017f13ac90ca7d946fc58a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:11 +0000

    preparation study damp injury lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
