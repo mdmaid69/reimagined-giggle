8b53ef9f433654b98dc0e71b44bb1e06f92c025c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:55:06 +0000

    hurry mail first silent find

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
