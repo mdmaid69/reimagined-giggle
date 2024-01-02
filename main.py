7db6cc5e0b6281ad07dca8c89d97dff61e3c9302
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:27 +0000

    life gap scare gentle whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
