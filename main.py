6164c927cd9255d16da9d67bbf168178ca7822ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:18 +0000

    on address position brother slowly

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def clear_array(array):
-        array *= 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
