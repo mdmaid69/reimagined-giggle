0d91625048f2878a85266af739ccbe3bbe2cd211
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:44:38 +0000

    disapprove family anywhere slowly Vv

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+def find_max(lst):
+        return max(lst)
