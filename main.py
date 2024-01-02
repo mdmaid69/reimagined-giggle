bde809b93842ef1e436da9fe274bd8c03dfbdcf2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:34 +0000

    roll accommodation listenfor soon ticket

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
