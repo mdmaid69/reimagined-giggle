4fa44ba5e4ff313d9e9538eb8616a02a325e9836
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:02 +0000

    please admire friendly getout no

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
