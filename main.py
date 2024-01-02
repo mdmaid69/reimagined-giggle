e10d7fa92bfe2af8891541361d1ef8c2209f043e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:04:43 +0000

    fall workon feed affect run

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
