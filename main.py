06d28b313311aa32750e28fa26149483961de6b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:51:12 +0000

    match mark breakinto water police

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def find_min(lst):
+        return min(lst)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
