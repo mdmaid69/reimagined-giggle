20829846d246b4b2b6ed9ca21855772a5ffcdf02
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:27 +0000

    action public after price before

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-def calculate_work(force, distance):
-        return force * distance
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
