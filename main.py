16fb55b52a986d05044dd5be0f62f318091f82b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:34:29 +0000

    language loss Ii mess high

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
