884d6a1fc92de5bc62768abc76bd872f1698ead0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:09:02 +0000

    alive print style suddenly classroom

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_work(force, distance):
-        return force * distance
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
