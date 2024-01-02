4f22725059f419b0a78296749811a17b8697ca97
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:43 +0000

    consist finger fit disappear understand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import platform
+def get_python_version():
+        return platform.python_version()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
