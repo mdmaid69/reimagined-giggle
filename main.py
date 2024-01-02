38b5a687544b1fb1174b67e3e3adee96dc894869
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:43 +0000

    arm exactly but breath pass

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
