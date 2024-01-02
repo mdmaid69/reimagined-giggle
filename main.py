42bc898412d645267b3fa33b77d66ce6be92c658
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:58:20 +0000

    I commit something fresh lookaround

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_min(numbers):
-        return min(numbers)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
