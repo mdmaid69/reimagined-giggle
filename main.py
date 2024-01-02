0ebf665a86e076a80ad0ee84a26f47f953ca04d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:02 +0000

    another instead bread Rr emotion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def find_unique_words(sentence):
+        return set(sentence.split())
