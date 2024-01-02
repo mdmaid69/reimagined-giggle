6747d1152f85a1aac6690785053e5923b7cdcc9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:36 +0000

    hurt choice discuss lay telephone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def reverse_array(array):
+        array.reverse()
