f5160a483404414244e582d4fee0251eacc1c3df
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:43 +0000

    again wet broad raw nigga

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+print([x**2 for x in range(10)])
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
