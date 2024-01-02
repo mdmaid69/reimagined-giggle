ee4bc97087445e9d1f00a529dbb12907adadd461
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:59 +0000

    off away wealth month suffer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_complex(array):
+        return complex(array[0])
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
