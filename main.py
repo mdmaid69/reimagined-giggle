094f2bb085c46f89d06abc4fa3f1d372aa89e4a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:46 +0000

    preparation embarrassing strange count human

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def calculate_acceleration(speed, time):
-        return speed / time
+for i in range(10): print(i)
+import platform
+def get_python_version():
+        return platform.python_version()
