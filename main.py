4b8e7ce5211ab1df84cd4fa391e9726cbf606e6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:44 +0000

    wash peace towards something disappear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def find_max(lst):
+        return max(lst)
