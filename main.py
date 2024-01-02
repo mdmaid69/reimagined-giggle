a55b38582219b9e67a3d0db60190f257335d17b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:15:42 +0000

    possibly picture walk process treat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
+import array
+def get_array_item(array, i):
+        return array[i]
