151620f38cefc3554e2cdccf614b6745a18dfd7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:30 +0000

    569enjoy big nose recent she

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
