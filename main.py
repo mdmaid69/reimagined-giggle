7d1b6b855767adb3a4139676a6c328a6de3663e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:01 +0000

    taste alone themselves sign damage

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
