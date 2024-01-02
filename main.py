a0d0d6d0bb4709fc5d98701066bead27200826df
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:32 +0000

    offend anywhere convince lie glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
