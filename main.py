c6856a20d9ccbe91e0baaee347a56a20b513245f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:15:21 +0000

    raise flat claim university wash

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
