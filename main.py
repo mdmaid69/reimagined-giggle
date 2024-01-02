916de3f40caa06521e44ed8ab86548aa2b01a457
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:42 +0000

    comment before coat blue woman

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+def find_min(numbers):
+        return min(numbers)
