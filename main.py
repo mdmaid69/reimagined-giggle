f86d1f247718f3a1a27931d9cd0c7015189f4d41
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:57:50 +0000

    fire feel march little stamp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
