462ce2dae0b9cba0438243ecb056dc91fd9b0317
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:41:56 +0000

    yes demonstrate read colour brush

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_as_bool(array):
+        return bool(array)
