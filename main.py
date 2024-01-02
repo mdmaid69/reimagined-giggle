89ea5c6fbc58834f9dbc4e9458e0df2a25a5711c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:38 +0000

    announce wind this now measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
