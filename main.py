affa2d757c7437e4d069c49c98ef064f5f3a60e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:01:29 +0000

    tip step sweep comeout brain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
