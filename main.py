06894799a5fcf1c8fc98b2f0df1eb56c6ae2a31a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:43 +0000

    pupil explain turn hers backwards

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
