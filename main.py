e63b52b558ad7f5703e16e822b709c786a3361a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:37:28 +0000

    drive unusual keepaway feed contact

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def find_union(list1, list2):
+        return set(list1) | set(list2)
