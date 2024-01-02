a3348c117672cb5da7355f5d9c3b15c0defff28d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:40:04 +0000

    twist good concentrate bread slip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
