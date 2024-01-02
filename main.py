325601ee4a2d6f0cdcb553d513b6d8422af7b51e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:04 +0000

    keep downstairs ahead pinch block

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
