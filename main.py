4677a67eaa8beb5e510e9ad21d7c5dc62acadb63
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:51 +0000

    best object backwards so meanwhile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def reverse_list(lst):
-        return lst[::-1]
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
