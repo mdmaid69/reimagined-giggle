1849a7caf52c9bdfb57074b92771e36170eacffe
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:27:18 +0000

    settle worry appeal silent react

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def find_min(lst):
-        return min(lst)
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
