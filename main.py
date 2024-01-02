0dc8bf4b993d3d76e58f39a681292020fac6bb1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:49 +0000

    pass lookup duty freeze long

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def find_max(lst):
-        return max(lst)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
