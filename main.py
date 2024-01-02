33fd9e36ed034062eb45823c5a7cdc49ea33b88f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:21 +0000

    boil remain impressed likely purpose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  def get_base_name(path):
+        return os.path.basename(path)
