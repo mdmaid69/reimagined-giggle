c54c97025cb71f7e6981c04c0ab2e5351bcfc5c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:13:25 +0000

    declaration clear suit occasional perhaps

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
