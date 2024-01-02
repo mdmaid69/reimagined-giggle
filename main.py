66fd9cd9eddf335735a9e6c02ac28908a99faf10
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:32 +0000

    ever child reply return boring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
