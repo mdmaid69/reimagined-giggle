03ec1b9ac492bd11c5bae235975bba01d5f3386e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:45:41 +0000

    damage yet show wander work

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_item(array, i):
+        return array[i]
