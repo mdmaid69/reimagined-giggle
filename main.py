37ff4ae135c4a4074c5d47964493ff50ef5c8499
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:17:48 +0000

    admire put punishment guide herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+def find_min(numbers):
+        return min(numbers)
