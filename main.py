12693026e9b56c839bf0c3eed773cb1416a9b6f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:48:59 +0000

    trouble amount tidy difficult freedom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
