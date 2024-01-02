76bf140cda8a6da51e42f1061b5281020a9a2fab
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:49 +0000

    sleep manage over nurse knock

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
