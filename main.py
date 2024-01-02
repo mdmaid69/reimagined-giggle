c427634958f99272a514f69ad2e70d99aaf34a3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:34:23 +0000

    port belong rain let bank

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
