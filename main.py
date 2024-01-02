f25d02bd7356c702a53a60b19e6f0b84236b8a1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:03:48 +0000

    careful still software contrast refer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
