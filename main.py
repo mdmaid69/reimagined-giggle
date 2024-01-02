a3f9c78f896e8904a61489b47f00c66da903b6e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:54 +0000

    while satisfactory interest inquire know

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
