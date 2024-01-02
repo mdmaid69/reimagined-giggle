139ba007ace012cf1c520753a46da3b80ae876c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:48:58 +0000

    life spin dig accordingto habit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
