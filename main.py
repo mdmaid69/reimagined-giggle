ec25388bd16ba7f664061600167b162ad5c2c7b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:51 +0000

    understand raise breakfast respect certain

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
