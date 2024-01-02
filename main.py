75ad9e3ccab35087d1d598f6340dd5e500d60901
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:34 +0000

    half branch nerve every criticize

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import csv
-def load_csv(filename):
+def read_csv_file(filename):
         with open(filename, "r") as f:
         reader = csv.reader(f)
         return list(reader)
