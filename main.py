5537ae62c92c8065e1589d17259aeaa7120325fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:27 +0000

    snow alone failure proud model

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+print([x**2 for x in range(10)])
