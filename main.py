c4bbe36509eb8417139d514affe884aa7462ff5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:17:23 +0000

    branch smile share bread live

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
