5e84a1258f97cbd1c9066edc446c4ab429803a5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:06:02 +0000

    sensitive trust second fill will

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        return sys.version
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def remove_directory(path):
+        os.rmdir(path)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
