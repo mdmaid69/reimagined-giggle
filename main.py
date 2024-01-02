b3b6e8bd7ac633702d8f7f9ebbb7947bf438825f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:42 +0000

    lock imagination appearance produce related

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
