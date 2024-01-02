d02451c7634e049c7fda03096b912d01fbfb1ad9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:17:32 +0000

    shade miss relationship eager concern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import sys
-def print_python_version():
-        return sys.version
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
