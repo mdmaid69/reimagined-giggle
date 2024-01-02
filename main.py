cc4aed2a56af6553f42a2ad00036e4449b568f39
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:48 +0000

    total base sure brush force

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
