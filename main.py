f59d97dfab82a0154f72e258c077194ed9271554
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:50 +0000

    pot contents stranger unlucky find

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def remove_duplicates(lst):
+        return list(set(lst))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
