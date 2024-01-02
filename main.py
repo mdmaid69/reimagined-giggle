349ae73d4c3824499cf6fe0e9b728c703ebc471d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:33:57 +0000

    plenty accidentally ours preparation tidy

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
