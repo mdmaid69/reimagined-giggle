a28eb5fee06c681a106e8d84a0a1b828bde03d9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:21 +0000

    serve black copy contact draw

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
