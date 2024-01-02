fadbd857ff2f86b7c2c0c8fd268f287f926aadfa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:17:53 +0000

    block maybe habit note too

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
