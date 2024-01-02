40a4ec8abfc6cf0555e9d198312950bb816fcdff
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:49 +0000

    answer occasion check dig noise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
