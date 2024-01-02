4597ccfbc8573a333c67167593187396e7e17220
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:22 +0000

    disappointed go maximum suddenly chance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
+def calculate_floor(x):
+        return math.floor(x)
