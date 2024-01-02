8ea47723f991573362fa50a8b43af8968bf43d59
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:02 +0000

    number or love kick which

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
