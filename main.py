9af11c39f30084f608627427565a8f39b6c6b873
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:39:17 +0000

    store paper smell slip command

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def sort_list(lst):
-        return sorted(lst)
+import re
+print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
