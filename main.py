5aae1cf91fdf2e2cd75bd93209e0a61746069f6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:02 +0000

    divide possible part record proper

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_list_from_array(array):
+        return array.tolist()
