85f2c5872d6a317839b54c16c2882de74c8575f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:57 +0000

    manage powerful Dd build once

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  def add_numbers(x, y):
+        return x + y
