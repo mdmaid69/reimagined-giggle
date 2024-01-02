805146087b4ee2f9b3b7478dd66d6e6d39e08424
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:04 +0000

    treat pleasure Ll mend sting

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
