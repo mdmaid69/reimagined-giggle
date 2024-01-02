a406113aa52af49d7ca63f63a253cd771df88e0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:36 +0000

    prevent until cover region judge

--- main.py
+++ main.py
@@ -2,5 +2,5 @@ import os
 def get_file_size(filename):
         return os.path.getsize(filename)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
