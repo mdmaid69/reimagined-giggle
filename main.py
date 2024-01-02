d0bfb20f592ceb5ba3f04a55122097d6f79be865
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:00 +0000

    release impressed answer authority plant

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def add_numbers(x, y):
+        return x + y
