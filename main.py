6bc0494eba6972c45e1b9c2b705556161d982593
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:05:48 +0000

    chin agree wide duty interview

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
 import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
