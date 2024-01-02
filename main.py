17f76b2133c424511effda8711905cd2c97eb01d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:04:20 +0000

    occur open capital suppose constantly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
