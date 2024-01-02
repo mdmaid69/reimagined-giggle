95ce73b86599553537f6b43455ac45cd71623b35
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:13 +0000

    turninto compete serve finally join

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
