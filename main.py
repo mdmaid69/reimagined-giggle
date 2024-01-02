9c005e0544e0743db510063fb316df5ad6d50f9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:37:52 +0000

    perfect term embarrassed fight spoil

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
