66585368caf2b2f18321734c525ab53f4016bb7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:02:28 +0000

    impossible impressive 572enter while afford

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def add_numbers(x, y):
+        return x + y
