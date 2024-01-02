e6fc45803d1e44583579ed7fd7084790475da4cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:03 +0000

    exaggerate deep town exploration pull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
