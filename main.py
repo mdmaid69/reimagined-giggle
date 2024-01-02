a4a7303a138be076d24101318d5118bfd03640f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:43 +0000

    count well fault brush each

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
