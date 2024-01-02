da66635b9088dbfce5bd038d266c77ce6e59d2d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:43:19 +0000

    fight name order wild past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import os
+print(os.getcwd())
