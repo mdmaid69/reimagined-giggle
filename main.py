bfd05a76a8eb0b6628e9962806435bd8f4a0bfaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:12:58 +0000

    crisis battery produce score confusing

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import numpy as np
+print(np.array([1, 2, 3]))
