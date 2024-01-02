34e10a6a85b01d9b88b4a7329517daad5dc7b4a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:54:17 +0000

    introduction exam control mine moon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_hex(n):
+        return hex(n)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
