febcd90f3252d929273997bf8e0a122622665f36
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:27:05 +0000

    disappointed polite continue workon comment

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
