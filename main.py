19d193b095b11f3b7aa9c337f40bcf2591a07bc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:46:17 +0000

    promise fill break smart turnover

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def is_even(n):
-        return n % 2 == 0
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
