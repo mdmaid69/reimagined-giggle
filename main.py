ec531a0909c9129002360dd70487b7ba0869c2b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:52:49 +0000

    reaction fall judge treat consider

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def find_max(numbers):
+        return max(numbers)
