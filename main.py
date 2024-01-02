c4cb37880c9f6dbece3dddec3690ba05764ac72a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:16:08 +0000

    inside my petrol steep defend

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
