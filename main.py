8667d8a35dbb8f80e7661ea7b64f9e6d77d4c129
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:11 +0000

    tap boil turn 568energy people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import os
+def get_current_working_directory():
+        return os.getcwd()
