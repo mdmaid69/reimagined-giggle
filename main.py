6a302ac01edb85ad687ac7d7bd01e30ef347fd30
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:02:26 +0000

    various conclusion relax serious tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
