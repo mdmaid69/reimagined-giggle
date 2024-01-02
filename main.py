03ca7192098b3c43d71bffe35d9064be27aac6e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:45 +0000

    stamp sure number on anywhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
