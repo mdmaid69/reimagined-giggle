695ca685a2882d86034be62afaf49e5b304f6ea8
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:55:20 +0000

    swing convinced holdout suckme otherwise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def is_odd(n):
+        return n % 2 != 0
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
