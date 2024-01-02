f9d6213eb7b77a4dad9f58c70a6177098b1c57a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:42:25 +0000

    work cause comeover trick swing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
