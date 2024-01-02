5021c248531c1217d0d6ea96608924e87054a5d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:58 +0000

    maintain listen proper now corner

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
