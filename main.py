21ba59e6bd7a09536277d973976f1e4154271993
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:41:02 +0000

    fit measure day smile care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
