fcdb007dd9454a2134d059fd29b3b17aa35356ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:09 +0000

    allow book discovery look hear

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import sys
+def exit_program():
+        sys.exit()
