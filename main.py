da31cb25affef2bb6bee9bd048897ad283f74674
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:52 +0000

    person thing movement feed emphasize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
