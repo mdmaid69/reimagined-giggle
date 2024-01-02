9e22e8d02d611acea8286ea90eb4bc68e187ddf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:25 +0000

    pretty character object brush area

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import sys
+def print_python_version():
+        print(sys.version)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
