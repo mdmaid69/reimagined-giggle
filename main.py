eeb0247dc67575518b1ed0cd31fa1c1705d6b395
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:05 +0000

    pressure fork crowded without charge

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  def subtract_numbers(x, y):
-        return x - y
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
