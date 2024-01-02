fa9e2074f1f9e74ad81c521e8b56966d3607942f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:51:41 +0000

    unable mood project situation field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
