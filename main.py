6ca0d02f82f8cf277689bd5f3494b0e5855c4b2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:22:08 +0000

    before wall behind able neck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_list_from_array(array):
+        return array.tolist()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
