495b07882453f5adf10c7849914fd8533421ddf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:06 +0000

    colour spin level place round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
