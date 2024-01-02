53d43663c5132ed87e2ea4e15bab6f8a19c718d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:00 +0000

    line upwards only clue until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
