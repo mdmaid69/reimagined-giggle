3b9bcb332d8013d864fa6c7ff13d952ab3df2879
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:32 +0000

    Ee land colour service hospital

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
