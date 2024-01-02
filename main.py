12ea74e0084516f0c10e784b3808cc0a1fe7b659
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:17:37 +0000

    watch face watch confusing friendly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import os
-print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
