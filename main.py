45392dfc3b9fdd0f7a8ef90085471ba2ac932d2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:29 +0000

    believe patience feel loss nose

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+print(os.getcwd())
