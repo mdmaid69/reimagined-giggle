9c94d12032712e6b2944f994963cb038f2ee17d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:00:30 +0000

    bound holiday yours bound lookaround

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
