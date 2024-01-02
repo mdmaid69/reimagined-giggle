c7340718fa1c5869d9768919f12483a096f53339
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:55 +0000

    goon finally remember forgive rare

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(10): print(i)
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
