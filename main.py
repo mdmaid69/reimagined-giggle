3eacf9db2d0e998247758cbfba955c2295282dc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:13 +0000

    train between black deep never

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
