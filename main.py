dcc4fdd2a4899ffa7292314cb5785b3498c1ae21
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:53:55 +0000

    sentence winter mine maintain secret

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
