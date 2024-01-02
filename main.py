7e5850bb9f673f3412f9bd051ab83bece7286c47
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:32 +0000

    blame switch apart deliberately cross

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+def convert_array_to_string(array):
+        return array.tostring()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
