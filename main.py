79902488b72484f47062abaf8592b52d8854820a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:18 +0000

    means spoil underneath small stage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
