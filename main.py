586e8d9e366888ddd05362bd3318eb24f9b28f8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:31 +0000

    die rain sort fast blind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-import re
-print(re.match("h.*o", "hello world"))
+print("Hello, world!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
