2e406ffc97c39b75df93afb0a3420909aeed9035
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:34:49 +0000

    comeround nearly vision mass many

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
