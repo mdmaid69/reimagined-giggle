9713abb42a746b46518fc6462505277adc73cdb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:45 +0000

    agreement secret fit cool earn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-print(sys.version)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
