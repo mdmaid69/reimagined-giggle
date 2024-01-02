72a5c1d1f3432ca831e8e9dce413da0ba1a9aef4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:22 +0000

    alone wet tired giveout though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_list(array):
+        return list(array)
