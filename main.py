2bc195b47c442537b671688e1c38231918fc3f2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:47:33 +0000

    bound if think dangerous regularly

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+print("Hello, world!")
