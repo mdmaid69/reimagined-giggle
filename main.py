46c89b506978edf45c20609bef8792174c1ef797
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:33 +0000

    next ought gooff coat live

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
