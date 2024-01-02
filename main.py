c2c8cc69638212986a2f033fa15fbb6c9d70d682
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:03:08 +0000

    afterwards check gentle understand flow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def split_path(path):
+        return os.path.split(path)
