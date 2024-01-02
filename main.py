53da804127a6edc00b914c700379d7811d6c8ae9
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:39:18 +0000

    advantage shape woman fight should

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import sys
-  def get_python_version():
-        return sys.version
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
