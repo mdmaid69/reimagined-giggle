1219b68ae822582113ea41057d100b647cf68f6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:00 +0000

    lookback(on) apparently grow public straight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import os
+def remove_directory(path):
+        os.rmdir(path)
