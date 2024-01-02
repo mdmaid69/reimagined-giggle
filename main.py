901519c0ebe0350a1376f04c0fa45df4769b409b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:35 +0000

    line up anyone flavour table

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
