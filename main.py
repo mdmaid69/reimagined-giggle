aa8eead8ba1b3ba8d8323bc7dd5259f723952d63
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:03:37 +0000

    birthday frame face waste cold

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+def is_odd(n):
+        return n % 2 != 0
