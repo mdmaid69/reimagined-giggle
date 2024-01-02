7d679314a7406d9952198af840cb5d9a6ce69705
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:57 +0000

    translate prevent continue able thorough

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
