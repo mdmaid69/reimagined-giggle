e524d7e0b8a2d1ddf0b2a55b43473df6691475f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:36 +0000

    fold belong match rain catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def remove_duplicates(lst):
+        return list(set(lst))
