e3d371b169397139f6148364e3c6bc32d6ce363c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:13:21 +0000

    top ago broken compete private

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
