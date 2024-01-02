e1eb7b60c5c25d0dc30f2adbf193f906544122e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:14:41 +0000

    entry juice everything image run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import array
-def get_array_as_set(array):
-        return set(array)
+def clear_array(array):
+        array *= 0
