ef882635430ae75b1506e642edc8bb548435856e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:41:03 +0000

    read interested myself brief cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+def calculate_arc_cosine(x):
+        return math.acos(x)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
