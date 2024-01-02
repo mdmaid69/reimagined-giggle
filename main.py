072139366380b0fbcc8af0dff05c78d1a7a62f34
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:17:52 +0000

    too along danger advise start

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
