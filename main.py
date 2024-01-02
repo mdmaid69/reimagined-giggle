5f9a2a4414ee84ce437d7b352aac21060bc9e142
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:11 +0000

    character present gentle keepout twist

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
-import time
-print(time.time())
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_acceleration(speed, time):
+        return speed / time
