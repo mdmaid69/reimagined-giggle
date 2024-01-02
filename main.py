bf109f526e4387c132111f08f99d22e94bcba764
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:05:39 +0000

    unusual muscle apparently spoil also

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
