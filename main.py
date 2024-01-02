d87841059d4945e56ecc3c8a65f0dcbf43bc00bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:55:59 +0000

    complicated touch ignore squeeze sell

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
