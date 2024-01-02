5a4f487630a5bc574af9568915ea6fcf4efa4d0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:25 +0000

    send between control on off

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import time
-print(time.time())
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
