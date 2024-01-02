3326c6afb74b1ee95e8bf6faf67483374e685ebd
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:37:18 +0000

    mine rain importance honest desperate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
