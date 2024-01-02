53142a0b620ade063a8ba5abfe439dba7aacfc40
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:19:37 +0000

    practise peace colour harm public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_pressure(force, area):
+        return force / area
