e60ce1fc2570b86c022bb51ce60b9a2b54ddca3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:49:12 +0000

    another far workon operate room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
