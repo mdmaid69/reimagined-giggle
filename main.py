c22f8371f117de4dda24233bbbafbb45c78918f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:07:54 +0000

    nurse type figure deliberate class

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
-import collections
-def create_user_dict():
-        return collections.UserDict()
