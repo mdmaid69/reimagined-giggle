a79d99705cc732dac2cb2b92d15dab07ed80eadf
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:35 +0000

    almost trousers badly positive careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
