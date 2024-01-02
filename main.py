8328d585c7c3abe019eae28e3c0bbff4bf5ca717
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:22:05 +0000

    pattern ability upwards guess she

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
