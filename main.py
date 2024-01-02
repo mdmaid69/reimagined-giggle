b3ff312223f764190fb214900fcd526060ff037e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:48 +0000

    book through relax trap paper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
