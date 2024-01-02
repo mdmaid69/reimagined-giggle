b301020d17358782731d55f3fcd48f640a8a968e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:09:10 +0000

    late left smell style cough

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
