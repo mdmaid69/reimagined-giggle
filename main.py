6c6f789c5b9aeadf271c07139e3eca8d6258430b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:01 +0000

    bunch change official flow attach

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
