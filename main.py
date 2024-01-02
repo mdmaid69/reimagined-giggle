b890a9a96936ad91c45301f71e5ccf70629c99a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:51:33 +0000

    card comfort square fierce on

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
