8ee6db34da57b4758bdeb3fc0e05f165bf05153c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:20:29 +0000

    ring me food never himself

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def pop_from_array(array, i=-1):
+        return array.pop(i)
