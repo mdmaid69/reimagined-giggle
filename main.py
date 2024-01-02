dcd5bcb9cfbf2e110b2cd97867a6c382e1731246
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:33 +0000

    activity Ee murder sign accidentally

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(x, y):
-        return x + y
-def add_numbers(x, y):
-        return x + y
+def calculate_density(mass, volume):
+        return mass / volume
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
