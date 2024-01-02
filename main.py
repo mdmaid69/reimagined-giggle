0a9bfd50bbd51ec7858867dd33b72143acdf0c6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:08 +0000

    bottom pinch about please refuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def divide_numbers(x, y):
+        return x / y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
