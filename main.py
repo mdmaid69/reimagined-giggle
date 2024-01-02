8f20e7a638955a9576bb5949aba824bcbea0d823
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:11:52 +0000

    separate praise town active fine

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def convert_to_binary(n):
+        return bin(n)
+import platform
+def get_python_version():
+        return platform.python_version()
