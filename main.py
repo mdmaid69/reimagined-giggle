ebb9ef715185064ccd63cd9741cf37fd47c7dd6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:59:33 +0000

    recognize order lose fasten region

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def is_odd(n):
+        return n % 2 != 0
