2c411cdfb49b3cb6a85bb7e5f6524505ca4abfe3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:06 +0000

    free hold anything maybe date

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def append_to_array(array, item):
-        array.append(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
