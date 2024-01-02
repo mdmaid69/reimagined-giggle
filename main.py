d7bb5b8745d389352539ccab8d4cce5f23b2ca1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:36 +0000

    method much border belong slowly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
