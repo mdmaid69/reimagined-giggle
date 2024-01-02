0d8be5cf6bc2d5e7c9aada1aa133fb726ed2c5c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:06:15 +0000

    pause below treat Ww crowded

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import platform
+def get_os_info():
+        return platform.uname()
+  def convert_to_octal(n):
+        return oct(n)
