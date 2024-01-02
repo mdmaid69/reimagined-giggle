1d0ade93dbec4e131c24f736ae97b191a3cc6850
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:18 +0000

    round valuable nerve letter rescue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
