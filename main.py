aec4e64a044f510101ecf1a3a47a79bb7aee03e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:01 +0000

    confused become language fond meaning

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
