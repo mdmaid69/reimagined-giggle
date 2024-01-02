98337a4a0715f6efcf41339454d97e4bbcdbaade
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:55 +0000

    guilty bath day travel mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
