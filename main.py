c48c29d8f776d4cf831ff63241520c7c059fed75
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:28 +0000

    equal age sort about workupto

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
