2919bcac872f6c1426693dd8f9914a1b263912ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:39 +0000

    interesting trade snow repeat weak

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import numpy as np
-print(np.array([1, 2, 3]))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def multiply_numbers(x, y):
+        return x * y
