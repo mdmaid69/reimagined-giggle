4533500fe290eabcff5a5e2d1d1d7b48add5b8f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:00:02 +0000

    home inquire shadow finger doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  def get_current_directory():
+        return os.getcwd()
