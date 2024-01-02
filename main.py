8026cda2c56779f0a770cb1cb895186e1510527b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:09:27 +0000

    need high switch total instruction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+import numpy as np
+print(np.array([1, 2, 3]))
