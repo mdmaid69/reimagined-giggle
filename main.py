aed8bf162a7bff66b0fa832a268b19b21fca1598
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:57 +0000

    preparation class who murder uponon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
