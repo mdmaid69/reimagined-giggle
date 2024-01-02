07f1aa06d5798260aaead294d14bf422828ccb42
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:19:17 +0000

    bread exam behind conduct until

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
