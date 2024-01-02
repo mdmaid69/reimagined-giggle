78bf246d55586d43ea0419f7595b8d3be60b71b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:33:29 +0000

    place charge decision sort suckme

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def set_array_item(array, i, item):
+        array[i] = item
