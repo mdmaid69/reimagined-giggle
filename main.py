9ea851f4f7feb17edfbd816e86acfc33a64bc165
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:19:52 +0000

    letter opposite peace definite tidy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_power(work, time):
-        return work / time
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
