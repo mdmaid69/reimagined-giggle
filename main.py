8e878bc26e31e275de857873044840e7132d6840
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:42 +0000

    sale culture confusing extra public

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
