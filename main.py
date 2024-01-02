a93ecf45b8330278d88967a03d8b058f75d60c14
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:45 +0000

    season neck make point powerful

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import time
-print(time.time())
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
