bf56f57912885f42ae2c2c13ec6fa7b2b92732f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:14:48 +0000

    drink scream natural steam door

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
