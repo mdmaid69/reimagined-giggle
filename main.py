b49a90619bb19c085a07e6d55ccf5f836405e694
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:06 +0000

    pupil lead opponent when patience

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  def convert_to_octal(n):
-        return oct(n)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
