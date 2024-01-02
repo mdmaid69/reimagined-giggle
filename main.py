fc20a25c21455ce29608b84f08f531ddf85c0583
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:57 +0000

    force exist mouth shake exit

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import collections
+def create_priority_queue():
+        return collections.deque()
