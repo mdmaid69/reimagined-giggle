29b7026a0ea1ebb21dc422c373cf28a0ce3529f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:48:13 +0000

    train common task dress inside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def is_even(n):
+        return n % 2 == 0
