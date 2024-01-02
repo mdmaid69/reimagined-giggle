31dc6300ea054c0ef2fae2480a83cd4cb49f85ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:34:25 +0000

    paper standard gate comedown him

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
