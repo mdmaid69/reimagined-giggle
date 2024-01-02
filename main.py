c0bfdfb17a366ced46910d50b80e7c7d0d4c99fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:03:32 +0000

    guide dance improvement unfortunate influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
 import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+def create_stack():
+        return collections.deque()
