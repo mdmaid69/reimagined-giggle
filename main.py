9d79d98a7d44b1b223aa6c644300d95030a39f20
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:27 +0000

    progress receive criticism ability rather

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
