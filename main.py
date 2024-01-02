a2740b7d146fc72fcf15f1ddbe6de45fa037dbfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:33 +0000

    watch die copy address if

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def reverse_list(lst):
+        return lst[::-1]
