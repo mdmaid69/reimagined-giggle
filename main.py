4da987e31f962fecc9f1fa885a64ebe00daea7cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:46 +0000

    bear discovery report begin space

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+import sys
+def exit_program():
+        sys.exit()
