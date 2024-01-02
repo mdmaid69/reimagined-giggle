04c483c65f91c03d5e2c92092ec4c8fda5a6f82f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:05:20 +0000

    mail appeal brush fence little

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
