d8919e8405ce69277ab2263eddc7addd2ae505d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:54:36 +0000

    invite wall than house towards

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
