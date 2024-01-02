7f627faf5eee67fa3c1c7618102b0532d75107f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:07 +0000

    stamp company company wire blame

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sys
+def print_python_version():
+        print(sys.version)
