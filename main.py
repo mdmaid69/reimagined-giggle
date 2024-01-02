17881d3bfc7b2504ae56fda84d8044b7886f8864
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:12 +0000

    snow proof mind appear violent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def append_to_array(array, item):
+        array.append(item)
