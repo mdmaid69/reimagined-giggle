6e762d424ef2ccff7d3eabacd1fba66b7cc71749
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:08 +0000

    comein lost aim instruction vote

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
