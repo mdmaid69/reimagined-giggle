8083125e5982c1e785adb012ab6e252f3977a513
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:21 +0000

    outside across movement see priority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_circle(r):
+        return 3.14 * r**2
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
