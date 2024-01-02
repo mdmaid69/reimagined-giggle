ef65dc87328c590b2f4e38e781cb374887cc9ad4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:16 +0000

    offer just rich emotion formal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
