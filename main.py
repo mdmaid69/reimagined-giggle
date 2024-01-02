e14cee93c504ed5cf4926b6c05ce9c33a6eb16ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:19 +0000

    coat forever action today draw

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
