3c726dc7d25073274026492f8679b90ca5d8eef7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:08 +0000

    natural open collect national support

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from collections import Counter
-print(Counter("hello world"))
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
