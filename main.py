89a7180fea6aea096288dd5d9f84d724a9adbfd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:58 +0000

    ride combine moment shape often

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import math
+print(math.pi)
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import array
-def get_array_length(array):
-        return len(array)
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
