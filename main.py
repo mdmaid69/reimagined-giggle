2d5109cebc928dde3f045778e6f3af1d05877e2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:33:08 +0000

    getaway increase crash convince ours

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def remove_duplicates(lst):
+        return list(set(lst))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
