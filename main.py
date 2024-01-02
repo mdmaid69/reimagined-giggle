7cf3a1894e593d385fdc06a1640ddd9df89ea38a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:04:47 +0000

    getoff yours notice punishment different

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
