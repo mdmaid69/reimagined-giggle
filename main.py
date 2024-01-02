91571c0d6a3f65286f51b6774ba14b54f95cfaf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:05 +0000

    unhappy takeback soft turnaway front

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+import os
+print(os.getcwd())
+import math
+print(math.pi)
