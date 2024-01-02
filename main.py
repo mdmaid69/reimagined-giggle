8b390df25fc33d074e8eeb132d136bb2a3203738
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:03:37 +0000

    walk repair interest close foreign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def calculate_volume(length, width, height):
+        return length * width * height
