d457954b939d006e1197066a894f17ef2af6f421
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:37:24 +0000

    different Rr gradually narrow none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import logging
-def log_message(message):
-        logging.info(message)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
