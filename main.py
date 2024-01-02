e11d6f4bf9128550dd355d460fa06f65c31d4414
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:21:00 +0000

    instruction personal keen record standard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
