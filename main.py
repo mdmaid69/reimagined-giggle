44a762097d5b7e038f258af03a22bf59cd65016b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:15:39 +0000

    later high belong feed Yy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_exponential(x):
+        return math.exp(x)
