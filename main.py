ec9632b5c1a33a589fe8d68da0e6bede5aa24563
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:51:25 +0000

    smooth push dinner middle responsible

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import getpass
+def get_username():
+        return getpass.getuser()
