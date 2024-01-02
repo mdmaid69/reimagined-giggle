ad3690272ae7baea7adac8013018d1baacaf2269
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:08 +0000

    later wealth empty else some

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
