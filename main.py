c2e4be5b26b598c308b35e1b499f4acce858cca4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:17 +0000

    flood exchange giveback will her

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
