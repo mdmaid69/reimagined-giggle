37eb227e16b1644595a0e55f7ef89608be5fb2c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:29:54 +0000

    guess backwards statement doubt fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
