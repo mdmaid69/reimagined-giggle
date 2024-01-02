0050794e3840bda9d18f96ebc833c3bd61cc30cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:06 +0000

    surprise polite means tidy collect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-print(math.pi)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
