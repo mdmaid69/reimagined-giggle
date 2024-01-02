3f2df8cf863592f2f6fdc34c12d2450d1f2b3efb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:40 +0000

    trip fasten clever separate decide

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
