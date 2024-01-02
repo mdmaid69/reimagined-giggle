50014adfacb519da4aebc747a1001a016499a2d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:34:49 +0000

    loud deliberately straight switch cold

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import sys
+  def get_python_version():
+        return sys.version
