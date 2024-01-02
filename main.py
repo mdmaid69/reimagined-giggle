32ef882a24e608d4bb76c44645036c2ccb0edbe9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:48:45 +0000

    range university loose late card

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-name = "Python"
-print("Hello,", name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
