aa159c0fd720c82bf5cb339d3940cf9699344705
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:10 +0000

    study person care hard money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def reverse_string(s):
+        return s[::-1]
