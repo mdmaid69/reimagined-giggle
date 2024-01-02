0617063ad8e1b4da4a875d0eed61559e435a298e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:14:44 +0000

    run history public run frightened

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  def reverse_list(lst):
+        return lst[::-1]
