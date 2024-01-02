3333db0309fb3de31f0bc29d18e01577b99b2ed6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:43:59 +0000

    stamp that manage contents many

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_work(force, distance):
+        return force * distance
+def find_max(numbers):
+        return max(numbers)
