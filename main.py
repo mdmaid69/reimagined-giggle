6cf5964aca723268e9d97712493cd973dc59809b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:17:31 +0000

    shrink 571enough remove fly road

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def find_max(lst):
-        return max(lst)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import array
+def convert_array_to_list(array):
+        return array.tolist()
