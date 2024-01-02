b43a60c6883ca523b769be834aeca4cd50b3092c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:03:02 +0000

    necessary person special under duty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
