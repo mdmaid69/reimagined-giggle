2fae1b34049aed4b26355b19c3b70fd6935759ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:56 +0000

    command away teach prepare their

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_floor(x):
+        return math.floor(x)
