963baf275bf0b54e0c260c2ca073eb4cbf013716
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:36:22 +0000

    office benefit idea hope pronunciation

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
