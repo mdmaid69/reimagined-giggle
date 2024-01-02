cfb4a0e54d4da28cedc62005dc7be6830ac3ad8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:43 +0000

    accordingto kind pressure alone myself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def split_path(path):
+        return os.path.split(path)
