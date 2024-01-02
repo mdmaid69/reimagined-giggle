cb8ea12e4a73b90bcfed0cfb2cbef1821d6b8dc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:31:07 +0000

    lead situation damp attempt desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
