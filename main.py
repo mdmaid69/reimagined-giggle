93739771cea62e7b3089104adfc51d8d82c61224
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:52:37 +0000

    history bend scream shall direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
