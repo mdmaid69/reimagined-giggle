fd3da72c47a7c0d1abb9076133d883f38f88e9af
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:54 +0000

    ever respect tap together here

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import array
+def get_array_as_int(array):
+        return int(array[0])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
