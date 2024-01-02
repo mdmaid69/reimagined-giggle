38423920c6ea370ca16a5b42568d02777958a822
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:56 +0000

    concentration until whistle national art

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
