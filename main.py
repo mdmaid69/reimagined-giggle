61eb2d8ca3348f2f1c46c7f89ddb619482fd9ea6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:52:41 +0000

    no turnup anywayanyhow contribute strong

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def add_numbers(x, y):
-        return x + y
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
