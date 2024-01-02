eb42a3f8c47e992c98f30725a867e8e80bd16bfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:31 +0000

    noisy recover payment permission office

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
