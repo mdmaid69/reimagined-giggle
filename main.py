9af7e2a3bb0606f385e8f0fdf5d95ff658b560e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:32:45 +0000

    lift excuse then platform either

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def add_numbers(x, y):
+        return x + y
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
