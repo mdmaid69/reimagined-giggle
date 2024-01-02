3339253845a639782ce44886d47f69b9aa37b5d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:42 +0000

    crash mail contribute another cross

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def sort_list(lst):
-        return sorted(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import random
+print(random.randint(0, 100))
