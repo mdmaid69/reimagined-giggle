450c38f37152825e8561c3a846141869d016390e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:06 +0000

    admire passenger home Zz Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def square_number(x):
-        return x**2
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
