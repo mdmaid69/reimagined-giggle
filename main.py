dcddf99c585d514db9dd13f548d4b46741f09431
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:11:06 +0000

    Dd 572enter warn bath rise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
