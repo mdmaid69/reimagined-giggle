331215945751fb9c7fa72dd96eea5c026f1e6caf
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:27 +0000

    free record waste software match

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def convert_to_hex(n):
+        return hex(n)
+import time
+def get_time_since_epoch():
+        return time.time()
