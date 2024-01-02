77b2c156568a014bd34cdfe453efe6fc864bab2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:09 +0000

    succeed sign sink breakfast stand

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import os
+def get_current_working_directory():
+        return os.getcwd()
