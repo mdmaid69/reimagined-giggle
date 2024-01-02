de849b0d5f6d1a4d5173950e3b23efe67d6560ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:38:21 +0000

    very turnout getout none appearance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+import time
+def get_current_time():
+        return time.time()
