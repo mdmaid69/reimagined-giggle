2ca416498e4eec24c19780a40822130926fd9676
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:46 +0000

    judge reduce detail butter spare

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
 import array
-def clear_array(array):
-        array *= 0
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_string(array):
+        return array.tostring()
