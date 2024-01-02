cb3ea0b67b27ec93c239bc2abdf6272b99aff431
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:00:35 +0000

    history nice suit lie fire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
