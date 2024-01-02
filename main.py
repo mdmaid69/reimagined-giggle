193acb1ae7b4705b48925ba1cc3d8fef64ae1cd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:21:33 +0000

    illness sign intend approve pair

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
