47309a2f9051c82915fd36f479c243eb3e4ab4e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:01 +0000

    team classroom yours all moral

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import sys
+  def get_python_version():
+        return sys.version
