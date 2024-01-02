ec78ba64837cc5945321fab09f217257751830e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:32:30 +0000

    mix because welcome body Kk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
