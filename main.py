1fa6fe14c03e36f81d25d06b5ddd031fb0e0230f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:51:41 +0000

    question stay naturally truth careful

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def sort_list(lst):
-        return sorted(lst)
+for i in range(5):
+        print(i)
+def divide_numbers(x, y):
+        return x / y
