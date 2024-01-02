e01916dd395f19307baccb6cf36cd71528119423
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:01 +0000

    combine earth joke precise talk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+import collections
+def create_priority_queue():
+        return collections.deque()
