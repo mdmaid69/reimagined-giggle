15f1cab871bfe7b6877bdc4c19cf485d4f273d3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:20 +0000

    steal completely always care fairly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def cube_number(x):
+        return x**3
+import array
+def convert_array_to_list(array):
+        return array.tolist()
