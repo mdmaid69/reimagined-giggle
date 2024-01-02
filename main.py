47838af04fc7c46cc8ec382c00bbf3860f8b5dd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:46 +0000

    569enjoy mind cap excuse trap

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
