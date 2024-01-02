5a50c383ecd10bc74115699df6b2f35ceb1e71a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:02 +0000

    praise schedule future watch soil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
