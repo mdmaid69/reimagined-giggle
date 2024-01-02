593b58ba8fdc44f6a906aad97521758c421876f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:57 +0000

    attract pronunciation neat point ought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
