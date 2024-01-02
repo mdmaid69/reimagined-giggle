64210a2e44a6fa44ac515f24c66a5899f7b0cc44
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:49 +0000

    connection marry scene passenger though

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
