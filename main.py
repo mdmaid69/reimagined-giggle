355c46701c86a4ed6e626cacb7e44b4ce17c2d14
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:43 +0000

    kick commit judge getback organization

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
