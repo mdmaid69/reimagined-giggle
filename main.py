df413237859e65f463c86670bc3b4b2375dcace4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:21 +0000

    smile tight sure bother female

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-import sys
-def exit_program():
-        sys.exit()
+def convert_to_binary(n):
+        return bin(n)
+  def sort_list(lst):
+        return sorted(lst)
