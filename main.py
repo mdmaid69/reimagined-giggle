3a36b6b9219f7b447fd56ab442f7ba16a8311b8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:49:15 +0000

    tip clear regard machine use

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
