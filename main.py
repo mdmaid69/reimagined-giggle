07877b05ceba0dcef874ad8a0e3c9699275b0faa
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:22:39 +0000

    chase cost change gooff long

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_sine(x):
+        return math.sin(x)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
