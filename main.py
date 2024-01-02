c20cb2afef652e27330e204347d39946d127800f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:48:19 +0000

    short way not fierce sense

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  def multiply_numbers(x, y):
+        return x * y
