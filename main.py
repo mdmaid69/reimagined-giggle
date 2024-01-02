d66f74cb5235d1ae5bae84ec322c979de741b0c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:17 +0000

    cope Rr tired ourselves Oo

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+  def count_elements(lst):
+        return len(lst)
