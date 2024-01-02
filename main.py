3eaae627fd8ff2435ccbba3ab12c148113799aba
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:15 +0000

    loan perform child message trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
