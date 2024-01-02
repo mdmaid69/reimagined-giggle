b7126220a324e859eafe6ae222220683feac3008
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:20 +0000

    justice benefit move spite represent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
