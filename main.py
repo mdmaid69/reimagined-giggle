0f6601b898f1e3ac941ca82a35b1a4cbe8316a9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:25:44 +0000

    deal remark catch luck no

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
