1cd11e1173d1d18255cef3720079b1f2ded57a5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:29:37 +0000

    fix name full print bit

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-  def add_numbers(x, y):
-        return x + y
-def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
