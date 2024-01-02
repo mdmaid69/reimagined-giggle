c0dc39592bcad3a85fefc02ca042bf0a90994317
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:46:46 +0000

    noise another type swim dry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
