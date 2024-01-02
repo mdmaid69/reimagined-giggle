486e836975d184c6d4182e94c7d2e3b4c2c2bf8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:04:35 +0000

    watch bar way considering whistle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
