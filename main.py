ba7d5cbbbaf3aca7ee8c94fa60eb92b06a04fb66
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:06 +0000

    money accident search steal along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
