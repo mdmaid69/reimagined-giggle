bbaf858251d889076da930b9541e3040d9ee0a22
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:20 +0000

    building runinto wheel yours fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+name = "Python"
+print("Hello,", name)
