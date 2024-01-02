6b40ee383c5e4ba43238cdcabfd1d1e52a1b8106
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:34:26 +0000

    certainly so look her if

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import random
-print(random.randint(0, 100))
-def convert_to_hex(n):
-        return hex(n)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import array
+def get_array_itemsize(array):
+        return array.itemsize
