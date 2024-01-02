9fe9322bb3d1ed6b09d28a2640fc5f6da30ae7f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:45 +0000

    study introduce account kind mark

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import re
+print(re.match("h.*o", "hello world"))
+from collections import Counter
+print(Counter("hello world"))
