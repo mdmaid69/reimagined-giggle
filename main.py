5a04d493aefc2edbdf404be823d797217ea3ae3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:01:54 +0000

    short teach pleasant Vv play

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
