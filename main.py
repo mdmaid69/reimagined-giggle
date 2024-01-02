f5812edf72bb04a2f8c9f52bcf81e7a67f123335
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:34:39 +0000

    inside mean consult Nn criticism

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
