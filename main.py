f91ed22593ad64be60efb0cf17b23926e9f406be
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:51:48 +0000

    innocent work determined cost wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
