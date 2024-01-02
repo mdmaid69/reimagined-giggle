87ca85265f1fcc40705e11cde15cc91720cd4613
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:05:50 +0000

    him sit finish image father

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def square_number(x):
+        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
