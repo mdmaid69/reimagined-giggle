4ba0b50d60da001e34025d3724226c19c2a3a0c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:41:38 +0000

    charge someone Tt direct touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
