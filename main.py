db5f275fc085763fce76535c20f9de9300b7c692
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:41:39 +0000

    570enjoyable if plant off use

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-print("Hello, world!")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
