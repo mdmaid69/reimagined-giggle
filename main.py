24c629a95700771e6a6334af36bb7ec60cb18cb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:09 +0000

    deliberately suck talkoutof come centre

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import collections
+def create_stack():
+        return collections.deque()
