9bb3fc1bccc103ffb5803b76e303d46160020efe
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:06:40 +0000

    schedule geton attach recover holiday

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
