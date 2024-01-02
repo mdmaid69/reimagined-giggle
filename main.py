d4f69e1222c24cc15d271d0657fddbc9eb14ddbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:12 +0000

    herself bear stupid makeinto zero

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
