529ad68bd0801eca300be48fd8685c8f2e219389
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:33:58 +0000

    week recovery priority stare present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_current_working_directory():
+        return os.getcwd()
