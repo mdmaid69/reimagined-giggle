bf120e2aa2dafeb7ac6fdff561cf37c0a3d75c58
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:45:13 +0000

    reaction owe broken leave relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
