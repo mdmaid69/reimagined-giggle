8bc1d5d86ad894b38b5031badb1ce4e5a14b988e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:26 +0000

    short wet circumstance Ss unless

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
