f4aa49572e1fabdfeced6e679e2bf135a896eeab
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:48:09 +0000

    follow I bringback generally pollution

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
