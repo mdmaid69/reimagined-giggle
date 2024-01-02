49760f18d05c14d306ca5f9dbe0b9547837817d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:36 +0000

    terrible obvious lesson permission open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
