46897afbbb8e11eede29807d1b37497e39a8c28b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:37 +0000

    game neat all behave foot

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import numpy as np
-print(np.array([1, 2, 3]))
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
