6b846fac2d7bd0e4d54026988645640f4eaf0a4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:02 +0000

    perhaps loud friend brother communication

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
