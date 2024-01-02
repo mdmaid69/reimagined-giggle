ea45c44b605ebb9473146da854b5be6ae2598311
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:39 +0000

    begin adjective spoil ground collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
 import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
