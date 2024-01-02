79ee66711003feac52ca61534d6f28b54448b942
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:39:01 +0000

    only shut can ear march

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def cube_number(x):
+        return x**3
