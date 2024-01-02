df1d88e450a37775134a2e16cc1dbe4216e09e52
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:15 +0000

    difference murder once maintain section

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def find_max(numbers):
+        return max(numbers)
