fd1a3cdfd7e1fcf998acdfa95f3b21d3ab717e9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:39 +0000

    comedown how late within attention

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
