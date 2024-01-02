353858f5a3f626aa1ca0a16948e232cca3877228
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:49:13 +0000

    goodbye evidence right lost pity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
