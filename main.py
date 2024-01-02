5237d4b0ccb37e25a7cb60fe89c9d7f7400a52cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:33 +0000

    development trust comment fire change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def convert_to_hex(n):
+        return hex(n)
