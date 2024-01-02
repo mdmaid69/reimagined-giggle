b9e4f1629db8be2be9c00a3b046dad3820ac1b29
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:31 +0000

    congratulations main success plenty emergency

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
