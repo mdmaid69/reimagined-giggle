13262e43f8c939e5c791a21daa79264e05570402
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:08 +0000

    interview matter line pull call

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def is_even(n):
+        return n % 2 == 0
