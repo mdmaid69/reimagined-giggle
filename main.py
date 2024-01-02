f9968fc52851a1c8750bd63d5a127e69101f2dd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:24 +0000

    study nerve spray spare getaway

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  def subtract_numbers(x, y):
-        return x - y
+def sort_numbers(numbers):
+        return sorted(numbers)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
