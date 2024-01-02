2266be1a92a53017aef35799148075b17a7bd073
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:04:41 +0000

    warn fierce unpleasant dull concentration

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
