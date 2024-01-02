0fa94512457c349148524f87b3d4114fc5c235d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:30 +0000

    patience us original male congratulations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
