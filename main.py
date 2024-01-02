a70c0c749a6b3d1f440775563addd62573cb76ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:08 +0000

    fun deal container prepare interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
