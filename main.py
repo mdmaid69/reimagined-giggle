eaeb9db03c4ebddfef2c382693c083149a6dbc90
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:16 +0000

    shop cross line sign couple

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
