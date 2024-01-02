e32c82ef52453c5f9df4c2fcc5f5238abec3270f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:58:43 +0000

    safety human nor announce exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
