bea11efea7559e8e5af616d90aea95d75229fd25
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:23 +0000

    supply natural begin standard substance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-print(math.pi)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
