191d50395cab8aba5883421159efbe0fbacde60f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:12 +0000

    inform sense satisfied consider when

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import time
-def get_time_since_epoch():
-        return time.time()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
