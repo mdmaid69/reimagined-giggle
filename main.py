0057868a2dfa43d9e07a7e225b34981aa1d2233b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:53 +0000

    talk firm have event copy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
