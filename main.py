bab091446599ad411d2f6e8d3dd0457138e4ef0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:56 +0000

    turnback stretch basic hungry taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
