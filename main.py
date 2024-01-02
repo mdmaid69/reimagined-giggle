2798f9a211614907d09eb629d99ef69098f1cb5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:10 +0000

    spring near intelligent fond offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
