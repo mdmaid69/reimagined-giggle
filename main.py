5f5c35cb744f0283af3412a5c1c5a260e2c68f7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:11:38 +0000

    right may sensitive blame Yy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
