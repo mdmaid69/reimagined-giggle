bd117ab02517e58a10bc3453b2353289009ea99b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:03 +0000

    traffic typical read spare course

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
