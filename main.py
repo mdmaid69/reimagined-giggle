b7af4bb6f6f64bbe9dc2b226bdac0b33bef88acc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:25 +0000

    before he floor lonely turnon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
