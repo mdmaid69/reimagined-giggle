aed48f598ce4db3bfc1c8d112b9acaf128073d5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:32:04 +0000

    regional principle deal little offence

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
