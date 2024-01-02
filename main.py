7338a53f0d3781363bf5352b139c66fb7b5a5390
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:09 +0000

    fine used dear valuable holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+def calculate_volume(length, width, height):
+        return length * width * height
