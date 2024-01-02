ee98b991af517fd31396978a01ca8ba75a33f6b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:08:05 +0000

    dull phone prepare correct positive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
