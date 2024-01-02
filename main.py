73d74b3962a37644e6ad4207ac86e84c6638a90f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:57:19 +0000

    acceptable little equal common professional

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def calculate_interest(principal, rate, time):
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_future_value(principal, rate, time):
         return principal * (1 + rate)**time
