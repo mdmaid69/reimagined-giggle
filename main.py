20a51b8eb59dd98cedc7d79bc3c68b2c9a4143cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:28:20 +0000

    happy however section heavy fix

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
