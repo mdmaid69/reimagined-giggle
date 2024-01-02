17a8e21d9dcdb4479943420f955013eacddb0c53
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:10:04 +0000

    fact ball the limit back

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-import time
-def get_current_time():
-        return time.ctime()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
