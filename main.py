7a7c5839d89b0d253a0c0fd4a284c96b40b4c9d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:29 +0000

    about dance act spin prevent

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
