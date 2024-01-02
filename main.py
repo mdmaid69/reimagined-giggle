5111944a891c7c9877de3c95afe15e2dfdbd6dba
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:18 +0000

    scheme breakinto way we preparation

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
