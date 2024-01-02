c2b832d6d1bd8fc6bc5cf7ec2c2331a0cab94385
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:54 +0000

    discuss stiff dance positive offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_base_name(path):
+        return os.path.basename(path)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
