691c4a4f283cb0214a717c3148ca5ab5fa544e68
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:14:20 +0000

    desperate frightened godown invite cap

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
