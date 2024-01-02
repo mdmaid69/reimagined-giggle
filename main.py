d50420166dac0e246570be2971b85f468fdc531a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:20:54 +0000

    prevent hello innocent wall giveup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_floor(x):
+        return math.floor(x)
