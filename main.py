bc1b652b5f0ad57fc8bb3219b792594a7ec45d35
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:43 +0000

    love serve beat cinema exaggerate

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
