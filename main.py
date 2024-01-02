928e2ffd93993a4b93663c3da3f6732e6a6ad457
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:56 +0000

    stamp reply sea touch press

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
