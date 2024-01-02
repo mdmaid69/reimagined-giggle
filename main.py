62fe4dc834e26fea34dab39ca0c236598f5272ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:33:05 +0000

    nigga into note hollow bear

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def reverse_list(lst):
+        return lst[::-1]
