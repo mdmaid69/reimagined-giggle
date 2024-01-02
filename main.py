3e7fdc76c45807e8fad6e485451cb85876ca23d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:51 +0000

    pen sport crazy rescue question

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
