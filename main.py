b878f703a11be8438ef0564c9f4590bb38347736
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:20:03 +0000

    goaway blind where break prepare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
