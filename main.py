1a2fd18be6744d9fe8eb3c2fa8fc66698df256b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:43 +0000

    insist vanish steep listenfor leg

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
