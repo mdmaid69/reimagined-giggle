fdc4f69745f6c9cb943f0a85e5029d2b3b96b043
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:41 +0000

    short back trick waste schedule

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
