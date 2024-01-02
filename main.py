bfe1baa160a81d5e27eb2438795fed24573ab0ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:15 +0000

    and Kk free win definite

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
