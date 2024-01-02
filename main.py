7e3ef02cb5ae0c38651aee252537a184d03d11ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:36:08 +0000

    estimate jump travel language gift

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-name = "Python"
-print("Hello,", name)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def count_elements(lst):
+        return len(lst)
