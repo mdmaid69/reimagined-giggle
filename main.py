d4a695a47410c12db9b9385ad45a07d2fea67d61
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:39:39 +0000

    bound pretty achieve memory gooff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  def square_number(x):
+        return x**2
