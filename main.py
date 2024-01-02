ef22f4a818f104577faf7159e002293afc6332f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:42 +0000

    tongue earth small connect practise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_absolute_value(x):
+        return math.fabs(x)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
