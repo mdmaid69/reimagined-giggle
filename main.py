26c17318f289d56da9f52feb7c28c2651a781867
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:27 +0000

    door takeback describe statement murder

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-print([x**2 for x in range(10)])
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+text = "Hello, world!"
+print("Uppercase:", text.upper())
