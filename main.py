6674d1bde580d4f00dedbc48261a575f14eeb8ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:08:26 +0000

    sensitive weekend mad surprising burn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
