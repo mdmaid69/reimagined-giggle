b089cf9c71b53ee6fa7401a1f7264ef3cd65085a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:16 +0000

    mystery minor exciting development maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def cube_number(x):
-        return x**3
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
