8c9c3dc605f726591e575a7bba61da4c0b5bab44
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:50 +0000

    behind lunch female sight fetch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_speed(distance, time):
+        return distance / time
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
