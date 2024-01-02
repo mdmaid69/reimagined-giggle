39bdaa20ecfecc33444aacfed8199ca87d08fa63
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:45:49 +0000

    pay spray in law bringdown

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
