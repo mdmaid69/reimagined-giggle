956f945107145f07330aef5fb9a0d1966fd6103b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:43:04 +0000

    tonight want legal taste wish

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_item(array, i):
-        return array[i]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
