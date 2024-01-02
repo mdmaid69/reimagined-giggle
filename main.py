9a9e6cbd338166b3ca5c551f2f85c025d5404dda
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:02:19 +0000

    admit inside attach save constantly

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+def subtract_numbers(x, y):
+        return x - y
+def calculate_density(mass, volume):
+        return mass / volume
