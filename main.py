dac50d7b878555fa3965e453815723ed6e794b9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:39 +0000

    party instrument less up trade

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
