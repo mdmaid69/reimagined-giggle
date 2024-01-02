215d3ae5b865fa3d0c61b7f42df8a47151855271
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:36:48 +0000

    Ff afterwards godown forget deserve

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
