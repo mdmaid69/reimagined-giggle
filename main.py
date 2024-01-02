0aa62fc73d88b93a820221f55c0a8a47984d75ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:22 +0000

    increase exactly still remain make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
