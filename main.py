8f2f7d337139b0ec0113db14bd9607bbac0d6e76
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:34 +0000

    lose characteristic emphasis raise yourself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def get_array_typecode(array):
+        return array.typecode
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
