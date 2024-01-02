c57d19fc78f86d9905f87ea77349bff5e2ed9f93
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:48 +0000

    throw cost workupto throughout Cc

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
