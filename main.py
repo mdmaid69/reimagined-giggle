06ae2311f3f5757ccb61c6863be8ca87001dc1ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:17 +0000

    exercise either available yes powerful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def subtract_numbers(x, y):
+        return x - y
