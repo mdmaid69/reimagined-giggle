7144ba195cfdf1947f1528a3efaeaff0347a2aaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:16:46 +0000

    arrive degree crime sorry city

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def convert_to_octal(n):
-        return oct(n)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_current_directory():
+        return os.getcwd()
