4916106a401336e4e2adda263dd51d6ba80341c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:40 +0000

    spill bet instead only gather

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
