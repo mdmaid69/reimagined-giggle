e1c3113287bf71cb54f42f0c72a69fa5d9f6f232
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:49:11 +0000

    limit narrow collection centre neither

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_array_to_string(array):
+        return array.tostring()
