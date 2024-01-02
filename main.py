47735998a23d20cb93a21965d26d3cbdba0796d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:18 +0000

    comeup clean fault order above

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
