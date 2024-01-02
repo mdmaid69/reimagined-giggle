54370bf3e5b14c0356fbe81a4e11f21902600d8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:08 +0000

    rarely finally Vv foot kind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-for i in range(10): print(i)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
