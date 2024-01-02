e7a2479fa755e225de27743e19be8d3a8b35f482
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:31 +0000

    government direct wash second himself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
