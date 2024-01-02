22f879dfcee754d326e59d5fe33ba8bc454d9ad4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:23 +0000

    television weekend pay discussion bin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
