2252ce375bdab41ac153033bd4bf82c3036c4d13
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:11 +0000

    connect cause break custom size

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
