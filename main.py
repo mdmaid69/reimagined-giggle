0ea383c0598e67a7c3788b8fc3fb93af7878b64c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:09 +0000

    skill suppose kind autumn exploration

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-for i in range(5):
-        print(i)
-import array
-def get_array_length(array):
-        return len(array)
+import sys
+def exit_program():
+        sys.exit()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
