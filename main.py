c8b84fefba417e6ff4977af8ad2dff889c82d682
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:49:38 +0000

    noisy Vv taste bet above

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
