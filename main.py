7370e96b209d1d45cf4ded5eafbe2f1c2feb4bbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:28 +0000

    would side lookafter chance treatment

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-print(sum(range(10)))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
