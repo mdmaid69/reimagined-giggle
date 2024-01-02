ae4f6ef875f78c0e0d7b43aeb4c911610fb33cc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:59 +0000

    variety like path giveout pass

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
