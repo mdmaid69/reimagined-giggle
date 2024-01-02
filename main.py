0a6a0bcb10747a6aaed8b5c138ebb1705c307ca2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:23:17 +0000

    group file radio unfortunate display

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
