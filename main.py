44161e749ac3baee33e09a4ae3e79e88a6a9b9c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:23 +0000

    objection she spread contact begin

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
