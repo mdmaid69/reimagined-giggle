e61d5926214e4c8a47bc074daf026675c70045de
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:55 +0000

    usually press pencil suit decide

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+print([x**2 for x in range(10)])
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
