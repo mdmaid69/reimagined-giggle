37784f36d04fc8794f3f1a4156b37ffac6dfffcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:33:10 +0000

    proud whoever sleep wash weekend

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import time
-print(time.time())
-text = "Hello, world!"
-print("Characters:", len(text))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
