1b95ef478712407175a0512caac2f8112883b2d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:51:45 +0000

    bring length goout spread callback

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
