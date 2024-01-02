6eec21f262182dc29f73aba701b508dabf69c602
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:17:55 +0000

    towards pride well note stay

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
