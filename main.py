f54925f2e9f34aa149afc6411352a72fe9110155
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:42:03 +0000

    closed discovery work brother expensive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  def count_elements(lst):
+        return len(lst)
