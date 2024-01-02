e7fe6f390a122ac5eb5683527b5d5087c7778989
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:48 +0000

    plant satisfactory unit wave young

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
