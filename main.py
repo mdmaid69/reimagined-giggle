60ce6b314abd890d9eb6f09783acca402c642c57
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:52 +0000

    error for woman whether vote

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
