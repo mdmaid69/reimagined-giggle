c6c32e81a7be491caf260127c5f3a435a1fad1a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:29 +0000

    old battery responsible 564encourage building

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
