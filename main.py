2d14d1119029763ace16b159634bfa5d26622064
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:25:39 +0000

    must bury lookup runaway step

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
