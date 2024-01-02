7b2d1e0369129d6f6449baadfcd5a880a282f706
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:12:15 +0000

    bet lose callat pour everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
