4aef70dd85b796ea5f2179f57fae508ed80d37de
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:31:46 +0000

    still claim concerned constantly friend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def remove_duplicates(lst):
-        return list(set(lst))
+def reverse_list(lst):
+        return lst[::-1]
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
