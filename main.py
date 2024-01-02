c4053fbe0cec90b90f878f31819eef7cfd214756
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:27:02 +0000

    justice regular embarrassing on opposite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  def reverse_list(lst):
+        return lst[::-1]
