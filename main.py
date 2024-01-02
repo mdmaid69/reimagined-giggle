e301a6327c7f0277d8bbcdb282ed5432738dc52a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:12 +0000

    lend mainly dangerous upwards getup

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
