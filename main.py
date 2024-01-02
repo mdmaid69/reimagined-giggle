fc3eb1017416cf3d8c54e2c731e6415dd9baa23f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:27:05 +0000

    before minimum anyone pay buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import datetime
+print(datetime.datetime.now())
