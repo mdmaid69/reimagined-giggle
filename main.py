3e4b02315b2557582a04c5f992a6970c5d38ad18
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:14 +0000

    while getup female period evidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def split_path(path):
+        return os.path.split(path)
+import array
+def get_array_item(array, i):
+        return array[i]
