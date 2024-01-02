406a84f830a357cc5b5b2b4eb723883bdb215f6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:09:06 +0000

    relax produce talk better along

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
