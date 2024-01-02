849ebd31488b54af9802e7283bed073b5dadfe4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:38 +0000

    relation breakdown uniform fortunate while

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
