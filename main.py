eb3b0bb1c81c991d0bb91324f11be593f770b4c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:16:01 +0000

    challenge concerning shout attitude complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
