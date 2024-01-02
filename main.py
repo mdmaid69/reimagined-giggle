a5ee32beb1060c2bc64f2290c7631f9714741547
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:54:39 +0000

    note shop thin outside fast

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
