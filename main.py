c9e1635ec9fdefbb1c7ab5f12bdba0af9762a9da
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:43:59 +0000

    hers mention lock slice disagree

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
