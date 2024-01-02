a8045bf6ed0a302e050adb4893e5070b357150b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:17:15 +0000

    Uu bar shout how achieve

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
