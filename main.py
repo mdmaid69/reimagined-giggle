6ea83277dd85218086dcfc9eb1339b183e48a802
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:28:17 +0000

    prepare letter happy everywhere anyone

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
