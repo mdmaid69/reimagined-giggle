d7b1a59873f993708618da45bf178508ad554cfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:34 +0000

    emotion voice sign rid together

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_max(lst):
+        return max(lst)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm(base, x):
+        return math.log(x, base)
