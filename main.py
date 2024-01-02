998dcae446ed7f5f931313b56e6db3fb560bcc3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:23 +0000

    dry positive long fasten congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
