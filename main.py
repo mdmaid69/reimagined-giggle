d91e1e3d462cc92adbc980cb1f06e4e6b85b60d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:49:31 +0000

    motion exit confusing lesson reasonable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def count_characters(sentence):
+        return len(sentence)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
