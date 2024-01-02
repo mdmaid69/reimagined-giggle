4b783cdabb07251994d99cdec971030378202271
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:34:35 +0000

    listen benefit wealth 569enjoy turnover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import tensorflow as tf
+print(tf.__version__)
