9b8e51f3cf2e1bc4bbe16d54185bab0bb9305f85
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:39 +0000

    comeout comment calloff find hour

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
