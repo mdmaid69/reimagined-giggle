3889717e1e6db8451d188bd560beb14984b95c2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:55 +0000

    fine under scale hope maximum

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
