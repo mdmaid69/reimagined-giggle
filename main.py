7c13c69fe83bc58d5887f6f0967c5f604926693c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:57:55 +0000

    scheme pass combine owe pure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
