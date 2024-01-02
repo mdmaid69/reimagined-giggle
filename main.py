3cecb4b26749d913770c5aad5aefd9d5ceac7eb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:34 +0000

    wrong forever easy interview wide

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+print(math.pi)
+def find_max(numbers):
+        return max(numbers)
