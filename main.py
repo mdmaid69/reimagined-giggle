8ad5d72e8fe01c4d1a7eb180bcc083babdf7f566
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:20 +0000

    interview fit discussion belong tooth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
