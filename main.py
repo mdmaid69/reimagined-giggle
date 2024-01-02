3fe5b1816d9960af4f043b3067bb11350c8bf25f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:08:35 +0000

    sentence female offence suck beginning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
