fb92a7b0a901087d522a4babbce82ad415823dda
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:03 +0000

    study getout winter inside calloff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
