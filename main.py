f059b0b7358c16f1a831d3c7e5c2f5fbfd960ded
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:45:15 +0000

    whole regret march rescue behaviour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_int(array):
+        return int(array[0])
