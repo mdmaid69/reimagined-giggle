70b198fe8a26369d91ea493639897cbe7eac9ca5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:21:02 +0000

    spot float holdup life shoulder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def sort_list(lst):
+        return sorted(lst)
