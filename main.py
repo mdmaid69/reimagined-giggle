71f43239370ff85f07d830a80dcaee90ccc6ce96
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:33 +0000

    drawing married word breakfast fold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
