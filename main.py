0ae6e570e7b7060c5071ed2f02daad96386d0faa
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:09:38 +0000

    breakdown workon Nn fond cap

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_list(array):
-        return list(array)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
