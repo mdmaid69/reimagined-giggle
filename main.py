fe568727750f12ffea249b9934f1c27925e65e9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:57 +0000

    me call winter after every

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def convert_to_hex(n):
+        return hex(n)
