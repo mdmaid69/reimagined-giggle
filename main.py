0c244e3474b8f268180ec2ca410180ba7baf5fcc
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:51:05 +0000

    black morning frequent thumb small

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def remove_duplicates(lst):
+        return list(set(lst))
