df4638dbf1156c014a0adf48024d67ccd5f6543c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:00:40 +0000

    by last upstairs juice future

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+import sys
+def exit_program():
+        sys.exit()
+def remove_duplicates(lst):
+        return list(set(lst))
