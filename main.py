d959b6e897cb209476f70273e145843836389983
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:12:08 +0000

    proper usually battery several judge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import time
+print(time.time())
