ba5c6fe5b80cf9bd23227d0b442af2c721a8b855
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:52:18 +0000

    gap wise look pretty clearly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
