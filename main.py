8f726a65ab82cf963e564e7c8866d5eb92a026f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:31 +0000

    course experiment warning spring measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_arc_tangent(x):
+        return math.atan(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
