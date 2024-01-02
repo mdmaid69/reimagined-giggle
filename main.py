febbad03f4e782487db8a5dcdca57cb7f560e0ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:32 +0000

    shout freeze base water consist

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
