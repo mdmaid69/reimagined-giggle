6c9bfafb6c7fc7a75db1480c466dd562aad025c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:46:08 +0000

    low clear nor custom find

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def is_odd(n):
+        return n % 2 != 0
