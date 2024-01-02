ef5351d8aa4bde769ec6c5573ef8032afab32750
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:22:29 +0000

    tune mend activity only once

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
