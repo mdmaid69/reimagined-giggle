f92ad54c371990c6ef4c2678e50e4df8d609c1fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:25:48 +0000

    cross loose 572enter mean sense

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
