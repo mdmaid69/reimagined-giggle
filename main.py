d49013b0d2437833f7b47e6afb8c1d089bb3747d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:19:42 +0000

    still twist only plenty influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from collections import Counter
-print(Counter("hello world"))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
