4dff32efd31fb596ed3a64c88108b6a5b7b45795
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:25 +0000

    subject past damage congratulate sweep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
