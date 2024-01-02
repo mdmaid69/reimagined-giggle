2dfed4c42014323545de8bb319c1f9aceb404c32
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:28:58 +0000

    organization read someone very accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import array
+def get_array_as_repr(array):
+        return repr(array)
