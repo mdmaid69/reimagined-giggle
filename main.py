a9dacc51c847a37018840c8faf998d7227fc8d1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:36:49 +0000

    calm many true challenge ago

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
