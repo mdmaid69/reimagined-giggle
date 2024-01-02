96c00c1cb17e7e0ca6abd152b48c15a80502e90c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:13:44 +0000

    this sail afternoon comedown principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
