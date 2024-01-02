e48bb15ac89bb56e24d36c0b3da5592ae2c2e31b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:36:30 +0000

    total game stupid cook contents

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
