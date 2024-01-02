1f589b6795c284f99891838fad61b5b1dc727be8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:12 +0000

    advantage crash fall convincing discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
