e662f1afd42b0299ef26241135826ef30b8ef6cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:45:14 +0000

    knowledge slowly back blow interview

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-print(random.randint(0, 100))
-import sys
-def print_python_version():
-        return sys.version
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def count_words(sentence):
+        return len(sentence.split())
