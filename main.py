2a8d70e550465fc90bceda779e14333b27740674
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:04 +0000

    hot ride giveout within rise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import sys
-print(sys.version)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def divide_numbers(x, y):
+        return x / y
