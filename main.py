002f736e767335ae46357151c7449cf30c97ae9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:57 +0000

    paper kind reserve street congratulate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_current_directory():
+        return os.getcwd()
