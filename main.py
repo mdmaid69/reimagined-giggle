ee389dd30e8bd41a2e3a9f0cd5679f957e65300f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:34:37 +0000

    mess someone definite sea thanks

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-import tensorflow as tf
-print(tf.__version__)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
