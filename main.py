39f424d0db1d00c63894d81f35143d456630206f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:58:55 +0000

    fold mention long appeal final

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
