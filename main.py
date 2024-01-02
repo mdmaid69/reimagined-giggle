2d3838864d29b3a6c4ef02246f2fb01f630f0731
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:03:03 +0000

    judge read promise explanation sense

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def convert_to_hex(n):
-        return hex(n)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
