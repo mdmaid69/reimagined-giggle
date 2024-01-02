8f088aeabae26da9a222290877017b3160e7d5a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:19:48 +0000

    satisfactory hole any me too

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def is_even(n):
+        return n % 2 == 0
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
