9ec96ed997961b5d9a062084f5cde667f9a120c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:50:32 +0000

    still declaration coffee pinch blue

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
