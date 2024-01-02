11458fa95b1cff4a9650172d0b3a89236d84c19d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:31 +0000

    switch consist ilovedicks surprising such

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
