3d3ef0c0b31d06425a2de1491489c0ebe18209f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:21:04 +0000

    fail wish will goback(to) language

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
