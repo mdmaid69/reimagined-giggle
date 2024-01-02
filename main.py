7c716eb17d82da1b0c633e28a15d7d4c510b34f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:46 +0000

    precise wind exciting most phone

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def get_array_as_frozenset(array):
+        return frozenset(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
