7387a9503080df73ccf9042ec69320ab8f83dd0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:02:47 +0000

    lost Hh walk comeround public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_typecode(array):
-        return array.typecode
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_area_circle(r):
+        return 3.14 * r**2
