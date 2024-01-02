1b92786388ccb464c7723bee98fa6f5c034303ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:39 +0000

    callat fold top match neck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-import math
-print(math.pi)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
