0053b337bfc0e7c42db2d2c9ba77f4b75eaa647d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:38 +0000

    relation total workupto handle twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
