d9ab1b815703226909bd673d39688a734bf2d1ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:17 +0000

    possession somewhere information already Ll

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def sort_list(lst):
-        return sorted(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
