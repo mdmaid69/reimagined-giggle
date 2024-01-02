c9b67adcc5600c074bab4623d6f3168bbc2be0c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:05:58 +0000

    whistle swim window remark for

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import sys
-  def get_python_version():
-        return sys.version
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
