fdd99576b119aaf314e2c282aa1aa0aa30bd85d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:07:46 +0000

    dangerous hour judge corner write

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def remove_duplicates(lst):
+        return list(set(lst))
