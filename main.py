51221af2c751aaf31229578fa1166a1ecc2cf8b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:34 +0000

    pattern phone tool attract copy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-for i in range(10): print(i)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_acceleration(speed, time):
+        return speed / time
