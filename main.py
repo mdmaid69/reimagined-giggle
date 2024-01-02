91fbf6b2841479081d49c99cf3107afddbd144a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:53:46 +0000

    tomorrow cause fault desperate hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
