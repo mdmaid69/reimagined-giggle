f689e019b15a435b7b73989f5ffaaba496fd16d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:39:40 +0000

    display nice this count lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
