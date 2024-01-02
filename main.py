4f747198035c5a466c65be8c255fe8b4e7fa6bba
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:57:44 +0000

    giveout getback yes to damage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def sort_numbers(numbers):
+        return sorted(numbers)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
