1a10c698f6a4bba4ca52cc53d2975f8a7a36635a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:07:11 +0000

    disappointed translate operate joke bringback

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
