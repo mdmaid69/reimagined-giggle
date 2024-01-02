f40ebe748b1d432a1fe0b26255edff7a2676306d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:04:59 +0000

    battery centimetre rough insist fault

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
