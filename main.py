91fab79fa910abf68b8a4b277d6bbd4d1895225b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:53 +0000

    scared follow interested sound silent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def change_working_directory(path):
+        os.chdir(path)
