6aaefcc020056efcbe103b1f46353f69499f1fea
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:15:30 +0000

    off loose sound gate might

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
