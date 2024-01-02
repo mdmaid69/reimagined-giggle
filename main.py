9010e25510418bf1b55dc907202c6a04b08eb8cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:19:10 +0000

    base advise telephone right shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
