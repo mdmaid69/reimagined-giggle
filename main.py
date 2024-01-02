3e719f2c5fb02aab0ae74a2d7254a857a15908e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:05 +0000

    pure offence warm object narrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def find_min(numbers):
+        return min(numbers)
