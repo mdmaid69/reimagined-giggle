c0c9c1584d0959d461492184451b47354a9e2a07
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:31:13 +0000

    authority dress vote above dress

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
