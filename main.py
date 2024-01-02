18f436f6e63b58a86d9b316ffa945c00180b63db
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:11:50 +0000

    meeting can piece artificial naturally

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-print("Hello, world!")
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
