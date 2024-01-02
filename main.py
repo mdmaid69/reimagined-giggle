d555c95c28420fbfa099cb1973142907c60cb27c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:51:47 +0000

    eat woman flat expensive foot

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import re
+print(re.match("h.*o", "hello world"))
