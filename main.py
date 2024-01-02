7da1710a6ab9c1c05a247ccd7e0849b2139b58b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:32:27 +0000

    wide petrol choice round whisper

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
 import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
