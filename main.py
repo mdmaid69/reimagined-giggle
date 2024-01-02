e9fe8c4bd80af2282939cbd2e0138358430ee137
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:18 +0000

    hurt side lucky fail hospital

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
