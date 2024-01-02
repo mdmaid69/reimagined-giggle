1ac3374108ab6919eecf3b83698202fb17bcd736
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:59 +0000

    now punishment grateful shoot miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_power(work, time):
-        return work / time
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def is_odd(n):
+        return n % 2 != 0
