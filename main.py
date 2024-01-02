85830cc58cd54122efda6918b89f2e11e4b31544
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:27 +0000

    bet hurt summer for excuse

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def count_words(sentence):
+        return len(sentence.split())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
