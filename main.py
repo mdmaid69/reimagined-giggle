890e5cd14d3bf73a64a7cc6146c4ffd64698cc3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:30 +0000

    birthday file danger damage chin

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
