58e336f216ca6f63dce35f5630985926356d8b67
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:35 +0000

    leg injury turnon measure suckme

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
