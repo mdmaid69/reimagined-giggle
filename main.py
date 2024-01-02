d9ed4669aa12e7ed5afc69c513b7b85659b49834
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:37:54 +0000

    moon honest come debt takeover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
