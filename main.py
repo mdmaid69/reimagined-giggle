3652e02f2e690926c422d2c23743c748c5ddff5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:50:59 +0000

    sleep lonely degree married shelter

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+print(sum(range(10)))
