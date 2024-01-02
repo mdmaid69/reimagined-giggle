42ee4c25878ea9140031b6f9461262a040b796d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:38 +0000

    branch guess nervous neither sharp

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
