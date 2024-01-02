e87b9a652b815821cb25e1bd04bf3d6985207408
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:59:45 +0000

    spoil work correct separate change

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
