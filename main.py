0eb25667af406a2f0beeb4f35c9cbf3583911b65
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:10 +0000

    importance strange tread lot realize

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
