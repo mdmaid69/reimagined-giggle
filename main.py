fbee307e6d725c7ec18c43848df3cfb012266109
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:53:43 +0000

    transparent extremely present runinto slowly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
