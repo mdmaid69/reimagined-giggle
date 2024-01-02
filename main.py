77a088d82d51f21411e7fab36ff7cf4112729522
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:20:45 +0000

    speech birthday pleasure familiar protest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
