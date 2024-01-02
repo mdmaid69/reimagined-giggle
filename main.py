bc7f11610557ffac4afdd0de5587f334a612ba18
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:59:07 +0000

    interesting regularly lower start introduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
