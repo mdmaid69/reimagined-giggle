7cab68070497a3c494d04d6f746aecca5ad90e35
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:50 +0000

    my record where thumb makeup

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def greet(name):
+        print(f"Hello, {name}!")
