ff79cb3a4ed336385f04610b70ba5d250b88a0e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:36:49 +0000

    unlikely left window often 566end

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def is_odd(n):
+        return n % 2 != 0
