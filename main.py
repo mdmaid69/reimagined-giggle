daf8bf4f5b0058dd6361fed8bf540d61847c48ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:56 +0000

    sound person getup balance quantity

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
