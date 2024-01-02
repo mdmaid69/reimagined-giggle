8060cf267201c2ee30318767bf1b0f90b7e4b503
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:38 +0000

    balance still week although hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_set(array):
+        return set(array)
