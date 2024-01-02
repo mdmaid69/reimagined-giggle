a71718cb25a551571894c7ecd48ee1dd1372e2fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:23:43 +0000

    turnup interest force public black

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import sys
-def print_python_version():
-        print(sys.version)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
