9ede5b6fb4dce05f0625a2d29c477a752fe88927
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:41:09 +0000

    interesting record organization downstairs medium

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
