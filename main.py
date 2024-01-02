d6e42115c7d996499918ab16b0193af541234879
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:12 +0000

    foreign consist closely letter rain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def sort_numbers(numbers):
+        return sorted(numbers)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
