d9ebd4cf4b6f599fe79eb9939eb6ecafc21cf748
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:23 +0000

    from themselves top rate goout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_array_to_list(array):
+        return array.tolist()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import random
-print(random.randint(0, 100))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
