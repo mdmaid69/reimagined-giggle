715fcac0c15ab9a6b92a477b38359834cbf4944a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:03 +0000

    substance back 565end surface or

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def square_number(x):
-        return x**2
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_speed(distance, time):
+        return distance / time
