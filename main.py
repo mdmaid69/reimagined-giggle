8d10f0502279a2592c1c2c6b3cf7b7971b6d981d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:11:56 +0000

    physical throw rain brain wrap

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
