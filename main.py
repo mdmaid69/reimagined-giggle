391922ec9739481f3436e8c5629471e1a748fd88
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:14 +0000

    cross flow stare understand polite

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
