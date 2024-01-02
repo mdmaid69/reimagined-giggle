ec483b801e70d8867c42a5c4a4f92283430fe98f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:16 +0000

    right naturally finally worth interested

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
