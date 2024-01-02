19decd539919b688aa8d6461486025dd687b8a12
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:15 +0000

    attract dream them share judge

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
