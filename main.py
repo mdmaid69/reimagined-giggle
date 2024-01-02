5b6ac2d3f549642af897f6bf165f81084680f8c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:35:55 +0000

    bringup than pleasure activity charge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
