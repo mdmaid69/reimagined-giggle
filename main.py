d9e7605986391a42af7009732aed4deb8e7d3670
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:54:21 +0000

    bag below contain back stop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
