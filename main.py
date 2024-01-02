a4d7ddb2ad06848c4779732b7da457779569e750
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:53:44 +0000

    trouble keep Yy book contents

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
