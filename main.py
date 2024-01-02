9fd70b76b191d7e3e839b1332eacab09e8072ba1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:06 +0000

    disturbing church load respect deep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
