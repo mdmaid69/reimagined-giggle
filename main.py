90ad1a5704be5e99942d022bcd70549928b557f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:21 +0000

    appeal fetch sound defend win

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
