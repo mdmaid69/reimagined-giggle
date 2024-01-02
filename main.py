462a6193b59d7df812e529c5673980e68b0dc96f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:01:18 +0000

    birthday polite swimming mad capital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
