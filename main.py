18e7f6a246a67f99b1dcb3715623821301846dbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:25:33 +0000

    waste close especially Pp least

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
 n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import collections
+def create_priority_queue():
+        return collections.deque()
