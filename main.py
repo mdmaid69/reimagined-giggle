5962d5453c5c47d99042c4af9f0a33a3646c0352
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:32 +0000

    regard tour accident sit yet

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import sys
+def exit_program():
+        sys.exit()
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def reverse_array(array):
+        array.reverse()
