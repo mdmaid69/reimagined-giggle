50c0f51409f7142ffc136edf56ecdf9157b67359
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:31 +0000

    rub substance else legal deliver

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
