480d65fc9ec693f8c4ffbb561139eea898252fcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:28:41 +0000

    crowded present set succeed lonely

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
