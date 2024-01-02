4b362b8dde1fa8edeba3a77e663693d8c46f025e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:27 +0000

    suggestion right us tight record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
