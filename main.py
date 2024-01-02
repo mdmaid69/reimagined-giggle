21782ea9d72e5eeb02c10528b3e7ed6cec6f683e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:29 +0000

    Ff walk steer card contact

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
