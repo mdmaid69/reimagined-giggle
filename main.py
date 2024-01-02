67c9bc18181480f1a1d28dce2a7c1d95b25aa41d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:24 +0000

    between stone but sing process

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
