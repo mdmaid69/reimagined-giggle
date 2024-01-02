6cf02615e0815c07d515c7193871eea8982c3aa5
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:11 +0000

    glad cover plastic send task

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_size_in_bytes(file_name):
+  def get_file_size(file_name):
         return os.stat(file_name).st_size
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
