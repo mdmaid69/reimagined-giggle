8700dc5c8f54a01688fb9cf5a889512b458124e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:40 +0000

    danger there group stamp exit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
 import array
-def get_array_item(array, i):
-        return array[i]
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def get_array_as_list(array):
+        return list(array)
