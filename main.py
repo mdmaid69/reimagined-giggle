36acf007e911645dbd76b217e3b5bec70da9d232
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:14 +0000

    sure push come offend runinto

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def reverse_string(s):
+        return s[::-1]
