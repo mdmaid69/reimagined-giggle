a6c72c94aaac7acf341f6adc9f49828b2379fce6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:22:52 +0000

    sorry tooth from rush test

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-  def convert_to_hex(n):
-        return hex(n)
-  def count_elements(lst):
-        return len(lst)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
