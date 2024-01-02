5aa3f6d7b8ef0e100e69ec5842f0cc1d500898a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:40:47 +0000

    give travel start thick broad

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def find_max(lst):
-        return max(lst)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import sys
+  def get_python_version():
+        return sys.version
