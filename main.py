1b727da2ac3ea6705cfa044405ae6dd7bdad53f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:17 +0000

    fact luck service Kk eye

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
