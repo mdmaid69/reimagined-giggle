ed9d1daee781e0dc63383004e9d756c5b29a6afa
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:20 +0000

    attempt release copy flat left

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
