830412c89a2b6afe9ff3dcc328aaea5e44cc4e8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:52:22 +0000

    him demand frightened care object

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
