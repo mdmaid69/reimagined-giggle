895899c0c521f3227e2c8ccf1b47f757537ee409
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:30 +0000

    hit cross bread address failure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
