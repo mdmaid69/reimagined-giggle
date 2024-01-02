4183cac40d3c01917dbfedcd5a8853052b71cc4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:55 +0000

    instead free normal natural centimetre

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+import sys
+def exit_program():
+        sys.exit()
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
