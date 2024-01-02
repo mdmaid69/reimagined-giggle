2c6abd790abe3ea3972126c0898866fa72d7962c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:49:54 +0000

    wire break impossible mad help

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
