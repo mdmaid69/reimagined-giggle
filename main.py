305a66b2744795582ef0062302dfcc59a46d3b15
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:44 +0000

    release hit visit spill odd

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
