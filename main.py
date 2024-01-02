f911050fec521b3cc936725071fbeb118fb0ff97
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:42:50 +0000

    kick share they branch remark

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
