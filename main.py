498c70ba496bd3d1a53fad2085187336a787a880
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:07 +0000

    free mysterious mixture occur fork

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
