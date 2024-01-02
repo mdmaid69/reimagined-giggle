932f84c9801f627dad8e7afb5ee45d42ba07d332
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:32:35 +0000

    fit town polite leave various

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
