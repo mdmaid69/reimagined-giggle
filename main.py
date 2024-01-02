2c37b105ce8b06fce1a7b19936558d9861555a83
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:08 +0000

    sometimes connect cheek healthy shame

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
