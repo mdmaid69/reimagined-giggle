1a3d32a5ef46f723e7b7d338ca4c43d013e9d40f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:46:01 +0000

    claim large dress soon happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
