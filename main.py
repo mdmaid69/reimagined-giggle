f1a5dc693dbce2a05107bf9c2888edc17895f266
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:52 +0000

    pretty constantly ill art laugh

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def find_max(numbers):
+        return max(numbers)
