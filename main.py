7e5f31bffac9bf46689074cde5936a789cf7649e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:26 +0000

    accidental shrink steam embarrassed university

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import numpy as np
-print(np.array([1, 2, 3]))
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
