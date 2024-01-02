9eb75b9e26a585fa075a1edb8460bbd9739fe2de
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:11 +0000

    repair set hope worse relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
