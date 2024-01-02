218217cdc41b2c472af2b50ec85a51736ad93724
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:40 +0000

    whether bring bit himself slight

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_tangent(x):
+        return math.tan(x)
