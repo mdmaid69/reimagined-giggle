6156e039c0dff0c0a5925ede9cf8e50cf7c0e59c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:08 +0000

    exaggerate when sort confused lookup

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def convert_to_binary(n):
-        return bin(n)
-  import sys
-  def get_python_version():
-        return sys.version
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def count_elements(lst):
+        return len(lst)
