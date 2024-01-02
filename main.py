9bf0360480e9856f2f05561371ab33fa096e8d15
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:47 +0000

    poor tone culture treat please

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
