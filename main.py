da0cc16a06cf472ba01b816c2d3e3dbf68f9d38a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:37 +0000

    load stand responsible now wide

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import os
+print(os.getcwd())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
