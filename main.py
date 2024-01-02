577ae91a4a535ffbaab4ebfc13071b1d6f13f06e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:57 +0000

    hide language private he name

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
