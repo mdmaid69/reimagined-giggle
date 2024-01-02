8b96f075f3c6f13505c702c58f2c724c24d3b2c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:39:10 +0000

    beginning short arrange bottom develop

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import re
+print(re.match("h.*o", "hello world"))
