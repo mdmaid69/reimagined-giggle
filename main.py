afe062a6b920e05ed973a9020ea50bc5101e5568
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:03:34 +0000

    alive further after herself taste

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import collections
-def create_stack():
-        return collections.deque()
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
