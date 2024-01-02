86d2d62e60629cb90af0a5fa3596bd64c6768d9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:20:11 +0000

    town rob Vv turn loose

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
