358b8797bcc394fabeb537e70209dea0f1013754
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:28 +0000

    maintain rarely contain woman win

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
