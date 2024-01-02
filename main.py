704d37fcf98189eeaf8a5c6b59c8eddc65b192b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:23 +0000

    there feature swallow threaten goup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
