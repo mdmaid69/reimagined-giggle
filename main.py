949f607c3c34055a4762caee91de8a82ae0d750a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:25 +0000

    extra sail enthusiastic clothes soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
