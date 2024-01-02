bd464e84feec440f06d41f09f324910a6b080968
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:39:14 +0000

    hope good detail discuss Qq

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
-def is_odd(n):
-        return n % 2 != 0
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
