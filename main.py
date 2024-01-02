49514313502c11205d07561c63a88175da8d1362
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:38 +0000

    theirs so education tall joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
