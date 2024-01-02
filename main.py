8b3f634448d995ceb982e2a370d65437768af2c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:44:54 +0000

    immediate tone insist getoff off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
