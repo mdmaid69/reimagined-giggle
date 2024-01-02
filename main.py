a86019f7748b87ae56c1a892509240307720cf79
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:40 +0000

    Hh name wave comedown list

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_acceleration(speed, time):
+        return speed / time
