115996628efdf6ba0d55b747f0db78fa94dc2b90
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:41 +0000

    sign middle opportunity fine read

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def convert_array_to_string(array):
+        return array.tostring()
