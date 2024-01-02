f96694bdd3e8bb3c96fdf480299d30d7deaf6d8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:42 +0000

    autumn uniform die anything test

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
