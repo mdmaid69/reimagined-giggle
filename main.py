9834e3a321dca80bfe17404d4afa51413828a160
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:19 +0000

    meaning floor load mainly firm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
