bb5e4d9a78a8a3a9373a5640a6e27a9d8f674838
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:29 +0000

    far destroy to exit process

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
