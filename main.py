08a334f07fcfe1c4158001a09a8325a24c52e271
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:43:19 +0000

    experiment language height intend delicate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
