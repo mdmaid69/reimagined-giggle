fce7ee2a81870774e2959a28d8dc51bf5c3db0bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:50 +0000

    here even store claim suffer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
