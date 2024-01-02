fc1f0488eb7efef0c7cbc35d91cbb6e7cb9f4e66
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:13 +0000

    tread excellent belief record impression

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
