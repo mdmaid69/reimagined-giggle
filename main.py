030b213a2c96cc96444f8a2e3e514b6ea70515ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:57:01 +0000

    tidy quickly rely regular sing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
