5242460a8448786ae31029b7bca3e369c44391cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:17:22 +0000

    afternoon little address guess at

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
