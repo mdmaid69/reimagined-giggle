4e5f4ae4a73e44f930850046e873a59c872b3cfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:40:50 +0000

    plenty smile used wing runinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
