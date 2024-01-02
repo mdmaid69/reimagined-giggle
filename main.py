c41f5eff9b708bc4a3276f09fe5b5d25b12a6211
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:18 +0000

    discover fit trust no claim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-name = "Python"
-print("Hello,", name)
+import math
+print(math.pi)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
