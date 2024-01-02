6e0579d511332bf8597774f31ac65451f4b51d23
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:45:40 +0000

    know since breathe unless school

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import array
+def get_array_typecode(array):
+        return array.typecode
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
