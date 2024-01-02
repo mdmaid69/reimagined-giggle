0a0dcfca4acc3a9096341721d382b5d930b10b3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:37:09 +0000

    return jump yes sort workupto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
