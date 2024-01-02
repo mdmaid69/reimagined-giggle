1b6557a6cd6807e1d119cce13616ea24fa8dcf00
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:20 +0000

    always block callout broken smile

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def get_array_as_set(array):
+        return set(array)
