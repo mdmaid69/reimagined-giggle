854d535e7f1f44b0e12258432761582663ff364e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:38:21 +0000

    event mainly defeat another scheme

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_pressure(force, area):
-        return force / area
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
