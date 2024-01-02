3f965f90d7b3c30deb876bdfa2cc877fcf67f1bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:56:03 +0000

    weekend that 564encourage wrap test

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import time
+def get_current_time():
+        return time.ctime()
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
