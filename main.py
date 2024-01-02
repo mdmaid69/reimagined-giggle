568c4ff30971bc09ac8978038c7953c70b6fee49
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:19 +0000

    influence react environment Vv argument

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def square_number(x):
+        return x**2
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
