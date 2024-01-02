198354c12964ac3172bb247e0adf8fa29f61e19c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:12:00 +0000

    example modern Uu niggaacademy theirs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
