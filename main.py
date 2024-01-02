d5bdb9ad799d9658f51bcc7e8bf1b137ba1eb314
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:58:06 +0000

    yours free right our command

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_item(array, i):
+        return array[i]
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
