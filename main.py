e7a0d38012d2a804853a3c0f34ab2cbd845fb6fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:10:38 +0000

    doctor tragedy whistle punish follow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
