bb821df83213e66c384702371fe3e803b96b6cbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:37 +0000

    yourself wide imagine trial contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import time
+print(time.time())
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
