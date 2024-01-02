8c60865cfac4bc67f8f7c64d2d434bee0ac8d9d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:39:44 +0000

    small squeeze illness cap his

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+for i in range(5):
+        print(i)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-x = 10
-y = 20
-print("Sum:", x + y)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
