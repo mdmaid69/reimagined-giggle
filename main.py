726ff8a614751f54f1a578a961f02d1fbcb90d78
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:23:41 +0000

    opposite wish bill crime wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
