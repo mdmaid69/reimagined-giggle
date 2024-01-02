134fc06ebf53511cf0030e2741b4a64d0c28b172
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:40 +0000

    name stir criticism actually possession

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
+import collections
+def create_priority_queue():
+        return collections.deque()
