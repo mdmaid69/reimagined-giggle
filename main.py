aa43062f01a26f339e47168f510add1609839fda
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:28:14 +0000

    dark transport attention model part

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def count_elements(lst):
+        return len(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
