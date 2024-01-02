40b6eaae29559004a73dafe6b0fedc72d5bf7742
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:26:06 +0000

    closed respect sell back nigga

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
