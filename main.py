d6c33da373fbd1856f3d4cad5a65d88b2b77058e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:32 +0000

    willing off makeup fond content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import os
+print(os.getcwd())
