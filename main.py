f5ee4c2730f1b184c25b265260c4146e3440fa0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:45 +0000

    this turn downstairs proud turnout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
