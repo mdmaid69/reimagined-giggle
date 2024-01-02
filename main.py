75865c8c20a6f9414a3bf63a8ed175e4ae985ddc
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:57 +0000

    post town mad promise organization

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
