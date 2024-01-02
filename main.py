ca03d9e4ba8c4520758ce3fc8a1a9b021d525796
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:46 +0000

    creature impatient send breakin mass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-name = "Python"
-print("Hello,", name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
