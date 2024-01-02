66ed38c46563e22e49f7bb0607bd286838f4e688
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:01 +0000

    circle command emotion stay motion

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import time
-def get_current_time():
-        return time.time()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
