cdd0505e768011dcbe8b62edbd0afd6701627ff0
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:40 +0000

    regular wind fear OKorokay sport

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
