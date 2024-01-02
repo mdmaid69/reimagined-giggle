e9015058593502a5757c0ec103b3ffc79e4dbc6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:40 +0000

    tax memory find benefit turnback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
