d422b5f56cc55734bdaf41ff502a0d2c3de8349c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:41:40 +0000

    dance weekend drive spoil he

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
