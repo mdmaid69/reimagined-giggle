01d1ab077745e06da059478e5ebc156c25441494
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:36 +0000

    art arm tidy it time

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
