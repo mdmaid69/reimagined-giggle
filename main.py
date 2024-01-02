294b66bbc7f47f786bed0f1cfef842fbb9f45457
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:59:08 +0000

    decision story favourite add confusing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import tensorflow as tf
+print(tf.__version__)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
