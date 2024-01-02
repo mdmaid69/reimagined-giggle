683d87a60fce29024d15e989a32e90d164679ad5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:36 +0000

    565end do road popular wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import random
-print(random.randint(0, 100))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
