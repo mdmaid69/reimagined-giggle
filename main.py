05340eb696651c1d46ac50dc7eff1191f61165f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:22:31 +0000

    rate recovery quantity drop grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
