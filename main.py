5da13edcdd44b5ef3219d93b6519f9fc046c4018
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:00 +0000

    big Uu schedule bringback scared

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
