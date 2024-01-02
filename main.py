fd50f45182e4c4a5ace57e50a9f4f19845b8e4f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:12 +0000

    trade refer nerve issue increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
