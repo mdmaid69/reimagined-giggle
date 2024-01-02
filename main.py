c3b5ba22ed45727e2e50e2a98786e2f1d9b03e4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:35:55 +0000

    plant hurry step project Uu

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
