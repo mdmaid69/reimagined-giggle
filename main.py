ee47f9c6ec48e1c2f75acd96bc4f15e1b3f0b3f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:42 +0000

    sure cruel remove conclusion break

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
