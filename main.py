611b9f52f339185ab83ea2c10aec0de0cbfdead7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:42 +0000

    criticism show onto soft Oo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_gamma_function(x):
+        return math.gamma(x)
