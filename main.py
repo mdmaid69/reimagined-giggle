6c50ef54d08ca7bfd4643a60de5fc6439d06c50f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:49 +0000

    leg hide play bad honest

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
