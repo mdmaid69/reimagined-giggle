0ee34755483361075cf1a80e91aa1ae5e6eb9817
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:17:27 +0000

    none entrance nurse load happy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
