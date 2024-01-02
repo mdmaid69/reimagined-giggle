4f597a73be0185de76da93ab5d633889a277a8c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:40:24 +0000

    seem unpleasant middle refuse test

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
