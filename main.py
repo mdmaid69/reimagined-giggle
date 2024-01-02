d57d54fbb5e1252b5b4c98d1895517e05d1dfde8
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:53:53 +0000

    help clue across brush typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+name = "Python"
+print("Hello,", name)
