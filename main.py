352ac25dccbdaa94d0b96e3b435da224fb67802d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:32 +0000

    vote result effort turnaway length

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
