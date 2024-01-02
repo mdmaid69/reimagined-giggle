6380a18dc4a39236ecf81ae6b4afce7e1778c143
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:41 +0000

    card issue than point solve

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
