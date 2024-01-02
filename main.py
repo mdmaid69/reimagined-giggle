0cd34425e077e315b65a019fe71a6d548c633eca
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:42 +0000

    improve war basis now move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def subtract_numbers(x, y):
-        return x - y
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
