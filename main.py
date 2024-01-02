6d396623c387830261029d5fb52d22dcdca11158
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:00 +0000

    history attention cope cry handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def sort_list(lst):
-        return sorted(lst)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
