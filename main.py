107a92c49d69afebe10d50396ff1057c9e9761ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:22 +0000

    criticism keepoff minority dear question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
