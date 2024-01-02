8a1febff612cfb851ce157aed59c60faf4b41a55
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:51 +0000

    must cold mainly description mark

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
