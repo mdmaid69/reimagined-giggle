6e0bec6d14c5bb70356c003add5eab3ba31fd0a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:08:26 +0000

    cause yourself drawing shadow operation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_queue():
+        return collections.deque()
