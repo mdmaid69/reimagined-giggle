d5d7f6b10072028eabb6a1dc6f638c80df0c9f54
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:26:46 +0000

    badly pipe moment price project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def convert_to_hex(n):
-        return hex(n)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import collections
+def create_counter():
+        return collections.Counter()
