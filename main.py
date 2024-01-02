3ef8444fbffafe2497b6a61a82c5272242cf9015
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:24:28 +0000

    sense lost scared worried fill

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def split_path(path):
+        return os.path.split(path)
