7cf154b39f0173fa419b5dcb44aabefedbd6d31a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:02 +0000

    typical promise pronunciation their kind

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
