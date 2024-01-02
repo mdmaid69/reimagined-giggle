3e5d46f67b6aa87773eef56399ea7def574e5b19
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:19 +0000

    address value fresh would come

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def calculate_acceleration(speed, time):
+        return speed / time
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
