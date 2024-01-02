d57c972a95a2ff1eb97344c74cac244df6aa3f8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:21 +0000

    full point blue arm before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+import array
+def get_array_as_set(array):
+        return set(array)
