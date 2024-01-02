cc1b7a8456ba57cc7788f97f8af695970f1842aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:25:56 +0000

    everybody bridge naturally keepup police

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def convert_to_binary(n):
+        return bin(n)
