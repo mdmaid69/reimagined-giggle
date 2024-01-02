3d1108f29c2b122f9cc8e41ce819f76462fb9a3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:57:27 +0000

    fly wonder keepup equal hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
