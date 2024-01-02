41a9ef0fa34e24b5ad556f89ac35ac9ae5ec3e51
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:18:24 +0000

    exchange apart meanwhile blind pause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
