889f8d9fa8ed220aa569ea07aa944d4f5ff04cb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:40:49 +0000

    judge his experience worth fine

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import platform
+def get_python_version():
+        return platform.python_version()
