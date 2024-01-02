a4eaaccb73b5852b3ec4653fb76c6d9886d9ffdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:09:23 +0000

    you arm little bend poor

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
