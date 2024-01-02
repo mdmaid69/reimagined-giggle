ecf274343e2b9325e42268ca9f08ce68cc66ebae
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:40 +0000

    whistle worried unable private murder

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_pressure(force, area):
-        return force / area
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
