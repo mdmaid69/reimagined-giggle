0ff1c68b3c42c6df14e4e9cd0ac236597c00630f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:43:12 +0000

    information block study minimum fire

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
