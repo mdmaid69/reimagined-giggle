be7d2e890f0380a4f2cb55cb326ae5a10b3f12b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:26 +0000

    discovery acceptable fat course handle

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+def remove_duplicates(lst):
+        return list(set(lst))
