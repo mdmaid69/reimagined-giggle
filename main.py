ff5904916b8f73d8a0759e7a39a7fbdafeff852f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:20:13 +0000

    hit makeup almost exchange attention

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  def add_numbers(x, y):
+        return x + y
