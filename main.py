99622920293e2e888d281c1a633e5671ef6ca063
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:06 +0000

    thick gentle back lay unfortunate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def convert_to_octal(n):
-        return oct(n)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+x = 10
+y = 20
+print("Sum:", x + y)
