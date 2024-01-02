72dd3ed60b295f2cfbf910e961aec2f2b6a4a7fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:48:22 +0000

    shut congratulations pretty production announce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
