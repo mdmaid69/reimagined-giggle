e18aca99e2a264706d185f2af4bc29644d450c2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:30 +0000

    height recently increase experiment they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
