13ca38de0614d789acc9e16174e3c2cd6745acd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:32:15 +0000

    difference rid quit interview pollution

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
