cbd135b1b676525a15715dd76f7dfe7229373d90
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:31 +0000

    earth talkback fasten meaning play

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
