f93a74755a109c2464afccbc5a50d205c24213e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:08 +0000

    over justice bang possible politics

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+for i in range(10): print(i)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
