bc87c46a46126506db015f1ceba7c05ce247acce
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:57 +0000

    both fear trap score lend

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
