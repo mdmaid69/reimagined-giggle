aff3232e563394901fc0cc14787ba24d47c7396e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:12 +0000

    sign eat cough nigga borrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
