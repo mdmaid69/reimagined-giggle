30fbb004d81419d4a2d6705bb20be2ed4861ab68
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:54:11 +0000

    idea church stage wide talk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def add_numbers(x, y):
+        return x + y
+  def subtract_numbers(x, y):
+        return x - y
