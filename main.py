e1ecf0ba13a8e634a4546de4ea9356d5f977b9f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:37 +0000

    pass describe spring me rank

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
