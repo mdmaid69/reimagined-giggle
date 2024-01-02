52e932d26db7fe9ad1a5e8d50cc2b0dc3542a62a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:58:53 +0000

    achievement getout glass jump roof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
