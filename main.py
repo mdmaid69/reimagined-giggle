acb4583a1954d58bcc5c6916764ef8913a40004a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:57 +0000

    test belong theirs mind regret

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
