d0c36bd8a63c3c4979db189b6f18241ca7be182b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:04:43 +0000

    travel worse suckme tight fire

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def divide_numbers(x, y):
-        return x / y
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
