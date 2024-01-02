510714bf23e1c6651d44a324bf2bac71c5b8cd23
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:02:10 +0000

    petrol cruel fat top Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
 def calculate_euclidean_distance(p, q):
         return math.dist(p, q)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
