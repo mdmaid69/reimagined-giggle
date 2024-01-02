b2c336397fd03e10a49365e545c3784427609647
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:13 +0000

    owe disagree workon everyone cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import sys
+print(sys.version)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
