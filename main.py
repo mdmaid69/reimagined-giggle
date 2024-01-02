1e0d0bf67f58b7bd29cf499fa9aa170c400c5c00
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:57 +0000

    grand fun prevent during tone

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_blocks(file_name):
+  def get_file_blocks_allocated(file_name):
         return os.stat(file_name).st_blocks
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
