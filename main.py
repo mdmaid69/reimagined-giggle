44d43a0354fb42086d90e9580588702dd37a2e29
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:49:05 +0000

    discussion bar tape crazy first

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_perpetuity(payment, rate):
+        return payment / rate
