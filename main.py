8c7000e94a772c386a95a9685486e1a38d0115dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:45 +0000

    gas left sweet priority such

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_priority_queue():
+        return collections.deque()
