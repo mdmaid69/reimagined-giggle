132e4bda19c91ea9882d9af660366c0a31f1fc70
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:56:52 +0000

    strong meaning furniture good owe

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
