d3b408705b91b95d2febc11ba2aad6a997e66d20
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:11 +0000

    tie far themselves suit smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
 import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
