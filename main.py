bb38948e9ac2672f61fdc33145bc0e79a6cfebad
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:49 +0000

    probably advanced sleep smell would

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
