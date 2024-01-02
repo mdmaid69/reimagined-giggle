357173d9213c0992424e31d79926f05a9d0d182a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:30 +0000

    bridge inside stand exact busy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
