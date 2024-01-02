11317fae4b5d0d65994b46ab491f15c6e1d03b60
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:24:07 +0000

    pretend who sweet load surprising

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def find_max(numbers):
+        return max(numbers)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
