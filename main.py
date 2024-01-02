5dfb5093016edc150c720cea43824965339125c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:11 +0000

    war wise unite process broad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import time
-def get_current_time():
-        return time.ctime()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
