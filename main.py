28463c977ab3143a77c3666e21c3730011bfe92f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:51 +0000

    measure deliberate creature pattern kind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
