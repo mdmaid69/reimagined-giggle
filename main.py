2fd84e786fd7fbb88c8064209d2c568d49732b99
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:56 +0000

    survive song he right come

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_item(array, i):
+        return array[i]
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
