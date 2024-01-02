118d313d693d7bc0783348d6a4ba1e67233fb6a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:35 +0000

    evening crisis course temperature radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
