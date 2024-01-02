65e3d793046b7094dcd44802722dfe042b7de95c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:31:05 +0000

    herself shout use exam past

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_stack():
-        return collections.deque()
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+def calculate_area_rectangle(l, w):
+        return l * w
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
