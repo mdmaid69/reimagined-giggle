728ad5dd1e5de86a7d5d463daaadd38a6823a040
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:32 +0000

    disappointed lend sound watch genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
