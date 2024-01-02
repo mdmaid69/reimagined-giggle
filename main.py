6bdc2cb58ea091dcd0835d90562f16e7c0b3aadd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:38 +0000

    level estimate float body cut

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import os
-print(os.getcwd())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
