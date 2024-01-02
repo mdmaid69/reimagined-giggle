d5efa990bc4e7d874ca1e7fa5fea9ed21a954e87
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:23:30 +0000

    length subject lookaround dangerous operate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
