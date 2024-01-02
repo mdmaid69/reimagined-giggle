baa6b6cd4aaabfa096a900f36026962f7533ae92
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:18 +0000

    tip interruption steal punish bang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
-name = "Python"
-print("Hello,", name)
+print(math.pi)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
