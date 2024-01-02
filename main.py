62f964a5560982ba75f2f2e5b3d65ccf5782ef54
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:56:20 +0000

    unlikely convincing there much radio

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
