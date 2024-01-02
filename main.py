3c5493580e271d35893833a9d5b4af51385b4762
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:43 +0000

    generally giveback protect under might

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def add_numbers(a, b):
-        return a + b
-import array
-def clear_array(array):
-        array *= 0
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def divide_numbers(x, y):
+        return x / y
