b899a93d38600e0fbd1f68eaf18fff42367d88ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:30 +0000

    everywhere rub study bit thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
