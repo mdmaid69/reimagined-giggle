2f8afca52e30688823aa229e6c0355f20c1c5662
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:02:26 +0000

    long share schedule culture excuse

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
