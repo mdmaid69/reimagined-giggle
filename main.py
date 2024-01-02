d90f2cd8eaa1eae6431bce7a773fdbc12db6807f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:43:21 +0000

    its near fierce turnoff generally

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
