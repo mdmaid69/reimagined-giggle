93e2315bacc79257e9ae4579910984d0bdf61f8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:42 +0000

    shake necessary Vv quarter second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def multiply_numbers(x, y):
+        return x * y
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
