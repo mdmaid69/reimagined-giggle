3c3bc1cf0177bbca2ffec357b589ffa2710d796e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:01:44 +0000

    breakinto connected realize already note

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
