3296ce1f72c96cfd41cf89a91ec4dc8ba26bd9ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:42 +0000

    shut favour Qq set high

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
