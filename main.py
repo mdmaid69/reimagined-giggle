e753d443adc2443893d8cebef11d5a0251d7a0cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:42 +0000

    after store fall report breath

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_string_from_array(array):
+        return array.tobytes()
