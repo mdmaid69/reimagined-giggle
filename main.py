e255a6cea40907de932dcc0977ec19ca410147e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:20:44 +0000

    satisfied tradition shock corner science

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
