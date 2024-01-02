b1be26126e01eb81316aa48551663dcbb83eb4ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:26 +0000

    here successful find proof still

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
