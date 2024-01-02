78f0b76be7f20a61461d7caa242646b066db1218
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:09:59 +0000

    collect soak suit suit match

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
