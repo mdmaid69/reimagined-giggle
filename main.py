f5e0a6a4d03020c2cd8198021275c257079f1233
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:05:12 +0000

    repair independent slip forever copy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def divide_numbers(x, y):
-        return x / y
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
