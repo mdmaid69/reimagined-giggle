caf51dad56290789e3844e0a7e641ff501851ec2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:32:32 +0000

    cause basic appearance lock operate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
