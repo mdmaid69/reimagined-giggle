bb3c1b38ebf5533c7fbef59ca34865ec5f40711d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:05 +0000

    comment peace furniture takeback middle

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
         plt.show()
