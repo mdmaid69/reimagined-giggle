5f48ab6b9c0d440723e1f99d3a0f5042a88cd54d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:55 +0000

    acceptable cope delight spin complain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
