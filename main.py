e2d1f0326981879a79857a3b4e9ef3735513e14d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:09:17 +0000

    account Tt maybe ignore jump

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
