702fb9c8289121cc844ef318bc5c47d03b5d70d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:23:38 +0000

    congratulate meet wide will violence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
