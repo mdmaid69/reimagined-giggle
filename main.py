d8873911496c36ba6d7df68dd6d1fd3504373086
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:37:15 +0000

    her accidentally attitude measure hold

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
