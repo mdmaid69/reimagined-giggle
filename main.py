baa972316909fa705de5d9d362ae597e72880db6
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:10 +0000

    seem drive tragedy so lock

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def square_number(x):
-        return x**2
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
