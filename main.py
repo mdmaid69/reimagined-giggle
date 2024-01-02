b0bf5f1e52ac1ac52c3a90c5afb0589f68f58e60
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:31 +0000

    fear choose round content left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
