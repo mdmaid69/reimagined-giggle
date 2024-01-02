9809fce75499734adb5ce14026ac87b1fe64556c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:46 +0000

    freeze nice uncomfortable win fresh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
