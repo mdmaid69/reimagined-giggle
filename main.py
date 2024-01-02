ef2b2968ed04ca8fbc53d434b8a1378e480a90c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:19 +0000

    town effect congratulate we train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
