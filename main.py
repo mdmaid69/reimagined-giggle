a8ce999fef1c7fad2649eb3d19243637c0be56bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:59:11 +0000

    should convincing regularly flow show

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_power(work, time):
+        return work / time
+for i in range(10): print(i)
