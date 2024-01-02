d23bf9f8b1254ecef074f8fada0b7b184d0bce99
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:54 +0000

    seat strike taste go can

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
