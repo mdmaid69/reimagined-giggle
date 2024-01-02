950bc5f17c483d24d2cc8e006175badd070c4cdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:07 +0000

    soak Kk interview touch listenfor

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
