e50f606a03f4f46d6f4c3a3eeddbc34db067452f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:58 +0000

    emotion meaning subject tired conclusion

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+def set_array_item(array, i, item):
+        array[i] = item
