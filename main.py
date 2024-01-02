51cfc3b0fe42cae07af38ac4d354003047cc8b9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:39 +0000

    explain yet conduct frighten wash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_characters(sentence):
-        return len(sentence)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
