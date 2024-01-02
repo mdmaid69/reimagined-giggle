5af16616e6eb18d315a63ea75aecfbe2a809f6b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:51 +0000

    record fire example demonstrate spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
