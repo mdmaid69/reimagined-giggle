c0058a27e7b9597ececcc64769fff5631aa202ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:32 +0000

    short border easy shade almost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
