7f71bc128067e730ed228abbdda6458b2fdaf477
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:30 +0000

    just innocent trade neck flow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
