ce742267dbd692bb57e02d70192425adaa034b5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:48:08 +0000

    sharp concentrate see choose question

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_power(work, time):
+        return work / time
