f18da4b2293b33e6b4a980054bd4cbfe96d2f28b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:40:26 +0000

    shade the count schedule dark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
