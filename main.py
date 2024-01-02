e7a164b3f5acea66a36c0ae58e52b29247db1a23
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:07:30 +0000

    worst regret here still or

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def greet(name):
-        print(f"Hello, {name}!")
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_acceleration(speed, time):
+        return speed / time
