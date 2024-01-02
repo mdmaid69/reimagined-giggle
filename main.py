053f85a038e5194b8d1136c95db26cef04404e3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:32:16 +0000

    interested serious Ii clever field

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_current_time():
-        return time.ctime()
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
