290f699a657a3e0c8803b2e75b622e2b7d284e9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:06 +0000

    like feature card should prove

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-import re
-print(re.match("h.*o", "hello world"))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
