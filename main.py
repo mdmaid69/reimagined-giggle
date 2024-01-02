cff5a1ea34f19f163fc626de0c2899106ab2b85c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:47:31 +0000

    571enough young last strike form

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
