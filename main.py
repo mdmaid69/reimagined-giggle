cd9b53cfd9c6f55a167e5c234075ad75caa497c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:01:11 +0000

    disturb both back first possibility

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
