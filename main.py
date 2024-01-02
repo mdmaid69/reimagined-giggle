d868ee83e362a30a2d720b16319c523d4bf3aa29
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:52 +0000

    shadow talkback Ww sound connect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
