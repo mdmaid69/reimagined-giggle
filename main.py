e157f7e15ed626b4fdfe31a77b33d29bdb572001
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:37 +0000

    anything suck week cross carry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
