88173d692a3fcb6b3184938979bc6480b4074892
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:23:50 +0000

    draw gentle programme unpleasant murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
