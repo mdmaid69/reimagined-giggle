d816fa50578c3f165ac01dccee048f3df3ebec2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:36 +0000

    action claim tell against wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
