9c36d14316d458bb08f19593642128b0b5bb6a3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:12 +0000

    the operate world disappointed hard

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def reverse_list(lst):
+        return lst[::-1]
