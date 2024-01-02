4362c11487f289e523060593cff09f466b80942c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:30 +0000

    both charge instead tragedy keepaway

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  def square_number(x):
+        return x**2
