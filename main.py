57dff46bfa666c31876ae35764a9cc6b1e81877e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:56:10 +0000

    blow solve your rush soft

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import platform
+def get_os_info():
+        return platform.uname()
+def convert_to_binary(n):
+        return bin(n)
