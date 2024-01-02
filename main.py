fffbc775ba261ea0df3c8b43d28a0f8d6846ba10
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:34:32 +0000

    above dead crash university do

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
