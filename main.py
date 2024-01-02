aa2b72e4df6893f67fe04e69cfebcaf6dd33ab7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:39 +0000

    all its fun beside shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
