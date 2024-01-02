8ffa50b8d72bddad92f25f77bc8781843118bb5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:57 +0000

    front file official break goaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
