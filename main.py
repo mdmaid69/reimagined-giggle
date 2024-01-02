a9a7b27589785cdafa1c2c8d09673bbb062d6057
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:37 +0000

    expression principle today finger skill

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
