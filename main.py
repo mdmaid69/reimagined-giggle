c26898c5b7524d340accb2831a6a2934901c69cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:56 +0000

    street care call remember ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+def divide_numbers(x, y):
+        return x / y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
