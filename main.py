86368ad5b2a64eb3f722b0cd39d3b0a32cee0f5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:48 +0000

    disgusting container tie memory offence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import array
-def get_array_as_str(array):
-        return str(array)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def get_array_typecode(array):
+        return array.typecode
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
