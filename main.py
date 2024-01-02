316a7e6505ca6beea4951e878edec2a95b0da9b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:28 +0000

    precise speech introduce matter takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def convert_array_to_list(array):
+        return array.tolist()
