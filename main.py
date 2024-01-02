dcc4a16dd45daafeef24cbc24afcdf4eec875c44
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:40 +0000

    male drop blue letter amount

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+print(sum(range(10)))
+def calculate_work(force, distance):
+        return force * distance
