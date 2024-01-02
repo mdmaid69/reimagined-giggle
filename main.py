155a6ec2c86337c73d1477addceb200140773a47
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:32:05 +0000

    dick operate honest divide famous

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
