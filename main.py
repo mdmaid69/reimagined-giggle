2fd4d7ad4a14ebe4f929afdbebf5cef719786bf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:07 +0000

    music weekend straight disagree wait

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import getpass
+def get_username():
+        return getpass.getuser()
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
