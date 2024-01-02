928fffe072df0934cc1cc37a9a720cacdbf2b531
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:04:50 +0000

    sore section match Oo far

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_work(force, distance):
-        return force * distance
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
