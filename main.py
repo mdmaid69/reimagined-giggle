c0f6ca7a70e907784ca7ed12fe806f76cc77c38b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:09 +0000

    sea company weather unless exactly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
