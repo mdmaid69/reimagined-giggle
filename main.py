d5d92a7c269b4ca558f5da85efd182530953b69f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:24:46 +0000

    Gg mass inform stick fierce

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_power(work, time):
+        return work / time
