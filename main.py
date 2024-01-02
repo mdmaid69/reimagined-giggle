bf996cd04cf72a7c90099b0f99af3df1c6cc9b1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:50:35 +0000

    ring nice workupto question want

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
