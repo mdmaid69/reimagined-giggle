19df4fcee517feb42865d3e9d49a9c905eea835e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:04:09 +0000

    delight preparation study straight gas

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
