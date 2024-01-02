00a3eb115d54af5347293b92e8631742de21d290
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:53:12 +0000

    flat like Rr soon scheme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_current_time():
-        return time.ctime()
+def find_max(lst):
+        return max(lst)
+text = "Hello, world!"
+print("Words:", len(text.split()))
