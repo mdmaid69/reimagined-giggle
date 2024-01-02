3f6511694cf7cfa2362621d12118551669de1a07
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:52:17 +0000

    truth sea sister discuss neither

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
