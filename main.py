b3fb8f578107ab86dafa93640b17984c1a103d0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:06:56 +0000

    since goback(to) patience wash switch

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def is_prime(n):
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
