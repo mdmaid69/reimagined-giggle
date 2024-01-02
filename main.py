302d632ca0625827abfcbc3531d1896f22c51821
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:32:57 +0000

    possibly term fresh I 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
