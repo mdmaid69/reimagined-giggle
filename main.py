77d675e2ad81d85805f4211ca3509037840871b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:03 +0000

    congratulations edge mysterious deep far

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
