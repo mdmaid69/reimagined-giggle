74f8846fe6b1542cf564837d07e6cd2a0e02a80f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:29 +0000

    ability fat emphasize minimum minority

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
