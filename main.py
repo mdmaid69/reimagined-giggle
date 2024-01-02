6902892da6211029bc606a511f7b1796c77b0cf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:53:32 +0000

    runover urgent point common cause

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
