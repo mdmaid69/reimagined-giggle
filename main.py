09ccaf1be82a83839f582f3a3b06e82f71182a8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:20 +0000

    skin better imagination concentrate last

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
