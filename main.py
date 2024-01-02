29a3afcc9d1c5491c38e1d16d29a277f39b87538
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:58:03 +0000

    front little this challenge responsible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
