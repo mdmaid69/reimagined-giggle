8ad5a5bdf2fd6363f4790b08d93b62ac86dc99b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:42 +0000

    dry refuse luck trial similar

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def reverse_array(array):
+        array.reverse()
