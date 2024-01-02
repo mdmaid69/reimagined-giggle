d85214e86b2f73e2145ee585dba37555d1938df5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:26:24 +0000

    destroy makeinto direct wear ahead

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
