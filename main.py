c32406e6844eb3288aacf9d603aa662d4eaa146b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:27:35 +0000

    everywhere tonight black so born

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+def divide_numbers(x, y):
+        return x / y
