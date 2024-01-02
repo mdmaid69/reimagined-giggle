e0edd7023de7328099b3cd810a4ecb527af4671c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:53:08 +0000

    sentence so blame pin plan

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
