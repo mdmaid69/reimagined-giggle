ee9f9799742c12fc46999a67f0a28d26aad0f240
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:31 +0000

    match fresh separate border mend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+print([x**2 for x in range(10)])
