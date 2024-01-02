453704471b537c450f38c98e66696f4395d72852
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:26:25 +0000

    variety repeat letter brave complete

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
