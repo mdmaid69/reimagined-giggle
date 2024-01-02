2b8ccf2ecfdeeb32ad5e8b4ee0c9d4059e484495
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:37 +0000

    feed heavy prove tidy shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
