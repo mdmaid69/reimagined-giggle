7a8d1d7d4584ee5c203b402d8d03742a44e36112
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:50 +0000

    ability land prevent good machine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
