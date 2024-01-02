67b1703a5ce949b31b20fb7e002aae95eb27fa24
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:27 +0000

    defend leave through delicate takeoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
