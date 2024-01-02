5b5ae5ff8214b9d0e56692f7904a480fe7d7807f
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:51 +0000

    nurse plenty over wide near

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
