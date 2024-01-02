3a099116d57f5ef5519e09a17f27256122685d17
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:06 +0000

    respect tour shopping rough mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
