8582123d049aef2b5dcbe901398fe8ebd27ccefb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:47:15 +0000

    fire before love separate leg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
