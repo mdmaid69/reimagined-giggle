6e3b9a1b2a6e75916bebdadf83252b8e96feccfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:17 +0000

    agree mess moon welcome group

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def reverse_string(s):
+        return s[::-1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
