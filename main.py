55af22224906290f303df28c1748105eea3d88fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:54:21 +0000

    throughout smooth without surprised card

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
