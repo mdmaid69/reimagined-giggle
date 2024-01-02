444d41fbf5e9e03ed515305f328d637eeff53db5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:48:01 +0000

    operate towards shout disappear habit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
