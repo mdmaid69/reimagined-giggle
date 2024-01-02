301482973c9996cf0758a5b93a5f7e56584f911d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:50 +0000

    benefit himself ticket rain next

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def is_odd(n):
+        return n % 2 != 0
+def count_words(sentence):
+        return len(sentence.split())
