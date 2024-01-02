dcb3c34d5de195b7c8ecfd4ac2aa6f0db31ec6f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:21:50 +0000

    recovery profit cause release doubt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
