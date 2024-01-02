dab5bd346165306d2b6700f65009d8220a9f5246
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:26:50 +0000

    I reaction challenge release mend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
