0144d1d9a404897b6a4c9daba242ce83264696e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:52:52 +0000

    intend Qq fix equal fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
