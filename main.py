dc733ffc46ad6ac3e92e81431be8667af6ea0b56
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:01:06 +0000

    fun Dd use careless price

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import collections
-def create_queue():
-        return collections.deque()
+def calculate_power(work, time):
+        return work / time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
