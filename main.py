ebb2f78ad88ac1fdd868411aec2c2190fab0c99b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:16 +0000

    damage underneath build date dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
