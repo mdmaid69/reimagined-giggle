6ca18987e1bf4749d61f2cc3389e36221e1535a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:12:38 +0000

    their risk goback(to) accidental squash

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def cube_number(x):
+        return x**3
