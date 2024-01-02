972522155d73f77eea97c59cb7213d104bc8054a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:03 +0000

    as after can well close

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import os
-print(os.getcwd())
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def get_array_itemsize(array):
+        return array.itemsize
