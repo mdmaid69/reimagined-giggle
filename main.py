dff6c06a07ab4ea77e633aaf2db6110bcdde79b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:21:43 +0000

    variety example very list protect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def set_array_item(array, i, item):
+        array[i] = item
