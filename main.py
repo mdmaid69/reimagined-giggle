63da62b271f9c66c8f36944c6450d3cc95d6c50a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:34 +0000

    subject step promise shout love

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def get_array_as_float(array):
+        return float(array[0])
