5808d0804350f644b2b4dd5a56a30df2851d79e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:19 +0000

    still row factory describe reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
