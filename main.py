f91b7a24606a4775433727375060083d9d628c7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:26 +0000

    dangerous expensive note fold century

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
