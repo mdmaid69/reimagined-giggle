5e33855af7315c6c44935d3c9a83e6cbedb1b91c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:20:46 +0000

    female deep story proud succeed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
