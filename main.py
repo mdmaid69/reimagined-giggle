e448fe1aadaceadc6eb665e9b4414c2d768edd5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:50:05 +0000

    sell difficult along everybody pile

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
