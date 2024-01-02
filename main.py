10606d0f63d4d3caae29a5e909f4447132c8c08b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:35 +0000

    sorry admire mine negative bury

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
