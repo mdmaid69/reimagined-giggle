8e1680c87bf36255da9a3ecd92905ee7ac6fa309
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:17 +0000

    rain workon say swim affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
