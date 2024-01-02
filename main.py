335bec3782f973844f83b93dc26792e235d24ac5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:46:15 +0000

    confused 571enough nose may trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
