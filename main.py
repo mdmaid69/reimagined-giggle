41c3720600a73b091b3c639d42043f7b5e3b5c96
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:36:49 +0000

    gentle taste once run round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
