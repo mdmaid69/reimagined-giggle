3712bcb707d093bcd6f65cbbde227ffd7399080f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:30:45 +0000

    rush behind foreign attractive twist

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def find_max(numbers):
+        return max(numbers)
