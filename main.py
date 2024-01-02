426ff39d2f75fa6bf96f0744c01dd567a016d441
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:20 +0000

    male character keen draw can

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
