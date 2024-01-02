b00deb6014007864f4d93653516f5b7ca2c1f723
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:04 +0000

    damage demand communicate by organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def get_array_as_bool(array):
+        return bool(array)
+import array
+def get_array_as_bool(array):
+        return bool(array)
