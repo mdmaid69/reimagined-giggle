ce9e069dfb6fbbc4ad6f2f9b5ae7e5ce3b1f4522
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:31:08 +0000

    television average noise wash shut

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+for i in range(10): print(i)
