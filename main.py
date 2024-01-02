0281c2213865c08e2ee7689a74cefb44b76e808c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:25 +0000

    side partner trial community broad

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
