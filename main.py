dc0369af0d1a393546ab862615387e65e067ed0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:09:33 +0000

    satisfied agreement while immediately confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
