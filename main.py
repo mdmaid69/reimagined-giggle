5b300ab6b3b5aaa9e01af2ae364fa0eaed68d504
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:02:09 +0000

    head use sensitive nearly near

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def reverse_string(s):
+        return s[::-1]
