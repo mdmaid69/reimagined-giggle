913de558d797790c09ff65c1eb98136661596400
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:55 +0000

    charge want far entry finally

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_user_dict():
+        return collections.UserDict()
