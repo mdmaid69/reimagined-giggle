fbc009bb9fd06e3ac3246c93321bda4c5c44b313
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:14:52 +0000

    butter equal public copy pattern

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
