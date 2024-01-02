fe268ce9171d7261435b320ef1c1a9db3f2bf13e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:18 +0000

    there century embarrassed person persuade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
