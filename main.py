fd11ecf66e8afd86e5147b35d1c82f6e2025db92
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:05:50 +0000

    simple collection type bang bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_area_rectangle(l, w):
+        return l * w
