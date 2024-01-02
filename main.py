3dda5529a757362c746fde22177bbb8f4e35886b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:29 +0000

    getoff concerned loud heavy glass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
