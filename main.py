8afb6cc28790cb449087b2466ffedb5ffae0a1ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:37 +0000

    steep tragedy star home switch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_max(lst):
-        return max(lst)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def count_words(sentence):
+        return len(sentence.split())
