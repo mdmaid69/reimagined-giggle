c7a1dc3e22e228a05afc6cc20fc6ba9ea3124736
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:18:25 +0000

    conclusion stare show translate minority

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+print(math.pi)
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
