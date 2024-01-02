4ec7123696ff07b01c6a338383682ea4ec1c51fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:08:25 +0000

    true few everywhere Ww equipment

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def clear_array(array):
+        array *= 0
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
