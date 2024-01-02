aa90e6fd1bf8f89f9c2be9819c4f5bffc5bb2bbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:55 +0000

    Tt carry sleep remain fence

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def count_elements(lst):
-        return len(lst)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
