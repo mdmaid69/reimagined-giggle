603e993fca3a4eeabd0a079597f25204facb4354
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:36 +0000

    swim wild charge tradition coat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
