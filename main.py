6d55c58bd063bdd249d2a19c05e43593aa55e078
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:00:23 +0000

    theirs match getback trade deep

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
