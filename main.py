b4a877b83db9d5567dcdcd03e11e8d5dabd4aaa6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:11:02 +0000

    ago type approval noisy shall

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
