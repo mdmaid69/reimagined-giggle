5a725a7a89158357a1def2c44fddc359afa421e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:04 +0000

    scrape sweep favour rain normal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+import platform
+def get_os_info():
+        return platform.uname()
