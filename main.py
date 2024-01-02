6cc6f94f5ca9cd9dfaf3971cbf5580c5612b9496
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:08 +0000

    discussion minimum reach lunch quit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
