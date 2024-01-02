d87320847bfd8eccfd65f2d93baa39d386921069
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:37:50 +0000

    once since father task makeup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def cube_number(x):
+        return x**3
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
