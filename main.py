a8e7d6f66c94740cfb42d3ffa1b8d5bdbfec4128
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:35:34 +0000

    keep fail image left search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
