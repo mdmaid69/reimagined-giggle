1a129bf016142c976d2d201bb7e39b42479d026f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:28 +0000

    law she work just how

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import random
+print(random.randint(0, 100))
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
