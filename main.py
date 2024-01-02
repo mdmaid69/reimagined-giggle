b3be4268f72686040b2783857aa542bb790753f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:01 +0000

    style moment wipe wheel smooth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+def calculate_work(force, distance):
+        return force * distance
+import os
+print(os.getcwd())
