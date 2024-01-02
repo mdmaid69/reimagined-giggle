5eb300c69c531f5576ca14a8ee7e69bfbb5d2a61
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:50 +0000

    storm pure left ocean post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import tensorflow as tf
+print(tf.__version__)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
