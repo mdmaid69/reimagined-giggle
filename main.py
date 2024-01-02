5687d9c4ffe4213c0071eca362a44a3cc216a6af
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:42:18 +0000

    slow swing future cinema acceptable

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
