307887ef894b96dc54f766c77111b6f2d1b19fc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:25:23 +0000

    call foreign meaning bar afraid

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def convert_to_octal(n):
-        return oct(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
