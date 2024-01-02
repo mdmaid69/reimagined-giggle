71f3988c85cf0c511184a709a3df866a689d236a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:43:45 +0000

    thinkthrough bad frighten warn top

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def reverse_string(s):
+        return s[::-1]
