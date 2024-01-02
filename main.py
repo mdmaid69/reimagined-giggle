7e0999cb8dcebdba49aa8ccd28fff19d8fecf2da
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:53 +0000

    previous disturbing ring chief compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def clear_array(array):
+        array *= 0
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
