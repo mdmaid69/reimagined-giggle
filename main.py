2ca750a22f7844140377b794d32eee5a0982d3b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:10:36 +0000

    confident no angry search circle

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import getpass
-def get_username():
-        return getpass.getuser()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
