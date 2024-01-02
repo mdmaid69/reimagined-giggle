04c33a6c6d1c0aa8c54ee0232e86ac804f8d516c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:26:46 +0000

    price willing mean strike able

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def square_number(x):
+        return x**2
