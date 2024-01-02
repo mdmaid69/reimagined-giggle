7c931f53a862eab30ab1e2836c5d58a52adbbca1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:32:38 +0000

    unless quick extraordinary confidence upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def get_array_as_bytes(array):
+        return bytes(array)
