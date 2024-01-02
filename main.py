1627c65a59b3547696f269370478ef9ff2d14c2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:25 +0000

    hole family welcome separate Ss

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+print(math.pi)
