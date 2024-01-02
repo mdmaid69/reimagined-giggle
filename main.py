98b537fa14c78d00d99686689eef9a3ce9c1d62c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:45:40 +0000

    getoff shoot beside lower spread

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def cube_number(x):
+        return x**3
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
