d97be5cf8e36f14d52efbdd9dc51dfc1e433f0bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:59 +0000

    getup collect what 565end scrap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
