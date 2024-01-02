441e0fe98b671df6115380df953a150606a92af5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:30 +0000

    569enjoy comeback change blame mainly

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+def get_array_as_list(array):
+        return list(array)
