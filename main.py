383e1e1a3b59926cccfdbfabdeeeaa12d193bca5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:18:02 +0000

    century with only flow intend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
