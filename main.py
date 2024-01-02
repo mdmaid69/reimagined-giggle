b99f423924f4b5cb992a278e59c6ebfc26175c4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:32 +0000

    improve case behaviour charge dance

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def find_max(numbers):
+        return max(numbers)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
