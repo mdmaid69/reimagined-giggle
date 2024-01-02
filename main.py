18d094b03f5a9fc2570488a87b60279349477045
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:54 +0000

    project immediate conduct convinced scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def subtract_numbers(x, y):
+        return x - y
+import array
+def convert_array_to_string(array):
+        return array.tostring()
