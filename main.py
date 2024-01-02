f0c0a5220d7d8c62c25d0784c7df2bbebbf4f980
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:49 +0000

    protection team store last thanks

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
