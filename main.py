a8ad684182ac771b6c5b87a7d8ee8d89fcea66a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:52 +0000

    ought dream good begin later

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
