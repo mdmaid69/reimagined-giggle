44dc154a1f4c0c866d86ef45494624fbba174904
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:02:47 +0000

    law copy shoot place oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import random
-print(random.randint(0, 100))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
