2717fa25b8dc4c6fa7d36c0f81024ba829ebb38f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:03:39 +0000

    stick dry process guilty gentle

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def convert_to_hex(n):
+        return hex(n)
