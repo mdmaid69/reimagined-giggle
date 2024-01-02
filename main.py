541b28e1dee3fc25d3ae8af347aa01d0aa5d9a6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:51 +0000

    our suit sensitive 573entertainment gas

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
