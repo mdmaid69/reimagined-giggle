d04912fb0b209c5e9fd8e3d2f54323d292249c77
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:01:29 +0000

    confused everything sure shape hurry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
