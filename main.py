780a253296d147bd5bf1a268689e9ad059c45eef
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:05 +0000

    temperature proper catch drop warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
