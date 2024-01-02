c259884eee553a419b7a72e82f17cd64b915baff
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:31 +0000

    tidy draw fat answer opinion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
