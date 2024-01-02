97c979b8c65bcc4aedc0b3f2c0e6f39010f05e85
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:09 +0000

    future speed bang number touch

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def find_min(lst):
-        return min(lst)
-def calculate_power(work, time):
-        return work / time
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
