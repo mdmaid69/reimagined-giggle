00f4b5778d655cae16e1585c510d3046d05dcc1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:14 +0000

    win flow cope butter afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
