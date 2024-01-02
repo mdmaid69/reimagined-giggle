7a4ba8efa07b90382beacfee7c47d72f729db9a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:34:49 +0000

    impressed contact world wild spin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
