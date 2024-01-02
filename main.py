4609ceb7596823334238a28f738946778afc1912
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:27:29 +0000

    hope ground fine during anxious

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import re
+print(re.match("h.*o", "hello world"))
