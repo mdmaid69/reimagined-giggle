4cce847559d407acced9950bdf7338e8acdf6f73
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:44 +0000

    past persuade grateful now walk

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
