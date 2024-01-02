bdfbe6639b88e6089622960cfada8d74f4793a2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:37:59 +0000

    promise pretend brown guess dinner

--- main.py
+++ main.py
@@ -2,6 +2,5 @@
   def get_web_page(url):
         response = requests.get(url)
         return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
