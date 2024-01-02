25ae52f8f722b3c3487807a80da9c450a8416a7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:17:27 +0000

    arrangement steal giveout ago prize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
