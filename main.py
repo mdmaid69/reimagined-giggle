9078d80c9c8a339b9b655ade46a5237dc2fe70e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:28 +0000

    leave influence spoil eat black

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  def is_even(n):
-        return n % 2 == 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
