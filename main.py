83fefcbe125101a3a978b645a5f9852c4ab4f23c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:21:18 +0000

    opponent manner post divide announce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-def is_odd(n):
-        return n % 2 != 0
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_hex(n):
+        return hex(n)
