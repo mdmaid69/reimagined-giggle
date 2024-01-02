8102d51f123c3179a66dd933ef84e64dcb393cc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:43:16 +0000

    skill her age think what

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
