0cf569063e6b6978add8bafc5c733575c53d74e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:27 +0000

    because tax importance without arrangement

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
