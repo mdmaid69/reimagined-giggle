c1feb95950666d0717646445722d8ecedbbf8f79
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:07 +0000

    will dare wet it left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
