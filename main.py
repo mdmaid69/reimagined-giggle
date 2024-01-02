3597a1a95ce777656890a3e1ef8107f87f21f005
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:55 +0000

    offend hole be fork beneath

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+def find_min(lst):
+        return min(lst)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
