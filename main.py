0e7d2c045ef36a0755a74701ccb9883ddccbd837
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:00:46 +0000

    life hospital foreign police suffer

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def count_words(sentence):
-        return len(sentence.split())
+  def sort_list(lst):
+        return sorted(lst)
+def find_max(numbers):
+        return max(numbers)
