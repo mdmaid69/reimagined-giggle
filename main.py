1ed7aebdbb9e53b8e2ee18172b3c66e5cb8b2dd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:11 +0000

    other form pause stay print

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
