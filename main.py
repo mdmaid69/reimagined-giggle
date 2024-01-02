05b88c810a411adace7bab22873dcbe0a1cc9065
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:19:04 +0000

    tongue clear stay improve excuse

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import json
+print(json.dumps({"name": "John", "age": 30}))
