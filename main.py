9c0af9a66918f9a5e6d8b573dc4e90a027238416
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:08 +0000

    considering impatient tour reduce letter

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
