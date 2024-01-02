7388ca5bdcf999c29c28b818a106db95d134afa7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:10:45 +0000

    dig charge getback want friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
