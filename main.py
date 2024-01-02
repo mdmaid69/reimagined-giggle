3cb5dec0202051d86161f67834f8e7b4c49de7fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:15:19 +0000

    mass country watch border connection

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
