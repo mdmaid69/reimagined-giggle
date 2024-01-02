b7f224fe7aebbe4333b3fb5e18ff68a08cb9b126
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:14 +0000

    by lookafter concerning check phone

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
