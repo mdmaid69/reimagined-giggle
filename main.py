d294438f0dcf5ec6099faa90663652ee1b51e7d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:23 +0000

    touch clear read payment glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
