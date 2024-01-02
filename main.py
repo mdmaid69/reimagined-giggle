bda0f7fbc19ca2f57e381f82d8cc139405e0d019
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:50 +0000

    lookafter just impression slight shade

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def reverse_string(s):
-        return s[::-1]
-import tensorflow as tf
-print(tf.__version__)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
