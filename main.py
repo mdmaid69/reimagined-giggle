e2448fa30dd93f9d0d2b7cf916ad1fbb3b614047
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:08:47 +0000

    suffer when wet trick clothes

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
