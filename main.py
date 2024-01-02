f5caa761c08f2e4b2f287fc150a70118f532241f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:23:50 +0000

    partner turnoff will centre music

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
