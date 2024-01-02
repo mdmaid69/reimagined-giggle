15439a448e6faed001a1bc0920b831c9d25eb5ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:57:12 +0000

    murder Kk black treat exit

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_item(array, i):
-        return array[i]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
