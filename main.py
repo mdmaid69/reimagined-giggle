e6bb519266bfd826d30a881fc741e9370472781f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:31 +0000

    term since doubt lookafter original

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def count_words(sentence):
+        return len(sentence.split())
+import array
+def append_to_array(array, item):
+        array.append(item)
