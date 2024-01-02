58c14e829738c37150b44e3d3aa33a49fe969f3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:06 +0000

    basically repair steal through pool

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
