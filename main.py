35974810a3a784ea984d35d7308cbd6895b524db
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:24:08 +0000

    influence than goon ask list

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
