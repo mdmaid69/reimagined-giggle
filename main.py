7e23ff85144fbef12662d4b58c27fd10c81b4525
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:59:44 +0000

    ashamed apologizeapologise health matter visit

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
