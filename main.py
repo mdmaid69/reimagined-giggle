00eec0ca4acffd6c7c2ab7134b8e64416800b7e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:39 +0000

    least turnout by like confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-def calculate_density(mass, volume):
-        return mass / volume
+import array
+def remove_from_array(array, item):
+        array.remove(item)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
