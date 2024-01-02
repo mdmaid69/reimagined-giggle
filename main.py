196b140017bec5a6b02e3ede86c0ef92e4150714
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:00:04 +0000

    remove whatever here put feature

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import time
-def get_current_time():
-        return time.time()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def split_path(path):
+        return os.path.split(path)
