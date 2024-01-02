1fe45f433fa117dafa4cbbbaff3649e5caafab80
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:22 +0000

    unlikely getup mass nowhere shoot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_ctime(file_name):
         return os.stat(file_name).st_ctime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
