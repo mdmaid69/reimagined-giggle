6dc2ab1b42e07354ec4695ed4bd5aebb4f52a3ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:56 +0000

    risk track just turn tooth

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-  def is_even(n):
-        return n % 2 == 0
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
