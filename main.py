7df564a756ed1535f0c67ddc88bdd0c70b0420d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:09:47 +0000

    remain factory full ilovedicks belief

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def find_max(numbers):
+        return max(numbers)
