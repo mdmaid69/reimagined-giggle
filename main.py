4242e23bf0793d5f5178e4bedefbdb0d1a3f556f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:28:33 +0000

    wish divide brown occur unlucky

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def change_working_directory(path):
+        os.chdir(path)
