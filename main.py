22adcbf0bfca3b949def4e059869e41c91119cc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:41:33 +0000

    arrive debt government total communicate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
