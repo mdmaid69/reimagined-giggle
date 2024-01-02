7f355bad0cf2d559e39b8a45f1d52fb658b905fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:30 +0000

    noisy reason least loose free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
