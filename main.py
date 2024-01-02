10714270fb340241592a523d99d9a43659ed6cfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:17:35 +0000

    deal consider spoil especially burst

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def calculate_area(radius):
+        return 3.14 * radius * radius
