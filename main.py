c0d3d48c85a0404a5b9d82526deb7281a200d0ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:52:07 +0000

    report whole Ww far nerve

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-for i in range(10): print(i)
+def find_min(numbers):
+        return min(numbers)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
