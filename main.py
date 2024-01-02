186f861b645fdde720a9f9abe6027065019e01bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:48:15 +0000

    our study relation quickly fairly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
