79b300d776cfd50d65706e38f43a47e8ddd5411a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:14:39 +0000

    rescue politics themselves choice stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
