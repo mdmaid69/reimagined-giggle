97fa7dd840b3aa3d81451cd3c6664dfe56563814
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:52 +0000

    call middle who draw except

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
