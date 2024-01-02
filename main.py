5bf770ae11b404a6a9d650b61d6cf4adfe205d67
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:54:52 +0000

    wear when old or no

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 sentence = "Hello, world!"
 print("Unique words:", len(set(sentence.split())))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
