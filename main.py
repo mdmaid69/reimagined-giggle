8e0057bcde3f1ab27e7bb8bc5775c52759be91d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:18:30 +0000

    term equal relationship smooth one

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def convert_to_binary(n):
+        return bin(n)
