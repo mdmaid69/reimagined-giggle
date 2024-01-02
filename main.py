5e0ee2874edb7333a03038b7f57876d831e472a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:19 +0000

    left usual set turnout available

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_as_list(array):
+        return list(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
