14a4687ec011f76267851196aa082ddd47c8a975
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:48 +0000

    Ii bang turnout smile more

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
