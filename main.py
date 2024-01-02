3cc66f21e119922927975e2252ec4449fde55800
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:51 +0000

    poor delight wish holdon ask

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
