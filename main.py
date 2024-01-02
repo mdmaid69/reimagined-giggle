e1b6537a26df544db0794ee8d694768d6184fe23
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:42:26 +0000

    thank forget support set 179bite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+for i in range(10): print(i)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
