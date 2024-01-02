a5852942a06aa6b2c6609c34cbed23a838f98b42
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:01 +0000

    explain goaway pause delight since

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_acceleration(speed, time):
+        return speed / time
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
