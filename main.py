619e2211de73f2dedb8d50f0966da14f72ac1eb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:31:16 +0000

    high block free yes yours

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
