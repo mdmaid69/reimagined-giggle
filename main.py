7d1b32a785793979dcbfdd4f590c2dae26d5e793
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:02:16 +0000

    defeat mysterious refer bury row

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
