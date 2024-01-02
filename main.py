a37eb56537e87e9dba5bb21e27e73fa8c8411ab7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:33 +0000

    roll last process only possible

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
