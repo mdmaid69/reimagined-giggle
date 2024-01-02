5bd67770452bdfe274da21abaea81ef1498499b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:20:57 +0000

    hollow all explanation ring subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
