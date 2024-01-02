33f6d625ae5ee2ef2d84c3d57d4cb983e6015f22
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:47 +0000

    birth skirt shoot picture leave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
