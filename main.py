b5b61c45129cb844caae3d95b4d76bbd0f619798
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:29:09 +0000

    previous inside night serious even

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
