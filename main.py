a25f07b62c0a7bccf50864f56af4b06c66349f8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:34:06 +0000

    you beat direct doubt runinto

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
