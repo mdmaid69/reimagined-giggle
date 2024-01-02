9988128ef42429a2c9417647f1182879bbc9320f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:00:21 +0000

    bus much dance even town

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
