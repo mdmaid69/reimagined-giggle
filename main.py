c7d01900722cac3fd43a6dd369e58e67a6b28eb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:59 +0000

    confident boil thin degree knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import array
+def get_array_as_list(array):
+        return list(array)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
