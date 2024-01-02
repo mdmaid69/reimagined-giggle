fda34d0ad716263ead163f97d8a4323a1d8c2fde
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:15:19 +0000

    press immediate mouth rest police

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import random
+print(random.randint(0, 100))
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
