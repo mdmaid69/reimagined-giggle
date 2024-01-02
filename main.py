3f72646640f07927a8ce28cae1c21be925832115
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:55 +0000

    curious scrape mean stop aware

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
