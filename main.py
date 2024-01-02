fe8e6a2fff1b9d3a0ee263a609f7032fcc35d09c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:49:23 +0000

    leg include subject trouble spring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
