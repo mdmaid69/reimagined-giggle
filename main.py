41dae7cdce1a01d2beb304f934512b943ba4c851
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:09 +0000

    star slightly comfort deliver yours

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print(math.pi)
