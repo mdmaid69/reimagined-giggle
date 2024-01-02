dd3b18f965f377f8bd20ce04ecd74b72ce0d61b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:32:49 +0000

    Ee suggest congratulations date myself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_power(work, time):
-        return work / time
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
