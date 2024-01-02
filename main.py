9cd24b275b5072903da722f8c079d0d4ac8deddf
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:27 +0000

    goon bus improve present notice

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
