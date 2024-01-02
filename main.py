bf35fa70180332615c4d8086f885b5545e24250e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:44 +0000

    maybe first hello thought plan

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
