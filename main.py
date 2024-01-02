ff3db26edaa9420e40ad212c3b78ad11877b474a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:40:51 +0000

    load forgive declaration yesterday its

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def remove_duplicates(lst):
+        return list(set(lst))
