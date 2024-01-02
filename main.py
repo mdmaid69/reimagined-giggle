4761a927ba41b29a48ae35e1483dc17c1bb86a20
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:47:42 +0000

    gap pump round wave thought

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
