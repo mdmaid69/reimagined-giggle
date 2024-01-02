1bfa7b801968e4faa06cb0848388c90e09714c45
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:28 +0000

    flow left region month mix

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
