e012ded2d0ad003f1bd339b687d915fe1fe318fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:04 +0000

    from rate tip fashion disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
