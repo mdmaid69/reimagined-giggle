f4ace7439060a8159c32aa9dda75a4847c524c0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:50:09 +0000

    wrong shop backwards board advice

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_array_as_str(array):
-        return str(array)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  def convert_to_octal(n):
+        return oct(n)
