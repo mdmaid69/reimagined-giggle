1eb521154dfc3efd7a702b7c52931019930c31fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:52:59 +0000

    lost anywayanyhow discover exploration prevent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
