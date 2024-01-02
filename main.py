4605b51afbbc25f987772240229b6e58a4e4b8df
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:23:03 +0000

    healthy father pretend sharp perhaps

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
