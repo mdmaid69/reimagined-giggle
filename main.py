6e7766770cac13233b8dc4a512580b1a9b701d2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:36:11 +0000

    mad best kind rest spread

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_hex(n):
-        return hex(n)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import math
+print(math.pi)
