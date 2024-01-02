ae5f5108d088b8b1cf4f64f56a2adf34ea5cb629
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:49:05 +0000

    connect hurry defeat release listenfor

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
