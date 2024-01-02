330a2b3d99871ca061dd02073e6a219b2f3932c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:09:17 +0000

    blame light comeover closed recognize

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_as_str(array):
+        return str(array)
