186f9dee134883fafdce86f7142ebf268cf091e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:11:16 +0000

    Dd height block characteristic future

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_user_dict():
+        return collections.UserDict()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
