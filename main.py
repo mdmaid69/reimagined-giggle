23f0f206ae28e5380e35c4648244c2f484a5b778
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:06:15 +0000

    pass workon concentration minority shade

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
