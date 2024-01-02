b9e86b9405a2f35dd6453198c92f96f0ff6fa627
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:49:25 +0000

    exist forever demonstrate previous interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
