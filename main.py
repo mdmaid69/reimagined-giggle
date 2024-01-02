0fb59e9c926771a958ee76fb751473428ad36cee
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:28 +0000

    transparent callout importance bread gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
