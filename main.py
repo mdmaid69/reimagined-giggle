d5c3266c648c3440d42c7fbadacbfbf5e2ee7ec2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:21 +0000

    physical level embarrassing without favourite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
