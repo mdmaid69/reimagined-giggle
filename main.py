fafd358305aabdfcfb8f2d2f48464187dec84f47
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:18:07 +0000

    survive once never I punish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def greet(name):
+        print(f"Hello, {name}!")
