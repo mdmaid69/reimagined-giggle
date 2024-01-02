6368297906a9858c059edf35ed41f15e68885f43
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:00 +0000

    foot night contents change meet

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
