5827be43d46ef6a469bba63a266a9fbb55bf5e51
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:28 +0000

    official office height payment break

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
