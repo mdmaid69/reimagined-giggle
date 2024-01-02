15f586378326f83c090438d670b0a50c7f248411
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:57:44 +0000

    matter with emphasis quick interesting

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-print(sum(range(10)))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
