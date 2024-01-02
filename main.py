18345de6b20980e777164a2466ffa81a70fd675a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:38 +0000

    club letter holdout past prize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
