647c3a8643a1a4fca61df9a91e07648596a7bb26
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:16:33 +0000

    fetch film obvious sale report

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
