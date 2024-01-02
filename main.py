5d75e615f2cc9ee8e3c8480c5794a4d80df2a547
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:13 +0000

    consider plate port spend bite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
