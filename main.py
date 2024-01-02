a600f0daffa2424bfb9ca17128fa37a188938934
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:05:11 +0000

    thick if reply foot journey

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+def square_number(x):
+        return x**2
