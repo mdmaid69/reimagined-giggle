2df107d6268860ae582ba596320f9f75f6786fb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:43:53 +0000

    talkinto count want plenty sight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
