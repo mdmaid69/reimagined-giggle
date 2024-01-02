86c2b2d558361ba44ea9f8ac1d794749720f3445
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:36:39 +0000

    shut than clean hang damage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import collections
+def create_stack():
+        return collections.deque()
