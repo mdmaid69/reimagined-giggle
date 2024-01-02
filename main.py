154b57a2fd175153857a7f9a8f0a23f4d567eca6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:54:44 +0000

    false surprise taste shut boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
