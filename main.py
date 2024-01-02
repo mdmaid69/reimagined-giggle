cec148bedf772e932c1b33ac2beb8bf3f68a46d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:52 +0000

    raw exist laugh needle space

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def convert_to_binary(n):
+        return bin(n)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
