c3039cd46a935f5fd2422d45843981d21836c685
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:39:11 +0000

    turnup but tool ordinary welcome

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
