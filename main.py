1936ac4d358998e86d6fcae51986d7ee272a009c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:50:48 +0000

    relationship lower shame pick majority

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
