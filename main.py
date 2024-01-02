e7e843cc95273160ec134930b543b1d90e96f2fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:35:05 +0000

    getout fasten dare rare stand

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
