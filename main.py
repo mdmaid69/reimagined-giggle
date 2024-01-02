be9cd19ac1d665ff28e963250010cdf451e7e914
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:17 +0000

    carry broken turnout deaf movement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
