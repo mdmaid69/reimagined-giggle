89bce47e341de3a9fdd4625d0020aa1bd6a67135
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:13 +0000

    characteristic pupil instead spray visit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
