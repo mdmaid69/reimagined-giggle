32a89ffbf49b529d3dc7421ef77ced55414ac6ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:35 +0000

    impression fast smile fit back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def clear_array(array):
+        array *= 0
