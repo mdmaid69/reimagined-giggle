1ef480486279f70c295130764ca5b381b50eb472
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:38:38 +0000

    generally might fast anywhere medium

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def convert_to_hex(n):
-        return hex(n)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def multiply_numbers(x, y):
+        return x * y
