175920a0c4291a160a6ad27bcd948db9d00f8920
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:05:35 +0000

    in describe importance attempt today

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
