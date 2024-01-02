1b17e84671bd8096d9b6b63b92e6b7dcd8bd31b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:36:24 +0000

    be central point good change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  def convert_to_binary(n):
-        return bin(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
