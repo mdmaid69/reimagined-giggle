a6c81dd7831b36f700a35a7fd54a20e1a6af81a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:17 +0000

    perhaps roof forever correct music

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_list(array):
-        return list(array)
 n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+from collections import Counter
+print(Counter("hello world"))
