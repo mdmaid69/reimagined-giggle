d620608b081dfd21509bcfb74040572609677528
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:06 +0000

    deaf teach breakinto ring spill

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
