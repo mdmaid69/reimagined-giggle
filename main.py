b36987d7a2d2880be1ee8f5831493a59d13076d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:11:41 +0000

    loss class pause strike blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        return sys.version
