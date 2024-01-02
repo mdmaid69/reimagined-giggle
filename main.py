1bf2332d19724ab4ba65e014c2fedbd78b438cb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:55 +0000

    rain enthusiastic pride present work

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
