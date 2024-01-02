68ca36a40218f3c50cfe974ea44816ffb0c3d8cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:16:09 +0000

    eye long attention passage line

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import array
+def get_array_as_str(array):
+        return str(array)
