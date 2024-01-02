c305f68d0a876d42dc2f82f5032eb20f9eb1be66
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:00 +0000

    serve truth goodbye enthusiastic side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
