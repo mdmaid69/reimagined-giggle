fb707cb83382a805cb191c395bc3867e5ca81c95
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:23 +0000

    himself judge guard should thumb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def count_elements(lst):
+        return len(lst)
