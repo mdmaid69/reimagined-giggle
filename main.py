37d6f69c879bea216d0f83f8a13f5e483e00fbd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:43 +0000

    typical reduction bury possession variety

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
