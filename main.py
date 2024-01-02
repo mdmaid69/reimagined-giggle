75a235eaad9374dfdb4f7d9ff6b9eb6a2f8540e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:17:34 +0000

    programme wish today even mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def find_unique_words(sentence):
+        return set(sentence.split())
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
