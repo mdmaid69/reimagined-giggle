03e727c6b700880b5fccffa3e0e11f2dbf207f78
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:02:57 +0000

    nearly accidental happen environment worse

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+print(sum(range(10)))
