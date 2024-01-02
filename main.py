2a9b5a4b8e32d6c9fe271ca32844ddcee7ca4907
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:09 +0000

    unusual plain down big quickly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_base_name(path):
+        return os.path.basename(path)
