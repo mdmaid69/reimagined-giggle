f6f7b851031c182c345e5c2e93afe162a121a52a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:52 +0000

    suggest solve invitation turnoff famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
