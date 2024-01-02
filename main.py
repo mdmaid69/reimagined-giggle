9c7cab06c179bc07aa8a1d3ed6ca760a7fb1a168
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:27:56 +0000

    fill catch improve train such

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
