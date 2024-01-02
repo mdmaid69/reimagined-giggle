4d091d44e80e26cdd469d738ca7c1f380795c9cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:25 +0000

    feel opinion waste central punish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
