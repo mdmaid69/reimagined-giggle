7158d627c8230ac67d21fee101ca78864314f719
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:10:43 +0000

    standard always like answer worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def calculate_area_circle(r):
+        return 3.14 * r**2
