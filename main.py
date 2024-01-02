cd119cba9dd1fe0f915f3718d3352943b7acda7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:05 +0000

    thank unlikely meet reject once

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+print("Hello, world!")
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
