e97e011e84b90931ab2a3df4e074af9c1d283f7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:33 +0000

    turnover that concern bad here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
