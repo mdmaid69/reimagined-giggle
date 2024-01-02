770bc861d9929e983cf881de11c9dcff2e4f265c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:26:36 +0000

    when geton state trick convenient

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
