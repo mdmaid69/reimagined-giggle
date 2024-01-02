e7b724a6ba8db29affa168a5796a4acc605db4c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:22:07 +0000

    wet keep holdout attach case

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
