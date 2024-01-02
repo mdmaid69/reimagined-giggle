8b0d448061f08d79ca2beae1762cb08b7d860a18
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:30:44 +0000

    awake strange little between aim

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
