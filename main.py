98f17af605e97dfd37ed70f102bc44214e55a8cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:15 +0000

    formal commit send argument certain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
