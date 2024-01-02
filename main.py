30ae968192eddc774ffc8f0afcf17b5642a515bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:52:26 +0000

    through Cc boat tonight in

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
