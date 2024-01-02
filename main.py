c7c93e2e639c623fb888184efef28dd4b41b9cc7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:18:28 +0000

    matter declare fortunate offer fat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def multiply_numbers(x, y):
+        return x * y
