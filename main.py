be008209791532079afc523a7584dc4a87ee79b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:20:55 +0000

    would 571enough its consult disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def add_numbers(x, y):
+        return x + y
