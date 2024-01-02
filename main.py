d5c7cc46e527b36260dc62b7e301796cdab1df4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:39:40 +0000

    contact scale hide still week

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+def sort_list(lst):
+        return sorted(lst)
