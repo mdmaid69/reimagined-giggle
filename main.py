391b559a676a1b50f361b19526a17f723e9acaa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:19:31 +0000

    comeover achievement serve suckme honest

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
