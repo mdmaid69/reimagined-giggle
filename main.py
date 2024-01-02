2b5fa0cbd29768d77b4053aaaab7d8cc9183b9e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:38:28 +0000

    principle hungry home pressure water

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def cube_number(x):
-        return x**3
-def reverse_list(lst):
-        return lst[::-1]
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import array
+def get_array_item(array, i):
+        return array[i]
