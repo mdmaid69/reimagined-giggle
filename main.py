caa406b2a9eb3cd2cc4d751fc4792ddb1612e1d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:05:12 +0000

    nose once shout prize visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
