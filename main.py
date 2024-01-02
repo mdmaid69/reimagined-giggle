9f805b974b30266411de00aecd02f1818593ff0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:56:14 +0000

    say press rarely eat report

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_typecode(array):
+        return array.typecode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
