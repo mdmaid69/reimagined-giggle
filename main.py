7b2fcab09846b93cce780eccee64fb4842c0a437
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:28 +0000

    guilty delight threaten tie tidy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
