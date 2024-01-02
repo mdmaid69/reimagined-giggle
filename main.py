97ce1e4bb10194765b5c5f4b89c25c4be4428c9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:12:29 +0000

    smart direction whichever copy when

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
