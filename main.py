88fce573a760d6f77b657b8de2c0c39600858664
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:52:53 +0000

    anywayanyhow total nothing nonsense goodbye

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
