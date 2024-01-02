a9b35096f5ab7560134c7ae575d81122aca48d8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:21:34 +0000

    reaction fasten breakin partner relationship

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
