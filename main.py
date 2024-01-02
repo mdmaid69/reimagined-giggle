d3f456b16d477030d53729534f5e5890aea14ea0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:32:09 +0000

    contrast impressed business violent thinkthrough

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_index(array, item):
+        return array.index(item)
