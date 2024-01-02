b371302e91d0b15ce0b20c660601ca26730903b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:55:01 +0000

    hit peaceful bury Jj improve

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(a, b):
-        return a + b
-def calculate_volume(length, width, height):
-        return length * width * height
+import os
+print(os.getcwd())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
