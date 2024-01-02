8578948a7bc77b4739eb551802f09167bfc007ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:09 +0000

    succeed activity capital talkinto touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_set(array):
+        return set(array)
