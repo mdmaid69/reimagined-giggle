9380ae51c5436cad6816223c13dfd7e52221c3cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:09 +0000

    temper little politics contain skirt

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
