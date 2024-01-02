0c0627bc8d51912e10078cca3da7938db152d1c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:05 +0000

    most mind comment safe swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
