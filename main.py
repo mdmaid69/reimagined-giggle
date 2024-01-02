7bc547cd5eb9926843086c5b9c30d5c8e190bc4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:16:21 +0000

    human himself warm set gentle

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
