7267d3653a662e0011e3aec8c63495ff58810521
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:07:06 +0000

    pay high difficult offer wipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
