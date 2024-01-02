fc885282829b8ee899f1d7664bb52b18c44cb52a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:58 +0000

    explanation behave yours blue flat

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
