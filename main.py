da7b9ddc712a6ac505224052c19c8c9830c2f49f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:36:56 +0000

    rank would news goback(to) you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
