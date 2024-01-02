8350e14fbb088204f8969396fdf5ffbadf99eb58
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:54:34 +0000

    mood rough deliberate forward single

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+print(sys.version)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
