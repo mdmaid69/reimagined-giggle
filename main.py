ee5150f7602a1ce4376fa29789d8914678bdb3c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:48:40 +0000

    interesting squeeze land married complaint

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
