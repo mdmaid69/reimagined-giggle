74ad6c7160675d98245f2b28b06bae21a07296ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:41 +0000

    undo defend unless opinion quiet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
   import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def calculate_variance(arr):
+        return np.var(arr)
+import array
+def clear_array(array):
+        array *= 0
