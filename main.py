4f4b38906e95c8b02a7f558ae33414b593c34f77
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:53 +0000

    keep properly heart fear share

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
