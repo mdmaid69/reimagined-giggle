69a477ef69740f7edad9f6d9fbddb79759064544
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:40 +0000

    approve preparation seem your doubt

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import sys
+print(sys.version)
