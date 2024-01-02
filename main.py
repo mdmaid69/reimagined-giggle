415dddd3a4f7af192c290ccf4b6b48d0f08d1463
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:47 +0000

    tight taste suggestion deliberately makeinto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import sys
+def print_python_version():
+        print(sys.version)
