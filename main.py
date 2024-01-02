bd58ab68cc8f167faea8b30f64796de1a6613fb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:57:14 +0000

    fault begin gentle send sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def delete_file(file_name):
+        os.remove(file_name)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
