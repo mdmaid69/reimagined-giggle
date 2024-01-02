5dce51d2f4c03c2c86005fe5d84d9316ab4b9703
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:27:29 +0000

    call reduction turnoff start fair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
