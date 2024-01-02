7807744f2a9ab514140eb916ed8d27c94eb404bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:32:24 +0000

    never this later achieve destroy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
