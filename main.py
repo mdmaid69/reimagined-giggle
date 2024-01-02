df137b84be11b1a86a26ef0dea9591f8de838d64
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:56:54 +0000

    sharp determined emotion actual belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
