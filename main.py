d6e2b19afe21252aebfacc1d2c05aa255fce38e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:50 +0000

    expect holiday somehow activity tape

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
