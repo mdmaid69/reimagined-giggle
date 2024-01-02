fa6abb9814ef88754e196d3e94d78f7f1b6fbbcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:39 +0000

    variety blue connect suck Tt

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
