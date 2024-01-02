16b4e86d1f7212ad3ade04ee69159fa97061378b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:40:43 +0000

    comeoff polite importance repair accidental

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
