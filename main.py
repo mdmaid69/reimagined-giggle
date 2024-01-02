dab50a7df10c34672b74a91b26346030bd61e098
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:02:41 +0000

    turnback beyond chat quality prefer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
