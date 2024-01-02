01a6058ae3dc3b88656bdc69c1483132da701c9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:38:36 +0000

    thing pride by tone sign

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
