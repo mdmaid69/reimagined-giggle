1285fee3dbff8cc9ef839af5019a9537dea8f27a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:25:08 +0000

    take storm pleased stretch 569enjoy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
