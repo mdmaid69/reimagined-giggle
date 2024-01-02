f34db285878f67ce09c93bf6952657189e6b6e77
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:21:27 +0000

    violence price degree advice unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_density(mass, volume):
+        return mass / volume
