a843013c3dcbf5559f46479d3d508e2418f34913
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:02:32 +0000

    thinkover fear prepare underneath past

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
