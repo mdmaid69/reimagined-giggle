88de7753a503caf92c2bd875be3a1a7e9a6b701b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:32 +0000

    tax promise height frame cheat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
