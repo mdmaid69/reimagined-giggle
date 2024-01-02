3ec0b9c90b11336d3d3e55f8167087a0b2f8afab
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:53:01 +0000

    mention while friend bar some

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print([x**2 for x in range(10)])
