67936d0bbbc720b8f0f6d0d5846cc07653b16fed
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:19 +0000

    branch jump dark extremely sing

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
