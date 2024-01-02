bac3035f0e99ea16f9c7e43a746fb64df6ce553a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:17 +0000

    clearly sport interrupt round before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def add_numbers(x, y):
+        return x + y
