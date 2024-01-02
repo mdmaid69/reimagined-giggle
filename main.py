046cd7bb2fe58bee1f32e4ea437206080b61d3b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:40:57 +0000

    peace band seriously turn after

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import datetime
+print(datetime.datetime.now())
