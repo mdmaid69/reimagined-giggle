ea699f4968a0ffe8d2e084a173e4131371e81d7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:21 +0000

    equal work wrong exciting drop

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import os
-print(os.getcwd())
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
