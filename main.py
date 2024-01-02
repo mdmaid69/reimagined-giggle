2a36e8a2c967edd2c46852c42fcaaacf05247d7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:32 +0000

    bar sharp for test fence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
