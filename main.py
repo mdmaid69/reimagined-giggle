d8bb7c05677db31718ce1495996a54e17e5139fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:11 +0000

    grade class total back unkind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
