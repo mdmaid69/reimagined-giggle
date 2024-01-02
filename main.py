ece4c82e965394d5742b2b5424581854f44d2009
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:21:36 +0000

    praise sport compare though down

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def multiply_numbers(x, y):
+        return x * y
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
