cf7de59ad3e56b938afd8e6f21d83f15c5e4f96a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:52 +0000

    this wish compete comeup teach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
