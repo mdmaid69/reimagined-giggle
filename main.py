37f4ce697380cd5ffb6b9f94730fb450580c6563
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:37 +0000

    lie giveaway divide Vv you

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
