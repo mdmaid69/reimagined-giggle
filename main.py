94026c0c99c87e29fa320513b3167f7ab7056bef
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:27:19 +0000

    basically rid home provide might

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
