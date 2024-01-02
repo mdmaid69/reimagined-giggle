885373070d28bccae21bd1eb2b3e3afde30bdb0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:23:04 +0000

    raise letter drink store storm

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def multiply_numbers(x, y):
-        return x * y
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
