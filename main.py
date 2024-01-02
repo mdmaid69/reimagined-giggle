73452b884738346efc7254ec89fa3cb4be988f1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:15 +0000

    trap duty grateful content refer

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  def remove_duplicates(lst):
+        return list(set(lst))
+def calculate_perpetuity(payment, rate):
+        return payment / rate
