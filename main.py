077213431269e2369f4241b938514e9b0368f691
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:29 +0000

    possibly hungry command at accept

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
