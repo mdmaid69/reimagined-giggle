79801656fbc10cc073521fbdaa99bc302103819a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:17 +0000

    alone convenient behind opposite serve

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
