14e571591beaab0a31eae70f2f1f85fd3744feee
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:42 +0000

    development workon touch worth accidentally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+def find_min(numbers):
+        return min(numbers)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
