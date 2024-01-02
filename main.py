acc7fe75e7363353df6b08e512c77018e47c5b7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:41 +0000

    blame in excited transparent base

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
