53165bff93efa9b4253576001e43db74362bccb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:03:42 +0000

    healthy offence may concentrate crash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+def calculate_density(mass, volume):
+        return mass / volume
