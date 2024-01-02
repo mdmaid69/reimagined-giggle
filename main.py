38b946a43783797ed6e46c15cff73d93c1eb8340
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:41:47 +0000

    impressed thanks makeinto goup scale

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
