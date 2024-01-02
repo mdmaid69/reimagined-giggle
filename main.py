33d79f92c329de72408adaae7a7ace4759ec23a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:09:21 +0000

    obvious shoulder here necessary future

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
