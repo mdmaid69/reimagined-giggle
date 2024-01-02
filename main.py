366dba41ec5cbde69438ce2847caf3e27a35daef
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:12 +0000

    land centre result cum mouth

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def count_words(sentence):
-        return len(sentence.split())
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
