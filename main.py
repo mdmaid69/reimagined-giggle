ce4b4165e39194b2b77c1c84020d56e12a9d1ecc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:26 +0000

    no quality wish dead glad

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
