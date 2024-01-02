1507dc7935f669c8742154b624ec7979fe005c3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:28 +0000

    even whistle deliberate justice room

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
