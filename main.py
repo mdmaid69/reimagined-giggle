75c3492c146d4e57e4b782b7c1aa15c9416b38eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:46:56 +0000

    swallow runout success flood justice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
