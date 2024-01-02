944201f62a3eb39f16d5cffdb8dd882bdf4f6709
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:10 +0000

    modern consist practise cry lucky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
