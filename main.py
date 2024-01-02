767cac77282509265e6ce72f3d6992aceee7a088
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:44 +0000

    improvement sort classroom under nice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
