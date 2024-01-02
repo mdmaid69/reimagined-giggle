9c09c9ae5ad516045acf2a538f6e8d41b5df87d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:43 +0000

    beneath neck Cc difficulty score

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
