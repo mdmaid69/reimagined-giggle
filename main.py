70d5a9b2f108cbe985867d2aeb3243b51671302e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:49 +0000

    trap chief Ii stamp copy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def count_elements(lst):
+        return len(lst)
+import sys
+print(sys.version)
