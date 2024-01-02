300246ab30723b62886d244d347198dbdb654d68
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:25 +0000

    wind considering talk talkback sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
