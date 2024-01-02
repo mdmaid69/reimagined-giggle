e64dddf0d34589fecdf733498cb7195762596d60
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:38 +0000

    shop every 573entertainment follow meaning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
