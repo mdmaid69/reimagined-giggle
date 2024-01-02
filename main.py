54339cc5e86acd787c495bf51a079ced59e68334
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:37:26 +0000

    insist tread storm fine performance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
