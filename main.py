099ff5c6b5a00d88156dfe9a96872f194fd93fde
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:58 +0000

    decide acceptable judge major this

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
