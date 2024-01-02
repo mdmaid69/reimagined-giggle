a088ede635856803aa391ce14087c4f25e4eb12b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:29:40 +0000

    quality comedown business beside female

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
 import math
-def calculate_exponential(x):
-        return math.exp(x)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
