92fa9f4876d1366af15b890b3092b9dd6c6ad0cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:26:40 +0000

    game discussion more that real

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-print(sum(range(10)))
+  def square_number(x):
+        return x**2
+def calculate_acceleration(speed, time):
+        return speed / time
