87289f940615ed701348221955fb9ec9304f70fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:26 +0000

    later style bus breakfast doubt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def subtract_numbers(x, y):
+        return x - y
