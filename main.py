5d47379c53de514e9557723af9905ccf078d380c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:23 +0000

    doubt throughout slowly similar keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
