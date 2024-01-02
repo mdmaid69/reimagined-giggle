493d3076c0b492ba2f77f74395bfa9351d63d294
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:26 +0000

    risk sort truth stop pass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
