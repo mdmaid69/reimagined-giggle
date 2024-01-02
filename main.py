dc2f375c618dc602198e159aadeedb6a487e7324
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:30:09 +0000

    Pp search nature machine paper

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
