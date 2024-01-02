3108f53cdfa28deb6e317851e11b0be880404a27
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:52 +0000

    match copy stage tight beginning

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def cube_number(x):
-        return x**3
-import os
-def get_current_working_directory():
-        return os.getcwd()
+def divide_numbers(x, y):
+        return x / y
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
