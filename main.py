7d6f65108dcbf59516e9b6342b4562713e96e079
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:09 +0000

    open classroom story keepaway telephone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
