01cc9e070a959cb6670629dffbf4da8497318eff
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:50 +0000

    awake lookback(on) treat believe copy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_base_name(path):
+        return os.path.basename(path)
