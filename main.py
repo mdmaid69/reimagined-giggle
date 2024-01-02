da34941b09b0e19d25823bf149eabdd9c28a4c76
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:04:21 +0000

    lend reason somewhere shine probably

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
