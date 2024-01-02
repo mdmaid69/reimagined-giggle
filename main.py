998609260b87700349701116aebe0bdeefd5695b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:20:07 +0000

    lookafter turnback quality because factory

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+x = 10
+y = 20
+print("Sum:", x + y)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
