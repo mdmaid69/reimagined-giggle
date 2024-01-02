bd92cb055ffcececb254cc3bd25e168dccf47743
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:24:00 +0000

    hurry information contrast strike cry

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_work(force, distance):
+        return force * distance
