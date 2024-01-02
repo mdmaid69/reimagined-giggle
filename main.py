f240cf1d63d7b11d3fb5db64219b7e258643c505
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:53:19 +0000

    winter spite right today everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
