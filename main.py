4737c4c0f8d37115837e919cd231715d904a2163
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:58:30 +0000

    left calloff before happy scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
