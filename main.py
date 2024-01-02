e5683119d3aacfb6a5b4331a4c990549e0bc1d9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:04:27 +0000

    getoff leave throw worst hide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
