04e92bf55bc1a621cb86077d7c2f62951d0c2a41
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:22:23 +0000

    anyone improvement silent waste wet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
