148b48eac54ce09b7d50233641a2c3eb3c345c2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:10 +0000

    second tool there share Nn

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def add_numbers(x, y):
+        return x + y
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
