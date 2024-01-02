5356aee1aaa2a5b9b02004e239ff39fefb34931d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:56 +0000

    none ourselves exercise slightly seat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
