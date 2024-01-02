76a5e61aa51fd1d9b4ad87fcb9c40230f0c02232
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:20 +0000

    way partner more look tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
