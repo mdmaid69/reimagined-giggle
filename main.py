764ac0c1a9961258b0f8d2c7ca1127e692731e5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:17 +0000

    tomorrow battery ahead support flavour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  def sort_list(lst):
-        return sorted(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_username():
+        return getpass.getuser()
