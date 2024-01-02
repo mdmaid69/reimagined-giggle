c97609a23303a522e92c7192ca153a78e5f1ad57
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:49 +0000

    money there blame boring lookup

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def convert_to_octal(n):
-        return oct(n)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
