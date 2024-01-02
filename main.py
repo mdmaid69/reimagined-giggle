fde081fcbc6d70ee71437f9bd948acd683f4c8df
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:26:35 +0000

    shake comeout means outside disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
