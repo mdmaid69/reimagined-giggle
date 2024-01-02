dce489b28ea406e3ebf6d061889f0c3710f1645b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:46:20 +0000

    why spring shopping pollution night

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
