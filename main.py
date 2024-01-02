1bd6faa91388612233ad2c7956263a0342aacf19
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:33:07 +0000

    rise talk cry add late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  def reverse_list(lst):
-        return lst[::-1]
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import sys
+  def get_python_version():
+        return sys.version
