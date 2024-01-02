0a6e2f0e19291f54791449d99cdf57455df21fa9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:10:15 +0000

    Nn thinkthrough next downstairs ever

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-def cube_number(x):
-        return x**3
+import time
+print(time.time())
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
