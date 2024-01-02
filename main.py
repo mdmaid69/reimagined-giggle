5d85472c5ac14cbcc6861afef3944011240542d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:34 +0000

    interview broad use ago order

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def count_words(sentence):
+        return len(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
