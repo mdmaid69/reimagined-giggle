3cb00de8fba868f17afdbace943a1c74db9738b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:47 +0000

    suit force stare close pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
