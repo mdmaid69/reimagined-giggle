0a42f90736c138c108001c7a1b62b2616c71f717
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:46:09 +0000

    truth violence explanation justice Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import tensorflow as tf
+print(tf.__version__)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
