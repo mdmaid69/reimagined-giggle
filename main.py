c3ceb683def8bd1b75a46e89c2fde15ac441bbd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:00:54 +0000

    apparently far environment themselves usual

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def split_path(path):
+        return os.path.split(path)
