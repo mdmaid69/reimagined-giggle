ee2351756e0d037c9b8a64d6bd9b896219acc27d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:43 +0000

    entry share us cook majority

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
