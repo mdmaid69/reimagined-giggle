36cc2794eed1f5e9c1f0ad1ee49a0e4e4f3f8e7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:06 +0000

    store step cross cover winter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import time
-print(time.time())
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
