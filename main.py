11d7473c0af35df6914eb45207cae3834c6af4e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:22 +0000

    may rid change able proof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
