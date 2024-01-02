d667126fcd5b9c6c892dde8737dff96ba46ad819
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:27 +0000

    health settle change description process

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def convert_to_binary(n):
-        return bin(n)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  def convert_to_octal(n):
+        return oct(n)
