3f7b6efa9a2e1f0382705a97bbc66c13a6970cf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:50:01 +0000

    warn quantity confuse damage line

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import time
+def get_time_since_epoch():
+        return time.time()
