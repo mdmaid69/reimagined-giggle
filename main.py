482d34e6650d9131127568c5e257ae013dcb8cac
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:34:02 +0000

    remove doctor news excellent risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
