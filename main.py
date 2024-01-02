c841d5deeafb81efa1004fdaeadadd1d16461b07
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:22 +0000

    clearly furniture thinkover smile agreement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
