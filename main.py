8b7afd5ed9ab63611c05e1b74dd35fb3a41fd928
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:45 +0000

    possibility fault often interfere front

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
