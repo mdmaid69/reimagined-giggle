77b5081aada0612d7bef9e30afb6a423f074cc14
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:03:33 +0000

    unpleasant tragedy no birthday round

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
