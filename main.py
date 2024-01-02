8e4387328b2899f293588fa1afe6046811e94634
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:32 +0000

    experiment note method sudden permission

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def reverse_list(lst):
-        return lst[::-1]
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
