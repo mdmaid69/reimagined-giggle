bf1f5923085231bb107bc358c841176caadabcd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:47 +0000

    afford admit estimate feeling light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
