0f8e27aa591e5b732510a5859753e80d88c00c78
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:18 +0000

    future except back holdout straight

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+import array
+def convert_array_to_list(array):
+        return array.tolist()
