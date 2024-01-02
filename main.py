1f2220a8be51fa460b60cb0cc18bc1120ae62b66
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:47 +0000

    health suckme love discover compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
