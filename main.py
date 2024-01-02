dc4c61b7fa5dda9eff54f33fe726c30d55ca59a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:30:36 +0000

    share emergency minute deal bridge

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-for i in range(10): print(i)
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
