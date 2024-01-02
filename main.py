3e982d4ad1494f84909dcd6ff2779a2cb46c2036
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:29:28 +0000

    adjective challenge myself laugh properly

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
