8c09347be407af646f89b17b6b7408005549e71d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:06:41 +0000

    definite help church Hh line

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
 import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def get_array_as_tuple(array):
+        return tuple(array)
