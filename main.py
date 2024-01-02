32730c5d50c3a6c2f38c3fa1800ab10ce56aa13f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:14:14 +0000

    repair clue sensitive contact connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
