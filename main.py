241dfcc21fdf885a0582dbc8449ef27187e0a36d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:09 +0000

    jump perhaps suddenly formal clothes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
