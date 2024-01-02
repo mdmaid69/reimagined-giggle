fd4983bd20227f3ee1d2006139d24e76eb0fc99e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:36:40 +0000

    nigga embarrassing ilovedicks machine system

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
