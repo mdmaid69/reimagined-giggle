fffca262596fb82cc71708274bcff21601f6872e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:55:05 +0000

    sign branch come quiet suggestion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
