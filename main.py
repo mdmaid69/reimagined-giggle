4cf0295b0cf9ea19383a8c22098dd5eb421501b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:53:03 +0000

    fast only takeback present later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
