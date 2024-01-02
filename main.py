e987086443ab5ea8fb6ecf447d6884d377b4cb64
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:09:43 +0000

    subject short danger marry ill

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def sort_list(lst):
+        return sorted(lst)
