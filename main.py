5a2e1bf531e32cc28183f117d1896c9adf7689f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:09:50 +0000

    571enough noisy negative back succeed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
