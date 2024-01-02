cf88e59def747ff88656019826d9cfe091f9507b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:25:56 +0000

    shop from Itself control line

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def pop_from_array(array, i=-1):
+        return array.pop(i)
