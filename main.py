497828f1d620aa63c26ef39478a204686fc5d82c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:42:43 +0000

    lose opposite classroom in drive

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
