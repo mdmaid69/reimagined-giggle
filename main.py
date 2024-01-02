b7b478f15cb0479d32c16f36f788f8b2344f694a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:00 +0000

    cry recently between right just

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  def multiply_numbers(x, y):
+        return x * y
