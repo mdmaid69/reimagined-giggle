8376826a16eed0b124e20d89b7c83e465c620a09
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:09:00 +0000

    bush rough jump use charge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import re
+print(re.match("h.*o", "hello world"))
