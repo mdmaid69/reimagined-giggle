78e558dcc8fd297538d8e7ca73a086030ac8009a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:51 +0000

    gooff bread bin hand burn

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
