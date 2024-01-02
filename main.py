3e41c7021b77bddb26795e8232fa57ba282e3deb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:38 +0000

    question they against as mainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
