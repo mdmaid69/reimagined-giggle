7bf6dc9418e086e7b84ad10576e6891595a7438c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:53:38 +0000

    spray flow another total attractive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
