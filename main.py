ee4ee649d4d81fbcc5e3d5c0e312babe5b539a64
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:04:37 +0000

    hers what concentration stay old

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
