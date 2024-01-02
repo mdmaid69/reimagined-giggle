f19ef4389733b90de6b84f00e143bcf82c2356a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:01:39 +0000

    statement regularly become everything personal

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-print([x**2 for x in range(10)])
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
