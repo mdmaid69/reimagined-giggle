40600b98605ad7b9ab893e0a4b4bd3c305367ab0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:06 +0000

    public pity his kind estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import time
-def get_current_time():
-        return time.ctime()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
