b670098c185bcae29f13f853c93e3421413c86f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:55 +0000

    doubt embarrassed perform collection container

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
