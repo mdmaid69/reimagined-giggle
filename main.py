20522ff4c1f4c29071538cbf9e1adf2c532f15d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:09:48 +0000

    Itself score exploration blame trouble

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+print("Hello, world!")
+def calculate_density(mass, volume):
+        return mass / volume
