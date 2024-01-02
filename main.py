5828e92e5223ab44af130c62dd094f1125fd9eb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:33:10 +0000

    bar check press bound just

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
