8481dc47eeddc506fe5a6672179ebd458f9d7281
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:28 +0000

    away sky realize unhappy callout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
