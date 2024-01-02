f2b627881fb2accf564644ece5f2eae7154455bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:36:16 +0000

    not might choice able schedule

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
