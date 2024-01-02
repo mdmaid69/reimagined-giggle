b637958e74c98d62ddbb3900ffc67c4d12fa35e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:11 +0000

    think come borrow wind base

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-text = "Hello, world!"
-print("Characters:", len(text))
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
