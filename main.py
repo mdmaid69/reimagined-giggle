3eb4167180609826c08a644c9ee7013f0036c4ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:38:54 +0000

    shop improvement then ashamed bath

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-print(random.randint(0, 100))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+def sort_numbers(numbers):
+        return sorted(numbers)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
