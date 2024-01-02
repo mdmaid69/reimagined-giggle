f949de12588380799fd14d2128cffe126ddace05
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:39:26 +0000

    push none force rescue mix

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-print([x**2 for x in range(10)])
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_density(mass, volume):
+        return mass / volume
