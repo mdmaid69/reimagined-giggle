a8c032926c49d6ffb5e60ef9015199b7a7872baf
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:35 +0000

    rare set busy something contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
