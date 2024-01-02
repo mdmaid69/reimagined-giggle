7c7f3fcff3aa86c056e84df4836a062bfb54dde9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:59 +0000

    proud lookaround translate complicated conduct

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-print([x**2 for x in range(10)])
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
