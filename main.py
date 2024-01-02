b2f6a8a0c8afecd2bf8ce02e7e9c35300543e5aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:01 +0000

    dance crime limit pity speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
