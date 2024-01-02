6d8412c5c673a5b001e85c8d12ad1e266ba7e04d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:09:49 +0000

    bread takeover address profit calloff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def find_min(lst):
+        return min(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
