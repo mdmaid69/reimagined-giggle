1820c9252e14ec6fcd07e4428af668ee76d96ed9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:45:13 +0000

    careful plenty delight smart body

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
