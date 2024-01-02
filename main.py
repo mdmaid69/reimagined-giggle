e85a9873ae486d89e468a7ea0ab1000b627efa78
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:02 +0000

    pocket try introduce test smoke

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
