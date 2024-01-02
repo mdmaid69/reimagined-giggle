2d55d532b6525be4f71c76620bb18d7f5194b125
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:46:22 +0000

    surface owe turnaway refer star

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
