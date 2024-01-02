29fdbabf802a60ca1835b551cd894f19b79c990a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:57 +0000

    service imagine bye rather breakin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
