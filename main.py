457f6b174dcd507015b03c676149bf87d4f0dca6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:25 +0000

    bang twist ours him next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
