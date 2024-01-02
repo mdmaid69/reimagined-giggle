94def535df93723ece5961b05c6b0f846aab4997
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:56:46 +0000

    can shop fear try actually

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
