8ba8a768b9bbe6c3a2c8bdaf1c4ef95049518bb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:31 +0000

    extremely concentration leg least distant

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
