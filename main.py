9a05b646368f5403ce75bd072d51d8a07dfe477c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:43 +0000

    offer further club finally manner

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
