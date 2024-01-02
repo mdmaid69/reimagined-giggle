0e6a1819272d057e89c01d295323cdfd6c9b1674
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:48:47 +0000

    eat serious indeed usually stir

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-import re
-print(re.match("h.*o", "hello world"))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
