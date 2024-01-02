d9b074832abf879590a4ff7d042e32982a1f6de4
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:36 +0000

    main thinkof flow welcome last

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
