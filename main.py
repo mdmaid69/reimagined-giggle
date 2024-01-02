0aeae295f5ba91851a5822283ce7a9a021760b96
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:15 +0000

    vanish already on hit test

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
