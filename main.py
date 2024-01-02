cf89a7af7cc239d67fa421bdffbd4e9243a1d6bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:04 +0000

    cause innocent hour convince travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
