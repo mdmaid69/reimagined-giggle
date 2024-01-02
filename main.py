d807b3817768d43c2b3a8df26ec7dc3a122c0d6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:18 +0000

    ill last Qq judge set

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def calculate_speed(distance, time):
+        return distance / time
