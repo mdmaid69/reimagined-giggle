8ccb2a1c63eae3e60e12cec2eec52cf84942ac05
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:38:30 +0000

    patient today breakup plan care

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
