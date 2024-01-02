32d2814040450eea3d7ab979aa22a817167ee8c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:08 +0000

    permission entrance concentrate pretty runaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
