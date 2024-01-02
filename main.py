bacefab18656fae6a20b30699fcded8998f8620c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:51:43 +0000

    whether thinkthrough worry minimum contribute

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def calculate_power(work, time):
+        return work / time
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
