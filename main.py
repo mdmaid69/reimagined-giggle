b36b23c341306271d779ff71298b999d1b8ea480
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:47:05 +0000

    back Yy wave around freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+import random
+def flip_coin():
+        return "Heads" if random.random() < 0.5 else "Tails"
