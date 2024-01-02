a83930ebd10a7dd0b386adf47a71002c2a5e8607
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:48:52 +0000

    soft effort consist straight capital

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
