8cc55ab1a44c7ff07ed8cc679d03fa563fc5ab8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:26:43 +0000

    first prize whoever pride past

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
