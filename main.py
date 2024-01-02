128e745feb603986e0a2aa70418cda38a301ac67
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:58 +0000

    content bill city scheme repair

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
