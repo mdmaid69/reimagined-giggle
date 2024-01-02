7e66c363e87bb41a2109dd58580d22caa55605be
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:33:48 +0000

    search show house divide offer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
