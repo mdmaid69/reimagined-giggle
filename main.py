cf4917b4f409fc40911f2e924659572f9df5d42d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:59 +0000

    breathe switch high practice offence

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def calculate_pressure(force, area):
+        return force / area
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
