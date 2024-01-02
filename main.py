84de37b258cec54f3162ae7836170e00e3195d7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:14 +0000

    leg share pass spot band

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
 import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
