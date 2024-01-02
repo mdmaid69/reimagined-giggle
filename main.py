e3e940570ca070a5a37f45f0c736dea97ce692b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:54 +0000

    interested artificial side pronounce private

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def find_min(numbers):
-        return min(numbers)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def reverse_list(lst):
+        return lst[::-1]
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
