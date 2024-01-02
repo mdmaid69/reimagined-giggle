5dbe1805a4febce3b7b8ccfb912df91df66587db
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:31 +0000

    far loan blame anxious hide

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
