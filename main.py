6902d8d4ef735c2e1efacf9edadcdfeab428d4e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:54:21 +0000

    plenty pool surprised offend although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_exponential(x):
+        return math.exp(x)
