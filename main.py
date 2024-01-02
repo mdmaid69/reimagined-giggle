b206c14cdefbe1d963d5fdcf3dab739f85a224a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:02:53 +0000

    zero round square home keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
