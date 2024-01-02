fe7ff11d18a4eba9f790311b25e1044941a9ced1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:30 +0000

    steep division capital worst have

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
