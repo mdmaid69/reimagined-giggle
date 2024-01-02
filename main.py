8f358bb7c68383b3692825cf15f97c4b8a3c4ea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:27 +0000

    peaceful march present each heart

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
