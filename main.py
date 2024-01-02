06f2326c273572840d10492273761a07cea2e47f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:57:09 +0000

    regional Ww typical error front

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
