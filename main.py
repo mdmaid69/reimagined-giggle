6d81c4441cf49328a0587fe59d5ee5ea9e98cc94
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:15:33 +0000

    this communication little sister goaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
