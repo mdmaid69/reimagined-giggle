6a62380a84ab2561d1848746314d2dfba61de2a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:13:18 +0000

    dare work left stick approve

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
