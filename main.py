6c08eff73983312ce1eeb16715eade630197203c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:20:42 +0000

    attempt as first spoil save

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
