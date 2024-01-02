f99e6c5cf7a5c360da7c14ec88c212f6a066d102
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:02:37 +0000

    bill through brain shadow wind

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
