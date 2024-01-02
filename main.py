af0ce200906659dcff59fe9614d833e5b579800a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:17:19 +0000

    sleep difference cool boat glass

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+import collections
+def create_user_string():
+        return collections.UserString()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
