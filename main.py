15ab8c09a924db1d543fa8462ab01a3f758c97bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:35:48 +0000

    balance experience comfortable lay cool

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import time
-def get_current_time():
-        return time.ctime()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
