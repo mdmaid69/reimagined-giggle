0817be3d8942006d346684e09e5157a5dce338ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:53 +0000

    autumn Ee chief organization hard

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
