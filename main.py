fd727af686a323a406bfbd1892fbef82b52a9328
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:23:21 +0000

    dinner slight once general train

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def clear_array(array):
+        array *= 0
