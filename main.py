704f4026fa22a6249321127284646b2c1984fcb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:40:01 +0000

    lookaround pollution spoil neither exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
