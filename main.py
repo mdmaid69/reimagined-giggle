b37c2dd8fb9dcd86383c7c716e865b6fa4d3eec3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:48:24 +0000

    concentrate gas awake comeover wish

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
