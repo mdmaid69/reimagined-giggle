e92aae07dac9cda560004dd69163d2d59e00420d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:24:49 +0000

    male arrange succeed claim loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
