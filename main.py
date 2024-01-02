21d66b57bd3ecf812df7bb6e8a4241d9a0624370
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:12:08 +0000

    steady shake middle brave finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import logging
-def log_message(message):
-        logging.info(message)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import sys
+  def get_python_version():
+        return sys.version
