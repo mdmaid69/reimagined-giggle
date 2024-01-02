c7e5f80309b57f47f588bf94926911dcd7f29c8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:46 +0000

    frame friendly thanks appeal switch

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
