22c185bdabb8705fee60d40de4c69bfa07968d3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:59:27 +0000

    consist though far base hard

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def add_numbers(x, y):
-        return x + y
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
