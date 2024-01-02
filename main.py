30950a35b194cd22637fcfcec9eb001918bf8ea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:21:39 +0000

    guard I married about surprise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
