f3f14c34bb8ad83810aa293efe92cb9fda2f2e6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:43 +0000

    nose rush female special lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  def convert_to_binary(n):
+        return bin(n)
