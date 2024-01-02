eac27af8c6bd42e4107683c193aa5ecfa7790b22
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:14 +0000

    important noisy bank comefrom loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
