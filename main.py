508db56acf571eca5e4a3daed4b1af796d0c2894
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:16:30 +0000

    awake something politics story every

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
