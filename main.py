78594befb85237a826d6caa85c38fc3151ed77d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:28:14 +0000

    aim unable track black age

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_array_to_list(array):
+        return array.tolist()
