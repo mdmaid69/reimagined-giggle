37884bd7f07a4c72d8423b600df022e338d4eb8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:59:30 +0000

    bin lay in shame exam

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-def find_min(numbers):
-        return min(numbers)
+import sys
+def print_python_version():
+        return sys.version
+import array
+def get_array_as_float(array):
+        return float(array[0])
