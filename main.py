e7471b2e35bc9e0c8b60a00824c98c4261d8300b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:57:19 +0000

    spring suckme loan shock should

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
