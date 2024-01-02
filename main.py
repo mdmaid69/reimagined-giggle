2f328f679fe43540e0bfbd254db83c10c7db9a1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:00 +0000

    brother without reply brain set

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+print("Hello, world!")
