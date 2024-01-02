69b39fb312a0ab7ec0dd51ee4b73ee7af9a82e2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:19 +0000

    tooth any win foot clear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-def calculate_power(work, time):
-        return work / time
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import array
+def get_array_as_int(array):
+        return int(array[0])
