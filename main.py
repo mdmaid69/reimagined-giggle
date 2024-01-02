abc201fbc2d620e35c6eef625deb7956fc29833e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:54 +0000

    challenge laugh anywayanyhow remark one

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+import tensorflow as tf
+print(tf.__version__)
