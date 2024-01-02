fbed17eea2baceb6218d741a58a8e3fff5ab04ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:30:31 +0000

    breathe only circumstance allow remove

--- main.py
+++ main.py
@@ -1,8 +1,3 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def square_number(x):
+        return x**2
+print(sum(range(10)))
