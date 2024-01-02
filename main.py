f5daf8e3988789848ae7479b58ae584003006405
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:56:00 +0000

    remember experience chief admit greeting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
