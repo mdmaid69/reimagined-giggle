c1307c8212fe3fbc26e66705df5829447522c327
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:23:10 +0000

    early entry quit recommend otherwise

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def get_array_typecode(array):
+        return array.typecode
+import platform
+def get_os_info():
+        return platform.uname()
