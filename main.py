c15687fb5aa773da76500d888622ad4b35e179ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:59 +0000

    plan Kk friendly record welcome

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
 import math
-def calculate_hyperbolic_arc_cosine(x):
+def calculate_inverse_hyperbolic_cosine(x):
         return math.acosh(x)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
