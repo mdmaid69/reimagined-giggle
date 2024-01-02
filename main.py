06c91db6141ab40c5ddedfc12b054f063dbe712f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:20 +0000

    only clear broad key place

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
