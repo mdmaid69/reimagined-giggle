64106190c3114879c77c2880724be325439bef09
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:44 +0000

    sleep by half declare smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
