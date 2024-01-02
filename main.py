27708320b761c80205cdfd01fbd35d36354bd8eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:46:19 +0000

    impression rid part late fail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def add_numbers(x, y):
+        return x + y
