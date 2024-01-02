43531665d7dc494ef9ef48b824d24d50a640c1d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:45 +0000

    trick trust bored correction lean

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_speed(distance, time):
-        return distance / time
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
