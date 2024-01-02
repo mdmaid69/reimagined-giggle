9900bb463d599499a8c1482d325ac49197a3ca76
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:31:48 +0000

    judge thirsty identify unfortunate tall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
