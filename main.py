862168e26372dc56436e6ead391f6cd43b94207e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:56 +0000

    warm clean getaway rarely beat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-for i in range(5):
-        print(i)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
