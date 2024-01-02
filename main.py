b8d4a3bb7347f132189d77cdc4d89150835d2071
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:05:35 +0000

    under turninto claim call complain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
