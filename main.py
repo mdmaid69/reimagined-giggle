4388a8fa7e0f34e72fd74874196fd45f81d229bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:17 +0000

    set ashamed only congratulations wake

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import os
+def remove_directory(path):
+        os.rmdir(path)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
