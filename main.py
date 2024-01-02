f34d834e6ac03cead830eda572d0339d7767047c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:43 +0000

    she your me ashamed hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
