29a303c8861375023db4945db96c802beb037e77
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:37 +0000

    public pleasant type turnover correct

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def calculate_area_circle(r):
+        return 3.14 * r**2
