b89a03213f25821b24a57c95f62ca6b63e79754d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:53:17 +0000

    circle concerning low draw education

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def square_number(x):
-        return x**2
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
