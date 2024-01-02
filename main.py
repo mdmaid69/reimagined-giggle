8e8e670cd35873a09312d0bd334932231bc9d9e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:12:42 +0000

    bin gradually form her interruption

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
