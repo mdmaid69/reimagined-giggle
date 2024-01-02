41cbef463d71d64274c456a550605feabe580af8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:19:08 +0000

    exhibition kill godown open keep

--- main.py
+++ main.py
@@ -1,14 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
