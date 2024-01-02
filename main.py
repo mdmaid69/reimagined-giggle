6d370fd47c3ecd023bf8529ae73eb6acf3124a91
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:52 +0000

    still side ashamed address comeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
