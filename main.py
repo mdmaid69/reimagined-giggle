893088524f650ea099870a4515481b2020c7072f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:00 +0000

    twist tool bank vision cheek

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
