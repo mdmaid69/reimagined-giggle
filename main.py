47e867725b137294b71a6a0af3b0e143b93d63e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:17 +0000

    part congratulations high image block

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
