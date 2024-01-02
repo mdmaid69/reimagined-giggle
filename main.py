58bf7d4e7a83fec1cb5eb2cf00b8dc076dee9ada
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:13 +0000

    circle wild rough extraordinary to

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
