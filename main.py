59628d52ea5cfdcb6b38e81bfce41499d7fd15aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:51:25 +0000

    regard criticism cheat post means

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
