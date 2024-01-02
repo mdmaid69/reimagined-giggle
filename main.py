1523d716dbcf7f718f03950a09b9abec85d8de8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:02:40 +0000

    lucky clean not soon reply

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def get_array_as_set(array):
+        return set(array)
