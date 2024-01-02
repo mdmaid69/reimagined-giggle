153e5905b50f079c6dd91cf11064717853fd4f53
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:05:03 +0000

    light getup circumstance content realize

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  def subtract_numbers(x, y):
+        return x - y
