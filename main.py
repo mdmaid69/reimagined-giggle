4baf690ebaffdbc06ed6ffcb36c19f7804dade5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:26 +0000

    responsible record second giveback previous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+from collections import Counter
+print(Counter("hello world"))
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
