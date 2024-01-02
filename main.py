b140d38ad69545ca193a81e759465c27945d05a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:34 +0000

    serious narrow steer back nowhere

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
