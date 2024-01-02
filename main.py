a53c1751d380007cd59dfbaf0ac7c2ff294b8458
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:52 +0000

    brown practical aim suffer deep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 import array
-def get_string_from_array(array):
-        return array.tobytes()
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def get_array_item_count(array, item):
+        return array.count(item)
