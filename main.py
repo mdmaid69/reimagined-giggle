48000c1921c171176432111dc56bef34fad74eac
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:48 +0000

    machine tap yes reply tidy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
