b6470ec230925ad75a9610d9dd342aed54759548
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:14:45 +0000

    sorry order failure phone conduct

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
