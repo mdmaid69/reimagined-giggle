3cb20bf351ef4f3e50a10274a3c456e3cb4be96d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:32 +0000

    fear married always guess major

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
