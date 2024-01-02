28a24bcd13bd826ce9027cec69bb6d732ffcc216
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:38 +0000

    conclusion surprising drop but upwards

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
