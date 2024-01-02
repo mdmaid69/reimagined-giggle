de43d9df26ce874b86e6dc4b1a645ace11942bbd
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:32 +0000

    Hh common mouth good cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
