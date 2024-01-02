ee1e13795127e7930509021c8b0af7f7ed1d3daf
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:11 +0000

    warm tune harm foot success

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
