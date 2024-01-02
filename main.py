e3fdb3e44b9342c79cbc088e914daee6e842d6d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:11 +0000

    Ii reason chin line still

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def multiply_numbers(x, y):
-        return x * y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import os
+def get_current_working_directory():
+        return os.getcwd()
