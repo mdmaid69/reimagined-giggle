4be672ab7ead68ee03c5d68477132961e9ca6c76
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:23:55 +0000

    move bury wake whichever lift

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-print(math.pi)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
