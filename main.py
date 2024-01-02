f74750dbffeb9562716615eb1eff5ef172aa97cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:17:03 +0000

    art system accommodation invitation twist

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
