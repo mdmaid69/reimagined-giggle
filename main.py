07e7653507b77d50ed4f3c8484b9489361e8070b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:16 +0000

    squash comfort tie half beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+def remove_directory(path):
+        os.rmdir(path)
