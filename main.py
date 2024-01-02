6327e598ee7b5edb730408bc5a3c25d9cb4ba105
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:46:06 +0000

    slide wide record hard disappointed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
