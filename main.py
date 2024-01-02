547ef6aec5ee5a74663dffc1dcb16569d474f3d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:20:14 +0000

    trip makeinto nearly cough guard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
