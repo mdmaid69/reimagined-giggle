78b68dde2bb0ef9610db21b6bfca8e8c3e4101b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:02:13 +0000

    cinema interview content fit emphasis

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def count_words(sentence):
+        return len(sentence.split())
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
