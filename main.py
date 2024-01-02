200cd32b40f35cecdb06b7af136d8c3bc2d00205
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:34 +0000

    excuse common straight trap Itself

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+def calculate_power(work, time):
+        return work / time
