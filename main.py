ec74dca6ae39aa6ac6f1fd672b7733a3ed725517
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:18:53 +0000

    last nervous mother round church

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
