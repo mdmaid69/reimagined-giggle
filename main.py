f1565a1d4afb47dc579580d2283693d5b2f1bc67
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:08 +0000

    before whenever march comment number

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
