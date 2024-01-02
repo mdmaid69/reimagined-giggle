bdcfdea683494e39706aff0057d36ba6bce8398f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:59:01 +0000

    takeoff instruction both sound complete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_list(array):
-        return list(array)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+def get_array_as_set(array):
+        return set(array)
+import os
+def get_current_working_directory():
+        return os.getcwd()
