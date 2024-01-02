8ae5d9c3e5f13fe18f497449eac733951f5633d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:27 +0000

    Tt available direction relief smile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  def convert_to_hex(n):
+        return hex(n)
