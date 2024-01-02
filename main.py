fbf2fee39d65adac57161fc48efb7e18375068ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:37:48 +0000

    today respect sleep bit affect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import random
-print(random.randint(0, 100))
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
