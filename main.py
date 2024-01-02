832f981e38ed8c18ab2ca0b91bf9ae4b2bac27e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:55 +0000

    clearly pot day to brother

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import os
-print(os.getcwd())
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
