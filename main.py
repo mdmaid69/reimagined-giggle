78aa26981e3c8de5397d373ccf23ea8ae21a67e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:07:11 +0000

    geton party he character bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
