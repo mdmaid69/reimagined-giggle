006c4cec0050cf53c24a1547e78bfe928b90ad1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:36:00 +0000

    pretty plastic frightened please wrap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
