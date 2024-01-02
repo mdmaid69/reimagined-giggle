d0f259f4df6e7c9d33f49e93d0686178e97d5ac9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:48:20 +0000

    possible win lesson twist keepout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def split_path(path):
+        return os.path.split(path)
