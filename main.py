ca46587a3efcc1ac203d6f0de79b877ea8807447
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:41:41 +0000

    grow takeoff rain workon getback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
 import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
