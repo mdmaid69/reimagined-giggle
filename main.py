4b27ce1a4352e45621a63c8890b839923a93fed4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:32 +0000

    sure correct shape through reach

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
