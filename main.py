5a6e64a22424eb57b34d3a02e25b7451f00475c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:52 +0000

    trust joke home drive thumb

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def convert_to_binary(n):
+        return bin(n)
+def subtract_numbers(x, y):
+        return x - y
