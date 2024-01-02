ba5308c36b9b6ca31602c679f57cfc04270e068c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:47 +0000

    scare avoid taste take we

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def reverse_string(s):
+        return s[::-1]
