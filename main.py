a1ff082e07436d08d3918691d6bd6237bf31fd9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:07:33 +0000

    far pay image foreign thanks

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def cube_number(x):
-        return x**3
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
