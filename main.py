d53bce3ff9b444bed79a98a1014d5671c8544277
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:41:16 +0000

    not recognize stop threaten behave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-def find_max(lst):
-        return max(lst)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
