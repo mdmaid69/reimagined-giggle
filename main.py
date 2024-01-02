fef834c860e8656fc8c906a6428fd68bd6f44136
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:22 +0000

    shoulder whoever beyond style command

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_index(array, item):
+        return array.index(item)
