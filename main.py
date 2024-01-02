0156d519bac9435b9c177e78aef7e63dc8d0edcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:13 +0000

    theirs warn takeout make coat

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
