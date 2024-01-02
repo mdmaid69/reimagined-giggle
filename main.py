7959c9cb1bd3f386d0af9c42cb870ebb8ed48c32
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:52 +0000

    concerned dress comment ear ourselves

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
