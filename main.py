092dad1343a5a2b57da504fdea81055b576fbfc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:47:31 +0000

    something describe apart university object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def count_characters(sentence):
+        return len(sentence)
