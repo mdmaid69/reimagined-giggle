d4bf71cafb550c16c96778033f0dc0e5623e8602
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:28:27 +0000

    freeze include throw this since

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
