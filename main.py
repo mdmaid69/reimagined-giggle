6fa7c8723d7d397de111342296510390630e78bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:44 +0000

    sick capital ear strange deaf

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
