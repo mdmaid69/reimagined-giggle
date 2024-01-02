137ba726e7875d35ad0aff6e12d501fcd3666046
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:20 +0000

    murder gentle winter contact keepfrom

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
