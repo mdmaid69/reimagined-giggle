88f3acba12c8bdd5c6e76653e4e3e987ce97be44
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:15 +0000

    you principle squeeze finger drink

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_speed(distance, time):
+        return distance / time
