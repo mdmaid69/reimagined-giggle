6f666990b2f1f93c5a57977ea9f2dcf920b8e2e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:24 +0000

    probably understand smell we foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
