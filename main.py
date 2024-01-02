30459ff02776bcc26ce5622f1c067e429c8411e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:30 +0000

    film hollow tape bye blame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
