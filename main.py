065eae4ba3f9f231797887d84c364804efbdb176
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:51:28 +0000

    any finish thirsty closed care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+def calculate_speed(distance, time):
+        return distance / time
