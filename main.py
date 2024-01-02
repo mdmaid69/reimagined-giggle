3cd1f277bd3308467f1cc7bee164d3f6792c26fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:35 +0000

    pinch information familiar design great

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_python_version():
+        return platform.python_version()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
