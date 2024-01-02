e6dbac72474f2d8f10e50ea70e7756b8a050303b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:52 +0000

    turnaway path authority runout come

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import platform
+def get_os_info():
+        return platform.uname()
