0a956029719c7fc36b8c1abb755056a7bc607482
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:00:42 +0000

    fork quantity original to find

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
