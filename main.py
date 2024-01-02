12944215363af1dadf42221364ae31c5226835df
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:16 +0000

    exit Nn emphasize agree plant

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+  def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
