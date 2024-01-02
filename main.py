11090243900553e44fd5939abd02ae5f63a54257
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:57 +0000

    except trial noisy laugh necessary

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-print(sum(range(10)))
+for i in range(5):
+        print(i)
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
