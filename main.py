83ffb8f0e024f534f67c03dad6753c472d248801
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:36:20 +0000

    offend cost side properly characteristic

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_speed(distance, time):
+        return distance / time
+def is_odd(n):
+        return n % 2 != 0
