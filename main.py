f7adc12edc53e1f2957754d34e3da6ef612de0a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:10:05 +0000

    possibility their attractive precise command

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
