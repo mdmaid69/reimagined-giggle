d49bc79d34d964a2f89f2c22ce129914d5034ffb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:34:25 +0000

    total boat sail direct exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
