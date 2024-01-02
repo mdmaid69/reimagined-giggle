fa35963d6239a0bb89618bd6d6174475e29e7168
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:02:01 +0000

    score note bring comeback hospital

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
