61a72182f728d5af53472732645ad3a4ad058642
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:36:53 +0000

    confidence flat contact getaway about

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+print(sum(range(10)))
