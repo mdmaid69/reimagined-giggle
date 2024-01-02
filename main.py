b7abc0617d92dbf7764ac5d06ef1b60e40bf9768
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:54:53 +0000

    great makeup tough table eager

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import array
-def append_to_array(array, item):
-        array.append(item)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
