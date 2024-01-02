1a1fa403dff3f75d1989784df18923c657eccb7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:55 +0000

    continue charge win design late

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_power(work, time):
-        return work / time
+def divide_numbers(x, y):
+        return x / y
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
