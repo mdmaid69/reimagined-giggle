fb2f4c95b9cd01874023dbc3f49e47a3807c1bfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:52 +0000

    scream spare sing pump difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+from collections import Counter
+print(Counter("hello world"))
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
