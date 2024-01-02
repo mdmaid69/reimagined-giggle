00bf01efc2d406daad88903913cf67226e1893d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:17 +0000

    makeinto door always doctor pipe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def is_odd(n):
-        return n % 2 != 0
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+def reverse_list(lst):
+        return lst[::-1]
