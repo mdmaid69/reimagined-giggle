3918e62d899e5a07dfdfe08156f4091a2408cd60
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:54:08 +0000

    information heart pay scare discovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-text = "Hello, world!"
-print("Words:", len(text.split()))
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
