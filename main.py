ccb6921c375565b87a4ee9d78ae0b60f690f2dee
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:53:07 +0000

    Rr reply family learn combine

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
