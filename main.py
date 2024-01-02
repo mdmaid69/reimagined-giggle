7cabf79d70b6d36b42194e918f3177cf65abf07d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:29:30 +0000

    promise new track market with

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import tensorflow as tf
+print(tf.__version__)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
