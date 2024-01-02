86dc52a9cb444525c6ad106e12a30180ef3db855
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:08:49 +0000

    language fact turn basis radio

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import re
-print(re.match("h.*o", "hello world"))
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+from collections import Counter
+print(Counter("hello world"))
