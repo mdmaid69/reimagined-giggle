3521e15ca60b288adee32334d7a0e53f80aa59ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:39:49 +0000

    communication reply sore cruelty ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def get_array_as_str(array):
-        return str(array)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
