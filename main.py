02a17aac8f60aafaf9fb6ae211a0151d39f215eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:46 +0000

    doctor like tragedy criticize suffer

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_directory_name(path):
+        return os.path.dirname(path)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
