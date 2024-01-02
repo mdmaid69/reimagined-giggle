c84fa7d405904269500b7476089d1ed813639b73
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:26:28 +0000

    terrible getback whoever tradition band

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_error_function(x):
+        return math.erf(x)
