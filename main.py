0f7d0dc4f981512dcd7b9d1b423880c442d02ff1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:45:15 +0000

    accidental judge stranger next improve

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def add_numbers(a, b):
+        return a + b
