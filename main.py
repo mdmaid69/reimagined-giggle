0588115e5105d6518bd6b1800c76c9e64beac0b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:17:32 +0000

    shout hope give gentle price

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
 def is_palindrome(s):
         return s == s[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
