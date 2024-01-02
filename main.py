44f6b08fce01c93d7f30bc2f2d50103cffb5cc79
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:39 +0000

    description rubbish slice plastic ilovedicks

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+import time
+print(time.time())
