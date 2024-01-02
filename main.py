ee79deccfd050f9c3767665a1d31d4e5456cd568
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:59 +0000

    not lucky sea run neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
