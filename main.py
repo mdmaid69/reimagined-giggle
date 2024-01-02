7a5e0f432d32f4a5ee25010bb17441d13eea567a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:57 +0000

    open stand keepfrom shopping phone

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_directory_name(path):
+        return os.path.dirname(path)
