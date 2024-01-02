845dccbf1583c9e2698712afeac08c2710092682
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:53 +0000

    show hardly opposite undo properly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import platform
+def get_python_version():
+        return platform.python_version()
