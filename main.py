1e87e4cc4320527e5e5b5ca6601b0583a08ff878
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:17 +0000

    classroom watch now pressure failure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
