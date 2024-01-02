7095ab9b8c0bf0cd2d8372ae96b3afbcb990ff15
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:43:00 +0000

    runout skill beside trade afraid

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_factorial(n):
+        return math.factorial(n)
