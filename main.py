16a7fd122c76f24f0527260d4839016098e5ae57
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:38 +0000

    note advise careless intelligent level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  def multiply_numbers(x, y):
-        return x * y
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
