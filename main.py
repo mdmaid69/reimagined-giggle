3fa32b84633bb99441b80d568f4a84d042015dd8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:01:17 +0000

    failure file exactly as beginning

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import json
+print(json.dumps({"name": "John", "age": 30}))
