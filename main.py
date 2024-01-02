9288e9bcc869b28d376c74dc5054f5ee5361e387
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:02:56 +0000

    school dangerous turnaway save difficult

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def clear_array(array):
-        array *= 0
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
