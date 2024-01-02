d8133f3c0e37d1b6cec8fd36aa8bd5a302039d75
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:53:47 +0000

    believe beat quickly confused hang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import time
-def get_current_time():
-        return time.time()
+def get_array_item_count(array, item):
+        return array.count(item)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
