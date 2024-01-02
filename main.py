a5dd5d2434f5490705f6179e156c6a92e78ccf3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:03:54 +0000

    woman judge damp increase spot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import platform
+def get_python_version():
+        return platform.python_version()
