f2ccb829d6ba36d575ec25445fbff84704e5bf71
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:11 +0000

    building recover politics connect bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+def is_even(n):
+        return n % 2 == 0
