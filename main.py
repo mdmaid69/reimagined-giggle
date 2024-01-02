20af04088f0219816c8b95e6f1d18ac99f31a25c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:16 +0000

    lose acceptable congratulations sleep table

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
 import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+def set_array_item(array, i, item):
+        array[i] = item
