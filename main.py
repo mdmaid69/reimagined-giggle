4bfe7fb51de5c8e928c06854f7ce2601ca633715
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:26 +0000

    row precise shine clean represent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def greet(name):
-        print(f"Hello, {name}!")
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
