448a164b39973d7b12f78a2cab8d85eac7b57a72
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:42:24 +0000

    increase air late towards detail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
