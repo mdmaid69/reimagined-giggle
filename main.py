492ed7bd4d92f6e3a963ac5af81c0bb7cf6a083d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:30 +0000

    polite prepare once break late

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
