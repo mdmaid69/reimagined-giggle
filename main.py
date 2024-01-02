bbd588180a1ed30a9ba1b5bf3430bf0bf9fd8458
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:18:19 +0000

    closed hold swallow turnback duty

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import array
+def set_array_item(array, i, item):
+        array[i] = item
