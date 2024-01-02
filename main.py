cdffd05e493f409897ddf66a5b8b98f7aff5f6cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:05 +0000

    level eager vote definite allow

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
