646aa49e747e92fbb656753f593b6fff15f6a2f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:06 +0000

    trousers back surprising explain delight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def convert_to_hex(n):
+        return hex(n)
