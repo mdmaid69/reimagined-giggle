d7caa7e49ab926c0c1e649bc5f0a1481616c2107
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:50 +0000

    uponon among calloff order peace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def cube_number(x):
+        return x**3
+import os
+def get_current_working_directory():
+        return os.getcwd()
