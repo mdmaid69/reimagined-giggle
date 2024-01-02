e6a7bc6ea6d79755afa7123f2f9f2ba22d28d441
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:46:03 +0000

    strike famous snow very design

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def square_number(x):
+        return x**2
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
