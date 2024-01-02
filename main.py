29741950efe1e448028b368e3d4679f475a7c518
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:54:11 +0000

    because now doctor strike also

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_pressure(force, area):
-        return force / area
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import numpy as np
+print(np.array([1, 2, 3]))
