45923573e40af49c2960333d3e6e4fd6a1810050
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:28 +0000

    short introduction borrow apply aware

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
