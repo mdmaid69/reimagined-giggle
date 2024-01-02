5853518afdf28d8a40ee1ee94e5cbc359c0b01da
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:52:11 +0000

    value bright frame come dare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_counter():
-        return collections.Counter()
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
