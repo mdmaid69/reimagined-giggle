f2a5078eb91b1991e13a1a80250acba6c90e73c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:05:51 +0000

    port immediately emotion thinkthrough call

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-def square_number(x):
-        return x**2
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
