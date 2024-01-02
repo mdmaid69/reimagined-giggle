5e2022efb75a806aa9b96a085ea76df5f7d79725
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:03 +0000

    well label married set deliver

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
