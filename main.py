208797244f30f831e4d3410eb6a79f8f39cac8af
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:46:15 +0000

    lesson deaf move but decision

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
