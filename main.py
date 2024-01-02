0e5e1b2e2adcb6a69c7ecf61c8961a5cd4dfbd57
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:38:49 +0000

    takeback rest arrival guess they

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
