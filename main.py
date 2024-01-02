697d3a6250c64b1bd8589329aab8737ac45ab418
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:10 +0000

    strike separate situation house either

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def remove_duplicates(lst):
-        return list(set(lst))
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def find_max(numbers):
+        return max(numbers)
