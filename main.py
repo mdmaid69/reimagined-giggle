5c35b8845ef69bca0feb35c587d1dfc5e1146896
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:00 +0000

    considering arrange thick level lookback(on)

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
