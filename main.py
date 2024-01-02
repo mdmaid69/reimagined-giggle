0ec04845dabc31ac63d4ce71fae448e8fc637331
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:10 +0000

    beat mean telephone win complicated

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
