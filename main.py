76a17a8f08b372295b5f1f7c0616d7a8767562ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:27 +0000

    concentration smart seem model more

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-for i in range(10): print(i)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
