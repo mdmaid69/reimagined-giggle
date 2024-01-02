0aef167e8fae51d087da50de2c4bee41cbd17a7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:38:01 +0000

    fire cut reach gradually look

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
