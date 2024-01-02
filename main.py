10488bd206d1c892a72491ae649b22af2b092b59
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:30:56 +0000

    wonder awake clean be private

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
