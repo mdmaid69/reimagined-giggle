0c72853a170d018d403aaaf76870cc8e62aa3589
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:56:58 +0000

    ignore possibly silent clever press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_list():
-        return collections.UserList()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
