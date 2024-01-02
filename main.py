4e84ffd357f8fbb1bf0d33c6905c8cb362cd8f7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:08:57 +0000

    completely quiet reserve turnback hollow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def remove_duplicates(lst):
+        return list(set(lst))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
