b7f3a56c694d4a960d4db2da44a8fa02adb9666a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:38 +0000

    many fight talk future characteristic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
