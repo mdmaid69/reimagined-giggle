68e42d191b9661a133e6f0fac2fc5595a862d12b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:04 +0000

    put rank phone smooth hit

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-print(os.getcwd())
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
