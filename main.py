3cffaaa3b37a34370bf981892cd12d1db52e8fdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:57:57 +0000

    content getout serve lookup birthday

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
