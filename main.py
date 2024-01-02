661950752e05553ff16789685819da210e1b903c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:10:33 +0000

    rid design club tap long

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
