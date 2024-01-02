1c22512e53baae210806cee27d4a45f23b90dd49
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:38:08 +0000

    speak question middle deep protection

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def sort_list(lst):
+        return sorted(lst)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
