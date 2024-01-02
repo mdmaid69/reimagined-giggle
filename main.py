ad43a9ca20b9e0416ebc8440d68a29437268bb08
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:50:19 +0000

    mark bottom mixture enthusiastic line

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
