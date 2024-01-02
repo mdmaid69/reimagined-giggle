576c0bd575d4891094e5d7b0d7847dd61e6949fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:10 +0000

    story amount prevent division proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import datetime
+def get_today_date():
+        return datetime.date.today()
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
