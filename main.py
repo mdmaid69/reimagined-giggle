3bad80f5961aa052d0d40557f5e1950e42cc1e7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:26:51 +0000

    more city frequent own be

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
