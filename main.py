cd32e2a1bd27091795b5ed7ea96e9b5c5b343870
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:24 +0000

    live into only 564encourage clothes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def calculate_area_rectangle(l, w):
+        return l * w
 import array
-def convert_bytes_to_array(bytes, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
