cc74d00457f9de6952db7a481428debe9db16ed1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:36 +0000

    tense whether drink better wake

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
