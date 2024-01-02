10fb3f771a9712db7eb49f37bce246c4489910b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:10 +0000

    variety whoever each dark progress

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
