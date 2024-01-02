947cbe7f5f7cd0b9bbef11e2ff59f950c7a3ac42
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:34:28 +0000

    fine drop regular aim effort

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
