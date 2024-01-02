9400ec1b5e3d1540656c8c6c09309f46a4479530
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:59 +0000

    busy party characteristic temper smell

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
