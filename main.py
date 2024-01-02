b0a96783e5ac33c51d11f826a2cf76d72d775f67
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:08:05 +0000

    information practical hand trick people

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def square_number(x):
+        return x**2
