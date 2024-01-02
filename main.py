360b20ffd98705b7d382415eb587a69344d8af94
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:29 +0000

    slide tradition live cope precise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
