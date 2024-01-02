7e195cdbc5d0ca8e483f9a24f2a65683a98344f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:20:17 +0000

    look break preparation need development

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import tensorflow as tf
+print(tf.__version__)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
