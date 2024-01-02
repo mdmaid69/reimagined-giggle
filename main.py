87b13ac84a5a6e456b1a6650e3477eb6dc2a522f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:03 +0000

    if mark careless mind tongue

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-name = "Python"
-print("Hello,", name)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+text = "Hello, world!"
+print("Words:", len(text.split()))
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
