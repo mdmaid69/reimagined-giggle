c5ada1f4945f6b181c8f4048fc995f55eab55d17
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:42 +0000

    underneath game male repeat arrive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_speed(distance, time):
+        return distance / time
