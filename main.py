b785f986a53f4fd776db66ec458d7475dbc32e92
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:37:30 +0000

    fine price offer needle seriously

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
