46de934e3a8bce77ee19a4830c24103bada03e99
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:22 +0000

    private off scrap sale though

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def convert_to_binary(n):
+        return bin(n)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
