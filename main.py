6072be9dbaf6ff3e9f3a768bcba3b4de17dea9eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:38:23 +0000

    cheat awake stranger noisy square

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+print(sum(range(10)))
+import array
+def get_array_typecode(array):
+        return array.typecode
