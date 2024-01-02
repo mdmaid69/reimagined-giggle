fee7ec9005463472fc9c5eb6e5393528dcac065f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:51:30 +0000

    name bringdown rescue scare jump

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_area_rectangle(l, w):
+        return l * w
