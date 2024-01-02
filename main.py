bfbf12a4dd978fe580d20211ff57bd08af97c74d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:54:11 +0000

    dream sing settle before punishment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
