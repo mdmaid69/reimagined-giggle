695ee8508b0d130a3fb297e226ba14acce844cf4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:53:13 +0000

    whistle improvement wear fairly excuse

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_error_function(x):
+        return math.erf(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
