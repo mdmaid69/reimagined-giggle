eed62426bd1c6d4a06eb65bab434f1a1f8efbbe4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:54:40 +0000

    yours pain complete exchange none

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_volume(length, width, height):
+        return length * width * height
