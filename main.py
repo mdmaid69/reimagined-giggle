cac5f8b3d52038dd5ae065ccabd5e58df90d816f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:59:18 +0000

    any eye behaviour confidence connected

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
