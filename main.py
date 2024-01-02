7654c478282ebd5a39150db10dd5fb75833a6708
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:44 +0000

    sight Mm freeze soil neither

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+for i in range(10): print(i)
