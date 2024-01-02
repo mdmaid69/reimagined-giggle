e37b6df0e3be65a5585c4d15a3701ed90fcbfabe
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:17:02 +0000

    formal turn unit bush unfortunate

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def reverse_string(s):
+        return s[::-1]
