eb6dfb01e0303d277e7ae13965f35e186739c438
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:21:39 +0000

    contents upstairs communicate difficult split

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_index(array, item):
+        return array.index(item)
