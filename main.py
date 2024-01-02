fef9c7f724300cf6593d285ebf3ac6d52477fa1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:51:51 +0000

    nor over probably also convincing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_exponential(x):
+        return math.exp(x)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
