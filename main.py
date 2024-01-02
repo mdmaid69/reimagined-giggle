80eec43dcdca4cd02247d203400c5a11a5616073
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:14:04 +0000

    possible in moon justice apparently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_factorial(n):
+        return math.factorial(n)
