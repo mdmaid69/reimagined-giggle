19244fb0630d496de762d62da28b9af0b3ba8d11
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:47 +0000

    stretch give dig journey sleep

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
