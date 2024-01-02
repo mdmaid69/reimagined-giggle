730b1d02d4376ac2653f635d631a39690c1cab4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:14:50 +0000

    Uu expression godown variety cheat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
