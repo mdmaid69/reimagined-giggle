6c7da71f29746b49fc2a969432e95ebaf17c8cae
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:55 +0000

    compete well fail smart room

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
