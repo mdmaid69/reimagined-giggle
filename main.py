16296bc4fce3cb082b9de556e8329108b118dba9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:48 +0000

    afternoon study cover ready about

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
