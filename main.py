60d6aa92b3f521f82e06bacb58593b4916d4ac94
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:29 +0000

    trust refer swallow whichever both

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
