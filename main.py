b4f09df0f16d0726a8bce14877441ed67a3fcf15
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:07:42 +0000

    satisfactory compete suppose right appeal

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
