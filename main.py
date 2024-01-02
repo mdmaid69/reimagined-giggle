56098021263c64e49efb3d910430d67726914ce8
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:08:59 +0000

    pocket reach lookaround only float

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+import os
+def change_working_directory(path):
+        os.chdir(path)
