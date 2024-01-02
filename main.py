bf7fe085f5500cefa2cfddd190ea5651c72bb4fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:57 +0000

    flow peace meal lookback(on) trap

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_power(work, time):
+        return work / time
