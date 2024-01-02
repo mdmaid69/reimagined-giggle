fe97f73b70056d2cb6abb01ff04d1dbee3248db3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:22:41 +0000

    even worth warm minute detail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def is_palindrome(s):
-        return s == s[::-1]
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
