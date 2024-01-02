6fa92ba2dfdae8366a30463bf3aecafae0672b8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:34:28 +0000

    plan spare suddenly confusing take

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_binary(n):
-        return bin(n)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
