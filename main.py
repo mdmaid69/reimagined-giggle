9d9d3988cf519f7a0b47d1a30303a29e320525ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:14:56 +0000

    dish calmly object there treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
