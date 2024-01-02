539f614a618c806fce561363e9af52024905e855
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:18:39 +0000

    trouble block run piece tight

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import sys
+def exit_program():
+        sys.exit()
+def calculate_force(mass, acceleration):
+        return mass * acceleration
