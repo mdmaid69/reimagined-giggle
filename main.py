38e17684a919e2b9b42eb8ed9cb270aae90cd813
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:00:36 +0000

    post listen bend month while

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_min(lst):
-        return min(lst)
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
+def count_words(sentence):
+        return len(sentence.split())
