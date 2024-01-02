f6924494e72b0cef41bcf25b5b2cffba7b1a84c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:14:58 +0000

    left deliberate professional really only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def convert_to_binary(n):
-        return bin(n)
+  import sys
+  def get_python_version():
+        return sys.version
+  def is_odd(n):
+        return n % 2 != 0
