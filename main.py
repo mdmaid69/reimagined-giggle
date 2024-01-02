98a6e03eba4c87b67dd4044a25e0a200a5ce8f84
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:58:26 +0000

    recover professional ever ready extremely

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  def square_number(x):
+        return x**2
+for i in range(10): print(i)
