e44781d0c98539a4f1f2572a3a4b2fdd15dbb29f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:07:43 +0000

    nigga aim hate cope damage

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
