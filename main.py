b965a1ef44d216688fb6af3c94649fb49fcf03c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:02:43 +0000

    passenger willing roof used wear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
