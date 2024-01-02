b68e8f7e54fbfb5ce1d05edc50fc5ac08eb1a8b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:43:22 +0000

    sink giveback pressure grow office

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
