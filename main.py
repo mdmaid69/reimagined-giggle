4164525e450b9a8d26f254d0f6805a34085b6fa1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:08 +0000

    free skill complaint cry holdup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
