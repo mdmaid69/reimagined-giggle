85ba55925d806eb345a69d67007cd3cc8cdb042c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:23 +0000

    dance past lost rid callback

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
