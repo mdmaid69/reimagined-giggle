15436c7870e47dbe2fca9704c304325bb6ca9910
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:54:38 +0000

    when image year official mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
