736fae623a3615b8fa7355e5a6bfea3f45bb182d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:02:01 +0000

    from unkind reduction wish sense

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
