d1c41574b01326bc48cfd418feb9762154dec87b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:44:08 +0000

    hang borrow callback satisfied sort

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
