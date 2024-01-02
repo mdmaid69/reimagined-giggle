6da82101efc5172cd452e049bb837605a03e9d6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:14 +0000

    considering pay lift Ee her

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
