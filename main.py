f9c243d38794d162db04ca5db59779fd212c4068
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:55 +0000

    cut takeout shoot effect region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
