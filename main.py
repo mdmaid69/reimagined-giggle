9ad7f208f495f43995a80b377a0da64bf202e5f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:36 +0000

    fight solve match form upwards

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import math
+print(math.pi)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
