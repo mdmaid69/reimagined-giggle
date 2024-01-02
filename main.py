ab209a6f3ce03d9f8784f188a4690ccdcbafc9c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:48 +0000

    past minor breakfast guess fault

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
