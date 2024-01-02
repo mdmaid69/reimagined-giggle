aceae16e883155ded98b1eaa1d43bd4694ecb90d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:16:55 +0000

    bang long fight agree pleasure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
