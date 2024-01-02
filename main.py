856d2621fdef4f6a14ebeeca2fdea0418b1886e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:56:58 +0000

    recognize ground system in exist

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
