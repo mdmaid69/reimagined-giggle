a1a85d61a2df47a25c1ca1e2f72476aa0bc57ff7
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:59 +0000

    bang sea proper drive accept

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
