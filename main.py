74a6544ef3bdf74569e58146e099a243b7e23767
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:31 +0000

    that trick anything love name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
