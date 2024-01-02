ceb56020b3fbd2ee92f052c65546a69f1e990d28
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:30 +0000

    this sink crazy path confused

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
