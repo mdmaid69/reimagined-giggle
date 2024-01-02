dafa030a19a9d460704f9eece659e08b33870926
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:56 +0000

    eager design telephone add who

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
