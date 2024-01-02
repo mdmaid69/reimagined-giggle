e0557129ee8421488416d684de90e0313613ed51
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:30 +0000

    small long increase transport circle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
