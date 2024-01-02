79b70933eb815a05e62a8fd7773c3c4a761d59df
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:12:01 +0000

    nose ordinary seat describe pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
