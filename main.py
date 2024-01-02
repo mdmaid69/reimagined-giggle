57decf1dbb86a81f71519fd1ce07b31af864a4db
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:26:27 +0000

    underneath reliable upstairs freedom bus

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
