4b83679f55ed15c2a0786d8b81a8ff25ff2b74cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:37 +0000

    all careful apply suit dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
