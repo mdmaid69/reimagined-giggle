67d7bd0972f5d9b951e2c4e2f5e515a27e10118e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:51 +0000

    doubt declare gather mark gas

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
