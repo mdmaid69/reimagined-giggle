01d0f414d8c4ecf566a0f24adb24d76f9bd045e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:30:06 +0000

    by rid interested clean translate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
