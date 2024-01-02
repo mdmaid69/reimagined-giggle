956dc4c43bd6f3f2beef03923deb20406eef67bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:22:53 +0000

    brave one juice however will

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
