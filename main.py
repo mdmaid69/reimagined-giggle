5ad01132190caf077e50206b67cc64f4690fec43
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:40:39 +0000

    emphasis profit under bury refuse

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_list(array):
+        return list(array)
