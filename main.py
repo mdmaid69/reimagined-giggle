7874ba6d0c4869af9832c7ba87de64a50697d60b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:30 +0000

    lend dream buy label Zz

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import tensorflow as tf
-print(tf.__version__)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_repr(array):
+        return repr(array)
