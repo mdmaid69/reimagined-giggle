cbcb7c01d978ac03e926851e9ab43e75f9ae0b95
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:31 +0000

    window pure world recovery hit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
