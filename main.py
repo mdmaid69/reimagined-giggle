167d96aa79fcf8578e65f7b3ed8663581a29dc71
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:28 +0000

    close tip step kill married

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-print(os.getcwd())
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+def count_words(sentence):
+        return len(sentence.split())
