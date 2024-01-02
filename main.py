f462c1f018e471726a3c77bc528a8ad8d4b93f0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:52 +0000

    bottom explain mine rarely in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
