fe51ff6cf2c006b27e9192ad445c92640dc4cc5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:07 +0000

    slowly call board wait tidy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
