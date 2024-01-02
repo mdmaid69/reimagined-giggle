9c09978d72dace563cad018b05b76d555b73bfb9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:40:20 +0000

    purpose maximum contribute public sport

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
