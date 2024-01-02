018834a112f4633311608978519ef412d87b0027
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:51:25 +0000

    after actual tough by his

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import array
-def convert_string_to_array(string, typecode):
+def convert_bytes_to_array(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def get_array_item(array, i):
+        return array[i]
