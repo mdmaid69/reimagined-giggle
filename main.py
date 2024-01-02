253689a91319b1f7d0935c4b35964cef79875c8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:16 +0000

    suit content spend question side

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import collections
-def create_counter():
-        return collections.Counter()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_pressure(force, area):
+        return force / area
