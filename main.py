66d8a444bd96d470bb6d213c19f2b4bf91b5e74d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:05:25 +0000

    spot whoever roof swallow adjective

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
