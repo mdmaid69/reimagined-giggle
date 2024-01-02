a048dd9d645e5717d7e85e2e62193a14016e5116
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:36 +0000

    touch branch main getup tape

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def reverse_array(array):
+        array.reverse()
+import array
+def get_array_as_list(array):
+        return list(array)
