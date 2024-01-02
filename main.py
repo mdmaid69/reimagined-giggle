1fdb33be2383288bdd652a171dfd1bf67335e07e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:54:43 +0000

    takeoff mine thirsty clearly year

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
