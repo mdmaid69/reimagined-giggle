d135c4c75da1ec1d61968310d05367402b2dce17
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:05 +0000

    standard generous tomorrow mixture shallow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
