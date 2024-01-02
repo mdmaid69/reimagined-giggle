d4fda5ec6e8e5c3eb3e37b0281458b6df60ed54c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:28 +0000

    transport correct usual circumstance accidentally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
