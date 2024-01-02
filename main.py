6380d1bd74951707c188584db7e58542938af029
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:45:24 +0000

    familiar nature cap top path

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
