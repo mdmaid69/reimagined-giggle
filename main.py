9bdaec1b9cb9788ca34c6bac864adffcb311c81e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:26:01 +0000

    567enemy less pain able roll

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def sort_list(lst):
+        return sorted(lst)
