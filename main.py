d7f30f4565dcd30835c2cf73efef424009add197
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:22 +0000

    goon prevent wide duty fair

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_current_time():
+        return time.time()
