4f477012910065493930d3f2e6d4931f4f8c49d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:14:02 +0000

    mistake unlikely sense foreign cruel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
