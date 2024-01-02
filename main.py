6f71758f132b8269a541f14ac35687a27a47fe07
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:59 +0000

    watch comfortable achieve copy settle

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
