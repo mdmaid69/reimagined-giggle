e9fcee33c69b11edfedbcdd5ad4ffc1c4936d978
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:32:32 +0000

    inquire keepout crime wild choose

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
