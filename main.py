b0117a19784f536a4a25334e6d0623ab7e6f9011
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:39:44 +0000

    maximum Jj popular space bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def reverse_array(array):
-        array.reverse()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
