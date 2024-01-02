ed290c3717745357b9aedbb513d363e527c47e85
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:24:00 +0000

    past broken shop hardly say

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+def find_min(lst):
+        return min(lst)
