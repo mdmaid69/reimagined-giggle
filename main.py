e854885a1e73fe14b3c4d54cb848896b0e690971
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:15:54 +0000

    by slight cover breathe hospital

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
