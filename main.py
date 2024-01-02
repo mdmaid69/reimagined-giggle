f062a32ec7dd1fc307212700c9078dd5f29c8171
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:40 +0000

    job typical basis since chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
