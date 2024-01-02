a1b26395692e388d3b3c7175555e19172e765a57
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:03 +0000

    union sore scream build city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def get_environment_variable(var):
+        return os.getenv(var)
+import collections
+def create_user_dict():
+        return collections.UserDict()
