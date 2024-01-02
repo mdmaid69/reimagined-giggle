03eac842ea19b2b44430a7c183b87be19d9b0619
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:29 +0000

    coat disturb half boring talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_length(array):
+        return len(array)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
