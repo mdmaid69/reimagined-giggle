50d6c29688923ed48e11b694bcea378a0ea9ee0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:09 +0000

    mess confident crash safety mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
