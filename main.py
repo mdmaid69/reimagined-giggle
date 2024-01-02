8e9c5d04b2987314c24d0fa99a2c8468c848ab35
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:31:53 +0000

    pipe mail accommodation blow perform

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
