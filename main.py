b5f587ff7e72c5a3c2aeb5851a4165d0be5b90b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:59:16 +0000

    general exactly short ago warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
