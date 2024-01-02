5a5f8e3ddf3348e0e10d7c5027fed2fa38c73383
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:37 +0000

    determined hold strike nowhere stare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
