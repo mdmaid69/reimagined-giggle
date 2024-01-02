3cbede317db020c5dd51263343a46c34cbabbd84
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:34 +0000

    store hole equal report sail

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
