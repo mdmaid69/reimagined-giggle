52875d0b4c2b95fd7fa1f782674a21bdcbafbf78
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:36:15 +0000

    position ordinary pressure slowly strange

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def reverse_string(s):
+        return s[::-1]
