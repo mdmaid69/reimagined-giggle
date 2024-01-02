aae3ffe812e22ec7750a348013fea3454301d5d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:18 +0000

    weekend ever push professional sky

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
