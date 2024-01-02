015feebfb386e54d187b6236c2d867d6251b9042
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:47:40 +0000

    example child breakinto final black

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def square_number(x):
-        return x**2
+import time
+def get_current_time():
+        return time.time()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
