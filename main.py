a4859ceb2dcac902c16e33b7c7cf2a6e0babd22b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:29 +0000

    survive notice mood pool concerned

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
