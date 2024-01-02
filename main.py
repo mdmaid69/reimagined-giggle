314d2a6bb9313f0f06b4b8c1f5f3718aa2adb489
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:03 +0000

    fall pleasure cover reasonable dear

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+print(math.pi)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
