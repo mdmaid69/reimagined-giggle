880ae0b23749dbfd4f21a82533d419fb2b936f9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:34 +0000

    control disturb niggaacademy Bb sleep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-name = "Python"
-print("Hello,", name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
