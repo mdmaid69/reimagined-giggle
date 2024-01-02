6bb64391b779fe7ffa1b85ef3b5170b443c1099a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:27 +0000

    entrance difficult Cc model loose

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
