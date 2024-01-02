a765043e887d8351f73b6782767f45085597f14b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:09 +0000

    compare leg traffic flavour about

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
