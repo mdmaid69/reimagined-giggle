1f48d0a68f53f92c5fc479bac0c11386e8c12c7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:55:41 +0000

    jump Vv getoff soon fly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import numpy as np
+print(np.array([1, 2, 3]))
