09430f9abda550a9cdf59dfb69d23a7f7ef92949
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:13:18 +0000

    forward mad bridge dead clothes

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
