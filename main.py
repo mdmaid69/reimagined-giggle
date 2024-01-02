309beefc666edfdc003f29329a1b73ba96c4b931
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:14 +0000

    reliable keep move drop careful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
