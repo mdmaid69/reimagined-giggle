da2455f55861ee1d850e154da1cb7b9518f1132e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:19 +0000

    refuse fresh great within thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
