5d7069b29cd01310a5db1e8fbd4e6129c8d8e3de
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:57:17 +0000

    language round unpleasant theirs round

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
