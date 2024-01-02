2057c062e1864c8bf2a3a5de5750cfa6d33b18ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:43 +0000

    owe throughout bye typical dry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import sys
+  def get_python_version():
+        return sys.version
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
