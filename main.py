9c644941c2904efb4dd7ecd9318ea766c8f2ddc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:13 +0000

    Ss outside inquire ear leg

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-print([x**2 for x in range(10)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def sort_list(lst):
+        return sorted(lst)
