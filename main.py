9fac5012fb22ea4535f03e2d5f40358ab6a0a644
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:55 +0000

    march performance round lunch lead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+def count_words(sentence):
+        return len(sentence.split())
