1170da5c16890f828a80943c01001dca782c3dc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:42 +0000

    thing under busy seem bringback

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
