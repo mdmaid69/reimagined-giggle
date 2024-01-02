b2d84d49a1d5237b7d678119072041557065ae51
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:46 +0000

    twist loud trip unable talkback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
