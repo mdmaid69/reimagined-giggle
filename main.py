f51bed865daca6b4d7323fb9c52564cb50569cb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:57:25 +0000

    arrive exactly forever process strange

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import time
-def get_time_since_epoch():
-        return time.time()
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
