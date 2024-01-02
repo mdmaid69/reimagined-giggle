92827eea7f3b423011df47a4c65011127772f779
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:41 +0000

    declare frame definite worst case

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+def square_number(x):
+        return x**2
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
