f3bd89acf5b3591c64b712eccdd26f8b71e22130
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:21 +0000

    morning act remain badly birthday

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def subtract_numbers(x, y):
+        return x - y
