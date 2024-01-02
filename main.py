990ac60678e2083a513660f50fd4b102394fd15c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:56 +0000

    manage law Ee gas over

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def get_array_as_complex(array):
+        return complex(array[0])
+import array
+def get_array_length(array):
+        return len(array)
