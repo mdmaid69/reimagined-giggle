33c04e57f18cfe15f9d05d15adc5d237e5f1ad73
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:32 +0000

    shoulder uponon throw strange comeover

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
