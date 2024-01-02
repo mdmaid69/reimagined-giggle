4467883be824feb86d9cf25d0c077f7f14338215
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:29:02 +0000

    solve hate job basis absolutely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
