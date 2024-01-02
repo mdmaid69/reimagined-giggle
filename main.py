3c4ea861cd642bd4a17414ef8c8fc50782942ffa
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:40 +0000

    sudden rather whether tall used

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import logging
+def log_message(message):
+        logging.info(message)
