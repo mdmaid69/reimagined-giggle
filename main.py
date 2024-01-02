4f67cd20b59caa1ca1a094413fffd1475479ca34
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:40 +0000

    generous peace pen reply border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
