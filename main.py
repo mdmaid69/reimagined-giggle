ed70716c00c10c662558295be5b92bbcd133f015
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:35:59 +0000

    press top convince union feed

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_as_repr(array):
+        return repr(array)
