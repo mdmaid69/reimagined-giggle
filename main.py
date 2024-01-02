ebec45b34df2001d35388d56bbb194c42bd2ef68
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:59:52 +0000

    thinkover pattern ordinary official increase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_as_float(array):
+        return float(array[0])
