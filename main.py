b17f48429b19e46faa576d5ce43f72707dce15d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:47:24 +0000

    discovery movement dead waste together

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
