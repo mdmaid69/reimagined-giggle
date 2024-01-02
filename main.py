2f807babbabc7b2142d5ba9e4fddcf873d21e3e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:19 +0000

    science thinkthrough quiet live either

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
