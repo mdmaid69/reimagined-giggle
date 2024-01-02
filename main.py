b052dfa66e7c86e5f282346d9e4e386737f5fba1
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:43 +0000

    disappointed hello case 573entertainment wind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
