806ee6a053f32a911f2cecb0bca555152b5424e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:40 +0000

    weekend through many voice star

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import tensorflow as tf
-print(tf.__version__)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_floor(x):
+        return math.floor(x)
