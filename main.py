6bd75081593fc777c8778787f615e503c5355092
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:31:08 +0000

    easy software kick air Cc

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
