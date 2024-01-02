67d915b7533d6f8a8e475848fb6ade5f5b15c132
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:06:03 +0000

    rich night because will book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import logging
+def log_message(message):
+        logging.info(message)
