7778213d626f132ab0a11ecf2a0d1091fb504562
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:53 +0000

    heavy fall right type brush

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+import array
+def get_array_item(array, i):
+        return array[i]
