c5e41dce9f4f4ca3d9cdbce0b8545f1de70400f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:58 +0000

    skin one amount passage cruelty

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
