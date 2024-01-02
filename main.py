032668b27183c51714817b8f010c0a318cb061d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:17 +0000

    detail shout thing borrow sleep

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  def remove_duplicates(lst):
-        return list(set(lst))
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
