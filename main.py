6d1ff3cd53e433e785a3578b2fdf3f7945caaeae
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:42 +0000

    pick emotion embarrassed copy pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import os
-print(os.getcwd())
+import array
+def get_array_typecode(array):
+        return array.typecode
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
