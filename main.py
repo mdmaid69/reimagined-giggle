b46043408242531ad350cb0dc4ba04e05e89036f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:02:26 +0000

    average blind until thinkthrough pool

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def convert_to_hex(n):
+        return hex(n)
