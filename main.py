10661613d718d6a494a1a93ae1a648475cd25d47
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:59:26 +0000

    film treat raise dress comedown

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import tensorflow as tf
-print(tf.__version__)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
