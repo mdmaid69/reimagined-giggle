e16b634340c0a7af31d1e23a0a7832c8fe89bc6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:42 +0000

    sometimes destroy offend cook willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
