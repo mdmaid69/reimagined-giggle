a9941c7285a7af15e185967884b13dcc311a7cd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:33:21 +0000

    scared reach basis alive join

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
