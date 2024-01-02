90e4ed64ba08b70aea28cc63e74b6b493fc2d500
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:34 +0000

    tie rob loud door stiff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import sys
-def exit_program():
-        sys.exit()
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def create_stack():
+        return collections.deque()
