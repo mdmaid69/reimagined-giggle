3e63fe64137c30e9d3320c130fda10f4996dd2f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:48:40 +0000

    often room hurry fix duty

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-print(math.pi)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
