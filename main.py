9d83aa4a69d27f4b25fc83ab3894688ba057133b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:43:16 +0000

    concern pen lookback(on) dig than

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
