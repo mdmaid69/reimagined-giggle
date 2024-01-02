add09302191fd40efd3b88f2536efc72e6139f5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:37 +0000

    watch healthy start arrangement Bb

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
