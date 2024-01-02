049b57da88d2bdf8a657345dc8f2949f80c8b985
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:53 +0000

    book manage swing break rich

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_power(work, time):
+        return work / time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
