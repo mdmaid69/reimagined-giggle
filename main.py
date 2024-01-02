f0e3c214b206d0bca97bc6623d015e985ddd6b4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:12:04 +0000

    blind scrap impression other standard

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
