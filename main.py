ca2a33d489a2029377aefab3ada3ebab7bc4b97b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:15 +0000

    afterwards centimetre science amount get

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def greet(name):
-        print(f"Hello, {name}!")
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def divide_numbers(x, y):
+        return x / y
