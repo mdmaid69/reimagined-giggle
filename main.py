97d642ce6fe47c8a4f362168b8a17d8aeff2018e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:41 +0000

    the beyond organize repeat search

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_power(work, time):
+        return work / time
