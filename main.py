dde8b01e664859036a1bd16ae0b60011ae5c1b16
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:02:31 +0000

    permission top how control fold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
