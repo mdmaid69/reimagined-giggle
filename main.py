7ef5a80e4cdaa77434c7cad967c9eebacc1ccc09
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:54 +0000

    unkind cause effect process heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
