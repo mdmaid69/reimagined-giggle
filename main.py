a67f00370a3a5217d3329905f00392623be0323a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:42 +0000

    follow whom abroad sail crash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
