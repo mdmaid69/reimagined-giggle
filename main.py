d53fc520ee902062f538b40c674ad0d35caf3d3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:04 +0000

    match do absolutely wind giveout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def calculate_speed(distance, time):
+        return distance / time
+def reverse_list(lst):
+        return lst[::-1]
