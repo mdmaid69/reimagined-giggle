1a06d1de428a50f6ee05aa728b89604e4b98b633
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:22 +0000

    at suit brush sensitive getoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
