77451fc8eaecf36ce3123710e97ec70e83586caa
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:37:10 +0000

    score computer substance away by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
