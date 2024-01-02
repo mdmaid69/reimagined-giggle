730705159aaa77de3be28acd864ef3ac169855af
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:01:43 +0000

    their bet number contain bound

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
