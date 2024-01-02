a7b58f9f87e3398b7fb0228f0cd8088343238e75
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:40 +0000

    train mine so turn hate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
