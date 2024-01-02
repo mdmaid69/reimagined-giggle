ca40cf045d57d5ac8e308170493ae6cf6e03843a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:34 +0000

    type weekend return mixture enthusiastic

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
