589ee8822942619f0e507d9a33430d8ebf6efca4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:08:07 +0000

    admire Qq arrival beginning he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
+def find_max(numbers):
+        return max(numbers)
