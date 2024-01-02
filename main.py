7ace7b4f0f401b7a22ed7d66720c662570bcad0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:16:40 +0000

    indeed lock children 571enough job

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
