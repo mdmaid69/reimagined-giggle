29b11c24e1f2ae4de6326fee053482ed36d9d05d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:20:44 +0000

    splash payment exit fill taste

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def square_number(x):
-        return x**2
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import array
+def get_array_item(array, i):
+        return array[i]
