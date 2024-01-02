933e8a629e797c9873ceb4b63a41b4678e6f71e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:53:49 +0000

    exploration none roof precise dance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
