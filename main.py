4cf2a590d2121386fabf4f849c37a4994f22aeda
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:34 +0000

    what attract road nigga doubt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+import array
+def get_array_as_list(array):
+        return list(array)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
