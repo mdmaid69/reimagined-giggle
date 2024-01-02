c5df35b4316f3ff0363800776f8968c595b0c290
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:05 +0000

    herself little nice noisy hand

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
