7e567f37063035caeee1fdb1c5f8d045338fab41
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:17:37 +0000

    open kick justice survive among

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
