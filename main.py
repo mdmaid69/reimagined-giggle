1360d05d078ecc7ef9fc9da004911dbddfc2a76f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:22 +0000

    follow accidental reasonable offer busy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_gamma_function(x):
+        return math.gamma(x)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
