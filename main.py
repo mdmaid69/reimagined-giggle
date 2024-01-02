2c2aa248fea20af054c48a9a4f2b0204b3533123
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:44 +0000

    left rare attach deal mention

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+for i in range(5):
+        print(i)
+def calculate_volume(length, width, height):
+        return length * width * height
