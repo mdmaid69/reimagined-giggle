b34c303444d2d50059d09e4dd235ed0528388e9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:46:48 +0000

    people reply big point could

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
