61e248107b5bf7545d3c2b7a867249556594b19d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:09 +0000

    eat alone near he risk

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
