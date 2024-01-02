896b50a74cbb0957a8d6ee1325778322364f0a80
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:40:15 +0000

    Dd there government ring write

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
 import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
