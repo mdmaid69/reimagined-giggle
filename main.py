10c61142e7b1edc824e714ce64a58ff4791d8cba
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:33:27 +0000

    mind watch wander temper pass

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def sort_list(lst):
+        return sorted(lst)
