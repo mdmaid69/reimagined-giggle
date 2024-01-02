e5871eab4ec88b8411d66ef29be2a09e86e41cf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:31:28 +0000

    realize best imagination bank comfort

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+def calculate_pressure(force, area):
+        return force / area
+def calculate_density(mass, volume):
+        return mass / volume
