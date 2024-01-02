71bbd62531ac4717a6e4988209b77e53a7009a39
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:45:03 +0000

    mysterious bill near trust character

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
