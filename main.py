f04f10ebfcba424b3597e15e5dd855f078fd8c63
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:45:32 +0000

    actually opposite hair general loss

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
