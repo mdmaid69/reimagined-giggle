b9b0d7e643bed879ff62092c028016a9badc74a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:34:40 +0000

    doctor decide claim knock calm

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_density(mass, volume):
+        return mass / volume
