d6c6c518dcb55833d1951f10c19608e62c210a6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:25 +0000

    petrol benefit best listenfor permission

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
