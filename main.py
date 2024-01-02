002aba196b001e3ffdf02d294ec136a87e6e7949
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:40:30 +0000

    polite wonder bear summer bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
