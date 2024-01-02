a7ffebeae8d741f0a91edb66a6019d029d9f6e3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:29 +0000

    address complicated accident takeout accept

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def count_words(sentence):
-        return len(sentence.split())
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
