f86c7674f288e3250e10e0548f5b0f1c3ed17059
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:09:54 +0000

    cause question later purpose unless

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-name = "Python"
-print("Hello,", name)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
