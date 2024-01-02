2c75fd58e8208f79dadf9fdfced3404ed81b4330
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:07:23 +0000

    involve dry whoever ours side

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_tuple(array):
+        return tuple(array)
