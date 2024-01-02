ec8fe46fec281449e053c0feef764c4776500bb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:41 +0000

    command slip objection connected spare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
