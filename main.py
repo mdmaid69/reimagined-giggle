d496efafbc180ff2058a5568b519dc3071016031
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:46 +0000

    soon picture myself snow brain

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import os
+print(os.getcwd())
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
