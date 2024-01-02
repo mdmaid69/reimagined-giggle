9f17b6259c12f62f343e4fd70bf0b3cec25acc44
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:45 +0000

    forgive among weekend somebody 565end

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
 import array
-def get_array_from_string(string, typecode):
+def get_array_from_bytes(bytes, typecode):
         a = array.array(typecode)
-        a.fromstring(string)
+        a.frombytes(bytes)
         return a
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
