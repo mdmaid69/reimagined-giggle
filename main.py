4aad389311a9096d1a2396bddfbc957b364225fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:42:49 +0000

    birthday material murder uncomfortable relief

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+print([x**2 for x in range(10)])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
