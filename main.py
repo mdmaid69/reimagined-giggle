18e11e863cf8342c8d5251375882abcc843a570f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:21 +0000

    transport release cover jump repeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def roll_die():
+        return random.randint(1, 6)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
