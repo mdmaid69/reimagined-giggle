423b91f96cf9629137df3da617dbd1a9c9f2dfcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:26:35 +0000

    window press alive under clever

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
