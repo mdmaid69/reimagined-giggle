c65f5b2217863684b44eec7c65ed93891460074f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:13 +0000

    thorough dare unable shelter basically

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
