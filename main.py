4ab29d9541db6f2eb92aada54ac5b9ddf74fdaf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:17:18 +0000

    advanced train the crisis live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def is_odd(n):
-        return n % 2 != 0
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
