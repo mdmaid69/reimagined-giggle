405ffdeb76864a51132753718ae19efcf6fa3b44
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:06 +0000

    dress note hungry tongue provide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
