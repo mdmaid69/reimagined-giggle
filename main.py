3389951afc03e1a848d466c6f0af013eda5d0f84
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:59 +0000

    repair tight get important wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-def square_number(x):
-        return x**2
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
