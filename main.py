9782b11a9002c0ff0d7c34870ebe7fa17557d8f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:44 +0000

    thinkthrough alive still quantity smell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_index(array, item):
+        return array.index(item)
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
