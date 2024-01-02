6911ffe85f0d32b0b1e379dbaf6ba95522cc24ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:39 +0000

    tour goout pen creature compare

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def find_max(lst):
+        return max(lst)
