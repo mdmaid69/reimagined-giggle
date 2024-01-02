e5d7fc864b136404f875c6a6532d4c9781df69ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:20:18 +0000

    purpose love best day possibly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
