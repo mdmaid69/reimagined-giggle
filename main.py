2578ba4020a4b7d76986da3c8e41ea3fb206cc5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:16:28 +0000

    report feature disapprove expression list

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def divide_numbers(x, y):
-        return x / y
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
