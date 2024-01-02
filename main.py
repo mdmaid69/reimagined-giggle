3d713b7039bfcb9afd8162ff07668672692e9188
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:24 +0000

    even great pretend instruction key

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_floor(x):
-        return math.floor(x)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
