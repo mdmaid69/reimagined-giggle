7e2e7cc73b1e463048f1f5aa73a68d39b907c7ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:47 +0000

    wish happen whoever round joke

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
