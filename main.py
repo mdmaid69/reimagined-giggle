dd91ddc28fcdb1d65060e5714899bca379bf0900
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:18 +0000

    gas so block impatient rather

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
