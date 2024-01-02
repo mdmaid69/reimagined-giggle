64a38400a31e309119995f47b57494ec882b5612
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:24:19 +0000

    affect tradition window chance adjective

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def is_palindrome(s):
-        return s == s[::-1]
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
