69ccc9580c8caaa6fad400ff9a3f4187e6813cc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:23:55 +0000

    cut fall bus her coffee

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
