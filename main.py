af96bf9f17d512cbc302c78bc8a4178051ea5657
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:23 +0000

    strange further second lot middle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
