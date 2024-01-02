59bcb6cf939b84ae918d299835f3cedc2fd3235a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:37 +0000

    open place depend table shall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
