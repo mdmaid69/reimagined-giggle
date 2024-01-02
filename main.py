c484bb0dc1d9b04dd12d9ec82f5133c0041156b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:25 +0000

    wrong for cheat judge violent

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
 import argparse
 def parse_arguments():
         parser = argparse.ArgumentParser()
         parser.add_argument("--input")
         parser.add_argument("--output")
         return parser.parse_args()
+def find_min(lst):
+        return min(lst)
