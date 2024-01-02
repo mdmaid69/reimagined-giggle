bad28da9c82aeca47257300eaab3f852bcc1e61b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:54 +0000

    make left murder score turnover

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import time
 def wait_for_seconds(seconds):
         time.sleep(seconds)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
