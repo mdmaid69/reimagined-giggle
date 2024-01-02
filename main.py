fc459af7845ab71c1657e768c7a6d0dabab10bb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:53:20 +0000

    so refuse its sure hollow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_volume(length, width, height):
-        return length * width * height
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
