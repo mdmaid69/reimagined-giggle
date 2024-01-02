fbdc8e8fe893e3960ae0d502058bd8ac5b4161ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:16 +0000

    pool long nearly about invitation

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
