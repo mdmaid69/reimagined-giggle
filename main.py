e965e7b7f8fdf6747a1b57d726813a847a879b80
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:21:37 +0000

    connection level swing cry prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
+  def sort_list(lst):
+        return sorted(lst)
