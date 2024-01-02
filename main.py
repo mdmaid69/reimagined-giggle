0acd23d4748482e03693f86a2ac6e8e4be3b8c8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:02:48 +0000

    someone dress land eager day

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+print(math.pi)
