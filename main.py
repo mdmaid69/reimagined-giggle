a4c2d8c80ab4339924840ddf77cbc3a9061703af
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:23 +0000

    dry fight view prove year

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+def reverse_string(s):
+        return s[::-1]
