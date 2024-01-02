7a4ec919bafcd06340782b6a2a5adeb1948c1844
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:35 +0000

    sad minimum advantage develop distance

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import time
+def get_current_time():
+        return time.time()
