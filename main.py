b5d90cf0fc46a86d4636a0393227dfaa4b8ac8d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:00:30 +0000

    either we watch feeling major

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
