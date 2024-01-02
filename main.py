26caae8b1aee6d16cbd0ccd4645b2c5cac420eaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:16 +0000

    play really near television bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
   import os
   def get_file_nlink(file_name):
         return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
