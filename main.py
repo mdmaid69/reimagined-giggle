2acef800ed8cfded6f833f0257310367441d4451
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:16 +0000

    carry rent lot contact degree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
