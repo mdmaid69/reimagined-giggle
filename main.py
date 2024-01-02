2eb3f92099755627ab29d423942f7fd932496f73
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:23:07 +0000

    combine habit turn world pencil

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+print(sum(range(10)))
