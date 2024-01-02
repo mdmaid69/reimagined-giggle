ae686568b0d7957b4b7e8a3ec21819584d5432c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:31:24 +0000

    gap unusual defend accommodation whole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_power(work, time):
+        return work / time
