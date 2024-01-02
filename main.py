1e550db516eeea2dd731aa31878346ea5af1449d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:03 +0000

    at late day consist formal

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
