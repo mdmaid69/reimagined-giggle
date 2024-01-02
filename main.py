83ff35fc0a3ef76367a016b20fe59039948f0fac
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:48:10 +0000

    traffic square vision middle club

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def square_number(x):
+        return x**2
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
