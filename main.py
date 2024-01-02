dace0fe969d5dded67def13984c1a0be916d407b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:43:33 +0000

    swing summer software bother want

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def calculate_area_rectangle(l, w):
+        return l * w
