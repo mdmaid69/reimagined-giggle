9faf0f331f99f19c52dc80cb6f743d41c06e29ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:05:37 +0000

    obvious there hers learn influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import sys
+def print_python_version():
+        return sys.version
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def get_array_as_int(array):
+        return int(array[0])
