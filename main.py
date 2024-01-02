489de0ff8dbbbf0b75a941aef9bb291f55c39d5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:01 +0000

    round miss completely chat fire

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
