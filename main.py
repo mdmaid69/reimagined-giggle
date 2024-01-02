f27cac4278739665f4d9a5d10b05df932bbe6444
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:29 +0000

    Ii shrink step disturbing 568energy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
