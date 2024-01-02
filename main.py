fb3d3643a19cfe92467e50202ce366361c45225c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:39 +0000

    chin store fall book claim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
