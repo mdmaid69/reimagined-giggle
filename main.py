0e61b5a6b46b190ea0c5689feb1c4980c2cbc66a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:25:37 +0000

    pipe mark experiment design base

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
