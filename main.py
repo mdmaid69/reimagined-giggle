655f465125fc7fb8983c7a976c161493c4cdcbfd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:27:43 +0000

    compete discussion begin emphasize fashion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
