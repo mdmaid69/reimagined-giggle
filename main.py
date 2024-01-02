fa6d0c455dcca9efb854ddb7f42a2c28dc5d69d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:52 +0000

    occasional everywhere cough cope design

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
