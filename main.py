b77bc00adbdb9c50523a64b1af5dd280735f2b63
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:55 +0000

    hope post Tt thumb sore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_exponential(x):
+        return math.exp(x)
