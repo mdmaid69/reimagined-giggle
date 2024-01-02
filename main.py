91da0b3de70f66a59ca41d0c66d4e60e20df6fb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:33 +0000

    radio comment along suggestion dig

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def greet(name):
-        print(f"Hello, {name}!")
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
