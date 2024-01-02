888851016f69bac91613e3ae6b96abc2480745b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:54:52 +0000

    curious smile either pass leave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
