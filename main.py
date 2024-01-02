419b6d1c767a6f374a046018d0a543a6f30da2bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:26:31 +0000

    beat book weak mess yesterday

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+def calculate_density(mass, volume):
+        return mass / volume
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
