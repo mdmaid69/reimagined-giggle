8e3a1deb7332965dd4e39c6681070f0f38ef7a86
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:51:10 +0000

    more turnout consult produce recent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+def greet(name):
+        print(f"Hello, {name}!")
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
