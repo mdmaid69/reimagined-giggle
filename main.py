302bb8a97d592afe97c50b406b1893fd99b210df
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:29:15 +0000

    minute error tap interrupt city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def delete_file(file_name):
+        os.remove(file_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
