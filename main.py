3a3e78a6a3f869b8b798a7833f7a8264afafbbc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:23:05 +0000

    quit factory 571enough draw blue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
