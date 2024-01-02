a2924b7330ceb10d4d570fe777607184d16d6dd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:16 +0000

    regret interesting impossible injury material

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
