76da5f6289587737b88d7a1d8406294a14c6570d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:51:33 +0000

    mark all glass failure indeed

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def is_palindrome(s):
+        return s == s[::-1]
