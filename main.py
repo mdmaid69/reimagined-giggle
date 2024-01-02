8bed6bf122906aaebb88f040a2a5074ee98f3b40
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:30 +0000

    go short scared produce crisis

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
-def count_words(sentence):
-        return len(sentence.split())
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
