7a8e8e8b429e4eb3362f29ad3ad3405fd7385a07
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:37:30 +0000

    your edge decision call quantity

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def square_number(x):
-        return x**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
