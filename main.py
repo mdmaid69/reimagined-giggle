bd8203a9a54767e09e4053bb52f8a672ca0efedd
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:44 +0000

    carry please ilovedicks contact steer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
