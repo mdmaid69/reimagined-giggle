7246d673e3317b91fe72eb9a6f4b21f742132c5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:31 +0000

    land Jj loose yes disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
