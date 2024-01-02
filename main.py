12ee6ee3bad79d4b5e306c1417b20ab0180792f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:01:08 +0000

    brief after threaten wise man

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import sys
-  def get_python_version():
-        return sys.version
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
