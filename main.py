ae7b7346b744b37525cac185fe01ab96435a9c5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:29 +0000

    drink conduct extremely makeinto regularly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_stack():
+        return collections.deque()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
