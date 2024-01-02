63658915f81b5afc1f4f565ad318c91b9d2c149e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:53 +0000

    dark area middle then radio

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def get_array_as_int(array):
+        return int(array[0])
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
