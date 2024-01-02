da6c15386be2144f2099799e57235d2f3a187c0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:14:51 +0000

    organization pure game whistle wipe

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
