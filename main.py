0cd5f53b9110ae0aaaa8123cda8328ffab0b9837
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:48:52 +0000

    brief weigh bear closed convinced

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
