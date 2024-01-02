97014cb482cba16058f15d341091d432d4f18517
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:58:19 +0000

    breakinto switch achievement nice advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
