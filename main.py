f31d5a7a28e523ef83b38782dec206ebeb3dab92
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:21 +0000

    more spin pleased leg discovery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
