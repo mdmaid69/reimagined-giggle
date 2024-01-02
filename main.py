1c5fe2850e9cba8ef79d89217e132b33177c5767
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:59 +0000

    environment flat seem certainly face

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
