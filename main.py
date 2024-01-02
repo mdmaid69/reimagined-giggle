99be5184a500c4b6974f4559d44dc7d03f871c50
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:27:47 +0000

    pity holdout wave church entrance

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
