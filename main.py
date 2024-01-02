4d593e2a012e3670b781d56e252d67c333d2c052
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:44:46 +0000

    crime describe callback rarely giveout

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
