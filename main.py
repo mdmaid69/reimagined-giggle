bdf907c4322ceb43bad2318f1a1b87e9927806dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:54:58 +0000

    Ss exit turndown picture window

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
