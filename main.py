3f0ce178ac91560a32f002e8041642fa95c661d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:00:42 +0000

    thinkover influence slowly happy accident

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
