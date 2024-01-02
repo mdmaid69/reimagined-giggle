059515567283f0ea18e441694179e9246b944043
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:51 +0000

    prepare forget raise 564encourage grateful

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
