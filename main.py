4f01c5f867b85b38b76361ca72597449e0a2992d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:08 +0000

    count whom many ask regional

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
