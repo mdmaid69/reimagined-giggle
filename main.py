ebee711be5fd2d07aceee8bd5beade5d926c8ed2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:46 +0000

    start behind previous safety suck

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+text = "Hello, world!"
+print("Characters:", len(text))
