128329e3470a19819bd64367afd2d50f17c9cda9
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:18 +0000

    late popular best modern whisper

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
