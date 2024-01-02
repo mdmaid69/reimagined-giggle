704f31b2f39c000c980033e430a78ebb7bdd43d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:14 +0000

    wave process discover spin slice

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
