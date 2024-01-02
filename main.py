3fd89db9320965e92defc7e8ce95626d83953198
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:08:18 +0000

    excellent market without future cheek

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+import math
+def calculate_hyperbolic_arc_cosine(x):
+        return math.acosh(x)
+import math
+def calculate_hyperbolic_arc_tangent(x):
+        return math.atanh(x)
