632bdb253bdedf8ce5a791c162243c5533ffd5b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:22 +0000

    rise common water add protest

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
