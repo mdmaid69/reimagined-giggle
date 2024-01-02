3e280147419d181fffb7bd95d755c66dd47f162d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:17:50 +0000

    various bank much compete terrible

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def convert_to_binary(n):
+        return bin(n)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
