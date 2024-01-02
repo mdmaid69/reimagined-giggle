6ddd5598efb52f3b35782bbf9119416ff109eccb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:21 +0000

    basic one behind imagine nearly

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-for i in range(5):
-        print(i)
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
