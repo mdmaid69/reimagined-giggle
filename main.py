0e94b7da3ca86838535ba4fbc30d52c68d8b9a79
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:24:06 +0000

    impressed instruction influence any standard

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def split_path(path):
+        return os.path.split(path)
+import os
+def remove_directory(path):
+        os.rmdir(path)
