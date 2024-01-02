af3994129ef5d0f0ca90f46d827d5ca563068e64
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:42:44 +0000

    hospital wherever dish fat between

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
