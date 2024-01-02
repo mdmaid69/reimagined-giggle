6dd9a9c1da98b6ff880203248765017b47497de3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:48:09 +0000

    shallow you form powerful through

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
