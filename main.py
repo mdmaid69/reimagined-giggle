dd4f9bf0b17211f65fd0dd41874e27361ddfb6ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:20:24 +0000

    expression dinner loose transport life

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def square_number(x):
-        return x**2
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
