0125f04a4cc780fd03bb4c6e847029b6f006d763
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:38 +0000

    beside steer minority money during

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
