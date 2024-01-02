8687ef0ead7b7a399e43d7287288c10300c78178
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:54:36 +0000

    practice measure heat shop carry

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-print(sys.version)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
