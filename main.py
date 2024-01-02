45bbfc7ea37f8b9f762df300283b3e209576ccba
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:08 +0000

    sound birth train pot subject

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
