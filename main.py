f457dc652bd828a94b6bab501d81b9d7d3155739
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:23:44 +0000

    cause than increase fast afterwards

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  def is_even(n):
-        return n % 2 == 0
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import array
+def get_array_as_repr(array):
+        return repr(array)
