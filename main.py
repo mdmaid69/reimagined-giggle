0dd164ed919a666680b59f211daa122b917fdcac
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:00 +0000

    smoke demonstrate lead coffee frequent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def find_unique_words(sentence):
+        return set(sentence.split())
