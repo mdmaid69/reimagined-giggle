b9c0eef71fd61debd8e46c9ea774b77ea10ca904
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:09 +0000

    realize trip voice size undo

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def count_elements(lst):
+        return len(lst)
+def calculate_volume(length, width, height):
+        return length * width * height
