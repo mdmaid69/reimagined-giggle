41d0cc669fefc46d88c58462e85619d1b15aeb92
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:02:37 +0000

    whistle rob bunch time tight

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
