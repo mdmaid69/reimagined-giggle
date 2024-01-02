bc95041d25ad8b2de770165e296806814d372177
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:07 +0000

    grateful every practise see must

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
