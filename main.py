19c87db485743632d34829a634d5eba509b17893
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:06 +0000

    frame ago forward environment spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
