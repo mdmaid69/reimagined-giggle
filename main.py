7a7b913e4f5f3b1ed4c13f96225de21add87605b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:52:18 +0000

    nose refuse air appear indeed

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  def remove_duplicates(lst):
-        return list(set(lst))
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import collections
+def create_priority_queue():
+        return collections.deque()
