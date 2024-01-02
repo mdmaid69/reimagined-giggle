5d4e9d2996e1887efcbd9b1b3af43ccd5110f725
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:57:42 +0000

    remark born slice practice fear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_as_list(array):
+        return list(array)
