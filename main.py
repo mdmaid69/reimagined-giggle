42a8fad2b4f9917caf1d39896e909b3c88133632
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:56 +0000

    Dd discuss justice main wealth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
