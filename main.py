557fac3a15e1d5fdd7a50ca766b6105e40eec545
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:24 +0000

    increase sweep stage we check

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-print(math.pi)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
