96aab6c1611184c18c21cacd9eaa501c247950c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:25:03 +0000

    such action picture nonsense convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
-        return x**2
-import platform
-def get_python_version():
-        return platform.python_version()
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
