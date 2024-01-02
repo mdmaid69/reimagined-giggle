a8c21218c8f4f1aa9324703d0ddc9fdaeeea07df
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:41 +0000

    interrupt bringup head slight clear

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
