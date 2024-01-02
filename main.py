3fada386d4c7d0e6c8a2856599716f8f11ec3474
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:51 +0000

    accidental sorry change this failure

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+import sys
+def exit_program():
+        sys.exit()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
