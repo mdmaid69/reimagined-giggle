b9a58f90a3f3b98f41bbc093cd4b8abf81c2b6aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:41:04 +0000

    less necessary rough getup error

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import re
-print(re.match("h.*o", "hello world"))
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
