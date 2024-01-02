c59934d5d1b76731993709042afb651dc8e313e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:54:36 +0000

    save experiment bill often share

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
