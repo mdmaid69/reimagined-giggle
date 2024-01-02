3192be4d3f9904a4c5bb327cefdb1a3e0251f45a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:13 +0000

    unable bad worth rain sort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+def calculate_circle_area(radius):
+        return math.pi * radius**2
