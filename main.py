84cada0813ccdb94eaf64a16a92ea0bac58e086d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:39 +0000

    goout square should conclusion breakup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
