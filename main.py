c531df190d75b80a1ef0e5c629c206b4c60fa827
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:04:31 +0000

    pot any Ee future order

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
