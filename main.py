19a31b3a447d97f799b56325f1aee2664b785e65
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:11 +0000

    trip bottom cost separate noise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
