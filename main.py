9e91ae6dd3250371b90e0e5c252bd49b8f1f101d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:04:04 +0000

    worse regard priority callat cheque

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def count_elements(lst):
-        return len(lst)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
