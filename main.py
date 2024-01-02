36bf60c5f70f798d5bfceab1090862dac691e7cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:53 +0000

    need match but bound inquire

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def convert_to_octal(n):
+        return oct(n)
+def calculate_density(mass, volume):
+        return mass / volume
