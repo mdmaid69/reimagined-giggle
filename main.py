82ff68e15b9575f2aa344997213a0f8eb1eb9f0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:57 +0000

    consider influence act brush music

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import numpy as np
+print(np.array([1, 2, 3]))
