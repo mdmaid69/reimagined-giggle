a0a93bf9b0822c57839916be6b309c3a9fb6cf44
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:03 +0000

    chase used thumb tax possession

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import collections
-def create_user_list():
-        return collections.UserList()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
