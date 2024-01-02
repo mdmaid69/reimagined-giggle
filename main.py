6d7145294d5f30230361cb4315c97b76f7ae7d79
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:52 +0000

    disgusting with Nn medium still

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_power(work, time):
+        return work / time
+def calculate_speed(distance, time):
+        return distance / time
