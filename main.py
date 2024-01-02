259031f9e4c4ce5f174d47ba18b8e89b2f3d372c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:27 +0000

    about cover represent suit awake

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_density(mass, volume):
+        return mass / volume
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
