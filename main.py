8820b6058ad0c0af9a159a75a9032fcf1d768824
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:40:30 +0000

    rob promise snow however short

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
