2b075995bfc551a99a871040de50dddcee1452f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:02 +0000

    proud past sink board Jj

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-for i in range(5):
-        print(i)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
