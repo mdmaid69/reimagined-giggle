10f9cd1596d6d43d00db9e3974d38e92075a18d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:32:21 +0000

    wet disappointed uponon authority although

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  def count_elements(lst):
-        return len(lst)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
