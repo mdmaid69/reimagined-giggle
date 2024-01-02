ffa459f9c3b57096b0982338b68e9908fa0a7a1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:53 +0000

    forward aim method reduction soft

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def get_file_size(file_name):
         return os.stat(file_name).st_size
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
