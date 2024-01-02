e597fcbfb6d02d2967a62b8745b73453131d218a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:10 +0000

    task border load such now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
