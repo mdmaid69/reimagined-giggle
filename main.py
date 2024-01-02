c9a6b6981daa812b18d6af6ac33d85a8fbbcfafe
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:48 +0000

    worth spare blame sensitive expression

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_pressure(force, area):
+        return force / area
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
