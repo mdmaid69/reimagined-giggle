95ea04be7d829199f9e2ff983e32f8e0f4db62f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:43:48 +0000

    order defeat criticism off succeed

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-for i in range(10): print(i)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
