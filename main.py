fba1b0d2245ad057bb50ac435f184212572ed96c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:52:42 +0000

    what eat might careful extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_power(work, time):
+        return work / time
