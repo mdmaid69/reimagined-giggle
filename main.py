def10645e22c725cc158d9c135d06df3eed71e8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:30:36 +0000

    visit cause prepare could otherwise

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
