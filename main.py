36dde0b8623bc23f39167a5a3a5ddfb595229571
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:42:10 +0000

    wall anybody flat hide damp

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
