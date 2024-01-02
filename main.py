c9d1135cd7f7e6409a8e237e463cf83254932eb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:12 +0000

    pair maximum card visit whether

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
