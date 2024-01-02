6d2f978efee2eee325b40748fade8e33051a6cff
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:58 +0000

    left situation affect angry within

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
