0188569f32bf2a207cbae2ee12353b6e96c35359
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:45:57 +0000

    born address nose takeover send

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import platform
+def get_os_info():
+        return platform.uname()
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
