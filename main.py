c1773b28b09ffacc0aa3124371fc0848511ce36d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:19:00 +0000

    pool march cheek protection difference

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
