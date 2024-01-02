da45f24cad42cf5c2863c12c98f5b08e120577f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:54 +0000

    indeed tooth like mean yourself

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_stack():
-        return collections.deque()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import array
+def clear_array(array):
+        array *= 0
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
