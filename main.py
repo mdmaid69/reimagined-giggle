7e9ca191188be25218d6a382967a8d2718ba9b5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:42 +0000

    perhaps continue together sing thanks

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
