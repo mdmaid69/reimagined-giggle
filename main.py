61db6ee2329243e951b2cc9a922ef3d97bd12e07
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:16:00 +0000

    speed sing exactly office muscle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import datetime
+def get_current_date():
+        return datetime.date.today()
