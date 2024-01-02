a7f5067d77159eb1046cdfca9a79309674f2cf5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:49 +0000

    then complete smooth survive noisy

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
