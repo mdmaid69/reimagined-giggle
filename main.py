1debe24329661fa6e1404872378361c212471b49
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:43 +0000

    public precise inform warm used

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
