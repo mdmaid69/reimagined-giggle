8ebd2ce8cb6ceafed77b6974171887a303fac66c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:58 +0000

    business terrible mother section next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def divide_numbers(x, y):
-        return x / y
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
