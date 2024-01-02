05e390391eb0e6a10d00f3d73b60eeb6e96583c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:47 +0000

    pride still Gg modern plant

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
