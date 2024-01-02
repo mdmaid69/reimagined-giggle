f67bfd6be74a86f12618ff0c78028bb95b0af469
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:14 +0000

    hour spill slow freeze tie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
