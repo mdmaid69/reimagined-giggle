13f55957b26bbcf29ab287d40d6bc4e95ffb91bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:14 +0000

    teach attempt more post mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
