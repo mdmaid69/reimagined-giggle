47725845abfce4a810910c37be0454179d914018
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:56 +0000

    seat ticket air holdon typical

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def reverse_array(array):
-        array.reverse()
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
