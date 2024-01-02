686b2c3fd11de3f594c469e7b4da04f9c6cfd3b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:34 +0000

    throughout permission bright better music

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
