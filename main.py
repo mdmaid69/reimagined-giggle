3be06cc317fb147fc448729c911d887f56094cf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:24:44 +0000

    doubt pronounce watch goaway Nn

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
-def count_elements(lst):
-        return len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
