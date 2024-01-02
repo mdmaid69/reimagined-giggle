2a87c7608dbd7bb1c0ec81bf8493d461b394bd0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:50:52 +0000

    whole scale comedown suckme concerned

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
