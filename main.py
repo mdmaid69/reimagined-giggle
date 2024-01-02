5ede05eb4f29bc10c07cbb640658722af55ab907
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:08 +0000

    police excellent admit somebody pleasure

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
