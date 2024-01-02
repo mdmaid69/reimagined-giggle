32d43319ead0ad7e3392ec67bc675834cb977f25
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:14:31 +0000

    single Ii mention telephone whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_set(array):
+        return set(array)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
