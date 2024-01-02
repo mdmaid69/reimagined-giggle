b8ad437638cb2fff033ce5864d07708c0b15e1cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:01:26 +0000

    complete narrow Pp history pool

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+print(sum(range(10)))
