7609064191ddc8b964c3bcd26dbd1cc59bf45e51
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:14 +0000

    clear behave fine promise rid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
