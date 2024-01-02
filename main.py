79da9612978e561961151f001545f20002a3f1a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:37 +0000

    later open getback thumb store

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def calculate_pressure(force, area):
+        return force / area
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
