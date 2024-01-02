2380faf0637bf8fd39617f04e909d1ddd0e15d4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:05:26 +0000

    performance team petrol able answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_inverse_hyperbolic_tangent(x):
+        return math.atanh(x)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
