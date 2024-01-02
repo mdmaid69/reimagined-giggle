9b8c39737f81b65b44537a1d95d267fc1b3b3d0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:34 +0000

    rain tragedy break sorry uniform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.ctime()
