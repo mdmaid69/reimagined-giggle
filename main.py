7b2db2dbb08ae354a77bd429384847179137d12c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:07:57 +0000

    other teach performance whatever slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def split_path(path):
+        return os.path.split(path)
+x = 10
+y = 20
+print("Sum:", x + y)
