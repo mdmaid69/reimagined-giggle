79bb7d08fe88b5d7048ee9c6a1de2eff2d85f5e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:34:16 +0000

    support cheque proof deaf then

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
