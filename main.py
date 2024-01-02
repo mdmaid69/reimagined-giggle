817d5a76359150775a6e58452a0f64fc7aaeff53
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:16 +0000

    negative during top makeup leave

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sys
-print(sys.version)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
