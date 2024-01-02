40938005a4ed4776a404a409481d6e79294553ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:48 +0000

    goback(to) fence avoid object nerve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_cosine(x):
+        return math.cos(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
