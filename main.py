75f4e4578b93f102cafc192908aeee144f8b5048
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:23:26 +0000

    challenge spread upwards ground themselves

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
