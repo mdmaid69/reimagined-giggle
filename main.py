ab9e39cfa6a3dd5a11f198c7b05152206c07fa19
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:57:53 +0000

    dear wake may beside shut

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
