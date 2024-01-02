27c6c729d256983745c8c2c4a2f6036cbb70a1e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:37:39 +0000

    music weak keep in break

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
