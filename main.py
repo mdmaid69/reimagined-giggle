a492323f7d9acefea49ec2f74401a4a0ea65c92f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:44 +0000

    everyone container dear degree giveout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
