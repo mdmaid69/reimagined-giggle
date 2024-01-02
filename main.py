0ed6f120764be3f5402916e653ee6836e7893dd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:38:48 +0000

    feature demand visit combine scale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
