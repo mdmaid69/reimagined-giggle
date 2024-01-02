093d4138310912780a44ce43dff928f553f5b55a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:34 +0000

    turninto makeup crash open be

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
