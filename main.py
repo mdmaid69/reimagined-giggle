3371cbaad6d57e5466c2776f59253f4d6cfe028a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:26 +0000

    owe week temperature improvement back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_birthtime_ns(file_name):
         return os.stat(file_name).st_birthtime_ns
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
