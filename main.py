6f0614aaef708308d70c3ff2e3a4d4318dabd56f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:22 +0000

    anything yours healthy tough sort

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
