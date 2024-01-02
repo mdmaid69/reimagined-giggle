b51f025bac47370aa6c0c3ff1830d905b9e71151
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:35:22 +0000

    secret also train music advise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
