c2922cf1fdfb1f567af56c35d5b0110072464a98
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:29 +0000

    wash scrap pain tired child

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
