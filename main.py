1039154e31d0a70154146c88df000fb9ec807b30
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:51 +0000

    refuse brave supply mad belief

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
