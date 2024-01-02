c3c3fb87926973f6da6486fc49b807956ca896f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:16:58 +0000

    every news pure owe performance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_elements(lst):
-        return len(lst)
+def is_odd(n):
+        return n % 2 != 0
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
