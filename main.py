a6f2d14b61503b8c69ffc3d356a0c66fb79c5bf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:07 +0000

    high at bringup favourite genuine

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
