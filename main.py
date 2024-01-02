a14bb39b72e7a5e92c3c6de72b8bdb30333fc008
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:20 +0000

    instead rub hear seat somehow

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
