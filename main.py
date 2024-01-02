1149d1fcb64beb4bdcc4b36d170b8483fecfeac4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:01 +0000

    answer feeling character stone be

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_even(n):
-        return n % 2 == 0
   import os
-  def get_file_blocks_allocated(file_name):
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
