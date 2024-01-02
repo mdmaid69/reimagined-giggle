3e23138f4ce8078a8cdeb25390838ddb11ae8d99
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:58 +0000

    tragedy land outside glass match

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import getpass
-def get_username():
-        return getpass.getuser()
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
