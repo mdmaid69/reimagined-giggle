2b1a5979cb4e29428287e7cd2738dc4101293ab0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:10:45 +0000

    offend sense confident design blame

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
