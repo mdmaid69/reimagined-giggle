8b17086ccf52ac5bf60abd496839fd91a6b773b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:21:37 +0000

    mixture inform touch the callat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
