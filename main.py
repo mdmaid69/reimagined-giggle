65a6796d8b7d2fc1438351cf684b125dc88465ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:00 +0000

    Oo worst lay interview fork

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import collections
-def create_counter():
-        return collections.Counter()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
