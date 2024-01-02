0d28610e369baf4575cd50f46ee79f2b4763f153
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:40:50 +0000

    threaten makeinto state rid university

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+def calculate_acceleration(speed, time):
+        return speed / time
