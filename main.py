680ba6bc7d56cc8076ca5677213c4d90734e49e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:32:50 +0000

    leg holdup comefrom speak trap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
