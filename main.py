ca6b9241dade51db1a220077ba8b12b18a52d2c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:02:51 +0000

    careless its downstairs head damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import datetime
-def get_today_date():
-        return datetime.date.today()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
