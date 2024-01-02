ab2a0e497ebaf2c3b3406a9b8473511e3f1f91b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:29 +0000

    straight likely fold produce week

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
