79e22370981511d8c7b637a1bc7515b9d1c576e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:47 +0000

    yesterday separate grateful us hang

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
