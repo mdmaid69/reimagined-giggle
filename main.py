ae49e08c8cfc2286a83c839570b12f6aa0ae2854
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:49 +0000

    already design speak row personal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
