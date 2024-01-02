602972f3c768241aa853438a1348b82bfedd6232
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:28 +0000

    job near pattern ilovedicks off

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
