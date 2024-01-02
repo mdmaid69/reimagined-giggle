d3e8f0b88d6b69ec722b8dfe10cb80aa53eb0fca
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:50 +0000

    carry congratulations brother news wonder

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
