f58a8e2ea7a65f3e7f26b1ce681acb2cbcbddde8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:47 +0000

    produce goback(to) better care lesson

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
