db96bc21e5bc6434d8cd91d2dda1c4c31fa868f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:33 +0000

    rain chat steam visit notice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
