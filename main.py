6c5e19d8313e89401ce4b3d4ed4a0165ba06d975
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:29 +0000

    regional children connect responsible whistle

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
