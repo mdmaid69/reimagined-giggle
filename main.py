905f110fb1a6201c783ee8fb1176d98e8e73ce91
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:40 +0000

    sweet risk ours ourselves under

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
