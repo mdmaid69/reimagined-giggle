f8a41f6a96cfa4c8417a63356bd16101ba002d6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:59 +0000

    free prove exhibition summer press

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print(sum(range(10)))
