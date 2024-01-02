6c40b0d54d58a3287c9b99282bb8211913b9f923
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:10 +0000

    cross or freedom feed yes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
