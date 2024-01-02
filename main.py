7a69aa0795b7aed4bba065a3f099182ed9b83ba6
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:58:38 +0000

    Bb second best catch runover

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
