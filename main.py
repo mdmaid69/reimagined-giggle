2c4fd733e878304724275a0bfd22f3c86d1c24fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:45 +0000

    soil afford fight subject flow

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-  def calculate_area_rectangle(l, w):
-        return l * w
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def divide_numbers(x, y):
+        return x / y
