cb866182985a7d06caf0f97711cd067d47ecb446
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:40 +0000

    upset concerned description clear agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def convert_to_octal(n):
+        return oct(n)
