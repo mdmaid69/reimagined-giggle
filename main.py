3b26392a998e75a5c59518c66dfb2a1c3f600f67
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:54:17 +0000

    Jj along breathe radio relax

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
