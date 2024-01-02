7e8d2e179668bca410fa024ceac2cc005d0a79ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:54 +0000

    interesting habit bury sound track

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-print("Hello, world!")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
