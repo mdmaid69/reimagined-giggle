761dea53bc7e7c7860750050ccf77b7d4a2875eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:32:22 +0000

    yet borrow backwards cause respect

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
