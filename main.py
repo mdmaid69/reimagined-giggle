10769a9924da7a23b52348cd673f6ff9532fb909
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:13:52 +0000

    open today hate advice slightly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
