8fcb02c7f9be25a633299fcf4424abba6fb4c70a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:40 +0000

    obtain use allow cheek majority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
