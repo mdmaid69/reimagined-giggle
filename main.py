3cc888d74be379b65f4b09306608d806649a4e45
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:23 +0000

    risk produce balance tool variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
