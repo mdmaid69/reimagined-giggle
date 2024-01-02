d8750560a26365425e144b9694093b3d93f38a66
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:20 +0000

    wipe develop on usual used

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
