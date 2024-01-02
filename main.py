33a974329a7993fed116c81ceb60683ca0bf3967
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:29:29 +0000

    taste factory letter season tomorrow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
