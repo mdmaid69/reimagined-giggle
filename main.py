4f25feebcbcc79676d9301928233cbbcb74cfceb
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:58 +0000

    natural extremely operation ask count

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-for i in range(5):
-        print(i)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
