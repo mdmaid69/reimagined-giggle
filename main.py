5afa7d3689b835cb4e06dfd8c7df25bd264c7ce7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:47 +0000

    collection project getout partner exercise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
