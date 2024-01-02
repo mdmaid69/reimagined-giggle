aaf6aa77719e6a79165464a0a926fd135a9823cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:41 +0000

    sort keepoff protest kind Ii

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
