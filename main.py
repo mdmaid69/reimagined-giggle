e293257e043a202598dd2f9eb2c8065a9e9758a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:46:03 +0000

    sentence wild flavour think sea

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
