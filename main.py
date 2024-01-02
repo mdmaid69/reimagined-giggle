52704565b42d8270de8c0c3c114b780fceb59d51
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:54:19 +0000

    contribute turn pay artificial lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
