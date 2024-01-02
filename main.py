ca7a5c4cd4a33afffd050a2b5cc8cceb0116d4a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:55 +0000

    loan circle tidy take unhappy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import getpass
-def get_username():
-        return getpass.getuser()
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
