96e7e6ed9c962b257632fc6c65f6be0a5b3f8b95
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:12:49 +0000

    false Vv later love however

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(a, b):
+        return a + b
