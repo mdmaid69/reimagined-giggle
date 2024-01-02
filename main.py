fbe75e4fe5a625a4773715bd95f526b657b211dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:29:32 +0000

    relax release butter autumn turnoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def is_even(n):
+        return n % 2 == 0
