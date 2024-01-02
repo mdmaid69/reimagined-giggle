fbad33b32931051f08e9c9cb9bef9cff7057dc56
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:07 +0000

    disgusting claim design delight warning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def is_palindrome(s):
+        return s == s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
