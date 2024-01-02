317a12f7c031fd2bd41a33879fd954aac4c77105
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:42:27 +0000

    later boat yet friend dance

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
