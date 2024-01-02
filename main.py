ef9a11ac3f527f3d716999d536489df05e0adcd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:29:17 +0000

    bit board stupid loan hardly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
