91c48406dcfb218c78e0cc9ce289b8b8d339dca9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:02 +0000

    getback frame plan advantage window

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
