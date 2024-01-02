552b03601bc0281feb17d96f63701bd5ba301b57
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:47 +0000

    alone on light trust sale

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def remove_duplicates(lst):
+        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
