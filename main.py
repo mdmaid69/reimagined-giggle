2a32ddfc6a36d986c6bc098b64eff0bc43fb1bed
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:57 +0000

    claim apply information already cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
