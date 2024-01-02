85a5583e55e4d86be6ef3fd1cda6c79baa106681
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:52:41 +0000

    turninto nigga kick same laugh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import datetime
+print(datetime.datetime.now())
