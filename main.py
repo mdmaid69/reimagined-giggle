f4b3548ff41e77c43af4e226f275efca568ebdc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:18:29 +0000

    contact load OKorokay pass replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
