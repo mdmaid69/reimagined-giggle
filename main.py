5060cc9cbeebe94ef550e762b05d1e45c0dcb364
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:11:08 +0000

    move close pair treat certain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
 import array
-def clear_array(array):
-        array *= 0
+def extend_array(array, iterable):
+        array.extend(iterable)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
