7290981b48ac225ec85c0e2edcc221df448a7a0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:00 +0000

    future trip welcome belief its

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_power(work, time):
+        return work / time
