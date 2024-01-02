fb00779fb96a6523b348729a3f4cef7154bf9469
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:05 +0000

    later protect meal limit visit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
+def cube_number(x):
+        return x**3
