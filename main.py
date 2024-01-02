2a1217d1f7c8e23986814d010b334979eafd3d22
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:08:45 +0000

    you no perform schedule slight

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
