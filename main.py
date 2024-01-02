7baad1eb9d613f71ba620d19d29f000a602dba48
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:36:29 +0000

    idea tall nonsense Gg shock

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
