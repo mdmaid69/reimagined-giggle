2d66439a5a526a028e462f744150b44a54305ae7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:37:13 +0000

    type sing pause breakin run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
