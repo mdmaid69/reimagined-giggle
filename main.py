d408db31a0dd2af6f652a619624e565c1f8f1691
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:39:38 +0000

    light live write hollow very

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
