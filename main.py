c7b52ebcc1f224f10f17e79de4fbd96aa1906f59
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:52:38 +0000

    moral interested bridge imagine shop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
