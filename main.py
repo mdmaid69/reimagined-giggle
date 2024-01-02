1b3a80c2e091fb2982ad3893427e585685c37524
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:04 +0000

    influence cold tread trouble sink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import sys
+def print_python_version():
+        print(sys.version)
