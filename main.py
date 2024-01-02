52c0634e5682594851038ded6c329c89cfd43411
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:52 +0000

    gas pronunciation start terrible trade

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def convert_to_binary(n):
+        return bin(n)
+def convert_to_hex(n):
+        return hex(n)
