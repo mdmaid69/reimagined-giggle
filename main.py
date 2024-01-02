7fabd671ad0bf0efec0ac7fe95280e90fa166746
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:54 +0000

    measure plan prevent together more

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
