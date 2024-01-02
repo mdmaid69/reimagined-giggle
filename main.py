40cea738d5483462a5cef8c3c4398408b1027a58
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:19 +0000

    wake belief congratulations health Tt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_density(mass, volume):
-        return mass / volume
+def find_max(lst):
+        return max(lst)
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
