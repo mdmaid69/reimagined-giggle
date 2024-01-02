87962618ce458e1f526650c98b11161bfb0693fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:51 +0000

    treat whether manage unpleasant wheel

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
