a442922bf4eb6443a4c459189d0a88112128b686
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:45 +0000

    reasonable shout naturally story strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def reverse_array(array):
-        array.reverse()
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
