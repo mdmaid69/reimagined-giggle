ad45b4d5a6f00f95384d6da0d9a502a98757e41a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:08:19 +0000

    Rr fortunately ever worry send

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
