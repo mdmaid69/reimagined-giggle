78a9dcf38e5c8fb12cb11969dd479d0cc68195c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:50:35 +0000

    command undo each certain might

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
