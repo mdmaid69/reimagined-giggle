86a1ad3c339d0aee04284dbdf4aaa026e3ee4138
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:32 +0000

    wake house lunch appeal best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
