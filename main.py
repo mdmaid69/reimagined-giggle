f961547c90706e6593ca082cd9e7eaa6b54fa009
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:39:16 +0000

    depend boat objection direct apart

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_logarithm_base_e(x):
+        return math.log(x)
