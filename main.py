023ba687098a156496d80319dd874fab10636d5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:41:14 +0000

    key circumstance next except neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def reverse_list(lst):
+        return lst[::-1]
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
