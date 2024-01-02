30efc2ff0715e256f1af932fe8d58b0e7a261a7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:52:35 +0000

    treat bath attract exam play

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
+import array
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
