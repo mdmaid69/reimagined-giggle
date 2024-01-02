e1607af85d5a1900014db92a57a26d8c5ce66889
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:27 +0000

    Bb spray brown hardly sing

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-for i in range(10): print(i)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
