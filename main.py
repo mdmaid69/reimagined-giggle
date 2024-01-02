609429792cc03cc1b468f94ef44c3e365952fa9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:15 +0000

    oil travel care when connected

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-print("Hello, world!")
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
