06846fcbd51299dd533c31406fd8f578be918e93
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:21 +0000

    across or will exploration female

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
 import random
 def flip_coin():
         return "Heads" if random.random() < 0.5 else "Tails"
