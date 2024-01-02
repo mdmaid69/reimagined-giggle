64ed945f115236603814ed682e565112d1aba406
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:35:10 +0000

    flood smile friend consult force

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(lst):
+        return min(lst)
