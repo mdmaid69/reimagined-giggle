700e1758fb4ceadc17cad47c0b01f5fdb481248b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:53 +0000

    open wide stage suppose couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def reverse_list(lst):
-        return lst[::-1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
