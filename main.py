ed91a89226e2fee8dc3b80362e46f45738057c08
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:19 +0000

    breakin rubbish regularly damp strike

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
