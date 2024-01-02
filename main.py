c71792b035df561f342c60f07b7d9667318fdb3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:31:09 +0000

    string somehow club sometimes inquire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import sys
+def exit_program():
+        sys.exit()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
