e5eb6f0da610946af69caf9b42942539f57a8037
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:57:07 +0000

    full slide knowledge recovery exit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+def clear_array(array):
+        array *= 0
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
