c0832fb53a470fc24bd49a432dfc80f7f19cd08a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:28 +0000

    who party scream division wipe

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
 n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def multiply_numbers(x, y):
+        return x * y
