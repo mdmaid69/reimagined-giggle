2c662e58c63711e6f073349827212de0e8849c46
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:49 +0000

    simple before honest miss history

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_queue():
-        return collections.deque()
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import time
+print(time.time())
