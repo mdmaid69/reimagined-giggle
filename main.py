709548ce2938eb089df32df7668c92dc79e40694
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:46:18 +0000

    cut except condition safe interview

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import sys
-def print_python_version():
-        return sys.version
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
