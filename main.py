f0e5bdfc45b72959cceebfbbf14f67eba4812c80
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:35 +0000

    whom everybody goup broken blind

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+import time
+print(time.time())
