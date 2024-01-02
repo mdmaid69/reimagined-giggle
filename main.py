fff5b7c5d54afd6f123edc65692a2795c9857a4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:06 +0000

    floor block mistake pleasant goback(to)

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
