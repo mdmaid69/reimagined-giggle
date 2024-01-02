39dbdae96526d56629e1216d312a158c9f40be4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:06 +0000

    join regular murder between famous

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
