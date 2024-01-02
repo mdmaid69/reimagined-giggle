a7265410ae3241309bd074f3a13940a8c8f91616
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:26 +0000

    close concentration regard perform sticky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
