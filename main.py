f6fecf1fc5d12e17ba39645d42df1d275adbd623
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:08:34 +0000

    welcome opponent near compare increase

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def split_path(path):
+        return os.path.split(path)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
