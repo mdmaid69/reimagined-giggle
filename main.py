c8ad250718e0c43040bff381f9107a1e61c2b1a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:28:54 +0000

    expect receive dry each cheek

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
