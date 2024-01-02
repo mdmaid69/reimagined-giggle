ceeb8055664c81de1bb23ab58d89eb95dd7078a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:13 +0000

    last meaning remain wise few

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import math
+def calculate_exponential(x):
+        return math.exp(x)
