f38b117c67234b8c06d2dfe8b28d9c39945de9f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:09:26 +0000

    contrast flat already show left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def get_array_as_list(array):
+        return list(array)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
