701b5ef4084e8a9eeab66595f11876157281d1ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:22 +0000

    gap among autumn poor plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
