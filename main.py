47cd5f6d53f0e8f8097d1779fc114a7a587c5527
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:14:29 +0000

    store occasion silent but later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-x = 10
-y = 20
-print("Sum:", x + y)
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
