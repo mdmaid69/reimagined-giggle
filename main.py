7307f774ec4cb94c10937ede860ff42857509f99
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:46 +0000

    real raw furniture uponon squash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import array
+def append_to_array(array, item):
+        array.append(item)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
