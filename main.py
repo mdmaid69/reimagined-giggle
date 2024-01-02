7d8c0ab3385327560a9f0870a34c18880f7cd990
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:07:33 +0000

    swing direct suit minimum wild

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
