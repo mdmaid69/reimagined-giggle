14f447992b5b810125c48268ec5ea414d254827f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:14:51 +0000

    delicate laugh smoke mend breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
