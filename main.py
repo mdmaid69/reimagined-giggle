0e5097841049df0cbda2607f4c0b9820c7751de3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:47 +0000

    interview present yes quickly now

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+  def reverse_list(lst):
+        return lst[::-1]
