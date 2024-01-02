8a3c742bbcf7e8eebc20ddf7f8e2ce2fa25c2afc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:14:22 +0000

    opposite common right profit getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+def calculate_power(work, time):
+        return work / time
