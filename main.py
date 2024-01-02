91bcd077823ab89778dfaea0995aa1d71f13a8bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:59:05 +0000

    need organization perform mix tap

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
