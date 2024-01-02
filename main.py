4dfc38160eac20ac7924c0644a45d4acd480e605
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:27:21 +0000

    check imagination payment often accidentally

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
