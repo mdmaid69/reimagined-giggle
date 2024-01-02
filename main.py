a72476a0cd628b886756546d5ec8a0f63eb1a484
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:03:15 +0000

    whatever nigga present task swing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
