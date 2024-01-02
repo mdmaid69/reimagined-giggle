855fc3829019620b60286adc46a2f389e855e34a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:22 +0000

    or battery reaction big sale

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
+import array
+def get_array_as_float(array):
+        return float(array[0])
