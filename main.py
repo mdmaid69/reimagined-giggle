d0956b6b70710af30e8eaf1ab25f62389ee3ab00
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:48:59 +0000

    Nn deliver charge cut image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
