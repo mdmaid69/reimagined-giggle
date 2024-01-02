81c3a187f26b2bef1e1987e371bf6d439c69aad0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:32 +0000

    I pile confuse fun trade

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
