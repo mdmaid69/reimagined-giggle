70a6ca036d9d929b57677e228869aa29912a190f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:51 +0000

    sign other deliberate we contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  def add_numbers(x, y):
+        return x + y
