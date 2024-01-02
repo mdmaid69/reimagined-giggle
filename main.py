feefa7c00bec797055fd60fb16ba1248ed34598e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:45:56 +0000

    crisis fashion door circle thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def count_elements(lst):
+        return len(lst)
+  def count_elements(lst):
+        return len(lst)
