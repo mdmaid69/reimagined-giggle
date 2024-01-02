fd452fcdd525bb7714196d5f0154992fdbc3d618
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:04:14 +0000

    brave callat pupil tonight read

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def is_odd(n):
+        return n % 2 != 0
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
