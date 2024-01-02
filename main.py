6bdc103276b788ce06ea7cad1f3ee1439249c71d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:22 +0000

    computer secret slice start flood

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
