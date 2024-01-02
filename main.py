75b517086343233a65507527f37bd7a23d2140e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:26:52 +0000

    decide natural narrow Ii secret

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-def count_words(sentence):
-        return len(sentence.split())
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
