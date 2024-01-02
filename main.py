d423d8fc5feeb61c02a338f8f34f9d86d89194eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:47 +0000

    worry fat length exhibition basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
