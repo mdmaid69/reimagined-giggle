38d98b53f4b8368f2b026847be5277fbdaba0d79
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:38 +0000

    sure watch risk upset something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
+def find_max(lst):
+        return max(lst)
