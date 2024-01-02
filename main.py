f418b82e557f43901601d5d86fd59b04c23c3bd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:16 +0000

    realize office murder march willing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
