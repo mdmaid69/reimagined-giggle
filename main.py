f63a67188ed6974c58a04060c5fdf3b58ec917cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:26 +0000

    rise illness stupid trip set

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
 def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
                 if n % i == 0:
                 return False
         return True
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
