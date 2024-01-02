161da94420e1d6b369e00a8b020707a572d8b552
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:40 +0000

    confuse beautiful almost storm favourite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+  def multiply_numbers(x, y):
+        return x * y
