6c3228f2f61b1a0c80259fe7390d821987212c3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:05:11 +0000

    repair heart congratulate public nonsense

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def convert_to_binary(n):
+        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
