62c736c673c9fa800cc7313b0f24b7229e1f7923
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:36 +0000

    stranger single extraordinary job address

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
