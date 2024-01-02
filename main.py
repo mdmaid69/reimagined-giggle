2e7111049f866d6bc438761ffa5ea81bdc2a4827
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:46:51 +0000

    reduction accommodation can power concentration

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+print("Hello, world!")
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
