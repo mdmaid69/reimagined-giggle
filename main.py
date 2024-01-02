866de78320a6c664848cbed271f248ae8aa840f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:01:05 +0000

    service young trouble soil account

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
