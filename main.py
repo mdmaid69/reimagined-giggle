c2b547123f08a471a24e9f2f6c600ff893cfc9f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:44 +0000

    mark responsibility worry ourselves room

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
