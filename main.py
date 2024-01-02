bf2544e2175404d60a16c3bbe8eefaa43ed37962
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:37:57 +0000

    fly judge progress relative breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
