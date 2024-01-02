11b004f6193e249d91defb4a7667fb5e90f2f24c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:44 +0000

    clear charge gas wonder through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
