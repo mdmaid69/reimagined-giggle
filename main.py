14cc4bb4a644e6323370b8247b74be6f586183e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:17 +0000

    certain 566end brief takeoff steer

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-def calculate_density(mass, volume):
-        return mass / volume
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
