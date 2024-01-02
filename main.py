22c3972d1972558e0d89ee948822af82c5b97e01
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:25:36 +0000

    ought sudden grand turndown spoil

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import array
+def convert_array_to_list(array):
+        return array.tolist()
