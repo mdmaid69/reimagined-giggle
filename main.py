0de08ae61c7489379315042dcaa92807a2969c7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:22 +0000

    keen wide ear prove argue

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+print(math.pi)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
