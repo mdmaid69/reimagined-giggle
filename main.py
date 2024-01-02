753422b28e625bde22891e8bb94c586412afa949
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:03:03 +0000

    business friend secret space shake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
