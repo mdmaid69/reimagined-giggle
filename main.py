81549514850bc3f5e63b44d6eb4f2fa2d9b5b0f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:05 +0000

    contribute pause steam description hit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
