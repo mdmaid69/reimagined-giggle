ff31063a731f4c5ec216def6441792e5dcac8f20
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:12 +0000

    opponent best prevent sharp upwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
