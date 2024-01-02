f311ac8f0ed366e041d8266450fd1fab230af8e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:05:18 +0000

    run used centimetre common cheque

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
