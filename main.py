6badc063dc9618299f5bda5f8e713d716f90ede9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:33 +0000

    react level substance pen equal

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def reverse_string(s):
+        return s[::-1]
