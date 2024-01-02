4616573653f09d62af8230fc86f1103c269f5ac1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:36 +0000

    cool fall trip know commit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
 import array
-def get_string_from_array(array):
-        return array.tobytes()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
