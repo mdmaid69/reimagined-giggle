17248a79c61a4a9fcce5e41587b4b8c90f459c73
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:18:58 +0000

    prefer dare steep represent OKorokay

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Characters:", len(text))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
