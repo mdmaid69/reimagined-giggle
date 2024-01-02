ca90b47e9a56efa69987fc594cd55d6ae068faff
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:24 +0000

    wish aim Bb disagree conversation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
