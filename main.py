45dd5fa48f86cf287f1ea4c1ac9f7d6267de0a19
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:34:55 +0000

    reduction thank rest now rank

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_as_repr(array):
-        return repr(array)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
