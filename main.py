fc463bef8048fdd2fa155d407b5e55e24e512665
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:29:58 +0000

    price false roll ashamed fast

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+import array
+def get_array_itemsize(array):
+        return array.itemsize
