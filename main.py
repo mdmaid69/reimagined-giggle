6ca1ce3193660c4ce71007d82af0a03095a063d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:29 +0000

    attach aim closed time different

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_acceleration(speed, time):
+        return speed / time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
