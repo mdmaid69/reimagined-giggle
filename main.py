1e73ad213b9de07a0e8b5a47913b847265d372dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:26:37 +0000

    pattern theirs declaration left bear

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import re
-print(re.match("h.*o", "hello world"))
+from collections import Counter
+print(Counter("hello world"))
+def find_min(numbers):
+        return min(numbers)
