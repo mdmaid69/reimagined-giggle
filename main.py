f14e431113034b704d9e77d3ef6ae40ced12f349
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:16:37 +0000

    spoil discovery innocent aim might

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
