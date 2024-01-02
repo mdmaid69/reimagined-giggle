b2efdfecf53d8b2de7552ccc09416543ba749e40
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:15 +0000

    balance just lookup typical trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
