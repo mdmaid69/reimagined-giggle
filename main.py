4196756651b9fb22097649832d214ad4bff02337
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:42:35 +0000

    bar maybe punish ear excuse

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+def get_current_date():
+        return datetime.date.today()
