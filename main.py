6a8cca2e144472970aa501f0bb822eacc18e880a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:52:50 +0000

    getout check themselves giveout unfair

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+import collections
+def create_stack():
+        return collections.deque()
