3bd1f48073696965485db98846ab85eeeeaaddd4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:04:30 +0000

    anxious drop disgusting hope hour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
