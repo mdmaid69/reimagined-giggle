6c8dac65ee07dee758c2120956bf2c37d413f762
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:06 +0000

    develop edge heavy Qq file

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+print("Hello, world!")
+import array
+def get_array_as_list(array):
+        return list(array)
