eaf713a8863723761d48356c588451b7ea4a2f8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:39 +0000

    valuable deliver another off achievement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_typecode(array):
+        return array.typecode
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
