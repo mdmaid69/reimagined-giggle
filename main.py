204bc41c8e912dbbc19bd3297788b27a985d91b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:35:02 +0000

    family computer direct cause blame

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_speed(distance, time):
-        return distance / time
-import time
-def get_time_since_epoch():
-        return time.time()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
