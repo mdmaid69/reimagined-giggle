b1cbba151ae584fb865e0da61d38c97b4864c2df
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:16:10 +0000

    sail left square rate afternoon

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_density(mass, volume):
+        return mass / volume
