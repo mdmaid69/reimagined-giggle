bbecb52628d974987d1d1632af83d140f2727cb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:51 +0000

    post regular reduce anywayanyhow just

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def sort_list(lst):
-        return sorted(lst)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
