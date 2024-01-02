9c8ceeddbe2168a8f8f1fce841ba9ba6d20add39
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:58:28 +0000

    search generally where swallow they

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-  def square_number(x):
-        return x**2
-for i in range(10): print(i)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
