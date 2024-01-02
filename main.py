46e1995ca910554b37eb34fc6bbff04b7b89a855
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:24 +0000

    question nor something vision giveup

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
