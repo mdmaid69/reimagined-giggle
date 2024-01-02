86d4781e94916d7eb4f1a8c4e4655c3cd24783ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:04 +0000

    near danger border comeround scheme

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import random
-def roll_die():
-        return random.randint(1, 6)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
