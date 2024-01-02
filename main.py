6cf52d428a935c27b30fa6a06a281d23c8ca648d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:51:45 +0000

    pool takeout few wide wide

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
