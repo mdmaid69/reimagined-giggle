d727b4b751e48747a0a5410459b00cbda1a902d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:58 +0000

    church process before message live

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
 import getpass
-def get_username():
-        return getpass.getuser()
+def get_password(prompt):
+        return getpass.getpass(prompt)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
