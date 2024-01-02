389097febd0320547a7f296328e806e3762ba8f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:41:35 +0000

    pile unable serve anybody cry

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
