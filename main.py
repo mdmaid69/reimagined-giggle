4f0688428b8e21c04d6e0faf65ab4e02bd0111c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:03:02 +0000

    stand sentence control answer object

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import sys
+  def get_python_version():
+        return sys.version
