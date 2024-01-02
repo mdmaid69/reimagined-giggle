7596ee3ed5dd109e3ac793e06ed3a97c32a6ea03
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:59 +0000

    price print formal wave suit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def sort_numbers(numbers):
+        return sorted(numbers)
