67304ef9327661ecc7d65bb36b0675a7a6660317
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:39:29 +0000

    excuse marry decision runinto bet

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import json
+print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
