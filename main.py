7da0d69ffb57b3b44d4ffc623e63ba5dcd0241e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:15:33 +0000

    stupid transparent near lean land

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import platform
+def get_python_version():
+        return platform.python_version()
