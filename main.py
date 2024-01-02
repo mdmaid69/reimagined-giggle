ff151a6a93bc4577f342ba82e4d16c432978f30c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:54:20 +0000

    change expression memory holdup beat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
 import collections
-def create_counter():
-        return collections.Counter()
+def create_user_dict():
+        return collections.UserDict()
