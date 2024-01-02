c631118fb55e6b9617724a3d51a7bc77e90be930
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:20 +0000

    prove later mainly talkinto measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+def calculate_power(work, time):
+        return work / time
