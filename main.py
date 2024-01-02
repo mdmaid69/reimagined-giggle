fc6d9e978d142b064c336b41eeb4c38825e72e8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:12 +0000

    spin love bar turnover catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
 import array
-def get_array_as_bool(array):
-        return bool(array)
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
