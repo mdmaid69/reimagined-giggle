3275923de5b6ffd3abc7d2d6f4b0075f47369123
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:34:08 +0000

    simple round fierce positive benefit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
