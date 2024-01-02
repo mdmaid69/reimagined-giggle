c22892c65291742d6c8771f8194ac1cd08466735
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:57 +0000

    second lay last extraordinary butter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-def reverse_list(lst):
-        return lst[::-1]
+def calculate_work(force, distance):
+        return force * distance
+import array
+def get_array_as_str(array):
+        return str(array)
