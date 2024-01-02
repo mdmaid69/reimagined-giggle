b770a34bae49c8f6f3f5a5e2faefa721dc3f1b66
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:55 +0000

    use emotion wash ready recently

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
