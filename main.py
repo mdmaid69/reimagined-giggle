e02bc8b77323fe7295a81fbf09087ac3ec30a72e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:39:29 +0000

    explain so interview rarely danger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+import os
+def remove_directory(path):
+        os.rmdir(path)
