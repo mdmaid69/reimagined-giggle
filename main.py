83933a2c6a73d54bd1d111f87d15f0bf26d2e5fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:58 +0000

    juice once circumstance anywhere Zz

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
