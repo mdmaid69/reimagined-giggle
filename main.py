c958e6f40cd39ae219b718146306dfe252c66105
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:45 +0000

    congratulations kick full information so

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  def convert_to_octal(n):
-        return oct(n)
+  def get_base_name(path):
+        return os.path.basename(path)
