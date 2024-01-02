f99b7410eb94fbce490bc8181801c4c1007f0d7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:59 +0000

    middle flood Vv thorough fit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
