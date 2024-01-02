715422c01c803a0a6444ee7f57fb4484b904edc7
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:41:15 +0000

    scene capital private alive cheap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
 import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
