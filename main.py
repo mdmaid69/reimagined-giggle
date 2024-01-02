b55d34ab4c6d9c5239fd6c2c12846736ddf3fde5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:24 +0000

    finger project beat not further

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
