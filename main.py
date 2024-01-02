4b29dd1d67c128d7843038654c5326786aa4a359
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:08 +0000

    573entertainment Vv congratulate quiet holdon

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
