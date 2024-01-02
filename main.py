24b0c2ec885d5cf5586c35c18b22b735cf8e3ed8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:16:14 +0000

    sweet gap fat disagree swimming

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
