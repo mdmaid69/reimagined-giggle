1b121662c12e2be2ab471bd3d9b9c8ff5552fd9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:08 +0000

    pronunciation peace goback(to) declaration place

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import getpass
+def get_username():
+        return getpass.getuser()
+for i in range(5):
+        print(i)
