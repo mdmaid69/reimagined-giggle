7f717875dec4d62659730cc40a225376521c3c1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:53:21 +0000

    left independent today genuine should

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-print(math.pi)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
