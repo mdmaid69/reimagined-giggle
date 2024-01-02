2f632f0d5ef4f7bb8e2d6d809f9368c574aede12
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:44:26 +0000

    hit no report wrap crash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
