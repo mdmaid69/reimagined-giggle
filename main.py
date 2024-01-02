3727561ee0609b657cd92a01091c7600ee156630
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:35:11 +0000

    entry furniture design cough wheel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
