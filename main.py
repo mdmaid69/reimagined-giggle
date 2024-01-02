5f61d61ca08390d3838d75d25ef98eb74f0c0ca0
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:08 +0000

    like suit desperate Ee advice

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-print(sum(range(10)))
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
