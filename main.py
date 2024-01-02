cbd01be0c4666c6767ac8d0e583d4d96773f049e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:21:30 +0000

    shadow clue achievement trust nervous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
