4aae7d1e2870dfa72049beec7d2ab22eaf1f3b5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:39 +0000

    band object child cheap sort

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import sklearn.datasets
 print(sklearn.datasets.load_iris())
+  def remove_duplicates(lst):
+        return list(set(lst))
