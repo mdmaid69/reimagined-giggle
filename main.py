9b8ee1522553402ac9be995ab8b5cac1051136f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:59:32 +0000

    claim because her border lie

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def exit_program():
-        sys.exit()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def count_characters(sentence):
+        return len(sentence)
+def find_max(lst):
+        return max(lst)
