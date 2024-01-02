87171840ca6bc21697d4f02eeac083bfe50f461d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:28:53 +0000

    accommodation maybe geton keepup niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
