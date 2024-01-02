430c8e888cc20e8f4092d02b9dba302be9aab4b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:36 +0000

    curious belong spin burn replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
