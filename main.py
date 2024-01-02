fd02eeae4aaac38ff34c8a45f81d07c9fec317ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:32 +0000

    doubt impression on content skin

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
