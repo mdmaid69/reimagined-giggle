3a85073cd765d08976548f544a93905c75930920
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:19:32 +0000

    test goaway meal hurt scale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import array
+def get_array_as_str(array):
+        return str(array)
