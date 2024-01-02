b01cc6611604e4f76c7f2b673d207789d6066fb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:59:07 +0000

    decide suggest flow peace eat

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def calculate_error_function(x):
+        return math.erf(x)
