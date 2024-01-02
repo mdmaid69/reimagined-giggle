c0464d6ef29b12af21b8f6a2db9dd1cc742d2603
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:27:58 +0000

    bye fail more sea boring

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
