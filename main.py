2531504c88346b5abd6ecf2f7b15eb542e1e913d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:12:08 +0000

    sudden strong with concentration excited

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-def fibonacci(n):
+  def fibonacci(n):
         return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import tensorflow as tf
+print(tf.__version__)
