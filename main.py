204d13aef41096fce8259f574c0a477a4a2b5328
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:56 +0000

    disappointed mention mouth convinced appear

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_acceleration(speed, time):
+        return speed / time
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
