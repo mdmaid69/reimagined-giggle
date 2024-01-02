fc0327de516ecd2bf642b6cfa43191205bdb4b6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:57 +0000

    meeting rubbish tone limit on

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
