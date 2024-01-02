c52541a7b102bc6d721ce6904befd89678a376aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:00:56 +0000

    bang field warn general friendly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+  def count_elements(lst):
+        return len(lst)
