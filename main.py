4b6f5f1a61fa36cdbb63551af95d46ee2974922b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:31:46 +0000

    worse before emergency past thinkover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
