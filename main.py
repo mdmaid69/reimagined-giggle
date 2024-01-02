2ec9c345bdcbbec5500bd9e75afa8f641b8f4ad9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:02 +0000

    shoot score turn apart recognize

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import array
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
