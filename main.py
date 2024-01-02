59255a724dd6cfb139992b32ef1118b1613a2663
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:18 +0000

    mother pool mess set themselves

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
