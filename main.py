41ff58a17cd51a3cd6878b754eb0fe3d00582306
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:35 +0000

    corner thankyou entry throughout direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_speed(distance, time):
-        return distance / time
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
