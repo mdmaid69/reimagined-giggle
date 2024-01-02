3d98f54af5497909a5e990c6a15246eb7f0e166b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:40:47 +0000

    female loud unite squeeze whisper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def is_odd(n):
-        return n % 2 != 0
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
