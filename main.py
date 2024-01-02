23c49750be9846cb053e574e6a3b0c80a750d2a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:22:36 +0000

    no turnaway however continue lookafter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import sys
-def print_python_version():
-        print(sys.version)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
