4df19170ffb423523760770bd2442163f39cfafa
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:48:25 +0000

    crowd fault mouth travel sky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
