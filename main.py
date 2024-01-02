0edf8072a4a4eb1f78b0a8e743065fbd39429ba5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:34 +0000

    ready ourselves treat attractive war

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
