494c5e4e71e23681c2cb80b55c0ed3c28665a313
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:12 +0000

    better adjective wave medium jump

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def is_odd(n):
-        return n % 2 != 0
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
