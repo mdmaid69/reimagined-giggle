31b5c18c2828750e5fbe0ce7b3b8cae3c459a10b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:46 +0000

    insist desperate some slice poison

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
