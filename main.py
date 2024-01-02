9c30afecfeffceaf0457685bdd24bf0d86eea029
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:00 +0000

    hurt slow collect set neck

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
