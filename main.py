55df5b41ede1433e3911546c0332fbfc0a80cf41
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:04:48 +0000

    dead Ss earn consult safe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
