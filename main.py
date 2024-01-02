17e37d0576075f2f27c288c352b4f1135c6d1da6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:45:17 +0000

    giveback treat tired city prepare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
