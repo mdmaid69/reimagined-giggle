4a3c2e36070a7843ff2c0371ec064010909a0bde
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:59:46 +0000

    miss still block beautiful national

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+print(re.match("h.*o", "hello world"))
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
