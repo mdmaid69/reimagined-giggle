c64e361a8102c6689ea1ef80a9b8692a6ca323b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:54 +0000

    whisper central illness squash hold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_odd(n):
+        return n % 2 != 0
