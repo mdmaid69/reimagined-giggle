00e96a79fde2016fe3f6b371ccfc913ced88280a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:30:01 +0000

    foreign listen pay purpose pin

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
