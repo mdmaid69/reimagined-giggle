30a37366257ef7aab60cf1f0132f350141b6b02f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:46:12 +0000

    improve everybody visit charge believe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
