3e0a4fc9376ac2279ec2402d27b07dc3be99aafd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:17:41 +0000

    moon claim exact exactly hot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-import os
-def remove_directory(path):
-        os.rmdir(path)
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
