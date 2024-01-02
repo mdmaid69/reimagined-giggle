3d5efc3d1b2e0008a6aa40a2f6b4f0292f1ba37e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:08:23 +0000

    fit knock formal evidence worry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def subtract_numbers(x, y):
-        return x - y
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
