ba9ddabf13a62091b1e41a91ac09e6d6bf878fe8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:54:36 +0000

    in far seem breakfast sleep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_list_from_array(array):
+        return array.tolist()
