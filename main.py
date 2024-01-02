e1a589e2b14568df2d0a38dce3bedb98c10bed63
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:34 +0000

    best various free reaction bar

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
