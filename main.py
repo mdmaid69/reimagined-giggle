7167171e4d5cd2a908bb830a9d617ea911b88fef
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:02 +0000

    black nervous near firm again

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
