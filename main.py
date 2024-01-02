5022fb34d4051cc6ae363bfab401d6ffeec2c869
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:24 +0000

    clear wash will us nigga

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def convert_to_hex(n):
+        return hex(n)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
