09f70533b2c4440ff30dc8041e021ceeaf63ab2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:14 +0000

    572enter eye necessary Pp takeoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_int(array):
+        return int(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
