37fbdb7bd452286f409c1fac74129e0aca890fd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:39:46 +0000

    dance skirt cause use chief

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
