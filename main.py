eb8a29cb1ca742e3c0e4bfb68a71b4970d1282d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:09:28 +0000

    lean scene collect swing window

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+import json
+print(json.dumps({"name": "John", "age": 30}))
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
