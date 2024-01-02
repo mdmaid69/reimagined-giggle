a90997a991e2c03a53f10239537851b763750391
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:08:36 +0000

    heart bend pin public talkback

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def split_path(path):
-        return os.path.split(path)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
