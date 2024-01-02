c623a80f0f950a131dc3e35674191d996bee308f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:05 +0000

    least distance brain mail necessary

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
