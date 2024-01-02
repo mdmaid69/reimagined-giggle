128a718a1303ec382cbff268cbbc3ebef8458ea1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:43 +0000

    clue sign drive read our

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_area_rectangle(l, w):
+        return l * w
