5ec7fbc7e6a8b16bbeb182ed4889564806ec6286
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:06 +0000

    trip within press surprising difference

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
 import array
-def reverse_array(array):
-        array.reverse()
+def check_if_array_contains_item(array, item):
+        return item in array
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
