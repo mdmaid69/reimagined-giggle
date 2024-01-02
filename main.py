f17f872cf8c322c72b91c6a9c03ab6215e893d7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:05:28 +0000

    tooth authority agree language deep

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
