3bab613e6f9973a9ae344729c5a06ac24b1d2f66
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:53:30 +0000

    follow play model intelligent so

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+def greet(name):
+        print(f"Hello, {name}!")
