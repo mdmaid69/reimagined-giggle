083dea58bff3c7d2c60e34efeedba3330827b64c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:30 +0000

    matter description receive repeat human

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def convert_to_json(data):
-        return json.dumps(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def exit_program():
+        sys.exit()
