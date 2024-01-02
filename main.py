aa99accb4f7540504041689e87794759d5eb2f4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:02 +0000

    cross married schedule when platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
