2b7849bfd5a05ccf86e045dedc80567c7c516251
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:34:41 +0000

    aim estimate should square sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
