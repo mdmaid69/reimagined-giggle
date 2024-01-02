d2fa960c70fbd83805537da30df2628c7d3af6e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:39 +0000

    relief properly fond proof position

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-for i in range(10): print(i)
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_repr(array):
+        return repr(array)
