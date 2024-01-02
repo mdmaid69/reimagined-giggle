6a34fc54983ce04e5d025fd5e87c6575fcc6e67e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:21:17 +0000

    wave impression dish either they

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import array
+def get_array_as_list(array):
+        return list(array)
+  def subtract_numbers(x, y):
+        return x - y
