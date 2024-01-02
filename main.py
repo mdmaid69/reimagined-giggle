51a20bee06bb6ba20d08e4ae74055273f01409ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:43 +0000

    match tend method wave pattern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_power(work, time):
-        return work / time
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def square_number(x):
+        return x**2
