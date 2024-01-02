d89e726cc6644e71f20ca75eda43ee6ce3b444c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:24 +0000

    challenge square talk more chin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
