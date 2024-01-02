341ddeed2a20a2f99cd9f136220a1579fab51fa0
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:06 +0000

    difficulty skirt cry meal Rr

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
