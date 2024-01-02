d1cebc291c46368ea476e03c629c93444fc86979
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:01:40 +0000

    unit few admit exam which

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
