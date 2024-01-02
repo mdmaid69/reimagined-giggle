7970b748f4a5efe31d356f6928b88b59fe2fece6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:23 +0000

    study formal expensive little grow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
