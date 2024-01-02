8d8d44991d5df3a76df00e87f23e8ec0ca616ded
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:32 +0000

    proud cruelty pure stretch motion

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  def convert_to_octal(n):
-        return oct(n)
+text = "Hello, world!"
+print("Characters:", len(text))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
