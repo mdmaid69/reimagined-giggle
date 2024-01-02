fc9df53f1deb0f899101167b413ddcea0dc68efa
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:22:31 +0000

    hollow thirsty respect pity company

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
