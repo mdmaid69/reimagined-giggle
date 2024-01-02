fe8a04480862f4d73d5e05d72b3ace1bbd419b6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:48:07 +0000

    weekend everyone flood anywhere become

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def sort_list(lst):
-        return sorted(lst)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
