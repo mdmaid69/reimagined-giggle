460632ae81c2bfbd7e5966dd12f3f92cc592f33b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:41 +0000

    communicate best family stare laugh

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
