d790542ebfb7d2ea047c44228942b6a7f4ff5c12
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:30:24 +0000

    organize dull often Rr false

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
