ca6e0ec1baf040a9eae12e0464d1c32cb4cdf80a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:10:29 +0000

    general sore object final bringup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_buffer_info(array):
+        return array.buffer_info()
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
