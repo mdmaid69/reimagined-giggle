ffa4b4ce248fc573d74091c08d36918996103b4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:09 +0000

    inform cope funny protest next

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
