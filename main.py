266d11069a07bcf9f0b352571b1ed45e6168f190
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:49:06 +0000

    bringback preparation affect aim hard

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-def roll_die():
-        return random.randint(1, 6)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def remove_duplicates(lst):
+        return list(set(lst))
