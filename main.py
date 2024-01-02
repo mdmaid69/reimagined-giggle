71e9158638da16fe02b2e05265a300a807905ecf
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:01:05 +0000

    switch news challenge dress goon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
