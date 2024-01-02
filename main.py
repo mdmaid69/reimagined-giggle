d16d06921aacbfa7b6b99b24c2b1ee3083819b8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:16 +0000

    sensible uncertain great reply measure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
