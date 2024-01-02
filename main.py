f9f3627b60acf686689cece6da5a85eefd699f29
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:46 +0000

    quarter season recover close finger

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
