f667194cb8a03f7850a5b667e35a6428b1b7ba8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:30:56 +0000

    giveback ahead OKorokay wave goaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
