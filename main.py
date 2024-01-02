09dca7bf3c3805524ec61b3bd6b3e4e138d4d9b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:58:36 +0000

    backwards consist otherwise difficult age

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def square_number(x):
-        return x**2
-import tensorflow as tf
-print(tf.__version__)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import array
+def get_array_as_int(array):
+        return int(array[0])
