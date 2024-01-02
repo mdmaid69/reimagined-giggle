e429528c9fa8807309604a71d28522869164288d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:56:13 +0000

    advise trip tomorrow quit money

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def count_characters(sentence):
+        return len(sentence)
