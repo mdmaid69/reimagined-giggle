ce3b69a0f4095992a6de643bfafbc4b71b6efe3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:07 +0000

    even extra like sticky explain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
