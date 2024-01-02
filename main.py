f18b53cf336a833fa96c891f17ff2931880af5f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:00:38 +0000

    increase memory march cheque about

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
