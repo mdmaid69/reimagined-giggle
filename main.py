fcef838e11c46edeccd3f4cb798e92479ed78064
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:17:45 +0000

    fasten beautiful can Kk perform

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
