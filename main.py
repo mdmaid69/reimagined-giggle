1f65a4739bb30342582ac7294a9c20bc7ec610e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:01:40 +0000

    punish talkinto miss learn know

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import sys
-  def get_python_version():
-        return sys.version
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
