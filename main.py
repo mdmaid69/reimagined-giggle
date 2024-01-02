c1796e5033735c2ac3eca3a1e297a7ce22564d31
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:17 +0000

    tone minute connect slightly black

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
