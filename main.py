f60c3b07461ec9a1494ab00146cd50f7c218a530
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:15:15 +0000

    she alone rent protest system

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
