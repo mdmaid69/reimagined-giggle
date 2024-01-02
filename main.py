1ba6b37e391223666870559ca14a975160ffd24f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:32:45 +0000

    substance natural head contents would

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
