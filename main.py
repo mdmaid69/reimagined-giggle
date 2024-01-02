f01353549e01dc2f32cd529ab2ae472d42ab1ba3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:56 +0000

    us snow shall by put

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import collections
+def create_user_list():
+        return collections.UserList()
