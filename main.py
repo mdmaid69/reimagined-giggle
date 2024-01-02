ef97aa59e239b3f70d7b7f17bb39b3e3d5fa6bce
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:59:15 +0000

    exaggerate imagine believe real left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import time
-print(time.time())
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
