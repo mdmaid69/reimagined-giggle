e37163e07261afb5a637814f9866c7a46c51df67
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:39:06 +0000

    show ilovedicks anxious pair issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def convert_to_json(data):
+        return json.dumps(data)
