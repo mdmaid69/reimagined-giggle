0526dd9d9ee36571899e38c7f02f5185fc130abb
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:58 +0000

    she apparently turnout pronounce deliver

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
