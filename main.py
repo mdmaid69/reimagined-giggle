8fae4eded1ee1d3da95b68439383f264f2b4cc8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:37:06 +0000

    opinion yes interesting front variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+def is_odd(n):
+        return n % 2 != 0
