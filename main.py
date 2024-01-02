5a5b81e28d8b6ebd01cbcc18e9881930de1b31d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:00 +0000

    woman suppose contact well conclusion

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
