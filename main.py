9f25f211ad8d0ab6f0e846878428190dccfd48cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:02 +0000

    Vv run support bringback contain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
