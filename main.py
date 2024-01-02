8c0b86263f386206b7a194c150dcecd21239e456
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:47 +0000

    office patient tip dark pen

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_floor(x):
-        return math.floor(x)
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
