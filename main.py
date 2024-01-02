faac11356a0a2fd99f99187765945f0beb229b52
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:03 +0000

    song field shock out pay

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+  import os
+  def split_path(path):
+        return os.path.split(path)
