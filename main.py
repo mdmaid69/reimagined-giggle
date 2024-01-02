c4f88de4eab789fd484e6d75916d05300edb6692
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:28 +0000

    firm such press stand usual

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
