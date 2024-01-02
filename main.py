807e8cbbbc167a9a55bef65fecc82362f5a54d36
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:47 +0000

    choose century certainly produce add

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_speed(distance, time):
+        return distance / time
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
