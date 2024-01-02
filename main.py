9db400d790ba1526b88a18802f5e4e0d2afd8cf0
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:33 +0000

    protest surface sign quiet become

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
