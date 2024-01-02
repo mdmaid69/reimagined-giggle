4b7db2c937d011b0d63b36631630381f36e5eb35
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:21 +0000

    side grow zero runinto divide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import random
+def roll_die():
+        return random.randint(1, 6)
+def find_max(lst):
+        return max(lst)
