1c5ebb1cbc1bcba792619f7e1104c4cae7717e54
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:39:44 +0000

    pressure related battery sky difference

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-def calculate_acceleration(speed, time):
-        return speed / time
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  def reverse_list(lst):
+        return lst[::-1]
