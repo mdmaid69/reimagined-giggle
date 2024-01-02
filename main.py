d2f3dc7b3bef2e832bf7458e86f4947cfc6e0d82
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:19:33 +0000

    reduce criticism thing hear oil

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
