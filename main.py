39db89a5199fd9da12934e8afa705e5d6843ce21
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:00:00 +0000

    179bite freeze zero art thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def create_stack():
-        return collections.deque()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
