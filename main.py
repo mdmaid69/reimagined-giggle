af3172a6ba3b81ecc6b96397caeaeadf1b4b61a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:27:37 +0000

    in mind increase post operate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
