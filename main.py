33a10dd86f76fc470871974160b227bf86cc4576
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:53 +0000

    trick accept blow work smooth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
