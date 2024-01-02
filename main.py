7d4769fe60f5dac43236ae610154522eaf316cf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:19 +0000

    hollow confidence keen yet bottom

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
