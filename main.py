7dcaa34ff67def79bfeed24efc93c34530b25742
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:47:50 +0000

    sentence laugh rise protect record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
