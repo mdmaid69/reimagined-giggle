734ad8ba874c2471f8c4efd3746e2157cb507d02
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:16 +0000

    call commit case tap this

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_stack():
+        return collections.deque()
