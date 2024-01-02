8e10cd7bdb6438326905adeec6c995f1e342ed8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:40 +0000

    godown yes dark load previous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import sys
+def print_python_version():
+        return sys.version
