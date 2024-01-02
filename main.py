195d01bafd422a8e771c14132337c09029e90995
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:27:21 +0000

    among generally tradition couple lift

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
