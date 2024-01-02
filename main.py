e94472441e8e6bf93afdb05141c46c3b40633d38
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:07 +0000

    male run shop chase film

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
   def count_elements(lst):
         return len(lst)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
