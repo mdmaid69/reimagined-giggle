35c8dd52fc3cacb1eb098fdc9b993c0e766daa50
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:12:52 +0000

    yet high Rr praise platform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+def calculate_volume(length, width, height):
+        return length * width * height
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
