2abe0a59b1727f6a4d0c3739ab70d84758ed1768
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:32:20 +0000

    drawing water pleasure pick correction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_base_name(path):
+        return os.path.basename(path)
