b80616033069148ebcb0548651e5ea0911ec670d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:42:15 +0000

    afraid maintain very grade fit

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
 import array
-def get_array_typecode(array):
-        return array.typecode
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
