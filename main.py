f4c4fd138f8609c3330e7f95bc6043a63b6d3562
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:44:37 +0000

    natural bringdown target careful easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
