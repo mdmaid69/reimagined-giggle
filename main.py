b7a5185cdbde8400540dd6c89cf7022ba524485b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:46 +0000

    concentrate afraid beside record safe

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
