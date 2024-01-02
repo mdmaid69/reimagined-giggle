e3f06aec1c339168b3a120142d7dff3572ffe9b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:22:34 +0000

    control by through bottom bet

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
 import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+def convert_array_to_unicode(array):
+        return array.tounicode()
