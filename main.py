479f7a29bedbb5feff3a09870651e8c8ddddbe76
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:05:18 +0000

    flat their exit consult front

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
