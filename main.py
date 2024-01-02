03f97898d141ae9bd1e2684e33b45120d0ec8af4
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:43:48 +0000

    goup disagree wise at range

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
