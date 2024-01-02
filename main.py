887f6e08c46574233eb8ae001884406397f8f616
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:48 +0000

    anybody 179bite slice breakinto apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
