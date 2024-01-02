69a04d4fc3311d8b5a37fc836f4e1457fff4faec
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:15 +0000

    defeat performance passage properly church

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
