d28712b19b0d4a4a44738de6603c799fec4ff9eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:40 +0000

    especially married possible count chief

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+import time
+def get_time_since_epoch():
+        return time.time()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
