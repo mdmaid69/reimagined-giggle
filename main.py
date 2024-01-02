332dc277558a069415b1293919b081343165bdb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:20:53 +0000

    impression Jj difficult performance popular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import sys
+def exit_program():
+        sys.exit()
+  def convert_to_binary(n):
+        return bin(n)
