567a27532e18537ee55b3cb3b17dab5e3a37aa8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:19:34 +0000

    callup scheme turnup lower eat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def multiply_numbers(x, y):
-        return x * y
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
