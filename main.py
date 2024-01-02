6749ad99345db2bd30e28f93a7879b5008b772a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:49 +0000

    system odd father dream common

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import sys
+def exit_program():
+        sys.exit()
