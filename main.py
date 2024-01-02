04d36aba6305d4bbb91a1b1ee26702e9e6903626
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:47 +0000

    in home straight proper promise

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-def divide_numbers(x, y):
-        return x / y
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import array
+def get_array_as_float(array):
+        return float(array[0])
