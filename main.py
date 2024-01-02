d0ade389c8cd4867943ab1d385475bf61d2de132
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:58:51 +0000

    belief stare service enthusiastic present

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
