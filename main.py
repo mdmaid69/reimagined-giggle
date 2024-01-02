654a354c0fe7b2eb62adef7e86d1756fa72b5c04
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:35 +0000

    free matter scrap age brush

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
