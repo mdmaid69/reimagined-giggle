0a2782136c340da6291823d011711ce5c3fdaba1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:27:04 +0000

    extra return travel I pencil

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-  def reverse_list(lst):
-        return lst[::-1]
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
