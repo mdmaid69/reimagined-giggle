8cdda71352d9bad946b64a5aeb537cea321a5372
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:48:27 +0000

    impressed belong visit cheap bush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
