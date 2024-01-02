bf70f77f40b0034373a1d49de3d56371f7a0bf56
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:17 +0000

    less drive contents wash step

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
