d933e098eb3c7c5b019044ab4e615acca9c970e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:37:10 +0000

    rare tense like history lean

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
