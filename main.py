04c83a6225b86adecdb3d5225eeb379e94ec3758
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:00:56 +0000

    free bath tonight acceptable deaf

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
