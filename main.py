f8b2409f5f8443d67905cbfa7f84516479db686c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:12:04 +0000

    bringup probably Ff stay maybe

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
