10753ff1b793385310d18baeabbb148d5f85876e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:32:24 +0000

    reach once wish fail child

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
