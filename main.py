bbf4a20ba998bfe9fb5e4e138b9bdefba68f61dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:45 +0000

    ability over sea heat example

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def calculate_area_circle(r):
+        return 3.14 * r**2
