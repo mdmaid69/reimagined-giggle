da35054ca0a8ecb39f1cb4683008639ee45d08ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:38 +0000

    stone expression left opposite through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_current_directory():
-        return os.getcwd()
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
