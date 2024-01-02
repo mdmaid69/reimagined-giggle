1c1463465d624a4875ca1108c7da4b181a8bb278
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:48:40 +0000

    side short whistle weather hers

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import array
+def reverse_array(array):
+        array.reverse()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
