0f10978d26e02c06aaeec6bf3dafc6516e634b5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:31 +0000

    declaration connected under other patience

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
