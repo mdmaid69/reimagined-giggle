fb772d490b7c2dab60c3189fd340a5457cd12661
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:45 +0000

    only explanation defeat anybody unite

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def is_palindrome(s):
+        return s == s[::-1]
