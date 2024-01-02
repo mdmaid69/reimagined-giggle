1688d338c91732f98f151987e67ae022a7eef95c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:02 +0000

    tidy neck your fair strength

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def square_number(x):
+        return x**2
