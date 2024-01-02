d6f57c0b3e98766cc152d4c57350de701b70bf2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:04:14 +0000

    wide leg criticism rush yourself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
