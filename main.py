7f62007f38b3ca06b6404be2cca121abd5e5975f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:57 +0000

    thinkthrough impression quality on balance

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_area_rectangle(l, w):
+        return l * w
