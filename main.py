ca6a441a70348ba9d200b754a38933b66db68724
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:12:18 +0000

    corner knock suck none whether

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_area_rectangle(l, w):
-        return l * w
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
