96b56f930890df3d0cbb21a9e34cb4241705557d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:55 +0000

    correction equipment familiar cheek punish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def find_unique_words(sentence):
+        return set(sentence.split())
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
