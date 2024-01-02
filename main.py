fc263662c2ff45526c3c956f05db315157156bf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:45 +0000

    distant coat contrast rent bag

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
