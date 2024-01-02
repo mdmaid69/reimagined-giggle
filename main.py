8c86fb2b82a0a7577e2c5c9c7d193da8b1b5fe83
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:19 +0000

    blind family low swallow payment

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
