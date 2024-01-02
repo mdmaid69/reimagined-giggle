d99f9460fbf5f6522676d38adc6978c725bf8a4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:20 +0000

    description shadow like motion lie

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-for i in range(5):
-        print(i)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def cube_number(x):
+        return x**3
