c860bbf42b8fe565d6bdc4d7ac9ad7c5eda54968
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:19:35 +0000

    contact spare keepfrom city scared

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_mode(file_name):
+  def get_file_permissions(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
