d2a6fa83c739b49af546a14046ebd14cdd435370
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:31:59 +0000

    female luck praise wish wake

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
