a60a590b7e04c9ceea64e3f3b0ff54f6f8b73d90
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:44 +0000

    boat invitation grow port feeling

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
