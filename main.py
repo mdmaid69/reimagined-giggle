c5a71a59ae73cf8cb2d562f4e80d15b1eedf476d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:35:03 +0000

    wild explain power light dare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def change_working_directory(path):
+        os.chdir(path)
