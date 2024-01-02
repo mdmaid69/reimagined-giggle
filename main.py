9542729e83abbd22d99a962f44e1f39ee28e02f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:44:32 +0000

    opinion best rest race square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
