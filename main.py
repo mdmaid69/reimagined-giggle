be90d2240e201f94f36277842e4f5b9ed295b0bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:03:47 +0000

    there settle punish offend pain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
