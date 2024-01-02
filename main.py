0c6b23e635182c744623cf9fa096e0b03586bcc1
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:32 +0000

    much change protest switch too

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+print(re.match("h.*o", "hello world"))
