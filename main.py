01d9734653dbc1e17275dd9a528a1540a4057a50
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:26:12 +0000

    contribute perfect nonsense squeeze bet

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_max(lst):
+        return max(lst)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
