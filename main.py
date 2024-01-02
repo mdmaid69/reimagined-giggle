421287a4ab57ed8d1b31f12bc7c4d2905de07757
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:27 +0000

    big us exact rich care

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
