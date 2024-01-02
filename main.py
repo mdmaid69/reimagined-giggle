72720fd1afccb291600de14298a31ecf8ba871d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:59 +0000

    friend 572enter underneath break correct

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
