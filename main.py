e3763e8c7dbd061e2d498082a73e506bb21b34cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:47 +0000

    talkinto traffic slice decide this

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.ctime()
+import array
+def get_array_as_float(array):
+        return float(array[0])
