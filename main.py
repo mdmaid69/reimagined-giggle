8e8177c21a3b2e41a5448e98196e9390cb01e5a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:01 +0000

    surprise study when doubt collection

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
