0730b7140f4eca3db7fbd334e79c61bcfafe300e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:32:57 +0000

    put dress detail probably variety

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_force(mass, acceleration):
+        return mass * acceleration
