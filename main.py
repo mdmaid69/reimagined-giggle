29983c4f9396b997c19791127d6704aa39fa41ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:55:22 +0000

    raw culture part comeup story

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
