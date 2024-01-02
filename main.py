daada0ef2be841ba3c680f97f70da1334f443a02
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:40 +0000

    exact length hurry pour whatever

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
