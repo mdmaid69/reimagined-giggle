f38c17c7115df72c6c72d753c325c4f86dad1ea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:06 +0000

    order scream tool related have

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def convert_to_octal(n):
+        return oct(n)
