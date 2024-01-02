3b83c78e9fe82e54dcc7c9f13cd71b8341e87d06
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:24 +0000

    basic light yesterday please stare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
