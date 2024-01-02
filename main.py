41e11c3be0707c55aa43f22aca8dda6e3bef2b74
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:31 +0000

    certain rule sure tradition cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_string(s):
-        return s[::-1]
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
