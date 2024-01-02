e26abb4f1a319331862d9a4abd748409d177b935
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:03:04 +0000

    basic measure left charge surprise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
