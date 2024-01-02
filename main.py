dbfaec51e906e3194a507034302315599e39787f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:54:48 +0000

    accidental anxious mess make steep

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
+  def convert_to_binary(n):
+        return bin(n)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
