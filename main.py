a71f620ed2dedeccfe9b5a474e298e90e54b1c54
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:59:47 +0000

    geton substance light raw habit

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
