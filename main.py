43bccea7785f7824062b5f238d590f8c8e5c7b27
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:52 +0000

    him university bet autumn emergency

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def convert_to_hex(n):
-        return hex(n)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
