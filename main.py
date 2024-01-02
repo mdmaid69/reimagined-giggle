e102af5f5b9e596c0b9d344ba605a4f69b34ccf4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:50 +0000

    wing unfortunate brief need train

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def convert_to_binary(n):
+        return bin(n)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
