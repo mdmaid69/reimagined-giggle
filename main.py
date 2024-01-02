77a1389cfe37227c5164d8f860d59af00c0de21a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:34 +0000

    quality secret do high warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import platform
-def get_python_version():
-        return platform.python_version()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
