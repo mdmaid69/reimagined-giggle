d503c97ed944d7389df74e614a18bfd261055146
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:00:51 +0000

    tend replace wire bin feed

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def convert_to_octal(n):
+        return oct(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
