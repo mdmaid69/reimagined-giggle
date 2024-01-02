b6aacc8ddb8cabe23fc6a229f1c53b8cda0f0262
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:11:35 +0000

    properly breakinto invitation turnon shoot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+import os
+def change_working_directory(path):
+        os.chdir(path)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
