c7b22777ca10cbb8e2dd70326b4beda68e193ddf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:58:38 +0000

    late only demand tooth thorough

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+  def square_number(x):
+        return x**2
