eb372fe52e9cee432e6ad3c454b10695e798306d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:50:39 +0000

    generally clear greeting complaint surprised

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
