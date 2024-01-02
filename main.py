6192ef31b783d334fd36fee48c884bf2c34b6128
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:07 +0000

    would remove worth sensitive game

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
