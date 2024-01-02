ce1dfba49026db14bf5602ca71c41b39b8b8aacd
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:58 +0000

    now protection tired embarrassing sound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
