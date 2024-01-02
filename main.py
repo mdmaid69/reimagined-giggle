7a1d7d8e14056b09a4c2c930a7f027d6e7577299
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:22:16 +0000

    scrap tight makeup experiment view

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print([x**2 for x in range(10)])
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
