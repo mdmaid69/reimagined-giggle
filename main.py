80d149d54336c60c53cab6d6c9b1893d461488db
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:33 +0000

    uponon closed geton underneath reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
