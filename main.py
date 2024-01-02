ed73d2248b86130c32c18d7603371457d64b1a2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:10:24 +0000

    yesterday definite level base your

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
