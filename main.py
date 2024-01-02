82ecee0e7f02fd67440e21d941daa5dcd7984737
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:45 +0000

    use gradually tap patience history

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
