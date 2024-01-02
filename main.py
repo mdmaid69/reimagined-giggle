d17b42c43d0d724ce675b0bb2d495ce1a9968249
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:00:33 +0000

    battery blow open share holiday

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
