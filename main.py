9c24f42cfe8049a34a2b9da2f846c3d2333a697f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:59 +0000

    turn listenfor black art brother

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
