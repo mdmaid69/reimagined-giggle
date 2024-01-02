60d9e5e1e9256d4beafe4ea01d1a6f482ec2b76f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:33:43 +0000

    acceptable mad before make female

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
