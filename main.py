5c7ba35e3264796f17ceadd28e7bb21bb572b4a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:49 +0000

    straight wrong great band until

--- main.py
+++ main.py
@@ -1,7 +1,12 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
