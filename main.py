fe1c9892a66bdc6042415d6f32454950eaef6d45
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:38:23 +0000

    basically 566end home guide wire

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
