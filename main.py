15ef89952de4df7272c7966cca33b22ba404a3a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:33:25 +0000

    goon though pretty degree intend

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import sys
-def exit_program():
-        sys.exit()
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
