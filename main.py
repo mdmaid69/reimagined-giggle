305687937ed60c11b6684a79530c7d2c00044dc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:52 +0000

    several wonder lucky wheel delicate

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
