721a83ff00a60e70cc276f1850879558ea8cff2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:08:34 +0000

    good under properly contain excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
