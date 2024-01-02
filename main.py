8ac60bc87c99f1ec9233efbbc3cc5a8bd6beeacd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:42 +0000

    rule light star control violence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import tensorflow as tf
+print(tf.__version__)
