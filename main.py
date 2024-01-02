ec43323892f73c43afeb6be4be641727f96d0ec4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:04:47 +0000

    behave scale turnaway trap choice

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 def calculate_area_rectangle(l, w):
         return l * w
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
