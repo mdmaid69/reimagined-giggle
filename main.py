9fcfe8b0f17c94d910b86b36c757b60f2830ba62
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:55 +0000

    wing hurt practise house except

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
