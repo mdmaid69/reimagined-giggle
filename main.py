078164e71baf0a92a3e1d74918e4c90c3f2312f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:52 +0000

    yours steam blue guard often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
