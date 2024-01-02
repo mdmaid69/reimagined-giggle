f4f7d9ddefd922f57a06bc821fa337388f275a27
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:09 +0000

    perform simple suggestion bit mystery

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+def convert_to_binary(n):
+        return bin(n)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
