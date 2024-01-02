8224055fe7bd71adafd4f465206718d069aee8bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:38:05 +0000

    ought pass break by add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
