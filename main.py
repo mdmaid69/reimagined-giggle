29adf9a1c09971cf2bd4be5bd712434018d12f09
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:34:17 +0000

    hungry progress getback freedom favour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+def convert_to_binary(n):
+        return bin(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
