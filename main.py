89fbe1edadedf024a4725fc5bca92beeadceda15
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:53 +0000

    collect part defeat morning terrible

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   def is_even(n):
         return n % 2 == 0
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
