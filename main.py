6d63775ef8622787547342909f93eba7f3c6b1d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:25 +0000

    listenfor poison combination reduction most

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  def subtract_numbers(x, y):
+        return x - y
