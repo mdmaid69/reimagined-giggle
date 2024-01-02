0c2e6f1a5d73bacaa056df8f8881a354e9415402
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:01 +0000

    arrangement bye ring quarter claim

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
