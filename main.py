3edae489fdc02376fc284507c9c7df28d379e985
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:14 +0000

    knock vote image dream workupto

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def get_array_as_set(array):
+        return set(array)
