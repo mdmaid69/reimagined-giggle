86e58fda368ec4f50d5b1840a48d6f2321044f05
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:23:02 +0000

    translate there will hit hospital

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
