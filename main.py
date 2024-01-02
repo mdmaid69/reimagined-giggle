5f0bac7ac1a391a7def2e7f0628211f031fc26fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:56:50 +0000

    keepaway gift half people wait

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
