535d5c9c521ef4eaf1c9c194fdfc47f50394e598
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:48:13 +0000

    motion suppose laugh wrong run

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
