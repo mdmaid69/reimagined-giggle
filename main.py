89ae5c8cd9b66a2610ebd0b709d81e533876a509
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:08:34 +0000

    embarrassing wheel brush under book

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import sys
+def exit_program():
+        sys.exit()
