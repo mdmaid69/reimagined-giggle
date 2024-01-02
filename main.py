95e032bb4db94e57e1cceb7167e72dd3238c23f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:36 +0000

    but task bunch group care

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
