680246b20528422898825f8c475a55ca0aa8330f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:58:47 +0000

    crowded pull steep chief begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def remove_from_array(array, item):
+        array.remove(item)
