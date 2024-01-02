903de2372b79a32af1a5eb392b2ee18b933888cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:13 +0000

    actual address profit react criticize

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def square_number(x):
-        return x**2
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
