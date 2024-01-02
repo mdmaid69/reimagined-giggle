dd977a6a740045716ef24fa1e2f02c0dc5af24ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:54:48 +0000

    reserve convince rank offer shine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import sys
+def exit_program():
+        sys.exit()
