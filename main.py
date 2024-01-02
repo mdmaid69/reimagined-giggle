43ffacad8b9b5b3e9c67a5c130ad41079dc6fd7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:42 +0000

    threaten touch concentrate capital unpleasant

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
