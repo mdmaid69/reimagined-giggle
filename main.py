956a9eec0604fe3ae917486c7877a06ff700b417
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:48 +0000

    pocket interview pure fly sensible

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  def calculate_area_rectangle(l, w):
+        return l * w
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
