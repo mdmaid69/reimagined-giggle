849e99dab461347b890a91eb43aa50717ea12759
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:21:56 +0000

    eager trust manner compare dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
