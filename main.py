934e3fedfe6568059f43e1c6bf210ef97ba47846
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:43 +0000

    bad praise movement trust stiff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+  def reverse_list(lst):
+        return lst[::-1]
