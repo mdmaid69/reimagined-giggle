0bcc8faedf231726b4352a5f72c6e20337a5d79f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:49:19 +0000

    answer rent survive bunch address

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-print([x**2 for x in range(10)])
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
