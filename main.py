bcc8f7faf470e304806d2208478906c0d31e8d2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:25 +0000

    risk rule serious mine issue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
