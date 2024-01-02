ceb701200dcece28c80ec08c7f9e5713112533f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:25:27 +0000

    word behind behind dress travel

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
