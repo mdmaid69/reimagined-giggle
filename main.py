3a20f79385a1fd68016c76830e93f96612fc394e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:09:35 +0000

    chance emotion question or turndown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
