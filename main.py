eb750ee48d27d8130122ef66d3e433e3a8c758a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:12 +0000

    little accept runinto like law

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def calculate_area_rectangle(l, w):
-        return l * w
+def convert_to_octal(n):
+        return oct(n)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
