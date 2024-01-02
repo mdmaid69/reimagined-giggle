e9bb828308beb0eb49633b12020cb1ac7cdcbb35
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:43:20 +0000

    common welcome rub onto Cc

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
