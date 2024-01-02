d913f09b96d503ef2c85d32a0c8938fc376ad451
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:09 +0000

    trouble depend limit open characteristic

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def reverse_array(array):
-        array.reverse()
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
