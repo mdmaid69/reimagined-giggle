c99481f3f8abb82bf0931831d120b27b6857d346
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:34 +0000

    picture stiff battery safe coat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+def convert_to_octal(n):
+        return oct(n)
