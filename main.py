10d803a7a0af4b09071e2e5bdecc863e6e815d8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:14:22 +0000

    Ff tight Oo corner fit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_os_info():
-        return platform.uname()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
