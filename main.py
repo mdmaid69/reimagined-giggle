dfcf26b4df9dcbd5fd3b11867065acf73658d509
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:04:42 +0000

    common defeat dream splash report

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
