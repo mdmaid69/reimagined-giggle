b695d6bab5b6b94ff6717f4dbe1cb2016293664d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:37:09 +0000

    turn neither develop right Ll

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
