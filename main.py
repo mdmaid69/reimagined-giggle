a0a81eb5be004b83abaea71f98f60522b45cb5b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:35 +0000

    whisper goon level undo not

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def subtract_numbers(x, y):
-        return x - y
+def divide_numbers(x, y):
+        return x / y
+import datetime
+def get_today_date():
+        return datetime.date.today()
