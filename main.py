578c8f1e16d2b549687a9b4fea56c387d70b78e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:41:07 +0000

    opposite to speech show ordinary

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
