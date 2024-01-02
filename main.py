bda431b188a8efb92d136970c07ecc764130aacf
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:12 +0000

    side basically opportunity uniform custom

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-def calculate_volume(length, width, height):
-        return length * width * height
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
