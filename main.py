bd31534038bf82d21e5b63fd284ee8b46e1fb58f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:53 +0000

    birth separate load temperature pain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
