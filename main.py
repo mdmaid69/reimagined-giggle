6ecbc269b54c9a6167a454e968c15ec66bb43d87
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:18:49 +0000

    Vv turn doubt tax anxious

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
