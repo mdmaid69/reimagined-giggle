026fbc66f9c0ce7de3d4c74431911ab1ceaae9be
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:57:41 +0000

    rush damp narrow begin temper

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def count_words(sentence):
-        return len(sentence.split())
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
