2ac399cbf36236f89878b9d83954fd6b41ce0eab
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:08:35 +0000

    relax order could relax handle

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
