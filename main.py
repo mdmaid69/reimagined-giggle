6ca21b444241680ec3ce4c4c99eb0062a1ed3bbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:07:24 +0000

    pronounce comein key round ignore

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
 import array
-def get_array_as_set(array):
-        return set(array)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
