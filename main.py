41dad0c7f85217ec7d7ea12fe76b0e942c08ec05
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:05 +0000

    interview sort gate obvious stamp

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
