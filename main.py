1faa0a06c22243be0e4d38923221d7d9ae1ddb44
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:55 +0000

    information circumstance bath respect already

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import platform
-def get_os_info():
-        return platform.uname()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def clear_array(array):
+        array *= 0
