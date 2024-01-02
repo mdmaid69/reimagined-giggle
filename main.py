65ff9db24fe1b43016c1101ee74e57ade5bb392c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:50 +0000

    reject waste apologizeapologise connected beat

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_volume(length, width, height):
+        return length * width * height
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
