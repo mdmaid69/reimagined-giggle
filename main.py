c4a4a6a2676b37981e18bc2b1860303a84670eff
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:24:22 +0000

    shall freedom fact bet like

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def multiply_numbers(x, y):
+        return x * y
