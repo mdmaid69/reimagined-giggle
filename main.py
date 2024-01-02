47c873deaece35749ea2ee08f6bae93225b8bcf2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:16 +0000

    excuse hide breakthrough sight adjective

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import os
+def remove_directory(path):
+        os.rmdir(path)
