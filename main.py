94dba99f1d0cee312ab1c0ddece484abd605c68a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:27:53 +0000

    target frighten platform opportunity special

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  def reverse_list(lst):
-        return lst[::-1]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
