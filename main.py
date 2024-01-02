701cd30c231caa9d8a09a0d345799a5a8564f16c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:59 +0000

    speech whom drink call weak

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
