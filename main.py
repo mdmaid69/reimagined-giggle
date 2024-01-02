0af1022d4525bc29c90a29d07c6e9faf4e68a0d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:46:46 +0000

    little hospital shadow big trip

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
