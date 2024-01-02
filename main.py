acf378b3afad881d6b5222c10919dd88dae06f3a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:39:14 +0000

    powerful to out variety sticky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_density(mass, volume):
-        return mass / volume
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
