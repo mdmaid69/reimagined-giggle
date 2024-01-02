acd8e1034670a73bf5b3f8f5355583da40933765
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:38 +0000

    smart soft by off live

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-print(math.pi)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+import tensorflow as tf
+print(tf.__version__)
