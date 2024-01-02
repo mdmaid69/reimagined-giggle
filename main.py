ad9e041b43802acfb8a9131fa87f76b7a63ec402
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:52:41 +0000

    variety moon ours your inform

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
