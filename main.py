cd4aa114247336e7ad7bc97a07d128f3b44b930a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:13 +0000

    guess neither speed visit but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
