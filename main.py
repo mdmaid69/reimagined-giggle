17d13a898db906c163f9c5868c2d7a3ea351f8ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:17 +0000

    second sore remember operation turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import array
+def clear_array(array):
+        array *= 0
