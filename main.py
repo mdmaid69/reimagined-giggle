141c73a2e297382afc1cf67ed4616a4cbd913c62
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:57:15 +0000

    understand right mainly operation road

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
