6a2bd941666bcc7d051b970dd5a316e6bf4ff076
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:50 +0000

    obtain bit maintain entry hour

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+print([x**2 for x in range(10)])
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
