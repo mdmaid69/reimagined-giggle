b3bc4a47bc1de80735a2d6823d065c85cbc41064
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:35:11 +0000

    fortunate forgive workout paper tape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_unique_words(sentence):
-        return set(sentence.split())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def count_characters(sentence):
+        return len(sentence)
