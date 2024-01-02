470a629e4b1f52969ca13df3bb610bced172bec9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:53:26 +0000

    Qq introduce Itself deliberate star

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
