4cae6d8b28970c6c223eebad0cc134b50bf5bfc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:02 +0000

    empty force ourselves public right

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
 import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
