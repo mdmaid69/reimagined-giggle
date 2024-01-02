32ec465261d5d416f690e396008eb0c9b48a2a8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:15 +0000

    tidy repair unlucky tax strength

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+import sys
+print(sys.version)
