51a089b2dea7838cc5208c62c7a3bd0601bccc55
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:18 +0000

    fasten thumb platform leave possibly

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_current_directory():
+        return os.getcwd()
