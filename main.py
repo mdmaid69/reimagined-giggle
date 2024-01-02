918873cc7dc27c765f3617c8e6329e10fefdb45e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:26:16 +0000

    everywhere language lie display holiday

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+  import os
+  def split_path(path):
+        return os.path.split(path)
