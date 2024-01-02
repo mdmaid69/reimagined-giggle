012b159229134e093bc009959f5193143d480389
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:43 +0000

    consist freedom bottom cost stay

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
