051e4b7f5f14f8ba24880c4943fd75b882b0ff6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:55 +0000

    art feel shadow anywayanyhow trip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(10): print(i)
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
