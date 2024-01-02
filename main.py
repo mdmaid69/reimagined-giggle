e557d57468e9f2f494a93d539a82ab88b47cb7be
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:51:09 +0000

    only bound meanwhile listenfor announce

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
