ec2a510b03904bd12499c8e69b263cbe0b2dfead
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:04:18 +0000

    opportunity notice middle can vanish

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import time
+print(time.time())
