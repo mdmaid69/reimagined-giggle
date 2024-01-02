29de826b116991a95f70e01d93d5880fa7146541
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:20:04 +0000

    Cc always turnout waste apologizeapologise

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def count_elements(lst):
-        return len(lst)
+def count_characters(sentence):
+        return len(sentence)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
