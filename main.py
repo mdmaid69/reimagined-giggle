6fb91d872230f42968c0355c0c4b66734ab2b58c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:00:55 +0000

    band name Uu whether public

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
