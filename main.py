5c5c3f84159d8cb5c73ef07a75d1e3127c7d4297
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:33:39 +0000

    sing free shout nowhere gather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
