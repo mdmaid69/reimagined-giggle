cf84791e7762803be56d7e2831eb5564bf025e3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:05:26 +0000

    perhaps breakfast tape plate phone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def convert_to_hex(n):
-        return hex(n)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+def greet(name):
+        print(f"Hello, {name}!")
