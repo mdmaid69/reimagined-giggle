3929732ee440157faf08467357c640ca85fe2170
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:23:58 +0000

    dull term middle cry share

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+import sys
+print(sys.version)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
