b305868d06b0bc99ceabf71bce98220b34791521
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:25:58 +0000

    later pure worried crowd slightly

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def convert_to_binary(n):
-        return bin(n)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
