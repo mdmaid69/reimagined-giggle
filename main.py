69df00fc330c23bb7fda74d1207680f26758902c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:11 +0000

    sweet fail body unfair left

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_current_directory():
+        return os.getcwd()
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
