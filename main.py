0a65c092a34dca0898a38f5f32477c9802d610bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:11:27 +0000

    fit round advise brief large

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_itemsize(array):
+        return array.itemsize
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
