16517f30812d7a8bccda74452f1b7990833f721c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:19 +0000

    appeal conduct adjective Ll memory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_area_circle(r):
+        return 3.14 * r**2
