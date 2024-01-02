2221ac2e98461f579d1eee1b639f45678eaaf858
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:33:32 +0000

    study occasional real turnup spot

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
   import os
   def get_file_lspare(file_name):
         return os.stat(file_name).st_lspare
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
