d18e3d1093d8e3d379c88bfc070f8c41a2514b9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:17 +0000

    soft trust mean sharp trick

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
