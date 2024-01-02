486be8c607dfcecc907c0bb8620a610119417b13
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:24:06 +0000

    watch rare early juice promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
