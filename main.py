5137d1f80b0f0cee0e3c2a84ea663a34c732e405
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:28 +0000

    war workupto aim suckme worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
