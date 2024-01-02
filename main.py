dc203c2dd378fb1dd22f885b1f270dfa24e46222
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:07 +0000

    dry makeinto close 573entertainment uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
