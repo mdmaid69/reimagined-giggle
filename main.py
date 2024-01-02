2394f7bb77bc92d88781ffb845b6f7c15a2cd5ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:23:20 +0000

    entrance stare tell protection proud

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
