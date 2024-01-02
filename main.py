61b5a04c324a91698f536e18bf3ea072523c3254
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:13:28 +0000

    conversation band this row deliver

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
