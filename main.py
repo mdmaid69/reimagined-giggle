d3f984fe4f5853372ffffdcdf518b7ce35b98394
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:19 +0000

    search motion address excellent cut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def remove_directory(path):
+        os.rmdir(path)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
