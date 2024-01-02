6056ac6ec202850ea79426cc39bb6957a261b8b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:27 +0000

    beneath central rest government either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import platform
+def get_python_version():
+        return platform.python_version()
+  def is_odd(n):
+        return n % 2 != 0
