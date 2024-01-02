cba095fb18983601197b9ff340dfa95cad278b11
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:16:58 +0000

    wheel live beat which shoot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
+import array
+def get_array_itemsize(array):
+        return array.itemsize
