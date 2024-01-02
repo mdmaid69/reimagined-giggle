2b903b663ba0449d5b556e7f3bdaf2335a3c858f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:43:14 +0000

    ocean track sound inquire practical

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import re
+print(re.match("h.*o", "hello world"))
