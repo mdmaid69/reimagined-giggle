18d7c91e450e58bc94ddf1dbe16835495463674d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:39 +0000

    bottom shopping which possibility back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
