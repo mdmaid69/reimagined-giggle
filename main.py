f9f7ab7e6e009db286e4b70011d5f100fe86aa4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:13 +0000

    strike trade level please bang

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def read_from_json(json_string):
+        return json.loads(json_string)
