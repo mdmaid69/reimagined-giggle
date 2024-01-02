7a6e3453b32928acf49cf2fd17548358134a51aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:02 +0000

    agree necessary runinto mean rise

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
