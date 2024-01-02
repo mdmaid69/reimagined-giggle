144945781acded536f62b1c04f7c7b0884513d83
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:42 +0000

    step breakdown nice road official

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
