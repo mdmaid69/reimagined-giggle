c0986f5d81923da7da2aadadcd85807e250dddf6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:13 +0000

    object march press check on

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+name = "Python"
+print("Hello,", name)
