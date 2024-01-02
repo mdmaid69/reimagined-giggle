660a393ec6de3b135c2fec5f0966c7b0b7ed3c68
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:16:41 +0000

    forgive right Hh pleasant stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_pressure(force, area):
+        return force / area
