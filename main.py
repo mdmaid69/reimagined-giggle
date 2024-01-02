2a7e9b5ee8c6fc8787017fba349f975a6d7ff3b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:07:56 +0000

    mix transparent trust night compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import collections
+def create_user_string():
+        return collections.UserString()
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
