e67a7bd3a8bf341e4cc37577a69d9eb1afa9affb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:36:31 +0000

    account complete repeat calloff so

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
