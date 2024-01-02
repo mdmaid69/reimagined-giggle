124c805bcdefab690169ec4923a9e88e6574d586
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:49:55 +0000

    ocean wing sight study direct

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+print(sum(range(10)))
