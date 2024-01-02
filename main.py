adc430f861b0609dec762b2ae98aa06c41cfc4c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:35:38 +0000

    giveup in plan dear pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
