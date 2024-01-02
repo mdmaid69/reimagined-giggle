ad40f90725aab747acf11c96d8e5c12174ab8290
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:25 +0000

    important plenty minority software thank

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def count_elements(lst):
-        return len(lst)
+import time
+print(time.time())
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
