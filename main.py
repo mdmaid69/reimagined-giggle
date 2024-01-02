cdae09feb6c3782d83d735f825ef878d5f2bae79
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:18 +0000

    silent explanation satisfied comfortable target

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import os
+print(os.getcwd())
