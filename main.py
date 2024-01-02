020164aa455a4e9d5baed80c52eec11bfdadacca
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:13 +0000

    choose general extra another poor

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
