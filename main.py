7493b7e7a1b2075aabd88a3ac831c0a1a03c8963
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:48:19 +0000

    final until deliberate object unless

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
