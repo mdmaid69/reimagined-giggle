3e6e43897e301817e6627b57da406cbbcbb7954c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:45:34 +0000

    listenfor holdon pull getaway environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
