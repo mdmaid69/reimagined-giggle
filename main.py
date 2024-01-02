fb91ed6c2cbf599fc105efe29a84147b6e4d244c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:25 +0000

    soil discuss degree clear dress

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_binary(n):
+        return bin(n)
