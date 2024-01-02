6f8c47d5152656416a208013b832ed0d7ea7d5f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:58 +0000

    sing guide hear bright sight

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
