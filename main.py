fa371d5aa2d1e20affb16c8d44f434845d5f8452
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:30:21 +0000

    reach move getaway window peaceful

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
