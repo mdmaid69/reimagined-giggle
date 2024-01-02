3ea02a59efebcc9379c2f272561380a9db9cdea3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:00:04 +0000

    suit instrument goon union simple

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+print("Hello, world!")
