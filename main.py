9d61a2b01e3148ada9e553e5e93535c032d11d9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:23 +0000

    dry whole remain activity Itself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
