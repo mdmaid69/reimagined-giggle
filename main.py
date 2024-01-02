6afa215bb2ff0664975555fba79eeb4b8a8971a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:01 +0000

    cool regret somebody attach creature

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
