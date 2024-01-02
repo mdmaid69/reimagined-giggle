e2a20259ecfefe166004599ce7eae7dec2a9a181
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:53 +0000

    generally less active bang nose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
