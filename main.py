7490f8fce2b19bc0975eaf264e6b8d1b872fc5be
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:54:41 +0000

    spread 568energy trick slice rush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
