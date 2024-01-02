bf58e604b7b97361dee8ddfa1ce5475445f12a3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:40:53 +0000

    top comefrom temper used me

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  def square_number(x):
+        return x**2
