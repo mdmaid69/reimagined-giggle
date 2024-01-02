f2ba0662a1739c8b3bfae74c33fe939a16767f22
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:36 +0000

    pause refuse fire love drawing

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
