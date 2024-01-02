97cf307dadcd9bddf062477d24e411a0bba1a2f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:45 +0000

    copy task union later usually

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_density(mass, volume):
+        return mass / volume
