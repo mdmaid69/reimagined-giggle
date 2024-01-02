f288900b67e57b49c1e57161d65ef85a536a1689
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:45 +0000

    want contrast intend dress lay

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def calculate_power(work, time):
+        return work / time
