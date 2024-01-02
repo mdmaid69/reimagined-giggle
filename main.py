c11d61fb1cb91ebc6570bf2765de7f139b8e0e66
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:32:41 +0000

    demand old strong vanish talkoutof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
