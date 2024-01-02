2fce07f01c1114f84243e68eaad887988fb8dab5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:18:33 +0000

    loud turn cold approve afternoon

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_power(work, time):
+        return work / time
 import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
