3704acacf6d8c540aa430a86984b05bc86504d5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:27:22 +0000

    Ff fire principle hard appear

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import sys
+print(sys.version)
