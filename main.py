7072580f4597c8473bb1913d61897295abb2ddb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:41:20 +0000

    hot hope detail right shelter

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def get_array_as_bool(array):
+        return bool(array)
