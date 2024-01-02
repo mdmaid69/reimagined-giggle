62754d7cfeab4f9ac31e9e20022705a62c3b000b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:01 +0000

    notice long dress region care

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+print(sum(range(10)))
