ee61d8997a9c1a984865ec77775878cb49a59c04
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:11 +0000

    rub pronunciation loss smile deep

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+for i in range(10): print(i)
+import time
+def get_current_time():
+        return time.ctime()
