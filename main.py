92a16e19b2e2e61ff665eef6f844a6f5fa39c84e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:15 +0000

    tough copy light touch gentle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
