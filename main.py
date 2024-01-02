a67e8a1b15bb1864d2fdf3f8f6eb230e887e3890
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:00:47 +0000

    of right thin usual recovery

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_list(array):
-        return list(array)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
