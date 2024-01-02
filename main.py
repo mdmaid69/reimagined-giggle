98fe39bf8feb259b261c42d927e2c1037f75fbba
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:06 +0000

    cry far way getup waste

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
