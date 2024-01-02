1b7b17872efeb648caef3a6fc6c0318da37e208d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:09 +0000

    wear rank main basic already

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
