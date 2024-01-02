7b1b341d3eac311ad48b0c8f83fa5048deb4d039
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:05 +0000

    contact favourite state wind noisy

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def cube_number(x):
+        return x**3
