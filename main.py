28c88cde5ce8a67cb5e5029429bb7e359d05c701
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:05:11 +0000

    brother partner belief ilovedicks smile

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
