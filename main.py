b61118fec1694133a2dc10f6b1562aaa7bf476a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:20 +0000

    offence contact limit flavour month

--- main.py
+++ main.py
@@ -1,6 +1,3 @@
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-import array
-def get_array_index(array, item):
-        return array.index(item)
+def count_characters(sentence):
+        return len(sentence)
+print(sum(range(10)))
