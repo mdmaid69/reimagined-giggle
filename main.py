5e7b4ca9e0e4fcec384c6cc8306bac576560eb49
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:30 +0000

    final middle present rough profit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def get_array_as_complex(array):
+        return complex(array[0])
