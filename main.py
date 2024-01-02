a38f24b954e6dd881205bbb3f3ae6534ae40af99
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:02 +0000

    like along immediately whatever confuse

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
