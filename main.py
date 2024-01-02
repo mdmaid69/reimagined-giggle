2fde9a63893cdc8fcaae3249804aa2adf1944152
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:13 +0000

    read possibly go fine boil

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
