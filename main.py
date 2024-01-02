2a16a1363190ca576e4eb7f5abd03e1a5d9d6746
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:52 +0000

    near process develop circle spend

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
