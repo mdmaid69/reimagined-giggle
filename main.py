8965bc0a095dfe61844704c873a75e1948799364
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:57:30 +0000

    inform thought brother occasion level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
+def count_words(sentence):
+        return len(sentence.split())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
