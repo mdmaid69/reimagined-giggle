95dbc30f385f9843e3cf0bd5ee61c7bb5ec3f06f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:25 +0000

    excellent minor thorough disturb always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
