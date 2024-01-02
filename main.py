cef148cfc377c3b9bf9a1dd7559cd3f6b5cdd4dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:44 +0000

    mean constantly should bunch better

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
