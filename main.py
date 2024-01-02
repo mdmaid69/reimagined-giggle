fc2f52b7360bf5eae0202170559bfd1b3ed73f4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:53 +0000

    favourite model accordingto base under

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def add_numbers(x, y):
-        return x + y
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
