a5f22207a8650468c2a6e682fce69ef25af717c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:52:15 +0000

    how list talk release sink

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
