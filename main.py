e52944e35ee6fbb0a2e3131c57dfb74bd4cbb516
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:44 +0000

    train loose stop small only

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_volume(length, width, height):
+        return length * width * height
