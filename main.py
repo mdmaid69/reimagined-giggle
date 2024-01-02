e927e4c2ff838c785ab4a4042608ed4cf8031651
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:23:05 +0000

    almost raise friend slightly instrument

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+print(os.getcwd())
