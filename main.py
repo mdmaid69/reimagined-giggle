1dcf7a39330fd87ff73f02c8d4b265cc84a216ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:45 +0000

    tragedy we object concern paper

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def find_min(numbers):
-        return min(numbers)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
