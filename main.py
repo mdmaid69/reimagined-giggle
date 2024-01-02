0a8a088459adcf0ef31aa9d017d5a48a22844652
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:30:28 +0000

    only minimum smell consider add

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
