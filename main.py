b03771719d916ee97815e942fe9d84e4b544cd70
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:37 +0000

    impression anywayanyhow remark sore advice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
