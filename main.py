1e6066ab5ece6e5035eff487d3f46da6f337b99b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:07:07 +0000

    reserve develop bread pleasure set

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
