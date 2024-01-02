d099aba398dd690d69d7cdf65df055386d935f60
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:25 +0000

    fortunate quiet contrast community concern

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
