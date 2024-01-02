271772b222ff87d6a130c0b304efd7ff9f3e8efe
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:51:02 +0000

    sensible tape together cool sentence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-for i in range(5):
-        print(i)
+import time
+print(time.time())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
