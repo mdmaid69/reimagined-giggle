709918384156b1696d5469696855f79cfdeb7634
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:31:22 +0000

    weather cross feeling tomorrow brother

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
