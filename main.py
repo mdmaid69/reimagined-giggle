675108d495dad30bc358a29bf48dec117f98bdb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:10:25 +0000

    keep wake describe store back

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import random
+def roll_die():
+        return random.randint(1, 6)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
