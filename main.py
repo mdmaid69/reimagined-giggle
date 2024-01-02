3fb91ee19b0c4f3b15a0694855297fe0f7fa8bc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:59 +0000

    start learn stand rain supply

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def find_min(lst):
+        return min(lst)
