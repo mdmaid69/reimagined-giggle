f472315021f8736da750ec0367585c7f367cfaf3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:11 +0000

    cruel day task confidence chief

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
