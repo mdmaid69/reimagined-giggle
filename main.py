2b01d94b3bf170e4cc28dda65fb23b127bec02ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:08:39 +0000

    how report floor accidental illness

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def is_even(n):
+        return n % 2 == 0
