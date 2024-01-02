0a52a86e5cc072ccb213afd4036992a5af71a571
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:22:25 +0000

    evening that artificial free pour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+def is_palindrome(s):
+        return s == s[::-1]
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
