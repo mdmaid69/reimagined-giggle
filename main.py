50877cbc71571a352f31862aa8b924b513a07b65
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:18 +0000

    angry building slice loud rank

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
