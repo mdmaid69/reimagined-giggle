8f2c61b678d091a0f8cd3bb767e4385452a3d4f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:10 +0000

    door legal black square list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
