599560f0ef82f3424bece13a4c2617d328fb427f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:22:50 +0000

    circumstance rich burst theirs than

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
