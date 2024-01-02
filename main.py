2ebfa330d561a5a41e19960c8721a6ff3dee9d01
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:49 +0000

    ourselves regard never book easy

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def sort_list(lst):
-        return sorted(lst)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
