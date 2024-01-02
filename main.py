6b178962978424e62e458f7677706c6e4033ba38
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:08 +0000

    its characteristic bend set like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
   import sqlite3
   def connect_to_database(db_name):
         return sqlite3.connect(db_name)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
