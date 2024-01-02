bf2caaead7b7e07070f38cbe0e61928621da636d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:02 +0000

    grateful score supply judge talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import time
+def get_current_time():
+        return time.ctime()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
