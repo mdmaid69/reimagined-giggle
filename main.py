a331096592dc5531cc89af389612dffe113c7834
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:58:59 +0000

    abroad difficulty both relax survive

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
