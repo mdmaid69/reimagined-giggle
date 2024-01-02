5cab21de8192ec1b7592c22495ba6fc150d5c42d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:23 +0000

    getaway ear crazy wear pause

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
