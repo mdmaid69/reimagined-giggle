d8b50ac62044c6ec47abcb81dd3c724aad199036
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:19 +0000

    paper trade union copy afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_circle(r):
-        return 3.14 * r**2
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
