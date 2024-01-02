80a299f5f7e09c240a9bf96bf49a41986416fe43
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:11:25 +0000

    smell future catch injury improvement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_sine(x):
-        return math.sin(x)
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
