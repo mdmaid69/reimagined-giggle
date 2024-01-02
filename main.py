e109a96b022f5a95ff7431dee7a43eb6c5269161
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:45 +0000

    march air creature never tidy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
