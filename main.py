03e26b1ca363496b3da8543280092f2bdbf8b469
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:18 +0000

    shoot attempt gas ball translate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def cube_number(x):
+        return x**3
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-def find_max(lst):
-        return max(lst)
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
