db334d37ae6aa66fff06eb1f6ff00fb77f843565
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:51 +0000

    previous desperate instead correct lunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+def calculate_density(mass, volume):
+        return mass / volume
