de3ad75e3860b7afe99f5d8b0536e115cd51d6dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:29 +0000

    stamp either exactly backwards born

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
