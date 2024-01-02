c54d01d965fcb5fffe220127c414b6c9afdc495b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:55:08 +0000

    anyone their mean while short

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
