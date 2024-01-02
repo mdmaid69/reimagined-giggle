aae4a83e2bcd2233b069debea7b9023b0eca404b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:44:12 +0000

    culture likely flight present wing

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
