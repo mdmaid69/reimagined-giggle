8b76b3aaa3ab82c917bda61643b7c99f0f3bd3c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:13 +0000

    fact reduction university ready negative

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
