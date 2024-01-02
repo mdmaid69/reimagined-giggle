156577ed60353933fe0122c19177807412dbc842
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:59 +0000

    affect tight treat matter journey

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
   import os
-  def split_path(path):
-        return os.path.split(path)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
