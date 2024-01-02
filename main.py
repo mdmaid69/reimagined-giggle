278f2ee0f41891f3e1359781588b7cda28517996
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:20 +0000

    sleep angry border morning Gg

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
