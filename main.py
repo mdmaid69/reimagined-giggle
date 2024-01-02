1d28897c736ecc84267c7c7f241917e039ee5a98
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:11:21 +0000

    birth terrible usual turnoff least

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-print("Hello, world!")
+text = "Hello, world!"
+print("Characters:", len(text))
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
