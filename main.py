e693cb0d44eb1c7b4495b287f9d0eda601cbf8cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:37:57 +0000

    bad downstairs future past trial

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
