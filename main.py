3d848e210f4c5fd11ccbfbf1899bbfa920b50e34
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:33:02 +0000

    born determined plate question legal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
   def get_file_flags(file_name):
         return os.stat(file_name).st_flags
