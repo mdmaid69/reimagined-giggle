d15a7fc35adcc32d468f4e85e9d98cef4c600b85
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:40 +0000

    tense recognize goback(to) through defeat

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
