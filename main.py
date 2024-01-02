236dfddef63ae8505ccaad56b64f8fcaf3436a28
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:07 +0000

    clear how grow pull everywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_array_length(array):
-        return len(array)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
