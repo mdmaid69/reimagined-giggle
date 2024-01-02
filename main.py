7e21b5fc326cc0c530bbdf53f66c394264672834
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:09:18 +0000

    trial let design spring battery

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
