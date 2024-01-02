e6593592c83d2d5dd4d71720ac75a784331020db
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:43:56 +0000

    embarrassed wrong thought politics content

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import array
+def get_array_as_repr(array):
+        return repr(array)
