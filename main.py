f0994642bc70d3b83efb63108ab25559d45f67d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:16 +0000

    think otherwise birth somewhere care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_as_set(array):
-        return set(array)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
