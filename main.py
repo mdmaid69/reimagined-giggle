4e172244c8c22be1cbe548c2e96c37cf0a68cb48
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:59 +0000

    congratulate second worry alone meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
