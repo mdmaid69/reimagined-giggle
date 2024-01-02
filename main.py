0d4920467f2bbd040e8b47f1bbcc30b9ecde84ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:22 +0000

    probably crash exploration go hope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
