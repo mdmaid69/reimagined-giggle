0bfa98574b1fe0e36a1f11122549ee3016a55889
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:11 +0000

    beside call bury contact idea

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
