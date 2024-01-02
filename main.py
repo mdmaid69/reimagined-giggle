e2894ff1055bf9d065d52feafa0389f7988f003d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:56:57 +0000

    crowd left that to charge

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+def square_number(x):
+        return x**2
+def square_number(x):
+        return x**2
