a635751151a3299ba457c9b1770d610710d2fe8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:00:07 +0000

    include hit delight payment clean

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
