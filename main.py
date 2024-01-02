c62554464a7344dadf52f9a4f6d8ce4f8bc9c6e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:59:13 +0000

    willing unless lose love fond

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
