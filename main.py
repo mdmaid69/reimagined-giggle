5d93cd8b8c90f311eca3fce548406a680a36e8cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:27:04 +0000

    unusual centimetre repair children reaction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
