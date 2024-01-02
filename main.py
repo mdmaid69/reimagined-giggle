0d93a28e28d74836feca04a203168568d78f00bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:37:05 +0000

    funny secret warm run I

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
