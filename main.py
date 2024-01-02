323ab590d86c8b6ccea3351939d9e87013d774bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:18 +0000

    chin one majority platform condition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
