b9a350011ec7808b1ce592f10d6d6dcf8d9e104d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:48:02 +0000

    female bring effort miss influence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
