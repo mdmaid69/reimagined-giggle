3a33908094c2cfb06c8838393be56dcbeae015f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:38 +0000

    study describe cry keepout shut

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
