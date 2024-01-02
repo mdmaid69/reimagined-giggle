f1da6fef85f19bb026dbcf6379488644279ac971
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:40 +0000

    unkind unlucky gate convincing especially

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
