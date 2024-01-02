feae6a536257584c65e38f61616497455f37e9f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:52 +0000

    lesson news in sound tough

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def calculate_work(force, distance):
+        return force * distance
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
