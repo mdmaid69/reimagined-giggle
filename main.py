97ccbd3bd0e95d1409a87da6bf977ab11324c788
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:26:28 +0000

    official excuse loss arrangement warm

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_array_as_set(array):
+        return set(array)
