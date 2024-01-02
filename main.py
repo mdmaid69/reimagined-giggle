776dd8afab5dd43016366f4dcdd49695593496b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:44:04 +0000

    limit gas rise could preparation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_density(mass, volume):
+        return mass / volume
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
