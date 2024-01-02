958809957efdaf194f7c386cd713761922f6ca5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:41 +0000

    sort free nonsense shape them

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_acceleration(speed, time):
+        return speed / time
