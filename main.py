89af66cb2767b920c54ca2c35fbf106129680567
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:02 +0000

    Cc off lay closed cool

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
