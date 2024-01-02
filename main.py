ffb4c6124addccba33917f41f4892dbd2e9ac57b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:12 +0000

    defend vision basically 566end official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_array_index(array, item):
+        return array.index(item)
