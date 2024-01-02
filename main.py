e297c9cd99e84b03df433f76ef58b813d4462e62
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:47 +0000

    part build purpose lesson towards

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+def sort_numbers(numbers):
+        return sorted(numbers)
+import os
+print(os.getcwd())
