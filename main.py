bff5c52e6689f74f825508bf1d46ff43a655236a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:52:16 +0000

    instrument company decision sport scrape

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def convert_to_binary(n):
+        return bin(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
