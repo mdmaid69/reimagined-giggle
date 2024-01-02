7564aadb44693795721e103b30ed60c95055f57f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:58 +0000

    tradition his Ss create nervous

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
