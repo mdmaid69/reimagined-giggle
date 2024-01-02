36792beffbf30471046f84b3d8b3917610663f3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:53:57 +0000

    carry person obtain job tour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
