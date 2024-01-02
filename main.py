a8f522099336feb9c1e3150d18ddf7f5ec5d4cc0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:34:51 +0000

    ask eat rescue stamp pronounce

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
