e50d33b6bfe8d405345da49aebeb76d2b3a9febe
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:37:03 +0000

    situation discover interruption its funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
