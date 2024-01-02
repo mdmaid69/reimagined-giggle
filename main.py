58ea6a19cba3d821d435fb036444a6a209476725
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:56:08 +0000

    by proof conversation balance suddenly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
