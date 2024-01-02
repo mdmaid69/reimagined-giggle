f47ffd47338514dd0284fd04ad34da97ace91620
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:40 +0000

    fail unit even cope regard

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_directory_name(path):
+        return os.path.dirname(path)
