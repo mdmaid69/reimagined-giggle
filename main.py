54ea0e2f1a4bf5d54a71bd0e743a08e2362d7201
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:44:25 +0000

    stranger town deal task off

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
