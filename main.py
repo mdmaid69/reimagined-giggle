f1a52da615fc6453b36ea112d438a00b09ffe455
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:57 +0000

    grow risk little sale may

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  def calculate_area_rectangle(l, w):
+        return l * w
