82c683a58bdf817b00150381a343112b53e77948
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:34:19 +0000

    see create relax tight soft

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
