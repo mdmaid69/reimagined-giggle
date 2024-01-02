27b522d594acd94b312090e19f0fc56567d6cf51
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:54 +0000

    turnup argue past average smooth

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
