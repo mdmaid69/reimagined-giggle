e87ec4c1f478f27a85e4fb101c83e46ef592db73
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:41 +0000

    wide smart immediate your storm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import getpass
-def get_username():
-        return getpass.getuser()
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
