2979a46916cbcb9acdbe85c7078fa9cc834498c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:23:40 +0000

    meal uniform habit Ll sure

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
