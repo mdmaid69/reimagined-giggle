87e18cd4e14621f25f55079352e4405b0a477a76
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:18 +0000

    correct whistle town boring commit

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import tensorflow as tf
+print(tf.__version__)
