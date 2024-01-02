3f0624eb58b0c8ef72ddb71424fba9297c9e9f79
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:19:37 +0000

    complicated bang just slice declaration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def subtract_numbers(x, y):
+        return x - y
