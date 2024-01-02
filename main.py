4580d01f6a29cd2a5d0ce75e1437293ae8408e26
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:06 +0000

    violent contribute my holdup table

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def sort_list(lst):
+        return sorted(lst)
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
