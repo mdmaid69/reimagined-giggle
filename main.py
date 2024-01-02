076e1d5ed648ba67b07c1f981b0ad13dcaa57cbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:31 +0000

    fine tired match trip touch

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def add_numbers(a, b):
-        return a + b
-  def cube_number(x):
-        return x**3
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
