f52da4ba129b13924937ac8b1c8ecea1b3f66aea
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:11:58 +0000

    prefer physical shake base start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import numpy as np
+print(np.array([1, 2, 3]))
