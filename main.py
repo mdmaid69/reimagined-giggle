de148676d5d24505a8ed669c265c52355642621b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:06:45 +0000

    sound freedom matter quickly spring

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
