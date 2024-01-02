e4692f037f67de0ac3deefe08a952bdec875a5d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:08:47 +0000

    event runaway escape talkoutof exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
