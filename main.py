6f1a1057521492f319a905f09496fd51eaccbd91
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:55 +0000

    book transport until injury start

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import random
-def roll_die():
-        return random.randint(1, 6)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def generate_random_sample(population, k):
+        return random.sample(population, k)
