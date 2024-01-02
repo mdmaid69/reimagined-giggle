a44568c37373a50d35c509e08c854b4a6bbe0123
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:03 +0000

    spoil almost good scrape wake

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def reverse_string(s):
+        return s[::-1]
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
