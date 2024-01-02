53c971362d6c9c6cebaa38763e5c4e27ab693c62
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:34 +0000

    own after year whisper perhaps

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
