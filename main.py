ef6e55fa347a2598c95a2cb206bd2665399f3a74
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:35 +0000

    worried simple put ticket pool

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
