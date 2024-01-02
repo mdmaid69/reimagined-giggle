c3142ae7a5cc97de790d4cb4b2021b0b809a5a52
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:28 +0000

    fix wide rare bang wild

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_counter():
+        return collections.Counter()
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
