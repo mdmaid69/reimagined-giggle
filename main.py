6f8f6d2c3c6938fcfe8457dd241c29dfef6f6d6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:23 +0000

    strange opportunity change pool through

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
