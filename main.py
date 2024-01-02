6704f81cc09124e309da1537fe6067e65be8f2cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:18:36 +0000

    ear sort sleep father treatment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
