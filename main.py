2e1dda908c552ad4f86b991d859a1f25f3bad36d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:32:44 +0000

    giveup only family contact bringup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+def calculate_speed(distance, time):
+        return distance / time
