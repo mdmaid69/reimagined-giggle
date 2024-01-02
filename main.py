78bd5ec5cde34797a19b77f3033d0786b91dbc8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:53 +0000

    only clear scrape worst depend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
-def get_current_time():
-        return time.ctime()
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
