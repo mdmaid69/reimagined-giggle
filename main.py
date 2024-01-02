d70848dc1a3b7f8c0eb4c0b77d78054fbc0459b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:38:54 +0000

    last environment somebody broad weigh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import platform
+def get_os_info():
+        return platform.uname()
