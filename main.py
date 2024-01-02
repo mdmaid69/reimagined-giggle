e346c7fb8a97306d2df10cb66786543d4f67e862
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:30:28 +0000

    murder anxious priority clue fence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
+import math
+print(math.pi)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
