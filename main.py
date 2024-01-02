bd39efbd4e66276694ab9dc041c1a8af720b5960
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:31 +0000

    consider unhappy watch last connected

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def delete_file(file_name):
+        os.remove(file_name)
+def count_characters(sentence):
+        return len(sentence)
