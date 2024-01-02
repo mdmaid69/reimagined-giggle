9f911fee96cfbf804183933af3e17ed81b3be338
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:00 +0000

    natural suffer excuse situation keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-text = "Hello, world!"
-print("Characters:", len(text))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
