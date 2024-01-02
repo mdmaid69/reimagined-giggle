d7b2be8f023dadc728d43eff29a3f98b45011a0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:48 +0000

    government doubt plan build learn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_item_count(array, item):
-        return array.count(item)
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+def get_array_slice(array, i, j):
+        return array[i:j]
