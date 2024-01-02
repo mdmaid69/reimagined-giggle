ea57e1d99cab1fd986c581035ec732ec3a5a907b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:09:12 +0000

    Ss connection giveup surprising reduction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(a, b):
-        return a + b
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def convert_to_octal(n):
+        return oct(n)
