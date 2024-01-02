92cecfcdb6c94c9f489a89bb7dc94fe333f5e21a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:44:36 +0000

    in over tough proof recovery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
