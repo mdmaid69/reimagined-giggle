02f748a14b6bcae70392ad9311ee1d519daa14c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:59 +0000

    fear clear pretty takeover justice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
