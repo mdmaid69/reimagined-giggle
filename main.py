0b0e54e599d7e626aed83899b021efcd15468cfa
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:46:55 +0000

    immediately tread poor substance everything

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
