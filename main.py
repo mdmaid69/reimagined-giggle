cb1c02d944bddae1ed5dcd31b1ff4b547b9452e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:20:57 +0000

    until demonstrate silent view spray

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
