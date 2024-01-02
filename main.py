bb10e45b8e043ee56742b0a6994cd353984da06d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:51:33 +0000

    interruption team rich sudden conduct

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
