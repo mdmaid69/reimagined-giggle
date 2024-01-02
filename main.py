7a1c53a0d1432c6f94e15e68fb51b770e0de9694
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:27 +0000

    into war giveout turnoff scale

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
