4bf7e48cf79f6a5c5c729518f32b4b9d541562bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:55 +0000

    seem low control basic pinch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import numpy as np
-print(np.array([1, 2, 3]))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
