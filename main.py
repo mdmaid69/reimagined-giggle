fbaad8128c94d2e0fff8f38a93b1f5e81f6726a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:02:32 +0000

    truth example point mood roof

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
