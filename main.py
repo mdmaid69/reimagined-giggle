d1934d6b7c356aef6c033bf61c8a66d8c985f592
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:41 +0000

    Bb building success get flat

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import random
-print(random.randint(0, 100))
+import sys
+def exit_program():
+        sys.exit()
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
