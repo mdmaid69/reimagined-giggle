70f0011016e2f37a6d4363fc71934ec9879d61a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:06 +0000

    help build question sensible fear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_power(work, time):
+        return work / time
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
