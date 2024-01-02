7f753d1610c9c198a4525d4a003b101cd782f87d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:13 +0000

    Cc and total food scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_area_circle(r):
+        return 3.14 * r**2
