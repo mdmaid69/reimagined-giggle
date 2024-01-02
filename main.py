3d65c320492238aaa24b7edd5d732ffddb0269a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:26 +0000

    see fetch slight cause hang

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-def calculate_power(work, time):
-        return work / time
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
