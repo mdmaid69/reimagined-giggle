2fc54d5d1c6a7aedaad0e0119f66d1931bcfb2c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:54:24 +0000

    few geton everything cheque spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+import sys
+print(sys.version)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
