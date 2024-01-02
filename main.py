d481a17e37c4e28e078b0104443fa66f5a8489a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:30:23 +0000

    well among show jump taste

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import os
+print(os.getcwd())
