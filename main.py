d1e58187eaf4798976efb2f427b9253dbe49416c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:39 +0000

    tidy pattern wave sign glass

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
