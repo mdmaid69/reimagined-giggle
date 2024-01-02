6f1a7eb8a62f25f0cb30a2cbb24d1efd3ec496be
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:12 +0000

    clearly move film public apart

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import sys
+def exit_program():
+        sys.exit()
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
