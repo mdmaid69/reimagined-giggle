fbc0b39e5e70025a22031e999f5599ee57c2143b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:46 +0000

    card stranger throughout callup pocket

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sys
+def print_python_version():
+        print(sys.version)
