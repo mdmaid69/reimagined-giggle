0e142ecd5ae353a076e842390213a7cbe601856e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:03:15 +0000

    design ordinary smell prepare dance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+def reverse_string(s):
+        return s[::-1]
+import array
+def set_array_item(array, i, item):
+        array[i] = item
