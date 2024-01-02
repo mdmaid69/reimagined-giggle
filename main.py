aa6871e4f9769477c3896f50074cf92effdde414
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:05:30 +0000

    well phone however insist breakout

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+print(sum(range(10)))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
