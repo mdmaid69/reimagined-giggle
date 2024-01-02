76d712fa3ff1838c12e7f0232b14c7a57a9c289a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:56 +0000

    bend certainly water common lookup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def append_to_array(array, item):
+        array.append(item)
