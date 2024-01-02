899ac77c1a7b97c5543daef81817b889fbce0f77
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:44:50 +0000

    light man company off division

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  def convert_to_octal(n):
+        return oct(n)
