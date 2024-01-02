f423b145e45a94359e21eee85edbb35b0839c7b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:28 +0000

    sure know great beginning crowded

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
