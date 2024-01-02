02081d0178c39111772801a5b6f827cc90bd9102
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:13:12 +0000

    neither music hardly 571enough perfect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def find_max(numbers):
+        return max(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
