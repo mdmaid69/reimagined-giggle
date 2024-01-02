f0e99b09fcbd5c9119cea9b9dfe4f5ce46e86a71
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:33 +0000

    zero tend night sense impatient

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import numpy as np
+print(np.array([1, 2, 3]))
