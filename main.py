733ff5f949f7b1c5b39a6351fd0eb5e5eaf278d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:06 +0000

    recently date may people friendly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
