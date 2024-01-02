a4f2a4665ddf4ec33cb7443fbf9867f72bf055fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:17:25 +0000

    everyone plan comment flavour fashion

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_work(force, distance):
-        return force * distance
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
