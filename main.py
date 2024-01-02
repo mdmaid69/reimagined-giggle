9e0e52c111a6adc0961abed8bef7c8509d630f1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:51 +0000

    claim thank belong Kk demand

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
