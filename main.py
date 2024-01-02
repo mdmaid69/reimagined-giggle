a40848fdbaa7fb400efd935632a56d1764c636d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:42 +0000

    rank measure lift march as

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
