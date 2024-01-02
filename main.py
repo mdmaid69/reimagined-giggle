826f2b71ce9e8a4b14a63b792c4243c2a083812e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:17 +0000

    furniture uncertain fashion future praise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
