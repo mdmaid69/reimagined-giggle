b2f479aa17461d6a9cd98dee80132045a525eaa3
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:10:00 +0000

    ball many blue in discovery

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def convert_to_binary(n):
+        return bin(n)
+def multiply_numbers(x, y):
+        return x * y
