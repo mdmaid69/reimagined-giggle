bbbf5ed05a2f24a51c4bfbd41f36a22d64b7eeb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:56 +0000

    receive course getoff threaten compare

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
