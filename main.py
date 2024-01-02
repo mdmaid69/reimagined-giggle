ad5ba1c52adea7e7a522829079f10ad25f8d93c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:37:55 +0000

    thirsty personal brain chin general

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
 import array
-def get_array_index(array, item):
-        return array.index(item)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
