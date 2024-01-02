c252204c6c33b81fb00298316dc4d33baa0c33c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:58:44 +0000

    course achieve danger food cheque

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
