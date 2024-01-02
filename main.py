f379b1007514b3f47f6d21815afedf4999d8bf31
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:34 +0000

    lend trap hope offence noone

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
