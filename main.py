a8733c690bb280c1f5b87a9d304b268e6e67806c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:37 +0000

    disapprove better expression late by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import datetime
+def get_current_datetime():
+        return datetime.datetime.now()
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
