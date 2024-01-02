ed950a47cce0dd909abf28639c5f5a5378439b36
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:46:38 +0000

    false bag switch compete be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
