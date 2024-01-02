967bcb1c2e4b0c6b2e386e76d8506cb105c342b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:57 +0000

    getaway union many soon account

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
