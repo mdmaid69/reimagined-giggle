6a6c5f17e065c6ba0be67328d76a6274ba8c200d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:53:39 +0000

    university describe physical sky central

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import tensorflow as tf
+print(tf.__version__)
