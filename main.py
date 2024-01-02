5fab6fb269f00b073dce206d0c25402dc01e27de
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:51:53 +0000

    bath sad Ll lonely release

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
