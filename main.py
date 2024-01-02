13054960224975c1da849cd88d50e39c78c13efb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:29:55 +0000

    degree know communicate deliberate solve

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_device(file_name):
+  def get_file_dev(file_name):
         return os.stat(file_name).st_dev
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
