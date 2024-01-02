708e623384674db55eacca57fceef75632c1a2d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:12 +0000

    meeting clean degree early arrive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import array
+def get_array_typecode(array):
+        return array.typecode
