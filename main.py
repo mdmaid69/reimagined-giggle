4b980365a6633785b6c1c4aa35f2a338ab9726be
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:36:21 +0000

    attach repeat worth supply himself

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import collections
+def create_user_string():
+        return collections.UserString()
