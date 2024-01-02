facfddd3066181081f49373649881914216ce789
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:48:28 +0000

    telephone arrange explanation success entrance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def square_number(x):
+        return x**2
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_factorial(n):
+        return math.factorial(n)
