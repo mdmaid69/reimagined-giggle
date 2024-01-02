e66dc439d10fd7407eaade98059911703c288e31
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:28:21 +0000

    comeup eye far tour swim

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from collections import Counter
-print(Counter("hello world"))
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def cube_number(x):
+        return x**3
