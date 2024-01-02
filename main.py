69ea230be459eac5e922dd06567de157c08c36c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:05 +0000

    break takeaway race pause public

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
