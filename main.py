f7c2ed5fb1364dff28de40916d9a797d15a8ee97
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:59 +0000

    talkback number do stiff drink

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def count_words(sentence):
+        return len(sentence.split())
