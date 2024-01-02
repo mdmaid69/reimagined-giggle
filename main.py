0c0c41f3de94052a136cbccaee94ba7af54ebc5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:35:59 +0000

    rather store fresh grow involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
