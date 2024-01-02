5a3e8a5e8f5b88526b6c02a84345f12ded6dedb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:23 +0000

    content turnup tip now by

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
