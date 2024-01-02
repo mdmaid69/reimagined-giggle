85ab12b830200f37cda74f00658ddcb2e4d14b59
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:19 +0000

    guilty belong meal together practical

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(lst):
-        return min(lst)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def count_words(sentence):
+        return len(sentence.split())
