74caad9c09ffca3caaa79199298a24f5ccd2c962
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:50:22 +0000

    slice square narrow charge method

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+def calculate_work(force, distance):
+        return force * distance
+def calculate_work(force, distance):
+        return force * distance
