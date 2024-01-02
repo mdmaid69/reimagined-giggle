18b39f4ffd9e69b0157fbd55e04c9bb8a603fcc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:54:02 +0000

    holdon real start trap order

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
