a60c438892c5827da351e8eed4e7bac97292edd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:45 +0000

    next sure flow bite exercise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-def convert_to_octal(n):
-        return oct(n)
+import sys
+def exit_program():
+        sys.exit()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
