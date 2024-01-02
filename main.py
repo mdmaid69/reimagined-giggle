7305f3196ce8f997d03070e9e2058a47fbd855ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:54 +0000

    up any comedown tool position

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import random
+print(random.randint(0, 100))
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
