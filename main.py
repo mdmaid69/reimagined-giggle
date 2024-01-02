d9b9e0d446a05b4e1cd8da3a65d759109d0cedb9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:17:43 +0000

    connected art division neither brother

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
