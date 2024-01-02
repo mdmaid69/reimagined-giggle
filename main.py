06804b9dcf0c09961ec48c67adcb1da5e4b8ebe8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:12 +0000

    trust pretend track notice pass

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import time
-print(time.time())
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
