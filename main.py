85b9ea5f3ec222f6868d6136f0c71e5a6f686654
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:00:54 +0000

    leg stand wing mother necessary

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import os
-print(os.getcwd())
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
