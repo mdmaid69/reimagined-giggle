7aefbc6c0998d46deed619beda3f4a5e77c017c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:34 +0000

    taste church patience cook seem

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
