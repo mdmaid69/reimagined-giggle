aa7fa63f87e4c33f0a607b59c8cf08cdc977bfb9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:54 +0000

    deal answer Ii off appeal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
