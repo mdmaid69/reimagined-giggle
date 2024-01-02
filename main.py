96c13ff244d4b1bdedb8a3f145d1ceeeab97d780
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:08 +0000

    Dd letter sick whistle noone

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_rectangle(l, w):
-        return l * w
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
