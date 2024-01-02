86bb1f8d565cb0b1ebddae68796a2c9b6e90aeec
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:52:29 +0000

    original earn congratulate stretch related

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
