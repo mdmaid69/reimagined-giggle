db3240ca3f76147e537eed05f1f9f38690cd6bc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:16:54 +0000

    hour callat punishment talkinto crisis

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
