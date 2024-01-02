a96cf0ec16737ef8733988b61823856cfce54b81
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:58:57 +0000

    congratulations hope reply each passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
