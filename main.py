5eef11487429f4d9df9a50eac1e429de53150be6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:22 +0000

    sensible appearance still upstairs not

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-  def is_odd(n):
-        return n % 2 != 0
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
