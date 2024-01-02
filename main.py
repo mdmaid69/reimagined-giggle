24368a5319509c8d21dc4c9d585d655daa6d1c88
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:39 +0000

    drop definite burn instant memory

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-import os
-print(os.getcwd())
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
