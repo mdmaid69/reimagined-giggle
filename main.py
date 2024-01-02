6213de87165fa816d8b4eaddd5bc862a507715f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:30:51 +0000

    criticize add birth account wait

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_pressure(force, area):
-        return force / area
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def is_odd(n):
+        return n % 2 != 0
