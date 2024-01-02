489f0b4391724d030e82a3ced108cca5b023eadc
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:50:02 +0000

    furniture price please teach holdout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
