ada221e6ef420de53bbf51bfa745e6baf965dbfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:28:03 +0000

    software full rub bus secret

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
