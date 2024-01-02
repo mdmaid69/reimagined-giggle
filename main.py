81d8c205c7e62a9afec78a8a88c60b78ac57ad4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:10:34 +0000

    introduce careless brain to surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
+def square_number(x):
+        return x**2
