d75941512e3c6d42fb2f52d066648ab7f7af9079
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:41:50 +0000

    if progress office bringdown almost

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-def find_min(lst):
-        return min(lst)
+  def sort_list(lst):
+        return sorted(lst)
+import array
+def get_array_as_list(array):
+        return list(array)
