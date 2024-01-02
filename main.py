79b36543bde4a84771622814c3ccb6a8acd060d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:19 +0000

    dress thorough somewhere yours effect

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
