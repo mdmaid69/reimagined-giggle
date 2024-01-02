cb2c105cd89585030948b8f96b0f4498b854f233
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:33 +0000

    large account doctor lay discover

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+def calculate_power(work, time):
+        return work / time
