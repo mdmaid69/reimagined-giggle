1b735b88559bd3afb4c1b645e0cc672872956738
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:13 +0000

    giveout notice person age necessary

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  def convert_to_binary(n):
+        return bin(n)
