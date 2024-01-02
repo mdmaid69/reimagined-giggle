d0e0836bafceab9b4d8eb1f6cc1cbb753cc4e061
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:29:55 +0000

    minimum sensitive clean sister recover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
