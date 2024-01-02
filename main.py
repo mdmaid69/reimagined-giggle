f72f639b6ef81b5c19776253acf36c84ca85edcd
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:15 +0000

    will burn reach myself speech

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import os
+print(os.getcwd())
+def count_words(sentence):
+        return len(sentence.split())
