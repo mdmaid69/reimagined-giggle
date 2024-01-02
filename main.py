5b95308e2010ace5226e7c7410820852c4f62f79
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:00 +0000

    quick pretty distance month sting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import sys
+  def get_python_version():
+        return sys.version
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
