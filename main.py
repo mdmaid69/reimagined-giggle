0bf99e100b9e65a86a2919eeb10d0a3e65ae91b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:33 +0000

    lock compare none sudden movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
