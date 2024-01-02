97128281a6ae9f75392c6f2ef19a3410847b6dd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:50 +0000

    plant sing half identify thankyou

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  def is_odd(n):
+        return n % 2 != 0
