07ad2c0849e60f16807f9f197f5f5fd0bf407f74
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:06 +0000

    light report middle explanation present

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
