7cb17b104b85226e2d8c3b4cb21c0e7b7997999f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:15:00 +0000

    represent authority respect cheek bus

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-for i in range(10): print(i)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
