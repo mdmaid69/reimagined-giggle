8f3f1570383bccbe7e6bb8b4f2e722c78d44f040
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:31:20 +0000

    tend injury shop order because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
