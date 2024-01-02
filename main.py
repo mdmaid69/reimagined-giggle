69ff91a889b995efec9800f71456b3f56a2c6eea
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:18:27 +0000

    perhaps finally crazy science could

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-  def square_number(x):
-        return x**2
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
