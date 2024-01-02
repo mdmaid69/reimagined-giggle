7a9313b1f092e4f045f1cb3693b33b267a843ec4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:18 +0000

    act comein right quit safe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import collections
-def create_stack():
-        return collections.deque()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
