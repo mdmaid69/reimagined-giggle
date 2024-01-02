57f61d12724cf3e98e48c9b10603de64c19056cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:35 +0000

    gate experiment promise impressed adjective

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
