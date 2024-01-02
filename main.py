aea2bd011970c7c3b0f851bc6b99ee1c30005146
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:48 +0000

    downstairs criticize pull shine common

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
