146eaa9f31ebbcabcdef91efef202eaa18a0b9a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:06 +0000

    way wash gate own to

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-print("Hello, world!")
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
