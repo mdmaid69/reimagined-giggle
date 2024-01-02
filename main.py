2023a36bc1973d2f2c5b4c53649615cac5599e36
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:53:43 +0000

    check comeback experiment pen threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
