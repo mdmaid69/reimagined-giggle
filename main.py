2c4779e7ad61c9fdb23f37e4dd4b04b666bf98a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:03:18 +0000

    far class half excuse principle

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
+def reverse_string(s):
+        return s[::-1]
