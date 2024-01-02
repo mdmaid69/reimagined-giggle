6be295de53ba061c14db01cb71c45e7755049efe
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:51 +0000

    acceptable tonight giveup sure straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+print([x**2 for x in range(10)])
