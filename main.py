f0ba2cc1ee0db8ab9390e0feebc4d093d028e662
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:51:37 +0000

    become tonight convenient nurse doctor

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
 import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
