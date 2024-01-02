3a17d189fbdcefdc76e1fd7dac54d25cac8e7cad
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:32:32 +0000

    confused file zero runover picture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
