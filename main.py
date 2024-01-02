ab571a056f6f086366e404944b7ac41b8c300743
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:40:13 +0000

    plant gooff bridge it tight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
