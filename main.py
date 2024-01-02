f60f3a099d9d8b377a3046710eaa675c8874a97c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:33:48 +0000

    touch off no pen star

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
