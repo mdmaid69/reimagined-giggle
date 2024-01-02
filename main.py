ca8c95d5915453c930e90fd3f5be1ba343bee0b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:04 +0000

    grade dry account rescue silent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
