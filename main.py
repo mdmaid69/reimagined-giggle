e3b3f656f853915ec86feb7fe79cd7a87cb0e98b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:42 +0000

    once familiar yourself short bit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
