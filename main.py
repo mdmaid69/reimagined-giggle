68026703dfc40d7a44178121423107af3889a40b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:22:21 +0000

    wear slightly theirs miss narrow

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-for i in range(10): print(i)
+def convert_to_hex(n):
+        return hex(n)
+import sys
+def exit_program():
+        sys.exit()
