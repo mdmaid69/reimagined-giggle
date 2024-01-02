bff9ea921028fe5de41373a15e18338aa2c763f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:48 +0000

    hospital who fact meanwhile contribute

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
