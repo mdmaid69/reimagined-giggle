f15889a3c71ccc5b106b70109b5bdf8a91d48a80
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:41:42 +0000

    government importance repair comeround confusing

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
