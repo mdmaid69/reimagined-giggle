d4e0c9e339b9c547a22e2fc9972c98cefa254a00
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:16 +0000

    arrive leave line issue impressed

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
