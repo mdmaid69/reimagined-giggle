f503075b3d706fe3a19e3ddaa0e975cd21333c96
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:55:47 +0000

    fit contact takeback both wave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_directory_name(path):
+        return os.path.dirname(path)
