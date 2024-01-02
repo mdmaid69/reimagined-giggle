e10a84aee95b948f14d3b6bd4e5f41f13767c81a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:17:12 +0000

    fire much in abroad busy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
