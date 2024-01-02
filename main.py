07b3bc8fac991fae964a2c5d3abb6da76d9ccff6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:13:42 +0000

    comeoff dull present answer which

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
