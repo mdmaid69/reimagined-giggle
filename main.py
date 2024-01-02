04ec49a11d2cfcae23ac6571a6bf4333a1290bed
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:28 +0000

    although prepare share excuse store

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
