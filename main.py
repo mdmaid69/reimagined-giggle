bb01c106316c049210f231b585c6d315089440f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:16:06 +0000

    business reason cum fasten avoid

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+import os
+print(os.getcwd())
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
