2ce2c65adbbce31b2545759229d65f648513a954
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:27:45 +0000

    subject legal practical leg load

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-print([x**2 for x in range(10)])
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
