804fe256690a05222aec256f481d4faea8c089e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:00:22 +0000

    make surprised little Pp challenge

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
