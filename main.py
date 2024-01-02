dccefefcbbaf6a4ec04c27e0e9bee0de460257f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:04 +0000

    goup convincing damage trust whether

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
