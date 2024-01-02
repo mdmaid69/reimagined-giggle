2f0e32a174f3ee17c3e52248af18df93c6d92c04
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:38 +0000

    bright track declaration slightly generally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
