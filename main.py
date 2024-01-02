43ae801c86efae00b00e5d31af2166037c1e1927
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:11 +0000

    first describe accident dry getaway

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_queue():
-        return collections.deque()
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
