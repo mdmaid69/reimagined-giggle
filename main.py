213f81e11fa89354d1115c05de977eb24b70ce9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:15:11 +0000

    he refer although makeinto luck

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+def remove_directory(path):
+        os.rmdir(path)
