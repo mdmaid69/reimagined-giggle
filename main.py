f0e6afb92736e63ba6a3e57fb887e960399f2db3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:05 +0000

    computer damp plan unite delight

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import re
-print(re.match("h.*o", "hello world"))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
