8849250c06ee965d39e765e5747f0f32a6320835
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:57 +0000

    design hurt swing choice fairly

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def find_max(lst):
+        return max(lst)
+  def calculate_area_rectangle(l, w):
+        return l * w
