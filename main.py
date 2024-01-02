ea2f16718af68112fe3bba5416bfd8bb19b3d672
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:32:32 +0000

    goup division stone keepup breakdown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(5):
+        print(i)
