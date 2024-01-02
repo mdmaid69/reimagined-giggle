71da7752db739a1f59dc9969862609b514d7827d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:04 +0000

    work with border seem impatient

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(numbers):
-        return max(numbers)
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
