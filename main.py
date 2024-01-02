b763ca27cbaa35a065c5134b09b8cd579e9eb560
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:47:09 +0000

    within freeze food flight stupid

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import platform
+def get_python_version():
+        return platform.python_version()
