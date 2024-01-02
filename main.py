eef015ea2741e824d9efece7f48a9791db782143
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:04:08 +0000

    instruction unfortunate might myself popular

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
