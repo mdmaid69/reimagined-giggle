056f5a46994922d40c0287d703fb4505c60c07c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:40 +0000

    patience danger sink about opinion

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def is_odd(n):
-        return n % 2 != 0
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
