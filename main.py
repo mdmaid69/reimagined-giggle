d2dc0f4f990bfbd4509445ed4bc8bed650529db3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:20 +0000

    Jj tongue control square hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def sort_list(lst):
+        return sorted(lst)
