d82df0d2859f8181c5eb37fc33487029ed118b6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:29 +0000

    forget always mind course somehow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
