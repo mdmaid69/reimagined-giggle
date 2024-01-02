319022984f0c7a2c5775b0626c05c64c110d6ff9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:23:15 +0000

    dear occasion bath angry birth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
