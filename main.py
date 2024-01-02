c1eda14020cd0bdd00ed9d00fe38af73f5742d13
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:41 +0000

    balance lunch real awake place

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
