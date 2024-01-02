7344ce2f4b0157362e27e62fa6dbe44f5aa73e6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:44:46 +0000

    attract row race turnon outside

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+  def subtract_numbers(x, y):
+        return x - y
