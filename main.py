248a4ed679c1d2ae3ed915ecc2408ba6fe9b0290
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:52 +0000

    scene comedown communication full different

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
   import os
   def get_file_rdev(file_name):
         return os.stat(file_name).st_rdev
