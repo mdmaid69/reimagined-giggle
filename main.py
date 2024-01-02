8222df915ae869daca27ec6e83e2d602e2034343
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:46:38 +0000

    back frame little hot keepaway

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-text = "Hello, world!"
-print("Characters:", len(text))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
