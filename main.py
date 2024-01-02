396ce709bb302adbeb0a2fd9843910c7fd41f67a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:34 +0000

    thinkof answer offer kick joke

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
