ef8428948fbef466e2f089e1c99372e25557914c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:00:02 +0000

    clever Ww future more further

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import time
+def get_current_time():
+        return time.time()
