a55504a20aa257b8de9175f8970934192de0b0a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:16:59 +0000

    guilty ourselves they ignore joke

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import sys
-def print_python_version():
-        print(sys.version)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
