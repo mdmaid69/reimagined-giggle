be9b6e78b9df86b73ab9673d777de43dc97a9d8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:16:56 +0000

    hurry bound either increase afterwards

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
