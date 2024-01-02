eb4e7b22af8a1b097bf246b7fb45cda6eccc130f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:55:15 +0000

    which strange wide study own

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+import logging
+def log_message(message):
+        logging.info(message)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
