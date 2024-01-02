4d7355bc5d02a6f6313954156eb476c48a5767d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:14 +0000

    translate whole far either strike

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def reverse_string(s):
-        return s[::-1]
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
