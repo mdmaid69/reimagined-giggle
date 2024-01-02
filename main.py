b58236d4a8698b26be2a907661035ebaa7cd3459
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:15 +0000

    repair practice kick nigga stick

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
