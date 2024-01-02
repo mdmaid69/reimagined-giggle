29ab3ea9d6e438b873516cf65a4451b940d332af
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:17 +0000

    tap pupil mine lie bunch

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def calculate_density(mass, volume):
+        return mass / volume
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
