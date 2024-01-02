e40a1a936886e9798cd7a1611b1747e3ee5ff020
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:01 +0000

    phone usual plastic hard sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import time
-def get_time_since_epoch():
-        return time.time()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
