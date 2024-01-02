84343c134bc438096383df7c62745a002bb17bb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:09:23 +0000

    apart address spend level shrink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
