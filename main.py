828c30b67b7fbb83ce5b7c69e6844eca89409ae6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:16:34 +0000

    second innocent please wave fail

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
