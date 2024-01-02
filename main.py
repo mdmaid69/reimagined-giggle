59227968b4d97270013408ce87c5a9c302e9ca18
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:38:49 +0000

    decision slightly bright ought deliver

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_index(array, item):
+        return array.index(item)
+def add_numbers(x, y):
+        return x + y
