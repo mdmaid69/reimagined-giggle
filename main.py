d1d020bbd451b5431e59ef4d4331ff0f7687ecec
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:31:20 +0000

    treatment small wing fetch score

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
