9c8040eaf6b2b00aaece8f947e83fc2c91772b50
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:25 +0000

    exchange takeout statement total niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import array
+def get_array_typecode(array):
+        return array.typecode
