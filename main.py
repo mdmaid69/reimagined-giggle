96d3b03c0ba03455b26e95b8f72eaefbc86d5078
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:19 +0000

    Ff offend sight glass sure

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Reversed:", text[::-1])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
