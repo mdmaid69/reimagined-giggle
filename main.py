c950e4caa0dacab961735bebf72eda58f1105e5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:21:05 +0000

    near skill accidentally foreign cheat

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
