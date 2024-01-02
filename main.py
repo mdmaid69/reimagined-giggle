56d9bc1a5a2316de0ae2747f25a7e9756d7328e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:57:21 +0000

    consider follow nowhere shrink brown

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import array
-def reverse_array(array):
-        array.reverse()
+import getpass
+def get_username():
+        return getpass.getuser()
+def square_number(x):
+        return x**2
