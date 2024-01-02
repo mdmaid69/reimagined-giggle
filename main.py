be712b1e0309b93d2086f12cebdcdba756944190
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:55:57 +0000

    grand cross luck so holdup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
