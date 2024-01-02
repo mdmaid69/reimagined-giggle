f6efbaf71e5990fd20f04d54cf701c13e8108287
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:13 +0000

    modern live water accept community

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
