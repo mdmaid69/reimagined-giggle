41c83d69c4002674521dabc248c3218bdf66b0d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:22:38 +0000

    flat all Ss exchange standard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
