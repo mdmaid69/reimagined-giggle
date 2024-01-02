a1964e628201ebc1bbec661836bb31014dfe5565
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:02 +0000

    Itself well neither time getup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
