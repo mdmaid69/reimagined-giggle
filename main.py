cda783c8a64562c8cb044500caaf8b595eaa219a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:41:48 +0000

    partner priority duty comment turnon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+def calculate_density(mass, volume):
+        return mass / volume
