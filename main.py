790676f7220a8041155e011e12852b802c5eb0bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:45:49 +0000

    abroad shout dance realize hope

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
