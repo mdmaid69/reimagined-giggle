71aff64f28d39f894f22d0f516762ac060901cce
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:07 +0000

    grand tradition shine inform target

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
