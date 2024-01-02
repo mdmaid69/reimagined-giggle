592817f3c72f1b45b29414f4c3356045da6a71b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:32:31 +0000

    exchange hollow splash pass peace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
