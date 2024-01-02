12c6b4558eabe05bf8f8a7cebb769b56bbdc1f97
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:15 +0000

    fire mean rule service Qq

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_volume(length, width, height):
-        return length * width * height
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
