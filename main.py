a199e9328c881b94a10fa0dfc5252a7288941796
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:33 +0000

    backwards backwards conduct sharp practise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(10): print(i)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
