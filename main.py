18f92b7cd11641645fe608ad95edd0f76893d9b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:19:01 +0000

    scrap giveaway lonely promise model

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
