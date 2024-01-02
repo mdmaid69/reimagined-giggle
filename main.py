5dd43745d6331a64b26b673a3ec6f76c76dcfa45
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:42:16 +0000

    brown meeting organize number approval

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
