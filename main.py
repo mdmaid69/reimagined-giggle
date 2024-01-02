4c2a81634243db52211b1579e77cf33e70c0184f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:13:51 +0000

    meaning window represent flat calm

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import numpy as np
+print(np.array([1, 2, 3]))
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
