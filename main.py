fe91dc74d7b817edc5a87443d269c345821934b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:16:12 +0000

    second waste prevent type during

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+text = "Hello, world!"
+print("Reversed:", text[::-1])
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
