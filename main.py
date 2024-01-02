665e6ef17697fe2a42f04d4344cd349b250cfff2
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:48:03 +0000

    ring common refer century comeover

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
