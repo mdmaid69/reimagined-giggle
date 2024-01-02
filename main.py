ce9031a900830007bea122278a1284a18d36a520
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:30:08 +0000

    circumstance could criticism century stiff

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
