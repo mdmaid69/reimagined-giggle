cfb3865af5d298ff903048bfae554bd2aed9f791
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:52 +0000

    goodbye gift discovery uponon discussion

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import os
-print(os.getcwd())
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
