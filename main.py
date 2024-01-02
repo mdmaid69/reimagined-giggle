7fe7816c4449f84e6bd11da673ec6ec40ff8682d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:59:49 +0000

    visit if breakout trust 567enemy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+name = "Python"
+print("Hello,", name)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
