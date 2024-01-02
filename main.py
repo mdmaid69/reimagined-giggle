ed68f0cf732b8fe4bb3d66627a75d2ae0307d1c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:13 +0000

    quality side hear increase change

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
