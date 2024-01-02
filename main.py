b04ad9aca7b12ebc4089788e7a755e4868428b38
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:47 +0000

    pretty boil life block give

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
