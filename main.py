ed88a7a2f3f051edc41d64653d39ca7a107d4366
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:38 +0000

    for range form always careless

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
