58818fceca359464347b7529eff24b7c9edb095e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:54 +0000

    actually pleasure fire rubbish hair

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def divide_numbers(x, y):
-        return x / y
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_pressure(force, area):
+        return force / area
