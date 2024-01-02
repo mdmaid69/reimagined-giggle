ab1fde1a6be8dadbbe988549cb52266e63ea5f6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:29 +0000

    every flow during production active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
