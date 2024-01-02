8115bf7516d7289e7ff1bf612272601c733ea59f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:36:51 +0000

    number word much behave strong

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
