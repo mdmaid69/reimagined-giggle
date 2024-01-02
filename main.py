1a636a84eba3fad671c34118d6292d0ff60a0268
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:08 +0000

    funny know save computer sail

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def square_number(x):
-        return x**2
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
