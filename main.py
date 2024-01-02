fdf4a30a2aaff1c909369e792990ec2af54923f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:07 +0000

    opposite friendly experience petrol coat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import time
-print(time.time())
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
