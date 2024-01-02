a909e3a657b1bf1ab60a710bb4c8901b60cdfa2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:35:22 +0000

    nor offer sorry increase record

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
