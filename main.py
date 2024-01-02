ce8bcfed15a2922302ee6013269bdb22274b5b23
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:33 +0000

    key pump store forgive container

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
