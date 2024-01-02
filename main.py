3821df43dee3566cb69404e8a7cedf44533c3e77
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:32 +0000

    treatment takeback science lucky loss

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
