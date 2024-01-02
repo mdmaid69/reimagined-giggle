66c50d9bc5271ae79f2778e6620b1050f9b98f89
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:12 +0000

    hurry rule fast he mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
