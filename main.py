634aa5b0e19fba47f2e3bfcad5156415b14d39f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:09:16 +0000

    sensitive pronounce hardly level anybody

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
