ebf4b9a1a8ef6d59817469879231cd34ea6a694c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:14:38 +0000

    argue wheel system taste only

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import collections
-def create_priority_queue():
-        return collections.deque()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def subtract_numbers(x, y):
+        return x - y
