8341b7e74770f22659f75e7bf9eecf0f8edd0a8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:46 +0000

    dry innocent tall every break

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def multiply_numbers(x, y):
+        return x * y
