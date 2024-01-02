22533bcc8ce13080977b41eeafc8f0bd285ffb9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:01 +0000

    dry influence tap goback(to) welcome

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
