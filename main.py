9119ca74c61f16d5be1b5302673b7364162325fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:26 +0000

    then unusual fact pattern late

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import tensorflow as tf
-print(tf.__version__)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def convert_array_to_list(array):
+        return array.tolist()
