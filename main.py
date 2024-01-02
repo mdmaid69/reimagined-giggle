d8b71acd76f82d92927851c1002b004b0477acf3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:46:26 +0000

    care hurt rely sensible crime

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
