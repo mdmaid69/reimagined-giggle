c8669be31a2db4d5bf4a05dbb3416e1cc52dacbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:22:07 +0000

    notice shut form list computer

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def get_string_from_array(array):
+        return array.tobytes()
