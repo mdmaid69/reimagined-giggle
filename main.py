f1f39c9ccc7f8bd1642e6fa521b64ed8f2239b6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:20:08 +0000

    exciting comeround century advanced taste

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def remove_from_array(array, item):
+        array.remove(item)
