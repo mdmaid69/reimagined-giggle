f94323d85ef08727e88fa1f0cdd28f101f29ee90
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:35:41 +0000

    innocent tip dress speak respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
