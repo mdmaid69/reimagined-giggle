ce25015ef4498e8821963998f27662ae5c9733f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:47 +0000

    breakdown someone frighten swim business

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
