f7700a94ef6c756fcf453ea6430f21277938b474
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:16:11 +0000

    exaggerate relative turndown land equal

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
