655c6edd807258c91dcf3bce957cc95b215c0160
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:28:19 +0000

    tie money far issue aim

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import collections
+def create_priority_queue():
+        return collections.deque()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
