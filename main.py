7fc0b6be9343eb43292ac7abd06ecd68f802ff42
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:57:15 +0000

    match winter direct book fence

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
