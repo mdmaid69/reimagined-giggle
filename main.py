1d2571f8c7c7963e5a3069c8cb74e775014a3ced
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:48:20 +0000

    her grade equal herself government

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def count_elements(lst):
-        return len(lst)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
