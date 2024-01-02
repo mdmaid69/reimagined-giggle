273c983b65b3370b466a0cbcdafb620f0ba52da7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:14:27 +0000

    difference perhaps disapprove arrive urgent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import sys
+def print_python_version():
+        return sys.version
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
