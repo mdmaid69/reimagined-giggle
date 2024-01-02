328bfd3458f125810548d6283258a21222c6bbc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:56 +0000

    probably takeaway phone poor night

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
