d29f3faaa5a7336ab6a9a4dc01bbcf3d66809fed
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:50 +0000

    tour takeover slice promise brain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
