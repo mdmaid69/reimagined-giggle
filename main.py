5bd403f780f2245a475e4d01cd21db55f38538a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:30 +0000

    meaning return affect bring load

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def convert_to_binary(n):
+        return bin(n)
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
