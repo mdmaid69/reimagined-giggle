b53923c37010edf301ce689666e14ac959ab3346
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:05:06 +0000

    name strength thank 564encourage climb

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
