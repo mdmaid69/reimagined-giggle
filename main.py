6fd43464f881c5fc1209021db604e75af7b1546b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:02 +0000

    offer myself 565end run obtain

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
