39a501684e69fe2517ea1a3b7919f6e88095b9a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:23 +0000

    drive child which spot school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
