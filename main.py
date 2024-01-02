045912e5e1069336e485cd98d037d1a75fbabdec
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:53:27 +0000

    nose hole keepaway opposite rule

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
-  def convert_to_hex(n):
-        return hex(n)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
