58f75f2efb7d08a3690fab9100e10d61b1e806d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:20:10 +0000

    recently flavour travel control giveback

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def remove_from_array(array, item):
-        array.remove(item)
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
