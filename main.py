4af266e944199200f5f46a365dcd87c6d3f7703e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:39:22 +0000

    actually alone suffer national personal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
