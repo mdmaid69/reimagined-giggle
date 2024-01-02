05567f452a200996d8367fe0fc8f6bdcc7ead9a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:15:38 +0000

    somehow sit entry wise start

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
