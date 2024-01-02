7eef33358cf5d2ee2b0e54b2f98e6e20b33801bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:21:10 +0000

    table share unkind danger sell

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import math
-def calculate_cosine(x):
-        return math.cos(x)
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
