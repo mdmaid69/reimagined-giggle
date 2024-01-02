7bed4a8aadff3cfcd9de3bdc45d43067e22a4261
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:07:19 +0000

    relationship storm swim sentence zero

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_list():
-        return collections.UserList()
-def is_odd(n):
-        return n % 2 != 0
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
