f49c1c688590f7a8b81feb5365f2c438ab3326f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:01:40 +0000

    hurt fair discuss male chase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
+import array
+def remove_from_array(array, item):
+        array.remove(item)
