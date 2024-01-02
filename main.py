cd669bb088e1900e1af0424e0cd41e856dc34408
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:11 +0000

    operation closely plain safety lesson

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def count_elements(lst):
+        return len(lst)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
