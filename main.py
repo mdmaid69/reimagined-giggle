63cbed941b958ed75f3e3585a484db62c85fbd0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:30 +0000

    tough strict course entrance profit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import datetime
-def get_today_date():
-        return datetime.date.today()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+def find_max(lst):
+        return max(lst)
