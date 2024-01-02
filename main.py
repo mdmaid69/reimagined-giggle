fb04cbf1f7eefbdd2618384c9ef5fd54c66a0df1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:52:36 +0000

    press use workon pleasure suppose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import math
+def calculate_error_function(x):
+        return math.erf(x)
