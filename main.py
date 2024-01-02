cd23da52a9e8be616a9ef69e9a2d1c005d8948e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:00:40 +0000

    order deserve wheel that cause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_str(array):
-        return str(array)
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
