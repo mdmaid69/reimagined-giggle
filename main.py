89e452c8ee0c9988ed5379483e5eebe3dff3df5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:31 +0000

    chat intend genuine side arrive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
