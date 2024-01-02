e26ffbd874861f478d398d96c8056e345cc23c7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:48:07 +0000

    well use receive bar blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
