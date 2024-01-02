8a30aa09807993a0e89e55fe9c76894616e1bf1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:17:14 +0000

    573entertainment hide plenty cry takeoff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
