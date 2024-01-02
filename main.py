f0c15a471e3de4452ec8b342707224ff9057751e
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:03:48 +0000

    build afternoon closely agreement tidy

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def subtract_numbers(x, y):
+        return x - y
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
