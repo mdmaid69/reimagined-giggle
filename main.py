786828a6eae63638902744d444178cee5d07d35f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:15:22 +0000

    thinkof after our disapprove influence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-for i in range(10): print(i)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
