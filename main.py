9a33770816042f86667201555fccf9b208dcd215
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:49 +0000

    up where Qq own date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import sys
-def print_python_version():
-        print(sys.version)
+def reverse_list(lst):
+        return lst[::-1]
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
