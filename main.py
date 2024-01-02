d45b2f4364bc74786a9f53c05cbc75651b5ea57b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:54 +0000

    university accordingto guess keep home

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
