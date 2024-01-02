d1a44dc49601bc78443e36de61c8439d26eecbc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:25:54 +0000

    sure damage shout importance upstairs

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
