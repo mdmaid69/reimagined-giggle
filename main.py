b122d69fbaae8f5c1e2c82042d7ed6d0f04e37bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:14 +0000

    stir above consult choice criticism

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+def find_max(numbers):
+        return max(numbers)
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
