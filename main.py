7617a5d940f683e6a7d1fc7a8e7b699d0225d552
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:28 +0000

    music old excited Cc between

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-print(time.time())
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
