71a72fc55691fd3140446eab364eb608046f8cf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:21:18 +0000

    profit difference effort accidentally off

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def convert_array_to_list(array):
+        return array.tolist()
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
