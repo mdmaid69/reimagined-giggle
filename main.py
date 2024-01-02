6ae5e034ea05865dfe5ebd3769bba791c1a375a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:21:20 +0000

    period body protection yours family

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
