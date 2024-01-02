207c2541410dd7b011bc0bb68fc48370c6d81034
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:19:38 +0000

    how Zz save match gradually

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
