04204aae9fc8efdb12b5362ef78abe5e46b2e28e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:22:44 +0000

    prevent comeout relation fat getout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def convert_to_binary(n):
+        return bin(n)
+  def sort_list(lst):
+        return sorted(lst)
