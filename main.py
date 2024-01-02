e3f2172e6e22bd373e5a5b697af1a8b702634cc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:33:41 +0000

    Rr profit study tight tip

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-for i in range(5):
-        print(i)
-def calculate_area_circle(r):
-        return 3.14 * r**2
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_current_directory():
+        return os.getcwd()
