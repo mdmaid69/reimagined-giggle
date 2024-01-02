5648b5ee34cfae89f6b0f658dca5811a28c8e814
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:19 +0000

    cough experience well work children

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
