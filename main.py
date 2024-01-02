518b260b975b305bb295e74960b29cf51b9044fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:38:33 +0000

    shock praise notice the print

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
   import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
         plt.show()
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
