2ace24079f4146f68c79f8a0d7e210d617e50ea6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:50 +0000

    scrap centre bye complete exciting

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
