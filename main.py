6ea42e05981d11a31fd0e87fbde6951c589fa0b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:12:40 +0000

    pretend content slide loose show

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def convert_to_binary(n):
+        return bin(n)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
