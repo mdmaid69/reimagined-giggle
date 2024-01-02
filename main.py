3ad0cc1f6da52c385a0e053e6a127ea1280c21c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:12:43 +0000

    trust Pp score absolutely left

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def reverse_array(array):
+        array.reverse()
+  def is_even(n):
+        return n % 2 == 0
