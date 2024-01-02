649bd19bfba49c3321fccc10953bef66b8798b4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:15:27 +0000

    view top desperate communicate unit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
