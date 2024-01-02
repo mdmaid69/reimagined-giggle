89ebb0ec433360ed5ec440abf4adedb4b25e5a5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:37:35 +0000

    never around on justice wash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def delete_file(file_name):
         os.remove(file_name)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
