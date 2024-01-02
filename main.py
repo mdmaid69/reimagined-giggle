885b3c637fa5288570aa669ce8b13ac9faa79cc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:35 +0000

    freedom score risk promise responsibility

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
