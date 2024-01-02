e181cec441460fa5048ca8dc0ee846455b468182
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:55 +0000

    beyond substance tap advise 573entertainment

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
