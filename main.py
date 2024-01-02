602761a0f84033bfab948c9b1741e997a9b25196
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:22 +0000

    as sight ground mother walk

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
 import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
