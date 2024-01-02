ab3bbb21a0a38037364c8f3d0dd1d5ac3c796ecf
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:31:27 +0000

    shock heavy different better better

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+from collections import Counter
+print(Counter("hello world"))
