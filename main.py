b3a521df7660dd3322f0b97bccf75d7b975ef8dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:07:55 +0000

    anything avoid scrape content border

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
