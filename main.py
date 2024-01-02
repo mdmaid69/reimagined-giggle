ed0a7d89fd46f3af404b0f849fd664ddaec35e29
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:57:58 +0000

    direct oil possession noisy kill

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
