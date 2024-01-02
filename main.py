5cc71a4daecb50c8495df5f1ed6415d654e0e461
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:48 +0000

    announce along chase comfort switch

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  def get_base_name(path):
+        return os.path.basename(path)
