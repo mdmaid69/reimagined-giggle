65707aa20e018e2f00a2d470484479c91b1105fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:15 +0000

    excited unusual throw illness train

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import time
+print(time.time())
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
