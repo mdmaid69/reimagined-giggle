60857a609ea5b07fd18c32f214bb45cf0985b4fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:41:00 +0000

    fight concentrate Jj medium before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_average(lst):
+        return sum(lst) / len(lst)
