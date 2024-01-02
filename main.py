e2f9bdfaeca7529c14010984a1af75ee4e71f3b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:51 +0000

    age physical forgive giveup method

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def add_numbers(a, b):
-        return a + b
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
