a0349c971e3682eebe054c4819a38fa887982713
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:33:06 +0000

    pull wall could touch but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
