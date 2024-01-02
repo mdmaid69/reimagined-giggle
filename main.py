8ce23b3f94bdfa232851d030c4c510101267be4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:30:01 +0000

    comeout hungry spoil sometimes warm

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
