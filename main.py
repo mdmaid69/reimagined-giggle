2ea0b89353a4475755333369e7252fed40cee506
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:27 +0000

    trouble middle maximum speak by

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+text = "Hello, world!"
+print("Words:", len(text.split()))
