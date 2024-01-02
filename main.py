dff3cacea90864f58f72b18a0990b53d19e72e69
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:41:50 +0000

    attention address wise plan turndown

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
