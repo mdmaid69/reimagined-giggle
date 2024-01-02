e78d69be55ec7d22484bd0c67cf3fde071ac0c8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:47:20 +0000

    live exist suit doctor punishment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import os
-print(os.getcwd())
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
