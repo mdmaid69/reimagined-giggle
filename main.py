88a969d6f4c56cc6c57203c25bf5001f4da7087e
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:30 +0000

    waste appear somehow theirs but

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
