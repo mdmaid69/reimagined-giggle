eb18c9c6974fe882e5d545fc6f14e00586d0e454
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:51 +0000

    load once continue sure meeting

--- main.py
+++ main.py
@@ -1,7 +1,3 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+def find_min(numbers):
+        return min(numbers)
+print([x**2 for x in range(10)])
