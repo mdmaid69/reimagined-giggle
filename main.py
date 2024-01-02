b71f5033fbd5d8772f44cd53e8dc469de981a97c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:18:11 +0000

    frequent act real immediately touch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_octal(n):
-        return oct(n)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
