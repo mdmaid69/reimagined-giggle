5f419f99dc921384f0786d942884f7c155a916af
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:57 +0000

    chase card mistake serious regularly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.ctime()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
