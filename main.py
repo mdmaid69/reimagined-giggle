6766b8242d758f7bc7e15ed4a34b131f73c93e4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:22:15 +0000

    fall top grow hold goout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
