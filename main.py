e5d9f7ba318d7f75096ea886270c61c9e80e6b98
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:57 +0000

    limit container government sweep mainly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
