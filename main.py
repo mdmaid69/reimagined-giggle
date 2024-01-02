6819d718c51805d7b37bf8c4cb38d3014b6537ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:39 +0000

    unless pin keepfrom excuse flavour

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
