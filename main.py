0ffcc1e4e2a97ebbeabfe4b21c17b2ba5763efdc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:21 +0000

    modern difficulty stretch cause last

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
