08710312724e4f4fd3a352f7ff81c013f31ddd62
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:10 +0000

    Tt theirs clear programme split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def convert_to_hex(n):
+        return hex(n)
