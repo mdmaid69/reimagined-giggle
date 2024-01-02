f6e269bd88f043e1cda4cca722664b03dbdf94e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:41:48 +0000

    comedown against connect slide backwards

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
