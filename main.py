513332121fb7776f8601d5b5ef3d2180bac8ef1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:01:04 +0000

    worse ride tidy side main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_current_time():
-        return time.time()
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
