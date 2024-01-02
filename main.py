1de485a153e86be60001b0260601ead20131e195
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:00 +0000

    block generally definite flavour quick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
