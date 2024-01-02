86eebaae62487650a9c8cd94b64c1aa03b3f908d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:41 +0000

    result Hh personal equal fire

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def square_number(x):
+        return x**2
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
