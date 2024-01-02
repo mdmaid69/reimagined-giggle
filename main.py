0774abef467bab698d1fde9655c572505fdc4650
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:22:50 +0000

    smell go Mm along wear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
