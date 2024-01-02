0a42b7fe5b3988f78ba28c4846b2ec48509ac40b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:33:46 +0000

    content arm tall force abroad

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
