a9b915d1cc90d25eedc3751ab140e4374507a7e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:34:30 +0000

    special name valuable by issue

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
-  def cube_number(x):
-        return x**3
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
