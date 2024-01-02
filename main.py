1c1029edbf4db6b2fe64deb44a9b629c896c3e73
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:34 +0000

    broken settle Zz moment image

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_work(force, distance):
-        return force * distance
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
