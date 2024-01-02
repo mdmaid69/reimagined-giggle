f9e9af76a93fa59a6a1ccbc89c3370a27b2b8e95
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:26:31 +0000

    holdout miss careful cover bear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-def calculate_acceleration(speed, time):
-        return speed / time
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
