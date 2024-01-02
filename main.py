53ecf2103c57f4290f3b61de172173cb476b2314
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:01:34 +0000

    release unlikely offer comeout freeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import getpass
+def get_username():
+        return getpass.getuser()
