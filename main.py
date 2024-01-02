97ca1fdcd240d60fb01b22c3152c91edbcb5f619
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:22:54 +0000

    prefer create attach step school

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import sys
+def exit_program():
+        sys.exit()
