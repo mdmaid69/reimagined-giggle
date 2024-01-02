7db85a233c315723de23915cdbaf203d2325f301
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:56:50 +0000

    refuse refer soak choice strike

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_list(array):
+        return list(array)
