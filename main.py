565c397abc50d4a8dbea506894b0fc643b3b162e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:41:35 +0000

    charge offer even learn strange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
