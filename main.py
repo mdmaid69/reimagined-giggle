9506f85ccb94c8fba2986489c1a0cacd8b1d8c25
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:46 +0000

    guide cope main choice determined

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
