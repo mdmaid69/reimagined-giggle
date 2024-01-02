75412a755ec1fbcab55b14c1b29d3bffcecda043
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:59:49 +0000

    stretch post concerning quality scene

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
