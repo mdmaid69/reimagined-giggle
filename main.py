d9d58c1ab99bfb9aa4f39ce9eb56122e750d4ffb
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:07:26 +0000

    lend summer disgusting drop begin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
+def find_max(lst):
+        return max(lst)
