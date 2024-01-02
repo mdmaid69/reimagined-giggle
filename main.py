c93bbdb1392027975a0a8b0ab3672bde1d85b0a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:44 +0000

    violence check attitude workupto square

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
