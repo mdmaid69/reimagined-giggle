73bd2a1d6ad351b1ac4c0877900810b1f4a981d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:00:05 +0000

    trick punish stop sink sticky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
