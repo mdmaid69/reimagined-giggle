d7a77f024d7b438788be7d158b26276c7042e6e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:25:03 +0000

    promise appear impressed play arrival

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
