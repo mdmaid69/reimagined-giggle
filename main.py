5055ec7ca9958699667d33b02965cde30b7c787b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:55:08 +0000

    natural rise spread union taste

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def convert_to_octal(n):
+        return oct(n)
