8b2fc843f1db45cdd282c681b5058e4d12b8e14e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:43 +0000

    matter aim love stiff anyone

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  def square_number(x):
-        return x**2
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def reverse_array(array):
+        array.reverse()
