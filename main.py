fca5cff0e7799496844bdba68d65f31e29263b27
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:57:18 +0000

    although finally punishment connected stranger

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
