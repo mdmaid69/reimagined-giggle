a65be727d7406b4c9183d9b2d8f3cc3074009a46
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:31:28 +0000

    even punish hand justice meanwhile

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
