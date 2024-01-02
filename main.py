84027502e8d8b0c361d7e0a5b6bf679c3994ff61
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:36 +0000

    race sit circumstance say name

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import getpass
+def get_username():
+        return getpass.getuser()
