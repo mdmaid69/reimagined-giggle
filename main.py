2af8818b5436a07ba5c94a4f371767b2a863602e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:25 +0000

    steal many anyone almost less

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def count_elements(lst):
-        return len(lst)
+import array
+def get_array_as_set(array):
+        return set(array)
+def calculate_work(force, distance):
+        return force * distance
