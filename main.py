c5429149dc6b691b2794ad6430ce3b28ee4ff8d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:27:56 +0000

    attention keepoff love strange generous

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
