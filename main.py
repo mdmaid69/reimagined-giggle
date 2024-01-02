302156651d373307b2b52836f7cf90888ce71b24
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:28:07 +0000

    turnon thinkover lot represent total

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
