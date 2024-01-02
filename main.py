5a1fe4d37add77eed710f3eec1f26e15df28187a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:25 +0000

    justice regret Pp aware next

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
+import re
+print(re.match("h.*o", "hello world"))
