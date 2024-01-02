886aeb1b35ef3a6009de7c7374a4e8cc0cbac481
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:46 +0000

    birth many break attitude also

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
