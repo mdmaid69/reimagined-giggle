1c6968c9c18e0c8e70003fb2584bd689548eeca0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:28:46 +0000

    approve whichever unable port weak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def multiply_numbers(x, y):
-        return x * y
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
