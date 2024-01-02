6827ef0a410cb27f903cf67b0f1d7829d0d7ff8f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:45 +0000

    certainly branch narrow temper slow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
