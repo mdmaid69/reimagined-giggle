b1e3af88b296e0b9c058bd6280d3c9ff2aecd3c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:51:57 +0000

    independent match board keepup stare

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+i = 0
+while i < 5:
+        print(i)
+        i += 1
