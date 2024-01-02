6ca5a1d72e20d12ef42f05c871079b0903cbfd0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:37 +0000

    bath independent forget trial report

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
