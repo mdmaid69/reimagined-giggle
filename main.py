33876e62f1b255a64d7de85a30b4cf53327f6129
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:44:22 +0000

    heavy each body gate snow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import re
+print(re.match("h.*o", "hello world"))
