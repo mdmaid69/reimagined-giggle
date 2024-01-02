95fd68c4a28a9e53717e2114e59d1a23e6169399
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:36:54 +0000

    getup news increase fine town

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def count_elements(lst):
+        return len(lst)
