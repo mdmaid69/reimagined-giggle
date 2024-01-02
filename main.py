d14ed0ed55398b9a7ae3feebc5f32f946f19bcf9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:13:57 +0000

    about increase band soil have

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
