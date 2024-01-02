73ed208ea1c298a56ac6d33457a4586bc7529067
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:15 +0000

    even snow size notice convince

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
