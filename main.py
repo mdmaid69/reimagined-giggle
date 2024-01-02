cc82d57642efd6a184ccc2eef26daa7b54cb8503
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:26:19 +0000

    properly combine justice among storm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
