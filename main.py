fb837ca14429ee59167c9c69863769cc9a351ac3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:16:22 +0000

    physical sister early border size

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def square_number(x):
-        return x**2
+import platform
+def get_os_info():
+        return platform.uname()
+  def is_odd(n):
+        return n % 2 != 0
