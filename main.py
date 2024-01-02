acbe6775255d6c5e8c118c20e62cbb223d30eb82
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:07:02 +0000

    turndown well design convenient brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
