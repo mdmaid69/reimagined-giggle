6848bb306644ccf292ec64728f68557da6974501
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:21:42 +0000

    drink book street receive mix

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
