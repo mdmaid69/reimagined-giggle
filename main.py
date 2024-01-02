c803ef9c20be5f2aa7e4c19a28f145038de0b9a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:42 +0000

    Gg transparent importance fill squeeze

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
