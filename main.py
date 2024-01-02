726318b539b5bc5d9b319147b124e56907ad6444
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:12 +0000

    flight anxious cap issue settle

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
