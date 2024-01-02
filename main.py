d91d7fc1c5e4a51ca2c552f5666b10cb4196e073
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:35 +0000

    life print involve correction fun

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
