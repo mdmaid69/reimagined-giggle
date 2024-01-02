fed73c590b3806005695d88c6a8a8f838097ae61
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:31:32 +0000

    like Cc increase squeeze gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
