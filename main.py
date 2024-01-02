03ef51a7bf270bfcfb775b56b5fbea2cc70cda1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:07:14 +0000

    179bite create rank few forever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
