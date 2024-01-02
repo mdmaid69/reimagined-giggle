bcd4f4db59551b40bae992b3ea299449ee977781
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:38 +0000

    accidentally down strength concentration impressive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def is_even(n):
+        return n % 2 == 0
