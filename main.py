fd5dae07de68a56e8eea2061b56c946704a2b89f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:53 +0000

    general lookafter row range obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-  def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
