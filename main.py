664a6c6014b1c1900fdda015b40147e51a900445
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:18:34 +0000

    slightly dream building interview within

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
