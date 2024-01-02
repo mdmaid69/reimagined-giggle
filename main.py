890c5c2a7bc59b9e7e1fcb0c94db484b62508877
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:28:27 +0000

    side common pupil rob lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def get_array_as_int(array):
+        return int(array[0])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
