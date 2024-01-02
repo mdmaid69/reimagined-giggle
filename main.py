2fbe7a4512519708d7c8604beabe9d0edad710a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:35 +0000

    reply turnover area design rich

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_time_since_epoch():
-        return time.time()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_max(lst):
+        return max(lst)
