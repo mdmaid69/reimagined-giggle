d5ac318102bcf284de65ef7d5a435b90ca19575e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:51:13 +0000

    emphasize train why but imagine

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+def find_min(lst):
+        return min(lst)
