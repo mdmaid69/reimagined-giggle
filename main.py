2fbe0cdb6f905147d108f20d0c74cdff77632239
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:50:01 +0000

    wheel Cc event rarely light

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import time
+print(time.time())
