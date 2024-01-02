82f23b77b70c7051c27c1ece13c7aec5171e2192
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:38 +0000

    rough comeover able scared some

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
