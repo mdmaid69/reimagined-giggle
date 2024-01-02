2dd6833d8c89ae7fd66bfa76066163bab5c03799
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:43 +0000

    danger principle past smell mystery

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def add_numbers(a, b):
-        return a + b
+import os
+print(os.getcwd())
+  def count_elements(lst):
+        return len(lst)
