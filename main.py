03a9f9de1a5dd201984a23a00a7c7613852f0c56
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:19:10 +0000

    everywhere violent leave instrument confidence

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
