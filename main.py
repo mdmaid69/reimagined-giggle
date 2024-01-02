a9da6f93ce782dd1e8f1c988d964edf5cf94a326
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:40:27 +0000

    key frightened offer work understand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-def sort_list(lst):
-        return sorted(lst)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
