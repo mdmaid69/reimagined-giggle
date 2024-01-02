9c65986cb52d29512755d622c702eb2a2d8b6ac6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:19 +0000

    oil Ff fall car breakout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+text = "Hello, world!"
+print("Characters:", len(text))
