94ab4a53194b6d1f0510030db6ce63a34ccee12c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:42 +0000

    cut from equal sore callout

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
