908334a78792b93312e2cb79ad626d514b319b19
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:57:14 +0000

    566end cross comeoff classroom movement

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-  def convert_to_hex(n):
-        return hex(n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
