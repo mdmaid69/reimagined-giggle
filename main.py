39f032ce9afbd17f778730fb4f36862bfb9ea3a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:20:36 +0000

    and test result undo open

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+import math
+def calculate_arc_sine(x):
+        return math.asin(x)
