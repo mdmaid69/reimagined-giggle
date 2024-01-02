d55ffd1f81aec7ef95f478e11f1ef003679838ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:15:20 +0000

    cheap legal unless completely point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
