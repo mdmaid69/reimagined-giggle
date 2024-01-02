1520a1f5c492c7597fcfd218f17e386010eaba3f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:00:23 +0000

    road mouth that size extraordinary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_inode(file_name):
+  def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
