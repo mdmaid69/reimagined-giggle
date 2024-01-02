5fc7a676576839f913d9b0a4eefd37ab9fa6dc60
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:03:46 +0000

    bother yourself guide plan practical

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
