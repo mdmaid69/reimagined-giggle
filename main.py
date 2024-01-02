886fc6da50cb6487596bcad4562e1ded2aded9c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:43:42 +0000

    here move flat give bunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
 def calculate_acceleration(speed, time):
         return speed / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
