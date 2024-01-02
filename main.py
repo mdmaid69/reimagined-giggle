3d38c9cdb7f8134d2c6d303a146e04e4afb5c4e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:54:15 +0000

    rough region furniture mess stop

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
