0f8dee487027ef648d784ed772a254d0437c9e56
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:09 +0000

    ordinary vision please compare but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
