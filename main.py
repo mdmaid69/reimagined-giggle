4d25026fe154274607c082dc29f434bbd5a01e48
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:45:39 +0000

    letter regular sea reject wheel

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
