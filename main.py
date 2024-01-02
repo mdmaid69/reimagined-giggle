a4ace25011b138d85b8a2cc95bfeb28c274257f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:58 +0000

    other juice distant stare getup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
