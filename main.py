04e6c27c879d024c494cb0faceb95cb3dee3206f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:47:10 +0000

    contain lay convince left false

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def subtract_numbers(x, y):
-        return x - y
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+print([x**2 for x in range(10)])
