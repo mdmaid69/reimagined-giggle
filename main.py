5b985f3e8281a3888b9dd3a24f1bfa2d7cd5040f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:51:59 +0000

    sale beside satisfactory dangerous since

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
 import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
