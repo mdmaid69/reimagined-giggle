c9e983550fdaff28ff4a666c17510ce3f8164024
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:00 +0000

    borrow fine cruel immediately talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def reverse_list(lst):
+        return lst[::-1]
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
