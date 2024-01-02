566b4e770479ededef3a2819ee8f30a516eaa4c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:13:40 +0000

    science necessary example environment criticism

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+print([x**2 for x in range(10)])
