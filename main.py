e0049f5642d4cc8f75e9be340206b306b89a5c7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:04:51 +0000

    still choice once complain house

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_even(n):
-        return n % 2 == 0
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
