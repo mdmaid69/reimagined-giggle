b760d8c5a497101d7e4a708d69148a1a37f71f3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:52:29 +0000

    before fresh remove Ll dull

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
