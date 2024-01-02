d8a37af0b4cb1032fab16d606f3deedb8367839f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:11 +0000

    vote trap child catch only

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
