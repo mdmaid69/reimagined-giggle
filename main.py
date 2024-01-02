35a83fc43d04ac52e9baaca23301b9e6a9d2b9f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:59 +0000

    through year minor rain precise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import sys
+def exit_program():
+        sys.exit()
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
