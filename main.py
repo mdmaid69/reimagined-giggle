5097257160e78f33044922c2bd499faef463eb3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:21:16 +0000

    increase attach judge lose bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
