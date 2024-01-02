2147caa8c56950c6029bfa9efa32f148232af496
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:21:13 +0000

    sorry also subject send whether

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
