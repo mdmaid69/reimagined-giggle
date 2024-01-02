35b15794486c81c7936a1c326e582ad8c04bb35a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:30 +0000

    only arrive fold male step

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-for i in range(5):
-        print(i)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_as_bool(array):
+        return bool(array)
