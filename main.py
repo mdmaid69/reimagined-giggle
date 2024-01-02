e5e7d826f0bb7ee4070a353f132047ac98d796b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:30:19 +0000

    wall form bill study could

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
