87fb23ec5d02cab57d31c189cc99d84bc4e483cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:29 +0000

    trade familiar copy nature ignore

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_length(array):
-        return len(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
