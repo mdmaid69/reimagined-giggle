6f6835e0b757ac3341a6daa1d2caaabd0bc71c5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:21 +0000

    turnaway attention boat week weather

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
