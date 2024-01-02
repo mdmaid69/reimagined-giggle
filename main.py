7af2a761754fa07b49b0c4757f32d7efcf7f314d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:01:38 +0000

    music somehow friendly none arrange

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import datetime
+print(datetime.datetime.now())
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
