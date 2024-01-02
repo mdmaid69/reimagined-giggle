5e8bd08a5239eca2d34327d004a0dbc11772d6e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:54:51 +0000

    voice material listenfor accidental Ee

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
+import os
+def change_working_directory(path):
+        os.chdir(path)
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
