af5b3787d7dca7b63cc18fcb0939aa5367c5f4aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:06:10 +0000

    whistle sick summer pen key

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_current_working_directory():
+        return os.getcwd()
+def greet(name):
+        print(f"Hello, {name}!")
