f9cbdb3fc1087e2d15136739fcf3c924a78469a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:58 +0000

    transparent century turnon odd wind

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
