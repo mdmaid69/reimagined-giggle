21543eea19317a33eb0d71a150005ca17b361922
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:02 +0000

    tragedy he date weigh complete

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
