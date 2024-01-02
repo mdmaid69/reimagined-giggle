06913a5d30d837a3778a4d6afb9a5ff0cdcf091e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:54:18 +0000

    keen Tt well set thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def calculate_density(mass, volume):
-        return mass / volume
+import os
+def change_working_directory(path):
+        os.chdir(path)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
