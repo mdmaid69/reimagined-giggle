72c92af78b3c7bcafaed2f239d6915af24ade2be
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:55 +0000

    door present downstairs reliable interested

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def find_min(numbers):
+        return min(numbers)
