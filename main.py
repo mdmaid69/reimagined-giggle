2c0fb40b4c8b3132026925dca46f1d454ea3d110
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:04 +0000

    excited near software full brave

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
