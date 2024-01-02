20d67527c48814a521a177028a872eae1973556d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:13:17 +0000

    rain respect convincing honest party

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_force(mass, acceleration):
+        return mass * acceleration
