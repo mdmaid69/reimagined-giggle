cc7c1cf381bbfb8054e7470fec7d7cbe534daf32
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:12:50 +0000

    bunch cross react single silent

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import time
-def get_time_since_epoch():
-        return time.time()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
