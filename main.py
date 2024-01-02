87dd815474b3d195ff8d0f9021f2428b00ed08a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:19:47 +0000

    kick fire bright word loss

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
