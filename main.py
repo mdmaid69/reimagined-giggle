15e74b68caba4ca16f40681ac2b522e7fb05b83e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:35 +0000

    try pinch minor heat slight

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import sys
+def exit_program():
+        sys.exit()
+import array
+def get_array_as_float(array):
+        return float(array[0])
