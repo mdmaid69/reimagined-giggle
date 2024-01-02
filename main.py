cb0591e8efdfa80c6b7c5cda8b8fa1e1a41a6530
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:00:35 +0000

    name invite copy break precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
