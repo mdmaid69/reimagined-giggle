1275d946720630ce0032ac62f28f9313b2067602
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:46 +0000

    rely natural bringup door beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
