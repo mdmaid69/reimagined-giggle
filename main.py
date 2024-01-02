baf128b6e9ff6999a36c673669438788ed2da819
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:17 +0000

    mark person second pause real

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
