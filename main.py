51c2f5d4e540a14a5bc2a4307931620af0a12771
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:00 +0000

    fence home spoil decide reasonable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
