37894d33a4689556c219370de6fc75ecd24673fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:15 +0000

    reliable ring task however uponon

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-print(sum(range(10)))
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
