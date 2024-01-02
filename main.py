0597f59d0ed9100713c4fdc0bcfbd02240db9c9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:23 +0000

    squash mine petrol innocent name

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
+def square_number(x):
+        return x**2
