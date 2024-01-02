10b7689fb25d491632da7ddf5adb3ccbffd65d7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:32:41 +0000

    flat dish middle plate long

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
+import collections
+def create_priority_queue():
+        return collections.deque()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
