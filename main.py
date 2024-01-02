d357b24dc42042b3834ac85be00d3b201f4a1fa6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:52:49 +0000

    show organize connect beginning some

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
