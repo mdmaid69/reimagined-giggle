f8f13c4c05c0e8f606058baa423e547346a530ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:01:51 +0000

    anybody convenient even dance bury

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
