9a7a0cee0b299483197bfbd7ea796466e634e00e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:02:10 +0000

    reserve greeting loose breakthrough match

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
