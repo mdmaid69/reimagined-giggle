7ad82c2ff49524bc5268061eb55a431edbcb3fbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:27:39 +0000

    attempt dance trick Cc afford

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
