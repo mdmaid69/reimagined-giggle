5bd14ae2ce307198de968ca65d46cebce8df9881
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:37 +0000

    tight wide deliberately willing married

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_item(array, i):
+        return array[i]
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
