6b094c99a2ac5dfed99ea397448d6b81ecc83c92
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:58:53 +0000

    together market doubt steady sore

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
