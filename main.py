0ad0b2314311f460383fe45f48b92a072254dade
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:10 +0000

    contents turndown fact opposite fear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-text = "Hello, world!"
-print("Characters:", len(text))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
