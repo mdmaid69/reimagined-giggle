8f4d13cb8f387960ada3009e30ced92f017583b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:13:33 +0000

    pile game mass change visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def greet(name):
+        print(f"Hello, {name}!")
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
