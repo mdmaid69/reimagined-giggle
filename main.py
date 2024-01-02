42aa2e45638f9142baba493b2c1b0494a23e6b33
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:50 +0000

    dear effort mainly aware rely

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def clear_array(array):
-        array *= 0
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+def multiply_numbers(x, y):
+        return x * y
