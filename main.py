5fc5ee4da419899e5c6cf4a44718190e2c44b252
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:54 +0000

    push equal hang wave actually

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
 def add_numbers(a, b):
         return a + b
