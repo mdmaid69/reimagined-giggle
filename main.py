294b426333aaebfc4fb1256ec6c86601a0d619fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:12:54 +0000

    pollution account read recently pick

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
