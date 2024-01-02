3e1052443bd77b0c02e08480064d915b87ae030d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:39:09 +0000

    post breakinto final then careful

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def calculate_arc_tangent(x):
+        return math.atan(x)
