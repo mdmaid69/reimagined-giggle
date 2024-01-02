d52564a9796e19b57002578927f2f7d0f66c0e1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:35:24 +0000

    deliberately space limit takeout wise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_as_str(array):
+        return str(array)
