5b9e616164bfc6ea76636cbfe120ca5d814f6fce
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:24:25 +0000

    stretch except worry mainly check

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
