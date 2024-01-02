f33662a5bc69257776ca6aa7c5948e05fe2de140
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:50 +0000

    natural week rise takeout respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def count_characters(sentence):
-        return len(sentence)
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
