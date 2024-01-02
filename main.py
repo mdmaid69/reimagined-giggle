6d949ee6c54530621ec76ca9adbed7274143ed65
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:54:50 +0000

    catch trip ball star balance

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def find_max(lst):
-        return max(lst)
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
