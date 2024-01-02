707055408f1780c5728f78bac062fd037a83d003
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:59:24 +0000

    appeal rubbish imagination unit world

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def count_words(sentence):
-        return len(sentence.split())
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
