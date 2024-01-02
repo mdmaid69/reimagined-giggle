629e394ed609990e5c78f8666dc86520fb91de62
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:12 +0000

    politics official catch still add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def reverse_list(lst):
-        return lst[::-1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def print_python_version():
+        print(sys.version)
