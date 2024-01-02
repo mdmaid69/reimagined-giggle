3cca33c3aa0d0df7cd10a03193f73c6ac687e4a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:22:34 +0000

    compete miss shall prove join

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_str(array):
-        return str(array)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
