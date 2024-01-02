679984581e8419b31a9fe672cde8abe03636189f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:57 +0000

    feed arrival either certainly possession

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
