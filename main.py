18605816287b58c3ce7d6ad16f3b85c2ddc23756
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:18:35 +0000

    interview purpose various chief regard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
