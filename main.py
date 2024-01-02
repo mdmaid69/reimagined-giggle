93a4bf0bb38d2e549412fa30ec9b6fabb0365387
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:22 +0000

    tax wonder point except detail

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
