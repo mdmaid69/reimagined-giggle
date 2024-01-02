d671c50cedbae1be3e510975a9c5fa8c8f3d303b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:10:19 +0000

    destroy ours supply telephone pile

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+def find_unique_words(sentence):
+        return set(sentence.split())
+import tensorflow as tf
+print(tf.__version__)
