7aefcfd34115280edd47e3a0f22f3d3cdfdea7b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:38:32 +0000

    divide share freeze new total

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import platform
+def get_python_version():
+        return platform.python_version()
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
