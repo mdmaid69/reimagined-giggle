c772e923d470c09ad7d4ff881e53be4a30a1b5d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:15 +0000

    mean task issue word sister

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
