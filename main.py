8c0735d18cdead8edc34f88507494f95906e0ce0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:26 +0000

    knock interruption spray laugh attract

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def is_even(n):
-        return n % 2 == 0
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
