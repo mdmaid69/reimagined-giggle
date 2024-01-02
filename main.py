ab1eb36cb0e47afcb8bde77aed3b52e8f42b7ad3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:24:52 +0000

    characteristic description extra live pair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
