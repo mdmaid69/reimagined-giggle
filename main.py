6ed45d92f6d568e45ceed34424ec9940feff9eb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:32:23 +0000

    Qq unfair empty show wave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
