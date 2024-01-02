12b7345111f11e0065dd79648787af127d97c9d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:16:24 +0000

    copy sound godown by course

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
