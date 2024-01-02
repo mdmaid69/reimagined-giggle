861be05b9aba784c24c0c24a88f69c71aafa20c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:49:27 +0000

    big single thumb warn gooff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
