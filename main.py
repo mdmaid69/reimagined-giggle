00ad20cdbdcc1b9fe477ea6d2161cfbdf9fc30bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:28:41 +0000

    machine dare Nn pair best

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
