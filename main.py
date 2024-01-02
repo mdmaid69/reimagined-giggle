edaaa51d8ca8e70da4493c018f1ea22a76ed18a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:01:52 +0000

    reduce shape by ready turnout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_volume(length, width, height):
+        return length * width * height
