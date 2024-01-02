0f11ed3c8daf467a47ff6ee51a0dcaa2825f110d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:22:46 +0000

    badly cost flow instead violent

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+def find_min(numbers):
+        return min(numbers)
