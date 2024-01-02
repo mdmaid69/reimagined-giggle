9e2ce11103e52e754e29726c3228422b2b1088c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:23:57 +0000

    scene backwards amount stage error

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import os
-print(os.getcwd())
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
