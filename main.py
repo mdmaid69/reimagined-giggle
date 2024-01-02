b84f61725c1998f52d136784d8517b8937122d87
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:31:13 +0000

    every experience comfortable step tired

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
