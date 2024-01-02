5f4915435054c991d868babc7bf5a4a0f4277fe6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:53 +0000

    organization fierce mess aim moment

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
