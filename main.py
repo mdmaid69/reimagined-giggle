8c015054707e1c9a1e775b8d0fff6d8e597ece7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:10 +0000

    visit prevent unlucky fetch suppose

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def calculate_power(work, time):
+        return work / time
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
