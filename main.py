826aa4480662da783766522570434a6328d857ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:05:24 +0000

    practise female change proper birthday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
