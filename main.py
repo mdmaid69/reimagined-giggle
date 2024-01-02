bde3213abfaa24f1f436e54c2d11b08cd8b2bf2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:34:02 +0000

    fly work somewhere borrow rush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def square_number(x):
+def square_number(x):
         return x**2
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
