f8f8c29b24ebd69ec00038b616870a53b38fc00c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:35 +0000

    any fall range achievement content

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
 numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def square_number(x):
-        return x**2
+print("Squared:", [n**2 for n in numbers])
