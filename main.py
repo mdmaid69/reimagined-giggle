e09a9890c381c3687ed52a9b86048f675aa72347
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:12 +0000

    anything trap guess union uponon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
