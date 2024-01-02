c96b0a71ac1363feeba74a3d201b2c21e70d946d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:09 +0000

    where popular lie Nn protest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
