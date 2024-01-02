986a400dd0b7c14f6ec78b078e8f86592ab45f0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:10 +0000

    child case crowded switch wet

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_pressure(force, area):
-        return force / area
-  def square_number(x):
-        return x**2
+import getpass
+def get_username():
+        return getpass.getuser()
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
