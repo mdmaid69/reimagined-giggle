af528914c1bbbd15d8f94330902c8e0750afdd68
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:31:01 +0000

    wild harm careless turnaway knock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
