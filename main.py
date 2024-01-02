6c19152d75e592a6e16941f9159b1b0146f2d549
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:55:04 +0000

    force first apply find difference

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
