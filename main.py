ad36a0dde5f57061dcc2991ed51c9f176701bcaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:17 +0000

    run bet danger touch middle

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
