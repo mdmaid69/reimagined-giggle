04968ce356fafa05f204b368da2e5e7622824bfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:26:38 +0000

    roof fetch admit quantity comfortable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
