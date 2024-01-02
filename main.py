83a95d3450b835be0699151c3352bf14c0b88ca1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:56 +0000

    often imagination quantity can frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
 import array
 def get_array_as_memoryview(array):
         return memoryview(array)
