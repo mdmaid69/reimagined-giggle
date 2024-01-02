e4148b36c0ed84f88e9dc5dddddbad61f76b1d01
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:36:46 +0000

    bringback figure already reduce far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
