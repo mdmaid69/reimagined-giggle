111d4b30c6a34f721f2edc68aa9af83e715580c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:25 +0000

    tidy wealth tongue hear stick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
