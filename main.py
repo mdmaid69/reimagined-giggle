782291689276bad197f494628e533513b1eeec1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:05:52 +0000

    off willing exact adjective project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
