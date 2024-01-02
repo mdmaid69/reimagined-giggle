a981c5d44d8c1aa5f3e424f1adfe58bf01d529ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:19:56 +0000

    neither flow later card breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import array
-def append_to_array(array, item):
-        array.append(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
