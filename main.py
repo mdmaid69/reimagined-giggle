1082a40a99034f67400658023cd73c99f6392de3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:06:32 +0000

    band world fetch excellent tour

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def calculate_volume(length, width, height):
+        return length * width * height
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
