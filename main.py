fd9bbdd3e2fedaf7babb8257c85f34374bcd204a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:21:38 +0000

    touch achievement appear news disgusting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def get_directory_name(path):
+        return os.path.dirname(path)
