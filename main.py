0544047a5ed038f2f4b2f8ef10347b1c2edf3ce4
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:45:13 +0000

    chance wind represent suggest computer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
