a4bc7d2fed4df09bf48a5b47f83ef1c622a1ee16
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:30:31 +0000

    ability sentence along job progress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
