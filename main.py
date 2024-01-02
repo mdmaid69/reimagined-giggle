81707fbed69635fdcbce4cb973c2342a940b557a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:32 +0000

    stage frame proof classroom relax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
