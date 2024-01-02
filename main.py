a690893b90c66e9e5ee55abb31baaee571742229
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:27 +0000

    throw equipment run support Ii

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import getpass
+def get_username():
+        return getpass.getuser()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
