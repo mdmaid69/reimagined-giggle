a7785df4e322ff809b0d1a3b294ee6f437f4470d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:11:45 +0000

    treat basically characteristic he narrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_bessel_function_of_second_kind(n, x):
-        return math.yn(n, x)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
