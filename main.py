fa55f87f1c64f985cb3f0837267606a7caf12690
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:25:28 +0000

    exercise loud demonstrate getback centre

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def find_max(numbers):
+        return max(numbers)
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
