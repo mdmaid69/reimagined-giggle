c157996ae0b299aad270f68614bb0bc4174c60df
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:47 +0000

    partner stop spill separate form

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-  def convert_to_octal(n):
-        return oct(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
