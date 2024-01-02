93acc9d027acfe15ce9be2e98043b47d47fb78a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:09 +0000

    method schedule become pass violent

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import array
-def get_array_length(array):
-        return len(array)
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
