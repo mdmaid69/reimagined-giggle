f9a44b6e833a496842bfee3227bcd6676162aa8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:21 +0000

    journey about 566end rest morning

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-import math
-def calculate_hyperbolic_arc_sine(x):
-        return math.asinh(x)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def set_array_item(array, i, item):
+        array[i] = item
