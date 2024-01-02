f79667887245a2a47d9c3c4e97be56a2514f9db8
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:47:01 +0000

    comeround laugh heat coat game

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
