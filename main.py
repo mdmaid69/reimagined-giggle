7aa5b59972da6cf33b2f14ee51a766733f95e3c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:45 +0000

    sensible ashamed left which equal

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
