fc2e28b447f0b2d2b69c06d37480fcf83864d7d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:12:44 +0000

    chase swing takeout from add

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_odd(n):
-        return n % 2 != 0
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
