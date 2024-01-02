7ca1ff608722a4094de815d2e34fe80bc590201c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:53 +0000

    team part bear principle roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
