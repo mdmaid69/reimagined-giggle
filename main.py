fe3c407954fad9cf88e98512a44906b2cd9d1dfa
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:24 +0000

    book poison real relief fence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-def is_palindrome(s):
-        return s == s[::-1]
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
