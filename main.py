dfff83710e6e70f1f6b6a5f28fb74af7de0add38
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:22 +0000

    rush thought opinion grateful phone

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
