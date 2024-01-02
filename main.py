c74c2ef2472943fe67b483948d41a12f78527c91
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:51:29 +0000

    tough every central afraid this

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import os
+def change_working_directory(path):
+        os.chdir(path)
