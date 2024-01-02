578c51bc8734ff4cd05940f6b42e756469daeda7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:00:29 +0000

    discovery burst estimate official shrink

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import collections
+def create_user_string():
+        return collections.UserString()
