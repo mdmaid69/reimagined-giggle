41f9ac14471eb63490715d45b6c27e07f1d92086
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:30 +0000

    convincing pupil back include pretty

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
