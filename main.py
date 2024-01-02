fa1894417b371500aeb354895c8ad5caa3831b1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:28:58 +0000

    change work disappear sore next

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
