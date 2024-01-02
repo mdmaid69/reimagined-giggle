98c8395d1f9568185a3ef2ccbfb3e53057563eb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:32:55 +0000

    popular fairly nowhere sting offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def delete_file(file_name):
+        os.remove(file_name)
