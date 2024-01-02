8fb08a9049f0277a6b6b8b472a77b4a4b2811270
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:19 +0000

    laugh represent whatever join properly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+import array
+def get_array_as_list(array):
+        return list(array)
+  def calculate_area_rectangle(l, w):
+        return l * w
