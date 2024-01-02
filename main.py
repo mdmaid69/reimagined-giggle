76c421cea90ef9101dc2527965af345ddf1c7593
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:22:26 +0000

    claim far obvious control evidence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
