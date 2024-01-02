21cd4e0018a2275fab56c1af46aaf964ff397864
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:31:08 +0000

    police value build unfair finger

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
+def reverse_string(s):
+        return s[::-1]
