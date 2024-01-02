fa24931c1c161d28c8164f4b61ca98664b6cc3d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:30 +0000

    outside mine key wave tragedy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_power(work, time):
+        return work / time
+def find_max(lst):
+        return max(lst)
