a732dab842bb860aeb3957616339aa3a39684057
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:36:29 +0000

    laugh star pride innocent brave

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def square_number(x):
+        return x**2
