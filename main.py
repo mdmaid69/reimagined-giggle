8c440ade34ca2192e13f62cc00f9deca8337c683
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:01 +0000

    sale innocent record road usual

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+print(sum(range(10)))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
