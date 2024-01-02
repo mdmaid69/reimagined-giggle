3d2166eb6f16c362f67e3d23292f49b5217a7390
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:52:43 +0000

    drink child bored fit bury

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
