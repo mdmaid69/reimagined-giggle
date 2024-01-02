73f40c7ad3316664ff536fbad2565833ceb95f35
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:09:34 +0000

    possible prevent someone occasional active

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
