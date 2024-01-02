7bf6572dad7b5276cb53edc83200685e8577cb4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:28:22 +0000

    giveout creature careless goout famous

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import math
-def calculate_tangent(x):
-        return math.tan(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+def calculate_ceiling(x):
+        return math.ceil(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
