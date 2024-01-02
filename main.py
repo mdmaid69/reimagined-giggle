8c6a5abf0339349ba7c19ab0ab6a2eb49e383731
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:04:37 +0000

    alive language top start come

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import collections
+def create_user_dict():
+        return collections.UserDict()
