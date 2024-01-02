b36ed87edffd12dc2ad6b464beca05e89fae94cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:33:12 +0000

    combination clever wipe run choice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
