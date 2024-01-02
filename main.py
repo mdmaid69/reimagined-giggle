384245a646e5b67c9bdeec7092c5488a9f35cbdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:50:01 +0000

    become rent majority pay replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
