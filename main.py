08f872c98f6b3d51b38abf21f79b8215f6764cc7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:38:12 +0000

    bit yes sentence succeed 179bite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  def add_numbers(x, y):
+        return x + y
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
