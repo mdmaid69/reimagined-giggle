cfee51c9421abecc3a76243bd1770d63809b2110
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:10:02 +0000

    luck love provide bet include

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-def find_unique_words(sentence):
-        return set(sentence.split())
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import getpass
+def get_username():
+        return getpass.getuser()
