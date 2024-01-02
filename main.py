10f3906dc2d9b437e9c03846d7b8116e0cfb56d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:11:13 +0000

    even drop fair matter breathe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
