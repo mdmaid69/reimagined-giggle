56a51b06a62f0c648d280ae3a83d11d69b58e4d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:33:20 +0000

    reply responsibility information point opposite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
