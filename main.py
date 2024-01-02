1bc8f477ef924c648d9b0ac22a8f3ccbad16f685
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:32:36 +0000

    best ear sudden attempt shall

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
