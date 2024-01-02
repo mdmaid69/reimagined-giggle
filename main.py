e6cfa6b5c38d09943d0c077c91866c366a5efcb3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:49 +0000

    contents tread willing demonstrate flat

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+import array
+def convert_array_to_string(array):
+        return array.tostring()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
