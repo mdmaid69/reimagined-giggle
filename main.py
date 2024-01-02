29a111e94f8ceb3b77c19a24f7a28df90a162423
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:08:00 +0000

    transport operation mouth tear imagine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_power(work, time):
-        return work / time
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
