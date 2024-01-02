151d20c37099402b975c40e9b146e9c99ac2efb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:38:30 +0000

    house fact rain equipment company

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
