0cf3266fe80c53b755fe64a81118287e2d084a38
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:53:14 +0000

    charge wrap study call never

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+  import pandas as pd
+  def read_excel_file(file_name):
+        return pd.read_excel(file_name)
