00fee6e3d61bc557d0471b33e494c9e172371e1e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:09:36 +0000

    soak home otherwise carry yes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import array
+def get_array_as_list(array):
+        return list(array)
