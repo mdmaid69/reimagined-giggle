b53f4bf95122e1e7194b2262c3d219662daa3367
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:52 +0000

    gift people welcome comeup separate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import collections
+def create_counter():
+        return collections.Counter()
