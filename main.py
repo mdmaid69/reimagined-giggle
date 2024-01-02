495e93436d5badb38f4cbf3d3dcd77e10f4386fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:01:12 +0000

    ignore spite separate disagree pay

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+print(sum(range(10)))
