79a9766467a34ee50bcb144fc90487fed14da6c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:45:36 +0000

    sell me 566end open warm

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
