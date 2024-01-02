897d72a03699dfe857f3209e08458a3a98a9e005
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:36 +0000

    connect respect little company lead

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
