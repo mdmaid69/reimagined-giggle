7afa907ac8cb849bec9c7f350706ffc3ea17b5df
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:53:57 +0000

    brief dare unit pronounce fit

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
 import collections
 def create_priority_queue():
         return collections.deque()
+import array
+def get_array_as_bool(array):
+        return bool(array)
