26924d88c27273ac508ce061d3318cf6e220a492
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:50:04 +0000

    aim any limit medium begin

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
 import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+def remove_directory(path):
+        os.rmdir(path)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
