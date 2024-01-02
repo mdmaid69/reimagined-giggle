5e4124c08d8a9103aef3917b8888e408a58ba8ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:14 +0000

    view responsibility frighten fill lookup

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def divide_numbers(x, y):
+        return x / y
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
