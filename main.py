59f54bad3d00b95ec10f190a7d54f1596ba3a0a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:22 +0000

    worst giveback different cut just

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print(sum(range(10)))
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import array
+def clear_array(array):
+        array *= 0
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
