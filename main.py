dbec034111cff53f5f958e9ce32e44eed1e71b79
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:28:48 +0000

    fetch quantity tool draw than

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-import os
-def change_working_directory(path):
-        os.chdir(path)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_current_directory():
+        return os.getcwd()
