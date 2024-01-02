e034fcf46e1889fed54fa3a2c2981c03a7956507
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:12:19 +0000

    bend excuse everybody survive equipment

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  def convert_to_hex(n):
+        return hex(n)
