82f74c0c77bbe16446f3baf14ef1fac7f5038522
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:36:10 +0000

    unhappy maintain produce and bear

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-from collections import Counter
-print(Counter("hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
