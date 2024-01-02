e296cac502bb4a3e89973e0db318114d47000b0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:19:16 +0000

    wealth complaint swimming crazy godown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
