7b28defdbe50cf50b092eb7290cfd3f68cc4ea70
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:17 +0000

    advice guide street below area

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
 import os
-def get_current_working_directory():
-        return os.getcwd()
-def convert_to_hex(n):
-        return hex(n)
+def list_files_in_directory(path):
+        return os.listdir(path)
