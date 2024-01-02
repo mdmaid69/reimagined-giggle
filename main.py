f898408dabcc0926217590d48413434c0e1487f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:40:59 +0000

    calmly rather wind suit smart

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def convert_to_binary(n):
+        return bin(n)
