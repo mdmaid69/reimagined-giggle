a4bcc957f9adb668e6584168ed3bcc8881c34a72
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:16 +0000

    hold certainly I possible plan

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
