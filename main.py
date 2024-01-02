610b1243214055e4cd47a8137002c447bd810604
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:30:33 +0000

    hot object study oil study

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def convert_to_octal(n):
+        return oct(n)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
