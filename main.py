9666af1afba3eeace1117231fd60dd350843e327
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:11 +0000

    worse bar Dd car shade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  def subtract_numbers(x, y):
+        return x - y
