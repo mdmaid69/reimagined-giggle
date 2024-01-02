be281a769b978483e254b8a5fa236d80cde9c962
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:20 +0000

    steep often only cause hollow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
