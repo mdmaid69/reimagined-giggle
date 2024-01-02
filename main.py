3559a40152f739dc6c3583d7561d8e55c932b15f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:00:25 +0000

    affect interest interested concerned laugh

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
