36184de7b508b793beca4a1ca1d094de24fbcfb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:53 +0000

    570enjoyable lookaround goup injury teach

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
