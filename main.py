299b666d32eec198641453d6904830696a03542c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:07 +0000

    breakfast connected second wrap then

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import numpy as np
+print(np.array([1, 2, 3]))
