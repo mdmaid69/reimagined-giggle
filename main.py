99b45c84d2981a63abd59d4f5bb94ad6fb18de44
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:52 +0000

    order keen maybe slight remark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
