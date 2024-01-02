7a427b1dfe54bde54ce59b660297316b9fa9a79d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:46 +0000

    excited number distance bank crime

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
