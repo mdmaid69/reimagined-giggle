23dad258d9c1a3d9f5f558737fdb3c45e940dd2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:09 +0000

    swim great slight second appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
