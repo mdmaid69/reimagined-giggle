34848e119e48c49946e6e40d5292aceb1e775d4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:15 +0000

    foot sound who scared official

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
