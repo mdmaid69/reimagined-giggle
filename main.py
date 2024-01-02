1f4f6602f79ca45b769c123ca3660234d0aefac0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:48:06 +0000

    possibility help its snow object

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_even(n):
-        return n % 2 == 0
+def convert_to_hex(n):
+        return hex(n)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
