e7080f1df730c9cc0a7aab8658353a875306beb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:57:59 +0000

    market other label influence lunch

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
