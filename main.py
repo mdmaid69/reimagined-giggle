7e5c4d4ad6bbbcd3addce06061893cea40ed52f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:20 +0000

    belong worth same on occasional

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def create_user_string():
-        return collections.UserString()
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
