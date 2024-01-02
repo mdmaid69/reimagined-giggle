ad737fb0d522314c22162f6caf79b5eb498418cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:36 +0000

    572enter nothing responsible shelter lookafter

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def get_array_as_int(array):
+        return int(array[0])
+import array
+def get_array_itemsize(array):
+        return array.itemsize
