4a7d246d255498514eb7d44341f80cf863c56204
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:33:27 +0000

    big show impression benefit thing

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_density(mass, volume):
+        return mass / volume
