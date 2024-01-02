01ba601c0f4525abf4a5faa2c2aaee8bef89249b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:40:31 +0000

    pressure shake sell Yy produce

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+def calculate_area_circle(r):
+        return 3.14 * r**2
+import array
+def get_array_as_int(array):
+        return int(array[0])
