4bacdc989a9ad42d8a39f59c8c8b9527edc5f836
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:45 +0000

    trick lesson healthy suckme through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+import heapq
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
