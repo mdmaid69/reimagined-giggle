6b7e1363c17178cfca0c64c302c648f270e43ce3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:27 +0000

    father interruption remove rain scheme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
