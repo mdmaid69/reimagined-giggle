183d81b244fcebb9421c8c55ccf76b9b7e9dda17
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:25:08 +0000

    treat firm turn prize involve

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
