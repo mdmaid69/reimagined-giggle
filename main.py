b59816607727e15a18fcfa00cfde140b5a549e5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:10 +0000

    possibility course shelter Ss raw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def calculate_speed(distance, time):
+        return distance / time
