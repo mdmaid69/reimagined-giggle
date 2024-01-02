8087b88ceacc7d588455230889ef142e005d6f18
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:29 +0000

    risk workon translate apparently simple

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
