48bbe1de01629e0891225ba091a88e66640431be
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:07 +0000

    take fail owe inquire mood

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+print("Sum:", sum(numbers))
