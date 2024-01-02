ad8e42dd24164117abd2577c64a7eddcff881259
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:04 +0000

    arrive tour behaviour issue peaceful

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_area(radius):
+        return 3.14 * radius * radius
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
