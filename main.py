79248af5a289a500517c007f37e144e033e89029
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:28:54 +0000

    under size never tape certainly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import platform
+def get_os_info():
+        return platform.uname()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
