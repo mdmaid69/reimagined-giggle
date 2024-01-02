96282f3cf3bfc7fd91205dd9a4ed0e2ae2c5e210
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:58:28 +0000

    average sort second effort through

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+def calculate_power(work, time):
+        return work / time
