27e01d4744d4c91f6af66f45949ae3e7f6f3f39e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:25 +0000

    anything no back influence receive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  def sort_list(lst):
+        return sorted(lst)
