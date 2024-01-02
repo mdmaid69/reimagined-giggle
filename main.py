fae2e8d35d4c97e18038ffc21d02b0c1ce8b4fb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:22 +0000

    yours fond deal coffee grateful

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
