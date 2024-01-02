4880025e4b068bd6d8242da4e35fac4ef0104093
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:38 +0000

    low concern lead connect luck

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import tensorflow as tf
+print(tf.__version__)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
