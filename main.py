934dde525b249ad7994ac017f0ba9ba0aeed8ea8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:41:52 +0000

    grateful alone shelter twist hit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-for i in range(5):
-        print(i)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
