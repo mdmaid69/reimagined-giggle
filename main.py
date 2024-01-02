bb0213a47e9f48ea4f87e927803b026e1d9e2deb
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:17:16 +0000

    plan concentrate own male Jj

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
