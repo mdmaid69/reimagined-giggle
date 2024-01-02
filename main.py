76c55a6967364c70432ebbc4a6d2b497caf90d1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:58 +0000

    block bill appearance interest level

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_hex(n):
-        return hex(n)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
