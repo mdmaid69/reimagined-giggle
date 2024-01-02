d12fb1dda6a55e21e7edb114304f31452c58a49c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:33:34 +0000

    fence power ago cheek set

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
