07bb0fc9dcf7f2904643e1c5ee753668d3d916c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:38 +0000

    include comeback deserve terrible lot

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
