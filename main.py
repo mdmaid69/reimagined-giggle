715b5277cc89bbcdf51b53c21d8b516c91d17cd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:41 +0000

    foreign path connected dark wash

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
