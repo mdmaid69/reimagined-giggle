263559033019a2f6351edb92231c7b9cbf26dcc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:37 +0000

    567enemy best disappear plan favourite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
