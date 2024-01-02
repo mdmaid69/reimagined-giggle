ba600acd6396e89ee0066219fa86dbed9ac5eb88
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:27:32 +0000

    raise bright suckme once splash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
