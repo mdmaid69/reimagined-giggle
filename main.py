64301df6ac8b03017c5c481928488ddab7d96442
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:18 +0000

    please proud continue characteristic unlikely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+def convert_array_to_list(array):
+        return array.tolist()
