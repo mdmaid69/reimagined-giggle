ff1ccb90e1ea3f1a82c9cff3283005c720071e2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:11:16 +0000

    father war giveaway could warn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def convert_to_octal(n):
+        return oct(n)
