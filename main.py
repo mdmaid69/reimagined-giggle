12d90b7fb1c1bd1258b578f9d95c8c62eace728e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:44:06 +0000

    rich safe legal threaten desperate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
