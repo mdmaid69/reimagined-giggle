ec6d94af2972ef3303ab7955f665b54b6213d3fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:48 +0000

    bag nice between gather interrupt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import collections
-def create_user_list():
-        return collections.UserList()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+def find_max(numbers):
+        return max(numbers)
