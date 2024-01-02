a53ab974c8911aeae057183a5e61ef3fe02bc4da
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:55:37 +0000

    afterwards Gg alive advantage drop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
