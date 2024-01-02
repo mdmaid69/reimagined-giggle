5097e03d7bf79070396306c9b462c2d93a11f6c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:41:56 +0000

    well radio stretch fear now

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
