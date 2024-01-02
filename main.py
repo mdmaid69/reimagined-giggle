94d4c0d8118bf43c855d336c272613c01719f21d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:47 +0000

    worry wind turn dress drink

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_min(lst):
+        return min(lst)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
