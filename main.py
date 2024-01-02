88bc9fd0c5d14a10c60f9f48d26dd0edaac7ad3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:29:28 +0000

    passenger needle experiment process capital

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  def is_odd(n):
+        return n % 2 != 0
