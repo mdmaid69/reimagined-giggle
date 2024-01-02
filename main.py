5b6dbded9828f86c1705000d1c4c8a8885029960
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:42:43 +0000

    rain theirs behaviour cut company

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
