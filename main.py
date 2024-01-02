ad9b822b8e6263ccf4663402e097187650b37e72
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:36 +0000

    chief excited contrast copy will

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-import array
-def get_list_from_array(array):
-        return array.tolist()
+def cube_number(x):
+        return x**3
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
