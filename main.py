1cf92904b681208ac43ff3f40f34b29933b13e9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:08 +0000

    female fix whole account that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_area_rectangle(l, w):
+        return l * w
