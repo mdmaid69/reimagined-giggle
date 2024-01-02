ad8023df44d72194f9d07799d4464ada0b966bb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:42:40 +0000

    my total meaning sick not

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
