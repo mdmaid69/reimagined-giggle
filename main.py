ac78cbb22d7f13a6a9fe92858a265c9712e3b93c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:48:59 +0000

    show fasten ashamed laugh trust

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
