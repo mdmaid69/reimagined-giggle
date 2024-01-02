cd901d63f046eea6cfd67bc6419f930dd97a4724
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:18:31 +0000

    ahead nature lost surprised feel

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
