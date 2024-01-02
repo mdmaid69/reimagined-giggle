7ac1514b5d90babf2fcc3fce4c760a9f78fb0f65
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:39:44 +0000

    runinto label complete worst track

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
