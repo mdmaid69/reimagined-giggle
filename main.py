a381a842fbf88083067ba9f3bad2637344013c5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:46:37 +0000

    negative work stretch closed mainly

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import collections
-def create_stack():
-        return collections.deque()
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
