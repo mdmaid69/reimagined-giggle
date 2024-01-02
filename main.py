aad087a2a9f1fe4001e2866e751eed151545b6ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:27:24 +0000

    impression go live present importance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
