40068ffcbf1d1463c7c1744600ebba3fed497cdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:15 +0000

    gate weekend Ww protection move

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
