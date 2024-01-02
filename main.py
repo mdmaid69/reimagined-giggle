75166b8cfaae3d226a565b26c5a5a099e176da9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:14:05 +0000

    see deaf happy by none

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+for i in range(5):
+        print(i)
