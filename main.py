b6bd0fb0aba437ae0fcba655772c1d1c83763700
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:16 +0000

    press statement swim common hello

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
