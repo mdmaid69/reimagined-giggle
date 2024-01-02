fd99b36399d8c2a67506f37ba22db97747b84dd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:02:08 +0000

    slice cope strength sort immediate

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def count_elements(lst):
-        return len(lst)
+def set_array_item(array, i, item):
+        array[i] = item
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
