b527ab6b4cbaaea3dc42f68e19f7adac72c74c5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:34 +0000

    evidence waste pleasant minor official

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
