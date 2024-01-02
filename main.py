6c7ba5deb3feced3170cb1b1059a4d349982162e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:52 +0000

    getout none message branch tidy

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
+print([x**2 for x in range(10)])
 def calculate_density(mass, volume):
         return mass / volume
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
