75fd8ad9f77cd27e67a9a9d86e811c331bc2fe1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:04 +0000

    earn thorough decision formal yours

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
