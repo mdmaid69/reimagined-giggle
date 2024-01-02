15fa3296995f8b6d7ad0c8516a7f28aa2a6827a0
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:43:57 +0000

    past lift table breakfast estimate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
