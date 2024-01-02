54c4061e0985bdac646125460851f48f050f805e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:27:50 +0000

    type valuable fit yes transport

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
