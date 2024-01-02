29361f912c62167d3d81120e24b6a6b9cf794021
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:09 +0000

    trap by none tool unkind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
