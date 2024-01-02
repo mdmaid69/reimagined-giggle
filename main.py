dcdde0e410ecf3c39d8237f16a1c449357d787ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:59:13 +0000

    now opinion smooth care now

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_power(work, time):
-        return work / time
-for i in range(10): print(i)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
