76a8f0092dc6aa9224bd6979d4fddf04c61cce89
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:36 +0000

    connect form enthusiastic decision kind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+def sort_list(lst):
+        return sorted(lst)
