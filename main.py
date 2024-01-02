f2e117a7a859393786d4977ca201d353dba03e5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:54:43 +0000

    estimate minor away score agreement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def convert_array_to_unicode(array):
+        return array.tounicode()
