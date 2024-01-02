c3ffa5a66ce780d62dc2ecef2961b2ba470e145d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:31 +0000

    holdup choose left provide drink

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
