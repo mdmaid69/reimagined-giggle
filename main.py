5c015c7e2a3ba8a838be7087f115ef6aa320fa60
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:46:25 +0000

    exit surprising ear project include

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import sys
-def print_python_version():
-        return sys.version
+import array
+def get_list_from_array(array):
+        return array.tolist()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
