12c32ba78de345f64b074db6607dd4aea4a16a65
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:17:26 +0000

    spoil hungry twist lonely use

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-def add_numbers(a, b):
-        return a + b
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
