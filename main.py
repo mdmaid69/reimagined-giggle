79ebb5ffc36aa779ae4ec92d498206ddf4d36f79
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:15 +0000

    quick goout under between again

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
