d2664d8fbcb5f68c905c64f8db9d0bc316c32b87
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:19 +0000

    near good region difficulty final

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
