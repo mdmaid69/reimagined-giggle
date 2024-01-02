bc08db2d9feed9c731b1f6ede5ef72d381bfe138
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:15:51 +0000

    further aim lesson holdon oil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Words:", len(text.split()))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
