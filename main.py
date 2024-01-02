ba8e5c929eb8c253a4b2cfdc941a99651d1bf85d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:02 +0000

    popular deal hurry wake slice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
