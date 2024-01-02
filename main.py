49554a539a9d986482765f9c5ece2014c0083240
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:18:41 +0000

    illness temper deliberately exactly able

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print([x**2 for x in range(10)])
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
