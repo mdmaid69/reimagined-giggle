313d9a324c90633594177d3ec27a9b404d82cafe
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:52 +0000

    makeinto round until pronounce suffer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
+  def get_file_device(file_name):
         return os.stat(file_name).st_dev
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+def calculate_power(work, time):
+        return work / time
