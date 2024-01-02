53988013afb6b44c5234b8971921c40bfd9855fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:22 +0000

    thinkthrough will another standard burn

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
 import array
-def get_array_as_set(array):
-        return set(array)
+def get_array_typecode(array):
+        return array.typecode
