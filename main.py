5a37f1dcb389fa76cbfbbc39ba4cc1aa9bd7840b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:51:05 +0000

    cope part war light success

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-def sort_numbers(numbers):
-        return sorted(numbers)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import array
+def get_array_as_str(array):
+        return str(array)
