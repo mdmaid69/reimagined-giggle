40649f8782065274efc6eefdd031242de9553745
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:08:16 +0000

    arrival phone trap hungry by

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_pressure(force, area):
+        return force / area
