b0c5893b2cd432a0d9ff4939088bfffd4069523d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:50:12 +0000

    lookafter jump feel getup news

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+  def add_numbers(x, y):
+        return x + y
