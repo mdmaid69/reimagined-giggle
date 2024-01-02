df393c8edb2a6b047137313f3cb441447e1b7eb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:31:25 +0000

    Mm by machine healthy basic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
