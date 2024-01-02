f6f6828649d33e92f5b32d9094197bff87a5d308
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:10:51 +0000

    not contain great home keepoff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import collections
+def create_priority_queue():
+        return collections.deque()
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
