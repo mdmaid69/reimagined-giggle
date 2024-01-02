10ef9eba166e0762d8f7528d639ddce77bfc038b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:20:06 +0000

    custom smoke both spray blind

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-import os
-def change_working_directory(path):
-        os.chdir(path)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
