c8d040d2202522c61fbaf02d8403976f1ab0cf41
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:23:45 +0000

    lay rough rain 568energy natural

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_string():
-        return collections.UserString()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
