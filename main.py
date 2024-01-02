98c1f4f9abc411ddc83a6061049359a9504e2fed
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:47 +0000

    stop and question but basic

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-for i in range(10): print(i)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
