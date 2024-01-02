4c209607dcf5ba99b82440252333ee2bf3cc8042
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:54:55 +0000

    challenge cut skill precise squash

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
