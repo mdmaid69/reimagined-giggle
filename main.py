cc17db08c7bdf21b7b3eb8a46e046f7bafe98c5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:19:51 +0000

    shout prevent emotion during through

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
