eedd96f56bc2a3d577248868b9e1ffb817baa9a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:03 +0000

    excuse wind introduce base worth

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
