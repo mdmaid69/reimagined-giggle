d496f5f2cc85cb1530d1a3d8f1b02c4fc82f691a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:19:45 +0000

    achieve innocent ring age level

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
