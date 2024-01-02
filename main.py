441d7ed7ecaa8b041c84d9760be0ddabd5a131cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:06:23 +0000

    sister activity jump embarrassing breakdown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-def sort_numbers(numbers):
-        return sorted(numbers)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
