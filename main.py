907a35d8e6fcf7215ee52049c86edeeb1f890172
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:04 +0000

    tap wonder real formal time

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
 import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
