0e4a2e90428d4b2768433ddc4c6ae5db34af70ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:15 +0000

    this talkinto equipment clear split

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
+def reverse_string(s):
+        return s[::-1]
