966d3c3ae4439c8663e430409842febe851f1a3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:07 +0000

    stand tip car provide fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+  import os
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
