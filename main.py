d2c76c8d94ec6da55d850c1523a930d7a759a522
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:25 +0000

    behaviour season total mend reply

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
