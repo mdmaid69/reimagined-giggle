a6e40bf74a6ac761517392f0a7ab76c1b8c72f18
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:18 +0000

    method vanish dinner Ii answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import os
 def get_file_size(filename):
         return os.path.getsize(filename)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
