2690d3cd8cccf6a7b42355ab6f040f47cb3a0fb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:21 +0000

    black famous think damage large

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
+  def cube_number(x):
+        return x**3
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
