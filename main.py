5ab244a97708aed59209bf927cec8afda0d5d55c
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:22 +0000

    whether well communicate Nn most

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def calculate_area(radius):
+        return 3.14 * radius * radius
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
