ce40628f577f3a20679a83aba2462b539b92948d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:06:35 +0000

    shock workout school possibility model

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
