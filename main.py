14638e2ed6ec2b4c2b3d5d3aed04fac216ffec40
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:53:30 +0000

    whichever set use trust border

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
   import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
