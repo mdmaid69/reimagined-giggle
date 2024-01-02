bce6c5cf0640a7e5f0b830b1fcdd76fbbf04a4ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:02:25 +0000

    trouble practical call uncomfortable prepare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+def find_unique_words(sentence):
+        return set(sentence.split())
