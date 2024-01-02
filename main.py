f9fbff25e85835b512a4069b1d0bbc8614335287
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:47 +0000

    soak contents exciting brain connect

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-print("Hello, world!")
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
