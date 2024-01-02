b857e845ff7b4d3c6da1df95c341786de6f12161
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:41 +0000

    machine common clever unit suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_power(work, time):
-        return work / time
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
