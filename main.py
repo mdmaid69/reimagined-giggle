bfecc293d6db3b6653fbc55881e8437816a730a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:50:29 +0000

    rescue hard possession cough needle

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+import os
+def get_current_working_directory():
+        return os.getcwd()
 import array
-def append_to_array(array, item):
-        array.append(item)
+def clear_array(array):
+        array *= 0
