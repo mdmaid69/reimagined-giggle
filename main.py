dfe5c951919e16a4b890a96fd9689525c0565e09
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:39:12 +0000

    attempt touch 565end style comein

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import random
-print(random.randint(0, 100))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
