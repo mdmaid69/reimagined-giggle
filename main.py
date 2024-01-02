01f51ee082e2719493f197596ccf8b496965bda7
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:50:50 +0000

    contrast national night warm tell

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import numpy as np
+print(np.array([1, 2, 3]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
