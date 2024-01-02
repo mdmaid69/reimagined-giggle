602cfaac101ecd4b2bb395166ff221224e0aca73
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:57 +0000

    demand point opportunity beautiful offend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
