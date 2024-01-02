9fd691f36592685f709c9da5f9e379d812f65a71
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:34:26 +0000

    brother front runinto 569enjoy flavour

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import time
-def get_current_time():
-        return time.time()
+import datetime
+def get_today_date():
+        return datetime.date.today()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
