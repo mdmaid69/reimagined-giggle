c7cb82d56304b85f76f68892ee819dfd1d003b49
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:11:45 +0000

    usually represent community possibly sorry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
