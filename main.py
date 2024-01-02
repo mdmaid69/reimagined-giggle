02a7847b29959a1a4607b29fde51468375b32c27
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:24:11 +0000

    fault rub money evening floor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_compound_interest(principal, rate, time):
-        return principal * (1 + rate)**time - principal
-def count_elements(lst):
-        return len(lst)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
