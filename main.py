4f7419528c49d83d9174285b6e6b504cf733adf8
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:33:30 +0000

    string limit praise brother reply

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import sys
-def exit_program():
-        sys.exit()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
