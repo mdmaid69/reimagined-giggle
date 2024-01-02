e23e761c1201afc454a38415bda3212667c695d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:30:32 +0000

    course smoke sweep family really

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-print(random.randint(0, 100))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
