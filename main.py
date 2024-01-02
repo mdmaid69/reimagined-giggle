2845b1439a671544aafaa2582fea0d38a6a05a34
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:20 +0000

    godown steal quarter curious right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+import collections
+def create_user_string():
+        return collections.UserString()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
