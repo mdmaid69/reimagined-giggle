50998ac6b0dc1c1f780c60b54ce419b1378f090a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:31:54 +0000

    pin relation curious earth pressure

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-import array
-def get_array_as_int(array):
-        return int(array[0])
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def remove_duplicates(lst):
+        return list(set(lst))
