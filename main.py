f6497b94c8348ac0e13d4077cc8c346aa2550f9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:25 +0000

    fight thumb sweep accidental system

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
