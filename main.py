bc52c5571c246d8053cd8abc13dbeb75dc9b0596
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:21:09 +0000

    length advantage comfortable centimetre several

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-def square_number(x):
-        return x**2
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+def convert_to_hex(n):
+        return hex(n)
