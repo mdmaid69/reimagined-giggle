9d748972911d68dd2c020d069b21f1845b3e3650
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:36:03 +0000

    money dream shop radio smile

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
