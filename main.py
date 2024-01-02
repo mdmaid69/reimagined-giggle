450cf5aa22612108a8f5cfdc235700027aaa1c41
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:38:19 +0000

    567enemy compete mess print frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import sys
+def exit_program():
+        sys.exit()
