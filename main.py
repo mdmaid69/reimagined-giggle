7f6c14c39edfe1abbba7ac0244986810a4caf6de
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:54:19 +0000

    would difficulty fall lonely violent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
