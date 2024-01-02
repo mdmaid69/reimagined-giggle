64f0743b35dab8799ad445be92de2e692d950249
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:04 +0000

    die sore bunch leave pull

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_floor(x):
+        return math.floor(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
