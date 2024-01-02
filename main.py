fe94ab1c417621bbbe9d0ff7ac729c7f8f0c455f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:40 +0000

    declare modern surprised bus blind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
