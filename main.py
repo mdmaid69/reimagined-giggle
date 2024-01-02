728ef13ad408c915d9bc72e5900f840304b91b5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:27:15 +0000

    new breakout kick tooth surprised

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+def find_unique_words(sentence):
+        return set(sentence.split())
