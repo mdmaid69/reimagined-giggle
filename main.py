e6d29b2a2f3f18661c35840b4b2000db49b14f7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:30 +0000

    then rarely butter performance tradition

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import platform
+def get_os_info():
+        return platform.uname()
+for i in range(5):
+        print(i)
