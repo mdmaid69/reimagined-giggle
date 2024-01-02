3dcfb387f66c374cae8d6af1d780f11b56de38d6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:28:09 +0000

    turnout woman history receive holiday

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
