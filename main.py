7fb3f25c13bd07b988e6466e4442a0f426910196
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:24:54 +0000

    drop breakout smell swing slow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
