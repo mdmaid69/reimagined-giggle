6c628cebd1b350e5ccca0facadb01161171a970c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:46:45 +0000

    Bb shut anywayanyhow fly possibly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def sort_list(lst):
+        return sorted(lst)
