a5194f15a3684fc348da77bee6b9ed0e25c7fd1e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:42 +0000

    carry upwards laugh drink broken

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
