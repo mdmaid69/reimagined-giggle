0ec395a94d0492d08052fb019bc3476180011a39
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:06:10 +0000

    content he summer majority nature

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
 import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
+def get_today_date():
+        return datetime.date.today()
