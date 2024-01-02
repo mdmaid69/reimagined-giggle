823bd5053bf7478d46f485a8ac5330b57869b86c
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:19:08 +0000

    mark neck stone belief cover

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import time
+def get_current_time():
+        return time.time()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
