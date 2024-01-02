e3521f8067e6a9cf6c0f5150474dcc6454652449
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:31:56 +0000

    search run me wash care

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
-print(sum(range(10)))
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
