cce9f76b15d022c40fc914f0a0d93e39ef9d6b39
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:31:07 +0000

    giveback help underneath mouth buy

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_power(work, time):
+        return work / time
+def calculate_pressure(force, area):
+        return force / area
