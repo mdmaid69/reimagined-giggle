eba15a64df47de7b1870f882adb4fa7971f8f847
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:07:50 +0000

    medium visit stamp hold each

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_pressure(force, area):
-        return force / area
+  def subtract_numbers(x, y):
+        return x - y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
