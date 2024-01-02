c75bbe0a51c661f49b0eae41172c9171cf884eac
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:19 +0000

    badly week smile order equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-print(time.time())
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
