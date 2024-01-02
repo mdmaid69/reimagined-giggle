b23f3e3ee5dc2917e85baeef31d44f9553ab1197
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:19 +0000

    fall far process shame respect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-def add_numbers(x, y):
-        return x + y
+def calculate_floor(x):
+        return math.floor(x)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
