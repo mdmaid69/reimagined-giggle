5d0d0dc1d08d0c91b6f9bb0122aa47a8c0c084f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:06:38 +0000

    narrow judge could control forget

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import os
+print(os.getcwd())
+  def remove_duplicates(lst):
+        return list(set(lst))
