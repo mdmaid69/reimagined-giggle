2f55cdd57ada9c4fe88e2bbbed5f3d4c2ee18402
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:56 +0000

    roll closed age pride process

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
-import math
-def calculate_arc_tangent(x):
-        return math.atan(x)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+for i in range(5):
+        print(i)
