b3551c2468ef6da3942be91d1a44e07352e72da4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:32 +0000

    meeting branch innocent OKorokay recent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-def find_min(lst):
-        return min(lst)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
