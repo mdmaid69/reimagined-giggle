f5a3e7a3c0d65208111098afe87e23c4bd085e20
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:26:26 +0000

    age although model grand wrong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import numpy as np
+print(np.array([1, 2, 3]))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
