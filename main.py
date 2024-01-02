8265f67cc63eaf6e6aedf303fb9e5fb4f3e76ae7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:59 +0000

    Qq both sink whisper introduction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
   import os
   def get_file_mode(file_name):
         return os.stat(file_name).st_mode
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
