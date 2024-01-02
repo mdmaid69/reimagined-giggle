3b3c26a299e959d71b122bfe5f104a04eaad9204
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:24:13 +0000

    remember well left black spring

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-for i in range(10): print(i)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_cosine(x):
+        return math.cos(x)
