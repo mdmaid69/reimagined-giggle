a4fe0617f9145dbc9b79b7602e8f4f0db877a7c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:41 +0000

    scheme kill suggestion round disappointed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
