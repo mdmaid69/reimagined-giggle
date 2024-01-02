244ea65cc90a510fe5f905af5acdfd7013822afb
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:37 +0000

    miss obtain prefer responsible makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
