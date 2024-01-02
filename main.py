c61995affa8359cb8ad3c5b48d215c82c8088fb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:22:16 +0000

    evidence careless brother callat dig

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
