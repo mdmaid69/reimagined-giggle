da5e10b50756261bd43a78866beebe4700213013
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:51:40 +0000

    evening believe vision certain whole

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
