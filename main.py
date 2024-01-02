ed3bc29332c48e348bf1288754d8e8081d4e2d6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:26:10 +0000

    wherever late notice past spare

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+def calculate_average(lst):
+        return sum(lst) / len(lst)
