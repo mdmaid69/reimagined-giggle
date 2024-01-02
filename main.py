a51a2eada77fbb0a0a2534e29b0a858dbb15a94c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:18:02 +0000

    afternoon instant Bb judge exit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-def calculate_power(work, time):
-        return work / time
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
