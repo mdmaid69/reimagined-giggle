b4edeaeb8b5395a5a824e216062c4eda5bd5fc3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:53:29 +0000

    innocent pin plastic honest excellent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
