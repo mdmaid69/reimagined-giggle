046b1175a07ade223198890030a058025bff28af
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:24:14 +0000

    low attract dick smell regular

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_current_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
