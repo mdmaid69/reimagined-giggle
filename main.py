818acec16f7b5f99daafb650beecb6e803a5416d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:53 +0000

    minute market fast sea upstairs

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def sort_list(lst):
+        return sorted(lst)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
