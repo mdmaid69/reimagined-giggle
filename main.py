d7a85cc7698b5e556976190596e7391fcf230f49
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:03:33 +0000

    market takeaway figure perfect slowly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
