80f0fcb89f49bd888ac9a059110c459e309b3e33
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:04:34 +0000

    difference young sign short live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+def square_number(x):
+        return x**2
