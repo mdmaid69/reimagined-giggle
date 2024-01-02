1bf02caf408c2f48636a0c82d003aaf23021df90
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:43:33 +0000

    fill avoid second basically convenient

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
