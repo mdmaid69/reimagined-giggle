51474b11efce05b3d89480b206aed22401e0afb6
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:21:38 +0000

    view trade piece fire smell

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+text = "Hello, world!"
+print("Reversed:", text[::-1])
