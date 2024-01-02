544a7c849ed0f21e606907ebd988950beb4aa0f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:10 +0000

    left or train arrange tooth

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
