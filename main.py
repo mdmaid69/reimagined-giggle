ad7dde22169da2433a64900395e0fca4768feb55
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:10 +0000

    stay grow quit key shoulder

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
