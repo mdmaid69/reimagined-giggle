6b2e3353addd408f09264ef3911d8724943fb2e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:51 +0000

    play couple brief height after

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_palindrome(s):
-        return s == s[::-1]
 import math
-def calculate_hyperbolic_arc_tangent(x):
-        return math.atanh(x)
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
