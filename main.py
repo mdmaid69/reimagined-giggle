93b06247ab7e571346a44852f849d928ca2b32c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:28:02 +0000

    clearly now label run regular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_current_working_directory():
+        return os.getcwd()
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
