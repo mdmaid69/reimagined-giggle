a1007fae710721e12edee0e6c3433976acbb9d52
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:52 +0000

    mix small compete popular practical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
