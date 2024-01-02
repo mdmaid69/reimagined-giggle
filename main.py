5ffad237318e1bf13fa92525947c10edb7d87438
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:43 +0000

    its knowledge crime improve scare

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+  def is_odd(n):
+        return n % 2 != 0
