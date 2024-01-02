06eeec7ba2f795478c8885fb3a810cdc78f71b8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:56 +0000

    appeal mistake visit careless music

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
