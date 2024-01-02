34b87b211e2f4c6b502879289ee7f68e325f1f44
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:34:53 +0000

    brush spin hold town fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+  def reverse_list(lst):
+        return lst[::-1]
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
