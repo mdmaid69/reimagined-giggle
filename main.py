6e16cd94d442faf66ddc06302e15092118dd9039
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:47 +0000

    ashamed someone closely still pause

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import collections
-def create_user_string():
-        return collections.UserString()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
