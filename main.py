695be0248043e38ead071832f865ccfbd2d2ba58
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:30 +0000

    violent characteristic future scale successful

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
