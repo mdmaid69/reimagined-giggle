0a08234486aef2e28f781656647313bcbd1b182e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:46:59 +0000

    brush come correct care interview

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
