9884a46670fd4505f50d8a570a4b4f647cf36dd6
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:30:05 +0000

    post mind succeed reliable clean

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-for i in range(10): print(i)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
