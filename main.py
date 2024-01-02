3f8db1fd7033647af79b9594ee1760550a4ac3d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:49 +0000

    send giveout brush perhaps or

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import numpy as np
+print(np.array([1, 2, 3]))
