1d1c5a3b8d0477cc092e02dcba2f28250f4ad8be
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:50 +0000

    Yy gap trust harm over

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
