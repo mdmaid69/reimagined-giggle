a38b9d49ac3f46cd6ccf4e4a052aec3c139fae42
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:48:09 +0000

    create often development Ii arrange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_hex(n):
-        return hex(n)
+import re
+print(re.match("h.*o", "hello world"))
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
