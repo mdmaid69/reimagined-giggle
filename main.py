db0818b41877e97014e4d54b0e3a8c88d8d625ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:49 +0000

    dark familiar rich pleasant movement

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
