4c0b2458cb3477018799ddd31eeac110f9b47348
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:14:02 +0000

    performance step normal house care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-print(sum(range(10)))
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
