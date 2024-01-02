93d6474e5432fe68a29c40030cbfdab052263410
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:35:43 +0000

    arrival entry fight wonder against

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
