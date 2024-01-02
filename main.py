d016242713782d575916f607a044ff4ffbd72004
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:17:30 +0000

    kind sit classroom cinema turnaway

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
