ead652cd8367aadb5d09e3cc5adf585a1396fe96
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:12:54 +0000

    commit hair community criticize war

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
