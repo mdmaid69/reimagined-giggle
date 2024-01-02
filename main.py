82b4e08c08818fe3782455aabc6223afac0c5512
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:58:59 +0000

    violence method guard fast cope

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import collections
+def create_counter():
+        return collections.Counter()
