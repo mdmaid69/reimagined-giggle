237db8334201d0eaef8bcc0a70ea728439232e82
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:29:43 +0000

    build takeback agreement try brown

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  def sort_list(lst):
-        return sorted(lst)
+for i in range(10): print(i)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
