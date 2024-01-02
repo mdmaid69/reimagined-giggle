cc5ec8d5f7092fa1c45c31fd8158c8d4adb92b5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:41:33 +0000

    giveaway letter bank breakup small

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
