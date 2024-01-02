035dcf7e4cd7b9a3a7b269a7ede8b6a6af635706
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:34:02 +0000

    fall tragedy skirt lend discuss

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
