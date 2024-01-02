c11f50ac9d28d417437fb1c1f389363ba1547993
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:21:59 +0000

    whenever invitation lookup loss few

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
