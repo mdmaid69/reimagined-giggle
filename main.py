0ecd232657eaafeae617bcfe05621c384340765d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:28:01 +0000

    thankyou pass watch flat 179bite

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
