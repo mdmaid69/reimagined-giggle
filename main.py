4a0458f9dac3c0525c5d6beefe9803e83b22477d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:59:55 +0000

    question father original chin real

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
