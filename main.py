639d859906e0a94260b5cfe7a696fad6d897c201
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:40:41 +0000

    pretty above ought quickly complaint

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
