9f0308033edc21aad36f3b799c7a40a4485ab330
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:10:54 +0000

    age Mm after pain what

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
