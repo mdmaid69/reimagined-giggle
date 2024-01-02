eae7353b613b2828050f246c81df5a8e910bb139
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:34:19 +0000

    image over disturb reduce guide

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import os
+print(os.getcwd())
