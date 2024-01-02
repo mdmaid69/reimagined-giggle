effb535cfc2639767222655623edcd8f4db7510d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:28:15 +0000

    sticky belong along pause brown

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def add_numbers(x, y):
+        return x + y
