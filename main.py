ee47eff8bda02afb4f11a1a4600b913f8f5c038a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:58:46 +0000

    edge foreign accordingto emotion colour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
