a2151d8bc8398763d484c60fbf82f0065c0ed964
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:34:41 +0000

    factory old clever hardly gate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
