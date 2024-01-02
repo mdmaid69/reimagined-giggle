ceb6af1ac3dabc5589e0e2ba327f681cab458f01
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:28:23 +0000

    fortunately suit hit mean convinced

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-def cube_number(x):
-        return x**3
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
