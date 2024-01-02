1855e4fa6913e68cfa7cf4ba2d80cc7fb388a6b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:58 +0000

    peaceful suppose quarter forget flat

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print(sum(range(10)))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def remove_duplicates(lst):
+        return list(set(lst))
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
