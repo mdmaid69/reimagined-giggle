61665b1fa49cd4a82f908fe7a6961abdeeb68240
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:22 +0000

    steam patience operation comment excited

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_density(mass, volume):
+        return mass / volume
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
