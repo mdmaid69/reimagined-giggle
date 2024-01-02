8a2e4716e004e8ce4d6924dad79efdb71510193f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:17 +0000

    clear office flavour direct correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def find_max(numbers):
-        return max(numbers)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
