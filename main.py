9d8019c79dffdf2e3724e333627610654a53cdb4
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:46:12 +0000

    set bar suck how about

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
