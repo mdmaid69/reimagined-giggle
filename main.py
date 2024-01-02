311be61fa470de2fc0f3451d559f422c58be64b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:14 +0000

    anybody firm activity round principle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+x = 10
+y = 20
+print("Sum:", x + y)
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
