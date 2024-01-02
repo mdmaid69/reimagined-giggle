babb7b759c1d1d5a2d134ba87d33a1d3571b8205
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:20:46 +0000

    join by but relation Cc

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
