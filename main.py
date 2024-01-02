048dc623391edcd1e5e4fa55c041d4f5af870290
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:25:34 +0000

    quit post drop season expect

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-name = "Python"
-print("Hello,", name)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
