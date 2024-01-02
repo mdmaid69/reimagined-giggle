5f98a84ac27895fb5ab5d86499a576fa0000ad7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:03 +0000

    region callat claim appear arrival

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  def remove_duplicates(lst):
-        return list(set(lst))
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
