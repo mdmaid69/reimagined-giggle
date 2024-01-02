aed2c678a72764bddebf90e13391eab249049836
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:41:47 +0000

    excuse that best buy along

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
