27fb4b86cdc0cc4d08dd4230040026f48e39f43b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:49:18 +0000

    post cruelty dress separate yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
