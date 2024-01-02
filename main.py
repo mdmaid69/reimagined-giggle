9afbd96f14efbafa4059fa91cd9a0ef607acb941
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:48 +0000

    smile ask job look practise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
