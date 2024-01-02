a7a6e65f32059be89de682a7c999fcb99c0cb173
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:07 +0000

    mass scream instead real after

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
