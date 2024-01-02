94e36f834dfc1f7164376c3f46792336dc0a9c2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:08:01 +0000

    impressive remain fight increase total

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
