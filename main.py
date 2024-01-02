261f93c058570a6dcd839948901560ca7000915e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:34 +0000

    instrument evidence lift wrap onto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+print(sum(range(10)))
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
