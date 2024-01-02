4c0a7f262dd513e117ef05215a18ff67be407fb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:29 +0000

    fit splash mention alone compare

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def convert_to_octal(n):
+        return oct(n)
