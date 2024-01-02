6fed70d91709802c71593f986a63d5e4b2cb44f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:22:01 +0000

    careless flood pile onto bread

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
