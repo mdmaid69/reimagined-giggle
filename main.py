256465b72e9d8a9707dba8637ef51f32d1130c90
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:02:51 +0000

    many fresh announce uponon touch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
