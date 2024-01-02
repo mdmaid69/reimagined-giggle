9f6f607713c272ea53bc7142b3501a5950c4a4c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:12 +0000

    flow safety Kk even study

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def is_odd(n):
-        return n % 2 != 0
 import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+def reverse_string(s):
+        return s[::-1]
