d366881f930ffaf1d03b10b8615d2ccdadd269a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:59:45 +0000

    crash relax wonder wing bank

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import array
-def get_array_as_str(array):
-        return str(array)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
