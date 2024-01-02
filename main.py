c3805dee24eb761027e6830075251fbf554a998a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:05 +0000

    suck minimum dinner compete wave

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import array
+def set_array_item(array, i, item):
+        array[i] = item
