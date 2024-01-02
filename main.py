be077f85598162e1dd98bafbf8fc847ee530daa8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:41:13 +0000

    confusing delicate yesterday computer single

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import array
-def get_array_length(array):
-        return len(array)
+def add_numbers(a, b):
+        return a + b
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
