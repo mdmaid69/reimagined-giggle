4aeeb5ee298d8fe1a0c3730da8eb3e893eadadab
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:12:19 +0000

    marry deal obtain week responsible

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_acceleration(speed, time):
-        return speed / time
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+for i in range(5):
+        print(i)
