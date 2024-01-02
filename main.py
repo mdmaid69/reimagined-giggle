8d0d4e7f548b8d16ae5a31fb3e11b3c02b4f6f59
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:10:08 +0000

    closely Uu completely giveout offer

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
