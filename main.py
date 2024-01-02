aca5646308cad3003bb46a2633bdaa18000630a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:19 +0000

    speech club law keepout away

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
