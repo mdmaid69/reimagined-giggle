0972eea150b05b34ef13367c54197dd4b8d94a0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:47 +0000

    date glad somebody section return

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_item(array, i):
-        return array[i]
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
