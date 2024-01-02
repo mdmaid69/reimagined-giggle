08c7a27a5e2b0ebd56b5ef746d8bc39ef353afbc
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:38 +0000

    must distant need prove advise

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import collections
 def create_ordered_dict():
         return collections.OrderedDict()
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
