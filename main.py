f71000eb27bb8de43c4f2437b537739ecadeebd9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:10:32 +0000

    turndown strict step wise turnback

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
-def is_even(n):
-        return n % 2 == 0
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
