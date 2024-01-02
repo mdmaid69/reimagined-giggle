0091f791c20abd46b19b6a19d78eb36470264d6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:11:04 +0000

    poor problem lift discovery apart

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def clear_array(array):
-        array *= 0
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+def calculate_area_circle(r):
+        return 3.14 * r**2
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
