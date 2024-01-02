3e6c3f637bee17f21a66520b8df70cf55962a07e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:20 +0000

    giveaway onto ability pretty steep

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
