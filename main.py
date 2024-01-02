75ed19b37d4a0c7ccd23cc2db338c4014e11ab42
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:46:38 +0000

    fight strength Rr arm measure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  def convert_to_binary(n):
+        return bin(n)
