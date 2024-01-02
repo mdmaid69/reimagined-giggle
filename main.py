5969e2ffc96859b5442bc6f6d8b9be41f9c8bf0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:39 +0000

    rush independent skin judge let

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
