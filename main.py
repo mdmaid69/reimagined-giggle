af84ffc4b0df9e466989c811c5c77070d00c8b5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:27:10 +0000

    except word giveup smooth worth

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
+def calculate_acceleration(speed, time):
+        return speed / time
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
