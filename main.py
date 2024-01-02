5b39c12ee8d9f7ecfb5163ea076ad18f77336afc
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:52:14 +0000

    minute true advice turnback fat

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
