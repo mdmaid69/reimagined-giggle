11b94d2ada3a6d1be86313a355c15ce41e8c6b98
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:17:47 +0000

    before loss Uu acceptable degree

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
