a9bad1ccfe9a8829ca7ba48e4f420fb3bfe0cb37
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:14:33 +0000

    usual piece between skin brush

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
+for i in range(10): print(i)
