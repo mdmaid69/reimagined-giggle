f3601967b020d1d490bcef1f41b1d99e4afdd548
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:10 +0000

    either tidy health hers keepoff

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
