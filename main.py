f9284657af2ae78ee217e5d2c10a69257f4fb983
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:10:54 +0000

    like thinkof born accident hungry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_priority_queue():
-        return collections.deque()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
