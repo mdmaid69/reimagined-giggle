bb3b401bb3e72b204aad3cf9417943415fd68a80
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:02:41 +0000

    crowded would underneath dream valuable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
