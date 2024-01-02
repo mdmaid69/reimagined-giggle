ebae74e5cf186edb07818193f41f7fee6aafd7f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:03 +0000

    either plastic term answer suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
