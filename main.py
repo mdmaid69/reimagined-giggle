b290152579b1157c9259a19b29a54d582da68998
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:30:28 +0000

    lookback(on) autumn holdup much warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
