524f3ac2ebe4006ced4e8a70215ede678282541f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:36:59 +0000

    properly bag worst temperature health

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import platform
+def get_os_info():
+        return platform.uname()
