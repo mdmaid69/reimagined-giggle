b47922bdfc86096182835fcb24d2a373fff24801
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:13:09 +0000

    bringforward bet movement breakup pour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
