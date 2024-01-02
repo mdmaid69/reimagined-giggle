c69893886678f7d9deac72a175db9b25ab6c7ef5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:40:24 +0000

    back declare fierce compete speak

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
