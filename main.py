fc68f2d4ed9d970374d8dd0f57fd0c6f0944faa3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:30 +0000

    bright wide relation taste memory

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def find_unique_words(sentence):
+        return set(sentence.split())
