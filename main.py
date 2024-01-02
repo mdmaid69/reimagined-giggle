81e9df8f59b39f5f9ae1027cc2ac2e9ca31ba26b
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:48:43 +0000

    Ww confident violence money strict

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
