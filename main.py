4678996dd6213dceef72b96ee68e49396d1e52d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:25 +0000

    cause street neck problem date

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def is_even(n):
+        return n % 2 == 0
+import array
+def get_array_as_bool(array):
+        return bool(array)
