7810494687ac28d856413083f2aa54625dc6e5df
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:38:29 +0000

    water image beside breakin private

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+i = 0
+while i < 5:
+        print(i)
+        i += 1
