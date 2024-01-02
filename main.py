2575a5aac01b7dd7dee4fabf3e4748c05500a632
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:08:42 +0000

    bad evidence offer rush comeout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
