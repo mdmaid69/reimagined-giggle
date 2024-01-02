83ce5ae27ca0d22e1e292bca8cd3b6afd4a2f8fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:42 +0000

    fault familiar own between stand

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
-print(sum(range(10)))
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
