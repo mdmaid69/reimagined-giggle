b2f1cdf5afb453d4ee33f6869f9eb2367902f74d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:52 +0000

    favour all sore black necessary

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
