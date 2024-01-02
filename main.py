1d99dc2d8d8dea010ce9a835a1e62e48252daf61
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:16 +0000

    find hand tomorrow exploration often

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def get_current_working_directory():
+        return os.getcwd()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
