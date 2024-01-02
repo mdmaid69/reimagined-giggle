ee6260cc64ce660275e9c452b17e2604ecf0cdb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:54 +0000

    in born mysterious wear produce

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-def calculate_work(force, distance):
-        return force * distance
+def find_min(lst):
+        return min(lst)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
