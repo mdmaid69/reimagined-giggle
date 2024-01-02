8601bd86cade201f2c1d75e6055d8b3741565a69
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:49:56 +0000

    ball exit answer thirsty fine

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_index(array, item):
-        return array.index(item)
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def get_array_as_str(array):
+        return str(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
