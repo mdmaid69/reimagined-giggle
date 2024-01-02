3415720d2354b61bc8835c20aee6ac887cd3304d
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:59:28 +0000

    sight now defeat detail create

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import platform
+def get_os_info():
+        return platform.uname()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
