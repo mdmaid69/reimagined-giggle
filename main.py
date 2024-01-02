2d4adde84c3d77ddd28a0df9c5200119f7faac19
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:51 +0000

    move form crime cheque minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
