637e3c4ca628b593708a4020b9c52c3d9fae0913
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:43:19 +0000

    turnon roll attach education scare

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_work(force, distance):
-        return force * distance
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import getpass
+def get_username():
+        return getpass.getuser()
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
