f7c4c06374523c9f3fb6d957f9a5357c1c70ff8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:15 +0000

    list interfere trouble under bye

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
+import collections
+def create_user_dict():
+        return collections.UserDict()
