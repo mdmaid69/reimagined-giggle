759b609a7d81377fe7408afa364d3bb370fd46c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:50:50 +0000

    let organization fire benefit adjective

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def find_max(lst):
-        return max(lst)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
