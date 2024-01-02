3cba743dbea59867047d09e6ed13fa401f381d5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:13:33 +0000

    broken strict habit common exchange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import math
+def calculate_floor(x):
+        return math.floor(x)
+def remove_duplicates(lst):
+        return list(set(lst))
