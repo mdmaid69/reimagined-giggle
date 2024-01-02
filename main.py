6f072bc177f5e47b47a28f418b2f233994769ee3
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:54:27 +0000

    scale factory content crazy instead

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def sort_list(lst):
+        return sorted(lst)
+def convert_to_hex(n):
+        return hex(n)
