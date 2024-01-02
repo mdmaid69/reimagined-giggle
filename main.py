06bcc1bedaae767f4f6009c742cbafaf34d93c40
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:16 +0000

    moment government unhappy turninto reasonable

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+name = "Python"
+print("Hello,", name)
