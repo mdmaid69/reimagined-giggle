ee09d1b4f20156afa1ef08d008dfa5c4f21431df
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:30:25 +0000

    stay meal wide strong movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
