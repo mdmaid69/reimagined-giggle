62c4215fc85941ef20e50e0565e7d3ea07b166ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:03 +0000

    approval passage tie evidence strict

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
