a881f944cdc1e02b5312067b19cea7e6e50fde43
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:09:01 +0000

    circle bringforward round react sorry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def extend_array(array, iterable):
+        array.extend(iterable)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
