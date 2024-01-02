539c449fcd5c93ca7c424120f850da2d08ae1851
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:09:56 +0000

    invite swim circle thumb tax

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import array
-def get_array_as_set(array):
-        return set(array)
-for i in range(10): print(i)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
