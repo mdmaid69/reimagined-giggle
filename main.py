4a8966b4609226d3bb1a0d9d09c278cb11d3a9bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:32:10 +0000

    practical black describe welcome suit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+import sys
+def exit_program():
+        sys.exit()
