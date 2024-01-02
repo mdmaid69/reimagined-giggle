9ee31cd7bfd8a0e14775c7f875b107e5b1e84402
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:04:07 +0000

    leg ride urgent scare behind

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
