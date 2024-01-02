81c2fecee2765022cc0be6f078a3b75e32e34f36
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:58:17 +0000

    wrap tragedy thin letter flood

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
