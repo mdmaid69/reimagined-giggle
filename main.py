96b1100aa013a68df7bbbd4f322ed23f417cfb88
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:32:26 +0000

    fair neither survive satisfied cry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
