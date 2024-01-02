2e35d395311fd7d49f90445cd18f8b4bdb3741ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:20:18 +0000

    busy rank walk in demonstrate

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+for i in range(5):
+        print(i)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
