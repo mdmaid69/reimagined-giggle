cacd3417ce0baaab77935e88b7ca7fdb03b5902f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:05 +0000

    past division watch quick amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-import array
-def convert_array_to_list(array):
-        return array.tolist()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
