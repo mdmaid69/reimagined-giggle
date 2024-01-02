da1c4d466c4cd6c9ccf0b2f78080fb53b3048d06
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:31:47 +0000

    exist worse swim pupil Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
