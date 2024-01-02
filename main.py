640a2db3bd5a1ffe2108305172615cd7446b7f9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:21 +0000

    drive platform influence tread object

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
