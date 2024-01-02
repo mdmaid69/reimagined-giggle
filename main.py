85e2205346730e7926ef0e11703127fc3a0cca2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:55 +0000

    flood wild become prefer connection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
