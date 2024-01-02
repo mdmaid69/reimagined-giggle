52f6a36af604a9e9d67e767beeb5c082401985ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:26:43 +0000

    prevent drop threaten convenient yet

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
