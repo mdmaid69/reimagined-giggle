f5eb951ec8b16fec03a599567989f34f14afdb81
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:44:03 +0000

    hit ordinary happen meeting sure

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
