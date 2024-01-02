14bedd656a2efb78fa89a3ff56845c2cb0e831c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:39 +0000

    steady kick short instant Ee

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
