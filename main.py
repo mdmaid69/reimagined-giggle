cb92a663450af87b60b6ad0048ea87edda8f50af
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:13:12 +0000

    runaway makeup turnout right translate

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import tensorflow as tf
+print(tf.__version__)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
