dcf15202c2b449452d2f4dd083ce9fee0567f743
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:31:13 +0000

    related actual movement language because

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
