7345e940453b4b3233549ea0b5e27755f913d030
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:26:46 +0000

    look must represent mess rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  def convert_to_octal(n):
-        return oct(n)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
