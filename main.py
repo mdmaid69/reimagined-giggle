67172f7dae3e061bbad579f2f28db2ea50fa4040
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:50:16 +0000

    room shop public again sick

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import os
+print(os.getcwd())
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
