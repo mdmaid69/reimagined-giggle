afa57d85b26f51bdce87134268557ac44eeeb3f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:23 +0000

    company lower advise because hair

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def find_max(numbers):
-        return max(numbers)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
