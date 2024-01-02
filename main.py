e86e0e51855cda5ec708980584e3f147397da4cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:01:41 +0000

    pupil weekend sentence hurt person

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
