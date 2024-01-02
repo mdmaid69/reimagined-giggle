aa5504736551236c4c4bb38d7617b17f4c7f41bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:14:35 +0000

    bear frighten type loud break

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def reverse_list(lst):
-        return lst[::-1]
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
