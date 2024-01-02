02f39308b95a58264a9f37d6bc415c883a69d63c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:56 +0000

    new second urgent thirsty wide

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import datetime
+print(datetime.datetime.now())
+  def calculate_area_circle(r):
+        return 3.14 * r**2
