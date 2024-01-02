0880a9195ed87261307f9c02bd45d474bc2f5455
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:29 +0000

    belief compete possibly invite accordingto

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import array
-def get_array_as_set(array):
-        return set(array)
+def remove_duplicates(lst):
+        return list(set(lst))
+def is_palindrome(s):
+        return s == s[::-1]
