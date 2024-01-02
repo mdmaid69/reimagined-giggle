020d5b081201362bc446c70100d894f835d668cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:19:58 +0000

    progress group tour protect police

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
