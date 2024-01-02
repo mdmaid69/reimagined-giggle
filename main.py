0b1da1bc6c40ab5fefa41d033dcc6bdae60f254d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:17:04 +0000

    Zz tooth measure prepare popular

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_sine(x):
+        return math.sin(x)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
