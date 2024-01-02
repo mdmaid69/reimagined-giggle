8e0966ee7cbed4dcc7ff0debcbded7ca8bb8011b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:43 +0000

    collect charge afterwards entry major

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
