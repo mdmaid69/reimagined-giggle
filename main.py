4bbac4e4e0d1bcf92b7bdf21bd93f3a777f1ed47
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:58 +0000

    after ring use soak contrast

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
