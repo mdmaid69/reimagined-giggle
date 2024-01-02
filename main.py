373a88c23512f1ff0ce20133c496bbbb8a94c7b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:08:28 +0000

    holiday both interested breathe table

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def square_number(x):
-        return x**2
-  def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
