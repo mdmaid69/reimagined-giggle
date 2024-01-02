484247188c216405ef37a521b6d026bc4e01e331
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:55:27 +0000

    complicated experiment street change worry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
