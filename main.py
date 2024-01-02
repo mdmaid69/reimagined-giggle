52ee14f7a5a690c918785c67c8149082a9032682
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:29:09 +0000

    disturb quick collect pride Kk

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  def count_elements(lst):
+        return len(lst)
