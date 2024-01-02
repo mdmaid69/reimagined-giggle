d92cb2dd260c154373b35ef7ab4c868a78021e3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:04:13 +0000

    along suppose machine estimate thing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
