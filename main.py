751f53b6b0db714826698387c602f53a9c3da0e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:49 +0000

    powerful cheap dead threaten kick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_binary(n):
+        return bin(n)
+import array
+def get_array_length(array):
+        return len(array)
