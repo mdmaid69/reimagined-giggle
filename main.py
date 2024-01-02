0537094d4ea09a7e94f9fee50a45d9c5595f765a
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:01 +0000

    hollow birth human sell steam

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-print([x**2 for x in range(10)])
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
