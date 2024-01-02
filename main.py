a5ebc3f3e4ebd0e2a7dca1c07459674942eb6a52
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:54 +0000

    convincing store science through illness

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+def calculate_annuity(payment, rate, time):
+        return payment * ((1 - (1 + rate)**-time) / rate)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
