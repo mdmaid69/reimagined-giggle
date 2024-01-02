9f5802daabfc662da3646613ac51175d234826be
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:59:20 +0000

    production ignore treat fasten uncomfortable

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
 import array
-def get_array_as_repr(array):
-        return repr(array)
+def convert_array_to_bytes(array):
+        return array.tobytes()
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
