4eaae15bfc48b76559d55a2909481ce2d0a6cd60
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:58 +0000

    trap slow sad backwards order

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import collections
+def create_stack():
+        return collections.deque()
 import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
