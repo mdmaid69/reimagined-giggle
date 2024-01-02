cb3442cc29cb5091b40f32d90d5ec06acd25b9cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:29:16 +0000

    away he whole but human

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_int(array):
-        return int(array[0])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
