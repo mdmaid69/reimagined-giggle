91a9ba643c8d6d7e917ed14a86849e4f8c3840ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:51 +0000

    door tax win content general

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def add_numbers(x, y):
-        return x + y
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
