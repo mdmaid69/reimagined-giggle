6cd00e64395e50d9158dd50971768ec38a2ee231
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:08 +0000

    greeting at mixture yesterday violent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
