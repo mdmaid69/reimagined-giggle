60a5b0a601281102ff7875c0b88f7f907517053d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:32:13 +0000

    past meet forgive beat interesting

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def calculate_force(mass, acceleration):
+        return mass * acceleration
