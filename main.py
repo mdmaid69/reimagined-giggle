3eff7e77cd0a6ac6a829156ca3fcebfddab55043
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:00:33 +0000

    length quit bet trial unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_power(base, exponent):
-        return math.pow(base, exponent)
+def get_array_as_str(array):
+        return str(array)
