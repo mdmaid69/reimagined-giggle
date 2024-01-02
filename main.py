84470648cc8f25d96e02938e6689e719362d6389
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:05:10 +0000

    plastic event importance cost of

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_max(lst):
-        return max(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_perpetuity(payment, rate):
+        return payment / rate
