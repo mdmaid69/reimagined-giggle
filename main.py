fdf8defedf25d3aa26f03338f67529cd62065409
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:23:05 +0000

    comfortable objection neither content record

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
-def calculate_power(work, time):
-        return work / time
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
