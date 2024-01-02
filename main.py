e49997d106c9eb38d5421cb7cea11013014230b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:00:54 +0000

    season win convincing lay doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import collections
+def create_priority_queue():
+        return collections.deque()
