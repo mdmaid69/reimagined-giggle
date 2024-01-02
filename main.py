b6d23deb4956e820a05039703d01a0244560883b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:04:02 +0000

    centimetre makeup neither rather letter

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def push_pop_heap(heap, item):
+        return heapq.heappushpop(heap, item)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_power(work, time):
-        return work / time
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
