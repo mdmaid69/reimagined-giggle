c7cf0203d978a7c43dd3596cf8347ae3f0fa6d95
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:42 +0000

    many back war fairly left

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+def calculate_pressure(force, area):
+        return force / area
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_error_function(x):
+        return math.erf(x)
