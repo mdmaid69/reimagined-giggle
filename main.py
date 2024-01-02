8e8a019b392c31fe0de5f6013e0f2b70aeb9bdc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:49:30 +0000

    offence onto welcome accident comfort

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
