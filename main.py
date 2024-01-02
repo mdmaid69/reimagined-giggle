2ae8c6accff007a6e495d56fab1c7a0c4ec55d5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:15:56 +0000

    target balance thick exaggerate decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
