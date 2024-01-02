b4e11b35f52b5d0c84a2eb4123efa0bb25e7874e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:01 +0000

    total arrangement what inform house

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
