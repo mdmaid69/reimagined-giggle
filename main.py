f4c904f6cb9b80d927ac1a0f43a390bdfb868c37
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:22 +0000

    test greeting stranger method would

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import getpass
+def get_username():
+        return getpass.getuser()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
