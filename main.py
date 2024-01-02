42d11d6489686212e92cae17c7b6e9739b3498e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:57:08 +0000

    occasion be care fix mixture

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import sys
+print(sys.version)
