fad29898c96b93f21a241de96504ffd7751b848f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:53:41 +0000

    tear disappear company bread often

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print([x**2 for x in range(10)])
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
