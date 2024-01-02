057b634d7bfbf6a21784adfcbde3a6a78e1125eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:24:01 +0000

    completely bit exhibition heat cry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
