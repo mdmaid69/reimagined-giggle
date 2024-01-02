1913cdb35596c49f4206f79ee150009eb4c067e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:00:48 +0000

    ask escape deliberate translate copy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_array_to_string(array):
+        return array.tostring()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
