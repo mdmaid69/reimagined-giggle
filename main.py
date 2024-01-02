485d76f4ed5fb53def84933e0e8cd1b7410c5e2e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:53:13 +0000

    fat temperature black past shut

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import sys
+def exit_program():
+        sys.exit()
