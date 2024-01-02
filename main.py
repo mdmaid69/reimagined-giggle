4afb2f68a43fad78f5cdb8269c97d3920de8c5fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:16:51 +0000

    top plate dare free majority

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
