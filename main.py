04a3dc8aab4d760b062f58e1673c186203b9d4bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:25:47 +0000

    finish shout pinch bring strange

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+def reverse_string(s):
+        return s[::-1]
