7797699f4de5b7a8d507f5324783b6f286995dfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:19:47 +0000

    jump plant squeeze complete sure

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
