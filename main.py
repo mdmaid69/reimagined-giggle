ae5773a0f3f84fc3c7ecf94fb573365201d68ad3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:56 +0000

    completely report interruption better aim

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+import array
+def get_array_as_set(array):
+        return set(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
