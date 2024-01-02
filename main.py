b2a8bfbf92e166265e89572f5565050179caf98e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:11:50 +0000

    able increase pipe suggest but

--- main.py
+++ main.py
@@ -1,11 +1,8 @@
-name = "Python"
-print("Hello,", name)
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
