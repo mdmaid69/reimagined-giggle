b98ce64a751e6c6e780b6a7835f45ca01642edb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:41 +0000

    convince talkinto spring judge crash

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
 import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_floor(x):
+        return math.floor(x)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
