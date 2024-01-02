7a827ede3e82bc104020eb85d93df83179985dd2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:27:14 +0000

    shopping tear pleasure finish poor

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-print(os.getcwd())
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
