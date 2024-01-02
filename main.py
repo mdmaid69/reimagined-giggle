223527e88a2744a60fa48cbde7f81239fe97ca86
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:00:38 +0000

    sort politics suck task replace

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+def count_elements(lst):
+        return len(lst)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
