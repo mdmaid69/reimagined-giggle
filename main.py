33cbcf0eb067940e12b7661af6720db221b61982
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:30:20 +0000

    fit lucky correct ability care

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
   import os
-  def split_path(path):
-        return os.path.split(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
