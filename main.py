75959ae267657122719dd5b376c368c4ef4e4b40
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:15 +0000

    bother ours official giveout always

--- main.py
+++ main.py
@@ -1,12 +1,8 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import array
-def get_array_index(array, item):
-        return array.index(item)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
+def calculate_density(mass, volume):
+        return mass / volume
