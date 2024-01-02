19289f0c93fa2b78d59bd7d6fef907a156d7ccad
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:53:20 +0000

    intelligent concerned opposite market raise

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def convert_to_octal(n):
-        return oct(n)
-import re
-print(re.match("h.*o", "hello world"))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
