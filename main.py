2cd7d5867b16a39585a12361b151e4e03bafb6f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:25:02 +0000

    good large practise there spill

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
