41d763bd52d329ccd42e8feaa36c664615b71e4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:55:38 +0000

    team determined shock mind congratulations

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
