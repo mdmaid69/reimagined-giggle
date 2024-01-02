1b8d85fe187a60449b933a1b7dfeef53ad0218d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:20:13 +0000

    breakdown key until issue recover

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+for i in range(10): print(i)
