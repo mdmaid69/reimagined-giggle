2656b6ca47aff5d869dcfd8df3a5f01ed25213ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:28:19 +0000

    brave familiar any wind trade

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
