9c37ddd3c8b2b0eff3eea1455f7c9f7f8d769eff
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:22:41 +0000

    display careful keepfrom apart spray

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
