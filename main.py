5d81bfe861684c291e34ccfa759ade6ecd6a36be
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:50 +0000

    lead pot trust happen stop

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+name = "Python"
+print("Hello,", name)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
