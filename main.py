0db504274d4f2a2ebd66b13a070092d860f92499
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:44 +0000

    usually attempt smile justice instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
