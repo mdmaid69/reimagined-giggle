a68507af21f70f2c6bf431e92ad7a87ac9dc977c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:17:21 +0000

    dry shut profit bringdown space

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
