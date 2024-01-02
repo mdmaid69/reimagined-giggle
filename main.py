546962e6a70d1401ba71f0d0920bfb7985102a4a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:28:14 +0000

    backwards lookback(on) holdon Cc minority

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
