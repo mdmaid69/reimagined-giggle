6ac249ebd959d318a21960732b89f5a9f9b7979b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:04:28 +0000

    platform no over report turnup

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_current_directory():
+        return os.getcwd()
