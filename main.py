be3c9aea8758a23c149f8fc4a74fb1c30953e464
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:31 +0000

    convince close getoff rent week

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_length(array):
+        return len(array)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
