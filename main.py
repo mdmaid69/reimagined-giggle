35fce097c24802ba05906cefe5ba4f13ef403f90
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:48:09 +0000

    company human the sharp window

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
