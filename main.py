713d51e8afa693e5eaf32b708518577e61958828
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:53:00 +0000

    group drawing advice temperature practise

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
