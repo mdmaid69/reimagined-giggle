0e0c31460f0db43c1c102fab6d14be15e178d01a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:47:45 +0000

    brush gentle pronounce little ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
