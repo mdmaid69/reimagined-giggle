c20f5fee7a63eba5b32ebcbd03f327aa9c0dea9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:34 +0000

    report normal community holdup embarrassing

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
-for i in range(10): print(i)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
