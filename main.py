5cf0247e61993226928828dcd6f42c79228ecd1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:08 +0000

    sting runaway family immediate remember

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
