37569034a7416e2df84eaecc4b174ab90590d98d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:45:37 +0000

    concern squeeze snow warm meanwhile

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
