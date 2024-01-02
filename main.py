7fbcffac2d318409b1f1bd757bcebe76c10d4687
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:15 +0000

    read comefrom getaway Qq instruction

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  def is_odd(n):
-        return n % 2 != 0
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
