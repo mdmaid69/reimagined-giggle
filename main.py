df1cc120894e536f350b4615f4b8c950de6a4ae8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:19:15 +0000

    what serious excited hide band

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def find_min(lst):
+        return min(lst)
