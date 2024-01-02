3bf0e5485b0d916873a5095d3425780a0313c06a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:45:06 +0000

    gate sister voice condition cruelty

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
