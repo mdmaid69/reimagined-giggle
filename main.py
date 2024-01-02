a5b2ee3d88f0e97c855f5a039db157008e252f87
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:35:40 +0000

    imagination point mass total rarely

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+import array
+def get_array_typecode(array):
+        return array.typecode
