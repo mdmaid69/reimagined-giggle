90235b31c7d4168963689797ec82c881a865a42c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:00 +0000

    smell pure report damage callup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  def calculate_area_rectangle(l, w):
+        return l * w
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
