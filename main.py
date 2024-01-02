550865e309bc416bf8fc03844a1b19161c95f57c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:47:32 +0000

    risk difficult support decision sit

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
