55765f92351708e0f972f132af983080d2f43f83
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:15 +0000

    hard shade meeting fly the

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import sys
-print(sys.version)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
