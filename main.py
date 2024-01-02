25a43c8c32927d6b69274adc5ca673f402a2616f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:22:06 +0000

    only poor even clean smooth

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
