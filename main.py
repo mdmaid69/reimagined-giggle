e8e94787e852c54212b7d6473941ec9fec3f236d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:03 +0000

    introduce basic cope whoever entry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_numbers(numbers):
+        return sorted(numbers)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
