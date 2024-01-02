0c168a0ed9cf257a19cc4ad4fdcb9dd2433cf090
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:43:31 +0000

    interfere strange uponon thin expression

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_string_from_array(array):
-        return array.tobytes()
+def calculate_area_rectangle(l, w):
+        return l * w
+def calculate_area(radius):
+        return 3.14 * radius * radius
