1a16c8cb04ecb0c4f09cfe70e5e7a6cd2d6edb45
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:43 +0000

    relative shopping date fail keep

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
