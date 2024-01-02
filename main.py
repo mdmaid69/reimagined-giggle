39e9b61225e4d83d9fd0907a0a56ba1dff77701d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:41 +0000

    concentration aim sister onto include

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+def add_numbers(a, b):
+        return a + b
+import array
+def clear_array(array):
+        array *= 0
