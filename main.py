bba234f85a11cf9251184d785afe01f7e648a651
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:02:18 +0000

    Kk whisper consist vanish success

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+def find_max(lst):
+        return max(lst)
