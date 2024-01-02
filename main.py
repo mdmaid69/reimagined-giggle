a69339fcb5a6bcd4826f8cd6298e32781c681baa
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:38 +0000

    pretty accident silent professional as

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_area_rectangle(l, w):
+        return l * w
