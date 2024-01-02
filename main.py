468daa2684608d752da530620e09dabe4f15181c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:37:47 +0000

    commit write train takeoff breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
