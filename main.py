baed177c56c1c9da64c6fca91ee67f0220dd0bb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:51:38 +0000

    squeeze plenty collection stranger guess

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
