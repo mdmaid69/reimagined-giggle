0a180cd8ef3b16b4dbc97bcdab697e2198022460
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:28:24 +0000

    relation possession friend large thin

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  def count_elements(lst):
+        return len(lst)
