3a6dc50c26e92de17b70ce806fa5acb78eca0d94
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:08:17 +0000

    doubt hers condition switch winter

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
