1696dd8ff020f4875a624215fa5ae1b06012aff7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:52:03 +0000

    explain choice sky visit artificial

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
