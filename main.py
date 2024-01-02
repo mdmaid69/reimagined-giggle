1f9440bdcb738a572165664526331b67909c1214
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:59:52 +0000

    colour record minute organization image

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import time
-print(time.time())
-import logging
-def log_message(message):
-        logging.info(message)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
