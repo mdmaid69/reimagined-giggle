acd36eda41a4a4125c4ae2bc25f8993e220426e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:37:27 +0000

    drink inform now bang draw

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  def square_number(x):
+        return x**2
