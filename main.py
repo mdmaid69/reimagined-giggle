a927b6fd48320c0ee1cc2b59d57d8674af3b08b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:03:09 +0000

    medium choose sweet back city

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  import sys
+  def get_python_version():
+        return sys.version
