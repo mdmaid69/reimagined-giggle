77805d94ec8072d57eecf996ad3f919120e756ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:13:45 +0000

    platform careless unlikely news squeeze

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
