f89b930adfa58ab55cf28ae3a53c183df48247b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:33:44 +0000

    content warn both noisy taste

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def square_number(x):
-        return x**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
