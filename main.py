f676ca40f40e1ae04dec539773957ba90d50c706
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:25 +0000

    firm shallow runaway practise vote

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
