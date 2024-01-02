16f6015cf8454e3d1fbc940fca5f6cd680d7f1a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:09 +0000

    warn common and interview also

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
