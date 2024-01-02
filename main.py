7eec7af3437a09fe8d1eeb43a478dadf58a9b1cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:24 +0000

    sort gooff pipe uponon later

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+def convert_array_to_bytes(array):
+        return array.tobytes()
