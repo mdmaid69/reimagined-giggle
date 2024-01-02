c3caebe7667df4c22db67af8be0144d4700d1146
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:59:09 +0000

    vote disturbing raw bus undo

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+x = 10
+y = 20
+print("Sum:", x + y)
