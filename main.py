fa85a8a50acc1d68a420620aa8e8283185b81feb
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:05:07 +0000

    message partner single hers crazy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  def add_numbers(x, y):
+        return x + y
