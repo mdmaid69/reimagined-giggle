187148978cedfe3305726db38025608eb1ca2e9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:26:22 +0000

    field right they all none

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_queue():
+        return collections.deque()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
