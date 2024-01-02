7cc1a15ccec5e78a2cba1f00fb4ef0eadd52ad7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:48:07 +0000

    listenfor popular after sail public

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
