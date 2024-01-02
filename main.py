53a09599a6b8b028b0e31e4abcf8dae304ae13cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:22 +0000

    pour legal tidy pick union

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
