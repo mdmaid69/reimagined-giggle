368bf00e10ff2d389ed111ebd039416edf177d00
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:37 +0000

    runover neither cough computer clue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_user_list():
+        return collections.UserList()
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
