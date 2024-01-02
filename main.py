c93f29b912dbf1bf53432a3f735864df87e4cf9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:38 +0000

    nowhere expensive arrive across crowd

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
