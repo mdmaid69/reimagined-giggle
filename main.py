bd14fef980c2b3bae540bdc6516d94b2891ab108
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:36:22 +0000

    attitude declare likely comefrom organization

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+  def convert_to_binary(n):
+        return bin(n)
