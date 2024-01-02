781cda67497e6e9ac88fee72e6f826ad0da73283
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:31:56 +0000

    less better young fine report

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+  def count_elements(lst):
+        return len(lst)
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
