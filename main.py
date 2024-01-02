d2065de6c98e47efc56cb20399a0e79e7f85f298
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:14 +0000

    pity on boil run breakfast

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import json
+print(json.dumps({"name": "John", "age": 30}))
