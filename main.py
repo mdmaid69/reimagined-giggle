9e79ca8aa26e7fd55365fa5d038600a0770b8a0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:42:37 +0000

    fond stop thinkof the judge

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-import math
-def calculate_sine(x):
-        return math.sin(x)
+import array
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
