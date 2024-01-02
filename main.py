e9fa8b7ed323f100bcd78b04cf5072645881fcb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:20 +0000

    remember explain Kk dish frame

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
