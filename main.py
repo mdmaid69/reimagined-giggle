e8483d2ffa6f7cfc946983e8b8dc04687e66031c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:42:00 +0000

    later second 179bite life depend

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+print([x**2 for x in range(10)])
