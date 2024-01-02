582f1d1620056329f9915260daff9ae7819fa771
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:51 +0000

    distant believe over involve truth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
