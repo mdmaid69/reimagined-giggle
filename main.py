b85bea12db013c06dbc769571940ab3cdd6755ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:17:14 +0000

    common record rob loan by

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-def is_even(n):
-        return n % 2 == 0
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
