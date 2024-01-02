e5705a1562ff380a4fbfe080bddcaf6042b779a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:14 +0000

    dinner near mark promise exact

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
+import os
+def get_current_working_directory():
+        return os.getcwd()
