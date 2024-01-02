8275e936db8b10a6afb2fe4f74b54a4b25dda0f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:12:20 +0000

    shine truth scheme force cope

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
 import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
