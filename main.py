30dd679495615483712c8bebec6ed392c014746b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:51:35 +0000

    that comfortable quarter godown catch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
