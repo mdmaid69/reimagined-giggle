4f3649519599ac8544aafa0da006724ad9ad45bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:12 +0000

    estimate not smoke connect admire

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def reverse_string(s):
-        return s[::-1]
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_string_from_array(array):
+        return array.tobytes()
