3f310a86255a120fb20f66c0268b93ac99c427dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:48 +0000

    favourite effect round constantly independent

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import os
-print(os.getcwd())
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
