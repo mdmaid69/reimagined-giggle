7ff17eae8d9e7e09b955144a9b2ab9409a183e20
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:38 +0000

    exit tradition sometimes air culture

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
