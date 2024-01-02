6bcbcd3f271c15f3bfd9cd51c64611a4216d5eaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:27:03 +0000

    frighten and game breath familiar

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import numpy as np
-print(np.array([1, 2, 3]))
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
