07c700a1de4d1ce8a84913edbcc29fb464820935
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:42:56 +0000

    anyone pressure scream used minimum

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
+def convert_to_binary(n):
+        return bin(n)
