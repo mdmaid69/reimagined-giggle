7a9b5c1d62664c3ff3638438873dabcc75a9e78f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:48 +0000

    firm improve culture mysterious forward

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
