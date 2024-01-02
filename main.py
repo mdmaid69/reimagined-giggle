40c81407087bc36dacce446e596682d0fa3547af
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:15 +0000

    drop excited the include something

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
