9bf164c803667c24021565cec789da4fa47763fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:04:24 +0000

    long work say understand run

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def cube_number(x):
-        return x**3
-import collections
-def create_stack():
-        return collections.deque()
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
