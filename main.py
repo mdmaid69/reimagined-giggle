fe63271bee37542abab9958df4aead9ee5eca3b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:36:01 +0000

    work approve translate cost he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import time
+def get_current_time():
+        return time.time()
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
