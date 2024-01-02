e2133f7a17d29c2f707c9bd878b56d2ad66c1424
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:16:41 +0000

    proof exchange luck call sting

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  def remove_duplicates(lst):
+        return list(set(lst))
