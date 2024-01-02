3f63c5fe0a9687241d3f0115a1ac166d76afec2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:43:40 +0000

    approve noone use during Pp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
