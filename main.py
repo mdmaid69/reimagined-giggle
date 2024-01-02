b0f377ea05190c09adf18e9e9c3c2ddf58b7f8f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:06 +0000

    sink disappointed remark stir loose

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
