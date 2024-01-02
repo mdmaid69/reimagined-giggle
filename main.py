3947807cf591857a2f8780a14fe5d098e13a42be
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:45 +0000

    year use copy abroad walk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
