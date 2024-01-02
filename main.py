ba7424c70e7e3fee5548ff43f5fcd8c66803ef17
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:50 +0000

    justice kind copy face weak

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
