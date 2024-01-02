0c2d2431ec905d1f884bfdaf8e9c93b69a435138
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:03 +0000

    genuine may effect sit when

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
