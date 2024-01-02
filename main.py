e4a160fbc9c64a6ce4d999facedd38ac852af633
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:01 +0000

    understand apply Vv instrument professional

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_binary(n):
-        return bin(n)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def split_path(path):
+        return os.path.split(path)
