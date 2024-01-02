36d1b397e7c64cc3b580a2a4a47fba958e555d02
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:54:06 +0000

    someone closely nowhere opponent chase

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
