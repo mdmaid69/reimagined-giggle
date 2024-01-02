1fe2160c16ec839324bb5978f0a629551628ef6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:38 +0000

    convinced just black depend think

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
