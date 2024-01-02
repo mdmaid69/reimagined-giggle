d3b781f55f9f2454d308d1f0a09558d04556b162
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:38:10 +0000

    mix speech breathe successful become

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def is_palindrome(s):
-        return s == s[::-1]
-import getpass
-def get_username():
-        return getpass.getuser()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def find_union(list1, list2):
+        return set(list1) | set(list2)
