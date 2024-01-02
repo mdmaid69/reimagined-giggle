4648c102f1ad925f7dc82d7e55aaaa7673b484d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:19:58 +0000

    too arrival early tradition man

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-def calculate_speed(distance, time):
-        return distance / time
+import array
+def get_array_length(array):
+        return len(array)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
