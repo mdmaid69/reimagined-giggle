d894221f311eabd89b5f96803872111eb09223cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:03 +0000

    basic warning lift just spill

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
