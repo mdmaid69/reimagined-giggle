ea619c8ff401ddb83fb5e395c05b19a1d45f2ddc
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:24 +0000

    thinkover behave snow match change

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
