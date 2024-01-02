02202ca2a4eb456033ebccdcbca1ba90c25711e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:09:59 +0000

    either ball stick unable peaceful

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
