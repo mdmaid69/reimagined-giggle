bddaaac9f3bdedeeb5987df443935d46fb4ddccc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:43:06 +0000

    charge deliberately match name love

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
