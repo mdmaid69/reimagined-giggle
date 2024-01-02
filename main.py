3b67f41d925217e96229ee0651f2f048839d1b71
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:29 +0000

    talkoutof tonight rubbish often 566end

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
