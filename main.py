e812dfca078b0c9835fcf86191b9009fd3190dba
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:34:57 +0000

    leave recognize subject sink bound

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_work(force, distance):
+        return force * distance
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
