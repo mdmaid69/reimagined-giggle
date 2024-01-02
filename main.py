9d56a596e5206a49399e3e2817ca98a66a1b69e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:09:06 +0000

    impatient which cheap collect regularly

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
