2e7cd2343b4f8e567c89b6cc0ff7c39fb5991a70
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:06 +0000

    tense bye book fortunate firm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import datetime
-def get_today_date():
-        return datetime.date.today()
+import collections
+def create_queue():
+        return collections.deque()
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
