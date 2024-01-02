07d75a7c8127d6ff674119504b9f4673b013a99d
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:44:47 +0000

    suffer again bad reduce ought

--- main.py
+++ main.py
@@ -1,12 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
