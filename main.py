ff83501f95075cb16b7d05b70e19d24b14c0578b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:07 +0000

    difference stiff breath fault contain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
