2ea11e327022566fe17c6c57098e34c8cd0fba91
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:49:46 +0000

    pride know year talk trick

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+from collections import Counter
+print(Counter("hello world"))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
