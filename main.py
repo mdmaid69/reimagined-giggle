b7c5eb64a4c55044a278c0475b3840abc6a54480
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:56:11 +0000

    face delight herself present destroy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
