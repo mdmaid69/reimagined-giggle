6160710f98da3ad384eef484274b31415e7312e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:15:40 +0000

    new contrast pipe impossible contain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
