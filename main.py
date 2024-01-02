6361bbecd013a474113942e4d280b2585afe5178
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:53:52 +0000

    contents worry glad confusing goodbye

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+import sys
+def print_python_version():
+        return sys.version
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
