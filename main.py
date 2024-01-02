abbb47709f2f4c085a971ed2d679ffe2013c430f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:39:18 +0000

    bother sail door scheme sky

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_as_bool(array):
+        return bool(array)
