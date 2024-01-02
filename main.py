4f36fc33c449cedc913938aff7a1de1373039d0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:23:47 +0000

    somehow collection think cope he

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
