1c32684b6fc97df86d47357ef7bea426f4e43370
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:42:39 +0000

    early contribute practise week cover

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
