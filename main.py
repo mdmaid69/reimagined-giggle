e2306a63f26887d5150dff36d1e815bd2a812326
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:12:05 +0000

    blame ball towards thanks badly

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-def count_characters(sentence):
-        return len(sentence)
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
