16d37da3cee2f8989a1b438f3ec9a41c594c8755
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:02:19 +0000

    break system somewhere sister doubt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import array
-def get_array_item(array, i):
-        return array[i]
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
