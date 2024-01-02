5a478c81b573c3f03b1183e6f6c50c26f070d4a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:27:02 +0000

    regularly reliable entrance boring try

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
