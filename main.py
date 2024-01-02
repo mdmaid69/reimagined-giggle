3d65e3b09af470de73e7fd944d838cc6418c6463
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:50:09 +0000

    runover pretend wise attract Yy

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
-def remove_duplicates(lst):
-        return list(set(lst))
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
