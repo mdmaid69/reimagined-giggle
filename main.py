5753e515f91aa452cd9f634c30fc9a56c8c02d8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:07 +0000

    advice hardly wet along breakout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_stack():
-        return collections.deque()
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
