2f5fe5b426df7ddc9d9a44457d906990adc3f13d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:06:58 +0000

    eye original under break list

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+def convert_to_hex(n):
+        return hex(n)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
