946f6c5847bd8ea7ef3b520d7e2555e1d5aaa8ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:50 +0000

    anywhere spread whichever ago cold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
-def calculate_density(mass, volume):
-        return mass / volume
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
