89162d1c0b511caa3a38fd390d2f13e0ec739f0a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:45:48 +0000

    mother base constantly little separate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def get_array_as_repr(array):
+        return repr(array)
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
