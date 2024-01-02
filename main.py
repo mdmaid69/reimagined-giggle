79734c57d844b6446b9510f3673873f8cd53f2e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:43:50 +0000

    report sometimes mad open sad

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def count_characters(sentence):
-        return len(sentence)
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
