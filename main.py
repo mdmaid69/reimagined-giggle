69b2f41c70ce7afb22f8dd6c8394acd135a0493d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:29:10 +0000

    exhibition manage everybody convinced confidence

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def reverse_list(lst):
+        return lst[::-1]
