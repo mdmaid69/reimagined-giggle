17a33581c4c0ed4e069d24094cec5ebceafa45a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:42:35 +0000

    murder collect trial giveout callat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def count_words(sentence):
+        return len(sentence.split())
