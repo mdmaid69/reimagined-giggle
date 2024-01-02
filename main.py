cb034d82ed85cd4993b782503d84db3658088582
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:06:13 +0000

    rate bar spin mind family

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
