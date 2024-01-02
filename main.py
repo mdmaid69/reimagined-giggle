98401ced6f3a2547a04957ee5fde2183037fa678
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:32:35 +0000

    laugh risk crash music bottom

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
