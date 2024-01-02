720b9210a01e4b190681d1c811394f9344988fa0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:54:16 +0000

    block comfort cum move correction

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import re
-print(re.match("h.*o", "hello world"))
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import collections
+def create_counter():
+        return collections.Counter()
