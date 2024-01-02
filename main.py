93cc70efc388368d4e78d3961a61de97335cfab6
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:23:33 +0000

    sister runinto Oo listen happy

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+text = "Hello, world!"
+print("Characters:", len(text))
