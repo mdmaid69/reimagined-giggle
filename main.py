00505fdeb8933ef5f77e5ec57089902319b87570
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:03:01 +0000

    rough popular strict convincing position

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
