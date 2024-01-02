64af9e0b36a8c1eb1c0459f4a7cbd6ac579788ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:34 +0000

    tax freeze firm change over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import array
+def get_array_as_list(array):
+        return list(array)
