4abdb0d89ed567efd93116b95026ab04ac4ac7b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:15:56 +0000

    number science dress grateful reasonable

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
