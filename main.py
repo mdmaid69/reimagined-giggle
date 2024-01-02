a6b58c24999ce975fb2f6c32370ba38b279e2d4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:31:12 +0000

    rich female involve discover relation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def convert_to_hex(n):
+        return hex(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
