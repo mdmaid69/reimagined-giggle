8bcdeae4f56c3335c5900c9763b7a155c1569605
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:51:27 +0000

    hear Vv profit century like

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
