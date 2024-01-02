81e13557802ba29a12c66421b5e460e8ca8b8778
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:43 +0000

    thorough more lead long late

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
