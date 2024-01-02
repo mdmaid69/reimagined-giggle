d83b8fab5b0a68e7aa9595b0686381e24250d624
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:11 +0000

    bright culture strict dark bored

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
