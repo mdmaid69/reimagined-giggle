f01f69d1b3f8ac0ea16deaabb59b4bba0eba350c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:43:37 +0000

    afterwards punish stretch Cc risk

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def delete_file(file_name):
+        os.remove(file_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
