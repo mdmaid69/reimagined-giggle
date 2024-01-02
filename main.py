05e66f21a511d7f1db88d784a6bf50e50beaa03b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:57:49 +0000

    natural breakthrough damp clean task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+  import os
+  def get_file_access_time(file_name):
+        return os.path.getatime(file_name)
