00dfe508043d3a31a498675caada36376248a65c
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:07:35 +0000

    stand dry wind news violent

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_volume(length, width, height):
+        return length * width * height
