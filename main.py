c847b3072a22739b929b1b7137becf88981840ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:51:36 +0000

    straight the fear house mine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+def sort_list(lst):
+        return sorted(lst)
+def sort_numbers(numbers):
+        return sorted(numbers)
