5c3db7de27573dded1ba0f3d1960792d3edffb68
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:33:49 +0000

    corner body swing 566end influence

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def clear_array(array):
+        array *= 0
+def remove_duplicates(lst):
+        return list(set(lst))
