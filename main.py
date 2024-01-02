c40993c8dae5761b55ec46965161c97a39e93150
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:53:07 +0000

    reply hurry wake command drink

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+for i in range(10): print(i)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
