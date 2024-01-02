a606908bd22116083fea6ddc90ba1d9274227fe1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:58:15 +0000

    steam lookback(on) right rare name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
