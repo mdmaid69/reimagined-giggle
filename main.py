d57cd4145bc072218b969d60eeec79f9b638290d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:29:27 +0000

    someone great laugh history wall

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def clear_array(array):
+        array *= 0
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
