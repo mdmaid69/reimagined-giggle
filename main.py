4bc0e83883837cd72fd1b89fef0cbb1a3e4b39a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:21:02 +0000

    danger before style benefit close

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import datetime
+def get_current_date():
+        return datetime.date.today()
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
