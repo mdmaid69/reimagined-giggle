7157525c364b71e7367a018b069f504569137eed
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:29 +0000

    me complain not care weak

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-  def sort_list(lst):
-        return sorted(lst)
+  def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
