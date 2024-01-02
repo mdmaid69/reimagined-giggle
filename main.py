5de72ef149a2297b3cd5055007ae232b8881df47
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:14:41 +0000

    round sort manage objection offence

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
