1ee9a2728af7e69a55c2b83dd7304663027fc552
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:24:50 +0000

    pretty prevent drink family crisis

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-  def cube_number(x):
-        return x**3
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+import array
+def get_array_as_int(array):
+        return int(array[0])
