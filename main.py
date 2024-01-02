ebe259ad83e7f9338c2f4a9135b71b2eff07087f
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:21:01 +0000

    eat Zz shoulder stranger world

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
 def append_to_array(array, item):
         array.append(item)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
