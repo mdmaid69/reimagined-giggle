f24f47690f163f63719968fb99d5ad5e676c649f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:59 +0000

    even exactly be on fat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import collections
-def create_stack():
-        return collections.deque()
+import array
+def get_array_as_str(array):
+        return str(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
