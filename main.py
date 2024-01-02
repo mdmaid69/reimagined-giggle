f192082cdc6b79bdac8f0a4c7d418eaef4557758
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:31:22 +0000

    sit concerning future closed once

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def calculate_area_rectangle(l, w):
+        return l * w
