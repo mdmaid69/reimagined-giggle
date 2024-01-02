f991d6a331deef262ed32b7bfd9af8c8cb8c5c4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:08:49 +0000

    strike address fit comeout but

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
