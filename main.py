9b92d1b438b53f12f67aa496a72d09ecb7abc0e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:08:51 +0000

    especially as shout nice introduce

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def calculate_power(work, time):
-        return work / time
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
