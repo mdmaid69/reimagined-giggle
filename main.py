0b886fc60a60c17f8b0d09160ffef23ff8bb32ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:24 +0000

    fun wonder full vote phone

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
+import datetime
+def get_today_date():
+        return datetime.date.today()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
