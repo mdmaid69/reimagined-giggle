5d5c3ba62208d8c967b47709d560769d08bbab65
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:20 +0000

    polite clear several interfere feed

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
