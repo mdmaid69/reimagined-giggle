49557d4e8348bf01dee9a0be2c11c7887b1287e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:36 +0000

    lock be love where likely

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
