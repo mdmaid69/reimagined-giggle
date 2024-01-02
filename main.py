2db88945107e24975cad8ba2188b0a0e4e6dd0f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:52:10 +0000

    reduction breakup by flat profit

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
