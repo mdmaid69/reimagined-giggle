1b932395b556b18f69079c198c57311966e18a70
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:40 +0000

    little borrow Ee cover rest

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_work(force, distance):
-        return force * distance
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_sign(x):
+        return math.copysign(1, x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
