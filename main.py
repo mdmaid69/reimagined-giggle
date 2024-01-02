52dd1477b95ed1a4bf85bab6522d5c62b5eb7cce
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:38:21 +0000

    certain getback over visit famous

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_length(array):
+        return len(array)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
