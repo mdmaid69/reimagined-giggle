e71922efc4ef429722f7176da44228427d807b44
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:27:41 +0000

    we careless move desperate weigh

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import logging
-def log_message(message):
-        logging.info(message)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
