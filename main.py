6acd611f1dd9f0cd1471cc3d77b985b8f44aac23
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:27:25 +0000

    here autumn without house weigh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  def multiply_numbers(x, y):
-        return x * y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
