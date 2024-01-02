5b0827cb20e6a3e26e7d294449531adf0e47f8fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:00:23 +0000

    rich forever evening task Hh

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def subtract_numbers(x, y):
-        return x - y
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
