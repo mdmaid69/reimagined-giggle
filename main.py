297fa7712dfa94c703a2f0529212165ce4195b8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:57:17 +0000

    original mainly mass passage attitude

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
