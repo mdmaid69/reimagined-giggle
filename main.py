81ade7d6e34352a4cc48419bdb1a40bde9b7c197
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:17:04 +0000

    track late below come upstairs

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
