e2af55226a1a6598f3b2891ba84a84f3c46ca31a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:59:50 +0000

    565end respect success row kind

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
