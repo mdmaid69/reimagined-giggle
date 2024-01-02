ae70167ed84f0c92fd0032afd5418ff0743e0832
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:29 +0000

    myself play insist round pocket

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+def calculate_work(force, distance):
+        return force * distance
