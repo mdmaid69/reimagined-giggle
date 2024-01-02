40569a952ae695d95989741c5396efcd3991d2a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:18:00 +0000

    group may ago sorry lean

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
 import array
-def remove_from_array(array, item):
-        array.remove(item)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
