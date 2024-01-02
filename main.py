7066200cfe4f44e4722e41dcd375349e92669f21
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:36:52 +0000

    minimum turnoff life command discovery

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
   import csv
   def read_csv_file(file_name):
         with open(file_name, "r") as file:
           return list(csv.reader(file))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
