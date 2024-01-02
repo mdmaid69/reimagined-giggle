b371524d8e091a0b4185b2bf124a08f5b0230312
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:08 +0000

    national if still very kind

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def find_max(numbers):
-        return max(numbers)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
