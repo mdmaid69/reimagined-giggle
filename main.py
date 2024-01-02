5d5c10c3e3a128b9341b00f430f90ef5518379a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:32:45 +0000

    culture wrap read length movement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
