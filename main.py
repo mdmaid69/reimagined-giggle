99b0c9224004d3a52198ddb070061ccf69f3cae0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:05 +0000

    run wrap 569enjoy early before

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-def calculate_power(work, time):
-        return work / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def count_elements(lst):
+        return len(lst)
