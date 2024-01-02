0e29304fb2e9d1d06913d5bf4614ba6254d5c8f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:02 +0000

    shock regret under otherwise only

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
 import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+print(math.pi)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
