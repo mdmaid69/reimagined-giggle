b4665a3f916ecd11e91e9a6540ac60a43e9c0f89
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:13:02 +0000

    comeover upstairs measure control content

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
