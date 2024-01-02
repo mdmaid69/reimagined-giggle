7cbe10fdbdc091bf9eafc1bf1d0bec31a52616f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:12 +0000

    fly fine whistle home hello

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
