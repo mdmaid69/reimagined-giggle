8c55907f3dd6dfe12ced1577654bb3bd444d228f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:15:56 +0000

    bye explanation command break mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
