2288ee5803786a2032868c7c04a76b588d88f701
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:13 +0000

    similar weak time defend rubbish

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
