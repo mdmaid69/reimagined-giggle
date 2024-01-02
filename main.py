93304c5e6503d00570a10f2556c8252c07708cbf
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:15 +0000

    door turn distance responsibility unite

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
