3b3cb65548c87a1ea8b8af1303a18c6183a1fa4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:56 +0000

    activity couple come affect undo

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+  import os
+  def get_current_directory():
+        return os.getcwd()
