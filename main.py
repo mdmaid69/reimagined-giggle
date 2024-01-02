f935358085271e79f1b2ba758f81a65b2ec17a6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:12:02 +0000

    deserve I conversation offer gate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
 import array
-def get_array_from_bytes(bytes, typecode):
+def convert_string_to_array(string, typecode):
         a = array.array(typecode)
-        a.frombytes(bytes)
+        a.fromstring(string)
         return a
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
