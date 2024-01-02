3641b9d4ecd6fab7b0942fa59fc892ec90dbe7d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:20:05 +0000

    upwards chief authority soak burst

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import random
+def roll_die():
+        return random.randint(1, 6)
