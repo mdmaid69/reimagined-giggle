930c83eae91146a5332765857a32560c98912b2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:27:20 +0000

    disturbing apparently common fun character

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import collections
+def create_user_string():
+        return collections.UserString()
