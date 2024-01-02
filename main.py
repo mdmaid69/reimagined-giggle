8478a721eb8d2083fbb26f7c687dde6da98e576f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:06:57 +0000

    urgent away mean create keepfrom

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-name = "Python"
-print("Hello,", name)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
