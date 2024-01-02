0ec27583f46d2c0fe1d0a3260ccac1c489332992
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:37 +0000

    carry trust leg lay search

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
