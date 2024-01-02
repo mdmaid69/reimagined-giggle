2f607f75876c582ad87d9c4060a2ab3758e9aad3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:08 +0000

    silent minority machine want rich

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import array
-def get_array_as_set(array):
-        return set(array)
+def convert_to_hex(n):
+        return hex(n)
+  def square_number(x):
+        return x**2
