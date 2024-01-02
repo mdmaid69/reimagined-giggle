b969c0e3b35cd55dd8fa80069eaee5176a2d7de4
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:36:26 +0000

    congratulate beside trade will suit

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
