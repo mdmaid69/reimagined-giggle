77c382edb8b58b133d2e0d11e8650f84b2045d80
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:45:07 +0000

    risk OKorokay yours suit spend

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
