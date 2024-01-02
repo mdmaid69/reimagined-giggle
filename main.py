0d5e84610f7be46d4675657ad43bd2d0f108e29d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:04 +0000

    warning mainly complete easy return

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
