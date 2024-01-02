a819c3b1935dfd4a1220455966909c7e978bae90
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:20:03 +0000

    news consult expensive platform copy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import time
+def get_time_since_epoch():
+        return time.time()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
