c5ba794cbf07d72a0bec408e5953c4527f68fd0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:49:57 +0000

    spend busy interesting opinion bringdown

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
