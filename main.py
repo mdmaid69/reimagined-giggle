05f144eb405fabef5a5ad54e893424af4adb8fc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:39:27 +0000

    which team phone loss connected

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_current_directory():
+        return os.getcwd()
