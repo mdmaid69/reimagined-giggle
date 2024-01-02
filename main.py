0564b705e15eb34684d1576d9b402ad937c28850
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:56:33 +0000

    something someone relax OKorokay book

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
-def is_prime(n):
+  def is_prime(n):
         for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
+          if n % i == 0:
+        return False
         return True
+  def convert_to_octal(n):
+        return oct(n)
