d5f388ed919f72038f3fc5bf5dc1b34451d4de0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:51:23 +0000

    strike recognize tradition paper prove

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+def calculate_acceleration(speed, time):
+        return speed / time
