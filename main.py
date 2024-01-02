17929fc472fdfa34448732b29ae8e23309959960
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:07 +0000

    murder aim beautiful grade boat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  def sort_list(lst):
-        return sorted(lst)
+def convert_to_octal(n):
+        return oct(n)
+import array
+def get_array_index(array, item):
+        return array.index(item)
