fac2237a514710c559b06a3fea9d793212a213c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:56:39 +0000

    instead badly fail worried confused

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def find_min(lst):
+        return min(lst)
