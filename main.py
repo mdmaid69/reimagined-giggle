926dddc0787012e7b06b2985775ad2b9ee566e35
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:56 +0000

    turnoff minute favour demonstrate professional

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import os
-print(os.getcwd())
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import collections
+def create_queue():
+        return collections.deque()
