af4b12aff37f9e678fb9f33dd71e3d24fb392191
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:59 +0000

    generally clean tool apologizeapologise 570enjoyable

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def calculate_area_rectangle(l, w):
-        return l * w
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
