5d1be7d3ea4039f81f1d4af536455f565cba6b09
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:28:03 +0000

    prefer crazy beneath say naturally

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
 import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
