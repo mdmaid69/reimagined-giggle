ea701bf94af8d2cff3484d864525307c52e67b2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:49:50 +0000

    attempt help game hurt probably

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
