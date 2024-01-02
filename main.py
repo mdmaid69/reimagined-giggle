d63e11585383944ef52313baa22afdf84eb1bd79
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:02:33 +0000

    justice under study crowd drop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def clear_array(array):
-        array *= 0
-def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
