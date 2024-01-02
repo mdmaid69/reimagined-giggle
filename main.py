ee4f26cca2cbdb908fd71f0f5c965e4c1bad8389
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:19:09 +0000

    expect level by fire file

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import os
+def remove_directory(path):
+        os.rmdir(path)
