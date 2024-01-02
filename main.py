68d9ef7ceee5788ac8f1e2b1d48cbb847dbff9f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:54:07 +0000

    perfect middle club greeting round

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def reverse_list(lst):
+        return lst[::-1]
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
