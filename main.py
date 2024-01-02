edb9cf58672b0d390ccdc7f3fc0d49da0cb9ef31
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:50:58 +0000

    meaning safe speak reach high

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
