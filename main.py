19c5bb2011c5ddbf6bb1ffc58492289f8d2b146e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:26:17 +0000

    clothes survive fly like lead

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import array
+def get_array_itemsize(array):
+        return array.itemsize
