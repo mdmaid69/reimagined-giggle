aafc14a547c4a1e9b05503cbbf313b16019656d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:51:48 +0000

    disturb walk before doctor drive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
