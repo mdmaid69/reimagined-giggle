d6ffcc5637cc5815ffd7c32a1d4de47ce7905370
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:15:13 +0000

    steam soft which unit help

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
