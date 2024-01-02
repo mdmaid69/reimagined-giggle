3fea66a8de7d2ce2e160357b5f3f13aff34a2ac8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:47:04 +0000

    rush original operation difference truth

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import collections
+def create_counter():
+        return collections.Counter()
