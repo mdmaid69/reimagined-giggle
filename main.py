3320fb7438d5af9d82540737f7d518bd14f99d90
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:10:29 +0000

    run gift control dry round

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_current_directory():
+import os
+def get_current_working_directory():
         return os.getcwd()
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
