246941edfcc3eb2d9b9f0f4f5cbc21a26021e16d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:28:51 +0000

    card alive interest break though

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
