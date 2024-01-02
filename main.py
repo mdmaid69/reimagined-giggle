742b821250d310418b4b221ea6e1598a5baa8462
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:36 +0000

    it holdon rule point communicate

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+def count_elements(lst):
+        return len(lst)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
