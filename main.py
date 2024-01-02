a58eb00af0378bfc38a0d9255e3ac0dd3c056cce
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:32:47 +0000

    bread Gg high trade Jj

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
 import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+def iterate_over_array(array):
+        for item in array:
+        print(item)
