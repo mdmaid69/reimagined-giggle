3b138223cf870c56c080cbe35fbdbd0bbaa347b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:28:08 +0000

    form power make in second

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+  def convert_to_hex(n):
+        return hex(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
