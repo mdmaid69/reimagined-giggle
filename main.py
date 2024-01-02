f174efd3fbf64caeec563be0f9359834fa112fa2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:04:18 +0000

    book ignore stranger bang reduction

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def count_characters(sentence):
-        return len(sentence)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
