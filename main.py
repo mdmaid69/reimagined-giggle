498b921387ac5ce4cc46fe65dc27cd001489fbe5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:44 +0000

    564encourage contribute Oo warning confusing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
