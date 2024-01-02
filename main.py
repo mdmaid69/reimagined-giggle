29889002af332684eee553e3c6a3d3c1e22e7901
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:18:03 +0000

    immediate story apply quantity if

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
