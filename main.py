f86fae8dac0c63ab2b0b38d2634f3e7c33e68af2
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:50:51 +0000

    opponent view order pride sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-import random
-def roll_die():
-        return random.randint(1, 6)
+import os
+def change_working_directory(path):
+        os.chdir(path)
+def convert_to_hex(n):
+        return hex(n)
