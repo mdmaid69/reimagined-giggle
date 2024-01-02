c1084376359b36b9454e50e3d014316a1e60e6b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:37:57 +0000

    trick available meaning beneath plan

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
