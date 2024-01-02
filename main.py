0df6d19d908d755808945c178d9c7d27e587f61a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:39 +0000

    bother become backwards level protection

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import array
+def get_array_length(array):
+        return len(array)
