8c6dec3d14c230c66f613093711ad7bb1d1b7d0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:11:58 +0000

    knock show provide limit negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def sort_list(lst):
+        return sorted(lst)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
