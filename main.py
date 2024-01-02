0cc8640677ab37a096e59816662b2da3ab5b2a24
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:47:11 +0000

    sale fire mess turnaway mainly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
