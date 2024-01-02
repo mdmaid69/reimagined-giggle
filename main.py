15b7afb217224880b191de164e62a7384053358f
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:25:25 +0000

    watch cross hit age Oo

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+print(sys.version)
