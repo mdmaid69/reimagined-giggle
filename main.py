fbee63eed5042b7425b536903a24b1540693aa99
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:27 +0000

    special charge fast load somebody

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-import array
-def convert_array_to_string(array):
-        return array.tostring()
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
