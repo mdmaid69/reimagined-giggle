683c8bf6a6693f1b043a6346ea7ac08bffca5ced
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:55:50 +0000

    underneath length object future increase

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def is_even(n):
-        return n % 2 == 0
+def calculate_pressure(force, area):
+        return force / area
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
