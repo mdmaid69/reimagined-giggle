56128f0f5651ce7ecd85cd93b03f2786afb9ba2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:55 +0000

    necessary friend arrive suckme basically

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
