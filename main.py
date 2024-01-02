e11fc7ef87e7623f95934985acf5dcfa8a1650a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:08 +0000

    its contact lucky argument how

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
