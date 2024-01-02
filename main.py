54536f3f76c1eba69b595b90d58491b163244e9d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:59:50 +0000

    exist record tongue stop pressure

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def count_elements(lst):
+        return len(lst)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
