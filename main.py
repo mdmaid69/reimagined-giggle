76f0ce330f34b4d495836d669b5a33396fac2068
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:42 +0000

    light transport hole score attach

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
