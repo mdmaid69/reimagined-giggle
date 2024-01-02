058a53319043eaca0a05598074feba46739b0062
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:18:47 +0000

    violent settle unit actually clothes

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
 n = 10
 print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
