fd9eb1527508656541456dc5b5982c0ff51a8612
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:06:55 +0000

    realize demonstrate through mouth tend

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
