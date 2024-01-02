1d1168737db608fbde28f774d17d155c38675197
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:14 +0000

    centre well me excellent threaten

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import logging
-def setup_logging(level):
-        logging.basicConfig(level=level)
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
