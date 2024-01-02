00916d7ac2372106215197a2ce56c94ba77d97c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:08:40 +0000

    breakup judge type entrance favour

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def subtract_numbers(x, y):
+        return x - y
