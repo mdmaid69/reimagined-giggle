b057fa9549a625393ef6bf50772cd7a67c9118f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:14:13 +0000

    easy born meaning morning nothing

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-  def reverse_list(lst):
-        return lst[::-1]
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
