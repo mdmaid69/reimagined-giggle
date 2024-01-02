c47fed581b92bbf1ba77f4644027ae45d67bf439
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:26:48 +0000

    runinto account transport flood children

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def multiply_numbers(x, y):
+        return x * y
