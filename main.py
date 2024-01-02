5eac8491a114870cc3bde73a06e1aa6d6f573312
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:51 +0000

    skin suck this stay light

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
