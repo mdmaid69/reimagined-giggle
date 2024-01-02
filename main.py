06372c7a26c82ec8523ba99b4aab7c275c338a26
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:41:29 +0000

    why between transport great cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  def get_base_name(path):
+        return os.path.basename(path)
