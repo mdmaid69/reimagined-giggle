db305d662f7fc1e8793c576635dfba4c955452ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:51:45 +0000

    disapprove recognize grateful pay copy

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-import array
-def get_array_as_float(array):
-        return float(array[0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
