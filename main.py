fb86e003880db0bb79ea729dad2eca7ecd4c9ed1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:08 +0000

    bend view noone control open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_current_working_directory():
-        return os.getcwd()
-  import os
-  def split_path(path):
-        return os.path.split(path)
+import sys
+def exit_program():
+        sys.exit()
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
