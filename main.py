dd462856e1189cfb6b9d2564762c1a447cf380a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:43:31 +0000

    treat still it interrupt tomorrow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
