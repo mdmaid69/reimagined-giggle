3710f437398ba18854d4537cfac77889340f7c53
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:53:53 +0000

    mind her save wave cry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
