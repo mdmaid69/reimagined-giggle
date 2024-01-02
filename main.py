dc759024e838b81c5e9ee613071b15c9f219ed04
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:21:20 +0000

    coffee appeal fasten turndown mark

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-def is_even(n):
-        return n % 2 == 0
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
