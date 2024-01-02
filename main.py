c76657b40180339d061c7a7281c4d3476c347c7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:03 +0000

    control battery repair pick uncertain

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
