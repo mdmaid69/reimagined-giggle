94afa9ef0bda1e715f92ffee4859463ca887ab18
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:52:59 +0000

    protect crime turnup quit difficult

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import datetime
-print(datetime.datetime.now())
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
