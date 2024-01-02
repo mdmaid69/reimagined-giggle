47d8d2cb2e30ddd356e8f251b2b54af5f70f4eee
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:31 +0000

    relative callout spread situation visit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
