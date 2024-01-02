46a93be492f13aaffa3a5f19bf50e5d1780a76ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:24:45 +0000

    smile less bit form that

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def set_array_item(array, i, item):
-        array[i] = item
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def square_number(x):
+        return x**2
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
