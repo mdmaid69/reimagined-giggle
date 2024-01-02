adef8d0b32ac53b877a6c29373a5ef4ef0d0b4dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:58:47 +0000

    read star else fork disappear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
