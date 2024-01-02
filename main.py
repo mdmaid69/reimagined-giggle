1e0797b99413320dab1e7ea817c5e8c61e1c66e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:25:02 +0000

    not cause cross side 573entertainment

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
