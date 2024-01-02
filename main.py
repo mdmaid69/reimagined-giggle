223291188df298f4213a74d5cb8f70e655fb5629
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:56 +0000

    better once zero ignore appear

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_length(array):
-        return len(array)
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
