c89dc02ed9c686372a3204be30252bd19eeb142f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:28 +0000

    kick fire takeover take view

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-def remove_duplicates(lst):
-        return list(set(lst))
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
