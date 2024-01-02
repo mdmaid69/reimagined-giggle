1ace5c3c68aa2efe018a4feadf4b16438ed8079d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:54 +0000

    cry meanwhile do them return

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
-def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
