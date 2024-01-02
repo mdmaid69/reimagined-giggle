798d240f8f2c66a605b6a907e9ddbad8abaf842e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:57 +0000

    crash day top wake cost

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
