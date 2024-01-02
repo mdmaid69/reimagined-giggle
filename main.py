f70a3e7e0013c72a1d8b88ef063c70c680e497d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:42:24 +0000

    introduction strike sport niggaacademy dance

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
