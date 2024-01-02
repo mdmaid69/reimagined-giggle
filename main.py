6d72c688156ec28602663c424c2849d552ddb2dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:21:05 +0000

    cheek cheap building lucky without

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
