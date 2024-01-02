6908d402f74c3c65a89f0e1fbe82dbef64e063f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:59 +0000

    cry ago comment soil mark

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-def calculate_area_rectangle(l, w):
-        return l * w
+import array
+def get_array_as_set(array):
+        return set(array)
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
