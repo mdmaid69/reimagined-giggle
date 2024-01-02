42ef611274b5b29cabd2226205ce61353b427d11
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:36:57 +0000

    extraordinary underneath danger listenfor exact

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
