5d21f27be4c6262a9bd592f12f2bfc499dfe586b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:54:51 +0000

    occur goodbye emotion share press

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
