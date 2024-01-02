74b560821f66b84ac418df1a4e96354b2757d3f8
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:23 +0000

    rid relative design interesting sensitive

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
