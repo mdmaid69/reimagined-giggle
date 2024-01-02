1c12eca4d6c3283e4aae893499bb6cec17b58110
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:51 +0000

    ability exit arrival lucky left

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-import sys
-def exit_program():
-        sys.exit()
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
 import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def get_array_as_repr(array):
+        return repr(array)
