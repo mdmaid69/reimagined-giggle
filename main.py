9a1412a31236b88b73177f217b7da2ad6bb579c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:46:08 +0000

    achievement attach genuine age thinkof

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+  import sys
+  def get_python_version():
+        return sys.version
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
