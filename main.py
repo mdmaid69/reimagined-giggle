f8f8672fee9a1e2b7b5d257d724ca419da9080b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:04:09 +0000

    word talkoutof Mm rob sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def square_number(x):
+        return x**2
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
