62d2d36987160209aa7309f6c45b4419c325aa3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:01:09 +0000

    basis clean exaggerate money introduction

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-  import sys
-  def get_python_version():
-        return sys.version
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+def is_palindrome(s):
+        return s == s[::-1]
