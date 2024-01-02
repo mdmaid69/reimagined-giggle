41bf99885188b5013b0a2a99c14b597cecd66f7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:46 +0000

    sea clever scale save organize

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import sys
+def exit_program():
+        sys.exit()
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
