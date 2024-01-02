4e814b180408b7e9a59ad6f5a3f733c4747e6e13
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:35 +0000

    injury seriously gradually contrast 564encourage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def add_numbers(x, y):
+        return x + y
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
