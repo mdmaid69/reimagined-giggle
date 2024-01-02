910da66c56fc18b0c85cf2857ec25c5bdb22646a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:36 +0000

    change little over wash defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
