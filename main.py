2ad3875f446420c9fc605ed753cfac05468e2173
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:38 +0000

    separate either minimum remark fresh

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import numpy as np
+print(np.array([1, 2, 3]))
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
