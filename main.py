2addccda6d4e0e1e50475a244f822fd8c757cb26
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:13 +0000

    exploration blame give week steam

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_elements(lst):
+  def count_elements(lst):
         return len(lst)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
