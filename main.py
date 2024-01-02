a15e378cedeafb08c53de0d6bc7eb41b378599ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:35:43 +0000

    keepfrom dangerous strict advanced obvious

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_density(mass, volume):
+        return mass / volume
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
