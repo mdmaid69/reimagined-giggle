5e14bc5be0333beeb95abe70054e87b53e95fa20
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:34 +0000

    fact slice male earth risk

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-  def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-import sys
-print(sys.version)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
