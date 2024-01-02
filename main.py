2c2f00f646cea375ea675bd1b14a2db878ee5217
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:33:47 +0000

    only work hair kick tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import collections
-def create_stack():
-        return collections.deque()
+def calculate_volume(length, width, height):
+        return length * width * height
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
