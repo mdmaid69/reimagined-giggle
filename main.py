f585ba839a9ee95f5fe8c053256fdbb5f9247f9a
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:44:19 +0000

    support into condition plastic time

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+print("Hello, world!")
