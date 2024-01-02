1c98aabd0ce20661dba4ae99f450fec226fbe592
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:33:31 +0000

    trouble hurt list lonely body

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import math
-def calculate_hyperbolic_arc_cosine(x):
-        return math.acosh(x)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
