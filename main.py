8c098f4c0d8833e14a17cd307114eba67f729fff
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:56:44 +0000

    unusual feature ordinary shout many

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def calculate_average(lst):
+        return sum(lst) / len(lst)
