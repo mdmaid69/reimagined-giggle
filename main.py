afbb1ac93a6ca32574a501d36938fb6b0c150e55
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:15:42 +0000

    natural sit plastic determined bear

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
