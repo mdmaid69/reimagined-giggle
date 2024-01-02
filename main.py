2bb9c359c9b2aaa1ab3d67b2ae98bf276d50f2c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:19 +0000

    late concentration in spin field

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import sys
+print(sys.version)
