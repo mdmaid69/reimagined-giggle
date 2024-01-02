b464291f33fea5137960fc8232997e9176910a18
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:36:59 +0000

    live fast turn performance touch

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
