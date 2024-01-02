9bce1130be96c2de5e74f87de2f1bae2005313bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:42:27 +0000

    believe about perform clearly obvious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+name = "Python"
+print("Hello,", name)
