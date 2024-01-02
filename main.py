c4506221d34561a75f75625582f40391e6137838
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:25:26 +0000

    win mark later pile proof

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(lst):
+        return max(lst)
