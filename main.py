063d37f70ff9dfd625baadb8c0d04057cf0278ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:02 +0000

    smell switch range crash accommodation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
