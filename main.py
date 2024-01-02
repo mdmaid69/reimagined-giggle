db1142181484ed8f3dbda922084c477f06612bfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:03:13 +0000

    exchange him calm cum some

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import getpass
+def get_username():
+        return getpass.getuser()
