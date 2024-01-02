e0267fa27b3e8766a0415d64ffbf83d4d1d8da2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:58:04 +0000

    produce tonight sail violence slight

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
 import datetime
-def get_current_date():
+def get_today_date():
         return datetime.date.today()
