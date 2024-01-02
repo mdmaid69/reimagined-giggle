a33dd8013b9c03efa08728cfffba054547d91864
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:50:44 +0000

    cost service 564encourage fork work

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
