b955bddf83e42f6540621ccce34ec7e334852195
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:19:45 +0000

    expression apart immediately even pocket

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import random
-def roll_die():
-        return random.randint(1, 6)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
