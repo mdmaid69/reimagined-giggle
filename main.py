cfc1b71e9abaaacec79e82ecdb9f401aba378c5e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:14:24 +0000

    direct turnoff worry rub personal

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def convert_to_binary(n):
-        return bin(n)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def get_time_since_epoch():
+        return time.time()
