8df2bfadc7f3474e8818824025cff71cdc9b725a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:33:20 +0000

    pleased this split fall task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
