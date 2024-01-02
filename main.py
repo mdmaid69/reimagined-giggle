acf1e7258dd263e08a8bab56cb7917bd4a19f83c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:05:18 +0000

    flight closely valuable cut type

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
