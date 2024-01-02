52c2b11fc6e9f6ce55655eaa3010319dcf0a528a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:58:53 +0000

    best dear takeback but file

--- main.py
+++ main.py
@@ -1,13 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import collections
+def create_counter():
+        return collections.Counter()
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
