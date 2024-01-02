7a9da8e6c8c77050ef6d11a9ce9c2e69fd599a20
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:46:02 +0000

    swing OKorokay section thumb steep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def calculate_area_circle(r):
+        return 3.14 * r**2
