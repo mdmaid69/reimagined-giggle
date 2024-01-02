f1e9e6cf491b48e28ffa47d94bcfd5b14705fd68
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:31 +0000

    various tense collect account wrong

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+def calculate_density(mass, volume):
+        return mass / volume
+import array
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
