2c95aebf4f579b162f113e8ca3a4b0dbdad6a353
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:19 +0000

    ticket period splash bother interest

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import itertools
-def get_combinations(iterable, r):
-        return list(itertools.combinations(iterable, r))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
