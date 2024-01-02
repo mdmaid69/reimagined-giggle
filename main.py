1ee8ca1795c5cfe9f567ce4c2fa818ee83e43232
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:54:16 +0000

    behave union goup thirsty label

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
