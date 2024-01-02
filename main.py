c83d76f900e0556b5dbf5523e33d66b636093e91
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:13:02 +0000

    characteristic steam middle frame heart

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-def find_max(numbers):
-        return max(numbers)
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
