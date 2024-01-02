f6a795665fdba7d5aedee8fee45db5c1f567ebdb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:12 +0000

    few cinema culture necessary tall

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import math
-print(math.pi)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
