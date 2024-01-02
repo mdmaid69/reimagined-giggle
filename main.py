6118fdc2deb403a3638582cf67279331cbf09e00
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:58 +0000

    thorough disagree fall tradition feel

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
