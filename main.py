6048288b4f303470c28a8846549396f397104932
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:51:33 +0000

    port also question address hope

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+def calculate_density(mass, volume):
+        return mass / volume
