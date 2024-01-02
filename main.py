28156aec695142dedf504b1874cd6f48937d993f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:09 +0000

    sit treat development uncertain bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
