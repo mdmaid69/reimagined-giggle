015f15f47b5f6b6ee2de11e441ac83d79c3b6cf5
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:30 +0000

    law gentle notice prevent then

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
