be82b52cf1318f70f94d599a2dae82dd3c12f9eb
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:34 +0000

    repeat that similar worse scheme

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import getpass
+def get_username():
+        return getpass.getuser()
