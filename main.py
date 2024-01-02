a957795b43b9086eecad41838a3898719d642c76
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:16:38 +0000

    purpose completely load face offend

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import platform
-def get_python_version():
-        return platform.python_version()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_work(force, distance):
+        return force * distance
