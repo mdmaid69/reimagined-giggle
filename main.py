52abedd44550c1915346a446f353834d058a4384
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:39:16 +0000

    chin complain question standard excuse

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import urllib.request
+def download_file(url, filename):
+        urllib.request.urlretrieve(url, filename)
