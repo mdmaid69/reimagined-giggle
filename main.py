4b46c936dcd2e57c53948480da6249a29bba56d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:15:28 +0000

    car tall nonsense open estimate

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
