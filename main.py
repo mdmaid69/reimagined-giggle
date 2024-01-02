dbe56690b5dccf589feaf10acc4dcb8617b5f5f1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:57:57 +0000

    ocean lookaround last danger estimate

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import os
+def get_file_modification_time(filename):
+        return os.path.getmtime(filename)
