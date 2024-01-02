b76b19f38a0e9dfdd66299091983ee06c29479b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:12:16 +0000

    deserve success past favourite pair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+import getpass
+def get_username():
+        return getpass.getuser()
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
