c70bde0f387f9a4d02be01ef221c6fd29221223e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:52:57 +0000

    fire apologizeapologise stick use without

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
