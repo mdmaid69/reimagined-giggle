afb6f2e4f98611e769ddca956dba9fe878d2aa1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:27:52 +0000

    spend press or holdup branch

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
