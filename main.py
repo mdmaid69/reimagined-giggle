9aef6b9647c270638fc73109d640f4bb053cc09a
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:46:16 +0000

    regional reach basis nonsense shock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
