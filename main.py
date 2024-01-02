17b6a98435679fb8025d9223c18f232d3f2d3214
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:08:01 +0000

    bringup consist defend cheque treatment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import tensorflow as tf
+print(tf.__version__)
+import collections
+def create_counter():
+        return collections.Counter()
