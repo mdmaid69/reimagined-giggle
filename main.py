81dc09b50098f4905e80ee1938a2863ca1819377
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:05:41 +0000

    praise bye protest memory sky

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import time
+def get_current_time():
+        return time.ctime()
+def calculate_density(mass, volume):
+        return mass / volume
