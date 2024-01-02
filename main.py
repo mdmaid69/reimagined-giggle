3d1c089ee088641de4364bebf76e61c8f5be2aac
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:13:34 +0000

    accidentally pay callout face habit

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_cosine(x):
-        return math.cos(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
