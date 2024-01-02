95785bcd68726ac10614a4c9885fb9b8745660a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:23:22 +0000

    history square alone within although

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
 import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def calculate_logarithm_base_e(x):
+        return math.log(x)
