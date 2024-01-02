db7cf192f6a9a889f372b74ddcb40c64b3c70d36
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:11:20 +0000

    tidy tomorrow damage quarter set

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+def calculate_speed(distance, time):
+        return distance / time
