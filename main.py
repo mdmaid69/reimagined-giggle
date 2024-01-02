5315a06fa77a8848caeb28c8959e348dcf0f7729
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:10:18 +0000

    mass bad wild door even

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_as_list(array):
-        return list(array)
+import math
+def calculate_tangent(x):
+        return math.tan(x)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
