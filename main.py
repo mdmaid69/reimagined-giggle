2d7395ed155ab052707c42e389647535c0fc09d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:14:14 +0000

    disturb fold zero programme up

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import math
+def calculate_remainder(x, y):
+        return math.remainder(x, y)
+import array
+def get_array_as_repr(array):
+        return repr(array)
