c403e884f3506c6f49cb228753751b889b67aaf3
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:08 +0000

    either anything home prepare treat

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_gen(file_name):
         return os.stat(file_name).st_gen
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
