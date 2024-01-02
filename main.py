bae6e96767af6a8e2bee2d426942d12c3c8c921b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:15:06 +0000

    put slow national bank hurt

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
