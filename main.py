8750e0ef979437f2bae18f7445029ea9d4a59d7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:25:43 +0000

    once bring comefrom switch custom

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_list_from_array(array):
+        return array.tolist()
