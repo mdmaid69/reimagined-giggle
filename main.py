45ef06d121797c66efb6609001d7c36a8f833df4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:40:41 +0000

    lookafter bang receive talk runout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import os
+def remove_directory(path):
+        os.rmdir(path)
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
