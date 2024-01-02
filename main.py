98516a6e9cbbe1c669c1ac0f814cc40abc6cb05c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:42:22 +0000

    ashamed Ee pronunciation offer visit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def get_array_item(array, i):
-        return array[i]
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
