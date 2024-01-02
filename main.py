2eeef93ecedb99d7d83621ca00f6ccad444fe6e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:21 +0000

    entrance last tidy pass attention

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+print("Hello, world!")
