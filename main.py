c73e4be0c06405943340d8699bc0a2f1bf3a4b1d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:02:28 +0000

    sure tap light offer minimum

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def set_array_item(array, i, item):
+        array[i] = item
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
