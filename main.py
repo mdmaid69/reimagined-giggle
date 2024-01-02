c1d2f75544be2ec488225091d6f8852f2fc56032
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:23 +0000

    shopping ought decide callat continue

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
