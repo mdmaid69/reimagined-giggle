24586651a32dd32f373ac19e96c97d69c70b8349
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:11:15 +0000

    behind welcome wheel effort sky

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_current_working_directory():
-        return os.getcwd()
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
