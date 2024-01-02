d5183ccbe104ef2d34dfa14cf725a046a839bf22
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:16:29 +0000

    blind still method clear scheme

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
