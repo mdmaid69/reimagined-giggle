844103c99508341388c89b0aeca44a641c543f34
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:10 +0000

    education force service begin miss

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Common elements:", set(list1) & set(list2))
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def calculate_power(work, time):
+        return work / time
