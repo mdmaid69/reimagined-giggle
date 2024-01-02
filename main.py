25cb59a3c4c0790ecb3b2dde997995e0539cf25c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:29 +0000

    split about trust unlikely idea

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+import math
+def calculate_arc_cosine(x):
+        return math.acos(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
