ef127c06a1ea8fd47c331394b06b103fad3a91e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:23:13 +0000

    path pressure slice design health

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import os
+def get_current_working_directory():
+        return os.getcwd()
