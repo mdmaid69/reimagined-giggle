e47205cb236bae1584f446a696d944b6c6ca2318
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:47:49 +0000

    train damage backwards stir behind

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-import os
-print(os.getcwd())
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def cube_number(x):
+        return x**3
