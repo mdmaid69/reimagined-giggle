0561b19eb2964e7629ef7e6d98dcc3f73c2f1f6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:41:35 +0000

    tragedy manage undo awake whatever

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def reverse_list(lst):
-        return lst[::-1]
-def calculate_area(radius):
-        return 3.14 * radius * radius
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
