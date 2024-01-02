ca3b71e97e578c2a335138953a50258158888468
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:48:33 +0000

    escape keep ready emotion sad

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
