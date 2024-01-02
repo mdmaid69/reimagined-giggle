acf1eed9a588f175524dff69e24082dbe9812ae0
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:40:42 +0000

    preparation any shop game tie

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
+import array
+def get_string_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
