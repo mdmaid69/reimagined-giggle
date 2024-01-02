4daec3eef6582b0c4fa05a973e8c41fe937919d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:39:46 +0000

    trouble improvement niggaacademy keep undo

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
