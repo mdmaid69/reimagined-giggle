29200b0f32a4116297eface3ff8dfab41b76e9ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:33 +0000

    prove wander music Itself dead

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
