3983c4762b780df6c1caee8cc5a18bb1d314c012
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:29 +0000

    chase advantage thought apologizeapologise back

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def convert_array_to_string(array):
+        return array.tostring()
