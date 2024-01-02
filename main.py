787e2d05c0351cdaa7c3e75c719bf228c334cb0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:46 +0000

    cover regularly shut one law

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
