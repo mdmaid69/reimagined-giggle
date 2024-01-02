15b175a3b64788a5005d6b003f02a7c4973ded82
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:43:11 +0000

    equal full miss use at

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
   import os
   def get_file_blocks(file_name):
         return os.stat(file_name).st_blocks
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
