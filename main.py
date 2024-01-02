1db1029bf27face00ffac24e0449a9a6c8a0a8a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:35:36 +0000

    where if attempt race insist

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-sentence = "Hello, world!"
-print("Unique words:", len(set(sentence.split())))
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
