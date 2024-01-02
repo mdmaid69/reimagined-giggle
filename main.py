0082c7b54a75c819f9d175548aeb753a0f8f34be
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:18:16 +0000

    offend Ww glass pronounce power

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
