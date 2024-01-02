00bb0ae7534a1748611498c72f9282d2e1fa4175
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:19 +0000

    account whoever remove touch worst

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
