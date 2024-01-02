076db55a467c863ca9d3ad0c0ff95cdf12cceb35
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:16 +0000

    mainly argument combine fire love

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+i = 0
+while i < 5:
+        print(i)
+        i += 1
