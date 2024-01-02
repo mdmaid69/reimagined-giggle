d805358d8d131262e8f35a8141db5521bdb32191
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:19:13 +0000

    when successful broken sensitive race

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def get_array_as_int(array):
+        return int(array[0])
