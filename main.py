7910dc616fb1e298323db469c0a3087ef6e90aae
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:33:43 +0000

    breathe unit still promise over

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_current_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
