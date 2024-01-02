98a13b6f2c8ae091c8a078066c6d177e22686962
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:16:41 +0000

    accordingto rough worth provide importance

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def reverse_array(array):
-        array.reverse()
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
