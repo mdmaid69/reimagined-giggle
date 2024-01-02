1596d2dd98f286d82185e4d3884b96b522f8fa58
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:37:16 +0000

    reasonable do otherwise rich lie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
