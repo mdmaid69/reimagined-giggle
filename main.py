611c1a6ee721da007cb378c7a27f112a9366d61e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:17 +0000

    hurt shout plenty because protection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
