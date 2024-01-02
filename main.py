c854480a4d4b95643a1ae9e59995fb5a5fd0e495
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:03 +0000

    sometimes money world advantage frighten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
