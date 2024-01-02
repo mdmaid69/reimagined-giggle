eb35d0f3b4e15621393387326e97a08d1f72bf80
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:41:17 +0000

    serve rubbish combine means exit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import collections
-def create_user_string():
-        return collections.UserString()
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def calculate_density(mass, volume):
+        return mass / volume
