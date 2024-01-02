013751d5c9151a4f32e852da63812e532780f394
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:44:03 +0000

    will no list mistake trip

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
