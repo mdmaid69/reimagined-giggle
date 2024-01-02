1514eb94296d2cd071d4ceb32f4101c731b60168
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:33:13 +0000

    later environment shake file path

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+import math
+def calculate_cosine(x):
+        return math.cos(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
