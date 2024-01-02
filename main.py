ece19e01aa40d2e660ad1428e7a2debabd14ef5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:39:29 +0000

    concentrate suit worried treat sad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
