b1e78254ab1065c4d4dc2ec93d2604af13491adf
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:11 +0000

    experiment aim display maybe call

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_work(force, distance):
+        return force * distance
