4033b8b0d0cf653e9bc7cbb24dbb903fb9e92922
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:02 +0000

    traffic constantly Qq bread opposite

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
