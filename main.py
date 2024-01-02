ed7e26c1115ee1731001c621f14ef994801dedfd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:55:18 +0000

    heat translate imagination medium century

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
