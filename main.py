6e935170fed6f91d482803f36d5371c8ce3807cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:10:11 +0000

    sensible lean flight estimate connect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
