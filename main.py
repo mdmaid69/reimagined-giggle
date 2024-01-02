9d2d21b72d19f335f1134cb4025b3353b1995381
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:29 +0000

    issue silent home finish free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
