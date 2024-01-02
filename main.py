896ddf253ea5d42e4c272b0d24c5ea08e8a81b3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:13:24 +0000

    window meanwhile share unit mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import time
+def get_current_time():
+        return time.time()
