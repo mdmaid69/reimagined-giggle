97cc3aafcdcc86d01dbc188a143b4a2600802eda
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:19:47 +0000

    improve influence reasonable tradition clean

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+import math
+def calculate_logarithm_base_2(x):
+        return math.log2(x)
