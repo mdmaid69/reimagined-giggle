2fda94ff38067556b686747db7e326906be96a1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:40 +0000

    increase learn false pay talk

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
