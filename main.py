e8b305ba6598adfa5b00ff796b3e8708b2d90767
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:03:17 +0000

    confuse comment fall charge step

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
 import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
