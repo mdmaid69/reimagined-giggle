bb6167f56345e9c5ce7e3362ac4f25deffb1895b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:41:08 +0000

    mystery example position aware average

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def multiply_numbers(x, y):
-        return x * y
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
