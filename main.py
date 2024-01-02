15c455c883e29ef46319576a44487c1714874a03
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:29:17 +0000

    strike tool unlikely home chat

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
