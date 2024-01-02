f0aeb600e8ff0c4b8190b80b0aefe588bc334ffb
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:16:52 +0000

    land bang bye whom shout

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import math
+def calculate_floor(x):
+        return math.floor(x)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
