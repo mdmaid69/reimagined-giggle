737eda3528f942625c5b0c7c9de0c6d1373ef04b
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:40 +0000

    record field unfortunate about float

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+def get_time_since_epoch():
+        return time.time()
