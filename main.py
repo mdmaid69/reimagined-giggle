303be8c92827a201a338e937085731989d541288
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:30 +0000

    unfortunate tight exit your only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
