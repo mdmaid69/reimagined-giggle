455658435ad57aef23296a65fd9a851a41345427
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:59 +0000

    refuse field spare protest night

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-def calculate_acceleration(speed, time):
-        return speed / time
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
