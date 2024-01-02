60b350a6bdd5f848edad84c2cb35ac11ceee12b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:50:54 +0000

    meal cum criticism shape frequent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
