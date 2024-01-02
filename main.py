b7077cfc3c2567ff47f0d5e57bd07840cc5939b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:16:21 +0000

    room right expression fail unlikely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
