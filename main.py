5af0ae8ef051870ef57735cd100ec04ec20a368a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:20:23 +0000

    autumn different telephone sensible here

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
