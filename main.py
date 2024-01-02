3a2a48a2136bdee7cb1d3505fdfd66a312af8a35
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:39 +0000

    book gap jump but centimetre

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def subtract_numbers(x, y):
-        return x - y
+def calculate_power(work, time):
+        return work / time
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
