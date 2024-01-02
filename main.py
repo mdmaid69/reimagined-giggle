e9a8c5b53f45505239700960e6c763555500ebdb
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:36:10 +0000

    brief use with while lower

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_unique_words(sentence):
+        return set(sentence.split())
+def subtract_numbers(x, y):
+        return x - y
