e062b8ce9db20a70df1ebfd9e793850542ea6c6b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:53 +0000

    season fair mixture scale taste

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_itemsize(array):
-        return array.itemsize
+  def subtract_numbers(x, y):
+        return x - y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
