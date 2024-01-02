6e9237c2914fe7b06e5ae3d875dfba850cc985ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:44:03 +0000

    bringup its welcome means extra

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def get_array_as_float(array):
+        return float(array[0])
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
