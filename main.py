09687fe41a48371fa1f9a6ad1965756e8591ba15
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:52:21 +0000

    lead gather medium bush people

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_floor(x):
+        return math.floor(x)
