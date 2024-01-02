5dce13b67290ab370954de90f2a38fddc9c75886
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:54:10 +0000

    surface information by failure contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  def divide_numbers(x, y):
+        return x / y if y != 0 else "Cannot divide by zero"
