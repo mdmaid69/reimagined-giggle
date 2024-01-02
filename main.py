30ca1695f68834f37e7f0c775f4382c4817511c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:04:55 +0000

    school upstairs grade regard tax

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+def calculate_average(lst):
+        return sum(lst) / len(lst)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
