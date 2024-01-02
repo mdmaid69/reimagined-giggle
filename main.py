21fcb621b9e972595243e44c92d45bd0727a4d36
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:18:05 +0000

    strict round ground sleep bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
