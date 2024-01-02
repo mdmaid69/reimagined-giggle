613d40a010ca7dc6a54bd88416c3b835920a52e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:26 +0000

    partner possible thankyou oil justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
-for i in range(5):
-        print(i)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
