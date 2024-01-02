8e0a51bf5a4cf7b0cb4ef7fcbeba6b113e6390ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:23:11 +0000

    return trip flight effect we

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+def find_unique_words(sentence):
+        return set(sentence.split())
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
