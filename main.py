e91caef44fca3a817b550a92090849d7a86cb0bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:10 +0000

    look joke spring raise precise

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+import tensorflow as tf
+print(tf.__version__)
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
