c1d2d7f84b014c39213c3219ddef21f60b51d274
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:35:47 +0000

    complicated post peaceful anywayanyhow amount

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
