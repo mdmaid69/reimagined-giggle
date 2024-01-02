6fc75b2ef2e805c821209e8030d9d434f8855701
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:58:00 +0000

    arrange perhaps event press to

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
