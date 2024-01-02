7558802d0186e8a330c5e44034c5ba53f71e7927
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:02 +0000

    with birth lesson marry nothing

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
