dc5aae61ad8d154f3bf5982f64b8eab1270866df
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:53:44 +0000

    this spin common other deal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_float(array):
+        return float(array[0])
