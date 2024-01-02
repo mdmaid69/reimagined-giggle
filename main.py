e3416ec3d59fe29ef56be50824cfd7465ba8fa70
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:32 +0000

    power back goup confident stamp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def convert_to_octal(n):
-        return oct(n)
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
