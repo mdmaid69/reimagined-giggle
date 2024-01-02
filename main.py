0c0b080893f60c04dcf980f63792da69ba30873b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:01:25 +0000

    minor happen demonstrate turnup modern

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sine(x):
-        return math.sin(x)
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
