d3c8e939496ebd5026bccf202753efce777efc2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:16:26 +0000

    callout frame arrange special until

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+import array
+def append_to_array(array, item):
+        array.append(item)
