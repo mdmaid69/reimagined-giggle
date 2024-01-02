dcc7ad7287216bd2b29e7999f7a256b39685156f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:40 +0000

    exhibition book scene pronunciation imagine

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
+import sys
+print(sys.version)
