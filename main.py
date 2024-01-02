bc601a0108a84f77a0eda57703c1a6b3c1b6d108
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:57:35 +0000

    do buy open learn front

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
-print(sum(range(10)))
-def convert_to_hex(n):
-        return hex(n)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
