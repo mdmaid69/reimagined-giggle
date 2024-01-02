b5c2eaec29f086170486ba37c6f948a2c22b69bb
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:34:43 +0000

    married even both understand pity

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import os
+print(os.getcwd())
