f67ad688b50c07267609ca9bf7193e65f3d500b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:42:39 +0000

    combine deep at save cold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
 import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+def calculate_sign(x):
+        return math.copysign(1, x)
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
