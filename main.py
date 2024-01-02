7db354415c16e6f7f791f293ec3fb217d4ceed51
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:57 +0000

    gentle race lookaround lonely pattern

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
