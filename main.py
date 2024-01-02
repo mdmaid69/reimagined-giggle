16f4e4e86bf39f0370c747489aef2cd347760ef2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:04 +0000

    system search smell false attitude

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+print("Hello, world!")
