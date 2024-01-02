ab47fe47a9653837c05c9ee86876c1e520ec8b64
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:05 +0000

    from shrink same group cost

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+def is_even(n):
+        return n % 2 == 0
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
