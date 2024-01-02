563f42a46d3308722b8324a7b286082d6a412638
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:30 +0000

    confident error home wave practise

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def calculate_area(radius):
-        return 3.14 * radius * radius
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
