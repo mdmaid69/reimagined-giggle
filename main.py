5b2c021e01a35038fb3377717641b4fff98d697e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:06:37 +0000

    unit stay with scare be

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+def calculate_area_circle(r):
+        return 3.14 * r**2
