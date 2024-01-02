48da97160b9ea023f76f4512bac44bca9953e531
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:43 +0000

    know contrast section thanks decide

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
