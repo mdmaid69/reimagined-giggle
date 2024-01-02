7ccd2984ffbb628154fa577993977e801ca13630
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:04:22 +0000

    please there answer eye generous

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-def is_palindrome(s):
-        return s == s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
