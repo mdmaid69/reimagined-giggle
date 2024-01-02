fe5a0aec9d84784a3ebe15cddc430d894ff52f6d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:17 +0000

    pattern test bill expression decision

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+import platform
+def get_python_version():
+        return platform.python_version()
