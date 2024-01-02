88dcd501148221de14c688dfd138adcea1d2ff93
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:24 +0000

    next spill wake no goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
