f24b0f8aedf14b1987a1edff9eb52d2690d80953
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:00:34 +0000

    sleep dance strike occasion invitation

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def convert_to_binary(n):
+        return bin(n)
