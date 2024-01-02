1d8c3171ca0e452f3bfa65b740aa83629ca9eaae
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:09:52 +0000

    influence be computer beginning stretch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
