8d0cd589b03f32690ab3c6f1783cd5ed7bfc1013
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:25 +0000

    win weather software even suppose

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import time
-print(time.time())
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
