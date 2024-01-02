b4429d421783de620a538cc9761385c6432eb04e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:48 +0000

    closed avoid sort fetch coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def convert_to_octal(n):
-        return oct(n)
+def calculate_power(work, time):
+        return work / time
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_working_directory():
+        return os.getcwd()
