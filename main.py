81c10a9123b712a80258aa7e5da8d890529d3c71
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:12 +0000

    fresh declaration rob arrangement thought

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import array
+def get_string_from_array(array):
+        return array.tobytes()
+import sys
+def print_python_version():
+        print(sys.version)
