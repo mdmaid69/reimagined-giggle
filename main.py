1e6c19fee29220d8ac7bb386164a4db87489cd8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:24:31 +0000

    remain dig call maintain order

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_item(array, i):
+        return array[i]
