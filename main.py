ce5e75dda75b8331150ade7c4ea65696909bc7f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:41:57 +0000

    secret benefit noisy interfere children

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
