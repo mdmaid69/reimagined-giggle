58ba9401a33297307d71d788b9924f1aca499dbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:44 +0000

    increase develop finally do correct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_typecode(array):
-        return array.typecode
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
