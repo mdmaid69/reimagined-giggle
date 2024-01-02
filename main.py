e1a83ece166f64be579895bed17b172e1a7fc9cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:40:18 +0000

    basically surface method breakinto this

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  def count_elements(lst):
+        return len(lst)
