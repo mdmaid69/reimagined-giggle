761940ea88c4b0ac1e6679e477c4a5001a040371
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:03:14 +0000

    respect 568energy choice herself height

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_tangent(x):
-        return math.tan(x)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
