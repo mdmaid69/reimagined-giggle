b0c2ec03fcc86df97e90f37cd988a6259bbd7e1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:48:20 +0000

    everywhere brush hair wait event

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
