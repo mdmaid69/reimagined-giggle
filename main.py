4ecb28fc357b0bc6008f7f5d922a13076340f5b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:37 +0000

    dry operate container wherever fortunately

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import tensorflow as tf
+print(tf.__version__)
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
