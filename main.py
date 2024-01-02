eb6efca721569af8ec4442365f6305de5d85003b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:04 +0000

    tune general purpose contact tear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-print("Hello, world!")
+from collections import Counter
+print(Counter("hello world"))
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
