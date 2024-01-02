02f963bcd8ec85a94934bf37e7fd4406de0ea616
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:24:27 +0000

    study sad school aware affect

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_density(mass, volume):
+        return mass / volume
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
