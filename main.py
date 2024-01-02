484341a3bf20d264ae9d26e01858bf80984367ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:51:06 +0000

    drink substance light weather over

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+x = 10
+y = 20
+print("Sum:", x + y)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
