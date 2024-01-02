7ccd4a86f62fd5a7e74c55ec91d89182264817d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:00 +0000

    minute almost holdout flood storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
