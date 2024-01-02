8869fb11a01f010f48fce43d32336a3916ca3ed1
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:29 +0000

    tap middle still base offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  def calculate_area_circle(r):
+        return 3.14 * r**2
