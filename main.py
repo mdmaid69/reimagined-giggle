7400ef92f8b126831795768086d15431103690b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:27 +0000

    settle deliberate strike objection rescue

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print("Hello, world!")
-import array
-def get_array_item(array, i):
-        return array[i]
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
