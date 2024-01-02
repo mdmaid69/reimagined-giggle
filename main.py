8eac5b3da255da011bf998f0269b777ccdffe736
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:19:36 +0000

    butter influence bury before change

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  def convert_to_binary(n):
+        return bin(n)
