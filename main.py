a9f4ccd57015689bd18070a80fe8ab041518e512
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:38:09 +0000

    inquire mixture conduct private both

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tensorflow as tf
-print(tf.__version__)
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
