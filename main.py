8f5bff3594b9a62e38c71a12861e6eec533e3ebe
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:29:11 +0000

    tap hear kind spring see

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
 import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def merge_sorted_iterables(*iterables):
+        return heapq.merge(*iterables)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
