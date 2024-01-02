08d1ea15a0b70ad95fbf7b2ef4765f45b91f4131
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:09:55 +0000

    classroom opposite used appear frequent

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
