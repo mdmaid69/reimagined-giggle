ce2db7c436f8b2ce105f74880a27a22d2c1bc602
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:42 +0000

    deal owe flat fair taste

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-def find_min(numbers):
-        return min(numbers)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
