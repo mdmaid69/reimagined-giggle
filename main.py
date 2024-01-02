7f84a31d5328483b06e193740cd4afe859f6ab1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:24:48 +0000

    dick seriously quiet slice above

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_power(work, time):
-        return work / time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def convert_to_binary(n):
+        return bin(n)
