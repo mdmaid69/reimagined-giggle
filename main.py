34bf4e8ed80c0888e90863b5d89cf032fd4c4b70
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:05 +0000

    television yesterday speed thanks unfair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
