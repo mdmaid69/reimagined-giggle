8e7092008c994fdf0484e99e6e9dc958841d7624
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:04 +0000

    lean lesson behaviour light spread

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
