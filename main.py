b8900add251a648237dd2e9bb126714eb386f5dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:48:37 +0000

    emphasize hold report section coat

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
