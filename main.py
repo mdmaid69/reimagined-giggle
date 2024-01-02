f4232c90b61f176d4cb919c31c13eef860e24370
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:18:17 +0000

    post shock high guilty discussion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
   import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_speed(distance, time):
+        return distance / time
