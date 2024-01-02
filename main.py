a0667459090fce59b68789918a6e830263c55ac8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:49:27 +0000

    share pull loan little scheme

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_list_from_array(array):
+        return array.tolist()
+  def sort_list(lst):
+        return sorted(lst)
