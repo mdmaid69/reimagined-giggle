cfb3c88fc402d4bcb6056052afde6217964cc2bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:31:02 +0000

    forgive dance attempt noone about

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-  def subtract_numbers(x, y):
-        return x - y
+  def count_elements(lst):
+        return len(lst)
+i = 0
+while i < 5:
+        print(i)
+        i += 1
