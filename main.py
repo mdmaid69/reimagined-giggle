1aa583652c08374a706d296533581f9010aa0194
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:57:04 +0000

    tune say obtain normal fast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import sys
+  def get_python_version():
+        return sys.version
