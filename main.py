6b09633039cbc10dd7b8f8434ddb96b68bc2a51d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:47:59 +0000

    disappointed woman with Uu declaration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
