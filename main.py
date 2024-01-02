a17310b8f3b62fcf1801e2d8610ffd8642120c19
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:04:13 +0000

    drawing tape thought impatient ball

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-import logging
-def log_message(message):
-        logging.info(message)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
