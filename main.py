5ac37a7c9e5766029d048d640d829773eb4f74e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:34:51 +0000

    roof above touch occur ought

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
