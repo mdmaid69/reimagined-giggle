0e159b6872409d6e0fbe431c50637fd2866a0ce5
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:22:24 +0000

    my train yes low money

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-def calculate_speed(distance, time):
-        return distance / time
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
