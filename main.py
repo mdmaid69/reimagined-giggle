e0a7e87056b2636b49c40a840177823f1b8e9353
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:34:52 +0000

    since giveback friendly quiet worried

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+import array
+def get_array_as_bool(array):
+        return bool(array)
 import pandas as pd
 print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
