f59f471c364d00cd6d8257710bc659a4ed357093
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:05:16 +0000

    pipe hot mark hide welcome

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def sort_list(lst):
-        return sorted(lst)
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
