1541d5c648670e2668f7a6f45ee6436824aa9a19
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:06:59 +0000

    clue practice occur would blow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_block_size(file_name):
+  def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+import array
+def get_array_as_bool(array):
+        return bool(array)
