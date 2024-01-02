316bcfd6ea3b51512ae2671d909e74a502f903b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:18:16 +0000

    clean injury eye interview earn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
 import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-import time
-def get_current_time():
-        return time.ctime()
+def remove_from_array(array, item):
+        array.remove(item)
