fd960206fe722789c1e2acf451d39c38cfe5e092
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:57:22 +0000

    part kind commit centimetre oil

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def count_elements(lst):
-        return len(lst)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
