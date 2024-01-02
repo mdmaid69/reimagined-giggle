e0049270939664d3b7bf5483482d8212790edb07
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:51:43 +0000

    keepfrom keepout habit shade concentration

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
