7b19dd57e370a0e93b791346d3d5fc824fa6aa90
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:17 +0000

    interfere provide quantity their vote

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def convert_to_hex(n):
+        return hex(n)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
