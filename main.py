eb08232c21da86031e0fe8310155bb7ce2ddd714
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:06:27 +0000

    obtain fast shake minimum hole

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-def square_number(x):
-        return x**2
+def calculate_speed(distance, time):
+        return distance / time
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
