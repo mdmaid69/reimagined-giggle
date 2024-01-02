7177cdeb30cfa589b8b6c564e2c72c6800832307
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:50:29 +0000

    cause Ll accident almost disturbing

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-  import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
