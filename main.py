cd208b92f31e029d0282031446b9483489df170f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:48:33 +0000

    for matter team deep speak

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+def calculate_density(mass, volume):
+        return mass / volume
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
