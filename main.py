74801e149995551b597fdf003d593dbce8030ad1
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:33:23 +0000

    state loan clever sweep display

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
 import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+def exit_program():
+        sys.exit()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
