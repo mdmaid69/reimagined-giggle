23a81d651e33869ac64ab3ada695b05a8a29027a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:02 +0000

    expression final could natural emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import numpy as np
+print(np.array([1, 2, 3]))
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
