49b8153ff05588f62940e2ef342f050dbfe26c1f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:47:48 +0000

    holiday burst guard fasten where

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_list_from_array(array):
+        return array.tolist()
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
