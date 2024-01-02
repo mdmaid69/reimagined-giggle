d7584a52db0cba9c64e442f487f5f740761f2699
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:22 +0000

    please Ww communicate meal order

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
