6b3fe3203cc56ce3a8024dcf6d1bb8ccb6fa5a87
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:47:52 +0000

    show there sort foreign fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def cube_number(x):
-        return x**3
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
