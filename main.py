5312332576c4991e3c3f8212027510f42bf64ad2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:43:58 +0000

    regard his getout taste police

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import array
-def get_array_as_str(array):
-        return str(array)
+def convert_to_octal(n):
+        return oct(n)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
