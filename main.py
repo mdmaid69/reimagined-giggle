25444746aac25db4b08f69c7a4a2d435a8b8deea
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:06:43 +0000

    leave compare accept excited owe

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+def calculate_density(mass, volume):
+        return mass / volume
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
