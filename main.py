d335fcff334d758b24dc9fd3b2e3f9be33dc504e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:52:59 +0000

    earn contents anybody like task

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def add_numbers(x, y):
+        return x + y
