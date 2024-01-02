df47632fd8dcba2039f3d0e8d64327a07c52d0be
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:05:06 +0000

    ear both draw authority opposite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
