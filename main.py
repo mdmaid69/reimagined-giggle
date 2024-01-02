3433d622bbff3dad8f1488902c2981ad8aa712cf
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:00:51 +0000

    comeback nose tooth disappointed miss

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def convert_to_hex(n):
-        return hex(n)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
