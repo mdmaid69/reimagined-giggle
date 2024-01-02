a644e4e8ca555aadea6b4da4fc1d591e5d018410
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:48 +0000

    my gradually advanced minor operation

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+  def is_even(n):
+        return n % 2 == 0
+import json
+print(json.dumps({"name": "John", "age": 30}))
