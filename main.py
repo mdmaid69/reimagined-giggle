470e630509826ea3e249ba5ee865702c087dba14
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:32:15 +0000

    everywhere occur welcome put makeinto

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
