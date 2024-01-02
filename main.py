4d302f2cc22230ffdaf17612d2a2bcbce5efbd5c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:42:37 +0000

    section furniture illness rush regional

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
