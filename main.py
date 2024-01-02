924e172507ca3bb4624337b434470b59b962ec23
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:24:06 +0000

    now part worse exchange quarter

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
