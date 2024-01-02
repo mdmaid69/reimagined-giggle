87c5f41ff73e339cf3699cd1555a6530959edeec
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:55:04 +0000

    pleased interfere getout turn report

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+def calculate_return_on_assets(net_income, total_assets):
+        return net_income / total_assets
