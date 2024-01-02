86e6ed2bc26147d841b2af111b20495d5ddf23b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:50:25 +0000

    mix Yy able dick weigh

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-for i in range(10): print(i)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
+import datetime
+print(datetime.datetime.now())
