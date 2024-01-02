2676896e803242a9a1e4a140be8839f3c9b612c1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:12:40 +0000

    ticket exhibition design treat star

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-def calculate_work(force, distance):
-        return force * distance
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
