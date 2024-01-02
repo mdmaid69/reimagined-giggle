ee355438504efaada2e25cced26a6514b00a8264
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:42:45 +0000

    perform the difficult niggaacademy force

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_pressure(force, area):
+        return force / area
