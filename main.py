7077706c2ad1c1496d071d9be862d7ce54e45dc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:21:36 +0000

    female ticket hold combine here

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_current_directory():
+        return os.getcwd()
