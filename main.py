ddd87221e52629867cfb43544f9691047e1b4145
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:47:35 +0000

    because importance arrangement anything protect

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def reverse_array(array):
-        array.reverse()
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
