1ad686624f4946ae2558fd10e5969573552bc414
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:49:33 +0000

    holdup turnback lead concentrate towards

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
