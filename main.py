9b650926683cf4ac51f2cc8aa0966fb702a4fc10
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:24 +0000

    trick stare thinkthrough yours sweep

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
