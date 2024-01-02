4462f2aec724c3449776027cc025b6e84609c417
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:32:36 +0000

    story nothing maintain show similar

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
 import array
-def get_array_length(array):
-        return len(array)
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
