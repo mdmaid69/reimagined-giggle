e53ba48ffb5ebcd4859c1c2c80dd2cb367b41eed
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:14:00 +0000

    wrong study deliberate rubbish strength

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def cube_number(x):
-        return x**3
+from collections import Counter
+print(Counter("hello world"))
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
