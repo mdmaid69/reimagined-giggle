867ed0bb18889a93b3783cc2f2b036a680686cc9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:31:43 +0000

    dead dance visit day coffee

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-import collections
-def create_queue():
-        return collections.deque()
+def find_max(numbers):
+        return max(numbers)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
