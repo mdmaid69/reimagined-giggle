4b1db10c743c77de6f16399c564e4eea76a00e23
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:03:02 +0000

    equal sleep reason zero cry

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-for i in range(5):
-        print(i)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
