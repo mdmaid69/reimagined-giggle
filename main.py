8dee185c62bc7aa8ea1d110a6cd85ab995c28e42
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:12:29 +0000

    turninto late please stamp song

--- main.py
+++ main.py
@@ -1,12 +1,4 @@
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+def calculate_area_rectangle(l, w):
+        return l * w
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
