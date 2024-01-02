595fef364b02342091fe622722cf06a88c2fd827
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:27 +0000

    couple slight tie total replace

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
 import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
+import array
+def get_array_as_float(array):
+        return float(array[0])
