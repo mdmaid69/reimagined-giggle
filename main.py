6201b81e6dfa2f0b6137385c9f369d9198cbe4e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:33:26 +0000

    sure convince avoid firm Ff

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
-def calculate_work(force, distance):
-        return force * distance
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
