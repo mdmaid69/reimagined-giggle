8d55e929a8d07f1bc490a1e81444c5c52fa05eb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:33:15 +0000

    my satisfactory stone undo line

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def find_min(numbers):
+        return min(numbers)
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
