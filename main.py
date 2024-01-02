58b04a84ae960476a403eb8f237c832432991b00
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:58 +0000

    trick path combination reach storm

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+def calculate_speed(distance, time):
+        return distance / time
