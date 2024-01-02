2d028ce812ba13bace8a58f42be571ac1c2927f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:16:13 +0000

    term trip cough part Ff

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
