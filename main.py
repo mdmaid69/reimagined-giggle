745ba4ae722cd6173344cf469589118f3a8a4044
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:50:20 +0000

    blow runinto bar summer free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
