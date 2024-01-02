d7e36dde06badecf19a4f506ca77a64fc80ecf6f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:58:59 +0000

    class notice them young comeup

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
