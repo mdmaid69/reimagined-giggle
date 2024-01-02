e65677ef4a5d302904bc17874b899ad59ea38014
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:05 +0000

    splash goout yet moon hang

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
