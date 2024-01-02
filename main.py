03fcbcdda5e7bebfcb683d3ee11a5bbc868c33b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:25:53 +0000

    survive right kind contrast mind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
