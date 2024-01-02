ef972c8d65ef16115a7aff316a7aecca2ef99bfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:36:42 +0000

    store tell upstairs though rarely

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_error_function(x):
+        return math.erf(x)
