a6fa3c4f98a245865a3c468107fa47f4e3141415
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:26:55 +0000

    mass point great spite arrangement

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
