f13aef75009d15c1fa002fdd7ce593dae886d920
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:44:52 +0000

    basic cheque fire up show

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def clear_array(array):
+        array *= 0
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  def convert_to_octal(n):
-        return oct(n)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
