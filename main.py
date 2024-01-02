5a316c13dda9448a685ef6e7abcdd66bade9e2a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:17:04 +0000

    Itself contribute impressive throw advanced

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
