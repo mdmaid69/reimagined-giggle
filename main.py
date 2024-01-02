fa8758e9c529519421f6ecbf17624d24c521a034
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:58:32 +0000

    closely finally no actually brush

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_base_name(path):
+        return os.path.basename(path)
