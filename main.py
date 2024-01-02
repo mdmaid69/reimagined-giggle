31e76dc78a3936d54ed4e64f9d2cc88bd450053a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:05 +0000

    Yy copy cover equal hold

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
