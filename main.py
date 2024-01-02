5e1bfc03f8561e4b0c879b395370cababa04adb5
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:38 +0000

    meanwhile tune finger position brain

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print(sum(range(10)))
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
