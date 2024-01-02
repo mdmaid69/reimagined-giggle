3ea3836807c997c30fda101ed0f4f0f7ef64b286
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:44 +0000

    friendly emphasis Bb nigga shall

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+name = "Python"
+print("Hello,", name)
