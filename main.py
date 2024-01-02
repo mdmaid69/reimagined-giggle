6006a6f0a215b13a934ee6a222d2d9c6ffed80d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:48 +0000

    instant unkind sure message wish

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-  def multiply_numbers(x, y):
-        return x * y
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
