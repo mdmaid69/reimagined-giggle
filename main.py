b9a5e64fa04fa5055298b0d61c25fa012f8ef0d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:53:50 +0000

    let mark risk get across

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
 import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
