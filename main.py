d6a0eaa70d5a71a7301f7a51b99d566cd2486a47
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:09 +0000

    passenger level from rule demonstrate

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
-def remove_duplicates(lst):
-        return list(set(lst))
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
