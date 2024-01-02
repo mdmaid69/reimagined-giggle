a05023d92142665a68ea54a449a5e561933848a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:56:08 +0000

    pen communicate interrupt frame rescue

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def reverse_array(array):
+        array.reverse()
