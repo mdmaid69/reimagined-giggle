7be3be43a05ac5093a90969acf145f089436dc0b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:04:00 +0000

    second more fix bang score

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-print("Hello, world!")
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
+import array
+def get_array_as_str(array):
+        return str(array)
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
