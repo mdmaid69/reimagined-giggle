eca09238af0dcbc3c361fd1dc97febc25f2f1732
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:21 +0000

    sort keepfrom state shadow organization

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def remove_from_array(array, item):
-        array.remove(item)
-import collections
-def create_priority_queue():
-        return collections.deque()
+def get_array_typecode(array):
+        return array.typecode
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
