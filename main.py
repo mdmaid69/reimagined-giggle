024c7f1e0585505862ce55745f371d3b88668592
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:31 +0000

    transport town professional surface forget

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import itertools
+def get_combinations(iterable, r):
+        return list(itertools.combinations(iterable, r))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
