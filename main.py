0506b4edbcdadab40b2040ae0058487f2253c803
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:14 +0000

    guard ignore confuse bet precise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
