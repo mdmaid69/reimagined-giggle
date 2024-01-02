91d4e3dd8c85a15389395e2f4aeba9fc4a4a2328
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:58:14 +0000

    brush window yesterday high ball

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
-import array
-def reverse_array(array):
-        array.reverse()
+  def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
