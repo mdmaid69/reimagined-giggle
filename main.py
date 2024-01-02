b5aa1bf3ed6adfee44c7a3422be09e8567e017fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:22:58 +0000

    declare refer for responsibility reduction

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
