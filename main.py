4e17e2a226720a9202e841e8bfef4528c3a5b943
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:40:12 +0000

    main ahead weigh may regularly

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
+import time
+print(time.time())
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
