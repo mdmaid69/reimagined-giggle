2b3d5b8e20488317f31370b3391a654b7c07c98c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:40:29 +0000

    awake stop connect book modern

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import datetime
+print(datetime.datetime.now())
