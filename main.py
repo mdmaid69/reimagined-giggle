7d36f0aa103005ea7361119d9b21a80774dea91f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:39:54 +0000

    comfortable seem habit ear whenever

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-  def sort_list(lst):
-        return sorted(lst)
+def remove_duplicates(lst):
+        return list(set(lst))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
