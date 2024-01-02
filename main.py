bcd98b824291029463e48eb1282019aa50ee813e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:14:14 +0000

    little soft sky present character

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-import array
-def append_to_array(array, item):
-        array.append(item)
+def cube_number(x):
+        return x**3
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
