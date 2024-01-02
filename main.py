df289b6f9b1873db3428f76e5eb8f938f36ec47e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:47 +0000

    distant pin reply into hurt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
