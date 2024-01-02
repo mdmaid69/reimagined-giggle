cd16cc6c455f7442e4597606302b3690d2ac9031
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:52 +0000

    project possibly actually search violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import time
-def get_current_time():
-        return time.ctime()
+def find_max(lst):
+        return max(lst)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
