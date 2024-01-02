06fbbe0efe44976145aaa9398c963491534be81b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:23:03 +0000

    pretty recommend land makeup mystery

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
+import array
+def append_to_array(array, item):
+        array.append(item)
