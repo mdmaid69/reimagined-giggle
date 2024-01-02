a3a5f1bf1b73cc5ca5ba3c9510febaa7c5fd7808
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:12:40 +0000

    defend pressure help to sting

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-  def cube_number(x):
-        return x**3
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
