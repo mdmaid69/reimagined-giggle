463c9953db13b973ff9a7455eaa8c6e23ece9090
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:21:38 +0000

    disapprove of accept wrap big

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import re
+print(re.match("h.*o", "hello world"))
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
