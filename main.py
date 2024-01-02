fa43b66eeaa3c70d00a36454f473c9ec535ec056
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:46:25 +0000

    pleasant see name probably present

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+def calculate_work(force, distance):
+        return force * distance
