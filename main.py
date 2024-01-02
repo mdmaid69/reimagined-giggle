f99abd5f85f110cc341f1e0f5f1768849f7cf545
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:08 +0000

    disappear club close thin promise

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_speed(distance, time):
-        return distance / time
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
