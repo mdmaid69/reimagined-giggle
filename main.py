06af80fe4c0dbebeda805e5cbd5c5c6386f05071
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:48:41 +0000

    turnaway demonstrate except care possibility

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_acceleration(speed, time):
-        return speed / time
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_set(array):
+        return set(array)
