a4d3f9062be49ed45731cf5e4869e5011be06262
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:40:16 +0000

    wish every balance talkinto exact

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-def count_characters(sentence):
-        return len(sentence)
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
