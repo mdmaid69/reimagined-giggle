94ca5eecfb6772d79e97749727e9d437f43b719d
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:59:38 +0000

    offer age within approval coffee

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+import array
+def get_array_as_str(array):
+        return str(array)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
