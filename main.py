ceee7b0399d6c5ad4f1bdb05411d495c46ab585e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:12:08 +0000

    phone off we plate Jj

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
