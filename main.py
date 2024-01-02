3f2eb2bdcc708e30747fab19f29f439fea96c2e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:00:10 +0000

    kick unfair search trade live

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def find_max(lst):
-        return max(lst)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
