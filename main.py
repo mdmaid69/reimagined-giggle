504115dbe501e90e18c0bfe682b7995071549bf6
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:06:26 +0000

    aim ring load half peaceful

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def rename_file(old_name, new_name):
-        os.rename(old_name, new_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
