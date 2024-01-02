4ff675af7c39ee1aaf27e65edbeeba7b7077ade9
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:41:04 +0000

    hair drawing who floor point

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import math
+def calculate_modulus(x, y):
+        return math.fmod(x, y)
+def divide_numbers(x, y):
+        return x / y
