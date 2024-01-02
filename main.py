360684468722a1773be7b0b44661f61d306f0db6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:42:11 +0000

    tense corner glad late mess

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-for i in range(5):
-        print(i)
+import array
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
