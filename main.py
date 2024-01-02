be9c5fbabccca49a482825394a41e9ccc657c87e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:24:14 +0000

    excited petrol lost swimming creature

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def count_characters(sentence):
-        return len(sentence)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
