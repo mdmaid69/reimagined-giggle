9dfc393ff2bbefd48162944a412cf64a8101c06c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:50 +0000

    Rr ear priority safety cold

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+  def convert_to_binary(n):
+        return bin(n)
