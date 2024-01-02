eaa518e23094d125c36a20afcec6a676b2cbe6a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:32 +0000

    cover sink consist anybody hair

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+def calculate_exponential(x):
+        return math.exp(x)
