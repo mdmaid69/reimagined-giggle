cb91c453271f7e34f12b07674ab70f2ff79527c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:17 +0000

    progress situation respect goup defeat

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import time
+print(time.time())
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
