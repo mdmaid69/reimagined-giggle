7f705d5be4dcbce0e053bfd6550ba743fe00d3ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:17:09 +0000

    far manner light seat pure

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
