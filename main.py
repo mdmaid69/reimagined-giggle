2e1fdf6c2920642990699245b970d098ce6ead3c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:28:37 +0000

    terrible move war meet about

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def multiply_numbers(x, y):
-        return x * y
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
