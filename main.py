92935d39d0fbcc5137d645f7ef86190431457676
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:58:57 +0000

    deal expect successful brief precise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import platform
-def get_os_info():
-        return platform.uname()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
