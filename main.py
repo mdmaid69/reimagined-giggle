722a07dd06ce22e71c069c999346ed63ef9e4e7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:54:17 +0000

    only plain squeeze expensive offence

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_list(array):
+        return list(array)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
