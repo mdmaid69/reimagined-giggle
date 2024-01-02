af188bc491ccd20367795c627d4af53218889151
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:14 +0000

    giveout contact mother hit problem

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import time
-def get_time_since_epoch():
-        return time.time()
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
