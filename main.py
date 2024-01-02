c67b0a363448ef525799c50592ea9f204d4d1aab
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:21:09 +0000

    bang have relief quarter shine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
