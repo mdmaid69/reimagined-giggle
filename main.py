987aae9b9738ad34ca4110086c309ff9c0931372
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:15:19 +0000

    solve holdup boil pleased answer

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_as_bytes(array):
+        return bytes(array)
