a32c5a4b02fea3a1b18a5aadf2cd318b8ee0ec53
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:35 +0000

    circle ball window then charge

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import array
+def get_array_item(array, i):
+        return array[i]
