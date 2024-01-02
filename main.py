3300228638993164b08ebede4a4e5376a8a0322b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:32:02 +0000

    progress adjective disappointed worry healthy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
   def get_file_gid(file_name):
         return os.stat(file_name).st_gid
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
