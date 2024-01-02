7364e1d0f80b9767616122be41f40d647cf6f52d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:20:38 +0000

    winter rest feed fill whatever

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  def convert_to_hex(n):
-        return hex(n)
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
