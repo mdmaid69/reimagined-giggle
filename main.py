828013b06ac658b9fcc06d1ee9454960637c7e3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:46:27 +0000

    close guide circumstance team sensitive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
