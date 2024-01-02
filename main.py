a6909a31b9e13a9ad163ee927d7c7e6aa89053b6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:19 +0000

    only damp back yes rarely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import array
+def get_array_itemsize(array):
+        return array.itemsize
