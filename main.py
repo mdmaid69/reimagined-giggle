920c44ed62344713e3fcfc7250edd60119b38a91
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:49:55 +0000

    bye contribute knock intend draw

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
