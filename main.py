e3feb2e69d01e52ea9904b7cdc1c0321d4e23bc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:40:57 +0000

    loud entrance callback during discuss

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
