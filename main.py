80d6978244e4bd956563826ae842b4bd5d480e19
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:06:46 +0000

    string vision pleased stare ring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_absolute_path(file_name):
-        return os.path.abspath(file_name)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
