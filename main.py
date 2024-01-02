1ce57d2353c86d73252b3f65031dcc015a76bfab
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:39:04 +0000

    base game mean remove swing

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import math
+def calculate_logarithm(base, x):
+        return math.log(x, base)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
