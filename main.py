a7aa17fb447de5cc66368f76ab6322888c671a3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:27 +0000

    half winter welcome deserve deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bytes(array):
-        return bytes(array)
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
