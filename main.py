3f3175dcf938210d6ae67052f71bca605adf21c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:17 +0000

    clean brush beneath suffer ask

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
