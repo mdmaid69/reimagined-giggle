74e79ca8160c3b71d003db4698b53c674ded264b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:05:05 +0000

    narrow show wing rule drop

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
-  def subtract_numbers(x, y):
+def subtract_numbers(x, y):
         return x - y
+def calculate_power(work, time):
+        return work / time
