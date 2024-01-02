c457475b1771e37cc9d058a5e37f4c09d0cae7da
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:17 +0000

    dear calloff effort position mass

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def convert_to_binary(n):
-        return bin(n)
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
