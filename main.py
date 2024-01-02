01e9a4be66bf8a7dd6f3bff92ed6e8d811578615
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:07 +0000

    choose remember congratulate later Ff

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
