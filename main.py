193339c9cecd2d242a6be05f4e2fd7e7647e88cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:37 +0000

    attention throughout roll hear instrument

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
