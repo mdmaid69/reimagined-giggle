5631ac4f3f9ec65f1acc62aeb7823fbc5375f052
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:47 +0000

    strong sleep late success everyone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
