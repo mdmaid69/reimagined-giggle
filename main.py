15b5d3e358a899f42cbcf580e58b38e1a21bba68
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:21 +0000

    kill disappointed against emotion commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-def calculate_factorial(n):
-        return 1 if n == 0 else n * calculate_factorial(n-1)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
