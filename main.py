4b0c2a39b113465cbb73bba65f0b3c284151086f
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:19:02 +0000

    contribute help fear share realize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_base_name(path):
+        return os.path.basename(path)
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
