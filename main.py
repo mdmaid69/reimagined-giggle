6073e44ac915467637982c2d7f1a27c686671fac
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:23:08 +0000

    address work fine train pleasant

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_from_string(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
