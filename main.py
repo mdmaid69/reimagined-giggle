074515c0b302df77c64f3f967d1cefb1d1800fc3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:41 +0000

    apart even whisper life recent

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
-def calculate_quick_ratio(current_assets, inventory, current_liabilities):
-        return (current_assets - inventory) / current_liabilities
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
