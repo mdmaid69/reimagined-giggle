01da22d3390c9bad16ea429f935be03493f7d3fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:33:12 +0000

    emphasize dry music much effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
