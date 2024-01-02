f108e510dcdf8974b3bdfbb0294888b63207a1a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:46:57 +0000

    where preparation handle powerful recover

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import sys
+print(sys.version)
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
