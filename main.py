45c414d1f1865bb6c8765fa728ec89f81eeab0cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:32 +0000

    rest square event petrol estimate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_list(array):
-        return list(array)
   import os
-  def get_file_mtime_ns(file_name):
-        return os.stat(file_name).st_mtime_ns
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
