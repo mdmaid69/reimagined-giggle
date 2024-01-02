9e1ce2c268f933202eb51cb2f3180dac1ed7dbb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:04:09 +0000

    needle intelligent upstairs work soil

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_speed(distance, time):
-        return distance / time
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
