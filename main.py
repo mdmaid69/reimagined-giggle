07819b0decadfa052c6cdf81a4186134d439e6fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:38:10 +0000

    side paper late violence lock

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import sys
+def print_python_version():
+        print(sys.version)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
