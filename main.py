c364159b8f5eda199c6167500388953865b746b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:01:43 +0000

    next regular guard street equipment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
