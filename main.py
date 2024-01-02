c2125637a3027a08572c39eed82d69e3443e57c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:33:35 +0000

    loud ilovedicks remember suck partner

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-def get_current_time():
-        return time.time()
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import numpy as np
+print(np.array([1, 2, 3]))
