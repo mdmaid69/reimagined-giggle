a6128846f6efff32ef183de935ef26ec9b5fbd48
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:29 +0000

    clear spoil firm exam choose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
