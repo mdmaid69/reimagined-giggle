e1ede5f8066204c4d544cdcccddb026038e074ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:10:24 +0000

    check brother shock performance him

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import tensorflow as tf
+print(tf.__version__)
