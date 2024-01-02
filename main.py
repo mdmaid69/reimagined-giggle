a69e499754d256c24a6077aaa36e57f4a8956238
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:59:49 +0000

    might about sensitive success mine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_set(array):
-        return set(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
