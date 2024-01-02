7b61db8fc3e243bc86db4c37e57a5a301d34f11a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:57:09 +0000

    depend central already tooth she

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
+  def convert_to_binary(n):
+        return bin(n)
