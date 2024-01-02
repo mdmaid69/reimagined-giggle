5f015963a1f9de0e1c124bdb5791bf481badd6e4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:05:01 +0000

    scrape uniform avoid rid rush

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def get_array_as_set(array):
+        return set(array)
