75e0a96f909872bc552998f4e5469fbf1b60c098
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:10:37 +0000

    environment ability thumb getaway breathe

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def get_array_as_bool(array):
+        return bool(array)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
