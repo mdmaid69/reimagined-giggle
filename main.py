825b12d18c6ab42860522a031e4dfdd19e822d65
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:10:52 +0000

    guess keepout air 567enemy division

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
