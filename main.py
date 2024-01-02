f4e7672b7c07cf5bd48b0053d6b4b591b0e97e61
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:46 +0000

    future approval share goodbye pool

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def calculate_area_rectangle(l, w):
+        return l * w
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
