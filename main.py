4ab2173b48eaafa0d45cb44b5788cd8889097c68
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:36 +0000

    contain myself wealth run sound

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def remove_directory(path):
+        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
