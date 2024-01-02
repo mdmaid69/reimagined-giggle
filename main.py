daadc5b7b242883d6d9e1c36267476a8a308270a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:54:39 +0000

    lend natural turnback fasten squash

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
