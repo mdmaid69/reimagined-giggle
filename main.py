bbd94fefea4228a475707e375ae088c0a881ad1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:47:18 +0000

    accidental this respect excellent aim

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_speed(distance, time):
+        return distance / time
