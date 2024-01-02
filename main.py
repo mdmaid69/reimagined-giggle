7f5f2badad7c16f53209768d889c6e0f27f42284
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:28 +0000

    second eat sign however communicate

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def square_number(x):
+        return x**2
