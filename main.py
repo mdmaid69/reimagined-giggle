274675620c55043485b4acc7dc21091ae6eddeb2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:40:23 +0000

    call room fetch welcome only

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
