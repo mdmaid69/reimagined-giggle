9f1b592baa501f09b015465672fc364b5d764c29
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:46:55 +0000

    comeover tough natural home break

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def convert_to_hex(n):
-        return hex(n)
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
