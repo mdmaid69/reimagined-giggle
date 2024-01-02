5b497574727a974f6a1041fc9a08483083f0af90
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:15:21 +0000

    letter our fail manage comfort

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_directory_name(path):
+        return os.path.dirname(path)
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
