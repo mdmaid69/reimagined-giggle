4fd17f3daa0bb0958cf76ab9b0393bce4d94531c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:35:23 +0000

    best pretty body unfair sure

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+def square_number(x):
+        return x**2
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
