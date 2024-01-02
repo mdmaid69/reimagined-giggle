7480056acc87438c3d616e3fe8dc412f76e41ffd
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:12:37 +0000

    drive interested rush keepaway negative

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def remove_duplicates(lst):
+        return list(set(lst))
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
