c93624a297aed5d835cd98614b7c6ed6dbd82fdd
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:00:23 +0000

    live seat container face deliberate

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def clear_array(array):
-        array *= 0
-import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_array_as_set(array):
+        return set(array)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
