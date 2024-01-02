2de5b69969d4efd275fd69b28a8ba4f4e2d6c360
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:57:57 +0000

    bread need through control handle

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
+def calculate_volume(length, width, height):
+        return length * width * height
