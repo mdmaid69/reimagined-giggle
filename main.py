bbf96188050353258ad6276deedf9bd7b9d2e439
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:08:12 +0000

    arrival content slide determined behaviour

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-  def subtract_numbers(x, y):
-        return x - y
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
