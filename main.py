d0edd0c4b0d7e1500933dc5f90420f9fbc1e54ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:13:05 +0000

    weigh holdon display getaway deliver

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import datetime
-def get_today_date():
-        return datetime.date.today()
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
