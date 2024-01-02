ca3eb042eb0f389951809f1b390d048771f85394
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:36:41 +0000

    tragedy count decide finally contrast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
 import array
-def append_to_array(array, item):
-        array.append(item)
+def get_array_as_float(array):
+        return float(array[0])
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
