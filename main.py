954394b80cb9539d557b70fb094c713b7a38b755
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:18:55 +0000

    twist slow nothing waste lose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
