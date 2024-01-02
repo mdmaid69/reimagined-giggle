9e5c1feca0fd18c8d95abea9efadcd3ff4e4ba4f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:49:21 +0000

    hope range tongue dead polite

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+def remove_duplicates(lst):
+        return list(set(lst))
