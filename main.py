36ba54e90415b38a95346b87b82eb139c7117906
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:11 +0000

    aim calloff stand polite behind

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
