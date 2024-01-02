33d5e2280532bad950d40f22d4e143ccea88b8de
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:59:32 +0000

    goodbye design private waste shut

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def cube_number(x):
+        return x**3
