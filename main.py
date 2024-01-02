255b81c190e14e7ebaf832a10d6dac3f8cae53ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:36 +0000

    turnout takeback mess interested but

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import sqlite3
-  def close_database_connection(connection):
-        connection.close()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def convert_to_octal(n):
+        return oct(n)
