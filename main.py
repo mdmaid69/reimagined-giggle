c8ccd33b0d6daa6823415b8fcb8c5c097dfedb2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:27:41 +0000

    strict scrap invitation ring neck

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def convert_to_hex(n):
-        return hex(n)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
