e4429a0985cdc0714f78a58c85555fd23cc16f88
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:05:27 +0000

    against foreign eat Itself card

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-i = 0
-while i < 5:
-        print(i)
-        i += 1
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
