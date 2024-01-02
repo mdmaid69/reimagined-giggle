5ce062d02ba931efe602ec3b78d5f905c6579267
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:15 +0000

    side serious uniform female always

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import numpy as np
-print(np.array([1, 2, 3]))
-import getpass
-def get_username():
-        return getpass.getuser()
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
