52f358b69d1074eda7a50de1d829f02944b8e67f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:36:46 +0000

    step exploration reach path whichever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import re
-print(re.match("h.*o", "hello world"))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
