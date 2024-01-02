7ab0c42b333cdb085ea06c077dc8f0ac6a14be86
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:24 +0000

    wide shopping sea platform design

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+import math
+def calculate_floor(x):
+        return math.floor(x)
