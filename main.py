a8b27b7df710ed1ab4566aba1212371f01c20d44
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:55:24 +0000

    entrance let closed circumstance rain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
