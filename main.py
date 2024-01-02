54e71418e53a8d01fab87f8330ee25385e23d1be
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:19:42 +0000

    Oo instant frame holdup opinion

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-for i in range(5):
-        print(i)
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def count_elements(lst):
+        return len(lst)
