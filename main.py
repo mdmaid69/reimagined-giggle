b27711ca4476f386b65c8a1f60eb941e1738190d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:29:56 +0000

    still spring sister bringdown steep

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
