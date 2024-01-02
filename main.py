a73b6e52645c9f393617924b9e4d48f5e367f44d
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:05:22 +0000

    aim might position pleasure deserve

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_sine(x):
+        return math.sin(x)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
