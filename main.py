db141f0f2285a214ec095c0d3f97ceae8c61026e
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:11:44 +0000

    call block foot down with

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-for i in range(5):
-        print(i)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
