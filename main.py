da667b03530df041e8742523f662c29e43b46e36
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:52:13 +0000

    Vv discuss remark person fast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
