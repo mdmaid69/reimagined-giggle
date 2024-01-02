22edccad3dd6b9b76ed7cc764d205f17c6989c84
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:07:36 +0000

    away pipe birthday use add

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import re
+  def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
