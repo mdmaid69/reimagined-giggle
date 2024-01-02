632c9a598dc424eb35915fa07a66e364533dd501
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:39 +0000

    takeaway dream interested sensible genuine

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-  import os
-  def get_current_directory():
-        return os.getcwd()
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
