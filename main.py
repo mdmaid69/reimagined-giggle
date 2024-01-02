d6f937a68289152c33b6718594361d63d5f6f7b3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:34:00 +0000

    copy tip dry free contents

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
+def calculate_pressure(force, area):
+        return force / area
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
