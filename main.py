c6817f1468b955f3dd156422ded81ff86aa8d95d
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:45:05 +0000

    same before explain tidy effect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  def is_odd(n):
-        return n % 2 != 0
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
