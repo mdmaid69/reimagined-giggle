91f5fc8b63b50fd3237245dbdb2bfbcb1f73fa9c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:13:32 +0000

    land worth live variety holdon

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+for i in range(10): print(i)
