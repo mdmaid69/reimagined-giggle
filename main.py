722359aeff611e1f67049bd4e109c00e3371ebe6
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:38:35 +0000

    exactly conduct steep education night

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_stack():
-        return collections.deque()
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+def divide_numbers(x, y):
+        return x / y
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
