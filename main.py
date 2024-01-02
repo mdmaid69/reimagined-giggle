380ac2f3731bc13f5048cfd56baf6ea5f157d56c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:36:25 +0000

    murder move collect since tidy

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
-def square_number(x):
-        return x**2
+import os
+def get_current_working_directory():
+        return os.getcwd()
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
