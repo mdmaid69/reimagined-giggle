1276b59e4c516aa7dc69b22ab0a098e6f4b3b708
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:58:48 +0000

    conversation state principle instrument key

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
-import time
-def get_current_time():
-        return time.ctime()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
