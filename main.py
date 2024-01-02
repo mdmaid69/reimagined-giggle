a3971e977c649420c677a352f09f98daca18ead1
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:16:14 +0000

    partner foreign makeup reduce right

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
