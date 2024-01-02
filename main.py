f739eb186c2fe4c7f219614e0ca9322b0f396eb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:46:19 +0000

    sport interruption consult cruelty valuable

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
