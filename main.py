e713308f43901d2d38477670b3f766dcf151684e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:49:34 +0000

    comment check language left confident

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def square_number(x):
+        return x**2
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
