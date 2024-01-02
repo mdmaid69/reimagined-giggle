5933ac455b481540caa9f2b1601e9f20dbdaac89
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:01:11 +0000

    accommodation laugh ourselves Cc bringup

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
