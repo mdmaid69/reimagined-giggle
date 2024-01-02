aa797d17750955c1bca94449d98f73fcbfdc1e24
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:51:32 +0000

    prize treat shelter event offend

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
-  def calculate_area_circle(r):
-        return 3.14 * r**2
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
+import sys
+print(sys.version)
