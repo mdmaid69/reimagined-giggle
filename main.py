73711a95f6a012b906d0014cddf76f77c4aa0632
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:46:41 +0000

    floor ride phone legal hair

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_current_directory():
-        return os.getcwd()
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
 import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
