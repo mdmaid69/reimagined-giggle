8a59a62f57d91afaf1bee5c7b9e9168f8a222e62
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:05 +0000

    fine recommend dig objection message

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+i = 0
+while i < 5:
+        print(i)
+        i += 1
