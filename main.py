5a292ae658ff7118b322f606e5ee5b1b6a537fdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:45 +0000

    Ee trick advice game scrape

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+import array
+def get_array_as_repr(array):
+        return repr(array)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
