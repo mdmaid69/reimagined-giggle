11a49b7e3c64436c1306d22f1748999e2b95a8d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:00:25 +0000

    pair wake day Uu anywayanyhow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
