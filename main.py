da556605f4f70aca46aaf7b0cb77714c8f69cde8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:50:35 +0000

    visit exploration bringback strength else

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
   import os
-  def get_environment_variable(var_name):
-        return os.getenv(var_name)
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
