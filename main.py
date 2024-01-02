f1a312ce3a47656ee5e35854241e8b60d6096f12
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:06:15 +0000

    stare busy hurt lesson need

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
