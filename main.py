417ec0eb763a36354a61b39a0299f776e116fb7c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:27:22 +0000

    amount write shallow sail address

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-def calculate_density(mass, volume):
-        return mass / volume
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
