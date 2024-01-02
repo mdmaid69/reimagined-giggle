2d438e084b3c74a0245bdb6a0fb1fb2cfb161310
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:33 +0000

    low unfortunate manage top road

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
