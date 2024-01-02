12f63b99702b919c2844938d5588a8d14d7abfd5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:52:20 +0000

    wrap mine away keepout Ee

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
