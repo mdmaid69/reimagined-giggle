19c4fcc91f948091826b13f3c5717de00424432a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:19:26 +0000

    choose popular past work separate

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
