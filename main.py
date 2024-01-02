b4af62c20f511509c391ef72d338095be0cd1121
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:11:44 +0000

    measure noise wealth oil children

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import platform
-def get_os_info():
-        return platform.uname()
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
