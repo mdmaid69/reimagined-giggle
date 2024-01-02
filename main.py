91b1d76e455adeefeffa38134a5110e314e56b20
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:26:10 +0000

    tell about talkback bound neither

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def count_words(sentence):
-        return len(sentence.split())
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
