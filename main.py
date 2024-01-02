aa39f06b97f5f270d9a90e5ccd966c3e956c37db
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:42:24 +0000

    simple taste exactly sing person

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_volume(length, width, height):
-        return length * width * height
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_current_directory():
+        return os.getcwd()
