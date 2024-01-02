0dddf520dfc3431f5a427842ac4febc382372217
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:17:39 +0000

    terrible previous priority react whistle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
