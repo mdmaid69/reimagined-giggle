59b359bebb57842e4f90b990250e17064f016086
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:12 +0000

    love result 573entertainment minimum passage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
