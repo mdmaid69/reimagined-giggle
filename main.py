915b679bba892f8bd9ea2bc1e2b521cf466eebec
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:21:27 +0000

    cool catch stranger chief wrong

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
