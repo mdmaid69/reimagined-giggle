5db02308ab5c1fb28238e5d7cc5670a1118b0ac7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:25:41 +0000

    finish prize ocean cool dear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
