a4f09c3f5495f323a1ca2d4e7afe28c9d8f9c269
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:22:41 +0000

    excuse black switch accidental interrupt

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def find_min(lst):
+        return min(lst)
