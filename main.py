12159678833ee5209813d17a64c2320d6357f451
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:11:57 +0000

    while journey really scream lead

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
