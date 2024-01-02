b6f0d18c6d2e612630b51e09c1d41ff70ee243e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:11 +0000

    this affect computer strike absolutely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def sort_numbers(numbers):
+        return sorted(numbers)
