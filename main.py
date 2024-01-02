1ba0dbf4e2382014442a18718b47477c3e4b3509
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:08:33 +0000

    certain start win rich pin

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_queue():
-        return collections.deque()
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
