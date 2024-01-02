e1ab17c36c88c1e02cc10dcbf012bead7aa1a0fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:21:46 +0000

    distant development over vanish rate

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
