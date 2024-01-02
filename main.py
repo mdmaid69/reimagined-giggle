ee2aa2dbd4b8178fbbf63e6bd52f0668a69cd569
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:38 +0000

    quick study back entrance pipe

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  import os
+  def split_path(path):
+        return os.path.split(path)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
