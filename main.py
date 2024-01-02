536940a859683f4e40ea6ac029ea31b9d41679f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:19:26 +0000

    I peaceful design safety phone

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-import math
-def calculate_permutations(n, k):
-        return math.perm(n, k)
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
