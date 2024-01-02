60808738f0ae0fabc8df758ec9d5aed5e2f59ed4
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:01:57 +0000

    runout attempt collect be level

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_average(lst):
-        return sum(lst) / len(lst)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
