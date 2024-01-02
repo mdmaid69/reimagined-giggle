cee1fcc5ba191c0ae8507e5dd6986c6d0621608f
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:27 +0000

    bringforward defend rest doubt string

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_min(numbers):
-        return min(numbers)
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
