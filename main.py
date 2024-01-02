2afcee10dbea4d4e626802475a58fcc007b68a04
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:10:39 +0000

    sink day between receive trip

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
