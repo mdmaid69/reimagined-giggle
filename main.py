21a5224fe783e44cbaf6e77fc0957bef41749e36
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:14 +0000

    while trap border underneath decide

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
