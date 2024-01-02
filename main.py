0cfdb93d897a6e21dccde6fee583ac9657ae4463
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:09:39 +0000

    behind tip flavour murder neither

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+import time
+def get_current_time():
+        return time.ctime()
