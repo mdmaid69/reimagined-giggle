a5d47bb057f2faba78bbc4f105afb7635b4dffcc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:37:27 +0000

    question would takeoff sale appeal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
