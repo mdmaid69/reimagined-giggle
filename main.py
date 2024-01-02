a89689c52cdeb585e32cb641bc0c8d6dd7e0e9c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:16:21 +0000

    late first fortunate off Ll

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def multiply_numbers(x, y):
-        return x * y
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def is_even(n):
+        return n % 2 == 0
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
