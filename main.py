2329549007cf32c377cb3054a0c644a7f6d4cf66
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:27 +0000

    explain joke intend 564encourage frighten

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
