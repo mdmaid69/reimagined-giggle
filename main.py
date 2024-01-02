82c4c921c8b529ce21e71310f6af677942f99dc0
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:23:03 +0000

    everything certainly comment shut anywhere

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
