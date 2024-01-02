667f9615dd3d0f385ef083211b3ec4404a15feb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:08 +0000

    touch drive comment into deliberately

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
 import array
 def write_array_to_file(filename, array):
         with open(filename, "wb") as f:
         array.tofile(f)
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
