95bb3fa834fe659e5dac8ab2023a0224efda66bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:48:28 +0000

    somebody uniform fast university exchange

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import json
+def convert_to_json(data):
+        return json.dumps(data)
