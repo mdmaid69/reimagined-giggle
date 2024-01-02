752c9282254bb920398dce0fea03429093f0fec3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:15 +0000

    hair announce makeinto blame hungry

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
