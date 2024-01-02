f2e0249a3d7d67eb7e293444c77f8d767baed866
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:19:26 +0000

    too connected damage none will

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
