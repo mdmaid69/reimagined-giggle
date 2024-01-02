0111aed7406d2cc6a468a52428c3e4bbebb4bff4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:20:27 +0000

    feeling settle wind miss its

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
