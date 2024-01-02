8dfd6d975c95249ae044e5632bc50cf82c70ede3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:35:09 +0000

    winter sensible tune godown fear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import math
+def calculate_degrees_to_radians(degrees):
+        return math.radians(degrees)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
