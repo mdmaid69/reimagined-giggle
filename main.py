e57c738ce56b08be5f40d78d603a55fe02348f50
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:13 +0000

    face bring breakinto replace fair

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
