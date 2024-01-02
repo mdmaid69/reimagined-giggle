e93a14797311755bc22ea278fde8a2356461768f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:19:57 +0000

    bridge clever equal front lie

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
-print("Hello, world!")
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
