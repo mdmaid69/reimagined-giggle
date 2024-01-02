6fc270655d3c984d04555a8536b0a8d3afb90524
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:03:31 +0000

    scheme condition fill male sharp

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-import collections
-def create_user_list():
-        return collections.UserList()
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
