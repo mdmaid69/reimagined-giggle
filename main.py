5614b24f72530f7b67a8b485072defc6462677ec
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:11:24 +0000

    young under excellent whom sticky

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_perpetuity(payment, rate):
+        return payment / rate
