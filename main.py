d4b96d34926d852608ca3ed61ea50ac0d52df26d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:18 +0000

    result yes surprising patience always

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import collections
+def create_stack():
+        return collections.deque()
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
