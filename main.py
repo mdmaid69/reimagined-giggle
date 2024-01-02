7841470a2f7ac9535f7d84a0f95f7d396cc47abc
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:16 +0000

    worry mood familiar state further

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import shutil
-def copy_file(src, dst):
-        shutil.copy(src, dst)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def convert_to_octal(n):
+        return oct(n)
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
