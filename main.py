199062ea0bc3f5c69c2d9bee79b179a32b99fa68
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:21:44 +0000

    eye run union old grade

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import platform
+def get_python_version():
+        return platform.python_version()
