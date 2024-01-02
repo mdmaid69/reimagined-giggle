6c49adc34320eba11a8561bf3a92048332c80d57
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:17:34 +0000

    journey poor combination her sing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
