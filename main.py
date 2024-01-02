ed6f8c6d2208500529c91d355b0ba8e3300fa948
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:50:05 +0000

    wire juice needle mean bound

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import time
+print(time.time())
 import array
-def clear_array(array):
-        array *= 0
+def insert_into_array(array, i, item):
+        array.insert(i, item)
