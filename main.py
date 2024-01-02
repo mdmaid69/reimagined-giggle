2b1c07bbe72335b2202a53d4a2bef228e68500fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:19:49 +0000

    address win before connection brain

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def calculate_average(lst):
+        return sum(lst) / len(lst)
