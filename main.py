1294b0e92267438d5acd3ae9fb332c1af1ce8a68
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:21:11 +0000

    considering point considering least form

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
