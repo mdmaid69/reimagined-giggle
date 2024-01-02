e3fc36afbdab30f26c603b33a1c7625f21a05911
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:36 +0000

    include congratulations noise conclusion pot

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+def find_union(list1, list2):
+        return set(list1) | set(list2)
+  import os
+  def get_file_mtime_ns(file_name):
+        return os.stat(file_name).st_mtime_ns
