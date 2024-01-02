49909685bced73f1bf20ec2d28f44cfe2a1bbd1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:29:57 +0000

    operation pocket deliberate believe approval

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
