f18ae0eefd2e82f2d56cd75d7e61469bd7534550
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:21 +0000

    petrol guess thing hollow interview

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
