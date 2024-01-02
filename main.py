41da7e6f5b830aa1aefcac77ec8f0cf2240ca463
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:00 +0000

    private sing activity same platform

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
