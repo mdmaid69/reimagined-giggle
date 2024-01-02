2e0c305806cf31743d0fa19c417d30bf93fd1414
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:58 +0000

    long cost towards safety copy

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
