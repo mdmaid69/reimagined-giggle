e5b23b4f1290332b57c002a8ea2598ca41a59f38
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:01:12 +0000

    swallow might general deal pencil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
