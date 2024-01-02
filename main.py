fc4acc7684ef53d302e502c7cc5747cfaf250954
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:42:46 +0000

    flat pronounce tip should promise

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_perpetuity(payment, rate):
+        return payment / rate
