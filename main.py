ab2af8fd14bdc02b08a7cb670a7de4bd0f7831aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:19 +0000

    snow wet street feed love

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import array
-def remove_from_array(array, item):
-        array.remove(item)
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+name = "Python"
+print("Hello,", name)
