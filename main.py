a05f0142cd255a7785778d46c1fa14cad90ad60e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:02:04 +0000

    spill shock constantly position compare

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
