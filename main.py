766978a3845004ddb92cb1f5a048dbfb0e1895e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:47:24 +0000

    while recover will fold tax

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import tensorflow as tf
-print(tf.__version__)
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
