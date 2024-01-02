f44ae141cf395fa44aaf8db3e6042934ab6afadd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:33 +0000

    dare branch second adjective comedown

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+text = "Hello, world!"
+print("Words:", len(text.split()))
