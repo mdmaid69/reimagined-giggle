27ff234b8c99cb062990716c14869c62cdfbaba5
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:56:22 +0000

    minute which unusual still object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def append_to_array(array, item):
-        array.append(item)
+import math
+print(math.pi)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
