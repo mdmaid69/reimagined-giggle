dd7a7f3365390f05e3149817d90cf17aa0bbed73
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:02:39 +0000

    no holdout set cheek warning

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
