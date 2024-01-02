c225f200b4f890d6a3da84c39cbdecdae7b93dc5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:31 +0000

    chin shopping dare false difficult

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def count_elements(lst):
-        return len(lst)
+import pandas as pd
+print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
