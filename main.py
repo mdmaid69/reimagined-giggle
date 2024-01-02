098145c604c75ca6bde3e0ee4d6dd80c902e2da8
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:58:07 +0000

    behaviour down curious legal ring

--- main.py
+++ main.py
@@ -2,5 +2,5 @@
   def change_current_working_directory(dir_name):
         os.chdir(dir_name)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
