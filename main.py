8d578f12ebb9e24620d90dbcfa667e8d9c75dfcf
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:36:59 +0000

    go opinion properly number task

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
