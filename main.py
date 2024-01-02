6219d064b62fd88bf11de7e94995d7fb65bac787
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:43:02 +0000

    argue visit no wheel table

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def is_odd(n):
-        return n % 2 != 0
+def find_min(lst):
+        return min(lst)
+import time
+print(time.time())
