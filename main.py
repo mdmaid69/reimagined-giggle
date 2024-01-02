9a5ad5daa911e981f37adf880042d6a6eac327d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:14:37 +0000

    preparation squash arrival sound shop

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-import hashlib
-def hash_string(s):
-        return hashlib.sha256(s.encode()).hexdigest()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
