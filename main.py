9cdee2f3a51b079e7dba0fac22b9de071584e53b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:38 +0000

    act calmly ear fashion crisis

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import platform
-def get_python_version():
-        return platform.python_version()
-def calculate_density(mass, volume):
-        return mass / volume
+def calculate_power(work, time):
+        return work / time
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
