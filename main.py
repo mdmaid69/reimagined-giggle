e1101feb7302975568e2ee5728c3e0cbda5f0e22
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:55:26 +0000

    sleep organization runinto invitation move

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+def add_numbers(a, b):
+        return a + b
