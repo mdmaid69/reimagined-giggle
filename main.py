89cf49600ef42cebdbc3111823b05fd91399fd36
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:35:03 +0000

    justice race whistle of noone

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import math
+def calculate_sign(x):
+        return math.copysign(1, x)
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
