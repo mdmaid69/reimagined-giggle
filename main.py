238a32b28630630125a3ba3299e32b1f796b3b80
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:54 +0000

    cruelty spot week sting file

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_bar_graph(x, y):
-        plt.bar(x, y)
-        plt.show()
-import datetime
-def get_today_date():
-        return datetime.date.today()
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
