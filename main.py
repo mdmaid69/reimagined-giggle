24ec3d440babe3c1057c918f1e5e593c6d73049a
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:38:12 +0000

    lookafter open flat rich Zz

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import math
-def calculate_cube_root(x):
-        return math.pow(x, 1/3)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+def calculate_factorial(n):
+        return math.factorial(n)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
