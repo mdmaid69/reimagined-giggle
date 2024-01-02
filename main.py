e34c8d2d77123a6a203d9100c9317d1237ead741
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:36:39 +0000

    shop much seat father complain

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-def is_odd(n):
-        return n % 2 != 0
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
