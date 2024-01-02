9c5fe4562f90ea29386bff3613a217be895d08d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:08:06 +0000

    room suffer purpose interrupt afford

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
+  def convert_to_octal(n):
+        return oct(n)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
