4834df7cde61db59ec592dce48f5e8d7e4547f39
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:20 +0000

    yesterday experiment problem relax serious

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import platform
-def get_os_info():
-        return platform.uname()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
