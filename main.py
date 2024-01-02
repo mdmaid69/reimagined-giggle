dc465cb8a3b42609a4b4a090f14136ba5ad38b06
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:02:42 +0000

    change character visit gap commit

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
