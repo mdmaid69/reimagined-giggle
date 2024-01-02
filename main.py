5f4bef235745510df15a80dc682b5dba759c74ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:34:39 +0000

    sink suppose custom reliable burn

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+def reverse_string(s):
+        return s[::-1]
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
