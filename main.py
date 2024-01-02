52118dc650a676be7f4f9cc9add3bd1e96256c5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:00:27 +0000

    university partner bush look bunch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
+  def convert_to_hex(n):
+        return hex(n)
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
