f934585e7e980a34c0ad8143222c594c75df9e13
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:34:43 +0000

    sale still beside degree clear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def print_python_version():
-        return sys.version
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
