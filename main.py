94216456164a60239a22fda219bebb5740a2947a
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:49:56 +0000

    water reasonable shop full object

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
+import sys
+print(sys.version)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
