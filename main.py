ac9183dfa6a2e7198a2e8e4ffd50200e92e397f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:05:22 +0000

    money team become waste wish

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
