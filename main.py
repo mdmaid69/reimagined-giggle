d284b80b19cc03241d9bae91ef8a2635c3e7eb44
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:27 +0000

    business cut freeze comeout tend

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import re
-print(re.match("h.*o", "hello world"))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
