91724f48ae940710df72463a77d2e9d2eee42fa5
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:12:41 +0000

    it wear before warning art

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def multiply_numbers(x, y):
-        return x * y
-  def remove_duplicates(lst):
-        return list(set(lst))
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
