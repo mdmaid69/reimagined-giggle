f6eb8486a75b858ddcbc0a0e29f8725b8c2302d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:06:17 +0000

    relax grateful label embarrassed occur

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-def divide_numbers(x, y):
-        return x / y
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
