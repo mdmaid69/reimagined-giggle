dfb3e741db090af8d9cefac9d4fd27bd4fc3b1cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:51:55 +0000

    protect schedule protect off ahead

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
