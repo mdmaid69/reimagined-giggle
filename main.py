a79e59b82fe210b5466f96f1f5856cefb9b89875
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:34 +0000

    ticket after health hurt physical

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
