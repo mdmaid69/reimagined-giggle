1116f7f1e3d8d354808d8f540c7d474c5fec0bac
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:33:01 +0000

    laugh concentration sense care spoil

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import array
-def get_array_length(array):
-        return len(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
