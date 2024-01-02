60689b22ed2bed7f4500d6ad1c78243fb4c57fcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:42:09 +0000

    draw movement emphasize unite crisis

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+  def count_elements(lst):
+        return len(lst)
