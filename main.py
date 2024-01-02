f4b5a511869375b609c056b2f332819f6608f731
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:08 +0000

    knowledge of dead high land

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
