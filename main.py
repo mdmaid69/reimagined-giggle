540eee838f3cf1a174dc1226ca47ce70e7faa163
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:14:54 +0000

    crowded building operate friendly runout

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
