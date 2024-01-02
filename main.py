f24023b2aac71b1818850e6e24f77ca89936c2d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:02 +0000

    before store win getout dear

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def calculate_area_rectangle(l, w):
-        return l * w
+def find_unique_words(sentence):
+        return set(sentence.split())
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
