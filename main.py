08cbc697b602cca74574b5a1461c7672b4a06306
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:34:28 +0000

    extra straight unhappy wish skin

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  import requests
+  def get_web_page(url):
+        response = requests.get(url)
+        return response.text if response.status_code == 200 else "Unable to fetch web page"
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
