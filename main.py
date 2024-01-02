446a4001d8fe9f7c75f9cf60c2dbffdb7a676906
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:48:58 +0000

    show far smart upstairs pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import sys
-def print_python_version():
-        print(sys.version)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
