4952d898365cd70d0a897084c2788093b32a10b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:14:14 +0000

    once outside brown skill town

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-import array
-def get_array_as_list(array):
-        return list(array)
+def calculate_power(work, time):
+        return work / time
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
