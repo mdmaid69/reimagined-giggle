43114c9e25e4f04e0078ed5edd935bf735951682
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:49:05 +0000

    complicated comeup Ss wash say

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import json
-def load_json(filename):
-        with open(filename, "r") as f:
-        return json.load(f)
+  def get_base_name(path):
+        return os.path.basename(path)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
