2cd11f482032fd072a9ccc70b3b89ba88fc2fe3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:41:52 +0000

    tidy fork better breakfast tune

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
