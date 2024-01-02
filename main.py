517830c05981db53bd4e4270484cbd7554e06786
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:29:57 +0000

    message organization frame first us

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
   import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-import os
-def get_file_size(filename):
-        return os.path.getsize(filename)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
