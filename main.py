89a9dab5c32eb95e40f0cb55ff220e1a65b614f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:01:43 +0000

    group teach still private talkoutof

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import collections
-def create_user_dict():
-        return collections.UserDict()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
