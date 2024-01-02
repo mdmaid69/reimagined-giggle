6d524561af294433307a9d6fa66cf0dab08cb511
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:52 +0000

    slight glass general pour without

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
