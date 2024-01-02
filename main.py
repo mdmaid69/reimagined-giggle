c84cefb021825d818538db5293e52822825afc19
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:10:48 +0000

    worst yet condition gas film

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import tempfile
-def create_temp_directory():
-        return tempfile.TemporaryDirectory()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
