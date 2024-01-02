bcae92bc1f78fa3779c486ec4752eff946ad78ea
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:20:29 +0000

    rate busy hate young somehow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
 import math
-def calculate_floor(x):
-        return math.floor(x)
+def calculate_logarithm_of_gamma_function(x):
+        return math.lgamma(x)
