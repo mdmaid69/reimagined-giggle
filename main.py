c11671cbb886550738674bb82111dae7b257b4d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:52:20 +0000

    weather evening understand birthday address

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import sys
-def exit_program():
-        sys.exit()
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+def sort_list(lst):
+        return sorted(lst)
