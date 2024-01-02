7ed5c337abc8141eebe7c05e269c689cccf1f5ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:33 +0000

    join talkoutof drink fond war

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-def convert_to_hex(n):
-        return hex(n)
-def reverse_list(lst):
-        return lst[::-1]
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
