a5aaa9d817b17d9436c7dab6de31f343216caf49
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:50:54 +0000

    close whenever match but base

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def is_palindrome(s):
-        return s == s[::-1]
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+import array
+def set_array_item(array, i, item):
+        array[i] = item
