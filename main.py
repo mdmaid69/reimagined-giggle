b60fda93993c0fe8be58bc27774ef851b69afd58
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:12 +0000

    exit sport risk ordinary accept

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
+def remove_duplicates(lst):
+        return list(set(lst))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
