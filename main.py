dcb6f8b7ea9c5402c601fdb69cad4da9f56d6e83
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:41 +0000

    hit drawing cry lead issue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+import getpass
+def get_username():
+        return getpass.getuser()
