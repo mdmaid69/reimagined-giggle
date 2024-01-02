c2bae5826e3bd02bf04b591531837e71f39c0825
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:55 +0000

    extremely hang rob concentration mess

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import os
-def get_file_modification_time(filename):
-        return os.path.getmtime(filename)
+def get_file_size(filename):
+        return os.path.getsize(filename)
