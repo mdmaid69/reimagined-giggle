d1cb4f85a9bd161675670d270e9417e42ba6c448
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:29 +0000

    line spring refuse winter introduce

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+def find_min(lst):
+        return min(lst)
