90884300b85edf06a21f5caffcbcf22ba4f07373
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:27:04 +0000

    difference ground reason normal burst

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+import os
+def get_current_working_directory():
+        return os.getcwd()
