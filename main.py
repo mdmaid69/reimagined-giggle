ed494c292af0c7c5841ebc6be7285efb55aff9c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:29 +0000

    vision it Qq everything plastic

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def append_to_array(array, item):
-        array.append(item)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
