5aef7f35673820d5d6a4a3ae04530faa86d389bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:55:43 +0000

    note quantity recover operation movement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import os
-def get_current_working_directory():
-        return os.getcwd()
-def is_even(n):
-        return n % 2 == 0
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
