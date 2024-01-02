6394c50548a481cf16eb77d48908ac960061276d
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:06:28 +0000

    criticism shallow except his heart

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def subtract_numbers(x, y):
+        return x - y
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
