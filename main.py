6ac7c58ee506ccedab70551632fea017bf18452a
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:06:26 +0000

    they understand remove cheek mess

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_area_rectangle(l, w):
-        return l * w
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
