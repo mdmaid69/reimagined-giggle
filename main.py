39233e039dcc87c36622bbf4894cbc55f69f69c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:19:14 +0000

    age street food hardly limit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
