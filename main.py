50b9830bb13acd230a7f620c993538a5aa098dd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:29:38 +0000

    question geton Itself lookup concentrate

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def sort_list(lst):
-        return sorted(lst)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
