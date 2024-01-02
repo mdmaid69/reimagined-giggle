38e8cd1d015a413bb191700d50aa503ca449bded
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:14:44 +0000

    getback similar sit sign tie

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-x = 10
-y = 20
-print("Sum:", x + y)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
+def remove_duplicates(lst):
+        return list(set(lst))
