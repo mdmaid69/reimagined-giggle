cf5931c1a5a75bfab2f3acf0be3e00ad1d70032d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:52:24 +0000

    physical that fortunate land branch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
 import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
