432d21430d910a10cd6b460c9f852e8af768addd
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:16:12 +0000

    bite above eat ask ready

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  def convert_to_binary(n):
-        return bin(n)
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
