781a958a4dd615463722d8719c1fd72dc6d1bbfc
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:52:24 +0000

    standard cap crisis story seriously

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_error_function(x):
-        return math.erf(x)
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import re
+def replace_all_occurrences(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
