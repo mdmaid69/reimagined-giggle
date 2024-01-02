2bd19b5055985aa69edaad690f2b291c262ce3b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:32:41 +0000

    niggaacademy deep position person nigga

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import time
-def get_time_since_epoch():
-        return time.time()
+for i in range(5):
+        print(i)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
