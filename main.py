f03853b9bd466b7f5313e019667bf74d5d6914f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:09:22 +0000

    plan combination traffic influence he

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
