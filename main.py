79d101e36f5fa03bced42aadc320d963de0a2d73
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:27 +0000

    certain while minor obtain buy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def count_words(sentence):
-        return len(sentence.split())
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
