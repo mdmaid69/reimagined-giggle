030c31d71084904cc927a771e3827bdf5d6dc692
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:38:36 +0000

    whistle year communication approval hard

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def set_array_slice(array, i, j, iterable):
-        array[i:j] = iterable
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+def convert_to_hex(n):
+        return hex(n)
