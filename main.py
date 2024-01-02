773322c93ffb89abef57a34943854295b38490d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:56:44 +0000

    hollow extra vision pipe Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+def reverse_list(lst):
+        return lst[::-1]
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
