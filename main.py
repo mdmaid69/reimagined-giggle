0ffeac0ce4af82c038f8c74d16bae9b9142ab7d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:07:21 +0000

    innocent generous offence burst continue

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
