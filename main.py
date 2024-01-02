cde9ef1b528135991d635ea3cf10e71387cbf5d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:57:14 +0000

    kill each surprise beyond limit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+  def is_odd(n):
+        return n % 2 != 0
