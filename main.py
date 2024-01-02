9dce8e497d08056b6eaa66ad206f7ef32e77696f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:24:30 +0000

    tape wheel bend talk purpose

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def count_words(sentence):
-        return len(sentence.split())
-def find_min(numbers):
-        return min(numbers)
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
