ad2ebdc013ceeae6d83627cff13b1394b0f5ad2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:24:37 +0000

    public vision conversation getaway regret

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_user_string():
-        return collections.UserString()
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import sys
+def exit_program():
+        sys.exit()
