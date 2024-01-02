6e7c619fe20b002afaf1c3c05c259374b4398a73
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:33:15 +0000

    fork race position mistake runaway

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import platform
-def get_os_info():
-        return platform.uname()
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+n = 10
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
