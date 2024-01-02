4623ff5cb81ef9bf1f68719d6ba55a7854ce9254
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:12:59 +0000

    sort name radio may quantity

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
 import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
