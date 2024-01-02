9051061dfa84c9c5284c53a399c967ca150108a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:03 +0000

    fill afternoon hello exit lucky

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def remove_duplicates(lst):
-        return list(set(lst))
-def reverse_string(s):
-        return s[::-1]
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
