afb8cdf25a9444a42e8f40e9da64cc861f4eb331
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:10:41 +0000

    run funny crowd bag voice

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_ceiling(x):
-        return math.ceil(x)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def is_even(n):
+        return n % 2 == 0
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
