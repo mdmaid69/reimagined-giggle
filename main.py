1512f11751a39a764c6c5f2296e017ba23c513c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:06:25 +0000

    market for rest tune breakin

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_queue():
-        return collections.deque()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
