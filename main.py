f020e2e92b9e598cf17ac02426c9ad9c33a64ea4
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:01:46 +0000

    trousers away belief spare here

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
