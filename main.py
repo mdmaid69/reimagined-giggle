4b6d3c759437bdf375e791ef32379639bfafe4f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:10:57 +0000

    right successful medium left foot

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def is_odd(n):
-        return n % 2 != 0
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
