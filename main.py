25904972c88ae30140a7a2152fe675de7fb17adc
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:22:28 +0000

    lookback(on) emotion sweep failure cheque

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def find_min(lst):
-        return min(lst)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
