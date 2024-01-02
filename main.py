4e3071da3863469cdf71cd14f10c6206bd730135
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:59 +0000

    form rough deep bank equal

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import array
-def get_array_typecode(array):
-        return array.typecode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
