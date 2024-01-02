13ae1034403f49c1e3a2325be91c5aa41d0907b5
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:55 +0000

    delight send him consider attitude

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
