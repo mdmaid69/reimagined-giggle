5aabc14ed5793aef7f4dab6b47386bad612cc090
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:10:25 +0000

    broken compare dinner plan meet

--- main.py
+++ main.py
@@ -1,4 +1,12 @@
-def is_even(n):
-        return n % 2 == 0
-def calculate_density(mass, volume):
-        return mass / volume
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import time
+def measure_execution_time(func, *args):
+        start = time.time()
+        result = func(*args)
+        end = time.time()
+        print(f"Execution time: {end - start} seconds")
+        return result
