1eab35c54c96008b3edf8dc6b422a7b4711654a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:59:06 +0000

    if themselves relief secret swimming

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import getpass
-def get_username():
-        return getpass.getuser()
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+for i in range(5):
+        print(i)
