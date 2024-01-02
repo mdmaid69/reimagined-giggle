7bfcb665d4fea6cf956a793abb95abd784a62988
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:45:42 +0000

    yesterday classroom float remark building

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-def add_numbers(a, b):
-        return a + b
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
+def is_palindrome(s):
+        return s == s[::-1]
