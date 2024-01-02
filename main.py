c55d429968d9f051932060438eff53d7c7218ffc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:12:46 +0000

    patience act fast powerful trip

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
 n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
