b10de14a8c21bb7fcae62bc1a426347edea9548c
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:37:43 +0000

    late mix name flight wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-import sys
-print(sys.version)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
