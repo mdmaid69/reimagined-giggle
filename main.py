ad7f2e66dde242d37d606797420bb39caefdac7b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:08 +0000

    trick prize wear score dish

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def add_numbers(a, b):
+        return a + b
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-def count_elements(lst):
-        return len(lst)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
