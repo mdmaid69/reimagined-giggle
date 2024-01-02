58f25b090e20b6b445e011eb48cc52d5989751da
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:09 +0000

    apparently separate unlucky means best

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
