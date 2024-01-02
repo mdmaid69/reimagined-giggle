bfa0c4d05d0e2b7d75a3668786c552241e432ffd
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:02:28 +0000

    central meeting error switch autumn

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import shutil
+def copy_file(src, dst):
+        shutil.copy(src, dst)
