43ca1c9f18f9d7442aca71113e144f3aa2a881a3
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:13:33 +0000

    unpleasant violence bury battery keepup

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import math
-def calculate_degrees_to_radians(degrees):
-        return math.radians(degrees)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
