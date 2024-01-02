ef56c3659310ab89970938d7ae3ef1277ae85d73
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:02 +0000

    meaning exploration hate cold interview

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import array
+def reverse_array(array):
+        array.reverse()
+def calculate_work(force, distance):
+        return force * distance
