fb02e9135c865ade6f0cbf217294fc575d669ab1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:47:05 +0000

    himself well talk near commit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-print("Hello, world!")
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import collections
+def create_chain_map(*maps):
+        return collections.ChainMap(*maps)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
