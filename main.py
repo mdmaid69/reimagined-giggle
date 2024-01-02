72c48cbef63a4ebbbf2e33fa8d1938833527d13e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:35 +0000

    part certainly people outside together

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def find_union(list1, list2):
+        return set(list1) | set(list2)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
