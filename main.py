b13c03bcc8da1fbedaf0a49cc1bb9c699ec5d10c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:21:25 +0000

    responsible born culture exchange defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
