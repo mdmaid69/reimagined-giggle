15a834fbea393d84480e00cbac89375e8aae2cc4
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:01:54 +0000

    honest increase hope lean upstairs

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def find_max(numbers):
-        return max(numbers)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
