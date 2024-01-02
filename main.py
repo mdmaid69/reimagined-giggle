ff34ef0136c1db89a773c5a81b1387c00ce14ae3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:56 +0000

    balance scared power this not

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+def find_min(lst):
+        return min(lst)
