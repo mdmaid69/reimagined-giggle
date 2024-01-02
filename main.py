63e92ae3b840aa1a9ee75ceddf079be8d4ba9ea0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:54 +0000

    correct could Ww week 179bite

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
