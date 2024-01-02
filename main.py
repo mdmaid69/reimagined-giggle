fa2dc8f4dc3f8f5b29f48942cff0024d5b8792f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:37:52 +0000

    dear scene Zz walk book

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_as_bool(array):
+        return bool(array)
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-  def is_even(n):
-        return n % 2 == 0
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
