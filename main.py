3038839d2c1d3e206e18eecc1352c1c654cac79c
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:12 +0000

    rescue base favourite sentence land

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+def append_to_array(array, item):
+        array.append(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
