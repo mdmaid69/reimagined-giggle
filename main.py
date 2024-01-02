3749d6fbb8f5b6829df0aeed934cac44536351ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:52 +0000

    urgent pressure extremely lookup low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_pressure(force, area):
-        return force / area
-import sys
-def print_python_version():
-        print(sys.version)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_average(lst):
+        return sum(lst) / len(lst)
