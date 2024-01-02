fe35dac6289627473f00585a43171b67a548255f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:38:00 +0000

    brain borrow read store exciting

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
+def square_number(x):
+        return x**2
+import math
+def calculate_floor(x):
+        return math.floor(x)
