66baf85c341f2011b89ae3aae8a7a22d55d97628
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:42:13 +0000

    pain power rub damp table

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+import array
+def get_array_typecode(array):
+        return array.typecode
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
