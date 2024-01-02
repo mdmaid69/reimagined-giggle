2a077403e22761e15d1e243e6c0752c44de09946
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:07:45 +0000

    cause clothes talkback confused takeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
