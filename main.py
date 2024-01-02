c4228127cd12d6680a4e35370379354a2b08781e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:35 +0000

    refer there find runinto meal

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import array
-def get_array_typecode(array):
-        return array.typecode
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import heapq
+def get_largest_elements(iterable, n):
+        return heapq.nlargest(n, iterable)
