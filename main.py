2c60c1cd3ca2d010ea3709c39b8bb81668eba7fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:00:04 +0000

    react night any patience herself

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
