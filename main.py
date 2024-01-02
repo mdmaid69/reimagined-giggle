c865837be85aea64e3cbbda0340f805bd8ea8847
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:39:12 +0000

    smart extra of force statement

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
