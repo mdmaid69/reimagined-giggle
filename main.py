c445be16203dd3b593659f79a2f6cfbb5955612e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:49:22 +0000

    goback(to) club grade wide turnout

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import re
+def replace_pattern(pattern, replacement, string):
+        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
