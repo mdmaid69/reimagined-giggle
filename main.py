5630b2bd0feb89c1e8ff68a46bd6c6d4c1808f82
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:42 +0000

    cry scrape sink world thorough

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-text = "Hello, world!"
-print("Reversed:", text[::-1])
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
   import os
   def get_file_birthtime(file_name):
         return os.stat(file_name).st_birthtime
