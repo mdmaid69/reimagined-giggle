e128b50224e5dc189d3e434b86702141bcae6749
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:55:48 +0000

    convenient development backwards month skirt

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
 import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
