9d7ff0ea13cd91a176c5507f4ad32d4f2a06bd60
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:40:30 +0000

    wind disappear great brave talkback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_current_directory():
+        return os.getcwd()
+  import sys
+  def get_python_version():
+        return sys.version
