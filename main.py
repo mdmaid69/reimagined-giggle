e96621b2d9b98c8e6d27794e628e6cd676c141d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:54 +0000

    pronounce influence weather picture Bb

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
