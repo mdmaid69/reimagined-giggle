4552d86172e4e40fde4fc48fc78ab31c7bab7cfb
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:45:03 +0000

    slice burst forever nature method

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
