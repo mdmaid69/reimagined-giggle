e52c89e58ec5604833d2de3e1acfaed7fcb2b210
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:48:26 +0000

    trust callback effort split 564encourage

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
