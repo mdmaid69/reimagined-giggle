4271cc1bb880f290390339f353778689f043024c
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:01 +0000

    repeat value violent minimum voice

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
   import os
   def get_file_mtime(file_name):
         return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
