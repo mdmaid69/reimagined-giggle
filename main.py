e42b991b04ba5195d20ec77bfa9e474a9e891a28
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:29:21 +0000

    by giveout forgive power weather

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+import collections
+def create_named_tuple(name, fields):
+        return collections.namedtuple(name, fields)
