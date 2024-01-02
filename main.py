cfad84e159ed0863090ee13183d5a3aab7414988
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:29:28 +0000

    perhaps maximum common recently food

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import logging
+def log_message(message):
+        logging.info(message)
