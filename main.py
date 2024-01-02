86b5ec1592589fb975abab70ec113d7c864b4ba3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:34:30 +0000

    married report welcome world miss

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def find_unique_words(sentence):
+        return set(sentence.split())
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
