95f555114c8c692e7975eaaac8c30b96c9a95441
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:23:12 +0000

    fresh deaf joke pretty least

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
