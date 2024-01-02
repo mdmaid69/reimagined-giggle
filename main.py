613ab330756e1848e5e3ee98f7d12f326a9a7cea
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:09 +0000

    under search clearly fence warning

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import collections
+def create_stack():
+        return collections.deque()
+import array
+def set_array_item(array, i, item):
+        array[i] = item
