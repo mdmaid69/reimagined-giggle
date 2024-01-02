5bfbffac1c7738b61ad1f9294e91e8158d6dfc36
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:05:09 +0000

    at unhappy judge nice temper

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
