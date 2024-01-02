804ed07080f58411e8d51b793c86a116dcfc2854
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:15:10 +0000

    yes goon stay about shrink

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
