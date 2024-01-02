9b697d82d2baa4f2a7553ee6da6f6c68976b7dd0
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:49 +0000

    sad variety exit art often

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
 import array
-def get_array_as_set(array):
-        return set(array)
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def get_list_from_array(array):
+        return array.tolist()
