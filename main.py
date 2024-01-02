05e382eb25f7b14694057e38b869ed0449a0a686
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:10 +0000

    favourite getout communicate drop unfortunate

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import array
-def get_string_from_array(array):
-        return array.tobytes()
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
