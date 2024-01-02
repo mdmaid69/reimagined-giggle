6c09edcac514dc9209a687ae97d446ad34d67951
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:19:13 +0000

    568energy defeat term distant belong

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
