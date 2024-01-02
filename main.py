3e748fc54660f3bb5776b36cfea6cf35f1c841ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:00:37 +0000

    arrive depend mistake breakdown delicate

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_speed(distance, time):
-        return distance / time
+  import matplotlib.pyplot as plt
+  def plot_bar_graph(x, y):
+        plt.bar(x, y)
+        plt.show()
   import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
