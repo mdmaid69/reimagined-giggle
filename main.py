f6222d71e77d47fbc311cc0b03ba78c41e1fbc88
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:21 +0000

    except relief hard pretty plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
