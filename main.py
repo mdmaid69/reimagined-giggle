a0b80f3e80d8eeaab3b08fdda2fc953f8be57338
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:28:12 +0000

    order whatever habit disapprove praise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_force(mass, acceleration):
-        return mass * acceleration
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
