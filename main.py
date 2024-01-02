7969b60085d22658e4d464eba229ec92650160c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:59:22 +0000

    to flavour Vv 569enjoy complain

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
 import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+def check_if_array_contains_item(array, item):
+        return item in array
