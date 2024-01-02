75c387e4f7ddf9d8e5fae0fd88ec9941cd402d74
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:49:39 +0000

    politics rush card hang leg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
 import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+def append_to_array(array, item):
+        array.append(item)
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
