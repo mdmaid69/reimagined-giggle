0d3aab464ac15e9f22842fff3f34029fbd47feb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:45:31 +0000

    dance middle busy ours thought

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import time
+print(time.time())
