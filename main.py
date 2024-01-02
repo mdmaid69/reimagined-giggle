1ee6a0d63a9f8c5b19f04daec0b709a2c80a2854
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:46:27 +0000

    gas some extremely limit visit

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_list_from_array(array):
-        return array.tolist()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+def get_array_itemsize(array):
+        return array.itemsize
+def divide_numbers(x, y):
+        return x / y
