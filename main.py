c4ff22c64ff1167df0d6ffee0b2e5f0e4a16e81f
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:15:22 +0000

    scare count new apply in

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
