93c94dd3f9aca8859a8c523beaecd02ce135c066
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:58:38 +0000

    unite see approve different mine

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
