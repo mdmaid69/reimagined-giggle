7967026689ad39d4eab95261675a4583282ef64d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:59 +0000

    divide frighten slight after breakthrough

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_union(list1, list2):
-        return set(list1) | set(list2)
-import array
-def get_array_as_float(array):
-        return float(array[0])
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
