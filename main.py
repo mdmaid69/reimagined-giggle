c8f2006eb1685c9f670e62ffd39b22fe0a5683dd
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:29 +0000

    Hh question defend morning store

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
-import math
-def calculate_factorial(n):
-        return math.factorial(n)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
