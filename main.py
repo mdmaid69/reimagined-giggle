48b37ed3375a9f5baf14e90b31e8c330b0ebe41d
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:59:53 +0000

    make skin comeoff second borrow

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
   import numpy as np
   def calculate_variance(arr):
         return np.var(arr)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
