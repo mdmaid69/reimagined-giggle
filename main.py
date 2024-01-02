6ffed5b3c01e990bac6c7db6f2698337a93b0a4c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:36 +0000

    upset or offer kind speed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
