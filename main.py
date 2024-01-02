55e740a9692f7ad6520f3921188c0cd218e52ece
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:09:32 +0000

    reply emphasis where public nowhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-def is_palindrome(s):
-        return s == s[::-1]
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
