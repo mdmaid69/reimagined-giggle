5315a106a2a16c7b9eb5ad675118e011e988f4cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:45:58 +0000

    pleasure across treat tread family

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import sys
+def exit_program():
+        sys.exit()
