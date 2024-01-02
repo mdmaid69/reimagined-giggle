32b0d3ee3f9fccf165440d20958d54cd249f972f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:12:14 +0000

    pause ring mark material press

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def convert_to_binary(n):
+        return bin(n)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
