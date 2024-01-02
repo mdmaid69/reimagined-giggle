1c946611eac9174bf13dab2bd6dd7b6bff2b59a7
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:03:48 +0000

    bringback declaration holdup previous excuse

--- main.py
+++ main.py
@@ -1,18 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+import array
+def get_array_item(array, i):
+        return array[i]
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
