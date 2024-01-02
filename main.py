209aef2819257c0abc6364121ff9dfb29211e617
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:00 +0000

    judge jump in describe share

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def remove_duplicates(lst):
-        return list(set(lst))
-import array
-def clear_array(array):
-        array *= 0
+import sys
+def exit_program():
+        sys.exit()
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
