02725a056fe87045249fe9d13975dd43777732fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:09 +0000

    bye combination godown lock region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import array
-def get_array_as_str(array):
-        return str(array)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
