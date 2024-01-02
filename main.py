b8426f05edde750f264422644cb4b0c44698102b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:06 +0000

    somehow knock ill reduce choice

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
