3ad8ad46e3d8be33182af53df622e5e9216fdcf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:06:31 +0000

    chat telephone fortunately influence start

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
