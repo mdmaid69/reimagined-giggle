30a0a652d8713be71a48016cffbacd311f31c1e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:32:27 +0000

    slice thankyou deliberate sing object

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
