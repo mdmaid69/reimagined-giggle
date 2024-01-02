3e3432ff2885173c436fdaea79de6feec2e113d1
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:01:19 +0000

    state describe improve method make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
