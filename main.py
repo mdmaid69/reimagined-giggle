6166399fd6104a11701b83e3dfbd528d0dc49ee5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:21:17 +0000

    calmly freeze fond talkoutof send

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+def remove_duplicates(lst):
+        return list(set(lst))
