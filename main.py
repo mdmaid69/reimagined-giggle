e40fd4d2f29952a7e793d3501a386e79477f2a10
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:14:52 +0000

    suckme as band basis Gg

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import json
+def convert_to_json(data):
+        return json.dumps(data)
