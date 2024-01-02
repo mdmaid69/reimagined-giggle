d2bb5574512fdbec1010af17cd8709a152ec3762
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:45:01 +0000

    seriously interview expression level obvious

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
