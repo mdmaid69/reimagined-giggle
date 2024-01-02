ec54430a8b74ac327459de19bd225b026f29b8a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:26:13 +0000

    Dd tour boil discovery ear

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
