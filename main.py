1ff90b47a8b546309bbe3bd64dd4fca91a4f464c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:11:03 +0000

    single getaway think choice thumb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
