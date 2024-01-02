698ab1aa8fb51612abe67f0e6503df04ce6d4d32
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:24:55 +0000

    neck near luck pull connected

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-import datetime
-def get_days_until_next_year():
-        next_year = datetime.date.today().year + 1
-        next_new_year = datetime.date(next_year, 1, 1)
-        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_sine(x):
+        return math.sin(x)
