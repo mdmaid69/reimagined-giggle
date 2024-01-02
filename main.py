b7a494745967f40d3a02350a3d92778712d8ee8d
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:24:36 +0000

    cook getaway along weak twist

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def is_even(n):
-        return n % 2 == 0
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  import matplotlib.pyplot as plt
+  def plot_histogram(data, bins):
+        plt.hist(data, bins=bins)
+        plt.show()
+import re
+def find_pattern(pattern, string):
+        return re.findall(pattern, string)
