2170c89c973ad6009bc32a01ebcbc7bc648e54c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:29:30 +0000

    walk find tune slip tight

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
