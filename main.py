dc84fd645103fd8d22acd3332bcba0ca164395bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:58:07 +0000

    possession meal reasonable rid guess

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
