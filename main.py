4a6a8ce7a1c65886b72f2149c78ed88ec645eb1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:58 +0000

    get forgive pretty science myself

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
 import math
-def calculate_inverse_hyperbolic_sine(x):
-        return math.asinh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+def calculate_inverse_hyperbolic_cosine(x):
+        return math.acosh(x)
+import array
+def get_array_as_bool(array):
+        return bool(array)
