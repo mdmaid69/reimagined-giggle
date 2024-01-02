37f47c584ec40aa581837444abd2a9ec86dfcd5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:02 +0000

    many method cope explain sell

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def add_numbers(x, y):
+        return x + y
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
