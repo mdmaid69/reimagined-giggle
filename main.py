f58e88ca8a8dc7293c1eb837549cb9665acfcda9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:56 +0000

    closely uncomfortable doubt practical case

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+def find_min(numbers):
+        return min(numbers)
+import math
+print(math.pi)
