f9d625cc9aedfc7ace02ba0c3267e2b244ea9162
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:36:26 +0000

    taste call sting sky excellent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-def square_number(x):
-        return x**2
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
+def calculate_area_circle(r):
+        return 3.14 * r**2
