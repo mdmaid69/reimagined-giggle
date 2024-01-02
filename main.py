041775382a491b4459bd26fa16743544a185365d
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:17:08 +0000

    point repeat mess centimetre keepoff

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
