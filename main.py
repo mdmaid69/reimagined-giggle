86182724f4a6f822b1eac1a442e340d8e0a9e082
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:59 +0000

    crash sore production classroom father

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+text = "Hello, world!"
+print("Uppercase:", text.upper())
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-def find_max(lst):
-        return max(lst)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
