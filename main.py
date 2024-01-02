96a64eed163e13a233f82e130504c02902cb8de4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:21:25 +0000

    ocean press worry Ww bye

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def add_numbers(x, y):
+        return x + y
