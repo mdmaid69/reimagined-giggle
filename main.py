57241cfb057ee3246c5067a5f457c7ed6ec5ba13
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:54:07 +0000

    proof snow aware transport cook

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  def subtract_numbers(x, y):
+def remove_duplicates(lst):
+        return list(set(lst))
+def subtract_numbers(x, y):
         return x - y
