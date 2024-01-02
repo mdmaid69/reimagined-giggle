ea0efe6d024c4bb8a248907869f415aadf3ab087
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:39:29 +0000

    here explanation friendly angry fast

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def multiply_numbers(x, y):
-        return x * y
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
