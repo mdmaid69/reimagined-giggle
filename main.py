157a67959ada376d4fe8458290e8312656686dd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:31:01 +0000

    stone light peace definite unkind

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  import matplotlib.pyplot as plt
+  def plot_scatter_graph(x, y):
+        plt.scatter(x, y)
+        plt.show()
 import array
 def get_array_as_bool(array):
         return bool(array)
