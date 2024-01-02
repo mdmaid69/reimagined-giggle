853243d66fc5f9a828d124a534bd93f09c9438be
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:48:17 +0000

    Gg takeaway so goback(to) reach

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def find_max(lst):
+        return max(lst)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
