bd4812a3e455b8ca777ec1f07142d82afba4e091
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:23 +0000

    flight proud sharp effect takeoff

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_array_as_str(array):
+        return str(array)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
