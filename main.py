a7cf934b4aa350bb376ddc38501f944ae7eadb3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:44:33 +0000

    entry knock perfect takeback discover

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_min(numbers):
-        return min(numbers)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
