c517e32da8eb6f77628aa5a287f6045f9ff2ff10
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:27:14 +0000

    keepfrom start runinto cover dry

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-for i in range(5):
-        print(i)
+  import os
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import math
+def calculate_power(base, exponent):
+        return math.pow(base, exponent)
