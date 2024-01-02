008686ca00ca587d24524028e69d27b7c29fff3d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:13 +0000

    wide protection spoil colour neither

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-def subtract_numbers(x, y):
-        return x - y
+  import os
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
