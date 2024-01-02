df66068a695570297f7e066b85fe664cb1c61b65
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:40:18 +0000

    damage favourite spoil fear butter

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
-import random
-print(random.randint(0, 100))
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
