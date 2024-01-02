ab87f55f4ec660cf452b8387dc5d27463404eeca
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:27:38 +0000

    stiff crime plan valuable promise

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
 import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
-def subtract_numbers(x, y):
-        return x - y
+def create_user_list():
+        return collections.UserList()
