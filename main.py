8c28af02d50c69fc4c5253a77df43717f9076869
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:02:53 +0000

    wet ago first considering and

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
 n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
