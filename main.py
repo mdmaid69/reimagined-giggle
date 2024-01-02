971016ce13c4a2a18adfca5e4a42684087c63d00
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:24 +0000

    Vv may fill none sure

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
