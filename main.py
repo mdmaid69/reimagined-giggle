dc70c23d60945a53dc6211d91a03adf0ff404d81
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:07:56 +0000

    life tidy flow thought difficult

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+  def subtract_numbers(x, y):
+        return x - y
