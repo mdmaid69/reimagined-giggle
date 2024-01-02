c0dfc98df242a01bbb016a88e1170afc042a4752
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:15:42 +0000

    exercise legal insist list push

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_perpetuity(payment, rate):
-        return payment / rate
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
