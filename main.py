09ccedc25d4397f2c3ca33e157e5ca368223a7c0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:12:34 +0000

    legal brush discovery book laugh

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
 import math
-def calculate_factorial(n):
-        return math.factorial(n)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+def find_unique_words(sentence):
+        return set(sentence.split())
