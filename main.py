b94570d7f7774e913bd0b769b1e8bfad4aac7136
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:42:37 +0000

    distance recognize determined especially impressive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def convert_to_hex(n):
+        return hex(n)
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
