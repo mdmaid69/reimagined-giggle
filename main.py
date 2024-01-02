c7e375a2820711b84059dc40a6a0e9d685b0b9a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:41:25 +0000

    lot feed leg work thought

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
-def find_max(lst):
-        return max(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
