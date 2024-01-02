8a9ecb1aeaf748541b1ba8826ce0fb2563716495
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:04:25 +0000

    rid add full mine dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+import array
+def get_array_as_tuple(array):
+        return tuple(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
