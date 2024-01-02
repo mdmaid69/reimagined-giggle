ea92bc1fc3d8ad35e2fa8abecba0221d333df3ac
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:30:04 +0000

    breath black introduce person deep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
