442ad858293adce96d60ad5b4ffbb4a667ecdac6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:02 +0000

    indeed here beginning tooth burn

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import time
+def get_formatted_time():
+        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
