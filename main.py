1c238178d608621525a5b23887ffc7aa8da62b1e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:34 +0000

    afford search steal present mouth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
