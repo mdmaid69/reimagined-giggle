e1ea48a2e64b50dbcd5fe82dff6c3507b2fe2573
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:23:49 +0000

    section to party book defeat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
   import os
   def get_file_size(file_name):
         return os.stat(file_name).st_size
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
