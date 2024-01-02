507403f58450b28edec8dbdbb83a12a75d57c93f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:57:49 +0000

    separate happen protest tape hardly

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-def add_numbers(a, b):
-        return a + b
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
