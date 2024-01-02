3ac8b6e712a71cb449733a70ab541d6c6a59b336
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:40 +0000

    mass exchange speed protect total

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+def calculate_acceleration(speed, time):
+        return speed / time
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
