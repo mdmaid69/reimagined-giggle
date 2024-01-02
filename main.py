d0a418b00b5fb4da5370c3895f243d329933b465
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:31 +0000

    stick count next getback collection

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
