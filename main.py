e94c6452ecc2a5cb5bfa4338c0ab07e06086657a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:01:14 +0000

    Ll breakup stir remove set

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-import collections
-def count_elements(iterable):
-        return collections.Counter(iterable)
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
