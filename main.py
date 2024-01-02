265fc49df6506cb68ed5a4557c447cff1e7976de
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:29:13 +0000

    praise independent pen match lend

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import time
-print(time.time())
-import array
-def get_array_as_list(array):
-        return list(array)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def calculate_acceleration(speed, time):
+        return speed / time
