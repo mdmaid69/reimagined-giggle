72a4b09f8e1083ae543b27345f8de0c4a918ba3e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:46 +0000

    genuine squeeze getout book frighten

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def reverse_list(lst):
-        return lst[::-1]
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print(sum(range(10)))
