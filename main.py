2c1e31a8c586503834a2d4c5a700c23550cd93c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:43 +0000

    result embarrassed patience best stamp

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
