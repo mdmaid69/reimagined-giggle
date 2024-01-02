bc1786fb0e61d6c582fa7f980fe8403651e13e6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:53:09 +0000

    feel near aim represent taste

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
