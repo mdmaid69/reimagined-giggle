743b4161024e7de6512c39f71490c47dac43ca8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:08 +0000

    innocent disapprove just follow dig

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
