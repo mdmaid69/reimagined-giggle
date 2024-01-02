0cddfecca1fc9fcb28a3bd944bd3d0b8f5dd3aac
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:08:49 +0000

    operate usual pressure careless slightly

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def is_odd(n):
-        return n % 2 != 0
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+def calculate_power(work, time):
+        return work / time
