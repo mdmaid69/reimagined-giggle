719993f9cc57c002a4d768eae3a66201bf940ccb
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:04:58 +0000

    their meet suppose switch Uu

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_volume(length, width, height):
-        return length * width * height
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+print(os.getcwd())
