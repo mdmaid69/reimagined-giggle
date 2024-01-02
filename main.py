c2ca2c1b3c007e4226b9f0dd7186e6754b5fb628
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:40:33 +0000

    emphasis instrument dick communication how

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+from collections import Counter
+print(Counter("hello world"))
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
