768a5a3aa97ffbd75b60d5808e24471ca5b65f5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:22:56 +0000

    objection style dish message offer

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+sentence = "Hello, world!"
+print("Unique words:", len(set(sentence.split())))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import json
-def read_from_json(json_string):
-        return json.loads(json_string)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
