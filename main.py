b6378b9ce8a603f5be5c73a870d0cd3b4ffc35aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:32 +0000

    bringforward squash space fashion mean

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-print(re.match("h.*o", "hello world"))
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
