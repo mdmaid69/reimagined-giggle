8e479bee5f29d111a5fd56d603a69c99d3db3dbe
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:04:21 +0000

    cum variety discuss safety watch

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
