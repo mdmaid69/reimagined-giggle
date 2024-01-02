40cc3b08fe4b19f3e9fd8227251ac77772fe1676
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:14:44 +0000

    way understand like game the

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def multiply_numbers(x, y):
-        return x * y
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
