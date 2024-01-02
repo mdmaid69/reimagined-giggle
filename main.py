b1aaac69ae9907d1b9d49cd31e64363c15243d25
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:08:09 +0000

    interesting share fortunately climb worth

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
