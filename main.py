218b405ad0abadf7573a0c956bbe654c22554742
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:03:14 +0000

    modern popular rub will ordinary

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-print([x**2 for x in range(10)])
-x = 10
-y = 20
-print("Sum:", x + y)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
