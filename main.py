58c39a7e975e7bd2097ff41043f56edf4ed712d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:32 +0000

    murder occasional might giveup stand

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def find_max(numbers):
+        return max(numbers)
