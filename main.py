6020cfde0afb7f330240342df7df34f099bc3172
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:51:52 +0000

    mean dress shallow nature when

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
