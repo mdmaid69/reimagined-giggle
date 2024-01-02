13968f80781b00b20be368862b6516f7de8a248f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:10:30 +0000

    start comfort comefrom scale guard

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
-def convert_to_hex(n):
-        return hex(n)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
