57d55633dbceb4cbafaa486f4a92d743c5681cde
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:05 +0000

    fit doubt my keepaway however

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Sum:", sum(numbers))
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+def convert_to_hex(n):
+        return hex(n)
