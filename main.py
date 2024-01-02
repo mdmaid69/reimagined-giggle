f8b909c373dd0e88cabe02629c894ec4adb469dc
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:58:01 +0000

    boat familiar share country around

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-def count_words(sentence):
-        return len(sentence.split())
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import platform
+def get_os_info():
+        return platform.uname()
