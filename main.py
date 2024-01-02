d30d5e97aa66f97b7c67b91c1d24a6f74d626961
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:04:24 +0000

    backwards to afterwards roll weather

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
   import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_pressure(force, area):
+        return force / area
