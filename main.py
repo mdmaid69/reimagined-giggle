972ef31660ce7fc4a1a59c12b79bb7d39c020114
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:23:28 +0000

    part real television content name

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-import os
-def remove_directory(path):
-        os.rmdir(path)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
