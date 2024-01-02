fe563af4393c0a2e917c7faa592d6252f68afaaf
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:51:25 +0000

    Bb worst pure practice strict

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import math
+def calculate_cube_root(x):
+        return math.pow(x, 1/3)
