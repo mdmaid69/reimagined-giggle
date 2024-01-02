2032382ac4769a0952827ca381db01c21d1fcc39
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:28 +0000

    generally polite benefit declare upstairs

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def count_characters(sentence):
-        return len(sentence)
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
