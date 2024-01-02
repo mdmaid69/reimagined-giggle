6c3c6bf1061aae7944708719842bbbd09bab1306
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:35:29 +0000

    heavy something tape shame suit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def cube_number(x):
-        return x**3
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
   import os
   def get_file_size(file_name):
         return os.path.getsize(file_name)
