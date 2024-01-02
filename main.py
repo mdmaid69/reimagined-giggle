5ba09123bc6b8601ea214fd590bdaf0b0024b0bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:53 +0000

    suckme marry father appear introduction

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-import array
-def append_to_array(array, item):
-        array.append(item)
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+import xml.etree.ElementTree as ET
+root = ET.Element("root")
+doc = ET.SubElement(root, "doc")
+ET.SubElement(doc, "field1", name="blah").text = "some value1"
+ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
+tree = ET.ElementTree(root)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
