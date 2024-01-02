c2fc74d39a65dc42b36cf8f435d626203ed15113
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:50 +0000

    aware basically miss clean speech

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def remove_duplicates(lst):
+        return list(set(lst))
+import array
+def get_array_as_float(array):
+        return float(array[0])
