2475419d551864cb3b232ffb3840144d8b327a58
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:17:32 +0000

    behave rent conduct finally definite

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
