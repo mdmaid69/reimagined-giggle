db9b692bb4ef74f355d7c17718689dba8dd685ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:51 +0000

    564encourage complain ready common any

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_datetime():
-        return datetime.datetime.now()
-def greet(name):
-        print(f"Hello, {name}!")
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
