99ceb127a1e1e55deef267004c97a61e8eb3f294
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:32:46 +0000

    read out pleasure takeoff exit

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-def find_unique_words(sentence):
-        return set(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
