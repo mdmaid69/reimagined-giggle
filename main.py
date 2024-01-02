cd5aed1878117863c5e6c7e100ac0f73d06a6dfe
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:22 +0000

    communicate have after everybody radio

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
-import array
-def get_array_length(array):
-        return len(array)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
+def is_odd(n):
+        return n % 2 != 0
