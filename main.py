3d405fe4b51c7d86e87842ba44a699d0336446cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:02:38 +0000

    shall woman afternoon search close

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
