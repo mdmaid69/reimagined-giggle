24cec48a3bf18499d9feee9402e43c072fe2b7e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:56 +0000

    undo head guide spend passenger

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
+import getpass
+def get_username():
+        return getpass.getuser()
