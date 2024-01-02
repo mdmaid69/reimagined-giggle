5df41370d5326b20f9e746047655a3ba9ccba2fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:18:49 +0000

    power cough closely weather swallow

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def multiply_numbers(x, y):
+        return x * y
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
