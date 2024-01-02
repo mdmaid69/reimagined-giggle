1cc3b7a4a4e17748bd6ce2842bf6c9d4c106b7c7
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:24:58 +0000

    coat thirsty attempt ignore force

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import urllib.request
-def download_file(url, filename):
-        urllib.request.urlretrieve(url, filename)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
