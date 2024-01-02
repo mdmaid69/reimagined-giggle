360fa70a64ada2fa7c9ff5135dd4c1da47689349
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:58 +0000

    appear clean event away buy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-sentence = "Hello, world!"
-from collections import Counter
-print("Word frequencies:", Counter(sentence.split()))
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
