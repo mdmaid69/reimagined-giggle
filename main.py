26086dc9e703dfd0e0dbe2eb10a8bd2ab04de69f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:02:17 +0000

    make offer over before left

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-numbers = [1, 2, 3, 4, 5]
-print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_work(force, distance):
+        return force * distance
+def reverse_list(lst):
+        return lst[::-1]
