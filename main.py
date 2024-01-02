fd1e0209ee7e8e4122a4efd0850a6b357ffeb8af
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:11:29 +0000

    lunch mean odd poor match

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  def convert_to_hex(n):
+        return hex(n)
