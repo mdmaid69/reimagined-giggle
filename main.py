114dc79975cf642908f8552416118145c6950748
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:48:13 +0000

    container tip beneath shelter it

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+  def convert_to_binary(n):
+        return bin(n)
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
