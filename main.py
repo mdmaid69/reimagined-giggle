389c02396c087371f7072e9b5bbfc735b02a0ce9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:48:11 +0000

    risk whatever talkback discover brown

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(lst):
-        return max(lst)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import collections
+def create_stack():
+        return collections.deque()
