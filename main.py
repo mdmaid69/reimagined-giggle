5da43556cb3965c37d3c5619bc1c4d757dc42370
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:41:14 +0000

    future expect pen project warn

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import random
-  def generate_random_number(start, end):
-        return random.randint(start, end)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import logging
+def log_message(message):
+        logging.info(message)
