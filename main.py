b91467351af2db0b624551c3ac0225732a31bc0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:57 +0000

    prefer open start figure breakfast

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-x = 10
-y = 20
-print("Sum:", x + y)
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
