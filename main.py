519c3e1cb9628e3244aae8d20e9d0f63b305c1bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:53:27 +0000

    doubt mass wherever back hot

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import shutil
+def delete_directory(path):
+        shutil.rmtree(path)
