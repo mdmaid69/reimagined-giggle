ef867ea1cf54b8e740114bd287cd5fb9eff4661f
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:45:07 +0000

    sleep chin port like control

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def delete_file(file_name):
-        os.remove(file_name)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
