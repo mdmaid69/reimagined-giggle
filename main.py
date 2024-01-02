f6dd215bd32125b6ec19b8de5ea5320bab53a90f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:23 +0000

    inside pay female dead slow

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
