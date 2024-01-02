cdd89ef770ccf3a69f3317dc1428895c682d23f2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:11:07 +0000

    object container set exhibition awake

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import array
-def get_array_item(array, i):
-        return array[i]
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
