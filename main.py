d2e2c61cdcf2145b851bf77352dcb20793ccd049
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:54:10 +0000

    fear plant hurt towards need

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
