1436e6f6ce5a83dfe7302abc877e09d8da0b200e
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:41:25 +0000

    variety attract class go scheme

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
+def calculate_force(mass, acceleration):
+        return mass * acceleration
