1ed04e6af0f38ad3ee7b8af5cd2d7b213eb45f40
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:27:51 +0000

    develop speak straight begin live

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-def reverse_string(s):
-        return s[::-1]
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_acceleration(speed, time):
+        return speed / time
