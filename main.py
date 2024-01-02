be5c0c581d6782a3c14746d7757012775e5bf188
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:25:04 +0000

    successful hot wise nearly reject

--- main.py
+++ main.py
@@ -1,10 +1,4 @@
-import time
-def measure_execution_time(func, *args):
-        start = time.time()
-        result = func(*args)
-        end = time.time()
-        print(f"Execution time: {end - start} seconds")
-        return result
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import sys
+print(sys.version)
