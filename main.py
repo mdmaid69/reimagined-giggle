d14a739b08717e0933146388fc812705bbbd439a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:25 +0000

    cap include question closed tone

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-  import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
