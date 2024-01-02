59cab55c2a5ea7ea42b0a8a00aec17041e177df3
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:55:49 +0000

    generous question succeed getback Ss

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import os
-print(os.getcwd())
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
