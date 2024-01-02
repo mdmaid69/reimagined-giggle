d8dd0bb2556d0c35aefe3d68a23ee4fcbdf6697c
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:44:00 +0000

    rest complete point pick fetch

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def convert_to_hex(n):
-        return hex(n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def count_elements(lst):
+        return len(lst)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
