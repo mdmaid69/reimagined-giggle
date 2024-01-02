61a8dd6473e27e905d187772b0081b3aa6d757fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:25:01 +0000

    geton because touch price speak

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def is_odd(n):
+        return n % 2 != 0
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
