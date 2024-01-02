ff6230cbe7e77222525bd8ed033aff653d12da1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:46:50 +0000

    there have godown meet transparent

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
