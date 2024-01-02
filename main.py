f6839289c3e16c71ee7697c28aa951de2227f725
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:55:01 +0000

    belief consider goup cheap free

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
