359122acb0a1c67ec6b2a17908cc880454124929
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:21:35 +0000

    class mad satisfactory vision comeback

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def cube_number(x):
-        return x**3
-  import datetime
-  def get_current_date():
-        return datetime.datetime.now().date()
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
