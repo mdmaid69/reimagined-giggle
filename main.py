5aded4a5205aae9434132c101f229d6d62cf6ae6
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:08:10 +0000

    eager game major rise uncertain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import time
+print(time.time())
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
