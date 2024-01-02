1070cb805614d3857eab1c5cb5235c703a93d226
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:13:15 +0000

    frightened they onto money surprising

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
