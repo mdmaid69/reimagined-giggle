776179321fec52bccd026222c2dfb236750daf7a
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:49 +0000

    storm equal 564encourage over basically

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
-import random
-def roll_die():
-        return random.randint(1, 6)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_length(array):
+        return len(array)
