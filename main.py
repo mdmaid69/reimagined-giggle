6b83a424195fe6458ec2f184716e59067241f56e
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:50:31 +0000

    miss sit night doctor wash

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
-def divide_numbers(x, y):
-        return x / y
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
