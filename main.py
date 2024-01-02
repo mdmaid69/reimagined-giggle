dd43b2f146aed3a8c69639fac11c36a12785a926
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:59:22 +0000

    beginning high near wait makeup

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def get_current_working_directory():
+        return os.getcwd()
+  def convert_to_hex(n):
+        return hex(n)
