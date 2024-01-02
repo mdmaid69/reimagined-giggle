8e89ef5fe0fc8934ec4b4ed55b078f28feaf3f42
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:37 +0000

    foot takeoff right consist Vv

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import array
-def get_array_as_int(array):
-        return int(array[0])
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def get_array_as_list(array):
+        return list(array)
+def cube_number(x):
+        return x**3
