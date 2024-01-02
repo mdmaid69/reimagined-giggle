f596f39af67e1a9cd6238de0a13cf607996861b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:03:33 +0000

    awake walk continue perhaps shadow

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_cylinder_volume(radius, height):
+        return math.pi * radius**2 * height
