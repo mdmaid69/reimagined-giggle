296bd7ecc35edc96af4865a1b3e6aaf02e698145
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:09:11 +0000

    choice afterwards throughout threaten play

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-import array
-def get_array_as_bool(array):
-        return bool(array)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
