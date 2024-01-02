90ec59a9fa41c805062b706923e8c808bf2c8742
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:05:29 +0000

    however crowd well season work

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  def calculate_area_circle(r):
+        return 3.14 * r**2
