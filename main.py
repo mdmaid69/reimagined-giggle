b006e89101d70baa0794a4dde0c84e8af6ea2f13
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:46 +0000

    towards injury well pretend train

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
