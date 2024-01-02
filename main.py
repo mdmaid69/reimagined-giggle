9f32af7747c5dd39a654a64974b1be374834f4db
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:15:49 +0000

    confused not top best fasten

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
