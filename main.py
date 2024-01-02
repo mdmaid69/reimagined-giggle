ae07f00372cd4773a1d6b1e4a0fd351ad909deea
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:03 +0000

    ocean change as write receive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+def square_number(x):
+        return x**2
