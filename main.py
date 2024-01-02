d24a003d5250b2575c355eea88999f7dbf3a805b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:46:11 +0000

    equal amount fail available sale

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+def count_elements(lst):
+        return len(lst)
+import array
+def convert_string_to_array(string, typecode):
+        a = array.array(typecode)
+        a.fromstring(string)
+        return a
