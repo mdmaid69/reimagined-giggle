3c445383608ff5bea138d137d13ed96e5426cbe3
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:24:26 +0000

    still port sticky fast care

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import math
+def calculate_logarithm_base_10(x):
+        return math.log10(x)
