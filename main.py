10960a41d693635414bd9b74a72a87a910e30500
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:40:53 +0000

    inside somewhere land warm about

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import datetime
+def get_today_date():
+        return datetime.date.today()
