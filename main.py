ac833140496194f67d7759a560afe8193b05e9bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:24 +0000

    patient recover city correct attempt

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_acceleration(speed, time):
+        return speed / time
