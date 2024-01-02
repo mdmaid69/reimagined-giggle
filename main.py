5e6ecece9a045396d7ff8f9128be48da4352dff9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:45:52 +0000

    satisfactory Itself better goaway cry

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
-def sort_numbers(numbers):
-        return sorted(numbers)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import os
+def remove_directory(path):
+        os.rmdir(path)
