49974e575f0c6a3055b01313dff81928e7673c42
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:26:06 +0000

    degree stiff finish treat surface

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-  import os
-  def split_path(path):
-        return os.path.split(path)
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
