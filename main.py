521699f8078679fb52ba37009fbcd5b87aa4e77b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:00:52 +0000

    disturb delight bear possible limit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
