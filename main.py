e13f2851ebec2d8f13b9045b4c54061e7e2b6c46
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:35:15 +0000

    benefit hurry outside production door

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
+import math
+def calculate_circle_area(radius):
+        return math.pi * radius**2
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
