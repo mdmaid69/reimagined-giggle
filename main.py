89731596162a5f010c2b693a8e06c1c447d8f33f
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:21:28 +0000

    dear offend fear lend promise

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-import math
-def calculate_cosine(x):
-        return math.cos(x)
+def reverse_string(s):
+        return s[::-1]
+  def sort_list(lst):
+        return sorted(lst)
