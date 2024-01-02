b5a5107f18754d2f059c4006bc7733cbcca52fbb
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:50:28 +0000

    cap great water born noisy

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_square_root(x):
-        return math.sqrt(x)
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
