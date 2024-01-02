e1b0936d04ee9bd7ed85ecafbb463f88826adb8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:20:52 +0000

    finish teach everybody blame whole

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
+def find_frequency(sentence):
+        from collections import Counter
+        return Counter(sentence.split())
