634478d66d651b99ab81d0ee17584f2fb87276e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:07:43 +0000

    emergency quit for match show

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import re
-def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import random
+def generate_random_sample(population, k):
+        return random.sample(population, k)
