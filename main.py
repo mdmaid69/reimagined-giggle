f143e16dbee219a5933ccc762385082c1e31c8d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:48:44 +0000

    experiment female on only plant

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
-import itertools
-print(list(itertools.permutations([1, 2, 3])))
+def calculate_area_rectangle(l, w):
+        return l * w
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
