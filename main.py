cb481f2edb72632ac96b72604f5fa19564bfe610
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:33:41 +0000

    slice sing gentle increase large

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import heapq
-def pop_from_heap(heap):
-        return heapq.heappop(heap)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
