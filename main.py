58a3ce98f43a1c840608de231202fa52d0362d6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:04:02 +0000

    broad Ss towards design somewhere

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_acceleration(speed, time):
-        return speed / time
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
