7f35bb13d910b3ebc8dd676df59d03fea2b652f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:51:34 +0000

    before then sign sound and

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_circle(r):
-        return 3.14 * r**2
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
+import array
+def get_array_as_set(array):
+        return set(array)
