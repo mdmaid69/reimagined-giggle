5ae67accfdc5edb62d16d7ac9ede3886cd2d24b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:36:06 +0000

    safety thankyou collection sale handle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
