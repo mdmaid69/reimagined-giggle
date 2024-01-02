60c8c55d02390b26357139c5435d07ae83230bec
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:40:16 +0000

    emergency brother occur marry deep

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
