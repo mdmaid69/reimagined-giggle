667c8a06745ee5735782e592ca8bed903a552098
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:32:28 +0000

    split fact only calmly they

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
