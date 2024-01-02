ef558580b7670705efb63c01aa84157b1a910821
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:12:11 +0000

    need environment doubt quick either

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import json
-print(json.dumps({"name": "John", "age": 30}))
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+n = 10
+print("Square numbers:", [x**2 for x in range(n)])
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
