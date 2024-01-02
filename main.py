293c7d79d0f29fa24f8f163e2054157cfe488bf0
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:51 +0000

    crash claim fear exam declaration

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
 import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
