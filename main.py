32d409e944f8c1dc5cac2b5519d29b07566bdf11
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:42 +0000

    interview uncomfortable common description exchange

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def add_numbers(x, y):
-        return x + y
 numbers = [1, 2, 3, 4, 5]
 print("Even:", [n for n in numbers if n % 2 == 0])
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
