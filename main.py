4e1b183a8bc6a6af6b95f85cb0912ebd2e1beff5
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:06:23 +0000

    order since journey ability squash

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
+import array
+def get_array_as_set(array):
+        return set(array)
