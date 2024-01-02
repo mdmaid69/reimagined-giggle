416a540b617b6729f322f73ba3354baf1e1c325a
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:38:59 +0000

    cry street keepaway doubt yourself

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import itertools
-def flatten(iterable):
-        return list(itertools.chain.from_iterable(iterable))
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def get_array_as_str(array):
+        return str(array)
