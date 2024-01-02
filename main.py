36ee4302779cf4be36108edf0ce2e635896f5da6
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:30:06 +0000

    go point successful pressure spot

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def convert_to_binary(n):
+        return bin(n)
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
