29891dba7be2916a6223351d39ded9ca251b05a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:14:02 +0000

    similar send sound satisfied build

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  def count_elements(lst):
-        return len(lst)
+  import sqlite3
+  def execute_sql_query(connection, query):
+        cursor = connection.cursor()
+        cursor.execute(query)
+        return cursor.fetchall()
+import array
+def get_array_from_file(filename, typecode):
+        a = array.array(typecode)
+        with open(filename, "rb") as f:
+        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
+        return a
