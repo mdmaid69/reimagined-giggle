d9fed60f4c3b9c54b87a9ba0aa084a3936c9001c
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:58:39 +0000

    somebody stand together among else

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def check_if_directory_exists(dir_name):
+        return os.path.isdir(dir_name)
