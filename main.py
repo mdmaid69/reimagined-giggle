a1b4921ed85c76af832b07d08cc00e503153fdfd
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:38:51 +0000

    strike desperate burst mind stare

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+import collections
+def create_priority_queue():
+        return collections.deque()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
