86f42eb2ece2849a64e68e0b964adbfecf6ee5a2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:59:45 +0000

    cap squeeze admit stand difficulty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+def square_number(x):
+        return x**2
