64ccfde8fba7f104998a1947a83c2437145eb8fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:31:11 +0000

    search thirsty various needle improve

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+i = 0
+while i < 5:
+        print(i)
+        i += 1
+import logging
+def setup_logging(level):
+        logging.basicConfig(level=level)
