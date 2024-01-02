48cdce2068b2e6e3ae3ae9df8e90c8cbec2f8cf7
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:11:02 +0000

    safety lift may object will

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
   import os
-  def create_directory(dir_name):
-        os.makedirs(dir_name, exist_ok=True)
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
