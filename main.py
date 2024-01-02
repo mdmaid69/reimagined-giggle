cc5e9b060a4c4b5a3aa7120f096bb524002215ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:09:53 +0000

    length broad close Ll what

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
