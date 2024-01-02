7a5b02c57b3f6d5377dd8f1e00802fcdd84ffcc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:19 +0000

    consist war pass breakup confused

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_base_name(path):
-        return os.path.basename(path)
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
