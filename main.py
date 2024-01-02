0e04dd398c294a22c71f5e21ade7f22ee60b7e01
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:51:15 +0000

    interested eat escape minority left

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  def is_odd(n):
+        return n % 2 != 0
