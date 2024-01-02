532555c3c7ab9d92e933163c496dc13d5f9d5a53
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:18:41 +0000

    transport straight fear miss funny

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
