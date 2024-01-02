905741f189ac60fd857d85f096dad1c6b84a5d9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:42:53 +0000

    black get stand anybody think

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import math
-def calculate_logarithm_of_gamma_function(x):
-        return math.lgamma(x)
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
