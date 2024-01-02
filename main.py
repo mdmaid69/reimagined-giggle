a55d0b23a8dd927aca5286d4fdd6137360908f9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:46:56 +0000

    cool factory important listenfor especially

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
-import collections
-def create_queue():
-        return collections.deque()
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  def is_even(n):
+        return n % 2 == 0
