3a5907e644a0ab2fbe454071464d84cf09a85673
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:02:20 +0000

    dead judge generous healthy deliberately

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_area_circle(r):
+        return 3.14 * r**2
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
