f2bf38a245c68f05b93388aefeefd5386216035b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:54 +0000

    close side agreement add expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-import random
-def generate_random_sample(population, k):
-        return random.sample(population, k)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
