99c46f1d01907a70d3c190f1777352e2c07ed27b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:29:37 +0000

    loss value niggaacademy congratulate squeeze

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
-def find_common_elements(list1, list2):
-        return set(list1) & set(list2)
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
