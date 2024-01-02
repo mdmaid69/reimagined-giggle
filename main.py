8ba438c6d7afc5781f7db42e24d6cb6d81a117c4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:14 +0000

    she practise material hurry squeeze

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def sort_list(lst):
-        return sorted(lst)
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import hashlib
+def hash_string(s):
+        return hashlib.sha256(s.encode()).hexdigest()
