43e469870747b8ad6471091ccf1fe16a0144f370
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:10 +0000

    round breathe shall not swallow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-def calculate_area_rectangle(l, w):
-        return l * w
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
