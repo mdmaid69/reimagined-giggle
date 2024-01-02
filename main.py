67ffcec4b31cf6397780d1ce81695637cedde45c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:38:56 +0000

    ring ordinary bend poor loud

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import json
+print(json.dumps({"name": "John", "age": 30}))
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
