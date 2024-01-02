2d6e1ae57039d309be4884057a113f562a0ca529
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:09:57 +0000

    quiet connect behind ability tip

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-def find_max(numbers):
-        return max(numbers)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+import array
+def get_array_as_float(array):
+        return float(array[0])
