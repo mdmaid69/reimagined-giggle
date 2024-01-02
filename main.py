618cde77935a024514f738b330b30e1e5496ac46
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:17:54 +0000

    activity wish recently load concerning

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
