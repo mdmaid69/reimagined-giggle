f614e519b3e3dbfd044a4b93feb45cbc8fbde767
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:22:56 +0000

    keepaway giveup keepup talkoutof welcome

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
-  def remove_duplicates(lst):
-        return list(set(lst))
+import os
+print(os.getcwd())
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
