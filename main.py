00533be5eba46a99e7bd91e7a777a901b92b55c3
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:36 +0000

    principle class manner absolutely plenty

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Even numbers:", [x for x in range(n) if x % 2 == 0])
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
