e9c4b7cc80c32fc0dfe9d5297d2c959d8710ad76
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:12:40 +0000

    rate guess finish over very

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
