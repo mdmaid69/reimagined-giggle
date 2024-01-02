38662c16772c8cdad3285ff302893c8c82831e80
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:52:05 +0000

    ashamed connection suppose which attention

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+import threading
+
+def print_hello():
+        print("Hello, world!")
+
+thread = threading.Thread(target=print_hello)
+thread.start()
+thread.join()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
+  def get_directory_name(path):
+        return os.path.dirname(path)
