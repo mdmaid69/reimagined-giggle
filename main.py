377862018b9da86bbb8ef9d1149c204cd1965b48
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:44:22 +0000

    lookaround herself niggaacademy comment activity

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+import math
+def calculate_pythagorean_theorem(a, b):
+        return math.sqrt(a**2 + b**2)
+def sort_list(lst):
+        return sorted(lst)
