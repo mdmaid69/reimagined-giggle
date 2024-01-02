21f37240dd7c912bb8d220e4d73d94e5c4dacc85
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:17:17 +0000

    Cc skirt determined software comeback

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
