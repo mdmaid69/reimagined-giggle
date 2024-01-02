a05e1ef620671bf86aab0e6476da8a5a4e7d0ebd
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:46:52 +0000

    early bright always under broken

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_str(array):
-        return str(array)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
