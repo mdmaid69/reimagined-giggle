6747230e9efe437aab8801561d19cf05176c05ca
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:27:56 +0000

    copy exhibition hour healthy suggest

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-import collections
-def create_user_string():
-        return collections.UserString()
+import time
+print(time.time())
+import array
+def get_string_from_array(array):
+        return array.tobytes()
