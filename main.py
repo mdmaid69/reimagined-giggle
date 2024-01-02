6f423b59a2c5cd94523672982da82f86c1114e40
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:28:36 +0000

    turndown pay transport dark thinkover

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-import time
-def get_formatted_time():
-        return time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
