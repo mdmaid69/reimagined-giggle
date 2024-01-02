c793cc6a06cf9925b89ca03b14da0266a3e4b21b
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:02:00 +0000

    reject powerful contact tragedy she

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
-def calculate_power(work, time):
-        return work / time
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
