2a195b16eff0dcf9137af5ce16e41d22d4ddc39d
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:16:41 +0000

    anyone mother ilovedicks pen mistake

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def change_file_permissions(file_name, mode):
+        os.chmod(file_name, mode)
