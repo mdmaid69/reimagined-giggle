11408f1650dfead46fafd36d6210181a1cd0891d
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:22:19 +0000

    under piece use limit now

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def find_max(lst):
+        return max(lst)
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
