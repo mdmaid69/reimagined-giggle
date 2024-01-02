5aa15a95006eac7bc78d4f697bf2e0606cee75bc
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:44:39 +0000

    by priority sweet sad 565end

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
