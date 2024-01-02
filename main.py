41497e7519f2f578aef7a1494eb8950998bf085a
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:52:39 +0000

    thanks intelligent bread rather expect

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
-import os
-def get_current_working_directory():
-        return os.getcwd()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
