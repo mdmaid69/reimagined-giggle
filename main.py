3bbfa7f1aba76fe49a3ba2f99a1d3710061a11fa
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:30:55 +0000

    bang drink system bright arrange

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import glob
-def find_files(pattern):
-        return glob.glob(pattern)
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
