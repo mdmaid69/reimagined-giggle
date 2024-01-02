db2a27de90d772428782be34e02cd70ee0ed0b69
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:40:07 +0000

    reasonable survive upset complaint balance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
+text = "Hello, world!"
+print("Words:", len(text.split()))
