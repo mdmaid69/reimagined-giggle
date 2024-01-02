89e0a3c99340e43e5c04c70c35651e13cd30f1cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:08:27 +0000

    improve authority preparation accordingto sport

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
   import os
-  def get_current_working_directory():
+  def get_current_directory():
         return os.getcwd()
