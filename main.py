c4880ef7d975d3ca00fc78a94cdc1a13ac137b38
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:20:49 +0000

    style compare smooth understand desperate

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def reverse_list(lst):
-        return lst[::-1]
-def calculate_average(lst):
-        return sum(lst) / len(lst)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  def convert_to_hex(n):
+        return hex(n)
