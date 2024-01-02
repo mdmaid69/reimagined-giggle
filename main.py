d5453a99afc03b8c510683bd5097fbdf94a0a553
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:32:35 +0000

    dinner develop return steam contact

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import tensorflow as tf
-print(tf.__version__)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_as_set(array):
+        return set(array)
