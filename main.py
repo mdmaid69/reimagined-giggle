2b89a35a89079f916cbce5f48545151ad7917f81
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:57 +0000

    wing fortunately early breakfast bother

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
-  def square_number(x):
-        return x**2
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+def remove_duplicates(lst):
+        return list(set(lst))
+import json
+print(json.dumps({"name": "John", "age": 30}))
