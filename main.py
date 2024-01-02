04ee827c92e6807211da99d49e73ab2728d8608e
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:46 +0000

    behave whisper pollution balance easy

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def is_odd(n):
-        return n % 2 != 0
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
+import heapq
+def pop_from_heap(heap):
+        return heapq.heappop(heap)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
