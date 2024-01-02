7efdb2243ee23edc654710a88d0d38b08e048b79
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:22:49 +0000

    take fair drive below possibly

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
