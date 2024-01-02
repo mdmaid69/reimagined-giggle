83091336864f6f728015f1e0d8822fa856e0f27b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:08:53 +0000

    round tend healthy thinkthrough rely

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
+def convert_to_octal(n):
+        return oct(n)
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
