5f23967534922dbc3a122b4f95a17e6e6699c791
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:30:19 +0000

    culture young whatever recovery while

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-import math
-def calculate_neper_number_to_power_x(x):
-        return math.exp(x)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
