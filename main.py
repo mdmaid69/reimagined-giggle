3296ba78a753538f0b6bbb06f2b01bd206502206
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:08 +0000

    colour mouth rain comfort scrap

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def sort_numbers(numbers):
-        return sorted(numbers)
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
