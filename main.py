b4850e3354d9913226b7494cad9427fbc1be6db2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:25:58 +0000

    could cook wind talkback especially

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import sys
+print(sys.version)
