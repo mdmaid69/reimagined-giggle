f9cd249d273248201904fd0d02898975b8e9847c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:11 +0000

    unlikely spare thanks extra environment

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
-def find_max(lst):
-        return max(lst)
+import math
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
