7d8fa73195cb56a7838dec95fe5c31f2ea8fc57d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:41:53 +0000

    for calm pronounce runout proper

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_mean(arr):
-        return np.mean(arr)
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
