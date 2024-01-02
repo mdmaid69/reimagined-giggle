09afdad66dca9fd94f2b9bfd378b55e913e16836
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:36:32 +0000

    however develop study Nn wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def find_max(lst):
+        return max(lst)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
