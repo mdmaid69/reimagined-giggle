9728076c99fbcbfce6e2ca5871b5f9e352d4f552
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:45:00 +0000

    talk write steer yours apologizeapologise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import platform
+def get_os_info():
+        return platform.uname()
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
