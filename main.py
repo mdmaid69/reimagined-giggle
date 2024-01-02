a82ef373c9cc6603eb43c970cec0f119188f6dcc
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:13:25 +0000

    ignore something neat perfect state

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-def square_number(x):
-        return x**2
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def multiply_numbers(x, y):
+        return x * y
