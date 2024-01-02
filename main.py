327ab94cda49b39ca33bc356ce426f4ce2ba9f35
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:25:48 +0000

    cheek more catch treat move

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-text = "Hello, world!"
-print("Is palindrome:", text == text[::-1])
-def calculate_area_rectangle(l, w):
-        return l * w
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import sys
+def exit_program():
+        sys.exit()
