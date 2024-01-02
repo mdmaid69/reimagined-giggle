eb0a0c536fda303ca4e3aaca31c81e2d71a9f2ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:23:45 +0000

    deep us lay public except

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def convert_to_binary(n):
-        return bin(n)
+def multiply_numbers(x, y):
+        return x * y
+import math
+def calculate_error_function(x):
+        return math.erf(x)
