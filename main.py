cd6ef343480b7f01a51d673389e7220409873f82
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:22:25 +0000

    pour grade artificial deliberate sea

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
-for i in range(5):
-        print(i)
+def get_bytes_from_array(array):
+        return array.tobytes()
