91e0f40ac82203ae619abc07529b162f3aa95462
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:07:07 +0000

    step door pair border mine

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_volume(length, width, height):
-        return length * width * height
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+import collections
+def count_elements(iterable):
+        return collections.Counter(iterable)
