0f56ce2194e7d5a78b6fa70c4cc47bbdbde56113
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:25 +0000

    keepaway sky alive word like

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+import time
+def get_current_time():
+        return time.time()
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
