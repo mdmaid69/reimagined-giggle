30c89fcf74500701c6f188c8dba1d4f0ebabd6b4
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:32:54 +0000

    somehow love area pleased trouble

--- main.py
+++ main.py
@@ -1,13 +1,5 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
+import array
+def pop_from_array(array, i=-1):
+        return array.pop(i)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
