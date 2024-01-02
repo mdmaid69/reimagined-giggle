afaa687a9f6318040d3887de0f266f5959c374ff
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:17:00 +0000

    afterwards switch comeback suppose object

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-print(sum(range(10)))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
