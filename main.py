27878fcac713079fdb82b42b5259792c9f757e62
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:40:57 +0000

    polite start land share full

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def cube_number(x):
-        return x**3
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import array
+def clear_array(array):
+        array *= 0
