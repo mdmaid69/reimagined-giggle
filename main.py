3a349b1694f1c4c9b6bf8ae68571a0fcd14aa07b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:41:15 +0000

    job morning there Ww hard

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
+import sys
+print(sys.version)
