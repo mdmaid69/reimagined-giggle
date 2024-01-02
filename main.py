d615462d0f60e21a23bb83268542219e9e50c7da
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:35:16 +0000

    mark determined persuade difference difference

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_speed(distance, time):
+        return distance / time
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
