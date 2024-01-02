544cb4d3c2fe76af4636378152be8ae24f0ec326
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:12:23 +0000

    if pot dark dear air

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import random
-def roll_die():
-        return random.randint(1, 6)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
