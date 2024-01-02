ef4acddb50f581b8c871c34bc5844b194a531e1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:11 +0000

    corner excuse government tragedy mixture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def is_odd(n):
-        return n % 2 != 0
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
+def calculate_eps(net_income, shares_outstanding):
+        return net_income / shares_outstanding
+import math
+def calculate_least_common_multiple(a, b):
+        return abs(a*b) // math.gcd(a, b)
