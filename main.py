3f9da54a301c291efe9a8f856aaca681fcdb9bc6
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:13:19 +0000

    quantity conversation exciting suggest crime

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def insert_into_array(array, i, item):
-        array.insert(i, item)
-import collections
-def create_stack():
-        return collections.deque()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
