8aabd6f5106500b8feadb5269d4688389bef4a91
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:25:21 +0000

    discussion charge across sad model

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+import bisect
+def binary_search(sorted_list, item):
+        i = bisect.bisect_left(sorted_list, item)
+        if i != len(sorted_list) and sorted_list[i] == item:
+        return i
+        else:
+        return -1
