0bfff954562ba39b77af13c43fccb42779f0ad6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:43:13 +0000

    mix street abroad pretty patience

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
+  def get_file_atime_ns(file_name):
+        return os.stat(file_name).st_atime_ns
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
