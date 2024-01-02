8a0fd364dbb4b7e0c666a4d03256e43b9f466bf8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:47:00 +0000

    quickly sick hang if corner

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import array
+def get_array_item_count(array, item):
+        return array.count(item)
