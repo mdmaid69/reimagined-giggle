aa0557ec9ff570d14f7d92decc86d92e9e67cbe4
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:05 +0000

    father shoot floor private disturbing

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-text = "Hello, world!"
-print("Characters:", len(text))
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
