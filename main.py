2e7d8ebbe49905a6514aa92135ee01f14d3eb5f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:56:01 +0000

    government level balance exit plenty

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
+import array
+def get_array_item(array, i):
+        return array[i]
