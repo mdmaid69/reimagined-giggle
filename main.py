7bbc995cb12ec3dbff8accbe652f1489ea2d6986
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:56:40 +0000

    fix complete 566end holdout central

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import os
+print(os.getcwd())
