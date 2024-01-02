118760464ccbbe1f199acdfd9af948ee4e0268e8
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:07:50 +0000

    cap badly mix creature mark

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
