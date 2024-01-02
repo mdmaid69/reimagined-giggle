9a8bea98549115955fdaafb04d4c3d9df41d5b03
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:52:20 +0000

    top month turnoff now below

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def join_paths(path1, path2):
-        return os.path.join(path1, path2)
+import sys
+def exit_program():
+        sys.exit()
+import time
+def get_time_since_epoch():
+        return time.time()
