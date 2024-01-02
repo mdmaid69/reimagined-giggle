eb74293554e3d299892dc828cb5376d5c0966ee4
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:43:32 +0000

    slip suggestion contact regularly tone

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
