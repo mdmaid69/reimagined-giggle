7fe843f1709f7230b73ea5bb1f280ebf8125f7d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:24:52 +0000

    happy recommend forward fault quiet

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-for i in range(5):
-        print(i)
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
