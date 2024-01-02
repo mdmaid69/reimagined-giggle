ca4d310785d9729ca65b3be70ee856988dec1397
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:29 +0000

    takeback below himself upset expensive

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
