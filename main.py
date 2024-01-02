07e9d20b000e6eb8109518c69ee120730a00b996
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:06:08 +0000

    impressed nervous our friend pressure

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import os
+def change_working_directory(path):
+        os.chdir(path)
