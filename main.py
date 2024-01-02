032839a02f6827db6f23a2e634009d838a0090f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:26:52 +0000

    split forward one meeting interview

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-print("Hello, world!")
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
