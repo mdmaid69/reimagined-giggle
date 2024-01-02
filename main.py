be56f8583345000d344a6e4afe8ea1906889b1b8
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:06 +0000

    promise goback(to) rely flow surprising

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+  def square_number(x):
+        return x**2
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
