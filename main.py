35502120f93081d6c64b1b958033c0b1afad2ec6
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:27:42 +0000

    how if surface rob question

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-import array
-def get_array_as_int(array):
-        return int(array[0])
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
