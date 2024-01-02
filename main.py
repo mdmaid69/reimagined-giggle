a6cee056186d84b5c05f2a399423db0977fd8b9e
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:59:06 +0000

    sentence exist regret kick sail

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-def calculate_future_value(principal, rate, time):
-        return principal * (1 + rate)**time
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
