71d7e67ef61133196dca58d2d82456de1a194c0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:01:30 +0000

    risk fit bend worse trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
 import array
 def remove_from_array(array, item):
         array.remove(item)
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
