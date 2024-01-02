66527ebd0a3759c7a40584559073eb7c355cbca6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:51:06 +0000

    near lost belong nonsense temper

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def print_python_version():
+        print(sys.version)
   import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-import array
-def get_list_from_array(array):
-        return array.tolist()
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
