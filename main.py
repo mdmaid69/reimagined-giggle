9918e0be656f15658111c03dee93e7a31a70cd9f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:57 +0000

    fit justice string free shallow

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-def calculate_density(mass, volume):
-        return mass / volume
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def greet(name):
+        print(f"Hello, {name}!")
