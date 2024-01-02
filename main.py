0cd9ed91fe19f1a7afd3e39bd172ba661e67d4e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:23:55 +0000

    each drink pain cruel breakinto

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
