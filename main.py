dafe39b5d3d204ebb893b6063bce27b22cc8d8f7
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:38:15 +0000

    recent tight take thinkthrough anything

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import array
-def get_list_from_array(array):
-        return array.tolist()
+def iterate_over_array(array):
+        for item in array:
+        print(item)
+import array
+def get_array_as_repr(array):
+        return repr(array)
