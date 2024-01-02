408df11890716a2b0bd9cc162c3f2fb906565b8b
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:23 +0000

    taste hurry talk for grand

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
+import time
+print(time.time())
