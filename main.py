c384aa68fbf3dbd3b815622822be9383dd765dc8
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:07:54 +0000

    offer tap dance bringback walk

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import math
-def calculate_sign(x):
-        return math.copysign(1, x)
-for i in range(10): print(i)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+import itertools
+def get_permutations(iterable):
+        return list(itertools.permutations(iterable))
