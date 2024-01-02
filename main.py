205f67c73a17fbf705e6d9c3928900f51f98bad6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:32 +0000

    characteristic grand kind knock instant

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
-import collections
-def create_chain_map(*maps):
-        return collections.ChainMap(*maps)
+name = "Python"
+print("Hello,", name)
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
