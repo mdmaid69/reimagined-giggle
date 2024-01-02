f0d6b22e1cc67c6b1e8284376bb0184cab6615d9
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:36:14 +0000

    accidental might control provide greeting

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_as_repr(array):
-        return repr(array)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import itertools
+def flatten(iterable):
+        return list(itertools.chain.from_iterable(iterable))
