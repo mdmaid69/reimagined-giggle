127d6a448b20300a3390e45ef04f14a3a552a876
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:09 +0000

    pair succeed quantity minimum calmly

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_list(list, typecode):
-        return array.array(typecode, list)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  import os
+  def get_base_name(path):
+        return os.path.basename(path)
+import random
+print(random.randint(0, 100))
