7d21ab7002ce8933fa6e569284c5f9e678b24e21
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:17:15 +0000

    block hungry advantage through allow

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import array
-def get_array_as_complex(array):
-        return complex(array[0])
-def reverse_list(lst):
-        return lst[::-1]
+def get_array_as_bytes(array):
+        return bytes(array)
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
