02836f889a8dca7727fb2a6ea126081d7752b4d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:48:15 +0000

    share ready sentence suffer make

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def exit_program():
-        sys.exit()
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
