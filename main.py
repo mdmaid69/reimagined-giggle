b5a00b3175f8ad2a33d3a38f1f462e1bfa692944
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:01:58 +0000

    him succeed right sink emergency

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+import itertools
+print(list(itertools.permutations([1, 2, 3])))
 import array
-def set_array_item(array, i, item):
-        array[i] = item
-import math
-def calculate_hypotenuse(a, b):
-        return math.sqrt(a**2 + b**2)
+def get_array_as_set(array):
+        return set(array)
