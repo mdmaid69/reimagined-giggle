dc551604c92cbb107ef602592b8dcaca45631394
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:48:35 +0000

    cheek bin introduction future spread

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
 import os
-def remove_directory(path):
-        os.rmdir(path)
+def get_current_working_directory():
+        return os.getcwd()
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
