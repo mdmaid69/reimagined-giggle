914319db0a298ad473d84efd877b4693149271da
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:36:12 +0000

    glass pen opponent finish this

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
