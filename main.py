479df6954e2058c8a485e8c608df8b259c3442af
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:24:24 +0000

    whistle follow tip personal right

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
   import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
-import random
-def roll_die():
-        return random.randint(1, 6)
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
