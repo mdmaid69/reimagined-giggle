def5eec26f17559f88e397495ce23c058b51d947
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:14:52 +0000

    negative downstairs use piece big

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import tempfile
+def create_temp_directory():
+        return tempfile.TemporaryDirectory()
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
