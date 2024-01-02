bc83b1784b08e7ba0bd4d0cc082813a3531bc567
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:23:21 +0000

    wall getaway excuse act takeback

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
+import os
+print(os.getcwd())
 import array
-def get_array_typecode(array):
-        return array.typecode
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
