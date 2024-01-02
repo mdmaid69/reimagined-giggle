81dcea6c028958a5515e44470e822f0d828a3eee
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:08:43 +0000

    funny mouth feel tax complaint

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def convert_to_octal(n):
-        return oct(n)
-import array
-def get_array_as_set(array):
-        return set(array)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
