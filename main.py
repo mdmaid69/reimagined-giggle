a2e29e424ad569a643ce463f79f94a683018ee84
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:56:42 +0000

    steady pleased dance tie argument

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-from collections import Counter
-print(Counter("hello world"))
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
+def count_words(sentence):
+        return len(sentence.split())
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
