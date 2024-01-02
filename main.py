f696a96e19aaf7e3f5d8b1e809005e28aa2fae76
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:45:20 +0000

    plant bottom theirs after mind

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
   import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
   def get_file_ino(file_name):
         return os.stat(file_name).st_ino
-i = 0
-while i < 5:
-        print(i)
-        i += 1
