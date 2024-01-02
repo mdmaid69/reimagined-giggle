6a48a4c06acd39e1433e5c07bb01b797b6210e9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:49:42 +0000

    field manage tired high hospital

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
