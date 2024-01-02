d5bcb11b254d48f17c54a8fd236e07bf394ddb4d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:51:44 +0000

    upwards Uu unit understand destroy

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
+def calculate_perimeter_rectangle(l, w):
+        return 2 * (l + w)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
