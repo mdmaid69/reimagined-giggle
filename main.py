f62b231b5c45ddf59988a0b60ce0985702fa05a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:31:05 +0000

    maintain mine glass control fine

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import re
+print(re.match("h.*o", "hello world"))
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
