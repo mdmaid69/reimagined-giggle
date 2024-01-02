1cb97808e26dd2ad55bd0de123b91d14f49aa77b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:11:04 +0000

    contain breath up then spread

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
-  def add_numbers(x, y):
-        return x + y
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+def multiply_numbers(x, y):
+        return x * y
