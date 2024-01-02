ee3d81146f309fa178c4617e886bc88eaf6f9f5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:10:43 +0000

    lower switch stamp sing on

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  import matplotlib.pyplot as plt
-  def plot_scatter_graph(x, y):
-        plt.scatter(x, y)
-        plt.show()
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+  import os
+  def get_file_owner(file_name):
+        return os.stat(file_name).st_uid
