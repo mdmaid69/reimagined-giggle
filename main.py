8b44a200e9b2ce3e8df3124470cb714b89336f48
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:34:02 +0000

    tight contents Tt lead safe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_pressure(force, area):
-        return force / area
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
