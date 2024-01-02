970d99ac8537cc362090b8e23bb0b58437282cb1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:20:26 +0000

    city step quiet noise practice

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
+def calculate_acceleration(speed, time):
+        return speed / time
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
