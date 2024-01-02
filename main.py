d912a5817a68618115c2e6ba01031cfd4842d976
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:12:58 +0000

    shoulder young common pattern stage

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
+import platform
+def get_python_version():
+        return platform.python_version()
