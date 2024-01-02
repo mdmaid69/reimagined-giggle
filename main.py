d7a2f05a53773e8f8f27049ce7445b08c1823649
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:42:26 +0000

    club mouth circumstance coat temperature

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
-def calculate_force(mass, acceleration):
-        return mass * acceleration
+n = 10
+print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
