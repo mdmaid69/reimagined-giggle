cc8327d210ba58cbf2e519e07acfbcf6f8e576e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:32:20 +0000

    work change Gg thumb cause

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
 import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
+print(re.match("h.*o", "hello world"))
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
