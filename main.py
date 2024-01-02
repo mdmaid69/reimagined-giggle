ad7d4662922c55ce1833e48dcf783d7e1f064638
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:59:04 +0000

    visit no wave hurry tight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import collections
-def create_counter():
-        return collections.Counter()
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
