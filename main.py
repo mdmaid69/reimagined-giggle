1b76b92b0ad017e8a3de167dd4a2f31501166e86
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:53:35 +0000

    war interested introduction note standard

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
 import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
+def calculate_tangent(x):
+        return math.tan(x)
+import array
+def get_array_as_bytearray(array):
+        return bytearray(array)
