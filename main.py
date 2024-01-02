443bcf0a18d7db7bfbab1071d0506bfb8dabe993
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:06 +0000

    light convinced away question luck

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
+import time
+def get_current_time():
+        return time.time()
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
