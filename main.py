6fbd9c88ca33ded9a85e936a07d3ffb8c6115f27
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:36:07 +0000

    cost Dd next mess part

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-def divide_numbers(x, y):
-        return x / y
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
