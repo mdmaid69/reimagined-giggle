8904d0b1b85ce9972c964b9db454710744becad2
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:53:58 +0000

    question Ss sink sting excellent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import subprocess
-def run_command(cmd):
-        return subprocess.check_output(cmd, shell=True)
+import re
+def split_by_pattern(pattern, string):
+        return re.split(pattern, string)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
