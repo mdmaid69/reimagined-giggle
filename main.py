c688b0e6f84a4990f803ddac98ca7320eabb8b5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:06:19 +0000

    maybe seat smoke Uu last

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  import os
-  def get_file_inode(file_name):
-        return os.stat(file_name).st_ino
-for i in range(10): print(i)
+import datetime
+def get_current_date():
+        return datetime.date.today()
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
