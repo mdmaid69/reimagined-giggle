2e24017f82220a9af72a7ac7761a7bc79a5b12d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:57:06 +0000

    mood combination goback(to) add difficult

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-  def reverse_list(lst):
-        return lst[::-1]
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
