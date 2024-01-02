e9e6f9361a130e0b93928bc79ff1ba1f3d41094f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:06 +0000

    office twist pause nigga joke

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-  import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
+  def reverse_list(lst):
+        return lst[::-1]
+import math
+def calculate_cartesian_to_polar_coordinates(x, y):
+        return math.rect(x, y)
