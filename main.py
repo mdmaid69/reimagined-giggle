b5c92a2c0bed70140b03e3d800ba2e585d0cc7e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:47:52 +0000

    workout couple wheel paper rush

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import smtplib
-def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
-        with smtplib.SMTP(smtp_server, port) as server:
-        server.login(username, password)
-        server.sendmail(from_addr, to_addr, f"Subject: {subject}
-
-{body}")
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
