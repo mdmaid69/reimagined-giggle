480ef89f67124bdec049cf4cfa39fac525cf7d53
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:19:45 +0000

    broad know swing may cross

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def get_largest_elements(iterable, n):
-        return heapq.nlargest(n, iterable)
-def reverse_string(s):
-        return s[::-1]
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
