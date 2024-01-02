28412ab3ccec90ac60510185d186820ff53680ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:19 +0000

    bag pay wear most fold

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-def find_unique_words(sentence):
-        return set(sentence.split())
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_pressure(force, area):
+        return force / area
