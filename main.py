f146059fe2520157691bcd9010f810ca46997f1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:25:06 +0000

    completely excited behind slightly comeover

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Common elements:", set(list1) & set(list2))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
