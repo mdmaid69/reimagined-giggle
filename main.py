fee3fe27116643837c270143adcba371de1cfa7d
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:41:01 +0000

    principle copy mess my scream

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
+  def find_max(lst):
+        return max(lst) if len(lst) != 0 else "List is empty"
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
