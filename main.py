4990d23f5df7566b8548f26f811b686e6eee7980
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:55:59 +0000

    activity central sure choice confuse

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+def calculate_acceleration(speed, time):
+        return speed / time
+import array
+def set_array_item(array, i, item):
+        array[i] = item
