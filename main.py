fac80e35bfa17193f7701358d2f6addfa5137e20
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:12:06 +0000

    before store preparation convincing lookaround

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
