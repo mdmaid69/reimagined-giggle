4eca4be48404a5590740620385dbf235be95f9ad
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:16:23 +0000

    effect tour happen solve low

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-import bisect
-def binary_search(sorted_list, item):
-        i = bisect.bisect_left(sorted_list, item)
-        if i != len(sorted_list) and sorted_list[i] == item:
-        return i
-        else:
-        return -1
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import math
+def calculate_root(x, n):
+        return math.pow(x, 1/n)
