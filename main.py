ee015cdf46be3e33c43e8c13e19a86a76fb01fd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:37 +0000

    home mainly pure offer combination

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def set_array_item(array, i, item):
+        array[i] = item
