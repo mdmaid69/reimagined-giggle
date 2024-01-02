c207f79e9af328be7bbc96b8bcd0a95cea99eb0f
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:58:30 +0000

    wet noise dear direct receive

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-  def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
