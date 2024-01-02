aa7267fde001c61fddb23b0730e8d1f4da9cc413
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:19:45 +0000

    entrance reliable march cause definite

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+print("Hello, world!")
