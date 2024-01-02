2c5847d73a5ac6621df9afd628f021e648521c1b
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:45:38 +0000

    realize university snow fair tight

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
+def find_max(lst):
+        return max(lst)
