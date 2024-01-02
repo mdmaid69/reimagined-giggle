144a50bc09b7f051352edd8a93adb3e519326f37
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:55:53 +0000

    attempt practice one continue scale

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
-import array
-def get_array_as_frozenset(array):
-        return frozenset(array)
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
