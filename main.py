48b8c0d7009fab68fa546a4dc7cd27dfb721399f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:31:44 +0000

    exaggerate decide type benefit natural

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+  import datetime
+  def get_current_date():
+        return datetime.datetime.now().date()
 import array
-def check_if_array_contains_item(array, item):
-        return item in array
+def get_array_as_bytearray(array):
+        return bytearray(array)
