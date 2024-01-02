c7523f49fa0d62778e4e3d17a24239a0d77d58fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:38:59 +0000

    branch rest contrast switch travel

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-  def add_numbers(x, y):
-        return x + y
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
