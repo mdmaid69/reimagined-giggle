ac466a88784bc2cc702d972a0a8a30f2a0451850
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:01:26 +0000

    paper work artificial central project

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-import sys
-def print_python_version():
-        return sys.version
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
