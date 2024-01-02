d9ae2ceffcd7d66459bceb4e5149f0fd5a050b49
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:25:18 +0000

    cruel custom here impatient bath

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
-import functools
-print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
