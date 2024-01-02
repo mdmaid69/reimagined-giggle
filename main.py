f5ce29c23f70c19c241f99592ae710add9ba0bce
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:53:37 +0000

    connect be so godown everything

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_work(force, distance):
-        return force * distance
-import math
-def calculate_product_of_sequence(start, stop, step):
-        return math.prod(range(start, stop, step))
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
