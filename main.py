93d1f1b886441cc057005d183a2cdf8c82f94727
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:13:20 +0000

    fix people confused Tt artificial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
