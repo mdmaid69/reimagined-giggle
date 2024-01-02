9bc0125af5785eb1f49fa176c84593e2d9f6cf03
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:39:16 +0000

    waste shake unit meet track

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_base_name(path):
+        return os.path.basename(path)
+import array
+def append_to_array(array, item):
+        array.append(item)
