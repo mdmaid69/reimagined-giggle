9a66f86acb9e7cc9fdace6715f6a5021148c113b
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:09:49 +0000

    content date breakup remember play

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-def calculate_energy(mass, c=3*10**8):
-        return mass * c**2
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+import math
+def calculate_cosine(x):
+        return math.cos(x)
