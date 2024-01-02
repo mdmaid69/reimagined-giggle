05512e9b134bfafe6b49634f71a883b71e0f12a5
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:52:25 +0000

    bringdown address themselves determined arrive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import os
+def remove_directory(path):
+        os.rmdir(path)
