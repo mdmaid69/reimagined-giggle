1173e40c61e70ea4e8476ef02ef41b01b4a197d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:05:54 +0000

    telephone bend visit almost hurry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
+import array
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
