0dce6919a29cd12e58649d29ae779d87257732b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:46:11 +0000

    calm man scare equal false

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import random
-def flip_coin():
-        return "Heads" if random.random() < 0.5 else "Tails"
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+import math
+def calculate_radians_to_degrees(radians):
+        return math.degrees(radians)
