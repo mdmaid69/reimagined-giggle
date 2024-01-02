5ed0ddec2af975cbcb702c303803fb6480e58baf
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:48:11 +0000

    heat emotion tune pollution family

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-import array
-def get_list_from_array(array):
-        return array.tolist()
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
