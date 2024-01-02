130144c890c2d987456e9c11e8ac7e5b70d24d02
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:38:40 +0000

    wish nervous change discover turnaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
