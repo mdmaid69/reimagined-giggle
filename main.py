ae3aed0f2a882becfa1e51f583ddabe6abc528e7
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:56:39 +0000

    after born rather comein quickly

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def subtract_numbers(x, y):
-        return x - y
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
   import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_directory_name(path):
+        return os.path.dirname(path)
