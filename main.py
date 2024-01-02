fd014f84a6a5afe0881b707557d47e157ee3303a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:23:01 +0000

    afford live pleasure discussion impression

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
+  def remove_duplicates(lst):
+        return list(set(lst))
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
