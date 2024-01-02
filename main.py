763832565ca3930e997c47d931a35a99f1bf2420
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:20:08 +0000

    purpose something ask right statement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
