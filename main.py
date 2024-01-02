dd47104810647b1792613b495e56cbe6b1724de6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:23:28 +0000

    swallow fly age moral them

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
+def calculate_power(work, time):
+        return work / time
