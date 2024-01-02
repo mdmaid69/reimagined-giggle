f752f9cd50b55e8d8fd5e28ecf3358e4b3c09002
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:05:38 +0000

    age run cheek content right

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_area_rectangle(l, w):
+        return l * w
