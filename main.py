8ecb3242c35c1210d91eb646b9698d0a4fa770a6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:33:12 +0000

    autumn necessary arrangement deaf keepaway

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
 import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+def get_array_as_memoryview(array):
+        return memoryview(array)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
