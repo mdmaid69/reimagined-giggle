885b876627fd3ce2bc6dd0744d7421d2edb6b539
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:30:25 +0000

    worth thinkof store settle similar

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def remove_directory(path):
-        os.rmdir(path)
-import math
-def calculate_absolute_value(x):
-        return math.fabs(x)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import argparse
+def parse_arguments():
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--input")
+        parser.add_argument("--output")
+        return parser.parse_args()
