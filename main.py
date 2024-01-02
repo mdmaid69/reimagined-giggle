712fa8e3520a3daa73751556d5bde22a1a2d3c6e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:33:52 +0000

    modern absolutely impressive favour male

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
