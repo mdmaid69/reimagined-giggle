fbfa1a1b690ef01c23e676e7841565ab7c91ab15
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:24:54 +0000

    sad house willing price culture

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-  import re
-  def find_all_occurrences(pattern, string):
-        return re.findall(pattern, string)
+import array
+def get_array_buffer_info(array):
+        return array.buffer_info()
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
