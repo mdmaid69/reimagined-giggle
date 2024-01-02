17b7310cb004e672ee07f6b30f9c33dc55bd42fc
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:45:31 +0000

    bury whoever glad finger minority

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_list(array):
-        return list(array)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import math
+def calculate_polar_to_cartesian_coordinates(r, theta):
+        return math.polar((r, theta))
