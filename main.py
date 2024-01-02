28383fd94dd8d9c513eeb02a62db551d05182a13
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:37:53 +0000

    concern cool life raise murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_average(lst):
+        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
