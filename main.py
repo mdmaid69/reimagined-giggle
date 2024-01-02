66129c1ba7483985facd268d2209c0be7c5465cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:22:10 +0000

    pair divide joke professional rich

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
 import array
-def convert_array_to_list(array):
-        return array.tolist()
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
