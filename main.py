a86af971c5bc21ba696fcc806cca8d6c9a0349d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:33:31 +0000

    birth believe wear matter Ff

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
