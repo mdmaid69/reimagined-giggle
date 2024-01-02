5917e6fc6b9f700efa3522f7e45593a4a04e708a
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:56:24 +0000

    absolutely forward sorry responsibility university

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def get_array_as_str(array):
+        return str(array)
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
