f4879e21effe9a7e24894c97c9808a1e15fdd3ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:31:41 +0000

    invite develop depend teach confused

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def count_characters(sentence):
+        return len(sentence)
