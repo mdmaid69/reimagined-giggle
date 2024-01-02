205e5650d77d867e84d27008c67a82b6e32fc607
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:58:30 +0000

    worst entrance unfair steady foot

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import matplotlib.pyplot as plt
-plt.plot([1, 2, 3], [4, 5, 6])
-plt.show()
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+  def convert_to_octal(n):
+        return oct(n)
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
