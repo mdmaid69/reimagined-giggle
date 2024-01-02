8b93b8f04622c13cf09503668c089cefcef2b133
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:26:32 +0000

    start beneath mouth surface improve

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
