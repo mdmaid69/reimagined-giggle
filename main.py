befb7b721c4f5cab573fe27145aef42b5b5e82b1
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:33 +0000

    read manner company settle opportunity

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
