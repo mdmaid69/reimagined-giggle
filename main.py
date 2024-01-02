b1e2c5c5e046bd12eb54b15e11294837fc1301e9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:20:08 +0000

    lookafter imagine couple public teach

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_complementary_error_function(x):
-        return math.erfc(x)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+import array
+def get_array_slice(array, i, j):
+        return array[i:j]
