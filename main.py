875cf3c7f13543c78a087310930375f624320316
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:57:38 +0000

    under character play front cook

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+  def reverse_list(lst):
+        return lst[::-1]
   import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
