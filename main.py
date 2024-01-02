832aed1600f02c0036bab89d426459deed463ff9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:55:58 +0000

    bush rather bet paper return

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
   import os
   def get_file_dev(file_name):
         return os.stat(file_name).st_dev
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
