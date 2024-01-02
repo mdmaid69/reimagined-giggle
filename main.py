6f8d7e1bbc56396b552531696a32c9a23fc10807
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:36:49 +0000

    pleasant perform talkoutof hard send

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
