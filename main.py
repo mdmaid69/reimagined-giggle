c3e953a49e0a8a56ec97b61f34c6a399263767cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:31:53 +0000

    connected occasional suckme freedom evidence

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
+print("Hello, world!")
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
