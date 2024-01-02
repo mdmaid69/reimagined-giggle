8f2fb6485198c5c64a2b44dfae4268c37da10722
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:57:46 +0000

    runinto callup concerned inquire increase

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+import math
+def calculate_euclidean_norm(v):
+        return math.hypot(*v)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
