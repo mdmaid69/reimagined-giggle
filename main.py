43c559de606dcc1e99092a9ad73b1218b8b8454f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:18:56 +0000

    thing belong cost hit cold

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
