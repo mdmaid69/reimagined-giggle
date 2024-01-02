b8feb5d728d1401f919056a70119eff2f90c9387
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:39:08 +0000

    warn godown company cause warning

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-i = 0
-while i < 5:
-        print(i)
-        i += 1
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
