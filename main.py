bc788ac0b383fc0db32ed4e0e72ec176b785d02a
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:39:31 +0000

    middle and connection argument bus

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
