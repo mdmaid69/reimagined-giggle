c47cba062e4f154f7dc1999aaacddf22ba48be5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:41 +0000

    classroom supply connection cost relief

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import array
-def get_array_item_count(array, item):
-        return array.count(item)
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+n = 10
+print("Cube numbers:", [x**3 for x in range(n)])
