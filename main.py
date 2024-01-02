1d9a1a0287467f751dcb9fa74e7d51f0e77c1997
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:06:17 +0000

    Kk more return in trousers

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_string_to_array(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
-  import sys
-  def get_python_version():
-        return sys.version
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
