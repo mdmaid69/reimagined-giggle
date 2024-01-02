73b568c1cbc4ef3a756c601c035c35613ea1101a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:03:21 +0000

    thinkover swimming eye mention remain

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_parent_directory(dir_name):
-        return os.path.dirname(dir_name)
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
+import array
+def iterate_over_array(array):
+        for item in array:
+        print(item)
