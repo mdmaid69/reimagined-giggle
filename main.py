2102dec6eafa0c9623bc8ec33b973877a19c81d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:34:29 +0000

    excuse movement snow limit splash

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import json
+def convert_to_json(data):
+        return json.dumps(data)
