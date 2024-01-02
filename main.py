f4e40382d696eeaee8f7179da540e1821686bd3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:12:39 +0000

    wish future power comein private

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
 import math
 def calculate_hypotenuse(a, b):
         return math.sqrt(a**2 + b**2)
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
