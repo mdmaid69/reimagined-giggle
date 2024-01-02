aca206202f09cec56536d0089ab9428c47d0fb2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:38:54 +0000

    future bury this flight coat

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def find_min(lst):
-        return min(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+numbers = [1, 2, 3, 4, 5]
+print("Even:", [n for n in numbers if n % 2 == 0])
