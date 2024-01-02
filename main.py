b78632e0372bcba6793d7446b070077db2f0c0ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:49:35 +0000

    excuse fence quality beat Ii

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
+import array
+def get_array_as_list(array):
+        return list(array)
