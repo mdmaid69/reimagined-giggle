9bd5a948885519f4d39d3086343c0aac676f48a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:37:57 +0000

    pay concern passenger protest each

--- main.py
+++ main.py
@@ -1,9 +1,7 @@
-import argparse
-def parse_arguments():
-        parser = argparse.ArgumentParser()
-        parser.add_argument("--input")
-        parser.add_argument("--output")
-        return parser.parse_args()
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+import array
+def get_array_item(array, i):
+        return array[i]
+i = 0
+while i < 5:
+        print(i)
+        i += 1
