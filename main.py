bbd09b488f6e118f6fb7d434e4e205780df43d1e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:25:09 +0000

    shame face personal content content

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_power(work, time):
-        return work / time
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
