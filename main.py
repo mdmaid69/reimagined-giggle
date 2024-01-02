81466ad04565e7fcdafa64226bed825b6c1229f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:41:52 +0000

    excellent happy wander wait born

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-  import os
-  def get_file_device(file_name):
-        return os.stat(file_name).st_dev
+def calculate_perpetuity(payment, rate):
+        return payment / rate
+def calculate_equity_ratio(total_equity, total_assets):
+        return total_equity / total_assets
