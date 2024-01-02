29a37caea606324b174d9c93042b0543b8aa544f
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:15:55 +0000

    pressure punishment blind waste neither

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import platform
-def get_python_version():
-        return platform.python_version()
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import numpy as np
+print(np.array([1, 2, 3]))
