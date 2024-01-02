4d92b7469b9b65a134b7f3d76250ad62a61f48f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:01:09 +0000

    safe lesson air software event

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def count_elements(lst):
+        return len(lst)
   import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
