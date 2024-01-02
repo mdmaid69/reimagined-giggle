dc604a9e556262ced9eee3964f39d76bbf4266cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:16:39 +0000

    fight run extremely keepup low

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+import heapq
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
