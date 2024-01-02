2991e124fe39ab2b67543de5ca3255fe585f6fd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:57:44 +0000

    interrupt cut visit right birth

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-import collections
-def create_counter():
-        return collections.Counter()
-def find_max(lst):
-        return max(lst)
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
