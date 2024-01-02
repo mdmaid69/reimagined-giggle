ac1d9382cb443c6d9e83870884095bcde2fbe6c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:01:15 +0000

    understand standard official occur his

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_pressure(force, area):
-        return force / area
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
