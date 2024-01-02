482f388b91df9df7ebd6dd980d260224b4a34192
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:34:51 +0000

    block look better family down

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
-name = "Python"
-print("Hello,", name)
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
