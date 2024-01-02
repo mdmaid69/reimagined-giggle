60115056027ecfeb4a30e03a8d0a729fd1dcde7f
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:10:41 +0000

    suckme everything book habit method

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-def calculate_npv(rate, cash_flows):
-        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
+import time
+def get_current_time():
+        return time.ctime()
+import array
+def convert_array_to_string(array):
+        return array.tostring()
