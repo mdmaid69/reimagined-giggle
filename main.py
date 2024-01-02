c158298d5e4a46de7b3e5768b7a1fc193d0b7a55
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:46:44 +0000

    calloff tap comment aware become

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_standard_deviation(arr):
-        return np.std(arr)
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
