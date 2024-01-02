83c34d305dc808ef8bc5e380de26a5a4c36c13f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:21 +0000

    small tidy wall before wave

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-  import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+def calculate_average(numbers):
+        return sum(numbers) / len(numbers)
+def cube_number(x):
+        return x**3
