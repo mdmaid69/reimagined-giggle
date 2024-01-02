5715edd64617c1876fa0b740e9afdee03fb3ec46
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:57:48 +0000

    polite slip extra that finally

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_euclidean_norm(v):
-        return math.hypot(*v)
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+def is_even(n):
+        return n % 2 == 0
