8b28a26627dafe47485283b4a9725b40bad0e1e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:44:38 +0000

    hope downstairs last relative yourself

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
