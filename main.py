2b34955963c215e39f37fcbb846df9f4d3b37d2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:47:17 +0000

    limit job hollow authority forgive

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-  import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-import random
-def shuffle_list(my_list):
-        random.shuffle(my_list)
-        return my_list
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+def calculate_density(mass, volume):
+        return mass / volume
