7b10301965f8ff6f58e994887ae070bb0aec9224
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:31 +0000

    refuse problem gooff pupil shout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-          if n % i == 0:
-        return False
-        return True
+def convert_to_octal(n):
+        return oct(n)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
