7351396d4db7652f7cff509c61a2ae785df70660
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:36:48 +0000

    spread notice transport holiday congratulations

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
-  def get_current_directory():
-        return os.getcwd()
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
