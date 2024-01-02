82728864e8a9da86445f9f26d28678bec34be1ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:53:06 +0000

    thinkthrough steam who black content

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-import math
-def calculate_logarithm_base_10(x):
-        return math.log10(x)
+  import numpy as np
+  def calculate_median(arr):
+        return np.median(arr)
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
