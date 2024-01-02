fe63268c3fd6ab74619a50a10aacbe478e2f2296
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:07:52 +0000

    cause control limit production law

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-  import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
+import array
+def convert_unicode_to_array(unicode, typecode):
+        a = array.array(typecode)
+        a.fromunicode(unicode)
+        return a
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
