1c75d8f73280a5726efb0a65b9e2461c0a9a2d3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:18:11 +0000

    walk dress season once contain

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-  import os
-  def get_file_access_time(file_name):
-        return os.path.getatime(file_name)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
