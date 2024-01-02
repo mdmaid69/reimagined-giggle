568246f2184e135f9008ec13ccf5c53d3d675ed8
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:47:44 +0000

    main hand show guide very

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-import math
-def calculate_combinations(n, k):
-        return math.comb(n, k)
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
