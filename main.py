8004a6d9c9348c174c6e219d586313886b88f1ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:31:32 +0000

    state 572enter emphasis family hole

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
