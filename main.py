c031cd01c1086e49a8084b06155121ab0eb3692c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:31:28 +0000

    prevent damage form paper arm

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
