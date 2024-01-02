3ec253bc7b22ffc9bfff4a52fe810a8d4757fd17
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:46:17 +0000

    arrange description feed proper spring

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
-import sys
-print(sys.version)
+import platform
+def get_python_version():
+        return platform.python_version()
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
