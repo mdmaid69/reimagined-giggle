335ed2760f74b0a7a3f3af07367ec38b52216822
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:55:48 +0000

    guess chance she official fence

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-print(sum(range(10)))
+  def multiply_numbers(x, y):
+        return x * y
+def calculate_quick_ratio(current_assets, inventory, current_liabilities):
+        return (current_assets - inventory) / current_liabilities
