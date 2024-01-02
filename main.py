8ee6f40a2d5038ae7e67688d87bd30fbc2ce39cc
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:09:39 +0000

    reduce if on talk smart

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_density(mass, volume):
-        return mass / volume
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def split_path(path):
+        return os.path.split(path)
+numbers = [1, 2, 3, 4, 5]
+print("Squared:", [n**2 for n in numbers])
