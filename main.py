0bc10b6b8f1f7e99cb58cf7e1098d4cf51459894
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:03:01 +0000

    nerve comeout break now love

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import http.server
-def start_http_server(port):
-        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+  def reverse_list(lst):
+        return lst[::-1]
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
