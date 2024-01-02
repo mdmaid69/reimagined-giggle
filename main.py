45dce9969152a2a7ff6d2de787c25ab229bab32e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:06:53 +0000

    happen port hear confused head

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_max(numbers):
-        return max(numbers)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
   import os
   def get_file_qspare(file_name):
         return os.stat(file_name).st_qspare
