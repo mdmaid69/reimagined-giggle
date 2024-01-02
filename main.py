846539b245a28c7c48b11c6613adf0002f250a4e
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:37:21 +0000

    blame judge tell form container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import itertools
+def get_cartesian_product(*iterables):
+        return list(itertools.product(*iterables))
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import math
-def calculate_hyperbolic_cosine(x):
-        return math.cosh(x)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
