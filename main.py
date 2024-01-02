43ba19cc2c6f62111fbb4b448fb05b2b7584ad5d
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:21:55 +0000

    truth consult successful swimming wander

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
