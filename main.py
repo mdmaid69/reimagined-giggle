adcf19880e95ff58013bf80787800ccb73735c2a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:07:04 +0000

    label wire system war total

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+for i in range(10): print(i)
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
