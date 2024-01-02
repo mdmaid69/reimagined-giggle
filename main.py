966755adf2c35a79682a3a3d0c8285697aa9e4d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:31:10 +0000

    aim plan note Dd extra

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
