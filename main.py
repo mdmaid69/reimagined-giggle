690c2f8886e825d9b7b1c3586ba055c22f5530de
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:52:30 +0000

    thing bringdown callback people suppose

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import re
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-def find_frequency(sentence):
-        from collections import Counter
-        return Counter(sentence.split())
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
