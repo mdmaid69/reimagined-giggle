3f86134f0262ad2a9c4c1527b540ee5e5295cd5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:08:41 +0000

    risk everybody workon face flavour

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import multiprocessing
-def get_cpu_count():
-        return multiprocessing.cpu_count()
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
+import math
+def calculate_error_function(x):
+        return math.erf(x)
