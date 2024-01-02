461ec5bc11fc358402e32f2bfc598adc548a3668
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:33:35 +0000

    hope afraid yes hardly important

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_logarithm(base, x):
-        return math.log(x, base)
-def calculate_current_ratio(current_assets, current_liabilities):
-        return current_assets / current_liabilities
+def calculate_neper_number_to_power_x(x):
+        return math.exp(x)
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
