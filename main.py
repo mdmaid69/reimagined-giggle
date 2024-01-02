3541bc760856479f5ce0aba7c685e72c68711081
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:05:45 +0000

    hour fault clearly maximum lunch

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
-def calculate_pressure(force, area):
-        return force / area
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
