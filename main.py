4a8a1be6834acbe05363f26550fac54e6038d890
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:21:41 +0000

    take food release equal nor

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+import math
+def calculate_error_function(x):
+        return math.erf(x)
+def convert_to_hex(n):
+        return hex(n)
