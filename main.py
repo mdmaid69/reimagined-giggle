83a50e79bcb4700889613791a13851b80e9a47d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:32:51 +0000

    pronunciation attention complaint holdout couple

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def greet(name):
-        print(f"Hello, {name}!")
-name = "Python"
-print("Hello,", name)
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
