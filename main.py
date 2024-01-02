7387afaccd76c2f16bcfb36635bdf352ce5afd30
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:29:43 +0000

    ask air loose open agree

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
+name = "Python"
+print("Hello,", name)
+  import os
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
