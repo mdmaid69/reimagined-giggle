0d0d7f5c661bfd9f2a08b6149f591208770fd5cb
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:14:55 +0000

    field telephone home proud second

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+def calculate_force(mass, acceleration):
+        return mass * acceleration
