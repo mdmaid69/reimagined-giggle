4faac53136fa1f7d96a3431a3e9b06156895a39d
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:08:02 +0000

    set nurse prepare properly comment

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
-def calculate_return_on_equity(net_income, total_equity):
-        return net_income / total_equity
-import array
-def get_array_index(array, item):
-        return array.index(item)
+print("Hello, world!")
+def find_max(numbers):
+        return max(numbers)
