ef947f0b51f6cbe22d210e646920e613ce49f381
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:06:08 +0000

    of beat scream turnback sing

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def check_if_array_contains_item(array, item):
-        return item in array
-def add_numbers(x, y):
-        return x + y
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
