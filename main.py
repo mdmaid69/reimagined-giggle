b5fd1350aeb7d7a4e14ef389c3c93ec85f3e6b17
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:59:27 +0000

    quarter think various calm ready

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+import array
+def get_array_as_memoryview(array):
+        return memoryview(array)
