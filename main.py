66d2d06605a9a998fddb09369237d3702e989205
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:12:59 +0000

    sweep thirsty commit inside afraid

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_repr(array):
-        return repr(array)
-import collections
-def create_ordered_dict():
-        return collections.OrderedDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+import heapq
+def get_smallest_elements(iterable, n):
+        return heapq.nsmallest(n, iterable)
