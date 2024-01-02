8cfba0e19e2e5738aaed4b140ede731bd2d0c290
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:46:56 +0000

    dish lean hate smell mainly

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import math
+  def calculate_square_root(n):
+        return math.sqrt(n)
 import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+def push_to_heap(heap, item):
+        heapq.heappush(heap, item)
