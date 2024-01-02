300a16c76caffc3fcc9e017a166f510dbe20038b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:41:04 +0000

    trouble takeaway hope in push

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
