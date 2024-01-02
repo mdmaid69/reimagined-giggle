b5fa3f102c489c7a5ac786036edede3219cc8f09
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:10:52 +0000

    spread anyone spend off better

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
+def calculate_area(radius):
+        return 3.14 * radius * radius
