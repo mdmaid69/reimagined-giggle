079d64e2f416c5702245feb9fe61e8dca8ddeaba
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:31 +0000

    world closely arrangement strike shape

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def get_array_itemsize(array):
+        return array.itemsize
   import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
