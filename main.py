3f696b02d158921a0a4de02cbb6d4b25a735b78d
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:05 +0000

    plate fall load better unusual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-import math
-def calculate_floor(x):
-        return math.floor(x)
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import array
+def get_array_as_bool(array):
+        return bool(array)
