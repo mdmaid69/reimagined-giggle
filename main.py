a9555be44b5d2e0060e57b08ae99fb2f053f9602
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:10 +0000

    emphasis stop convinced protest mean

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import re
-def find_pattern(pattern, string):
-        return re.findall(pattern, string)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  def is_odd(n):
+        return n % 2 != 0
