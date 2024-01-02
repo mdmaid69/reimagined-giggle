51011733a89b67065d951b84d20b9a0516aaf1bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:52:20 +0000

    thumb come appearance Gg hardly

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import getpass
+def get_username():
+        return getpass.getuser()
+import heapq
+def pop_push_heap(heap, item):
+        return heapq.heapreplace(heap, item)
