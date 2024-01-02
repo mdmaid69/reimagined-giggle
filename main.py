d42119c3e787b2381b7dfc75c1c9fbe028a796f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:15:03 +0000

    heat race the cool please

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def square_number(x):
-        return x**2
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
