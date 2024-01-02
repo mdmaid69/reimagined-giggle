5c5c08e35e328fb5230c3dc7a7748a13f5846992
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:44:41 +0000

    careless star allow pull play

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import math
+def calculate_hyperbolic_tangent(x):
+        return math.tanh(x)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
