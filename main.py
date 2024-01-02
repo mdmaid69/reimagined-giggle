bf05012e9602c9d98b588706f2d1f6c5ace45fb0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:53:27 +0000

    burn butter excuse grow silent

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-print("Hello, world!")
-import math
-def calculate_euclidean_distance(p, q):
-        return math.dist(p, q)
+  import os
+  def get_directory_name(path):
+        return os.path.dirname(path)
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
