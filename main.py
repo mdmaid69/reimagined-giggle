d0b6729960760f94aec51565aa06a4254bde1292
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:17:46 +0000

    hang slice middle centimetre brush

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import numpy as np
-  def create_numpy_array(lst):
-        return np.array(lst)
-  import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
-        plt.show()
+import collections
+def create_user_dict():
+        return collections.UserDict()
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
