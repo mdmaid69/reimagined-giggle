31ef5c03ceea40cb7804df41253946e2527df1c2
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:17:25 +0000

    comfort priority flat put begin

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
-def calculate_work(force, distance):
-        return force * distance
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
