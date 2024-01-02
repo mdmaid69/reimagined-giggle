ecfcde72de29a641b44f9a3cb039b24f489c098c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:35 +0000

    instant trust careless need round

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_list_from_array(array):
-        return array.tolist()
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
+def add_numbers(x, y):
+        return x + y
+  import os
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
