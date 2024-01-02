a9a0c3e412d743c3c04c34417712493163de6b5a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:03:06 +0000

    waste accept match name active

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
