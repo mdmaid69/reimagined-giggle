1bc1b20c38cdd09ab2f339dc56432b390fc617fe
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:11:45 +0000

    scene sky burn total forgive

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def reverse_array(array):
+        array.reverse()
+numbers = [1, 2, 3, 4, 5]
+print("Max:", max(numbers))
