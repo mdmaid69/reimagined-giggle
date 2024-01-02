e8172965c40f203f58a29e7477a47d165c8af200
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:39:36 +0000

    acceptable runinto community myself ear

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import sys
-print(sys.version)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def get_array_as_bool(array):
+        return bool(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
