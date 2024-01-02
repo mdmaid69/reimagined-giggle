ff18be38c416b8989bf85b844e509c542975d144
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:00 +0000

    environment brain study many vote

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import collections
+def create_ordered_dict():
+        return collections.OrderedDict()
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
