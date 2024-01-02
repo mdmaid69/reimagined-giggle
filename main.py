8e8f2db5e79bccc249894e0a0ccd56340634dc27
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:53:04 +0000

    wide calm attractive print determined

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_debt_ratio(total_debt, total_assets):
-        return total_debt / total_assets
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
