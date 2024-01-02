5cd1316ecdd602e39ff563c75ee0459bc9ab42c8
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:18:03 +0000

    weigh distant occur follow check

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
 import math
-def calculate_bessel_function_of_first_kind(n, x):
-        return math.jn(n, x)
+def calculate_tangent(x):
+        return math.tan(x)
