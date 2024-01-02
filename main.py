bcbc8a665258c5bb58f281ffed6542eec364929d
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:55:58 +0000

    market left cheap hardly sale

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
+  def is_odd(n):
+        return n % 2 != 0
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
