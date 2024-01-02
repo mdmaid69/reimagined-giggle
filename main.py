8131f0226487a052fc983b03544724ab17198059
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:55:35 +0000

    fault kill behind lunch use

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
-import shutil
-def delete_directory(path):
-        shutil.rmtree(path)
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+import queue
+
+q = queue.Queue()
+
+for i in range(5):
+        q.put(i)
+
+while not q.empty():
+        print(q.get())
+import array
+def convert_array_to_string(array):
+        return array.tostring()
