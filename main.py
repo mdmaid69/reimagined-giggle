dfb13d91118adbb70372297947a404e96289b1cd
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:14 +0000

    switch birth translate beyond shopping

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def count_elements(lst):
-        return len(lst)
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_energy(mass, c=3*10**8):
+        return mass * c**2
