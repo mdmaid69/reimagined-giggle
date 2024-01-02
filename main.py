cd7f1bd408bc3dc73d9684f2855973f3b2d21ae9
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:56:10 +0000

    trousers trousers cost balance minor

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+import threading
+def create_thread(target):
+        thread = threading.Thread(target=target)
+        thread.start()
+        return thread
