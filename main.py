44df8f7a0ba64bf336707906c5d66f465a57e17e
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:41:49 +0000

    noise support tall turnup flow

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-import random
-print(random.randint(0, 100))
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
