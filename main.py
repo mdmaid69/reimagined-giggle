749ddca48326c2139f9e84eea02817b85bd0ff03
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:00:17 +0000

    normal time fear broad wise

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import array
-def convert_list_to_array(list, typecode):
-        return array.array(typecode, list)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import math
+def calculate_gamma_function(x):
+        return math.gamma(x)
