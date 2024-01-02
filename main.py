d73db493c374aeb14e0f4c418c7f5de10e531b0d
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:09:44 +0000

    sound tough place Kk book

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import csv
-def save_csv(data, filename):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
