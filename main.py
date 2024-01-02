985a2beced8958d02a25b3ed80ddfd51f073714d
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:00:30 +0000

    steady dance go piece unless

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import array
-def extend_array(array, iterable):
-        array.extend(iterable)
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
+def find_min(numbers):
+        return min(numbers)
