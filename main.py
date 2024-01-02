363dfbe7ca66715331a823f1c3ad12f47fac80b0
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:47:03 +0000

    pile real during defend sleep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import math
-def calculate_inverse_hyperbolic_tangent(x):
-        return math.atanh(x)
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
