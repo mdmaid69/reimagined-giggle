35ba897fc00277414bd36bb0cafe37a67c34327c
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:35:02 +0000

    cruel none relationship system couple

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def convert_to_binary(n):
-        return bin(n)
-  import json
-  def convert_dict_to_json(d):
-        return json.dumps(d)
+import math
+def calculate_circle_circumference(radius):
+        return 2 * math.pi * radius
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
