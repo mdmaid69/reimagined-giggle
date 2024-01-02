43f23d3fafac77d0deb5180de37c463da04156f4
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:07:11 +0000

    door forward please wall cheap

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_length(array):
-        return len(array)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import os
+def list_files_in_directory(path):
+        return os.listdir(path)
