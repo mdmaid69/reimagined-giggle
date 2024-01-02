b75232b2d4dc7e494d713c308eff7bba57d58dad
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:03 +0000

    tear back comeoff receive wish

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Is prime:", all(n % i != 0 for i in range(2, int(n**0.5) + 1)))
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+print("Hello, world!")
+import math
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
