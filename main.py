9e382af1552dfdb9759693b9560f0edc9cc03a0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:38:30 +0000

    earth herself prevent callup city

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-text = "Hello, world!"
-print("Uppercase:", text.upper())
+import array
+def append_to_array(array, item):
+        array.append(item)
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.show()
