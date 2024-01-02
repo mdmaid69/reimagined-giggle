201dbe31275ff237fe85a6960fcb7cc77034bd42
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:29:59 +0000

    period yesterday knock scared pause

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
+  def get_file_group(file_name):
+        return os.stat(file_name).st_gid
