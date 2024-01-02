2bfae687a2716783ea15c16b57f90f7628368b5b
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:05:58 +0000

    water relation deliberately 567enemy lift

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  import sqlite3
-  def execute_sql_query(connection, query):
-        cursor = connection.cursor()
-        cursor.execute(query)
-        return cursor.fetchall()
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
