f9340332ecce669baa4f75c5efca549face34acf
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:39 +0000

    file opponent record press turnout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import collections
-def create_user_list():
-        return collections.UserList()
+def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
