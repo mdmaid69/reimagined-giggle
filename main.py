dd221b9b93bec5c3892cc4c982e9a04fd789184a
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:00:06 +0000

    out Gg Vv treat disturb

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+x = 10
+y = 20
+print("Sum:", x + y)
+import array
+def reverse_array(array):
+        array.reverse()
