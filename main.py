48326132070aa8f072633fae437ca17119f29bf1
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:02:37 +0000

    know address happy Ww middle

--- main.py
+++ main.py
@@ -1,10 +1,5 @@
-n = 5
-result = 1
-for i in range(1, n + 1):
-        result *= i
-print("Factorial:", result)
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+def find_max(lst):
+        return max(lst)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
