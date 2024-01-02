a954c11bf06160244c5686c231362dd982fdca8e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:21:34 +0000

    image train sign fun equipment

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+import array
+def get_array_index(array, item):
+        return array.index(item)
+import random
+print(random.randint(0, 100))
