8f44220241231d1c86dd075fdde783e9ce70b964
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:52:17 +0000

    replace amount much shoot which

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def cube_number(x):
-        return x**3
-import random
-print(random.randint(0, 100))
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
