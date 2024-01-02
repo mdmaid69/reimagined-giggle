20cc09551f924209220be7d79b7106a2a51e8810
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:45:31 +0000

    serious play address pump admire

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import math
-def calculate_gamma_function(x):
-        return math.gamma(x)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
