ae03d3cbfa6962ca1cafd8a8d72ef2ac74708dd8
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:17 +0000

    convince command majority wet actual

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-print([x**2 for x in range(10)])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
