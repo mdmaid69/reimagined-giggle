ba9a620761a90be98734fc6f95ba7c6f68e88953
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:10:33 +0000

    thing old concentration earth handle

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_floor(x):
-        return math.floor(x)
-  import os
-  def get_file_extension(file_name):
-        return os.path.splitext(file_name)[1]
+def calculate_power(work, time):
+        return work / time
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
