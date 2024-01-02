47b78e0cd36579c77c012f02eb6f31d22cbffe2b
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:58:26 +0000

    turnon side safety talk worth

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_circle_circumference(radius):
-        return 2 * math.pi * radius
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
