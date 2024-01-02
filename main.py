82f11ec69ad74b1a2ba4de7c7a1a19745b6dd053
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:43:08 +0000

    choose couple quality inform chin

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import array
-def get_array_from_bytes(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
+  import pandas as pd
+  def write_to_excel_file(file_name, data):
+        df = pd.DataFrame(data)
+        df.to_excel(file_name, index=False)
+def add_numbers(x, y):
+        return x + y
