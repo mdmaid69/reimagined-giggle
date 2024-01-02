1a7511402c7da0d43a37530465ac23b9913d5ae6
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:30:40 +0000

    month face strict accept bet

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-for i in range(10): print(i)
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+import json
+print(json.dumps({"name": "John", "age": 30}))
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
