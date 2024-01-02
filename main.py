bc65b3a24e82e7a5b26ddc3bd69ecfcf249a2bd7
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:21:08 +0000

    beyond like person leave takeout

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import sys
-def exit_program():
-        sys.exit()
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+def calculate_npv(rate, cash_flows):
+        return sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
