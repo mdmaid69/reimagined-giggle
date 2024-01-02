2e0786fadb2f030f3a969385f8a858a2abba2deb
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:20:22 +0000

    market offer dick ability tap

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
-def calculate_pressure(force, area):
-        return force / area
+def square_number(x):
+        return x**2
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
