c49be60c58121196b1e7d653e4985ef085deba90
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:32:56 +0000

    wind like getback crowd side

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def sort_list(lst):
-        return sorted(lst)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+def square_number(x):
+        return x**2
+import time
+def wait_for_seconds(seconds):
+        time.sleep(seconds)
