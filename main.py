6d4b6479ea01b3f1a25bb7411c95aa49cd548915
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:10:34 +0000

    modern guard good during replace

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+import re
+def split_string(pattern, string):
+        return re.split(pattern, string)
