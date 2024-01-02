91a3b47e362761514aeac76742cc6d07dd77cec8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:52:30 +0000

    hardly pressure translate cause tradition

--- main.py
+++ main.py
@@ -1,9 +1,4 @@
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
-import json
-def save_json(data, filename):
-        with open(filename, "w") as f:
-        json.dump(data, f)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
