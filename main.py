e9c1685c81d309e2049630cf1ead2306f652759d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:11:25 +0000

    pass turnout react people point

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import sys
-def exit_program():
-        sys.exit()
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+import math
+def calculate_logarithm_base_e(x):
+        return math.log(x)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
