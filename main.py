b1f9138c2f9c01ede725f024b60a41781b77a6ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:33 +0000

    scream fine impression bang early

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-def calculate_eps(net_income, shares_outstanding):
-        return net_income / shares_outstanding
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  import time
+  def wait_for_seconds(seconds):
+        time.sleep(seconds)
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
