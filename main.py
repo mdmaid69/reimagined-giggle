8905b0f0835309bc0d030fcd28692d497fa7fa03
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:27:00 +0000

    better behind trap disturb drink

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+import array
+def get_array_as_dict(array):
+        return {i: item for i, item in enumerate(array)}
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
