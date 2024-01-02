217d1d37d0679e980c110eeecfb74c940f8fe8d5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:30:21 +0000

    broken future properly noise doubt

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-import collections
-def create_default_dict(default_type):
-        return collections.defaultdict(default_type)
-import math
-def calculate_arc_sine(x):
-        return math.asin(x)
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
+def is_prime(n):
+        if n < 2: return False
+        for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+                return False
+        return True
