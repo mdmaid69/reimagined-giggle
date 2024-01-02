d057f05d8b1fb32c06210a5239fbc3fa75ec0900
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:49:44 +0000

    heart tradition careless find generally

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import array
-def write_array_to_file(filename, array):
-        with open(filename, "wb") as f:
-        array.tofile(f)
+  def get_file_name_without_extension(file_name):
+        return os.path.splitext(file_name)[0]
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
