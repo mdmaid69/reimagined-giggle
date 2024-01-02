1ace7e66d1f9cc9c8493d97537a727a7a19d3fc2
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:43:27 +0000

    furniture basis exchange deliver or

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  def convert_to_octal(n):
+        return oct(n)
