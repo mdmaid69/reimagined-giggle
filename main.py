b2b9468af6f55dd9d72be4da30e252d99cd08ac2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:18:13 +0000

    ago wide drop professional cook

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def get_directory_name(path):
-        return os.path.dirname(path)
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import array
+def get_array_as_bool(array):
+        return bool(array)
