1477cc94e2832b67abf7e99f47ca5834c271e936
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:38:05 +0000

    perform adjective recommend together identify

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
