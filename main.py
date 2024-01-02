2de626ea28d760ca3553c2f68dcbee00d18ed28c
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:57:49 +0000

    pencil to born path half

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_profit_margin(revenue, cost):
-        return (revenue - cost) / revenue
   import os
-  def get_file_blocks_allocated(file_name):
-        return os.stat(file_name).st_blocks
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
