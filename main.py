eae54abc189ce93d2ef9da4347158be8bf825e46
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:38:41 +0000

    out cut feel weak rely

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
 import math
-def calculate_sine(x):
-        return math.sin(x)
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
