3bc4c8b57494c7cc12bff4e92578837d998acea1
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:55:29 +0000

    moment apparently bank fetch giveout

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_bytes_to_array(bytes, typecode):
-        a = array.array(typecode)
-        a.frombytes(bytes)
-        return a
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+def find_min(lst):
+        return min(lst)
