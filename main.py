47bd7abcf8b021df4942b50a602e5a04257d4521
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:59:20 +0000

    noone experiment fair original culture

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
