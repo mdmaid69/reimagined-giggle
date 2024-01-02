d39794d81347e668f12ee0ab697a27834b1f5bf3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:42:36 +0000

    tap term star arm start

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-  def find_min(lst):
-        return min(lst) if len(lst) != 0 else "List is empty"
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+x = 10
+y = 20
+print("Sum:", x + y)
