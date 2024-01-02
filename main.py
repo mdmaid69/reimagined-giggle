87cdbd6b5cde66662149769cefddc08551b616ed
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:42 +0000

    burn common weigh instead drink

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Max:", max(numbers))
   import os
   def get_file_atime(file_name):
         return os.stat(file_name).st_atime
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
