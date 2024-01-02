3571d266d4c3cbdae3dabc52af139d39079af7c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:39 +0000

    guess underneath wise black since

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-print(sum(range(10)))
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+  import random
+  def generate_random_number(start, end):
+        return random.randint(start, end)
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
