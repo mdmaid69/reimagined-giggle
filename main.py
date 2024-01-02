fed504641ef7c496f5ffaa795bb82048489960d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:27:30 +0000

    thin far wise change crime

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import time
-def get_current_time():
-        return time.time()
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+from collections import Counter
+print(Counter("hello world"))
