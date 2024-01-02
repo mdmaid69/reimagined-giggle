d751b452f08e59d4ac5939fa556288d56ace8bd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:42:40 +0000

    touch surprising announce deep spoil

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import sqlite3
-  def connect_to_database(db_name):
-        return sqlite3.connect(db_name)
+import json
+def pretty_print_json(data):
+        return json.dumps(data, indent=4)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
