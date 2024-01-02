b647d5cd6af467469870597dfdeb94a87e74a200
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:31:03 +0000

    comment sound load fun change

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_return_on_assets(net_income, total_assets):
-        return net_income / total_assets
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
