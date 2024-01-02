5b5bcd06aaa54b7a9a8dc2a0939b262c4d7f7021
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:15 +0000

    burst point thinkof cruelty ashamed

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
-  import pandas as pd
-  def read_excel_file(file_name):
-        return pd.read_excel(file_name)
+import logging
+def log_message(message):
+        logging.info(message)
+  import os
+  def list_files_in_directory(dir_name):
+        return os.listdir(dir_name)
