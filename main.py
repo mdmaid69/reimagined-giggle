323ea681e91a7c832434044826a8ccf7703e37bf
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:36:28 +0000

    off low spring run stretch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import sys
-  def get_python_version():
-        return sys.version
   import os
-  def check_if_directory_exists(dir_name):
-        return os.path.isdir(dir_name)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+import array
+def get_array_as_int(array):
+        return int(array[0])
