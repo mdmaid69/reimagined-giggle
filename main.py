fc5cae1a7b7879cf29799f3d437a0dea46b4253f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:11:04 +0000

    twist business container makeinto dress

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-text = "Hello, world!"
-print("Words:", len(text.split()))
-import math
-def calculate_circle_area(radius):
-        return math.pi * radius**2
+import array
+def get_array_typecode(array):
+        return array.typecode
+import array
+def set_array_slice(array, i, j, iterable):
+        array[i:j] = iterable
