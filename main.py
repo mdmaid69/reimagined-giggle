448e2444dc5352fc321b3e90b78947e787a78ad2
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:43 +0000

    fairly interested second anxious catch

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
 import array
-def get_array_as_bytearray(array):
-        return bytearray(array)
+def get_bytes_from_array(array):
+        return array.tobytes()
