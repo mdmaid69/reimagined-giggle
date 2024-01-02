a53536a8b278b1cacc0dbb0e84502d18b724432f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:03:12 +0000

    tidy city turnout stick dare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-import platform
-def get_python_version():
-        return platform.python_version()
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+import datetime
+print(datetime.datetime.now())
