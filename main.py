e449f3c5ce6c9e2127ec2c1d970ee8319e85c3e1
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:43:03 +0000

    good stamp when tend chat

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_tangent(x):
-        return math.tan(x)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
