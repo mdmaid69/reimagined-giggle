c52f51354ec6eeb6878a7826efda84d4ab5a51df
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:49:29 +0000

    ourselves unfortunate kind to point

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def calculate_bessel_function_of_second_kind(n, x):
+        return math.yn(n, x)
