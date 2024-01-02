cf8fc80a1565c9b1eb56d3ea644205266ee2f686
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:04:26 +0000

    into behind breakinto use label

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-import numpy as np
-print(np.array([1, 2, 3]))
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
+  def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
