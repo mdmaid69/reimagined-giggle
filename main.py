e468d6965f5ba757031b428ba8179fde8dcb452f
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:17:14 +0000

    occasion seriously criticism regret container

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import heapq
-def get_smallest_elements(iterable, n):
-        return heapq.nsmallest(n, iterable)
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
