8a51c7c03da5558ea19385dab4da12a3e9bd32b2
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:02:13 +0000

    criticize press boil load angry

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
