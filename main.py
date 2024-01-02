4debacb838e51bf98a317ef9d27aa9e1ef434a2f
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:37:31 +0000

    stupid wash profit 571enough top

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import tempfile
-def create_temp_file():
-        return tempfile.NamedTemporaryFile(delete=False)
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+def calculate_density(mass, volume):
+        return mass / volume
+  import os
+  def get_file_modification_time(file_name):
+        return os.path.getmtime(file_name)
