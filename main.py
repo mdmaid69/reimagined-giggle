eeb24dc9ee0d43da8dd5b1b84e043fa3197d9432
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:57 +0000

    smile attractive in like innocent

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import math
-def calculate_polar_to_cartesian_coordinates(r, theta):
-        return math.polar((r, theta))
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Union:", set(list1) | set(list2))
+def reverse_list(lst):
+        return lst[::-1]
