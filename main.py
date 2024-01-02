d7b1d2524890cfdeca325431875b2154ba0b56e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:31:05 +0000

    now see past return move

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_density(mass, volume):
-        return mass / volume
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import math
+def calculate_hyperbolic_sine(x):
+        return math.sinh(x)
+import math
+def calculate_exponential(x):
+        return math.exp(x)
