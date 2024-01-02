23553e99cabf8d4730b0a72c121c3de3bb5249b9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:55:47 +0000

    happy allow giveback mixture boat

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_irr(cash_flows):
-        rate = 0.1
-        for _ in range(100):
-        npv = sum(cf / (1 + rate)**i for i, cf in enumerate(cash_flows))
-        if abs(npv) < 1e-6:
-                return rate
-        rate += 0.01
-        return None
-import array
-def get_array_as_set(array):
-        return set(array)
+import collections
+def create_default_dict(default_type):
+        return collections.defaultdict(default_type)
+def find_union(list1, list2):
+        return set(list1) | set(list2)
