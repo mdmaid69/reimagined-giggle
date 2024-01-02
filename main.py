d9a00cd56e01ae445f876d84d8bf4e2f2a8a9f95
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:12 +0000

    hour hurry suit extremely injury

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
