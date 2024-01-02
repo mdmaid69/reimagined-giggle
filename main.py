453d96a0efbbfea58753471634ea695d090b598e
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:56:36 +0000

    bound attitude church quick detail

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-def calculate_area(radius):
-        return 3.14 * radius * radius
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
+n = 10
+a, b = 0, 1
+while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
