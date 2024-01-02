fed67fd8504b847c3864ae92f802768aa212d2fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:30 +0000

    star little habit careless slow

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def convert_to_binary(n):
-        return bin(n)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
