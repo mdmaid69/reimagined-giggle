4e3b7d72dfaed1f43465712e945f4b96d660b3aa
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:46:34 +0000

    under come beyond effort behave

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_as_bool(array):
-        return bool(array)
 import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+def find_all_occurrences(pattern, string):
+        return re.findall(pattern, string)
+import collections
+def create_user_string():
+        return collections.UserString()
