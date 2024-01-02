06f5a7d292805ad067c69b1d469c80df3121a9e3
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:15:19 +0000

    same yes frightened both correct

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+import os
+def get_file_creation_time(filename):
+        return os.path.getctime(filename)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
