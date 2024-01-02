21b298387c3000c6f23ae7c7b21d6ddaa039bf44
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:25:01 +0000

    recommend art cum reply left

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-def convert_to_octal(n):
-        return oct(n)
+def is_odd(n):
+        return n % 2 != 0
+def calculate_average(lst):
+        return sum(lst) / len(lst)
