70b8dac67fde3bc69a3f05e15409c6c9d8ebbfd1
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:55:38 +0000

    patient floor laugh turnaway dance

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import math
-  def calculate_square_root(n):
-        return math.sqrt(n)
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
 import array
-def get_array_as_int(array):
-        return int(array[0])
+def get_array_as_repr(array):
+        return repr(array)
