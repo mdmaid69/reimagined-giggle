51705f85b00176da7a55cdb60a8d59a08c1fda96
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:48:40 +0000

    delight birth press own lunch

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def find_unique_words(sentence):
-        return set(sentence.split())
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
+  def get_base_name(path):
+        return os.path.basename(path)
+def calculate_return_on_equity(net_income, total_equity):
+        return net_income / total_equity
