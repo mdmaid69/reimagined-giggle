641b8e9c6a453d27cefec0d539a62d682e68a983
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:26:39 +0000

    left past breakinto cope fall

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import heapq
-def pop_push_heap(heap, item):
-        return heapq.heapreplace(heap, item)
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
+import csv
+def write_csv_file(filename, data):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
