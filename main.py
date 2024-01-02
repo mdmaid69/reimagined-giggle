c83ad39851452540eec0277b4b5f47353074bc93
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:06:13 +0000

    straight because flow away spare

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-  def add_numbers(x, y):
-        return x + y
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_compound_interest(principal, rate, time):
+        return principal * (1 + rate)**time - principal
+print(sum(range(10)))
