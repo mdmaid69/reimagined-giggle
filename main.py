fb467a4c538f8b4d48e0e1b9c2d6ef5e0a30ac41
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:40:08 +0000

    slow affect bright education beautiful

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def change_current_working_directory(dir_name):
+        os.chdir(dir_name)
