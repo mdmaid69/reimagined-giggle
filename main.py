4fb6cc897b69d0fd73bd0b455f7450ad111860d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:15:59 +0000

    mind stupid clothes move sale

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 def calculate_acceleration(speed, time):
         return speed / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
