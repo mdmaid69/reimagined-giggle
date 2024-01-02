40c3765a665ca1d4c48a87263b1a8b6ce2d0d95e
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:42:16 +0000

    general maybe prevent talkinto turnon

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
   import os
-  def get_file_size(file_name):
-        return os.path.getsize(file_name)
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import random
+def generate_random_choice(choices):
+        return random.choice(choices)
