fd6ae0ce8f7f75f9aa2febe6142074aeae10ca59
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:37:55 +0000

    lend plant comefrom recover sweep

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
-  import os
-  def get_file_ctime_ns(file_name):
-        return os.stat(file_name).st_ctime_ns
+  import re
+  def replace_all_occurrences(pattern, replace_with, string):
+        return re.sub(pattern, replace_with, string)
+import re
+print(re.match("h.*o", "hello world"))
