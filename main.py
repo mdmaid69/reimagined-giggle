efdebb6b795fe800e35546303370843f6c3cab19
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:35:25 +0000

    politics pleasant ever black damage

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
 import array
-def reverse_array(array):
-        array.reverse()
+def convert_array_to_list(array):
+        return array.tolist()
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
