09481928a09fc2675fcbc293005c4ce6c5d93c81
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:04 +0000

    risk classroom might tear concerning

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import collections
-def create_counter():
-        return collections.Counter()
-import array
-def get_array_as_dict(array):
-        return {i: item for i, item in enumerate(array)}
+def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+                if n % i == 0:
+                return False
+        return True
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
