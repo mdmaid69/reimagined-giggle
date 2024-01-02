133e95bb882567e9e81d54b066ffd87ec1366e75
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:05:28 +0000

    thinkover extremely Gg edge gap

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
-  import os
-  def get_file_number_of_links(file_name):
-        return os.stat(file_name).st_nlink
+def calculate_area(radius):
+        return 3.14 * radius * radius
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
