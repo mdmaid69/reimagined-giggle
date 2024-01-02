abc6fc2efae88284643f25b0a3316c144ad8ac66
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:51:25 +0000

    cut key stretch along turnback

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_radians_to_degrees(radians):
-        return math.degrees(radians)
-import json
-def pretty_print_json(data):
-        return json.dumps(data, indent=4)
+  def is_even(n):
+        return n % 2 == 0
+def is_odd(n):
+        return n % 2 != 0
