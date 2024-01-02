35c7c876246c2f606406a97af20edbc82994e6fd
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:57:14 +0000

    herself type success pile recognize

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
   import json
   def convert_dict_to_json(d):
         return json.dumps(d)
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
