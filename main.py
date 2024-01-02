73970577d17cd4ac536a1f7e9756a250c9bed4e2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:50:17 +0000

    basis boat impossible hospital imagine

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-def calculate_power(work, time):
-        return work / time
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
