220a8aee49b886d6edfee05d0647f108867defbd
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:08 +0000

    unfair myself alone justice goon

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
 import math
 def calculate_bessel_function_of_second_kind(n, x):
         return math.yn(n, x)
