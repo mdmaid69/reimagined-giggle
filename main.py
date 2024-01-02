86735dd458a8e4973d0486e65dc7a9f9b7b0187b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:06:37 +0000

    might fault whichever tell bring

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import array
-def set_array_item(array, i, item):
-        array[i] = item
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
