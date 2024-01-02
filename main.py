a8012c9c038eb17986eb3beef2ab4f37d049b6d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:18:45 +0000

    comein loud big in hurt

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-def find_difference(list1, list2):
-        return set(list1) - set(list2)
+  def sort_list(lst):
+        return sorted(lst)
+def calculate_force(mass, acceleration):
+        return mass * acceleration
