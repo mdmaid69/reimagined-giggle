5d36f529e2650fc77076519b3c9ff59c54487d33
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:18:02 +0000

    practical lot pattern sink bank

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_circumference_circle(r):
-        return 2 * 3.14 * r
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
