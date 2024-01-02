2c4180008c0aae20585802f300118bb888c2f71b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:02 +0000

    test progress luck fit it

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  def calculate_area_rectangle(l, w):
-        return l * w
-  import os
-  def get_file_owner(file_name):
-        return os.stat(file_name).st_uid
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+  def reverse_list(lst):
+        return lst[::-1]
