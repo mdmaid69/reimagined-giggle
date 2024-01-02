c6d8a2ef485f7433a342cf7f078261e472e687a9
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:03 +0000

    throw search claim desperate murder

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def delete_directory(dir_name):
-        os.rmdir(dir_name)
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_power(work, time):
+        return work / time
