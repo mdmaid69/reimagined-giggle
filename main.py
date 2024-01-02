4951f43d30fc13804cec967f3db2eed226f7054f
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:23:59 +0000

    acceptable 564encourage model upwards height

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
