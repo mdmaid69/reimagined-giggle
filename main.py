799cd2fec27058ba3748680deb77eae2ed505d23
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:25:13 +0000

    getup die else dangerous throughout

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_permissions(file_name):
-        return os.stat(file_name).st_mode
-import math
-print(math.pi)
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_volume(length, width, height):
+        return length * width * height
