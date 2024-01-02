02c61e90293c3691bac88fbd1c4517a4db82342a
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:59:24 +0000

    question rub truth open instruction

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-import os
-def get_file_creation_time(filename):
-        return os.path.getctime(filename)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import numpy as np
+  def create_numpy_array(lst):
+        return np.array(lst)
