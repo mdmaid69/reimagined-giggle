773b4db229c0e3ffbf869031f55f79f61778fe01
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:27:45 +0000

    illness push duty takeoff deliberate

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import random
-def generate_random_choice(choices):
-        return random.choice(choices)
-  def convert_to_binary(n):
-        return bin(n)
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
