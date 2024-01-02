6e4c4d49549ef1fbf5e37231c7e8641a8d43c98a
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:30:46 +0000

    raw pronunciation touch emphasize ours

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-import math
-def calculate_arc_cosine(x):
-        return math.acos(x)
-from flask import Flask
-app = Flask(__name__)
-
-@app.route("/")
-def hello():
-        return "Hello, World!"
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
