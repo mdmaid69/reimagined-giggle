dc8267110912c3770263e8fb713c94f1ae9aff3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:26:33 +0000

    soak tear giveback show connected

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import csv
-def load_csv(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
