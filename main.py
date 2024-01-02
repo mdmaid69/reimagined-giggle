65dbc11c0fac6e2d82e7115cdfbe252e99d2cc7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:56:08 +0000

    underneath occasional either under later

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import os
-  def get_base_name(path):
-        return os.path.basename(path)
-for i in range(5):
-        print(i)
+import array
+def get_array_index(array, item):
+        return array.index(item)
+  def calculate_area_circle(r):
+        return 3.14 * r**2
