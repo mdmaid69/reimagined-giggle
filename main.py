8abb5c1f51844d42cfcdd49248102b5856df563c
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:22 +0000

    entry Vv market angry mysterious

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
