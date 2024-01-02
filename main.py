307185223d3f762b4e8186537f626b98855f8d4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:12:15 +0000

    once country someone expect accident

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
 import array
-def get_array_itemsize(array):
-        return array.itemsize
-def calculate_simple_interest(principal, rate, time):
-        return principal * rate * time
+def reverse_array(array):
+        array.reverse()
