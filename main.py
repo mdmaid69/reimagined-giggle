d7c6164d35504d22414b7d2ed6b50299be69a8d7
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:50:30 +0000

    achieve say laugh lonely typical

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import re
-def split_by_pattern(pattern, string):
-        return re.split(pattern, string)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import os
+  def rename_file(old_name, new_name):
+        os.rename(old_name, new_name)
