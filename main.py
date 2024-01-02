de32c2ddffae715bb07af92b3f4070e1e5ac202d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:06 +0000

    deliberate have train could run

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  def count_elements(lst):
-        return len(lst)
-import collections
-def create_stack():
-        return collections.deque()
+import re
+print(re.match("h.*o", "hello world"))
+def count_characters(sentence):
+        return len(sentence)
