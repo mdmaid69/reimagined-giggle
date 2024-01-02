d1b9c64a298f09cd26c41425b3b153ee27ec2147
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:15:04 +0000

    intend bear second stretch club

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import sys
-def print_python_version():
-        print(sys.version)
-import math
-def calculate_error_function(x):
-        return math.erf(x)
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def find_difference(list1, list2):
+        return set(list1) - set(list2)
