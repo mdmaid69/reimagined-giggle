63cc1a058955f53071a0d3f7dc6409548ddcd772
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:14:06 +0000

    more charge survive thanks invitation

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  import csv
-  def read_csv_file(file_name):
-        with open(file_name, "r") as file:
-          return list(csv.reader(file))
-print([x**2 for x in range(10)])
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
