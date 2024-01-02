c46b901e097dc3d833e4c3387a7cf1202db808d3
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:14:39 +0000

    cost guess land goodbye dare

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-  def convert_to_binary(n):
-        return bin(n)
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
+def calculate_interest(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_density(mass, volume):
+        return mass / volume
