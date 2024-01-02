a8e2b7f3cb0413419136dcd7693ce55b6fa1369c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:54:37 +0000

    plain my his opposite immediately

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_birthtime_ns(file_name):
-        return os.stat(file_name).st_birthtime_ns
+import http.server
+def start_http_server(port):
+        http.server.HTTPServer(("", port), http.server.SimpleHTTPRequestHandler).serve_forever()
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
