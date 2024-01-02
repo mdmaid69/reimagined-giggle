e7357323f94619caa3f26a62b152da86c75f3627
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:44:31 +0000

    mouth trip free wire muscle

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+sentence = "Hello, world!"
+from collections import Counter
+print("Word frequencies:", Counter(sentence.split()))
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-import logging
-logging.basicConfig(level=logging.INFO)
-logging.info("This is an info message")
+  def get_file_block_size(file_name):
+        return os.stat(file_name).st_blksize
