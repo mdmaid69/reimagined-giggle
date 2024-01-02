5d29c57cea1a31315888b1fe5ba4c5cbff9695d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:26:50 +0000

    uniform sore middle surprising meal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
-import math
-def calculate_logarithm_base_2(x):
-        return math.log2(x)
+import array
+def reverse_array(array):
+        array.reverse()
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
