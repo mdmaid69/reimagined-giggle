b11a424ff2dcd14569238a35bc8f0ce08f63ce75
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:02:13 +0000

    pure might flow thumb pair

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import array
+def set_array_item(array, i, item):
+        array[i] = item
   import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-  def subtract_numbers(x, y):
-        return x - y
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
