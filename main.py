60be451a84e83637f17348c7e931f933ec805706
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:09:21 +0000

    search face prepare distant town

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import array
-def get_array_length(array):
-        return len(array)
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+n = 10
+print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def count_characters(sentence):
+        return len(sentence)
