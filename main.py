b61640a44262e26fa116b3f805c23e38042dfb31
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:18:25 +0000

    glad butter risk plan again

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def get_array_as_int(array):
-        return int(array[0])
-import math
-def calculate_hyperbolic_sine(x):
-        return math.sinh(x)
+  def is_prime(n):
+        for i in range(2, int(n**0.5) + 1):
+          if n % i == 0:
+        return False
+        return True
+  import os
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
