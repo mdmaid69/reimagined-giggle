07d94159bde456ccdcc7dc3c213d6675ddfc66bd
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:35:41 +0000

    train interruption lean discussion cough

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def change_current_working_directory(dir_name):
-        os.chdir(dir_name)
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+  def calculate_area_circle(r):
+        return 3.14 * r**2
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
