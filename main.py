e1e6c5ed2abe440b33bcfc38562146453b3fae07
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:01 +0000

    crisis prove need boring protect

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import logging
-def log_message(message):
-        logging.info(message)
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
