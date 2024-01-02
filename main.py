18c3ca803dc1c70b2ea6e56c5f16de57f9be1f6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:20 +0000

    kick black defend along advice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import heapq
-def push_to_heap(heap, item):
-        heapq.heappush(heap, item)
-def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  import os
+  def get_file_ctime_ns(file_name):
+        return os.stat(file_name).st_ctime_ns
+  import os
+  def delete_file(file_name):
+        os.remove(file_name)
