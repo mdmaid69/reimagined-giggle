e86f1cc4e6149c4a97c12b3f1f5fdc30e152c48e
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:38:20 +0000

    ill keepout talkoutof before seem

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import csv
-def read_csv_file(filename):
-        with open(filename, "r") as f:
-        reader = csv.reader(f)
-        return list(reader)
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
