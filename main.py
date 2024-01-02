7bf71374383d949f40c45d11c6fc133d8102173e
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:03:32 +0000

    prefer mouth total stiff satisfied

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
