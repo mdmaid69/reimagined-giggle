d66c9a71bbf9143026233d04bb086d3562a374e5
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:20:07 +0000

    nature moment reply give improve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import json
+  def convert_json_to_dict(json_str):
+        return json.loads(json_str)
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def sort_numbers(numbers):
-        return sorted(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
