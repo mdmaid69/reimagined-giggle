6f7b1f93fc16f07b53b27e6edd60301353e6a295
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:49:02 +0000

    remove closely or display accordingto

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-for i in range(5):
-        print(i)
+def find_common_elements(list1, list2):
+        return set(list1) & set(list2)
   import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
