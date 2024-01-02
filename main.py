945b5036a4cb01447e6dd24d73a41e0d057d6936
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:49:28 +0000

    against declare weak water neat

--- main.py
+++ main.py
@@ -1,12 +1,6 @@
+  import numpy as np
+  def calculate_mean(arr):
+        return np.mean(arr)
   import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
-import functools
-def memoize(func):
-        cache = {}
-        @functools.wraps(func)
-        def wrapper(*args):
-        if args not in cache:
-                cache[args] = func(*args)
-        return cache[args]
-        return wrapper
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
