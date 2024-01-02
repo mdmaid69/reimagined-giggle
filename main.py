16ddb133db898992f391d271d6e684829f447b0e
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:57:23 +0000

    ear begin loan replace certainly

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-def square_number(x):
-        return x**2
+import sys
+print(sys.version)
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
