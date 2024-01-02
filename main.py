b887e49a93dcdf2d5c99430ac5719ea6463beaf4
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:18:19 +0000

    valuable another change contribute large

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-  def calculate_average(lst):
-        return sum(lst) / len(lst) if len(lst) != 0 else "List is empty"
+def calculate_current_ratio(current_assets, current_liabilities):
+        return current_assets / current_liabilities
+def calculate_speed(distance, time):
+        return distance / time
