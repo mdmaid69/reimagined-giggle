6c4c1b452d195252f6f118528bdfdccaed233668
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:50:52 +0000

    challenge tense compare could shelter

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+  import sqlite3
+  def close_database_connection(connection):
+        connection.close()
+def is_palindrome(s):
+        return s == s[::-1]
