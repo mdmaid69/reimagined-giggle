9d52912790297b496f4fd12cddf4834c737ee06f
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:25:02 +0000

    share pause ashamed thirsty usual

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-  def count_elements(lst):
-        return len(lst)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import array
+def convert_list_to_array(list, typecode):
+        return array.array(typecode, list)
