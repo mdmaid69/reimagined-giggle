dd9e12d108df6704a076fe88950933de157078ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:41:32 +0000

    crime hot list shake which

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-def is_prime(n):
-        if n < 2: return False
-        for i in range(2, int(n**0.5) + 1):
-        if n % i == 0:
-                return False
-        return True
-  def sort_list(lst):
-        return sorted(lst)
+  import os
+  def get_file_creation_time(file_name):
+        return os.path.getctime(file_name)
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
