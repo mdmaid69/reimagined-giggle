9b823d8f9a1e75feade2314b340dc86183a39eeb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:59:39 +0000

    past near she national lift

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_sphere_volume(radius):
-        return 4/3 * math.pi * radius**3
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def square_number(x):
+        return x**2
+  def remove_duplicates(lst):
+        return list(set(lst))
