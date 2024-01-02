4069b95dbc0d905b3e1e2af65662ce41e4e7a971
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:02:44 +0000

    entrance press wheel unkind herself

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+for i in range(10): print(i)
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
