2f0782d1f36d4d275a9fc2ab2e3705a5af232f6c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:22:36 +0000

    house name cum announce drive

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def fibonacci(n):
-        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
-import os
-def list_files_in_directory(path):
-        return os.listdir(path)
+import array
+def check_if_array_does_not_contain_item(array, item):
+        return item not in array
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
