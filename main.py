be43b4aba9dbdb76f0591bcc88651ea1c7ba5f17
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:38:43 +0000

    give either bringup computer before

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
