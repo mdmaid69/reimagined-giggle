46f13613778628e1286ee6f520a9731484ed6c08
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:41:29 +0000

    follow must worse argument considering

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+  def calculate_area_triangle(b, h):
+        return 0.5 * b * h
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
