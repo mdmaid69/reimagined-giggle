9b7512b3b0ac77a02572bd12eddd3a89631ee033
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:36:03 +0000

    strength spoil surprise another scared

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
-import collections
-def create_counter():
-        return collections.Counter()
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
+import math
+def calculate_cone_volume(radius, height):
+        return 1/3 * math.pi * radius**2 * height
