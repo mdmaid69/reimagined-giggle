489c78f3035564362036bcd4a091c5f771576336
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:50:06 +0000

    giveup offend dream feeling admit

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import os
-def change_working_directory(path):
-        os.chdir(path)
-def calculate_equity_ratio(total_equity, total_assets):
-        return total_equity / total_assets
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
