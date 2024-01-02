a8078bcfcd9aa97af6c37aadad01771f1d8bfe6a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:00:52 +0000

    butter share holdout watch no

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_extension(file_name):
+        return os.path.splitext(file_name)[1]
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
