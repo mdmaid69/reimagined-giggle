53d9cd8269ff498a59aa00624ec91a5ed0be0e37
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:39:55 +0000

    although bread spare bite bound

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import sklearn.datasets
-print(sklearn.datasets.load_iris())
-import time
-print(time.time())
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
