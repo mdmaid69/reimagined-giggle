878033d58ff33bd6541c663c5f9b2a17bfc67c30
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:03:29 +0000

    mix protest shout aim shock

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import sys
+def exit_program():
+        sys.exit()
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
