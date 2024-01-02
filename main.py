52958cc3201f719923f7b4c01181d707a176cd7e
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:03:05 +0000

    performance best rate goaway vanish

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
-import getpass
-def get_password(prompt):
-        return getpass.getpass(prompt)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+import platform
+def get_python_version():
+        return platform.python_version()
