f0f0ca7493d7c2caec5ec4fbce2e72b7c85bb40e
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:44:44 +0000

    either reserve odd Ll whoever

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import array
-def get_array_as_float(array):
-        return float(array[0])
+  import os
+  def get_file_size_in_bytes(file_name):
+        return os.stat(file_name).st_size
+import sys
+print(sys.version)
