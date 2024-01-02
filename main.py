e856be9581517c007cbc5ca05f03365f59100e3b
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:07:51 +0000

    blind exciting serious interested attractive

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-import math
-def calculate_exponential(x):
-        return math.exp(x)
-  import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import numpy as np
+print(np.array([1, 2, 3]))
