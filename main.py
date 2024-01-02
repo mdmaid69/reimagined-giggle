96b3917d68a1905ddfe8dbd62f34241497d86ab6
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:43:42 +0000

    dull operation welcome anxious power

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-  import csv
-  def write_to_csv_file(file_name, data):
-        with open(file_name, "w", newline="") as file:
-          writer = csv.writer(file)
-          writer.writerows(data)
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
+import csv
+def load_csv(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
