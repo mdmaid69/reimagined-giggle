f1d3d4c50ef48eb35e8fb95950a75be9de5da0d4
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:16:02 +0000

    matter hold friendly Hh factory

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
+  def is_odd(n):
+        return n % 2 != 0
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-print([x**2 for x in range(10)])
+def get_array_from_list(list, typecode):
+        return array.array(typecode, list)
