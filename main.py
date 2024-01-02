1722f1078a10e211af22532c767b963caf665e55
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:56:27 +0000

    shrink phone connect this arrive

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import array
-def get_array_as_bool(array):
-        return bool(array)
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
+import random
+def shuffle_list(my_list):
+        random.shuffle(my_list)
+        return my_list
