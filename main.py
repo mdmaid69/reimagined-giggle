340dbed7f6705e3119a99e2d5b5f16182c04f47b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:41:27 +0000

    weekend soil sorry mystery mix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+  import json
+  def convert_dict_to_json(d):
+        return json.dumps(d)
