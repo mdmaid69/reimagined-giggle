4bd43f73bc2adddecaae0c027b7806fe96b46c16
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:17:50 +0000

    wide lead wide deliberately rubbish

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
   import matplotlib.pyplot as plt
-  def plot_histogram(data, bins):
-        plt.hist(data, bins=bins)
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
         plt.show()
-import array
-def create_array(typecode, initializer):
-        return array.array(typecode, initializer)
