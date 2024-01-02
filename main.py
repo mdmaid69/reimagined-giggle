8ee8a7e8ac65b6acc13452fce635b4ce9efdcb63
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:23:43 +0000

    punish inside mass phone spray

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def convert_array_to_unicode(array):
-        return array.tounicode()
 import collections
-def group_by(iterable, key_func):
-        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+def create_user_string():
+        return collections.UserString()
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
