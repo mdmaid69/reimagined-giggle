b41e0b4c4698a0a12587d33b748c7a87534ca0f9
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:43:37 +0000

    towards free genuine chase cost

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
-  def add_numbers(x, y):
-        return x + y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+  import os
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
