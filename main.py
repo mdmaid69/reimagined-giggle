ee8cbd788b04b67c98b5cbc3fa5ed9310e00a8d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:58:16 +0000

    pinch fail body cause everybody

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+numbers = [1, 2, 3, 4, 5]
+print("Average:", sum(numbers) / len(numbers))
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
