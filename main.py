ba66e37a6fb1ddb562c344d09df7cd1376105e79
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:44:24 +0000

    off choose turndown figure justice

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
-import array
-def get_array_typecode(array):
-        return array.typecode
+print(sum(range(10)))
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
