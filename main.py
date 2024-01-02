4a3dd28fb7f0ddc49c011b1fa2caa11f715f7a9b
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:00:10 +0000

    tradition strong offence either glad

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import math
-def calculate_least_common_multiple(a, b):
-        return abs(a*b) // math.gcd(a, b)
+  import os
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+def find_union(list1, list2):
+        return set(list1) | set(list2)
