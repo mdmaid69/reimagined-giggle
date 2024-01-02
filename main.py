c8e3b03a8dfd39cfcc2bca6bc170af29ddafe8f0
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:52:43 +0000

    turn deep trial demonstrate uniform

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+  def get_absolute_path(file_name):
+        return os.path.abspath(file_name)
+def find_min(lst):
+        return min(lst)
