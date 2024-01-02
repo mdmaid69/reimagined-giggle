59e9677a991503f596f0c1558810e3dff228c517
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:05:03 +0000

    whistle ashamed persuade Gg wherever

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import matplotlib.pyplot as plt
-  def plot_graph(x, y):
-        plt.plot(x, y)
-        plt.show()
-  def reverse_list(lst):
-        return lst[::-1]
+  def calculate_perimeter_triangle(a, b, c):
+        return a + b + c
+  import os
+  def get_file_atime(file_name):
+        return os.stat(file_name).st_atime
