37ca006b05540866540e01326be0eb5143d6b214
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:01:44 +0000

    basic outside passenger some receive

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import xml.etree.ElementTree as ET
-root = ET.Element("root")
-doc = ET.SubElement(root, "doc")
-ET.SubElement(doc, "field1", name="blah").text = "some value1"
-ET.SubElement(doc, "field2", name="asdfasd").text = "some vlaue2"
-tree = ET.ElementTree(root)
-n = 10
-print("Powers of 2:", [2**x for x in range(n)])
+  import numpy as np
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
+def calculate_pressure(force, area):
+        return force / area
