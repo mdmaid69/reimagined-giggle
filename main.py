069fd9ea2dcea822046150aef85c5087110b3681
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:08:45 +0000

    quantity preparation Dd intend bang

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_tuple(array):
-        return tuple(array)
-import math
-def calculate_modulus(x, y):
-        return math.fmod(x, y)
+def count_characters(sentence):
+        return len(sentence)
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
