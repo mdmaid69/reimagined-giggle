4b6a782049d0c8217b81873d7ab32b1309cb2d52
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:46:31 +0000

    autumn always shock want niggaacademy

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-import tensorflow as tf
-print(tf.__version__)
+print([x**2 for x in range(10)])
+import json
+def save_json(data, filename):
+        with open(filename, "w") as f:
+        json.dump(data, f)
