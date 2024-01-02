4b8ab8961e6efc49b74fd09e2574539496841e2d
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:28:34 +0000

    shoot accommodation sight no respect

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-import math
-def calculate_pythagorean_theorem(a, b):
-        return math.sqrt(a**2 + b**2)
+def count_words(sentence):
+        return len(sentence.split())
+  def convert_to_octal(n):
+        return oct(n)
