513e27ebcec7bb528dfd54d3bbdc68269e42fa09
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:14:45 +0000

    along approve mix bury beautiful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_cartesian_to_polar_coordinates(x, y):
-        return math.rect(x, y)
-def calculate_acceleration(speed, time):
-        return speed / time
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
