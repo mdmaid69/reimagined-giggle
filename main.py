a7f8eedd2bc75d7e908ddc753543d6cf4a1dd619
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:36:28 +0000

    advise reply breathe embarrassing town

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def fibonacci(n):
-        a, b = 0, 1
-        while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import math
+def calculate_sphere_volume(radius):
+        return 4/3 * math.pi * radius**3
