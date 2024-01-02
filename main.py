352e65eb156ec4fec178bef87b7580e1d9cfb9ef
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:47:03 +0000

    middle that join declaration meaning

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-  import matplotlib.pyplot as plt
-  def plot_pie_chart(labels, sizes):
-        plt.pie(sizes, labels=labels)
-        plt.show()
-  import pandas as pd
-  def write_to_excel_file(file_name, data):
-        df = pd.DataFrame(data)
-        df.to_excel(file_name, index=False)
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import datetime
+def get_today_date():
+        return datetime.date.today()
