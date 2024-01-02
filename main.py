04817ad3ad7d87f0d3a4ff9462d120a25c231bec
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:17:45 +0000

    completely breakfast high see place

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_complex(array):
-        return complex(array[0])
-import threading
-def create_thread(target):
-        thread = threading.Thread(target=target)
-        thread.start()
-        return thread
+import os
+print(os.getcwd())
+  import sqlite3
+  def connect_to_database(db_name):
+        return sqlite3.connect(db_name)
