72cbbd77854cc7682a20878c4677fb1ed0865495
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:32:02 +0000

    warm sticky attention behind character

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
+n = 5
+result = 1
+for i in range(1, n + 1):
+        result *= i
+print("Factorial:", result)
