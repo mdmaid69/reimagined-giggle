06fcd5c64a6fe3a14926809034bda02701eaa10e
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:28:43 +0000

    practise experience wait feel noise

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import json
+def convert_to_json(data):
+        return json.dumps(data)
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-def calculate_volume(length, width, height):
-        return length * width * height
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
