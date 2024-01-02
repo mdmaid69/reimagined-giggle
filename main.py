6f1f4567886c8fc1e73b43cd63409cb9672417d2
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:31:18 +0000

    regret one happen treatment brush

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-n = 10
-print("Even numbers:", [x for x in range(n) if x % 2 == 0])
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
