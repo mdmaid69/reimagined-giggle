0094454387f69256294039cd21bd9a101c574d10
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:37:22 +0000

    equal man force doubt mean

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_speed(distance, time):
-        return distance / time
-import time
-def get_time_since_epoch():
-        return time.time()
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
