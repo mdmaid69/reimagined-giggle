eea270e00f859696c55e947d94af58977049fc56
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:13:55 +0000

    spread on permission generally pain

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
