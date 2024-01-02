a1ee9ab7ec18a9300db1c8f8f8723acd676950c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:15:51 +0000

    team although spare dish freeze

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
-def find_union(list1, list2):
-        return set(list1) | set(list2)
+def calculate_average(lst):
+        return sum(lst) / len(lst)
+import array
+def get_array_as_str(array):
+        return str(array)
