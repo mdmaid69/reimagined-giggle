29dcf3511595d1b8c63a8e59842887215b7c4a63
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:44:40 +0000

    miss voice record they free

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-import math
-def calculate_cylinder_volume(radius, height):
-        return math.pi * radius**2 * height
+  import csv
+  def write_to_csv_file(file_name, data):
+        with open(file_name, "w", newline="") as file:
+          writer = csv.writer(file)
+          writer.writerows(data)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
