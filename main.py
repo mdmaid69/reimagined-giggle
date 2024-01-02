e956fbeecd8404e3d4804d1cdc926fa0ba7867d0
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:50:41 +0000

    talkinto pinch can mistake benefit

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import array
-def convert_array_to_list(array):
-        return array.tolist()
-import array
 def convert_array_to_bytes(array):
         return array.tobytes()
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
