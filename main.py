cdb49a97112eabd17d412a21f94e27d67216f02b
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:35:46 +0000

    classroom tap their steer trip

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
-from collections import Counter
-print(Counter("hello world"))
+  def multiply_numbers(x, y):
+        return x * y
+  import os
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
