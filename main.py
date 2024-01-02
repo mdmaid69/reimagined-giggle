2e1fe29b9b86474ddf5176bcb1bec8fae8a8a93d
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:53:47 +0000

    remove talk giveaway embarrassed naturally

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import math
-def calculate_greatest_common_divisor(a, b):
-        return math.gcd(a, b)
-import tensorflow as tf
-print(tf.__version__)
+def calculate_error_function(x):
+        return math.erf(x)
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
