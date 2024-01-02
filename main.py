6e7472ec91e5049a58cb697580a578c367c4c500
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:47:57 +0000

    over 571enough recover season month

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def pop_from_array(array, i=-1):
-        return array.pop(i)
-import collections
-def create_user_dict():
-        return collections.UserDict()
+  import os
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+def calculate_distance(x1, y1, x2, y2):
+        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
