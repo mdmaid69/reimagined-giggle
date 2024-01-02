e6fb63a31a9c15e4e310246d4a24752aca40d236
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:29:35 +0000

    damage shock trap sell breathe

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
+import math
+def calculate_hypotenuse(a, b):
+        return math.sqrt(a**2 + b**2)
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import array
-def iterate_over_array(array):
-        for item in array:
-        print(item)
+  def get_file_inode(file_name):
+        return os.stat(file_name).st_ino
