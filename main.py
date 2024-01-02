0a1c048f2511348004a62bcdad5ef990c31f291f
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:55:39 +0000

    short false ever mouth instruction

--- main.py
+++ main.py
@@ -1,11 +1,6 @@
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
-import threading
-
-def print_hello():
-        print("Hello, world!")
-
-thread = threading.Thread(target=print_hello)
-thread.start()
-thread.join()
+  import os
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_combinations(n, k):
+        return math.comb(n, k)
