3424b2189698862172a54431057e018a468683ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:30:57 +0000

    hard regular spend stranger wipe

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import array
-def convert_array_to_string(array):
-        return array.tostring()
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+import csv
+def read_csv_file(filename):
+        with open(filename, "r") as f:
+        reader = csv.reader(f)
+        return list(reader)
