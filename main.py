65721fbe003d3befa87382fb45993d317e07f6c9
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:07:37 +0000

    nearly nowhere 572enter smile nowhere

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import re
-def replace_all_occurrences(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
+import array
+def get_array_as_float(array):
+        return float(array[0])
