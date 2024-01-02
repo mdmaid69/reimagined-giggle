830b72d69cb020c413bf8338528c10ba4ec2e019
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:06 +0000

    warning instrument attempt make arrangement

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+  import numpy as np
+  def calculate_variance(arr):
+        return np.var(arr)
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def subtract_numbers(x, y):
-        return x - y
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
