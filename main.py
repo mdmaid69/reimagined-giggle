ce7fb11da9e15c7b49580e430730ac2d28b56ac0
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:42:58 +0000

    score difference ill help trial

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_name_without_extension(file_name):
-        return os.path.splitext(file_name)[0]
+import math
+def calculate_factorial(n):
+        return math.factorial(n)
+for i in range(5):
+        print(i)
