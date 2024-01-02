6dc01a036bb6e6c95e892e2eb5caf11674573de7
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:12:54 +0000

    will back central in throw

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-import sys
-print(sys.version)
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+def calculate_payback_period(cash_flows):
+        cumulative_cash_flow = 0
+        for i, cf in enumerate(cash_flows):
+        cumulative_cash_flow += cf
+        if cumulative_cash_flow >= 0:
+                return i
+        return None
+import array
+def get_list_from_array(array):
+        return array.tolist()
