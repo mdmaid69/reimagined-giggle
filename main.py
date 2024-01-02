c53e9c29398a8c0a9dbaddf7b83c7783911e6120
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:54:15 +0000

    secret fortunate most deliver decision

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import tensorflow as tf
-print(tf.__version__)
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
+  def reverse_list(lst):
+        return lst[::-1]
+import array
+def get_array_as_bool(array):
+        return bool(array)
