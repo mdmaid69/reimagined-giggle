8a2c3361046f166bf52d35b8d3a832ddd655cefa
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:50:31 +0000

    later business side round clean

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_area_triangle(b, h):
-        return 0.5 * b * h
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import array
+def insert_into_array(array, i, item):
+        array.insert(i, item)
