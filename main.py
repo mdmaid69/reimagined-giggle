c03f2eca0002708030b64a48bad85ceaefdfff21
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:33:50 +0000

    ours blame limit spoil any

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def split_path(path):
-        return os.path.split(path)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import array
+def get_bytes_from_array(array):
+        return array.tobytes()
