e278007e56963bef4f7e27a97ff14e60c2672edd
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:45:37 +0000

    short move concern turn pity

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
   def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  def convert_to_octal(n):
-        return oct(n)
+        return os.path.getsize(file_name)
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
