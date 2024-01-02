7c842c889fec8492f9344745c2f9c48024c68e2c
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:57:22 +0000

    concern car considering release look

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_as_float(array):
-        return float(array[0])
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+  import os
+  def get_file_size(file_name):
+        return os.path.getsize(file_name)
