463835ce2db5192040cc307d1a13bf904baffd09
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:37:58 +0000

    spill home soft sentence increase

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-  def add_numbers(x, y):
-        return x + y
-n = 10
-print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
+import array
+def get_array_as_float(array):
+        return float(array[0])
+def divide_numbers(x, y):
+        return x / y
