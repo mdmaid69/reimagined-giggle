048195440c252576ec92f23b80a98ff8239c7752
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:46:41 +0000

    murder speed mind introduction work

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import re
-def split_string(pattern, string):
-        return re.split(pattern, string)
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
