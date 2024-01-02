aafddbdd91caff2f41a4f5cc3fca7ffae9b638a8
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:15:47 +0000

    argument life far black boring

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import collections
-def create_priority_queue():
-        return collections.deque()
-  import os
-  def get_file_birthtime(file_name):
-        return os.stat(file_name).st_birthtime
+def factorial(n):
+        if n == 0:
+        return 1
+        else:
+        return n * factorial(n-1)
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
