1d2a3456b82d71e713f58372fd200d24e4578544
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:48:04 +0000

    funny passenger organization vote decision

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+def calculate_acceleration(speed, time):
+        return speed / time
