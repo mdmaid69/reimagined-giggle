cefc1c4730fdbb5f58e35512c7082a470ae06be4
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:55:45 +0000

    speak colour arm spare sister

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-  import os
-  def delete_file(file_name):
-        os.remove(file_name)
-  import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
+import unittest
+
+class TestStringMethods(unittest.TestCase):
+        def test_upper(self):
+        self.assertEqual("foo".upper(), "FOO")
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
