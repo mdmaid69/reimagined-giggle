19007c6826c80d324ac48dd88d7e51fb47dc9d45
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:27:38 +0000

    put cause return future image

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import shutil
-def move_file(src, dst):
-        shutil.move(src, dst)
-  def multiply_numbers(x, y):
-        return x * y
+import random
+def generate_random_number(start, end):
+        return random.randint(start, end)
+  import os
+  def get_file_blocks_allocated(file_name):
+        return os.stat(file_name).st_blocks
