bc1f45a8ed6da43e8a315f364a96a5be44aa2e12
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:19:37 +0000

    manage matter judge nowhere change

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-numbers = [1, 2, 3, 4, 5]
-print("Average:", sum(numbers) / len(numbers))
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
+x = 10
+y = 20
+print("Sum:", x + y)
+  import os
+  def get_file_blocks(file_name):
+        return os.stat(file_name).st_blocks
