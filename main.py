ae454365b8c675132bd00a5441ac0dc635a0b0de
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:27:36 +0000

    will uniform themselves report fence

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-def count_words(sentence):
-        return len(sentence.split())
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+def calculate_future_value(principal, rate, time):
+        return principal * (1 + rate)**time
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
