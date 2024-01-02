39ae78ae356ee7fb5a43d363cc39f0c2e6d15fa4
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:08:11 +0000

    so straight mad Qq complete

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-def square_number(x):
-        return x**2
-import http.client
-conn = http.client.HTTPSConnection("www.python.org")
-conn.request("GET", "/")
-r1 = conn.getresponse()
-print(r1.status, r1.reason)
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
+import heapq
+def create_heap(iterable):
+        h = list(iterable)
+        heapq.heapify(h)
+        return h
