0ceb2d9b1c79234628cc87e0a39e2406a7b88692
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:16:16 +0000

    hollow form produce could alone

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Prime numbers:", [x for x in range(2, n) if all(x % i != 0 for i in range(2, int(x**0.5) + 1))])
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+import array
+def get_array_as_bool(array):
+        return bool(array)
+def calculate_area_circle(r):
+        return 3.14 * r**2
