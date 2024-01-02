b7b32e2937a87d037fbfcaae199d1b8baa106cdf
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:40:06 +0000

    odd past release question address

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_annuity(payment, rate, time):
-        return payment * ((1 - (1 + rate)**-time) / rate)
-def count_elements(lst):
-        return len(lst)
+import array
+def write_array_to_file(filename, array):
+        with open(filename, "wb") as f:
+        array.tofile(f)
+text = "Hello, world!"
+print("Uppercase:", text.upper())
