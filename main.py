6bd2e3d1027827b1e7197cc251241c2b29e0a67d
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:24:13 +0000

    recommend sure calloff after lock

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-n = 10
-print("Cube numbers:", [x**3 for x in range(n)])
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
 def calculate_pressure(force, area):
         return force / area
