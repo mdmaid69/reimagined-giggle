7c53cf0e2935711164583516a48aedbac13a09fb
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:09:45 +0000

    tell direct stay quarter exaggerate

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_payback_period(cash_flows):
-        cumulative_cash_flow = 0
-        for i, cf in enumerate(cash_flows):
-        cumulative_cash_flow += cf
-        if cumulative_cash_flow >= 0:
-                return i
-        return None
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
