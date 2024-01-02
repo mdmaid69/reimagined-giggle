072e7feace823007de9496ef15da9c65270985e6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:45:17 +0000

    before future within rule board

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-  import time
-  def wait_for_seconds(seconds):
-        time.sleep(seconds)
-import array
-def get_array_as_bytes(array):
-        return bytes(array)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+  import csv
+  def read_csv_file(file_name):
+        with open(file_name, "r") as file:
+          return list(csv.reader(file))
