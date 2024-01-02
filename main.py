1c4f681c56e3db39d788f0b0a354d827a82b02c5
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:57:22 +0000

    achieve lose clean pattern region

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_atime_ns(file_name):
-        return os.stat(file_name).st_atime_ns
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import glob
+def find_files(pattern):
+        return glob.glob(pattern)
