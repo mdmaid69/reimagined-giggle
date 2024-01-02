23eff8f5946348a95516482dfd1271129ba997be
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:41:52 +0000

    project show round dear report

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
-def calculate_perimeter_triangle(a, b, c):
-        return a + b + c
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+  import os
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
