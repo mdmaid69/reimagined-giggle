1b273c8b7fece7e3c1faac8f5bf2421567c84b80
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:54:36 +0000

    mixture weigh hate mother callup

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-def factorial(n):
-        if n == 0:
-        return 1
-        else:
-        return n * factorial(n-1)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+import multiprocessing
+def get_cpu_count():
+        return multiprocessing.cpu_count()
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
