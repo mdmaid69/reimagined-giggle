50ed7cb0ec758b8a6ab4ed76ae0434100ab9d326
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:50:47 +0000

    country preparation do sign seat

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-def calculate_interest(principal, rate, time):
-        return principal * (1 + rate)**time
+def calculate_area_rectangle(l, w):
+        return l * w
+  def cube_number(x):
+        return x**3
