ab13412db36e184864c2310fa6a089a592f0fd36
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:41:29 +0000

    blind care contrast tool finish

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def find_max(lst):
-        return max(lst) if len(lst) != 0 else "List is empty"
-import re
-def replace_pattern(pattern, replacement, string):
-        return re.sub(pattern, replacement, string)
+  import os
+  def get_file_permissions(file_name):
+        return os.stat(file_name).st_mode
+import math
+def calculate_bessel_function_of_first_kind(n, x):
+        return math.jn(n, x)
