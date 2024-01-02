1b675464612ebc988e398ba1a0a11186b66d6935
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:48:03 +0000

    bet habit getback wash awake

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-from collections import Counter
-print(Counter("hello world"))
+  import os
+  def delete_directory(dir_name):
+        os.rmdir(dir_name)
+def calculate_debt_ratio(total_debt, total_assets):
+        return total_debt / total_assets
