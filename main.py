a3678e28c6de7a0300f9f8634f7431bd10f6bc22
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:05:51 +0000

    settle report personal further introduce

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
+  def get_file_blksize(file_name):
+        return os.stat(file_name).st_blksize
+import math
+def calculate_absolute_value(x):
+        return math.fabs(x)
