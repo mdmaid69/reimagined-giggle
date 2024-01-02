870b442e18b00d7b74468ed563a552f14c9b8eec
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:42:58 +0000

    order avoid before clever news

--- main.py
+++ main.py
@@ -1,11 +1,7 @@
-import array
-def get_array_from_file(filename, typecode):
-        a = array.array(typecode)
-        with open(filename, "rb") as f:
-        a.fromfile(f, os.path.getsize(filename) // a.itemsize)
-        return a
-import heapq
-def create_heap(iterable):
-        h = list(iterable)
-        heapq.heapify(h)
-        return h
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+def calculate_profit_margin(revenue, cost):
+        return (revenue - cost) / revenue
