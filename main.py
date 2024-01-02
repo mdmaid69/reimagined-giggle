384ad4b650f82b1dc7d89d4d4f290580df53e1f6
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:39:19 +0000

    yet dear 570enjoyable warn responsibility

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import sqlite3
-conn = sqlite3.connect(":memory:")
-c = conn.cursor()
-c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
-  import re
-  def replace_all_occurrences(pattern, replace_with, string):
-        return re.sub(pattern, replace_with, string)
+  import os
+  def get_parent_directory(dir_name):
+        return os.path.dirname(dir_name)
+def calculate_pressure(force, area):
+        return force / area
