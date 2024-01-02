d08827c26352765f4c212534331970de946c876e
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:28:40 +0000

    swallow common relief valuable your

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-n = 10
-print("Fibonacci:", [((1 + 5**0.5) / 2)**n / 5**0.5 for n in range(n)])
+import math
+def calculate_error_function(x):
+        return math.erf(x)
   import numpy as np
-  def calculate_variance(arr):
-        return np.var(arr)
+  def calculate_standard_deviation(arr):
+        return np.std(arr)
