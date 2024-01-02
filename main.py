ec0e6bb38e0fd1b4b66d02570ece003822e4fca2
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:54:19 +0000

    though mark their attract prefer

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
-import json
-print(json.dumps({"name": "John", "age": 30}))
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import logging
+logging.basicConfig(level=logging.INFO)
+logging.info("This is an info message")
