b876f771895ccefbff093aa57d8e123abb7cda60
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:24:57 +0000

    apart piece bend foreign worth

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import array
-def convert_array_to_bytes(array):
-        return array.tobytes()
-  import os
-  def get_file_gid(file_name):
-        return os.stat(file_name).st_gid
+import smtplib
+def send_email(smtp_server, port, username, password, from_addr, to_addr, subject, body):
+        with smtplib.SMTP(smtp_server, port) as server:
+        server.login(username, password)
+        server.sendmail(from_addr, to_addr, f"Subject: {subject}
+
+{body}")
+import os
+def get_environment_variable(var):
+        return os.getenv(var)
