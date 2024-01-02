e250fd6fc0f93f996041b1763747f4a4efaf9ef0
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:44:20 +0000

    afterwards remark take moment completely

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import sys
-def print_python_version():
-        return sys.version
-import time
-print(time.time())
+  import os
+  def get_file_rdev(file_name):
+        return os.stat(file_name).st_rdev
+import array
+def append_to_array(array, item):
+        array.append(item)
