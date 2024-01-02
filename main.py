f4954bfb03be05e4e5a4ab6441654bcd5e037128
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:03:31 +0000

    slice frequent us recently dream

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+n = 10
+print("Factorial numbers:", [1 if x == 0 else x * factorial(x - 1) for x in range(n)])
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_roi(gain, cost):
-        return (gain - cost) / cost
+  def set_environment_variable(var_name, value):
+        os.environ[var_name] = value
