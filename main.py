a2b4ad31c1a54d7ab822004972c42cfaabf611a1
mdmaid69 <robot@alexsyw.me>
2024-01-02 14:57:34 +0000

    sky on all bye culture

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-name = "Python"
-print("Hello,", name)
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import math
+def calculate_euclidean_distance(p, q):
+        return math.dist(p, q)
+def calculate_present_value(future_value, rate, time):
+        return future_value / (1 + rate)**time
