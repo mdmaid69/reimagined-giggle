ce8c5c9d567500296bd5d9e47ff37179b3861e87
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:10:37 +0000

    across order after realize block

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def set_environment_variable(var_name, value):
-        os.environ[var_name] = value
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Difference:", set(list1) - set(list2))
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
+import array
+def set_array_item(array, i, item):
+        array[i] = item
