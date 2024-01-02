084b48430ba87e29eb135239b2ae1958c27ce9e0
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:17:03 +0000

    custom extremely angry share in

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+import array
+def get_array_as_int(array):
+        return int(array[0])
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+        return "Hello, World!"
