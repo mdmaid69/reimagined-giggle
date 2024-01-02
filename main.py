b7eb1dc475a7ab6c1c1c0a11033e48530cdf83ab
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:08 +0000

    pencil innocent practical warning grateful

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
   import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
-def calculate_perpetuity(payment, rate):
-        return payment / rate
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
