85e12d5d459c3ed84bea2c5b54f4b51e1511a27b
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:22:44 +0000

    best tradition clue bang from

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-  def reverse_list(lst):
-        return lst[::-1]
-  import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
+import functools
+def memoize(func):
+        cache = {}
+        @functools.wraps(func)
+        def wrapper(*args):
+        if args not in cache:
+                cache[args] = func(*args)
+        return cache[args]
+        return wrapper
+text = "Hello, world!"
+print("Characters:", len(text))
