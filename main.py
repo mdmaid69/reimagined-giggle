f2868d49ecb90cebce636165bdb3d2af4d0cca93
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:29:57 +0000

    meeting statement quality healthy error

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_bool(array):
-        return bool(array)
-  import json
-  def convert_json_to_dict(json_str):
-        return json.loads(json_str)
+def get_array_length(array):
+        return len(array)
+  import os
+  def get_file_number_of_links(file_name):
+        return os.stat(file_name).st_nlink
