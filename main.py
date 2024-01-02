e45ff5129fe6dfc08c79e344bf166fd08a66fe61
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:15:11 +0000

    slice explain weak lookafter keepout

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+text = "Hello, world!"
+print("Uppercase:", text.upper())
