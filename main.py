b83206a02eee4ec13736ca787b65d2611b005495
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:42:25 +0000

    male person contact stick straight

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import array
-def get_array_index(array, item):
-        return array.index(item)
+text = "Hello, world!"
+print("Words:", len(text.split()))
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
