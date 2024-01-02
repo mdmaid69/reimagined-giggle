7b9d36f56979d544542724abd9bf5f2c9208cdb7
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:28:13 +0000

    home occur whether software solve

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-print(datetime.datetime.now())
   import os
-  def check_if_file_exists(file_name):
-        return os.path.isfile(file_name)
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
