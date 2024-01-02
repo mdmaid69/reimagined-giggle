214b7ee093e42d86e32af91e3eb58e2f800a261f
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:02:47 +0000

    throughout thinkover responsibility future develop

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
-numbers = [1, 2, 3, 4, 5]
-print("Squared:", [n**2 for n in numbers])
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+import array
+def remove_from_array(array, item):
+        array.remove(item)
