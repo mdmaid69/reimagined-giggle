4d7a22702fb83798cd2266302ba21ec10f7fbe99
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:04:20 +0000

    hospital high exact picture acceptable

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-list1 = [1, 2, 3]
-list2 = [2, 3, 4]
-print("Union:", set(list1) | set(list2))
-import math
-def calculate_inverse_hyperbolic_cosine(x):
-        return math.acosh(x)
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def is_palindrome(s):
+        return s == s[::-1]
