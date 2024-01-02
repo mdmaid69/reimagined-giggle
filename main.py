ea0d58e0c54033a87ba3ea7168e5748b7a7f2b5f
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:26:17 +0000

    meanwhile turnon person public late

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
+def calculate_perpetuity(payment, rate):
+        return payment / rate
   import os
-  def change_file_permissions(file_name, mode):
-        os.chmod(file_name, mode)
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
