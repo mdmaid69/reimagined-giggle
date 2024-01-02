323115c6a160c16e16493b8d81ba4c0da94255c6
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:43:30 +0000

    turninto lot full perhaps else

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-def calculate_pe_ratio(price_per_share, eps):
-        return price_per_share / eps
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+import os
+def create_directory(path):
+        os.makedirs(path, exist_ok=True)
+import math
+def calculate_error_function(x):
+        return math.erf(x)
