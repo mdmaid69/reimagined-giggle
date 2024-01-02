1ebbd0160c7012f8913e6ca156498f8ab44d9c12
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:16:02 +0000

    education quickly opportunity answer money

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-def calculate_speed(distance, time):
-        return distance / time
+list1 = [1, 2, 3]
+list2 = [2, 3, 4]
+print("Difference:", set(list1) - set(list2))
+import sys
+def add_to_python_path(path):
+        sys.path.append(path)
