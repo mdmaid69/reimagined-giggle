a02c1e1b07d3079fb1ec32f209a5a0e818213e4b
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:02:35 +0000

    bang judge on ground direct

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def list_files_in_directory(dir_name):
-        return os.listdir(dir_name)
-  def calculate_perimeter_rectangle(l, w):
-        return 2 * (l + w)
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
