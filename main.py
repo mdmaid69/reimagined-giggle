5a68d88e172434f6093320e77eab8a2f257b2dcb
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:13:09 +0000

    platform repair race among flow

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
-import math
-def calculate_cone_volume(radius, height):
-        return 1/3 * math.pi * radius**2 * height
+text = "Hello, world!"
+print("Is palindrome:", text == text[::-1])
+def find_union(list1, list2):
+        return set(list1) | set(list2)
