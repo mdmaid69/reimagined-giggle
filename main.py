0ca0a0ca255fe312f34e31cc4fc44555bbc9f0f3
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:43:59 +0000

    model correction stupid capital general

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import array
-def get_array_buffer_info(array):
-        return array.buffer_info()
-import sys
-print(sys.version)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
