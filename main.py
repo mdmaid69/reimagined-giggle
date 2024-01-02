ac8574e24043815230f231ee95adda234f978c8c
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:16:23 +0000

    while precise impossible convincing 571enough

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
-def calculate_mortgage(principal, rate, time):
-        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
+  import matplotlib.pyplot as plt
+  def plot_pie_chart(labels, sizes):
+        plt.pie(sizes, labels=labels)
+        plt.show()
+def reverse_list(lst):
+        return lst[::-1]
