d056e86731d8c6119327c523ca9767683a25dbf6
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:18:03 +0000

    period meet bringforward through period

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+def calculate_area_triangle(b, h):
+        return 0.5 * b * h
 import math
-def calculate_remainder(x, y):
-        return math.remainder(x, y)
+def calculate_floor(x):
+        return math.floor(x)
