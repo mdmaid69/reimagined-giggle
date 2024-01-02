0b399b3802ac0bb89bb2eda7fcd2a71a3e948a62
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:52:54 +0000

    back cough plenty typical first

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
