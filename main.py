a166049768ff153a108ef7da6978d67d0bf6e0ce
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:56:40 +0000

    range behind person cover equal

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import functools
+print(functools.reduce(lambda x, y: x+y, [1, 2, 3, 4, 5]))
   import os
-  def get_file_lspare(file_name):
-        return os.stat(file_name).st_lspare
-def convert_to_octal(n):
-        return oct(n)
+  def get_file_size(file_name):
+        return os.stat(file_name).st_size
