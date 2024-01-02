cac8d968fd98bf99dc8fefe7f5d529347f1e810f
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:13:56 +0000

    flavour sound whisper owe quantity

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import array
-def get_array_as_float(array):
-        return float(array[0])
-import collections
-def create_named_tuple(name, fields):
-        return collections.namedtuple(name, fields)
+def create_array(typecode, initializer):
+        return array.array(typecode, initializer)
+  import os
+  def get_file_uid(file_name):
+        return os.stat(file_name).st_uid
