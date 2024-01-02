66b6aa253a89b5a7a6158cbba667d5221943d682
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:12:08 +0000

    design drink goback(to) cook fall

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
-  def reverse_list(lst):
-        return lst[::-1]
-import platform
-def get_os_info():
-        return platform.uname()
+n = 10
+print("Powers of 2:", [2**x for x in range(n)])
+def fibonacci(n):
+        a, b = 0, 1
+        while a < n:
+        print(a, end=" ")
+        a, b = b, a+b
