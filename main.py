195c431257b8c0beeca364c479ef77727b5ab15a
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:08:12 +0000

    pupil back offence small blue

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
-import itertools
-def get_permutations(iterable):
-        return list(itertools.permutations(iterable))
+import array
+def check_if_array_contains_item(array, item):
+        return item in array
+import sys
+def exit_program():
+        sys.exit()
