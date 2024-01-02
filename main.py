49debd8b290d8fbd83da205d9fe33645942ae824
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:32:45 +0000

    scale content taste either getback

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
   def get_file_blksize(file_name):
         return os.stat(file_name).st_blksize
-  import os
-  def get_file_mode(file_name):
-        return os.stat(file_name).st_mode
+i = 0
+while i < 5:
+        print(i)
+        i += 1
