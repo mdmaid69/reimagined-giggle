bb9da11c209a87f9a44d106406321017441b9419
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:48:47 +0000

    education agree perform discover roof

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
+import os
+def get_file_size(filename):
+        return os.path.getsize(filename)
+def calculate_perpetuity(payment, rate):
+        return payment / rate
