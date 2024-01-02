3be641695db596231075b628ebf3b71c94e225b7
mdmaid69 <robot@alexsyw.me>
2024-01-02 21:29:01 +0000

    disapprove set character back rain

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import socket
-
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect(("www.python.org", 80))
-def is_palindrome(s):
-        return s == s[::-1]
+import datetime
+def get_days_until_next_year():
+        next_year = datetime.date.today().year + 1
+        next_new_year = datetime.date(next_year, 1, 1)
+        return (next_new_year - datetime.date.today()).days
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
