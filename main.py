e5bee7b528c8ea97ed0539c91bf39794fefaf41a
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:41:03 +0000

    fortunately invite confuse use education

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-import array
-def get_array_as_format(array, format_spec):
-        return format(array, format_spec)
-import datetime
-def get_current_date():
-        return datetime.date.today()
+  def sort_list(lst):
+        return sorted(lst)
+  import os
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
