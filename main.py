07e1cae88ae756fc46c387e6db7e049cf1a1c3d8
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:41:25 +0000

    which squeeze cheap cross breakinto

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 def calculate_speed(distance, time):
         return distance / time
-import os
-def get_environment_variable(var):
-        return os.getenv(var)
+import socket
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect(("www.python.org", 80))
