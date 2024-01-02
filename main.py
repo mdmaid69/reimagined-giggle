15a1eb34c7f4665fff7a7d6d53621bea675f303b
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:01:18 +0000

    area lend warm blue when

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
-import random
-def generate_random_number(start, end):
+  import random
+  def generate_random_number(start, end):
         return random.randint(start, end)
-def calculate_present_value(future_value, rate, time):
-        return future_value / (1 + rate)**time
+import csv
+with open("some.csv", "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerow(["spam", "eggs", "bacon", "ham"])
+        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
