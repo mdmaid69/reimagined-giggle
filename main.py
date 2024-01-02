3d5b7a884a45afa2c0e76f19abf0fb171ec2e955
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:15:23 +0000

    she better sensitive meanwhile wander

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-def calculate_acceleration(speed, time):
-        return speed / time
+def calculate_work(force, distance):
+        return force * distance
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_ino(file_name):
+        return os.stat(file_name).st_ino
