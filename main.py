0e952137354672be0fd65728ff0cb42c2c6b740c
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:24:11 +0000

    solve negative sit pure cause

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
   import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-print(sum(range(10)))
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
