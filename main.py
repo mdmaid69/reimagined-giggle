8cf621898efc9e5b17d351fe11bd8aa6c448dd55
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:45:47 +0000

    treatment transport week deep content

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-import math
-def calculate_hyperbolic_tangent(x):
-        return math.tanh(x)
+  def get_file_birthtime_ns(file_name):
+        return os.stat(file_name).st_birthtime_ns
+import array
+def get_array_from_bytes(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
