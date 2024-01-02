99669a3949caf5a1b6b1ba31b444b8e37afd7d85
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:21:10 +0000

    fit lock comeround church march

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
-import sys
-def add_to_python_path(path):
-        sys.path.append(path)
+import array
+def get_array_as_bytes(array):
+        return bytes(array)
+import array
+def convert_bytes_to_array(bytes, typecode):
+        a = array.array(typecode)
+        a.frombytes(bytes)
+        return a
