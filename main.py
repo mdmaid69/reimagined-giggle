df4032a543e8a4ded381de32dc3866272d9a4bb8
mdmaid69 <robot@alexsyw.me>
2024-01-02 22:39:08 +0000

    continue smell love process lookaround

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import array
+def extend_array(array, iterable):
+        array.extend(iterable)
   import os
-  def get_file_blocks(file_name):
-        return os.stat(file_name).st_blocks
-import json
-def convert_to_json(data):
-        return json.dumps(data)
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
