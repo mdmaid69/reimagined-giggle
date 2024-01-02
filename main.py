7238399bc7484822a50c04f77ad85ceb43692670
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:13:14 +0000

    health degree environment another Ss

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_creation_time(file_name):
-        return os.path.getctime(file_name)
-  import os
-  def get_file_group(file_name):
-        return os.stat(file_name).st_gid
+  def check_if_file_exists(file_name):
+        return os.path.isfile(file_name)
+  def find_min(lst):
+        return min(lst) if len(lst) != 0 else "List is empty"
