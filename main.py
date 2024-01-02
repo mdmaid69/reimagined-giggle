ec35bc4bd066b24b04591001b379351a4e566217
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:06:40 +0000

    band object brown study lower

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import heapq
-def merge_sorted_iterables(*iterables):
-        return heapq.merge(*iterables)
-import unittest
-
-class TestStringMethods(unittest.TestCase):
-        def test_upper(self):
-        self.assertEqual("foo".upper(), "FOO")
+def convert_to_binary(n):
+        return bin(n)
+  import os
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
