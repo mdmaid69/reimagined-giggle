b1b80fe2b197064dfeb283cad2c8430ccaf7dd1c
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:05:48 +0000

    uponon circle strike their main

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
-def calculate_distance(x1, y1, x2, y2):
-        return ((x2 - x1)**2 + (y2 - y1)**2)**0.5
-import queue
-
-q = queue.Queue()
-
-for i in range(5):
-        q.put(i)
-
-while not q.empty():
-        print(q.get())
+import math
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
+def is_odd(n):
+        return n % 2 != 0
