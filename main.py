2b1305ad670d8ed8cb80c9a58d3ef650993e270c
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:23 +0000

    turndown person behave chin avoid

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-def calculate_average(numbers):
-        return sum(numbers) / len(numbers)
-def cube_number(x):
-        return x**3
+  import os
+  def get_file_gen(file_name):
+        return os.stat(file_name).st_gen
+import csv
+def save_csv(data, filename):
+        with open(filename, "w", newline="") as f:
+        writer = csv.writer(f)
+        writer.writerows(data)
