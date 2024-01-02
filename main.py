6e2a52f14e575ec5481409399b7fc8dd3e4b3079
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:30:44 +0000

    objection guess maintain coffee temperature

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
   import os
-  def get_file_size(file_name):
-        return os.stat(file_name).st_size
-import pandas as pd
-print(pd.DataFrame({"A": [1, 2], "B": [3, 4]}))
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import numpy as np
+print(np.array([1, 2, 3]))
