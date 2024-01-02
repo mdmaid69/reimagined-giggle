bcaab6da387071a6b3d73b88f7fd1ed53895ee68
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:08:42 +0000

    repeat another sting fasten still

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-n = 10
-print("Square numbers:", [x**2 for x in range(n)])
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+import array
+def get_array_as_complex(array):
+        return complex(array[0])
+def calculate_mortgage(principal, rate, time):
+        return (principal * rate * (1 + rate)**time) / ((1 + rate)**time - 1)
