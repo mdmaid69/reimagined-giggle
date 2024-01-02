ac892eed89b5e9c2686a7eaeed16a3ded607c944
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:04:21 +0000

    outside off pain need object

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
-import csv
-def write_csv_file(filename, data):
-        with open(filename, "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerows(data)
+  import os
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  import os
+  def get_file_nlink(file_name):
+        return os.stat(file_name).st_nlink
