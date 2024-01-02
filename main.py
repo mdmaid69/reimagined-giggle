4844782829113eb9e2fbfb4c6627efb96420cb40
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:30:53 +0000

    murder loss play nurse contribute

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+import array
+def get_array_as_float(array):
+        return float(array[0])
+import math
+def calculate_complementary_error_function(x):
+        return math.erfc(x)
