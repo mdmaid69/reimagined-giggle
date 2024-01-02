814bcf48d0a2456ba0c2ac026052b4ce755db8ba
mdmaid69 <robot@alexsyw.me>
2024-01-02 19:03:42 +0000

    century child lookafter same impressed

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
-def count_words(sentence):
-        return len(sentence.split())
+import math
+def calculate_ceiling(x):
+        return math.ceil(x)
+import array
+def get_array_as_frozenset(array):
+        return frozenset(array)
