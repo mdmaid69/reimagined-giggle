da218bb9623873298d7d77019ea7fc3cc640a37b
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:35:16 +0000

    you will aim lookafter challenge

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import numpy as np
-  def calculate_median(arr):
-        return np.median(arr)
   import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_environment_variable(var_name):
+        return os.getenv(var_name)
+  def is_odd(n):
+        return n % 2 != 0
