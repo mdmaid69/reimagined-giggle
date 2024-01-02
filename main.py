463851843a9531bd6dff21eda2fc63741d0eaa75
mdmaid69 <robot@alexsyw.me>
2024-01-02 23:04:31 +0000

    genuine image tool supply steal

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-  def square_number(x):
-        return x**2
-import array
-def get_array_typecode(array):
-        return array.typecode
+import collections
+def group_by(iterable, key_func):
+        return collections.defaultdict(list, ((key, list(group)) for key, group in itertools.groupby(sorted(iterable, key=key_func), key_func)))
+import subprocess
+def run_command(cmd):
+        return subprocess.check_output(cmd, shell=True)
