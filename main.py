5ddd3cfdfd50a7848daa9f86a084fd82549b6b8a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:32:27 +0000

    card turndown since exaggerate tragedy

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
   import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-  import os
-  def get_file_mtime(file_name):
-        return os.stat(file_name).st_mtime
+  def get_file_gid(file_name):
+        return os.stat(file_name).st_gid
+import json
+def load_json(filename):
+        with open(filename, "r") as f:
+        return json.load(f)
