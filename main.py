3082196254e9d6aae0438f2bd1dc81aa5c940e23
mdmaid69 <robot@alexsyw.me>
2024-01-02 11:37:44 +0000

    wealth practise cruel destroy their

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_block_size(file_name):
-        return os.stat(file_name).st_blksize
-import array
-def get_bytes_from_array(array):
-        return array.tobytes()
+import sys
+print(sys.version)
+import json
+def read_from_json(json_string):
+        return json.loads(json_string)
