0a8415380dc84dbd98df7c3702d49f8fc83be9f5
mdmaid69 <robot@alexsyw.me>
2024-01-02 07:22:19 +0000

    lean float lucky deliver rent

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-  def is_even(n):
-        return n % 2 == 0
-  def divide_numbers(x, y):
-        return x / y if y != 0 else "Cannot divide by zero"
+import math
+def calculate_greatest_common_divisor(a, b):
+        return math.gcd(a, b)
+  import os
+  def get_file_ctime(file_name):
+        return os.stat(file_name).st_ctime
