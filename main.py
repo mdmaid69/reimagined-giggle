e96a7c3717165a17de100f62d446a13a449d68a4
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:35:18 +0000

    holiday telephone cruelty sit plastic

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-n = 10
-a, b = 0, 1
-while a < n:
-        print(a, end=" ")
-        a, b = b, a+b
-import array
-def clear_array(array):
-        array *= 0
+  import os
+  def get_file_device(file_name):
+        return os.stat(file_name).st_dev
+import http.client
+conn = http.client.HTTPSConnection("www.python.org")
+conn.request("GET", "/")
+r1 = conn.getresponse()
+print(r1.status, r1.reason)
