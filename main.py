24c17acb142983278ee7a4ec985b9196caf9e3ae
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:25:15 +0000

    complete relation where storm contrast

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-  import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
+def find_max(lst):
+        return max(lst)
+import getpass
+def get_password(prompt):
+        return getpass.getpass(prompt)
