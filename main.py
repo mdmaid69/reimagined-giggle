d8d152644c1a1c9656eb943beb8877af14d31320
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:26:47 +0000

    fun hard grow clearly able

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-  import numpy as np
-  def calculate_correlation(arr1, arr2):
-        return np.corrcoef(arr1, arr2)
-def remove_duplicates(lst):
-        return list(set(lst))
+n = 10
+print("Odd numbers:", [x for x in range(n) if x % 2 != 0])
+import math
+def calculate_square_root(x):
+        return math.sqrt(x)
