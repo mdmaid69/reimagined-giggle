bdbdd5f7ef47eaacf41f2a3f3696a7a493303358
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:10:29 +0000

    press bath please command boat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import heapq
-def push_pop_heap(heap, item):
-        return heapq.heappushpop(heap, item)
-import array
-def get_array_as_memoryview(array):
-        return memoryview(array)
+import math
+def calculate_permutations(n, k):
+        return math.perm(n, k)
+import platform
+def get_os_info():
+        return platform.uname()
