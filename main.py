dc56be71f2f2b047aaebbb8b7c6864a92155c8be
mdmaid69 <robot@alexsyw.me>
2024-01-02 15:33:57 +0000

    below advantage lead watch window

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def get_array_index(array, item):
-        return array.index(item)
-import array
-def get_array_slice(array, i, j):
-        return array[i:j]
+  import numpy as np
+  def calculate_correlation(arr1, arr2):
+        return np.corrcoef(arr1, arr2)
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
