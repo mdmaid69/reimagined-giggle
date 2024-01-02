28340c0f5a0072f86ce555cd6ecb39f313e62560
mdmaid69 <robot@alexsyw.me>
2024-01-02 02:32:01 +0000

    willing 566end punish surprising record

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-print(sys.version)
-  import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+def calculate_amortization(principal, rate, time):
+        return (principal * rate) / (1 - (1 + rate)**-time)
+def is_palindrome(s):
+        return s == s[::-1]
