03f4d3a0b28f77a352d9aee35a393c881c22b98a
mdmaid69 <robot@alexsyw.me>
2024-01-02 12:52:45 +0000

    describe earn occasion positive passenger

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-  import os
-  def get_file_gen(file_name):
-        return os.stat(file_name).st_gen
-def is_prime(n):
-        for i in range(2, int(n**0.5) + 1):
-                if n % i == 0:
-                return False
-        return True
+  def square_number(x):
+        return x**2
+x = 10
+y = 20
+print("Sum:", x + y)
