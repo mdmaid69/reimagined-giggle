6031650162868e73436379a2a5638763af486a68
mdmaid69 <robot@alexsyw.me>
2024-01-02 01:56:06 +0000

    discuss bring contribute gather open

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
+import datetime
+def get_current_date():
+        return datetime.date.today()
   import os
-  def get_file_modification_time(file_name):
-        return os.path.getmtime(file_name)
-  import os
-  def get_file_rdev(file_name):
-        return os.stat(file_name).st_rdev
+  def get_file_dev(file_name):
+        return os.stat(file_name).st_dev
