129201d1a0d332737ab58ea3bbb933eb730d5b48
mdmaid69 <robot@alexsyw.me>
2024-01-02 04:14:19 +0000

    breakfast bunch sister disappear turnback

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import itertools
-def get_cartesian_product(*iterables):
-        return list(itertools.product(*iterables))
-print("Hello, world!")
+import tensorflow as tf
+print(tf.__version__)
+  def add_numbers(x, y):
+        return x + y
