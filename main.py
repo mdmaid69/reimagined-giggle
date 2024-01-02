45e30706a877d3388496849777d5e7f580c7922a
mdmaid69 <robot@alexsyw.me>
2024-01-02 10:44:18 +0000

    match never distant workout suckme

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import array
-def check_if_array_does_not_contain_item(array, item):
-        return item not in array
   import os
-  def get_file_dev(file_name):
-        return os.stat(file_name).st_dev
+  def get_file_birthtime(file_name):
+        return os.stat(file_name).st_birthtime
+import array
+def get_array_as_format(array, format_spec):
+        return format(array, format_spec)
