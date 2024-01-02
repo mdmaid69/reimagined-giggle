fa61e671643ab84417ad834099798e88e0176bb9
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:49:15 +0000

    without past especially clearly once

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-  import os
-  def get_file_atime(file_name):
-        return os.stat(file_name).st_atime
-def calculate_amortization(principal, rate, time):
-        return (principal * rate) / (1 - (1 + rate)**-time)
+def calculate_speed(distance, time):
+        return distance / time
+def calculate_pe_ratio(price_per_share, eps):
+        return price_per_share / eps
