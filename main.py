cc8f19b23e5ba350b203905c573e80dcb68c376a
mdmaid69 <robot@alexsyw.me>
2024-01-02 18:16:47 +0000

    prevent cost stand second definite

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-  import requests
-  def get_web_page(url):
-        response = requests.get(url)
-        return response.text if response.status_code == 200 else "Unable to fetch web page"
-import sys
-def exit_program():
-        sys.exit()
+  import os
+  def join_paths(path1, path2):
+        return os.path.join(path1, path2)
+  def calculate_circumference_circle(r):
+        return 2 * 3.14 * r
