8f22c854252239e964232cdf15067eecaacb6699
mdmaid69 <robot@alexsyw.me>
2024-01-02 00:56:38 +0000

    rest read leg peace lunch

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-  import os
-  def get_file_size_in_bytes(file_name):
-        return os.stat(file_name).st_size
-import math
-def calculate_logarithm_base_e(x):
-        return math.log(x)
+import sys
+def exit_program():
+        sys.exit()
+import shutil
+def move_file(src, dst):
+        shutil.move(src, dst)
