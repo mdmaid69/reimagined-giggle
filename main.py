50485def251d56d64302e5c95915c8cf45c34a1a
mdmaid69 <robot@alexsyw.me>
2024-01-02 17:29:25 +0000

    imagination either statement game slide

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-import csv
-with open("some.csv", "w", newline="") as f:
-        writer = csv.writer(f)
-        writer.writerow(["spam", "eggs", "bacon", "ham"])
-        writer.writerow(["Hello, world!", "eggs", "bacon", "ham"])
-def is_palindrome(s):
-        return s == s[::-1]
+import tempfile
+def create_temp_file():
+        return tempfile.NamedTemporaryFile(delete=False)
+def count_words(sentence):
+        return len(sentence.split())
