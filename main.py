8ef39f1a382286de028eaaeea9c56a9739a0f78e
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:22:43 +0000

    film sister slice fortunate company

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def get_array_from_string(string, typecode):
-        a = array.array(typecode)
-        a.fromstring(string)
-        return a
   import os
-  def get_file_ctime(file_name):
-        return os.stat(file_name).st_ctime
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+def fibonacci(n):
+        return n if n <= 1 else fibonacci(n-1) + fibonacci(n-2)
