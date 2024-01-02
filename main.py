91e43bdb7d33aa0ff55becc4d208e95f37074ffa
mdmaid69 <robot@alexsyw.me>
2024-01-02 03:15:29 +0000

    dig mouth fail Tt consider

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-  import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
-for i in range(10): print(i)
+def find_max(lst):
+        return max(lst)
+def calculate_simple_interest(principal, rate, time):
+        return principal * rate * time
