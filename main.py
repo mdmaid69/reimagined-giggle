bcf72bb34d4eb9491bd88338c11840c52e781b05
mdmaid69 <robot@alexsyw.me>
2024-01-02 05:44:11 +0000

    year through suckme swim cum

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+  import os
+  def get_file_mtime(file_name):
+        return os.stat(file_name).st_mtime
 n = 5
 result = 1
 for i in range(1, n + 1):
         result *= i
 print("Factorial:", result)
-import json
-print(json.dumps({"name": "John", "age": 30}))
