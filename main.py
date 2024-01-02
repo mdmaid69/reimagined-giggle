bb3071ea6298d35ea94cf751a6779d7043b6e545
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:43:21 +0000

    value dare part meaning trust

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
+def calculate_volume(length, width, height):
+        return length * width * height
 import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
-  import os
-  def get_file_flags(file_name):
-        return os.stat(file_name).st_flags
+def calculate_product_of_sequence(start, stop, step):
+        return math.prod(range(start, stop, step))
