7f98320be577a3bad21ee15248752231f48c8b73
mdmaid69 <robot@alexsyw.me>
2024-01-02 16:07:51 +0000

    child spring exciting opponent hit

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
   import os
-  def get_file_uid(file_name):
-        return os.stat(file_name).st_uid
-import os
-def create_directory(path):
-        os.makedirs(path, exist_ok=True)
+  def get_file_qspare(file_name):
+        return os.stat(file_name).st_qspare
+def calculate_roi(gain, cost):
+        return (gain - cost) / cost
