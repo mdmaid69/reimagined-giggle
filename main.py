863d49ce6c563a7ecbc28a8448a36628ccee56ee
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:21:45 +0000

    scale lost Gg invitation smile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import random
-def generate_random_number(start, end):
-        return random.randint(start, end)
-import time
-def wait_for_seconds(seconds):
-        time.sleep(seconds)
+  import os
+  def get_file_mode(file_name):
+        return os.stat(file_name).st_mode
+import sqlite3
+conn = sqlite3.connect(":memory:")
+c = conn.cursor()
+c.execute("""CREATE TABLE stocks (date text, trans text, symbol text, qty real, price real)""")
