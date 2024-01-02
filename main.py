06a4b0dbca1c2a3103a8661e0c33ab496a32afd3
mdmaid69 <robot@alexsyw.me>
2024-01-02 06:20:19 +0000

    past close fire work confidence

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-import datetime
-def get_current_date():
-        return datetime.date.today()
-def calculate_area_triangle(b, h):
-        return 0.5 * b * h
+  import os
+  def get_file_flags(file_name):
+        return os.stat(file_name).st_flags
+  import os
+  def create_directory(dir_name):
+        os.makedirs(dir_name, exist_ok=True)
