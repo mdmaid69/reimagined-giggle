c9a2abc38336c3bc287f4fb693215cba55a4227d
mdmaid69 <robot@alexsyw.me>
2024-01-02 08:38:57 +0000

    recovery communication system nothing cheat

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-import getpass
-def get_username():
-        return getpass.getuser()
-  import os
-  def get_file_nlink(file_name):
-        return os.stat(file_name).st_nlink
+  import matplotlib.pyplot as plt
+  def plot_graph(x, y):
+        plt.plot(x, y)
+        plt.show()
+def convert_to_hex(n):
+        return hex(n)
