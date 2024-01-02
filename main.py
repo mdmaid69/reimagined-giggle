e8b1362fd40da7b242ea5e55734d6daf6b96dcaa
mdmaid69 <robot@alexsyw.me>
2024-01-02 09:56:54 +0000

    alone sign level question wild

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-import array
-def convert_unicode_to_array(unicode, typecode):
-        a = array.array(typecode)
-        a.fromunicode(unicode)
-        return a
+import sklearn.datasets
+print(sklearn.datasets.load_iris())
   import os
-  def get_file_blksize(file_name):
-        return os.stat(file_name).st_blksize
+  def get_file_lspare(file_name):
+        return os.stat(file_name).st_lspare
