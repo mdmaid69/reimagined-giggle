0bdd7c2e400f7abe0459a2dcaa0cd5c4d8e83422
mdmaid69 <robot@alexsyw.me>
2024-01-02 13:45:52 +0000

    begin close safe criticism not

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
+import array
+def convert_array_to_bytes(array):
+        return array.tobytes()
 def calculate_average(numbers):
         return sum(numbers) / len(numbers)
-  import os
-  def get_file_qspare(file_name):
-        return os.stat(file_name).st_qspare
