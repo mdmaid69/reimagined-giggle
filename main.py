8bb256ec915f8c6a317f44e0275474245036dd0c
mdmaid69 <robot@alexsyw.me>
2024-01-02 20:58:35 +0000

    steal breakout on charge violent

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
-  import os
-  def get_file_ino(file_name):
-        return os.stat(file_name).st_ino
-import math
-def calculate_root(x, n):
-        return math.pow(x, 1/n)
+  def calculate_factorial(n):
+        return 1 if n == 0 else n * calculate_factorial(n-1)
+numbers = [1, 2, 3, 4, 5]
+print("Sum:", sum(numbers))
